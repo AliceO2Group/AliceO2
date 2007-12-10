1584356e0941b1a7442bf8f8c13e705aabbf7324
alibrary <alibrary@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-12-10 13:44:40 +0000

    One fabs was left...

--- GPU/TPCCAGPUTracking/tracking-ca/AliHLTTPCCADisplay.cxx
+++ GPU/TPCCAGPUTracking/tracking-ca/AliHLTTPCCADisplay.cxx
@@ -383,7 +383,7 @@ void AliHLTTPCCADisplay::DrawTrack( AliHLTTPCCATrack &track, Int_t color )
       
       Double_t xc = t.Par()[0] + qq*t.Par()[4]/pt/bc;
       Double_t yc = t.Par()[1] - qq*t.Par()[3]/pt/bc;
-      Double_t r = TMath::Abs(qq)/fabs(bc);
+      Double_t r = TMath::Abs(qq)/TMath::Abs(bc);
 	
       Double_t vx, vy;
       Sec2View( xc, yc, &vx, &vy );
