9ec7023da483122554ccc0dab975024210b343b6
richterm <richterm@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-10-21 13:21:31 +0000

    correcting doxygen warnings

--- GPU/TPCCAGPUTracking/tracking-ca/AliHLTTPCCATrackerComponent.h
+++ GPU/TPCCAGPUTracking/tracking-ca/AliHLTTPCCATrackerComponent.h
@@ -35,19 +35,19 @@ public:
   // Public functions to implement AliHLTComponent's interface.
   // These functions are required for the registration process
   
-  /** @see component interface @ref AliHLTComponent::GetComponentID */
+  /** @see component interface AliHLTComponent::GetComponentID */
   const char* GetComponentID() ;
   
-  /** @see component interface @ref AliHLTComponent::GetInputDataTypes */
+  /** @see component interface AliHLTComponent::GetInputDataTypes */
   void GetInputDataTypes( vector<AliHLTComponentDataType>& list)  ;
   
-  /** @see component interface @ref AliHLTComponent::GetOutputDataType */
+  /** @see component interface AliHLTComponent::GetOutputDataType */
   AliHLTComponentDataType GetOutputDataType() ;
 
-  /** @see component interface @ref AliHLTComponent::GetOutputDataSize */
+  /** @see component interface AliHLTComponent::GetOutputDataSize */
   virtual void GetOutputDataSize( unsigned long& constBase, double& inputMultiplier ) ;
 
-  /** @see component interface @ref AliHLTComponent::Spawn */
+  /** @see component interface AliHLTComponent::Spawn */
   AliHLTComponent* Spawn() ;
 
 protected:
@@ -56,10 +56,10 @@ protected:
   // These functions provide initialization as well as the actual processing
   // capabilities of the component. 
   
-  /** @see component interface @ref AliHLTComponent::DoInit */
+  /** @see component interface AliHLTComponent::DoInit */
   int DoInit( int argc, const char** argv );
   
-  /** @see component interface @ref AliHLTComponent::DoDeinit */
+  /** @see component interface AliHLTComponent::DoDeinit */
   int DoDeinit();
   
   /** @see component interface @ref AliHLTProcessor::DoEvent */
