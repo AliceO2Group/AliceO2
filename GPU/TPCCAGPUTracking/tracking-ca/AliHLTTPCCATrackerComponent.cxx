3dbe9e91abc90b54f5222d9423da8f2e2fcd2e55
richterm <richterm@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-11-08 13:52:04 +0000

    bugfix: wrong result convention was used in r36516

--- GPU/TPCCAGPUTracking/tracking-ca/AliHLTTPCCATrackerComponent.cxx
+++ GPU/TPCCAGPUTracking/tracking-ca/AliHLTTPCCATrackerComponent.cxx
@@ -305,7 +305,7 @@ int AliHLTTPCCATrackerComponent::Configure( const char* cdbEntry, const char* ch
 
   //* read magnetic field
 
-  int iResult2 = true; //ReadCDBEntry( kAliHLTCDBSolenoidBz, chainId );
+  int iResult2 = 0; //ReadCDBEntry( kAliHLTCDBSolenoidBz, chainId );
   fSolenoidBz = GetBz();
 
   //* read the actual CDB entry if required
