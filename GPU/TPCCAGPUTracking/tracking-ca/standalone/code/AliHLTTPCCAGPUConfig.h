228026a3f5776cc9c598265823c474eec51862fc
drohr <drohr@cern.ch>
2010-08-08 22:34:38 +0000

    fermi optimizations
    git-svn-id: http://qon.jwdt.org/var/svn/catracker@177 97746f1a-7bc6-4ed5-8832-28bd89886fd8

--- GPU/TPCCAGPUTracking/tracking-ca/standalone/code/AliHLTTPCCAGPUConfig.h
+++ GPU/TPCCAGPUTracking/tracking-ca/standalone/code/AliHLTTPCCAGPUConfig.h
@@ -6,13 +6,15 @@
 //#define FERMI
 
 #ifdef FERMI
-#define HLTCA_GPU_BLOCK_COUNT 15
+#define HLTCA_GPU_BLOCK_COUNT 30
+#define HLTCA_GPU_BLOCK_COUNT_SELECTOR 45
 #define HLTCA_GPU_THREAD_COUNT 256
-#define HLTCA_GPU_THREAD_COUNT_CONSTRUCTOR 384
+#define HLTCA_GPU_THREAD_COUNT_CONSTRUCTOR 256
 #define HLTCA_GPU_THREAD_COUNT_SELECTOR 256
 #define HLTCA_GPU_THREAD_COUNT_FINDER 256
 #else
 #define HLTCA_GPU_BLOCK_COUNT 30
+#define HLTCA_GPU_BLOCK_COUNT_SELECTOR 30
 #define HLTCA_GPU_THREAD_COUNT 256
 #define HLTCA_GPU_THREAD_COUNT_CONSTRUCTOR 256
 #define HLTCA_GPU_THREAD_COUNT_SELECTOR 256
@@ -42,11 +44,7 @@
 //#define HLTCA_GPU_TRACKLET_CONSTRUCTOR_DO_PROFILE		//Output Profiling Data for Tracklet Constructor Tracklet Scheduling
 //#define HLTCA_GPU_TIME_PROFILE						//Output Time Profiling Data for asynchronous DMA transfer
 
-#ifdef FERMI
-#define HLTCA_GPU_TRACKLET_SELECTOR_HITS_REG_SIZE 46
-#else
 #define HLTCA_GPU_TRACKLET_SELECTOR_HITS_REG_SIZE 12
-#endif
 #define HLTCA_GPU_TRACKLET_SELECTOR_SLICE_COUNT 3		//Currently must be smaller than avaiable MultiProcessors on GPU or will result in wrong results
 
 #define HLTCA_GPU_SORT_DUMPDATA							//Sort Start Hits etc before dumping to file
