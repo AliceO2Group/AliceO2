032f166d8c11f09e3581a07f74407ecea4a6f726
sgorbuno <sgorbuno@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-05-19 00:18:21 +0000

    data structures for MC labels of TPC clusters are moved from AliHLTTPCClusterFinder class to AliHLTTPCClusterMCData.h

---
+++ GPU/TPCCAGPUTracking/AliHLTTPCClusterMCData.h
@@ -0,0 +1,54 @@
+#ifndef _ALIHLTTPCCLUSTERMCDATA_H_
+#define _ALIHLTTPCCLUSTERMCDATA_H_
+
+//* This file is property of and copyright by the ALICE HLT Project        * 
+//* ALICE Experiment at CERN, All rights reserved.                         *
+//* See cxx source for full Copyright notice                               *
+
+#include "AliHLTDataTypes.h"
+
+
+/**
+ * @struct AliHLTTPCClusterMCWeight
+ * This in a struct for MC weights
+ * @ingroup alihlt_tpc
+ */
+struct AliHLTTPCClusterMCWeight
+{
+  AliHLTInt32_t  fMCID;     // MC track ID
+  AliHLTFloat32_t fWeight; // weight of the track ID
+};  
+
+typedef struct AliHLTTPCClusterMCWeight AliHLTTPCClusterMCWeight;
+
+/**
+ * @struct AliHLTTPCClusterMCLabel
+ * This in a struct for MC labels
+ * @ingroup alihlt_tpc
+ */
+struct AliHLTTPCClusterMCLabel
+{
+  AliHLTTPCClusterMCWeight fClusterID[3]; // three most relevant MC labels
+};
+
+typedef struct AliHLTTPCClusterMCLabel AliHLTTPCClusterMCLabel;
+
+
+/**
+ * @struct AliHLTTPCClusterMCData
+ * This in a container for MC labels
+ * @ingroup alihlt_tpc
+ */
+struct AliHLTTPCClusterMCData 
+{
+  AliHLTUInt32_t fCount;
+#if defined(__HP_aCC) || defined(__DECCXX) || defined(__SUNPRO_CC)
+  AliHLTTPCClusterMCLabel fLabels[1];
+#else
+  AliHLTTPCClusterMCLabel fLabels[];
+#endif
+};
+
+typedef struct AliHLTTPCClusterMCData AliHLTTPCClusterMCData;
+
+#endif
