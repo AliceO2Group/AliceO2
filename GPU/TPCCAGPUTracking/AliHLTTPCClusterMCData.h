69bddabb63273ee270e71d6f036821a8398fd43d
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-08-31 14:20:39 +0000

    Reverting rev. 51375,51372,51371 since they break the compilation

--- GPU/TPCCAGPUTracking/AliHLTTPCClusterMCData.h
+++ GPU/TPCCAGPUTracking/AliHLTTPCClusterMCData.h
@@ -15,10 +15,6 @@
  */
 struct AliHLTTPCClusterMCWeight
 {
-  //* constructor **/
-  AliHLTTPCClusterMCWeight(): fMCID(-1), fWeight(0)
-  {}
-
   AliHLTInt32_t  fMCID;     // MC track ID
   AliHLTFloat32_t fWeight; // weight of the track ID
 };  
