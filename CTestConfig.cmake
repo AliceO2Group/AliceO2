e237b4b6c36426caf0f450609621d4447c317abd
Mohammad Al-Turany <m.al-turany@gsi.de>
2014-10-15 13:40:19 +0200

    configure  CTest for AliceO2 project

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -7,12 +7,12 @@
  ################################################################################
 # Dashboard is opened for submissions for a 24 hour period starting at
 # the specified NIGHLY_START_TIME. Time is specified in 24 hour format.
-set(CTEST_PROJECT_NAME "MyProj")
+set(CTEST_PROJECT_NAME "AliceO2")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 CEST")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "cdash.gsi.de")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=MyProj")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=AliceO2")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
 set(CTEST_TESTING_TIMEOUT 60)
