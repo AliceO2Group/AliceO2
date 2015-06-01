1170830102590335f350adca3dff6de9f3dc1f06
Mohammad Al-Turany <m.al-turany@gsi.de>
2015-06-01 16:40:20 +0200

    CDash The Cdash server at GSI moved to https, change the drop method accordingly

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -10,9 +10,9 @@
 set(CTEST_PROJECT_NAME "AliceO2")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 CEST")
 
-set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_METHOD "https")
 set(CTEST_DROP_SITE "cdash.gsi.de")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=AliceO2")
+set(CTEST_DROP_LOCATION "/submit.php?project=AliceO2")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
 set(CTEST_TESTING_TIMEOUT 60)
