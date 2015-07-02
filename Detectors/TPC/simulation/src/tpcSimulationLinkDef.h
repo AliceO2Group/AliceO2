6e05a51577dae56af9e50668365647c9a76442df
wiechula <Jens.Wiechula@cern.ch>
2015-07-02 15:44:28 +0200

    Implement TPC material, geometry and simple hit creation
    o First working TPC hit generation
    o Still dirty stuff to clean up
    o ATO-157

--- Detectors/TPC/simulation/src/tpcSimulationLinkDef.h
+++ Detectors/TPC/simulation/src/tpcSimulationLinkDef.h
@@ -8,4 +8,21 @@
 #pragma link C++ class AliceO2::TPC::Detector+;
 #pragma link C++ class AliceO2::TPC::Point+;
 
+#pragma link C++ class AliTPCRF1D-;                    // 1D Response Function (used for Time Response Function)
+#pragma link C++ class AliTPCPRF2D-;                   // 2D Pad Response Function
+
+#pragma link C++ class AliH2F+;                        // Additional functionality to 2D Histogram (used in Draw padResponse func)
+                                                       // --- remove it, check miminal code needed for drawing
+#pragma link C++ class AliDetectorParam+;              // Base class for AliTPCParam (before also used for TRD)
+#pragma link C++ class AliTPCParam+;                   // Parameterize the Geometry, Diffusion, ResponseFunction, Default HV, ...
+                                                       // Base class for AliTPCParamSR
+#pragma link C++ class AliTPCParamSR-;                 // SR = Straight Rows
+                                                       // --- In principle only 1 class of (AliDetectorParam, AliTPCParam,
+                                                       //     AliTPCParamSR) is needed - can be merged, but breaks OCDB
+#pragma link C++ class AliTPCROC+;                     // Geometry for 1 ROC (ReadOutChamber) - hardcoded
+                                                       // --- (possible) duplication of AliTPCParam
+
+#pragma link C++ class AliLog+;
+#pragma link C++ class AliMathBase+;
+
 #endif
