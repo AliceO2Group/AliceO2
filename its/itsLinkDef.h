6feca515565a880955b8966c47abbbd0f9363f59
mfasDa <markus.fasel@web.de>
2015-07-21 16:48:35 +0200

    Add digits writeout buffer for time-based simulation, starting from Tutorial 3 (for the moment almost identical)

--- its/itsLinkDef.h
+++ its/itsLinkDef.h
@@ -16,6 +16,7 @@
 #pragma link C++ class AliceO2::ITS::MisalignmentParameter+;
 #pragma link C++ class AliceO2::ITS::Point+;
 #pragma link C++ class AliceO2::ITS::Digit+;
+#pragma link C++ class AliceO2::ITS::DigitWriteoutBuffer+;
 #pragma link C++ class AliceO2::ITS::Digitizer+;
 #pragma link C++ class AliceO2::ITS::DigitizerTask+;
 
