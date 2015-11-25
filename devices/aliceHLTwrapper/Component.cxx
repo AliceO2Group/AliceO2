AliceO2Group/AliceO2

commit a658a3584ce66cfe1e036a59f4e1ba33b03aab51

forwarding event header also if there are no output blocks; adjusting conditions for warning messages

devices/aliceHLTwrapper/Component.cxx
@@ -192,7 +192,7 @@ int Component::process(vector<MessageFormat::BufferDesc_t>& dataArray)
   mFormatHandler.addMessages(dataArray);
   vector<AliHLTComponentBlockData>& inputBlocks = mFormatHandler.getBlockDescriptors();
   unsigned nofInputBlocks = inputBlocks.size();
-  if (dataArray.size() > 0 && nofInputBlocks == 0) {
+  if (dataArray.size() > 0 && nofInputBlocks == 0 && mFormatHandler.getEvtDataList().size() == 0) {
     cerr << "warning: none of " << dataArray.size() << " input buffer(s) recognized as valid input" << endl;
   }
   dataArray.clear();
@@ -262,7 +262,7 @@ int Component::process(vector<MessageFormat::BufferDesc_t>& dataArray)
   } while (iResult == ENOSPC && --nofTrials > 0);
 
   // prepare output
-  if (outputBlockCnt > 0) {
+  if (outputBlockCnt >= 0) {
     AliHLTUInt8_t* pOutputBufferStart = &mOutputBuffer[0];
     AliHLTUInt8_t* pOutputBufferEnd = pOutputBufferStart + mOutputBuffer.size();
     // consistency check for data blocks

devices/aliceHLTwrapper/MessageFormat.cxx
@@ -112,10 +112,11 @@ int MessageFormat::addMessages(const vector<BufferDesc_t>& list)
   int i = 0;
   for (vector<BufferDesc_t>::const_iterator data = list.begin(); data != list.end(); data++, i++) {
     if (data->mSize > 0) {
+      unsigned nofEventHeaders=mListEvtData.size();
       int result = addMessage(data->mP, data->mSize);
       if (result > 0)
         totalCount += result;
-      else if (result == 0) {
+      else if (result == 0 && nofEventHeaders==mListEvtData.size()) {
         cerr << "warning: no valid data blocks in message " << i << endl;
       } else {
 	// severe error in the data
@@ -255,7 +256,7 @@ vector<MessageFormat::BufferDesc_t> MessageFormat::createMessages(const AliHLTCo
         startPosition = position;
       }
     }
-    if (mOutputMode == kOutputModeSequence) {
+    if (mOutputMode == kOutputModeSequence || count==0) {
       // send one single descriptor for all concatenated blocks
       mMessages.push_back(MessageFormat::BufferDesc_t(&mDataBuffer[startPosition], position - startPosition));
     }

commit 6538569ebbec3104bae483f5d05d2e204a9e728d

Fix for GH issue #90
Removing  tautological comparison which was introduced in
commit a658a3584ce66cfe1e036a59f4e1ba33b03aab51
Author: Matthias Richter <Matthias.Richter@scieq.net>
Date:   Thu Mar 19 21:55:30 2015 +0100

    forwarding event header also if there are no output blocks; adjusting conditions for warning messages

devices/aliceHLTwrapper/Component.cxx
@@ -264,7 +264,7 @@ int Component::process(vector<MessageFormat::BufferDesc_t>& dataArray,
   } while (iResult == ENOSPC && --nofTrials > 0);
 
   // prepare output
-  if (outputBlockCnt >= 0) {
+  { // keep this after removing condition to preserve formatting
     AliHLTUInt8_t* pOutputBufferStart = &mOutputBuffer[0];
     AliHLTUInt8_t* pOutputBufferEnd = pOutputBufferStart + mOutputBuffer.size();
     // consistency check for data blocks