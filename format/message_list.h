54d57492f50f99ef39e3f7b62ee4e000d184b7de
Matthias Richter <Matthias.Richter@scieq.net>
2016-04-06 13:47:49 +0200

    Introducing conditional search in the list iterator

--- format/message_list.h
+++ format/message_list.h
@@ -23,7 +23,7 @@
 #include <cstddef>
 #include <vector>
 #include <cstring> // memset
-#include "memory-format.h"
+#include <functional> // std::function
 
 namespace AliceO2 {
 namespace Format {
@@ -35,13 +35,20 @@ class messageList {
  public:
   typedef MsgT message_type;
   typedef HdrT header_type;
+  /// comparison metric for selection of elements
+  /// an external function can be defined by the caller of begin() to
+  /// apply a selection of elements
+  typedef std::function<bool(const HdrT& hdr)> HdrComparison;
 
   messageList() {}
   messageList(const messageList& other); // not yet implemented
   messageList& operator=(const messageList& other); // not yet implemented
   ~messageList() {}
 
-  /** add data block to list */
+  /// add data block to list
+  /// both header and payload message parts are required to add an entry
+  /// the actual header of type HdrT is extracted from the header
+  /// message part.
   int add(MsgT& headerMsg, MsgT& payloadMsg) {
     // conversion relies on the conversion operator for complex types
     const uint8_t* headerData = headerMsg;
@@ -88,14 +95,40 @@ class messageList {
   //  return (first->mHeader != second->mHeader) || (first->mPayload != second->mPayload);
   //}
 
+  /**
+   * @class iterator
+   * Implementation of navigation through the list and access to elements.
+   *
+   * An optional comparison metric @ref HdrComparison can be used to provide
+   * a selection of elements.
+   */
   class iterator {
    public:
     typedef iterator self_type;
     typedef MsgT value_type;
-  
-    iterator(const pairIt_t& dataIterator) : mDataIterator(dataIterator) { }
+
+    iterator(const pairIt_t& dataIterator, const pairIt_t& iteratorRange,
+	     const HdrComparison hdrsel = HdrComparison())
+      : mDataIterator(dataIterator)
+      , mEnd(iteratorRange)
+      , mHdrSelection(hdrsel)
+    { }
+    iterator(const pairIt_t& dataIterator)
+      : mDataIterator(dataIterator)
+      , mEnd(dataIterator)
+      , mHdrSelection(HdrComparison())
+    { }
     // prefix increment
-    self_type operator++() { ++mDataIterator; return *this; }
+    self_type& operator++() {
+      while (++mDataIterator != mEnd) {
+	// operator bool() of std::function is used to determine whether
+	// a selector is set or not, the default is not callable.
+	// if the std::function container has an assigned target, this is
+	// called with the header as parameter
+	if (!mHdrSelection || mHdrSelection(mDataIterator->mHeader)) break;
+      }
+      return *this;
+    }
     // postfix increment
     self_type operator++(int unused) {self_type copy(*this); ++*this; return copy;}
     // TODO: given the fact that the data which is hold is a pointer, it always needs to be
@@ -103,15 +136,11 @@ class messageList {
     MsgT& operator*() { return *((*mDataIterator).mPayload);}
     MsgT* operator->() { return (*mDataIterator).mPayload;}
 
+    /** return header at iterator position */
     HdrT& getHdr() const {return (*mDataIterator).mHeader;}
 
     bool operator==(const self_type& other) { return mDataIterator == other.mDataIterator; }
     bool operator!=(const self_type& other) { return mDataIterator != other.mDataIterator; }
-
-    /** TODO: comparison object to be used
-    bool operator==(const PayloadMetaData_t& md) { return (*mDataIterator).mHeader == md; }
-    bool operator!=(const PayloadMetaData_t& md) { return (*mDataIterator).mHeader != md; }
-    */
 
     /** conversion operator to PayloadMetaData_t struct */
     operator HdrT() {return (*mDataIterator).mHeader;}
@@ -120,6 +149,8 @@ class messageList {
 
   private:
     pairIt_t mDataIterator;
+    pairIt_t mEnd;
+    HdrComparison mHdrSelection;
   };
 
   /** to be defined
@@ -128,14 +159,19 @@ class messageList {
   };
   */
 
-  iterator begin() {
-    pairIt_t it = mDataArray.begin();
-    return iterator(it);
+  iterator begin(const HdrComparison hdrsel = HdrComparison()) {
+    iterator ret(mDataArray.begin(), mDataArray.end(), hdrsel);
+    // if the std::function container has an assigned target, this is
+    // is used used to check whether the iterator matches the selection
+    // further checks are in the increment operator.
+    // the iterator class implements a type cast operator which allows
+    // to use it directly in the HdrComparison
+    if (hdrsel && !hdrsel(ret)) ++ret;
+    return ret;
   }
 
   iterator end() {
-    pairIt_t it = mDataArray.end();
-    return iterator(it);
+    return iterator(mDataArray.end());
   }
 
  private:
