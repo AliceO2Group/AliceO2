4c89633bb9a277364d5b35fada9bbb43d9b4a214
wiechula <Jens.Wiechula@cern.ch>
2016-07-05 17:37:05 +0200

    sort alphabetical, add missing class

--- Detectors/TPC/base/src/tpcBaseLinkDef.h
+++ Detectors/TPC/base/src/tpcBaseLinkDef.h
@@ -8,16 +8,18 @@
 #pragma link C++ class AliceO2::TPC::GlobalPosition3D;
 #pragma link C++ class AliceO2::TPC::LocalPosition2D;
 #pragma link C++ class AliceO2::TPC::LocalPosition3D;
-#pragma link C++ class AliceO2::TPC::PadCentre;
+
 #pragma link C++ class AliceO2::TPC::ContainerFactory;
+#pragma link C++ class AliceO2::TPC::CRU;
+#pragma link C++ class AliceO2::TPC::DigitPos;
 #pragma link C++ class AliceO2::TPC::FECInfo;
 #pragma link C++ class AliceO2::TPC::Mapper;
+#pragma link C++ class AliceO2::TPC::PadCentre;
 #pragma link C++ class AliceO2::TPC::PadInfo;
 #pragma link C++ class AliceO2::TPC::PadPos;
 #pragma link C++ class AliceO2::TPC::PadRegionInfo;
 #pragma link C++ class AliceO2::TPC::PadSecPos;
 #pragma link C++ class AliceO2::TPC::ROC;
-#pragma link C++ class AliceO2::TPC::DigitPos;
 #pragma link C++ class AliceO2::TPC::Sector;
 
 #endif
