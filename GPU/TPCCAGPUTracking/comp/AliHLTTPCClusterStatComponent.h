181979167936f6255b9c1297b66b25a1db6d06a2
drohr <drohr@cern.ch>
2016-07-06 19:57:11 +0200

    add aggregate option to clusterstat component

--- GPU/TPCCAGPUTracking/comp/AliHLTTPCClusterStatComponent.h
+++ GPU/TPCCAGPUTracking/comp/AliHLTTPCClusterStatComponent.h
@@ -54,6 +54,7 @@ class AliHLTTPCClusterStatComponent : public AliHLTProcessor, public AliOptionPa
   int fPrintClusters; //!
   int fPrintClustersScaled; //!
   int fDumpClusters; //!
+  int fAggregate; //!
   
   FILE* fp;
 
