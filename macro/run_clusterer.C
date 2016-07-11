fa89535b5bbb6986e5c2dac38b5a0893b7c2430e
Peter Christiansen <peter.christiansen@hep.lu.se>
2016-07-11 18:49:28 +0200

    Adding macro for clusterer (testing how to commit).

---
+++ macro/run_clusterer.C
@@ -0,0 +1,65 @@
+void run_clusterer(Int_t nEvents = 10, TString mcEngine = "TGeant3")
+{
+  // Initialize logger
+  FairLogger *logger = FairLogger::GetLogger();
+  logger->SetLogVerbosityLevel("HIGH");
+  logger->SetLogScreenLevel("DEBUG"); 
+  
+  // Input and output file name
+  std::stringstream inputfile, outputfile, paramfile;
+  inputfile << "AliceO2_" << mcEngine << ".digi_" << nEvents << "_event.root";
+  paramfile << "AliceO2_" << mcEngine << ".params_" << nEvents << ".root";
+  outputfile << "AliceO2_" << mcEngine << ".clusters_" << nEvents << "_event.root";
+  
+  // Setup timer
+  TStopwatch timer;
+  
+  // Setup FairRoot analysis manager
+  FairRunAna * fRun = new FairRunAna;
+  fRun->SetInputFile(inputfile.str().c_str());
+  fRun->SetOutputFile(outputfile.str().c_str());
+  
+  // Setup Runtime DB
+  FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
+  FairParRootFileIo* parInput1 = new FairParRootFileIo();
+  parInput1->open(paramfile.str().c_str());
+  rtdb->setFirstInput(parInput1);
+  
+  TGeoManager::Import("geofile_full.root");
+  
+  // Setup clusterer
+  AliceO2::TPC::ClustererTask *clustTPC = new AliceO2::TPC::ClustererTask;
+  fRun->AddTask(clustTPC);
+  
+  fRun->Init();
+  
+  timer.Start();
+  fRun->Run();
+  
+  std::cout << std::endl << std::endl;
+  
+  // Extract the maximal used memory an add is as Dart measurement
+  // This line is filtered by CTest and the value send to CDash
+  FairSystemInfo sysInfo;
+  Float_t maxMemory=sysInfo.GetMaxMemory();
+  std::cout << "<DartMeasurement name=\"MaxMemory\" type=\"numeric/double\">";
+  std::cout << maxMemory;
+  std::cout << "</DartMeasurement>" << std::endl;
+  
+  timer.Stop();
+  Double_t rtime = timer.RealTime();
+  Double_t ctime = timer.CpuTime();
+  
+  Float_t cpuUsage=ctime/rtime;
+  cout << "<DartMeasurement name=\"CpuLoad\" type=\"numeric/double\">";
+  cout << cpuUsage;
+  cout << "</DartMeasurement>" << endl;
+  
+  std::cout << endl << std::endl;
+  std::cout << "Output file is "    << outputfile.str() << std::endl;
+  //std::cout << "Parameter file is " << parFile << std::endl;
+  std::cout << "Real time " << rtime << " s, CPU time " << ctime
+	    << "s" << endl << endl;
+  std::cout << "Macro finished succesfully." << std::endl;
+}
+
