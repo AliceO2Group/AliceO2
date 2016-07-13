a6a04938005c30a4ecc9a72831270607f6d9b186
wiechula <Jens.Wiechula@cern.ch>
2016-07-13 11:48:22 +0200

    remove wrongly added file

--- Common/Header/AliceO2Config.h
+++
@@ -1,2 +0,0 @@
-#define O2PROTO1_MAGF_DIR "/home/amathis/O2Dir/AliceO2/src/Common/Resources/maps/mfchebKGI_sym.root"
-#define O2PROTO1_MAGF_CREATEFIELDMAP_DIR "/home/amathis/O2Dir/AliceO2/src/Common/Resources/maps/mfchebKGI_sym.root"
