bdff992f9806c6b9f9bc3410bfa9bed848715556
Matthias Richter <Matthias.Richter@scieq.net>
2016-09-28 00:00:42 +0200

    Implementing write and read of multiple model configurations.
    Allows the full write (dump) of configurations for a sequence of models.
    Multiple models are separated by blank lines, each models dump is pre-
    ceeded by the model index and the name
    
    Example:
    0 name_model1
    ...<model dump>
    
    1 name_model2
    ...<model dump>
    
    Adding also function to get the coding direction.

--- Utilities/DataCompression/CodingModelDispatcher.h
+++ Utilities/DataCompression/CodingModelDispatcher.h
@@ -21,6 +21,10 @@
 
 #include "mpl_tools.h"
 #include "runtime_container.h"
+#include <iostream>
+#include <fstream>
+#include <boost/type.hpp>
+#include <boost/mpl/for_each.hpp>
 
 namespace ALICE {
 namespace O2 {
@@ -30,6 +34,12 @@ namespace O2 {
  * @brief Runtime dispatcher interface for probability model definitions
  *
  * ModelDefinition single coding model or mpl sequence of models
+ *
+ * TODO:
+ * - consistency check for coding direction, all model definitions have to obey
+ *   same direction
+ * - probably one should also require the same code type for all definitions, at
+ *   least in the codec. Multiple code types do not make much sense in the codec
  */
 template<typename ModelDefinition>
 class CodingModelDispatcher {
@@ -48,6 +58,8 @@ public:
   // of the sequence of data types which define the probability model
   typedef typename create_rtc<definition_type, RuntimeContainer<>>::type container_type;
 
+  typedef typename container_type::wrapped_type::code_type code_type;
+
   /// get the number of models in the definition
   static int getNumberOfModels() {return boost::mpl::size<definition_type>::value;}
 
@@ -56,17 +68,17 @@ public:
 
   /// functor to execute encoding on runtime container level
   template<typename CodeType, typename ValueType>
-  class encode {
+  class encodeFctr {
   public:
-    encode(ValueType _v, CodeType& _code, uint16_t& _codeLength)
+    encodeFctr(ValueType _v, CodeType& _code, uint16_t& _codeLength)
       : code(_code), value(_v), codeLength(_codeLength) {}
-    ~encode() {}
+    ~encodeFctr() {}
 
     typedef bool return_type;
 
     template<typename T>
     return_type operator()(T& stage) {
-      (*stage).Encode(value, code, codeLength);
+      code = (*stage).Encode(value, codeLength);
       return true;
     }
 
@@ -78,27 +90,30 @@ public:
 
   /**
    * Encode a value
+   *
+   * Dispatcher increments to the next model definition after decoding if
+   * parameter switchToNextModel is true.
    */
   template<typename CodeType, typename ValueType>
-  bool Encode(ValueType v, CodeType& code, uint16_t& codeLength) {
-    bool result = mContainer.apply(mPosition, encode<CodeType, ValueType>(v, code, codeLength));
-    if (++mPosition >= getNumberOfModels()) mPosition = 0;
+  bool encode(ValueType v, CodeType& code, uint16_t& codeLength, bool switchToNextModel = true) {
+    bool result = mContainer.apply(mPosition, encodeFctr<CodeType, ValueType>(v, code, codeLength));
+    if (switchToNextModel && ++mPosition >= getNumberOfModels()) mPosition = 0;
     return result;
   }
 
   /// Functor to execute decoding on runtime container level
   template<typename CodeType, typename ValueType>
-  class decode {
+  class decodeFctr {
   public:
-    decode(ValueType& _v, CodeType _code, uint16_t& _codeLength)
+    decodeFctr(ValueType& _v, CodeType _code, uint16_t& _codeLength)
       : code(_code), value(_v), codeLength(_codeLength) {}
-    ~decode() {}
+    ~decodeFctr() {}
 
     typedef bool return_type;
 
     template<typename T>
     return_type operator()(T& stage) {
-      (*stage).Decode(value, code, codeLength);
+      value = (*stage).Decode(code, codeLength);
       return true;
     }
 
@@ -110,14 +125,109 @@ public:
 
   /**
    * Decode a code sequence
-   * Code direction is controlled by template parameter orderMSB and can be
-   * either from MSB to LSB or LSB to MSB.
+   * Code direction can be either from MSB to LSB or LSB to MSB, controlled
+   * by template parameter orderMSB of the probability model.
+   *
+   * Dispatcher increments to the next model definition after decoding if
+   * parameter switchToNextModel is true.
    */
-  template<typename ValueType, typename CodeType, bool orderMSB = true>
-  bool Decode(ValueType& v, CodeType code, uint16_t& codeLength) {
-    bool result = mContainer.apply(mPosition, decode<CodeType, ValueType>(v, code, codeLength));
-    if (++mPosition >= getNumberOfModels()) mPosition = 0;
+  template<typename ValueType, typename CodeType>
+  bool decode(ValueType& v, CodeType code, uint16_t& codeLength, bool switchToNextModel = true) {
+    bool result = mContainer.apply(mPosition, decodeFctr<CodeType, ValueType>(v, code, codeLength));
+    if (switchToNextModel && ++mPosition >= getNumberOfModels()) mPosition = 0;
     return result;
+  }
+
+  class getCodingDirectionFctr {
+  public:
+    typedef bool return_type;
+    template<typename T>
+    return_type operator()(T& stage) {
+      return T::wrapped_type::orderMSB;
+    }
+  };
+
+  /**
+   * Get coding direction for model at current position
+   */
+  bool getCodingDirection() {
+    return mContainer.apply(mPosition, getCodingDirectionFctr());
+  }
+
+  /// write functor
+  class writeFctr {
+  public:
+    writeFctr(std::ostream& out, container_type& container) : mOut(out), mContainer(container) {}
+    ~writeFctr() {}
+
+    typedef std::ostream& return_type;
+
+    template<typename T>
+    return_type operator()(boost::type<T>) {
+      T& stage = static_cast<T&>(mContainer);
+      if (T::level::value > 0) mOut << std::endl; // blank line between dumps
+      mOut << T::level::value << " " << (*stage).getName() << std::endl;
+      (*stage).write(mOut);
+      return mOut;
+    }
+
+  private:
+    std::ostream& mOut;
+    container_type& mContainer;
+  };
+
+  /**
+   * Write configuration
+   *
+   * TODO: introduce a general storage policy, a text file is used for now
+   */
+  int write(const char* filename = nullptr) {
+    std::ofstream ofile(filename);
+    boost::mpl::for_each<typename container_type::types , boost::type<boost::mpl::_> >(writeFctr(ofile.good()?ofile:std::cout, mContainer));
+    ofile.close();
+    return 0;
+  }
+
+  /// read functor
+  class readFctr {
+  public:
+    readFctr(std::istream& in, container_type& container) : mIn(in), mContainer(container) {}
+    ~readFctr() {}
+
+    typedef bool return_type;
+
+    template<typename T>
+    return_type operator()(boost::type<T>) {
+      T& stage = static_cast<T&>(mContainer);
+      std::string level, name, remaining;
+      mIn >> level; mIn >> name;
+      if (!mIn) return false;
+      if (std::stoi(level) != T::level::value ||
+	  name.compare((*stage).getName())) {
+	std::cerr << "Format error: expecting level '" << T::level::value << "' and name '" << (*stage).getName() << "', got: " << level << " " << name << std::endl;
+      }
+      std::cout << "reading configuration for model " << name << std::endl;
+      std::getline(mIn, remaining); // flush the current line
+      (*stage).read(mIn);
+      return true;
+    }
+
+  private:
+    std::istream& mIn;
+    container_type& mContainer;
+  };
+
+  /**
+   * Read configuration
+   *
+   * TODO: introduce a general storage policy, a text file is used for now
+   */
+  int read(const char* filename) {
+    std::ifstream input(filename);
+    if (!input.good()) return -1;
+    // TODO: probably need mpl fold here to propagate the return value
+    boost::mpl::for_each<typename container_type::types , boost::type<boost::mpl::_> >(readFctr(input, mContainer));
+    return 0;
   }
 
 private:
