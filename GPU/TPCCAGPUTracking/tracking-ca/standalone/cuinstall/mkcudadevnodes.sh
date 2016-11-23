d1cd71079234e5a99ab3478182923edcb02e7745
drohr <drohr@cern.ch>
2016-11-23 18:36:29 +0100

    remove obsolete files

--- GPU/TPCCAGPUTracking/tracking-ca/standalone/cuinstall/mkcudadevnodes.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-for i in `seq 0 \`lspci | grep -i nvidia | grep "VGA\|3D" | wc -l\``; do
-mknod -m 666 /dev/nvidia$i c 195 $i
-chown root:video /dev/nvidia$i
-done
-/bin/mknod -m 666 /dev/nvidiactl c 195 255
