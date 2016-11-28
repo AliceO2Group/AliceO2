035255364dcc10047e85a92dcaf921f81f1f4fd6
marten <ole.schmidt@cern.ch>
2016-11-28 15:56:03 +0100

    Rename AliHLTTRDtrack to AliHLTTRDTrack after cleanup for consistency

--- GPU/TPCCAGPUTracking/TRDTracking/AliHLTTRDTrack.h
+++ GPU/TPCCAGPUTracking/TRDTracking/AliHLTTRDTrack.h
@@ -1,5 +1,5 @@
-#ifndef ALIHLTTRDTRACK1_H
-#define ALIHLTTRDTRACK1_H
+#ifndef ALIHLTTRDTRACK_H
+#define ALIHLTTRDTRACK_H
 /* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. *
  * See cxx source for full Copyright notice                               */
 
@@ -10,15 +10,15 @@ class AliESDtrack;
 class AliHLTTRDTrackDataRecord;
 
 //_____________________________________________________________________________
-class AliHLTTRDtrack : public AliKalmanTrack
+class AliHLTTRDTrack : public AliKalmanTrack
 {
  public:
 
-  AliHLTTRDtrack();
-  AliHLTTRDtrack(AliESDtrack& t,Bool_t c=kFALSE) throw (const Char_t *);
-  AliHLTTRDtrack(AliExternalTrackParam& t ) throw (const Char_t *);
-  AliHLTTRDtrack(const AliHLTTRDtrack& t);
-  AliHLTTRDtrack &operator=(const AliHLTTRDtrack& t);
+  AliHLTTRDTrack();
+  AliHLTTRDTrack(AliESDtrack& t,Bool_t c=kFALSE) throw (const Char_t *);
+  AliHLTTRDTrack(AliExternalTrackParam& t ) throw (const Char_t *);
+  AliHLTTRDTrack(const AliHLTTRDTrack& t);
+  AliHLTTRDTrack &operator=(const AliHLTTRDTrack& t);
 
   Int_t GetTPCtrackId() const { return fTPCtrackId; }
   Int_t GetNtracklets() const { return fNtracklets; }
@@ -31,11 +31,11 @@ class AliHLTTRDtrack : public AliKalmanTrack
   using AliExternalTrackParam::GetPredictedChi2;
 
   //methods below need to be implemented from abstract base class
-  Double_t GetPredictedChi2(const AliCluster *c) { Error("AliHLTTRDtrack", "This is a dummy method and should never be used"); return -1.0; }
-  Double_t GetPredictedChi2(const AliCluster *c) const { Error("AliHLTTRDtrack", "This is a dummy method and should never be used"); return -1.0; }
-  Double_t GetPredictedChi2(Double_t cy, Double_t cz, Double_t cerr2Y, Double_t cerr2Z) const { Error("AliHLTTRDtrack", "This is a dummy method and should never be used"); return -1.0; }
-  Bool_t PropagateTo(Double_t xr, Double_t x0, Double_t rho) { Error("AliHLTTRDtrack", "This is a dummy method and should never be used"); return kFALSE; }
-  Bool_t Update(const AliCluster* c, Double_t chi2, Int_t index) { Error("AliHLTTRDtrack", "This is a dummy method and should never be used"); return kFALSE; }
+  Double_t GetPredictedChi2(const AliCluster *c) { Error("AliHLTTRDTrack", "This is a dummy method and should never be used"); return -1.0; }
+  Double_t GetPredictedChi2(const AliCluster *c) const { Error("AliHLTTRDTrack", "This is a dummy method and should never be used"); return -1.0; }
+  Double_t GetPredictedChi2(Double_t cy, Double_t cz, Double_t cerr2Y, Double_t cerr2Z) const { Error("AliHLTTRDTrack", "This is a dummy method and should never be used"); return -1.0; }
+  Bool_t PropagateTo(Double_t xr, Double_t x0, Double_t rho) { Error("AliHLTTRDTrack", "This is a dummy method and should never be used"); return kFALSE; }
+  Bool_t Update(const AliCluster* c, Double_t chi2, Int_t index) { Error("AliHLTTRDTrack", "This is a dummy method and should never be used"); return kFALSE; }
 
   // virtual method of AliKalmanTrack, it is used when the TRD track is saved in AliESDTrack
   Int_t GetTrackletIndex(Int_t iLayer) const { 
@@ -55,7 +55,7 @@ class AliHLTTRDtrack : public AliKalmanTrack
   Int_t fNtracklets;            // number of attached TRD tracklets
   Int_t fAttachedTracklets[6];  // IDs for attached tracklets sorted by layer
 
-  ClassDef(AliHLTTRDtrack,1) //HLT TRD tracker
+  ClassDef(AliHLTTRDTrack,1) //HLT TRD tracker
 };
 
 
