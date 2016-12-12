2960f1dd54e5a1438f1f6ce0076162973734ad3d
mkrzewic <mikolaj.krzewicki@cern.ch>
2016-12-12 13:55:17 +0100

    Change license of new files to O2 GPL

--- Utilities/fairMQmonitor/include/fairMQmonitor/FairMQmonitor.h
+++ Utilities/fairMQmonitor/include/fairMQmonitor/FairMQmonitor.h
@@ -1,16 +1,19 @@
-/********************************************************************************
- *    Copyright (C) 2014 Frankfurt Institute for Advanced Studies               *
- *                                                                              *
- *              This software is distributed under the terms of the             *
- *         GNU Lesser General Public Licence version 3 (LGPL) version 3,        *
- *                  copied verbatim in the file "LICENSE"                       *
- ********************************************************************************/
-/**
- * FairMQmonitor.h
- *
- * @since 2014-12-10
- * @author M. Krzewicki <mkrzewic@cern.ch>
- */
+/// @copyright
+/// © Copyright 2014 Copyright Holders of the ALICE O2 collaboration.
+/// See https://aliceinfo.cern.ch/AliceO2 for details on the Copyright holders.
+/// This software is distributed under the terms of the
+/// GNU General Public License version 3 (GPL Version 3).
+///
+/// License text in a separate file.
+///
+/// In applying this license, CERN does not waive the privileges and immunities
+/// granted to it by virtue of its status as an Intergovernmental Organization
+/// or submit itself to any jurisdiction.
+
+/// @file FairMQmonitor.h
+///
+/// @since 2014-12-10
+/// @author M. Krzewicki <mkrzewic@cern.ch>
 
 #ifndef FAIRMQMONITOR_H_
 #define FAIRMQMONITOR_H_
