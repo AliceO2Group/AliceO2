99d599c1d349f3c175aad312cceee4bd419c3843
a-mathis <andreas.mathis@cern.ch>
2017-01-24 14:27:03 +0100

    Added Vc functionality to electron drift

--- Detectors/TPC/simulation/include/TPCSimulation/ElectronTransport.h
+++ Detectors/TPC/simulation/include/TPCSimulation/ElectronTransport.h
@@ -12,6 +12,11 @@
 
 namespace AliceO2 {
 namespace TPC {
+  
+template <typename T>
+T diffusion(T random, T width, T pos) {
+  return (random*width) + pos;
+}
     
 /// \class ElectronTransport
 /// \brief Class taking care of the transport of electrons in the active volume of the TPC
@@ -31,24 +36,20 @@ class ElectronTransport
     /// @return Array with 3d position of the electrons after the drift taking into account diffusion
     void getElectronDrift(Float_t *posEle);
     
+    /// Drift of electrons in electric field taking into account diffusion using Vc
+    /// @param *xyz Array with 3d position of the electrons
+    /// @return Array with 3d position of the electrons after the drift taking into account diffusion
+    void getElectronDriftVc(Float_t *posEle);
+    
     /// Attachment probability for a given drift time
     /// @param driftTime Drift time of the electron
     /// @return Probability for attachment during the drift
-    Float_t getAttachmentProbability(Float_t driftTime);
+    Float_t getAttachmentProbability(Float_t driftTime) const { return ATTCOEF * OXYCONT * driftTime; }
     
         
   private:
     RandomRing     mRandomGaus;        ///<  Circular random buffer containing random values of the Gauss distribution to take into account diffusion of the electrons
 };
-
-// Inline implementation
-
-inline
-Float_t ElectronTransport::getAttachmentProbability(Float_t driftTime)
-{
-  return ATTCOEF * OXYCONT * driftTime;
-}
-
 }
 }
 
