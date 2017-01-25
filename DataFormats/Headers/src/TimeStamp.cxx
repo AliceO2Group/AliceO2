3f95d80df9b75839af59b14f56cbf62246a6a1d7
Matthias Richter <Matthias.Richter@scieq.net>
2017-01-25 09:57:01 +0100

    Added definition of TimeStamp and LHCClock classes (squashed)
    The TimeStamp class is an interface to a 64bit time stamp field
    consisting of
    - 16 bit unit identifier
    - 16 bit bunch number of sub tick
    - 32 bit orbit counter or tick
    It still needs to be clarified what's the epoch of the clock base.
    Returning a duration in the first version of the get() function, we
    have to see what is practical.
    
    The unit is expressed in termes of a 16 bit Descriptor, currently
    units 'LHCClock' (AC) and microseconds (US) are supported.
    
    The LHCclock class uses fixed integral numbers corresponding to
    accelerator parameters (number of bunches, bunch spacing) to define
    clock period.

---
+++ DataFormats/Headers/src/TimeStamp.cxx
@@ -0,0 +1,13 @@
+/// @file   TimeStamp.cxx
+/// @author Matthias Richter
+/// @since  2017-01-25
+/// @brief  A std chrono implementation of LHC clock and timestamp
+
+#include "Headers/TimeStamp.h"
+
+using namespace AliceO2::Header;
+
+// the only reason for the cxx file is the implementation of the
+// constants
+TimeStamp::TimeUnitID const TimeStamp::sClockLHC("AC");
+TimeStamp::TimeUnitID const TimeStamp::sMicroSeconds("US");
