9a694280d0186f534aa06fc7c576b07ac7808408
Matthias Richter <Matthias.Richter@scieq.net>
2017-02-09 22:38:17 +0100

    Implementation of HeartbeatSampler
    The sampler devices is "self-triggered", it publishes the HeartbeatHeader and
    HeartbeatTrailer wrapped into the HeartbeatFrameEnvelope as additional header
    in the header message. The payload consists of some statistics information.
    
    Options:
    --period <n>  Period in ns (default 89100)
    --output-channel-name <name>  Name of the output channel (default 'output')
    
    In addition to the DataHeader also the HeartbeatFrameEnvelope header
    is send in the header message.

--- Utilities/DataFlow/include/DataFlow/HeartbeatSampler.h
+++ Utilities/DataFlow/include/DataFlow/HeartbeatSampler.h
@@ -3,29 +3,57 @@
 #ifndef HEARTBEATSAMPLER_H
 #define HEARTBEATSAMPLER_H
 
+// @file   HeartbeatSampler.h
+// @author Matthias Richter
+// @since  2017-02-03
+// @brief  Heartbeat sampler device
+
 #include "Headers/DataHeader.h"
 #include "Headers/HeartbeatFrame.h"
 #include "O2Device/O2Device.h"
+#include <string>
 
 namespace AliceO2 {
 namespace DataFlow {
+
+/// @class HeartbeatSampler
+/// @brief A sampler for heartbeat triggers
+///
+/// The device is going to be used in an emulation of the reader
+/// processes on the FLP
+/// The heartbeat triggers are sent out with constant frequency, the
+/// period in nano seconds can be configured by option --period
+///
+/// TODO: the class can evolve to a general clock sampler device with
+/// configurable period, even randomly distributed
 class HeartbeatSampler : public Base::O2Device
 {
 public:
+  typedef AliceO2::Base::O2Message O2Message;
+
+  static constexpr const char* OptionKeyOutputChannelName = "output-channel-name";
+  static constexpr const char* OptionKeyPeriod = "period";
+
   /// Default constructor
   HeartbeatSampler();
 
   /// Default destructor
-  virtual ~HeartbeatSampler();
+  virtual ~HeartbeatSampler() final;
 
 protected:
   /// overloading the InitTask() method of FairMQDevice
-  virtual void InitTask();
+  virtual void InitTask() final;
 
   /// overloading ConditionalRun method of FairMQDevice
-  virtual bool ConditionalRun();
+  virtual bool ConditionalRun() final;
 
 private:
+  /// publishing period (configurable)
+  uint32_t mPeriod;
+  /// name of the (configurable)
+  std::string mOutputChannelName;
+  /// number of elapsed periods
+  int mCount;
 };
 
 }; // namespace DataFlow
--- Utilities/DataFlow/src/runHeartbeatSampler.cxx
+++ Utilities/DataFlow/src/runHeartbeatSampler.cxx
@@ -6,7 +6,12 @@ namespace bpo = boost::program_options;
 void addCustomOptions(bpo::options_description& options)
 {
   options.add_options()
-    ("verbosity",bpo::value<int>()->default_value(0), "verbosity level");
+    (AliceO2::DataFlow::HeartbeatSampler::OptionKeyPeriod,
+     bpo::value<uint32_t>()->default_value(89100),
+     "sampling period")
+    (AliceO2::DataFlow::HeartbeatSampler::OptionKeyOutputChannelName,
+     bpo::value<std::string>()->default_value("output"),
+     "Name of the output channel");
 }
 
 FairMQDevicePtr getDevice(const FairMQProgOptions& /*config*/)
