8ecfc6a6d2e2250a7a52d1962f4e9728a15b9b8c
Matthias Richter <Matthias.Richter@scieq.net>
2017-02-09 22:38:17 +0100

    Implementation of HeartbeatSampler
    The sampler devices is "self-triggered", it publishes the HeartbeatHeader and
    HeartbeatTrailer wrapped into the HeartbeatFrameEnvelope as additional header
    in the header message. The payload consists of some statistics information.
    
    Options:
    --period <n>  Period in ns (default 89100)
    --output-channel-name <name>  Name of the output channel (default 'output')
    
    In addition to the DataHeader also the HeartbeatFrameEnvelope header
    is send in the header message.

--- Utilities/DataFlow/src/HeartbeatSampler.cxx
+++ Utilities/DataFlow/src/HeartbeatSampler.cxx
@@ -1,7 +1,20 @@
+// @file   HeartbeatSampler.h
+// @author Matthias Richter
+// @since  2017-02-03
+// @brief  Heartbeat sampler device
+
+#include <thread> // this_thread::sleep_for
+#include <chrono>
+
 #include "DataFlow/HeartbeatSampler.h"
+#include "Headers/HeartbeatFrame.h"
+#include "FairMQProgOptions.h"
 
 AliceO2::DataFlow::HeartbeatSampler::HeartbeatSampler()
   : O2Device()
+  , mPeriod(89100)
+  , mOutputChannelName("output")
+  , mCount(0)
 {
 }
 
@@ -11,9 +24,38 @@ AliceO2::DataFlow::HeartbeatSampler::~HeartbeatSampler()
 
 void AliceO2::DataFlow::HeartbeatSampler::InitTask()
 {
+  mPeriod = fConfig->GetValue<uint32_t>(OptionKeyPeriod);
+  mOutputChannelName = fConfig->GetValue<std::string>(OptionKeyOutputChannelName);
 }
 
 bool AliceO2::DataFlow::HeartbeatSampler::ConditionalRun()
 {
+  std::this_thread::sleep_for(std::chrono::nanoseconds(mPeriod));
+
+  AliceO2::Header::HeartbeatStatistics hbfPayload;
+
+  AliceO2::Header::DataHeader dh;
+  dh.dataDescription = AliceO2::Header::gDataDescriptionHeartbeatFrame;
+  dh.dataOrigin = AliceO2::Header::DataOrigin("SMPL");
+  dh.subSpecification = 0;
+  dh.payloadSize = sizeof(hbfPayload);
+
+  // Note: the block type of both header an trailer members of the envelope
+  // structure are autmatically initialized to the appropriate block type
+  // and size '1' (i.e. only one 64bit word)
+  AliceO2::Header::HeartbeatFrameEnvelope specificHeader;
+  specificHeader.header.orbit = mCount;
+  specificHeader.trailer.hbAccept = 1;
+
+  O2Message outgoing;
+
+  // build multipart message from header and payload
+  AddMessage(outgoing, {dh, specificHeader}, NewSimpleMessage(hbfPayload));
+
+  // send message
+  Send(outgoing, mOutputChannelName.c_str());
+  outgoing.fParts.clear();
+
+  mCount++;
   return true;
 }
