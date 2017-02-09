c6651239ee7689d47caa12985b691fe4b87e4f7b
Matthias Richter <Matthias.Richter@scieq.net>
2017-02-09 15:08:52 +0100

    Adding simple start script and configuration for the basic setup
    The basic setup contains one subframeBuilder, one flpSender and one
    epnReceiver in the first version. The device configuration is in
    confBasicSetup.json. The file needs to be in the running directory
    or the path in the start script needs to be adjusted.

---
+++ Utilities/DataFlow/run/startBasicSetup.sh
@@ -0,0 +1,11 @@
+# simple start script to lauch a basic setup
+# Prerequisites:
+#  - expects the configuration file to be in the working directory
+#  - O2 bin and lib set n the shell environment
+
+
+xterm -geometry 80x25+0+0 -hold -e epnReceiver --id epnReceiver --mq-config confBasicSetup.json --in-chan-name input --out-chan-name output --num-flps 1 &
+
+xterm -geometry 80x25+0+0 -hold -e flpSender --id flpSender --mq-config confBasicSetup.json --in-chan-name input --out-chan-name output --num-epns 1 &
+
+xterm -geometry 80x25+0+0 -hold -e SubframeBuilderDevice --id subframeBuilder --mq-config confBasicSetup.json &
