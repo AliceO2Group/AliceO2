10c4a0ddbbfd559bb835add3a7a674c78b23f046
drohr <drohr@cern.ch>
2017-02-14 23:07:36 +0100

    add option to disable all new features and revert back to original cluster stat behavior, add edge flag stats, fix totally broken indentation

--- GPU/TPCCAGPUTracking/comp/AliHLTTPCClusterStatComponent.h
+++ GPU/TPCCAGPUTracking/comp/AliHLTTPCClusterStatComponent.h
@@ -78,8 +78,9 @@ class AliHLTTPCClusterStatComponent : public AliHLTProcessor, public AliOptionPa
   AliHLTTPCCAParam* fSliceParam;
   float fPolinomialFieldBz[6];
 
-  int fTotal, fSplitPad, fSplitTime, fSplitPadTime, fSplitPadOrTime, fAssigned; //!
+  int fTotal, fEdge, fSplitPad, fSplitTime, fSplitPadTime, fSplitPadOrTime, fAssigned; //!
 
+  int fCompressionStudy; //!
   int fPrintClusters; //!
   int fPrintClustersScaled; //!
   int fDumpClusters; //!
