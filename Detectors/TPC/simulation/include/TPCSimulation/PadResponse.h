bb26cf394b44e7c076d92fed7105c5e26c6cda76
amathis <andreas.mathis@cern.ch>
2017-03-23 09:43:14 +0100

    First version of the GEM Pad Response Function

--- Detectors/TPC/simulation/include/TPCSimulation/PadResponse.h
+++ Detectors/TPC/simulation/include/TPCSimulation/PadResponse.h
@@ -1,48 +1,42 @@
 /// \file PadResponse.h
-/// \brief Pad Response object
+/// \brief Pad Response class
 /// \author Andi Mathis, andreas.mathis@ph.tum.de
 #ifndef ALICEO2_TPC_PadResponse_H_
 #define ALICEO2_TPC_PadResponse_H_
 
+#include "TGraph2D.h"
+
+#include "TPCBase/Mapper.h"
 namespace AliceO2 {
 namespace TPC {
-    
 /// \class PadResponse
 /// \brief Object for the pad hits due to the PRF
-    
+
 class PadResponse {
   public:
-      
     /// Default constructor
     PadResponse();
-      
-    /// Constructor
-    /// @param pad Pad of the signal
-    /// @param row Row of the signal
-    /// @param weight Weight of the signal
-    PadResponse(int pad, int row, float weight);
-      
+
     /// Destructor
     virtual ~PadResponse();
-      
-    /// Get the pad
-    /// @return Pad
-    float getPad() const { return mPad; }
-      
-    /// Get the row
-    /// @return Row
-    float getRow() const { return mRow; }
-      
-    /// Get the weighted signal
-    /// @return Weighted signal
-    float getWeight() const { return mWeight; }
-      
-  private:
-    float             mWeight;  ///< Weight of the signal on the specific pad, wrt to the central pad above which the electron arrives
-    unsigned char     mPad;     ///< Pad with signal, wrt to the central pad above which the electron arrives
-    unsigned char     mRow;     ///< Row with signal, wrt to the central pad above which the electron arrives
-};
 
+    /// Import the PRF from a .dat file to a TGraph2D
+    /// @param file Name of the .dat file
+    /// @param grPRF TGraph2D to which the PRF will be written
+    /// @return Boolean if succesful or not
+    bool importPRF(std::string file, std::unique_ptr<TGraph2D>& grPRF);
+
+    /// Compute the impact of the pad response for electrons arriving at the GEM stack
+    /// @param posEle Position of the electron in real space
+    /// @param digiPadPos Position of the electron in pad space
+    /// @return Normalized pad response
+    float getPadResponse(GlobalPosition3D posEle, DigitPos digiPadPos);
+
+  private:
+    std::unique_ptr<TGraph2D> mIROC;   ///< TGraph2D holding the PRF for the IROC (4x7.5 mm2 pads)
+    std::unique_ptr<TGraph2D> mOROC12; ///< TGraph2D holding the PRF for the OROC1 and OROC2 (6x10 mm2 pads)
+    std::unique_ptr<TGraph2D> mOROC3;  ///< TGraph2D holding the PRF for the OROC3 (6x15 mm2 pads)
+};
 }
 }
 
--- Detectors/TPC/simulation/src/PadResponse.cxx
+++ Detectors/TPC/simulation/src/PadResponse.cxx
@@ -3,18 +3,81 @@
 
 #include "TPCSimulation/PadResponse.h"
 
+#include "TPCBase/Mapper.h"
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "FairLogger.h"
+
 using namespace AliceO2::TPC;
 
 PadResponse::PadResponse()
-  : mWeight()
-  , mPad()
-  , mRow()
+  : mIROC()
+  , mOROC12()
+  , mOROC3()
+{
+  mIROC   = std::unique_ptr<TGraph2D> (new TGraph2D());
+  mOROC12 = std::unique_ptr<TGraph2D> (new TGraph2D());
+  mOROC3  = std::unique_ptr<TGraph2D> (new TGraph2D());
+  
+  importPRF("PRF_IROC.dat", mIROC);
+  importPRF("PRF_OROC1-2.dat", mOROC12);
+  importPRF("PRF_OROC3.dat", mOROC3);
+}
+
+PadResponse::~PadResponse()
 {}
 
-PadResponse::PadResponse(int pad, int row, float weight)
-  : mWeight(weight)
-  , mPad(pad)
-  , mRow(row)
-{}
 
-PadResponse::~PadResponse(){}
+bool PadResponse::importPRF(std::string file, std::unique_ptr<TGraph2D> & grPRF)
+{
+  std::string inputDir;
+  const char* aliceO2env=std::getenv("O2_ROOT");
+  if (aliceO2env) inputDir=aliceO2env;
+  inputDir+="/share/Detectors/TPC/files/";
+
+  float x, y, normalizedPadResponse;
+  int i=0;
+  std::ifstream prfFile(inputDir+file, std::ifstream::in);
+  if(!prfFile) {
+    LOG(FATAL) << "TPC::PadResponse - Input file '" << inputDir+file << "' does not exist! No PRF loaded!" << FairLogger::endl;
+    return false;
+  }
+  for (std::string line; std::getline(prfFile, line); ) {
+    std::istringstream is(line);
+    while(is >> x >> y >> normalizedPadResponse) {
+      grPRF->SetPoint(i++, x, y, normalizedPadResponse);
+    }
+  }
+  return true;
+}
+
+float PadResponse::getPadResponse(GlobalPosition3D posEle, DigitPos digiPadPos)
+{
+  /// @todo The procedure to find the global position of the pad centre should be made easier
+  const Mapper& mapper = Mapper::instance();
+  const PadCentre padCentre = mapper.padCentre(mapper.globalPadNumber(digiPadPos.getPadPos()));
+  const CRU cru(digiPadPos.getCRU());
+  const Sector sector(digiPadPos.getPadSecPos().getSector());
+  const LocalPosition3D padCentreLocal(padCentre.getX(), padCentre.getY(), posEle.getZ());
+  const GlobalPosition3D padCentrePos = mapper.LocalToGlobal(padCentreLocal, sector);
+
+  std::cout << padCentrePos.getX() << " " << posEle.getX() << " " << padCentrePos.getY() << " " << posEle.getY() << "\n";
+
+  const int gemStack = int(cru.gemStack());
+  const float offsetX = std::fabs(posEle.getX() - padCentre.getX())*10.f; // GlobalPosition3D and DigitPos in cm, PRF in mm
+  const float offsetY = std::fabs(posEle.getY() - padCentre.getY())*10.f; // GlobalPosition3D and DigitPos in cm, PRF in mm
+  float normalizedPadResponse = 0;
+  if(gemStack == 0) {
+    normalizedPadResponse = mIROC->Interpolate(offsetX, offsetY);
+  }
+  else if(gemStack == 1 || gemStack == 2) {
+    normalizedPadResponse = mOROC12->Interpolate(offsetX, offsetY);
+  }
+  else {
+    normalizedPadResponse = mOROC3->Interpolate(offsetX, offsetY);
+  }
+  return normalizedPadResponse;
+}
