53b01eb9ea7dd2503ab7a5f5b6c3a823c68488b0
Sandro Wenzel <sandro.wenzel@cern.ch>
2017-04-04 18:51:35 +0200

    Adding hit mother class; move to DataFormats/simulation
     * restructuring O2 hit class; we need to have
       a common base class with an interface as needed by the particle stack
     * move class to DataFormats/simulation to avoid cyclic dependency
     * commit achieves a complete decoupling from FairMC point; now
       all of our detectors have to use the AliceO2::BaseHit and derived classes

--- DataFormats/simulation/include/SimulationDataFormat/BaseHits.h
+++ DataFormats/simulation/include/SimulationDataFormat/BaseHits.h
@@ -3,7 +3,6 @@
 #include "FairMultiLinkedData_Interface.h"
 #include "Math/GenVector/DisplacementVector3D.h"
 #include "Math/GenVector/PositionVector3D.h"
-#include "Math/SVector.h"
 
 template <typename T>
 using Point3D = ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<T>, ROOT::Math::DefaultCoordinateSystemTag>;
@@ -12,37 +11,47 @@ using Vector3D = ROOT::Math::DisplacementVector3D<ROOT::Math::Cartesian3D<T>, RO
 
 namespace AliceO2
 {
-namespace Base
+
+// Mother class of all hit classes for AliceO2
+// just defines what is absolutely necessary to have
+// as common interface
+// at the moment ony GetTrackID() used by Stack.h
+// eventually we could add some interfaces to retrieve
+// the coordinates as floats or something
+class BaseHit : public FairMultiLinkedData_Interface
 {
+ public:
+  BaseHit() = default;
+  BaseHit(int id) : mTrackID{id} {}
+  int GetTrackID() const { return mTrackID; }
+  void SetTrackID(int id) { mTrackID = id; }
+
+ private:
+  int mTrackID; // track_id
+  ClassDefOverride(BaseHit, 1);
+};
+
 // a set of configurable classes to define basic hit types
 // these are meant to be an alternative to FairMCPoint
 // which always includes the momentum and is purely based on double values
 
-// A basic hit class template, encoding cartesian coordinates, energy loss
-// time_of_flight, detectorID and trackID
-
-// we need to derive from FairMultiLinkedData_Interface for the
-// MC truth handling
-
 // T is basic type for position, E is basic type for time and energy loss
 template <typename T, typename E=float>
-class BasicXYZEHit : public FairMultiLinkedData_Interface
+class BasicXYZEHit : public BaseHit
 {
   Point3D<T> mPos; // cartesian position of Hit
   E mTime;         // time of flight
   E mELoss;        // energy loss
-  int mTrackID;    // track_id; this might be part of link?
   short mDetectorID; // the detector/sensor id
 
  public:
   BasicXYZEHit() = default; // for ROOT IO
 
   // constructor
- BasicXYZEHit(T x, T y, T z, E time, E e, int trackid, short did)
-    :  mPos(x, y, z), mTime(time), mELoss(e), mTrackID(trackid), mDetectorID(did)
+  BasicXYZEHit(T x, T y, T z, E time, E e, int trackid, short did)
+    :  mPos(x, y, z), mTime(time), mELoss(e), BaseHit(trackid), mDetectorID(did)
   {
   }
-
 
   // getting the cartesian coordinates
   T GetX() const { return mPos.X(); }
@@ -55,10 +64,8 @@ class BasicXYZEHit : public FairMultiLinkedData_Interface
   E GetTime() const { return mTime; }
   // get detector + track information
   short GetDetectorID() const { return mDetectorID; }
-  int GetTrackID() const { return mTrackID; }
 
   // modifiers
-  void SetTrackID(int id) { mTrackID = id; }
   void SetTime(E time) { mTime = time; }
   void SetEnergyLoss(E eLoss) { mELoss = eLoss; }
   void SetDetectorID(short detID) { mDetectorID = detID; }
@@ -73,10 +80,8 @@ class BasicXYZEHit : public FairMultiLinkedData_Interface
   }
   void SetPos(Point3D<T> const &p) { mPos = p; }
 
-
-  ClassDef(BasicXYZEHit, 1);
+  ClassDefOverride(BasicXYZEHit, 1);
 };
 
-} // end namespace Base
 } // end namespace AliceO2
 #endif
