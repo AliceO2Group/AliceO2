d746661e40b326f6b07a6fe98e48849e7dbdd580
drohr <drohr@cern.ch>
2017-04-05 10:27:18 +0200

    Add option to write out tracks in binary format

---
+++ GPU/TPCCAGPUTracking/tracking-ca/standalone/interface/outputtrack.h
@@ -0,0 +1,17 @@
+#ifndef OUTPUTTRACK_H
+#define OUTPUTTRACK_H
+
+struct OutputTrack
+{
+	float Alpha;
+	float X;
+	float Y;
+	float Z;
+	float SinPhi;
+	float DzDs;
+	float QPt;
+	int NClusters;
+	int FitOK;
+};
+
+#endif
