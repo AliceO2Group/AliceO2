AliceO2Group/AliceO2

commit ab80994bbbeb6dd31d24b2034f5d276cfce159d5

Catch the case with Modular build

cmake/modules/FindFairMQ.cmake
@@ -6,13 +6,29 @@
 # also have to be available in the required (minimal) version.
 #
 
+if(FairRoot_DIR)
+  set(FAIRROOTPATH ${FairRoot_DIR})
+else()
+  set(FAIRROOTPATH $ENV{FAIRROOTPATH})
+endif(FairRoot_DIR)
+
+if(FAIRROOTPATH)
+  if(NOT FairMQ_FIND_QUIETLY)
+  MESSAGE(STATUS "FairRoot ... - found ${FAIRROOTPATH}")
+  endif(NOT FairMQ_FIND_QUIETLY)
+else()
+  if(NOT FairMQ_FIND_QUIETLY)
+  MESSAGE(FATAL_ERROR "FairRoot installation not found")
+  endif(NOT FairMQ_FIND_QUIETLY)
+endif(FAIRROOTPATH)
+
 set(FAIRMQ_REQUIRED_HEADERS FairMQDevice.h)
 if(NOT FairMQ_FIND_QUIETLY)
   message(STATUS "Looking for FairMQ functionality in FairRoot ...")
 endif(NOT FairMQ_FIND_QUIETLY)
 
 find_path(FAIRMQ_INCLUDE_DIR NAMES ${FAIRMQ_REQUIRED_HEADERS}
-  PATHS ${FairRoot_DIR}/include
+  PATHS ${FAIRROOTPATH}/include
   NO_DEFAULT_PATH
 )
 
@@ -27,7 +43,7 @@ if(FAIRMQ_INCLUDE_DIR)
   set(FAIRMQ_FOUND TRUE)
 else(FAIRMQ_INCLUDE_DIR)
   if(FairMQ_FIND_REQUIRED)
-    message(FATAL_ERROR "FairRoot is not built with FairMQ support")
+    message(STATUS "FairRoot is not built with FairMQ support")
   else(FairMQ_FIND_REQUIRED)
     if(NOT FairMQ_FIND_QUIETLY)
       message(STATUS "Looking for FairMQ functionality in FairRoot: no")

CMakeLists.txt
@@ -56,7 +56,8 @@ else(NOT ALICEO2_MODULAR_BUILD)
 endif(NOT ALICEO2_MODULAR_BUILD)
 
 find_package(AliRoot)
-find_package(FairRoot)
+find_package(FairRoot REQUIRED)
+find_package(FairMQ REQUIRED)
 
 # Load some basic macros which are needed later on
 include(FairMacros)
@@ -108,7 +109,6 @@ CHECK_EXTERNAL_PACKAGE_INSTALL_DIR()
 # mandatory
 
 find_package(ROOT 5.32.00 REQUIRED)
-find_package(FairMQ REQUIRED)
 find_package(Pythia8)
 find_package(Pythia6)
 if(ALICEO2_MODULAR_BUILD)

commit 0c1e8893183c86ec4bd815d40a120142b69ad484

Fix #345: FATAL_ERROR when not found and REQUIRED flag set
Bug was introduced in commit ab80994

cmake/modules/FindFairMQ.cmake
@@ -43,7 +43,7 @@ if(FAIRMQ_INCLUDE_DIR)
   set(FAIRMQ_FOUND TRUE)
 else(FAIRMQ_INCLUDE_DIR)
   if(FairMQ_FIND_REQUIRED)
-    message(STATUS "FairRoot is not built with FairMQ support")
+    message(FATAL_ERROR "FairRoot is not built with FairMQ support")
   else(FairMQ_FIND_REQUIRED)
     if(NOT FairMQ_FIND_QUIETLY)
       message(STATUS "Looking for FairMQ functionality in FairRoot: no")