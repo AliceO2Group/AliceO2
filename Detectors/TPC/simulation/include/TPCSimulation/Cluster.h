c3226c9c37c0df7ba64101b8056e1369cb762ea2
Sebastian Klewin <sebastian.klewin@cern.ch>
2017-05-04 15:35:31 +0200

    time mean/sigma stored as fairTimeStamp/Error + constructors moved inline

--- Detectors/TPC/simulation/include/TPCSimulation/Cluster.h
+++ Detectors/TPC/simulation/include/TPCSimulation/Cluster.h
@@ -11,82 +11,130 @@
 namespace boost { namespace serialization { class access; } }
 
 namespace o2{
-  namespace TPC{
+namespace TPC{
+  
+/// \class Cluster
+/// \brief Cluster class for the TPC
+///
+class Cluster : public FairTimeStamp {
+  public:
     
-    /// \class Cluster
-    /// \brief Cluster class for the TPC
-    ///
-    class Cluster : public FairTimeStamp {
-    public:
-      
-      /// Default constructor
-      Cluster();
-      
-      /// Constructor, initializing all values
-      /// \param cru CRU
-      /// \param row Row
-      /// \param q Total charge of cluster
-      /// \param qmax Maximum charge in a single cell (pad, time)
-      /// \param padmean Mean position of cluster in pad direction
-      /// \param padsigma Sigma of cluster in pad direction
-      /// \param timemean Mean position of cluster in time direction
-      /// \param timesigma Sigma of cluster in time direction
-      Cluster(short cru, short row, float q, float qmax, 
-	      float padmean, float padsigma, float timemean, float timesigma);
-      
-      /// Destructor
-      ~Cluster() override = default;
+    /// Default constructor
+    Cluster();
+    
+    /// Constructor, initializing all values
+    /// \param cru CRU
+    /// \param row Row
+    /// \param q Total charge of cluster
+    /// \param qmax Maximum charge in a single cell (pad, time)
+    /// \param padmean Mean position of cluster in pad direction
+    /// \param padsigma Sigma of cluster in pad direction
+    /// \param timemean Mean position of cluster in time direction
+    /// \param timesigma Sigma of cluster in time direction
+    Cluster(short cru, short row, float q, float qmax, 
+        float padmean, float padsigma, float timemean, float timesigma);
+    
+    /// Destructor
+    ~Cluster() override = default;
 
-      /// Copy Constructor
-      Cluster(const Cluster& other);
+    /// Copy Constructor
+    Cluster(const Cluster& other);
 
-      /// Set parameters of cluster
-      /// \param cru CRU
-      /// \param row Row
-      /// \param q Total charge of cluster
-      /// \param qmax Maximum charge in a single cell (pad, time)
-      /// \param padmean Mean position of cluster in pad direction
-      /// \param padsigma Sigma of cluster in pad direction
-      /// \param timemean Mean position of cluster in time direction
-      /// \param timesigma Sigma of cluster in time direction
-      void setParameters(short cru, short row, float q, float qmax, 
-	      float padmean, float padsigma, float timemean, float timesigma);
-            
-      int getCRU() const { return mCRU; }
-      int getRow() const { return mRow; }
-      float getQ() const { return mQ; }
-      float getQmax() const { return mQmax; }
-      float getPadMean() const { return mPadMean; }
-      float getTimeMean() const { return mTimeMean; }
-      float getPadSigma() const { return mPadSigma; }
-      float getTimeSigma() const { return mTimeSigma; }
-      
-      /// Print function: Print basic digit information on the  output stream
-      /// \param output Stream to put the digit on
-      /// \return The output stream
-      friend std::ostream& operator<< (std::ostream& out, const Cluster &c) { return c.Print(out); }; 
-//      std::ostream& Print(std::ostream &output) const;
+    /// Set parameters of cluster
+    /// \param cru CRU
+    /// \param row Row
+    /// \param q Total charge of cluster
+    /// \param qmax Maximum charge in a single cell (pad, time)
+    /// \param padmean Mean position of cluster in pad direction
+    /// \param padsigma Sigma of cluster in pad direction
+    /// \param timemean Mean position of cluster in time direction
+    /// \param timesigma Sigma of cluster in time direction
+    void setParameters(short cru, short row, float q, float qmax, 
+        float padmean, float padsigma, float timemean, float timesigma);
+          
+    int getCRU() const { return mCRU; }
+    int getRow() const { return mRow; }
+    float getQ() const { return mQ; }
+    float getQmax() const { return mQmax; }
+    float getPadMean() const { return mPadMean; }
+    float getTimeMean() const { return GetTimeStamp(); }
+    float getPadSigma() const { return mPadSigma; }
+    float getTimeSigma() const { return GetTimeStampError(); }
+    
+    /// Print function: Print basic digit information on the  output stream
+    /// \param output Stream to put the digit on
+    /// \return The output stream
+    friend std::ostream& operator<< (std::ostream& out, const Cluster &c) { return c.Print(out); }; 
 
-    protected:      
-      std::ostream& Print(std::ostream& out) const;
-      
-    private:
+  protected:      
+    std::ostream& Print(std::ostream& out) const;
+    
+  private:
 #ifndef __CINT__
-      friend class boost::serialization::access;
+  friend class boost::serialization::access;
 #endif
 
-      short   mCRU;
-      short   mRow;
-      float   mQ;
-      float   mQmax;
-      float   mPadMean;
-      float   mPadSigma;
-      float   mTimeMean;
-      float   mTimeSigma;
-            
-      ClassDefOverride(Cluster, 1);
-    };
-  }
+    short   mCRU;
+    short   mRow;
+    float   mQ;
+    float   mQmax;
+    float   mPadMean;
+    float   mPadSigma;
+          
+    ClassDefOverride(Cluster, 1);
+};
+//________________________________________________________________________
+inline 
+Cluster::Cluster()
+  : Cluster(-1, -1, -1, -1, -1, -1, -1, -1)
+{
+}
+
+//________________________________________________________________________
+inline
+Cluster::Cluster(short cru, short row, float q, float qmax,
+		 float padmean, float padsigma,
+		 float timemean, float timesigma)
+  : FairTimeStamp(timemean, timesigma)
+  , mCRU(cru)
+  , mRow(row)
+  , mQ(q)
+  , mQmax(qmax)
+  , mPadMean(padmean)
+  , mPadSigma(padsigma)
+{
+}
+
+//________________________________________________________________________
+inline
+Cluster::Cluster(const Cluster& other)
+  : FairTimeStamp(other)
+  , mCRU(other.mCRU)
+  , mRow(other.mRow)
+  , mQ(other.mQ)
+  , mQmax(other.mQmax)
+  , mPadMean(other.mPadMean)
+  , mPadSigma(other.mPadSigma)
+{
+}
+
+//________________________________________________________________________
+inline
+void Cluster::setParameters(short cru, short row, float q, float qmax,
+			    float padmean, float padsigma,
+			    float timemean, float timesigma)
+{
+  mCRU = cru;
+  mRow = row;
+  mQ = q;
+  mQmax = qmax;
+  mPadMean = padmean;
+  mPadSigma = padsigma;
+  SetTimeStamp(timemean);
+  SetTimeStampError(timesigma);
+}
+
+}
 }
 
 #endif /* ALICEO2_TPC_CLUSTER_H */   
