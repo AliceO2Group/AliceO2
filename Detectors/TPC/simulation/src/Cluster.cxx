513205c86966acba4c9ef2d1644503af65b06e41
Sebastian Klewin <sebastian.klewin@cern.ch>
2017-05-04 15:35:31 +0200

    time mean/sigma stored as fairTimeStamp/Error + constructors moved inline

--- Detectors/TPC/simulation/src/Cluster.cxx
+++ Detectors/TPC/simulation/src/Cluster.cxx
@@ -8,65 +8,13 @@ ClassImp(o2::TPC::Cluster)
 using namespace o2::TPC;
 
 //________________________________________________________________________
-Cluster::Cluster()
-  : Cluster(-1, -1, -1, -1, -1, -1, -1, -1)
-{
-}
-
-//________________________________________________________________________
-Cluster::Cluster(short cru, short row, float q, float qmax,
-		 float padmean, float padsigma,
-		 float timemean, float timesigma)
-  : mCRU(cru)
-  , mRow(row)
-  , mQ(q)
-  , mQmax(qmax)
-  , mPadMean(padmean)
-  , mPadSigma(padsigma)
-  , mTimeMean(timemean)
-  , mTimeSigma(timesigma)
-  , FairTimeStamp()
-{
-}
-
-//________________________________________________________________________
-Cluster::Cluster(const Cluster& other)
-  : mCRU(other.mCRU)
-  , mRow(other.mRow)
-  , mQ(other.mQ)
-  , mQmax(other.mQmax)
-  , mPadMean(other.mPadMean)
-  , mPadSigma(other.mPadSigma)
-  , mTimeMean(other.mTimeMean)
-  , mTimeSigma(other.mTimeSigma)
-  , FairTimeStamp(other)
-{
-}
-
-//________________________________________________________________________
-void Cluster::setParameters(short cru, short row, float q, float qmax,
-			    float padmean, float padsigma,
-			    float timemean, float timesigma)
-{
-  mCRU = cru;
-  mRow = row;
-  mQ = q;
-  mQmax = qmax;
-  mPadMean = padmean;
-  mPadSigma = padsigma;
-  mTimeMean = timemean;
-  mTimeSigma = timesigma;
-}
-
-
-//________________________________________________________________________
 std::ostream& Cluster::Print(std::ostream& out) const {
 //std::ostream &Cluster::Print(std::ostream &output) const
 //{
   out << "TPC Cluster in CRU [" << mCRU << "], pad row ["
 	 << mRow << "] with charge/maxCharge " << mQ << "/" << mQmax
-	 << " and coordinates (" << mPadMean << ", " << mTimeMean << ")"
-	 << " and width (" << mPadSigma << ", " << mTimeSigma << ")";
+	 << " and coordinates (" << mPadMean << ", " << GetTimeStamp() << ")"
+	 << " and width (" << mPadSigma << ", " << GetTimeStampError() << ")";
   return out;
 }
 
