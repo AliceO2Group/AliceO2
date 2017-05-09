05a6de6d33026f04e3c6a28e76e96640e7a3d3d5
Marten Ole Schmidt <ole.schmidt@cern.ch>
2017-05-09 14:47:19 +0200

    Bugfix in TRD tracker related to track updates
    - do not square the tracklet error twice when updating the tracks with TRD information

--- GPU/TPCCAGPUTracking/TRDTracking/AliHLTTRDTracker.h
+++ GPU/TPCCAGPUTracking/TRDTracking/AliHLTTRDTracker.h
@@ -28,8 +28,8 @@ public:
 
   // struct to hold the information on the space points
   struct AliHLTTRDSpacePointInternal {
-    double fX[3];
-    double fCov[2];
+    double fX[3]; // 3d position in sector coordinates
+    double fCov[2]; // sigma_y^2, sigma_yz, sigma_z^2
     int fId;
     int fLabel;
     unsigned short fVolumeId;
