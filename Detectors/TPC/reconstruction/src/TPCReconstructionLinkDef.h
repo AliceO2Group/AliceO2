83a032b83c766ee1f3b99ec27f18c460af76ee83
Sebastian Klewin <sebastian.klewin@cern.ch>
2017-05-15 16:21:47 +0200

    new RawReaderArray
    Conflicts:
    	Detectors/TPC/reconstruction/include/TPCReconstruction/RawReader.h

--- Detectors/TPC/reconstruction/src/TPCReconstructionLinkDef.h
+++ Detectors/TPC/reconstruction/src/TPCReconstructionLinkDef.h
@@ -20,6 +20,7 @@
 #pragma link C++ class o2::TPC::GBTFrameContainer;
 #pragma link C++ class o2::TPC::HalfSAMPAData;
 #pragma link C++ class o2::TPC::RawReader;
+#pragma link C++ class o2::TPC::RawReaderArray;
 #pragma link C++ class o2::TPC::SyncPatternMonitor;
 #pragma link C++ class o2::TPC::TrackTPC+;
 
