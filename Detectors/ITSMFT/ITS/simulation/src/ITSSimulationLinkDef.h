a273b573387a61df59c81a9b280109b9300af51f
Mario Sitta <Mario.Sitta@cern.ch>
2017-05-18 12:45:47 +0200

    Porting latest ITSU geometry from AliRoot v2 to O2

--- Detectors/ITSMFT/ITS/simulation/src/ITSSimulationLinkDef.h
+++ Detectors/ITSMFT/ITS/simulation/src/ITSSimulationLinkDef.h
@@ -9,7 +9,6 @@
 #pragma link C++ class o2::ITS::V3Layer+;
 #pragma link C++ class o2::ITS::Detector+;
 #pragma link C++ class o2::ITS::GeometryHandler+;
-#pragma link C++ class o2::ITS::DigitChip+;
 #pragma link C++ class o2::ITS::DigitWriteoutBuffer+;
 #pragma link C++ class o2::ITS::Digitizer+;
 #pragma link C++ class o2::ITS::DigitizerTask+;
