46e647595dce2a9d178ea3906ce05747d2a8b7b7
Giulio Eulisse <giulio.eulisse@cern.ch>
2017-06-16 15:27:53 +0200

    Introduce PayloadMerger helper class
    This is now done by introducing a new generic class PayloadMerger which
    can be used to merge together multiple payloads into a given buffer,
    which can then be sent via O2.
    
    The PayloadMerger class can be configured w.r.t.:
    
    - Given a Payload, determine is the unique identifier for the class
      of equivalence it belongs to.
    - Given an id, determine wether or not the class of equivalence it
      represents is complete.
    - Given a payload, extract the parts you are interested from it.

---
+++ Utilities/DataFlow/include/DataFlow/SubframeUtils.h
@@ -0,0 +1,45 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See https://alice-o2.web.cern.ch/ for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+#ifndef DATAFLOW_SUBFRAMEUTILS_H
+#define DATAFLOW_SUBFRAMEUTILS_H
+
+#include <tuple>
+#include <cstddef>
+#include "Headers/HeartbeatFrame.h"
+
+namespace o2 { namespace dataflow {
+
+int64_t extractDetectorPayloadStrip(char **payload, char *buffer, size_t bufferSize) {
+  *payload = buffer + sizeof(o2::Header::HeartbeatHeader);
+  return bufferSize - sizeof(o2::Header::HeartbeatHeader) - sizeof(o2::Header::HeartbeatTrailer);
+}
+
+
+struct SubframeId {
+  size_t timeframeId;
+  size_t socketId;
+
+  // operator needed for the equal_range algorithm/ multimap method
+  bool operator<(const SubframeId& rhs) const {
+    return std::tie(timeframeId, socketId) < std::tie(rhs.timeframeId, rhs.socketId);
+  }
+};
+
+SubframeId makeIdFromHeartbeatHeader(const Header::HeartbeatHeader &header, size_t socketId, size_t orbitsPerTimeframe) {
+  SubframeId id = {
+    .timeframeId = header.orbit / orbitsPerTimeframe,
+    .socketId = socketId
+  };
+  return id;
+}
+
+} /* namespace dataflow */ } /* namespace o2 */
+
+#endif // DATAFLOW_SUBFRAMEUTILS_H
