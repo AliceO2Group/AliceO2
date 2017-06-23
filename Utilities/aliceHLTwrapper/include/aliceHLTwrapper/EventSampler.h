35f48575a3fa7848e61619e248c2b89ec501767c
Matthias Richter <Matthias.Richter@scieq.net>
2017-06-23 14:03:32 +0200

    Added command line option to specify the OCDB configuration
    - the OCDB URI can now be specified as command line option --ocdb,
      the existence of ALIHLT_HCDBDIR is checked otherwise
    - logging through the FairMQ logging channel
    - minor code cleanup

--- Utilities/aliceHLTwrapper/include/aliceHLTwrapper/EventSampler.h
+++ Utilities/aliceHLTwrapper/include/aliceHLTwrapper/EventSampler.h
@@ -1,3 +1,4 @@
+//-*- Mode: C++ -*-
 // Copyright CERN and copyright holders of ALICE O2. This software is
 // distributed under the terms of the GNU General Public License v3 (GPL
 // Version 3), copied verbatim in the file "COPYING".
@@ -7,8 +8,6 @@
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
-
-//-*- Mode: C++ -*-
 
 #ifndef EVENTSAMPLER_H
 #define EVENTSAMPLER_H
