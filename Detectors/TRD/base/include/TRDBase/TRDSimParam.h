da94ae7c39a9126e51628f0a272564940e8669e6
Sandro Wenzel <sandro.wenzel@cern.ch>
2017-07-06 17:18:33 +0200

    Porting the TRD geometry to O2
    This commit ports (large parts) of the TRD geometry and related services
    to AliceO2
    
    This porting is based on commit 291f4ef608b91d9125f12fb1d8fa8a1af83a5a24 of AliRoot
    
    The code has been adopted to follow AliceO2 coding conventions (especially true for member names)
    and formatting (done by clang-format)
    
    Some code modernization has been performed (stl::containers, inheritance structure, etc)
    
    Some functions not needed for simulations have been commented out
    and need to be revisited later.

---
+++ Detectors/TRD/base/include/TRDBase/TRDSimParam.h
@@ -0,0 +1,132 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See https://alice-o2.web.cern.ch/ for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#ifndef O2_TRDSIMPARAM_H
+#define O2_TRDSIMPARAM_H
+
+#include <Rtypes.h> // for ClassDef
+
+namespace o2
+{
+namespace trd
+{
+////////////////////////////////////////////////////////////////////////////
+//                                                                        //
+// Class containing constant simulation parameters                        //
+//                                                                        //
+////////////////////////////////////////////////////////////////////////////
+class TRDSimParam
+{
+ public:
+  enum { kNplan = 6, kNcham = 5, kNsect = 18, kNdet = 540 };
+
+  static TRDSimParam* Instance();
+  static void Terminate();
+
+  void SetGasGain(float gasgain) { mGasGain = gasgain; }
+  void SetNoise(float noise) { mNoise = noise; }
+  void SetChipGain(float chipgain) { mChipGain = chipgain; }
+  void SetADCoutRange(float range) { mADCoutRange = range; }
+  void SetADCinRange(float range) { mADCinRange = range; }
+  void SetADCbaseline(int basel) { mADCbaseline = basel; }
+  void SetDiffusion(int diffOn = 1) { mDiffusionOn = diffOn; }
+  void SetElAttach(int elOn = 1) { mElAttachOn = elOn; }
+  void SetElAttachProp(float prop) { mElAttachProp = prop; }
+  void SetTimeResponse(int trfOn = 1)
+  {
+    mTRFOn = trfOn;
+    ReInit();
+  }
+  void SetCrossTalk(int ctOn = 1)
+  {
+    mCTOn = ctOn;
+    ReInit();
+  }
+  void SetPadCoupling(float v) { mPadCoupling = v; }
+  void SetTimeCoupling(float v) { mTimeCoupling = v; }
+  void SetTimeStruct(bool tsOn = 1) { mTimeStructOn = tsOn; }
+  void SetPadResponse(int prfOn = 1) { mPRFOn = prfOn; }
+  void SetNTimeBins(int ntb) { mNTimeBins = ntb; }
+  void SetNTBoverwriteOCDB(bool over = true) { mNTBoverwriteOCDB = over; }
+  float GetGasGain() const { return mGasGain; }
+  float GetNoise() const { return mNoise; }
+  float GetChipGain() const { return mChipGain; }
+  float GetADCoutRange() const { return mADCoutRange; }
+  float GetADCinRange() const { return mADCinRange; }
+  int GetADCbaseline() const { return mADCbaseline; }
+  float GetTRFlo() const { return mTRFlo; }
+  float GetTRFhi() const { return mTRFhi; }
+  float GetPadCoupling() const { return mPadCoupling; }
+  float GetTimeCoupling() const { return mTimeCoupling; }
+  int GetNTimeBins() const { return mNTimeBins; }
+  bool GetNTBoverwriteOCDB() const { return mNTBoverwriteOCDB; }
+  bool DiffusionOn() const { return mDiffusionOn; }
+  bool ElAttachOn() const { return mElAttachOn; }
+  float GetElAttachProp() const { return mElAttachProp; }
+  bool TRFOn() const { return mTRFOn; }
+  bool CTOn() const { return mCTOn; }
+  bool TimeStructOn() const { return mTimeStructOn; }
+  bool PRFOn() const { return mPRFOn; }
+  double TimeResponse(double time) const;
+  double CrossTalk(double time) const;
+
+  void ReInit();
+
+ protected:
+  static TRDSimParam* fgInstance; //  Instance of this class (singleton implementation)
+  static bool fgTerminated;       //  Defines if this class has already been terminated and
+                                  //  therefore does not return instances in GetInstance anymore
+
+  // Digitization parameter
+  float mGasGain;  //  Gas gain
+  float mNoise;    //  Electronics noise
+  float mChipGain; //  Electronics gain
+
+  float mADCoutRange; //  ADC output range (number of channels)
+  float mADCinRange;  //  ADC input range (input charge)
+  int mADCbaseline;   //  ADC intrinsic baseline in ADC channel
+
+  int mDiffusionOn; //  Switch for the diffusion
+
+  int mElAttachOn;     //  Switch for the electron attachment
+  float mElAttachProp; //  Propability for electron attachment (for 1m)
+
+  int mTRFOn;     //  Switch for the time response
+  float* mTRFsmp; //! Integrated time response
+  int mTRFbin;    //  Number of bins for the TRF
+  float mTRFlo;   //  Lower boundary of the TRF
+  float mTRFhi;   //  Higher boundary of the TRF
+  float mTRFwid;  //  Bin width of the integrated TRF
+
+  int mCTOn;     //  Switch for cross talk
+  float* mCTsmp; //! Integrated cross talk
+
+  float mPadCoupling;  //  Pad coupling factor
+  float mTimeCoupling; //  Time coupling factor (image charge of moving ions)
+  int mTimeStructOn;   //  Switch for cell time structure
+
+  int mPRFOn; //  Switch for the pad response
+
+  int mNTimeBins;         //  Number of time bins (only used it fNTBoverwriteOCDB = true)
+  bool mNTBoverwriteOCDB; //  Switch to overwrite number of time bins from PCDB
+
+ private:
+  // This is a singleton, constructor is private!
+  TRDSimParam();
+  ~TRDSimParam();
+
+  void Init();
+  void SampleTRF();
+
+  ClassDefNV(TRDSimParam, 1) // The TRD simulation parameters
+};
+}
+}
+#endif
---
+++ Detectors/TRD/base/src/TRDPadPlane.cxx
@@ -0,0 +1,274 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See https://alice-o2.web.cern.ch/ for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+//  Describes a pad plane of a TRD ROC                                       //
+//                                                                           //
+//  Contains the information on pad postions, pad dimensions,                //
+//  tilting angle, etc.                                                      //
+//  It also provides methods to identify the current pad number from         //
+//  global coordinates.                                                      //
+//  The numbering and coordinates should follow the official convention      //
+//  (see David Emschermanns note on TRD convention)                          //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+#include "TRDBase/TRDPadPlane.h"
+#include <TMath.h>
+
+using namespace o2::trd;
+
+//_____________________________________________________________________________
+TRDPadPlane::TRDPadPlane()
+  : mLayer(0),
+    mStack(0),
+    mLength(0),
+    mWidth(0),
+    mLengthRim(0),
+    mWidthRim(0),
+    mLengthOPad(0),
+    mWidthOPad(0),
+    mLengthIPad(0),
+    mWidthIPad(0),
+    mRowSpacing(0),
+    mColSpacing(0),
+    mNrows(0),
+    mNcols(0),
+    mTiltingAngle(0),
+    mTiltingTan(0),
+    mPadRow(nullptr),
+    mPadCol(nullptr),
+    mPadRowSMOffset(0),
+    mAnodeWireOffset(0)
+{
+  //
+  // Default constructor
+  //
+}
+
+//_____________________________________________________________________________
+TRDPadPlane::TRDPadPlane(int layer, int stack)
+  : mLayer(layer),
+    mStack(stack),
+    mLength(0),
+    mWidth(0),
+    mLengthRim(0),
+    mWidthRim(0),
+    mLengthOPad(0),
+    mWidthOPad(0),
+    mLengthIPad(0),
+    mWidthIPad(0),
+    mRowSpacing(0),
+    mColSpacing(0),
+    mNrows(0),
+    mNcols(0),
+    mTiltingAngle(0),
+    mTiltingTan(0),
+    mPadRow(nullptr),
+    mPadCol(nullptr),
+    mPadRowSMOffset(0),
+    mAnodeWireOffset(0)
+{
+  //
+  // Constructor
+  //
+}
+
+//_____________________________________________________________________________
+TRDPadPlane::~TRDPadPlane()
+{
+  //
+  // TRDPadPlane destructor
+  //
+
+  if (mPadRow) {
+    delete[] mPadRow;
+    mPadRow = nullptr;
+  }
+
+  if (mPadCol) {
+    delete[] mPadCol;
+    mPadCol = nullptr;
+  }
+}
+
+/*
+//_____________________________________________________________________________
+void TRDPadPlane::Copy(TObject &p) const
+{
+  //
+  // Copy function
+  //
+
+  int iBin = 0;
+
+  ((TRDPadPlane &) p).fLayer           = fLayer;
+  ((TRDPadPlane &) p).fStack           = fStack;
+
+  ((TRDPadPlane &) p).fLength          = fLength;
+  ((TRDPadPlane &) p).fWidth           = fWidth;
+  ((TRDPadPlane &) p).fLengthRim       = fLengthRim;
+  ((TRDPadPlane &) p).fWidthRim        = fWidthRim;
+  ((TRDPadPlane &) p).fLengthOPad      = fLengthOPad;
+  ((TRDPadPlane &) p).fWidthOPad       = fWidthOPad;
+  ((TRDPadPlane &) p).fLengthIPad      = fLengthIPad;
+  ((TRDPadPlane &) p).fWidthIPad       = fWidthIPad;
+
+  ((TRDPadPlane &) p).fRowSpacing      = fRowSpacing;
+  ((TRDPadPlane &) p).fColSpacing      = fColSpacing;
+
+  ((TRDPadPlane &) p).fNrows           = fNrows;
+  ((TRDPadPlane &) p).fNcols           = fNcols;
+
+  ((TRDPadPlane &) p).fTiltingAngle    = fTiltingAngle;
+  ((TRDPadPlane &) p).fTiltingTan      = fTiltingTan;
+
+  ((TRDPadPlane &) p).fPadRowSMOffset  = fPadRowSMOffset;
+  ((TRDPadPlane &) p).fAnodeWireOffset = fAnodeWireOffset;
+
+  if (((TRDPadPlane &) p).fPadRow) {
+    delete [] ((TRDPadPlane &) p).fPadRow;
+  }
+  ((TRDPadPlane &) p).fPadRow = new double[fNrows];
+  for (iBin = 0; iBin < fNrows; iBin++) {
+    ((TRDPadPlane &) p).fPadRow[iBin] = fPadRow[iBin];
+  }
+
+  if (((TRDPadPlane &) p).fPadCol) {
+    delete [] ((TRDPadPlane &) p).fPadCol;
+  }
+  ((TRDPadPlane &) p).fPadCol = new double[fNrows];
+  for (iBin = 0; iBin < fNrows; iBin++) {
+    ((TRDPadPlane &) p).fPadCol[iBin] = fPadCol[iBin];
+  }
+
+  TObject::Copy(p);
+
+}
+*/
+
+//_____________________________________________________________________________
+void TRDPadPlane::SetTiltingAngle(double t)
+{
+  //
+  // Set the tilting angle of the pads
+  //
+
+  mTiltingAngle = t;
+  mTiltingTan = TMath::Tan(TMath::Pi() / 180.0 * mTiltingAngle);
+}
+
+//_____________________________________________________________________________
+int TRDPadPlane::GetPadRowNumber(double z) const
+{
+  //
+  // Finds the pad row number for a given z-position in local supermodule system
+  //
+
+  int row = 0;
+  int nabove = 0;
+  int nbelow = 0;
+  int middle = 0;
+
+  if ((z > GetRow0()) || (z < GetRowEnd())) {
+    row = -1;
+
+  } else {
+    nabove = mNrows + 1;
+    nbelow = 0;
+    while (nabove - nbelow > 1) {
+      middle = (nabove + nbelow) / 2;
+      if (z == (mPadRow[middle - 1] + mPadRowSMOffset)) {
+        row = middle;
+      }
+      if (z > (mPadRow[middle - 1] + mPadRowSMOffset)) {
+        nabove = middle;
+      } else {
+        nbelow = middle;
+      }
+    }
+    row = nbelow - 1;
+  }
+
+  return row;
+}
+
+//_____________________________________________________________________________
+int TRDPadPlane::GetPadRowNumberROC(double z) const
+{
+  //
+  // Finds the pad row number for a given z-position in local ROC system
+  //
+
+  int row = 0;
+  int nabove = 0;
+  int nbelow = 0;
+  int middle = 0;
+
+  if ((z > GetRow0ROC()) || (z < GetRowEndROC())) {
+    row = -1;
+
+  } else {
+    nabove = mNrows + 1;
+    nbelow = 0;
+    while (nabove - nbelow > 1) {
+      middle = (nabove + nbelow) / 2;
+      if (z == mPadRow[middle - 1]) {
+        row = middle;
+      }
+      if (z > mPadRow[middle - 1]) {
+        nabove = middle;
+      } else {
+        nbelow = middle;
+      }
+    }
+    row = nbelow - 1;
+  }
+
+  return row;
+}
+
+//_____________________________________________________________________________
+int TRDPadPlane::GetPadColNumber(double rphi) const
+{
+  //
+  // Finds the pad column number for a given rphi-position
+  //
+
+  int col = 0;
+  int nabove = 0;
+  int nbelow = 0;
+  int middle = 0;
+
+  if ((rphi < GetCol0()) || (rphi > GetColEnd())) {
+    col = -1;
+
+  } else {
+    nabove = mNcols;
+    nbelow = 0;
+    while (nabove - nbelow > 1) {
+      middle = (nabove + nbelow) / 2;
+      if (rphi == mPadCol[middle]) {
+        col = middle;
+      }
+      if (rphi > mPadCol[middle]) {
+        nbelow = middle;
+      } else {
+        nabove = middle;
+      }
+    }
+    col = nbelow;
+  }
+
+  return col;
+}
+
+ClassImp(TRDPadPlane)
