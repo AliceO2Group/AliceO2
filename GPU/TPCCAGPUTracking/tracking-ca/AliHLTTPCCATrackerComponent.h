197ff63baa684641aae5a59e9f9d3575c1153f0a
drohr <drohr@cern.ch>
2017-07-10 13:08:31 +0200

    add option to dump event for standalone tracker

--- GPU/TPCCAGPUTracking/tracking-ca/AliHLTTPCCATrackerComponent.h
+++ GPU/TPCCAGPUTracking/tracking-ca/AliHLTTPCCATrackerComponent.h
@@ -117,6 +117,7 @@ class AliHLTTPCCATrackerComponent : public AliHLTProcessor
     TString fGPULibrary;			  //Name of the library file that provides the GPU tracker object
     int fGPUStuckProtection;		//Protect from stuck GPUs
 	int fAsync;                       //Run tracking in async thread to catch GPU hangs....
+	int fDumpEvent;					//Debug function to dump event for standalone tracker
 
     /** set configuration parameters **/
     void SetDefaultConfiguration();
