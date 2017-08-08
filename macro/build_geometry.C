2dc3cc1e37d8f1b1aeefc5eb51dfe003895a8874
Sandro Wenzel <sandro.wenzel@cern.ch>
2017-08-08 14:15:09 +0200

    started global o2sim macro
    reuses build_geometry macro which was generalized

--- macro/build_geometry.C
+++ macro/build_geometry.C
@@ -1,36 +1,41 @@
 #if (!defined(__CINT__) && !defined(__CLING__)) || defined(__MAKECINT__)
-  #include "TGeoManager.h"
-  #include "TString.h"
-  #include "TSystem.h"
+#include "TGeoManager.h"
+#include "TString.h"
+#include "TSystem.h"
 
-  #include "FairRunSim.h"
-  #include "DetectorsPassive/Cave.h"
-  #include "DetectorsPassive/FrameStructure.h"
+#include "DetectorsPassive/Cave.h"
+#include "DetectorsPassive/FrameStructure.h"
+#include "FairRunSim.h"
 #endif
 
 // macro to test correct initialization of the
 // (complete) geometry structure
-void build_geometry() {
+void build_geometry(FairRunSim* run = nullptr)
+{
+  bool geomonly = (run == nullptr);
+
   // minimal macro to test setup of the geometry
-  
+
   TString dir = getenv("VMCWORKDIR");
   TString geom_dir = dir + "/Detectors/Geometry/";
-  gSystem->Setenv("GEOMPATH",geom_dir.Data());
+  gSystem->Setenv("GEOMPATH", geom_dir.Data());
 
   TString tut_configdir = dir + "/Detectors/gconfig";
-  gSystem->Setenv("CONFIG_DIR",tut_configdir.Data());
+  gSystem->Setenv("CONFIG_DIR", tut_configdir.Data());
 
-  // Create simulation run
-  FairRunSim* run = new FairRunSim();
-  run->SetOutputFile("foo.root"); // Output file
-  run->SetName("TGeant3");      // Transport engine
+  // Create simulation run if it does not exist
+  if (run == nullptr) {
+    run = new FairRunSim();
+    run->SetOutputFile("foo.root"); // Output file
+    run->SetName("TGeant3");        // Transport engine
+  }
   // Create media
   run->SetMaterials("media.geo"); // Materials
 
   // we need a field to properly init the media
-  auto field = new o2::field::MagneticField("field","field +5kG");
+  auto field = new o2::field::MagneticField("Maps", "Maps", -1., -1., o2::field::MagFieldParam::k5kG);
   run->SetField(field);
-  
+
   // Create geometry
   o2::Passive::Cave* cave = new o2::Passive::Cave("CAVE");
   // the experiment hall (cave)
@@ -38,7 +43,7 @@ void build_geometry() {
   run->AddModule(cave);
 
   // the frame structure to support other detectors
-  auto frame = new o2::passive::FrameStructure("Frame","Frame");
+  auto frame = new o2::passive::FrameStructure("Frame", "Frame");
   run->AddModule(frame);
 
   // TOF
@@ -55,8 +60,9 @@ void build_geometry() {
 
   // emcal
   run->AddModule(new o2::EMCAL::Detector("EMCAL", true));
-  
-  run->Init();
 
-  gGeoManager->Export("O2geometry.root");
+  if (geomonly) {
+    run->Init();
+    gGeoManager->Export("O2geometry.root");
+  }
 }
