d5f5502dba5a46183b061d480dcf509574295ddb
wiechula <Jens.Wiechula@cern.ch>
2017-08-08 18:05:20 +0200

    fixes and additions for drawing pad-wise calibration data
    o CalArray
      - fix setValue/getValue for the row, pad case
      - improve naming
      - fix constructor
      - implement +=
    o Mapper
      - general getter for pad number in pad subset
      - fix rotation call
    o ROC
      - fix sector number
    o Painter / LinkDef
      - fix template implementation
      - add explicit instantiations, otherwise everything
        would need to be in the header
      - fix 2D histogram creation
      - fix calDet drawing

--- Detectors/TPC/base/src/Painter.cxx
+++ Detectors/TPC/base/src/Painter.cxx
@@ -8,12 +8,17 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
+#include <string>
+#include <algorithm>
+
 #include "TCanvas.h"
 #include "TString.h"
 #include "TAxis.h"
 #include "TH1.h"
 #include "TH2.h"
 
+#include "TPCBase/ROC.h"
+#include "TPCBase/Sector.h"
 #include "TPCBase/Mapper.h"
 #include "TPCBase/CalDet.h"
 #include "TPCBase/CalArray.h"
@@ -21,88 +26,64 @@
 
 using namespace o2::TPC;
 
-//template <class T>
+template <class T>
 void Painter::draw(const CalDet<T>& calDet)
 {
   using DetType = CalDet<T>;
   using CalType = CalArray<T>;
 
-  const auto& name = calDet.getName().c_str();
+  static const Mapper& mapper = Mapper::instance();
+
+  // ===| name and title |======================================================
+  static const auto title = calDet.getName().c_str();
+  std::string name = calDet.getName();
+  std::replace(name.begin(), name.end(), ' ', '_');
 
   // ===| define histograms |===================================================
   // TODO: auto scaling of ranges based on mean and variance?
   //       for the moment use roots auto scaling
-  auto hAside1D = new TH1F(Form("h_Aside_1D_%s", name), Form("%s (A-Side)", name),
+
+  // set buffer size such that autoscaling uses the full range. This is about 2MB per histogram!
+  const int bufferSize = TH1::GetDefaultBufferSize();
+  TH1::SetDefaultBufferSize(Sector::MAXSECTOR*mapper.getPadsInSector());
+
+  auto hAside1D = new TH1F(Form("h_Aside_1D_%s", name.c_str()), Form("%s (A-Side)", title),
                          300, 0, 0); //TODO: modify ranges
 
-  auto hCside1D = new TH1F(Form("h_Cside_1D_%s", name), Form("%s (C-Side)", name),
+  auto hCside1D = new TH1F(Form("h_Cside_1D_%s", name.c_str()), Form("%s (C-Side)", title),
                          300, 0, 0); //TODO: modify ranges
 
-  auto hAside2D = new TH2F(Form("h_Aside_2D_%s;x (cm);y (cm)", name), Form("%s (A-Side)", name),
+  auto hAside2D = new TH2F(Form("h_Aside_2D_%s;x (cm);y (cm)", name.c_str()), Form("%s (A-Side)", title),
                          300, -300, 300, 300, -300, 300);
 
-  auto hCside2D = new TH2F(Form("h_Cside_2D_%s;x (cm);y (cm)", name), Form("%s (C-Side)", name),
+  auto hCside2D = new TH2F(Form("h_Cside_2D_%s;x (cm);y (cm)", name.c_str()), Form("%s (C-Side)", title),
                          300, -300, 300, 300, -300, 300);
 
 
-  const Mapper& mapper = Mapper::instance();
-
-  for (auto& cal : calDet.getData()) {
-
-    int calPadSubsetNumber = cal.getPadSubsetNumber();
-    int row = -1;
-    int pad = -1;
-    int offset=0;
-    Sector sector;
-    switch (cal.getPadSubset()) {
-      case PadSubset::ROC: {
-        ROC roc(calPadSubsetNumber);
-        offset = (roc.rocType() == RocType::IROC)?0:mapper.getPadsInIROC();
-        sector = roc.getSector();
-        break;
-      }
-      case PadSubset::Partition: {
-        const int npartitions = mapper.getNumberOfPartitions();
-        const int partition = calPadSubsetNumber%npartitions;
-        const int rowOffset = mapper.getPartitionInfo(partition).getGlobalRowOffset();
-        offset = mapper.globalPadNumber(PadPos(rowOffset, 0));
-        sector = calPadSubsetNumber/npartitions;
-        break;
-      }
-      case PadSubset::Region: {
-        const int nregions = mapper.getNumberOfPadRegions();
-        const int region = calPadSubsetNumber%mapper.getNumberOfPadRegions();
-        const int rowOffset = mapper.getPadRegionInfo(region).getGlobalRowOffset();
-        offset = mapper.globalPadNumber(PadPos(rowOffset, 0));
-        sector = calPadSubsetNumber/nregions;
-        break;
-      }
-    }
+  for (ROC roc; !roc.looped(); ++roc) {
 
     auto hist2D = hAside2D;
     auto hist1D = hAside1D;
-    if (sector.side() == Side::C) {
+    if (roc.side() == Side::C) {
       hist2D = hCside2D;
       hist1D = hCside1D;
     }
 
-    int padNumber = offset;
-    for (const auto& val : cal.getData()) {
-      const PadCentre& localCoord = mapper.padCentre(padNumber);
-      const GlobalPosition3D pos = mapper.LocalToGlobal(LocalPosition3D(localCoord.X(), localCoord.Y(), 0), sector);
-
-      Int_t bin = hist2D->FindBin(pos.X(), pos.Y());
-
-      //hist2D->SetBinContent(bin, val);
-      hist2D->Fill(pos.X(), pos.Y(), val);
-      hist1D->Fill(val);
-
-      ++padNumber;
+    const int nrows = mapper.getNumberOfRowsROC(roc);
+    for (int irow=0; irow<nrows; ++irow) {
+      const int npads = mapper.getNumberOfPadsInRowROC(roc, irow);
+      for (int ipad=0; ipad<npads; ++ipad) {
+        const auto val = calDet.getValue(roc, irow, ipad);
+        const GlobalPosition2D pos = mapper.getPadCentre(PadROCPos(roc, irow, ipad));
+        const int bin = hist2D->FindBin(pos.X(), pos.Y());
+        if (!hist2D->GetBinContent(bin)) hist2D->SetBinContent(bin, val);
+        hist1D->Fill(val);
+      }
     }
   }
 
   // ===| Draw histograms |=====================================================
-  auto c = new TCanvas(Form("c_%s", name));
+  auto c = new TCanvas(Form("c_%s", name.c_str()));
   c->Divide(2,2);
 
   c->cd(1);
@@ -116,23 +97,36 @@ void Painter::draw(const CalDet<T>& calDet)
 
   c->cd(4);
   hCside1D->Draw();
+
+  // reset the buffer size
+  TH1::SetDefaultBufferSize(bufferSize);
 }
 
 //______________________________________________________________________________
-//template <class T>
+template <class T>
 void Painter::draw(const CalArray<T>& calArray)
 {
+  std::string name = calArray.getName();
+  std::replace(name.begin(), name.end(), ' ', '_');
+  auto c = new TCanvas(Form("c_%s", name.c_str()));
+
+  auto hist = getHistogram2D(calArray);
+  hist->Draw("colz");
 }
 
 //______________________________________________________________________________
-//template <class T>
+template <class T>
 TH2* Painter::getHistogram2D(const CalDet<T>& calDet, Side side)
 {
   static const Mapper& mapper = Mapper::instance();
 
-  const auto name = calDet.getName().c_str();
+  const auto title = calDet.getName().c_str();
+  std::string name = calDet.getName();
+  std::replace(name.begin(), name.end(), ' ', '_');
   const char side_name = (side == Side::A)?'A':'C';
-  auto h2D = new TH2F(Form("h_%cside_2D_%s;x (cm);y (cm)", side_name, name), Form("%s (%c-Side)", name, side_name),
+
+  auto h2D = new TH2F(Form("h_%cside_2D_%s;x (cm);y (cm)", side_name, name.c_str()),
+                      Form("%s (%c-Side)", title, side_name),
                          300, -300, 300, 300, -300, 300);
 
   for (ROC roc; !roc.looped(); ++roc) {
@@ -143,9 +137,8 @@ TH2* Painter::getHistogram2D(const CalDet<T>& calDet, Side side)
       for (int ipad=0; ipad<npads; ++ipad) {
         const auto val = calDet.getValue(roc, irow, ipad);
         const GlobalPosition2D pos = mapper.getPadCentre(PadROCPos(roc, irow, ipad));
-        const int binX = h2D->GetXaxis()->FindBin(pos.X());
-        const int binY = h2D->GetXaxis()->FindBin(pos.Y());
-        h2D->SetBinContent(binX, binY, val);
+        const int bin = h2D->FindBin(pos.X(), pos.Y());
+        if (!h2D->GetBinContent(bin)) h2D->SetBinContent(bin, val);
       }
     }
   }
@@ -154,7 +147,7 @@ TH2* Painter::getHistogram2D(const CalDet<T>& calDet, Side side)
 }
 
 //______________________________________________________________________________
-//template <class T>
+template <class T>
 TH2* Painter::getHistogram2D(const CalArray<T>& calArray)
 {
   static const Mapper& mapper = Mapper::instance();
@@ -167,19 +160,53 @@ TH2* Painter::getHistogram2D(const CalArray<T>& calArray)
   const int npads = mapper.getNumberOfPadsInRow(padSubset, position, nrows-1);
 
   // ===| create histogram |====================================================
-  const auto name = calArray.getName().c_str();
-  auto hist = new TH2D(Form("h_%s", name), Form("%s;pad row;pad", name), nrows, 0., nrows, npads, -npads/2, npads/2);
+  const auto title = calArray.getName().c_str();
+  std::string name = calArray.getName();
+  std::replace(name.begin(), name.end(), ' ', '_');
+  auto hist = new TH2D(Form("h_%s", name.c_str()),
+                       Form("%s;pad row;pad", title),
+                       nrows, 0., nrows,
+                       npads, -npads/2, npads/2);
 
   // ===| fill hist |===========================================================
   for (int irow = 0; irow<nrows; ++irow) {
     const int padsInRow = mapper.getNumberOfPadsInRow(padSubset, position, irow);
     for (int ipad = 0; ipad<padsInRow; ++ipad) {
-       const auto val = calArray.getValue(irow, ipad);
-       const int cpad = ipad - padsInRow/2;
-       hist->Fill(irow, cpad, val);
+      const GlobalPadNumber pad = mapper.getPadNumber(padSubset, position, irow, ipad);
+      const auto val = calArray.getValue(pad);
+      const int cpad = ipad - padsInRow/2;
+      hist->Fill(irow, cpad, val);
+      printf("%d %d: %f\n", irow, cpad, val);
     }
   }
   return hist;
 }
 
 
+// ===| explicit instantiations |===============================================
+// this is required to force the compiler to create instances with the types
+// we usually would like to deal with
+template void Painter::draw<float>(const CalDet<float>& calDet);
+template void Painter::draw<float>(const CalArray<float>& calArray);
+template TH2* Painter::getHistogram2D<float>(const CalDet<float>& calDet, Side side);
+template TH2* Painter::getHistogram2D<float>(const CalArray<float>& calArray);
+
+template void Painter::draw<double>(const CalDet<double>& calDet);
+template void Painter::draw<double>(const CalArray<double>& calArray);
+template TH2* Painter::getHistogram2D<double>(const CalDet<double>& calDet, Side side);
+template TH2* Painter::getHistogram2D<double>(const CalArray<double>& calArray);
+
+template void Painter::draw<int>(const CalDet<int>& calDet);
+template void Painter::draw<int>(const CalArray<int>& calArray);
+template TH2* Painter::getHistogram2D<int>(const CalDet<int>& calDet, Side side);
+template TH2* Painter::getHistogram2D<int>(const CalArray<int>& calArray);
+
+template void Painter::draw<short>(const CalDet<short>& calDet);
+template void Painter::draw<short>(const CalArray<short>& calArray);
+template TH2* Painter::getHistogram2D<short>(const CalDet<short>& calDet, Side side);
+template TH2* Painter::getHistogram2D<short>(const CalArray<short>& calArray);
+
+template void Painter::draw<bool>(const CalDet<bool>& calDet);
+template void Painter::draw<bool>(const CalArray<bool>& calArray);
+template TH2* Painter::getHistogram2D<bool>(const CalDet<bool>& calDet, Side side);
+template TH2* Painter::getHistogram2D<bool>(const CalArray<bool>& calArray);
