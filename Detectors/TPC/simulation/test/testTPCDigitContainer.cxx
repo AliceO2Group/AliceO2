60b990d5ed7533086681fdacbfeddfa086567c92
Andreas Mathis <andreas.mathis@cern.ch>
2017-08-10 16:12:40 +0200

    Fix compile error of TPC DigitContainer ctest

--- Detectors/TPC/simulation/test/testTPCDigitContainer.cxx
+++ Detectors/TPC/simulation/test/testTPCDigitContainer.cxx
@@ -20,6 +20,7 @@
 #include "TPCSimulation/DigitContainer.h"
 #include "TPCSimulation/DigitMC.h"
 #include "TPCSimulation/SAMPAProcessing.h"
+#include <memory>
 
 namespace o2 {
 namespace TPC {
@@ -29,34 +30,34 @@ namespace TPC {
   BOOST_AUTO_TEST_CASE(DigitContainer_test1)
   {
     const SAMPAProcessing& sampa = SAMPAProcessing::instance();
-    auto *mDigitContainer = new DigitContainer();
+    static FairRootManager *mgr = FairRootManager::Instance();
+    DigitContainer digitContainer;
 
-    /// \todo add test with FairLink stuff if possible
-    std::vector<int> hitID = {1, 250, 3, 62, 1000};
-    //std::vector<int> MCevent = {1, 250, 3, 62, 1000};
-    //std::vector<int> MCtrack = {22, 3, 4, 23, 523};
-    std::vector<int> CRU     = {1, 23, 36, 53, 214};
-    std::vector<int> Time    = {231, 2, 500, 230, 1};
-    std::vector<int> Row     = {12, 5, 6, 2, 6};
-    std::vector<int> Pad     = {1, 15, 14, 23, 5};
-    std::vector<int> nEle    = {60, 100, 250, 1023, 2};
+    const std::vector<int> MCevent = {1, 250, 3, 62, 1000};
+    const std::vector<int> MCtrack = {22, 3, 4, 23, 523};
+    const std::vector<int> CRU     = {1, 23, 36, 53, 214};
+    const std::vector<int> Time    = {231, 2, 500, 230, 1};
+    const std::vector<int> Row     = {12, 5, 6, 2, 6};
+    const std::vector<int> Pad     = {1, 15, 14, 23, 5};
+    const std::vector<int> nEle    = {60, 100, 250, 1023, 2};
 
     for(int i=0; i<CRU.size(); ++i) {
-      //mDigitContainer->addDigit(MCevent[i], MCtrack[i], CRU[i], Time[i], Row[i], Pad[i], nEle[i]);
-      mDigitContainer->addDigit(hitID[i], CRU[i], Time[i], Row[i], Pad[i], nEle[i]);
+      mgr->SetEntryNr(MCevent[i]);
+      digitContainer.addDigit(MCtrack[i], CRU[i], Time[i], Row[i], Pad[i], nEle[i]);
     }
 
+    /// here the raw pointer is needed owed to the internal handling of the TClonesArrays in FairRoot
+    /// Usually the mDigitsArray is what is registered to the FairRootManager
     auto *mDigitsArray = new TClonesArray("o2::TPC::DigitMC");
-    mDigitContainer->fillOutputContainer(mDigitsArray);
+    digitContainer.fillOutputContainer(mDigitsArray, 1000);
 
     BOOST_CHECK(CRU.size() == mDigitsArray->GetEntriesFast());
 
     int digits = 0;
     for(auto digitsObject : *mDigitsArray) {
       DigitMC *digit = static_cast<DigitMC *>(digitsObject);
-      /// \todo fix this
-      //BOOST_CHECK(digit->getMCEvent(0) == MCevent[digits]);
-      //BOOST_CHECK(digit->getMCTrack(0) == MCtrack[digits]);
+      BOOST_CHECK(digit->getMCEvent(0) == MCevent[digits]);
+      BOOST_CHECK(digit->getMCTrack(0) == MCtrack[digits]);
       BOOST_CHECK(digit->getCRU() == CRU[digits]);
       BOOST_CHECK(digit->getTimeStamp() == Time[digits]);
       BOOST_CHECK(digit->getRow() == Row[digits]);
@@ -64,6 +65,8 @@ namespace TPC {
       BOOST_CHECK(digit->getCharge() == static_cast<int>(sampa.getADCSaturation(nEle[digits])));
       ++digits;
     }
+
+    delete mDigitsArray;
   }
 
 
@@ -74,42 +77,42 @@ namespace TPC {
   BOOST_AUTO_TEST_CASE(DigitContainer_test2)
   {
     const SAMPAProcessing& sampa = SAMPAProcessing::instance();
-    auto *mDigitContainer = new DigitContainer();
+    static FairRootManager *mgr = FairRootManager::Instance();
+    DigitContainer digitContainer;
 
-    /// \todo add test with FairLink stuff if possible
-    std::vector<int> hitID = { 1, 62,  1, 62, 62, 50, 62, 1, 1, 1};
-    //std::vector<int> MCevent = { 1, 62,  1, 62, 62, 50, 62, 1, 1, 1};
-    //std::vector<int> MCtrack = {22,  3, 22,  3,  3, 70,  3, 7, 7, 7};
-    std::vector<int> CRU     = {23, 23, 23, 23, 23, 23, 23, 23, 23, 23};
-    std::vector<int> Time    = {231, 231, 231, 231, 231, 231, 231, 231, 231, 231};
-    std::vector<int> Row     = {12, 12, 12, 12, 12, 12, 12, 12, 12, 12};
-    std::vector<int> Pad     = {15, 15, 15, 15, 15, 15, 15, 15, 15, 15};
-    std::vector<int> nEle    = {60, 1, 252, 10, 2, 3, 5, 25, 24, 23};
+    const std::vector<int> MCevent = { 1, 62,  1, 62, 62, 50, 62, 1, 1, 1};
+    const std::vector<int> MCtrack = {22, 3, 22, 3, 3, 70, 3, 7, 7, 7};
+    const std::vector<int> CRU     = {23, 23, 23, 23, 23, 23, 23, 23, 23, 23};
+    const std::vector<int> Time    = {231, 231, 231, 231, 231, 231, 231, 231, 231, 231};
+    const std::vector<int> Row     = {12, 12, 12, 12, 12, 12, 12, 12, 12, 12};
+    const std::vector<int> Pad     = {15, 15, 15, 15, 15, 15, 15, 15, 15, 15};
+    const std::vector<int> nEle    = {60, 1, 252, 10, 2, 3, 5, 25, 24, 23};
 
-    std::vector<int> MCeventSorted = {62, 1, 1, 50};
-    std::vector<int> MCtrackSorted = {3, 7, 22, 70};
+    const std::vector<int> MCeventSorted = {62, 1, 1, 50};
+    const std::vector<int> MCtrackSorted = {3, 7, 22, 70};
 
     int nEleSum = 0;
     for(int i=0; i<CRU.size(); ++i) {
-      //mDigitContainer->addDigit(MCevent[i], MCtrack[i], CRU[i], Time[i], Row[i], Pad[i], nEle[i]);
-      mDigitContainer->addDigit(hitID[i], CRU[i], Time[i], Row[i], Pad[i], nEle[i]);
+      mgr->SetEntryNr(MCevent[i]);
+      digitContainer.addDigit(MCtrack[i], CRU[i], Time[i], Row[i], Pad[i], nEle[i]);
       nEleSum += nEle[i];
     }
 
+    /// here the raw pointer is needed owed to the internal handling of the TClonesArrays in FairRoot
+    /// Usually the mDigitsArray is what is registered to the FairRootManager
     auto *mDigitsArray = new TClonesArray("o2::TPC::DigitMC");
-    mDigitContainer->fillOutputContainer(mDigitsArray);
+    digitContainer.fillOutputContainer(mDigitsArray, 1000);
 
     BOOST_CHECK(mDigitsArray->GetEntriesFast() == 1);
 
     int digits = 0;
     for(auto digitsObject : *mDigitsArray) {
       DigitMC *digit = static_cast<DigitMC *>(digitsObject);
-      /// \todo fix this
-      //BOOST_CHECK(digit->getMCEvent(0) == MCevent[digits]);
-      //for(int j=0; j<digit->getNumberOfMClabels(); ++j) {
-        //BOOST_CHECK(digit->getMCEvent(j) == MCeventSorted[j]);
-        //BOOST_CHECK(digit->getMCTrack(j) == MCtrackSorted[j]);
-      //}
+      BOOST_CHECK(digit->getMCEvent(0) == MCeventSorted[digits]);
+      for(int j=0; j<digit->getNumberOfMClabels(); ++j) {
+        BOOST_CHECK(digit->getMCEvent(j) == MCeventSorted[j]);
+        BOOST_CHECK(digit->getMCTrack(j) == MCtrackSorted[j]);
+      }
       BOOST_CHECK(digit->getCRU() == CRU[digits]);
       BOOST_CHECK(digit->getTimeStamp() == Time[digits]);
       BOOST_CHECK(digit->getRow() == Row[digits]);
@@ -117,6 +120,8 @@ namespace TPC {
       BOOST_CHECK(digit->getCharge() == static_cast<int>(sampa.getADCSaturation(nEleSum)));
       ++digits;
     }
+
+    delete mDigitsArray;
   }
 
 }
