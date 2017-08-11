96cbc092e25447c4eb773805cab0721dbe20bac3
Sandro Wenzel <sandro.wenzel@cern.ch>
2017-08-11 16:10:46 +0200

    Basic configuration for simulation / An o2sim executable
    This commit is providing a first version of an o2sim executable
    which tries to integrate various modules together
    
    We also provide a very basic SimConfig class for the purpose of
    configuring or playing with the simulation from the command line.
    This configuration is temporary and will be revised into a more
    comprehensive scheme.

---
+++ Common/Configuration/test/TestConfig.cxx
@@ -0,0 +1,29 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See https://alice-o2.web.cern.ch/ for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#include <Configuration/SimConfig.h>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  auto& conf = o2::conf::SimConfig::Instance();
+  conf.resetFromArguments(argc, argv);
+
+  std::cout << "Selected VMC engine " << conf.getMCEngine() << "\n";
+  std::cout << "Selected Modules:\n";
+
+  auto v = conf.getActiveDetectors();
+  for (auto &m : v) {
+    std::cout << "@ " << m << "\n";
+  }
+
+  std::cout << "Selected Generator " << conf.getGenerator() << "\n";
+  
+  return 0;
+}
--- macro/build_geometry.C
+++ macro/build_geometry.C
@@ -1,3 +1,13 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See https://alice-o2.web.cern.ch/ for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
 #if (!defined(__CINT__) && !defined(__CLING__)) || defined(__MAKECINT__)
 #include "TGeoManager.h"
 #include "TString.h"
@@ -5,17 +15,35 @@
 
 #include "DetectorsPassive/Cave.h"
 #include "DetectorsPassive/FrameStructure.h"
+#include <Field/MagneticField.h>
+#include <TPCSimulation/Detector.h>
+#include <EMCALSimulation/Detector.h>
+#include <TOFSimulation/Detector.h>
+#include <TRDSimulation/Detector.h>
+#include <DetectorsPassive/Cave.h>
+#include <DetectorsPassive/FrameStructureRun3.h>
+#include <Configuration/SimConfig.h>
 #include "FairRunSim.h"
+#include <algorithm>
 #endif
 
-// macro to test correct initialization of the
-// (complete) geometry structure
+bool isActivated(std::string s) {
+// access user configuration for list of wanted modules
+  auto& modulelist = o2::conf::SimConfig::Instance().getActiveDetectors();
+  auto active = std::find(modulelist.begin(), modulelist.end(), s)!=modulelist.end();
+  if (active) {
+    std::cout << "Activating " << s << " module \n";
+  }
+  return active;
+}
+
+// a "factory" like macro to instantiate the O2 geometry
 void build_geometry(FairRunSim* run = nullptr)
 {
   bool geomonly = (run == nullptr);
 
   // minimal macro to test setup of the geometry
-
+  
   TString dir = getenv("VMCWORKDIR");
   TString geom_dir = dir + "/Detectors/Geometry/";
   gSystem->Setenv("GEOMPATH", geom_dir.Data());
@@ -37,30 +65,41 @@ void build_geometry(FairRunSim* run = nullptr)
   run->SetField(field);
 
   // Create geometry
+  // we always need the gave
   o2::Passive::Cave* cave = new o2::Passive::Cave("CAVE");
   // the experiment hall (cave)
   cave->SetGeometryFileName("cave.geo");
   run->AddModule(cave);
 
-  // the frame structure to support other detectors
-  auto frame = new o2::passive::FrameStructure("Frame", "Frame");
-  run->AddModule(frame);
+  if (isActivated("TOF") || isActivated("TRD")) {
+    // the frame structure to support other detectors
+    auto frame = new o2::passive::FrameStructure("Frame", "Frame");
+    run->AddModule(frame);
+  }
 
-  // TOF
-  auto tof = new o2::tof::Detector("TOF", true);
-  run->AddModule(tof);
+  if (isActivated("TOF")){
+    // TOF
+    auto tof = new o2::tof::Detector("TOF", true);
+    run->AddModule(tof);
+  }
 
-  // TRD
-  auto trd = new o2::trd::Detector("TRD", true);
-  run->AddModule(trd);
+  if (isActivated("TRD")) {
+    // TRD
+    auto trd = new o2::trd::Detector("TRD", true);
+    run->AddModule(trd);
+  }
 
-  // tpc
-  auto tpc = new o2::TPC::Detector("TPC", true);
-  run->AddModule(tpc);
+  if (isActivated("TPC")){
+    // tpc
+    auto tpc = new o2::TPC::Detector("TPC", true);
+    run->AddModule(tpc);
+  }
 
-  // emcal
-  run->AddModule(new o2::EMCAL::Detector("EMCAL", true));
-
+  if (isActivated("EMCAL")){
+    // emcal
+    run->AddModule(new o2::EMCAL::Detector("EMCAL", true));
+  }
+   
   if (geomonly) {
     run->Init();
     gGeoManager->Export("O2geometry.root");
