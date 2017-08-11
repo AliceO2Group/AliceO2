8adbad80a60bfdfe2cd13ac1b162023481e4a077
Sandro Wenzel <sandro.wenzel@cern.ch>
2017-08-11 16:10:46 +0200

    Basic configuration for simulation / An o2sim executable
    This commit is providing a first version of an o2sim executable
    which tries to integrate various modules together
    
    We also provide a very basic SimConfig class for the purpose of
    configuring or playing with the simulation from the command line.
    This configuration is temporary and will be revised into a more
    comprehensive scheme.

---
+++ Common/Configuration/src/SimConfig.cxx
@@ -0,0 +1,58 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See https://alice-o2.web.cern.ch/ for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#include <Configuration/SimConfig.h>
+#include <boost/program_options.hpp>
+#include <iostream>
+
+using namespace o2::conf;
+
+bool SimConfig::resetFromArguments(int argc, char* argv[])
+{
+  namespace bpo = boost::program_options;
+
+  // Arguments parsing
+  bpo::variables_map vm;
+  bpo::options_description desc("Allowed options");
+  desc.add_options()("help,h", "Produce help message.")(
+    "mcEngine,e", bpo::value<std::string>()->default_value("TGeant3"), "VMC backend to be used.")(
+    "generator,g", bpo::value<std::string>()->default_value("boxgen"), "Event generator to be used.")(
+    "modules,m",
+    bpo::value<std::vector<std::string>>()->multitoken()->default_value(std::vector<std::string>({ "EMCAL TOF TPC TRD" }), "EMCAL TOF TPC TRD"),
+    "list of detectors")
+    ("nEvents,n", bpo::value<unsigned int>()->default_value(1), "number of events")
+    ("startEvent", bpo::value<unsigned int>()->default_value(0), "index of first event to be used (when applicable)")
+    ("extKinFile", bpo::value<std::string>()->default_value("Kinematics.root"), "name of kinematics file for event generator from file (when applicable)");
+
+  try {
+    bpo::store(parse_command_line(argc, argv, desc), vm);
+    bpo::notify(vm);
+  } catch (const std::exception& ex) {
+    std::cerr << "exception caught\n";
+    return false;
+  }
+
+  // help
+  if (vm.count("help")) {
+    std::cout << desc << std::endl;
+    return false;
+  }
+
+  mMCEngine = vm["mcEngine"].as<std::string>();
+  mActiveDetectors = vm["modules"].as<std::vector<std::string>>();
+  mGenerator = vm["generator"].as<std::string>();
+  mNEvents = vm["nEvents"].as<unsigned int>();
+  mExtKinFileName = vm["extKinFile"].as<std::string>();
+  mStartEvent = vm["startEvent"].as<unsigned int>();
+
+  return true;
+}
+
+ClassImp(o2::conf::SimConfig);
--- macro/o2sim.C
+++ macro/o2sim.C
@@ -1,23 +1,63 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See https://alice-o2.web.cern.ch/ for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
 #include "build_geometry.C"
+#if (!defined(__CINT__) && !defined(__CLING__)) || defined(__MAKECINT__)
+#include <FairBoxGenerator.h>
+#include <FairPrimaryGenerator.h>
+#include <TStopwatch.h>
+#include <memory>
+#include "FairParRootFileIo.h"
+#include "FairSystemInfo.h"
+#include <Configuration/SimConfig.h>
+#include <Generators/GeneratorFromFile.h>
+#endif
 
 void o2sim()
 {
+  auto& confref = o2::conf::SimConfig::Instance();
+  auto genconfig = confref.getGenerator();
+
   auto run = new FairRunSim();
-  run->SetOutputFile("o2sim.root"); // Output file
-  run->SetName("TGeant3");          // Transport engine
+  run->SetOutputFile("o2sim.root");            // Output file
+  run->SetName(confref.getMCEngine().c_str()); // Transport engine
 
   // construct geometry / including magnetic field
   build_geometry(run);
 
   // setup generator
   auto primGen = new FairPrimaryGenerator();
-  auto boxGen = new FairBoxGenerator(211, 10); /*protons*/
-  boxGen->SetEtaRange(-0.9, 0.9);
-  boxGen->SetPRange(0.1, 5);
-  boxGen->SetPhiRange(0., 360.);
-  boxGen->SetDebug(kTRUE);
-  primGen->AddGenerator(boxGen);
+
+  if (genconfig.compare("boxgen") == 0) {
+    // a simple "box" generator
+    std::cout << "Init box generator\n";
+    auto boxGen = new FairBoxGenerator(211, 10); /*protons*/
+    boxGen->SetEtaRange(-0.9, 0.9);
+    boxGen->SetPRange(0.1, 5);
+    boxGen->SetPhiRange(0., 360.);
+    boxGen->SetDebug(kTRUE);
+    primGen->AddGenerator(boxGen);
+  } else if (genconfig.compare("extkin") == 0) {
+    // external kinematics
+    // needs precense of a kinematics file "Kinematics.root"
+    // TODO: make this configurable and check for presence
+    auto extGen =  new o2::eventgen::GeneratorFromFile(confref.getExtKinematicsFileName().c_str());
+    extGen->SetStartEvent(confref.getStartEvent());
+    primGen->AddGenerator(extGen);
+    std::cout << "using external kinematics\n";
+  }
   run->SetGenerator(primGen);
+
+  // Timer
+  TStopwatch timer;
+  timer.Start();
 
   // run init
   run->Init();
@@ -31,5 +71,21 @@ void o2sim()
   rtdb->saveOutput();
   rtdb->print();
 
-  run->Run(1);
+  run->Run(confref.getNEvents());
+
+  // needed ... otherwise nothing flushed?
+  delete run;
+
+  // Finish
+  timer.Stop();
+  Double_t rtime = timer.RealTime();
+  Double_t ctime = timer.CpuTime();
+
+  // extract max memory usage
+  FairSystemInfo sysinfo;
+
+  std::cout << "\n\n";
+  std::cout << "Macro finished succesfully.\n";
+  std::cout << "Real time " << rtime << " s, CPU time " << ctime << "s\n";
+  std::cout << "Memory used " << sysinfo.GetMaxMemory() << " MB\n";
 }
