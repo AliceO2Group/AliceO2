68c647bded5f44380cd787823803196361bd621e
Sandro Wenzel <sandro.wenzel@cern.ch>
2017-08-17 11:55:16 +0200

    MCStepLogger: Log processes per volume + add logging of magnetic field access
     * Add information about physics processes per volume
     * Add another logger to monitor magnetic field accesses per volume
     * refactor code to avoid code duplication for dispatch function
       (application of variadic templates)
     * some clang-formatting

--- Utilities/MCStepLogger/src/MCStepInterceptor.cxx
+++ Utilities/MCStepLogger/src/MCStepInterceptor.cxx
@@ -26,44 +26,78 @@
 //  @brief  A LD_PRELOAD logger hooking into Stepping of TVirtualMCApplication's
 
 class TVirtualMCApplication;
+class TVirtualMagField;
 
 // (re)declare symbols to be able to hook into them
-#define DECLARE_INTERCEPT_SYMBOLS(APP)                                                       \
-  class APP                                                                                  \
-  {                                                                                          \
-     public:                                                                                 \
-       void Stepping();                                                                      \
-       void FinishEvent();                                                                   \
+#define DECLARE_INTERCEPT_SYMBOLS(APP) \
+  class APP                            \
+  {                                    \
+   public:                             \
+    void Stepping();                   \
+    void FinishEvent();                \
   };
 
 DECLARE_INTERCEPT_SYMBOLS(FairMCApplication)
 DECLARE_INTERCEPT_SYMBOLS(AliMC)
 
+// same for field
+#define DECLARE_INTERCEPT_FIELD_SYMBOLS(FIELD)                 \
+  class FIELD                                                  \
+  {                                                            \
+   public:                                                     \
+    void GetFieldValue(const double point[3], double* bField); \
+  };
+
+namespace o2
+{
+namespace field
+{
+DECLARE_INTERCEPT_FIELD_SYMBOLS(MagneticField);
+}
+}
 
 extern "C" void performLogging(TVirtualMCApplication*);
-extern "C" void dispatchOriginal(TVirtualMCApplication*, char const *libname, char const*);
+extern "C" void logField();
+extern "C" void dispatchOriginal(TVirtualMCApplication*, char const* libname, char const*);
+extern "C" void dispatchOriginalField(TVirtualMagField*, char const* libname, char const*, const double x[3],
+                                      double* B);
 extern "C" void flushLog();
 
-#define INTERCEPT_STEPPING(APP, LIB, SYMBOL)                                                 \
-  void APP::Stepping()                                                                       \
-  {                                                                                          \
-     auto baseptr=reinterpret_cast<TVirtualMCApplication*>(this);                            \
-     performLogging(baseptr);                                                                \
-     dispatchOriginal(baseptr, LIB, SYMBOL);                                                 \
-  }                                                                                          \
-
-#define INTERCEPT_FINISHEVENT(APP, LIB, SYMBOL)                                              \
-  void APP::FinishEvent()                                                                    \
-  {                                                                                          \
-     auto baseptr=reinterpret_cast<TVirtualMCApplication*>(this);                            \
-     flushLog();                                                                             \
-     dispatchOriginal(baseptr, LIB, SYMBOL);                                                 \
+#define INTERCEPT_STEPPING(APP, LIB, SYMBOL)                       \
+  void APP::Stepping()                                             \
+  {                                                                \
+    auto baseptr = reinterpret_cast<TVirtualMCApplication*>(this); \
+    performLogging(baseptr);                                       \
+    dispatchOriginal(baseptr, LIB, SYMBOL);                        \
   }
 
+#define INTERCEPT_FINISHEVENT(APP, LIB, SYMBOL)                    \
+  void APP::FinishEvent()                                          \
+  {                                                                \
+    auto baseptr = reinterpret_cast<TVirtualMCApplication*>(this); \
+    flushLog();                                                    \
+    dispatchOriginal(baseptr, LIB, SYMBOL);                        \
+  }
 
 // the runtime will now dispatch to these functions due to LD_PRELOAD
-INTERCEPT_STEPPING(FairMCApplication,"libBase","_ZN17FairMCApplication8SteppingEv")
-INTERCEPT_STEPPING(AliMC,"libSTEER","_ZN5AliMC8SteppingEv")
+INTERCEPT_STEPPING(FairMCApplication, "libBase", "_ZN17FairMCApplication8SteppingEv")
+INTERCEPT_STEPPING(AliMC, "libSTEER", "_ZN5AliMC8SteppingEv")
 
-INTERCEPT_FINISHEVENT(FairMCApplication,"libBase","_ZN17FairMCApplication11FinishEventEv")
-INTERCEPT_FINISHEVENT(AliMC,"libSTEER","_ZN5AliMC11FinishEventEv")
+INTERCEPT_FINISHEVENT(FairMCApplication, "libBase", "_ZN17FairMCApplication11FinishEventEv")
+INTERCEPT_FINISHEVENT(AliMC, "libSTEER", "_ZN5AliMC11FinishEventEv")
+
+#define INTERCEPT_FIELD(FIELD, LIB, SYMBOL)                        \
+  void FIELD::GetFieldValue(const double point[3], double* bField) \
+  {                                                                \
+    auto baseptr = reinterpret_cast<TVirtualMagField*>(this);      \
+    logField();                                                    \
+    dispatchOriginalField(baseptr, LIB, SYMBOL, point, bField);    \
+  }
+
+namespace o2
+{
+namespace field
+{
+INTERCEPT_FIELD(MagneticField, "libField", "_ZN2o25field13MagneticField13GetFieldValueEPKdPd");
+}
+}
