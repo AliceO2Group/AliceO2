240af3baddbf61dea9819ec9907bf049a2213d6f
Ruben Shahoyan <ruben.shahoyan@cern.ch>
2017-09-19 01:01:10 +0200

    Base class for ITS/MFT geom interface
    Derived from DetMatrixCache, responsible for providing transformation matrices and
    loading det-specific sensor segmenations. ITS and MFT should derive their own implementation
    of detector sensors layout and fillMatrixCache to populate caches.

---
+++ Detectors/ITSMFT/common/base/src/GeometryTGeo.cxx
@@ -0,0 +1,24 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file GeometryTGeo.cxx : common base for ITS and MFT interface to TGeometry
+/// \brief Implementation of the GeometryTGeo class
+/// \author cvetan.cheshkov@cern.ch - 15/02/2007
+/// \author ruben.shahoyan@cern.ch - adapted to ITSupg 18/07/2012
+
+#include "ITSMFTBase/GeometryTGeo.h"
+
+
+ClassImp(o2::ITSMFT::GeometryTGeo);
+
+using namespace o2::ITSMFT;
+
+std::array<std::string,GeometryTGeo::kNChipTypes> GeometryTGeo::sChipTypeName { "Pix" }; ///< upg detType Names
+
