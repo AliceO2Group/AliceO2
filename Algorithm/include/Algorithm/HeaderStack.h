c70fc8a7cc5749e1aa796f59aa1eb86f0776a5e8
Matthias Richter <Matthias.Richter@scieq.net>
2017-09-19 23:51:40 +0200

    Starting module 'Algorithm' for generic tools and utilities
    First algorithms:
    - parsing of O2 header stack
    - forward and reverse parsers for frame sequences consisting of
      header-payload-trailer composites

---
+++ Algorithm/include/Algorithm/HeaderStack.h
@@ -0,0 +1,169 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#ifndef ALGORITHM_HEADERSTACK_H
+#define ALGORITHM_HEADERSTACK_H
+
+/// @file   HeaderStack.h
+/// @author Matthias Richter
+/// @since  2017-09-19
+/// @brief  Utilities for the O2 header stack
+
+// the implemented functionality relies on o2::Header::get defined in
+// DataHeader.h; all O2 headers inherit from BaseHeader, this is required
+// to check the consistency of the header stack, also the next-header-flag
+// is part of the BaseHeader
+#include "Headers/DataHeader.h" // for o2::Header::get
+
+namespace o2 {
+
+namespace algorithm {
+
+/**
+ * Generic utility for the O2 header stack, redirect to header specific callbacks
+ *
+ * The O2 header stack consists of one or more headers. The DataHeader is the
+ * first one and mandatory. Other optional headers can be recursively extracted
+ * from a buffer with one call to this utility function. For each header a pair
+ * of type and callback has to be provided. The header type can be provided as
+ * a dummy parameter, the callback with a lambda or any callable object.
+ *
+ * Usage:
+ *   dispatchHeaderStackCallback(ptr, size,
+ *                               MyHeader(),
+ *                               [] (const auto & h) {
+ *                                 // do something with h
+ *                               }
+ */
+template<
+  typename PtrType,
+  typename SizeType,
+  typename HeaderType,
+  typename HeaderCallbackType,
+  typename... MoreTypes
+  >
+void dispatchHeaderStackCallback(PtrType ptr,
+                                 SizeType size,
+                                 HeaderType header,
+                                 HeaderCallbackType onHeader,
+                                 MoreTypes&&... types);
+
+// template specialization: handler for one pair of type and callback
+template<
+  typename PtrType,
+  typename SizeType,
+  typename HeaderType,
+  typename HeaderCallbackType
+  >
+void dispatchHeaderStackCallback(PtrType ptr,
+                                 SizeType size,
+                                 HeaderType /*dummy*/,
+                                 HeaderCallbackType onHeader)
+{
+  const HeaderType* h = o2::Header::get<HeaderType>(ptr, size);
+  if (h) {
+    onHeader(*h);
+  }
+}
+
+// an empty function in case no StackTypes have been provided to call
+template<typename PtrType, typename SizeType>
+void dispatchHeaderStackCallback(PtrType ptr, SizeType size) {}
+
+// actual implementation
+template<
+  typename PtrType,
+  typename SizeType,
+  typename HeaderType,
+  typename HeaderCallbackType,
+  typename... MoreTypes
+  >
+void dispatchHeaderStackCallback(PtrType ptr,
+                                 SizeType size,
+                                 HeaderType header,
+                                 HeaderCallbackType onHeader,
+                                 MoreTypes&&... types)
+{
+  // call for current
+  dispatchHeaderStackCallback(ptr, size, header, onHeader);
+  // call for next
+  dispatchHeaderStackCallback(ptr, size, types...);
+}
+
+/**
+ * Generic utility for the O2 header stack, extract headers
+ *
+ * The O2 header stack consists of one or more headers. The DataHeader is the
+ * first one and mandatory. Other optional headers can be recursively extracted
+ * from a buffer with one call to this utility function. For each header to be
+ * extracted, a variable can be passed be reference. If a header of corresponding
+ * type is in the stack, its content will be assigned to the variable.
+ *
+ * Usage:
+ *   parseHeaderStack(ptr, size,
+ *                   MyHeader(),
+ *                   [] (const auto & h) {
+ *                     // do something with h
+ *                   }
+ */
+template<
+  typename PtrType,
+  typename SizeType,
+  typename HeaderType,
+  typename... MoreTypes
+  >
+void parseHeaderStack(PtrType ptr,
+                      SizeType size,
+                      HeaderType & header,
+                      MoreTypes&&... types);
+
+// template specialization: handler for one type
+template<
+  typename PtrType,
+  typename SizeType,
+  typename HeaderType
+  >
+void parseHeaderStack(PtrType ptr,
+                      SizeType size,
+                      HeaderType & header)
+{
+  const HeaderType* h = o2::Header::get<HeaderType>(ptr, size);
+  if (h) {
+    header = *h;
+  }
+}
+
+// an empty function in case no StackTypes have been provided to call
+template<typename PtrType, typename SizeType>
+void parseHeaderStack(PtrType ptr, SizeType size) {}
+
+// generic implementation
+template<
+  typename PtrType,
+  typename SizeType,
+  typename HeaderType,
+  typename... MoreTypes
+  >
+void parseHeaderStack(PtrType ptr,
+                      SizeType size,
+                      HeaderType & header,
+                      MoreTypes&&... types)
+{
+  // call for current
+  parseHeaderStack(ptr, size, header);
+  // call for next
+  parseHeaderStack(ptr, size, types...);
+}
+
+}; // namespace algorithm
+
+}; // namespace o2
+
+#endif //ALGORITHM_HEADERSTACK_H
---
+++ Algorithm/test/parser.cxx
@@ -0,0 +1,227 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// @file   parser.cxx
+/// @author Matthias Richter
+/// @since  2017-09-20
+/// @brief  Unit test for data parsing methods in Algorithm/Parser.h
+
+#define BOOST_TEST_MODULE Test Algorithm Parser
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include "../include/Algorithm/Parser.h"
+
+namespace {
+
+/**
+ * Helper struct to define a composite element from a header, some payload
+ * and a trailer
+ */
+template <typename HeaderT
+          , typename TrailerT>
+struct Composite {
+  using HeaderType = HeaderT;
+  using TrailerType = TrailerT;
+  size_t length = 0;
+
+  template<size_t N>
+  constexpr Composite(const HeaderType& h, const char  (&d)[N], const TrailerType& t)
+    : header(h)
+    , data(d)
+    , trailer(t)
+  {
+    length = sizeof(HeaderType) + N + sizeof(TrailerType);
+  }
+
+  constexpr size_t getDataLength() const noexcept {
+    return length - sizeof(HeaderType) - sizeof(TrailerType);
+  }
+
+  const HeaderType& header;
+  const char* data = nullptr;
+  const TrailerType& trailer;
+};
+
+/// recursively calculate the length of the sequence
+template <typename T, typename... TArgs>
+constexpr size_t sequenceLength(const T& first, const TArgs... args) noexcept {
+  return sequenceLength(first) + sequenceLength(args...);
+}
+
+/// terminating template secialization of sequence length calculation
+template <typename T>
+constexpr size_t sequenceLength(const T& first) noexcept {
+  return first.length;
+}
+
+/// recursive insert of variable number of elements
+template <typename InputType, typename T, typename... TArgs>
+constexpr size_t sequenceInsert(InputType* buffer, const T& first, const TArgs... args) noexcept {
+  size_t length = sequenceLength(first);
+  sequenceInsert(buffer, first);
+  buffer += length;
+  length += sequenceInsert(buffer, args...);
+  return length;
+}
+
+/// terminating template specialization, i.e. for the last element
+template <typename InputType, typename T>
+constexpr size_t sequenceInsert(InputType* buffer, const T& element) noexcept {
+  size_t length = 0;
+  memcpy(buffer + length, &element.header, sizeof(typename T::HeaderType));
+  length += sizeof(typename T::HeaderType);
+  memcpy(buffer + length, element.data, element.getDataLength());
+  length += element.getDataLength();
+  memcpy(buffer + length, &element.trailer, sizeof(typename T::TrailerType));
+  length += sizeof(typename T::TrailerType);
+  return length;
+}
+
+/**
+ * Helper struct to create a buffer from multiple blocks
+ */
+struct TestFrame {
+  using BufferType = std::unique_ptr<unsigned char[]>;
+
+  BufferType buffer;
+  size_t length;
+
+  TestFrame() = delete;
+
+  template <typename CompositeType, typename... Targs>
+  TestFrame(CompositeType block, Targs... args)
+  {
+    length = sequenceLength(block, args...);
+    std::cout << "make TestFrame length " << length << std::endl;
+    buffer  = std::make_unique<BufferType::element_type[]>(length);
+    sequenceInsert(buffer.get(), block, args...);
+  }
+};
+
+// header test class
+struct Header {
+  unsigned identifier = 0xdeadbeef;
+  size_t payloadSize = 0;
+
+  Header(size_t ps) : payloadSize(ps) {}
+};
+
+// trailer test class
+struct Trailer {
+  unsigned identifier = 0xaaffee00;
+  unsigned char flags = 0xaa;
+
+  Trailer(unsigned char f) : flags(f) {}
+};
+
+// trailer test class including payload size
+struct SizedTrailer {
+  unsigned identifier = 0xaaffee00;
+  unsigned char flags = 0xaa;
+  size_t payloadSize = 0;
+
+  SizedTrailer(size_t s, unsigned char f) : payloadSize(s), flags(f) {}
+};
+
+BOOST_AUTO_TEST_CASE(test_forwardparser_header_and_trailer)
+{
+  using FrameT = Composite<Header, Trailer>;
+  // note: the length of the data is set in the header word
+  TestFrame tf(FrameT({16}, "lotsofsillydata", {0xaa}),
+               FrameT({5},  "test",            {0xcc}),
+               FrameT({10}, "dummydata",       {0x33})
+               );
+
+  using ParserT = o2::algorithm::ForwardParser<typename FrameT::HeaderType,
+					       typename FrameT::TrailerType>;
+
+  auto checkHeader = [] (const typename FrameT::HeaderType& header) {
+    return header.identifier == 0xdeadbeef;
+  };
+  auto checkTrailer = [] (const typename FrameT::TrailerType& trailer) {
+    return trailer.identifier == 0xaaffee00;
+  };
+  auto getFrameSize = [] (const typename ParserT::HeaderType& header) {
+    return header.payloadSize + ParserT::totalOffset;
+  };
+
+  std::vector<typename ParserT::FrameInfo> frames;
+  auto insert = [&frames] (typename ParserT::FrameInfo& info) {
+    frames.emplace_back(info);
+    return true;
+  };
+
+  ParserT parser;
+  auto result = parser.parse(tf.buffer.get(), tf.length,
+                             checkHeader,
+	                     checkTrailer,
+	                     getFrameSize,
+                             insert
+	                     );
+
+  BOOST_REQUIRE(result == 3);
+  BOOST_REQUIRE(frames.size() == 3);
+
+  BOOST_CHECK(memcmp(frames[0].payload, "lotsofsillydata", frames[0].length) == 0);
+  BOOST_CHECK(memcmp(frames[1].payload, "test",            frames[1].length) == 0);
+  BOOST_CHECK(memcmp(frames[2].payload, "dummydata",       frames[2].length) == 0);
+}
+
+// TODO: make a test of a composite with header and payload
+
+BOOST_AUTO_TEST_CASE(test_reverseparser)
+{
+  using FrameT = Composite<Header, SizedTrailer>;
+  // note: the length of the data is set in the trailer word
+  TestFrame tf(FrameT({0}, "lotsofsillydata", {{16}, 0xaa}),
+               FrameT({0}, "test",            {{5},  0xcc}),
+               FrameT({0}, "dummydata",       {{10}, 0x33})
+               );
+
+  using ParserT = o2::algorithm::ReverseParser<typename FrameT::HeaderType,
+					       typename FrameT::TrailerType>;
+
+  auto checkHeader = [] (const typename FrameT::HeaderType& header) {
+    return header.identifier == 0xdeadbeef;
+  };
+  auto checkTrailer = [] (const typename FrameT::TrailerType& trailer) {
+    return trailer.identifier == 0xaaffee00;
+  };
+  auto getFrameSize = [] (const typename ParserT::TrailerType& trailer) {
+    return trailer.payloadSize + ParserT::totalOffset;
+  };
+
+  std::vector<typename ParserT::FrameInfo> frames;
+  auto insert = [&frames] (const typename ParserT::FrameInfo& info) {
+    frames.emplace_back(info);
+    return true;
+  };
+
+  ParserT parser;
+  auto result = parser.parse(tf.buffer.get(), tf.length,
+                             checkHeader,
+	                     checkTrailer,
+	                     getFrameSize,
+                             insert
+	                     );
+
+  BOOST_REQUIRE(result == 3);
+  BOOST_REQUIRE(frames.size() == 3);
+
+  BOOST_CHECK(memcmp(frames[2].payload, "lotsofsillydata", frames[2].length) == 0);
+  BOOST_CHECK(memcmp(frames[1].payload, "test",            frames[1].length) == 0);
+  BOOST_CHECK(memcmp(frames[0].payload, "dummydata",       frames[0].length) == 0);
+}
+
+} // unnamed namespace
