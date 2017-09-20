fad5e822634e73cb9fb36500dd39677729680234
Sandro Wenzel <sandro.wenzel@cern.ch>
2017-09-20 22:05:33 +0200

    Framework: fix compiler warning
    fix warning about missing return

--- Framework/Core/src/DummyDebugGUI.cxx
+++ Framework/Core/src/DummyDebugGUI.cxx
@@ -12,7 +12,7 @@
 namespace o2 {
 namespace framework {
 
-void *initGUI(const char *) {}
+void *initGUI(const char *) {return nullptr;}
 bool pollGUI(void *context, std::function<void(void)> guiCallback) {
   return true;
 }
