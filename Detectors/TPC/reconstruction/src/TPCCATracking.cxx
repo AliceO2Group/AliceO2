da3cd8647e50caf4ab58c82d8d49900fbde5f85a
David Rohr <drohr@jwdt.org>
2017-10-06 19:06:39 +0200

    update copyright notice, comply with rule checker everywhere: replace one NULL by nullptr

--- Detectors/TPC/reconstruction/src/TPCCATracking.cxx
+++ Detectors/TPC/reconstruction/src/TPCCATracking.cxx
@@ -2,7 +2,7 @@
 // distributed under the terms of the GNU General Public License v3 (GPL
 // Version 3), copied verbatim in the file "COPYING".
 //
-// See https://alice-o2.web.cern.ch/ for full licensing information.
+// See http://alice-o2.web.cern.ch/license for full licensing information.
 //
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
@@ -66,7 +66,7 @@ int TPCCATracking::initialize(const char* options) {
 void TPCCATracking::deinitialize() {
   mTrackingCAO2Interface.reset();
   mClusterData_UPTR.reset();
-  mClusterData = NULL;
+  mClusterData = nullptr;
 }
 
 int TPCCATracking::runTracking(const TClonesArray* inputClusters, std::vector<TrackTPC>* outputTracks) {
