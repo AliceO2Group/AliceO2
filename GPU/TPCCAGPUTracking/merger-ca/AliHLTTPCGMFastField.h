b71e9c71f2a91790a5253415e421ba026fd3b781
sgorbuno <sergey.gorbunov@fias.uni-frankfurt.de>
2017-10-13 16:36:04 +0200

    update of fast field class: new polynom, new coefficients for Bx By Bz

--- GPU/TPCCAGPUTracking/merger-ca/AliHLTTPCGMFastField.h
+++ GPU/TPCCAGPUTracking/merger-ca/AliHLTTPCGMFastField.h
@@ -11,8 +11,6 @@
 #ifndef AliHLTTPCGMFastField_H
 #define AliHLTTPCGMFastField_H
 
-#include "AliHLTTPCCAMath.h"
-
 /**
  * @class AliHLTTPCGMFastField
  *
@@ -26,53 +24,50 @@ public:
   
   void Init();
 
-  float GetBx( float x, float y, float z ) const;
-  float GetBy( float x, float y, float z ) const;
-  float GetBz( float x, float y, float z ) const;
-
+  void GetField( float x, float y, float z, float B[] ) const;
   void DumpField( const char *fileName="field.root" ) const;
 
 private:
-  
-  float fBx[6];
-  float fBy[6];
-  float fBz[6];
+
+  static const int fkM = 9;
+  float fBx0;
+  float fBy0;
+  float fBz0; 
+  float fBx[fkM];
+  float fBy[fkM];
+  float fBz[fkM];
 };
 
 inline void AliHLTTPCGMFastField::Init()
 {
-  const float cBx[6] = { 0, 0, 0, 0, 0, 0};
-  const float cBy[6] = { 0, 0, 0, 0, 0, 0};
-  const float cBz[6] = { 0.999286, -4.54386e-7, 2.32950e-5, -2.99912e-7, -2.03442e-8, 9.71402e-8 };
-  for( int i=0; i<6; i++ ){
-    fBx[i] = cBx[i];
-    fBy[i] = cBy[i];
-    fBz[i] = cBz[i];
+  const float cBx[fkM+1] = { -2.58322252193e-05, 2.25564940592e-06, -4.14718357433e-08, -2.75251750281e-06,
+			   -8.72029382037e-09, 1.72417402577e-09, 3.19352068345e-07, -3.28086002810e-09, 5.64790381130e-10, 8.92192542068e-09 };
+
+  const float cBy[fkM+1] = { 6.37950097371e-06, -4.46194050596e-08, 9.01212274584e-07, 8.26001087262e-06,
+			   7.99017740860e-10, -7.45108241773e-09, 4.81764572680e-10, 8.35443714209e-10, 3.14677095048e-07, -1.18421328299e-09 };
+  
+  const float cBz[fkM+1] = { 9.99663949013e-01, -3.54553162651e-06, 7.73496958573e-06, -2.90551361104e-06,
+			   1.69738939348e-07, 5.00871899511e-10, 2.10037196524e-08, 1.66827078374e-07, -2.64136179595e-09, -3.02637317873e-07 };
+  fBx0 = cBx[0];
+  fBy0 = cBy[0];
+  fBz0 = cBz[0];
+  for( int i=0; i<fkM; i++ ) fBx[i] = cBx[i+1];
+  for( int i=0; i<fkM; i++ ) fBy[i] = cBy[i+1];
+  for( int i=0; i<fkM; i++ ) fBz[i] = cBz[i+1];
+}
+
+inline void AliHLTTPCGMFastField::GetField( float x, float y, float z, float B[] ) const
+{
+  float f[fkM] = { x, y, z, x*x, x*y, x*z, y*y, y*z, z*z };
+  float bx = 0.f, by = 0.f, bz = 0.f;
+  for( int i=0; i<fkM; i++){
+    bx+= fBx[i]*f[i];
+    by+= fBy[i]*f[i];
+    bz+= fBz[i]*f[i];
   }
-}
-
-inline float AliHLTTPCGMFastField::GetBx( float x, float y, float z ) const
-{
-  float r2 = x * x + y * y;
-  float r  = sqrt( r2 );
-  const float *c = fBx;
-  return ( c[0] + c[1]*z  + c[2]*r  + c[3]*z*z + c[4]*z*r + c[5]*r2 );
-}
-
-inline float AliHLTTPCGMFastField::GetBy( float x, float y, float z ) const
-{
-  float r2 = x * x + y * y;
-  float r  = sqrt( r2 );
-  const float *c = fBy;
-  return ( c[0] + c[1]*z  + c[2]*r  + c[3]*z*z + c[4]*z*r + c[5]*r2 );
-}
-
-inline float AliHLTTPCGMFastField::GetBz( float x, float y, float z ) const
-{
-  float r2 = x * x + y * y;
-  float r  = sqrt( r2 );
-  const float *c = fBz;
-  return ( c[0] + c[1]*z  + c[2]*r  + c[3]*z*z + c[4]*z*r + c[5]*r2 );
+  B[0] = fBx0 + bx;
+  B[1] = fBy0 + by;
+  B[2] = fBz0 + bz;
 }
 
 #endif 
