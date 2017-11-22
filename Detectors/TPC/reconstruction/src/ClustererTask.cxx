f0d5f798484e84cb471af0c810804fe72c64a77b
Sandro Wenzel <sandro.wenzel@cern.ch>
2017-11-22 09:11:08 +0100

    Change branch name for TPC cluster MC labels; Remove empty file
    This is a small usability improvement: We need to add a trailing
    dot (.) to some branch names (when they are a TNamed) otherwise ROOT
    cannot draw from these branches in a TBrowser
    
    Remove empty source file

--- Detectors/TPC/reconstruction/src/ClustererTask.cxx
+++ Detectors/TPC/reconstruction/src/ClustererTask.cxx
@@ -90,7 +90,7 @@ InitStatus ClustererTask::Init()
 
     // Register MC Truth output container
     mClustersMCTruthArray = std::make_unique<MCLabelContainer>();
-    mgr->Register("TPCClusterMCTruth", "TPC", mClustersMCTruthArray.get(), kTRUE);
+    mgr->Register("TPCClusterMCTruth.", "TPC", mClustersMCTruthArray.get(), kTRUE);
 
     // create clusterer and pass output pointer
     mBoxClusterer = std::make_unique<BoxClusterer>(mClustersArray);
@@ -103,7 +103,7 @@ InitStatus ClustererTask::Init()
 
     // Register MC Truth output container
     mHwClustersMCTruthArray = std::make_unique<MCLabelContainer>();
-    mgr->Register("TPCClusterHWMCTruth", "TPC", mHwClustersMCTruthArray.get(), kTRUE);
+    mgr->Register("TPCClusterHWMCTruth.", "TPC", mHwClustersMCTruthArray.get(), kTRUE);
 
      // create clusterer and pass output pointer
     mHwClusterer = std::make_unique<HwClusterer>(mHwClustersArray,mHwClustersMCTruthArray.get());//,0,359);
