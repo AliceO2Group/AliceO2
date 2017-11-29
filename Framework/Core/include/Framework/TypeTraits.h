b3f59cfaa6dc59b398217bb5e7f790d2b02b3e26
Giulio Eulisse <giulio.eulisse@cern.ch>
2017-11-29 10:14:43 +0100

    Add type trait to check for template specialization
    This is useful to specialize behavior of templated functions based on
    the kind of collection they belong to.

---
+++ Framework/Core/include/Framework/TypeTraits.h
@@ -0,0 +1,26 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#include <type_traits>
+
+namespace o2 {
+namespace framework {
+/// Helper trait to determine if a given type T
+/// is a specialization of a given reference type Ref.
+/// See Framework/Core/test_TypeTraits.cxx for an example
+
+template<typename T, template<typename...> class Ref>
+struct is_specialization : std::false_type {};
+
+template<template<typename...> class Ref, typename... Args>
+struct is_specialization<Ref<Args...>, Ref>: std::true_type {};
+
+}
+}
