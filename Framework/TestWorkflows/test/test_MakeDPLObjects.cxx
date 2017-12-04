1ae8cb2d35d7f2d336367256571d7a0bf63cc25c
Giulio Eulisse <giulio.eulisse@cern.ch>
2017-12-04 12:34:11 +0100

    Improve DataAllocator API for std::vector, gsl::span (#701)
    DataAllocator now handles `std::vector` and `gsl::span`. The
    rationale is:
    
    - Anything which is created and owned by the framework will be exposed
    as a `gsl::span`.
    - Anything which is adopted by the framework has to be passed as either
    a `gsl::span` or an `std::vector`.
    - Old `o2::framework::Collection<T>` is removed (since it was basically a
    `gsl::span`.

--- Framework/TestWorkflows/test/test_MakeDPLObjects.cxx
+++ Framework/TestWorkflows/test/test_MakeDPLObjects.cxx
@@ -9,9 +9,9 @@
 // or submit itself to any jurisdiction.
 #include "Framework/runDataProcessing.h"
 #include "Framework/DataRefUtils.h"
-#include "Framework/Collection.h"
 #include "Framework/ControlService.h"
 #include <TH1F.h>
+#include <gsl/gsl>
 
 using namespace o2::framework;
 
@@ -29,17 +29,26 @@ void defineDataProcessing(std::vector<DataProcessorSpec> &specs) {
       {
         OutputSpec{"TST", "HISTO"},
         OutputSpec{"TST", "POINT"},
-        OutputSpec{"TST", "POINTS"}
+        OutputSpec{"TST", "POINTS"},
+        OutputSpec{"TST", "VECTOR"}
       },
       AlgorithmSpec{
         [](ProcessingContext &ctx) {
           // A new message with 1 XYZ instance in it
           XYZ &x = ctx.allocator().make<XYZ>(OutputSpec{"TST", "POINT", 0});
-          // A new message with a Collection<XYZ> with 1000 items
-          Collection<XYZ> y = ctx.allocator().make<XYZ>(OutputSpec{"TST", "POINTS", 0}, 1000);
+          // A new message with a gsl::span<XYZ> with 1000 items
+          gsl::span<XYZ> y = ctx.allocator().make<XYZ>(OutputSpec{"TST", "POINTS", 0}, 1000);
+          y[0] = XYZ{1,2,3};
+          y[999] = XYZ{1,2,3};
           // A new message with a TH1F inside
           auto h = ctx.allocator().make<TH1F>(OutputSpec{"TST", "HISTO"},
                                              "h", "test", 100, -10., 10.);
+          // A snapshot for an std::vector
+          std::vector<XYZ> v{1000};
+          v[0] = XYZ{1,2,3};
+          v[999] = XYZ{1,2,3};
+          ctx.allocator().snapshot(OutputSpec{"TST", "VECTOR"}, v);
+          v[999] = XYZ{2,3,4};
         }
       }
     },
@@ -48,7 +57,8 @@ void defineDataProcessing(std::vector<DataProcessorSpec> &specs) {
       Inputs{
         InputSpec{"point", "TST", "POINT"},
         InputSpec{"points", "TST", "POINTS"},
-        InputSpec{"histo", "TST", "HISTO"}
+        InputSpec{"histo", "TST", "HISTO"},
+        InputSpec{"vector", "TST", "VECTOR"},
       },
       {},
       AlgorithmSpec{
@@ -57,9 +67,23 @@ void defineDataProcessing(std::vector<DataProcessorSpec> &specs) {
           auto h = ctx.inputs().get<TH1F>("histo");
           // A new message with 1 XYZ instance in it
           XYZ const &x = ctx.inputs().get<XYZ>("point");
-          // A new message with a Collection<XYZ> with 1000 items
-          auto ref = ctx.inputs().get("points");
-          Collection<XYZ> c = DataRefUtils::as<XYZ>(ref);
+          // A new message with a gsl::span<XYZ> with 1000 items
+          auto ref1 = ctx.inputs().get("points");
+          gsl::span<XYZ> c = DataRefUtils::as<XYZ>(ref1);
+          assert(c[0].x == 1);
+          assert(c[0].y == 2);
+          assert(c[0].z == 3);
+          assert(c[999].x == 1);
+          assert(c[999].y == 2);
+          assert(c[999].z == 3);
+          auto ref2 = ctx.inputs().get("vector");
+          gsl::span<XYZ> c2 = DataRefUtils::as<XYZ>(ref2);
+          assert(c2[0].x == 1);
+          assert(c2[0].y == 2);
+          assert(c2[0].z == 3);
+          assert(c2[999].x == 1);
+          assert(c2[999].y == 2);
+          assert(c2[999].z == 3);
           ctx.services().get<ControlService>().readyToQuit(true);
         }
       }
