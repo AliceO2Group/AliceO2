d92dad67496a5b163be5954a1b617dff2263fc62
dstocco <diego.stocco@cern.ch>
2017-12-11 17:43:02 +0100

    Add fast track generator for MID

---
+++ Detectors/MUON/MID/TestingSimTools/include/MIDTestingSimTools/TrackGenerator.h
@@ -0,0 +1,67 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file   MIDTestingSimTools/TrackGenerator.h
+/// \brief  Fast track generator for MID
+/// \author Diego Stocco <Diego.Stocco at cern.ch>
+/// \date   11 December 2017
+
+#ifndef O2_MID_TRACKGENERATOR_H
+#define O2_MID_TRACKGENERATOR_H
+
+#include <array>
+#include <vector>
+#include <random>
+#include "DataFormatsMID/Track.h"
+
+namespace o2
+{
+namespace mid
+{
+/// Class to generate tracks for MID
+class TrackGenerator
+{
+ public:
+  TrackGenerator();
+  virtual ~TrackGenerator() = default;
+
+  TrackGenerator(const TrackGenerator&) = delete;
+  TrackGenerator& operator=(const TrackGenerator&) = delete;
+  TrackGenerator(TrackGenerator&&) = delete;
+  TrackGenerator& operator=(TrackGenerator&&) = delete;
+
+  std::vector<Track> generate();
+  std::vector<Track> generate(int nTracks);
+
+  /// Sets the mean number of track per events
+  void setMeanTracksPerEvent(int meanTracksPerEvent) { mMeanTracksPerEvent = meanTracksPerEvent; }
+  /// Sets the limits of the track slope
+  void setSlopeLimits(float slopeXmin, float slopeXmax, float slopeYmin, float slopeYmax)
+  {
+    mSlopeLimits = { { slopeXmin, slopeXmax, slopeYmin, slopeYmax } };
+  }
+  /// Sets the limits of the track origin
+  void setPositionLimits(float xMin, float xMax, float yMin, float yMax, float zMin, float zMax)
+  {
+    mPositionLimits = { { xMin, xMax, yMin, yMax, zMin, zMax } };
+  }
+
+ private:
+  std::array<float, 4> getLimitsForAcceptance(std::array<float, 3> pos);
+
+  int mMeanTracksPerEvent;               ///< Mean tracks per event
+  std::array<float, 4> mSlopeLimits;     ///< Limits for track slope
+  std::array<float, 6> mPositionLimits;  ///< x,y,z position limits
+  std::default_random_engine mGenerator; ///< Random numbers generator
+};
+} // namespace mid
+} // namespace o2
+
+#endif /* O2_MID_TRACKGENERATOR_H */
---
+++ Detectors/MUON/MID/TestingSimTools/src/TrackGenerator.cxx
@@ -0,0 +1,96 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file   MID/TestingSimTools/src/TrackGenerator.cxx
+/// \brief  Implementation of a fast track generator for MID
+/// \author Diego Stocco <Diego.Stocco at cern.ch>
+/// \date   11 December 2017
+
+#include "MIDTestingSimTools/TrackGenerator.h"
+
+namespace o2
+{
+namespace mid
+{
+//______________________________________________________________________________
+TrackGenerator::TrackGenerator()
+  : mMeanTracksPerEvent(1),
+    mSlopeLimits{ { -0.2, 0.2, -0.5, 0.5 } },
+    mPositionLimits{ { -2., 2, -2., 2., -5., 5. } },
+    mGenerator(std::default_random_engine())
+{
+  /// Default constructor
+}
+
+//______________________________________________________________________________
+std::vector<Track> TrackGenerator::generate()
+{
+  /// Generate tracks. The number of tracks follows a poissonian distribution
+  std::poisson_distribution<> distTracks(mMeanTracksPerEvent);
+  int nTracks = distTracks(mGenerator);
+  return generate(nTracks);
+}
+
+//______________________________________________________________________________
+std::vector<Track> TrackGenerator::generate(int nTracks)
+{
+  /// Generate N tracks
+  /// @param nTracks Number of tracks to generate
+  std::vector<Track> tracks;
+  Track track;
+  std::array<float, 3> pos;
+  std::array<float, 2> dir;
+  for (int itrack = 0; itrack < nTracks; ++itrack) {
+    for (int ipos = 0; ipos < 3; ++ipos) {
+      pos[ipos] =
+        std::uniform_real_distribution<float>{ mPositionLimits[2 * ipos], mPositionLimits[2 * ipos + 1] }(mGenerator);
+    }
+    track.setPosition(pos[0], pos[1], pos[2]);
+
+    std::array<float, 4> limits = getLimitsForAcceptance(pos);
+
+    for (int idir = 0; idir < 2; ++idir) {
+      dir[idir] = std::uniform_real_distribution<float>{ limits[2 * idir], limits[2 * idir + 1] }(mGenerator);
+    }
+    track.setDirection(dir[0], dir[1], 1.);
+    tracks.push_back(track);
+  }
+  return tracks;
+}
+
+//______________________________________________________________________________
+std::array<float, 4> TrackGenerator::getLimitsForAcceptance(std::array<float, 3> pos)
+{
+  /// Restricts the limits of the slopes
+  /// so that the track enters in the spectrometer acceptance
+  std::array<float, 4> limits = mSlopeLimits;
+  float dZ = -1600. - pos[2];
+  if (dZ == 0.) {
+    dZ = 0.0001;
+  }
+
+  // These are the maximum x and y position of MT11 with the standard alignment
+  std::array<float, 2> maxValues{ { 257., 306.7 } };
+  for (int icoor = 0; icoor < 2; ++icoor) {
+    int imin = 2 * icoor;
+    int imax = 2 * icoor + 1;
+    float minSlope = (pos[icoor] - maxValues[icoor]) / dZ;
+    float maxSlope = (maxValues[icoor] - pos[icoor]) / dZ;
+    if (minSlope > limits[imin]) {
+      limits[imin] = minSlope;
+    }
+    if (maxSlope < limits[imax]) {
+      limits[imax] = maxSlope;
+    }
+  }
+  return limits;
+}
+} // namespace mid
+} // namespace o2
