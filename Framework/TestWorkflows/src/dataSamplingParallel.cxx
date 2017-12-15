098468697d4d808f666848756e52cf54cfc73a10
Piotr Konopka <piotr.jan.konopka@cern.ch>
2017-12-15 12:35:42 +0100

    Minor fix of path and CMake clean-up

--- Framework/TestWorkflows/src/dataSamplingParallel.cxx
+++ Framework/TestWorkflows/src/dataSamplingParallel.cxx
@@ -154,7 +154,7 @@ void defineDataProcessing(std::vector<DataProcessorSpec> &specs)
 
   std::vector<std::string> taskNames = {"simpleQcTask"};
   //todo: get path as argument?
-  std::string configurationSource = "file:///home/pkonopka/alice/O2/Framework/Core/test/exampleDataSamplerConfig.ini";
+  std::string configurationSource = "file:///home/pkonopka/alice/O2/Framework/TestWorkflows/exampleDataSamplerConfig.ini";
 
   DataSampling::GenerateInfrastructure(specs, configurationSource, taskNames);
 }
