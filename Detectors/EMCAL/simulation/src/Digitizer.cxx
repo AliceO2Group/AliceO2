03355519515102be1ddc5d3d7e40d085c20bda2f
Anders Knospe <anders.knospe@cern.ch>
2018-01-06 12:24:19 +0100

    Revert "modified digit storage structure"
    This reverts commit 3c60bf9ce85d38515fd78a1f1bf8271dce467d22.

--- Detectors/EMCAL/simulation/src/Digitizer.cxx
+++ Detectors/EMCAL/simulation/src/Digitizer.cxx
@@ -33,48 +33,108 @@ using namespace o2::EMCAL;
 //_______________________________________________________________________
 void Digitizer::init()
 {
-  for (auto tower=mDigits.begin(); tower!=mDigits.end(); ++tower){
-    *tower = std::make_unique< std::vector<Digit> >();
+  for (Int_t tower=0; tower<mGeometry->GetNCells(); tower++) mDigits[tower]=new std::vector<Digit>;
+  /*
+  const Int_t numOfChips = mGeometry->getNumberOfChips();
+
+  if (mParams.getHit2DigitsMethod() == DigiParams::p2dCShape && !mParams.getAlpSimResponse()) {
+    mAlpSimResp = std::make_unique<o2::ITSMFT::AlpideSimResponse>();
+    mAlpSimResp->initData();
+    mParams.setAlpSimResponse(mAlpSimResp.get());
   }
+    
+  for (Int_t i = 0; i < numOfChips; i++) {
+    mSimulations.emplace_back(&mParams, i, &mGeometry->getMatrixL2G(i));
+  }
+  */
 }
 
 //_______________________________________________________________________
 void Digitizer::finish()
 {
+  for(Int_t tower=0; tower<mGeometry->GetNCells(); tower++) delete mDigits[tower];
 }
 
 //_______________________________________________________________________
 void Digitizer::process(const std::vector<Hit>* hits, std::vector<Digit>* digits)
 {
-  digits->clear();
-  for (auto tower=mDigits.begin(); tower!=mDigits.end(); ++tower){
-    (*tower)->clear();
-  }
+  Int_t tower;
 
+  digits->clear();
+  for(tower=0; tower<mGeometry->GetNCells(); tower++) mDigits[tower]->clear();
+
+  std::vector<Digit>::reverse_iterator rit;
+  bool flag;
+  
   for(auto hit : *hits) {
     Digit digit = HitToDigit(hit);
-    Int_t id = digit.GetTower();
+    tower = digit.GetTower();
 
-    try {
-      mDigits.at(id);
-    }
-    catch (const std::out_of_range& oor) {
-      LOG(WARNING) << "tower index out of range: " << id << FairLogger::endl;
-      continue;
-    }
-
-    Bool_t flag=false;
-    for(auto rit=(mDigits.at(id))->rbegin(); rit!=(mDigits.at(id))->rend(); ++rit){
+    flag=false;
+    for(rit=mDigits[tower]->rbegin(); rit!=mDigits[tower]->rend(); ++rit){
       if(rit->CanAdd(digit)) {
 	(*rit) += digit;
 	flag = true;
 	break;
       }
     }
-    if(!flag) (mDigits.at(id))->push_back(digit);
+    if(!flag) mDigits[tower]->push_back(digit);
   }
 
   fillOutputContainer(digits);
+
+  /*
+  // digitize single event
+  
+  const Int_t numOfChips = mGeometry->getNumberOfChips();  
+
+  // estimate the smalles RO Frame this event may have
+  double hTime0 = mEventTime - mParams.getTimeOffset();
+  if (hTime0 > UINT_MAX) {
+    LOG(WARNING) << "min Hit RO Frame undefined: time: " << hTime0 << " is in far future: "
+		 << " EventTime: " << mEventTime << " TimeOffset: "
+		 << mParams.getTimeOffset() << FairLogger::endl;
+    return;
+  }
+
+  if (hTime0<0) hTime0 = 0.;
+  UInt_t minNewROFrame = static_cast<UInt_t>(hTime0/mParams.getROFrameLenght());
+
+  LOG(INFO) << "Digitizing ITS event at time " << mEventTime
+	    << " (TOffset= " << mParams.getTimeOffset() << " ROFrame= " << minNewROFrame << ")"
+	    << " cont.mode: " << isContinuous() << " current Min/Max RO Frames "
+	    << mROFrameMin << "/" << mROFrameMax << FairLogger::endl ;
+  
+  if (mParams.isContinuous() && minNewROFrame>mROFrameMin) {
+    // if there are already digits cached for previous RO Frames AND the new event
+    // cannot contribute to these digits, move them to the output container
+    if (mROFrameMax<minNewROFrame) mROFrameMax = minNewROFrame-1;
+    for (auto rof=mROFrameMin; rof<minNewROFrame; rof++) {
+      fillOutputContainer(digits, rof);
+    }
+    //    fillOutputContainer(digits, minNewROFrame-1);
+  }
+  
+  // accumulate hits for every chip
+  for(auto& hit : *hits) {
+    // RS: ATTENTION: this is just a trick until we clarify how the hits from different source are
+    // provided and identified. At the moment we just create a combined identifier from eventID
+    // and sourceID and store it TEMPORARILY in the cached Point's TObject UniqueID
+    const_cast<Hit&>(hit).SetSrcEvID(mCurrSrcID,mCurrEvID); 
+    mSimulations[hit.GetDetectorID()].InsertHit(&hit);
+  }
+    
+  // Convert hits to digits  
+  for (auto &simulation : mSimulations) {
+    simulation.Hits2Digits(mEventTime, mROFrameMin, mROFrameMax);
+    simulation.ClearHits();
+  }
+
+  // in the triggered mode store digits after every MC event
+  if (!mParams.isContinuous()) {
+    fillOutputContainer(digits, mROFrameMax);
+  }
+  */
 }
 
 //_______________________________________________________________________
@@ -98,21 +158,34 @@ void Digitizer::setEventTime(double t)
     LOG(FATAL) << "New event time (" << t << ") is < previous event time (" << mEventTime << ")" << FairLogger::endl;
   }
   mEventTime = t;
+  /*
+  // to limit the range of RO Frame IDs we subtract the meaningless offset
+  if (mContinuous) { // in continuous mode we set the offset only in the very beginning
+    if (!mParams.isTimeOffsetSet()) { // offset is initially at -inf
+      mParams.setTimeOffset(mEventTime + mParams.getROFrameLenght()*(gRandom->Rndm()-0.5));
+    }
+  }
+  else { // in the triggered mode we start from 0 ROFrame in every event
+    mParams.setTimeOffset( mEventTime + mParams.getROFrameLenght()*(gRandom->Rndm()-0.5));
+    mROFrameMin = 0;  // so we reset the frame counters
+    mROFrameMax = 0;
+  }
+  */
 }
 
 //_______________________________________________________________________
 void Digitizer::fillOutputContainer(std::vector<Digit>* digits)
 {
   std::forward_list<Digit> l;
+  std::vector<Digit>::reverse_iterator vit;
 
-  for (auto tower=mDigits.rbegin(); tower!=mDigits.rend(); ++tower){
-    std::vector<Digit> v = *(*tower);
-    for(auto vit=v.rbegin(); vit!=v.rend(); ++vit) l.push_front(*vit);
+  for(Int_t tower=mGeometry->GetNCells()-1; tower>=0; tower--) {
+    for(vit=mDigits[tower]->rbegin(); vit!=mDigits[tower]->rend(); ++vit) l.push_front(*vit);
   }
 
   l.sort();
 
-  for(auto digit : l) digits->push_back(digit);
+  for(std::forward_list<Digit>::iterator lit=l.begin(); lit!=l.end(); ++lit) digits->push_back(*lit);
 }
 
 //_______________________________________________________________________
