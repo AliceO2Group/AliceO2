e9b79962d6a49c3d4091fd3ca471863b696f6772
Anders Knospe <anders.knospe@cern.ch>
2018-01-06 12:50:18 +0100

    Revert "modification to structure of EMCal Digit class"
    This reverts commit 5e0220b693e07c91e47a8b213f9d1c9d4e743341.

--- Detectors/EMCAL/base/include/EMCALBase/Digit.h
+++ Detectors/EMCAL/base/include/EMCALBase/Digit.h
@@ -19,44 +19,47 @@
 #include <boost/serialization/base_object.hpp>  // for base_object
 #endif
 
+
 namespace o2 {
   namespace EMCAL {
+    
     /// \class Digit
     /// \brief EMCAL digit implementation
     class Digit : public FairTimeStamp {
     public:
       Digit() = default;
-
-      Digit(Int_t tower, Double_t amplitude, Double_t time);
+      
+      Digit(Int_t module, Int_t tower, Double_t amplitude, Double_t time);
       ~Digit() override = default;
-
+      
       bool operator<(const Digit &ref) const;
-
-      const Digit operator+(const Digit& other);// Adds energy of two digits.
-
-      Digit& operator+=(const Digit& other); // Adds energy of other digits to this digit.
-
+      
+      Int_t GetModule() const { return mModule; }
+      
       Int_t GetTower() const { return mTower; }
-
+      
       Double_t GetAmplitude() const { return mAmplitude; }
-
+      
+      void SetModule(Int_t module) { mModule = module; }
+      
       void SetTower(Int_t tower) { mTower = tower; }
-
+      
       void SetAmplitude(Double_t amplitude) { mAmplitude = amplitude; }
-
+      
       void PrintStream(std::ostream &stream) const;
-
+      
     private:
 #ifndef __CINT__
       friend class boost::serialization::access;
 #endif
-
-      Int_t             mTower;                 ///< Tower index (absolute cell ID)
+      
+      Int_t             mModule;                ///< Supermodule index
+      Int_t             mTower;                 ///< Tower index inside supermodule
       Double_t          mAmplitude;             ///< Amplitude
-
+      
       ClassDefOverride(Digit, 1);
     };
-
+    
     std::ostream &operator<<(std::ostream &stream, const Digit &dig);
   }
 }
