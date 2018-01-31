21b097b8c4f5feea6391a9ff2973a0ab77d4c253
Giulio Eulisse <giulio.eulisse@cern.ch>
2018-01-31 12:18:35 +0100

    Add labels to DataProcessorSpec
    This allows optionally specifying one or more label to be associated
    to the DataProcessorSpec, to be later used for things like generating
    sub-topologies for a given label or to customize behavior for devices
    associated with a DataProcessorSpec with a given label.

---
+++ Framework/Core/include/Framework/DataProcessorLabel.h
@@ -0,0 +1,20 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+namespace o2
+{
+namespace framework
+{
+/// A label that can be associated to a DataProcessorSpec
+struct DataProcessorLabel {
+  std::string value;
+};
+} // namespace framework
+} // namespace o2
