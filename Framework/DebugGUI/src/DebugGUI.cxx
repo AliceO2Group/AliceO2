ff09882a86df1c985af94744c0d53c5f72fdbd97
Giulio Eulisse <giulio.eulisse@cern.ch>
2018-02-06 21:41:45 +0100

    Fix messup by clang-format

--- Framework/DebugGUI/src/DebugGUI.cxx
+++ Framework/DebugGUI/src/DebugGUI.cxx
@@ -1,9 +1,9 @@
+#include "imgui.h"
+#include "imgui_impl_glfw_gl3.h"
+#include "GL/gl3w.h" // This example is using gl3w to access OpenGL functions (because it is small). You may use glew/glad/glLoadGen/etc. whatever already works for you.
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 #include <functional>
-#include "GL/gl3w.h" // This example is using gl3w to access OpenGL functions (because it is small). You may use glew/glad/glLoadGen/etc. whatever already works for you.
-#include "imgui.h"
-#include "imgui_impl_glfw_gl3.h"
 
 static void error_callback(int error, const char* description)
 {
