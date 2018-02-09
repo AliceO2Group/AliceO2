68752cd87b6e21ccdd0601328c665cf2c012d931
Piotr Konopka <piotr.jan.konopka@cern.ch>
2018-02-09 11:03:58 +0100

    Prepare and describe an example of using Data Sampling with Readout and Quality Control

--- Framework/TestWorkflows/src/dataSamplingFullChain.cxx
+++ Framework/TestWorkflows/src/dataSamplingFullChain.cxx
@@ -24,75 +24,50 @@
 
 #include "FairMQDevice.h"
 #include "FairMQTransportFactory.h"
-//#include "zeromq/FairMQTransportFactoryZMQ.h"
-//#include <options/FairMQProgOptions.h>
+
+/// This is an example of using Data Sampling with both FairMQ inputs and outputs and no processing inside DPL workflow.
+/// It is configurable by config file. At first, it looks for 'dataSamplingFullChainConfig.ini' in current working
+/// directory. If it is not found there, it tries to use 'O2/Framework/TestWorkflows/dataSamplingFullChainConfig.ini'.
+/// You can use this file as a template to adapt Data Sampling to your own needs. Input channel configuration is
+/// declared in readoutInput/channelConfig, while output channel at readoutQcTaskDefinition/channelConfig. More details
+/// regarding options are provided inside config file.
+///
+/// To run it:
+/// > alienv load O2/latest
+/// > dataSamplingFullChain
+///
+/// To obtain full chain (FMQ Readout -> DPL Data Sampling -> FMQ Quality Control) with default configuration:
+/// - Readout - set following options in Readout/configDummy.cfg file:
+///     ...
+///     exitTimeout=-1
+///     ...
+///     [sampling]
+///     # enable/disable data sampling (1/0)
+///     enabled=1
+///     # which class of datasampling to use (FairInjector, MockInjector)
+///     class=FairInjector
+///     ...
+///   And run:
+///   > alienv load Readout/latest
+///   > readout.exe file:///your/absolute/path/to/Readout/configDummy.cfg
+///
+/// - Quality Control - configure Quality Control to subscribe for data at port 26525 (or any other, if you change it
+///                     in config file). More information on running Quality Control can be found here:
+///                     https://github.com/AliceO2Group/QualityControl
+
 
 using namespace o2::framework;
-using DataHeader = o2::Header::DataHeader;
-
-size_t collectionChunkSize = 100;
-struct FakeCluster {
-    float x;
-    float y;
-    float z;
-    float q;
-};
-
-// Run 'readout.exe file:///home/pkonopka/alice/Readout/configDummy.cfg' to inject data into DPL
-// Otherwise, you can run any other publisher on 5558 port
-//
-// Run 'runFairMqSink --id sink1 --mq-config /home/pkonopka/alice/O2/Utilities/FairMqSink/ex1-sampler-sink.json'
-// to receive data leaving DPL. Any other subscriber on 26525 port should be also fine.
-
 
 void defineDataProcessing(std::vector<DataProcessorSpec> &specs)
 {
-  DataProcessorSpec processingStage{
-    "processingStage",
-    Inputs{
-      {"its-raw", o2::Header::gDataOriginITS, o2::Header::gDataDescriptionRawData, 0, InputSpec::Timeframe}
-    },
-    Outputs{
-      {o2::Header::gDataOriginITS, o2::Header::gDataDescriptionClusters, OutputSpec::Timeframe}
-    },
-    AlgorithmSpec{
-      (AlgorithmSpec::ProcessCallback) [](ProcessingContext &ctx) {
-        size_t index = ctx.services().get<ParallelContext>().index1D();
+  std::string configFilePath = std::ifstream(std::string(getenv("PWD")) + "/dataSamplingFullChainConfig.ini").good() ?
+                               std::string("file://") + getenv("PWD") + "/dataSamplingFullChainConfig.ini" :
+                               std::string("file://") + getenv("BASEDIR")
+                               + "/../../O2/Framework/TestWorkflows/dataSamplingFullChainConfig.ini";
 
-        auto ItsClusters = ctx.allocator().make<FakeCluster>(
-          OutputSpec{o2::Header::gDataOriginITS, o2::Header::gDataDescriptionClusters, index}, collectionChunkSize);
-        int i = 0;
-        for(auto& cluster : ItsClusters){
-          assert( i < collectionChunkSize);
-          cluster.x = i;
-          cluster.y = 2*i;
-          cluster.z = 3*i-1;
-          cluster.q = rand();
-          i++;
-        }
-      }
-    }
-  };
+  LOG(INFO) << "Using config file '" << configFilePath << "'";
 
-  DataProcessorSpec sink{
-    "sink",
-    Inputs{
-      {"its-clusters", o2::Header::gDataOriginITS, o2::Header::gDataDescriptionClusters, InputSpec::Timeframe}
-    },
-    Outputs{},
-    AlgorithmSpec{
-      (AlgorithmSpec::ProcessCallback) [](ProcessingContext &ctx) {
-        LOG(DEBUG) << "Invoked";
-      }
-    }
-  };
-
-  specs.push_back(processingStage);
-  specs.push_back(sink);
-
-  std::string configurationSource = std::string("file://") + getenv("BASEDIR")
-                                    + "/../../O2/Framework/TestWorkflows/dataSamplingFullChainConfig.ini";
-  DataSampling::GenerateInfrastructure(specs, configurationSource);
+  DataSampling::GenerateInfrastructure(specs, configFilePath);
 
 }
 
