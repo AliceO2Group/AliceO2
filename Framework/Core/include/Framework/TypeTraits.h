348d60390e27aad0b16e07e2837dda07609f72b1
Matthias Richter <Matthias.Richter@scieq.net>
2018-02-09 20:52:55 +0100

    Adding type traits for detecting root dictionary and stl containers
    - Type trait is_container detects stl containers by checking availability
      usual container members.
    
    - Type trait has_root_dictionary checks for the existence of member
      function Class() which is added to the type by ROOT ClassDef/Imp which
      only works if a ROOT dictionary is also generated for that type.
      This does not include all cases of existence of ROOT dictionary
      because the only requirement to generate the serializer code is the
      entry in the LinkDef file. This is however not detectable at compile
      time.
    
    - A specialization of has_root_dictionary for container types forwards
      to check of value type. This solution does not work for std::map as the
      value_type is a std::pair in that case.
    
    - Adding possibility to force types to be non-messageable

--- Framework/Core/include/Framework/TypeTraits.h
+++ Framework/Core/include/Framework/TypeTraits.h
@@ -11,6 +11,7 @@
 #define FRAMEWORK_TYPETRAITS_H
 
 #include <type_traits>
+#include <vector>
 
 namespace o2 {
 namespace framework {
@@ -24,11 +25,99 @@ struct is_specialization : std::false_type {};
 template<template<typename...> class Ref, typename... Args>
 struct is_specialization<Ref<Args...>, Ref>: std::true_type {};
 
+// helper struct to mark a type as non-messageable by defining a type alias
+// with name 'non-messageable'
+struct MarkAsNonMessageable {};
+
+// detect if a type is forced to be non-messageable, this is done by defining
+// a type alias with name 'non-messageable' of the type MarkAsNonMessageable
+template< typename T, typename _ = void >
+struct is_forced_non_messageable : public std::false_type {};
+
+// specialization to detect the type a lias
+template <typename T>
+struct is_forced_non_messageable<
+  T,
+  typename std::enable_if<std::is_same<typename T::non_messageable, MarkAsNonMessageable>::value>::type
+  > : public std::true_type {};
+
 // TODO: extend this to exclude structs with pointer data members
 // see e.g. https://stackoverflow.com/questions/32880990/how-to-check-if-class-has-pointers-in-c14
 template <typename T>
-struct is_messageable : std::conditional<std::is_trivially_copyable<T>::value && !std::is_polymorphic<T>::value,
+struct is_messageable : std::conditional<std::is_trivially_copyable<T>::value &&
+                                         !std::is_polymorphic<T>::value &&
+                                         !is_forced_non_messageable<T>::value,
                                          std::true_type, std::false_type>::type {};
+
+// Detect a container by checking on the container properties
+// this is the default trait implementation inheriting from false_type
+template <typename T, typename _ = void>
+struct is_container : std::false_type {};
+
+// helper to be substituted if the specified template arguments are
+// available
+template <typename... Ts>
+struct class_member_checker {};
+
+// the specialization for container types inheriting from true_type
+// the helper can be substituted if all the specified members are available
+// in the type
+template <typename T>
+struct is_container<
+  T,
+  std::conditional_t<
+    false,
+    class_member_checker<
+      typename T::value_type,
+      typename T::size_type,
+      typename T::allocator_type,
+      typename T::iterator,
+      typename T::const_iterator,
+      decltype(std::declval<T>().size()),
+      decltype(std::declval<T>().begin()),
+      decltype(std::declval<T>().end()),
+      decltype(std::declval<T>().cbegin()),
+      decltype(std::declval<T>().cend())
+      >,
+    void
+    >
+  > : public std::true_type {};
+
+
+// Detect whether a class has a ROOT dictionary
+// This member detector idiom is implemented using SFINAE idiom to look for
+// a 'Class()' method.
+// This is actually only covering classes using the ClasDef/Imp macros. ROOT
+// serialization however is also possible for types only having the link
+// in the LinkDef file. Such types can only be detected at runtime.
+template <typename T, typename _ = void>
+struct has_root_dictionary : std::false_type {};
+
+template <typename T>
+struct has_root_dictionary<
+  T,
+  std::conditional_t<
+    false,
+    class_member_checker<
+      decltype(std::declval<T>().Class())
+      >,
+    void
+    >
+  > : public std::true_type {};
+
+// specialization for containers
+// covers cases with T::value_type having ROOT dictionary, meaning that
+// std::map is not supported out of the box
+//
+// Note: this is only a SFINAE idiom if the default type of the second template
+// argument in the declaration is the same as the conditionally produced one in
+// the specialization (void in this case)
+template <typename T>
+class has_root_dictionary<T, typename std::enable_if<is_container<T>::value>::type>
+  : public has_root_dictionary<typename T::value_type>
+{
+};
+
 }
 }
 #endif // FRAMEWORK_TYPETRAITS_H
