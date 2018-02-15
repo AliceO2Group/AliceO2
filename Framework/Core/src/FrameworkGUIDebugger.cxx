75f2a78f7fcdd51d65df3d9a948198c32e5a9cc5
sawenzel <sandro.wenzel@cern.ch>
2018-02-15 12:51:05 +0100

    Fix shadow-variable compiler warning (#859)

--- Framework/Core/src/FrameworkGUIDebugger.cxx
+++ Framework/Core/src/FrameworkGUIDebugger.cxx
@@ -408,8 +408,7 @@ void displayDriverInfo(DriverInfo const& driverInfo, DriverControl& driverContro
   ImGui::Text("State stack (depth %lu)", driverInfo.states.size());
 
   for (size_t i = 0; i < driverInfo.states.size(); ++i) {
-    DriverState const& state = driverInfo.states[i];
-    ImGui::Text("#%lu: %s", i, DriverHelper::stateToString(state));
+    ImGui::Text("#%lu: %s", i, DriverHelper::stateToString(driverInfo.states[i]));
   }
 
   ImGui::End();
