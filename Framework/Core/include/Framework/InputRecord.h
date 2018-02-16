ca8ff30a99736218a93f7ee88d4ef4ba7adb2923
Matthias Richter <Matthias.Richter@scieq.net>
2018-02-16 12:40:34 +0100

    Updating the DPL input API
    Messageable objects are now considered to be non-serialized by default
    even if they have root dictionary. For types for which the serialization
    method can not be automatically deduced, the type wrapper ROOTSerialized
    can be used to force ROOT serialization method

--- Framework/Core/include/Framework/InputRecord.h
+++ Framework/Core/include/Framework/InputRecord.h
@@ -29,77 +29,13 @@
 #include <memory>
 #include <type_traits>
 
-template<typename T>
+template <typename T>
 using default_delete = std::default_delete<T>;
 
 namespace o2 {
 namespace framework {
 
 struct InputSpec;
-
-/// A deleter type to be used with unique_ptr, which can be marked that
-/// it does not own the underlying resource and thus should not delete it.
-/// The resource ownership property controls the behavior and can only be
-/// set at construction of the deleter in the unique_ptr. Falls back to
-/// default_delete if not initialized to 'NotOwning'.
-/// Usage: unique_ptr<T, Deleter<T>> ptr(..., Deleter<T>(false))
-///
-/// By contract, the underlying, not owned resource is supposed to be
-/// available during the lifetime of the object, which is the case in the
-/// InputRecord and DPL processing APIs. The Deleter can be extended
-/// to support a callback to call a resource management outside.
-template<typename T>
-class Deleter : public default_delete<T>
-{
- public:
-  enum class OwnershipProperty : short
-  {
-    Unknown = -1,
-    NotOwning = 0, /// don't delete the underlying buffer
-    Owning = 1     /// normal behavior, falling back to default deleter
-  };
-
-  using base =  default_delete<T>;
-  using self_type = Deleter<T>;
-  //using pointer = typename base::pointer;
-
-  constexpr Deleter() = default;
-  constexpr Deleter(bool isOwning)
-    : base::default_delete()
-    , mProperty(isOwning ? OwnershipProperty::Owning : OwnershipProperty::NotOwning)
-  {
-  }
-
-  // copy constructor is needed in the setup of unique_ptr
-  // check that assignments happen only to uninitialized instances
-  constexpr Deleter(const self_type& other)
-  {
-    if (mProperty == OwnershipProperty::Unknown) {
-      mProperty = other.mProperty;
-    } else if (mProperty != other.mProperty) {
-      throw std::runtime_error("Attemp to change resource control");
-    }
-  }
-
-  // copy constructor for the default delete which simply sets the
-  // resource ownership control to 'Owning'
-  constexpr Deleter(const base& other)
-  {
-    mProperty = OwnershipProperty::Owning;
-  }
-
-  // forbid assignment operator to prohibid changing the Deleter
-  // resource control property once used in the unique_ptr
-  self_type& operator=(const self_type&) = delete;
-
-  void operator()(T* ptr) const
-  {
-    if (mProperty == OwnershipProperty::NotOwning) return;
-    base::operator()(ptr);
-  }
- private:
-  OwnershipProperty mProperty = OwnershipProperty::Unknown;
-};
 
 /// This class holds the inputs which  are being processed by the system while
 /// they are  being processed.  The user can  get an instance  for it  via the
@@ -111,6 +47,67 @@ class InputRecord {
 public:
   InputRecord(std::vector<InputRoute> const &inputs,
               std::vector<std::unique_ptr<FairMQMessage>> const &cache);
+
+  /// A deleter type to be used with unique_ptr, which can be marked that
+  /// it does not own the underlying resource and thus should not delete it.
+  /// The resource ownership property controls the behavior and can only be
+  /// set at construction of the deleter in the unique_ptr. Falls back to
+  /// default_delete if not initialized to 'NotOwning'.
+  /// Usage: unique_ptr<T, Deleter<T>> ptr(..., Deleter<T>(false))
+  ///
+  /// By contract, the underlying, not owned resource is supposed to be
+  /// available during the lifetime of the object, which is the case in the
+  /// InputRecord and DPL processing APIs. The Deleter can be extended
+  /// to support a callback to call a resource management outside.
+  template <typename T>
+  class Deleter : public default_delete<T>
+  {
+   public:
+    enum struct OwnershipProperty : short {
+      Unknown = -1,
+      NotOwning = 0, /// don't delete the underlying buffer
+      Owning = 1     /// normal behavior, falling back to default deleter
+    };
+
+    using base = default_delete<T>;
+    using self_type = Deleter<T>;
+    // using pointer = typename base::pointer;
+
+    constexpr Deleter() = default;
+    constexpr Deleter(bool isOwning)
+      : base::default_delete()
+      , mProperty(isOwning ? OwnershipProperty::Owning : OwnershipProperty::NotOwning)
+    {
+    }
+
+    // copy constructor is needed in the setup of unique_ptr
+    // check that assignments happen only to uninitialized instances
+    constexpr Deleter(const self_type& other)
+    {
+      if (mProperty == OwnershipProperty::Unknown) {
+        mProperty = other.mProperty;
+      } else if (mProperty != other.mProperty) {
+        throw std::runtime_error("Attemp to change resource control");
+      }
+    }
+
+    // copy constructor for the default delete which simply sets the
+    // resource ownership control to 'Owning'
+    constexpr Deleter(const base& other) { mProperty = OwnershipProperty::Owning; }
+
+    // forbid assignment operator to prohibid changing the Deleter
+    // resource control property once used in the unique_ptr
+    self_type& operator=(const self_type&) = delete;
+
+    void operator()(T* ptr) const
+    {
+      if (mProperty == OwnershipProperty::NotOwning) return;
+      base::operator()(ptr);
+    }
+
+   private:
+    OwnershipProperty mProperty = OwnershipProperty::Unknown;
+  };
 
   int getPos(const char *name) const;
   int getPos(const std::string &name) const;
@@ -130,7 +127,9 @@ public:
   // not be used if the type is a TObject as extra deserialization
   // needs to happen.
   template <typename T>
-  typename std::enable_if<is_messageable<T>::value && std::is_same<T, DataRef>::value == false && has_root_dictionary<T>::value == false, T>::type const&
+  typename std::enable_if<is_messageable<T>::value &&
+                          std::is_same<T, DataRef>::value == false &&
+                          has_root_dictionary<T>::value == false, T>::type const&
   get(char const* binding) const
   {
     // TODO: check the serialization type, the cast makes only sense for
@@ -186,17 +185,21 @@ public:
   // objects in a single place so that we can avoid duplicate deserializations?
   template <class T>
   typename std::unique_ptr<typename std::enable_if<has_root_dictionary<T>::value == true && is_messageable<T>::value == false, T>::type const>
-  get(char const *binding) const {
+  get(char const* binding) const
+  {
     using DataHeader = o2::header::DataHeader;
 
     auto ref = this->get(binding);
     return std::move(DataRefUtils::as<T>(ref));
   }
 
-  // substitution for messageable objects with dictionary
+  // substitution for messageable objects with ROOT dictionary
   // the operation depends on the transmitted serialization method
   template <typename T>
-  typename std::enable_if<is_messageable<T>::value && std::is_same<T, DataRef>::value == false && has_root_dictionary<T>::value, std::unique_ptr<T const, Deleter<T const>>>::type
+  typename std::enable_if<is_messageable<T>::value &&
+                          std::is_same<T, DataRef>::value == false &&
+                          has_root_dictionary<T>::value,
+                          std::unique_ptr<T const, Deleter<T const>>>::type
   get(char const* binding) const
   {
     using DataHeader = o2::header::DataHeader;
@@ -205,14 +208,48 @@ public:
     auto header = o2::header::get<const DataHeader>(ref.header);
     auto method = header->payloadSerializationMethod;
     if (method == o2::header::gSerializationMethodNone) {
-      auto const* ptr = reinterpret_cast<T const *>(ref.payload);
+      auto const* ptr = reinterpret_cast<T const*>(ref.payload);
+      // return type with non-owning Deleter instance
       std::unique_ptr<T const, Deleter<T const>> result(ptr, Deleter<T const>(false));
       return std::move(result);
     } else if (method == o2::header::gSerializationMethodROOT) {
-      std::unique_ptr<T const, Deleter<T const>> result(DataRefUtils::extract<T>(ref).release());
+      // explicitely specify serialization method to ROOT-serialized because type T
+      // is messageable and a different method would be deduced in DataRefUtils
+      // return type with owning Deleter instance, forwarding to default_deleter
+      std::unique_ptr<T const, Deleter<T const>> result(DataRefUtils::as<ROOTSerialized<T>>(ref).release());
       return std::move(result);
     } else {
-      throw std::runtime_error("Attempt to extract message with unsupported serializayion type");
+      throw std::runtime_error("Attempt to extract object from message with unsupported serializayion type");
+    }
+  }
+
+  // substitution for non-messageable objects without ROOT dictionary
+  // the operation depends on the transmitted serialization method
+  // FIXME: some of the substitutions can for sure be combined when the return types
+  // will be unified in a later refactoring
+  template <typename T>
+  typename std::enable_if<is_messageable<T>::value == false &&
+                          std::is_same<T, DataRef>::value == false &&
+                          has_root_dictionary<T>::value == false,
+                          std::unique_ptr<T const, Deleter<T const>>>::type
+  get(char const* binding) const
+  {
+    using DataHeader = o2::header::DataHeader;
+
+    auto ref = this->get(binding);
+    auto header = o2::header::get<const DataHeader>(ref.header);
+    auto method = header->payloadSerializationMethod;
+    if (method == o2::header::gSerializationMethodNone) {
+      throw std::runtime_error(
+        "Type mismatch: attempt to extract a non-messagable object from message with unserialized data");
+    } else if (method == o2::header::gSerializationMethodROOT) {
+      // explicitely specify serialization method to ROOT-serialized because type T
+      // is messageable and a different method would be deduced in DataRefUtils
+      // return type with owning Deleter instance, forwarding to default_deleter
+      std::unique_ptr<T const, Deleter<T const>> result(DataRefUtils::as<ROOTSerialized<T>>(ref).release());
+      return std::move(result);
+    } else {
+      throw std::runtime_error("Attempt to extract object from message with unsupported serializayion type");
     }
   }
 
