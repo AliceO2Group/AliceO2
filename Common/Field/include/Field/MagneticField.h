e22269179161de2d1dd969c3247ae864e9cd0411
shahoian <ruben.shahoyan@cern.ch>
2018-02-16 15:34:26 +0100

    Do not parse stored field-map path
    To make the MC production usable on the system different from where it was generated
    we should store unparsed path to the data file
    
    Applied clang-format

--- Common/Field/include/Field/MagneticField.h
+++ Common/Field/include/Field/MagneticField.h
@@ -15,21 +15,28 @@
 #ifndef ALICEO2_FIELD_MAGNETICFIELD_H_
 #define ALICEO2_FIELD_MAGNETICFIELD_H_
 
-#include "FairField.h"         // for FairField
+#include "FairField.h" // for FairField
 #include "Field/MagFieldParam.h"
 #include "Field/MagneticWrapperChebyshev.h" // for MagneticWrapperChebyshev
 #include "Field/MagFieldFast.h"
 #include "TSystem.h"
-#include "Rtypes.h"            // for Double_t, Char_t, Int_t, Float_t, etc
-#include "TNamed.h"            // for TNamed
-#include <memory>              // for str::unique_ptr
+#include "Rtypes.h" // for Double_t, Char_t, Int_t, Float_t, etc
+#include "TNamed.h" // for TNamed
+#include <memory>   // for str::unique_ptr
 
 class FairParamList;
 
-namespace o2 { namespace field { class MagneticWrapperChebyshev; }}  // lines 19-19
-namespace o2 {
-namespace field {
-
+namespace o2
+{
+namespace field
+{
+class MagneticWrapperChebyshev;
+}
+} // lines 19-19
+namespace o2
+{
+namespace field
+{
 
 /// Interface between the TVirtualMagField and MagneticWrapperChebyshev: wrapper to the set of magnetic field data +
 /// Tosca
@@ -37,259 +44,198 @@ namespace field {
 class MagneticField : public FairField
 {
 
-  public:
-    enum PolarityConvention_t
-    {
-        kConvLHC, kConvDCS2008, kConvMap2005
-    };
-    enum
-    {
-        kOverrideGRP = BIT(14)
-    }; // don't recreate from GRP if set
+ public:
+  enum PolarityConvention_t { kConvLHC, kConvDCS2008, kConvMap2005 };
+  enum { kOverrideGRP = BIT(14) }; // don't recreate from GRP if set
 
-    /// Default constructor
-    MagneticField();
+  /// Default constructor
+  MagneticField();
 
-    /// Initialize the field with Geant integration option "integ" and max field "fmax",
-    /// Impose scaling of parameterized L3 field by factorSol and of dipole by factorDip.
-    /// The "be" is the energy of the beam in GeV/nucleon
-    MagneticField(const char *name, const char *title, Double_t factorSol = 1., Double_t factorDip = 1.,
-                  MagFieldParam::BMap_t maptype = MagFieldParam::k5kG,
-		  MagFieldParam::BeamType_t btype = MagFieldParam::kBeamTypepp,
-		  Double_t benergy = -1, Int_t integ = 2,
-                  Double_t fmax = 15, const std::string path = std::string(gSystem->ExpandPathName("$(O2_ROOT)"))
-		  + std::string("/share/Common/maps/mfchebKGI_sym.root")
-    );
+  /// Initialize the field with Geant integration option "integ" and max field "fmax",
+  /// Impose scaling of parameterized L3 field by factorSol and of dipole by factorDip.
+  /// The "be" is the energy of the beam in GeV/nucleon
+  MagneticField(const char* name, const char* title, Double_t factorSol = 1., Double_t factorDip = 1.,
+                MagFieldParam::BMap_t maptype = MagFieldParam::k5kG,
+                MagFieldParam::BeamType_t btype = MagFieldParam::kBeamTypepp, Double_t benergy = -1, Int_t integ = 2,
+                Double_t fmax = 15, const std::string path = "$(O2_ROOT)/share/Common/maps/mfchebKGI_sym.root");
 
+  MagneticField(const MagFieldParam& param);
 
-    MagneticField(const MagFieldParam& param);
+  MagneticField& operator=(const MagneticField& src);
 
-    MagneticField &operator=(const MagneticField &src);
+  /// Default destructor
+  ~MagneticField() override = default;
 
-    /// Default destructor
-    ~MagneticField() override = default;
+  /// real field creation is here
+  void CreateField();
 
-    /// real field creation is here
-    void CreateField();
+  /// allow fast field param
+  void AllowFastField(bool v = true);
 
-    /// allow fast field param
-    void        AllowFastField(bool v=true);
-    
-    /// Virtual methods from FairField
+  /// Virtual methods from FairField
 
-    /// X component, avoid using since slow
-    Double_t GetBx(Double_t x, Double_t y, Double_t z) override {
-      double xyz[3]={x,y,z},b[3];
-      MagneticField::Field(xyz,b);
-      return b[0];
-    } 
+  /// X component, avoid using since slow
+  Double_t GetBx(Double_t x, Double_t y, Double_t z) override
+  {
+    double xyz[3] = { x, y, z }, b[3];
+    MagneticField::Field(xyz, b);
+    return b[0];
+  }
 
-    /// Y component, avoid using since slow
-    Double_t GetBy(Double_t x, Double_t y, Double_t z) override {
-      double xyz[3]={x,y,z},b[3];
-      MagneticField::Field(xyz,b);
-      return b[1];
-    }
+  /// Y component, avoid using since slow
+  Double_t GetBy(Double_t x, Double_t y, Double_t z) override
+  {
+    double xyz[3] = { x, y, z }, b[3];
+    MagneticField::Field(xyz, b);
+    return b[1];
+  }
 
-    /// Z component
-    Double_t GetBz(Double_t x, Double_t y, Double_t z) override {
-      double xyz[3]={x,y,z};
-      return getBz(xyz); 
-    } 
+  /// Z component
+  Double_t GetBz(Double_t x, Double_t y, Double_t z) override
+  {
+    double xyz[3] = { x, y, z };
+    return getBz(xyz);
+  }
 
-    /// Method to calculate the field at point xyz
-    /// Main interface from TVirtualMagField used in simulation
-    void Field(const Double_t* __restrict__ point, Double_t* __restrict__ bField) override;
+  /// Method to calculate the field at point xyz
+  /// Main interface from TVirtualMagField used in simulation
+  void Field(const Double_t* __restrict__ point, Double_t* __restrict__ bField) override;
 
-    /// 3d field query alias for Alias Method to calculate the field at point xyz
-    void GetBxyz(const Double_t p[3], Double_t* b) override { MagneticField::Field(p,b); }
+  /// 3d field query alias for Alias Method to calculate the field at point xyz
+  void GetBxyz(const Double_t p[3], Double_t* b) override { MagneticField::Field(p, b); }
 
-    /// Fill Paramater
-    void FillParContainer() override;
-    
-    /// Method to calculate the integral_0^z of br,bt,bz
-    void getTPCIntegral(const Double_t *xyz, Double_t *b) const;
+  /// Fill Paramater
+  void FillParContainer() override;
 
-    /// Method to calculate the integral_0^z of br,bt,bz
-    void getTPCRatIntegral(const Double_t *xyz, Double_t *b) const;
+  /// Method to calculate the integral_0^z of br,bt,bz
+  void getTPCIntegral(const Double_t* xyz, Double_t* b) const;
 
-    /// Method to calculate the integral_0^z of br,bt,bz in cylindrical coordinates ( -pi<phi<pi convention )
-    void getTPCIntegralCylindrical(const Double_t *rphiz, Double_t *b) const;
+  /// Method to calculate the integral_0^z of br,bt,bz
+  void getTPCRatIntegral(const Double_t* xyz, Double_t* b) const;
 
-    /// Method to calculate the integral_0^z of bx/bz,by/bz and (bx/bz)^2+(by/bz)^2 in
-    /// cylindrical coordinates ( -pi<phi<pi convention )
-    void getTPCRatIntegralCylindrical(const Double_t *rphiz, Double_t *b) const;
+  /// Method to calculate the integral_0^z of br,bt,bz in cylindrical coordinates ( -pi<phi<pi convention )
+  void getTPCIntegralCylindrical(const Double_t* rphiz, Double_t* b) const;
 
-    /// Method to calculate the field at point xyz
-    Double_t getBz(const Double_t *xyz) const;
+  /// Method to calculate the integral_0^z of bx/bz,by/bz and (bx/bz)^2+(by/bz)^2 in
+  /// cylindrical coordinates ( -pi<phi<pi convention )
+  void getTPCRatIntegralCylindrical(const Double_t* rphiz, Double_t* b) const;
 
-    MagneticWrapperChebyshev *getMeasuredMap() const { return mMeasuredMap.get();}
-    
-    /// get fast field direct pointer
-    const MagFieldFast* getFastField() const {return mFastField.get();}
-    
-    // Former MagF methods or their aliases
+  /// Method to calculate the field at point xyz
+  Double_t getBz(const Double_t* xyz) const;
 
-    /// Sets the sign/scale of the current in the L3 according to sPolarityConvention
-    void setFactorSolenoid(Float_t fc = 1.);
+  MagneticWrapperChebyshev* getMeasuredMap() const { return mMeasuredMap.get(); }
 
-    /// Sets the sign*scale of the current in the Dipole according to sPolarityConvention
-    void setFactorDipole(Float_t fc = 1.);
+  /// get fast field direct pointer
+  const MagFieldFast* getFastField() const { return mFastField.get(); }
 
-    /// Returns the sign*scale of the current in the Dipole according to sPolarityConventionthe
-    Double_t getFactorSolenoid() const;
+  // Former MagF methods or their aliases
 
-    /// Return the sign*scale of the current in the Dipole according to sPolarityConventionthe
-    Double_t getFactorDipole() const;
+  /// Sets the sign/scale of the current in the L3 according to sPolarityConvention
+  void setFactorSolenoid(Float_t fc = 1.);
 
-    Double_t Factor() const
-    {
-      return getFactorSolenoid();
-    }
+  /// Sets the sign*scale of the current in the Dipole according to sPolarityConvention
+  void setFactorDipole(Float_t fc = 1.);
 
-    Double_t getCurrentSolenoid() const
-    {
-      return getFactorSolenoid() * (mMapType == MagFieldParam::k2kG ? 12000 : 30000);
-    }
+  /// Returns the sign*scale of the current in the Dipole according to sPolarityConventionthe
+  Double_t getFactorSolenoid() const;
 
-    Double_t getCurrentDipole() const
-    {
-      return getFactorDipole() * 6000;
-    }
+  /// Return the sign*scale of the current in the Dipole according to sPolarityConventionthe
+  Double_t getFactorDipole() const;
 
-    Bool_t IsUniform() const
-    {
-      return mMapType == MagFieldParam::k5kGUniform;
-    }
+  Double_t Factor() const { return getFactorSolenoid(); }
 
-    void MachineField(const Double_t * __restrict__ x, Double_t * __restrict__ b) const;
+  Double_t getCurrentSolenoid() const
+  {
+    return getFactorSolenoid() * (mMapType == MagFieldParam::k2kG ? 12000 : 30000);
+  }
 
-    MagFieldParam::BMap_t getMapType() const
-    {
-      return mMapType;
-    }
+  Double_t getCurrentDipole() const { return getFactorDipole() * 6000; }
 
-    MagFieldParam::BeamType_t getBeamType() const
-    {
-      return mBeamType;
-    }
+  Bool_t IsUniform() const { return mMapType == MagFieldParam::k5kGUniform; }
 
-    /// Returns beam type in text form
-    const char *getBeamTypeText() const;
+  void MachineField(const Double_t* __restrict__ x, Double_t* __restrict__ b) const;
 
-    Double_t getBeamEnergy() const
-    {
-      return mBeamEnergy;
-    }
+  MagFieldParam::BMap_t getMapType() const { return mMapType; }
 
-    Double_t Max() const
-    {
-      return mMaxField;
-    }
+  MagFieldParam::BeamType_t getBeamType() const { return mBeamType; }
 
-    Int_t Integral() const
-    {
-      return mDefaultIntegration;
-    }
+  /// Returns beam type in text form
+  const char* getBeamTypeText() const;
 
-    Int_t precIntegral() const
-    {
-      return mPrecisionInteg;
-    }
+  Double_t getBeamEnergy() const { return mBeamEnergy; }
 
-    Double_t solenoidField() const
-    {
-      return mSolenoid;
-    }
+  Double_t Max() const { return mMaxField; }
 
-    Char_t *getDataFileName() const
-    {
-      return (Char_t *) mParameterNames.GetName();
-    }
+  Int_t Integral() const { return mDefaultIntegration; }
 
-    Char_t *getParameterName() const
-    {
-      return (Char_t *) mParameterNames.GetTitle();
-    }
+  Int_t precIntegral() const { return mPrecisionInteg; }
 
-    void setDataFileName(const Char_t *nm)
-    {
-      mParameterNames.SetName(nm);
-    }
+  Double_t solenoidField() const { return mSolenoid; }
 
-    void setParameterName(const Char_t *nm)
-    {
-      mParameterNames.SetTitle(nm);
-    }
+  Char_t* getDataFileName() const { return (Char_t*)mParameterNames.GetName(); }
 
-    /// Prints short or long info
-    void Print(Option_t *opt) const override;
+  Char_t* getParameterName() const { return (Char_t*)mParameterNames.GetTitle(); }
 
-    Bool_t loadParameterization();
+  void setDataFileName(const Char_t* nm) { mParameterNames.SetName(nm); }
 
-    static Int_t getPolarityConvention()
-    {
-      return Int_t(sPolarityConvention);
-    }
+  void setParameterName(const Char_t* nm) { mParameterNames.SetTitle(nm); }
 
-    /// The magnetic field map, defined externally...
-    /// L3 current 30000 A  -> 0.5 T
-    /// L3 current 12000 A  -> 0.2 T
-    /// dipole current 6000 A
-    /// The polarities must match the convention (LHC or DCS2008)
-    /// unless the special uniform map was used for MC
-    static MagneticField *createFieldMap(Float_t l3Current = -30000., Float_t diCurrent = -6000., Int_t convention = 0,
-                                         Bool_t uniform = kFALSE, Float_t beamenergy = 7000, const Char_t *btype = "pp",
-                                         const std::string path = std::string(gSystem->Getenv("VMCWORKDIR")) +
-                                                                  std::string("/Common/maps/mfchebKGI_sym.root")
-    );
+  /// Prints short or long info
+  void Print(Option_t* opt) const override;
 
-  protected:
-    // not supposed to be changed during the run, set only at the initialization via constructor
-    void initializeMachineField(MagFieldParam::BeamType_t btype, Double_t benergy);
+  Bool_t loadParameterization();
 
-    void setBeamType(MagFieldParam::BeamType_t type)
-    {
-      mBeamType = type;
-    }
+  static Int_t getPolarityConvention() { return Int_t(sPolarityConvention); }
 
-    void setBeamEnergy(Float_t energy)
-    {
-      mBeamEnergy = energy;
-    }
+  /// The magnetic field map, defined externally...
+  /// L3 current 30000 A  -> 0.5 T
+  /// L3 current 12000 A  -> 0.2 T
+  /// dipole current 6000 A
+  /// The polarities must match the convention (LHC or DCS2008)
+  /// unless the special uniform map was used for MC
+  static MagneticField* createFieldMap(Float_t l3Current = -30000., Float_t diCurrent = -6000., Int_t convention = 0,
+                                       Bool_t uniform = kFALSE, Float_t beamenergy = 7000, const Char_t* btype = "pp",
+                                       const std::string path = std::string(gSystem->Getenv("VMCWORKDIR")) +
+                                                                std::string("/Common/maps/mfchebKGI_sym.root"));
 
-  private:
-    std::unique_ptr<MagneticWrapperChebyshev> mMeasuredMap; //! Measured part of the field map
-    std::unique_ptr<MagFieldFast>             mFastField; // ! optional fast parametrization
-    MagFieldParam::BMap_t mMapType;         ///< field map type
-    Double_t mSolenoid;                     ///< Solenoid field setting
-    MagFieldParam::BeamType_t mBeamType;    ///< Beam type: A-A (mBeamType=0) or p-p (mBeamType=1)
-    Double_t mBeamEnergy;                   ///< Beam energy in GeV
+ protected:
+  // not supposed to be changed during the run, set only at the initialization via constructor
+  void initializeMachineField(MagFieldParam::BeamType_t btype, Double_t benergy);
 
-    Int_t mDefaultIntegration;             ///< Default integration method as indicated in Geant
-    Int_t mPrecisionInteg;                 ///< Alternative integration method, e.g. for higher precision
-    Double_t mMultipicativeFactorSolenoid; ///< Multiplicative factor for solenoid
-    Double_t mMultipicativeFactorDipole;   ///< Multiplicative factor for dipole
-    Double_t mMaxField;                    ///< Max Field as indicated in Geant
-    Bool_t mDipoleOnOffFlag;               ///< Dipole ON/OFF flag
+  void setBeamType(MagFieldParam::BeamType_t type) { mBeamType = type; }
 
-    Double_t mQuadrupoleGradient; ///< Gradient field for inner triplet quadrupoles
-    Double_t mDipoleField;        ///< Field value for D1 and D2 dipoles
-    Double_t mCompensatorField2C; ///< Side C 2nd compensator field
-    Double_t mCompensatorField1A; ///< Side A 1st compensator field
-    Double_t mCompensatorField2A; ///< Side A 2nd compensator field
+  void setBeamEnergy(Float_t energy) { mBeamEnergy = energy; }
 
-    TNamed mParameterNames; ///< file and parameterization loaded
+ private:
+  std::unique_ptr<MagneticWrapperChebyshev> mMeasuredMap; //! Measured part of the field map
+  std::unique_ptr<MagFieldFast> mFastField;               // ! optional fast parametrization
+  MagFieldParam::BMap_t mMapType;                         ///< field map type
+  Double_t mSolenoid;                                     ///< Solenoid field setting
+  MagFieldParam::BeamType_t mBeamType;                    ///< Beam type: A-A (mBeamType=0) or p-p (mBeamType=1)
+  Double_t mBeamEnergy;                                   ///< Beam energy in GeV
 
-    static const Double_t sSolenoidToDipoleZ;  ///< conventional Z of transition from L3 to Dipole field
-    static const UShort_t sPolarityConvention; ///< convention for the mapping of the curr.sign on main component sign
+  Int_t mDefaultIntegration;             ///< Default integration method as indicated in Geant
+  Int_t mPrecisionInteg;                 ///< Alternative integration method, e.g. for higher precision
+  Double_t mMultipicativeFactorSolenoid; ///< Multiplicative factor for solenoid
+  Double_t mMultipicativeFactorDipole;   ///< Multiplicative factor for dipole
+  Double_t mMaxField;                    ///< Max Field as indicated in Geant
+  Bool_t mDipoleOnOffFlag;               ///< Dipole ON/OFF flag
 
-    MagneticField(const MagneticField &src);
+  Double_t mQuadrupoleGradient; ///< Gradient field for inner triplet quadrupoles
+  Double_t mDipoleField;        ///< Field value for D1 and D2 dipoles
+  Double_t mCompensatorField2C; ///< Side C 2nd compensator field
+  Double_t mCompensatorField1A; ///< Side A 1st compensator field
+  Double_t mCompensatorField2A; ///< Side A 2nd compensator field
 
+  TNamed mParameterNames; ///< file and parameterization loaded
 
-    
-    ClassDefOverride(o2::field::MagneticField,
-    3) // Class for all Alice MagField wrapper for measured data + Tosca parameterization
+  static const Double_t sSolenoidToDipoleZ;  ///< conventional Z of transition from L3 to Dipole field
+  static const UShort_t sPolarityConvention; ///< convention for the mapping of the curr.sign on main component sign
+
+  MagneticField(const MagneticField& src);
+
+  ClassDefOverride(o2::field::MagneticField,
+                   3) // Class for all Alice MagField wrapper for measured data + Tosca parameterization
 };
 }
 }
