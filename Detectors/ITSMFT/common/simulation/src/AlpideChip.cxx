808243c8f78fac0f5240745a759ba8b24740f53d
Bogdan Vulpescu <Bogdan.Vulpescu@clermont.in2p3.fr>
2018-02-23 16:25:30 +0100

    ITS and MFT use common materials for the ALPIDE chip

--- Detectors/ITSMFT/common/simulation/src/AlpideChip.cxx
+++ Detectors/ITSMFT/common/simulation/src/AlpideChip.cxx
@@ -15,6 +15,9 @@
 #include "ITSMFTSimulation/AlpideChip.h"
 #include "ITSMFTBase/SegmentationAlpide.h"
 
+#include "DetectorsBase/MaterialManager.h"
+#include "DetectorsBase/Detector.h"
+
 #include <TGeoBBox.h>         // for TGeoBBox
 #include <TGeoManager.h>      // for gGeoManager, TGeoManager
 #include "TGeoVolume.h"       // for TGeoVolume
@@ -78,9 +81,17 @@ TGeoVolume* AlpideChip::createChip(const Double_t ychip,
 
 
   // We have all shapes: now create the real volumes
-  TGeoMedium *medSi    = mgr->GetMedium("ITS_SI$");
-  TGeoMedium *medAir   = mgr->GetMedium("ITS_AIR$");
-  TGeoMedium *medMetal = mgr->GetMedium("ITS_METALSTACK$");
+  TGeoMedium *medSi    = mgr->GetMedium("ALPIDE_SI$");
+  if (!medSi) {
+    Int_t fieldType;
+    Float_t maxField;
+    o2::Base::Detector::initFieldTrackingParams(fieldType, maxField);
+    createMaterials(0, fieldType, maxField);
+    medSi    = mgr->GetMedium("ALPIDE_SI$");
+  }
+  TGeoMedium *medAir   = mgr->GetMedium("ALPIDE_AIR$");
+  TGeoMedium *medMetal = mgr->GetMedium("ALPIDE_METALSTACK$");
+  
   TGeoMedium *medChip;
 
   if (dummy)
@@ -118,4 +129,60 @@ TGeoVolume* AlpideChip::createChip(const Double_t ychip,
 
   // Done, return the chip
   return chipVol;
+}
+
+//________________________________________________________________________
+void AlpideChip::createMaterials(Int_t id, Int_t ifield, Float_t fieldm)
+{
+  // create common materials with the MFT
+
+  auto& mgr = o2::Base::MaterialManager::Instance();
+
+  Float_t tmaxfd    = 0.1;     // Degree
+  Float_t stemax    = 1.0;     // cm
+  Float_t deemax    = 0.1;     // Fraction of particle's energy 0<deemax<=1
+  Float_t epsil     = 1.0E-4;  // cm
+  Float_t stmin     = 0.0;     // cm "Default value used"
+
+  Float_t tmaxfdSi  = 0.1;     // Degree
+  Float_t stemaxSi  = 0.0075;  // cm
+  Float_t deemaxSi  = 0.1;     // Fraction of particle's energy 0<deemax<=1
+  Float_t epsilSi   = 1.0E-4;  // cm
+  Float_t stminSi   = 0.0;     // cm "Default value used"
+
+  Float_t tmaxfdAir = 0.1;     // Degree
+  Float_t stemaxAir = 1.0;     // cm
+  Float_t deemaxAir = 0.1;     // Fraction of particle's energy 0<deemax<=1
+  Float_t epsilAir  = 1.0E-4;  // cm
+  Float_t stminAir  = 0.0;     // cm "Default value used"
+
+  // BEOL (Metal interconnection stack in Si sensors)
+  Float_t aBEOL[3] = {26.982, 28.086, 15.999};
+  Float_t zBEOL[3] = {13, 14, 8}; // Al, Si, O
+  Float_t wBEOL[3] = {0.170, 0.388, 0.442};
+  Float_t dBEOL = 2.28;
+
+  // AIR
+  Float_t aAir[4] = {12.0107, 14.0067, 15.9994, 39.948};
+  Float_t zAir[4] = {6., 7., 8., 18.};
+  Float_t wAir[4] = {0.000124, 0.755267, 0.231781, 0.012827};
+  Float_t dAir = 1.20479E-3;
+
+  if (mgr.getMediumID("ALPIDE",id) < 0) {
+    mgr.Mixture("ALPIDE", id, "METALSTACK$", aBEOL, zBEOL, dBEOL, 3, wBEOL);
+    mgr.Medium("ALPIDE", id, "METALSTACK$", id, 0, ifield, fieldm, tmaxfd, stemax, deemax, epsil, stmin);
+    id++;
+  }
+
+  if (mgr.getMediumID("ALPIDE",id) < 0) {
+    mgr.Mixture("ALPIDE", id, "AIR$", aAir, zAir, dAir, 4, wAir);
+    mgr.Medium("ALPIDE", id, "AIR$", id, 0, ifield, fieldm, tmaxfdAir, stemaxAir, deemaxAir, epsilAir, stminAir);
+    id++;
+  }
+
+  if (mgr.getMediumID("ALPIDE",id) < 0) {
+    mgr.Material("ALPIDE", id, "SI$", 0.28086E+02, 0.14000E+02, 0.23300E+01, 0.93600E+01, 0.99900E+03);
+    mgr.Medium("ALPIDE", id, "SI$", id, 0, ifield, fieldm, tmaxfdSi, stemaxSi, deemaxSi, epsilSi, stminSi);
+  }
+
 }
