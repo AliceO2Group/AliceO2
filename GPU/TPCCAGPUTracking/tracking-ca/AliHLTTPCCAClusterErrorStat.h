a70f114e9d66eea1a1a5fd97d8e036b8db9ae907
drohr <drohr@cern.ch>
2018-02-28 23:01:48 +0100

    add tool to dump cluster to track residuals in HLT tracking, some cleanup, remove deprecated debug code

---
+++ GPU/TPCCAGPUTracking/tracking-ca/AliHLTTPCCAClusterErrorStat.h
@@ -0,0 +1,58 @@
+#ifndef ALIHLTTPCCACLUSTERERRORSTAT_H
+#define ALIHLTTPCCACLUSTERERRORSTAT_H
+
+//#define EXTRACT_RESIDUALS
+
+#ifdef EXTRACT_RESIDUALS
+#include "cagpu/AliHLTTPCCAGPURootDump.h"
+
+struct AliHLTTPCCAClusterErrorStat
+{
+	AliHLTTPCCAClusterErrorStat(int maxN) : fTupBuf(maxN) {}
+	
+	static AliHLTTPCCAGPURootDump<TNtuple> fTup;
+	static long long int fCount;
+	
+	std::vector<std::array<float, 10>> fTupBuf;
+
+	void Fill(float x, float y, float z, float alpha, float trkX, float *fP, float *fC, int ihit, int iWay)
+	{
+		if (iWay == 1)
+		{
+			fTupBuf[ihit] = {fP[0], fP[1], fP[2], fP[3], fP[4], fC[0], fC[2], fC[5], fC[9], fC[14]};
+		}
+		else if (iWay == 2)
+		{
+			fTup.Fill(x, y, z, alpha, trkX,
+				(fP[0] * fTupBuf[ihit][5] + fTupBuf[ihit][0] * fC[0]) / (fTupBuf[ihit][5] + fC[0]),
+				(fP[1] * fTupBuf[ihit][6] + fTupBuf[ihit][1] * fC[2]) / (fTupBuf[ihit][6] + fC[2]),
+				(fP[2] * fTupBuf[ihit][7] + fTupBuf[ihit][2] * fC[5]) / (fTupBuf[ihit][7] + fC[5]),
+				(fP[3] * fTupBuf[ihit][8] + fTupBuf[ihit][3] * fC[9]) / (fTupBuf[ihit][8] + fC[9]),
+				(fP[4] * fTupBuf[ihit][9] + fTupBuf[ihit][4] * fC[14]) / (fTupBuf[ihit][9] + fC[14]),
+				fC[0] * fTupBuf[ihit][5] / (fC[0] + fTupBuf[ihit][5]),
+				fC[2] * fTupBuf[ihit][6] / (fC[2] + fTupBuf[ihit][6]),
+				fC[5] * fTupBuf[ihit][7] / (fC[5] + fTupBuf[ihit][7]),
+				fC[9] * fTupBuf[ihit][8] / (fC[9] + fTupBuf[ihit][8]),
+				fC[14] * fTupBuf[ihit][9] / (fC[14] + fTupBuf[ihit][9]));
+			if (++fCount == 671689)
+			{
+				printf("Reached %lld clusters in error stat, exiting\n", fCount);
+				fTup.~AliHLTTPCCAGPURootDump<TNtuple>();
+				exit(0);
+			}
+		}
+	}
+};
+
+AliHLTTPCCAGPURootDump<TNtuple> AliHLTTPCCAClusterErrorStat::fTup("clusterres.root", "clusterres", "clusterres", "clX:clY:clZ:angle:trkX:trkY:trkZ:trkSinPhi:trkDzDs:trkQPt:trkSigmaY2:trkSigmaZ2:trkSigmaSinPhi2:trkSigmaDzDs2:trkSigmaQPt2");
+long long int AliHLTTPCCAClusterErrorStat::fCount = 0;
+#else
+struct AliHLTTPCCAClusterErrorStat
+{
+	AliHLTTPCCAClusterErrorStat(int maxN) {}
+	void Fill(float x, float y, float z, float alpha, float trkX, float *fP, float *fC, int ihit, int iWay) {}
+};
+
+#endif
+
+#endif
