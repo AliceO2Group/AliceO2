9e23cab68639a92dc4936cc6f33c73d080002b82
shahoian <ruben.shahoyan@cern.ch>
2018-03-01 19:19:26 +0100

    Basic vertex class with templated timeStamp + test

---
+++ DataFormats/Reconstruction/include/ReconstructionDataFormats/Vertex.h
@@ -0,0 +1,133 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#ifndef ALICEO2_VERTEX_H
+#define ALICEO2_VERTEX_H
+
+#include "MathUtils/Cartesian3D.h"
+#include "CommonDataFormat/TimeStamp.h"
+#include <array>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+
+namespace o2
+{
+namespace dataformats
+{
+
+// Base primary vertex class, with position, error, N candidates and flags field
+// The Stamp template parameter allows to define vertex (time)stamp in different
+// formats (ITS ROFrame ID, real time + error etc)
+
+template <typename Stamp = o2::dataformats::TimeStamp<int>>
+class Vertex
+{
+  using ushort = unsigned short;
+
+ public:
+  enum CovElems : int { kCovXX, kCovXY, kCovYY, kCovXZ, kCovYZ, kCovZZ, kNCov };
+  static ushort constexpr FlagsMask = 0xffff;
+
+  Vertex() = default;
+  ~Vertex() = default;
+  Vertex(const Point3D<float>& pos, const std::array<float, kNCov>& cov, ushort nCont, float chi2)
+    : mPos(pos), mCov(cov), mNContributors(nCont), mChi2(chi2)
+  {
+  }
+
+  void print() const;
+
+  // getting the cartesian coordinates and errors
+  float getX() const { return mPos.X(); }
+  float getY() const { return mPos.Y(); }
+  float getZ() const { return mPos.Z(); }
+  float getSigmaX2() const { return mCov[kCovXX]; }
+  float getSigmaY2() const { return mCov[kCovYY]; }
+  float getSigmaZ2() const { return mCov[kCovZZ]; }
+  float getSigmaXY() const { return mCov[kCovXY]; }
+  float getSigmaXZ() const { return mCov[kCovXZ]; }
+  float getSigmaYZ() const { return mCov[kCovYZ]; }
+  const std::array<float, kNCov>& getCov() const { return mCov; }
+
+  Point3D<float> getXYZ() const { return mPos; }
+  Point3D<float>& getXYZ() { return mPos; }
+
+  void setX(float x) { mPos.SetX(x); }
+  void setY(float y) { mPos.SetY(y); }
+  void setZ(float z) { mPos.SetZ(z); }
+
+  void setXYZ(float x, float y, float z)
+  {
+    setX(x);
+    setY(y);
+    setZ(z);
+  }
+  void setPos(const Point3D<float>& p) { mPos = p; }
+
+  void setSigmaX2(float v) { mCov[kCovXX] = v; }
+  void setSigmaY2(float v) { mCov[kCovYY] = v; }
+  void setSigmaZ2(float v) { mCov[kCovZZ] = v; }
+  void setSigmaXY(float v) { mCov[kCovXY] = v; }
+  void setSigmaXZ(float v) { mCov[kCovXZ] = v; }
+  void setSigmaYZ(float v) { mCov[kCovYZ] = v; }
+  void setCov(float sxx, float sxy, float syy, float sxz, float syz, float szz)
+  {
+    setSigmaX2(sxx);
+    setSigmaY2(syy);
+    setSigmaZ2(szz);
+    setSigmaXY(sxy);
+    setSigmaXZ(sxz);
+    setSigmaYZ(syz);
+  }
+  void setCov(const std::array<float, kNCov>& cov) { mCov = cov; }
+
+  ushort getNContributors() const { return mNContributors; }
+  void setNContributors(ushort v) { mNContributors = v; }
+
+  ushort getBits() const { return mBits; }
+  bool isBitSet(int bit) const { return mBits & (FlagsMask & (0x1 << bit)); }
+  void setBits(ushort b) { mBits = b; }
+  void setBit(int bit) { mBits |= FlagsMask & (0x1 << bit); }
+  void resetBit(int bit) { mBits &= ~(FlagsMask & (0x1 << bit)); }
+
+  void setChi2(float v) { mChi2 = v; }
+  float getChi2() const { return mChi2; }
+
+  const Stamp& getTimeStamp() const { return mTimeStamp; }
+  Stamp& getTimeStamp() { return mTimeStamp; }
+  void setTimeStamp(const Stamp& v) { mTimeStamp = v; }
+
+ private:
+  Point3D<float> mPos;           ///< cartesian position
+  std::array<float, kNCov> mCov; ///< errors, see CovElems enum
+  float mChi2 = 0;               ///< chi2 or quality of tracks to vertex attachment
+  ushort mNContributors = 0;     ///< N contributors
+  ushort mBits = 0;              ///< bit field for flags
+  Stamp mTimeStamp;              ///< vertex time-stamp
+
+  ClassDefNV(Vertex, 1);
+};
+
+template <typename Stamp>
+std::ostream& operator<<(std::ostream& os, const Vertex<Stamp>& v)
+{
+  // stream itself
+  os << std::scientific << "Vertex X: " << v.getX() << " Y: " << v.getY() << " Z: " << v.getZ()
+     << " NCont: " << v.getNContributors() << " Chi2: " << v.getChi2() << "\nCov.mat:\n"
+     << v.getSigmaX2() << '\n'
+     << v.getSigmaXY() << ' ' << v.getSigmaY2() << '\n'
+     << v.getSigmaXZ() << ' ' << v.getSigmaYZ() << ' ' << v.getSigmaZ2() << '\n'
+     << "TimeStamp: " << v.getTimeStamp();
+  return os;
+}
+}
+} // end namespace AliceO2
+#endif
---
+++ DataFormats/Reconstruction/test/testVertex.cxx
@@ -0,0 +1,57 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#define BOOST_TEST_MODULE Test Vertex class
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+#include "CommonDataFormat/TimeStamp.h"
+#include "ReconstructionDataFormats/Vertex.h"
+#include <TFile.h>
+#include <array>
+
+namespace o2
+{
+using myTS = o2::dataformats::TimeStampWithError<double, double>;
+using myVtx = o2::dataformats::Vertex<myTS>;
+
+// basic Vertex tests
+BOOST_AUTO_TEST_CASE(Vertex)
+{
+  const Point3D<float> pos(0.1, 0.2, 3.0);
+  const std::array<float, myVtx::kNCov> cov = { 1e-4, -1e-9, 2e-4, -1e-9, 1e-9, 1e-4 };
+  int nc = 10;
+  float chi2 = 5.5f;
+  myVtx vtx(pos, cov, nc, chi2);
+  myTS ts(1234.567, 0.99);
+  vtx.setTimeStamp(ts);
+  std::cout << vtx << std::endl;
+  BOOST_CHECK_CLOSE(vtx.getX() + vtx.getY() + vtx.getZ(), pos.X() + pos.Y() + pos.Z(), 1e-5);
+  for (int i = 0; i < myVtx::kNCov; i++) {
+    BOOST_CHECK_CLOSE(vtx.getCov()[i], cov[i], 1e-5);
+  }
+  BOOST_CHECK_CLOSE(vtx.getChi2(), chi2, 1e-5);
+  BOOST_CHECK(vtx.getNContributors() == nc);
+  BOOST_CHECK(vtx.getTimeStamp() == ts);
+
+  // test writing
+  TFile flOut("tstVtx.root", "recreate");
+  flOut.WriteObjectAny(&vtx, vtx.Class(), "vtx");
+  flOut.Close();
+  // test reading
+  std::cout << "reading back written vertex" << std::endl;
+  TFile flIn("tstVtx.root");
+  auto vtr = static_cast<myVtx*>(gFile->GetObjectUnchecked("vtx"));
+  flIn.Close();
+  std::cout << *vtr << std::endl;
+  BOOST_CHECK(vtx.getNContributors() == vtr->getNContributors());
+}
+
+} // end namespace AliceO2
