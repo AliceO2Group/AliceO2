0374de0e1641d5e5bb1167a7bbc84e71fac62609
Gabriele Gaetano Fronzé <gabriele.gaetano.fronze@cern.ch>
2018-04-20 16:50:23 +0200

    Updating copyright notices

--- Framework/Utils/test/DPLBroadcasterMerger.cxx
+++ Framework/Utils/test/DPLBroadcasterMerger.cxx
@@ -1,6 +1,14 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
 //
-// Created by Gabriele Gaetano Fronzé on 15/04/2018.
+// See http://alice-o2.web.cern.ch/license for full licensing information.
 //
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \author Gabriele Gaetano Fronzé, gfronze@cern.ch
 
 #include <fstream>
 #include "DPLBroadcasterMerger.h"
