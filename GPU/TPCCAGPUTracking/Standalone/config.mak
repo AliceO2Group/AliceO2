6469f55e67260723015442265c27fd2f56555437
David Rohr <drohr@jwdt.org>
2018-04-25 10:40:19 +0200

    First step to get the new repository working, fix compilation of standalone version of tracking

--- GPU/TPCCAGPUTracking/Standalone/config.mak
+++ GPU/TPCCAGPUTracking/Standalone/config.mak
@@ -43,6 +43,3 @@ LIBSUSE						+= $(shell root-config --libs)
 endif
 
 ALLDEP						+= config_common.mak config_options.mak
-
-o2:
-							make CONFIGFILE=config_o2.mak -f makefile
--- GPU/TPCCAGPUTracking/Standalone/include/include.h
+++ GPU/TPCCAGPUTracking/Standalone/include/include.h
@@ -8,14 +8,25 @@ extern HANDLE semLockDisplay;
 extern pthread_mutex_t semLockDisplay;
 extern void* OpenGLMain( void* );
 #endif
+
+#ifdef BUILD_EVENT_DISPLAY
 extern void ShowNextEvent();
 extern void DisplayExit();
 extern void SetCollisionFirstCluster(unsigned int collision, int slice, int cluster);
 extern volatile int exitButton;
 extern volatile int displayEventNr;
 extern volatile int sendKey;
+#else
+static void ShowNextEvent() {}
+static void DisplayExit() {}
+static void SetCollisionFirstCluster(unsigned int collision, int slice, int cluster) {}
+static int exitButton;
+static volatile int displayEventNr;
+static volatile int sendKey;
+#endif
 
 //QA
+#ifdef BUILD_QA
 extern void InitQA();
 extern void RunQA(bool matchOnly = false);
 extern int DrawQAHistograms();
@@ -23,6 +34,15 @@ extern void SetMCTrackRange(int min, int max);
 extern bool SuppressTrack(int iTrack);
 extern bool SuppressHit(int iHit);
 extern int GetMCLabel(unsigned int trackId);
+#else
+static void InitQA() {}
+static void RunQA(bool matchOnly = false) {}
+static int DrawQAHistograms() {}
+static void SetMCTrackRange(int min, int max) {}
+static bool SuppressTrack(int iTrack) {}
+static bool SuppressHit(int iHit) {}
+static int GetMCLabel(unsigned int trackId) {}
+#endif
 
 //QA - Event Generator
 extern void InitEventGenerator();
