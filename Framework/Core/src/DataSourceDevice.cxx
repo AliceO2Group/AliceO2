5d4781b2d64df7a6f1b9a2c50f8fb9f9647cdd1d
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2018-05-04 13:13:04 +0200

    remove remaining include

--- Framework/Core/src/DataSourceDevice.cxx
+++ Framework/Core/src/DataSourceDevice.cxx
@@ -8,7 +8,6 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 #include "Framework/DataSourceDevice.h"
-#include "Framework/MetricsService.h"
 #include "Framework/TMessageSerializer.h"
 #include "Framework/DataProcessor.h"
 #include "Framework/FairOptionsRetriever.h"
