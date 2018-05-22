e9e09c9c1393e2a4107b99039f72bfc7f5ba4ed7
mkrzewic <mikolaj.krzewicki@cern.ch>
2018-05-22 13:17:47 +0200

    Add empty test

---
+++ Utilities/O2Device/test/test_O2Device.cxx
@@ -0,0 +1,22 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#define BOOST_TEST_MODULE Test O2Device
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_DYN_LINK
+
+#include "O2Device/O2Device.h"
+#include <boost/test/unit_test.hpp>
+#include <iostream>
+#include <vector>
+
+using namespace o2::Base;
+
+BOOST_AUTO_TEST_CASE(TestO2Device) {}
