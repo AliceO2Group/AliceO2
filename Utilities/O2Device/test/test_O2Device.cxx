356901c8cb0e9e3abd1543be3b117832189c05e7
Mikolaj Krzewicki <mikolaj.krzewicki@cern.ch>
2018-06-11 11:22:15 +0200

    additional Stack tests (#1180)
    * Implement the full virtual interface for SpectatorMessageResource
    
    * Add more tests of Stack and the allocator interface

--- Utilities/O2Device/test/test_O2Device.cxx
+++ Utilities/O2Device/test/test_O2Device.cxx
@@ -13,10 +13,53 @@
 #define BOOST_TEST_DYN_LINK
 
 #include "O2Device/O2Device.h"
+#include "Headers/NameHeader.h"
 #include <boost/test/unit_test.hpp>
 #include <iostream>
 #include <vector>
 
 using namespace o2::Base;
+using namespace o2::header;
+using namespace o2::memoryResources;
 
-BOOST_AUTO_TEST_CASE(TestO2Device) {}
+auto factoryZMQ = FairMQTransportFactory::CreateTransportFactory("zeromq");
+auto factorySHM = FairMQTransportFactory::CreateTransportFactory("shmem");
+auto allocZMQ = getTransportAllocator(factoryZMQ.get());
+auto allocSHM = getTransportAllocator(factorySHM.get());
+
+BOOST_AUTO_TEST_CASE(getMessage_Stack)
+{
+  {
+    //check that a message is constructed properly with the default new_delete_resource
+    Stack s1{ DataHeader{ gDataDescriptionInvalid, gDataOriginInvalid, DataHeader::SubSpecificationType{ 0 }, 0 },
+              NameHeader<9>{ "somename" } };
+
+    auto message = o2::memoryResources::getMessage(std::move(s1), allocZMQ);
+
+    BOOST_REQUIRE(s1.data() == nullptr);
+    BOOST_REQUIRE(message != nullptr);
+    auto* h3 = get<NameHeader<0>*>(message->GetData());
+    BOOST_REQUIRE(h3 != nullptr);
+    BOOST_CHECK(h3->getNameLength() == 9);
+    BOOST_CHECK(0 == std::strcmp(h3->getName(), "somename"));
+    BOOST_CHECK(message->GetType() == fair::mq::Transport::ZMQ);
+  }
+  {
+    //check that a message is constructed properly, cross resource
+    Stack s1{ allocZMQ, DataHeader{ gDataDescriptionInvalid, gDataOriginInvalid, DataHeader::SubSpecificationType{ 0 }, 0 },
+              NameHeader<9>{ "somename" } };
+    BOOST_TEST(allocZMQ->getNumberOfMessages() == 1);
+
+    auto message = o2::memoryResources::getMessage(std::move(s1), allocSHM);
+
+    BOOST_TEST(allocZMQ->getNumberOfMessages() == 0);
+    BOOST_TEST(allocSHM->getNumberOfMessages() == 0);
+    BOOST_REQUIRE(s1.data() == nullptr);
+    BOOST_REQUIRE(message != nullptr);
+    auto* h3 = get<NameHeader<0>*>(message->GetData());
+    BOOST_REQUIRE(h3 != nullptr);
+    BOOST_CHECK(h3->getNameLength() == 9);
+    BOOST_CHECK(0 == std::strcmp(h3->getName(), "somename"));
+    BOOST_CHECK(message->GetType() == fair::mq::Transport::SHM);
+  }
+}
