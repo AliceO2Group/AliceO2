7f15472c59159b38576a6ef3abf13ccd95b9aef3
Florian Damas <florian.damas@cern.ch>
2018-06-26 10:18:37 +0200

    Simplify the MANU positioning

--- Detectors/MUON/MCH/Simulation/src/Station345Geometry.cxx
+++ Detectors/MUON/MCH/Simulation/src/Station345Geometry.cxx
@@ -353,9 +353,7 @@ void createPCBs()
 
       for (int j = 0; j < nMANU; j++) {
         pcb->AddNode(gGeoManager->GetVolume("MANU345"), 100 * i + j,
-                     new TGeoTranslation((j - nMANU / 2) * (length / nMANU) - (nMANU % 2 - 1) * (length / (2 * nMANU)) +
-                                           (pcbShift + shift) / 2,
-                                         y, z));
+                     new TGeoTranslation(-(pcbShift + shift + length) / 2. + (j + 1) * length / nMANU, y, z));
       }
     } // end of the MANUs loop
   }   // end of the PCBs loop
