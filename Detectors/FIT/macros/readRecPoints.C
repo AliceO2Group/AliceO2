506ca3a69ccf59885c19e34b3a9fc7f0bf4a46ab
Alla Maevskaya <Alla.Maevskaya@cern.ch>
2018-06-28 12:04:31 +0200

    add macros for digitzation, reconstruction, read hits, digits and recpoints

---
+++ Detectors/FIT/macros/readRecPoints.C
@@ -0,0 +1,58 @@
+#include "FITSimulation/Detector.h"
+#include "FITBase/Digit.h"
+#include <TH2F.h>
+void readRecPoints() 
+{
+  using namespace o2::fit;
+  //  using namespace o2::fit::Digit;
+ 
+  // Create histograms
+  TDirectory* cwd = gDirectory;
+  gDirectory = 0x0;
+ 
+  TH2F *hMultHit = new TH2F("hMultHits","photons Hits ",210, 0, 210, 500, 0, 5000);
+  TH2F *hTimeHit = new TH2F("hTimeAChit", "Time Hits", 210, 0, 210, 1000, 1000, 13000);  
+  TH2F *hMultRec = new TH2F("hMultRec","photons Recits ",210, 0, 210, 500, 0, 1000);
+  TH2F *hTimeRec = new TH2F("hTimeRec", "Time Recits", 210, 0, 210, 1000, 1, 13);  
+
+  gDirectory = cwd; 
+  
+  TFile* frec = TFile::Open("o2reco_fit.root");
+  std::cout << " Open rec file " << std::endl;
+  TTree* recTree = (TTree*)frec->Get("o2sim");
+  o2::fit::RecPoints * recArr = new RecPoints;
+  recTree->SetBranchAddress("FITRecPoints", &recArr);
+  Int_t nevD = recTree->GetEntries(); // recits in cont. readout may be grouped as few events per entry
+  std::cout << "Found " << nevD << " events with recits " << std::endl;
+  Float_t cfd[208], amp[208];
+  for (Int_t iev = 0; iev < nevD; iev++) {
+    recTree->GetEvent(iev);
+    
+    std::cout<<iev<<" AC "<<recArr->GetCollisionTime(0)<<std::endl;
+    for (int ii=0; ii<208; ii++)  { cfd[ii]=amp[ii]=0; }
+     for (const auto& d : recArr->getChDgData()) {
+       Int_t mcp = d.ChId;
+       cfd[mcp] = d.CFDTime;
+       amp[mcp] = d.QTCAmpl;
+       cout<<iev<<" "<<mcp<<" "<< cfd[mcp]<<" "<< amp[mcp]<<endl;
+       hMultRec ->Fill(Float_t (mcp),amp[mcp]); 
+       hTimeRec ->Fill(Float_t (mcp),cfd[mcp]);
+     }
+   }
+   TFile *Hfile = new TFile("FigFit_rec_pp.root","RECREATE");
+   printf("Writting histograms to root file \n");
+   Hfile->cd();
+  //Create a canvas, set the view range, show histograms
+  //  TCanvas *c1 = new TCanvas("c1","Alice T0 Time ",400,10,600,600);
+  hTimeHit->Write();
+  hMultHit->Write();
+  hTimeRec->Write();
+  hMultRec->Write();
+ 
+
+} // end of macro
+
+
+
+
+
