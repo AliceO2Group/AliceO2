d33d9e25c67b79b3d59df678d4e1ba2cacde917c
Barthelemy von Haller <barthelemy.von.haller@cern.ch>
2018-08-31 08:25:22 +0200

    CINT -> CLING

--- CCDB/src/CCDBLinkDef.h
+++ CCDB/src/CCDBLinkDef.h
@@ -8,7 +8,7 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-#ifdef __CINT__
+#ifdef __CLING__
 
 #pragma link off all globals;
 #pragma link off all classes;
