7dc7aca8dc1a963d4e1c549a192e31763d4861c0
Giulio Eulisse <giulio.eulisse@cern.ch>
2018-09-11 15:56:55 +0200

    DPL: adapt to support new FairMQ versions
    FairMQ changed `FairMQDevice::fDevice` to a shared_ptr from a
    unique_ptr. This makes sure we can compile DPL with both version and
    introduces an helper to handle this kind of situations in the future
    when updating might not be an option.

---
+++ Framework/Core/test/test_PtrHelpers.cxx
@@ -0,0 +1,43 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#define BOOST_TEST_MODULE Test Framework PtrHelpers
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_DYN_LINK
+
+#include <boost/test/unit_test.hpp>
+#include "Framework/TypeTraits.h"
+#include <memory>
+
+struct Base {
+  int v;
+};
+struct A : public Base {
+  A(int v_, int va_) : Base{ v_ }, va{ va_ } {}
+  int va;
+};
+struct B : public Base {
+  B(int v_, int vb_) : Base{ v_ }, vb{ vb_ } {}
+  int vb;
+};
+
+BOOST_AUTO_TEST_CASE(MatchingPtrMaker)
+{
+
+  std::shared_ptr<Base> s = std::move(o2::framework::make_matching<decltype(s), A>(1, 3));
+  std::unique_ptr<Base> u = std::move(o2::framework::make_matching<decltype(u), B>(2, 4));
+
+  BOOST_REQUIRE(s != nullptr);
+  BOOST_REQUIRE(u != nullptr);
+  BOOST_CHECK_EQUAL(s->v, 1);
+  BOOST_CHECK_EQUAL(u->v, 2);
+  BOOST_CHECK_EQUAL(static_cast<A*>(s.get())->va, 3);
+  BOOST_CHECK_EQUAL(static_cast<B*>(u.get())->vb, 4);
+}
