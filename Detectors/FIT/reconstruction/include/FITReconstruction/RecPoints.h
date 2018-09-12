45f683e1d3a4617141346b49c445423756857ba7
Alla Maevskaya <Alla.Maevskaya@cern.ch>
2018-09-12 14:03:42 +0200

    add Copyright

--- Detectors/FIT/reconstruction/include/FITReconstruction/RecPoints.h
+++ Detectors/FIT/reconstruction/include/FITReconstruction/RecPoints.h
@@ -1,3 +1,15 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file RecPoints.h
+/// \brief Definition of the FIT RecPoints object
 #ifndef ALICEO2_FIT_RECPOINTS_H
 #define ALICEO2_FIT_RECPOINTS_H
 
@@ -9,7 +21,7 @@
 
 namespace o2
 {
-namespace fit
+namespace fit 
 {
 class RecPoints
 {
