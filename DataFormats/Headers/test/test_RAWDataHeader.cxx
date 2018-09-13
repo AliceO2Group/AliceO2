ecbfddafd9f6a209d2641ca33957e5cb82fc0a1a
Matthias Richter <Matthias.Richter@scieq.net>
2018-09-13 11:35:28 +0200

    Updating RAWDataHeader to version 3
    RDH v3 consists of 4 64 bit words, each of the words is extended to 128 bits
    by the CRU
    https://docs.google.com/document/d/1otkSDYasqpVBDnxplBI7dWNxaZohctA-bvhyrzvtLoQ
    accessed on Sep 12 2018, a pdf version is available under this link
    https://alice-o2-project.web.cern.ch/_webdav/files/pdf/RDH%20V3_2.pdf

--- DataFormats/Headers/test/test_RAWDataHeader.cxx
+++ DataFormats/Headers/test/test_RAWDataHeader.cxx
@@ -19,64 +19,79 @@ using RDH = o2::header::RAWDataHeader;
 
 BOOST_AUTO_TEST_CASE(test_rdh)
 {
-  static_assert(sizeof(RDH) == 32, "the RAWDataHeader is supposed to be 256 bit");
+  static_assert(sizeof(RDH) == 64, "the RAWDataHeader is supposed to be 512 bit");
 
   // check the defaults
   RDH defaultRDH;
-  BOOST_CHECK(defaultRDH.version == 2);
+  BOOST_CHECK(defaultRDH.version == 3);
   BOOST_CHECK(defaultRDH.blockLength == 0);
   BOOST_CHECK(defaultRDH.feeId == 0xffff);
-  BOOST_CHECK(defaultRDH.linkId == 0xff);
-  BOOST_CHECK(defaultRDH.headerSize == 4); // header size in 64 bit words
+  //BOOST_CHECK(defaultRDH.linkId == 0xff);
+  BOOST_CHECK(defaultRDH.headerSize == 8); // header size in 64 bit words
 
   using byte = unsigned char;
   std::array<byte, sizeof(RDH)> buffer;
   buffer.fill(0);
 
-  buffer[0] = 0x2; // set version 2
-  buffer[1] = 0x6;
-  buffer[2] = 0x1;
-  buffer[3] = 0xad;
-  buffer[4] = 0xde;
-  buffer[5] = 0xaf;
-  buffer[6] = 0x04;
+  buffer[0] = 0x3;  // set version 3
+  buffer[1] = 0x08; // header size 8 64 bit words
+  buffer[2] = 0x6;
+  buffer[3] = 0x1;
+  buffer[4] = 0xad;
+  buffer[5] = 0xde;
+  buffer[6] = 0;
   buffer[7] = 0;
 
-  buffer[16] = 0x23;
-  buffer[17] = 0x71;
-  buffer[18] = 0x98;
-  buffer[19] = 0xba;
-  buffer[20] = 0xdc;
-  buffer[21] = 0x3e;
-  buffer[22] = 0x12;
-  buffer[23] = 0;
+  // do not set anything in words 1,2, and 3
 
-  buffer[24] = 0x0f;
-  buffer[25] = 0xd0;
-  buffer[26] = 0;
-  buffer[27] = 0xad;
-  buffer[28] = 0xde;
-  buffer[29] = 0;
+  buffer[32] = 0x23;
+  buffer[33] = 0x71;
+  buffer[34] = 0x98;
+  buffer[35] = 0xba;
+  buffer[36] = 0xdc;
+  buffer[37] = 0x0e;
+  buffer[38] = 0x12;
+  buffer[39] = 0x0a;
+
+  // do not set anything in word 5
+
+  buffer[48] = 0x00;
+  buffer[49] = 0x0f;
+  buffer[50] = 0xd0;
+  buffer[51] = 0x00;
+  buffer[52] = 0xad;
+  buffer[53] = 0xde;
+  buffer[54] = 0;
+  buffer[55] = 0;
+
+  // do not set anything in word 7
 
   auto rdh = reinterpret_cast<RDH*>(buffer.data());
 
   // if the tests fail, we are probbaly on a big endian architecture or
   // there are alignment issues
-  BOOST_CHECK(rdh->version == 2);
+  BOOST_CHECK(rdh->version == 3);
   BOOST_CHECK(rdh->blockLength == 262);
   BOOST_CHECK(rdh->feeId == 0xdead);
-  BOOST_CHECK(rdh->linkId == 0xaf);
-  BOOST_CHECK(rdh->headerSize == 4);
+  //BOOST_CHECK(rdh->linkId == 0xaf);
+  BOOST_CHECK(rdh->headerSize == 8);
+  BOOST_CHECK(rdh->priority == 0);
   BOOST_CHECK(rdh->zero0 == 0);
+  BOOST_CHECK(rdh->word1 == 0);
+  BOOST_CHECK(rdh->word2 == 0);
+  BOOST_CHECK(rdh->word3 == 0);
   BOOST_CHECK(rdh->triggerOrbit == 0);
   BOOST_CHECK(rdh->heartbeatOrbit == 0);
-  BOOST_CHECK(rdh->triggerBC == 0x123);
-  BOOST_CHECK(rdh->triggerType == 0xedcba987);
-  BOOST_CHECK(rdh->heartbeatBC == 0x123);
-  BOOST_CHECK(rdh->zero2 == 0);
+  BOOST_CHECK(rdh->triggerType == 0xba987123);
+  BOOST_CHECK(rdh->triggerBC == 0xedc);
+  BOOST_CHECK(rdh->heartbeatBC == 0xa12);
+  BOOST_CHECK(rdh->zero41 == 0);
+  BOOST_CHECK(rdh->zero42 == 0);
+  BOOST_CHECK(rdh->word5 == 0);
   BOOST_CHECK(rdh->pageCnt == 0xd00f);
   BOOST_CHECK(rdh->stop == 0);
   BOOST_CHECK(rdh->detectorField == 0xdead);
   BOOST_CHECK(rdh->par == 0);
-  BOOST_CHECK(rdh->zero3 == 0);
+  BOOST_CHECK(rdh->zero6 == 0);
+  BOOST_CHECK(rdh->word7 == 0);
 }
