561e37e4690fef4bd6ce8cda0ad739536a6f44be
Giulio Eulisse <giulio.eulisse@cern.ch>
2018-10-03 10:24:13 +0200

    DPL: workaround hanging test
    While this should have been gracefully handled by FairMQ, it does not
    really make sense to quit when the producer is done, as all the testing
    happens in the consumer.

--- Framework/Core/test/test_ProcessorOptions.cxx
+++ Framework/Core/test/test_ProcessorOptions.cxx
@@ -38,7 +38,6 @@ WorkflowSpec defineDataProcessing(ConfigContext const&)
             // there is nothing to do, simply stop the workflow but we have to send at least one message
             // to make sure that the callback of the consumer is called
             ctx.outputs().make<int>(Output{ "TST", "TEST", 0, Lifetime::Timeframe }) = 42;
-            ctx.services().get<ControlService>().readyToQuit(true);
           };
         },
       },
