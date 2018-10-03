a79b51242f1748f322449b611daef2d5b250b25a
David Rohr <drohr@jwdt.org>
2018-10-03 12:16:15 +0200

    Add flag for tracks crossing CE, unify flags

--- GPU/TPCCAGPUTracking/Merger/AliHLTTPCGMMergedTrack.h
+++ GPU/TPCCAGPUTracking/Merger/AliHLTTPCGMMergedTrack.h
@@ -15,7 +15,7 @@
 
 /**
  * @class AliHLTTPCGMMergedTrack
- * 
+ *
  * The class is used to store merged tracks in AliHLTTPCGMMerger
  */
 class AliHLTTPCGMMergedTrack
@@ -32,40 +32,41 @@ class AliHLTTPCGMMergedTrack
   GPUd() float LastX()                           const { return fLastX;           }
   GPUd() float LastY()                           const { return fLastY;           }
   GPUd() float LastZ()                           const { return fLastZ;           }
-  GPUd() bool OK()                               const { return fOK;              }
-  GPUd() char CSide()                            const { return fCSide;           }
-  GPUd() bool Looper()                           const { return fLooper;          }
-
+  GPUd() bool OK()                               const { return fFlags & 0x01;    }
+  GPUd() bool Looper()                           const { return fFlags & 0x02;    }
+  GPUd() bool CSide()                            const { return fFlags & 0x04;    }
+  GPUd() bool CCE()                              const { return fFlags & 0x08;    }
+  
   GPUd() void SetNClusters      ( int v )                { fNClusters = v;       }
   GPUd() void SetNClustersFitted( int v )                { fNClustersFitted = v; }
   GPUd() void SetFirstClusterRef( int v )                { fFirstClusterRef = v; }
-  GPUd() void SetParam( const AliHLTTPCGMTrackParam &v ) { fParam = v;      }     
-  GPUd() void SetAlpha( float v )                        { fAlpha = v;      }  
+  GPUd() void SetParam( const AliHLTTPCGMTrackParam &v ) { fParam = v;      }
+  GPUd() void SetAlpha( float v )                        { fAlpha = v;      }
   GPUd() void SetLastX( float v )                        { fLastX = v; }
   GPUd() void SetLastY( float v )                        { fLastY = v; }
   GPUd() void SetLastZ( float v )                        { fLastZ = v; }
-  GPUd() void SetOK( bool v ) {fOK = v;}
-  GPUd() void SetLooper( bool v ) {fLooper = v;}
-  GPUd() void SetCSide( char v ) {fCSide = v;}
+  GPUd() void SetOK( bool v )                            { if (v) fFlags |= 0x01; else fFlags &= 0xFE; }
+  GPUd() void SetLooper( bool v )                        { if (v) fFlags |= 0x02; else fFlags &= 0xFD; }
+  GPUd() void SetCSide( bool v )                         { if (v) fFlags |= 0x04; else fFlags &= 0xFB; }
+  GPUd() void SetCCE( bool v )                           { if (v) fFlags |= 0x08; else fFlags &= 0xF7; }
+  GPUd() void SetFlags ( unsigned char v )               { fFlags = v; }
   
   GPUd() const AliHLTTPCGMTrackParam::AliHLTTPCCAOuterParam& OuterParam() const {return fOuterParam;}
   GPUd() AliHLTTPCGMTrackParam::AliHLTTPCCAOuterParam& OuterParam() {return fOuterParam;}
   
  private:
 
-  AliHLTTPCGMTrackParam fParam; //* fitted track parameters 
+  AliHLTTPCGMTrackParam fParam; //* fitted track parameters
   AliHLTTPCGMTrackParam::AliHLTTPCCAOuterParam fOuterParam; //* outer param
 
-  float fAlpha;                 //* alpha angle 
+  float fAlpha;                 //* alpha angle
   float fLastX; //* outer X
   float fLastY; //* outer Y
   float fLastZ; //* outer Z
   int fFirstClusterRef;         //* index of the first track cluster in corresponding cluster arrays
   int fNClusters;               //* number of track clusters
   int fNClustersFitted;         //* number of clusters used in fit
-  bool fOK;
-  bool fLooper;
-  char fCSide;
+  unsigned char fFlags;
 };
 
-#endif 
+#endif
