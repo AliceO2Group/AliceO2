52072227c1164a7656de249022ba78644399d80e
Giulio Eulisse <giulio.eulisse@cern.ch>
2018-10-16 16:53:46 +0200

    DPL: add query builder for DataDescriptors from a config string
    This can be used to e.g. allow the user to select which outputs to save
    from command line, or to write a InputSpec in a generic manner, without
    having to rely on the O2 Data Model descriptors.
    
    For the moment the syntax is very basic:
    
        <origin>/<description>/<subSpec>,<...>
    
    but we can complicate it as needed in later PRs.

--- Framework/Core/test/test_DataDescriptorMatcher.cxx
+++ Framework/Core/test/test_DataDescriptorMatcher.cxx
@@ -12,8 +12,10 @@
 #define BOOST_TEST_MAIN
 #define BOOST_TEST_DYN_LINK
 
-#include <boost/test/unit_test.hpp>
 #include "Framework/DataDescriptorMatcher.h"
+#include "Framework/DataDescriptorQueryBuilder.h"
+
+#include <boost/test/unit_test.hpp>
 
 using namespace o2::framework;
 using namespace o2::header;
@@ -46,14 +48,14 @@ BOOST_AUTO_TEST_CASE(TestSimpleMatching)
   header4.subSpecification = 0;
 
   DataDescriptorMatcher matcher{
-    OriginValueMatcher{ "TPC" },
     DataDescriptorMatcher::Op::And,
+    OriginValueMatcher{ "TPC" },
     std::make_unique<DataDescriptorMatcher>(
-      DescriptionValueMatcher{ "CLUSTERS" },
       DataDescriptorMatcher::Op::And,
+      DescriptionValueMatcher{ "CLUSTERS" },
       std::make_unique<DataDescriptorMatcher>(
-        SubSpecificationTypeValueMatcher{ 1 },
         DataDescriptorMatcher::Op::And,
+        SubSpecificationTypeValueMatcher{ 1 },
         ConstantValueMatcher{ true }))
   };
 
@@ -64,8 +66,8 @@ BOOST_AUTO_TEST_CASE(TestSimpleMatching)
   BOOST_CHECK(matcher.match(header4) == false);
 
   DataDescriptorMatcher matcher1{
-    OriginValueMatcher{ "TPC" },
     DataDescriptorMatcher::Op::Or,
+    OriginValueMatcher{ "TPC" },
     OriginValueMatcher{ "ITS" }
   };
 
@@ -76,8 +78,7 @@ BOOST_AUTO_TEST_CASE(TestSimpleMatching)
   BOOST_CHECK(matcher1.match(header4) == false);
 
   DataDescriptorMatcher matcher2{
-    ConstantValueMatcher{ true },
-    DataDescriptorMatcher::Op::And,
+    DataDescriptorMatcher::Op::Just,
     DescriptionValueMatcher{ "TRACKLET" }
   };
 
@@ -86,4 +87,60 @@ BOOST_AUTO_TEST_CASE(TestSimpleMatching)
   BOOST_CHECK(matcher2.match(header2) == true);
   BOOST_CHECK(matcher2.match(header3) == false);
   BOOST_CHECK(matcher2.match(header4) == true);
+}
+
+BOOST_AUTO_TEST_CASE(TestQueryBuilder)
+{
+  DataHeader header0;
+  header0.dataOrigin = "TPC";
+  header0.dataDescription = "CLUSTERS";
+  header0.subSpecification = 1;
+
+  DataHeader header1;
+  header1.dataOrigin = "ITS";
+  header1.dataDescription = "TRACKLET";
+  header1.subSpecification = 2;
+
+  DataHeader header2;
+  header2.dataOrigin = "TPC";
+  header2.dataDescription = "TRACKLET";
+  header2.subSpecification = 1;
+
+  DataHeader header3;
+  header3.dataOrigin = "TPC";
+  header3.dataDescription = "CLUSTERS";
+  header3.subSpecification = 0;
+
+  DataHeader header4;
+  header4.dataOrigin = "TRD";
+  header4.dataDescription = "TRACKLET";
+  header4.subSpecification = 0;
+
+  auto matcher1 = DataDescriptorQueryBuilder::buildFromKeepConfig("TPC/CLUSTERS/1");
+  BOOST_CHECK(matcher1->match(header0) == true);
+  BOOST_CHECK(matcher1->match(header1) == false);
+  BOOST_CHECK(matcher1->match(header2) == false);
+  BOOST_CHECK(matcher1->match(header3) == false);
+  BOOST_CHECK(matcher1->match(header4) == false);
+
+  auto matcher2 = DataDescriptorQueryBuilder::buildFromKeepConfig("ITS/TRACKLET/2");
+  BOOST_CHECK(matcher2->match(header0) == false);
+  BOOST_CHECK(matcher2->match(header1) == true);
+  BOOST_CHECK(matcher2->match(header2) == false);
+  BOOST_CHECK(matcher2->match(header3) == false);
+  BOOST_CHECK(matcher2->match(header4) == false);
+
+  auto matcher3 = DataDescriptorQueryBuilder::buildFromKeepConfig("TPC/CLUSTERS/1,ITS/TRACKLET/2");
+  BOOST_CHECK(matcher3->match(header0) == true);
+  BOOST_CHECK(matcher3->match(header1) == true);
+  BOOST_CHECK(matcher3->match(header2) == false);
+  BOOST_CHECK(matcher3->match(header3) == false);
+  BOOST_CHECK(matcher3->match(header4) == false);
+
+  auto matcher4 = DataDescriptorQueryBuilder::buildFromKeepConfig("");
+  BOOST_CHECK(matcher4->match(header0) == false);
+  BOOST_CHECK(matcher4->match(header1) == false);
+  BOOST_CHECK(matcher4->match(header2) == false);
+  BOOST_CHECK(matcher4->match(header3) == false);
+  BOOST_CHECK(matcher4->match(header4) == false);
 }
