2a8851b5299afa026d01e765f5dd6dcee92efe86
Andreas Mathis <andreas.mathis@cern.ch>
2018-10-19 16:25:03 +0200

    Noise and pedestal retrieval from CDB interface

--- Detectors/TPC/simulation/src/TPCSimulationLinkDef.h
+++ Detectors/TPC/simulation/src/TPCSimulationLinkDef.h
@@ -14,7 +14,6 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class o2::TPC::Baseline + ;
 #pragma link C++ class o2::TPC::Detector + ;
 #pragma link C++ class o2::Base::DetImpl < o2::TPC::Detector > +;
 #pragma link C++ class o2::TPC::DigitMCMetaData + ;
