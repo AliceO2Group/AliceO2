8bc7254ed64db373cd8c68676982fdf3b55a4dc9
Maximiliano Puccio <maximiliano.puccio@cern.ch>
2018-10-31 14:36:43 +0100

    Remove old classes from the linkdef

--- Detectors/ITSMFT/ITS/reconstruction/src/ITSReconstructionLinkDef.h
+++ Detectors/ITSMFT/ITS/reconstruction/src/ITSReconstructionLinkDef.h
@@ -14,13 +14,9 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class o2::ITS::CA::Event+;
-// #pragma link C++ class o2::ITS::CA::Vertexer+;
 //#pragma link C++ class o2::ITS::TrivialClustererTask+;
 #pragma link C++ class o2::ITS::ClustererTask+;
 #pragma link C++ class o2::ITS::CookedTrackerTask+;
 #pragma link C++ class o2::ITS::CookedTracker + ;
-
-#pragma link C++ class o2::ITS::CA::VertexerTask+;
 
 #endif
