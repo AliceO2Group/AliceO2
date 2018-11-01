84e7c7010d3200939fa76757c8450d8222adb56a
David Rohr <drohr@jwdt.org>
2018-11-01 09:28:08 +0100

    Add internal O2Dependencies file, so changes in directors structure need not be synchred with O2Dependencies.cmake in O2

---
+++ GPU/cmake/O2Dependencies.cmake
@@ -0,0 +1,59 @@
+o2_define_bucket(
+    NAME
+    AliTPCCommonBase_bucket
+
+    DEPENDENCIES
+
+    INCLUDE_DIRECTORIES
+    ${ALITPCCOMMON_DIR}/sources/Common
+)
+
+o2_define_bucket(
+    NAME
+    TPCFastTransformation_bucket
+
+    DEPENDENCIES
+    dl
+    pthread
+    root_base_bucket
+    common_vc_bucket
+    AliTPCCommonBase_bucket
+
+    INCLUDE_DIRECTORIES
+    ${ROOT_INCLUDE_DIR}
+    ${ALITPCCOMMON_DIR}/sources/TPCFastTransformation
+)
+
+o2_define_bucket(
+    NAME
+    TPCCAGPUTracking_bucket
+
+    DEPENDENCIES
+    dl
+    pthread
+    root_base_bucket
+    common_vc_bucket
+    AliTPCCommonBase_bucket
+
+    INCLUDE_DIRECTORIES
+    ${ROOT_INCLUDE_DIR}
+    ${ALITPCCOMMON_DIR}/sources/TPCCAGPUTracking/SliceTracker
+    ${ALITPCCOMMON_DIR}/sources/TPCCAGPUTracking/Merger
+    ${ALITPCCOMMON_DIR}/sources/TPCCAGPUTracking/TRDTracking
+    ${ALITPCCOMMON_DIR}/sources/TPCCAGPUTracking/Interface
+    ${ALITPCCOMMON_DIR}/sources/TPCCAGPUTracking/HLTHeaders
+    ${ALITPCCOMMON_DIR}/sources/TPCCAGPUTracking/Standalone/cmodules
+    ${ALITPCCOMMON_DIR}/sources/TPCCAGPUTracking/Standalone/include
+)
+
+o2_define_bucket(
+    NAME
+    TPCSpaceChargeBase_bucket
+
+    DEPENDENCIES
+    root_base_bucket Hist MathCore Matrix Physics AliTPCCommonBase_bucket
+
+    INCLUDE_DIRECTORIES
+    ${ROOT_INCLUDE_DIR}
+    ${ALITPCCOMMON_DIR}/sources/TPCSpaceChargeBase
+)
