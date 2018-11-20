ea1b4c8bcc386e9067ee1bbdb649be00590252e6
David Rohr <drohr@jwdt.org>
2018-11-20 18:29:47 +0100

    Add some comments

--- GPU/TPCCAGPUTracking/GlobalTracker/AliGPUReconstruction.h
+++ GPU/TPCCAGPUTracking/GlobalTracker/AliGPUReconstruction.h
@@ -26,6 +26,11 @@ class AliGPUReconstruction
 public:
 	virtual ~AliGPUReconstruction();
 	
+	//General definitions
+	constexpr static size_t MIN_ALIGNMENT = 64;
+	constexpr static unsigned int NSLICES = 36;
+
+	//Definition of the Geometry: Are we AliRoot or O2
 	enum GeometryType : unsigned int {RESERVED_GEOMETRY = 0, ALIROOT = 1, O2 = 2};
 	static constexpr const char* const GEOMETRY_TYPE_NAMES[] = {"INVALID", "ALIROOT", "O2"};
 	#ifdef HLTCA_TPC_GEOMETRY_O2
@@ -34,14 +39,14 @@ public:
 		static constexpr GeometryType geometryType = ALIROOT;
 	#endif
 	
+	//Functionality to create an instance of AliGPUReconstruction for the desired device
 	enum DeviceType : unsigned int {RESERVED_DEVICE = 0, CPU = 1, CUDA = 2, HIP = 3, OCL = 4};
 	static constexpr const char* const DEVICE_TYPE_NAMES[] = {"INVALID", "CPU", "CUDA", "HIP", "OCL"};
 	static AliGPUReconstruction* CreateInstance(DeviceType type = CPU, bool forceType = true);
 	static AliGPUReconstruction* CreateInstance(int type, bool forceType) {return CreateInstance((DeviceType) type, forceType);}
 	static AliGPUReconstruction* CreateInstance(const char* type, bool forceType);
 	
-	constexpr static size_t MIN_ALIGNMENT = 64;
-	constexpr static unsigned int NSLICES = 36;
+	//Structures for input and output data
 	struct InOutPointers
 	{
 		InOutPointers() : mcLabelsTPC(nullptr), nMCLabelsTPC(0), mcInfosTPC(nullptr), nMCInfosTPC(0),
@@ -68,9 +73,7 @@ public:
 		AliHLTTRDTrackletWord* trdTracklets;
 		unsigned int nTRDTracklets;
 	} mIOPtrs;
-	enum InOutPointerType : unsigned int {CLUSTER_DATA = 0, SLICE_OUT_TRACK = 1, SLICE_OUT_CLUSTER = 2, MC_LABEL_TPC = 3, MC_INFO_TPC = 4, MERGED_TRACK = 5, MERGED_TRACK_HIT = 6, TRD_TRACK = 7, TRD_TRACKLET = 8};
-	static constexpr const char* const IOTYPENAMES[] = {"TPC Clusters", "TPC Slice Tracks", "TPC Slice Track Clusters", "TPC Cluster MC Labels", "TPC Track MC Informations", "TPC Tracks", "TPC Track Clusters", "TRD Tracks", "TRD Tracklets"};
-	
+
 	struct InOutMemory
 	{
 		InOutMemory();
@@ -86,12 +89,16 @@ public:
 		std::unique_ptr<AliHLTTRDTrackletWord[]> trdTracklets;
 	} mIOMem;
 	
+	//Functionality to dump and read input / output data
+	enum InOutPointerType : unsigned int {CLUSTER_DATA = 0, SLICE_OUT_TRACK = 1, SLICE_OUT_CLUSTER = 2, MC_LABEL_TPC = 3, MC_INFO_TPC = 4, MERGED_TRACK = 5, MERGED_TRACK_HIT = 6, TRD_TRACK = 7, TRD_TRACKLET = 8};
+	static constexpr const char* const IOTYPENAMES[] = {"TPC Clusters", "TPC Slice Tracks", "TPC Slice Track Clusters", "TPC Cluster MC Labels", "TPC Track MC Informations", "TPC Tracks", "TPC Track Clusters", "TRD Tracks", "TRD Tracklets"};
+
 	void ClearIOPointers();
 	void AllocateIOMemory();
-	
 	void DumpData(const char* filename);
 	int ReadData(const char* filename);
-		
+	
+	//Helpers for memory allocation
 	static inline size_t getAlignment(size_t addr, size_t alignment = MIN_ALIGNMENT)
 	{
 		if (alignment <= 1) return 0;
@@ -103,7 +110,6 @@ public:
 	{
 		return(getAlignment(reinterpret_cast<size_t>(addr), alignment));
 	}
-
 	template <class S> static inline S* getPointerWithAlignment(size_t& basePtr, size_t nEntries = 1)
 	{
 		basePtr += getAlignment(basePtr, alignof(S));
@@ -115,34 +121,39 @@ public:
 	{
 		return getPointerWithAlignment<S>(reinterpret_cast<size_t&>(basePtr), nEntries);
 	}
-
 	template <class T, class S> static inline void computePointerWithAlignment(T*& basePtr, S*& objPtr, size_t nEntries = 1)
 	{
 		objPtr = getPointerWithAlignment<S>(reinterpret_cast<size_t&>(basePtr), nEntries);
 	}
 	
-	int RunTRDTracking();
-	
+	//Getters for external usage of tracker classes
 	AliHLTTRDTracker* GetTRDTracker() {return mTRDTracker.get();}
 	AliHLTTPCCAGPUTracker* GetTPCTracker() {return mTPCTracker.get();}
 	o2::ITS::TrackerTraits* GetITSTrackerTraits() {return mITSTrackerTraits.get();}
 	
+	//Processing functions
+	int RunTRDTracking();
+	
+	//Getters / setters for parameters
 	DeviceType GetDeviceType() const {return mDeviceType;}
 	void SetParam(const AliGPUCAParam& param) {mParam = param;}
 	const AliGPUCAParam& GetParam() const {return mParam;}
 	AliGPUCAParam& GetParam() {return mParam;}
 	
 protected:
-	AliGPUReconstruction(DeviceType type);
+	AliGPUReconstruction(DeviceType type);								//Constructor
 	
+	//Private helper functions
 	template <class T> void DumpData(FILE* fp, T** entries, unsigned int* num, InOutPointerType type);
 	template <class T> void ReadData(FILE* fp, T** entries, unsigned int* num, std::unique_ptr<T[]>* mem, InOutPointerType type);
 	template <class T> void AllocateIOMemoryHelper(unsigned int n, T* &ptr, std::unique_ptr<T[]> &u);
 	
+	//Pointers to tracker classes
 	std::unique_ptr<AliHLTTRDTracker> mTRDTracker;
 	std::unique_ptr<AliHLTTPCCAGPUTracker> mTPCTracker;
 	std::unique_ptr<o2::ITS::TrackerTraits> mITSTrackerTraits;
 	
+	//Helpers for loading device library via dlopen
 	class LibraryLoader
 	{
 	public:
@@ -166,7 +177,7 @@ protected:
 	std::shared_ptr<LibraryLoader> mMyLib = nullptr;
 	DeviceType mDeviceType;
 	
-	AliGPUCAParam mParam;
+	AliGPUCAParam mParam;												//Reconstruction parameters
 };
 
 #endif
