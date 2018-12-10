a341ae0d22d1806b33b84873437f3f810586147b
franck manso <manso@clermont.in2p3.fr>
2018-12-10 09:59:20 +0100

    New Heat Exchanger

--- Detectors/ITSMFT/MFT/base/include/MFTBase/HeatExchanger.h
+++ Detectors/ITSMFT/MFT/base/include/MFTBase/HeatExchanger.h
@@ -41,7 +41,7 @@ class HeatExchanger : public TNamed
   void createHalfDisk2(Int_t half);
   void createHalfDisk3(Int_t half);
   void createHalfDisk4(Int_t half);
-  void createManyfold(Int_t disk);
+  void createManifold(Int_t disk);
 
   Double_t getWaterRadius() { return mRWater; }
   void setWaterRadius(Double_t& Rwater) { mRWater = Rwater; }
@@ -66,7 +66,8 @@ class HeatExchanger : public TNamed
 
   const static Int_t sNMaxDisks;
 
-  enum { Bottom, Top };
+  enum { Bottom,
+         Top };
 
   TGeoVolumeAssembly* mHalfDisk;
 
@@ -81,6 +82,8 @@ class HeatExchanger : public TNamed
 
   Double_t mRohacellThickness;
 
+  Double_t mMoreLength; // additional length of water pipes outside the rohacell plate
+
   // Parameters for carbon and Rohacell
   Int_t mNDisks;      // number of planes
   Int_t mNPart[5];    // number of part of each half-disk
@@ -91,31 +94,45 @@ class HeatExchanger : public TNamed
   Double_t** mSupportXDimensions;
   Double_t** mSupportYDimensions;
 
-  // Parameters for disk0, disk1 and disk2
-  Double_t mLWater;        // Length of tube part
-  Double_t mXPosition0[3]; // position on x axis of each tube for disk 0, 1 and 2
-  Double_t mAngle0;        // angle of the sides torus part of each pipe for disk 0, 1 and 2
-  Double_t mRadius0;       // radius of the sides torus part for disk 0, 1 and 2
-  Double_t mLpartial0;     // length of partial tube part
+  // Parameters for disk0
+  Double_t mLWater0[3];    // Length of tube part
+  Double_t mXPosition0[3]; // position on x axis of each tube for disk 0
+  Double_t mAngle0[3];     // angle of the sides torus part of each pipe for disk 0
+  Double_t mRadius0[3];    // radius of the sides torus part for disk 0
+  Double_t mLpartial0[3];  // length of partial tube part
+
+  // Parameters for disk1
+  Double_t mLWater1[3];    // Length of tube part
+  Double_t mXPosition1[3]; // position on x axis of each tube for disk 1
+  Double_t mAngle1[3];     // angle of the sides torus part of each pipe for disk 1
+  Double_t mRadius1[3];    // radius of the sides torus part for disk 1
+  Double_t mLpartial1[3];  // length of partial tube part
+
+  // Parameters for disk2
+  Double_t mLWater2[3];    // Length of tube part
+  Double_t mXPosition2[3]; // position on x axis of each tube for disk 2
+  Double_t mAngle2[3];     // angle of the sides torus part of each pipe for disk 2
+  Double_t mRadius2[3];    // radius of the sides torus part for disk 2
+  Double_t mLpartial2[3];  // length of partial tube part
 
   // Parameters for disk3
-  Double_t mLWater3[3];      // length of tube part for third plan
+  Double_t mLWater3[4];      // length of tube part for third plan
   Double_t mXPosition3[4];   // tube position on x axe of each tube for disk 3
-  Double_t mAngle3[3];       // angle of sides torus of each pipe for disk 3
-  Double_t mRadius3[3];      // radius of the sides torus for disk 3
+  Double_t mAngle3[4];       // angle of sides torus of each pipe for disk 3
+  Double_t mRadius3[4];      // radius of the sides torus for disk 3
   Double_t mAngleThirdPipe3; // angle with x axe of tube part of third pipe
-  Double_t mLpartial3[2];    // length of partial tube
+  Double_t mLpartial3[4];    // length of partial tube
 
   Double_t mRadius3fourth[4]; // radius of fourth pipe torus of fourth pipe
   Double_t mAngle3fourth[4];  // angle of fourth pipe torus of fourth pipe
   Double_t mBeta3fourth[3];   // shift angle of different torus part of fourth pipe of disk 3
 
   // Parameters for disk4
-  Double_t mLWater4[3];      // length of tube part for fourth plan
+  Double_t mLWater4[4];      // length of tube part for fourth plan
   Double_t mXPosition4[5];   // tube position on x axe of each tube for disk 4
   Double_t mAngle4[6];       // angle of sides torus of each pipe for disk 4
   Double_t mRadius4[5];      // radius of the sides torus for disk 4
-  Double_t mLpartial4[2];    // length of partial tube for first and second pipe of disk 4
+  Double_t mLpartial4[4];    // length of partial tube for first and second pipe of disk 4
   Double_t mAngle4fifth[4];  // angle of torus for fifth pipe of disk 4
   Double_t mRadius4fifth[4]; // radius of torus for fifth pipe of disk 4
 
