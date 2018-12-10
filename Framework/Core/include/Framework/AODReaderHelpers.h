742a76a8d53d395730e206f9d13be42a3adbdaf6
Giulio Eulisse <giulio.eulisse@cern.ch>
2018-12-10 16:11:32 +0100

    DPL: initial attempt at a generic AOD reader
    This introduces a few helper classes which can be extended to read
    ROOT Trees which contains the new AOD structure as implemented by Peter.
    The o2AODDummy example is reworked to use it, rather then the old fake
    data.

---
+++ Framework/Core/include/Framework/AODReaderHelpers.h
@@ -0,0 +1,33 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#ifndef o2_framework_readers_AODReaderHelpers_INCLUDED_H
+#define o2_framework_readers_AODReaderHelpers_INCLUDED_H
+
+#include "Framework/AlgorithmSpec.h"
+
+namespace o2
+{
+namespace framework
+{
+namespace readers
+{
+
+struct AODReaderHelpers {
+  static AlgorithmSpec rootFileReaderCallback();
+  /// Just to make sure things work, even without a file...
+  static AlgorithmSpec fakeReaderCallback();
+};
+
+} // namespace readers
+} // namespace framework
+} // namespace o2
+
+#endif
