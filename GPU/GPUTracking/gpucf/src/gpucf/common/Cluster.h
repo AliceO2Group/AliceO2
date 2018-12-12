6a2100d18a82c4892d845d2ec9848e6ee224474a
Felix Weiglhofer <felix.weiglhofer@gmail.com>
2018-12-12 19:00:05 +0100

    Add data loader for data in text format.

---
+++ GPU/GPUTracking/gpucf/src/gpucf/common/Cluster.h
@@ -0,0 +1,14 @@
+#pragma once
+
+struct Cluster {
+    int cru;
+    int row;
+    int Q;
+    int QMax;
+    float padMean;
+    float timeMean;
+    float padSigma;
+    float timeSigma;
+};
+
+// vim: set ts=4 sw=4 sts=4 expandtab:
--- GPU/GPUTracking/gpucf/src/helloworld.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello world!" << std::endl;
-    return 0;    
-}
-
-// vim: set ts=4 sw=4 sts=4 expandtab:
