2de5f5e36ad04d5389fb6305136b3be60cfb94b7
Markus Fasel <markus.fasel@cern.ch>
2018-12-13 13:50:20 +0100

    Defining constant for EMCAL sample width (#1528)
    * Defining constant for EMCAL sample width
    
    * Addition macro to inspect hit processing for debugging
    purpose.

---
+++ macro/readEMCHits.C
@@ -0,0 +1,48 @@
+#if !defined(__CLING__) || defined(__ROOTCLING__)
+
+#include <TFile.h>
+#include <TTree.h>
+#include <TStopwatch.h>
+#include <memory>
+#include "FairLogger.h"
+#include "DataFormatsITSMFT/ROFRecord.h"
+#include "ITSMFTBase/Digit.h"
+#include "SimulationDataFormat/RunContext.h"
+#include "SimulationDataFormat/MCTruthContainer.h"
+#include "SimulationDataFormat/MCCompLabel.h"
+
+#endif
+
+/// read and draw the hits for EMC obtained from simulation
+void readEMCHits(std::string path = "./",
+                 std::string mcfileName = "o2sim.root")
+{
+  if (path.back() != '/') {
+    path += '/';
+  }
+  TH2* ep = new TH2F("etaph", "hist", 100, -0.7, 0.7, 100, 0., 7.);
+
+  std::unique_ptr<TFile> mcfile(TFile::Open((path + mcfileName).c_str()));
+  if (!mcfile || mcfile->IsZombie()) {
+    std::cout << "Failed to open input hit file " << (path + mcfileName) << std::endl;
+    return;
+  }
+
+  TTree* hitTree = (TTree*)mcfile->Get("o2sim");
+  if (!hitTree) {
+    std::cout << "Failed to get hit tree" << std::endl;
+    return;
+  }
+
+  std::vector<o2::EMCAL::Hit>* dv = nullptr;
+  hitTree->SetBranchAddress("EMCHit", &dv);
+
+  for (int iev = 0; iev < hitTree->GetEntries(); iev++) {
+    hitTree->GetEntry(iev);
+    for (const auto& h : *dv) {
+      TVector3 posvec(h.GetX(), h.GetY(), h.GetZ());
+      ep->Fill(posvec.Eta(), TVector2::Phi_0_2pi(posvec.Phi()));
+    }
+  }
+  ep->Draw("colz");
+}
