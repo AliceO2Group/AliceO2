299ad4d3768524e906c061e7dff841b0532b0a77
Felix Weiglhofer <felix.weiglhofer@gmail.com>
2018-12-15 19:19:54 +0100

    Add ClEnv class.

---
+++ GPU/GPUTracking/gpucf/src/gpucf/ClCanary.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class ClEnv;
+
+class ClCanary {
+
+public:
+    run(ClEnv &);
+
+};
+
+// vim: set ts=4 sw=4 sts=4 expandtab:
--- GPU/GPUTracking/gpucf/src/gpucf/cl.h
+++ GPU/GPUTracking/gpucf/src/gpucf/cl.h
@@ -1,6 +1,7 @@
 #pragma once
 
+#define CL_HPP_ENABLE_EXCEPTIONS
 #define CL_TARGET_OPENCL_VERSION 200
-#include <CL/cl.h>
+#include <CL/cl.hpp>
 
 // vim: set ts=4 sw=4 sts=4 expandtab:
