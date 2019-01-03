04f4365f45d6e996f042194cac49e98b6650ab0c
Felix Weiglhofer <felix.weiglhofer@gmail.com>
2019-01-03 20:15:00 +0100

    Finish clcanary.

---
+++ GPU/GPUTracking/gpucf/src/gpucf/Executable.cpp
@@ -0,0 +1,20 @@
+#include "Executable.h"
+
+#include <iostream>
+
+
+int Executable::main(int argc, const char *argv[]) {
+    args::HelpFlag help(parser, "help", "Display help menu", {'h', "help"});
+    setupFlags(parser);
+
+    try {
+        parser.ParseCLI(argc, argv);
+    } catch (const args::Help &) {
+        std::cout << parser;
+        std::exit(0);
+    }
+
+    return mainImpl();
+}
+
+// vim: set ts=4 sw=4 sts=4 expandtab:
