43316b4f7f285b59c1b41d673be6dca393335368
Michael Winn <michael.winn@cern.ch>
2019-01-04 13:34:35 +0100

    Response clean up

--- Detectors/MUON/MCH/Simulation/include/MCHSimulation/Response.h
+++ Detectors/MUON/MCH/Simulation/include/MCHSimulation/Response.h
@@ -26,15 +26,15 @@ class Response
   Response() =default;
   
   ~Response() = default;
-  Float_t getQspreadX(){ return mQspreadX;};
-  Float_t getQspreadY(){ return mQspreadY;};
-  Float_t getChargeSat(){ return mChargeSat;};
-  Float_t getChargeThreshold(){ return mChargeThreshold;};
-  Float_t etocharge(Float_t edepos);  
-  Double_t chargePad(Float_t x, Float_t y, Float_t xmin, Float_t xmax, Float_t ymin, Float_t ymax, Int_t detID, Float_t charge);
-  Double_t response(Float_t charge, Int_t detID);
-  Float_t getAnod(Float_t x, Int_t detID);
-Float_t chargeCorr();
+  float getQspreadX(){ return mQspreadX;};
+  float getQspreadY(){ return mQspreadY;};
+  float getChargeSat(){ return mChargeSat;};
+  float getChargeThreshold(){ return mChargeThreshold;};
+  float etocharge(float edepos);  
+  double chargePad(float x, float y, float xmin, float xmax, float ymin, float ymax, int detID, float charge);
+  double response(float charge, int detID);
+  float getAnod(float x, int detID);
+  float chargeCorr();
 
  private:  
   //proper parameter in aliroot in AliMUONResponseFactory.cxx
@@ -42,18 +42,18 @@ Float_t chargeCorr();
   //anything in segmentation foreseen?
   //seem to be only two different values (st. 1 and st. 2-5)...overhead limited
   //any need for separate values as in old code? in principle not...I think
-  const Float_t mQspreadX = 0.144; //charge spread in cm
-  const Float_t mQspreadY = 0.144;
+  const float mQspreadX = 0.144; //charge spread in cm
+  const float mQspreadY = 0.144;
 
   //ChargeSlope for Station 2-5
   //to be done: ChargeSlope 10 for station 1
-  const Float_t mChargeSlope = 25;//why float in Aliroot?
-  const Float_t mChargeCorr = 0.11;// number from line 122
+  const float mChargeSlope = 25;//why float in Aliroot?
+  const float mChargeCorr = 0.11;// number from line 122
   //of AliMUONResponseFactory.cxx
 
-  const Float_t mChargeThreshold= 1e-4;
+  const float mChargeThreshold= 1e-4;
   //AliMUONResponseV0.cxx constr.
-  const Float_t mChargeSat=0.61*1.25*0.2;
+  const float mChargeSat=0.61*1.25*0.2;
   //from AliMUONResponseV0.cxx
   //equals AliMUONConstants::DefaultADC2MV()*AliMUONConstants::DefaultA0()*AliMUONConstants::DefaultCapa()
   
@@ -69,17 +69,17 @@ Float_t chargeCorr();
   //  Float_t cy1 = fKy2 * fSqrtKy3 / 4. / TMath::ATan(Double_t(fSqrtKy3));
   //  fKy4 = cy1 / fKy2 / fSqrtKy3; //this line from AliMUONMathieson::SetSqrtKy3AndDeriveKy2Ky4
   //why this multiplicitation before again division? any number small compared to Float precision?
-  const Double_t mK2x[2] = {1.021026,1.010729};
-  const Double_t mSqrtK3x[2] = {0.7000,0.7131};
-  const Double_t mK4x[2]     = {0.0,0.0};
-   const Double_t mK2y[2] = {0.9778207,0.970595};
-  const Double_t mSqrtK3y[2] = {0.7550,0.7642};
-  const Double_t mK4y[2]     = {0.0,0.0};
+  const double mK2x[2]     = {1.021026,1.010729};
+  const double mSqrtK3x[2] = {0.7000,0.7131};
+  const double mK4x[2]     = {0.0,0.0};
+  const double mK2y[2]     = {0.9778207,0.970595};
+  const double mSqrtK3y[2] = {0.7550,0.7642};
+  const double mK4y[2]     = {0.0,0.0};
   //chargecorr 0.11
   
   //anode-cathode Pitch in 1/cm
   //Station 1 first entry, Station 2-5 second entry
-  const Float_t mInversePitch[2] ={1./0.21,1./0.25};
+  const float mInversePitch[2] ={1./0.21,1./0.25};
 
 
   
--- Detectors/MUON/MCH/Simulation/src/Response.cxx
+++ Detectors/MUON/MCH/Simulation/src/Response.cxx
@@ -16,7 +16,7 @@
 using namespace o2::mch;
 
 //_____________________________________________________________________
-Float_t Response::etocharge(Float_t edepos){
+float Response::etocharge(float edepos){
   //Todo convert in charge in number of electrons
   //equivalent if IntPH in AliMUONResponseV0 in Aliroot
   //to be clarified:
@@ -26,11 +26,11 @@ Float_t Response::etocharge(Float_t edepos){
   //with central seed to be reproducible?
   //TODO: dependence on station
   //TODO: check slope meaning in thesis
-  Int_t nel = Int_t(edepos*1.e9/27.4);
-  Float_t charge=0;
+  int nel = int(edepos*1.e9/27.4);
+  float charge=0;
   if (nel ==0) nel=1;
-  for (Int_t i=1; i<=nel;i++) {
-    Float_t arg=0.;
+  for (int i=1; i<=nel;i++) {
+    float arg=0.;
     while(!arg) arg = gRandom->Rndm();
     charge -= mChargeSlope*TMath::Log(arg);
     
@@ -38,10 +38,10 @@ Float_t Response::etocharge(Float_t edepos){
   return charge;
 }
 //_____________________________________________________________________
-Double_t Response::chargePad(Float_t x, Float_t y, Float_t xmin, Float_t xmax, Float_t ymin, Float_t ymax, Int_t detID, Float_t charge ){
+double Response::chargePad(float x, float y, float xmin, float xmax, float ymin, float ymax, int detID, float charge ){
   //see AliMUONResponseV0.cxx (inside DisIntegrate)
   // and AliMUONMathieson.cxx (IntXY)
-  Int_t station = 0;
+  int station = 0;
   if(detID>299) station = 1;//wrong numbers!
   //correct? should take info from segmentation
   // normalise w.r.t. Pitch
@@ -50,32 +50,32 @@ Double_t Response::chargePad(Float_t x, Float_t y, Float_t xmin, Float_t xmax, F
   ymin *= mInversePitch[station];
   ymax *= mInversePitch[station];
   // The Mathieson function
-  Double_t ux1=mSqrtK3x[station]*TMath::TanH(mK2x[station]*xmin);
-  Double_t ux2=mSqrtK3x[station]*TMath::TanH(mK2x[station]*xmax);
+  double ux1=mSqrtK3x[station]*TMath::TanH(mK2x[station]*xmin);
+  double ux2=mSqrtK3x[station]*TMath::TanH(mK2x[station]*xmax);
   
-  Double_t uy1=mSqrtK3y[station]*TMath::TanH(mK2y[station]*ymin);
-  Double_t uy2=mSqrtK3y[station]*TMath::TanH(mK2y[station]*ymax);
+  double uy1=mSqrtK3y[station]*TMath::TanH(mK2y[station]*ymin);
+  double uy2=mSqrtK3y[station]*TMath::TanH(mK2y[station]*ymax);
   
   return 4.*mK4x[station]*(TMath::ATan(ux2)-TMath::ATan(ux1))*
     mK4y[station]*(TMath::ATan(uy2)-TMath::ATan(uy1))*charge;
 }
 //______________________________________________________________________
-Double_t Response::response(Float_t charge, Int_t detID){
+double Response::response(float charge, int detID){
   //to be done: calculate from induced charge signal
   return charge;
 }
 //______________________________________________________________________
-Float_t Response::getAnod(Float_t x, Int_t detID){
+float Response::getAnod(float x, int detID){
 
-  Float_t pitch = mInversePitch[1];
+  float pitch = mInversePitch[1];
   if(detID<299) pitch = mInversePitch[0]; //guess for numbers!
   
-  Int_t n = Int_t(x/pitch);
-  Float_t wire = (x>0) ? n+0.5 : n-0.5;
+  int n = Int_t(x/pitch);
+  float wire = (x>0) ? n+0.5 : n-0.5;
   return pitch*wire;
 }
 //______________________________________________________________________
-Float_t Response::chargeCorr(){
+float Response::chargeCorr(){
   //taken from AliMUONResponseV0
   //conceptually not at all understood why this should make sense
   //mChargeCorr not taken
