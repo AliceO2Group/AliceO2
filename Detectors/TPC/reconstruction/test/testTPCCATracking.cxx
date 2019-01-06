eb926fdae69c456eedd2c5092874aed64aa78c56
David Rohr <drohr@jwdt.org>
2019-01-06 22:33:28 +0100

    Make TPCCATracking configurable via config objects instead of string

--- Detectors/TPC/reconstruction/test/testTPCCATracking.cxx
+++ Detectors/TPC/reconstruction/test/testTPCCATracking.cxx
@@ -22,6 +22,9 @@
 #include "DataFormatsTPC/ClusterNative.h"
 #include "TPCReconstruction/TPCCATracking.h"
 
+#include "AliGPUCAConfiguration.h"
+#include "AliGPUReconstruction.h"
+
 #include <vector>
 #include <iostream>
 #include <iomanip>
@@ -37,7 +40,29 @@ namespace TPC
 BOOST_AUTO_TEST_CASE(CATracking_test1)
 {
   TPCCATracking tracker;
-  tracker.initialize("");
+
+  float solenoidBz = -5.00668; //B-field
+  float refX = 1000.;          //transport tracks to this x after tracking, >500 for disabling
+  bool continuous = false;     //time frame data v.s. triggered events
+
+  AliGPUCAConfiguration config;
+  config.configProcessing.deviceType = AliGPUReconstruction::DeviceType::CPU;
+  config.configProcessing.forceDeviceType = true;
+
+  config.configDeviceProcessing.nThreads = 4;           //4 threads if we run on the CPU, 1 = default, 0 = auto-detect
+  config.configDeviceProcessing.runQA = true;           //Run QA after tracking
+  config.configDeviceProcessing.eventDisplay = nullptr; //Ptr to event display backend, for running standalone OpenGL event display
+  //config.configDeviceProcessing.eventDisplay = new AliGPUCADisplayBackendX11;
+
+  config.configEvent.solenoidBz = solenoidBz;
+  config.configEvent.continuousMaxTimeBin = continuous ? 0.023 * 5e6 : 0; //Number of timebins in timeframe if continuous, 0 otherwise
+
+  config.configReconstruction.NWays = 3;               //Should always be 3!
+  config.configReconstruction.NWaysOuter = true;       //Will create outer param for TRD
+  config.configReconstruction.SearchWindowDZDR = 2.5f; //Should always be 2.5 for looper-finding and/or continuous tracking
+  config.configReconstruction.TrackReferenceX = refX;
+
+  tracker.initialize(config);
   std::vector<TrackTPC> tracks;
   std::vector<ClusterNativeContainer> cont(Constants::MAXGLOBALPADROW);
 
