df5dfd210e51e1fed6ade19e271b3d7160ee2701
Felix Weiglhofer <felix.weiglhofer@gmail.com>
2019-01-09 13:02:31 +0100

    Remove unused code.

--- GPU/GPUTracking/gpucf/src/gpucf/errorcodes.h
+++
@@ -1,9 +0,0 @@
-/**Translate OpenCL error codes to strings.
- * 
- * Source: http://web.engr.oregonstate.edu/~mjb/cs575e/
- */
-#include <gpucf/cl.h>
-
-#include <string>
-
-std::string PrintCLError(cl_int);
