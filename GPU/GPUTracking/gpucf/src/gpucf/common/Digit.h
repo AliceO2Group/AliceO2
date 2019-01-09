f19432e9efae1687828783d7998e70850e10a664
Felix Weiglhofer <felix.weiglhofer@gmail.com>
2019-01-09 18:14:51 +0100

    Setup shared datatypes between opencl host and device.

--- GPU/GPUTracking/gpucf/src/gpucf/common/Digit.h
+++ GPU/GPUTracking/gpucf/src/gpucf/common/Digit.h
@@ -1,26 +1,9 @@
 #pragma once
 
+#include <shared/Digit.h>
+
 #include <iosfwd>
 
-
-struct Digit 
-{
-    float charge;
-    int cru;
-    int row;
-    int pad;
-    int time;   
-
-    Digit(float _charge, int _cru, int _row, int _pad, int _time)
-        : charge(_charge)
-        , cru(_cru)
-        , row(_row)
-        , pad(_pad)
-        , time(_time)
-    {
-    }
-
-};
 
 std::ostream &operator<<(std::ostream &, const Digit &);
 
--- GPU/GPUTracking/gpucf/src/run_gpucf.cpp
+++ GPU/GPUTracking/gpucf/src/run_gpucf.cpp
@@ -1,5 +1,4 @@
 #include <gpucf/io/DigitReader.h>
-#include <gpucf/io/filename.h>
 #include <gpucf/io/BinaryWriter.h>
 
 #include <iostream>
@@ -14,20 +13,11 @@ int main(int argc, char *argv[])
 
     log::Debug() << "Start.";
 
-    std::string head = getHead(file);
-    std::string binfile = head + ".bin";
-
-    log::Debug() << "binfile = " << binfile;
-
 
     log::Info() << "Reding text file " << file << ". This could take a while.";
     DigitReader reader(file);
 
     log::Info() << "Read " << reader.get().size() << " digits";
-
-    log::Info() << "Writing to binary file " << binfile;
-    BinaryWriter writer(binfile);
-    writer.write(reader.get());
 
     return 0;
 }
