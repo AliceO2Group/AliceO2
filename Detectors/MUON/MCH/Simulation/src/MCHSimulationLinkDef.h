fd029ed5f4de614f34eb9bb23cd0f1f79d9426f5
mwinn2 <mwinn@cern.ch>
2019-01-18 14:31:24 +0000

    correct to enable test-namespace

--- Detectors/MUON/MCH/Simulation/src/MCHSimulationLinkDef.h
+++ Detectors/MUON/MCH/Simulation/src/MCHSimulationLinkDef.h
@@ -14,7 +14,9 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-
+#pragma link C++ namespace o2;
+#pragma link C++ namespace o2::mch;
+#pragma link C++ namespace o2::mch::test;
 
 #pragma link C++ class o2::mch::test::Dummy;
 #pragma link C++ class o2::mch::Detector+;
