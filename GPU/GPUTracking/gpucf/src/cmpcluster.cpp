df64ceb0d02c65c1289728971392ed45507692f3
Felix Weiglhofer <felix.weiglhofer@gmail.com>
2019-01-25 19:17:58 +0100

    Add cmpcluster executable.

---
+++ GPU/GPUTracking/gpucf/src/cmpcluster.cpp
@@ -0,0 +1,12 @@
+#include <gpucf/executable/CompareClusters.h>
+
+
+int main(int argc, const char *argv[]) 
+{
+    gpucf::CompareClusters cc;
+
+    return cc.main(argc, argv);
+}
+
+// vim: set ts=4 sw=4 sts=4 expandtab:
+
