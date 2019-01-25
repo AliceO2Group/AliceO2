2ff363a37dc517beda872b664d4700310c84ab10
Felix Weiglhofer <felix.weiglhofer@gmail.com>
2019-01-25 17:46:06 +0100

    Fix float_t definition.

--- GPU/GPUTracking/gpucf/cl/config.h
+++ GPU/GPUTracking/gpucf/cl/config.h
@@ -11,9 +11,11 @@
 #if defined(USE_HALF_TYPES)
 typedef HalfCluster Cluster;
 typedef HalfDigit   Digit;
+typedef half        float_t;
 #else
 typedef FloatCluster Cluster;
 typedef FloatDigit   Digit;
+typedef float        float_t;
 #endif
 
 #endif //!defined(CONFIG_H)
