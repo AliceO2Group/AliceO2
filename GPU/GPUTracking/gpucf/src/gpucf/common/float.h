3011892aacfe67a03d5a6e60217a484fc39640ad
Felix Weiglhofer <felix.weiglhofer@gmail.com>
2019-01-26 18:51:26 +0100

    Extend ClusterChecker.

--- GPU/GPUTracking/gpucf/src/gpucf/common/float.h
+++ GPU/GPUTracking/gpucf/src/gpucf/common/float.h
@@ -1,6 +1,10 @@
 #pragma once
 
-#define EPSILON 0.5
-#define FLOAT_EQ(f1, f2) (std::abs(f1-f2) <= EPSILON)
+static constexpr float FEQ_EPSILON = 0.5;
+
+static inline bool floatEq(float f1, float f2, float epsilon=FEQ_EPSILON)
+{
+    return std::abs(f1 - f2) <= epsilon;
+}
 
 // vim: set ts=4 sw=4 sts=4 expandtab:
