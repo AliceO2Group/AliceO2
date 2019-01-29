e9eac2b380e13010bef8894ae233adf4ee50bbed
David Rohr <drohr@jwdt.org>
2019-01-29 13:28:43 +0100

    Remove obsolete kernel wrappers

--- GPU/CAGPUTracking/SliceTracker/AliGPUTPCProcess.h
+++ GPU/CAGPUTracking/SliceTracker/AliGPUTPCProcess.h
@@ -40,24 +40,6 @@ GPUg() void AliGPUTPCProcess(int iSlice)
 }
 
 template <class TProcess>
-GPUg() void AliGPUTPCProcessMultiA(int firstSlice, int nSliceCount, int nVirtualBlocks)
-{
-	if (get_group_id(0) >= nSliceCount) return;
-	AliGPUTPCTracker &tracker = gGPUConstantMem.tpcTrackers[firstSlice + get_group_id(0)];
-
-	GPUshared() typename TProcess::AliGPUTPCSharedMemory smem;
-
-	for (int i = 0; i < nVirtualBlocks; i++)
-	{
-		for (int iSync = 0; iSync <= TProcess::NThreadSyncPoints(); iSync++)
-		{
-			GPUsync();
-			TProcess::Thread(nVirtualBlocks, get_local_size(0), i, get_local_id(0), iSync, smem, tracker);
-		}
-	}
-}
-
-template <class TProcess>
 GPUg() void AliGPUTPCProcessMulti(int firstSlice, int nSliceCount)
 {
 	const int iSlice = nSliceCount * (get_group_id(0) + (get_num_groups(0) % nSliceCount != 0 && nSliceCount * (get_group_id(0) + 1) % get_num_groups(0) != 0)) / get_num_groups(0);
@@ -71,24 +53,6 @@ GPUg() void AliGPUTPCProcessMulti(int firstSlice, int nSliceCount)
 	{
 		GPUsync();
 		TProcess::Thread(sliceGridDim, get_local_size(0), sliceBlockId, get_local_id(0), iSync, smem, tracker);
-	}
-}
-
-template <typename TProcess>
-GPUg() void AliGPUTPCProcess1()
-{
-	AliGPUTPCTracker &tracker = *gGPUConstantMem.tpcTrackers;
-	AliGPUTPCTrackParam tParam;
-
-	GPUshared() typename TProcess::AliGPUTPCSharedMemory sMem;
-
-	typename TProcess::AliGPUTPCThreadMemory rMem;
-
-	for (int iSync = 0; iSync <= TProcess::NThreadSyncPoints(); iSync++)
-	{
-		GPUsync();
-		TProcess::Thread(get_num_groups(0), get_local_size(0), get_group_id(0), get_local_id(0), iSync,
-		                 sMem, rMem, tracker, tParam);
 	}
 }
 
@@ -110,23 +74,6 @@ void AliGPUTPCProcess(int nBlocks, int nThreads, AliGPUTPCTracker &tracker)
 	}
 }
 
-template <typename TProcess>
-void AliGPUTPCProcess1(int nBlocks, int nThreads, AliGPUTPCTracker &tracker)
-{
-	for (int iB = 0; iB < nBlocks; iB++)
-	{
-		typename TProcess::AliGPUTPCSharedMemory smem;
-		typename TProcess::AliGPUTPCThreadMemory *rMem = new typename TProcess::AliGPUTPCThreadMemory[nThreads];
-		AliGPUTPCTrackParam *tParam = new AliGPUTPCTrackParam[nThreads];
-		for (int iS = 0; iS <= TProcess::NThreadSyncPoints(); iS++)
-		{
-			for (int iT = 0; iT < nThreads; iT++)
-				TProcess::Thread(nBlocks, nThreads, iB, iT, iS, smem, rMem[iT], tracker, tParam[iT]);
-		}
-		delete[] rMem;
-		delete[] tParam;
-	}
-}
 
 #endif
 
