947b2096e1f9787d4995012c9fa1422822eb105b
David Rohr <drohr@jwdt.org>
2019-01-30 18:07:02 +0100

    Add event handling on CUDA backend (not needed yet, but bring it to same feature level as OpenCL backend)

--- GPU/CAGPUTracking/GlobalTracker/opencl/AliGPUReconstructionOCLInternals.h
+++ GPU/CAGPUTracking/GlobalTracker/opencl/AliGPUReconstructionOCLInternals.h
@@ -82,7 +82,8 @@ static int GPUFailedMsgA(int error, const char* file, int line)
 	//Check for OPENCL Error and in the case of an error display the corresponding error string
 	if (error == CL_SUCCESS) return(0);
 	printf("OCL Error: %d / %s (%s:%d)\n", error, opencl_error_string(error), file, line);
-	throw std::runtime_error("OpenCL Failure");
+	//throw std::runtime_error("OpenCL Failure");
+	return 1;
 }
 
 static inline int OCLsetKernelParameters_helper(cl_kernel &k,int i) {return 0;}
