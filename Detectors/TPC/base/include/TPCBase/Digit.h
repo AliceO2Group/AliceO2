ad1a15fdb15263ca7f35f80d74d70e48c476ee6b
Andreas Mathis <andreas.mathis@cern.ch>
2019-02-01 10:31:38 +0100

    Clarify documentation of the digit

--- Detectors/TPC/base/include/TPCBase/Digit.h
+++ Detectors/TPC/base/include/TPCBase/Digit.h
@@ -18,81 +18,80 @@
 #include "CommonDataFormat/TimeStamp.h"
 #include "TObject.h"
 
-namespace o2 {
+namespace o2
+{
 namespace TPC
 {
 /// \class Digit
 /// This is the definition of the common Digit object, which is the final entity after Digitization
-/// Its coordinates are defined by the CRU, the time bin, the pad row and the pad.
+/// Its coordinates are defined by the CRU, the time bin, the global pad row and the pad.
 /// It holds the ADC value of a given pad on the pad plane.
 using DigitBase = o2::dataformats::TimeStamp<int>;
-class Digit : public DigitBase {
-  public:
+class Digit : public DigitBase
+{
+ public:
+  /// Default constructor
+  Digit();
 
-    /// Default constructor
-    Digit();
+  /// Constructor, initializing values for position, charge, time and common mode
+  /// \param cru CRU of the Digit
+  /// \param charge Accumulated charge of Digit
+  /// \param row Global pad row in which the Digit was created
+  /// \param pad Pad in which the Digit was created
+  Digit(int cru, float charge, int row, int pad, int time);
 
-    /// Constructor, initializing values for position, charge, time and common mode
-    /// \param cru CRU of the Digit
-    /// \param charge Accumulated charge of Digit
-    /// \param row Row in which the Digit was created
-    /// \param pad Pad in which the Digit was created
-    Digit(int cru, float charge, int row, int pad, int time);
+  /// Destructor
+  ~Digit() = default;
 
-    /// Destructor
-    ~Digit() = default;
+  /// Get the accumulated charged of the Digit in ADC counts.
+  /// The conversion is such that the decimals are simply stripped
+  /// \return charge of the Digit
+  int getCharge() const { return static_cast<int>(mCharge); }
 
-    /// Get the accumulated charged of the Digit in ADC counts.
-    /// The conversion is such that the decimals are simply stripped
-    /// \return charge of the Digit
-    int getCharge() const { return static_cast<int>(mCharge); }
+  /// Get the accumulated charged of the Digit as a float
+  /// \return charge of the Digit as a float
+  float getChargeFloat() const { return mCharge; }
 
-    /// Get the accumulated charged of the Digit as a float
-    /// \return charge of the Digit as a float
-    float getChargeFloat() const { return mCharge; }
+  /// Get the CRU of the Digit
+  /// \return CRU of the Digit
+  int getCRU() const { return mCRU; }
 
-    /// Get the CRU of the Digit
-    /// \return CRU of the Digit
-    int getCRU() const { return mCRU; }
+  /// Get the global pad row of the Digit
+  /// \return Global pad row of the Digit
+  int getRow() const { return mRow; }
 
-    /// Get the pad row of the Digit
-    /// \return pad row of the Digit
-    int getRow() const { return mRow; }
+  /// Get the pad of the Digit
+  /// \return pad of the Digit
+  int getPad() const { return mPad; }
 
-    /// Get the pad of the Digit
-    /// \return pad of the Digit
-    int getPad() const { return mPad; }
+ protected:
+  float mCharge;       ///< ADC value of the Digit
+  unsigned short mCRU; ///< CRU of the Digit
+  unsigned char mRow;  ///< Global pad row of the Digit
+  unsigned char mPad;  ///< Pad of the Digit
 
-  protected:
-
-    float                   mCharge;          ///< ADC value of the Digit
-    unsigned short          mCRU;             ///< CRU of the Digit
-    unsigned char           mRow;             ///< Row of the Digit
-    unsigned char           mPad;             ///< Pad of the Digit
-
-
-    ClassDefNV(Digit, 1);
+  ClassDefNV(Digit, 1);
 };
 
-inline
-Digit::Digit()
+inline Digit::Digit()
   : DigitBase(),
     mCharge(0.f),
     mCRU(-1),
     mRow(-1),
     mPad(-1)
-{}
+{
+}
 
-inline
-Digit::Digit(int cru, float charge, int row, int pad, int time)
+inline Digit::Digit(int cru, float charge, int row, int pad, int time)
   : DigitBase(time),
     mCharge(charge),
     mCRU(cru),
     mRow(row),
     mPad(pad)
-{}
+{
+}
 
-}
-}
+} // namespace TPC
+} // namespace o2
 
 #endif // ALICEO2_TPC_DIGIT_H_
