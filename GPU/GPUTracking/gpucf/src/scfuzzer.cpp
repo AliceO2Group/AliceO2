1e71ff98da2984d7c0f318b7522a01c6fd0e42b5
Felix Weiglhofer <felix.weiglhofer@gmail.com>
2019-02-08 20:02:41 +0100

    Make StreamCompactionTest standalone and test for race conditions.

---
+++ GPU/GPUTracking/gpucf/src/scfuzzer.cpp
@@ -0,0 +1,12 @@
+#include <gpucf/executable/RunSCFuzzer.h>
+
+
+int main(int argc, const char *argv[]) 
+{
+    gpucf::RunSCFuzzer fuzzer;
+
+    return fuzzer.main(argc, argv);
+}
+
+// vim: set ts=4 sw=4 sts=4 expandtab:
+
