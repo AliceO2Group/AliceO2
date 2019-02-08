67dbdaea465e452acc9dfed92f4b1139216330a3
David Rohr <drohr@jwdt.org>
2019-02-08 21:50:15 +0100

    Fix tools for creating TRD geometry dump

--- GPU/CAGPUTracking/Standalone/tools/createGeo.sh
+++ GPU/CAGPUTracking/Standalone/tools/createGeo.sh
@@ -5,7 +5,7 @@ export ALIBUILD_WORK_DIR="$HOME/alice/sw"
 eval "`alienv shell-helper`"
 alienv load O2/latest
 
-#o2sim -n 1
+o2sim -n 1
 
 export ROOT_INCLUDE_PATH=$ROOT_INCLUDE_PATH:/home/qon/alice/AliTPCCommon/Common/:/home/qon/alice/AliTPCCommon/CAGPUTracking/GlobalTracker:/home/qon/alice/AliTPCCommon/CAGPUTracking/SliceTracker:/home/qon/alice/AliTPCCommon/CAGPUTracking/Merger:/home/qon/alice/AliTPCCommon/CAGPUTracking/TRDTracking
 root -l -q -b createGeo.C+
