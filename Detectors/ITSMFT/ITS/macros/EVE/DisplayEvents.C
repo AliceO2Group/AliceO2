c5627ec6fd26eaeba2c948cd0b5181c020b2d174
Iouri Belikov <iouri.belikov@cern.ch>
2019-02-12 18:36:16 +0100

    Adding a chip view

--- Detectors/ITSMFT/ITS/macros/EVE/DisplayEvents.C
+++ Detectors/ITSMFT/ITS/macros/EVE/DisplayEvents.C
@@ -1,46 +1,106 @@
 /// \file DisplayEvents.C
-/// \brief Simple macro to display ITS clusters and tracks
+/// \brief Simple macro to display ITS digits, clusters and tracks
 
 #if !defined(__CLING__) || defined(__ROOTCLING__)
 #include <iostream>
 #include <array>
+#include <algorithm>
 
 #include <TFile.h>
 #include <TTree.h>
 #include <TEveManager.h>
+#include <TEveBrowser.h>
+#include <TGTab.h>
+#include <TGLCameraOverlay.h>
+#include <TEveFrameBox.h>
+#include <TEveQuadSet.h>
+#include <TEveTrans.h>
 #include <TEvePointSet.h>
 #include <TEveTrackPropagator.h>
 #include <TEveTrack.h>
 
 #include "EventVisualisationView/MultiView.h"
 
+#include "ITSMFTBase/SegmentationAlpide.h"
+#include "ITSMFTBase/Digit.h"
 #include "ITSBase/GeometryTGeo.h"
 #include "DataFormatsITSMFT/Cluster.h"
 #include "DataFormatsITS/TrackITS.h"
 #endif
 
+using namespace o2::ITSMFT;
+
 extern TEveManager* gEve;
 
+static TTree* gDigiTree = nullptr;
 static TTree* gClusTree = nullptr;
 static TTree* gTracTree = nullptr;
+static Int_t gEntry = -1, gChipID = -1;
 
 static TEveElementList* gEvent = nullptr;
+static TEveElementList* gDigits = nullptr;
 
-void displayEvent(int entry = 0)
+void displayEvent(int entry = 0, int chip = 0)
 {
-  if ((entry < 0) || (entry >= gClusTree->GetEntries())) {
+  if ((entry < 0) || (entry >= gDigiTree->GetEntries())) {
     std::cerr << "Out of event range ! " << entry << '\n';
     return;
   }
   std::cout << "\n*** Event #" << entry << " ***\n";
 
+  // Digits
+  std::vector<Digit> digi, *digiArr = &digi;
+  gDigiTree->SetBranchAddress("ITSDigit", &digiArr);
+  gDigiTree->GetEvent(entry);
+  std::cout << "Number of digits: " << digi.size() << '\n';
+
+  auto gman = o2::ITS::GeometryTGeo::Instance();
+  std::vector<int> occup(gman->getNumberOfChips());
+  
+  const float sizey = SegmentationAlpide::ActiveMatrixSizeRows;
+  const float sizex = SegmentationAlpide::ActiveMatrixSizeCols;
+  const float dy = SegmentationAlpide::PitchRow;
+  const float dx = SegmentationAlpide::PitchCol;
+
+  static TEveFrameBox *box = new TEveFrameBox();
+  box->SetAAQuadXY(0, 0, 0, sizex, sizey);
+  box->SetFrameColor(kGray);
+
+  std::string cname("ALPIDE chip #");
+  cname += std::to_string(chip);
+  TEveQuadSet* q = new TEveQuadSet(cname.c_str());
+  q->SetOwnIds(kTRUE);
+  q->SetFrame(box);
+  q->Reset(TEveQuadSet::kQT_RectangleXY, kFALSE, 32);
+
+  for (const auto &d : digi)
+  {
+    auto id=d.getChipIndex();
+    occup[id]++;
+    if (id != chip) continue;
+
+    int row = d.getRow();
+    int col = d.getColumn();
+    int charge=d.getCharge();
+    q->AddQuad(col*dx,row*dy,0.,dx,dy);
+    q->QuadValue(charge);
+  }   
+  q->RefitPlex();
+
+  TEveTrans& t = q->RefMainTrans();
+  t.RotateLF(1, 3, 0.5*TMath::Pi());
+  t.SetPos(0, 0, 0);
+
+  auto most = std::distance(occup.begin(), std::max_element(occup.begin(), occup.end()));
+  std::cout<<"Most occupied chip: " << most << " ("<<occup[most]<<" digits)\n";
+  
+
   // Clusters
-  std::vector<o2::ITSMFT::Cluster> clus, *clusArr = &clus;
+  std::vector<Cluster> clus, *clusArr = &clus;
   gClusTree->SetBranchAddress("ITSCluster", &clusArr);
   gClusTree->GetEvent(entry);
   std::cout << "Number of clusters: " << clus.size() << '\n';
 
-  auto gman = o2::ITS::GeometryTGeo::Instance();
   TEvePointSet* points = new TEvePointSet("clusters");
   points->SetMarkerColor(kBlue);
   for (const auto& c : clus) {
@@ -72,7 +132,7 @@ void displayEvent(int entry = 0)
     int nc = rec.getNumberOfClusters();
     while (nc--) {
       Int_t idx = rec.getClusterIndex(nc);
-      o2::ITSMFT::Cluster& c = (*clusArr)[idx];
+      Cluster& c = (*clusArr)[idx];
       const auto& gloC = c.getXYZGloRot(*gman);
       tpoints->SetNextPoint(gloC.X(), gloC.Y(), gloC.Z());
     }
@@ -83,35 +143,48 @@ void displayEvent(int entry = 0)
   tracks->MakeTracks();
 
   // Event
-  std::string ename("Event");
+  std::string ename("Event #");
   ename += std::to_string(entry);
   delete gEvent;
   gEvent = new TEveElementList(ename.c_str());
   gEvent->AddElement(points);
   gEvent->AddElement(tracks);
-
-  gEve->AddElement(gEvent);
   auto multi = o2::EventVisualisation::MultiView::getInstance();
   multi->registerEvent(gEvent);
 
-  gEve->Redraw3D();
+  delete gDigits;
+  gDigits = new TEveElementList(ename.c_str());
+  gDigits->AddElement(q);
+  gEve->AddElement(gDigits);
+
+  gEve->Redraw3D(kFALSE);
 }
 
-Int_t nev = -1;
-
-void load(int i = 0)
+void load(int i = 0, int c = 0)
 {
-  nev = i;
-  displayEvent(nev);
+  gEntry = i;
+  gChipID = c;
+  displayEvent(gEntry, gChipID);
 }
 
-void init(int i = 0,
+void init(int entry = 0, int chip = 13, 
+          std::string digifile = "itsdigits.root",
           std::string clusfile = "o2clus_its.root",
           std::string tracfile = "o2trac_its.root",
           std::string inputGeom = "O2geometry.root")
 {
-  TEveManager::Create();
+  TEveManager::Create(kTRUE,"V");
+  TEveBrowser *browser = gEve->GetBrowser();
 
+  // Chip View
+  browser->GetTabRight()->SetText("Chip View");
+  TGLViewer* v = gEve->GetDefaultGLViewer();
+  v->SetCurrentCamera(TGLViewer::kCameraOrthoZOY);
+  TGLCameraOverlay* co = v->GetCameraOverlay();
+  co->SetShowOrthographic(kTRUE);
+  co->SetOrthographicMode(TGLCameraOverlay::kGridFront);
+
+  // Event View
   auto multi = o2::EventVisualisation::MultiView::getInstance();
   multi->drawGeometryForDetector("ITS");
 
@@ -120,24 +193,52 @@ void init(int i = 0,
   gman->fillMatrixCache(o2::utils::bit2Mask(o2::TransformType::T2L, o2::TransformType::T2GRot,
                                             o2::TransformType::L2G));
 
+  TFile::Open(digifile.data());
+  gDigiTree = (TTree*)gFile->Get("o2sim");
   TFile::Open(clusfile.data());
   gClusTree = (TTree*)gFile->Get("o2sim");
   TFile::Open(tracfile.data());
   gTracTree = (TTree*)gFile->Get("o2sim");
 
-  load(i);
+  std::cout<<"\n **** Navigation over events and chips ****\n";
+  std::cout<<" load(event, chip) \t jump to the specified event and chip\n";
+  std::cout<<" next() \t\t load next event \n";
+  std::cout<<" prev() \t\t load previous event \n";
+  std::cout<<" loadChip(chip) \t jump to the specified chip within the current event \n";
+  std::cout<<" nextChip() \t\t load the next chip within the current event \n";
+  std::cout<<" prevChip() \t\t load the previous chip within the current event \n";
+  
+  load(entry, chip);
+  gEve->Redraw3D(kTRUE);
 }
 
 void next()
 {
-  nev++;
-  displayEvent(nev);
+  gEntry++;
+  displayEvent(gEntry, gChipID);
 }
 
 void prev()
 {
-  nev--;
-  displayEvent(nev);
+  gEntry--;
+  displayEvent(gEntry, gChipID);
+}
+
+void loadChip(int chip) {
+  gChipID = chip;
+  load(gEntry, gChipID);
+}
+
+void nextChip()
+{
+  gChipID++;
+  load(gEntry, gChipID);
+}
+
+void prevChip()
+{
+  gChipID--;
+  load(gEntry, gChipID);
 }
 
 void DisplayEvents()
