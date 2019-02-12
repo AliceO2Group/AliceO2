442fcb8ad890b097c3ce78216a0a15e0e4e110dd
noferini <noferini@bo.infn.it>
2019-02-12 16:38:05 +0100

    TOF calib params ready to work

--- DataFormats/Reconstruction/include/ReconstructionDataFormats/CalibParamTOF.h
+++ DataFormats/Reconstruction/include/ReconstructionDataFormats/CalibParamTOF.h
@@ -14,6 +14,8 @@
 #ifndef ALICEO2_CALIBPARAMTOF_H
 #define ALICEO2_CALIBPARAMTOF_H
 
+#include <vector>
+
 namespace o2
 {
 namespace dataformats
@@ -21,9 +23,26 @@ namespace dataformats
 class CalibParamTOF
 {
  public:
-  CalibParamTOF() {};
+  static const int NCHANNELS=157248;
+  static const int NSECTORS=18;
+  static const int NCHANNELXSECTORSECTOR=NCHANNELS/NSECTORS;
+
+  CalibParamTOF();
+
+  float getLHCphase(int timestamp) const;
+  float evalTimeSlewing(int channel, float tot) const;
+
+  void addLHCphase(int timestamp, float phaseLHC);
+  void addTimeSlewingInfo(int channel, float tot, float time);
 
  private:
+  // LHCphase calibration
+  std::vector<std::pair <int,float>> mLHCphase;                     ///< <timestamp,LHCphase> from which the LHCphase measurement is valid
+
+  // TOF channel calibrations
+  int mChannelStart[NSECTORS][NCHANNELXSECTORSECTOR]; ///< output LHC phase in ps
+  std::vector<std::pair<float,float>> mTimeSlewing[NSECTORS];           ///< timeslweing correction >tot,time>
+
   //  ClassDefNV(CalibParamTOF, 1);
 };
 }
--- DataFormats/Reconstruction/src/CalibParamTOF.cxx
+++ DataFormats/Reconstruction/src/CalibParamTOF.cxx
@@ -11,8 +11,92 @@
 /// \file CalibParamTOF.cxx
 /// \brief Class to store the output of the matching to TOF for calibration
 
+#include <algorithm>
 #include "ReconstructionDataFormats/CalibParamTOF.h"
 
 using namespace o2::dataformats;
 
 //ClassImp(o2::dataformats::CalibParamTOF);
+
+CalibParamTOF::CalibParamTOF(){
+  for(int i=0;i < NSECTORS;i++){
+    for(int j=0;j < NCHANNELXSECTORSECTOR;j++){
+      mChannelStart[i][j] = -1;
+    }
+  }
+}
+//______________________________________________
+
+float CalibParamTOF::evalTimeSlewing(int channel,float tot) const {
+  int sector = channel/NCHANNELXSECTORSECTOR;
+  channel = channel%NCHANNELXSECTORSECTOR;
+  
+  if(sector >= NSECTORS) return 0.; // something went wrong!
+  
+  int n=mChannelStart[sector][channel];
+  if(n < 0) return 0.;
+  
+  int nstop=mTimeSlewing[sector].size();
+  if(channel < NCHANNELXSECTORSECTOR-1) nstop=mChannelStart[sector][channel+1];
+
+  if(n >= nstop) return 0.; // something went wrong!
+ 
+  while(n < nstop && tot < mTimeSlewing[sector][n].first) n++;
+  n--;
+
+  if(n < 0){ // tot is lower than the first available value
+    return 0;
+  }
+
+  if(n == nstop-1) return mTimeSlewing[sector][n].second; // use the last value stored for that channel
+
+  float w1 = tot - mTimeSlewing[sector][n].first;
+  float w2 = mTimeSlewing[sector][n+1].first - tot;
+
+  return (mTimeSlewing[sector][n].second*w2 + mTimeSlewing[sector][n+1].second*w1)/(mTimeSlewing[sector][n+1].first - mTimeSlewing[sector][n].first);
+}
+//______________________________________________
+
+float CalibParamTOF::getLHCphase(int timestamp) const{
+  int n=0;
+  while(n < mLHCphase.size() && mLHCphase[n].first < timestamp) n++;
+  n--;
+  
+  if(n < 0){ // timestamp is before of the first available value
+    return 0;
+  }
+  return mLHCphase[n].second;
+}
+//______________________________________________
+
+void CalibParamTOF::addLHCphase(int timestamp, float phaseLHC){
+  // optimized if timestamp are given in increasing order
+  int n = mLHCphase.size();
+  mLHCphase.emplace_back(timestamp,phaseLHC);
+  
+  if(n && mLHCphase[n+1].first < mLHCphase[n].first){ // in the wrong order sort!
+    std::sort( mLHCphase.begin( ), mLHCphase.end( ), [ ]( const auto& lhs, const auto& rhs ){
+	return lhs.first < rhs.first;
+      });
+  }
+}
+//______________________________________________
+void CalibParamTOF::addTimeSlewingInfo(int channel, float tot, float time){
+  // WE ARE ASSUMING THAT:
+  // channels have to be filled in increasing order (within the sector)
+  // tots have to be filled in increasing order (within the channel)
+  int sector = channel/NCHANNELXSECTORSECTOR;
+  channel = channel%NCHANNELXSECTORSECTOR;
+  
+  if(sector >= NSECTORS) return; // something went wrong!
+  
+  int currentch = channel;
+  while(mChannelStart[sector][currentch] == -1 && currentch > -1){
+    // set also all the previous ones which were not filled
+    mChannelStart[sector][currentch] = mTimeSlewing[sector].size();
+    currentch--;
+  }
+
+  mTimeSlewing[sector].emplace_back(tot,time);
+}
+//______________________________________________
