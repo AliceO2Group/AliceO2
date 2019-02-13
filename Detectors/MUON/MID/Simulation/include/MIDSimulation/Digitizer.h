aae361a56243820941531ee20ed451d13a3afe06
dstocco <diego.stocco@cern.ch>
2019-02-13 18:14:58 +0100

    First version of MID digitizer

---
+++ Detectors/MUON/MID/Simulation/include/MIDSimulation/Digitizer.h
@@ -0,0 +1,87 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file   MIDSimulation/Digitizer.h
+/// \brief  Digitizer for MID
+/// \author Diego Stocco <Diego.Stocco at cern.ch>
+/// \date   01 March 2018
+#ifndef O2_MID_DIGITIZER_H
+#define O2_MID_DIGITIZER_H
+
+#include <random>
+#include <vector>
+#include <array>
+#include "MathUtils/Cartesian3D.h"
+#include "SimulationDataFormat/MCTruthContainer.h"
+#include "MIDBase/Mapping.h"
+#include "MIDBase/GeometryTransformer.h"
+#include "MIDSimulation/ChamberResponse.h"
+#include "MIDSimulation/ChamberEfficiencyResponse.h"
+#include "MIDSimulation/Hit.h"
+#include "MIDSimulation/ColumnDataMC.h"
+#include "MIDSimulation/MCLabel.h"
+
+namespace o2
+{
+namespace mid
+{
+class Digitizer
+{
+ public:
+  Digitizer(const ChamberResponse& chamberResponse, const ChamberEfficiencyResponse& efficiencyResponse, const GeometryTransformer& transformer);
+  virtual ~Digitizer() = default;
+
+  void process(const std::vector<Hit>& hits, std::vector<ColumnDataMC>& digitStore, o2::dataformats::MCTruthContainer<MCLabel>& mcContainer);
+
+  /// Sets the event time
+  void setEventTime(double timeNS) { mTime = timeNS; }
+
+  /// Sets the event ID
+  void setEventID(int entryID) { mEventID = entryID; }
+
+  /// Sets the source ID
+  void setSrcID(int sourceID) { mSrcID = sourceID; }
+
+  /// Sets the geometry transformer
+  void setGeometryTransformer(const GeometryTransformer& transformer) { mTransformer = transformer; }
+
+  /// Sets the chamber response
+  void setChamberResponse(const ChamberResponse& chamberResponse) { mResponse = chamberResponse; }
+
+  /// Sets the seed
+  void setSeed(unsigned int seed) { mGenerator.seed(seed); }
+
+ private:
+  void addStrip(const Mapping::MpStripIndex& stripIndex, int cathode, int deId);
+  bool addBPStrips(double xPos, double yPos, int deId, double prob, double xOffset);
+  bool addNeighbours(const Mapping::MpStripIndex& stripIndex, int cathode, int deId, double prob,
+                     const std::array<double, 2>& initialDist, double xOffset = 0.);
+  bool hitToDigits(const Hit& hit);
+  bool getLabelLimits(int cathode, const ColumnDataMC& col, int& firstStrip, int& lastStrip) const;
+
+  double mTime = 0.;
+  int mEventID = 0;
+  int mSrcID = 0;
+
+  std::default_random_engine mGenerator;          ///< Random numbers generator
+  std::uniform_real_distribution<double> mRandom; ///< Uniform distribution
+  ChamberResponse mResponse;                      ///< Chamber response
+  ChamberEfficiencyResponse mEfficiencyResponse;  ///< Chamber efficiency response
+  Mapping mMapping;                               ///< Mapping
+  GeometryTransformer mTransformer;               ///< Geometry transformer
+  std::vector<ColumnDataMC> mDigits;              /// Digits per hit
+};
+
+Digitizer createDefaultDigitizer();
+
+} // namespace mid
+} // namespace o2
+
+#endif /* O2_MID_DIGITIZER_H */
---
+++ Detectors/MUON/MID/Simulation/src/Digitizer.cxx
@@ -0,0 +1,188 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file   MID/Simulation/src/Digitizer.cxx
+/// \brief  Implementation of the digitizer for MID
+/// \author Diego Stocco <Diego.Stocco at cern.ch>
+/// \date   01 March 2018
+#include "MIDSimulation/Digitizer.h"
+
+namespace o2
+{
+namespace mid
+{
+//______________________________________________________________________________
+Digitizer::Digitizer(const ChamberResponse& chamberResponse, const ChamberEfficiencyResponse& efficiencyResponse, const GeometryTransformer& transformer) : mGenerator(std::default_random_engine()), mRandom(), mResponse(chamberResponse), mEfficiencyResponse(efficiencyResponse), mMapping(), mTransformer(transformer), mDigits()
+{
+  /// Default constructor
+}
+
+//______________________________________________________________________________
+void Digitizer::addStrip(const Mapping::MpStripIndex& stripIndex, int cathode, int deId)
+{
+  /// Adds a single digit
+  for (auto& col : mDigits) {
+    if (col.deId == deId && col.columnId == stripIndex.column) {
+      col.addStrip(stripIndex.strip, cathode, stripIndex.line);
+      return;
+    }
+  }
+
+  mDigits.emplace_back(ColumnDataMC{ (uint8_t)deId, (uint8_t)stripIndex.column });
+  mDigits.back().addStrip(stripIndex.strip, cathode, stripIndex.line);
+}
+
+//______________________________________________________________________________
+bool Digitizer::addBPStrips(double xPos, double yPos, int deId, double prob, double xOffset)
+{
+  /// Checks the BP strips in the neighbour columns
+
+  // The offset is used to check the strips in the bending plane
+  // placed in the board close to the fired one
+  // If the response says that the strip is too far away, there is no need to check further
+  if (!mResponse.isFired(prob, std::abs(xOffset), 0, deId)) {
+    return false;
+  }
+
+  Mapping::MpStripIndex stripIndex = mMapping.stripByPosition(xPos + 1.01 * xOffset, yPos, 0, deId, false);
+  // Check if the point is still inside the RPC
+  if (!stripIndex.isValid()) {
+    return false;
+  }
+  addStrip(stripIndex, 0, deId);
+  MpArea area = mMapping.stripByLocation(stripIndex.strip, 0, stripIndex.line, stripIndex.column, deId);
+  std::array<double, 2> dist = { area.getYmax() - yPos, yPos - area.getYmin() };
+  addNeighbours(stripIndex, 0, deId, prob, dist, xOffset);
+  return true;
+}
+
+//______________________________________________________________________________
+bool Digitizer::addNeighbours(const Mapping::MpStripIndex& stripIndex, int cathode, int deId, double prob,
+                              const std::array<double, 2>& initialDist, double xOffset)
+{
+  /// Add neighbour strips
+
+  double xOffset2 = xOffset * xOffset;
+  for (int idir = 0; idir < 2; ++idir) {
+    // Search for neighbours in the two directions
+    // up and down for the BP, right and left for the NBP
+    double dist = initialDist[idir];
+    Mapping::MpStripIndex neigh = mMapping.nextStrip(stripIndex, cathode, deId, idir);
+    while (neigh.isValid() && mResponse.isFired(prob, std::sqrt(dist * dist + xOffset2), cathode, deId)) {
+      addStrip(neigh, cathode, deId);
+      dist += mMapping.getStripSize(neigh.strip, cathode, neigh.column, deId);
+      neigh = mMapping.nextStrip(neigh, cathode, deId, idir);
+    }
+  }
+  return true;
+}
+
+//______________________________________________________________________________
+bool Digitizer::hitToDigits(const Hit& hit)
+{
+  /// Generate digits from the hit
+
+  // Convert point from global to local coordinates
+  auto midPt = hit.middlePoint();
+  int deId = hit.GetDetectorID();
+  Point3D<double> localPoint = mTransformer.globalToLocal(deId, (double)midPt.x(), (double)midPt.y(), (double)midPt.z());
+
+  // First get the touched BP strip
+  Mapping::MpStripIndex stripIndex = mMapping.stripByPosition(localPoint.x(), localPoint.y(), 0, deId);
+
+  // Check if the chamber was efficient
+  bool isEfficientBP, isEfficientNBP;
+  if (!mEfficiencyResponse.isEfficient(deId, stripIndex.column, stripIndex.line, isEfficientBP, isEfficientNBP)) {
+    return false;
+  }
+
+  // Digitize if the RPC was efficient
+  mDigits.clear();
+  double prob = mRandom(mGenerator);
+
+  if (isEfficientBP) {
+    addStrip(stripIndex, 0, deId);
+    MpArea area = mMapping.stripByLocation(stripIndex.strip, 0, stripIndex.line, stripIndex.column, deId);
+    // This is the distance between the hit point and the edges of the strip along y
+    std::array<double, 2> dist = { area.getYmax() - localPoint.y(), localPoint.y() - area.getYmin() };
+    addNeighbours(stripIndex, 0, deId, prob, dist);
+    // Search for neighbours in the close column toward inside
+    addBPStrips(localPoint.x(), localPoint.y(), deId, prob, area.getXmin() - localPoint.x());
+    // Search for neighbours in the close column toward outside
+    addBPStrips(localPoint.x(), localPoint.y(), deId, prob, area.getXmax() - localPoint.x());
+  }
+
+  // Then check the touched NBP strip
+  if (isEfficientNBP) {
+    stripIndex = mMapping.stripByPosition(localPoint.x(), localPoint.y(), 1, deId);
+    addStrip(stripIndex, 1, deId);
+    MpArea area = mMapping.stripByLocation(stripIndex.strip, 1, stripIndex.line, stripIndex.column, deId);
+    // This is the distance between the hit point and the edges of the strip along x
+    std::array<double, 2> dist = { area.getXmax() - localPoint.x(), localPoint.x() - area.getXmin() };
+    addNeighbours(stripIndex, 1, deId, prob, dist);
+  }
+
+  return true;
+}
+
+//______________________________________________________________________________
+void Digitizer::process(const std::vector<Hit>& hits, std::vector<ColumnDataMC>& digitStore, o2::dataformats::MCTruthContainer<MCLabel>& mcContainer)
+{
+  /// Generate digits from a vector of hits
+  // mMCTruthContainer.clear();
+  digitStore.clear();
+  mcContainer.clear();
+  int firstStrip = 0, lastStrip = 0;
+
+  for (auto& hit : hits) {
+    hitToDigits(hit);
+
+    for (auto& digit : mDigits) {
+      digitStore.emplace_back(digit);
+      for (int icathode = 0; icathode < 2; ++icathode) {
+        if (getLabelLimits(icathode, digit, firstStrip, lastStrip)) {
+          MCLabel label(hit.GetTrackID(), mEventID, mSrcID, digit.deId, digit.columnId, icathode, firstStrip, lastStrip);
+          mcContainer.addElement(digitStore.size() - 1, label);
+        }
+      }
+    }
+  }
+}
+
+//______________________________________________________________________________
+bool Digitizer::getLabelLimits(int cathode, const ColumnDataMC& col, int& firstStrip, int& lastStrip) const
+{
+  /// Gets the label limits
+  int nLines = (cathode == 0) ? 4 : 1;
+  int invalid = 9999;
+  firstStrip = invalid, lastStrip = invalid;
+  for (int iline = 0; iline < nLines; ++iline) {
+    for (int istrip = 0; istrip < 16; ++istrip) {
+      if (col.isStripFired(istrip, cathode, iline)) {
+        lastStrip = MCLabel::getStrip(istrip, iline);
+        if (firstStrip == invalid) {
+          firstStrip = lastStrip;
+        }
+      } else if (firstStrip != invalid) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+//______________________________________________________________________________
+Digitizer createDefaultDigitizer()
+{
+  return Digitizer(createDefaultChamberResponse(), createDefaultChamberEfficiencyResponse(), createDefaultTransformer());
+}
+
+} // namespace mid
+} // namespace o2
