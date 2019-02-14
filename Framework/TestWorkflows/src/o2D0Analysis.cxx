810b58b21af794b3dffc5a42b3bb2d194710a065
Giulio Eulisse <giulio.eulisse@cern.ch>
2019-02-14 15:11:35 +0100

    DPL: improve D0 analysis example
    Do a deltaPhi / deltaEta plot using the new RCombinedDS. This is
    still wrong due to the fact we do the correlations across all the
    candidates.

--- Framework/TestWorkflows/src/o2D0Analysis.cxx
+++ Framework/TestWorkflows/src/o2D0Analysis.cxx
@@ -8,6 +8,7 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 #include "Framework/runDataProcessing.h"
+#include "Framework/RCombinedDS.h"
 
 #include <ROOT/RDataFrame.hxx>
 #include <ROOT/RArrowDS.hxx>
@@ -39,21 +40,43 @@ WorkflowSpec defineDataProcessing(ConfigContext const& specs)
             ///
             /// auto rdf = ctx.inputs().get<RDataSource>("xz");
             auto table = s->asArrowTable();
-            if (table->num_rows() == 0) {
-              LOG(ERROR) << "Arrow table is TRACKPAR is empty" << table->num_rows();
-            }
-            if (table->num_columns() != 17) {
-              LOG(ERROR) << "Wrong number of columns for the arrow table" << table->num_columns();
-            }
-            LOG(info) << table->num_rows();
 
-            auto source = std::make_unique<ROOT::RDF::RArrowDS>(s->asArrowTable(), std::vector<std::string>{});
+            //auto flatD0 = std::make_unique<ROOT::RDF::RArrowDS>(s->asArrowTable(), std::vector<std::string>{"inv_mass_ML", "cand_type_ML"});
+            //auto d0 = std::make_unique<ROOT::RDF::RArrowDS>(s->asArrowTable(), std::vector<std::string>{"cand_type_ML", "phi_cand_ML" , "eta_cand_ML"});
+            //auto d0bar = std::make_unique<ROOT::RDF::RArrowDS>(s->asArrowTable(), std::vector<std::string>{"cand_type_ML", "phi_cand_ML" , "eta_cand_ML"});
+            //auto d0d0bar = std::make_unique<ROOT::RDF::RCombinedDS>(std::move(d0), std::move(d0bar), "d0_", "d0bar_");
 
-            ROOT::RDataFrame rdf(std::move(source));
-            /// FIXME: for now we work on one file at the time
+            auto flatD0 = std::make_unique<ROOT::RDF::RArrowDS>(s->asArrowTable(), std::vector<std::string>{});
+            auto d0 = std::make_unique<ROOT::RDF::RArrowDS>(s->asArrowTable(), std::vector<std::string>{});
+            auto d0bar = std::make_unique<ROOT::RDF::RArrowDS>(s->asArrowTable(), std::vector<std::string>{});
+            auto d0d0bar = std::make_unique<ROOT::RDF::RCombinedDS>(std::move(d0), std::move(d0bar), "d0_", "d0bar_");
+
             TFile f("result.root", "RECREATE");
-            auto h1 = rdf.Filter("(bool) (((int) cand_type_ML) & 0x1)").Histo1D("inv_mass_ML");
+            ROOT::RDataFrame rdf1(std::move(flatD0));
+            auto candFilter = [](float x) -> bool { return (((int)x) & 0x1); };
+            auto bothCandFilter = [](float x, float y) -> bool { return (((int)x) & 0x1) && (((int)y) & 0x1); };
+            /// FIXME: for now we work on one file at the time
+            auto h1 = rdf1.Filter(candFilter, { "cand_type_ML" }).Histo1D("inv_mass_ML");
+
+            assert(d0d0bar->HasColumn("d0_cand_type_ML"));
+            assert(d0d0bar->HasColumn("d0_inv_mass_ML"));
+            assert(d0d0bar->HasColumn("d0_phi_cand_ML"));
+            assert(d0d0bar->HasColumn("d0_eta_cand_ML"));
+            ROOT::RDataFrame rdf2(std::move(d0d0bar));
+            auto delta = [](float x, float y) { return x - y; };
+            auto h2 = rdf2.Filter(bothCandFilter, { "d0_cand_type_ML", "d0bar_cand_type_ML" })
+                        .Define("delta_phi", delta, { "d0_phi_cand_ML", "d0bar_phi_cand_ML" })
+                        .Histo1D("delta_phi");
+            auto h3 = rdf2.Filter(bothCandFilter, { "d0_cand_type_ML", "d0bar_cand_type_ML" })
+                        .Define("delta_eta", delta, { "d0_eta_cand_ML", "d0bar_eta_cand_ML" })
+                        .Histo1D("delta_eta");
+
+            h1->SetName("h1");
             h1->Write();
+            h2->SetName("h2");
+            h2->Write();
+            h3->SetName("h3");
+            h3->Write();
           };
         } } }
   };
