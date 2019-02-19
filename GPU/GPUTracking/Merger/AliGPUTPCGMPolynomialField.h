3a4cc6b4272500b4e770bb0494525839475bc758
sgorbuno <sergey.gorbunov@fias.uni-frankfurt.de>
2019-02-19 14:39:36 +0100

    add polynomial field for the ITS; optimisation

--- GPU/GPUTracking/Merger/AliGPUTPCGMPolynomialField.h
+++ GPU/GPUTracking/Merger/AliGPUTPCGMPolynomialField.h
@@ -27,9 +27,11 @@ class AliGPUTPCGMPolynomialField
 
 	void Reset();
 
-	void Set(float nominalBz,
-	         const float *Bx, const float *By, const float *Bz,
-	         const float *TrdBx, const float *TrdBy, const float *TrdBz);
+        void SetFieldNominal( float nominalBz );
+
+        void SetFieldTpc( const float *Bx, const float *By, const float *Bz );
+        void SetFieldTrd( const float *Bx, const float *By, const float *Bz );
+        void SetFieldIts( const float *Bx, const float *By, const float *Bz );
 
 	GPUdi() float GetNominalBz() const { return fNominalBz; }
 
@@ -39,40 +41,52 @@ class AliGPUTPCGMPolynomialField
 	GPUd() void GetFieldTrd(float x, float y, float z, float B[3]) const;
 	GPUd() float GetFieldTrdBz(float x, float y, float z) const;
 
+        GPUd() void GetFieldIts(float x, float y, float z, float B[3]) const;
+	GPUd() float GetFieldItsBz(float x, float y, float z) const;
+
 	void Print() const;
 
-	static CONSTEXPR int fkM = 10;    // number of coefficients
-	static CONSTEXPR int fkTrdM = 20; // number of coefficients for TRD field
+	static CONSTEXPR int fkTpcM = 10;    // number of coefficients
+	static CONSTEXPR int fkTrdM = 20; // number of coefficients for the TRD field
+	static CONSTEXPR int fkItsM = 10; // number of coefficients for the ITS field
 
-	GPUd() static void GetPolynoms(float x, float y, float z, float f[fkM]);
+	GPUd() static void GetPolynomsTpc(float x, float y, float z, float f[fkTpcM]);
 	GPUd() static void GetPolynomsTrd(float x, float y, float z, float f[fkTrdM]);
+	GPUd() static void GetPolynomsIts(float x, float y, float z, float f[fkItsM]);
 
-	const float *GetCoeffBx() const { return fBx; }
-	const float *GetCoeffBy() const { return fBy; }
-	const float *GetCoeffBz() const { return fBz; }
+	const float *GetCoeffTpcBx() const { return fTpcBx; }
+	const float *GetCoeffTpcBy() const { return fTpcBy; }
+	const float *GetCoeffTpcBz() const { return fTpcBz; }
 
 	const float *GetCoeffTrdBx() const { return fTrdBx; }
 	const float *GetCoeffTrdBy() const { return fTrdBy; }
 	const float *GetCoeffTrdBz() const { return fTrdBz; }
 
+	const float *GetCoeffItsBx() const { return fItsBx; }
+	const float *GetCoeffItsBy() const { return fItsBy; }
+	const float *GetCoeffItsBz() const { return fItsBz; }
+
   private:
 	float fNominalBz; // nominal constant field value in [kG * 2.99792458E-4 GeV/c/cm]
-	float fBx[fkM];   // polynomial coefficients
-	float fBy[fkM];
-	float fBz[fkM];
+	float fTpcBx[fkTpcM];   // polynomial coefficients
+	float fTpcBy[fkTpcM];
+	float fTpcBz[fkTpcM];
 	float fTrdBx[fkTrdM]; // polynomial coefficients
 	float fTrdBy[fkTrdM];
 	float fTrdBz[fkTrdM];
+	float fItsBx[fkItsM]; // polynomial coefficients
+	float fItsBy[fkItsM];
+	float fItsBz[fkItsM];
 };
 
 inline void AliGPUTPCGMPolynomialField::Reset()
 {
 	fNominalBz = 0.f;
-	for (int i = 0; i < fkM; i++)
+	for (int i = 0; i < fkTpcM; i++)
 	{
-		fBx[i] = 0.f;
-		fBy[i] = 0.f;
-		fBz[i] = 0.f;
+		fTpcBx[i] = 0.f;
+		fTpcBy[i] = 0.f;
+		fTpcBz[i] = 0.f;
 	}
 	for (int i = 0; i < fkTrdM; i++)
 	{
@@ -80,32 +94,59 @@ inline void AliGPUTPCGMPolynomialField::Reset()
 		fTrdBy[i] = 0.f;
 		fTrdBz[i] = 0.f;
 	}
+	for (int i = 0; i < fkItsM; i++)
+	{
+		fItsBx[i] = 0.f;
+		fItsBy[i] = 0.f;
+		fItsBz[i] = 0.f;
+	}
 }
 
-inline void AliGPUTPCGMPolynomialField::Set(float nominalBz, const float *Bx, const float *By, const float *Bz, const float *TrdBx, const float *TrdBy, const float *TrdBz)
+
+inline void AliGPUTPCGMPolynomialField::SetFieldNominal( float nominalBz )
 {
-	if (!Bx || !By || !Bz || !TrdBx || !TrdBy || !TrdBz)
-	{
-		Reset();
-		return;
-	}
 	fNominalBz = nominalBz;
-	for (int i = 0; i < fkM; i++)
+}
+
+inline void AliGPUTPCGMPolynomialField::SetFieldTpc( const float *Bx, const float *By, const float *Bz )
+{
+	if ( Bx && By && Bz )
 	{
-		fBx[i] = Bx[i];
-		fBy[i] = By[i];
-		fBz[i] = Bz[i];
+	  for (int i = 0; i < fkTpcM; i++)
+	    {
+	      fTpcBx[i] = Bx[i];
+	      fTpcBy[i] = By[i];
+	      fTpcBz[i] = Bz[i];
+	    }
 	}
-	for (int i = 0; i < fkTrdM; i++)
-	{
-		fTrdBx[i] = TrdBx[i];
-		fTrdBy[i] = TrdBy[i];
-		fTrdBz[i] = TrdBz[i];
+}
+
+inline void AliGPUTPCGMPolynomialField::SetFieldTrd( const float *Bx, const float *By, const float *Bz )
+{
+	if ( Bx && By && Bz ){
+	  for (int i = 0; i < fkTrdM; i++)
+	    {
+	      fTrdBx[i] = Bx[i];
+	      fTrdBy[i] = By[i];
+	      fTrdBz[i] = Bz[i];
+	    }
+	}
+}
+
+inline void AliGPUTPCGMPolynomialField::SetFieldIts( const float *Bx, const float *By, const float *Bz )
+{
+	if ( Bx && By && Bz ){
+	  for (int i = 0; i < fkItsM; i++)
+	    {
+	      fItsBx[i] = Bx[i];
+	      fItsBy[i] = By[i];
+	      fItsBz[i] = Bz[i];
+	    }
 	}
 }
 
 
-GPUdi() void AliGPUTPCGMPolynomialField::GetPolynoms( float x, float y, float z, float f[fkM] )
+GPUdi() void AliGPUTPCGMPolynomialField::GetPolynomsTpc( float x, float y, float z, float f[fkTpcM] )
 {
 	f[0]=1.f;
 	f[1]=x;   f[2]=y;   f[3]=z;
@@ -114,12 +155,17 @@ GPUdi() void AliGPUTPCGMPolynomialField::GetPolynoms( float x, float y, float z,
 
 GPUdi() void AliGPUTPCGMPolynomialField::GetField( float x, float y, float z, float B[3] ) const
 {
-	const float f[fkM] = { 1.f, x, y, z, x*x, x*y, x*z, y*y, y*z, z*z };
-	float bx = 0.f, by = 0.f, bz = 0.f;
-	for( int i=0; i<fkM; i++){
-		bx += fBx[i]*f[i];
-		by += fBy[i]*f[i];
-		bz += fBz[i]*f[i];
+        const float *fBxS = &fTpcBx[1];
+        const float *fByS = &fTpcBy[1];
+        const float *fBzS = &fTpcBz[1];
+
+	const float f[fkTpcM-1] = { x, y, z, x*x, x*y, x*z, y*y, y*z, z*z };
+	float bx = fTpcBx[0], by = fTpcBy[0], bz = fTpcBz[0];
+	for( int i=fkTpcM-1; i--;) {
+	  //for (int i=0;i<fkTpcM-1; i++){
+		bx += fBxS[i]*f[i];
+		by += fByS[i]*f[i];
+		bz += fBzS[i]*f[i];
 	}
 	B[0] = bx;
 	B[1] = by;
@@ -128,10 +174,12 @@ GPUdi() void AliGPUTPCGMPolynomialField::GetField( float x, float y, float z, fl
 
 GPUdi() float AliGPUTPCGMPolynomialField::GetFieldBz( float x, float y, float z ) const
 {
-	const float f[fkM] = { 1.f, x, y, z, x*x, x*y, x*z, y*y, y*z, z*z };
-	float bz = 0.f;
-	for( int i=0; i<fkM; i++){
-		bz += fBz[i]*f[i];
+        const float *fBzS = &fTpcBz[1];
+
+	const float f[fkTpcM-1] = { x, y, z, x*x, x*y, x*z, y*y, y*z, z*z };
+	float bz = fTpcBz[0];
+	for( int i=fkTpcM-1; i--;) {
+		bz += fBzS[i]*f[i];
 	}
 	return bz;
 }
@@ -169,6 +217,49 @@ GPUdi() float AliGPUTPCGMPolynomialField::GetFieldTrdBz( float x, float y, float
 	float bz = 0.f;
 	for( int i=0; i<fkTrdM; i++){
 		bz += fTrdBz[i]*f[i];
+	}
+	return bz;
+}
+
+GPUdi() void AliGPUTPCGMPolynomialField::GetPolynomsIts( float x, float y, float z, float f[fkItsM] )
+{
+	float xx=x*x, xy=x*y, xz=x*z, yy=y*y, yz=y*z, zz=z*z;
+	f[ 0]=1.f;
+	f[ 1]=x;    f[ 2]=y;    f[ 3]=z;
+	f[ 4]=xx;   f[ 5]=xy;   f[ 6]=xz;   f[ 7]=yy;   f[ 8]=yz;   f[ 9]=zz;
+	/*
+	f[10]=x*xx; f[11]=x*xy; f[12]=x*xz; f[13]=x*yy; f[14]=x*yz; f[15]=x*zz;
+	f[16]=y*yy; f[17]=y*yz; f[18]=y*zz;
+	f[19]=z*zz;
+	*/
+}
+
+GPUdi() void AliGPUTPCGMPolynomialField::GetFieldIts( float x, float y, float z, float B[3] ) const
+{
+        const float *fBxS = &fItsBx[1];
+        const float *fByS = &fItsBy[1];
+        const float *fBzS = &fItsBz[1];
+
+	const float f[fkItsM-1] = { x, y, z, x*x, x*y, x*z, y*y, y*z, z*z };
+	float bx = fItsBx[0], by = fItsBy[0], bz = fItsBz[0];
+	for( int i=fkItsM-1; i--;) {	  
+		bx += fBxS[i]*f[i];
+		by += fByS[i]*f[i];
+		bz += fBzS[i]*f[i];
+	}
+	B[0] = bx;
+	B[1] = by;
+	B[2] = bz;
+}
+
+GPUdi() float AliGPUTPCGMPolynomialField::GetFieldItsBz( float x, float y, float z ) const
+{
+        const float *fBzS = &fItsBz[1];
+
+	const float f[fkItsM-1] = { x, y, z, x*x, x*y, x*z, y*y, y*z, z*z };
+	float bz = fItsBz[0];
+	for( int i=fkItsM-1; i--;) {	  
+		bz += fBzS[i]*f[i];
 	}
 	return bz;
 }
--- GPU/GPUTracking/Merger/macros/fitPolynomialField.C
+++
@@ -1,9 +0,0 @@
-
-int fitPolynomialField()
-{
-  gSystem->Load("libAliHLTTPC.so");
-  AliGPUTPCGMPolynomialField polyField;
-  AliMagF* fld = new AliMagF("Fit", "Fit", 1., 1., AliMagF::k2kG);
-  AliGPUTPCGMPolynomialFieldManager::FitFieldTPC( fld,  polyField );
-  return 0;
-}
