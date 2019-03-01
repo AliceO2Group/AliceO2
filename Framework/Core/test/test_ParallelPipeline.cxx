6d23c7170b497f75f2a2c034624743f017c58481
Matthias Richter <Matthias.Richter@scieq.net>
2019-03-01 14:19:21 +0100

    Adding helper function to create parallel pipelines from a pre-defined workflow
    This DPL helper function creates a workflow with parallel pipelines, each serving
    multiples of data channels of the same origin and description but different sub
    specification. If total number of parallel channels is larger than the number of
    pipelines, the channels are distributed among the pipelines.

---
+++ Framework/Core/test/test_ParallelPipeline.cxx
@@ -0,0 +1,153 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#include "Framework/InputSpec.h"
+#include "Framework/DataProcessorSpec.h"
+#include "Framework/DataSpecUtils.h"
+#include "Framework/ParallelContext.h"
+#include "Framework/runDataProcessing.h"
+#include "Framework/ControlService.h"
+#include "Framework/ParallelContext.h"
+#include <iostream>
+#include <algorithm>
+#include <memory>
+#include <unordered_map>
+
+#define ASSERT_ERROR(condition)                                   \
+  if ((condition) == false) {                                     \
+    LOG(ERROR) << R"(Test condition ")" #condition R"(" failed)"; \
+  }
+
+using DataHeader = o2::header::DataHeader;
+using namespace o2::framework;
+
+size_t nPipelines = 4;
+size_t nParallelChannels = 6;
+size_t nRolls = 1;
+
+std::vector<DataProcessorSpec> defineDataProcessing(ConfigContext const&)
+{
+  // define a template workflow with processors to be executed in a pipeline
+  std::vector<DataProcessorSpec> workflowSpecs{
+    { "processor1",
+      Inputs{
+        { "input", "TST", "TRIGGER", 0, Lifetime::Timeframe } },
+      Outputs{
+        { { "output" }, "TST", "PREPROC", 0, Lifetime::Timeframe } },
+      AlgorithmSpec{ [](ProcessingContext& ctx) {
+        for (auto const& input : ctx.inputs()) {
+          auto const& parallelContext = ctx.services().get<ParallelContext>();
+          std::cout << "instance " << parallelContext.index1D() << " of " << parallelContext.index1DSize() << ": "
+                    << *input.spec << ": " << *((int*)input.payload) << std::endl;
+          auto const* dataheader = DataRefUtils::getHeader<o2::header::DataHeader*>(input);
+          //auto data& = ctx.outputs().make<int>(OutputRef{"output", dataheader->subSpecification});
+          auto& data = ctx.outputs().make<int>(Output{ "TST", "PREPROC", dataheader->subSpecification, Lifetime::Timeframe });
+          ASSERT_ERROR(ctx.inputs().get<int>(input.spec->binding.c_str()) == parallelContext.index1D());
+          data = parallelContext.index1D();
+        }
+      } } },
+    { "processor2",
+      Inputs{
+        { "input", "TST", "PREPROC", 0, Lifetime::Timeframe } },
+      Outputs{
+        { { "output" }, "TST", "DATA", 0, Lifetime::Timeframe },
+        { { "metadt" }, "TST", "META", 0, Lifetime::Timeframe } },
+      AlgorithmSpec{ [](ProcessingContext& ctx) {
+        for (auto const& input : ctx.inputs()) {
+          auto const& parallelContext = ctx.services().get<ParallelContext>();
+          std::cout << "instance " << parallelContext.index1D() << " of " << parallelContext.index1DSize() << ": "
+                    << *input.spec << ": " << *((int*)input.payload) << std::endl;
+          ASSERT_ERROR(ctx.inputs().get<int>(input.spec->binding.c_str()) == parallelContext.index1D());
+          auto const* dataheader = DataRefUtils::getHeader<o2::header::DataHeader*>(input);
+          // TODO: there is a bug in the API for using OutputRef, returns an rvalue which can not be bound to
+          // lvalue reference
+          //auto& data = ctx.outputs().make<int>(OutputRef{"output", dataheader->subSpecification});
+          auto& data = ctx.outputs().make<int>(Output{ "TST", "DATA", dataheader->subSpecification, Lifetime::Timeframe });
+          data = ctx.inputs().get<int>(input.spec->binding.c_str());
+          //auto meta& = ctx.outputs().make<int>(OutputRef{"metadt", dataheader->subSpecification});
+          auto& meta = ctx.outputs().make<int>(Output{ "TST", "META", dataheader->subSpecification, Lifetime::Timeframe });
+          meta = dataheader->subSpecification;
+        }
+      } } },
+  };
+
+  // create parallel pipelines from the template workflow, the number of parallel channel is defined by
+  // nParallelChannels and is distributed among the pipelines
+  std::vector<o2::header::DataHeader::SubSpecificationType> subspecs(nParallelChannels);
+  std::generate(subspecs.begin(), subspecs.end(), [counter = std::make_shared<int>(0)]() { return 0x1 << (*counter)++; });
+  workflowSpecs = parallelPipeline(workflowSpecs, nPipelines,
+                                   [&subspecs]() { return subspecs.size(); },
+                                   [&subspecs](size_t index) { return subspecs[index]; });
+
+  // define a producer process with outputs for all subspecs
+  auto producerOutputs = [&subspecs]() {
+    Outputs outputs;
+    for (auto const& subspec : subspecs) {
+      outputs.emplace_back("TST", "TRIGGER", subspec, Lifetime::Timeframe);
+    }
+    return outputs;
+  };
+
+  // we keep the correspondence between the subspec and the instance which serves this particular subspec
+  // this is checked in the final consumer
+  auto checkMap = std::make_shared<std::unordered_map<o2::header::DataHeader::SubSpecificationType, int>>();
+  workflowSpecs.emplace_back(DataProcessorSpec{
+    "trigger",
+    Inputs{},
+    producerOutputs(),
+    AlgorithmSpec{ [subspecs, checkMap, counter = std::make_shared<int>(0)](ProcessingContext& ctx) {
+      if (*counter < nRolls) {
+        size_t multiplicity = subspecs.size() / nPipelines;
+        if (subspecs.size() % nPipelines) {
+          multiplicity++;
+        }
+        size_t instance = 0;
+        size_t inputRank = 0;
+        for (size_t index = 0, end = subspecs.size(); index < end; index++) {
+          ctx.outputs().make<int>(Output{ "TST", "TRIGGER", subspecs[index], Lifetime::Timeframe }) = instance;
+          (*checkMap)[subspecs[index]] = instance;
+          if (++inputRank == multiplicity) {
+            inputRank = 0;
+            instance++;
+            if (instance < nPipelines && ((subspecs.size() - index - 1) % (nPipelines - instance)) == 0) {
+              multiplicity = subspecs.size() / nPipelines;
+            }
+          }
+        }
+        (*counter)++;
+      }
+      if (*counter == nRolls) {
+        ctx.services().get<ControlService>().readyToQuit(false);
+      }
+    } } });
+
+  // the final consumer
+  workflowSpecs.emplace_back(DataProcessorSpec{
+    "consumer",
+    mergeInputs({ { "datain", "TST", "DATA", 0, Lifetime::Timeframe },
+                  { "metain", "TST", "META", 0, Lifetime::Timeframe } },
+                subspecs.size(),
+                [&subspecs](InputSpec& input, size_t index) {
+                  DataSpecUtils::updateMatchingSubspec(input, subspecs[index]);
+                }),
+    Outputs(),
+    AlgorithmSpec{ [checkMap](ProcessingContext& ctx) {
+      for (auto const& input : ctx.inputs()) {
+        std::cout << "consuming : " << *input.spec << ": " << *((int*)input.payload) << std::endl;
+        auto const* dataheader = DataRefUtils::getHeader<o2::header::DataHeader*>(input);
+        if (input.spec->binding.compare(0, 6, "datain") == 0) {
+          ASSERT_ERROR((*checkMap)[dataheader->subSpecification] == ctx.inputs().get<int>(input.spec->binding.c_str()));
+        }
+      }
+      ctx.services().get<ControlService>().readyToQuit(true);
+    } } });
+
+  return workflowSpecs;
+}
