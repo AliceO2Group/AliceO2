3defd2b30d878d978a62998304ff4684d169f58d
dstocco <diego.stocco@cern.ch>
2019-03-01 16:35:30 +0100

    Add MCLabel for MID digits

--- Detectors/MUON/MID/Simulation/src/MIDSimulationLinkDef.h
+++ Detectors/MUON/MID/Simulation/src/MIDSimulationLinkDef.h
@@ -23,5 +23,7 @@
 #pragma link C++ class std::vector < o2::mid::Hit > +;
 #pragma link C++ class o2::mid::ColumnDataMC + ;
 #pragma link C++ class std::vector < o2::mid::ColumnDataMC > +;
+#pragma link C++ class o2::mid::MCLabel + ;
+#pragma link C++ class o2::dataformats::MCTruthContainer < o2::mid::MCLabel > +;
 
 #endif
