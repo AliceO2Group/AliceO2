52d31a0812d39eda456d6574c47469f5fc1bdc97
carlos-soncco <33285924+carlos-soncco@users.noreply.github.com>
2019-03-12 15:44:35 -0500

    Update HalfCone.cxx
    changing 2 volume name

--- Detectors/ITSMFT/MFT/base/src/HalfCone.cxx
+++ Detectors/ITSMFT/MFT/base/src/HalfCone.cxx
@@ -119,8 +119,8 @@ TGeoVolumeAssembly* HalfCone::createHalfCone(Int_t half)
   combi_2hole_mb0_b->RegisterYourself();
 
   // shape for cross_mb0
-  TGeoShape* box_mb0 = new TGeoBBox("S_BOX_MB0", x_boxmb0 / 2, y_boxmb0 / 2, z_boxmb0 / 2);
-  TGeoShape* hole1_mb0 = new TGeoTube("hole1_MB0", radin_1hmb0, radout_1hmb0, high_1hmb0 / 2);
+  TGeoShape* box_mb0 = new TGeoBBox("box_mb0", x_boxmb0 / 2, y_boxmb0 / 2, z_boxmb0 / 2);
+  TGeoShape* hole1_mb0 = new TGeoTube("hole1_mb0", radin_1hmb0, radout_1hmb0, high_1hmb0 / 2);
   TGeoShape* hole2_mb0 = new TGeoTube("hole2_mb0", radin_2hmb0, radout_2hmb0, high_2hmb0 / 2);
 
   ///composite shape for mb0
