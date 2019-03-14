cd15deb0070610910cb33bfb7f60bf0f30339d83
dstocco <diego.stocco@cern.ch>
2019-03-14 10:15:31 +0100

    Fix bug in digitizer

--- Detectors/MUON/MID/Simulation/src/Digitizer.cxx
+++ Detectors/MUON/MID/Simulation/src/Digitizer.cxx
@@ -175,7 +175,7 @@ bool Digitizer::getLabelLimits(int cathode, const ColumnDataMC& col, int& firstS
       }
     }
   }
-  return false;
+  return (firstStrip != invalid);
 }
 
 //______________________________________________________________________________
