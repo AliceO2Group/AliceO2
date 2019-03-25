c83e1d77e6fccec80fabbaa4e3f358ddc639faf5
Felix Weiglhofer <felix.weiglhofer@gmail.com>
2019-03-25 16:47:10 +0100

    Add different memory layout for chargemap.

--- GPU/GPUTracking/gpucf/cl/shared/tpc.h
+++ GPU/GPUTracking/gpucf/cl/shared/tpc.h
@@ -3,10 +3,16 @@
 
 #define PADDING 2
 #define TPC_ROWS_PER_CRU 18
+#define TPC_NUM_OF_ROWS 152
 #define TPC_PADS_PER_ROW 138
 #define TPC_PADS_PER_ROW_PADDED (TPC_PADS_PER_ROW+2*PADDING)
+#define TPC_NUM_OF_PADS (TPC_NUM_OF_ROWS * TPC_PADS_PER_ROW_PADDED)
 #define TPC_MAX_TIME 1500
 #define TPC_MAX_TIME_PADDED (TPC_MAX_TIME+2*PADDING)
+
+// Maps the position of a pad given as row and index in that row to a unique
+// index between 0 and TPC_NUM_OF_PADS.
+#define TPC_GLOBAL_PAD_IDX(row, pad) (TPC_PADS_PER_ROW_PADDED * (row) + (pad))
 
 #endif //!defined(SHARED_TPC_H)
 
