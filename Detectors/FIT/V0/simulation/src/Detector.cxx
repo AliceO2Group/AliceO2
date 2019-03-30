dcb391556ad78356fe96d8c8a2c230e54783684c
Maciej Slupecki <maciej.slupecki@gmail.com>
2019-03-30 15:27:30 +0200

    Fixed material definition in FIT-V0+

--- Detectors/FIT/V0/simulation/src/Detector.cxx
+++ Detectors/FIT/V0/simulation/src/Detector.cxx
@@ -61,9 +61,9 @@ Detector::Detector(Bool_t isActive)
 
 void Detector::InitializeO2Detector()
 {
-  LOG(INFO) << "Initializing FIT V0 geometry\n";
+  LOG(INFO) << "FIT_V0: Initializing O2 detector";
 
-  TGeoVolume* volSensitive = gGeoManager->GetVolume("cell");
+  TGeoVolume* volSensitive = gGeoManager->GetVolume("V0cell");
   if (!volSensitive) {
     LOG(FATAL) << "Can't find FIT V0 sensitive volume: cell";
   }
@@ -151,6 +151,7 @@ o2::v0::Hit* Detector::addHit(Int_t trackId, Int_t cellId, Int_t particleId,
 // TODO: -> verify Todos inside the function
 void Detector::createMaterials()
 {
+  LOG(INFO) << "FIT_V0: Creating materials";
   // Air mixture
   const Int_t nAir = 4;
   Float_t aAir[nAir] = { 12.0107,  14.0067,  15.9994,  39.948 };
@@ -195,7 +196,7 @@ void Detector::createMaterials()
   o2::Base::Detector::Mixture(++matId, "Air$", aAir, zAir, dAir, nAir, wAir);
   o2::Base::Detector::Medium(Air, "Air$", matId, unsens, fieldType, maxField, tmaxfd, stemax, deemax, epsil, stmin);
 
-  o2::Base::Detector::Mixture(++matId, "Scintillator", aScint, zScint, dScint, nScint, wScint);
+  o2::Base::Detector::Mixture(++matId, "Scintillator$", aScint, zScint, dScint, nScint, wScint);
   o2::Base::Detector::Medium(Scintillator, "Scintillator$", matId, unsens, fieldType, maxField, tmaxfd, stemax, deemax, epsil, stmin);
 
 //  o2::Base::Detector::Material(++matId, "Alu$", aAlu, zAlu, dAlu, radAlu, absAlu);
@@ -206,7 +207,7 @@ void Detector::createMaterials()
 
 void Detector::ConstructGeometry()
 {
-  LOG(DEBUG) << "Creating FIT V0 geometry\n";
+  LOG(INFO) << "FIT_V0: Constructing geometry";
   createMaterials();
   mGeometry = new Geometry(Geometry::eOnlySensitive);
 }
@@ -233,151 +234,3 @@ void Detector::Reset()
     mHits->clear();
   }
 }
-
-
-
-
-
-
-/*void Detector::DefineOpticalProperties()
-{
-  // Path of the optical properties input file
-  TString inputDir;
-  const char* aliceO2env = std::getenv("O2_ROOT");
-  if (aliceO2env)
-    inputDir = aliceO2env;
-  inputDir += "/share/Detectors/T0/files/";
-
-  TString optPropPath = inputDir + "quartzOptProperties.txt";
-  optPropPath = gSystem->ExpandPathName(optPropPath.Data()); // Expand $(ALICE_ROOT) into real system path
-
-  if (ReadOptProperties(optPropPath.Data()) < 0) {
-    // Error reading file
-    LOG(ERROR) << "Could not read FIT optical properties" << FairLogger::endl;
-    return;
-  }
-  Int_t nBins = mPhotonEnergyD.size();
-  // set QE
-  mPMTeff = new TGraph(nBins, &(mPhotonEnergyD[0]), &(mQuantumEfficiency[0]));
-
-  // Prepare pointers for arrays with constant and hardcoded values (independent on wavelength)
-  FillOtherOptProperties();
-
-  // Quick conversion from vector<Double_t> to Double_t*: photonEnergyD -> &(photonEnergyD[0])
-  TVirtualMC::GetMC()->SetCerenkov(getMediumID(kOpGlass), nBins, &(mPhotonEnergyD[0]), &(mAbsorptionLength[0]),
-                                   &(mEfficAll[0]), &(mRefractionIndex[0]));
-  // TVirtualMC::GetMC()->SetCerenkov (getMediumID(kOpGlassCathode), kNbins, aPckov, aAbsSiO2, effCathode, rindexSiO2);
-  TVirtualMC::GetMC()->SetCerenkov(getMediumID(kOpGlassCathode), nBins, &(mPhotonEnergyD[0]), &(mAbsorptionLength[0]),
-                                   &(mEfficAll[0]), &(mRefractionIndex[0]));
-
-  // Define a border for radiator optical properties
-  //TVirtualMC::GetMC()->DefineOpSurface("surfRd", kGlisur, kDielectric_metal, kPolished, 0.);
-  TVirtualMC::GetMC()->DefineOpSurface("surfRd", kUnified, kDielectric_metal, kPolished, 0.);
-  TVirtualMC::GetMC()->SetMaterialProperty("surfRd", "EFFICIENCY", nBins, &(mPhotonEnergyD[0]), &(mEfficMet[0]));
-  TVirtualMC::GetMC()->SetMaterialProperty("surfRd", "REFLECTIVITY", nBins, &(mPhotonEnergyD[0]), &(mReflMet[0]));
-}
-
-void Detector::FillOtherOptProperties()
-{
-  // Set constant values to the other arrays
-  for (Int_t i = 0; i < mPhotonEnergyD.size(); i++) {
-    mEfficAll.push_back(1.);
-    mRindexAir.push_back(1.);
-    mAbsorAir.push_back(0.3);
-    mRindexCathodeNext.push_back(0.);
-    mAbsorbCathodeNext.push_back(0.);
-    mEfficMet.push_back(0.);
-    mReflMet.push_back(1.);
-  }
-}
-
-//------------------------------------------------------------------------
-Bool_t Detector::RegisterPhotoE(float energy)
-{
-  //  Float_t hc=197.326960*1.e6; //mev*nm
-  float hc = 1.973 * 1.e-6; // gev*nm
-  float lambda = hc / energy;
-  float eff = mPMTeff->Eval(lambda);
-  float p = gRandom->Rndm();
-
-  if (p > eff)
-    return kFALSE;
-
-  return kTRUE;
-}
-
-Int_t Detector::ReadOptProperties(const std::string filePath)
-{
-  std::ifstream infile;
-  infile.open(filePath.c_str());
-
-  // Check if file is opened correctly
-  if (infile.fail() == true) {
-    // AliFatal(Form("Error opening ascii file: %s", filePath.c_str()));
-    return -1;
-  }
-
-  std::string comment;             // dummy, used just to read 4 first lines and move the cursor to the 5th, otherwise unused
-  if (!getline(infile, comment)) { // first comment line
-    //         AliFatal(Form("Error opening ascii file (it is probably a folder!): %s", filePath.c_str()));
-    return -2;
-  }
-  getline(infile, comment); // 2nd comment line
-
-  // Get number of elements required for the array
-  Int_t nLines;
-  infile >> nLines;
-  if (nLines < 0 || nLines > 1e4) {
-    //   AliFatal(Form("Input arraySize out of range 0..1e4: %i. Check input file: %s", kNbins, filePath.c_str()));
-    return -4;
-  }
-
-  getline(infile, comment); // finish 3rd line after the nEntries are read
-  getline(infile, comment); // 4th comment line
-
-  // read the main body of the file (table of values: energy, absorption length and refractive index)
-  Int_t iLine = 0;
-  std::string sLine;
-  getline(infile, sLine);
-  while (!infile.eof()) {
-    if (iLine >= nLines) {
-      //      AliFatal(Form("Line number: %i reaches range of declared arraySize: %i. Check input file: %s", iLine,
-      //      kNbins, filePath.c_str()));
-      return -5;
-    }
-    std::stringstream ssLine(sLine);
-    // First column:
-    Double_t energy;
-    ssLine >> energy;
-    energy *= 1e-9; // Convert eV -> GeV immediately
-    mPhotonEnergyD.push_back(energy);
-    // Second column:
-    Double_t absorption;
-    ssLine >> absorption;
-    mAbsorptionLength.push_back(absorption);
-    // Third column:
-    Double_t refraction;
-    ssLine >> refraction;
-    mRefractionIndex.push_back(refraction);
-    // Fourth column:
-    Double_t efficiency;
-    ssLine >> efficiency;
-    mQuantumEfficiency.push_back(efficiency);
-    if (!(ssLine.good() || ssLine.eof())) { // check if there were problems with numbers conversion
-      //    AliFatal(Form("Error while reading line %i: %s", iLine, ssLine.str().c_str()));
-      return -6;
-    }
-    getline(infile, sLine);
-    iLine++;
-  }
-  if (iLine != mPhotonEnergyD.size()) {
-    //    AliFatal(Form("Total number of lines %i is different than declared %i. Check input file: %s", iLine, kNbins,
-    //    filePath.c_str()));
-    return -7;
-  }
-
-  //  AliInfo(Form("Optical properties taken from the file: %s. Number of lines read: %i",filePath.c_str(),iLine));
-  return 0;
-}
-*/
-
