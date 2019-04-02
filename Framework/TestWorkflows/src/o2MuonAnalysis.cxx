ee479a95dfccafd105b48a20e9eed5b589bfd1fc
Michael Winn <michael.winn@cern.ch>
2019-04-02 11:32:51 +0200

    bug fix

--- Framework/TestWorkflows/src/o2MuonAnalysis.cxx
+++ Framework/TestWorkflows/src/o2MuonAnalysis.cxx
@@ -37,7 +37,7 @@ WorkflowSpec defineDataProcessing(ConfigContext const& specs)
         // D0 candidates schema. The first string is just a label
         // so that the algorithm can be in principle be reused for different
         // kind of candidates.
-        InputSpec{ "candidates", "AOD", "DIMUONFLAGGED" },//tobe modified for Dimuons
+        InputSpec{ "tracks", "RN2", "MUON" },//tobe modified for Dimuons
       },
       // No outputs for the time being.TODO tuples!
       Outputs{},
@@ -46,14 +46,14 @@ WorkflowSpec defineDataProcessing(ConfigContext const& specs)
         // be the contents of a file or part of it.
         // FIXME: Too much boilerplate.
         adaptStateless([](InputRecord& inputs) {
-          auto input = inputs.get<TableConsumer>("candidates");
+	    auto input = inputs.get<TableConsumer>("tracks");//tbc
 
           // This does a single loop on all the candidates in the input message
           // using a simple mask on the cand_type_ML column and does
           // a simple 1D histogram of the filtered entries.
-          auto candidates = o2::analysis::doSingleLoopOn(input);
+	    auto tracks = o2::analysis::doSingleLoopOn(input);//need to introduce "CombineParticle"
 
-          auto h1 = candidates.Filter("(bool)(cand_type_ML & 0x1)").Histo1D("inv_mass_ML");
+          auto h1 = tracks.Filter("fInverseBendingMomentum < 1.0").Histo1D("fInverseBendingMomentum");
 
           // A lambda function subtracting two quantities. This defines
           // a function "delta" which can be invoked with
@@ -62,7 +62,16 @@ WorkflowSpec defineDataProcessing(ConfigContext const& specs)
           //
           // and will return 1 - 2.
           auto delta = [](float x, float y) { return x - y; };
-
+	  auto massmuon = [](float invBendMom1, float invBendMom2, float thetax1, float thetax2, float thetay1, float thetay2, float mass1 = 0.14, float mass2 = 0.14)
+	    {
+	    float e1 = sqrt( mass1 * mass1 + 1/ (invBendMom1 * invBendMom1) * ( 1.0 + sin(thetay1)*sin(thetay1) * (1.0 + 1.0 / (sin(thetax1)*sin(thetax1)) ) ));
+	    float e2 = sqrt( mass2 * mass2 + 1/ (invBendMom2 * invBendMom2) * ( 1.0 + sin(thetay2)*sin(thetay2) * (1.0 + 1.0 / (sin(thetax2)*sin(thetax2)) ) ));
+	    float scalprod = 1.0 / (invBendMom1 * invBendMom2) * (1.0 + sin(thetay1) * sin(thetay1) * (1.0 + 1.0 / sin(thetax1) * sin(thetax2) ) );
+	    return sqrt( mass1 * mass1 + mass2 * mass2 + 2.0 * e1 * e2 - 2.0 * scalprod);
+	  };
+	  //introduce function combine
+	  //introduce function "diff"
+	  //for correlation analysis for Olltraut's paper
           // This does all the combinations for all the candidates which have
           // the same value for cand_evtID_ML (the Event ID).
           // d0_ is the prefix assigned to the outer variable of the double loop.
@@ -73,10 +82,20 @@ WorkflowSpec defineDataProcessing(ConfigContext const& specs)
           // * Define a column delta_phi with the difference in phi between d0 and d0bar phi
           // * Define a column delta_eta with the difference in phi between d0 and d0bar eta
           // * Do two histograms with delta_phi, delta_eta
-          auto combinations = o2::analysis::doSelfCombinationsWith(input, "dimuon", "cand_evtID_ML");
-          auto deltas = combinations.Filter("dimuon_cand_type_ML & 0x1 && dimuonbar_cand_type_ML & 0x1")
-	    .Define("delta_phi", delta, { "dimuon_phi_cand_ML", "dimuon_phi_cand_ML" })//fix me!
-	    .Define("delta_eta", delta, { "dimuon_eta_cand_ML", "dimuon_eta_cand_ML" });//fix me!
+	  //ok, this can be used
+          auto combinations = o2::analysis::doSelfCombinationsWith(input, "tracks", "cand_evtID_ML");
+          auto deltas = combinations.Filter("fID4mu & 0x1 && fID4mu & 0x1")//check meaning how charge is defined
+	    .Define("delta_phi", delta, { "dimuon_phi_cand_ML", "dimuon_phi_cand_ML" })//fix me!!!!! no phi defined in muon framework
+	    .Define("delta_eta", delta, { "dimuon_eta_cand_ML", "dimuon_eta_cand_ML" });//fix me! no phi defined in muon framework
+	  auto massmuons = combinations.Filter("FID4mu & 0x1 && fIDmu & 0x1")
+	    .Define("massmuons", massmuon, {"fInverseBendingMomentum", "fInverseBendingMomentum", "fThetaX", "fThetaX", "fThetaY", "fThetaY", "105.65837", "105.65837" });//need to think how it knows which variable belongs to which particle
+	  
+	  //make mass plot
+	  //construct lambda function,
+	  //assume mass = (p1+p2)^2 = m1^2 + m2^2 + 2 * (E1*E2 - p1*p2)
+	  //as argument mass assumption E is sqrt(m^2 + p^2)
+	  // p^2 is 1/fInverseBending*
+	  //FInverseBendingMomentum should be inverse of py
           auto h2 = deltas.Histo1D("delta_phi");
           auto h3 = deltas.Histo1D("delta_eta");
 
