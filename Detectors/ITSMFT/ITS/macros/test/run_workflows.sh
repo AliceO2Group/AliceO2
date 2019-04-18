34edb9ff608b2d0277d7c9c1c32ad1f8c5da6fd9
Iouri Belikov <iouri.belikov@cern.ch>
2019-04-18 14:59:56 +0200

    An option for digitizing in triggered mode

--- Detectors/ITSMFT/ITS/macros/test/run_workflows.sh
+++ Detectors/ITSMFT/ITS/macros/test/run_workflows.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 o2sim -n 10 -e TGeant3 -g boxgen -m PIPE ITS >& sim.log 
 digitizer-workflow >& digi.log
-#digitizer-workflow --ITStriggered >& digi.log
+#digitizer-workflow --configKeyValues "ITSDigitizerParam.continuous=0" >& digi.log
 its-reco-workflow >& reco.log
 
 # These macros can be run to check the quality of the results 
