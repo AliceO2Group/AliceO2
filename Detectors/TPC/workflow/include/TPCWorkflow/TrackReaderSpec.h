8055c165ce279eb2897d7012f26370001c4ee68a
shahoian <ruben.shahoyan@cern.ch>
2019-04-22 23:38:12 +0200

    added TPC TrackReader DPL specs

---
+++ Detectors/TPC/workflow/include/TPCWorkflow/TrackReaderSpec.h
@@ -0,0 +1,63 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// @file   TrackReaderSpec.h
+
+#ifndef O2_TPC_TRACKREADER
+#define O2_TPC_TRACKREADER
+
+#include "TFile.h"
+
+#include "Framework/DataProcessorSpec.h"
+#include "Framework/Task.h"
+#include "Headers/DataHeader.h"
+#include "DataFormatsTPC/TrackTPC.h"
+#include "SimulationDataFormat/MCCompLabel.h"
+#include "SimulationDataFormat/MCTruthContainer.h"
+
+using namespace o2::framework;
+
+namespace o2
+{
+namespace TPC
+{
+///< DPL device to read and send the TPC tracks (+MC) info
+
+class TrackReader : public Task
+{
+ public:
+  TrackReader(bool useMC = true);
+  ~TrackReader() override = default;
+  void init(InitContext& ic) final;
+  void run(ProcessingContext& pc) final;
+
+ private:
+  void accumulate();
+
+  std::vector<o2::TPC::TrackTPC>*mTracksInp = nullptr, mTracksOut;
+  o2::dataformats::MCTruthContainer<o2::MCCompLabel>*mMCTruthInp = nullptr, mMCTruthOut;
+
+  bool mFinished = false;
+  bool mUseMC = true; // use MC truth
+
+  std::string mInputFileName = "tpctracks.root";
+  std::string mTrackTreeName = "events";
+  std::string mTrackBranchName = "Tracks";
+  std::string mTrackMCTruthBranchName = "TracksMCTruth";
+};
+
+/// create a processor spec
+/// read TPC track data from a root file
+framework::DataProcessorSpec getTPCTrackReaderSpec(bool useMC = true);
+
+} // namespace TPC
+} // namespace o2
+
+#endif /* O2_TPC_TRACKREADER */
---
+++ Detectors/TPC/workflow/src/TrackReaderSpec.cxx
@@ -0,0 +1,120 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// @file   TrackReaderSpec.cxx
+
+#include <vector>
+
+#include "TTree.h"
+
+#include "Framework/ControlService.h"
+#include "TPCWorkflow/TrackReaderSpec.h"
+
+using namespace o2::framework;
+
+namespace o2
+{
+namespace TPC
+{
+
+TrackReader::TrackReader(bool useMC)
+{
+  mUseMC = useMC;
+}
+
+void TrackReader::init(InitContext& ic)
+{
+  mInputFileName = ic.options().get<std::string>("tpc-tracks-infile");
+}
+
+void TrackReader::run(ProcessingContext& pc)
+{
+
+  if (mFinished) {
+    return;
+  }
+  accumulate();
+
+  LOG(INFO) << "TPCTrackReader pushes " << mTracksOut.size() << " tracks";
+  pc.outputs().snapshot(Output{ "TPC", "TRACKS", 0, Lifetime::Timeframe }, mTracksOut);
+  if (mUseMC) {
+    pc.outputs().snapshot(Output{ "TPC", "TRACKSMCLBL", 0, Lifetime::Timeframe }, mMCTruthOut);
+  }
+
+  mFinished = true;
+  pc.services().get<ControlService>().readyToQuit(false);
+}
+
+void TrackReader::accumulate()
+{
+  // load data from files
+  TFile trFile(mInputFileName.c_str(), "read");
+  if (trFile.IsZombie()) {
+    LOG(FATAL) << "Failed to open tracks file " << mInputFileName;
+  }
+  TTree* trTree = (TTree*)trFile.Get(mTrackTreeName.c_str());
+  if (!trTree) {
+    LOG(FATAL) << "Failed to load tracks tree " << mTrackTreeName << " from " << mInputFileName;
+  }
+  LOG(INFO) << "Loaded tracks tree " << mTrackTreeName << " from " << mInputFileName;
+
+  trTree->SetBranchAddress(mTrackBranchName.c_str(), &mTracksInp);
+  if (mUseMC) {
+    if (trTree->GetBranch(mTrackMCTruthBranchName.c_str())) {
+      trTree->SetBranchAddress(mTrackMCTruthBranchName.c_str(), &mMCTruthInp);
+      LOG(INFO) << "Will use MC-truth from " << mTrackMCTruthBranchName;
+    } else {
+      LOG(INFO) << "MC-truth is missing";
+      mUseMC = false;
+    }
+  }
+  int nEnt = trTree->GetEntries();
+  if (nEnt == 1) {
+    trTree->GetEntry(0);
+    mTracksOut.swap(*mTracksInp);
+    if (mUseMC) {
+      mMCTruthOut.mergeAtBack(*mMCTruthInp);
+    }
+  } else {
+    int lastEntry = -1;
+    int ntrAcc = 0;
+    for (int iev = 0; iev < nEnt; iev++) {
+      trTree->GetEntry(0);
+      auto tr0 = mTracksInp->begin();
+      auto tr1 = mTracksInp->end();
+      std::copy(tr0, tr1, std::back_inserter(mTracksOut));
+      // MC
+      if (mUseMC) {
+        mMCTruthOut.mergeAtBack(*mMCTruthInp);
+      }
+    }
+  }
+}
+
+DataProcessorSpec getTPCTrackReaderSpec(bool useMC)
+{
+  std::vector<OutputSpec> outputSpec;
+  outputSpec.emplace_back("TPC", "TRACKS", 0, Lifetime::Timeframe);
+  if (useMC) {
+    outputSpec.emplace_back("TPC", "TRACKSMCLBL", 0, Lifetime::Timeframe);
+  }
+
+  return DataProcessorSpec{
+    "tpc-track-reader",
+    Inputs{},
+    outputSpec,
+    AlgorithmSpec{ adaptFromTask<TrackReader>(useMC) },
+    Options{
+      { "tpc-tracks-infile", VariantType::String, "tpctracks.root", { "Name of the input track file" } } }
+  };
+}
+
+} // namespace TPC
+} // namespace o2
