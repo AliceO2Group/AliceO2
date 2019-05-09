4b0f3519989efa9523dc6b36e7210a76a1b52656
Felix Weiglhofer <felix.weiglhofer@gmail.com>
2019-05-09 14:40:22 +0200

    Reduce global writes performed by stream compaction.

--- GPU/GPUTracking/gpucf/cl/debug.h
+++ GPU/GPUTracking/gpucf/cl/debug.h
@@ -2,15 +2,17 @@
 #    define  DEBUG_H
 
 #if defined(NDEBUG)
-#  define SOFT_ASSERT(test)        ((void) 0)  
-#  define DBGPR_1(str, arg1)       ((void) 0)  
-#  define DBGPR_2(str, arg1, arg2) ((void) 0)  
+#  define SOFT_ASSERT(test)              ((void) 0)  
+#  define DBGPR_1(str, arg1)             ((void) 0)  
+#  define DBGPR_2(str, arg1, arg2)       ((void) 0)  
+#  define DBGPR_3(str, arg1, arg2, arg3) ((void) 0)  
 #else
 #  define SOFT_ASSERT(test) \
     if (!(test)) printf("%s:%d: Failed assertion " #test "\n", __FILE__, __LINE__)
 
-#  define DBGPR_1(str, arg1)       printf(str "\n", arg1)
-#  define DBGPR_2(str, arg1, arg2) printf(str "\n", arg1, arg2)
+#  define DBGPR_1(str, arg1)             printf(str "\n", arg1)
+#  define DBGPR_2(str, arg1, arg2)       printf(str "\n", arg1, arg2)
+#  define DBGPR_3(str, arg1, arg2, arg3) printf(str "\n", arg1, arg2, arg3)
 #endif
 
 #endif // !defined(DEBUG_H)
