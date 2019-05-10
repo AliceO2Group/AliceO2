71fb549cdc96028a51e363c2c359fb5391476e2b
wiechula <Jens.Wiechula@cern.ch>
2019-05-10 16:39:42 +0200

    Add definition of laser track and laser track positions
    * A class to store information about all laser tracks in the TPC
    * A text file with all positions and directions of the laser tracks
    * A demo macro to simulate the laser tracks
      This will need to be modified at some point, since laser tracks are
      not protons. It might be that a custom particle is required with
      special care in the step manager

---
+++ DataFormats/Detectors/TPC/src/LaserTrack.cxx
@@ -0,0 +1,47 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file LaserTrack.cxx
+/// \brief Laser track parameters
+/// \author Jens Wiechula, jens.wiechula@ikf.uni-frankfurt.de
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <FairMQLogger.h>
+
+#include "DataFormatsTPC/LaserTrack.h"
+
+using namespace o2::tpc;
+
+void LaserTrackContainer::loadTracksFromFile()
+{
+  int id;
+  float x, alpha, p0, p1, p2, p3, p4;
+
+  const std::string o2Root(std::getenv("O2_ROOT"));
+  const std::string file = o2Root + "/share/Detectors/TPC/files/laserTrackData.txt";
+
+  std::string line;
+  std::ifstream infile(file, std::ifstream::in);
+
+  if (!infile.is_open()) {
+    LOG(ERROR) << "Could not open laser track file " << file;
+    return;
+  }
+
+  while (std::getline(infile, line)) {
+    std::stringstream streamLine(line);
+    streamLine >> id >> x >> alpha >> p0 >> p1 >> p2 >> p3 >> p4;
+
+    //printf("%3d: %f %f %f %f %f %f %f \n", id, x, alpha, p0, p1, p2, p3, p4);
+    mLaserTracks[id] = LaserTrack(id, x, alpha, {p0, p1, p2, p3, p4});
+  }
+}
