39758b06d23304f00b2220254c5803f7048e849a
Giulio Eulisse <giulio.eulisse@cern.ch>
2019-05-22 09:29:35 +0200

    DPL Analysis: improve AnalysisTask
    * Introduces a few new user hooks for AnalysisTask and splits
    the tracks on a per event basis where it applies.
    * Introduce a SoA layer which builds on top of Apache Arrow and allows
      to have a simple C++ like iterator interface to iterate on Rows in
      an arrow::Table.
    * Introduce a few Arrow Kernels for Analysis related processing,
      e.g. grouping tracks belonging to the same event together.

---
+++ Framework/Core/test/test_ASoA.cxx
@@ -0,0 +1,81 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#define BOOST_TEST_MODULE Test Framework ASoA
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_DYN_LINK
+
+#include "Framework/ASoA.h"
+#include "Framework/TableBuilder.h"
+#include <boost/test/unit_test.hpp>
+
+using namespace o2::framework;
+using namespace arrow;
+using namespace o2::soa;
+
+namespace test
+{
+DECLARE_SOA_COLUMN(X, x, uint64_t, "x");
+DECLARE_SOA_COLUMN(Y, y, uint64_t, "y");
+} // namespace test
+
+BOOST_AUTO_TEST_CASE(TestTableIteration)
+{
+  TableBuilder builder;
+  auto rowWriter = builder.persist<uint64_t, uint64_t>({ "x", "y" });
+  rowWriter(0, 0, 0);
+  rowWriter(0, 0, 1);
+  rowWriter(0, 0, 2);
+  rowWriter(0, 0, 3);
+  rowWriter(0, 1, 4);
+  rowWriter(0, 1, 5);
+  rowWriter(0, 1, 6);
+  rowWriter(0, 1, 7);
+  auto table = builder.finalize();
+
+  auto i = ColumnIterator<uint64_t>(table->column(0));
+  BOOST_CHECK_EQUAL(*i++, 0);
+  BOOST_CHECK_EQUAL(*i++, 0);
+  BOOST_CHECK_EQUAL(*i++, 0);
+  BOOST_CHECK_EQUAL(*i++, 0);
+  BOOST_CHECK_EQUAL(*i++, 1);
+  BOOST_CHECK_EQUAL(*i++, 1);
+  BOOST_CHECK_EQUAL(*i++, 1);
+  BOOST_CHECK_EQUAL(*i++, 1);
+
+  RowView<test::X, test::Y> tests(table);
+  BOOST_CHECK_EQUAL(tests.x(), 0);
+  BOOST_CHECK_EQUAL(tests.y(), 0);
+  ++tests;
+  BOOST_CHECK_EQUAL(tests.x(), 0);
+  BOOST_CHECK_EQUAL(tests.y(), 1);
+  using Test = o2::soa::Table<test::X, test::Y>;
+  Test tests2{ table };
+  size_t value = 0;
+  auto b = tests2.begin();
+  auto e = tests2.end();
+  BOOST_CHECK(b != e);
+  b++;
+  b++;
+  b++;
+  b++;
+  b++;
+  b++;
+  b++;
+  b++;
+  BOOST_CHECK(b == e);
+
+  for (auto t : tests2) {
+    BOOST_CHECK_EQUAL(t.x(), value / 4);
+    BOOST_CHECK_EQUAL(t.y(), value);
+    BOOST_REQUIRE(value < 8);
+    value++;
+  }
+}
