1591c84175480c379fa61bf5040fed8d84f0a282
Barthélémy von Haller <barthelemy.von.haller@cern.ch>
2019-06-05 13:14:10 +0200

    More robust CCDB - API test
    * protect against nullptr
    
    * add method to determine wether the host is reachable
    
    * tests are disabled if the host is not reachable

--- CCDB/src/CcdbApi.cxx
+++ CCDB/src/CcdbApi.cxx
@@ -389,5 +389,30 @@ void CcdbApi::truncate(std::string path)
     curl_easy_cleanup(curl);
   }
 }
+
+size_t write_data(void* buffer, size_t size, size_t nmemb, void* userp)
+{
+  return size * nmemb;
+}
+
+bool CcdbApi::isHostReachable()
+{
+  CURL* curl;
+  CURLcode res;
+  bool result = false;
+
+  curl = curl_easy_init();
+  if (curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, mUrl.data());
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+    res = curl_easy_perform(curl);
+    result = (res == CURLE_OK);
+
+    /* always cleanup */
+    curl_easy_cleanup(curl);
+  }
+  return result;
+}
+
 } // namespace ccdb
 } // namespace o2
