2ea56018e67999092b400178fe16bd014fbe1db3
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2019-06-08 09:59:55 +0200

    WIP - MC specific parts (with detection of Geant3, 4, ...

--- dependencies/Findpythia.cmake
+++ dependencies/Findpythia.cmake
@@ -13,27 +13,21 @@ find_path(${CMAKE_FIND_PACKAGE_NAME}_DATA
 if(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR
    AND ${CMAKE_FIND_PACKAGE_NAME}_LIBRARY_SHARED
    AND ${CMAKE_FIND_PACKAGE_NAME}_DATA)
-  set(${CMAKE_FIND_PACKAGE_NAME}_FOUND TRUE)
-  if(NOT ${CMAKE_FIND_PACKAGE_NAME}_FIND_QUIETLY)
-    message(
-      STATUS
-        "pythia library found: ${${CMAKE_FIND_PACKAGE_NAME}_LIBRARY_SHARED}")
-    message(
-      STATUS "pythia includes found: ${${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR}")
-    message(STATUS "pythia data found: ${${CMAKE_FIND_PACKAGE_NAME}_DATA}")
-  endif()
   add_library(pythia SHARED IMPORTED)
   set_target_properties(pythia
                         PROPERTIES IMPORTED_LOCATION
                                    ${${CMAKE_FIND_PACKAGE_NAME}_LIBRARY_SHARED}
                                    INTERFACE_INCLUDE_DIRECTORIES
                                    ${${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR}/..)
-else()
-  set(${CMAKE_FIND_PACKAGE_NAME}_FOUND FALSE)
-  set(${CMAKE_FIND_PACKAGE_NAME}_NOT_FOUND_MESSAGE
-      "Sorry. Could not find pythia8")
 endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+  ${CMAKE_FIND_PACKAGE_NAME}
+  REQUIRED_VARS ${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR
+                ${CMAKE_FIND_PACKAGE_NAME}_LIBRARY_SHARED
+                ${CMAKE_FIND_PACKAGE_NAME}_DATA)
 
 mark_as_advanced(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR
                  ${CMAKE_FIND_PACKAGE_NAME}_LIBRARY_SHARED
-                 ${CMAKE_FIND_PACKAGE_NAME}_LIBRARY_DATA)
+                 ${CMAKE_FIND_PACKAGE_NAME}_DATA)
