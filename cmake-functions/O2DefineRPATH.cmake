af530f79aff68de7d8a02de94723c29d421e2676
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2019-06-09 11:38:49 +0200

    Do not build with install rpath on Mac unless we know what we are doing

--- cmake-functions/O2DefineRPATH.cmake
+++ cmake-functions/O2DefineRPATH.cmake
@@ -16,8 +16,20 @@ function(o2_define_rpath)
   # use, i.e. do not skip, the full RPATH in the _build_ tree
   set(CMAKE_SKIP_BUILD_RPATH FALSE PARENT_SCOPE)
   # when building, do not use the install RPATH already (will only be used when
-  # actually installing)
+  # actually installing), unless we are on a Mac (where the install is otherwise
+  # pretty slow) and we are _not_ developping (in which case we don't really to
+  # work off the build tree)
   set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE PARENT_SCOPE)
+  if(APPLE)
+    if(NOT BUILD_FOR_DEV)
+      set(CMAKE_BUILD_WITH_INSTALL_RPATH TRUE PARENT_SCOPE)
+    else()
+      message(
+        WARNING
+          "Not building with install RPATH on a Mac : installation phase will be slow"
+        )
+    endif()
+  endif()
 
   # add to the install RPATH the (automatically determined) parts of the RPATH
   # that point to directories outside the build tree
