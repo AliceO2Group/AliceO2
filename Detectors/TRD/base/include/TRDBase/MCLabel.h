33cb2ea5e59785304bb43815a91353deea63a7b7
Jorge Lopez <jorgext@gmail.com>
2019-06-24 12:58:36 +0200

    TRD Basic MC Labeling with DPL integration (#2088)
    * basic mc labeling in TRD digitization

---
+++ Detectors/TRD/base/include/TRDBase/MCLabel.h
@@ -0,0 +1,40 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+// Declaration of a transient MC label class for ZDC
+
+#ifndef ALICEO2_TRD_MCLABEL_H_
+#define ALICEO2_TRD_MCLABEL_H_
+
+#include "SimulationDataFormat/MCCompLabel.h"
+
+namespace o2
+{
+namespace trd
+{
+
+class MCLabel : public o2::MCCompLabel
+{
+ private:
+  bool mIsDigit{ false };
+
+ public:
+  MCLabel() = default;
+  MCLabel(int trackID, int eventID, int srcID, int isDigit)
+    : o2::MCCompLabel(trackID, eventID, srcID), mIsDigit(isDigit) {}
+  int isDigit() const { return mIsDigit; }
+
+  ClassDefNV(MCLabel, 1);
+};
+
+} // namespace trd
+} // namespace o2
+
+#endif
