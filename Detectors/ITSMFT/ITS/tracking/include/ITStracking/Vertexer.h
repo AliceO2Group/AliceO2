42e6b7869dc8395e04a17c60bf9b76edb6e29487
a-kieffer <akieffer@orange.fr>
2019-06-27 16:27:08 +0200

    fix bad param setters

--- Detectors/ITSMFT/ITS/tracking/include/ITStracking/Vertexer.h
+++ Detectors/ITSMFT/ITS/tracking/include/ITStracking/Vertexer.h
@@ -48,11 +48,11 @@ class Vertexer
   Vertexer& operator=(const Vertexer&) = delete;
 
   void setROframe(const uint32_t ROframe) { mROframe = ROframe; }
-  void setParameters(const VertexingParameters& verPar) { mVertParams = verPar; }
+  void setParameters(const VertexingParameters& verPar);
+  VertexingParameters getVertParameters() const;
 
   uint32_t getROFrame() const { return mROframe; }
   std::vector<Vertex> exportVertices();
-  VertexingParameters getVertParameters() const { return mVertParams; }
   VertexerTraits* getTraits() const { return mTraits; };
 
   float clustersToVertices(ROframe&, const bool useMc = false, std::ostream& = std::cout);
@@ -85,7 +85,6 @@ class Vertexer
  private:
   std::uint32_t mROframe = 0;
   VertexerTraits* mTraits = nullptr;
-  VertexingParameters mVertParams;
 };
 
 template <typename... T>
@@ -103,6 +102,16 @@ void Vertexer::findTracklets(T&&... args)
 inline void Vertexer::findTrivialMCTracklets()
 {
   mTraits->computeTrackletsPureMontecarlo();
+}
+
+inline VertexingParameters Vertexer::getVertParameters() const
+{
+  return mTraits->getVertexingParameters();
+}
+
+inline void Vertexer::setParameters(const VertexingParameters& verPar)
+{
+  mTraits->updateVertexingParameters(verPar);
 }
 
 inline void Vertexer::dumpTraits()
