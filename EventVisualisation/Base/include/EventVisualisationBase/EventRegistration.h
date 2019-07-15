7ee9f044467fc3869f86acb2012159216f05e1c5
Julian Myrcha <julian.myrcha@cern.ch>
2019-07-15 14:48:42 +0200

    Added base class for MultiView in BASE module to remove cyclic module dependency

---
+++ EventVisualisation/Base/include/EventVisualisationBase/EventRegistration.h
@@ -0,0 +1,29 @@
+//
+// Created by jmy on 15.07.19.
+//
+
+#ifndef ALICE_O2_EVENTVISUALISATION_BASE_EVENTREGISTRATION_H
+#define ALICE_O2_EVENTVISUALISATION_BASE_EVENTREGISTRATION_H
+
+#include <TEveElement.h>
+
+namespace o2 {
+namespace event_visualisation {
+
+class EventRegistration {
+private:
+    static EventRegistration* instance;
+public:
+    /// Registers an event to be drawn
+    virtual void registerEvent(TEveElement *event) = 0;
+
+    /// Removes all shapes representing current event
+    virtual void destroyAllEvents() = 0;
+
+    static EventRegistration* getInstance() { return instance;}
+    static void setInstance(EventRegistration* instance) { EventRegistration::instance = instance;}
+};
+
+}
+}
+#endif //ALICE_O2_EVENTVISUALISATION_BASE_EVENTREGISTRATION_H
---
+++ EventVisualisation/Base/src/EventRegistration.cxx
@@ -0,0 +1,13 @@
+//
+// Created by jmy on 15.07.19.
+//
+
+#include <EventVisualisationBase/EventRegistration.h>
+
+namespace o2  {
+namespace event_visualisation {
+
+EventRegistration* EventRegistration::instance = nullptr;
+
+}
+}
