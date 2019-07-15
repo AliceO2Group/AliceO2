2b840b52d8c6fbead05c6f8d63184ebe1606dd88
shahoian <ruben.shahoyan@cern.ch>
2019-07-15 14:48:00 +0200

    Add CcdApi to dictionary generation

--- CCDB/src/CCDBLinkDef.h
+++ CCDB/src/CCDBLinkDef.h
@@ -33,9 +33,9 @@
 #pragma link C++ class o2::ccdb::GridStorageFactory + ;
 #pragma link C++ class o2::ccdb::GridStorageParameters + ;
 #pragma link C++ class o2::ccdb::XmlHandler + ;
+#pragma link C++ class o2::ccdb::CcdbApi + ;
 /// for the unit test
-#pragma link C++ class TestClass+;
-#pragma link C++ class o2::TObjectWrapper<TestClass>+;
-
+#pragma link C++ class TestClass + ;
+#pragma link C++ class o2::TObjectWrapper < TestClass> + ;
 
 #endif
