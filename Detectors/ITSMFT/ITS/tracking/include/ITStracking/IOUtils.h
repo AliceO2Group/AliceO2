b3c416fcda55045089c8549e0d35eadd5c1d27c2
a-kieffer <52200304+a-kieffer@users.noreply.github.com>
2019-07-18 12:43:14 +0200

    Add a function to generate simple clusters and fix fake pileup (#2211)
    * Add a function to generate simple clusters
    
    * Fix fake pileup
    
    * Generate uneven number of clusters
    
    Formatting

--- Detectors/ITSMFT/ITS/tracking/include/ITStracking/IOUtils.h
+++ Detectors/ITSMFT/ITS/tracking/include/ITStracking/IOUtils.h
@@ -61,6 +61,7 @@ void loadEventData(ROframe& events, const std::vector<itsmft::Cluster>* mCluster
                    const dataformats::MCTruthContainer<MCCompLabel>* mClsLabels = nullptr);
 int loadROFrameData(const o2::itsmft::ROFRecord& rof, ROframe& events, const std::vector<itsmft::Cluster>* mClustersArray,
                     const dataformats::MCTruthContainer<MCCompLabel>* mClsLabels = nullptr);
+void generateSimpleData(ROframe& event, const int phiDivs, const int zDivs);
 
 std::vector<std::unordered_map<int, Label>> loadLabels(const int, const std::string&);
 void writeRoadsReport(std::ofstream&, std::ofstream&, std::ofstream&, const std::vector<std::vector<Road>>&,
