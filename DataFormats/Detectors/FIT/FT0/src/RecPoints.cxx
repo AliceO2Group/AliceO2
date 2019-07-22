a31c9f1435828a0e4a14c7c09e5c10487f4b47b9
Ryan Nevils <rnevils@calpoly.edu>
2019-07-22 10:26:57 +0200

    more file renames, directory renames

--- DataFormats/Detectors/FIT/FT0/src/RecPoints.cxx
+++ DataFormats/Detectors/FIT/FT0/src/RecPoints.cxx
@@ -8,7 +8,7 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-#include "DataFormatsFITT0/RecPoints.h"
+#include "DataFormatsFT0/RecPoints.h"
 #include <cmath>
 #include <cassert>
 #include <iostream>
--- Detectors/FIT/workflow/include/FITWorkflow/FT0RecPointReaderSpec.h
+++ Detectors/FIT/workflow/include/FITWorkflow/FT0RecPointReaderSpec.h
@@ -17,7 +17,7 @@
 
 #include "Framework/DataProcessorSpec.h"
 #include "Framework/Task.h"
-#include "DataFormatsFITT0/RecPoints.h"
+#include "DataFormatsFT0/RecPoints.h"
 
 using namespace o2::framework;
 
