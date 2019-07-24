1cdf12db3fc689f20a70c36c589f021ff4086df4
Ryan Nevils <rnevils@calpoly.edu>
2019-07-24 09:09:05 +0200

    FIT V0 renames

--- Detectors/FIT/V0/simulation/src/Detector.cxx
+++ Detectors/FIT/V0/simulation/src/Detector.cxx
@@ -65,9 +65,9 @@ Detector::Detector(Bool_t isActive)
 void Detector::InitializeO2Detector()
 {
   LOG(INFO) << "FV0: Initializing O2 detector. Adding sensitive volume.";
-  TGeoVolume* volSensitive = gGeoManager->GetVolume("V0cell");
+  TGeoVolume* volSensitive = gGeoManager->GetVolume("FV0cell");
   if (!volSensitive) {
-    LOG(FATAL) << "Can't find FIT V0 sensitive volume: cell";
+    LOG(FATAL) << "Can't find FV0 sensitive volume: cell";
   } else {
     AddSensitiveVolume(volSensitive);
     LOG(INFO) << "FV0: Sensitive volume: " << volSensitive->GetName() << "   " << volSensitive->GetNumber();
