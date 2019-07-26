ecdc23ad0bc8f30ce485fc5540bdd5ad190817de
Ryan Nevils <rnevils@calpoly.edu>
2019-07-26 15:01:09 +0200

    fixing erroneous T0 and V0 changes

--- EventVisualisation/DataConverter/include/EventVisualisationDataConverter/ConversionConstants.h
+++ EventVisualisation/DataConverter/include/EventVisualisationDataConverter/ConversionConstants.h
@@ -25,9 +25,9 @@ enum ETrackType {
   KinkMother,
   KinkDaughter,
 
-  FV0NegativeDaughter,
-  FV0PositiveDaughter,
-  FV0Mother,
+  V0NegativeDaughter,
+  V0PositiveDaughter,
+  V0Mother,
 
   CascadePrimaryMother,
   CascadePrimaryDaughter,
@@ -82,9 +82,9 @@ const std::string gTrackTypes[nTrackTypes] = {
   "kink_mother",
   "kink_daughter",
 
-  "FV0_negative_daughter",
-  "FV0_positive_daughter",
-  "FV0_mother",
+  "V0_negative_daughter",
+  "V0_positive_daughter",
+  "V0_mother",
 
   "cascade_primary_mother",
   "cascade_primary_daughter",
