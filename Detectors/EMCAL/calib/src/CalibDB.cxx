40834055c24dd55585c0bf31cee22b584ac2d3a2
Hadi Hassan <hadi.hassan@cern.ch>
2019-07-26 18:35:51 +0200

    [EMCAL-575, EMCAL-583, EMCAL-581, EMCAL-585] Read/Write test Time and Temperature Calibration objects to emcccdb-test.cern.ch

--- Detectors/EMCAL/calib/src/CalibDB.cxx
+++ Detectors/EMCAL/calib/src/CalibDB.cxx
@@ -9,6 +9,8 @@
 // or submit itself to any jurisdiction.
 
 #include "EMCALCalib/BadChannelMap.h"
+#include "EMCALCalib/TimeCalibrationParams.h"
+#include "EMCALCalib/TempCalibrationParams.h"
 #include "EMCALCalib/CalibDB.h"
 
 using namespace o2::emcal;
@@ -28,20 +30,64 @@ void CalibDB::storeBadChannelMap(BadChannelMap* bcm, const std::map<std::string,
 {
   if (!mInit)
     init();
-  mCCDBManager.store(new o2::TObjectWrapper<o2::emcal::BadChannelMap>(bcm), "BadChannelMap/EMC", metadata, rangestart, rangeend);
+  mCCDBManager.store(new o2::TObjectWrapper<o2::emcal::BadChannelMap>(bcm), "EMC/BadChannelMap", metadata, rangestart, rangeend);
+}
+
+void CalibDB::storeTimeCalibParam(TimeCalibrationParams* tcp, const std::map<std::string, std::string>& metadata, ULong_t rangestart, ULong_t rangeend)
+{
+  if (!mInit)
+    init();
+  mCCDBManager.store(new o2::TObjectWrapper<o2::emcal::TimeCalibrationParams>(tcp), "EMC/TimeCalibParams", metadata, rangestart, rangeend);
+}
+
+void CalibDB::storeTempCalibParam(TempCalibrationParams* tcp, const std::map<std::string, std::string>& metadata, ULong_t rangestart, ULong_t rangeend)
+{
+  if (!mInit)
+    init();
+  mCCDBManager.store(new o2::TObjectWrapper<o2::emcal::TempCalibrationParams>(tcp), "EMC/TempCalibParams", metadata, rangestart, rangeend);
 }
 
 BadChannelMap* CalibDB::readBadChannelMap(ULong_t timestamp, const std::map<std::string, std::string>& metadata)
 {
   if (!mInit)
     init();
-  auto result = mCCDBManager.retrieve("BadChannelMap/EMC", metadata, timestamp);
+  auto result = mCCDBManager.retrieve("EMC/BadChannelMap", metadata, timestamp);
   if (!result)
-    throw ObjectNotFoundException(mCCDBServer, "BadChannelMap/EMC", metadata, timestamp);
+    throw ObjectNotFoundException(mCCDBServer, "EMC/BadChannelMap", metadata, timestamp);
   if (result->IsA() != TObjectWrapper<o2::emcal::BadChannelMap>::Class())
     throw TypeMismatchException("TObjectWrapper<o2::emcal::BadChannelMap>", result->IsA()->GetName());
   auto wrap = dynamic_cast<TObjectWrapper<o2::emcal::BadChannelMap>*>(result);
   if (!wrap)
     throw TypeMismatchException("TObjectWrapper<o2::emcal::BadChannelMap>", result->IsA()->GetName()); // type checked before - should not enter here
+  return wrap->getObj();
+}
+
+TimeCalibrationParams* CalibDB::readTimeCalibParam(ULong_t timestamp, const std::map<std::string, std::string>& metadata)
+{
+  if (!mInit)
+    init();
+  auto result = mCCDBManager.retrieve("EMC/TimeCalibParams", metadata, timestamp);
+  if (!result)
+    throw ObjectNotFoundException(mCCDBServer, "EMC/TimeCalibParams", metadata, timestamp);
+  if (result->IsA() != TObjectWrapper<o2::emcal::TimeCalibrationParams>::Class())
+    throw TypeMismatchException("TObjectWrapper<o2::emcal::TimeCalibrationParams>", result->IsA()->GetName());
+  auto wrap = dynamic_cast<TObjectWrapper<o2::emcal::TimeCalibrationParams>*>(result);
+  if (!wrap)
+    throw TypeMismatchException("TObjectWrapper<o2::emcal::TimeCalibrationParams>", result->IsA()->GetName()); // type checked before - should not enter here
+  return wrap->getObj();
+}
+
+TempCalibrationParams* CalibDB::readTempCalibParam(ULong_t timestamp, const std::map<std::string, std::string>& metadata)
+{
+  if (!mInit)
+    init();
+  auto result = mCCDBManager.retrieve("EMC/TempCalibParams", metadata, timestamp);
+  if (!result)
+    throw ObjectNotFoundException(mCCDBServer, "EMC/TempCalibParams", metadata, timestamp);
+  if (result->IsA() != TObjectWrapper<o2::emcal::TempCalibrationParams>::Class())
+    throw TypeMismatchException("TObjectWrapper<o2::emcal::TempCalibrationParams>", result->IsA()->GetName());
+  auto wrap = dynamic_cast<TObjectWrapper<o2::emcal::TempCalibrationParams>*>(result);
+  if (!wrap)
+    throw TypeMismatchException("TObjectWrapper<o2::emcal::TempCalibrationParams>", result->IsA()->GetName()); // type checked before - should not enter here
   return wrap->getObj();
 }
