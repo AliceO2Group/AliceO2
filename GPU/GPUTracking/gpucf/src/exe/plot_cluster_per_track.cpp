1e6859f0e0ac99ab6e515e642f98815370d391fe
Felix Weiglhofer <felix.weiglhofer@gmail.com>
2019-07-30 19:05:04 +0200

    Split noise suppression pipeline.

--- GPU/GPUTracking/gpucf/src/exe/plot_cluster_per_track.cpp
+++ GPU/GPUTracking/gpucf/src/exe/plot_cluster_per_track.cpp
@@ -2,6 +2,7 @@
 #include <gpucf/common/LabelContainer.h>
 #include <gpucf/common/RawDigit.h>
 #include <gpucf/common/serialization.h>
+#include <gpucf/noisesuppression/PeakCount.h>
 
 #include <args/args.hxx>
 
@@ -15,44 +16,6 @@
 using namespace gpucf;
 
 
-struct PeakCount
-{
-    size_t peakNum0 = 0;
-    size_t peakNum1 = 0;
-    size_t peakNum10 = 0;
-    size_t peakNumX = 0;
-
-
-    PeakCount() = default;
-
-    PeakCount(const std::unordered_map<MCLabel, size_t> &peaksPerTrack)
-    {
-        for (auto p : peaksPerTrack)
-        {
-            update(p.second);
-        }
-    }
-
-    PeakCount(const SectorMap<PeakCount> &peakCounts)
-    {
-        for (const PeakCount &pc : peakCounts)
-        {
-            peakNum0 += pc.peakNum0;
-            peakNum1 += pc.peakNum1;
-            peakNum10 += pc.peakNum10;
-            peakNumX += pc.peakNumX;
-        }
-    }
-
-    void update(size_t c)
-    {
-        peakNum0  += (c == 0);
-        peakNum1  += (c == 1);
-        peakNum10 += (c > 1 && c < 10);
-        peakNumX  += (c >= 10);
-    }
-
-};
 
 
 class LabelBucketCounter
@@ -127,14 +90,6 @@ private:
 
 };
 
-std::ostream &operator<<(std::ostream &o, const PeakCount &pc)
-{
-    return o << "Number of tracks with...\n"
-             << " ... no peaks    -> " << pc.peakNum0 << "\n"
-             << " ... one peak    -> " << pc.peakNum1 << "\n"
-             << " ... <  10 peaks -> " << pc.peakNum10 << "\n"
-             << " ... >= 10 peaks -> " << pc.peakNumX;
-}
 
 
 int clamp(int x, int l, int r)
---
+++ GPU/GPUTracking/gpucf/src/gpucf/noisesuppression/NoiseSuppression.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <gpucf/common/Digit.h>
+#include <gpucf/common/Map.h>
+#include <gpucf/common/RowMap.h>
+#include <gpucf/common/View.h>
+
+#include <vector>
+
+
+namespace gpucf
+{
+    
+class NoiseSuppression
+{
+
+public:
+
+    RowMap<std::vector<Digit>> run(
+            const RowMap<std::vector<Digit>> &,
+            const RowMap<Map<bool>> &,
+            const Map<float> &);
+
+    std::string getName() const
+    {
+        return name;
+    }
+
+protected:
+    
+    NoiseSuppression(const std::string &name)
+        : name(name)
+    {
+    }
+
+    virtual std::vector<Digit> runImpl(
+            View<Digit>,
+            const Map<bool> &,
+            const Map<float> &) = 0;
+
+private:
+
+    std::string name;
+
+};
+
+} // namespace gpucf
+
+// vim: set ts=4 sw=4 sts=4 expandtab:
+
