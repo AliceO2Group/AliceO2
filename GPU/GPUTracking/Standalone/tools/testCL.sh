a6b8cde860443aac4d9bb9e6dd84aa0d4ced6e99
David Rohr <drohr@jwdt.org>
2019-08-17 12:25:17 +0200

    GPU: Fix OpenCL 2 test script and clang cl-std setting

--- GPU/GPUTracking/Standalone/tools/testCL.sh
+++ GPU/GPUTracking/Standalone/tools/testCL.sh
@@ -14,27 +14,28 @@ DEFINES="-DGPUCA_STANDALONE -DGPUCA_ENABLE_GPU_TRACKER -DGPUCA_GPULIBRARY=OCL -D
 FLAGS="-cl-denorms-are-zero -cl-mad-enable -cl-no-signed-zeros -ferror-limit=1000 -Xclang -finclude-default-header -Dcl_clang_storage_class_specifiers -Wno-invalid-constexpr"
 
 echo Test1 - Preprocess
-echo $COMPILER -cl-std=c++ -x cl $INCLUDES $DEFINES -Dcl_clang_storage_class_specifiers -E ../Base/opencl/GPUReconstructionOCL.cl > test.cl
-     $COMPILER -cl-std=c++ -x cl $INCLUDES $DEFINES -Dcl_clang_storage_class_specifiers -E ../Base/opencl/GPUReconstructionOCL.cl > test.cl
+echo $COMPILER -cl-std=clc++ -x cl $INCLUDES $DEFINES -Dcl_clang_storage_class_specifiers -E ../Base/opencl-common/GPUReconstructionOCL.cl > test.cl
+     $COMPILER -cl-std=clc++ -x cl $INCLUDES $DEFINES -Dcl_clang_storage_class_specifiers -E ../Base/opencl-common/GPUReconstructionOCL.cl > test.cl
 if [ $? != 0 ]; then exit 1; fi
     #Test 1A - Compile Preprocessed
     #$COMPILER -cl-std=c++ -x cl --target=amdgcn-amd-amdhsa -mcpu=gfx906 -cl-denorms-are-zero -cl-mad-enable -cl-no-signed-zeros -ferror-limit=1000 -Xclang -finclude-default-header -c test.cl -o test.o
     #exit
 
 echo Test2 - Clang OCL
-echo clang-ocl -I/usr/lib/llvm/9/lib64/clang/9.0.0/include/opencl-c.h -cl-std=c++ -mcpu=gfx906 $FLAGS $INCLUDES $DEFINES -o test-clang-ocl.o ../Base/opencl/GPUReconstructionOCL.cl
-     clang-ocl -I/usr/lib/llvm/9/lib64/clang/9.0.0/include/opencl-c.h -cl-std=c++ -mcpu=gfx906 $FLAGS $INCLUDES $DEFINES -o test-clang-ocl.o ../Base/opencl/GPUReconstructionOCL.cl
+echo clang-ocl -cl-std=clc++ -mcpu=gfx906 $FLAGS $INCLUDES $DEFINES -o test-clang-ocl.o ../Base/opencl-common/GPUReconstructionOCL.cl
+     clang-ocl -cl-std=clc++ -mcpu=gfx906 $FLAGS $INCLUDES $DEFINES -o test-clang-ocl.o ../Base/opencl-common/GPUReconstructionOCL.cl
+rm -f test-clang-ocl.o.*
 if [ $? != 0 ]; then exit 1; fi
 
 echo Test3 - SPIR-V
-echo $COMPILER -cl-std=c++ -x cl -emit-llvm --target=spir64-unknown-unknown $FLAGS $INCLUDES $DEFINES -c ../Base/opencl/GPUReconstructionOCL.cl -o test.bc
-     $COMPILER -cl-std=c++ -x cl -emit-llvm --target=spir64-unknown-unknown $FLAGS $INCLUDES $DEFINES -c ../Base/opencl/GPUReconstructionOCL.cl -o test.bc
+echo $COMPILER -cl-std=clc++ -x cl -emit-llvm --target=spir64-unknown-unknown $FLAGS $INCLUDES $DEFINES -c ../Base/opencl-common/GPUReconstructionOCL.cl -o test.bc
+     $COMPILER -cl-std=clc++ -x cl -emit-llvm --target=spir64-unknown-unknown $FLAGS $INCLUDES $DEFINES -c ../Base/opencl-common/GPUReconstructionOCL.cl -o test.bc
 if [ $? != 0 ]; then exit 1; fi
 echo $LLVM_SPIRV test.bc
      $LLVM_SPIRV test.bc
 if [ $? != 0 ]; then exit 1; fi
 
 echo Test4 - amdgcn
-echo $COMPILER -cl-std=c++ -x cl --target=amdgcn-amd-amdhsa -mcpu=gfx906 $FLAGS $INCLUDES $DEFINES -c ../Base/opencl/GPUReconstructionOCL.cl -o test.o
-     $COMPILER -cl-std=c++ -x cl --target=amdgcn-amd-amdhsa -mcpu=gfx906 $FLAGS $INCLUDES $DEFINES -c ../Base/opencl/GPUReconstructionOCL.cl -o test.o
+echo $COMPILER -cl-std=clc++ -x cl --target=amdgcn-amd-amdhsa -mcpu=gfx906 $FLAGS $INCLUDES $DEFINES -c ../Base/opencl-common/GPUReconstructionOCL.cl -o test.o
+     $COMPILER -cl-std=clc++ -x cl --target=amdgcn-amd-amdhsa -mcpu=gfx906 $FLAGS $INCLUDES $DEFINES -c ../Base/opencl-common/GPUReconstructionOCL.cl -o test.o
 if [ $? != 0 ]; then exit 1; fi
