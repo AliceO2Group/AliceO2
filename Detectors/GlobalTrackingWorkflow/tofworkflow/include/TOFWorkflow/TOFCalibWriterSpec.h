ad9ddd87a2a2fed74f0e3a8192c7df06aaf94eb6
noferini <noferini@bo.infn.it>
2019-08-20 09:51:16 +0200

    fix tof writer spec by adding tasks

--- Detectors/GlobalTrackingWorkflow/tofworkflow/include/TOFWorkflow/TOFCalibWriterSpec.h
+++ Detectors/GlobalTrackingWorkflow/tofworkflow/include/TOFWorkflow/TOFCalibWriterSpec.h
@@ -8,19 +8,43 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
+/// @file   TOFCalibWriterSpec.h
+
 #ifndef TOFWORKFLOW_TOFCALIBWRITER_H_
 #define TOFWORKFLOW_TOFCALIBWRITER_H_
 
+#include "TFile.h"
+
 #include "Framework/DataProcessorSpec.h"
+#include "Framework/Task.h"
+#include <string>
+
+using namespace o2::framework;
 
 namespace o2
 {
 namespace tof
 {
 
+class TOFCalibWriter : public Task
+{
+ public:
+  TOFCalibWriter() = default;
+  ~TOFCalibWriter() override = default;
+  void init(InitContext& ic) final;
+  void run(ProcessingContext& pc) final;
+
+ private:
+  bool mFinished = false;
+  std::string mOutFileName; // read from workflow
+  std::string mOutTreeName; // read from workflow
+};
+
+/// create a processor spec
+/// write TOF calbi info in a root file
 o2::framework::DataProcessorSpec getTOFCalibWriterSpec();
 
-} // end namespace tof
-} // end namespace o2
+} // namespace tof
+} // namespace o2
 
 #endif /* TOFWORKFLOW_TOFCALIBWRITER_H_ */
--- Detectors/GlobalTrackingWorkflow/tofworkflow/include/TOFWorkflow/TOFMatchedWriterSpec.h
+++ Detectors/GlobalTrackingWorkflow/tofworkflow/include/TOFWorkflow/TOFMatchedWriterSpec.h
@@ -8,19 +8,44 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
+/// @file   TOFMatchedWriterSpec.h
+
 #ifndef TOFWORKFLOW_TOFMATCHEDWRITER_H_
 #define TOFWORKFLOW_TOFMATCHEDWRITER_H_
 
+#include "TFile.h"
+
 #include "Framework/DataProcessorSpec.h"
+#include "Framework/Task.h"
+#include <string>
+
+using namespace o2::framework;
 
 namespace o2
 {
 namespace tof
 {
 
+class TOFMatchedWriter : public Task
+{
+ public:
+  TOFMatchedWriter(bool useMC = true) : mUseMC(useMC) {}
+  ~TOFMatchedWriter() override = default;
+  void init(InitContext& ic) final;
+  void run(ProcessingContext& pc) final;
+
+ private:
+  bool mFinished = false;
+  bool mUseMC = true;
+  std::string mOutFileName; // read from workflow
+  std::string mOutTreeName; // read from workflow
+};
+
+/// create a processor spec
+/// write TOF matching info in a root file
 o2::framework::DataProcessorSpec getTOFMatchedWriterSpec(bool useMC);
 
-} // end namespace tof
-} // end namespace o2
+} // namespace tof
+} // namespace o2
 
 #endif /* TOFWORKFLOW_TOFMATCHEDWRITER_H_ */
