ea437de3138932534f1216834c80bf498351a65d
Giulio Eulisse <giulio.eulisse@cern.ch>
2019-08-24 09:04:47 +0200

    DPL Analysis: add example for new collections

---
+++ Framework/AnalysisTutorial/src/newCollections.cxx
@@ -0,0 +1,60 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+#include "Framework/runDataProcessing.h"
+#include "Framework/AnalysisTask.h"
+#include "Framework/AnalysisDataModel.h"
+
+namespace o2::aod
+{
+namespace etaphi
+{
+DECLARE_SOA_COLUMN(Eta, eta, float, "fEta");
+DECLARE_SOA_COLUMN(Phi, phi, float, "fPhi");
+} // namespace etaphi
+DECLARE_SOA_TABLE(EtaPhi, "RN2", "ETAPHI",
+                  etaphi::Eta, etaphi::Phi);
+} // namespace o2::aod
+
+using namespace o2;
+using namespace o2::framework;
+
+// This is a very simple example showing how to iterate over tracks
+// and create a new collection for them.
+// FIXME: this should really inherit from AnalysisTask but
+//        we need GCC 7.4+ for that
+struct ATask {
+  Produces<aod::EtaPhi> etaphi;
+
+  void process(aod::Tracks const& tracks)
+  {
+    for (auto& track : tracks) {
+      auto phi = asin(track.snp()) + track.alpha() + M_PI;
+      auto eta = log(tan(0.25 * M_PI - 0.5 * atan(track.tgl())));
+
+      etaphi(phi, eta);
+    }
+  }
+};
+
+struct BTask {
+  void process(aod::EtaPhi const& etaPhis)
+  {
+    for (auto& etaPhi : etaPhis) {
+      LOGF(ERROR, "(%f, %f)", etaPhi.eta(), etaPhi.phi());
+    }
+  }
+};
+
+WorkflowSpec defineDataProcessing(ConfigContext const&)
+{
+  return WorkflowSpec{
+    adaptAnalysisTask<ATask>("produce-etaphi"),
+    adaptAnalysisTask<BTask>("consume-etaphi")};
+}
