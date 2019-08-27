cbac84074b57e3d4fa469da22bc38bf81ac34d4a
Sandro Wenzel <sandro.wenzel@cern.ch>
2019-08-27 12:02:14 +0200

    CcdbApi: new features and sanitizing
    This commit adds the following (sometime prototypic) new functionality to CcdbApi:
    
    * ability to write/retrieve any arbitrary class T (having a ROOT
      dictionary) to the CCDB (via the TMemFile) mechanism
      - the function is offered next to existing API for the moment
      - a unit test checking this functionality was added
    
    * ability parse and return the recursive subfolder structure inside the CCDB
    
    * ability to generate a local snapshot of the CCDB (given some top level path and timestamp)
      - checked in unit test
    
    * ability to init the CCDB from the snapshot in order to serve cases when there
      is no network connection
      - works only with the new template<T> API for the moment
    
    In addition, this commit sanitizes existing code a bit:
    
    * apply const correctness
    
    * pass values by reference not by value
    
    * separate some timestamp utility functions and reduce code duplication
    
    * make object extraction from TFile standalone according to DRY principle
    
    * extend unit test to make sure any timestamp in a given interval works correctly

--- CCDB/test/testCcdbApi.cxx
+++ CCDB/test/testCcdbApi.cxx
@@ -18,7 +18,11 @@
 #define BOOST_TEST_DYN_LINK
 
 #include "CCDB/CcdbApi.h"
+#include "CCDB/IdPath.h"    // just as test object
+#include "CCDB/Condition.h" // just as test object
+#include "CCDB/CCDBTimeStampUtils.h"
 #include <boost/test/unit_test.hpp>
+#include <boost/filesystem.hpp>
 #include <cassert>
 #include <iostream>
 #include <cstdio>
@@ -32,6 +36,7 @@
 #include <TStreamerInfo.h>
 #include <TGraph.h>
 #include <TTree.h>
+#include <TString.h>
 
 using namespace std;
 using namespace o2::ccdb;
@@ -81,25 +86,6 @@ struct test_fixture {
   map<string, string> metadata;
 };
 
-long getFutureTimestamp(int secondsInFuture)
-{
-  std::chrono::seconds sec(secondsInFuture);
-  auto future = std::chrono::system_clock::now() + sec;
-  auto future_ms = std::chrono::time_point_cast<std::chrono::milliseconds>(future);
-  auto epoch = future_ms.time_since_epoch();
-  auto value = std::chrono::duration_cast<std::chrono::milliseconds>(epoch);
-  return value.count();
-}
-
-long getCurrentTimestamp()
-{
-  auto now = std::chrono::system_clock::now();
-  auto now_ms = std::chrono::time_point_cast<std::chrono::milliseconds>(now);
-  auto epoch = now_ms.time_since_epoch();
-  auto value = std::chrono::duration_cast<std::chrono::milliseconds>(epoch);
-  return value.count();
-}
-
 BOOST_AUTO_TEST_CASE(storeTMemFile_test, *utf::precondition(if_reachable()))
 {
   test_fixture f;
@@ -118,6 +104,105 @@ BOOST_AUTO_TEST_CASE(storeTMemFile_test, *utf::precondition(if_reachable()))
   tree->Branch("det", &a, "a/I");
   tree->Fill();
   f.api.storeAsTFile(tree, "Test/tree", f.metadata);
+}
+
+BOOST_AUTO_TEST_CASE(store_retrieve_TMemFile_templated_test, *utf::precondition(if_reachable()))
+{
+  test_fixture f;
+
+  // try to store a user defined class
+  // since we don't depend on anything, we are putting an object known to CCDB
+  o2::ccdb::IdPath path;
+  path.setPath("HelloWorld");
+
+  f.api.storeAsTFileAny(&path, "Test/CCDBPath", f.metadata);
+
+  // try to retrieve strongly typed user defined class
+  // since we don't depend on anything, we are using an object known to CCDB
+  o2::ccdb::IdPath* path2 = nullptr;
+
+  path2 = f.api.retrieveFromTFileAny<o2::ccdb::IdPath>("Test/CCDBPath", f.metadata);
+  BOOST_CHECK_NE(path2, nullptr);
+
+  // check some non-trivial data content
+  BOOST_CHECK(path2 && path2->getPathString().CompareTo("HelloWorld") == 0);
+
+  // try to query with different type and verify that we get nullptr
+  BOOST_CHECK(f.api.retrieveFromTFileAny<o2::ccdb::Condition>("Test/CCDBPath", f.metadata) == nullptr);
+
+  //-----------------------------------------------------------------------------------------------
+  // test if writing/reading complicated objects like TTree works (because of particular ownership)
+  // ----------------------------------------------------------------------------------------------
+  auto tree = new TTree("tree123", "tree123");
+  int a = 4;
+  tree->Branch("det", &a, "a/I");
+  tree->Fill();
+  f.api.storeAsTFileAny(tree, "Test/tree2", f.metadata);
+  delete tree;
+
+  // read back
+  tree = f.api.retrieveFromTFileAny<TTree>("/Test/tree2", f.metadata);
+  BOOST_CHECK(tree != nullptr);
+  BOOST_CHECK(tree != nullptr && std::strcmp(tree->GetName(), "tree123") == 0);
+  BOOST_CHECK(tree != nullptr && tree->GetEntries() == 1);
+
+  // ---------------------------
+  // test the snapshot mechanism
+  // ---------------------------
+  // a) create a local snapshot of the Test folder
+  auto ph = boost::filesystem::unique_path();
+  boost::filesystem::create_directories(ph);
+  f.api.snapshot("/Test", ph.string(), o2::ccdb::getCurrentTimestamp());
+  std::cout << "Creating snapshot at " << ph.string() << "\n";
+
+  // b) init a new instance from the snapshot and query something from it
+  o2::ccdb::CcdbApi snapshot;
+  snapshot.initInSnapshotMode(ph.string());
+
+  // c) query from the snapshot
+  BOOST_CHECK(snapshot.retrieveFromTFileAny<o2::ccdb::IdPath>("Test/CCDBPath", f.metadata) != nullptr);
+
+  {
+    auto tree = snapshot.retrieveFromTFileAny<TTree>("/Test/tree2", f.metadata);
+    BOOST_CHECK(tree != nullptr);
+    BOOST_CHECK(tree != nullptr && std::strcmp(tree->GetName(), "tree123") == 0);
+    BOOST_CHECK(tree != nullptr && tree->GetEntries() == 1);
+  }
+}
+
+/// A test verifying that the DB responds the correct result for given timestamps
+BOOST_AUTO_TEST_CASE(timestamptest, *utf::precondition(if_reachable()))
+{
+  test_fixture f;
+
+  // try to store a user defined class
+  // since we don't depend on anything, we are putting an object known to CCDB
+  o2::ccdb::IdPath path;
+  path.setPath("HelloWorld");
+
+  const long timestamp = 1000;             // inclusive start of validity
+  const long endvalidity = timestamp + 10; // exclusive end of validitiy
+  f.api.storeAsTFileAny(&path, "Test/CCDBPathUnitTest", f.metadata, timestamp, endvalidity);
+
+  // try to retrieve strongly typed user defined class
+  // since we don't depend on anything, we are using an object known to CCDB
+  o2::ccdb::IdPath* path2 = nullptr;
+
+  path2 = f.api.retrieveFromTFileAny<o2::ccdb::IdPath>("Test/CCDBPathUnitTest", f.metadata, timestamp);
+  BOOST_CHECK_NE(path2, nullptr);
+
+  // check that we get something for the whole time range
+  for (int t = timestamp; t < endvalidity; ++t) {
+    auto p = f.api.retrieveFromTFileAny<o2::ccdb::IdPath>("Test/CCDBPathUnitTest", f.metadata, t);
+    BOOST_CHECK_NE(p, nullptr);
+  }
+
+  // check that answer is null for anything outside
+  auto plower = f.api.retrieveFromTFileAny<o2::ccdb::IdPath>("Test/CCDBPathUnitTest", f.metadata, timestamp - 1);
+  BOOST_CHECK(plower == nullptr);
+
+  auto pupper = f.api.retrieveFromTFileAny<o2::ccdb::IdPath>("Test/CCDBPathUnitTest", f.metadata, endvalidity);
+  BOOST_CHECK(pupper == nullptr);
 }
 
 BOOST_AUTO_TEST_CASE(retrieveTMemFile_test, *utf::precondition(if_reachable()))
@@ -212,8 +297,8 @@ BOOST_AUTO_TEST_CASE(delete_test, *utf::precondition(if_reachable()))
   test_fixture f;
 
   auto h1 = new TH1F("object1", "object1", 100, 0, 99);
-  long from = getCurrentTimestamp();
-  long to = getFutureTimestamp(60 * 60 * 24 * 365 * 10);
+  long from = o2::ccdb::getCurrentTimestamp();
+  long to = o2::ccdb::getFutureTimestamp(60 * 60 * 24 * 365 * 10);
   f.api.store(h1, "Test/Detector", f.metadata, from, to); // test with explicit dates
   auto h2 = f.api.retrieve("Test/Detector", f.metadata);
   BOOST_CHECK(h2 != nullptr);
