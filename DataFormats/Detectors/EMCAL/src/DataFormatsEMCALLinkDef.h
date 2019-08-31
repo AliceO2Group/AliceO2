59f604fa950d2ceac26970ebfc82f3e919053c45
Markus Fasel <markus.fasel@cern.ch>
2019-08-31 10:27:29 +0200

    [EMCAL-609] Add ChannelType_t to linkdef in DataFormatsEMCAL

--- DataFormats/Detectors/EMCAL/src/DataFormatsEMCALLinkDef.h
+++ DataFormats/Detectors/EMCAL/src/DataFormatsEMCALLinkDef.h
@@ -22,4 +22,7 @@
 #pragma link C++ class std::vector < o2::emcal::Digit > +;
 #pragma link C++ class std::vector < o2::emcal::Cluster > +;
 
+// For channel type in digits and cells
+#pragma link C++ enum class o2::emcal::ChannelType_t + ;
+
 #endif
