b4f80c81f2c1259db0e79c144f7f5ecd3e2d2010
Felix Weiglhofer <felix.weiglhofer@gmail.com>
2019-09-01 19:47:51 +0200

    Conditionally load charges into LDS.

--- GPU/GPUTracking/gpucf/run/benchmarks.sh
+++ GPU/GPUTracking/gpucf/run/benchmarks.sh
@@ -2,9 +2,6 @@
 
 set -euo pipefail
 
-remoteTgt=$1
-tgtDir=$2
-
 measurementsDir='measurements'
 resdir="$measurementsDir/in"
 buildDir='build'
@@ -15,8 +12,8 @@ declare -a configs=(
     "--layoutTiling4x4 -o$resdir/tiling4x4_vega20.json"
     "--layoutTiling4x4 --builderScratchPad -o$resdir/scratchpad4x4_vega20.json"
     "--layoutTimeMajor --builderScratchPad -o$resdir/scratchpadTime_vega20.json"
-    "--layoutTimeMajor -o$resdir/timemajor_vega10.json -g1"
-    "--layoutTiling4x4 --builderScratchPad -o$resdir/scratchpad4x4_vega10.json -g1"
+    # "--layoutTimeMajor -o$resdir/timemajor_vega10.json -g1"
+    # "--layoutTiling4x4 --builderScratchPad -o$resdir/scratchpad4x4_vega10.json -g1"
     "--layoutTimeMajor -cpad -o$resdir/timemajor_vega20_pad.json"
     "--layoutTimeMajor -crandom -o$resdir/timemajor_vega20_rand.json"
     "--layoutTimeMajor -cfull -o$resdir/timemajor_vega20_full.json"
@@ -25,23 +22,11 @@ declare -a configs=(
     "--layoutTiling4x4 -cfull --builderScratchPad -o$resdir/scratchpad4x4_vega20_full.json"
 )
 
+mkdir -p $measurementsDir/in
+mkdir -p $measurementsDir/out
+make -sC$buildDir/release benchmark -j64
+
 for cfg in "${configs[@]}"; do
-    ssh $remoteTgt <<-ENDSSH
-        cd $tgtDir
-        mkdir -p $measurementsDir/in
-        mkdir -p $measurementsDir/out
-        make -sC$buildDir/release benchmark -j64
-            ./$buildDir/release/bin/benchmark -scl -ddata/digits_ev10_pythia8hi.bin $cfg
-ENDSSH
+    ./$buildDir/release/bin/benchmark -scl -ddata/digits_ev10_pythia8hi.bin $cfg
 done
 
-scp $remoteTgt:$tgtDir/$measurementsDir/in/* $measurementsDir/in
-
-
-plotFiles=$(find $measurementsDir -name '*.toml')
-plotCmd='run/plot.py'
-
-for config in ${plotFiles[*]}
-do
-    $plotCmd $config
-done
