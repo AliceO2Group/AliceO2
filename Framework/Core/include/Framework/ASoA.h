aaf2736aa6c46ab77485ac580f5c1e0030923834
Giulio Eulisse <giulio.eulisse@cern.ch>
2019-09-06 14:08:35 +0200

    DPL Analysis: add dynamic columns to Arrow Skins
    This allows to add columns to an SoA table which are not actually backed
    up by an `arrow::Column`, but which are calculated on the fly from
    values stored in the normal "Persistent" `soa::Columns`.

--- Framework/Core/include/Framework/ASoA.h
+++ Framework/Core/include/Framework/ASoA.h
@@ -11,6 +11,8 @@
 #ifndef O2_FRAMEWORK_ASOA_H_
 #define O2_FRAMEWORK_ASOA_H_
 
+#include "Framework/FunctionalHelpers.h"
+#include "Framework/Traits.h"
 #include <arrow/table.h>
 #include <arrow/array.h>
 #include <cassert>
@@ -22,6 +24,22 @@ namespace soa
 
 template <typename T>
 struct arrow_array_for {
+};
+template <>
+struct arrow_array_for<int8_t> {
+  using type = arrow::Int8Array;
+};
+template <>
+struct arrow_array_for<uint8_t> {
+  using type = arrow::UInt8Array;
+};
+template <>
+struct arrow_array_for<int16_t> {
+  using type = arrow::Int16Array;
+};
+template <>
+struct arrow_array_for<uint16_t> {
+  using type = arrow::UInt16Array;
 };
 template <>
 struct arrow_array_for<int32_t> {
@@ -138,26 +156,65 @@ class ColumnIterator
 
 template <typename T, typename INHERIT>
 struct Column {
+  using storage_t = T;
+  using inherited_t = INHERIT;
+  Column(ColumnIterator<T> it)
+    : mColumnIterator{it}
+  {
+  }
+
+  using persistent = std::true_type;
   using type = T;
   static constexpr const char* const& label() { return INHERIT::mLabel; }
   ColumnIterator<T> const& getIterator() const
   {
     return mColumnIterator;
   }
+  /// FIXME: rather than keeping this public we should have a protected
+  /// non-const getter and mark this private.
   ColumnIterator<T> mColumnIterator;
 };
+
+/// The purpose of this class is to store the lambda which is associated to the
+/// method call.
+template <typename F, typename INHERIT>
+struct DynamicColumn {
+  using storage_t = F;
+  using inherited_t = INHERIT;
+
+  DynamicColumn(F callable)
+    : mCallable{callable}
+  {
+  }
+
+  using persistent = std::false_type;
+  static constexpr const char* const& label() { return INHERIT::mLabel; }
+  F& callable() { return mCallable; }
+
+  F mCallable;
+};
+
+template <typename T>
+using is_not_persistent_t = typename std::decay_t<T>::persistent::type;
+
+template <typename T>
+using is_persistent_t = std::is_same<typename std::decay_t<T>::persistent::type, std::false_type>;
 
 template <typename... C>
 struct RowView : public C... {
  public:
+  using persistent_columns_t = framework::filtered_pack<is_not_persistent_t, C...>;
+  using dynamic_columns_t = framework::filtered_pack<is_persistent_t, C...>;
+
   RowView(std::shared_ptr<arrow::Table> const& table)
-    : C{ColumnIterator<typename C::type>(table->column(table->schema()->GetFieldIndex(C::label())))}...
+    : C(table)...
   {
+    bindAllDynamicColumns(dynamic_columns_t{});
   }
 
   RowView<C...>& operator++()
   {
-    (C::mColumnIterator.operator++(), ...);
+    incrementPersistent(persistent_columns_t{});
     return *this;
   }
 
@@ -173,19 +230,65 @@ struct RowView : public C... {
     return *this;
   }
 
+  /// Notice this relies on the fact that the iterators in the column are all
+  /// in sync and therefore we check only for the first one.
   bool operator!=(RowView<C...> const& other) const
   {
-    return (static_cast<C const&>(*this).mColumnIterator.operator!=(static_cast<C const&>(other).mColumnIterator) || ...);
+    return doCompareNotEqual(persistent_columns_t{}, other);
   }
 
   bool operator==(RowView<C...> const& other) const
   {
-    return (static_cast<C const&>(*this).mColumnIterator.operator==(static_cast<C const&>(other).mColumnIterator) || ...);
+    return doCompareEqual(persistent_columns_t{}, other);
   }
 
   void moveToEnd()
   {
-    (C::mColumnIterator.moveToEnd(), ...);
+    doMoveToEnd(persistent_columns_t{});
+  }
+
+ private:
+  /// Helper to only increment the iterators for persistent columns which actually have
+  /// it.
+  template <typename... PC>
+  void incrementPersistent(framework::pack<PC...> pack)
+  {
+    (PC::mColumnIterator.operator++(), ...);
+  }
+
+  /// Helper to move at the end of columns which actually have an iterator.
+  template <typename... PC>
+  void doMoveToEnd(framework::pack<PC...> pack)
+  {
+    (PC::mColumnIterator.moveToEnd(), ...);
+  }
+
+  /// Helper to move at the end of columns which actually have an iterator.
+  template <typename... PC>
+  bool doCompareEqual(framework::pack<PC...> pack, RowView<C...> const& other) const
+  {
+    using first_t = framework::pack_element_t<0, decltype(pack)>;
+    return (static_cast<first_t const&>(*this).mColumnIterator.operator==(static_cast<first_t const&>(other).mColumnIterator));
+  }
+
+  template <typename... PC>
+  bool doCompareNotEqual(framework::pack<PC...> pack, RowView<C...> const& other) const
+  {
+    using first_t = framework::pack_element_t<0, decltype(pack)>;
+    return (static_cast<first_t const&>(*this).mColumnIterator.operator!=(static_cast<first_t const&>(other).mColumnIterator));
+  }
+
+  template <typename... DC>
+  auto bindAllDynamicColumns(framework::pack<DC...> pack)
+  {
+    using namespace o2::soa;
+    (bindDynamicColumn<DC>(typename DC::bindings_t{}), ...);
+  }
+
+  template <typename DC, typename... B>
+  auto bindDynamicColumn(framework::pack<B...> bindings)
+  {
+    DC::boundIterators = std::make_tuple(&(B::mColumnIterator)...);
   }
 };
 
@@ -195,7 +298,7 @@ class Table
  public:
   using iterator = RowView<C...>;
   using const_iterator = RowView<C...>;
-  using columns = std::tuple<C...>;
+  using columns = framework::pack<C...>;
 
   Table(std::shared_ptr<arrow::Table> table)
     : mTable(table)
@@ -240,6 +343,30 @@ class Table
   std::shared_ptr<arrow::Table> mTable;
 };
 
+template <typename T>
+struct PackToTable {
+  static_assert(framework::always_static_assert_v<T>, "Not a pack");
+};
+
+template <typename... C>
+struct PackToTable<framework::pack<C...>> {
+  using table = o2::soa::Table<C...>;
+};
+
+template <typename T>
+struct FilterPersistentColumns {
+  static_assert(framework::always_static_assert_v<T>, "Not a soa::Table");
+};
+
+template <typename... C>
+struct FilterPersistentColumns<soa::Table<C...>> {
+  using columns = typename soa::Table<C...>::columns;
+  using persistent_columns_pack = framework::filtered_pack<is_not_persistent_t, C...>;
+  using persistent_table_t = typename PackToTable<persistent_columns_pack>::table;
+};
+
+/// Template trait which allows to map a given
+/// Table type to its O2 DataModel origin and description
 template <typename INHERIT>
 class TableMetadata
 {
@@ -258,15 +385,88 @@ class TableMetadata
     using metadata = std::void_t<T>; \
   }
 
-#define DECLARE_SOA_COLUMN(_Name_, _Getter_, _Type_, _Label_) \
-  struct _Name_ : o2::soa::Column<_Type_, _Name_> {           \
-    static constexpr const char* mLabel = _Label_;            \
-    using type = _Type_;                                      \
-                                                              \
-    _Type_ const _Getter_() const                             \
-    {                                                         \
-      return *mColumnIterator;                                \
-    }                                                         \
+#define DECLARE_SOA_COLUMN(_Name_, _Getter_, _Type_, _Label_)                                                                            \
+  struct _Name_ : o2::soa::Column<_Type_, _Name_> {                                                                                      \
+    static constexpr const char* mLabel = _Label_;                                                                                       \
+    using type = _Type_;                                                                                                                 \
+    using column_t = _Name_;                                                                                                             \
+    _Name_(std::shared_ptr<arrow::Table> table)                                                                                          \
+      : o2::soa::Column<_Type_, _Name_>(o2::soa::ColumnIterator<type>(table->column(table->schema()->GetFieldIndex(column_t::label())))) \
+    {                                                                                                                                    \
+    }                                                                                                                                    \
+                                                                                                                                         \
+    _Type_ const _Getter_() const                                                                                                        \
+    {                                                                                                                                    \
+      return *mColumnIterator;                                                                                                           \
+    }                                                                                                                                    \
+  }
+
+/// A dynamic column is a column whose values are derived
+/// from those of other real columns. These can be used for
+/// example to provide different coordinate systems (e.g. polar,
+/// cylindrical) from a persister representation (e.g. cartesian).
+/// _Name_ is a unique typename which will be associated with the
+/// column. _Getter_ is a mnemonic to retrieve the value of
+/// the column in a given row. The variadic template argument
+/// (...) is used to capture a lambda or callable object which
+/// will be used to perform the transformation.
+/// Notice that the macro will define a template type _Name_
+/// which will have as template argument the types of the columns
+/// to be bound for the operation.
+///
+/// For example, let's assume you have:
+///
+/// \code{.cpp}
+///
+/// namespace point {
+/// DECLARE_SOA_COLUMN(X, x, float, "fX");
+/// DECLARE_SOA_COLUMN(Y, y, float, "fY");
+/// }
+///
+/// DECLARE_SOA_DYNAMIC_COLUMN(R2, r2, [](x, y) { return x*x + y+y; });
+///
+/// DECLARE_SOA_TABLE(Point, "MISC", "POINT",
+///                   X, Y, (R2<X,Y>));
+/// \endcode
+///
+#define DECLARE_SOA_DYNAMIC_COLUMN(_Name_, _Getter_, ...)                                                                                  \
+  struct _Name_##Callback {                                                                                                                \
+    static inline constexpr auto getLambda() { return __VA_ARGS__; }                                                                       \
+  };                                                                                                                                       \
+                                                                                                                                           \
+  struct _Name_##Helper {                                                                                                                  \
+    using callable_t = decltype(framework::FunctionMetadata(std::declval<decltype(_Name_##Callback::getLambda())>()));                     \
+    using return_type = typename callable_t::return_type;                                                                                  \
+  };                                                                                                                                       \
+  template <typename... Bindings>                                                                                                          \
+  struct _Name_ : o2::soa::DynamicColumn<typename _Name_##Helper::callable_t::type, _Name_<Bindings...>> {                                 \
+    using base = o2::soa::DynamicColumn<typename _Name_##Helper::callable_t::type, _Name_<Bindings...>>;                                   \
+    using helper = _Name_##Helper;                                                                                                         \
+    using callback_holder_t = _Name_##Callback;                                                                                            \
+    using callable_t = helper::callable_t;                                                                                                 \
+    using callback_t = callable_t::type;                                                                                                   \
+                                                                                                                                           \
+    _Name_(const std::shared_ptr<arrow::Table> table)                                                                                      \
+      : o2::soa::DynamicColumn<typename _Name_##Helper::callable_t::type, _Name_<Bindings...>>(callback_t(callback_holder_t::getLambda())) \
+    {                                                                                                                                      \
+    }                                                                                                                                      \
+    static constexpr const char* mLabel = #_Name_;                                                                                         \
+    using type = typename callable_t::return_type;                                                                                         \
+                                                                                                                                           \
+    template <typename... FreeArgs>                                                                                                        \
+    type const _Getter_(FreeArgs... freeArgs) const                                                                                        \
+    {                                                                                                                                      \
+      return boundGetter(std::make_index_sequence<std::tuple_size_v<decltype(boundIterators)>>{}, freeArgs...);                            \
+    }                                                                                                                                      \
+                                                                                                                                           \
+    template <size_t... Is, typename... FreeArgs>                                                                                          \
+    type const boundGetter(std::integer_sequence<size_t, Is...>&& index, FreeArgs... freeArgs) const                                       \
+    {                                                                                                                                      \
+      return base::mCallable((**std::get<Is>(boundIterators))..., freeArgs...);                                                            \
+    }                                                                                                                                      \
+                                                                                                                                           \
+    using bindings_t = typename framework::pack<Bindings...>;                                                                              \
+    std::tuple<soa::ColumnIterator<typename Bindings::type> const*...> boundIterators;                                                     \
   }
 
 #define DECLARE_SOA_TABLE(_Name_, _Origin_, _Description_, ...)        \
