82be071eb27ae08838be2dfbee7c47778d34a5be
Giulio Eulisse <giulio.eulisse@cern.ch>
2019-09-06 14:07:13 +0200

    DPL: improve helpers for function / lambda reflection

--- Framework/Foundation/include/Framework/FunctionalHelpers.h
+++ Framework/Foundation/include/Framework/FunctionalHelpers.h
@@ -10,6 +10,8 @@
 #ifndef o2_framework_FunctionalHelpers_H_INCLUDED
 #define o2_framework_FunctionalHelpers_H_INCLUDED
 
+#include <functional>
+
 namespace o2
 {
 namespace framework
@@ -22,12 +24,25 @@ struct memfun_type {
   using type = void;
 };
 
+/// Type helper to hold a parameter pack.  This is different from a tuple
+/// as there is no data associated to it.
+template <typename... Args>
+struct pack {
+};
+
+/// Type helper to hold metadata about a lambda or a class
+/// method.
 template <typename Ret, typename Class, typename... Args>
 struct memfun_type<Ret (Class::*)(Args...) const> {
   using type = std::function<Ret(Args...)>;
+  using args = pack<Args...>;
+  using return_type = Ret;
 };
 } // namespace
 
+/// Funtion From Lambda. Helper to create an std::function from a
+/// lambda and therefore being able to use the std::function type
+/// for template matching.
 /// @return an std::function from a lambda (or anything actually callable). This
 /// allows doing further template matching tricks to extract the arguments of the
 /// function.
@@ -36,6 +51,14 @@ typename memfun_type<decltype(&F::operator())>::type
   FFL(F const& func)
 {
   return func;
+}
+
+/// @return metadata associated to method or a lambda.
+template <typename F>
+memfun_type<decltype(&F::operator())>
+  FunctionMetadata(F const& func)
+{
+  return memfun_type<decltype(&F::operator())>();
 }
 
 } // namespace framework
