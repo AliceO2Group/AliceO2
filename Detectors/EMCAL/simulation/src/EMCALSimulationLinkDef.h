739feaf5a3983a6e80dd581083818d503dc7a9c0
Anders Knospe <anders.knospe@cern.ch>
2019-09-10 00:43:43 +0200

    [EMCAL-518] implement time resolution for Digits

--- Detectors/EMCAL/simulation/src/EMCALSimulationLinkDef.h
+++ Detectors/EMCAL/simulation/src/EMCALSimulationLinkDef.h
@@ -19,5 +19,10 @@
 #pragma link C++ class o2::emcal::Digitizer + ;
 #pragma link C++ class o2::emcal::DigitizerTask + ;
 #pragma link C++ class o2::emcal::SimParam + ;
+#pragma link C++ class o2::emcal::MCLabel + ;
+#pragma link C++ class o2::emcal::LabeledDigit + ;
+
+#pragma link C++ class std::vector < o2::emcal::MCLabel > +;
+#pragma link C++ class std::list < o2::emcal::LabeledDigit > +;
 
 #endif
