d6db53b0d7de390065ddc4bd8667fa332ab7869f
Felix Weiglhofer <felix.weiglhofer@gmail.com>
2019-09-15 18:41:28 +0200

    Prepend Alice license to all source files.

--- GPU/GPUTracking/gpucf/src/exe/load_label.cpp
+++ GPU/GPUTracking/gpucf/src/exe/load_label.cpp
@@ -1,3 +1,12 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
 #include <gpucf/common/LabelContainer.h>
 #include <gpucf/common/log.h>
 #include <gpucf/common/serialization.h>
