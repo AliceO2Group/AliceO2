0e049974f747fe78eab8074bfbf356b60659492c
David Rohr <drohr@jwdt.org>
2019-09-26 17:36:36 +0200

    GPU: Remove several #ifdefs, which are no longer needed since CUDA/OpenCL2/HIP builds are the same

--- GPU/GPUTracking/DataCompression/GPUTPCCompressionKernels.h
+++ GPU/GPUTracking/DataCompression/GPUTPCCompressionKernels.h
@@ -33,7 +33,6 @@ class GPUTPCCompressionKernels : public GPUKernelTemplate
  public:
   GPUhdi() static GPUDataTypes::RecoStep GetRecoStep() { return GPUDataTypes::RecoStep::TPCCompression; }
 
-#if defined(GPUCA_BUILD_TPCCOMPRESSION) && !defined(GPUCA_ALIROOT_LIB)
   struct GPUTPCSharedMemory {
 #if !defined(GPUCA_GPUCODE)
     GPUTPCSharedMemory() : nCount(0)
@@ -45,7 +44,6 @@ class GPUTPCCompressionKernels : public GPUKernelTemplate
 
   template <int iKernel = 0>
   GPUd() static void Thread(int nBlocks, int nThreads, int iBlock, int iThread, GPUsharedref() GPUTPCSharedMemory& smem, processorType& processors);
-#endif
 
  public:
   template <int I>
