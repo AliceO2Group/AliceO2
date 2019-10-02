735d1215a48f3fa33e799cf316a34cd1adce9b13
dstocco <diego.stocco@cern.ch>
2019-10-02 14:12:06 +0200

    Create dedicated data reconstruction workflow

--- Detectors/MUON/MID/Workflow/include/MIDWorkflow/RawReaderSpec.h
+++ Detectors/MUON/MID/Workflow/include/MIDWorkflow/RawReaderSpec.h
@@ -8,22 +8,22 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-/// \file   MIDWorkflow/RecoWorkflow.h
-/// \brief  Definition of the reconstruction workflow for MID
+/// \file   MIDWorkflow/RawReaderSpec.h
+/// \brief  Data processor specs for MID digits reader device
 /// \author Diego Stocco <Diego.Stocco at cern.ch>
 /// \date   11 April 2019
 
-#ifndef O2_MID_RECOWORKFLOWSPEC_H
-#define O2_MID_RECOWORKFLOWSPEC_H
+#ifndef O2_MID_RAWREADERSPEC_H
+#define O2_MID_RAWREADERSPEC_H
 
-#include "Framework/WorkflowSpec.h"
+#include "Framework/DataProcessorSpec.h"
 
 namespace o2
 {
 namespace mid
 {
-framework::WorkflowSpec getRecoWorkflow();
+framework::DataProcessorSpec getRawReaderSpec();
 }
 } // namespace o2
 
-#endif //O2_MID_RECOWORKFLOWSPEC_H
+#endif //O2_MID_RAWREADERSPEC_H
