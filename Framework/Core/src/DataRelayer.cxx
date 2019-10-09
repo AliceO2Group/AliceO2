7d6c3b8de4961f4dd9199ae374d3cdf03ead3c61
Matthias Richter <Matthias.Richter@scieq.net>
2019-10-10 00:02:01 +0200

    Enhancing error message
    The DataHeader info is added to the error message in case the data can
    not be relayed.

--- Framework/Core/src/DataRelayer.cxx
+++ Framework/Core/src/DataRelayer.cxx
@@ -24,6 +24,7 @@
 #include <Monitoring/Monitoring.h>
 
 #include <gsl/span>
+#include <string>
 
 using namespace o2::framework::data_matcher;
 using DataHeader = o2::header::DataHeader;
@@ -362,15 +363,26 @@ DataRelayer::RelayChoice
   VariableContext pristineContext;
   std::tie(input, timeslice) = getInputTimeslice(pristineContext);
 
+  auto DataHeaderInfo = [&header]() {
+    std::string error;
+    const auto* dh = o2::header::get<o2::header::DataHeader*>(header->GetData());
+    if (dh) {
+      error += dh->dataOrigin.as<std::string>() + "/" + dh->dataDescription.as<std::string>() + "/" + dh->subSpecification;
+    } else {
+      error += "invalid header";
+    }
+    return error;
+  };
+
   if (input == INVALID_INPUT) {
-    LOG(ERROR) << "Could not match incoming data to any input";
+    LOG(ERROR) << "Could not match incoming data to any input route: " << DataHeaderInfo();
     mStats.malformedInputs++;
     mStats.droppedIncomingMessages++;
     return WillNotRelay;
   }
 
   if (TimesliceId::isValid(timeslice) == false) {
-    LOG(ERROR) << "Could not determine the timeslice for input";
+    LOG(ERROR) << "Could not determine the timeslice for input: " << DataHeaderInfo();
     mStats.malformedInputs++;
     mStats.droppedIncomingMessages++;
     return WillNotRelay;
