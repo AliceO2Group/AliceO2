d0582600c4436fd9961e2531b2e708c49c591039
dstocco <dstocco@users.noreply.github.com>
2019-10-14 15:27:14 +0200

    Split detector and geometry constant parameters (#2475)
    * Split detector and geometry constant parameters
    
    Some detector constants are used everywhere, while some others are used only when geometrical information is needed.
    Splitting the two series of constants allows for a more efficient handling of the two use case.
    This also allows to move some general information outside of Geometry.h, so that one does not need to depend from Simulation just to have those values.
    Finally, in the current implementation the functions are grouped inside namespaces. This is better than implementing them as static methods of one class, as was done before.

--- Detectors/MUON/MID/Base/src/DetectorParameters.cxx
+++ Detectors/MUON/MID/Base/src/DetectorParameters.cxx
@@ -8,11 +8,11 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-/// \file   MID/Base/src/Constants.cxx
-/// \brief  Implementation of constants for MID
+/// \file   MID/Base/src/DetectorParameters.cxx
+/// \brief  Implementation of detector params for MID
 /// \author Diego Stocco <Diego.Stocco at cern.ch>
 /// \date   08 March 2018
-#include "MIDBase/Constants.h"
+#include "MIDBase/DetectorParameters.h"
 
 #include <stdexcept>
 #include <sstream>
@@ -22,29 +22,38 @@ namespace o2
 {
 namespace mid
 {
-constexpr std::array<const double, 4> Constants::sScaleFactors;
-constexpr std::array<const double, 4> Constants::sDefaultChamberZ;
-
-void Constants::assertDEId(int deId)
+namespace detparams
+{
+void assertDEId(int deId)
 {
   /// Checks if the detection element ID is valid
-  if (deId < 0 || deId > sNDetectionElements) {
+  if (deId < 0 || deId > NDetectionElements) {
     throw std::out_of_range("Detection element ID must be between 0 and 72");
   }
 }
 
-//_____________________________________________________________________________
-std::string Constants::getDEName(int deId)
+int getDEId(bool isRight, int chamber, int rpc)
+{
+  /// Gets detection element Id
+  /// \param isRight RPC is in right side
+  /// \param chamber The chamber ID (0-3)
+  /// \param rpc RPC ID (0-8)
+  int deOffset = (isRight) ? 0 : NDetectionElementsPerSide;
+  return deOffset + NRPCLines * chamber + rpc;
+}
+
+std::string getDEName(int deId)
 {
   /// Gets the detection element name from its ID
-  /// @param deId The detection element ID
+  /// \param deId The detection element ID
   int chId = getChamber(deId);
   int stId = 1 + chId / 2;
   int planeId = 1 + chId % 2;
   std::stringstream deName;
-  deName << "MT" << stId << planeId << ((deId / 36 == 0) ? "In" : "Out") << (deId % 9) + 1;
+  deName << "MT" << stId << planeId << ((deId / NDetectionElementsPerSide == 0) ? "In" : "Out") << (deId % NRPCLines) + 1;
   return deName.str();
 }
 
+} // namespace detparams
 } // namespace mid
 } // namespace o2
