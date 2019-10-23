8c6caac674599344dd512ef58ef4b4e6016adc2d
David Rohr <drohr@jwdt.org>
2019-10-23 14:47:09 +0200

    GPU: Use ugly macro wrapper for reasonable kernel names (nvprof doesn't understand templates)

--- GPU/Common/GPUDefMacros.h
+++ GPU/Common/GPUDefMacros.h
@@ -9,7 +9,7 @@
 // or submit itself to any jurisdiction.
 
 /// \file GPUDefMacros.h
-/// \author David Rohr, Sergey Gorbunov
+/// \author David Rohr
 
 // clang-format off
 #ifndef GPUDEFMACROS_H
@@ -20,6 +20,18 @@
 
 #define GPUCA_M_STR_X(a) #a
 #define GPUCA_M_STR(a) GPUCA_M_STR_X(a)
+
+#define GPUCA_M_CAT_A(a, b) a ## b
+#define GPUCA_M_CAT(...) GPUCA_M_CAT_A(__VA_ARGS__)
+#define GPUCA_M_CAT3_A(a, b, c) a ## b ## c
+#define GPUCA_M_CAT3(...) GPUCA_M_CAT3_A(__VA_ARGS__)
+
+#define GPUCA_M_FIRST_A(a, ...) a
+#define GPUCA_M_FIRST(...) GPUCA_M_FIRST_A(__VA_ARGS__)
+#define GPUCA_M_SHIFT_A(a, ...) __VA_ARGS__
+#define GPUCA_M_SHIFT(...) GPUCA_M_SHIFT(__VA_ARGS__)
+
+#define GPUCA_M_STRIP_FIRST(a) GPUCA_M_FIRST(GPUCA_M_STRIP(a))
 
 #endif
 // clang-format on
