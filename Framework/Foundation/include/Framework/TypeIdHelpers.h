2e143c0599d070de9aa4870389cf497bdec9bb74
Giulio Eulisse <giulio.eulisse@cern.ch>
2019-11-01 08:25:05 +0100

    DPL: optimize ContextRegistry lookup (#2562)
    Now it does the hash calculation at compile time and it uses fast
    hash table to do the lookup.

--- Framework/Foundation/include/Framework/TypeIdHelpers.h
+++ Framework/Foundation/include/Framework/TypeIdHelpers.h
@@ -11,20 +11,60 @@
 #ifndef O2_FRAMEWORK_TYPEIDHELPERS_H_
 #define O2_FRAMEWORK_TYPEIDHELPERS_H_
 
-#include "Framework/StringHelpers.h"
+#include <cstddef>
+#include <cstdint>
 
 namespace o2::framework
 {
-
+/// helper to get constexpr unique string and typeid from a given type
+/// Adapted from https://github.com/Manu343726/ctti
 struct TypeIdHelpers {
-  /// Return a unique id for a given type
-  /// This works just fine with GCC and CLANG,
-  /// C++20 will allow us to use:
-  ///    std::experimental::source_location::current().function_name();
-  template <typename T>
-  constexpr static uint32_t uniqueId()
+  // From https://github.com/foonathan/string_id. As usually, thanks Jonathan.
+
+  using hash_t = uint64_t;
+
+  // See http://www.isthe.com/chongo/tech/comp/fnv/#FNV-param
+  constexpr static hash_t fnv_basis = 14695981039346656037ull;
+  constexpr static hash_t fnv_prime = 1099511628211ull;
+
+  // FNV-1a 64 bit hash
+  constexpr static hash_t fnv1a_hash(size_t n, const char* str, hash_t hash = fnv_basis)
   {
-    return compile_time_hash(__PRETTY_FUNCTION__);
+    return n > 0 ? fnv1a_hash(n - 1, str + 1, (hash ^ *str) * fnv_prime) : hash;
+  }
+
+  template <size_t N>
+  constexpr static hash_t fnv1a_hash(const char (&array)[N])
+  {
+    return fnv1a_hash(N - 1, &array[0]);
+  }
+
+  struct cstring {
+    template <size_t N>
+    constexpr cstring(const char (&str)[N]) : str{&str[0]},
+                                              length{N - 1}
+    {
+    }
+
+    constexpr hash_t hash() const
+    {
+      return TypeIdHelpers::fnv1a_hash(length, str);
+    }
+
+    const char* str;
+    size_t length;
+  };
+  template <typename T>
+  constexpr static cstring typeName()
+  {
+    return {__PRETTY_FUNCTION__};
+  }
+
+  template <typename TYPE>
+  constexpr static size_t uniqueId()
+  {
+    constexpr auto h = typeName<TYPE>().hash();
+    return h;
   }
 };
 
