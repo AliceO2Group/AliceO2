17104cf725899b2965a89075699ae430c3d9588d
wiechula <Jens.Wiechula@cern.ch>
2019-11-12 13:22:42 +0100

    Extend tree dump of pad-wise calibration values
    * Make dumping of calibration values working
    * Add electronics parameters
      * sampa chip number
      * sampa channel
      * trace length
    * Add default aliases

--- Detectors/TPC/calibration/src/CalibTreeDump.cxx
+++ Detectors/TPC/calibration/src/CalibTreeDump.cxx
@@ -8,20 +8,27 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-#include <boost/format.hpp>
+#include <fmt/format.h>
+#include <cstdlib>
+#include <string_view>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <cassert>
 
 #include "TFile.h"
 #include "TTree.h"
 #include "TMath.h"
 
 #include "MathUtils/MathBase.h"
-#include "TPCBase/CalDet.h"
 #include "TPCBase/CalArray.h"
+#include "TPCBase/CalDet.h"
+#include "TPCBase/CRU.h"
+#include "TPCBase/Mapper.h"
+#include "TPCBase/Sector.h"
 
 #include "TPCCalibration/CalibTreeDump.h"
 
-using boost::format;
-using boost::str;
 using o2::math_utils::math_base::median;
 
 using namespace o2::tpc;
@@ -36,8 +43,16 @@ void CalibTreeDump::dumpToFile(const std::string filename)
   // ===| add default mappings |================================================
   addDefaultMapping(tree);
 
+  // ===| add FEE mapping |=====================================================
+  if (mAddFEEInfo) {
+    addFEEMapping(tree);
+  }
+
   // ===| fill calDet objects |=================================================
   addCalDetObjects(tree);
+
+  // ===| default aliases |=====================================================
+  setDefaultAliases(tree);
 
   file->Write();
 }
@@ -59,9 +74,9 @@ void CalibTreeDump::addDefaultMapping(TTree* tree)
   std::vector<float> lx;
   std::vector<float> ly;
   // row and pad
-  std::vector<unsigned short> row;
-  std::vector<unsigned short> pad;
-  std::vector<unsigned short> cpad;
+  std::vector<unsigned char> row;
+  std::vector<unsigned char> pad;
+  std::vector<short> cpad;
 
   // ===| add branches with default mappings |==================================
   tree->Branch("roc", &rocNumber);
@@ -69,6 +84,9 @@ void CalibTreeDump::addDefaultMapping(TTree* tree)
   tree->Branch("gy", &gy);
   tree->Branch("lx", &lx);
   tree->Branch("ly", &ly);
+  tree->Branch("row", &row);
+  tree->Branch("pad", &pad);
+  tree->Branch("cpad", &cpad);
 
   // ===| loop over readout chambers |==========================================
   for (ROC roc; !roc.looped(); ++roc) {
@@ -79,6 +97,9 @@ void CalibTreeDump::addDefaultMapping(TTree* tree)
     gy.clear();
     lx.clear();
     ly.clear();
+    row.clear();
+    pad.clear();
+    cpad.clear();
 
     // ===| loop over pad rows |================================================
     const int numberOfRows = mapper.getNumberOfRowsROC(roc);
@@ -108,91 +129,202 @@ void CalibTreeDump::addDefaultMapping(TTree* tree)
 }
 
 //______________________________________________________________________________
+void CalibTreeDump::addFEEMapping(TTree* tree)
+{
+  // loop over ROCs
+  //
+
+  // ===| mapper |==============================================================
+  const auto& mapper = Mapper::instance();
+
+  readTraceLengths();
+
+  // ===| default mapping objects |=============================================
+  // FEC
+  std::vector<unsigned char> fecInSector;
+  std::vector<unsigned char> sampaOnFEC;
+  std::vector<unsigned char> channelOnSampa;
+  // trace length
+  std::vector<float>* traceLength = nullptr;
+
+  // ===| add branches with default mappings |==================================
+  auto brFecInSector = tree->Branch("fecInSector", &fecInSector);
+  auto brSampaOnFEC = tree->Branch("sampaOnFEC", &sampaOnFEC);
+  auto brChannelOnSampa = tree->Branch("channelOnSampa", &channelOnSampa);
+  auto brTraceLength = tree->Branch("traceLength", &traceLength);
+
+  // ===| loop over readout chambers |==========================================
+  for (ROC roc; !roc.looped(); ++roc) {
+    int rocNumber = roc;
+    //tree->GetEntry(rocNumber);
+    traceLength = ((roc.rocType() == RocType::IROC) ? &mTraceLengthIROC : &mTraceLengthOROC);
+
+    // ===| clear position vectors |============================================
+    fecInSector.clear();
+    sampaOnFEC.clear();
+    channelOnSampa.clear();
+
+    const int rowOffset = (roc.rocType() == RocType::OROC) ? mapper.getPadsInIROC() : 0;
+    // ===| loop over pad rows |================================================
+    const int numberOfRows = mapper.getNumberOfRowsROC(roc);
+    for (int irow = 0; irow < numberOfRows; ++irow) {
+
+      // ===| loop over pads in row |===========================================
+      const int numberOfPadsInRow = mapper.getNumberOfPadsInRowROC(roc, irow);
+      for (int ipad = 0; ipad < numberOfPadsInRow; ++ipad) {
+        const PadROCPos padROCPos(rocNumber, irow, ipad);
+        const auto& fecInfo = mapper.getFECInfo(padROCPos);
+        const auto globalPadNumber = mapper.globalPadNumber(fecInfo);
+
+        const CRU cru(mapper.getCRU(roc.getSector(), globalPadNumber));
+
+        fecInSector.emplace_back(fecInfo.getIndex());
+        sampaOnFEC.emplace_back(fecInfo.getSampaChip());
+        channelOnSampa.emplace_back(fecInfo.getSampaChannel());
+      }
+    }
+    brFecInSector->Fill();
+    brSampaOnFEC->Fill();
+    brChannelOnSampa->Fill();
+    brTraceLength->Fill();
+  }
+}
+
+//______________________________________________________________________________
 void CalibTreeDump::addCalDetObjects(TTree* tree)
 {
-
-  // declare forwarding visitors for mean and median
-  auto visitorMean = make_forwarding_visitor<double>([](const auto& t) { return TMath::Mean(t.begin(), t.end()); });
-  auto visitorMedian = make_forwarding_visitor<double>([](const auto& t) { return TMath::Median(t.size(), t.data()); });
-
-  auto visitorMeanVector = make_forwarding_visitor<std::vector<float>>([](const auto& t) {
-    std::vector<float> values;
-    // ===| loop over ROCs and fill |===
-    for (const auto& calArray : t.getData()) {
-      auto& data = calArray.getData();
-      values.emplace_back(float(TMath::Mean(data.begin(), data.end())));
-    }
-    return values;
-  });
-
-  auto visitorMedianVector = make_forwarding_visitor<std::vector<float>>([](const auto& t) {
-    std::vector<float> values;
-    // ===| loop over ROCs and fill |===
-    for (const auto& calArray : t.getData()) {
-      auto& data = calArray.getData();
-      values.emplace_back(float(TMath::Median(data.size(), data.data())));
-    }
-    return values;
-  });
-
-  auto visitorValVector = make_forwarding_visitor<std::vector<std::vector<float>>>([](const auto& t) {
-    std::vector<std::vector<float>> values;
-    // ===| loop over ROCs and fill |===
-    for (const auto& calArray : t.getData()) {
-      values.emplace_back();
-      auto& vector = values.back();
-      auto& data = calArray.getData();
-      for (const auto& val : data)
-        vector.emplace_back(float(val));
-    }
-    return values;
-  });
-
-  auto visitorName = make_forwarding_visitor<std::string>([](const auto& t) { return t.getName(); });
-  //auto visitorData = make_forwarding_visitor<std::vector>([](const auto& t){ return t.getName();});
 
   int iter = 0;
   for (auto& calDet : mCalDetObjects) {
     // ===| branch names |===
-    //std::string name = calDet.getName();
-    std::string name = boost::apply_visitor(visitorName, calDet);
+    std::string name = calDet.getName();
 
     if (name == "PadCalibrationObject" || name.size() == 0) {
-      name = str(format("calDet_%1$02d") % iter);
+      name = fmt::format("calDet_{%02d}", iter);
     }
 
-    std::string meanName = str(format("%1%_mean") % name);
-    std::string medianName = str(format("%1%_median") % name);
+    std::string meanName = fmt::format("{}_mean", name);
+    std::string stdDevName = fmt::format("{}_stdDev", name);
+    std::string medianName = fmt::format("{}_median", name);
 
     // ===| branch variables |===
-    std::vector<float> value;
-    float mean;
-    float median;
+    std::vector<float>* data = nullptr;
+    float mean{};
+    float stdDev{};
+    float median{};
 
     // ===| branch definitions |===
-    TBranch* brData = tree->Branch(name.data(), &value);
     TBranch* brMean = tree->Branch(meanName.data(), &mean);
+    TBranch* brStdDev = tree->Branch(stdDevName.data(), &stdDev);
     TBranch* brMedian = tree->Branch(medianName.data(), &median);
-
-    auto meanVector = boost::apply_visitor(visitorMeanVector, calDet);
-    //auto medianVector = boost::apply_visitor(visitorMedianVector, calDet);
-    auto valueVector = boost::apply_visitor(visitorValVector, calDet);
+    TBranch* brData = tree->Branch(name.data(), &data);
 
     // ===| loop over ROCs and fill |===
-    //for (const auto& calArray : calDet.getData()) {
-    //auto& data = calArray.getData();
+    for (auto& calArray : calDet.getData()) {
+      // ---| set data |---
+      data = &calArray.getData();
 
-    //value.clear();
+      // ---| statistics |---
+      mean = TMath::Mean(data->begin(), data->end());
+      stdDev = TMath::StdDev(data->begin(), data->end());
+      median = TMath::Median(data->size(), data->data());
 
-    //// ---| statistics |---
-    //mean   = boost::apply_visitor(visitorMean,   data);
-    //median = boost::apply_visitor(visitorMedian, data);
+      // ---| filling |---
+      brData->Fill();
+      brMean->Fill();
+      brStdDev->Fill();
+      brMedian->Fill();
+    }
+  }
+}
 
-    //for (const auto& val : data) value.emplace_back(val);
+//______________________________________________________________________________
+void CalibTreeDump::readTraceLengths(std::string_view mappingDir)
+{
+  std::string inputDir = mappingDir.data();
+  if (!inputDir.size()) {
+    const char* aliceO2env = std::getenv("O2_ROOT");
+    if (aliceO2env)
+      inputDir = aliceO2env;
+    inputDir += "/share/Detectors/TPC/files";
+  }
 
-    //brData->Fill();
-    //brMean->Fill();
-    //brMedian->Fill();
-    //}
+  mTraceLengthIROC.reserve(Mapper::getPadsInIROC());
+  mTraceLengthOROC.reserve(Mapper::getPadsInOROC());
+  setTraceLengths(inputDir + "/LENGTH-IROC.txt", mTraceLengthIROC);
+  setTraceLengths(inputDir + "/LENGTH-OROC1.txt", mTraceLengthOROC);
+  setTraceLengths(inputDir + "/LENGTH-OROC2.txt", mTraceLengthOROC);
+  setTraceLengths(inputDir + "/LENGTH-OROC3.txt", mTraceLengthOROC);
+
+  assert(mTraceLengthIROC.size() == Mapper::getPadsInIROC());
+  assert(mTraceLengthOROC.size() == Mapper::getPadsInOROC());
+}
+
+//______________________________________________________________________________
+void CalibTreeDump::setTraceLengths(std::string_view inputFile, std::vector<float>& length)
+{
+
+  std::ifstream infile(inputFile.data(), std::ifstream::in);
+  if (!infile.is_open()) {
+    std::cout << "could not open file " << inputFile.data() << "\n";
+    exit(1);
+  }
+
+  // e.g. IROC file
+  // Col 0 -> INDEX (0 - 5279)
+  // Col 1 -> PADROW (0 - 62)
+  // Col 2 -> PAD (0 - (Np-1))
+  // Col 3 -> Connector (1 - 132)
+  // Col 4 -> Pin (1 - 40)
+  // Col 5 -> Trace length (mm)
+  // Col 6 -> Number of vias
+
+  unsigned int index{};
+  unsigned int padrow{};
+  unsigned int pad{};
+  unsigned int connector{};
+  unsigned int pin{};
+  float traceLength{};
+  unsigned int numberOfVias{};
+
+  std::string line;
+  while (std::getline(infile, line)) {
+    std::stringstream streamLine(line);
+    streamLine >> index >> padrow >> pad >> connector >> pin >> traceLength >> numberOfVias;
+
+    traceLength /= 10.f;
+
+    length.emplace_back(traceLength);
+  }
+}
+
+//______________________________________________________________________________
+void CalibTreeDump::setDefaultAliases(TTree* tree)
+{
+  tree->SetAlias("sector", "roc%36");
+  tree->SetAlias("rowInSector", "row + (roc>35)*63");
+  tree->SetAlias("padWidth", "0.4 + (roc > 35) * 0.2");
+  tree->SetAlias("padHeight", "0.75 + (rowInSector > 62) * 0.25 + (rowInSector > 96) * 0.2 + (rowInSector > 126) * 0.3");
+  tree->SetAlias("padArea", "padWidth * padHeight");
+
+  tree->SetAlias("cruInSector", "(rowInSector >= 17) + (rowInSector >= 32) + (rowInSector >= 48) + (rowInSector >= 63) + (rowInSector >= 81) + (rowInSector >= 97) + (rowInSector >= 113) + (rowInSector >= 127) + (rowInSector >= 140)");
+  tree->SetAlias("cruID", "cruInSector + sector*10");
+  tree->SetAlias("region", "cruInSector");
+  tree->SetAlias("partition", "int(cruInSector / 2)");
+
+  tree->SetAlias("IROC", "roc < 36");
+  tree->SetAlias("OROC", "roc >= 36");
+  tree->SetAlias("OROC1", "partition == 2");
+  tree->SetAlias("OROC2", "partition == 3");
+  tree->SetAlias("OROC3", "partition == 4");
+
+  tree->SetAlias("A_Side", "sector < 36");
+  tree->SetAlias("C_Side", "sector >= 36");
+
+  if (mAddFEEInfo) {
+    tree->SetAlias("fecID", "fecInSector + sector * 91");
+    tree->SetAlias("sampaInSector", "sampaOnFEC + 5 * fecInSector");
+    tree->SetAlias("channelOnFEC", "channelOnSampa + 32 * sampaOnFEC");
+    tree->SetAlias("channelInSector", "channelOnFEC + 160 * fecInSector");
   }
 }
