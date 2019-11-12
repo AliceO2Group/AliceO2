ff623008df0364e48e8d2c603d14f59c6ab3f623
Felix Weiglhofer <felix.weiglhofer@gmail.com>
2019-11-12 19:23:43 +0100

    Move ClusterAccumulator into its own file.

---
+++ GPU/GPUTracking/TPCClusterFinder/CfUtils.h
@@ -0,0 +1,34 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file CfUtils.h
+/// \author Felix Weiglhofer
+
+#ifndef O2_GPU_CF_UTILS_H
+#define O2_GPU_CF_UTILS_H
+
+#include "GPUDef.h"
+#include "cl/clusterFinderDefs.h"
+
+namespace GPUCA_NAMESPACE
+{
+namespace gpu
+{
+
+// TODO wrap these function in a CfUtils class
+GPUdi() bool isAtEdge(const deprecated::Digit* d)
+{
+  return (d->pad < 2 || d->pad >= TPC_PADS_PER_ROW - 2);
+}
+
+} // namespace gpu
+} // namespace GPUCA_NAMESPACE
+
+#endif
