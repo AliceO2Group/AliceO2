7d9a1d8af5603b40e1f62cf753fe1daeefc3ead4
Felix Weiglhofer <felix.weiglhofer@gmail.com>
2019-11-13 13:23:57 +0100

    Move cluster finder constants into CfConsts.h header.

--- GPU/GPUTracking/TPCClusterFinder/GPUTPCClusterFinderKernels.cxx
+++ GPU/GPUTracking/TPCClusterFinder/GPUTPCClusterFinderKernels.cxx
@@ -18,6 +18,7 @@
 #include "GPUTPCClusterFinder.h"
 #include "Array2D.h"
 #include "PackedCharge.h"
+#include "CfConsts.h"
 #include "CfUtils.h"
 #include "ClusterAccumulator.h"
 #include "cl/clusterFinderDefs.h"
