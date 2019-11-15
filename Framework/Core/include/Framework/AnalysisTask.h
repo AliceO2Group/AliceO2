b1f33b3776ed0e4795b5aac8bf4d2a40d2436dc4
Anton Alkin <anton.alkin@cern.ch>
2019-11-15 07:36:26 +0100

    Mysterious extra whitespace in license bit

--- Framework/Core/include/Framework/AnalysisTask.h
+++ Framework/Core/include/Framework/AnalysisTask.h
@@ -1,4 +1,4 @@
-﻿// Copyright CERN and copyright holders of ALICE O2. This software is
+// Copyright CERN and copyright holders of ALICE O2. This software is
 // distributed under the terms of the GNU General Public License v3 (GPL
 // Version 3), copied verbatim in the file "COPYING".
 //
@@ -7,6 +7,7 @@
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
+
 #ifndef FRAMEWORK_ANALYSIS_TASK_H_
 #define FRAMEWORK_ANALYSIS_TASK_H_
 
