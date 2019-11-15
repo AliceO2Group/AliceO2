3e044610c1bcd3f91d5ac1f6f6bbd84472e93458
Giulio Eulisse <giulio.eulisse@cern.ch>
2019-11-15 13:05:15 +0100

    Framework: add support for concatenated tables

--- Framework/Core/src/ASoA.cxx
+++ Framework/Core/src/ASoA.cxx
@@ -9,9 +9,11 @@
 // or submit itself to any jurisdiction.
 
 #include "Framework/ASoA.h"
+#include "ArrowDebugHelpers.h"
 
 namespace o2::soa
 {
+
 std::shared_ptr<arrow::Table> ArrowHelpers::joinTables(std::vector<std::shared_ptr<arrow::Table>>&& tables)
 {
   std::vector<std::shared_ptr<arrow::Column>> columns;
@@ -25,4 +27,42 @@ std::shared_ptr<arrow::Table> ArrowHelpers::joinTables(std::vector<std::shared_p
   }
   return arrow::Table::Make(std::make_shared<arrow::Schema>(fields), columns);
 }
+
+std::shared_ptr<arrow::Table> ArrowHelpers::concatTables(std::vector<std::shared_ptr<arrow::Table>>&& tables)
+{
+  std::vector<std::shared_ptr<arrow::Column>> columns;
+  assert(tables.size() > 1);
+  std::vector<std::shared_ptr<arrow::Field>> resultFields = tables[0]->schema()->fields();
+  auto compareFields = [](std::shared_ptr<arrow::Field> const& f1, std::shared_ptr<arrow::Field> const& f2) {
+    // Let's do this with stable sorting.
+    return (!f1->Equals(f2)) && (f1->name() < f2->name());
+  };
+  for (size_t i = 1; i < tables.size(); ++i) {
+    auto& fields = tables[i]->schema()->fields();
+    std::vector<std::shared_ptr<arrow::Field>> intersection;
+
+    std::set_intersection(resultFields.begin(), resultFields.end(),
+                          fields.begin(), fields.end(),
+                          std::back_inserter(intersection), compareFields);
+    resultFields.swap(intersection);
+  }
+
+  for (auto& field : resultFields) {
+    arrow::ArrayVector chunks;
+    for (auto& table : tables) {
+      auto ci = table->schema()->GetFieldIndex(field->name());
+      if (ci == -1) {
+        throw std::runtime_error("Unable to find field " + field->name());
+      }
+      auto column = table->column(ci);
+      auto otherChunks = column->data()->chunks();
+      chunks.insert(chunks.end(), otherChunks.begin(), otherChunks.end());
+    }
+    columns.push_back(std::make_shared<arrow::Column>(field, chunks));
+  }
+
+  auto result = arrow::Table::Make(std::make_shared<arrow::Schema>(resultFields), columns);
+  return result;
+}
+
 } // namespace o2::soa
