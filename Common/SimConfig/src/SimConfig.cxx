6ad473a7ac0a6c1e36e94aedfa04cb126e258600
Sandro Wenzel <sandro.wenzel@cern.ch>
2019-11-21 14:44:49 +0100

    Simulation: Set default sub-event size to 500
    This value should ensure better "default" load balancing.
    Ok to use since we now have better behaving IO.

--- Common/SimConfig/src/SimConfig.cxx
+++ Common/SimConfig/src/SimConfig.cxx
@@ -45,7 +45,7 @@ void SimConfig::initOptions(boost::program_options::options_description& options
     "logverbosity", bpo::value<std::string>()->default_value("low"), "level of verbosity for FairLogger (low, medium, high, veryhigh)")(
     "configKeyValues", bpo::value<std::string>()->default_value(""), "semicolon separated key=value strings (e.g.: 'TPC.gasDensity=1;...")(
     "configFile", bpo::value<std::string>()->default_value(""), "Path to an INI or JSON configuration file")(
-    "chunkSize", bpo::value<unsigned int>()->default_value(5000), "max size of primary chunk (subevent) distributed by server")(
+    "chunkSize", bpo::value<unsigned int>()->default_value(500), "max size of primary chunk (subevent) distributed by server")(
     "chunkSizeI", bpo::value<int>()->default_value(-1), "internalChunkSize")(
     "seed", bpo::value<int>()->default_value(-1), "initial seed (default: -1 random)")(
     "field", bpo::value<int>()->default_value(-5), "L3 field rounded to kGauss, allowed values +-2,+-5 and 0")(
