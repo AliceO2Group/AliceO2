d6e18cc8515f2820f4a9767089596c63632084f2
shahoian <ruben.shahoyan@cern.ch>
2019-11-22 10:31:52 +0100

    Rewritten digitization code

--- Common/MathUtils/include/MathUtils/Utils.h
+++ Common/MathUtils/include/MathUtils/Utils.h
@@ -110,6 +110,15 @@ inline float Angle2Alpha(float phi)
 }
 
 //-------------------------------------->>>
+// fast bit count
+inline int numberOfBitsSet(uint32_t x)
+{
+  // count number of non-0 bits in 32bit word
+  x = x - ((x >> 1) & 0x55555555);
+  x = (x & 0x33333333) + ((x >> 2) & 0x33333333);
+  return (((x + (x >> 4)) & 0x0F0F0F0F) * 0x01010101) >> 24;
+}
+
 // recursive creation of bitmask
 template <typename T>
 constexpr int bit2Mask(T v)
