97437cb900b035afce0e8e254be6e02023266de7
Anton Alkin <anton.alkin@cern.ch>
2019-12-01 13:58:43 +0100

    DPL Analysis: more options for associated table
    A template is_base_of_template is added to allow Join<> and
    Filtered<> as an associated table that are template classes,
    inheriting from o2::soa::Table

--- Framework/AnalysisTutorial/src/associatedExample.cxx
+++ Framework/AnalysisTutorial/src/associatedExample.cxx
@@ -15,11 +15,14 @@ namespace o2::aod
 {
 namespace etaphi
 {
-DECLARE_SOA_COLUMN(Eta, eta, float, "fEta");
-DECLARE_SOA_COLUMN(Phi, phi, float, "fPhi");
+DECLARE_SOA_COLUMN(Eta, etas, float, "fEta");
+DECLARE_SOA_COLUMN(Phi, phis, float, "fPhi");
+DECLARE_SOA_COLUMN(Pt, pts, float, "fPt");
 } // namespace etaphi
 DECLARE_SOA_TABLE(EtaPhi, "AOD", "ETAPHI",
                   etaphi::Eta, etaphi::Phi);
+DECLARE_SOA_TABLE(EtaPhiPt, "AOD", "ETAPHIPT",
+                  etaphi::Eta, etaphi::Phi, etaphi::Pt);
 } // namespace o2::aod
 
 using namespace o2;
@@ -31,24 +34,36 @@ using namespace o2::framework;
 //        we need GCC 7.4+ for that
 struct ATask {
   Produces<aod::EtaPhi> etaphi;
+  Produces<aod::EtaPhiPt> etaphipt;
 
   void process(aod::Tracks const& tracks)
   {
     for (auto& track : tracks) {
       float phi = asin(track.snp()) + track.alpha() + static_cast<float>(M_PI);
       float eta = log(tan(0.25f * static_cast<float>(M_PI) - 0.5f * atan(track.tgl())));
-
-      etaphi(phi, eta);
+      float pt = 1.f / track.signed1Pt();
+      etaphi(eta, phi);
+      etaphipt(eta, phi, pt);
     }
   }
 };
 
 struct BTask {
-  void process(aod::EtaPhi const& etaPhis)
+  void process(aod::Collision const& collision, soa::Join<aod::Tracks, aod::EtaPhi> const& extTracks)
   {
-    for (auto& etaPhi : etaPhis) {
-      LOGF(INFO, "(%f, %f)", etaPhi.eta(), etaPhi.phi());
+    LOG(INFO) << "TF: " << collision.timeframeID() << std::endl;
+    LOG(INFO) << "Rows: " << extTracks.asArrowTable()->num_rows() << std::endl;
+    for (auto& track : extTracks) {
+      LOGF(INFO, "(%f, %f) - (%f, %f)", track.eta(), track.phi(), track.etas(), track.phis());
     }
+  }
+};
+
+struct CTask {
+  void process(aod::Collision const& collision, soa::Concat<aod::EtaPhi, aod::EtaPhiPt> const& concatenated)
+  {
+    LOG(INFO) << "TF: " << collision.timeframeID() << std::endl;
+    LOG(INFO) << "Rows: " << concatenated.asArrowTable()->num_rows() << std::endl;
   }
 };
 
@@ -56,5 +71,6 @@ WorkflowSpec defineDataProcessing(ConfigContext const&)
 {
   return WorkflowSpec{
     adaptAnalysisTask<ATask>("produce-etaphi"),
-    adaptAnalysisTask<BTask>("consume-etaphi")};
+    adaptAnalysisTask<BTask>("consume-etaphi"),
+    adaptAnalysisTask<CTask>("consume-etaphi-twice")};
 }
