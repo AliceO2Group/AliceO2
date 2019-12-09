dc249052e0594b4c9a71d0d17cc748501dd154dc
Hadi Hassan <hadi.hassan@cern.ch>
2019-12-09 14:50:18 +0100

    Removing the all the TObjectWrapper from the EMCALCalib

--- Detectors/EMCAL/calib/macros/TempCalibParamSM_CalibDBTest.C
+++ Detectors/EMCAL/calib/macros/TempCalibParamSM_CalibDBTest.C
@@ -51,7 +51,7 @@ void TempCalibParamSM_CalibDBTest(const std::string_view ccdbserver = "emcccdb-t
   std::string inputDir = " ";
   if (aliceO2env)
     inputDir = aliceO2env;
-  inputDir += "/share/Detectors/EMCAL/files/";
+  inputDir += "/share/Detectors/EMC/files/";
 
   std::string file = inputDir + "TempCalibSM_LHC18k_289166.txt";
   std::ifstream fileTempCalibSM(file, std::ifstream::in);
