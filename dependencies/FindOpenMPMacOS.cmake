c9b1afc765ab643d3c5b8bd875993d9ecf55ce51
David Rohr <drohr@jwdt.org>
2020-01-01 12:39:36 +0100

    Use MacOS FindOpenMP workaround only on MacOS, and only when normal CMake FindOpenMP doesn't work

--- dependencies/FindOpenMPMacOS.cmake
+++ dependencies/FindOpenMPMacOS.cmake
@@ -9,7 +9,7 @@ find_path(OpenMP_INCLUDE_DIR
 mark_as_advanced(OpenMP_LIBRARY OpenMP_INCLUDE_DIR)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(OpenMP DEFAULT_MSG 
+find_package_handle_standard_args(OpenMP DEFAULT_MSG
     OpenMP_LIBRARY OpenMP_INCLUDE_DIR)
 
 if (OpenMP_FOUND)
@@ -17,8 +17,10 @@ if (OpenMP_FOUND)
     set(OpenMP_INCLUDE_DIRS ${OpenMP_INCLUDE_DIR})
     set(OpenMP_COMPILE_OPTIONS -Xpreprocessor -fopenmp)
 
-    add_library(OpenMP::OpenMP SHARED IMPORTED)
-    set_target_properties(OpenMP::OpenMP PROPERTIES
+    set(OpenMP_CXX_FOUND True)
+    set(OpenMPMacOS_FOUND True)
+    add_library(OpenMP::OpenMP_CXX SHARED IMPORTED)
+    set_target_properties(OpenMP::OpenMP_CXX PROPERTIES
         IMPORTED_LOCATION ${OpenMP_LIBRARIES}
         INTERFACE_INCLUDE_DIRECTORIES "${OpenMP_INCLUDE_DIRS}"
         INTERFACE_COMPILE_OPTIONS "${OpenMP_COMPILE_OPTIONS}"
