334c93b2be94782d2b9ada08199aff4c2ab1c3cd
Michael Lettrich <michael.lettrich@cern.ch>
2020-01-08 13:23:21 +0100

    sorting with thrust for AMD HIP + unittests

--- GPU/Common/GPUCommonAlgorithm.h
+++ GPU/Common/GPUCommonAlgorithm.h
@@ -199,8 +199,8 @@ typedef GPUCommonAlgorithm CAAlgo;
 } // namespace gpu
 } // namespace GPUCA_NAMESPACE
 
-#ifdef __CUDACC__
-#include "GPUCommonAlgorithmCUDA.cuh"
+#if defined(__CUDACC__) || defined(__HIPCC__)
+#include "GPUCommonAlgorithmThrust.h"
 
 #else
 
