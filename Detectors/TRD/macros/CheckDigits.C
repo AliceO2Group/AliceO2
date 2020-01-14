21fa4c6a92f14fc4e3d71bbeaf14872905be01a7
Jorge Lopez <jorgext@gmail.com>
2020-01-14 07:51:08 -0300

    TRD: Fixed ADC spectrum (#2776)
    * improving trd digitizer
    
    * changed the signal type to float instead of int
    
    * removed map to vector conversion (buggy part) - output correct ADC spectrum
    
    * comment out ccdb failing method

--- Detectors/TRD/macros/CheckDigits.C
+++ Detectors/TRD/macros/CheckDigits.C
@@ -19,24 +19,32 @@
 
 #include "FairLogger.h"
 #include "TRDBase/Digit.h"
+#include "TRDBase/TRDSimParam.h"
+#include "TRDBase/TRDCommonParam.h"
 #endif
+
+using namespace o2::trd;
+
+constexpr int kMINENTRIES = 1000;
 
 void CheckDigits(std::string digifile = "trddigits.root",
                  std::string hitfile = "o2sim.root",
                  std::string inputGeom = "O2geometry.root",
                  std::string paramfile = "o2sim_par.root")
 {
-  using o2::trd::Digit;
   TFile* fin = TFile::Open(digifile.data());
   TTree* digitTree = (TTree*)fin->Get("o2sim");
-  std::vector<o2::trd::Digit>* digitCont = nullptr;
+  std::vector<Digit>* digitCont = nullptr;
   digitTree->SetBranchAddress("TRDDigit", &digitCont);
   int nev = digitTree->GetEntries();
 
   TH1F* hDet = new TH1F("hDet", ";Detector number;Counts", 504, 0, 539);
   TH1F* hRow = new TH1F("hRow", ";Row number;Counts", 16, 0, 15);
   TH1F* hPad = new TH1F("hPad", ";Pad number;Counts", 144, 0, 143);
-  TH1* hADC = new TH1F("hADC", ";ADC value;Counts", 300, 0, 300);
+  TH1* hADC[540];
+  for (int d = 0; d < 540; ++d) {
+    hADC[d] = new TH1F(Form("hADC_%d", d), ";ADC value;Counts", 1024, 0, 1023);
+  }
 
   LOG(INFO) << nev << " entries found";
   for (int iev = 0; iev < nev; ++iev) {
@@ -50,11 +58,13 @@ void CheckDigits(std::string digifile = "trddigits.root",
       hDet->Fill(det);
       hRow->Fill(row);
       hPad->Fill(pad);
-      int tb = 0;
-      for (const auto& adc : adcs) {
-        if (++tb > 30)
-          break;
-        hADC->Fill(adc);
+      for (int tb = 0; tb < kTimeBins; ++tb) {
+        ADC_t adc = adcs[tb];
+        if (adc == (ADC_t)TRDSimParam::Instance()->GetADCoutRange()) {
+          // LOG(INFO) << "Out of range ADC " << adc;
+          continue;
+        }
+        hADC[det]->Fill(adc);
       }
     }
   }
@@ -68,6 +78,63 @@ void CheckDigits(std::string digifile = "trddigits.root",
   hPad->Draw();
   c->cd(4);
   c->cd(4)->SetLogy();
-  hADC->Draw();
+  int first = 0;
+  int count = 0;
+  int max = 0;
+  for (int d = 1; d < 540; ++d) {
+    if (hADC[d]->GetEntries() < kMINENTRIES) {
+      continue;
+    }
+    if (count > 6) {
+      break;
+    }
+    hADC[d]->SetLineColor(count + 1);
+    if (count == 0) {
+      hADC[d]->Draw();
+      first = d;
+      if (max < hADC[d]->GetMaximum()) {
+        max = hADC[d]->GetMaximum();
+      }
+      count++;
+    } else {
+      hADC[d]->Draw("SAME");
+      if (max < hADC[d]->GetMaximum()) {
+        max = hADC[d]->GetMaximum();
+      }
+      count++;
+    }
+  }
+  hADC[first]->GetYaxis()->SetLimits(1.1, max + 100);
   c->SaveAs("testCheckDigits.pdf");
+
+  TCanvas* c1 = new TCanvas("c1", "trd digits analysis", 600, 600);
+  first = 0;
+  count = 0;
+  max = 0;
+  for (int d = 1; d < 540; ++d) {
+    if (hADC[d]->GetEntries() < kMINENTRIES) {
+      continue;
+    }
+    if (count > 6) {
+      break;
+    }
+    hADC[d]->SetLineColor(count + 1);
+    if (count == 0) {
+      hADC[d]->Draw();
+      first = d;
+      if (max < hADC[d]->GetMaximum()) {
+        max = hADC[d]->GetMaximum();
+      }
+      count++;
+    } else {
+      hADC[d]->Draw("SAME");
+      if (max < hADC[d]->GetMaximum()) {
+        max = hADC[d]->GetMaximum();
+      }
+      count++;
+    }
+  }
+  hADC[first]->GetYaxis()->SetLimits(1.1, max + 100);
+  c1->SetLogy();
+  c1->SaveAs("testCheckDigits_ADCs.pdf");
 }
