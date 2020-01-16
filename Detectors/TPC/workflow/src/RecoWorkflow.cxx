6fe3ecb643055e934f96391bfa47ca3671056dac
Matthias Richter <Matthias.Richter@scieq.net>
2020-01-16 14:58:11 +0100

    Rework of the TPC reco workflow to support compressed clusters
    New output types added
     - compressed-clusters
     - encoded-clusters
    
    - integrating the handling of compressed clusters object produced by
      the CA track, data is sent out with spec TPC/COMPCLUSTERS/0
    - adding a skeleton for the TPC cluster encoder

--- Detectors/TPC/workflow/src/RecoWorkflow.cxx
+++ Detectors/TPC/workflow/src/RecoWorkflow.cxx
@@ -15,22 +15,24 @@
 
 #include "Framework/WorkflowSpec.h"
 #include "Framework/DataSpecUtils.h"
+#include "Framework/Logger.h"
 #include "DPLUtils/MakeRootTreeWriterSpec.h"
 #include "TPCWorkflow/RecoWorkflow.h"
 #include "TPCWorkflow/PublisherSpec.h"
 #include "TPCWorkflow/ClustererSpec.h"
 #include "TPCWorkflow/ClusterDecoderRawSpec.h"
 #include "TPCWorkflow/CATrackerSpec.h"
+#include "TPCWorkflow/EntropyEncoderSpec.h"
 #include "Algorithm/RangeTokenizer.h"
 #include "TPCBase/Digit.h"
 #include "DataFormatsTPC/Constants.h"
 #include "DataFormatsTPC/ClusterGroupAttribute.h"
 #include "DataFormatsTPC/TrackTPC.h"
 #include "DataFormatsTPC/TPCSectorHeader.h"
+#include "DataFormatsTPC/CompressedClusters.h"
 #include "SimulationDataFormat/MCTruthContainer.h"
 #include "SimulationDataFormat/MCCompLabel.h"
 
-#include "FairMQLogger.h"
 #include <string>
 #include <stdexcept>
 #include <fstream>
@@ -59,6 +61,8 @@ const std::unordered_map<std::string, InputType> InputMap{
   {"clustershardware", InputType::ClustersHardware},
   {"clusters", InputType::Clusters},
   {"zsraw", InputType::ZSRaw},
+  {"compressed-clusters", InputType::CompClusters},
+  {"encoded-clusters", InputType::EncodedClusters},
 };
 
 const std::unordered_map<std::string, OutputType> OutputMap{
@@ -66,6 +70,8 @@ const std::unordered_map<std::string, OutputType> OutputMap{
   {"clustershardware", OutputType::ClustersHardware},
   {"clusters", OutputType::Clusters},
   {"tracks", OutputType::Tracks},
+  {"compressed-clusters", OutputType::CompClusters},
+  {"encoded-clusters", OutputType::EncodedClusters},
   {"disable-writer", OutputType::DisableWriter},
 };
 
@@ -153,18 +159,53 @@ framework::WorkflowSpec getWorkflow(std::vector<int> const& tpcSectors, std::vec
                                                    laneConfiguration,
                                                  },
                                                  propagateMC));
+  } else if (inputType == InputType::CompClusters) {
+    // TODO: need to check if we want to store the MC labels alongside with compressed clusters
+    // for the moment reading of labels is disabled (last parameter is false)
+    // TODO: make a different publisher spec for only one output spec, for now using the
+    // PublisherSpec with only sector 0, '_0' is thus appended to the branch name
+    specs.emplace_back(o2::tpc::getPublisherSpec(PublisherConf{
+                                                   "tpc-compressed-cluster-reader",
+                                                   "tpcrec",
+                                                   {"clusterbranch", "TPCCompClusters", "Branch with TPC compressed clusters"},
+                                                   {"clustermcbranch", "TPCClusterNativeMCTruth", "MC label branch"},
+                                                   OutputSpec{"TPC", "COMPCLUSTERS"},
+                                                   OutputSpec{"TPC", "CLNATIVEMCLBL"},
+                                                   std::vector<int>(1, 0),
+                                                   std::vector<int>(1, 0),
+                                                 },
+                                                 false));
+  } else if (inputType == InputType::EncodedClusters) {
+    // TODO: need to check if we want to store the MC labels alongside with encoded clusters
+    // for the moment reading of labels is disabled (last parameter is false)
+    specs.emplace_back(o2::tpc::getPublisherSpec(PublisherConf{
+                                                   "tpc-encoded-cluster-reader",
+                                                   "tpcrec",
+                                                   {"clusterbranch", "TPCEncodedClusters", "Branch with TPC encoded clusters"},
+                                                   {"clustermcbranch", "TPCClusterNativeMCTruth", "MC label branch"},
+                                                   OutputSpec{"TPC", "ENCCLUSTERS"},
+                                                   OutputSpec{"TPC", "CLNATIVEMCLBL"},
+                                                   std::vector<int>(1, 0),
+                                                   std::vector<int>(1, 0),
+                                                 },
+                                                 false));
   }
 
   // output matrix
-  bool runTracker = isEnabled(OutputType::Tracks);
-  bool runDecoder = !caClusterer && (runTracker || isEnabled(OutputType::Clusters));
-  bool runClusterer = !caClusterer && (runDecoder || isEnabled(OutputType::ClustersHardware));
+  // Note: the ClusterHardware format is probably a deprecated legacy format and also the
+  // ClusterDecoderRawSpec
+  bool produceCompClusters = isEnabled(OutputType::CompClusters) || isEnabled(OutputType::EncodedClusters);
+  bool produceTracks = isEnabled(OutputType::Tracks);
+  bool runTracker = produceTracks || produceCompClusters;
+  bool runHWDecoder = !caClusterer && (runTracker || isEnabled(OutputType::Clusters));
+  bool runClusterer = !caClusterer && (runHWDecoder || isEnabled(OutputType::ClustersHardware));
   bool zsDecoder = inputType == InputType::ZSRaw;
+  bool runClusterEncoder = isEnabled(OutputType::EncodedClusters);
 
   // input matrix
   runClusterer &= inputType == InputType::Digitizer || inputType == InputType::Digits;
-  runDecoder &= runClusterer || inputType == InputType::ClustersHardware;
-  runTracker &= caClusterer || (runDecoder || inputType == InputType::Clusters);
+  runHWDecoder &= runClusterer || inputType == InputType::ClustersHardware;
+  runTracker &= caClusterer || (runHWDecoder || inputType == InputType::Clusters);
 
   WorkflowSpec parallelProcessors;
   //////////////////////////////////////////////////////////////////////////////////////////////
@@ -181,7 +222,7 @@ framework::WorkflowSpec getWorkflow(std::vector<int> const& tpcSectors, std::vec
   // cluster decoder process(es)
   //
   //
-  if (runDecoder) {
+  if (runHWDecoder) {
     parallelProcessors.push_back(o2::tpc::getClusterDecoderRawSpec(propagateMC));
   }
 
@@ -347,7 +388,23 @@ framework::WorkflowSpec getWorkflow(std::vector<int> const& tpcSectors, std::vec
   //
   // selected by output type 'tracks'
   if (runTracker) {
-    specs.emplace_back(o2::tpc::getCATrackerSpec(propagateMC, caClusterer, zsDecoder, laneConfiguration));
+    specs.emplace_back(o2::tpc::getCATrackerSpec(ca::Config{
+                                                   propagateMC ? ca::Operation::ProcessMC : ca::Operation::Noop,
+                                                   caClusterer ? ca::Operation::CAClusterer : ca::Operation::Noop,
+                                                   zsDecoder ? ca::Operation::ZSDecoder : ca::Operation::Noop,
+                                                   produceTracks ? ca::Operation::OutputTracks : ca::Operation::Noop,
+                                                   produceCompClusters ? ca::Operation::OutputCompClusters : ca::Operation::Noop,
+                                                 },
+                                                 laneConfiguration));
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // tracker process
+  //
+  // selected by output type 'encoded-clusters'
+  if (runClusterEncoder) {
+    specs.emplace_back(o2::tpc::getEntropyEncoderSpec());
   }
 
   //////////////////////////////////////////////////////////////////////////////////////////////
@@ -355,7 +412,7 @@ framework::WorkflowSpec getWorkflow(std::vector<int> const& tpcSectors, std::vec
   // a writer process for tracks
   //
   // selected by output type 'tracks'
-  if (isEnabled(OutputType::Tracks) && !isEnabled(OutputType::DisableWriter)) {
+  if (produceTracks && !isEnabled(OutputType::DisableWriter)) {
     // defining the track writer process using the generic RootTreeWriter and generator tool
     //
     // defaults
@@ -390,6 +447,30 @@ framework::WorkflowSpec getWorkflow(std::vector<int> const& tpcSectors, std::vec
                                              MakeRootTreeWriterSpec::TerminationCondition{checkReady}, //
                                              std::move(tracksdef), std::move(clrefdef))());            //
     }
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // a writer process for compressed clusters container
+  //
+  // selected by output type 'compressed-clusters'
+  if (produceCompClusters && !isEnabled(OutputType::DisableWriter)) {
+    // defining the track writer process using the generic RootTreeWriter and generator tool
+    //
+    // defaults
+    const char* processName = "tpc-compcluster-writer";
+    const char* defaultFileName = "tpc-compclusters.root";
+    const char* defaultTreeName = "tpcrec";
+
+    //branch definitions for RootTreeWriter spec
+    using CCluSerializedType = ROOTSerialized<CompressedClusters>;
+    auto ccldef = BranchDefinition<CCluSerializedType>{InputSpec{"inputCompCl", "TPC", "COMPCLUSTERS"}, //
+                                                       "TPCCompClusters_0", "compcluster-branch-name"}; //
+
+    specs.push_back(MakeRootTreeWriterSpec(processName, defaultFileName, defaultTreeName,            //
+                                           MakeRootTreeWriterSpec::TerminationPolicy::Process,       //
+                                           MakeRootTreeWriterSpec::TerminationCondition{checkReady}, //
+                                           std::move(ccldef))());                                    //
   }
 
   return std::move(specs);
