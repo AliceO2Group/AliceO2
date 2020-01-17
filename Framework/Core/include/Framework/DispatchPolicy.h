14c49b15a3c504ccaed9021a7e501add3f55332b
Matthias Richter <Matthias.Richter@scieq.net>
2020-01-17 10:48:16 +0100

    DPL: Implementing trigger matcher in the DispatchPolicy
    While messages which become ready for sending during computation are scheduled,
    a trigger matcher decides when to send all scheduled messages. All messages
    are sent as multimessage. This makes sure that data sets belonging together
    will be received together on the receiver side and simplifies implementation
    of CompletionPolicy.

--- Framework/Core/include/Framework/DispatchPolicy.h
+++ Framework/Core/include/Framework/DispatchPolicy.h
@@ -41,7 +41,7 @@ struct DispatchPolicy {
 
   using DeviceMatcher = std::function<bool(DeviceSpec const& device)>;
   // OutputMatcher can be a later extension, but not expected to be of high priority
-  using OutputMatcher = std::function<bool(Output const&)>;
+  using TriggerMatcher = std::function<bool(Output const&)>;
 
   /// Name of the policy itself.
   std::string name;
@@ -50,6 +50,8 @@ struct DispatchPolicy {
   DeviceMatcher deviceMatcher;
   /// the action to be used for matched devices
   DispatchOp action = DispatchOp::AfterComputation;
+  /// matcher on specific output to trigger sending
+  TriggerMatcher triggerMatcher = [](Output const&) { return true; };
 
   /// Helper to create the default configuration.
   static std::vector<DispatchPolicy> createDefaultPolicies();
--- Framework/Core/src/DataProcessingDevice.cxx
+++ Framework/Core/src/DataProcessingDevice.cxx
@@ -14,6 +14,8 @@
 #include "Framework/DataProcessor.h"
 #include "Framework/DataSpecUtils.h"
 #include "Framework/DeviceState.h"
+#include "Framework/DispatchPolicy.h"
+#include "Framework/DispatchControl.h"
 #include "Framework/EndOfStreamContext.h"
 #include "Framework/FairOptionsRetriever.h"
 #include "Framework/FairMQDeviceProxy.h"
@@ -79,9 +81,15 @@ DataProcessingDevice::DataProcessingDevice(DeviceSpec const& spec, ServiceRegist
   auto dispatcher = [this](FairMQParts&& parts, std::string const& channel, unsigned int index) {
     DataProcessor::doSend(*this, std::move(parts), channel.c_str(), index);
   };
+  auto matcher = [policy = spec.dispatchPolicy](o2::header::DataHeader const& header) {
+    if (policy.triggerMatcher == nullptr) {
+      return true;
+    }
+    return policy.triggerMatcher(Output{header});
+  };
 
   if (spec.dispatchPolicy.action == DispatchPolicy::DispatchOp::WhenReady) {
-    mFairMQContext.init(dispatcher);
+    mFairMQContext.init(DispatchControl{dispatcher, matcher});
   }
 }
 
