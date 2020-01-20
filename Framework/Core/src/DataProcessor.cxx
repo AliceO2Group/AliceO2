a24713e29b9fa41c92a140132296b02dfa67f830
Matthias Richter <Matthias.Richter@scieq.net>
2020-01-20 10:01:33 +0100

    DPL: Optimization for associating output messages with actual channels
    This is a follow-up to commit f6d950b1
    
    1. Introducing a registry of channel names in the MessageContext
    
    The unique references of string objects from the registry allow fast access
    and association of messages to actual FairMQChannels by avoiding string
    allocation.
    
    2. Optimization to unordered_map
    
    Using pointer to channel string as key to avoid allocation of strings.
    The ContextObjects hold references of the string variables from the registry.
    The string references from the OutputRoute definition of the device do not
    fulfill the requirement to refer to one single string object per channel,
    because the device stores a vector of OutputRoutes.

--- Framework/Core/src/DataProcessor.cxx
+++ Framework/Core/src/DataProcessor.cxx
@@ -38,7 +38,7 @@ void DataProcessor::doSend(FairMQDevice& device, FairMQParts&& parts, const char
 
 void DataProcessor::doSend(FairMQDevice& device, MessageContext& context)
 {
-  std::unordered_map<std::string, FairMQParts> outputs;
+  std::unordered_map<std::string const*, FairMQParts> outputs;
   auto contextMessages = context.getMessagesForSending();
   for (auto& message : contextMessages) {
     //     monitoringService.send({ message->parts.Size(), "outputs/total" });
@@ -46,11 +46,11 @@ void DataProcessor::doSend(FairMQDevice& device, MessageContext& context)
     assert(message->empty());
     assert(parts.Size() == 2);
     for (auto& part : parts) {
-      outputs[message->channel()].AddPart(std::move(part));
+      outputs[&(message->channel())].AddPart(std::move(part));
     }
   }
   for (auto& [channel, parts] : outputs) {
-    device.Send(parts, channel, 0);
+    device.Send(parts, *channel, 0);
   }
 }
 
