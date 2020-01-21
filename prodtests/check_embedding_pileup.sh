b56e4a67011b86e313ce973d5d38d0da58d1bcb6
Sandro Wenzel <sandro.wenzel@cern.ch>
2020-01-21 17:28:34 +0100

    Fully automatic pileup and embedding feature check

--- prodtests/check_embedding_pileup.sh
+++ prodtests/check_embedding_pileup.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -x
+
 # Script verifying pileup / embedding features of digitizers
 # It is a basic and fully automatic status check of what should
 # work
@@ -15,37 +18,38 @@
 dets=(       ITS     TPC   TRD    EMC     PHS    TOF      CPV    HMP     MCH   MID   MFT   FV0   FT0   FDD   ZDC )
 generators=(boxgen boxgen boxgen boxgen hmpidgun  boxgen  boxgen  hmpidgun fwpigen fwpigen fwpigen  fddgen fddgen fddgen pythia8 )
 
+dets=(       ITS  TPC  TOF  EMC  HMP  MCH  MID  MFT  FV0  FT0  FDD)
+generators=( boxgen boxgen boxgen boxgen hmpidgun fwpigen fwpigen fwpigen fddgen fddgen fddgen)
+
 for idx in "${!dets[@]}"; do
   d=${dets[$idx]}
   gen=${generators[$idx]}
   # we put the detector plus pipe and magnet as materials
-  o2-sim-serial -m PIPE MAG ${d} -g ${gen} -n 2 --seed 1 -o o2sim${d} > simlog${d} 2>&1
+  o2-sim-serial -m PIPE MAG ${d} -g ${gen} -n 1 --seed 1 -o o2sim${d} > simlog${d} 2>&1
+
+  # we duplicate the events/hits a few times in order to have a sufficient
+  # condition for pileup
+  f=4
+  origin="o2sim${d}.root"
+  target="o2sim${d}_${f}.root"
+  root -q -b -l ${O2_ROOT}/share/macro/duplicateHits.C\(\"${origin}\",\"${target}\",${f}\)
 
   unlink o2sim.root
   unlink o2sim_grp.root
-  ln -s o2sim${d}.root o2sim.root
+  ln -s ${target} o2sim.root
   ln -s o2sim${d}_grp.root o2sim_grp.root
 
   # verify that we have hits
-  NUMHITS=`root -q -b -l ~/alisw_new/O2/macro/analyzeHits.C | grep "${d}" | awk '//{print $2}'`
-  echo "Found ${NUMHITS} hits for ${d}" 
-  
-  # digitize with extreme bunch crossing
-  o2-sim-digitizer-workflow --onlyDet ${d} -b --tpc-lanes 1 > digilog${d} 2>&1
+  NUMHITS=`root -q -b -l ${O2_ROOT}/share/macro/analyzeHits.C | grep "${d}" | awk '//{print $2}'`
+  MSG="Found ${NUMHITS} hits for ${d}"
+  echo $MSG
 
-  # verify that digit file is here
-  if [ -a ${d}digits.root ]; then
-    # verify that we have a single digit (here checked via the labels)
-    # root -q -b -l checkEmbeddingPileupFeature("DETECTOR", file);
-    echo "checking pileup for $d"
+  # digitize with extreme bunch crossing as well as with embedding the signal onto itself
+  o2-sim-digitizer-workflow --onlyDet ${d} --interactionRate 1e9 -b --tpc-lanes 1 --simFileS o2sim.root > digilog${d} 2>&1
 
-      
-    # digitize with embedding but normal bunch crossing
-    # o2-sim-digitizer-workflow --onlyDet ${d} -b --simFile o2sim.root --simFileS o2sim.root
+  # find newly created digitfile
+  digitfile=`ls -lt *digi*.root | head -n 1 | awk '//{print $9}'`
 
-    # root -q -b -l checkEmbeddingPileupFeature("DETECTOR", file);
-  else
-    # no digit file file
-    echo "NO DIGIT FILE FOUND FOR ${d}"
-  fi 
+  root -q -b -l ${O2_ROOT}/share/macro/analyzeDigitLabels.C\(\"${digitfile}\",\"${d}\"\)
+
 done
