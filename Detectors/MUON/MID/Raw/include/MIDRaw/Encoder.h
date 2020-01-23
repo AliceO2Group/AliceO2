83f844d95c09ac7114c086dd2d8c381675ce1093
dstocco <diego.stocco@cern.ch>
2020-01-23 12:01:16 +0100

    Improve encoder
    - Complete last TF
    - Correctly treat superpages in MID encoder
    - Allow to save only non-empty TFs. This is useful to limit the size when converting Run2 data

--- Detectors/MUON/MID/Raw/include/MIDRaw/Encoder.h
+++ Detectors/MUON/MID/Raw/include/MIDRaw/Encoder.h
@@ -37,22 +37,31 @@ class Encoder
  public:
   void process(gsl::span<const ColumnData> data, const InteractionRecord& ir, EventType eventType = EventType::Standard);
   const std::vector<raw::RawUnit>& getBuffer();
+  void finalize();
   /// Gets the size in bytes of the buffer
   size_t getBufferSize() { return mBytes.size() * raw::sElementSizeInBytes; }
   /// Sets the next header offset in bytes
   void setHeaderOffset(bool headerOffset = true);
   void clear();
+  /// Sets the maximum size of the superpage
+  void setMaximumSuperpageSize(unsigned long int maxSize) { mMaxSuperpageSize = maxSize; }
+  /// Sets flag to skip empty Time Frames
+  void setSkipEmptyTFs(bool skip = true) { mSkipEmptyTFs = skip; }
 
  private:
   bool convertData(gsl::span<const ColumnData> data);
   void newHeader(const InteractionRecord& ir);
+  void flushGBT(CRUUserLogicEncoder& linkEncoder);
 
   std::array<CRUUserLogicEncoder, crateparams::sNGBTs> mCRUUserLogicEncoders{}; /// Array of encoders per link
-  std::map<uint16_t, LocalBoardRO> mROData{};                                   /// Map of data per board
-  std::vector<raw::RawUnit> mBytes{};                                           /// Vector with encoded information
-  CrateMapper mCrateMapper{};                                                   /// Crate mapper
-  o2::raw::HBFUtils mHBFUtils{};                                                /// Utility for HBF
-  InteractionRecord mLastIR{};                                                  /// Last interaction record
+
+  std::map<uint16_t, LocalBoardRO> mROData{};     /// Map of data per board
+  std::vector<raw::RawUnit> mBytes{};             /// Vector with encoded information
+  CrateMapper mCrateMapper{};                     /// Crate mapper
+  o2::raw::HBFUtils mHBFUtils{};                  /// Utility for HBF
+  InteractionRecord mLastIR{};                    /// Last interaction record
+  unsigned long int mMaxSuperpageSize{0x1000000}; /// Superpage size
+  bool mSkipEmptyTFs{false};                      /// Skip empty Time Frames
 };
 } // namespace mid
 } // namespace o2
