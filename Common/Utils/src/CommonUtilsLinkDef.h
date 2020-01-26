1ac17a6776f573b909299e5f5db34eb66aceab88
shahoian <ruben.shahoyan@cern.ch>
2020-01-26 01:07:55 +0100

    Moved HBFUtils from Common/Utils to Detectors/Raw
    Classes depending on HBFUtils were fixed

--- Common/Utils/src/CommonUtilsLinkDef.h
+++ Common/Utils/src/CommonUtilsLinkDef.h
@@ -18,6 +18,5 @@
 #pragma link C++ class o2::utils::TreeStreamRedirector + ;
 #pragma link C++ class o2::utils::RootChain + ;
 #pragma link C++ class o2::utils::RngHelper;
-#pragma link C++ class o2::utils::HBFUtils + ;
 
 #endif
