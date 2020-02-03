9bc6fe55e36f89f5c2d0ea78f4f6db5bf9856084
jgrosseo <jgrosseo@cern.ch>
2020-02-03 14:47:06 +0100

    first version of correlation analysis (only same event) and related container classes (#2872)

---
+++ Analysis/Tasks/correlationsCollection.cxx
@@ -0,0 +1,120 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+#include "Framework/runDataProcessing.h"
+#include "Framework/AnalysisTask.h"
+#include "Framework/AnalysisDataModel.h"
+#include "Analysis/StepTHn.h"
+#include "Analysis/CorrelationContainer.h"
+#include <TH1F.h>
+#include <cmath>
+
+namespace o2::aod
+{
+namespace etaphi
+{
+DECLARE_SOA_COLUMN(Eta2, eta2, float, "fEta2");
+DECLARE_SOA_COLUMN(Phi2, phi2, float, "fPhi2");
+} // namespace etaphi
+DECLARE_SOA_TABLE(EtaPhi, "AOD", "ETAPHI",
+                  etaphi::Eta2, etaphi::Phi2);
+} // namespace o2::aod
+
+using namespace o2;
+using namespace o2::framework;
+
+struct ATask {
+  Produces<aod::EtaPhi> etaphi;
+
+  void process(aod::Tracks const& tracks)
+  {
+    for (auto& track : tracks) {
+      float phi = asin(track.snp()) + track.alpha() + static_cast<float>(M_PI);
+      float eta = log(tan(0.25f * static_cast<float>(M_PI) - 0.5f * atan(track.tgl())));
+
+      etaphi(phi, eta);
+    }
+  }
+};
+
+struct CorrelationTask {
+  OutputObj<CorrelationContainer> same{"sameEvent"};
+  //OutputObj<CorrelationContainer> mixed{"mixedEvent"};
+
+  void init(o2::framework::InitContext&)
+  {
+    const char* binning =
+      "vertex: -7, -5, -3, -1, 1, 3, 5, 7\n"
+      "delta_phi: -1.570796, -1.483530, -1.396263, -1.308997, -1.221730, -1.134464, -1.047198, -0.959931, -0.872665, -0.785398, -0.698132, -0.610865, -0.523599, -0.436332, -0.349066, -0.261799, -0.174533, -0.087266, 0.0, 0.087266, 0.174533, 0.261799, 0.349066, 0.436332, 0.523599, 0.610865, 0.698132, 0.785398, 0.872665, 0.959931, 1.047198, 1.134464, 1.221730, 1.308997, 1.396263, 1.483530, 1.570796, 1.658063, 1.745329, 1.832596, 1.919862, 2.007129, 2.094395, 2.181662, 2.268928, 2.356194, 2.443461, 2.530727, 2.617994, 2.705260, 2.792527, 2.879793, 2.967060, 3.054326, 3.141593, 3.228859, 3.316126, 3.403392, 3.490659, 3.577925, 3.665191, 3.752458, 3.839724, 3.926991, 4.014257, 4.101524, 4.188790, 4.276057, 4.363323, 4.450590, 4.537856, 4.625123, 4.712389\n"
+      "delta_eta: -2.0, -1.9, -1.8, -1.7, -1.6, -1.5, -1.4, -1.3, -1.2, -1.1, -1.0, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0\n"
+      "p_t_assoc: 0.5, 1.0, 1.5, 2.0, 3.0, 4.0, 6.0\n"
+      "p_t_trigger: 0.5, 1.0, 1.5, 2.0, 3.0, 4.0, 6.0, 10.0\n"
+      "multiplicity: 0, 5, 10, 20, 30, 40, 50, 100.1\n"
+      "eta: -1.0, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0\n"
+      "p_t_leading: 0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0, 9.5, 10.0, 10.5, 11.0, 11.5, 12.0, 12.5, 13.0, 13.5, 14.0, 14.5, 15.0, 15.5, 16.0, 16.5, 17.0, 17.5, 18.0, 18.5, 19.0, 19.5, 20.0, 20.5, 21.0, 21.5, 22.0, 22.5, 23.0, 23.5, 24.0, 24.5, 25.0, 25.5, 26.0, 26.5, 27.0, 27.5, 28.0, 28.5, 29.0, 29.5, 30.0, 30.5, 31.0, 31.5, 32.0, 32.5, 33.0, 33.5, 34.0, 34.5, 35.0, 35.5, 36.0, 36.5, 37.0, 37.5, 38.0, 38.5, 39.0, 39.5, 40.0, 40.5, 41.0, 41.5, 42.0, 42.5, 43.0, 43.5, 44.0, 44.5, 45.0, 45.5, 46.0, 46.5, 47.0, 47.5, 48.0, 48.5, 49.0, 49.5, 50.0\n"
+      "p_t_leading_course: 0.5, 1.0, 2.0, 3.0, 4.0, 6.0, 8.0\n"
+      "p_t_eff: 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25, 2.5, 2.75, 3.0, 3.25, 3.5, 3.75, 4.0, 4.5, 5.0, 6.0, 7.0, 8.0\n"
+      "vertex_eff: -10, -8, -6, -4, -2, 0, 2, 4, 6, 8, 10\n";
+
+    same.setObject(new CorrelationContainer("sameEvent", "sameEvent", "NumberDensityPhiCentrality", binning));
+    //mixed.setObject(new CorrelationContainer("mixedEvent", "mixedEvent", "NumberDensityPhiCentrality", binning));
+  }
+
+  void process(aod::Collision const& collision, soa::Join<aod::Tracks, aod::EtaPhi> const& tracks)
+  {
+    LOGF(info, "Tracks for collision: %d", tracks.size());
+
+    for (auto it1 = tracks.begin(); it1 != tracks.end(); ++it1) {
+      auto& track1 = *it1;
+      if (track1.pt() < 0.5)
+        continue;
+
+      double eventValues[3];
+      eventValues[0] = track1.pt();
+      eventValues[1] = 0; // collision.v0mult();;
+      eventValues[2] = collision.posZ();
+
+      same->getEventHist()->Fill(eventValues, CorrelationContainer::kCFStepReconstructed);
+      //mixed->getEventHist()->Fill(eventValues, CorrelationContainer::kCFStepReconstructed);
+
+      for (auto it2 = it1 + 1; it2 != tracks.end(); ++it2) {
+        auto& track2 = *it2;
+        if (track1 == track2)
+          continue;
+        if (track2.pt() < 0.5)
+          continue;
+
+        double values[6] = {0};
+
+        values[0] = track1.eta2() - track2.eta2();
+        values[1] = track1.pt();
+        values[2] = track2.pt();
+        values[3] = 0; // collision.v0mult();
+
+        values[4] = track1.phi2() - track2.phi2();
+        if (values[4] > 1.5 * TMath::Pi())
+          values[4] -= TMath::TwoPi();
+        if (values[4] < -0.5 * TMath::Pi())
+          values[4] += TMath::TwoPi();
+
+        values[5] = collision.posZ();
+
+        same->getTrackHist()->Fill(values, CorrelationContainer::kCFStepReconstructed);
+        //mixed->getTrackHist()->Fill(values, CorrelationContainer::kCFStepReconstructed);
+      }
+    }
+  }
+};
+
+WorkflowSpec defineDataProcessing(ConfigContext const&)
+{
+  return WorkflowSpec{
+    adaptAnalysisTask<ATask>("produce-etaphi"),
+    adaptAnalysisTask<CorrelationTask>("correlation-task")};
+}
