ca1783aed3be6839a6e9e91abbdf7d39ec1d7e52
Gian Michele Innocenti <ginnocen@cern.ch>
2020-02-04 22:02:00 +0100

    First commit of the vertexerhf task

--- Framework/Foundation/include/Framework/StructToTuple.h
+++ Framework/Foundation/include/Framework/StructToTuple.h
@@ -16,7 +16,7 @@ namespace o2::framework
 {
 struct any_type {
   template <class T>
-  constexpr operator T();  // non explicit
+  constexpr operator T(); // non explicit
 };
 
 template <class T, typename... Args>
@@ -60,7 +60,22 @@ template <class T>
 auto constexpr to_tuple_refs(T&& object) noexcept
 {
   using type = std::decay_t<T>;
-  if constexpr (is_braces_constructible<type, any_type, any_type, any_type, any_type>{}) {
+  if constexpr (is_braces_constructible<type, any_type, any_type, any_type, any_type, any_type, any_type, any_type, any_type, any_type>{}) {
+    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8] = object;
+    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8);
+  } else if constexpr (is_braces_constructible<type, any_type, any_type, any_type, any_type, any_type, any_type, any_type, any_type>{}) {
+    auto&& [p0, p1, p2, p3, p4, p5, p6, p7] = object;
+    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7);
+  } else if constexpr (is_braces_constructible<type, any_type, any_type, any_type, any_type, any_type, any_type, any_type>{}) {
+    auto&& [p0, p1, p2, p3, p4, p5, p6] = object;
+    return std::tie(p0, p1, p2, p3, p4, p5, p6);
+  } else if constexpr (is_braces_constructible<type, any_type, any_type, any_type, any_type, any_type, any_type>{}) {
+    auto&& [p0, p1, p2, p3, p4, p5] = object;
+    return std::tie(p0, p1, p2, p3, p4, p5);
+  } else if constexpr (is_braces_constructible<type, any_type, any_type, any_type, any_type, any_type>{}) {
+    auto&& [p0, p1, p2, p3, p4] = object;
+    return std::tie(p0, p1, p2, p3, p4);
+  } else if constexpr (is_braces_constructible<type, any_type, any_type, any_type, any_type>{}) {
     auto&& [p0, p1, p2, p3] = object;
     return std::tie(p0, p1, p2, p3);
   } else if constexpr (is_braces_constructible<type, any_type, any_type, any_type>{}) {
@@ -77,4 +92,4 @@ auto constexpr to_tuple_refs(T&& object) noexcept
   }
 }
 
-}  // namespace o2::framework
+} // namespace o2::framework
