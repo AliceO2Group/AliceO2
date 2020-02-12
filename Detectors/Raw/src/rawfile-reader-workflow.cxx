4b2002fd661ce87e91846ca7b8e25d9333d429b6
shahoian <ruben.shahoyan@cern.ch>
2020-02-12 14:06:39 +0100

    Avoid Framework dependency of the library, move to executable
    Root has problem to generate a dictionary for the code depending on the include/boost/container/...
    coming via Framework ... MemoryResources.h dependence.

--- Detectors/Raw/src/rawfile-reader-workflow.cxx
+++ Detectors/Raw/src/rawfile-reader-workflow.cxx
@@ -8,7 +8,7 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-#include "DetectorsRaw/RawFileReaderWorkflow.h"
+#include "RawFileReaderWorkflow.h"
 #include <string>
 
 using namespace o2::framework;
