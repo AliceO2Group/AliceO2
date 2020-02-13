ac31a51ea6540387044859b5e752123bd73b9f53
Sandro Wenzel <sandro.wenzel@cern.ch>
2020-02-13 12:07:25 +0100

    CCDB: command line options and stability improvements

--- CCDB/src/DownloadCCDBFile.cxx
+++ CCDB/src/DownloadCCDBFile.cxx
@@ -14,21 +14,62 @@
 #include <map>
 #include "TFile.h"
 #include <iostream>
+#include <boost/program_options.hpp>
+
+namespace bpo = boost::program_options;
+
+bool initOptionsAndParse(bpo::options_description& options, int argc, char* argv[], bpo::variables_map& vm)
+{
+  options.add_options()(
+    "host", bpo::value<std::string>()->default_value("ccdb-test.cern.ch:8080"), "CCDB server")(
+    "path,p", bpo::value<std::string>(), "CCDB path")(
+    "dest,d", bpo::value<std::string>(), "destination path")(
+    "timestamp,t", bpo::value<long>()->default_value(-1), "timestamp - default -1 = now")(
+    "help,h", "Produce help message.");
+
+  try {
+    bpo::store(parse_command_line(argc, argv, options), vm);
+
+    // help
+    if (vm.count("help")) {
+      std::cout << options << std::endl;
+      return false;
+    }
+
+    bpo::notify(vm);
+  } catch (const bpo::error& e) {
+    std::cerr << e.what() << "\n\n";
+    std::cerr << "Error parsing command line arguments; Available options:\n";
+
+    std::cerr << options << std::endl;
+    return false;
+  }
+  return true;
+}
 
 // a simple tool to download the CCDB blob and store in a ROOT file
 int main(int argc, char* argv[])
 {
-
-  // this is very prototypic; needs proper command line options and configurability
-
-  if (argc < 3) {
-    std::cerr << "Usage: " << argv[0] << " CCDBPATH TARGETPATH\n";
+  bpo::options_description options("Allowed options");
+  bpo::variables_map vm;
+  if (!initOptionsAndParse(options, argc, argv, vm)) {
+    return -1;
   }
+
   o2::ccdb::CcdbApi api;
-  api.init("ccdb-test.cern.ch:8080");
+  auto host = vm["host"].as<std::string>();
+  api.init(host);
 
   std::map<std::string, std::string> filter;
-  api.retrieveBlob(argv[1], argv[2], filter, o2::ccdb::getCurrentTimestamp());
+  long timestamp = vm["timestamp"].as<long>();
+  if (timestamp == -1) {
+    timestamp = o2::ccdb::getCurrentTimestamp();
+  }
+  auto path = vm["path"].as<std::string>();
+  auto dest = vm["dest"].as<std::string>();
+
+  std::cout << "Querying host " << host << " for path " << path << " and timestamp " << timestamp << "\n";
+  api.retrieveBlob(path, dest, filter, timestamp);
 
   return 0;
 }
