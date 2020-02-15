6f549acf7877114b5eabe18a18578c169aa012a5
Anton Alkin <anton.alkin@cern.ch>
2020-02-15 15:21:06 +0100

    DPL: Helper to determine type position in a pack

--- Framework/Foundation/test/test_FunctionalHelpers.cxx
+++ Framework/Foundation/test/test_FunctionalHelpers.cxx
@@ -28,6 +28,7 @@ BOOST_AUTO_TEST_CASE(TestOverride)
   static_assert(pack_size(pack<int, float>{}) == 2, "Bad size for pack");
   static_assert(has_type_v<int, pack<int, float>> == true, "int should be in pack");
   static_assert(has_type_v<double, pack<int, float>> == false, "int should be in pack");
+  static_assert(has_type_at<int>(pack<float, char, int, bool>{}) == 2, "int should be a 2nd entry");
 
   static_assert(std::is_same_v<selected_pack<is_int_t, int, float, char>, pack<int>>, "selector should select int");
   static_assert(std::is_same_v<filtered_pack<is_int_t, int, float, char>, pack<float, char>>, "filter should remove int");
