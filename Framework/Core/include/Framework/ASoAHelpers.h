e212163dc36a91643ec3320ae4feb6e18ff2ae76
saganatt <8majak8@gmail.com>
2020-02-17 11:30:13 +0100

    TuplesGenerator to generate k-tuples of iterators from a table

--- Framework/Core/include/Framework/ASoAHelpers.h
+++ Framework/Core/include/Framework/ASoAHelpers.h
@@ -13,24 +13,217 @@
 
 #include "Framework/ASoA.h"
 
+#include <iterator>
+#include <tuple>
+#include <utility>
+
 namespace o2::soa
 {
 
-/// @return a vector of pairs with all the possible
+template <typename T2, unsigned N, typename... REST>
+struct generateTupleType {
+  typedef typename generateTupleType<T2, N - 1, T2, REST...>::type type;
+};
+
+template <typename T2, typename... REST>
+struct generateTupleType<T2, 0, REST...> {
+  typedef std::tuple<REST...> type;
+};
+
+template <std::size_t... Is>
+constexpr auto indexSequenceReverse(std::index_sequence<Is...> const&)
+  -> decltype(std::index_sequence<sizeof...(Is) - 1U - Is...>{});
+
+template <std::size_t N>
+using makeIndexSequenceReverse = decltype(indexSequenceReverse(std::make_index_sequence<N>{}));
+
+template <std::size_t V>
+struct num {
+  static const constexpr auto value = V;
+};
+
+template <class F, std::size_t... Is>
+void for_(F func, std::index_sequence<Is...>)
+{
+  using expander = int[];
+  (void)expander{0, ((void)func(num<Is>{}), 0)...};
+}
+
+template <std::size_t N, typename F>
+void for_(F func)
+{
+  for_(func, std::make_index_sequence<N>());
+}
+
+template <std::size_t N, typename F>
+void reverseFor(F func)
+{
+  for_(func, makeIndexSequenceReverse<N>());
+}
+
+template <typename... Ts>
+void addOne(std::tuple<Ts...>& tuple, int maxOffset, bool& isEnd)
+{
+  constexpr auto size = std::tuple_size_v<std::tuple<Ts...>>;
+  bool modify = true;
+  reverseFor<size>([&](auto i) {
+    if (modify) {
+      std::get<i.value>(tuple)++;
+      if (std::get<i.value>(tuple) < maxOffset + i.value + 1) {
+        modify = false;
+      }
+    }
+  });
+  for_<size>([&](auto i) {
+    if constexpr (i.value != 0) {
+      if (std::get<i.value>(tuple) == maxOffset + i.value + 1) {
+        std::get<i.value>(tuple) = std::get<i.value - 1>(tuple) + 1;
+      }
+    }
+  });
+  isEnd = std::get<0>(tuple) == maxOffset + 1;
+}
+
+template <typename... Ts>
+void updateTuple(std::tuple<Ts...>& tuple, std::function<int(size_t index)> func, int maxOffset, bool& isEnd)
+{
+  constexpr auto size = std::tuple_size_v<std::tuple<Ts...>>;
+  for_<size>([&](auto i) {
+    std::get<i.value>(tuple) = func(i.value);
+  });
+  isEnd = std::get<0>(tuple) == maxOffset + 1;
+}
+
+template <typename T2, typename... Is, typename... Ts>
+void indicesToIterators(std::tuple<Is...>& tuple, std::tuple<Ts...>& iteratorsTuple, T2 tableBegin)
+{
+  constexpr auto size = std::tuple_size_v<std::tuple<Ts...>>;
+  for_<size>([&](auto i) {
+    std::get<i.value>(iteratorsTuple) = tableBegin + std::get<i.value>(tuple);
+  });
+}
+
+/// @return a vector of K-tuples with all the possible
 /// combinations of the rows of the table T.
 /// FIXME: move to coroutines once we have C++20
-template <typename T>
-std::vector<std::pair<typename T::iterator, typename T::iterator>>
-  combinations(T const& table)
+template <typename T, int K>
+class TuplesGenerator
 {
-  std::vector<std::pair<typename T::iterator, typename T::iterator>> result;
-  result.reserve((table.size() + 1) * table.size() / 2);
-  for (auto t0 = table.begin(); t0 + 1 != table.end(); ++t0) {
-    for (auto t1 = t0 + 1; t1 != table.end(); ++t1) {
-      result.push_back(std::make_pair(t0, t1));
+ public:
+  using IteratorType = typename T::iterator;
+  using IndexTupleType = typename generateTupleType<int, K>::type;
+  using TupleType = typename generateTupleType<IteratorType, K>::type;
+  using FunctionType = std::function<bool(const TupleType&)>;
+
+  class TuplesIterator : public std::iterator<std::forward_iterator_tag, TupleType>
+  {
+   public:
+    using reference = TupleType&;
+    using value_type = TupleType;
+    using pointer = TupleType*;
+    using iterator_category = std::forward_iterator_tag;
+
+    TuplesIterator() = delete;
+
+    TuplesIterator(IteratorType begin, IteratorType end, int n, FunctionType condition)
+      : mN(n), mK(K), mIsEnd(false), mTableBegin(begin), mTableEnd(end), mCondition(condition)
+    {
+      // First permutation
+      goToBegin();
     }
+
+    ~TuplesIterator() = default;
+
+    // prefix increment
+    TuplesIterator& operator++()
+    {
+      if (!mIsEnd) {
+        addOne(mCurrentIndices, mN - mK, mIsEnd);
+        indicesToIterators(mCurrentIndices, mCurrent, mTableBegin);
+      }
+      while (!mIsEnd && !mCondition(mCurrent)) {
+        addOne(mCurrentIndices, mN - mK, mIsEnd);
+        indicesToIterators(mCurrentIndices, mCurrent, mTableBegin);
+      }
+      return *this;
+    }
+    // postfix increment
+    TuplesIterator operator++(int /*unused*/)
+    {
+      TuplesIterator copy(*this);
+      operator++();
+      return copy;
+    }
+    // return reference
+    reference operator*()
+    {
+      return mCurrent;
+    }
+    // Assuming iterators with the same permutation to be at the same "position"
+    bool operator==(const TuplesIterator& rh)
+    {
+      return (mIsEnd && rh.mIsEnd) || (mCurrent == rh.mCurrent);
+    }
+    bool operator!=(const TuplesIterator& rh)
+    {
+      return !(*this == rh);
+    }
+
+    void goToBegin()
+    {
+      updateTuple(
+        mCurrentIndices, [](size_t index) { return index; }, mN - mK, mIsEnd);
+      indicesToIterators(mCurrentIndices, mCurrent, mTableBegin);
+      if (!mIsEnd && !mCondition(mCurrent)) {
+        operator++();
+      }
+    }
+
+    void goToEnd()
+    {
+      updateTuple(
+        mCurrentIndices, [this](size_t index) { return mN - mK + index + 1; }, mN - mK, mIsEnd);
+      indicesToIterators(mCurrentIndices, mCurrent, mTableBegin);
+    }
+
+   private:
+    TupleType mCurrent;
+    IndexTupleType mCurrentIndices;
+    int mN;                   // number of elements
+    int mK;                   // tuple size, number of elements to choose
+    bool mIsEnd;              // whether there are any more tuples available
+    FunctionType mCondition;  // only tuples satisfying the condition will be outputed
+    IteratorType mTableBegin; // start of the table for which tuples are generated
+    IteratorType mTableEnd;   // end of the table for which tuples are generated
+  };
+
+  using iterator = TuplesIterator;
+  using const_iterator = TuplesIterator;
+
+  inline iterator begin()
+  {
+    return iterator(mTableBegin, mTableEnd, mN, mCondition);
   }
-  return result;
+  inline iterator end()
+  {
+    auto it = iterator(mTableBegin, mTableEnd, mN, mCondition);
+    it.goToEnd();
+    return it;
+  }
+
+  TuplesGenerator() = delete;
+  TuplesGenerator(const T& table, FunctionType condition)
+    : mTableBegin(table.begin()), mTableEnd(table.end()), mN(table.size()), mCondition(condition)
+  {
+    static_assert(K > 0);
+  }
+  ~TuplesGenerator() = default;
+
+ private:
+  IteratorType mTableBegin;
+  IteratorType mTableEnd;
+  int mN;
+  FunctionType mCondition;
 };
 
 } // namespace o2::soa
--- Framework/Core/test/test_ASoAHelpers.cxx
+++ Framework/Core/test/test_ASoAHelpers.cxx
@@ -27,6 +27,166 @@ DECLARE_SOA_COLUMN(Z, z, int32_t, "z");
 DECLARE_SOA_DYNAMIC_COLUMN(Sum, sum, [](int32_t x, int32_t y) { return x + y; });
 } // namespace test
 
+BOOST_AUTO_TEST_CASE(AddOneToTuple)
+{
+  using TupleType2 = typename generateTupleType<int, 2>::type;
+  TupleType2 indexTuple2(0, 1);
+  bool isEnd = false;
+
+  addOne(indexTuple2, 1, isEnd);
+  BOOST_CHECK_EQUAL(std::get<0>(indexTuple2), 0);
+  BOOST_CHECK_EQUAL(std::get<1>(indexTuple2), 2);
+  BOOST_CHECK_EQUAL(isEnd, false);
+
+  addOne(indexTuple2, 1, isEnd);
+  addOne(indexTuple2, 1, isEnd);
+  BOOST_CHECK_EQUAL(std::get<0>(indexTuple2), 2);
+  BOOST_CHECK_EQUAL(std::get<1>(indexTuple2), 3);
+  BOOST_CHECK_EQUAL(isEnd, true);
+
+  using TupleType3 = typename generateTupleType<int, 3>::type;
+  TupleType3 indexTuple3(0, 2, 3);
+  isEnd = false;
+
+  addOne(indexTuple3, 1, isEnd);
+  BOOST_CHECK_EQUAL(std::get<0>(indexTuple3), 1);
+  BOOST_CHECK_EQUAL(std::get<1>(indexTuple3), 2);
+  BOOST_CHECK_EQUAL(std::get<2>(indexTuple3), 3);
+  BOOST_CHECK_EQUAL(isEnd, false);
+
+  addOne(indexTuple3, 1, isEnd);
+  BOOST_CHECK_EQUAL(std::get<0>(indexTuple3), 2);
+  BOOST_CHECK_EQUAL(std::get<1>(indexTuple3), 3);
+  BOOST_CHECK_EQUAL(std::get<2>(indexTuple3), 4);
+  BOOST_CHECK_EQUAL(isEnd, true);
+}
+
+BOOST_AUTO_TEST_CASE(UpdateTuple)
+{
+  using TupleType2 = typename generateTupleType<int, 2>::type;
+  TupleType2 indexTuple2(1, 2);
+  bool isEnd = false;
+
+  updateTuple(
+    indexTuple2, [](size_t ind) { return ind + 1; }, 1, isEnd);
+  BOOST_CHECK_EQUAL(std::get<0>(indexTuple2), 1);
+  BOOST_CHECK_EQUAL(std::get<1>(indexTuple2), 2);
+  BOOST_CHECK_EQUAL(isEnd, false);
+
+  updateTuple(
+    indexTuple2, [](size_t ind) { return ind + 2; }, 1, isEnd);
+  BOOST_CHECK_EQUAL(std::get<0>(indexTuple2), 2);
+  BOOST_CHECK_EQUAL(std::get<1>(indexTuple2), 3);
+  BOOST_CHECK_EQUAL(isEnd, true);
+
+  using TupleType3 = typename generateTupleType<int, 3>::type;
+  TupleType3 indexTuple3(0, 2, 3);
+  isEnd = false;
+
+  updateTuple(
+    indexTuple3, [](size_t ind) { return ind + 1; }, 1, isEnd);
+  BOOST_CHECK_EQUAL(std::get<0>(indexTuple3), 1);
+  BOOST_CHECK_EQUAL(std::get<1>(indexTuple3), 2);
+  BOOST_CHECK_EQUAL(std::get<2>(indexTuple3), 3);
+  BOOST_CHECK_EQUAL(isEnd, false);
+
+  updateTuple(
+    indexTuple3, [](size_t ind) { return ind + 2; }, 1, isEnd);
+  BOOST_CHECK_EQUAL(std::get<0>(indexTuple3), 2);
+  BOOST_CHECK_EQUAL(std::get<1>(indexTuple3), 3);
+  BOOST_CHECK_EQUAL(std::get<2>(indexTuple3), 4);
+  BOOST_CHECK_EQUAL(isEnd, true);
+}
+
+//BOOST_AUTO_TEST_CASE(IndicesToIterators)
+//{
+//  TableBuilder builderA;
+//  auto rowWriterA = builderA.persist<int32_t, int32_t>({"x", "y"});
+//  rowWriterA(0, 2, 0);
+//  rowWriterA(0, 3, 0);
+//  rowWriterA(0, 4, 0);
+//  auto tableA = builderA.finalize();
+//  BOOST_REQUIRE_EQUAL(tableA->num_rows(), 3);
+//
+//  using TestA = o2::soa::Table<o2::soa::Index<>, test::X, test::Y>;
+//
+//  TestA tests{tableA};
+//
+//  using IndexTupleType2 = typename generateTupleType<int, 2>::type;
+//  using TupleType2 = typename generateTupleType<TestA::iterator, 2>::type;
+//  IndexTupleType2 indexTuple2(0, 1);
+//  TupleType2 iteratorsTuple2;
+//
+//  indicesToIterators(indexTuple2, iteratorsTuple2, tests.begin());
+//
+//  BOOST_REQUIRE_NE(static_cast<test::X>(std::get<0>(iteratorsTuple2)).getIterator().mCurrentPos, nullptr);
+//  BOOST_REQUIRE_EQUAL(*(static_cast<test::X>(std::get<0>(iteratorsTuple2)).getIterator().mCurrentPos), 0);
+//  BOOST_CHECK_EQUAL(std::get<0>(iteratorsTuple2).x(), 2);
+//  BOOST_REQUIRE_NE(static_cast<test::X>(std::get<1>(iteratorsTuple2)).getIterator().mCurrentPos, nullptr);
+//  BOOST_REQUIRE_EQUAL(*(static_cast<test::X>(std::get<1>(iteratorsTuple2)).getIterator().mCurrentPos), 1);
+//  BOOST_CHECK_EQUAL(std::get<1>(iteratorsTuple2).x(), 3);
+//
+//  using IndexTupleType3 = typename generateTupleType<int, 3>::type;
+//  using TupleType3 = typename generateTupleType<TestA::iterator, 3>::type;
+//  IndexTupleType3 indexTuple3(0, 2, 1);
+//  TupleType3 iteratorsTuple3;
+//
+//  indicesToIterators(indexTuple3, iteratorsTuple3, tests.begin());
+//
+//  BOOST_REQUIRE_NE(static_cast<test::X>(std::get<0>(iteratorsTuple3)).getIterator().mCurrentPos, nullptr);
+//  BOOST_REQUIRE_EQUAL(*(static_cast<test::X>(std::get<0>(iteratorsTuple3)).getIterator().mCurrentPos), 0);
+//  BOOST_CHECK_EQUAL(std::get<0>(iteratorsTuple3).x(), 2);
+//  BOOST_REQUIRE_NE(static_cast<test::X>(std::get<1>(iteratorsTuple3)).getIterator().mCurrentPos, nullptr);
+//  BOOST_REQUIRE_EQUAL(*(static_cast<test::X>(std::get<1>(iteratorsTuple3)).getIterator().mCurrentPos), 2);
+//  BOOST_CHECK_EQUAL(std::get<1>(iteratorsTuple3).x(), 4);
+//  BOOST_REQUIRE_NE(static_cast<test::X>(std::get<2>(iteratorsTuple3)).getIterator().mCurrentPos, nullptr);
+//  BOOST_REQUIRE_EQUAL(*(static_cast<test::X>(std::get<2>(iteratorsTuple3)).getIterator().mCurrentPos), 1);
+//  BOOST_CHECK_EQUAL(std::get<2>(iteratorsTuple3).x(), 3);
+//}
+
+BOOST_AUTO_TEST_CASE(TuplesGeneratorConstruction)
+{
+  TableBuilder builderA;
+  auto rowWriterA = builderA.persist<int32_t, int32_t>({"x", "y"});
+  rowWriterA(0, 0, 0);
+  rowWriterA(0, 1, 0);
+  rowWriterA(0, 2, 0);
+  rowWriterA(0, 3, 0);
+  rowWriterA(0, 4, 0);
+  rowWriterA(0, 5, 0);
+  rowWriterA(0, 6, 0);
+  rowWriterA(0, 7, 0);
+  auto tableA = builderA.finalize();
+  BOOST_REQUIRE_EQUAL(tableA->num_rows(), 8);
+
+  using TestA = o2::soa::Table<o2::soa::Index<>, test::X, test::Y>;
+
+  TestA tests{tableA};
+
+  BOOST_REQUIRE_EQUAL(8, tests.size());
+
+  auto comb2 = TuplesGenerator<TestA, 2>(tests, [](const auto testTuple) { return true; });
+
+  bool isBeginTuplesIterator = std::is_same_v<decltype(comb2.begin()), TuplesGenerator<TestA, 2>::TuplesIterator>;
+  BOOST_REQUIRE(isBeginTuplesIterator == true);
+  bool isBeginValueTuple = std::is_same_v<decltype(*(comb2.begin())), TuplesGenerator<TestA, 2>::TupleType&>;
+  BOOST_REQUIRE(isBeginValueTuple == true);
+
+  auto beginTuple = *(comb2.begin());
+  BOOST_REQUIRE_NE(static_cast<test::X>(std::get<0>(beginTuple)).getIterator().mCurrentPos, nullptr);
+  BOOST_REQUIRE_EQUAL(*(static_cast<test::X>(std::get<0>(beginTuple)).getIterator().mCurrentPos), 0);
+  BOOST_REQUIRE_NE(static_cast<test::X>(std::get<1>(beginTuple)).getIterator().mCurrentPos, nullptr);
+  BOOST_REQUIRE_EQUAL(*(static_cast<test::X>(std::get<1>(beginTuple)).getIterator().mCurrentPos), 1);
+
+  BOOST_REQUIRE(comb2.begin() != comb2.end());
+
+  auto endTuple = *(comb2.end());
+  BOOST_REQUIRE_NE(static_cast<test::X>(std::get<0>(endTuple)).getIterator().mCurrentPos, nullptr);
+  BOOST_REQUIRE_EQUAL(*(static_cast<test::X>(std::get<0>(endTuple)).getIterator().mCurrentPos), 7);
+  BOOST_REQUIRE_NE(static_cast<test::X>(std::get<1>(endTuple)).getIterator().mCurrentPos, nullptr);
+  BOOST_REQUIRE_EQUAL(*(static_cast<test::X>(std::get<1>(endTuple)).getIterator().mCurrentPos), 8);
+}
+
 BOOST_AUTO_TEST_CASE(Combinations)
 {
   TableBuilder builderA;
@@ -47,22 +207,78 @@ BOOST_AUTO_TEST_CASE(Combinations)
   TestA tests{tableA};
 
   BOOST_REQUIRE_EQUAL(8, tests.size());
-  auto c = combinations(tests);
-  BOOST_CHECK_EQUAL(c.size(), 7 * (7 + 1) / 2);
+  int n = tests.size();
 
-  int n = 0;
+  auto comb2 = TuplesGenerator<TestA, 2>(tests, [](const auto testTuple) { return true; });
+
+  int count = 0;
   int i = 0;
   int j = 1;
-  for (auto [t1, t2] : combinations(tests)) {
-    BOOST_CHECK_EQUAL(t1.x(), i);
-    BOOST_CHECK_EQUAL(t2.x(), j);
-    if (j == 7) {
-      ++i;
-      j = i + 1;
-      continue;
-    }
-    n++;
+  for (auto comb : comb2) {
+    BOOST_CHECK_EQUAL(std::get<0>(comb).x(), i);
+    BOOST_CHECK_EQUAL(std::get<1>(comb).x(), j);
+    count++;
     j++;
+    if (j == n) {
+      i++;
+      j = i + 1;
+    }
   }
-  //BOOST_CHECK_EQUAL(i, 28);
+  BOOST_CHECK_EQUAL(count, 28);
+
+  auto comb2_cond = TuplesGenerator<TestA, 2>(tests, [](const auto testTuple) { return std::get<1>(testTuple).x() == 5; });
+
+  count = 0;
+  i = 0;
+  j = 5;
+  for (auto comb : comb2_cond) {
+    BOOST_CHECK_EQUAL(std::get<0>(comb).x(), i);
+    BOOST_CHECK_EQUAL(std::get<1>(comb).x(), j);
+    count++;
+    i++;
+  }
+  BOOST_CHECK_EQUAL(count, 5);
+
+  auto comb3 = TuplesGenerator<TestA, 3>(tests, [](const auto testTuple) { return true; });
+
+  count = 0;
+  i = 0;
+  j = 1;
+  int k = 2;
+  for (auto comb : comb3) {
+    BOOST_CHECK_EQUAL(std::get<0>(comb).x(), i);
+    BOOST_CHECK_EQUAL(std::get<1>(comb).x(), j);
+    BOOST_CHECK_EQUAL(std::get<2>(comb).x(), k);
+    count++;
+    k++;
+    if (k == n) {
+      if (j == n - 2) {
+        i++;
+        j = i;
+      }
+      j++;
+      k = j + 1;
+    }
+  }
+  BOOST_CHECK_EQUAL(count, 56);
+
+  auto comb3_cond = TuplesGenerator<TestA, 3>(tests, [](const auto testTuple) { return std::get<1>(testTuple).x() == 6; });
+
+  count = 0;
+  i = 0;
+  j = 6;
+  k = 7;
+  for (auto comb : comb3_cond) {
+    BOOST_CHECK_EQUAL(std::get<0>(comb).x(), i);
+    BOOST_CHECK_EQUAL(std::get<1>(comb).x(), j);
+    BOOST_CHECK_EQUAL(std::get<2>(comb).x(), k);
+    count++;
+    k++;
+    if (k == n) {
+      i++;
+      k = j + 1;
+    }
+  }
+
+  BOOST_CHECK_EQUAL(count, 6);
 }
