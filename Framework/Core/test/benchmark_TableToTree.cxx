df71054b668b26a09f76c246d1e99b1cd15486bd
Paul Buehler <paul.buehler@oeaw.ac.at>
2020-02-18 14:44:21 +0100

    apply latest modifications on fresh fork of  AliceO2Group/AliceO2

--- Framework/Core/test/benchmark_TableToTree.cxx
+++ Framework/Core/test/benchmark_TableToTree.cxx
@@ -8,10 +8,8 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-#include "Framework/ASoA.h"
-#include "Framework/TableBuilder.h"
-#include "Framework/AnalysisDataModel.h"
 #include "Framework/CommonDataProcessors.h"
+#include "Framework/TableTreeHelpers.h"
 #include <benchmark/benchmark.h>
 #include <random>
 #include <vector>
@@ -33,23 +31,23 @@ DECLARE_SOA_DYNAMIC_COLUMN(Sum, sum, [](float x, float y) { return x + y; });
 #ifdef __APPLE__
 constexpr unsigned int maxrange = 15;
 #else
-constexpr unsigned int maxrange = 20;
+constexpr unsigned int maxrange = 24;
 #endif
 
-static void BM_Table2Tree(benchmark::State& state)
+static void BM_TableToTree(benchmark::State& state)
 {
 
   // initialize a random generator
   std::default_random_engine e1(1234567891);
   std::uniform_real_distribution<double> rd(0, 1);
   std::normal_distribution<float> rf(5., 2.);
-  std::discrete_distribution<ULong64_t> rl({10, 20, 30, 30, 5, 5});
+  std::discrete_distribution<long> rl({10, 20, 30, 30, 5, 5});
   std::discrete_distribution<int> ri({10, 20, 30, 30, 5, 5});
 
   // create a table and fill the columns with random numbers
   TableBuilder builder;
   auto rowWriter =
-    builder.persist<double, float, ULong64_t, int>({"a", "b", "c", "d"});
+    builder.persist<double, float, long, int>({"a", "b", "c", "d"});
   for (auto i = 0; i < state.range(0); ++i) {
     rowWriter(0, rd(e1), rf(e1), rl(e1), ri(e1));
   }
@@ -59,20 +57,20 @@ static void BM_Table2Tree(benchmark::State& state)
   for (auto _ : state) {
 
     // Open file and create tree
-    TFile* fout = new TFile("table2tree.root", "RECREATE");
-    TTree* tout = new TTree("table2tree", "table2tree");
+    TFile fout("table2tree.root", "RECREATE");
 
-    // benchmark the CommonDataProcessors::table2tree function
-    CommonDataProcessors::table2tree(tout, table, false);
+    // benchmark TableToTree
+    TableToTree ta2tr(table, &fout, "table2tree");
+    if (ta2tr.AddAllBranches())
+      ta2tr.Process();
 
     // clean up
-    fout->Close();
-    delete fout;
+    fout.Close();
   }
 
   state.SetBytesProcessed(state.iterations() * state.range(0) * 24);
 }
 
-BENCHMARK(BM_Table2Tree)->Range(8, 8 << maxrange);
+BENCHMARK(BM_TableToTree)->Range(8, 8 << maxrange);
 
 BENCHMARK_MAIN();
