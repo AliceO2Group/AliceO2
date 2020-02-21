1fc82dd37eb1be782a44dcef046eadcfc146eab1
saganatt <8majak8@gmail.com>
2020-02-21 17:23:09 +0100

    CombinationsGenerator to provide std::array of k distinct iterators from a table

--- Framework/Core/include/Framework/ASoAHelpers.h
+++ Framework/Core/include/Framework/ASoAHelpers.h
@@ -20,137 +20,65 @@
 namespace o2::soa
 {
 
-template <typename T2, unsigned N, typename... REST>
-struct generateTupleType {
-  typedef typename generateTupleType<T2, N - 1, T2, REST...>::type type;
-};
-
-template <typename T2, typename... REST>
-struct generateTupleType<T2, 0, REST...> {
-  typedef std::tuple<REST...> type;
-};
-
-template <std::size_t... Is>
-constexpr auto indexSequenceReverse(std::index_sequence<Is...> const&)
-  -> decltype(std::index_sequence<sizeof...(Is) - 1U - Is...>{});
-
-template <std::size_t N>
-using makeIndexSequenceReverse = decltype(indexSequenceReverse(std::make_index_sequence<N>{}));
-
-template <std::size_t V>
-struct num {
-  static const constexpr auto value = V;
-};
-
-template <class F, std::size_t... Is>
-void for_(F func, std::index_sequence<Is...>)
+template <typename T2, std::size_t K2>
+void addOne(std::array<T2, K2>& array, const T2& maxOffset, bool& isEnd)
 {
-  using expander = int[];
-  (void)expander{0, ((void)func(num<Is>{}), 0)...};
-}
-
-template <std::size_t N, typename F>
-void for_(F func)
-{
-  for_(func, std::make_index_sequence<N>());
-}
-
-template <std::size_t N, typename F>
-void reverseFor(F func)
-{
-  for_(func, makeIndexSequenceReverse<N>());
-}
-
-template <typename... Ts>
-void addOne(std::tuple<Ts...>& tuple, int maxOffset, bool& isEnd)
-{
-  constexpr auto size = std::tuple_size_v<std::tuple<Ts...>>;
-  bool modify = true;
-  reverseFor<size>([&](auto i) {
-    if (modify) {
-      std::get<i.value>(tuple)++;
-      if (std::get<i.value>(tuple) < maxOffset + i.value + 1) {
-        modify = false;
+  for (int i = 0; i < K2; i++) {
+    array[K2 - i - 1]++;
+    if (array[K2 - i - 1] != maxOffset + K2 - i - 1) { // no < operator for RowViewBase
+      for (int j = K2 - i; j < K2; j++) {
+        array[j].setCursor(array[j - 1].mRowIndex + 1);
       }
+      isEnd = false;
+      return;
     }
-  });
-  for_<size>([&](auto i) {
-    if constexpr (i.value != 0) {
-      if (std::get<i.value>(tuple) == maxOffset + i.value + 1) {
-        std::get<i.value>(tuple) = std::get<i.value - 1>(tuple) + 1;
-      }
-    }
-  });
-  isEnd = std::get<0>(tuple) == maxOffset + 1;
+  }
+  isEnd = true;
 }
 
-template <typename... Ts>
-void updateTuple(std::tuple<Ts...>& tuple, std::function<int(size_t index)> func, int maxOffset, bool& isEnd)
-{
-  constexpr auto size = std::tuple_size_v<std::tuple<Ts...>>;
-  for_<size>([&](auto i) {
-    std::get<i.value>(tuple) = func(i.value);
-  });
-  isEnd = std::get<0>(tuple) == maxOffset + 1;
-}
-
-template <typename T2, typename... Is, typename... Ts>
-void indicesToIterators(std::tuple<Is...>& tuple, std::tuple<Ts...>& iteratorsTuple, T2 tableBegin)
-{
-  constexpr auto size = std::tuple_size_v<std::tuple<Ts...>>;
-  for_<size>([&](auto i) {
-    std::get<i.value>(iteratorsTuple) = tableBegin + std::get<i.value>(tuple);
-  });
-}
-
-/// @return a vector of K-tuples with all the possible
-/// combinations of the rows of the table T.
+/// @return next K-combination of the rows of the table T.
 /// FIXME: move to coroutines once we have C++20
 template <typename T, int K>
-class TuplesGenerator
+class CombinationsGenerator
 {
  public:
   using IteratorType = typename T::iterator;
-  using IndexTupleType = typename generateTupleType<int, K>::type;
-  using TupleType = typename generateTupleType<IteratorType, K>::type;
-  using FunctionType = std::function<bool(const TupleType&)>;
+  using CombinationType = std::array<IteratorType, K>;
+  using FunctionType = std::function<bool(const CombinationType&)>;
 
-  class TuplesIterator : public std::iterator<std::forward_iterator_tag, TupleType>
+  class CombinationsIterator : public std::iterator<std::forward_iterator_tag, CombinationType>
   {
    public:
-    using reference = TupleType&;
-    using value_type = TupleType;
-    using pointer = TupleType*;
+    using reference = CombinationType&;
+    using value_type = CombinationType;
+    using pointer = CombinationType*;
     using iterator_category = std::forward_iterator_tag;
 
-    TuplesIterator() = delete;
+    CombinationsIterator() = delete;
 
-    TuplesIterator(IteratorType begin, IteratorType end, int n, FunctionType condition)
-      : mN(n), mK(K), mIsEnd(false), mTableBegin(begin), mTableEnd(end), mCondition(condition)
+    CombinationsIterator(const IteratorType& begin, int n, const FunctionType& condition)
+      : mN(n), mIsEnd(false), mTableBegin(begin), mMaxOffset(begin + n - K + 1), mCondition(condition)
     {
-      // First permutation
-      goToBegin();
+      initIterators();
     }
 
-    ~TuplesIterator() = default;
+    ~CombinationsIterator() = default;
 
     // prefix increment
-    TuplesIterator& operator++()
+    CombinationsIterator& operator++()
     {
       if (!mIsEnd) {
-        addOne(mCurrentIndices, mN - mK, mIsEnd);
-        indicesToIterators(mCurrentIndices, mCurrent, mTableBegin);
+        addOne(mCurrent, mMaxOffset, mIsEnd);
       }
       while (!mIsEnd && !mCondition(mCurrent)) {
-        addOne(mCurrentIndices, mN - mK, mIsEnd);
-        indicesToIterators(mCurrentIndices, mCurrent, mTableBegin);
+        addOne(mCurrent, mMaxOffset, mIsEnd);
       }
       return *this;
     }
     // postfix increment
-    TuplesIterator operator++(int /*unused*/)
+    CombinationsIterator operator++(int /*unused*/)
     {
-      TuplesIterator copy(*this);
+      CombinationsIterator copy(*this);
       operator++();
       return copy;
     }
@@ -159,69 +87,66 @@ class TuplesGenerator
     {
       return mCurrent;
     }
-    // Assuming iterators with the same permutation to be at the same "position"
-    bool operator==(const TuplesIterator& rh)
+    bool operator==(const CombinationsIterator& rh)
     {
       return (mIsEnd && rh.mIsEnd) || (mCurrent == rh.mCurrent);
     }
-    bool operator!=(const TuplesIterator& rh)
+    bool operator!=(const CombinationsIterator& rh)
     {
       return !(*this == rh);
     }
 
-    void goToBegin()
+    void initIterators()
     {
-      updateTuple(
-        mCurrentIndices, [](size_t index) { return index; }, mN - mK, mIsEnd);
-      indicesToIterators(mCurrentIndices, mCurrent, mTableBegin);
-      if (!mIsEnd && !mCondition(mCurrent)) {
+      for (int i = 0; i < K; i++) {
+        mCurrent[i] = mTableBegin + i;
+      }
+      if (!mCondition(mCurrent)) {
         operator++();
       }
     }
 
     void goToEnd()
     {
-      updateTuple(
-        mCurrentIndices, [this](size_t index) { return mN - mK + index + 1; }, mN - mK, mIsEnd);
-      indicesToIterators(mCurrentIndices, mCurrent, mTableBegin);
+      for (int i = 0; i < K; i++) {
+        mCurrent[i].setCursor(mN - K + i);
+      }
+      operator++();
     }
 
    private:
-    TupleType mCurrent;
-    IndexTupleType mCurrentIndices;
+    CombinationType mCurrent;
     int mN;                   // number of elements
-    int mK;                   // tuple size, number of elements to choose
     bool mIsEnd;              // whether there are any more tuples available
     FunctionType mCondition;  // only tuples satisfying the condition will be outputed
     IteratorType mTableBegin; // start of the table for which tuples are generated
-    IteratorType mTableEnd;   // end of the table for which tuples are generated
+    IteratorType mMaxOffset;  // one position past maximum acceptable position for 0th element of combination
   };
 
-  using iterator = TuplesIterator;
-  using const_iterator = TuplesIterator;
+  using iterator = CombinationsIterator;
+  using const_iterator = CombinationsIterator;
 
   inline iterator begin()
   {
-    return iterator(mTableBegin, mTableEnd, mN, mCondition);
+    return iterator(mTableBegin, mN, mCondition);
   }
   inline iterator end()
   {
-    auto it = iterator(mTableBegin, mTableEnd, mN, mCondition);
+    auto it = iterator(mTableBegin, mN, mCondition);
     it.goToEnd();
     return it;
   }
 
-  TuplesGenerator() = delete;
-  TuplesGenerator(const T& table, FunctionType condition)
-    : mTableBegin(table.begin()), mTableEnd(table.end()), mN(table.size()), mCondition(condition)
+  CombinationsGenerator() = delete;
+  CombinationsGenerator(const T& table, const FunctionType& condition)
+    : mTableBegin(table.begin()), mN(table.size()), mCondition(condition)
   {
     static_assert(K > 0);
   }
-  ~TuplesGenerator() = default;
+  ~CombinationsGenerator() = default;
 
  private:
   IteratorType mTableBegin;
-  IteratorType mTableEnd;
   int mN;
   FunctionType mCondition;
 };
