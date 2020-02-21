f20ebaf21fc6e89bed5fb17dd0dee58d0a8b8b3b
Giulio Eulisse <giulio.eulisse@cern.ch>
2020-02-21 11:02:30 +0100

    DPL: do not sleep when no dangling outputs available
    This actually fills the FairMQ queue in any case, so we must avoid it. One more reason to do the adaptive rate.

--- Framework/Core/src/CommonDataProcessors.cxx
+++ Framework/Core/src/CommonDataProcessors.cxx
@@ -479,7 +479,6 @@ DataProcessorSpec
           LOG(DEBUG) << "No dangling output to be saved.";
           once = true;
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
       });
     }
 
@@ -615,13 +614,10 @@ DataProcessorSpec
     if (hasOutputsToWrite == false) {
       return std::move([](ProcessingContext& pc) mutable -> void {
         static bool once = false;
-        /// We do it like this until we can use the interruptible sleep
-        /// provided by recent FairMQ releases.
         if (!once) {
           LOG(DEBUG) << "No dangling output to be dumped.";
           once = true;
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
       });
     }
     auto output = std::make_shared<std::ofstream>(filename.c_str(), std::ios_base::binary);
