f9f399f940d0dc73e42741d3f58974f497822b7c
David Rohr <drohr@jwdt.org>
2020-02-21 02:58:38 +0100

    GPU: More fine-grained GPU Thread Count constants

--- GPU/GPUTracking/Base/cuda/GPUReconstructionCUDA.cu
+++ GPU/GPUTracking/Base/cuda/GPUReconstructionCUDA.cu
@@ -552,6 +552,12 @@ void GPUReconstructionCUDABackend::SetThreadCounts()
   mTRDThreadCount = GPUCA_THREAD_COUNT_TRD;
   mClustererThreadCount = GPUCA_THREAD_COUNT_CLUSTERER;
   mScanThreadCount = GPUCA_THREAD_COUNT_SCAN;
+  mConverterThreadCount = GPUCA_THREAD_COUNT_CONVERTER;
+  mCompression1ThreadCount = GPUCA_THREAD_COUNT_COMPRESSION1;
+  mCompression2ThreadCount = GPUCA_THREAD_COUNT_COMPRESSION2;
+  mCFDecodeThreadCount = GPUCA_THREAD_COUNT_CFDECODE;
+  mFitThreadCount = GPUCA_THREAD_COUNT_FIT;
+  mITSThreadCount = GPUCA_THREAD_COUNT_ITS;
 }
 
 int GPUReconstructionCUDABackend::registerMemoryForGPU(void* ptr, size_t size)
