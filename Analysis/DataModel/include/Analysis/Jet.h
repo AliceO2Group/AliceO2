eb8361461b4c497363731defebdaec325cc5d31d
Jochen Klein <jochen.klein@cern.ch>
2020-02-28 11:28:02 +0100

    Add jet finding
    - add table for jets and constituents to data model
    - add jet finder task
    - add linking to fastjet for jet finding
    - add jet analysis using jets from jet finder task

---
+++ Analysis/DataModel/include/Analysis/Jet.h
@@ -0,0 +1,50 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+// table definitions for jets
+//
+// Author: Jochen Klein
+
+#pragma once
+
+#include "Framework/AnalysisDataModel.h"
+
+namespace o2::aod
+{
+namespace jet
+{
+DECLARE_SOA_INDEX_COLUMN(Collision, collision);
+DECLARE_SOA_COLUMN(Eta, eta, float, "fEta");
+DECLARE_SOA_COLUMN(Phi, phi, float, "fPhi");
+DECLARE_SOA_COLUMN(Pt, pt, float, "fPt");
+DECLARE_SOA_COLUMN(Area, area, float, "fArea");
+DECLARE_SOA_COLUMN(Energy, energy, float, "fEnergy");
+DECLARE_SOA_COLUMN(Mass, mass, float, "fMass");
+} // namespace jet
+
+DECLARE_SOA_TABLE(Jets, "AOD", "JET",
+                  o2::soa::Index<>, jet::CollisionId,
+                  jet::Eta, jet::Phi, jet::Pt, jet::Area, jet::Energy, jet::Mass);
+
+using Jet = Jets::iterator;
+
+// TODO: absorb in jet table
+// when list of references available
+namespace constituents
+{
+DECLARE_SOA_INDEX_COLUMN(Jet, jet);
+DECLARE_SOA_INDEX_COLUMN(Track, track);
+} // namespace constituents
+
+DECLARE_SOA_TABLE(JetConstituents, "AOD", "JETCONSTITUENTS",
+                  constituents::JetId, constituents::TrackId);
+
+using JetConstituent = JetConstituents::iterator;
+} // namespace o2::aod
