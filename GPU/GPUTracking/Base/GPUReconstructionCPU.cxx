d39a81f5495d23eb7f5c3cee5ccc846cb4bb9a1e
David Rohr <drohr@jwdt.org>
2020-03-01 05:06:45 +0100

    GPU: Report total time also with debug level >= 0

--- GPU/GPUTracking/Base/GPUReconstructionCPU.cxx
+++ GPU/GPUTracking/Base/GPUReconstructionCPU.cxx
@@ -250,6 +250,8 @@ int GPUReconstructionCPU::RunChains()
     }
     printf("Execution Time: Total   : %50s Time: %'10d us\n", "Total kernel time", (int)(kernelTotal * 1000000 / mStatNEvents));
     printf("Execution Time: Total   : %50s Time: %'10d us\n", "Total time", (int)(timerTotal.GetElapsedTime() * 1000000 / mStatNEvents));
+  } else if (GetDeviceProcessingSettings().debugLevel >= 0) {
+    printf("Total Time: %'d us\n", (int)(timerTotal.GetElapsedTime() * 1000000 / mStatNEvents));
   }
   if (mDeviceProcessingSettings.resetTimers) {
     mStatNEvents = 0;
