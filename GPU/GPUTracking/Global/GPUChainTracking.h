3b16715e4906cc8c53aa6c6d3ce3423a21d5f365
David Rohr <drohr@jwdt.org>
2020-03-01 06:35:11 +0100

    GPU: Add support to dump/load TPC ZS pages in standalone benchmark

--- GPU/GPUTracking/Global/GPUChainTracking.h
+++ GPU/GPUTracking/Global/GPUChainTracking.h
@@ -81,6 +81,9 @@ class GPUChainTracking : public GPUChain, GPUReconstructionHelpers::helperDelega
     InOutMemory(InOutMemory&&);
     InOutMemory& operator=(InOutMemory&&);
 
+    std::unique_ptr<char[]> tpcZSpages;
+    std::unique_ptr<GPUTrackingInOutZS> tpcZSmeta;
+    std::unique_ptr<GPUTrackingInOutZS::GPUTrackingInOutZSMeta> tpcZSmeta2;
     std::unique_ptr<deprecated::PackedDigit[]> tpcDigits[NSLICES];
     std::unique_ptr<GPUTPCClusterData[]> clusterData[NSLICES];
     std::unique_ptr<AliHLTTPCRawCluster[]> rawClusters[NSLICES];
