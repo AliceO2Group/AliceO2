a5eaf48aaa500073a76b21d9f2c59b571aa57f10
Sandro Wenzel <sandro.wenzel@cern.ch>
2020-03-03 14:13:00 +0100

    Simcuts for passive volumes

--- Detectors/Passive/include/DetectorsPassive/FrameStructure.h
+++ Detectors/Passive/include/DetectorsPassive/FrameStructure.h
@@ -17,7 +17,7 @@
 #ifndef O2_PASSIVE_FRAMESTRUCTURE_H
 #define O2_PASSIVE_FRAMESTRUCTURE_H
 
-#include <FairModule.h>
+#include "DetectorsPassive/PassiveBase.h"
 
 class TGeoCompositeShape;
 
@@ -26,7 +26,7 @@ namespace o2
 namespace passive
 {
 // class supposed to provide the frame support structure common to TOF and TRD
-class FrameStructure : public FairModule
+class FrameStructure : public PassiveBase
 {
  public:
   FrameStructure(const char* name, const char* title = "FrameStruct");
