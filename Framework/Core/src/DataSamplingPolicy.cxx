cab3acd0ab35b2b8d6d243b6261de3b1346ba03d
Piotr Konopka <piotr.jan.konopka@cern.ch>
2020-03-05 10:53:49 +0100

    Data Sampling - delete deprecated methods of configuring inputs

--- Framework/Core/src/DataSamplingPolicy.cxx
+++ Framework/Core/src/DataSamplingPolicy.cxx
@@ -47,29 +47,7 @@ void DataSamplingPolicy::configure(const ptree& config)
 
   mPaths.clear();
   size_t outputId = 0;
-  std::vector<InputSpec> inputSpecs;
-
-  if (config.get_optional<std::string>("query").has_value()) {
-    inputSpecs = DataDescriptorQueryBuilder::parse(config.get<std::string>("query").c_str());
-  } else {
-    // for a while, we leave an old way of specyfing the inputs, so we can gracefully update QC
-    LOG(WARN) << "Specifying policy inputs by dataHeaders structures is deprecated and "
-                 "soon it will not be possible anymore. Please use the queries mechanism.";
-    for (const auto& dataHeaderConfig : config.get_child("dataHeaders")) {
-
-      header::DataOrigin origin;
-      header::DataDescription description;
-      origin.runtimeInit(dataHeaderConfig.second.get<std::string>("dataOrigin").c_str());
-      description.runtimeInit(dataHeaderConfig.second.get<std::string>("dataDescription").c_str());
-
-      std::string binding = dataHeaderConfig.second.get<std::string>("binding");
-      if (subSpec == -1) {
-        inputSpecs.push_back({binding, {origin, description}});
-      } else {
-        inputSpecs.push_back({binding, origin, description, static_cast<o2::header::DataHeader::SubSpecificationType>(subSpec)});
-      }
-    }
-  }
+  std::vector<InputSpec> inputSpecs = DataDescriptorQueryBuilder::parse(config.get<std::string>("query").c_str());
 
   for (const auto& inputSpec : inputSpecs) {
 
