4da8f22c9df6acbf44b6d8072666337d15f20003
shahoian <ruben.shahoyan@cern.ch>
2020-03-08 18:33:35 +0100

    TOF LHCClock calibration workflow

--- Detectors/TOF/calibration/src/TOFCalibrationLinkDef.h
+++ Detectors/TOF/calibration/src/TOFCalibrationLinkDef.h
@@ -18,4 +18,9 @@
 #pragma link C++ class o2::globaltracking::CalibTOF + ;
 #pragma link C++ class o2::globaltracking::CollectCalibInfoTOF + ;
 
+#pragma link C++ class o2::tof::LHCClockDataHisto + ;
+#pragma link C++ class o2::calibration::TimeSlot < o2::tof::LHCClockDataHisto> + ;
+#pragma link C++ class o2::calibration::TimeSlotCalibration < o2::dataformats::CalibInfoTOF, o2::tof::LHCClockDataHisto> + ;
+#pragma link C++ class o2::tof::LHCClockCalibrator + ;
+
 #endif
