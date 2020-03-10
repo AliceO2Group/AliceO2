041df8c829dd8cbddc14af82a1d7dd451d3015be
Giulio Eulisse <giulio.eulisse@cern.ch>
2020-03-10 13:53:11 +0100

    Update test_SimpleWildcard02.cxx

--- Framework/Core/test/test_SimpleWildcard02.cxx
+++ Framework/Core/test/test_SimpleWildcard02.cxx
@@ -8,7 +8,6 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-#include "Framework/RootSerializationSupport.h"
 #include "Framework/runDataProcessing.h"
 #include "Framework/CallbackService.h"
 #include "Framework/ControlService.h"
