7f606461f39963ff89d8c840de29391fcc5222b3
Roberto Preghenella <preghenella@bo.infn.it>
2020-03-11 15:39:15 +0530

    New functionalities: DeepTrigger, Pythia8::UserHooks, Pythia6 (#3037)
    * New functionalities: DeepTrigger, Pythia8::UserHooks, Pythia6
    
    * Update detector simulation README

--- Generators/include/Generators/GeneratorPythia6Param.h
+++ Generators/include/Generators/GeneratorPythia6Param.h
@@ -10,12 +10,11 @@
 
 /// \author R+Preghenella - January 2020
 
-#ifndef ALICEO2_EVENTGEN_GENERATORPYTHIA8PARAM_H_
-#define ALICEO2_EVENTGEN_GENERATORPYTHIA8PARAM_H_
+#ifndef ALICEO2_EVENTGEN_GENERATORPYTHIA6PARAM_H_
+#define ALICEO2_EVENTGEN_GENERATORPYTHIA6PARAM_H_
 
 #include "SimConfig/ConfigurableParam.h"
 #include "SimConfig/ConfigurableParamHelper.h"
-#include <string>
 
 namespace o2
 {
@@ -24,18 +23,20 @@ namespace eventgen
 
 /**
  ** a parameter class/struct to keep the settings of
- ** the Pythia8 event generator and
+ ** the pythia6 event-generator
  ** allow the user to modify them 
  **/
-
-struct GeneratorPythia8Param : public o2::conf::ConfigurableParamHelper<GeneratorPythia8Param> {
-  std::string config = "";
-  std::string hooksFileName = "";
-  std::string hooksFuncName = "";
-  O2ParamDef(GeneratorPythia8Param, "Pythia8");
+struct GeneratorPythia6Param : public o2::conf::ConfigurableParamHelper<GeneratorPythia6Param> {
+  std::string config;
+  int tune = 350;
+  std::string frame = "CMS";
+  std::string beam = "p";
+  std::string target = "p";
+  double win = 14000.;
+  O2ParamDef(GeneratorPythia6Param, "Pythia6");
 };
 
 } // end namespace eventgen
 } // end namespace o2
 
-#endif // ALICEO2_EVENTGEN_GENERATORPYTHIA8PARAM_H_
+#endif // ALICEO2_EVENTGEN_GENERATORPYTHIA6PARAM_H_
