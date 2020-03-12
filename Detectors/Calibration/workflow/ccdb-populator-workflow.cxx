28f3a0602fd89887dca62199ddd613d6643681d6
shahoian <ruben.shahoyan@cern.ch>
2020-03-12 10:35:56 +0100

    apply the completion policy to right component

--- Detectors/Calibration/workflow/ccdb-populator-workflow.cxx
+++ Detectors/Calibration/workflow/ccdb-populator-workflow.cxx
@@ -19,7 +19,7 @@ void customize(std::vector<o2::framework::CompletionPolicy>& policies)
   // we customize the pipeline processors to consume data as it comes
   using CompletionPolicy = o2::framework::CompletionPolicy;
   using CompletionPolicyHelpers = o2::framework::CompletionPolicyHelpers;
-  policies.push_back(CompletionPolicyHelpers::defineByName("lhc-clockphase-workflow.*", CompletionPolicy::CompletionOp::Consume));
+  policies.push_back(CompletionPolicyHelpers::defineByName("ccdb-populator.*", CompletionPolicy::CompletionOp::Consume));
 }
 
 // ------------------------------------------------------------------
