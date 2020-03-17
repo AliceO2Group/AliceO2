948cad8134f226283bd226c928d79bc5e8a4eef0
wiechula <Jens.Wiechula@cern.ch>
2020-03-17 22:22:21 +0100

    Add possibility to upload objects to CCDB

--- Detectors/TPC/base/src/CDBInterface.cxx
+++ Detectors/TPC/base/src/CDBInterface.cxx
@@ -12,12 +12,15 @@
 /// \brief Simple interface to the CDB manager
 /// \author Jens Wiechula, Jens.Wiechula@ikf.uni-frankfurt.de
 
+// system includes
+#include <cxxabi.h>
+#include <ctime>
+#include <fmt/format.h>
+#include <fmt/chrono.h>
+
 // root includes
 #include "TFile.h"
 #include "TRandom.h"
-
-// fairroot includes
-#include "FairLogger.h"
 
 // o2 includes
 #include "TPCBase/CDBInterface.h"
@@ -27,6 +30,18 @@
 #include "TPCBase/ParameterGas.h"
 
 using namespace o2::tpc;
+
+// anonymous namespace to prevent usage outside of this file
+namespace
+{
+/// utility function to demangle cxx type names
+std::string demangle(std::string_view name)
+{
+  int status = -4; // some arbitrary value to eliminate the compiler warning
+  std::unique_ptr<char, void (*)(void*)> res{abi::__cxa_demangle(name.data(), nullptr, nullptr, &status), std::free};
+  return (status == 0) ? res.get() : name.data();
+}
+} // end anonymous namespace
 
 //______________________________________________________________________________
 const CalPad& CDBInterface::getPedestals()
@@ -41,7 +56,7 @@ const CalPad& CDBInterface::getPedestals()
       createDefaultPedestals();
   } else {
     // return from CDB, assume that check for object existence are done there
-    return getObjectFromCDB<CalPad>("TPC/Calib/Pedestals");
+    return getObjectFromCDB<CalPad>(CDBTypeMap.at(CDBType::CalPedestal));
   }
 
   if (!mPedestals) {
@@ -64,7 +79,7 @@ const CalPad& CDBInterface::getNoise()
       createDefaultNoise();
   } else {
     // return from CDB, assume that check for object existence are done there
-    return getObjectFromCDB<CalPad>("TPC/Calib/Noise");
+    return getObjectFromCDB<CalPad>(CDBTypeMap.at(CDBType::CalNoise));
   }
 
   if (!mNoise) {
@@ -87,7 +102,7 @@ const CalPad& CDBInterface::getGainMap()
       createDefaultGainMap();
   } else {
     // return from CDB, assume that check for object existence are done there
-    return getObjectFromCDB<CalPad>("TPC/Calib/Gain");
+    return getObjectFromCDB<CalPad>(CDBTypeMap.at(CDBType::CalPadGainFull));
   }
 
   if (!mGainMap) {
@@ -105,7 +120,7 @@ const ParameterDetector& CDBInterface::getParameterDetector()
   }
 
   // return from CDB, assume that check for object existence are done there
-  return getObjectFromCDB<ParameterDetector>("TPC/Parameter/Detector");
+  return getObjectFromCDB<ParameterDetector>(CDBTypeMap.at(CDBType::ParDetector));
 }
 
 //______________________________________________________________________________
@@ -116,7 +131,7 @@ const ParameterElectronics& CDBInterface::getParameterElectronics()
   }
 
   // return from CDB, assume that check for object existence are done there
-  return getObjectFromCDB<ParameterElectronics>("TPC/Parameter/Electronics");
+  return getObjectFromCDB<ParameterElectronics>(CDBTypeMap.at(CDBType::ParElectronics));
 }
 
 //______________________________________________________________________________
@@ -127,7 +142,7 @@ const ParameterGas& CDBInterface::getParameterGas()
   }
 
   // return from CDB, assume that check for object existence are done there
-  return getObjectFromCDB<ParameterGas>("TPC/Parameter/Gas");
+  return getObjectFromCDB<ParameterGas>(CDBTypeMap.at(CDBType::ParGas));
 }
 
 //______________________________________________________________________________
@@ -138,18 +153,17 @@ const ParameterGEM& CDBInterface::getParameterGEM()
   }
 
   // return from CDB, assume that check for object existence are done there
-  return getObjectFromCDB<ParameterGEM>("TPC/Parameter/GEM");
+  return getObjectFromCDB<ParameterGEM>(CDBTypeMap.at(CDBType::ParGEM));
 }
 
 //______________________________________________________________________________
 void CDBInterface::loadNoiseAndPedestalFromFile()
 {
-  auto file = TFile::Open(mPedestalNoiseFileName.data());
+  std::unique_ptr<TFile> file(TFile::Open(mPedestalNoiseFileName.data()));
   CalPad* pedestals{nullptr};
   CalPad* noise{nullptr};
   file->GetObject("Pedestals", pedestals);
   file->GetObject("Noise", noise);
-  delete file;
 
   if (!pedestals) {
     LOG(FATAL) << "No valid pedestal object was loaded";
@@ -168,10 +182,9 @@ void CDBInterface::loadNoiseAndPedestalFromFile()
 //______________________________________________________________________________
 void CDBInterface::loadGainMapFromFile()
 {
-  auto file = TFile::Open(mGainMapFileName.data());
+  std::unique_ptr<TFile> file(TFile::Open(mGainMapFileName.data()));
   CalPad* gain{nullptr};
-  file->GetObject("Gain", gain);
-  delete file;
+  file->GetObject("GainMap", gain);
 
   if (!gain) {
     LOG(FATAL) << "No valid gain map object was loaded";
@@ -263,4 +276,121 @@ void CDBInterface::createDefaultGainMap()
       val = random;
     }
   }
+}
+
+//______________________________________________________________________________
+bool CDBStorage::checkMetaData(MetaData_t metaData) const
+{
+  if (!metaData.size()) {
+    LOGP(error, "no meta data set");
+  }
+
+  const std::array<std::string_view, 3> requirements{
+    "Required",
+    "Recommended",
+    "Optional"};
+
+  const std::array<std::vector<std::string_view>, 3> tests{{
+    {"Responsible", "Reason", "Intervention"}, //errors
+    {"JIRA"},                                  //warnings
+    {"Comment"}                                //infos
+  }};
+
+  std::array<int, 3> counts{};
+
+  for (size_t i = 0; i < requirements.size(); ++i) {
+    for (const auto& test : tests[i]) {
+      if (!metaData[test.data()].size()) {
+        const auto message = fmt::format("{} field {} not set in meta data", requirements[i], test);
+        if (i == 0) {
+          LOG(error) << message;
+        } else if (i == 1) {
+          LOG(warning) << message;
+        } else {
+          LOG(info) << message;
+        }
+        ++counts[i];
+      } else {
+        LOGP(debug, "{} field '{}' set to '{}'", requirements[i], test, metaData[test.data()]);
+      }
+    }
+  }
+
+  return counts[0] == 0;
+}
+
+//______________________________________________________________________________
+void CDBStorage::uploadNoiseAndPedestal(std::string_view fileName, long first, long last)
+{
+  std::unique_ptr<TFile> file(TFile::Open(fileName.data()));
+  CalPad* pedestals{nullptr};
+  CalPad* noise{nullptr};
+  file->GetObject("Pedestals", pedestals);
+  file->GetObject("Noise", noise);
+
+  if (!pedestals) {
+    LOGP(fatal, "No valid pedestal object was loaded from file {}", fileName);
+  }
+
+  if (!noise) {
+    LOGP(fatal, "No valid noise object was loaded from file {}", fileName);
+  }
+
+  storeObject(pedestals, CDBType::CalPedestal, first, last);
+  storeObject(noise, CDBType::CalNoise, first, last);
+}
+
+//______________________________________________________________________________
+void CDBStorage::uploadGainMap(std::string_view fileName, bool isFull, long first, long last)
+{
+  std::unique_ptr<TFile> file(TFile::Open(fileName.data()));
+  CalPad* gain{nullptr};
+  file->GetObject("GainMap", gain);
+
+  if (!gain) {
+    LOG(FATAL) << "No valid gain map object was loaded";
+  }
+
+  storeObject(gain, isFull ? CDBType::CalPadGainFull : CDBType::CalPadGainResidual, first, last);
+}
+
+//______________________________________________________________________________
+void CDBStorage::uploadPulserData(std::string_view fileName, long first, long last)
+{
+  std::unique_ptr<TFile> f(TFile::Open(fileName.data()));
+  CalDet<float>*t0 = nullptr, *width = nullptr, *qtot = nullptr;
+  f->GetObject("T0", t0);
+  f->GetObject("Width", width);
+  f->GetObject("Qtot", qtot);
+
+  if (!t0 || !width || !qtot) {
+    LOGP(fatal, "Missing pulser object in file {}: T0 ({}), Width ({}), Qtot({})", fileName, (void*)t0, (void*)width, (void*)qtot);
+  }
+
+  std::unordered_map<std::string, CalDet<float>> pulserCalib;
+  pulserCalib["T0"] = *t0;
+  pulserCalib["Width"] = *width;
+  pulserCalib["Qtot"] = *qtot;
+
+  storeObject(&pulserCalib, CDBType::CalPulser, first, last);
+}
+
+//______________________________________________________________________________
+void CDBStorage::printObjectSummary(std::string_view name, CDBType const type, MetaData_t const& metadata, long start, long end) const
+{
+  std::time_t tstart(start);
+  std::time_t tend(end);
+
+  std::string message = fmt::format("Writing object of type '{}'\n", demangle(name)) +
+                        fmt::format("          to storage '{}'\n", mCCDB.getURL()) +
+                        fmt::format("          into path '{}'\n", CDBTypeMap.at(type)) +
+                        fmt::format("          with validity [{}, {}] :", start, end) +
+                        fmt::format("          [{:%d.%m.%Y %H:%M:%S}, {:%d.%m.%Y %H:%M:%S}]\n", *std::localtime(&tstart), *std::localtime(&tend)) +
+                        std::string("          Meta data:\n");
+
+  for (const auto& [key, value] : metadata) {
+    message += fmt::format("{:>20} = {}\n", key, value);
+  }
+
+  LOGP(info, message);
 }
