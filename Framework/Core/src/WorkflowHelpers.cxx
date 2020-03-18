17f98023d9a8aae3d324086ae4de5b6681e814ef
Paul Buehler <paul.buehler@oeaw.ac.at>
2020-03-18 16:06:27 +0100

    corrctions

--- Framework/Core/src/WorkflowHelpers.cxx
+++ Framework/Core/src/WorkflowHelpers.cxx
@@ -723,25 +723,30 @@ std::vector<InputSpec> WorkflowHelpers::computeDanglingOutputs(WorkflowSpec cons
 
 std::vector<InputSpec> WorkflowHelpers::computeAODOutputs(WorkflowSpec const& workflow)
 {
-  LOG(INFO) << "Selecting OutputSpecs of origin AOD - " << specs.size();
+  LOG(INFO) << "Selecting OutputSpecs of origin AOD";
 
   std::vector<InputSpec> results;
 
   /// Prepare an index to do the iterations quickly.
-  for (size_t wi = 0, we = workflow.size(); wi != we; ++wi) {
+  for (size_t wi = 0; wi<workflow.size(); wi++) {
     auto& wfspec = workflow[wi];
         
     for (size_t oi = 0; oi<wfspec.outputs.size(); oi++) {
-      auto outspec = wfspec.outputs[oi];
+      auto output = wfspec.outputs[oi];
       
       // add it to the AOD list ...
-      if (DataSpecUtils::partialMatch(*outspec, header::DataOrigin("AOD")))
-        results.emplace_back(*outspec);
+      if (DataSpecUtils::partialMatch(output, header::DataOrigin("AOD"))) {
+        auto input = DataSpecUtils::matchingInput(output);
+        char buf[64];
+        input.binding = (snprintf(buf, 63, "aod_%zu_%zu", wi, oi), buf);
+
+        results.emplace_back(input);
+      }
 
     }
   }
 
-  LOG(INFO) << "Number of AODs " << results.size() << " - " << specs.size() << std::endl;
+  LOG(INFO) << "Number of AODs " << results.size();
   return results;
 }
 
