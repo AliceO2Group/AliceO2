1b817e50e6177af979185026a2225b5df910c3ee
rpezzi <rafael.pezzi@cern.ch>
2020-03-24 14:40:50 -0300

    [MFT] New configurable parameters for MFT geometry builder (#3194)
    * [MFT] Configurable parameters for geometry builder
    
    Missing features added to supports + small bug fixes
    
    * Full MFT is built by default

--- Detectors/ITSMFT/MFT/base/src/GeometryBuilder.cxx
+++ Detectors/ITSMFT/MFT/base/src/GeometryBuilder.cxx
@@ -21,6 +21,7 @@
 #include "MFTBase/HalfCone.h"
 #include "MFTBase/Barrel.h"
 #include "MFTBase/PatchPanel.h"
+#include "MFTBase/MFTBaseParam.h"
 
 #include "TGeoVolume.h"
 #include "TGeoManager.h"
@@ -37,7 +38,7 @@ void GeometryBuilder::buildGeometry()
 {
 
   Geometry* mftGeo = Geometry::instance();
-
+  auto& mftBaseParam = MFTBaseParam::Instance();
   TGeoVolume* volMFT = new TGeoVolumeAssembly(GeometryTGeo::getMFTVolPattern());
 
   LOG(INFO) << "GeometryBuilder::buildGeometry volume name = " << GeometryTGeo::getMFTVolPattern();
@@ -60,38 +61,44 @@ void GeometryBuilder::buildGeometry()
   }
 
   /// \todo Add the service, Barrel, etc Those objects will probably be defined into the COMMON ITSMFT area.
+  if (!mftBaseParam.minimal && mftBaseParam.buildCone) {
+    auto* halfCone = new HalfCone();
+    TGeoVolumeAssembly* halfCone1 = halfCone->createHalfCone(0);
+    TGeoVolumeAssembly* halfCone2 = halfCone->createHalfCone(1);
+    volMFT->AddNode(halfCone1, 1);
+    volMFT->AddNode(halfCone2, 1);
+  }
 
-  auto* halfCone = new HalfCone();
-  TGeoVolumeAssembly* halfCone1 = halfCone->createHalfCone(0);
-  TGeoVolumeAssembly* halfCone2 = halfCone->createHalfCone(1);
-  volMFT->AddNode(halfCone1, 1);
-  volMFT->AddNode(halfCone2, 1);
-  //barrel services
-  auto* t_barrel0 = new TGeoTranslation("translation_barrel", 0.0, 0.7, -80.17);
-  auto* r_barrel0 = new TGeoRotation("rotation_barrel", 0.0, 0.0, 0.0);
-  auto* p_barrel0 = new TGeoCombiTrans(*t_barrel0, *r_barrel0);
-  auto* t_barrel1 = new TGeoTranslation("translation_barrel", 0.0, 0.7, -80.17);
-  auto* r_barrel1 = new TGeoRotation("rotation_barrel", 0.0, 0.0, 180.0);
-  auto* p_barrel1 = new TGeoCombiTrans(*t_barrel1, *r_barrel1);
+  if (!mftBaseParam.minimal && mftBaseParam.buildBarrel) {
+    //barrel services
+    auto* t_barrel0 = new TGeoTranslation("translation_barrel", 0.0, 0.7, -80.17);
+    auto* r_barrel0 = new TGeoRotation("rotation_barrel", 0.0, 0.0, 0.0);
+    auto* p_barrel0 = new TGeoCombiTrans(*t_barrel0, *r_barrel0);
+    auto* t_barrel1 = new TGeoTranslation("translation_barrel", 0.0, 0.7, -80.17);
+    auto* r_barrel1 = new TGeoRotation("rotation_barrel", 0.0, 0.0, 180.0);
+    auto* p_barrel1 = new TGeoCombiTrans(*t_barrel1, *r_barrel1);
 
-  auto* halfBarrel = new Barrel();
-  TGeoVolumeAssembly* halfBarrel0 = halfBarrel->createBarrel();
-  volMFT->AddNode(halfBarrel0, 1, p_barrel0);
-  TGeoVolumeAssembly* halfBarrel1 = halfBarrel->createBarrel();
-  volMFT->AddNode(halfBarrel1, 1, p_barrel1);
+    auto* halfBarrel = new Barrel();
+    TGeoVolumeAssembly* halfBarrel0 = halfBarrel->createBarrel();
+    volMFT->AddNode(halfBarrel0, 1, p_barrel0);
+    TGeoVolumeAssembly* halfBarrel1 = halfBarrel->createBarrel();
+    volMFT->AddNode(halfBarrel1, 1, p_barrel1);
+  }
 
-  auto* t_patchpanel0 = new TGeoTranslation("translation_patchpanel", 0.0, 0., -81.5); //z (0,0.7, -81.5 -1.3; 0..81.7 --1.5
-  auto* r_patchpanel0 = new TGeoRotation("rotation_patchpanel", 0.0, 0.0, 0.0);
-  auto* p_patchpanel0 = new TGeoCombiTrans(*t_patchpanel0, *r_patchpanel0);
-  auto* t_patchpanel1 = new TGeoTranslation("translation_patchpanel", 0.0, 0., -81.5); //z( 0, 0.7, -81.5-1.3; 0.. 81.7 --1.5
-  auto* r_patchpanel1 = new TGeoRotation("rotation_patchpanel", 0.0, 0.0, 180.0);
-  auto* p_patchpanel1 = new TGeoCombiTrans(*t_patchpanel1, *r_patchpanel1);
+  if (!mftBaseParam.minimal && mftBaseParam.buildPatchPanel) {
+    auto* t_patchpanel0 = new TGeoTranslation("translation_patchpanel", 0.0, 0., -81.5); //z (0,0.7, -81.5 -1.3; 0..81.7 --1.5
+    auto* r_patchpanel0 = new TGeoRotation("rotation_patchpanel", 0.0, 0.0, 0.0);
+    auto* p_patchpanel0 = new TGeoCombiTrans(*t_patchpanel0, *r_patchpanel0);
+    auto* t_patchpanel1 = new TGeoTranslation("translation_patchpanel", 0.0, 0., -81.5); //z( 0, 0.7, -81.5-1.3; 0.. 81.7 --1.5
+    auto* r_patchpanel1 = new TGeoRotation("rotation_patchpanel", 0.0, 0.0, 180.0);
+    auto* p_patchpanel1 = new TGeoCombiTrans(*t_patchpanel1, *r_patchpanel1);
 
-  auto* halfpatchpanel = new PatchPanel();
-  TGeoVolumeAssembly* halfpatchpanel0 = halfpatchpanel->createPatchPanel();
-  TGeoVolumeAssembly* halfpatchpanel1 = halfpatchpanel->createPatchPanel();
-  volMFT->AddNode(halfpatchpanel0, 1, p_patchpanel0);
-  volMFT->AddNode(halfpatchpanel1, 1, p_patchpanel1);
+    auto* halfpatchpanel = new PatchPanel();
+    TGeoVolumeAssembly* halfpatchpanel0 = halfpatchpanel->createPatchPanel();
+    TGeoVolumeAssembly* halfpatchpanel1 = halfpatchpanel->createPatchPanel();
+    volMFT->AddNode(halfpatchpanel0, 1, p_patchpanel0);
+    volMFT->AddNode(halfpatchpanel1, 1, p_patchpanel1);
+  }
 
   vALIC->AddNode(volMFT, 0);
 }
