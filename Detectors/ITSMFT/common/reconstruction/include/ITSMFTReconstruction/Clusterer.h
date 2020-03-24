51eae1d9a6411f073e0eb582f62394e853033c8a
Iouri Belikov <iouri.belikov@cern.ch>
2020-03-24 16:01:31 +0100

    Removing cluster indices from the stream of cluster patterns

--- Detectors/ITSMFT/common/reconstruction/include/ITSMFTReconstruction/Clusterer.h
+++ Detectors/ITSMFT/common/reconstruction/include/ITSMFTReconstruction/Clusterer.h
@@ -106,8 +106,8 @@ class Clusterer
     mPattIdConverter.loadDictionary(fileName);
   }
 
-  void setPatterns(std::vector<o2::itsmft::ClusterTopology>* patt) { mPatterns = patt; }
-  std::vector<o2::itsmft::ClusterTopology>* getPatterns() const { return mPatterns; }
+  void setPatterns(std::vector<o2::itsmft::ClusterPattern>* patt) { mPatterns = patt; }
+  std::vector<o2::itsmft::ClusterPattern>* getPatterns() const { return mPatterns; }
 
  private:
   void initChip(UInt_t first);
@@ -235,7 +235,7 @@ class Clusterer
 
   LookUp mPattIdConverter; //! Convert the cluster topology to the corresponding entry in the dictionary.
 
-  std::vector<o2::itsmft::ClusterTopology>* mPatterns = nullptr; // Not owned
+  std::vector<o2::itsmft::ClusterPattern>* mPatterns = nullptr; // Not owned
 
 #ifdef _PERFORM_TIMING_
   TStopwatch mTimer;
