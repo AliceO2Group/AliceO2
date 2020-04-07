9fd332c0eba67f1f9c97e43488cfd7c54978a171
shahoian <ruben.shahoyan@cern.ch>
2020-04-07 20:09:36 +0200

    promote NameConf::pathExists to class method + extra names

--- DataFormats/Detectors/Common/src/NameConf.cxx
+++ DataFormats/Detectors/Common/src/NameConf.cxx
@@ -11,22 +11,43 @@
 #include "DetectorsCommonDataFormats/NameConf.h"
 #include <sys/stat.h>
 
-bool pathExists(const std::string_view p)
+using namespace o2::base;
+using DId = o2::detectors::DetID;
+
+// Check if the path exists
+bool NameConf::pathExists(const std::string_view p)
 {
   struct stat buffer;
   return (stat(p.data(), &buffer) == 0);
 }
 
-using namespace o2::base;
+// Check if the path is a directory
+bool NameConf::pathIsDirectory(const std::string_view p)
+{
+  struct stat buffer;
+  return (stat(p.data(), &buffer) == 0) && S_ISDIR(buffer.st_mode);
+}
 
 // Filename to store geometry file
 std::string NameConf::getGeomFileName(const std::string_view prefix)
 {
   // check if the prefix is an existing path
-  const bool prefixispath = pathExists(prefix);
+  const bool prefixispath = pathIsDirectory(prefix);
   if (prefixispath) {
     return o2::utils::concat_string(prefix, "/", STANDARDSIMPREFIX, "_", GEOM_FILE_STRING, ".root");
   } else {
     return o2::utils::concat_string(prefix.empty() ? STANDARDSIMPREFIX : prefix, "_", GEOM_FILE_STRING, ".root");
+  }
+}
+
+// Filename to store geometry file
+std::string NameConf::getDictionaryFileName(DId det, const std::string_view prefix, const std::string_view ext)
+{
+  // check if the prefix is an existing path
+  const bool prefixispath = pathIsDirectory(prefix);
+  if (prefixispath) {
+    return o2::utils::concat_string(prefix, "/", det.getName(), DICTFILENAME, ext);
+  } else {
+    return o2::utils::concat_string(prefix, det.getName(), DICTFILENAME, ext);
   }
 }
