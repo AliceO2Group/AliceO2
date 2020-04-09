99185593b835fd4470eb81bb9ca4f13ec92d1bce
sgorbuno <sergey.gorbunov@fias.uni-frankfurt.de>
2020-04-09 18:50:43 +0000

    reorganization of splines

--- GPU/Common/GPUCommonMath.h
+++ GPU/Common/GPUCommonMath.h
@@ -53,6 +53,8 @@ class GPUCommonMath
   GPUhdni() static float ATan2(float y, float x);
   GPUhdni() static float Sin(float x);
   GPUhdni() static float Cos(float x);
+  GPUhdni() static void SinCos(float x, float& s, float& c);
+  GPUhdni() static void SinCos(double x, double& s, double& c);
   GPUhdni() static float Tan(float x);
   GPUhdni() static float Copysign(float x, float y);
   GPUhdni() static float TwoPi() { return 6.28319f; }
@@ -149,6 +151,28 @@ GPUhdi() float GPUCommonMath::Sin(float x) { return CHOICE(sinf(x), sinf(x), sin
 
 GPUhdi() float GPUCommonMath::Cos(float x) { return CHOICE(cosf(x), cosf(x), cos(x)); }
 
+GPUhdi() void GPUCommonMath::SinCos(float x, float& s, float& c)
+{
+#if !defined(GPUCA_GPUCODE_DEVICE) && defined(__APPLE__)
+  __sincosf(x, &s, &c);
+#elif !defined(GPUCA_GPUCODE_DEVICE) && defined(__GNU_SOURCE__)
+  sincosf(x, &s, &c);
+#else
+  CHOICE({s = sin(x); c = cos(x); }, sincosf(x, &s, &c), s = sincos(x, &c));
+#endif
+}
+
+GPUhdi() void GPUCommonMath::SinCos(double x, double& s, double& c)
+{
+#if !defined(GPUCA_GPUCODE_DEVICE) && defined(__APPLE__)
+  __sincos(x, &s, &c);
+#elif !defined(GPUCA_GPUCODE_DEVICE) && defined(__GNU_SOURCE__)
+  sincos(x, &s, &c);
+#else
+  CHOICE({s = sin(x); c = cos(x); }, sincos(x, &s, &c), s = sincos(x, &c));
+#endif
+}
+
 GPUhdi() float GPUCommonMath::Tan(float x) { return CHOICE(tanf(x), tanf(x), tan(x)); }
 
 GPUhdi() unsigned int GPUCommonMath::Clz(unsigned int x)
@@ -168,7 +192,7 @@ GPUhdi() unsigned int GPUCommonMath::Clz(unsigned int x)
 GPUhdi() unsigned int GPUCommonMath::Popcount(unsigned int x)
 {
 #if (defined(__GNUC__) || defined(__clang__) || defined(__CUDACC__) || defined(__HIPCC__)) && (!defined(__OPENCL__) /*|| defined(__OPENCLCPP__)*/) // TODO: remove OPENCLCPP workaround when reported SPIR-V bug is fixed
-  return CHOICE(__builtin_popcount(x), __popc(x), __builtin_popcount(x)); // use builtin if available
+  return CHOICE(__builtin_popcount(x), __popc(x), __builtin_popcount(x));                                                                          // use builtin if available
 #else
   unsigned int retVal = 0;
   for (int i = 0; i < 32; i++) {
