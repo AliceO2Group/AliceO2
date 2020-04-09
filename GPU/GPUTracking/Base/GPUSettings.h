8c98711ba4586794312458f56a4e8164a4709ff3
David Rohr <drohr@jwdt.org>
2020-04-09 17:26:36 +0200

    GPU: Make fit in projection settable via rec param

--- GPU/GPUTracking/Base/GPUSettings.h
+++ GPU/GPUTracking/Base/GPUSettings.h
@@ -80,6 +80,7 @@ struct GPUSettingsRec {
   unsigned char dropLoopers;             // Drop all clusters after starting from the second loop from tracks
   unsigned char mergerCovSource;         // 0 = simpleFilterErrors, 1 = use from track following
   unsigned char mergerInterpolateErrors; // Use interpolation for cluster rejection based on chi-2 instead of extrapolation
+  char fitInProjections;                 // -1 for automatic
 };
 
 // Settings describing the events / time frames
