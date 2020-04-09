0e9510b2688d1ab0b4adac6d45e5b677f31b85d6
David Rohr <drohr@jwdt.org>
2020-04-09 17:26:36 +0200

    GPU: Make fit in projection settable via rec param

--- GPU/GPUTracking/Merger/GPUTPCGMTrackParam.cxx
+++ GPU/GPUTracking/Merger/GPUTPCGMTrackParam.cxx
@@ -106,7 +106,7 @@ GPUd() bool GPUTPCGMTrackParam::Fit(const GPUTPCGMMerger* GPUrestrict() merger, 
     prop.SetSeedingErrors(!(refit && attempt == 0));
 
     ResetCovariance();
-    prop.SetFitInProjections(iWay != 0);
+    prop.SetFitInProjections(param.rec.fitInProjections == -1 ? (iWay != 0) : param.rec.fitInProjections);
     prop.SetTrack(this, iWay ? prop.GetAlpha() : Alpha);
     ConstrainSinPhi(prop.GetFitInProjections() ? 0.95f : GPUCA_MAX_SIN_PHI_LOW);
     CADEBUG(printf("Fitting track %d way %d (sector %d, alpha %f)\n", iTrk, iWay, (int)(prop.GetAlpha() / kSectAngle + 0.5) + (mP[1] < 0 ? 18 : 0), prop.GetAlpha()));
