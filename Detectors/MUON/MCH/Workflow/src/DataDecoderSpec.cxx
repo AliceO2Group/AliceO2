854f304ddd16cbc9cca63ba7bb7b25cb3bf1b758
alice-mch <andrea.ferrero@cern.ch>
2020-04-11 17:55:22 +0200

    [MCH] replaced manu2ds by ds2manu and updated documentation Previously the code was applying the channel number remapping in the wrong direction

--- Detectors/MUON/MCH/Workflow/src/DataDecoderSpec.cxx
+++ Detectors/MUON/MCH/Workflow/src/DataDecoderSpec.cxx
@@ -9,16 +9,10 @@
 // or submit itself to any jurisdiction.
 
 ///
-/// \file    runFileReader.cxx
+/// \file    DatDecoderSpec.cxx
 /// \author  Andrea Ferrero
 ///
-/// \brief This is an executable that reads a data file from disk and sends the data to QC via DPL.
-///
-/// This is an executable that reads a data file from disk and sends the data to QC via the Data Processing Layer.
-/// It can be used as a data source for QC development. For example, one can do:
-/// \code{.sh}
-/// o2-qc-run-file-reader --infile=some_data_file | o2-qc --config json://${QUALITYCONTROL_ROOT}/etc/your_config.json
-/// \endcode
+/// \brief Implementation of a data processor to run the raw decoding
 ///
 
 #include <random>
@@ -44,9 +38,6 @@
 #include "MCHRawCommon/RDHManip.h"
 
 #include "MCHWorkflow/DataDecoderSpec.h"
-//#include "RawBufferDecoder.h"
-
-// Dans ce code, on récupère un infut aui est un message avec le buffer, on fait tourner le code de base decodeBuffer qui est dans Handlers, et on renvoir un message de sortie (inspiré de FileReader de Andrea)
 
 namespace o2::header
 {
@@ -66,14 +57,18 @@ using namespace o2::mch::mapping;
 using RDHv4 = o2::header::RAWDataHeaderV4;
 
 std::array<int, 64> refManu2ds_st345 = {
-  63, 62, 61, 60, 59, 57, 56, 53, 51, 50, 47, 45, 44, 41, 38, 35,
-  36, 33, 34, 37, 32, 39, 40, 42, 43, 46, 48, 49, 52, 54, 55, 58,
-  7, 8, 5, 2, 6, 1, 3, 0, 4, 9, 10, 15, 17, 18, 22, 25,
-  31, 30, 29, 28, 27, 26, 24, 23, 20, 21, 16, 19, 12, 14, 11, 13};
+    63, 62, 61, 60, 59, 57, 56, 53, 51, 50, 47, 45, 44, 41, 38, 35,
+    36, 33, 34, 37, 32, 39, 40, 42, 43, 46, 48, 49, 52, 54, 55, 58,
+    7, 8, 5, 2, 6, 1, 3, 0, 4, 9, 10, 15, 17, 18, 22, 25,
+    31, 30, 29, 28, 27, 26, 24, 23, 20, 21, 16, 19, 12, 14, 11, 13};
+std::array<int, 64> refDs2manu_st345;
 
-int manu2ds(int i)
-{
+int manu2ds(int i){
   return refManu2ds_st345[i];
+}
+
+int ds2manu(int i){
+  return refDs2manu_st345[i];
 }
 
 class DataDecoderTask
@@ -83,9 +78,9 @@ class DataDecoderTask
     size_t ndigits{0};
 
     auto channelHandler = [&](DsElecId dsElecId, uint8_t channel, o2::mch::raw::SampaCluster sc) {
-      auto s = asString(dsElecId);
-      channel = manu2ds(int(channel));
+      if(mDs2manu) channel = ds2manu(int(channel));
       if (mPrint) {
+        auto s = asString(dsElecId);
         auto ch = fmt::format("{}-CH{}", s, channel);
         std::cout << ch << std::endl;
       }
@@ -97,7 +92,7 @@ class DataDecoderTask
 
       int deId;
       int dsIddet;
-      if (auto opt = Elec2Det(dsElecId); opt.has_value()) {
+      if (auto opt = mElec2Det(dsElecId); opt.has_value()) {
         DsDetId dsDetId = opt.value();
         dsIddet = dsDetId.dsId();
         deId = dsDetId.deId();
@@ -106,8 +101,6 @@ class DataDecoderTask
       int padId = -1;
       try {
         const Segmentation& segment = segmentation(deId);
-        //Segmentation segment(deId);
-
         padId = segment.findPadByFEE(dsIddet, int(channel));
         if (mPrint)
           std::cout << "DS " << (int)dsElecId.elinkId() << "  CHIP " << ((int)channel) / 32 << "  CH " << ((int)channel) % 32 << "  ADC " << digitadc << "  DE# " << deId << "  DSid " << dsIddet << "  PadId " << padId << std::endl;
@@ -118,11 +111,6 @@ class DataDecoderTask
       int time = 0;
 
       digits.emplace_back(o2::mch::Digit(time, deId, padId, digitadc));
-      //o2::mch::Digit& mchdigit = digits.back();
-      //mchdigit.setDetID(deId);
-      //mchdigit.setPadID(padId);
-      //mchdigit.setADC(digitadc);
-      //mchdigit.setTimeStamp(time);
 
       if (mPrint)
         std::cout << "DIGIT STORED:\nADC " << digits.back().getADC() << " DE# " << digits.back().getDetID() << " PadId " << digits.back().getPadID() << " time " << digits.back().getTimeStamp() << std::endl;
@@ -132,11 +120,11 @@ class DataDecoderTask
     const auto patchPage = [&](gsl::span<const std::byte> rdhBuffer) {
       auto rdhPtr = reinterpret_cast<o2::header::RAWDataHeaderV4*>(const_cast<std::byte*>(&rdhBuffer[0]));
       auto& rdh = *rdhPtr;
-      nrdhs++;
+      mNrdhs++;
       auto cruId = rdhCruId(rdh);
       rdhFeeId(rdh, cruId * 2 + rdhEndpoint(rdh));
       if (true) {
-        std::cout << nrdhs << "--" << rdh << "\n";
+        std::cout << mNrdhs << "--" << rdh << "\n";
       }
     };
 
@@ -149,10 +137,19 @@ class DataDecoderTask
   //_________________________________________________________________________________________________
   void init(framework::InitContext& ic)
   {
-    Elec2Det = createElec2DetMapper<ElectronicMapperGenerated>();
-    fee2Solar = o2::mch::raw::createFeeLink2SolarMapper<ElectronicMapperGenerated>();
-    nrdhs = 0;
+    mElec2Det = createElec2DetMapper<ElectronicMapperGenerated>();
+    mNrdhs = 0;
 
+    for (int i = 0; i < 64; i++) {
+      for (int j = 0; j < 64; j++) {
+        if (refManu2ds_st345[j] != i)
+          continue;
+        refDs2manu_st345[i] = j;
+        break;
+      }
+    }
+
+    mDs2manu = ic.options().get<bool>("ds2manu");
     mPrint = ic.options().get<bool>("print");
   }
 
@@ -170,37 +167,13 @@ class DataDecoderTask
       auto const* rdh = it.get_if<o2::header::RAWDataHeaderV4>();
       // retrieving the raw pointer of the page
       auto const* raw = it.raw();
-      // retrieving payload pointer of the page
-      //auto const* payload = it.data();
       // size of payload
       size_t payloadSize = it.size();
-      // offset of payload in the raw page
-      //size_t offset = it.offset();
 
       if (payloadSize == 0)
         continue;
 
-      //std::cout<<"\n\npayloadSize: "<<payloadSize<<std::endl;
-      //std::cout<<"raw:     "<<(void*)raw<<std::endl;
-      //std::cout<<"payload: "<<(void*)payload<<std::endl;
-
       gsl::span<const std::byte> buffer(reinterpret_cast<const std::byte*>(raw), sizeof(o2::header::RAWDataHeaderV4) + payloadSize);
-      decodeBuffer(buffer, digits);
-    }
-
-    for (auto&& input : pc.inputs()) {
-      //QcInfoLogger::GetInstance() << "run RawDataProcessor: input " << input.spec->binding << AliceO2::InfoLogger::InfoLogger::endm;
-
-      if (input.spec->binding != "readout")
-        continue;
-
-      const auto* header = o2::header::get<header::DataHeader*>(input.header);
-      if (!header)
-        continue;
-
-      //std::cout<<"payloadSize: "<<header->payloadSize<<std::endl;
-
-      gsl::span<const std::byte> buffer((const std::byte*)input.payload, header->payloadSize);
       decodeBuffer(buffer, digits);
     }
 
@@ -223,11 +196,11 @@ class DataDecoderTask
   }
 
  private:
-  std::function<std::optional<DsDetId>(DsElecId)> Elec2Det;
-  std::function<std::optional<uint16_t>(FeeLinkId id)> fee2Solar;
-  size_t nrdhs{0};
+  std::function<std::optional<DsDetId>(DsElecId)> mElec2Det;
+  size_t mNrdhs{0};
 
   std::ifstream mInputFile{}; ///< input file
+  bool mDs2manu = false;      ///< print convert channel numbering from Run3 to Run1-2 order
   bool mPrint = false;        ///< print digits
 };
 
@@ -236,12 +209,11 @@ o2::framework::DataProcessorSpec getDecodingSpec()
 {
   return DataProcessorSpec{
     "DataDecoder",
-    //Inputs{InputSpec{"readout", "ROUT", "RAWDATA", Lifetime::Timeframe}},
     o2::framework::select("TF:MCH/RAWDATA"),
-    //o2::framework::select("readout:ROUT/RAWDATA"),
     Outputs{OutputSpec{"MCH", "DIGITS", 0, Lifetime::Timeframe}},
     AlgorithmSpec{adaptFromTask<DataDecoderTask>()},
-    Options{{"print", VariantType::Bool, false, {"print digits"}}}};
+    Options{{"print", VariantType::Bool, false, {"print digits"}}, {"ds2manu", VariantType::Bool, false, {"convert channel numbering from Run3 to Run1-2 order"}}}
+  };
 }
 
 } // end namespace raw
