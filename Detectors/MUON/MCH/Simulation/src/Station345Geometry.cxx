8edb7938e9b5f88eab6818c306b2c432fe1ef199
Florian Damas <florian.damas@cern.ch>
2020-04-13 17:23:12 +0200

    Fix the placements of slat stations

--- Detectors/MUON/MCH/Simulation/src/Station345Geometry.cxx
+++ Detectors/MUON/MCH/Simulation/src/Station345Geometry.cxx
@@ -638,6 +638,9 @@ void buildHalfChambers(TGeoVolume& topVolume)
   Value& hChs = doc["HalfChambers"];
   assert(hChs.IsArray());
 
+  // mother volume to place the half-chambers
+  auto motherVolume = &topVolume;
+
   // loop over the objects (half-chambers) of the array
   for (const auto& halfCh : hChs.GetArray()) {
     // check that "halfCh" is an object
@@ -678,18 +681,26 @@ void buildHalfChambers(TGeoVolume& topVolume)
 
     } // end of the node loop
 
-    // place the half-chamber in the top volume
-    topVolume.AddNode(
+    Double_t zpos = halfCh["position"][2].GetDouble();
+
+    // change the mother volume if the Dipole and/or 'YOUT2' are present in the geometry
+    if ((nCh == 5 || nCh == 6) && gGeoManager->GetVolume("Dipole")) {
+      motherVolume = gGeoManager->GetVolume("DDIP");
+      zpos *= -1.; // z > 0 convention for dipole volumes
+    }
+
+    if (nCh > 6 && gGeoManager->GetVolume("YOUT2")) {
+      motherVolume = gGeoManager->GetVolume("YOUT2");
+    }
+
+    // place the half-chamber in the mother volume
+    motherVolume->AddNode(
       halfChVol, moduleID,
-      new TGeoCombiTrans(halfCh["position"][0].GetDouble(), halfCh["position"][1].GetDouble(), halfCh["position"][2].GetDouble(),
+      new TGeoCombiTrans(halfCh["position"][0].GetDouble(), halfCh["position"][1].GetDouble(), zpos,
                          new TGeoRotation(Form("%srotation", name.data()), halfCh["rotation"][0].GetDouble(),
                                           halfCh["rotation"][1].GetDouble(), halfCh["rotation"][2].GetDouble(),
                                           halfCh["rotation"][3].GetDouble(), halfCh["rotation"][4].GetDouble(),
                                           halfCh["rotation"][5].GetDouble())));
-
-    // if the dipole is present in the geometry, we place the station 3 half-chambers in it (actually not working)
-    if (gGeoManager->GetVolume("Dipole") && (nCh == 5 || nCh == 6))
-      topVolume.GetNode(Form("%s_%d", name.data(), moduleID))->SetMotherVolume(gGeoManager->GetVolume("DDIP"));
 
   } // end of the half-chambers loop
 }
