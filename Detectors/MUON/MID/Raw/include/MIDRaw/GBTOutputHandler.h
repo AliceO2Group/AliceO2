dc323b0f0386fcf065b2330662e1767cf1df36de
dstocco <diego.stocco@cern.ch>
2020-04-14 18:53:40 +0200

    MID raw decoders per GBT link
    This includes:
    - decoders/encoders for the CRU user logic data
    - decoders for the CRU data without the user logic

---
+++ Detectors/MUON/MID/Raw/include/MIDRaw/GBTOutputHandler.h
@@ -0,0 +1,71 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file   MIDRaw/GBTOutputHandler.h
+/// \brief  MID GBT decoder output handler
+/// \author Diego Stocco <Diego.Stocco at cern.ch>
+/// \date   14 April 2020
+#ifndef O2_MID_GBTOUTPUTHANDLER_H
+#define O2_MID_GBTOUTPUTHANDLER_H
+
+#include <cstdint>
+#include <array>
+#include <vector>
+#include "CommonDataFormat/InteractionRecord.h"
+#include "DataFormatsMID/ROFRecord.h"
+#include "MIDRaw/CrateParameters.h"
+#include "MIDRaw/ELinkDecoder.h"
+#include "MIDRaw/LocalBoardRO.h"
+
+namespace o2
+{
+namespace mid
+{
+class GBTOutputHandler
+{
+ public:
+  /// Sets the FEE Id
+  void setFeeId(uint16_t feeId) { mFeeId = feeId; }
+
+  void setIR(uint16_t bc, uint32_t orbit, int pageCnt);
+
+  void onDoneLoc(size_t ilink, const ELinkDecoder& decoder);
+  void onDoneLocDebug(size_t ilink, const ELinkDecoder& decoder);
+  void onDoneReg(size_t, const ELinkDecoder&){}; /// Dummy function
+  void onDoneRegDebug(size_t ilink, const ELinkDecoder& decoder);
+
+  /// Gets the vector of data
+  const std::vector<LocalBoardRO>& getData() const { return mData; }
+
+  /// Gets the vector of data RO frame records
+  const std::vector<ROFRecord>& getROFRecords() const { return mROFRecords; }
+
+  void clear();
+
+ private:
+  std::vector<LocalBoardRO> mData{};    /// Vector of output data
+  std::vector<ROFRecord> mROFRecords{}; /// List of ROF records
+  uint16_t mFeeId{0};                   /// FEE ID
+  InteractionRecord mIRFirstPage{};     /// Interaction record of the first page
+
+  std::array<InteractionRecord, crateparams::sNELinksPerGBT> mIRs{}; /// Interaction records per link
+  std::array<uint16_t, crateparams::sNELinksPerGBT> mCalibClocks{};  /// Calibration clock
+  std::array<uint16_t, crateparams::sNELinksPerGBT> mLastClock{};    /// Last clock per link
+
+  void addBoard(size_t ilink, const ELinkDecoder& decoder);
+  void addLoc(size_t ilink, const ELinkDecoder& decoder);
+  bool checkLoc(size_t ilink, const ELinkDecoder& decoder);
+  bool updateIR(size_t ilink, const ELinkDecoder& decoder);
+  bool invertPattern(LocalBoardRO& loc);
+};
+} // namespace mid
+} // namespace o2
+
+#endif /* O2_MID_GBTOUTPUTHANDLER_H */
---
+++ Detectors/MUON/MID/Raw/src/GBTUserLogicEncoder.cxx
@@ -0,0 +1,138 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file   MID/Raw/src/GBTUserLogicEncoder.cxx
+/// \brief  Raw data encoder for MID CRU user logic
+/// \author Diego Stocco <Diego.Stocco at cern.ch>
+/// \date   18 November 2019
+
+#include "MIDRaw/GBTUserLogicEncoder.h"
+
+#include "MIDRaw/CrateParameters.h"
+#include "RawInfo.h"
+
+namespace o2
+{
+namespace mid
+{
+
+void GBTUserLogicEncoder::addShort(uint16_t shortWord)
+{
+  /// Adds a 16 bits word
+  mBytes.emplace_back((shortWord >> 8) & 0xFF);
+  mBytes.emplace_back(shortWord & 0xFF);
+}
+
+void GBTUserLogicEncoder::addBoard(uint8_t statusWord, uint8_t triggerWord, uint16_t localClock, uint8_t id, uint8_t firedChambers)
+{
+  /// Adds the board information
+  mBytes.emplace_back(statusWord);
+  mBytes.emplace_back(triggerWord);
+  addShort(localClock);
+  addIdAndChambers(id, firedChambers);
+}
+
+void GBTUserLogicEncoder::processTrigger(const uint16_t bc, uint8_t triggerWord)
+{
+  /// Adds the information in triggered mode
+  for (int ireg = 0; ireg < 2; ++ireg) {
+    uint8_t firedLoc = (mMask >> (4 * ireg)) & 0xF;
+    addReg(bc, triggerWord, ireg, firedLoc);
+  }
+  for (int iloc = 0; iloc < 8; ++iloc) {
+    if (mMask & (1 << iloc)) {
+      addBoard(raw::sSTARTBIT | raw::sCARDTYPE, triggerWord, bc, iloc + 8 * crateparams::getGBTIdInCrate(mFeeId), 0);
+      if (triggerWord & (raw::sSOX | raw::sEOX)) {
+        /// Write masks
+        for (int ich = 0; ich < 4; ++ich) {
+          addShort(0xFFFF); // BP
+          addShort(0xFFFF); // NBP
+        }
+      }
+    }
+  }
+}
+
+bool GBTUserLogicEncoder::checkAndAdd(gsl::span<const LocalBoardRO> data, const uint16_t bc, uint8_t triggerWord)
+{
+  /// Checks the local boards and write the regional and local output if needed
+  uint8_t activeBoards = 0;
+  for (auto& loc : data) {
+    int regId = loc.boardId / 4;
+    for (int ich = 0; ich < 4; ++ich) {
+      if (loc.patternsBP[ich] && loc.patternsNBP[ich]) {
+        activeBoards |= (1 << (loc.boardId % 8));
+      }
+    }
+  }
+  for (int ireg = 0; ireg < 2; ++ireg) {
+    uint8_t firedLoc = (activeBoards >> (4 * ireg)) & 0xF;
+    if (firedLoc > 0) {
+      addReg(bc, triggerWord, ireg, firedLoc);
+    }
+  }
+  for (auto& loc : data) {
+    if (activeBoards & (1 << (loc.boardId % 8))) {
+      addLoc(loc, bc, triggerWord);
+    }
+  }
+  return (activeBoards > 0);
+}
+
+void GBTUserLogicEncoder::addReg(uint16_t bc, uint8_t triggerWord, uint8_t id, uint8_t firedChambers)
+{
+  /// Adds the regional board information
+  mBytes.emplace_back(raw::sSTARTBIT);
+  mBytes.emplace_back(triggerWord);
+  uint16_t localClock = bc;
+  if (triggerWord == 0) {
+    localClock += sDelayBCToLocal + sDelayRegToLocal;
+  }
+  addShort(localClock);
+  addIdAndChambers(id + 8 * crateparams::getGBTIdInCrate(mFeeId), firedChambers);
+}
+
+void GBTUserLogicEncoder::addLoc(const LocalBoardRO& loc, uint16_t bc, uint8_t triggerWord)
+{
+  /// Adds the local board information
+
+  mBytes.emplace_back(loc.statusWord);
+  mBytes.emplace_back(triggerWord ? triggerWord : loc.triggerWord);
+
+  uint16_t localClock = bc;
+  if (loc.triggerWord == 0) {
+    localClock += sDelayBCToLocal;
+  }
+  addShort(localClock);
+
+  addIdAndChambers(loc.boardId, loc.firedChambers);
+  for (int ich = 4; ich >= 0; --ich) {
+    if (loc.firedChambers & (1 << ich)) {
+      addShort(loc.patternsBP[ich]);
+      addShort(loc.patternsNBP[ich]);
+    }
+  }
+}
+
+void GBTUserLogicEncoder::process(gsl::span<const LocalBoardRO> data, const uint16_t bc, uint8_t triggerWord)
+{
+  /// Encode data
+  if (triggerWord != 0) {
+    processTrigger(bc, triggerWord);
+  }
+  checkAndAdd(data, bc, triggerWord);
+  if (triggerWord == raw::sCALIBRATE) {
+    // Add FET
+    checkAndAdd(data, bc + sDelayCalibToFET, 0);
+  }
+}
+
+} // namespace mid
+} // namespace o2
