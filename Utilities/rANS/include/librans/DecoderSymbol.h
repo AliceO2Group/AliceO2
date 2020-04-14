0e1eb9a8666753b677b5238ad7a8016a548b8c0d
Michael Lettrich <michael.lettrich@cern.ch>
2020-04-14 21:28:00 +0200

    rANS: add library for rANS entropy coding
    Library that provides close to entropy encoding at high data rates using
    rANS coder by Jarek Duda. This code has been taken from
    https://github.com/rygorous/ryg_rans (no copyright) and heavily reengineered

---
+++ Utilities/rANS/include/librans/DecoderSymbol.h
@@ -0,0 +1,45 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// @file   DecoderSymbol.h
+/// @author Michael Lettrich
+/// @since  2019-05-21
+/// @brief  Structure containing all relevant information for decoding a rANS encoded symbol
+
+#ifndef RANS_DECODERSYMBOL_H
+#define RANS_DECODERSYMBOL_H
+
+#include <cstdint>
+
+namespace o2
+{
+namespace rans
+{
+
+// Decoder symbols are straightforward.
+struct DecoderSymbol {
+  // Initialize a decoder symbol to start "start" and frequency "freq"
+  DecoderSymbol(uint32_t start, uint32_t freq, uint32_t probabilityBits)
+    : start(start), freq(freq)
+  {
+    (void)probabilityBits; // silence compiler warning.
+    // TODO(lettrich): a check should be definitely done here.
+    //		RansAssert(start <= (1 << 16));
+    //		RansAssert(freq <= (1 << 16) - start);
+  };
+
+  uint32_t start; // Start of range.
+  uint32_t freq;  // Symbol frequency.
+};
+
+} // namespace rans
+} // namespace o2
+
+#endif /* RANS_DECODERSYMBOL_H */
