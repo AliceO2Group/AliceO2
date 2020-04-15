301abffd903ae1d8208a1d1a6d75c0c557c58229
David Rohr <drohr@jwdt.org>
2020-04-15 11:12:25 +0200

    GPU: HLT components should propagate the error when GPU initialization fails

--- GPU/GPUTracking/SliceTracker/GPUTPCTrackerComponent.cxx
+++ GPU/GPUTracking/SliceTracker/GPUTPCTrackerComponent.cxx
@@ -365,7 +365,7 @@ int GPUTPCTrackerComponent::Configure(const char* cdbEntry, const char* chainId,
   return iResult1 ? iResult1 : (iResult2 ? iResult2 : iResult3);
 }
 
-void GPUTPCTrackerComponent::ConfigureSlices()
+int GPUTPCTrackerComponent::ConfigureSlices()
 {
   // Initialize the tracker slices
   GPUSettingsRec rec;
@@ -398,7 +398,7 @@ void GPUTPCTrackerComponent::ConfigureSlices()
 
   fRec->SetSettings(&ev, &rec, &devProc, &steps);
   fChain->LoadClusterErrors();
-  fRec->Init();
+  return fRec->Init();
 }
 
 void* GPUTPCTrackerComponent::TrackerInit(void* par)
@@ -410,7 +410,9 @@ void* GPUTPCTrackerComponent::TrackerInit(void* par)
   }
   fChain = fRec->AddChain<GPUChainTracking>();
 
-  ConfigureSlices();
+  if (ConfigureSlices()) {
+    return ((void*)-1);
+  }
   return (nullptr);
 }
 
