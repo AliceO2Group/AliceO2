2e35f50cae48a1f1669fbae73a8abf7dc5b65f19
Roberto Preghenella <preghenella@bo.infn.it>
2020-04-16 15:06:44 +0200

    Example to run AliRoot Hijing simulation

---
+++ run/SimExamples/AliRoot_Hijing/run.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# This is a simple simulation example showing how to run event simulation
+# using the Hijing event generator interface from AliRoot
+#
+# The configuration of AliGenHijing is performed by the function
+# 'hijing(double energy = 5020., double bMin = 0., double bMax = 20.)' defined 
+# in the macro 'aliroot_hijing.macro'.
+#
+# The macro file is specified via the argument of '--extGenFile'
+# whereas the specific function call to retrieven the configuration
+# is specified via the argument of '--extGenFunc'
+# 
+# IMPORTANT
+# To run this example you need to load an AliRoot package compatible with the O2.
+# for more details, https://alice.its.cern.ch/jira/browse/AOGM-246
+#
+#
+
+set -x
+
+NEV=5
+ENERGY=5020.
+BMIN=0
+BMAX=20.
+o2-sim-serial -j 20 -n ${NEV} -g extgen -m PIPE ITS TPC -o sim \
+	      --extGenFile "aliroot_hijing.macro" --extGenFunc "hijing(${ENERGY}, ${BMIN}, ${BMAX})" \
+	      --configKeyValue "Diamond.position[2]=0.1;Diamond.width[2]=0.05"
