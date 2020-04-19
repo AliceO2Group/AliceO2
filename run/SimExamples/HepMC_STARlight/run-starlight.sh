efac00a21b6b7dbe0ee2c5355c666cca5a8e65cf
Roberto Preghenella <preghenella@bo.infn.it>
2020-04-19 09:44:24 +0200

    Example to run STARlight via HepMC file

---
+++ run/SimExamples/HepMC_STARlight/run-starlight.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+echo " --- run-starlight.sh ---"
+
+set -x
+
+# prepare environment
+VERSION=latest
+eval $(alienv printenv -q STARlight/$VERSION)
+STARLIGHT_ROOT=$(starlight-config)
+
+# populate working directory
+cp $STARLIGHT_ROOT/config/slight.in .
+cp $STARLIGHT_ROOT/HepMC/*.awk .
+
+# run STARlight
+starlight slight.in | tee slight.log
+cat slight.log slight.out | \
+    awk -f pdgMass.awk -f starlight2hepmc.awk > starlight.hepmc
---
+++ run/SimExamples/HepMC_STARlight/run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# This is a simple simulation example showing the following things
+#
+
+# a) how to start STARlight event generator in a clean environment to produce HepMC files
+# b) how to run simulation reading from the HepMC file
+
+set -x
+
+# PART a)
+env -i HOME="$HOME" USER="$USER" PATH="/bin:/usr/bin:/usr/local/bin" \
+    ALIBUILD_WORK_DIR="$ALIBUILD_WORK_DIR" ./run-starlight.sh 
+
+# PART b)
+NEV=1000
+o2-sim -j 20 -n ${NEV} -g hepmc -m PIPE ITS -o sim \
+       --HepMCFile starlight.hepmc \
+       --configKeyValue "Diamond.position[2]=0.1;Diamond.width[2]=0.05"
