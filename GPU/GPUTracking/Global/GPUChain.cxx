445cba9816169f429c9a21c4064dcaa95a921069
David Rohr <drohr@jwdt.org>
2020-04-20 19:35:16 +0200

    GPU: Reorganize GPU launch_bounds and thread count constants per kernel

--- GPU/GPUTracking/Global/GPUChain.cxx
+++ GPU/GPUTracking/Global/GPUChain.cxx
@@ -17,8 +17,23 @@ using namespace GPUCA_NAMESPACE::gpu;
 constexpr GPUChain::krnlRunRange GPUChain::krnlRunRangeNone;
 constexpr GPUChain::krnlEvent GPUChain::krnlEventNone;
 
-GPUChain::krnlExec GPUChain::GetGrid(unsigned int totalItems, unsigned int nThreads, int stream)
+GPUChain::krnlExec GPUChain::GetGrid(unsigned int totalItems, unsigned int nThreads, int stream, GPUReconstruction::krnlDeviceType d, GPUCA_RECO_STEP st)
 {
   const unsigned int nBlocks = (totalItems + nThreads - 1) / nThreads;
-  return {nBlocks, nThreads, stream};
+  return {nBlocks, nThreads, stream, d, st};
+}
+
+GPUChain::krnlExec GPUChain::GetGrid(unsigned int totalItems, int stream, GPUReconstruction::krnlDeviceType d, GPUCA_RECO_STEP st)
+{
+  return {(unsigned int)-1, totalItems, stream, d, st};
+}
+
+GPUChain::krnlExec GPUChain::GetGridBlk(unsigned int nBlocks, int stream, GPUReconstruction::krnlDeviceType d, GPUCA_RECO_STEP st)
+{
+  return {nBlocks, (unsigned int)-1, stream, d, st};
+}
+
+GPUChain::krnlExec GPUChain::GetGridBlkStep(unsigned int nBlocks, int stream, GPUCA_RECO_STEP st)
+{
+  return {nBlocks, (unsigned int)-1, stream, GPUReconstruction::krnlDeviceType::Auto, st};
 }
