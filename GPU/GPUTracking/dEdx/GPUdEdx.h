77f1e95e1b4f4593f8c486e420ad106a7355c6e6
sgorbuno <sergey.gorbunov@fias.uni-frankfurt.de>
2020-04-21 13:34:10 +0000

    add a FlatObject interface

--- GPU/GPUTracking/dEdx/GPUdEdx.h
+++ GPU/GPUTracking/dEdx/GPUdEdx.h
@@ -102,7 +102,7 @@ GPUdi() void GPUdEdx::fillCluster(float qtot, float qmax, int padRow, float trac
 
   const float sec2 = 1.f / (1.f - snp2);
   // angleZ: z angle - dz/dx (cm/cm)
-  const float angleZ = CAMath::Sqrt(tgl2 * sec2); // fast
+  float angleZ = CAMath::Sqrt(tgl2 * sec2); // fast
   if (angleZ > 3) {
     angleZ = 3;
   }
