9bb5dd67d57cbc4225a86b830f2681f5c188f857
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2020-04-24 15:32:59 +0200

    Rewriting

--- cmake/O2AddWorkflow.cmake
+++ cmake/O2AddWorkflow.cmake
@@ -11,50 +11,48 @@
 include_guard()
 
 #
-# o2_add_dpl_workflow(basename SOURCES ...) add a new dpl workflow
-# executable. Besides building the executable, this will also generate
-# a configuration json file via <executable-name> --dump so that it can
-# be easily registered and deployed e.g. in the train infrastructure.
+# o2_add_dpl_workflow(basename SOURCES ...) add a new dpl workflow executable.
+# Besides building the executable, this will also generate a configuration json
+# file via <executable-name> --dump so that it can be easily registered and
+# deployed e.g. in the train infrastructure.
 #
 # For most of the options please see how o2_add_executable works.
 #
-# The installed executable will be named o2[-exeType][-component_name]-basename
-# The installed JSON file will be named o2-[exeType][-component_name]-basename.json.
+# The installed executable will be named as regular executables (see
+# o2_add_executable for details)
+#
+# The installed JSON file will be named <executable-name>.json and installed
+# under share/dpl directory
 #
 
 function(o2_add_dpl_workflow baseTargetName)
 
-  cmake_parse_arguments(PARSE_ARGV
-                        1
-                        A
-                        ""
-                        "COMPONENT_NAME;TARGETVARNAME"
+  cmake_parse_arguments(PARSE_ARGV 1 A "" "COMPONENT_NAME;TARGETVARNAME"
                         "SOURCES;PUBLIC_LINK_LIBRARIES")
 
   if(A_UNPARSED_ARGUMENTS)
     message(FATAL_ERROR "Got trailing arguments ${A_UNPARSED_ARGUMENTS}")
   endif()
 
-  if(A_COMPONENT_NAME)
-    string(TOLOWER ${A_COMPONENT_NAME} component)
-    set(comp -${component})
-  endif()
-  set(exeName o2${exeType}${comp}-${baseTargetName})
+  o2_add_executable(${baseTargetName}
+    COMPONENT_NAME ${A_COMPONENT_NAME} TARGETVARNAME targetExeName
+    SOURCES ${A_SOURCES}
+    PUBLIC_LINK_LIBRARIES O2::Framework ${A_PUBLIC_LINK_LIBRARIES})
 
-  o2_add_executable(${baseTargetName} 
-                    COMPONENT_NAME ${A_COMPONENT_NAME}
-                    TARGETVARNAME ${A_TARGETVARNAME}
-                    SOURCES ${A_SOURCES}
-                    PUBLIC_LINK_LIBRARIES O2::Framework ${A_PUBLIC_LINK_LIBRARIES})
-  add_custom_command(OUTPUT ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${exeName}.json 
-                     DEPENDS ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${exeName}
-                     COMMAND ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${exeName} | cat > ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${exeName}.json
-                    )
-  
-  add_custom_target(${exeName}_dpl_config 
-                    DEPENDS ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${exeName}.json
-                   )
-  install(FILES ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${exeName}.json
-          DESTINATION ${CMAKE_INSTALL_DATADIR}/dpl
-  )
+  if(A_TARGETVARNAME)
+    set(${A_TARGETVARNAME}
+        ${targetExeName}
+        PARENT_SCOPE)
+  endif()
+
+  set(jsonFile $<TARGET_FILE_BASE_NAME:${targetExeName}>.json)
+
+  add_custom_command(
+    TARGET ${targetExeName} POST_BUILD
+    COMMAND $<TARGET_FILE:${targetExeName}> -b | cat > ${jsonFile})
+
+  install(
+    FILES ${CMAKE_CURRENT_BINARY_DIR}/${jsonFile}
+    DESTINATION ${CMAKE_INSTALL_DATADIR}/dpl)
+
 endfunction()
