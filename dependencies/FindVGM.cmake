1eb05c0795a5413c0f258acfa8c1449df63934cf
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2020-04-26 10:51:46 +0200

    CMake fixes: (#3437)
    CMake fixes:
    Added FindVGM.cmake to amend the target provided by VGM installation

---
+++ dependencies/FindVGM.cmake
@@ -0,0 +1,31 @@
+
+# Copyright CERN and copyright holders of ALICE O2. This software is distributed
+# under the terms of the GNU General Public License v3 (GPL Version 3), copied
+# verbatim in the file "COPYING".
+#
+# See http://alice-o2.web.cern.ch/license for full licensing information.
+#
+# In applying this license CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization or
+# submit itself to any jurisdiction.
+
+# use the VGMConfig.cmake provided by the VGM installation but
+# amend the target VGM::XmlVGM with the include  and link directories
+
+find_package(VGM NO_MODULE)
+if(NOT VGM_FOUND)
+  return()
+endif()
+
+# VGM uses namespace since 4.7
+if(${VGM_VERSION} VERSION_LESS "4.7")
+  set(targetVGM XmlVGM)
+else()
+  set(targetVGM VGM::XmlVGM)
+endif()
+
+set_target_properties(${targetVGM}
+                      PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
+                                "${VGM_INCLUDE_DIRS}"
+                      INTERFACE_LINK_DIRECTORIES
+                                $<TARGET_FILE_DIR:${targetVGM}>)
