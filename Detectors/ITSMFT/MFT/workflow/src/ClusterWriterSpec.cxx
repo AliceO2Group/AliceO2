599a1876e8798fcb49d62ea12356e2d014f2f54f
Matthias Richter <Matthias.Richter@scieq.net>
2020-04-27 12:04:23 +0200

    Simplifying ITSMFT workflow writer specs
    Duplicate boiler plate code is replaced with standard RootTreeWriter.

--- Detectors/ITSMFT/MFT/workflow/src/ClusterWriterSpec.cxx
+++ Detectors/ITSMFT/MFT/workflow/src/ClusterWriterSpec.cxx
@@ -12,17 +12,13 @@
 
 #include <vector>
 
-#include "TTree.h"
-
-#include "Framework/ControlService.h"
-#include "Framework/ConfigParamRegistry.h"
 #include "MFTWorkflow/ClusterWriterSpec.h"
+#include "DPLUtils/MakeRootTreeWriterSpec.h"
 #include "DataFormatsITSMFT/CompCluster.h"
 #include "DataFormatsITSMFT/Cluster.h"
 #include "SimulationDataFormat/MCCompLabel.h"
 #include "SimulationDataFormat/MCTruthContainer.h"
 #include "DataFormatsITSMFT/ROFRecord.h"
-#include "CommonUtils/StringUtils.h"
 
 using namespace o2::framework;
 
@@ -32,92 +28,49 @@ namespace mft
 {
 
 template <typename T>
-TBranch* getOrMakeBranch(TTree* tree, const char* brname, T* ptr)
-{
-  if (auto br = tree->GetBranch(brname)) {
-    br->SetAddress(static_cast<void*>(ptr));
-    return br;
-  }
-  return tree->Branch(brname, ptr); // otherwise make it
-}
-
-void ClusterWriter::init(InitContext& ic)
-{
-  auto filename = ic.options().get<std::string>("mft-cluster-outfile");
-  mFile = std::make_unique<TFile>(filename.c_str(), "RECREATE");
-  if (!mFile->IsOpen()) {
-    throw std::runtime_error(o2::utils::concat_string("failed to open MFT clusters output file ", filename));
-  }
-  mTree = std::make_unique<TTree>("o2sim", "Tree with MFT clusters");
-}
-
-void ClusterWriter::run(ProcessingContext& pc)
-{
-  auto compClusters = pc.inputs().get<const std::vector<o2::itsmft::CompClusterExt>>("compClusters");
-  auto pspan = pc.inputs().get<gsl::span<unsigned char>>("patterns");
-  auto clusters = pc.inputs().get<const std::vector<o2::itsmft::Cluster>>("clusters");
-  auto rofs = pc.inputs().get<const std::vector<o2::itsmft::ROFRecord>>("ROframes");
-
-  std::unique_ptr<const o2::dataformats::MCTruthContainer<o2::MCCompLabel>> labels;
-  const o2::dataformats::MCTruthContainer<o2::MCCompLabel>* plabels = nullptr;
-  std::vector<o2::itsmft::MC2ROFRecord> mc2rofs, *mc2rofsPtr = &mc2rofs;
-  std::vector<unsigned char> patterns(pspan.begin(), pspan.end());
-
-  LOG(INFO) << "MFTClusterWriter pulled " << clusters.size() << " clusters, in " << rofs.size() << " RO frames";
-
-  auto compClustersPtr = &compClusters;
-  getOrMakeBranch(mTree.get(), "MFTClusterComp", &compClustersPtr);
-  auto patternsPtr = &patterns;
-  getOrMakeBranch(mTree.get(), "MFTClusterPatt", &patternsPtr);
-  auto clustersPtr = &clusters;
-  getOrMakeBranch(mTree.get(), "MFTCluster", &clustersPtr); // RSTODO being eliminated
-  auto rofsPtr = &rofs;
-  getOrMakeBranch(mTree.get(), "MFTClustersROF", &rofsPtr);
-
-  if (mUseMC) {
-    labels = pc.inputs().get<const o2::dataformats::MCTruthContainer<o2::MCCompLabel>*>("labels");
-    plabels = labels.get();
-    getOrMakeBranch(mTree.get(), "MFTClusterMCTruth", &plabels);
-
-    // write MC2ROFrecord vector (directly inherited from digits input) to a tree
-    const auto m2rvec = pc.inputs().get<gsl::span<o2::itsmft::MC2ROFRecord>>("MC2ROframes");
-    mc2rofs.reserve(m2rvec.size());
-    for (const auto& m2rv : m2rvec) {
-      mc2rofs.push_back(m2rv);
-    }
-    getOrMakeBranch(mTree.get(), "MFTClustersMC2ROF", &mc2rofsPtr);
-  }
-
-  mTree->Fill();
-}
-
-void ClusterWriter::endOfStream(o2::framework::EndOfStreamContext& ec)
-{
-  LOG(INFO) << "Finalizing MFT cluster writing";
-  mTree->Write();
-  mTree.release()->Delete();
-  mFile->Close();
-}
+using BranchDefinition = MakeRootTreeWriterSpec::BranchDefinition<T>;
+using CompClusType = std::vector<o2::itsmft::CompClusterExt>;
+using PatternsType = std::vector<unsigned char>;
+using ClustersType = std::vector<o2::itsmft::Cluster>;
+using ROFrameRType = std::vector<o2::itsmft::ROFRecord>;
+using LabelsType = o2::dataformats::MCTruthContainer<o2::MCCompLabel>;
+using ROFRecLblT = std::vector<o2::itsmft::MC2ROFRecord>;
+using namespace o2::header;
 
 DataProcessorSpec getClusterWriterSpec(bool useMC)
 {
-  std::vector<InputSpec> inputs;
-  inputs.emplace_back("compClusters", "MFT", "COMPCLUSTERS", 0, Lifetime::Timeframe);
-  inputs.emplace_back("patterns", "MFT", "PATTERNS", 0, Lifetime::Timeframe);
-  inputs.emplace_back("clusters", "MFT", "CLUSTERS", 0, Lifetime::Timeframe);
-  inputs.emplace_back("ROframes", "MFT", "MFTClusterROF", 0, Lifetime::Timeframe);
-  if (useMC) {
-    inputs.emplace_back("labels", "MFT", "CLUSTERSMCTR", 0, Lifetime::Timeframe);
-    inputs.emplace_back("MC2ROframes", "MFT", "MFTClusterMC2ROF", 0, Lifetime::Timeframe);
-  }
-
-  return DataProcessorSpec{
-    "mft-cluster-writer",
-    inputs,
-    Outputs{},
-    AlgorithmSpec{adaptFromTask<ClusterWriter>(useMC)},
-    Options{
-      {"mft-cluster-outfile", VariantType::String, "mftclusters.root", {"Name of the output file"}}}};
+  // Spectators for logging
+  // this is only to restore the original behavior
+  auto clustersSize = std::make_shared<int>(0);
+  auto clustersSizeGetter = [clustersSize](ClustersType const& clusters) {
+    *clustersSize = clusters.size();
+  };
+  auto logger = [clustersSize](std::vector<o2::itsmft::ROFRecord> const& rofs) {
+    LOG(INFO) << "MFTClusterWriter pulled " << *clustersSize << " clusters, in " << rofs.size() << " RO frames";
+  };
+  return MakeRootTreeWriterSpec("mft-cluster-writer",
+                                "mftclusters.root",
+                                MakeRootTreeWriterSpec::TreeAttributes{"o2sim", "Tree with MFT clusters"},
+                                BranchDefinition<CompClusType>{InputSpec{"compclus", "MFT", "COMPCLUSTERS", 0},
+                                                               "MFTClusterComp"},
+                                BranchDefinition<PatternsType>{InputSpec{"patterns", "MFT", "PATTERNS", 0},
+                                                               "MFTClusterPatt"},
+                                // this has been marked to be removed in the original implementation
+                                // RSTODO being eliminated
+                                BranchDefinition<ClustersType>{InputSpec{"clusters", "MFT", "CLUSTERS", 0},
+                                                               "MFTCluster",
+                                                               clustersSizeGetter},
+                                BranchDefinition<ROFrameRType>{InputSpec{"ROframes", "MFT", "MFTClusterROF", 0},
+                                                               "MFTClustersROF",
+                                                               logger},
+                                BranchDefinition<LabelsType>{InputSpec{"labels", "MFT", "CLUSTERSMCTR", 0},
+                                                             "MFTClusterMCTruth",
+                                                             (useMC ? 1 : 0), // one branch if mc labels enabled
+                                                             ""},
+                                BranchDefinition<ROFRecLblT>{InputSpec{"MC2ROframes", "MFT", "MFTClusterMC2ROF", 0},
+                                                             "MFTClustersMC2ROF",
+                                                             (useMC ? 1 : 0), // one branch if mc labels enabled
+                                                             ""})();
 }
 
 } // namespace mft
