6a6d3a47022600074db6b75a1a2d3ad579dce3a2
dstocco <diego.stocco@cern.ch>
2020-04-29 11:57:17 +0200

    Adopt the final version of the MID user logic

--- Detectors/MUON/MID/Workflow/include/MIDWorkflow/RawAggregatorSpec.h
+++ Detectors/MUON/MID/Workflow/include/MIDWorkflow/RawAggregatorSpec.h
@@ -8,27 +8,22 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-/// \file   MID/Raw/src/RawInfo.h
-/// \brief  Raw data format MID
+/// \file   MIDWorkflow/RawAggregatorSpec.h
+/// \brief  Data processor spec for MID raw data aggregator devices
 /// \author Diego Stocco <Diego.Stocco at cern.ch>
-/// \date   30 September 2019
-#ifndef O2_MID_RAWINFO_H
-#define O2_MID_RAWINFO_H
+/// \date   26 February 2020
 
-#include <cstdint>
+#ifndef O2_MID_RAWAGGREGATORSPEC_H
+#define O2_MID_RAWAGGREGATORSPEC_H
+
+#include "Framework/DataProcessorSpec.h"
 
 namespace o2
 {
 namespace mid
 {
-
-/// Parameters for local board encoding/decoding
-// The delays are in local clocks, and correspond to the LHC clocks (aka BCs)
-static constexpr uint16_t sDelayCalibToFET = 10;
-static constexpr uint16_t sDelayBCToLocal = 0;
-static constexpr uint16_t sDelayRegToLocal = 3;
-
+framework::DataProcessorSpec getRawAggregatorSpec();
 } // namespace mid
 } // namespace o2
 
-#endif /* O2_MID_RAWINFO_H */
+#endif //O2_MID_RAWAGGREGATORSPEC_H
