0a21b3a914536d0630c16aceb7bdc0bdb1da0e8a
David Rohr <drohr@jwdt.org>
2020-05-03 23:11:48 +0200

    GPU: Make TPC Grid creation compile (for now) on GPU, reuse available temp memory instead of allocating std::vectors, reshuffle to create row by row for later parallelization

--- GPU/GPUTracking/SliceTracker/GPUTPCGrid.h
+++ GPU/GPUTracking/SliceTracker/GPUTPCGrid.h
@@ -32,7 +32,7 @@ MEM_CLASS_PRE()
 class GPUTPCGrid
 {
  public:
-  void CreateEmpty();
+  GPUd() void CreateEmpty();
   GPUd() void Create(float yMin, float yMax, float zMin, float zMax, int ny, int nz);
 
   GPUd() int GetBin(float Y, float Z) const;
