1d4c1bab763fe8d1da3d2065450b9f9069c00cf6
knopers8 <piotr.jan.konopka@cern.ch>
2020-05-05 09:34:03 +0200

    [QC-119] [QC-296] An overhaul of Mergers (#3425)
    Code restructurization - separate devices for merging differences and full objects,
     separate merging algorithm. Custom mergeable objects don't have to inherit TObject
     anymore. Reduced complexity, improved performance. Added benchmarks.

---
+++ Utilities/Mergers/include/Mergers/IntegratingMerger.h
@@ -0,0 +1,69 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#ifndef ALICEO2_INTEGRATINGMERGER_H
+#define ALICEO2_INTEGRATINGMERGER_H
+
+/// \file IntegratingMerger.h
+/// \brief Definition of O2 IntegratingMerger, v0.1
+///
+/// \author Piotr Konopka, piotr.jan.konopka@cern.ch
+
+#include "Mergers/MergerConfig.h"
+#include "Mergers/MergeInterface.h"
+#include "Mergers/ObjectStore.h"
+
+#include "Framework/Task.h"
+
+#include <memory>
+
+class TObject;
+
+namespace o2::monitoring
+{
+class Monitoring;
+}
+
+namespace o2::mergers
+{
+
+/// \brief IntegratingMerger data processor class.
+///
+/// Mergers are DPL devices able to merge ROOT objects produced in parallel.
+class IntegratingMerger : public framework::Task
+{
+ public:
+  /// \brief Default constructor. It expects Merger configuration and subSpec of output channel.
+  IntegratingMerger(const MergerConfig&, const header::DataHeader::SubSpecificationType&);
+  /// \brief Default destructor.
+  ~IntegratingMerger() override = default;
+
+  /// \brief IntegratingMerger init callback.
+  void init(framework::InitContext& ctx) override;
+  /// \brief IntegratingMerger process callback.
+  void run(framework::ProcessingContext& ctx) override;
+
+ private:
+  void publish(framework::DataAllocator& allocator);
+
+ private:
+  header::DataHeader::SubSpecificationType mSubSpec;
+  ObjectStore mMergedObject = std::monostate{};
+  MergerConfig mConfig;
+  std::unique_ptr<monitoring::Monitoring> mCollector;
+
+  // stats
+  int mTotalObjectsMerged = 0;
+  int mObjectsMerged = 0;
+};
+
+} // namespace o2::mergers
+
+#endif //ALICEO2_INTEGRATINGMERGER_H
