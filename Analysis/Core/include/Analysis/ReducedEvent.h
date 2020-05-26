cc594e15f1254420b35d6cbb72d208f3830643e1
Ionut Arsene <iarsene@cern.ch>
2020-05-26 17:49:25 +0200

    Added a task which produces trees of ReducedEvent objects

--- Analysis/Core/include/Analysis/ReducedEvent.h
+++ Analysis/Core/include/Analysis/ReducedEvent.h
@@ -7,6 +7,7 @@
 
 class ReducedTrack;
 
+
 //_________________________________________________________________________
 class ReducedEvent : public TObject {
 
@@ -15,22 +16,24 @@ class ReducedEvent : public TObject {
   virtual ~ReducedEvent();
   
   // getters
-  uint64_t      EventTag()              const {return fEventTag;}
-  bool          TestEventTag(short bit) const {return (bit<8*sizeof(uint64_t) ? (fEventTag&(uint64_t(1)<<bit)) : false);}
-  float         Vertex(short axis)      const {return (axis>=0 && axis<=2 ? fVtx[axis] : 0);}
+  uint64_t      EventTag()                       const {return fEventTag;}
+  bool          TestEventTag(unsigned short bit) const {return (bit<8*sizeof(uint64_t) ? (fEventTag&(uint64_t(1)<<bit)) : false);}
+  float         Vertex(short axis)               const {return (axis>=0 && axis<=2 ? fVtx[axis] : 0);}
   float         CentVZERO()             const {return fCentVZERO;}
   ReducedTrack* GetTrack(int i)         const {return (fTracks && i>=0 && i<fTracks->GetEntries() ? (ReducedTrack*)fTracks->At(i) : 0x0);}
   TClonesArray* GetTracks()             const {return fTracks;}
+  int           NTracks()               const {return (fTracks ? fTracks->GetEntries() : 0);}
   
   // setters
-  void SetEventTags(uint64_t tag)      {fEventTag = tag;}
-  void SetEventTag(short iflag)      {if (iflag>=8*sizeof(uint64_t)) return; fEventTag |= (uint64_t(1)<<iflag);}
-  void UnsetEventTag(short iflag)    {if (iflag>=8*sizeof(uint64_t)) return; if(TestEventTag(iflag)) fEventTag^=(uint64_t(1)<<iflag);}
-  void SetVertex(short axis, float value) {if(axis>=0 && axis<3) fVtx[axis] = value;}
+  void SetEventTags(uint64_t tag)          {fEventTag = tag;}
+  void SetEventTag(unsigned short iflag)   {if (iflag>=8*sizeof(uint64_t)) return; fEventTag |= (uint64_t(1)<<iflag);}
+  void UnsetEventTag(unsigned short iflag) {if (iflag>=8*sizeof(uint64_t)) return; if(TestEventTag(iflag)) fEventTag^=(uint64_t(1)<<iflag);}
+  void SetVertex(short axis, float value)  {if(axis>=0 && axis<3) fVtx[axis] = value;}
+  void SetCentVZERO(float value)           {fCentVZERO = value;}
   
   void ClearEvent();
     
- protected:
+ private:
   uint64_t fEventTag;         // Event tags to be used either during analysis or to filter events
   float    fVtx[3];           // global event vertex vector in cm
   float    fCentVZERO;        // centrality; 0-V0M, 1-CL1, 2-TRK, 3-ZEMvsZDC, 4-V0A, 5-V0C, 6-ZNA
@@ -43,5 +46,4 @@ class ReducedEvent : public TObject {
 
   ClassDef(ReducedEvent, 1);
 };
-
 #endif
