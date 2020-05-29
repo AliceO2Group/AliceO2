79eac0bd07b2a663dfa15aebf7d11f9f4a97bd4d
wiechula <Jens.Wiechula@cern.ch>
2020-05-29 15:49:05 +0200

    fix OROC3 PID cluster count

--- GPU/GPUTracking/dEdx/GPUdEdx.cxx
+++ GPU/GPUTracking/dEdx/GPUdEdx.cxx
@@ -46,8 +46,8 @@ GPUd() void GPUdEdx::computedEdx(GPUdEdxInfo& GPUrestrict() output, const GPUPar
   output.NHitsSubThresholdOROC1 = countOROC1;
   output.NHitsOROC2 = countOROC2 - mNClsROCSubThresh[2];
   output.NHitsSubThresholdOROC2 = countOROC2;
-  output.NHitsOROC2 = countOROC3 - mNClsROCSubThresh[3];
-  output.NHitsSubThresholdOROC2 = countOROC3;
+  output.NHitsOROC3 = countOROC3 - mNClsROCSubThresh[3];
+  output.NHitsSubThresholdOROC3 = countOROC3;
 }
 
 GPUd() float GPUdEdx::GetSortTruncMean(float* GPUrestrict() array, int count, int trunclow, int trunchigh)
