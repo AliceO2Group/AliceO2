3a7e0a56f449eab56ee88d452f1cbf4b3dd0b4b0
shahoian <ruben.shahoyan@cern.ch>
2020-06-01 17:54:31 +0200

    Base class for for flat/persistent CTF EncodedBlocks + CTF Header
    * EncodedBlocks is a layout of the flat container template for multiple binary blocks augmented by
    detector-specific and rANS POD headers. Can be created either as a proper object (non-flat, then can be flattened)
    or reinterpret_casted on preallocated memore segment.
    Provides basic methods for storing/retrieving itself from the TTree.
    Specific detector CTF objects inherit from EncodedBlocks w/o adding any persistent data members.

--- DataFormats/Detectors/Common/src/DetID.cxx
+++ DataFormats/Detectors/Common/src/DetID.cxx
@@ -14,6 +14,7 @@
 
 #include "DetectorsCommonDataFormats/DetID.h"
 #include <cassert>
+#include <string>
 #include "FairLogger.h"
 
 using namespace o2::detectors;
@@ -29,9 +30,50 @@ constexpr DetID::ID DetID::ITS, DetID::TPC, DetID::TRD, DetID::TOF, DetID::PHS, 
 
 constexpr int DetID::nDetectors;
 
+/// detector masks from any non-alpha-num delimiter-separated list (empty if NONE is supplied)
+DetID::mask_t DetID::getMask(const std::string_view detList)
+{
+  DetID::mask_t mask;
+  std::string ss(detList);
+  auto pos = ss.find(NONE);
+  if (pos != std::string::npos) {
+    ss.replace(pos, NONE.size(), "");
+  } else {
+    for (auto id = DetID::First; id <= DetID::Last; id++) {
+      auto pos = ss.find(DetID::getName(id));
+      if (pos != std::string::npos) {
+        mask.set(id);
+        ss.replace(pos, strlen(DetID::getName(id)), "");
+      }
+    }
+  }
+  // make sure that no names are left in the input strings
+  if (std::count_if(ss.begin(), ss.end(), [](unsigned char c) { return std::isalnum(c); })) {
+    LOG(ERROR) << "Ill-formed detectors list " << std::string(detList);
+    throw std::runtime_error("Ill-formed detectors list");
+  }
+  return mask;
+}
+
 //_______________________________
 DetID::DetID(const char* name) : mID(nameToID(name, First))
 {
   // construct from the name
   assert(mID < nDetectors);
+}
+
+//_______________________________
+std::string DetID::getNames(DetID::mask_t mask)
+{
+  // construct from the name
+  std::string ns;
+  for (auto id = DetID::First; id <= DetID::Last; id++) {
+    if (mask[id]) {
+      if (!ns.empty()) {
+        ns += ',';
+      }
+      ns += getName(id);
+    }
+  }
+  return ns;
 }
