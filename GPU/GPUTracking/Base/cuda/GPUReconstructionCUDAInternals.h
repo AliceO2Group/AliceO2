af447523710370ee709d6f55b584d4dcc1fd6344
David Rohr <drohr@jwdt.org>
2020-06-08 20:46:48 +0200

    GPU: Switch to CUDA 11

--- GPU/GPUTracking/Base/cuda/GPUReconstructionCUDAInternals.h
+++ GPU/GPUTracking/Base/cuda/GPUReconstructionCUDAInternals.h
@@ -68,7 +68,8 @@ class ThrustVolatileAsyncAllocator
 } // namespace GPUCA_NAMESPACE
 
 // Override synchronize call at end of thrust algorithm running on stream, just don't run cudaStreamSynchronize
-THRUST_BEGIN_NS
+namespace thrust
+{
 namespace cuda_cub
 {
 
@@ -85,6 +86,6 @@ __host__ __device__ inline cudaError_t synchronize<thrustStreamPolicy>(thrustStr
 }
 
 } // namespace cuda_cub
-THRUST_END_NS
+} // namespace thrust
 
 #endif
