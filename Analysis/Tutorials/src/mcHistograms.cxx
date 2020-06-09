f966b27125772d4def9e1354b57a73f0e65d2902
jgrosseo <jgrosseo@cern.ch>
2020-06-09 20:14:42 +0200

    Adding MC example (#3744)

---
+++ Analysis/Tutorials/src/mcHistograms.cxx
@@ -0,0 +1,64 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+#include "Framework/runDataProcessing.h"
+#include "Framework/AnalysisTask.h"
+#include "Framework/AnalysisDataModel.h"
+#include <TH1F.h>
+
+#include <cmath>
+
+using namespace o2;
+using namespace o2::framework;
+
+// Simple access to collision
+struct ATask {
+  OutputObj<TH1F> vertex{TH1F("vertex", "vertex", 100, -10, 10)};
+
+  void process(aod::McCollision const& mcCollision)
+  {
+    LOGF(info, "MC. vtx-z = %f", mcCollision.z());
+    vertex->Fill(mcCollision.z());
+  }
+};
+
+// Grouping between MC particles and collisions
+struct BTask {
+  OutputObj<TH1F> phiH{TH1F("phi", "phi", 100, 0., 2. * M_PI)};
+  OutputObj<TH1F> etaH{TH1F("eta", "eta", 102, -2.01, 2.01)};
+
+  void process(aod::McCollision const& mcCollision, aod::McParticles const& mcParticles)
+  {
+    for (auto& mcParticle : mcParticles) {
+      phiH->Fill(mcParticle.phi());
+      etaH->Fill(mcParticle.eta());
+    }
+  }
+};
+
+// Access from tracks to MC particle
+struct CTask {
+  OutputObj<TH1F> etaDiff{TH1F("etaDiff", ";eta_{MC} - eta_{Rec}", 100, -0.1, 0.1)};
+
+  void process(aod::Collision const& collision, soa::Join<aod::Tracks, aod::McTrackLabels> const& tracks, aod::McParticles const& mcParticles)
+  {
+    for (auto& track : tracks) {
+      etaDiff->Fill(track.label().eta() - track.eta());
+    }
+  }
+};
+
+WorkflowSpec defineDataProcessing(ConfigContext const&)
+{
+  return WorkflowSpec{
+    adaptAnalysisTask<ATask>("vertex-histogram"),
+    adaptAnalysisTask<BTask>("etaphi-histogram"),
+    adaptAnalysisTask<CTask>("eta-resolution-histogram"),
+  };
+}
