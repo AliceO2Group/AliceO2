1dd4a75e9ed1b9927890242ea3a65ed6d7090f0b
Giulio Eulisse <giulio.eulisse@cern.ch>
2020-06-10 15:51:54 +0200

    DPL: make TypeIdHelper guaranteed constexpr

--- Framework/Foundation/include/Framework/TypeIdHelpers.h
+++ Framework/Foundation/include/Framework/TypeIdHelpers.h
@@ -11,20 +11,42 @@
 #ifndef O2_FRAMEWORK_TYPEIDHELPERS_H_
 #define O2_FRAMEWORK_TYPEIDHELPERS_H_
 
+#include <string_view>
 #include "Framework/StringHelpers.h"
+
+#if defined(__GNUC__) && (__GNUC__ < 8)
+#define PRETTY_FUNCTION_CONSTEXPR const
+#else
+#define PRETTY_FUNCTION_CONSTEXPR constexpr
+#endif
 
 namespace o2::framework
 {
+
+template <typename T>
+struct unique_type_id {
+  static constexpr auto get() noexcept
+  {
+    PRETTY_FUNCTION_CONSTEXPR std::string_view full_name{__PRETTY_FUNCTION__};
+    return full_name;
+  }
+
+  static constexpr std::string_view value{get()};
+};
+
+template <typename T>
+inline constexpr auto unique_type_id_v = unique_type_id<T>::value;
 
 struct TypeIdHelpers {
   /// Return a unique id for a given type
   /// This works just fine with GCC and CLANG,
   /// C++20 will allow us to use:
-  ///    std::experimental::source_location::current().function_name();
+  ///    std::source_location::current().function_name();
   template <typename T>
   constexpr static uint32_t uniqueId()
   {
-    return compile_time_hash(__PRETTY_FUNCTION__);
+    PRETTY_FUNCTION_CONSTEXPR uint32_t r = crc32(unique_type_id_v<T>.data(), unique_type_id_v<T>.size());
+    return r;
   }
 };
 
