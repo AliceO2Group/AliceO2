0cfdfa80092dda10494430ce4836ff1c8eee7b79
wiechula <Jens.Wiechula@cern.ch>
2020-06-11 15:56:39 +0200

    upate qtot binning

--- Detectors/TPC/calibration/macro/runPulser.C
+++ Detectors/TPC/calibration/macro/runPulser.C
@@ -29,8 +29,9 @@ void runPulser(std::vector<std::string_view> fileInfos, TString outputFileName =
   calib.setADCRange(adcMin, adcMax);
   calib.setTimeBinRange(firstTimeBin, lastTimeBin);
   calib.setDebugLevel();
+  calib.setQtotBinning(140, 22, 302);
   if (type == 1) {
-    calib.setQtotBinning(150,2,302);
+    calib.setQtotBinning(150, 2, 302);
     calib.setMinQtot(8);
     calib.setMinQmax(6);
     calib.setMaxTimeBinRange(6);
