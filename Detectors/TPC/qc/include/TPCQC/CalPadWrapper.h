74118a744c53a1e50af3f2b8117e5869b45dacca
Thomas Klemenz <thomas.klemenz@tum.de>
2020-06-16 12:16:49 +0200

    added CalPadWrapper to enable publishing of CalPad to QCG

---
+++ Detectors/TPC/qc/include/TPCQC/CalPadWrapper.h
@@ -0,0 +1,61 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#ifndef O2_CalPadWrapper_H
+#define O2_CalPadWrapper_H
+
+#include <TClass.h>
+#include <TObject.h>
+
+#include "TPCBase/CalDet.h"
+
+namespace o2
+{
+namespace tpc
+{
+namespace qc
+{
+/// Temporary solution until objects not inheriting from TObject can be handled in QualityControl
+/// A wrapper class to easily promote CalDet<float> objects to a TObject
+/// does not take ownership of wrapped object and should not be used
+/// in tight loops since construction expensive
+class CalPadWrapper : public TObject
+{
+ public:
+  CalPadWrapper(o2::tpc::CalDet<float>* obj) : mObj(obj), TObject()
+  {
+  }
+
+  CalPadWrapper() = default;
+
+  void setObj(o2::tpc::CalDet<float>* obj)
+  {
+    mObj = obj;
+  }
+
+  o2::tpc::CalDet<float>* getObj()
+  {
+    return mObj;
+  }
+
+  virtual const char* GetName() const override { return mObj ? mObj->getName().data() : "unset"; }
+
+  virtual ~CalPadWrapper() override = default;
+
+ private:
+  o2::tpc::CalDet<float>* mObj{}; ///< wrapped CalDet<float> (aka CalPad)
+
+  ClassDefOverride(CalPadWrapper, 1);
+};
+} // namespace qc
+} // namespace tpc
+} // namespace o2
+
+#endif
