7e244d04bdeb0118ca765104da967c33a3a0c5a5
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2020-06-21 22:03:27 +0200

    Add class to convert Run number to timestamp

---
+++ Analysis/Core/include/Analysis/RunToTimestamp.h
@@ -0,0 +1,45 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+//
+// Class for conversion between run number and timestamp
+//
+
+#ifndef RunToTimestamp_H
+#define RunToTimestamp_H
+
+#include <map>
+#include <FairLogger.h>
+#include "TNamed.h"
+
+class RunToTimestamp : public TNamed
+{
+ public:
+  RunToTimestamp() = default;
+  ~RunToTimestamp() = default;
+
+  /// Checks if the converter has a particular run
+  bool Has(uint run) { return mMap.count(run); }
+
+  /// Inserts a new run with a timestamp in the converter database
+  bool insert(uint runNumber, long timestamp);
+
+  /// Gets the timestamp of a run
+  long getTimestamp(uint run);
+
+  /// Prints the content of the converter
+  void print();
+
+ private:
+  std::map<uint, long> mMap;
+  ClassDef(RunToTimestamp, 1) // converter class between run number and timestamp
+};
+
+#endif
