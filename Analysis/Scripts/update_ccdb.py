97ea497b36750279729e4a311e1430b9b2fda2ed
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2020-06-22 16:50:21 +0200

    Add copyright statement, add env python3

--- Analysis/Scripts/update_ccdb.py
+++ Analysis/Scripts/update_ccdb.py
@@ -1,4 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+# Copyright CERN and copyright holders of ALICE O2. This software is
+# distributed under the terms of the GNU General Public License v3 (GPL
+# Version 3), copied verbatim in the file "COPYING".
+#
+# See http://alice-o2.web.cern.ch/license for full licensing information.
+#
+# In applying this license CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
 
 """
 Script to update the CCDB with timestamp non-overlapping objects.
