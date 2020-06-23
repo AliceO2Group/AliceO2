62393527106f41d5e33719c7f0cb28f8900ecc1e
Gian Michele Innocenti <ginnocen@cern.ch>
2020-06-23 15:48:39 +0200

    Split the loop in positive/negative particles

--- Analysis/Tasks/hftrackindexskimscreator.cxx
+++ Analysis/Tasks/hftrackindexskimscreator.cxx
@@ -100,7 +100,8 @@ struct HFTrackIndexSkimsCreator {
                                         "stop iterations if largest change of any X is smaller than this"};
   Configurable<double> d_minrelchi2change{"d_minrelchi2change", 0.9,
                                           "stop iterations is chi2/chi2old > this"};
-
+  Configurable<double> d_minmassDp{"d_minmassDp", 1.5, "min mass dplus presel"};
+  Configurable<double> d_maxmassDp{"d_maxmassDp", 2.1, "max mass dplus presel"};
   Filter seltrack = (aod::seltrack::issel == 1);
 
   void process(aod::Collision const& collision,
@@ -133,90 +134,119 @@ struct HFTrackIndexSkimsCreator {
     df3.setMinParamChange(d_minparamchange);
     df3.setMinRelChi2Change(d_minrelchi2change);
 
-    for (auto it0 = tracks.begin(); it0 != tracks.end(); ++it0) {
-      auto& track_0 = *it0;
-      float x0_ = track_0.x();
-      float alpha0_ = track_0.alpha();
-      std::array<float, 5> arraypar0 = {track_0.y(), track_0.z(), track_0.snp(),
-                                        track_0.tgl(), track_0.signed1Pt()};
-      std::array<float, 15> covpar0 = {track_0.cYY(), track_0.cZY(), track_0.cZZ(),
-                                       track_0.cSnpY(), track_0.cSnpZ(),
-                                       track_0.cSnpSnp(), track_0.cTglY(), track_0.cTglZ(),
-                                       track_0.cTglSnp(), track_0.cTglTgl(),
-                                       track_0.c1PtY(), track_0.c1PtZ(), track_0.c1PtSnp(),
-                                       track_0.c1PtTgl(), track_0.c1Pt21Pt2()};
-      o2::track::TrackParCov trackparvar0(x0_, alpha0_, arraypar0, covpar0);
-      for (auto it1 = it0 + 1; it1 != tracks.end(); ++it1) {
-        auto& track_1 = *it1;
-        if (track_0.signed1Pt() * track_1.signed1Pt() > 0)
+    for (auto i_p1 = tracks.begin(); i_p1 != tracks.end(); ++i_p1) {
+      auto& track_p1 = *i_p1;
+      float x_p1 = track_p1.x();
+      float alpha_p1 = track_p1.alpha();
+      std::array<float, 5> arraypar_p1 = {track_p1.y(), track_p1.z(), track_p1.snp(),
+                                          track_p1.tgl(), track_p1.signed1Pt()};
+      std::array<float, 15> covpar_p1 = {track_p1.cYY(), track_p1.cZY(), track_p1.cZZ(),
+                                         track_p1.cSnpY(), track_p1.cSnpZ(),
+                                         track_p1.cSnpSnp(), track_p1.cTglY(), track_p1.cTglZ(),
+                                         track_p1.cTglSnp(), track_p1.cTglTgl(),
+                                         track_p1.c1PtY(), track_p1.c1PtZ(), track_p1.c1PtSnp(),
+                                         track_p1.c1PtTgl(), track_p1.c1Pt21Pt2()};
+      o2::track::TrackParCov trackparvar_p1(x_p1, alpha_p1, arraypar_p1, covpar_p1);
+      for (auto i_n1 = tracks.begin(); i_n1 != tracks.end(); ++i_n1) {
+        auto& track_n1 = *i_n1;
+        if (track_n1.signed1Pt() > 0)
           continue;
-        float x1_ = track_1.x();
-        float alpha1_ = track_1.alpha();
-        std::array<float, 5> arraypar1 = {track_1.y(), track_1.z(), track_1.snp(),
-                                          track_1.tgl(), track_1.signed1Pt()};
-        std::array<float, 15> covpar1 = {track_1.cYY(), track_1.cZY(), track_1.cZZ(),
-                                         track_1.cSnpY(), track_1.cSnpZ(),
-                                         track_1.cSnpSnp(), track_1.cTglY(), track_1.cTglZ(),
-                                         track_1.cTglSnp(), track_1.cTglTgl(),
-                                         track_1.c1PtY(), track_1.c1PtZ(), track_1.c1PtSnp(),
-                                         track_1.c1PtTgl(), track_1.c1Pt21Pt2()};
-        o2::track::TrackParCov trackparvar1(x1_, alpha1_, arraypar1, covpar1);
+        float x_n1 = track_n1.x();
+        float alpha_n1 = track_n1.alpha();
+        std::array<float, 5> arraypar_n1 = {track_n1.y(), track_n1.z(), track_n1.snp(),
+                                            track_n1.tgl(), track_n1.signed1Pt()};
+        std::array<float, 15> covpar_n1 = {track_n1.cYY(), track_n1.cZY(), track_n1.cZZ(),
+                                           track_n1.cSnpY(), track_n1.cSnpZ(),
+                                           track_n1.cSnpSnp(), track_n1.cTglY(), track_n1.cTglZ(),
+                                           track_n1.cTglSnp(), track_n1.cTglTgl(),
+                                           track_n1.c1PtY(), track_n1.c1PtZ(), track_n1.c1PtSnp(),
+                                           track_n1.c1PtTgl(), track_n1.c1Pt21Pt2()};
+        o2::track::TrackParCov trackparvar_n1(x_n1, alpha_n1, arraypar_n1, covpar_n1);
         df.setUseAbsDCA(true);
-        int nCand = df.process(trackparvar0, trackparvar1);
+        int nCand = df.process(trackparvar_p1, trackparvar_n1);
         if (nCand == 0)
           continue;
         const auto& vtx = df.getPCACandidate();
         //LOGF(info, "vertex x %f", vtx[0]);
-        std::array<float, 3> pvec0;
-        std::array<float, 3> pvec1;
-        df.getTrack(0).getPxPyPzGlo(pvec0);
-        df.getTrack(1).getPxPyPzGlo(pvec1);
-        float mass_ = invmass2prongs(pvec0[0], pvec0[1], pvec0[2], masspion,
-                                     pvec1[0], pvec1[1], pvec1[2], masskaon);
-        float masssw_ = invmass2prongs(pvec0[0], pvec0[1], pvec0[2], masskaon,
-                                       pvec1[0], pvec1[1], pvec1[2], masspion);
-        hftrackindexprong2(track_0.collisionId(),
-                           track_0.globalIndex(),
-                           track_1.globalIndex(), 1.);
+        //std::array<float, 3> pvec0;
+        //std::array<float, 3> pvec1;
+        //df.getTrack(0).getPxPyPzGlo(pvec0);
+        //df.getTrack(1).getPxPyPzGlo(pvec1);
+
+        hftrackindexprong2(track_p1.collisionId(),
+                           track_p1.globalIndex(),
+                           track_n1.globalIndex(), 1.);
         if (do3prong == 1) {
-          for (auto it2 = it0 + 1; it2 != tracks.end(); ++it2) {
-            if (it2 == it0 || it2 == it1)
+          //second loop on positive tracks
+          for (auto i_p2 = i_p1 + 1; i_p2 != tracks.end(); ++i_p2) {
+            auto& track_p2 = *i_p2;
+            if (track_p2.signed1Pt() < 0)
               continue;
-            auto& track_2 = *it2;
-            if (track_1.signed1Pt() * track_2.signed1Pt() > 0)
-              continue;
-            float x2_ = track_2.x();
-            float alpha2_ = track_2.alpha();
-            double mass3prong2 = invmass3prongs2(track_0.px(), track_0.py(), track_0.pz(), masspion,
-                                                 track_1.px(), track_1.py(), track_1.pz(), masspion,
-                                                 track_2.px(), track_2.py(), track_2.pz(), masspion);
-            if (mass3prong2 < 1.5 * 1.5 || mass3prong2 > 2.5 * 2.5)
+            float x_p2 = track_p2.x();
+            float alpha_p2 = track_p2.alpha();
+            double mass3prong2 = invmass3prongs2(track_p1.px(), track_p1.py(), track_p1.pz(), masspion,
+                                                 track_n1.px(), track_n1.py(), track_n1.pz(), masskaon,
+                                                 track_p2.px(), track_p2.py(), track_p2.pz(), masspion);
+            if (mass3prong2 < d_minmassDp * d_minmassDp || mass3prong2 > d_maxmassDp * d_maxmassDp)
               continue;
 
-            std::array<float, 5> arraypar2 = {track_2.y(), track_2.z(), track_2.snp(),
-                                              track_2.tgl(), track_2.signed1Pt()};
-            std::array<float, 15> covpar2 = {track_2.cYY(), track_2.cZY(), track_2.cZZ(),
-                                             track_2.cSnpY(), track_2.cSnpZ(),
-                                             track_2.cSnpSnp(), track_2.cTglY(), track_2.cTglZ(),
-                                             track_2.cTglSnp(), track_2.cTglTgl(),
-                                             track_2.c1PtY(), track_2.c1PtZ(), track_2.c1PtSnp(),
-                                             track_2.c1PtTgl(), track_2.c1Pt21Pt2()};
-            o2::track::TrackParCov trackparvar2(x2_, alpha2_, arraypar2, covpar2);
+            std::array<float, 5> arraypar_p2 = {track_p2.y(), track_p2.z(), track_p2.snp(),
+                                                track_p2.tgl(), track_p2.signed1Pt()};
+            std::array<float, 15> covpar_p2 = {track_p2.cYY(), track_p2.cZY(), track_p2.cZZ(),
+                                               track_p2.cSnpY(), track_p2.cSnpZ(),
+                                               track_p2.cSnpSnp(), track_p2.cTglY(), track_p2.cTglZ(),
+                                               track_p2.cTglSnp(), track_p2.cTglTgl(),
+                                               track_p2.c1PtY(), track_p2.c1PtZ(), track_p2.c1PtSnp(),
+                                               track_p2.c1PtTgl(), track_p2.c1Pt21Pt2()};
+            o2::track::TrackParCov trackparvar_p2(x_p2, alpha_p2, arraypar_p2, covpar_p2);
             df3.setUseAbsDCA(true);
-            int nCand3 = df3.process(trackparvar0, trackparvar1, trackparvar2);
+            int nCand3 = df3.process(trackparvar_p1, trackparvar_n1, trackparvar_p2);
             if (nCand3 == 0)
               continue;
             const auto& vtx3 = df3.getPCACandidate();
-            std::array<float, 3> pvec0_3p;
-            std::array<float, 3> pvec1_3p;
-            std::array<float, 3> pvec2_3p;
-            df3.getTrack(0).getPxPyPzGlo(pvec0_3p);
-            df3.getTrack(1).getPxPyPzGlo(pvec1_3p);
-            df3.getTrack(2).getPxPyPzGlo(pvec2_3p);
-            hftrackindexprong3(track_0.collisionId(),
-                               track_0.globalIndex(),
-                               track_1.globalIndex(),
-                               track_2.globalIndex(), 1.);
+            hftrackindexprong3(track_p1.collisionId(),
+                               track_p1.globalIndex(),
+                               track_n1.globalIndex(),
+                               track_p1.globalIndex(), 2.);
+          }
+          //second loop on negative tracks
+          for (auto i_n2 = i_n1 + 1; i_n2 != tracks.end(); ++i_n2) {
+            auto& track_n2 = *i_n2;
+            if (track_n2.signed1Pt() > 0)
+              continue;
+            float x_n2 = track_n2.x();
+            float alpha_n2 = track_n2.alpha();
+            double mass3prong2 = invmass3prongs2(track_n1.px(), track_n1.py(), track_n1.pz(), masspion,
+                                                 track_p1.px(), track_p1.py(), track_p1.pz(), masskaon,
+                                                 track_n2.px(), track_n2.py(), track_n2.pz(), masspion);
+            if (mass3prong2 < d_minmassDp * d_minmassDp || mass3prong2 > d_maxmassDp * d_maxmassDp)
+              continue;
+
+            std::array<float, 5> arraypar_n2 = {track_n2.y(), track_n2.z(), track_n2.snp(),
+                                                track_n2.tgl(), track_n2.signed1Pt()};
+            std::array<float, 15> covpar_n2 = {track_n2.cYY(), track_n2.cZY(), track_n2.cZZ(),
+                                               track_n2.cSnpY(), track_n2.cSnpZ(),
+                                               track_n2.cSnpSnp(), track_n2.cTglY(), track_n2.cTglZ(),
+                                               track_n2.cTglSnp(), track_n2.cTglTgl(),
+                                               track_n2.c1PtY(), track_n2.c1PtZ(), track_n2.c1PtSnp(),
+                                               track_n2.c1PtTgl(), track_n2.c1Pt21Pt2()};
+            o2::track::TrackParCov trackparvar_n2(x_n2, alpha_n2, arraypar_n2, covpar_n2);
+            df3.setUseAbsDCA(true);
+            int nCand3 = df3.process(trackparvar_n1, trackparvar_p1, trackparvar_n2);
+            if (nCand3 == 0)
+              continue;
+            const auto& vtx3 = df3.getPCACandidate();
+            //std::array<float, 3> pvec0_3p;
+            //std::array<float, 3> pvec1_3p;
+            //std::array<float, 3> pvec2_3p;
+            //df3.getTrack(0).getPxPyPzGlo(pvec0_3p);
+            //df3.getTrack(1).getPxPyPzGlo(pvec1_3p);
+            //df3.getTrack(2).getPxPyPzGlo(pvec2_3p);
+
+            hftrackindexprong3(track_n1.collisionId(),
+                               track_n1.globalIndex(),
+                               track_p1.globalIndex(),
+                               track_n1.globalIndex(), 2.);
           }
         }
       }
