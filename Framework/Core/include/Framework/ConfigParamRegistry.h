abab3cf3e25322577ec511fe2e9fbd596beec505
Giulio Eulisse <giulio.eulisse@cern.ch>
2020-06-24 10:44:03 +0200

    DPL Analysis: add support for Configurable<long>

--- Framework/Core/include/Framework/ConfigParamRegistry.h
+++ Framework/Core/include/Framework/ConfigParamRegistry.h
@@ -54,6 +54,7 @@ class ConfigParamRegistry
     try {
       if constexpr (std::is_same_v<T, int> ||
                     std::is_same_v<T, int64_t> ||
+                    std::is_same_v<T, long> ||
                     std::is_same_v<T, float> ||
                     std::is_same_v<T, double> ||
                     std::is_same_v<T, bool>) {
