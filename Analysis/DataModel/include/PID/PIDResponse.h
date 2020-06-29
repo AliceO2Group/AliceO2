de2cb6f88011e41a37f296c2cbb8e537b97187ea
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2020-06-29 18:23:55 +0200

    Update PID response of TOF

--- Analysis/DataModel/include/PID/PIDResponse.h
+++ Analysis/DataModel/include/PID/PIDResponse.h
@@ -9,15 +9,13 @@
 // or submit itself to any jurisdiction.
 
 ///
+/// Class to provide PID response
 /// \file   PIDResponse.h
 /// \author Nicolo' Jacazio
 ///
 
 #ifndef O2_FRAMEWORK_PIDRESPONSE_H_
 #define O2_FRAMEWORK_PIDRESPONSE_H_
-
-// ROOT includes
-#include "TMath.h"
 
 // O2 includes
 #include "Framework/ASoA.h"
@@ -26,11 +24,12 @@
 #include "Framework/AnalysisDataModel.h"
 #include "ReconstructionDataFormats/Track.h"
 #include "PID/PIDTOF.h"
+#include "ReconstructionDataFormats/PID.h"
 
 namespace o2::aod
 {
 
-namespace pidTOF
+namespace pidTOFbeta
 {
 DECLARE_SOA_COLUMN(Beta, beta, float);
 DECLARE_SOA_COLUMN(BetaError, betaerror, float);
@@ -40,211 +39,109 @@ DECLARE_SOA_COLUMN(ExpBetaElError, expbetaelerror, float);
 //
 DECLARE_SOA_COLUMN(SeparationBetaEl, separationbetael, float);
 DECLARE_SOA_DYNAMIC_COLUMN(DiffBetaEl, diffbetael, [](float beta, float expbetael) -> float { return beta - expbetael; });
-//
-DECLARE_SOA_COLUMN(ExpSigmaPi, expsigmaPi, float);
-DECLARE_SOA_COLUMN(ExpSigmaKa, expsigmaKa, float);
-DECLARE_SOA_COLUMN(ExpSigmaPr, expsigmaPr, float);
-//
-DECLARE_SOA_COLUMN(NSigmaPi, nsigmaPi, float);
-DECLARE_SOA_COLUMN(NSigmaKa, nsigmaKa, float);
-DECLARE_SOA_COLUMN(NSigmaPr, nsigmaPr, float);
-//
-DECLARE_SOA_COLUMN(ExppEl, exppEl, float);
-DECLARE_SOA_COLUMN(ExppMu, exppMu, float);
-DECLARE_SOA_COLUMN(ExppPi, exppPi, float);
-DECLARE_SOA_COLUMN(ExppKa, exppKa, float);
-DECLARE_SOA_COLUMN(ExppPr, exppPr, float);
-DECLARE_SOA_COLUMN(ExppDe, exppDe, float);
-//
-DECLARE_SOA_COLUMN(CExpTimeEl_El, cexpTimeEl_El, float);
-DECLARE_SOA_COLUMN(CExpTimeMu_El, cexpTimeMu_El, float);
-DECLARE_SOA_COLUMN(CExpTimePi_El, cexpTimePi_El, float);
-DECLARE_SOA_COLUMN(CExpTimeKa_El, cexpTimeKa_El, float);
-DECLARE_SOA_COLUMN(CExpTimePr_El, cexpTimePr_El, float);
-DECLARE_SOA_COLUMN(CExpTimeDe_El, cexpTimeDe_El, float);
-//
-// DECLARE_SOA_COLUMN(CExpTimeEl_Mu, cexpTimeEl_Mu, float);
-// DECLARE_SOA_COLUMN(CExpTimeMu_Mu, cexpTimeMu_Mu, float);
-// DECLARE_SOA_COLUMN(CExpTimePi_Mu, cexpTimePi_Mu, float);
-// DECLARE_SOA_COLUMN(CExpTimeKa_Mu, cexpTimeKa_Mu, float);
-// DECLARE_SOA_COLUMN(CExpTimePr_Mu, cexpTimePr_Mu, float);
-// DECLARE_SOA_COLUMN(CExpTimeDe_Mu, cexpTimeDe_Mu, float);
-// //
-// DECLARE_SOA_COLUMN(CExpTimeEl_Pi, cexpTimeEl_Pi, float);
-// DECLARE_SOA_COLUMN(CExpTimeMu_Pi, cexpTimeMu_Pi, float);
-// DECLARE_SOA_COLUMN(CExpTimePi_Pi, cexpTimePi_Pi, float);
-// DECLARE_SOA_COLUMN(CExpTimeKa_Pi, cexpTimeKa_Pi, float);
-// DECLARE_SOA_COLUMN(CExpTimePr_Pi, cexpTimePr_Pi, float);
-// DECLARE_SOA_COLUMN(CExpTimeDe_Pi, cexpTimeDe_Pi, float);
-// //
-// DECLARE_SOA_COLUMN(CExpTimeEl_Ka, cexpTimeEl_Ka, float);
-// DECLARE_SOA_COLUMN(CExpTimeMu_Ka, cexpTimeMu_Ka, float);
-// DECLARE_SOA_COLUMN(CExpTimePi_Ka, cexpTimePi_Ka, float);
-// DECLARE_SOA_COLUMN(CExpTimeKa_Ka, cexpTimeKa_Ka, float);
-// DECLARE_SOA_COLUMN(CExpTimePr_Ka, cexpTimePr_Ka, float);
-// DECLARE_SOA_COLUMN(CExpTimeDe_Ka, cexpTimeDe_Ka, float);
-// //
-// DECLARE_SOA_COLUMN(CExpTimeEl_Pr, cexpTimeEl_Pr, float);
-// DECLARE_SOA_COLUMN(CExpTimeMu_Pr, cexpTimeMu_Pr, float);
-// DECLARE_SOA_COLUMN(CExpTimePi_Pr, cexpTimePi_Pr, float);
-// DECLARE_SOA_COLUMN(CExpTimeKa_Pr, cexpTimeKa_Pr, float);
-// DECLARE_SOA_COLUMN(CExpTimePr_Pr, cexpTimePr_Pr, float);
-// DECLARE_SOA_COLUMN(CExpTimeDe_Pr, cexpTimeDe_Pr, float);
-// //
-// DECLARE_SOA_COLUMN(CExpTimeEl_De, cexpTimeEl_De, float);
-// DECLARE_SOA_COLUMN(CExpTimeMu_De, cexpTimeMu_De, float);
-// DECLARE_SOA_COLUMN(CExpTimePi_De, cexpTimePi_De, float);
-// DECLARE_SOA_COLUMN(CExpTimeKa_De, cexpTimeKa_De, float);
-// DECLARE_SOA_COLUMN(CExpTimePr_De, cexpTimePr_De, float);
-// DECLARE_SOA_COLUMN(CExpTimeDe_De, cexpTimeDe_De, float);
-//
+} // namespace pidTOFbeta
+
+namespace pidTOF
+{
+// Expected times
+DECLARE_SOA_COLUMN(ExpTimeEl, expTimeEl, float);
+DECLARE_SOA_COLUMN(ExpTimeMu, expTimeMu, float);
+DECLARE_SOA_COLUMN(ExpTimePi, expTimePi, float);
+DECLARE_SOA_COLUMN(ExpTimeKa, expTimeKa, float);
+DECLARE_SOA_COLUMN(ExpTimePr, expTimePr, float);
+DECLARE_SOA_COLUMN(ExpTimeDe, expTimeDe, float);
+DECLARE_SOA_COLUMN(ExpTimeTr, expTimeTr, float);
+DECLARE_SOA_COLUMN(ExpTimeHe, expTimeHe, float);
+DECLARE_SOA_COLUMN(ExpTimeAl, expTimeAl, float);
+// Expected sigma
+DECLARE_SOA_COLUMN(ExpSigmaEl, expSigmaEl, float);
+DECLARE_SOA_COLUMN(ExpSigmaMu, expSigmaMu, float);
+DECLARE_SOA_COLUMN(ExpSigmaPi, expSigmaPi, float);
+DECLARE_SOA_COLUMN(ExpSigmaKa, expSigmaKa, float);
+DECLARE_SOA_COLUMN(ExpSigmaPr, expSigmaPr, float);
+DECLARE_SOA_COLUMN(ExpSigmaDe, expSigmaDe, float);
+DECLARE_SOA_COLUMN(ExpSigmaTr, expSigmaTr, float);
+DECLARE_SOA_COLUMN(ExpSigmaHe, expSigmaHe, float);
+DECLARE_SOA_COLUMN(ExpSigmaAl, expSigmaAl, float);
+// NSigma
+DECLARE_SOA_COLUMN(NSigmaEl, nSigmaEl, float);
+DECLARE_SOA_COLUMN(NSigmaMu, nSigmaMu, float);
+DECLARE_SOA_COLUMN(NSigmaPi, nSigmaPi, float);
+DECLARE_SOA_COLUMN(NSigmaKa, nSigmaKa, float);
+DECLARE_SOA_COLUMN(NSigmaPr, nSigmaPr, float);
+DECLARE_SOA_COLUMN(NSigmaDe, nSigmaDe, float);
+DECLARE_SOA_COLUMN(NSigmaTr, nSigmaTr, float);
+DECLARE_SOA_COLUMN(NSigmaHe, nSigmaHe, float);
+DECLARE_SOA_COLUMN(NSigmaAl, nSigmaAl, float);
 } // namespace pidTOF
+
+using namespace pidTOFbeta;
+DECLARE_SOA_TABLE(pidRespTOFbeta, "AOD", "pidRespTOFbeta",
+                  Beta, BetaError,
+                  ExpBetaEl, ExpBetaElError,
+                  SeparationBetaEl,
+                  DiffBetaEl<Beta, ExpBetaEl>);
 using namespace pidTOF;
 DECLARE_SOA_TABLE(pidRespTOF, "AOD", "pidRespTOF",
-                  Beta,
-                  BetaError,
-                  ExpBetaEl,
-                  ExpBetaElError,
-                  SeparationBetaEl,
-                  DiffBetaEl<Beta, ExpBetaEl>,
-                  ExpSigmaPi,
-                  ExpSigmaKa,
-                  ExpSigmaPr,
-                  NSigmaPi,
-                  NSigmaKa,
-                  NSigmaPr
-                  // ExppEl,
-                  // ExppMu,
-                  // ExppPi,
-                  // ExppKa,
-                  // ExppPr,
-                  // ExppDe
-                  // CExpTimeEl_El,
-                  // CExpTimeMu_El,
-                  // CExpTimePi_El,
-                  // CExpTimeKa_El,
-                  // CExpTimePr_El,
-                  // CExpTimeDe_El
-                  // //
-                  // CExpTimeEl_Mu,
-                  // CExpTimeMu_Mu,
-                  // CExpTimePi_Mu,
-                  // CExpTimeKa_Mu,
-                  // CExpTimePr_Mu,
-                  // CExpTimeDe_Mu,
-                  // //
-                  // CExpTimeEl_Pi,
-                  // CExpTimeMu_Pi,
-                  // CExpTimePi_Pi,
-                  // CExpTimeKa_Pi,
-                  // CExpTimePr_Pi,
-                  // CExpTimeDe_Pi,
-                  // //
-                  // CExpTimeEl_Ka,
-                  // CExpTimeMu_Ka,
-                  // CExpTimePi_Ka,
-                  // CExpTimeKa_Ka,
-                  // CExpTimePr_Ka,
-                  // CExpTimeDe_Ka,
-                  // //
-                  // CExpTimeEl_Pr,
-                  // CExpTimeMu_Pr,
-                  // CExpTimePi_Pr,
-                  // CExpTimeKa_Pr,
-                  // CExpTimePr_Pr,
-                  // CExpTimeDe_Pr,
-                  // //
-                  // CExpTimeEl_De,
-                  // CExpTimeMu_De,
-                  // CExpTimePi_De,
-                  // CExpTimeKa_De,
-                  // CExpTimePr_De,
-                  // CExpTimeDe_De
-                  // //
-);
+                  ExpTimeEl, ExpTimeMu, ExpTimePi, ExpTimeKa, ExpTimePr, ExpTimeDe, ExpTimeTr, ExpTimeHe, ExpTimeAl,
+                  ExpSigmaEl, ExpSigmaMu, ExpSigmaPi, ExpSigmaKa, ExpSigmaPr, ExpSigmaDe, ExpSigmaTr, ExpSigmaHe, ExpSigmaAl,
+                  NSigmaEl, NSigmaMu, NSigmaPi, NSigmaKa, NSigmaPr, NSigmaDe, NSigmaTr, NSigmaHe, NSigmaAl);
+
 } // namespace o2::aod
 
 using namespace o2;
 using namespace o2::framework;
 using namespace o2::pid::tof;
 using namespace o2::framework::expressions;
+using namespace o2::track;
 
 struct pidTOFTask {
   Produces<aod::pidRespTOF> tofpid;
+  Produces<aod::pidRespTOFbeta> tofpidbeta;
 
   void process(aod::Collision const& collision, soa::Join<aod::Tracks, aod::TracksExtra> const& tracks)
   {
     LOGF(info, "Tracks for collision: %d", tracks.size());
     Response resp = pid::tof::Response();
     for (auto i : tracks) {
+      resp.InitResponse(i.p(), i.tofExpMom(), i.length(), i.tofSignal());
       // float EVTIME = collision.collisionTime0();
       float EVTIME = collision.collisionTime();
-      float BETA = beta(i.length(), i.tofSignal(), EVTIME);
-      float BETAERROR = betaerror(i.length(), i.tofSignal(), EVTIME);
-      float EXPBETAEL = expbeta(p(i.eta(), i.signed1Pt()), kMassEl);
+      float EXPBETAEL = expbeta(p(i.eta(), i.signed1Pt()), PID::getMass(PID::Electron));
       float EXPBETAELERROR = 0;
-      //  resp.mParam.GetExpectedSigma(i.p(), i.tofSignal(), collision.collisionTimeRes0(), kMassPi),
-      //  resp.mParam.GetExpectedSigma(i.p(), i.tofSignal(), collision.collisionTimeRes0(), kMassKa),
-      //  resp.mParam.GetExpectedSigma(i.p(), i.tofSignal(), collision.collisionTimeRes0(), kMassPr),
-      //
-      //  0,0,0
-      //  resp.mParam.GetNSigma(i.p(), i.tofSignal(), i.tofExpPi(), collision.collisionTime0(), collision.collisionTimeRes0(), kMassPi),
-      //  resp.mParam.GetNSigma(i.p(), i.tofSignal(), i.tofExpKa(), collision.collisionTime0(), collision.collisionTimeRes0(), kMassKa),
-      //  resp.mParam.GetNSigma(i.p(), i.tofSignal(), i.tofExpPr(), collision.collisionTime0(), collision.collisionTimeRes0(), kMassPr)
-      tofpid(BETA,
-             BETAERROR,
-             EXPBETAEL,
-             EXPBETAELERROR,
-             BETA > 0 ? (BETA - EXPBETAEL) / sqrt(BETAERROR * BETAERROR + EXPBETAELERROR * EXPBETAELERROR) : -999,
-             resp.mParam.GetExpectedSigma(i.p(), i.tofSignal(), collision.collisionTimeRes(), kMassPi),
-             resp.mParam.GetExpectedSigma(i.p(), i.tofSignal(), collision.collisionTimeRes(), kMassKa),
-             resp.mParam.GetExpectedSigma(i.p(), i.tofSignal(), collision.collisionTimeRes(), kMassPr),
-             resp.mParam.GetNSigma(i.p(), i.tofSignal(), i.tofExpPi(), collision.collisionTime(), collision.collisionTimeRes(), kMassPi),
-             resp.mParam.GetNSigma(i.p(), i.tofSignal(), i.tofExpKa(), collision.collisionTime(), collision.collisionTimeRes(), kMassKa),
-             resp.mParam.GetNSigma(i.p(), i.tofSignal(), i.tofExpPr(), collision.collisionTime(), collision.collisionTimeRes(), kMassPr)
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpEl(), i.length(), kMassEl), i.length(), kMassEl),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpEl(), i.length(), kMassEl), i.length(), kMassMu),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpEl(), i.length(), kMassEl), i.length(), kMassPi),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpEl(), i.length(), kMassEl), i.length(), kMassKa),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpEl(), i.length(), kMassEl), i.length(), kMassPr),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpEl(), i.length(), kMassEl), i.length(), kMassDe)
-             //  //
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpMu(), i.length(), kMassMu), i.length(), kMassEl),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpMu(), i.length(), kMassMu), i.length(), kMassMu),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpMu(), i.length(), kMassMu), i.length(), kMassPi),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpMu(), i.length(), kMassMu), i.length(), kMassKa),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpMu(), i.length(), kMassMu), i.length(), kMassPr),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpMu(), i.length(), kMassMu), i.length(), kMassDe),
-             //  //
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpPi(), i.length(), kMassPi), i.length(), kMassEl),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpPi(), i.length(), kMassPi), i.length(), kMassMu),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpPi(), i.length(), kMassPi), i.length(), kMassPi),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpPi(), i.length(), kMassPi), i.length(), kMassKa),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpPi(), i.length(), kMassPi), i.length(), kMassPr),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpPi(), i.length(), kMassPi), i.length(), kMassDe),
-             //  //
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpKa(), i.length(), kMassKa), i.length(), kMassEl),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpKa(), i.length(), kMassKa), i.length(), kMassMu),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpKa(), i.length(), kMassKa), i.length(), kMassPi),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpKa(), i.length(), kMassKa), i.length(), kMassKa),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpKa(), i.length(), kMassKa), i.length(), kMassPr),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpKa(), i.length(), kMassKa), i.length(), kMassDe),
-             //  //
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpPr(), i.length(), kMassPr), i.length(), kMassEl),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpPr(), i.length(), kMassPr), i.length(), kMassMu),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpPr(), i.length(), kMassPr), i.length(), kMassPi),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpPr(), i.length(), kMassPr), i.length(), kMassKa),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpPr(), i.length(), kMassPr), i.length(), kMassPr),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpPr(), i.length(), kMassPr), i.length(), kMassDe),
-             //  //
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpDe(), i.length(), kMassDe), i.length(), kMassEl),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpDe(), i.length(), kMassDe), i.length(), kMassMu),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpDe(), i.length(), kMassDe), i.length(), kMassPi),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpDe(), i.length(), kMassDe), i.length(), kMassKa),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpDe(), i.length(), kMassDe), i.length(), kMassPr),
-             //  ComputeTOFExpTime(ComputeExpectedMomentum(i.tofExpDe(), i.length(), kMassDe), i.length(), kMassDe)
-             //  //
-      );
+      tofpidbeta(resp.GetBeta(),
+                 resp.GetBetaExpectedSigma(),
+                 EXPBETAEL,
+                 EXPBETAELERROR,
+                 resp.GetBeta() > 0 ? (resp.GetBeta() - EXPBETAEL) / sqrt(resp.GetBetaExpectedSigma() * resp.GetBetaExpectedSigma() + EXPBETAELERROR * EXPBETAELERROR) : -999);
+      tofpid(
+        ComputeTOFExpTime(i.tofExpMom(), i.length(), PID::getMass(PID::Electron)),
+        ComputeTOFExpTime(i.tofExpMom(), i.length(), PID::getMass(PID::Muon)),
+        ComputeTOFExpTime(i.tofExpMom(), i.length(), PID::getMass(PID::Pion)),
+        ComputeTOFExpTime(i.tofExpMom(), i.length(), PID::getMass(PID::Kaon)),
+        ComputeTOFExpTime(i.tofExpMom(), i.length(), PID::getMass(PID::Proton)),
+        ComputeTOFExpTime(i.tofExpMom(), i.length(), PID::getMass(PID::Deuteron)),
+        ComputeTOFExpTime(i.tofExpMom(), i.length(), PID::getMass(PID::Triton)),
+        ComputeTOFExpTime(i.tofExpMom(), i.length(), PID::getMass(PID::Helium3)),
+        ComputeTOFExpTime(i.tofExpMom(), i.length(), PID::getMass(PID::Alpha)),
+        resp.mParam.GetExpectedSigma(i.p(), i.tofSignal(), collision.collisionTimeRes(), PID::getMass(PID::Electron)),
+        resp.mParam.GetExpectedSigma(i.p(), i.tofSignal(), collision.collisionTimeRes(), PID::getMass(PID::Muon)),
+        resp.mParam.GetExpectedSigma(i.p(), i.tofSignal(), collision.collisionTimeRes(), PID::getMass(PID::Pion)),
+        resp.mParam.GetExpectedSigma(i.p(), i.tofSignal(), collision.collisionTimeRes(), PID::getMass(PID::Kaon)),
+        resp.mParam.GetExpectedSigma(i.p(), i.tofSignal(), collision.collisionTimeRes(), PID::getMass(PID::Proton)),
+        resp.mParam.GetExpectedSigma(i.p(), i.tofSignal(), collision.collisionTimeRes(), PID::getMass(PID::Deuteron)),
+        resp.mParam.GetExpectedSigma(i.p(), i.tofSignal(), collision.collisionTimeRes(), PID::getMass(PID::Triton)),
+        resp.mParam.GetExpectedSigma(i.p(), i.tofSignal(), collision.collisionTimeRes(), PID::getMass(PID::Helium3)),
+        resp.mParam.GetExpectedSigma(i.p(), i.tofSignal(), collision.collisionTimeRes(), PID::getMass(PID::Alpha)),
+        resp.mParam.GetNSigma(i.p(), i.tofSignal(), i.tofExpPi(), collision.collisionTime(), collision.collisionTimeRes(), PID::getMass(PID::Electron)),
+        resp.mParam.GetNSigma(i.p(), i.tofSignal(), i.tofExpPi(), collision.collisionTime(), collision.collisionTimeRes(), PID::getMass(PID::Muon)),
+        resp.mParam.GetNSigma(i.p(), i.tofSignal(), i.tofExpPi(), collision.collisionTime(), collision.collisionTimeRes(), PID::getMass(PID::Pion)),
+        resp.mParam.GetNSigma(i.p(), i.tofSignal(), i.tofExpKa(), collision.collisionTime(), collision.collisionTimeRes(), PID::getMass(PID::Kaon)),
+        resp.mParam.GetNSigma(i.p(), i.tofSignal(), i.tofExpPr(), collision.collisionTime(), collision.collisionTimeRes(), PID::getMass(PID::Proton)),
+        resp.mParam.GetNSigma(i.p(), i.tofSignal(), i.tofExpPi(), collision.collisionTime(), collision.collisionTimeRes(), PID::getMass(PID::Deuteron)),
+        resp.mParam.GetNSigma(i.p(), i.tofSignal(), i.tofExpPi(), collision.collisionTime(), collision.collisionTimeRes(), PID::getMass(PID::Triton)),
+        resp.mParam.GetNSigma(i.p(), i.tofSignal(), i.tofExpPi(), collision.collisionTime(), collision.collisionTimeRes(), PID::getMass(PID::Helium3)),
+        resp.mParam.GetNSigma(i.p(), i.tofSignal(), i.tofExpPi(), collision.collisionTime(), collision.collisionTimeRes(), PID::getMass(PID::Alpha)));
     }
   }
 };
