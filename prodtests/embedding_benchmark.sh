a48b032fd80cb8dd9ab745eeed74a37f3f28bbfd
Sandro Wenzel <sandro.wenzel@cern.ch>
2020-06-30 12:10:51 +0200

    Improved embedding benchmark
    * able to benchmark local + network scenario
    * in network scenario do asyncronous network cp together with signal production
    * somewhat specific to EOS at the moment and needs further generalization for
      real GRID usage

--- prodtests/embedding_benchmark.sh
+++ prodtests/embedding_benchmark.sh
@@ -37,7 +37,7 @@ taskwrapper() {
 
   # launch the actual command in the background
   echo "Launching task: ${command} &> $logfile &"
-  command="TIME=\"#walltime %e\" ${O2_ROOT}/share/scripts/monitor-mem.sh /usr/bin/time --output=${logfile}_time '${command}'"
+  command="TIME=\"#walltime %e %M\" ${O2_ROOT}/share/scripts/monitor-mem.sh /usr/bin/time --output=${logfile}_time '${command}'"
   eval ${command} &> $logfile &
 
   # THE NEXT PART IS THE SUPERVISION PART
@@ -106,18 +106,19 @@ taskwrapper() {
 
 
 # default number of events
-nevS=100   # nevents signal
-nevBG=10   # nevents background
+nevS=${nevS:-100}  # nevents signal
+nevBG=${nevBG:-10}  # nevents background
 NCPUS=8    # default on GRID
 
 # default interaction rates in kHz
 intRate=50
 
-generS="pythia8hf"
-generBG="pythia8hi"
+generS=${generS:-"pythia8hf"}
+generBG=${generBG:-"pythia8hi"}
+scenario=${scenario:-"local"} # or network
 
 # default sim engine
-engine="TGeant3"
+engine=${engine:-"TGeant3"}
 
 # options to pass to every workflow
 gloOpt=" -b --run "
@@ -129,6 +130,34 @@ Usage()
   exit
 }
 
+CACHEPATH="/eos/user/a/aliperf/simulation/mock_data/background_cache/1"
+
+# function performing network transfer of kinematic background files --> needed for signal production
+Fetch_Background_EOS_Kine() {
+  echo "Start Fetching background kinematics"
+  eos cp root://eosuser.cern.ch//${CACHEPATH}/o2simbg_Kine.root .
+  echo "Fetching background kinematics finished"
+}
+
+# function performing network transfer of hits background files --> needed for digitization
+Fetch_Background_EOS_Hits() {
+  echo "Start Fetching background hits"
+  # better do this with an xrootd query but should be ok to start from
+  export EOS_MGM_URL=root://eosuser.cern.ch
+  eos cp ${CACHEPATH}/o2simbg_geometry.root .
+  eos cp ${CACHEPATH}/o2simbg_grp.root .
+  eos cp ${CACHEPATH}/o2simbg_Hits*.root .
+  echo "Fetching background hits finished"
+}
+
+# Upload to EOS
+Upload_Background_EOS() {
+  echo "Start uploading background event"
+  export EOS_MGM_URL=root://eosuser.cern.ch
+  eos cp o2simbg* ${CACHEPATH}
+  echo "Uploading background hits finished"
+}
+
 while [ $# -gt 0 ] ; do
     case $1 in
 	-e) engine=$2; shift 2 ;;
@@ -137,33 +166,78 @@ while [ $# -gt 0 ] ; do
     esac
 done
 
-echo "Running background simulation for $nevBG $collSyst events with $generBG generator and engine $engine"
-taskwrapper sim_bg.log o2-sim -j ${NCPUS} -n"$nevBG" --configKeyValue "Diamond.width[2]=6." -g "$generBG" -e "$engine" -o o2simbg --skipModules ZDC
-# extract time
+produce_background() 
+{
+  echo "Running background simulation for $nevBG $collSyst events with $generBG generator and engine $engine"
+  taskwrapper sim_bg.log o2-sim -j ${NCPUS} -n"$nevBG" --configKeyValue "Diamond.width[2]=6." -g "$generBG" -e "$engine" -o o2simbg --skipModules ZDC --seed ${SEED:-1}
+}
 
-echo "Running signal simulation for $nevS $collSyst events with $generS generator and engine $engine"
-taskwrapper sim_s.log o2-sim -j ${NCPUS} -n"$nevS" --configKeyValue "Diamond.width[2]=6." -g "$generS" -e "$engine" -o o2sims --embedIntoFile o2simbg_Kine.root --skipModules ZDC
-# extract time
+produce_signal() 
+{
+  echo "Running signal simulation for $nevS $collSyst events with $generS generator and engine $engine"
+  taskwrapper sim_s.log o2-sim -j ${NCPUS} -n"$nevS" --configKeyValue "Diamond.width[2]=6." -g "$generS" -e "$engine" -o o2sims --embedIntoFile o2simbg_Kine.root --skipModules ZDC --seed ${SEED:-1}
+}
 
-echo "Running digitization for $intRate kHz interaction rate"
-# We run the digitization in stages in order to use
-# the given number of CPUs in a good way. The order and sequence here can be modified and improvements will be
-# achievable when "REST" contains more parallelism. TPC and TRD should use the resources fully already.
-intRate=$((1000*(intRate)));
-taskwrapper digi_TPC.log o2-sim-digitizer-workflow --tpc-lanes ${NCPUS} --onlyDet TPC $gloOpt --sims o2simbg,o2sims -n ${nevS} --interactionRate $intRate --disable-mc
-taskwrapper digi_TRD.log o2-sim-digitizer-workflow --onlyDet TRD $gloOpt --configKeyValues="TRDSimParams.digithreads=${NCPUS}" --sims o2simbg,o2sims -n ${nevS} --interactionRate $intRate --disable-mc
-taskwrapper digi_REST.log o2-sim-digitizer-workflow --skipDet TRD,TPC $gloOpt --sims o2simbg,o2sims -n ${nevS} --interactionRate $intRate --disable-mc
+do_transport_local() 
+{
+  produce_background
+  Upload_Background_EOS
+  produce_signal
+}
 
-# extract gain from embedding procedure and background reusage by analysing reported times:
-transporttime_BG=`awk '/walltime/{print $2}' sim_bg.log_time`
-transporttime_S=`awk '/walltime/{print $2}' sim_s.log_time`
-digi_time_TPC=`awk '/walltime/{print $2}' digi_TPC.log_time`
-digi_time_TRD=`awk '/walltime/{print $2}' digi_TRD.log_time`
-digi_time_REST=`awk '/walltime/{print $2}' digi_REST.log_time`
+do_transport_usecachedBG() 
+{
+  echo "Start fetching background kinematics"
+  Fetch_Background_EOS_Kine &
+  KINE_PID=$!
 
-echo "$transporttime_BG $transporttime_S $digi_time_TPC $digi_time_TRD $digi_time_REST" | awk -v nS="${nevS}" -v nB="${nevBG}" '//{noembed=($1*nS/(1.*nB) + $2) + $3 + $4 + $5;embed=($1 + $2) + $3 + $4 + $5; print "NBG "nB " NSignal "nS" Ordinary-time "noembed," Embed-time "embed" Ratio ",noembed/embed }'
+  Fetch_Background_EOS_Hits &
+  HITS_PID=$!
 
-# TODO: compare this to scenario where we fetch background events from the network!
-# Run this actually containerized to fix the CPU + MEMORY space.
+  wait ${KINE_PID}
+  produce_signal
+
+  wait ${HITS_PID}
+}
+
+digitize() {
+  echo "Running digitization for $intRate kHz interaction rate"
+  # We run the digitization in stages in order to use
+  # the given number of CPUs in a good way. The order and sequence here can be modified and improvements will be
+  # achievable when "REST" contains more parallelism. TPC and TRD should use the resources fully already.
+  intRate=$((1000*(intRate)));
+  taskwrapper digi_TPC.log o2-sim-digitizer-workflow --tpc-lanes ${NCPUS} --onlyDet TPC $gloOpt --sims o2simbg,o2sims -n ${nevS} --interactionRate $intRate --disable-mc --
+  taskwrapper digi_TRD.log o2-sim-digitizer-workflow --onlyDet TRD $gloOpt --configKeyValues="TRDSimParams.digithreads=${NCPUS}" --sims o2simbg,o2sims -n ${nevS} --interactionRate $intRate --disable-mc --incontext collisioncontext.root
+  taskwrapper digi_REST.log o2-sim-digitizer-workflow --skipDet TRD,TPC $gloOpt --sims o2simbg,o2sims -n ${nevS} --interactionRate $intRate --disable-mc --incontext collisioncontext.root
+}
+
+if [[ "${scenario}" == "local" ]]; then
+  do_transport_local
+  digitize
+
+  # extract gain from embedding procedure and background reusage by analysing reported times:
+  transporttime_BG=`awk '/walltime/{print $2}' sim_bg.log_time`
+  transporttime_S=`awk '/walltime/{print $2}' sim_s.log_time`
+  digi_time_TPC=`awk '/walltime/{print $2}' digi_TPC.log_time`
+  digi_time_TRD=`awk '/walltime/{print $2}' digi_TRD.log_time`
+  digi_time_REST=`awk '/walltime/{print $2}' digi_REST.log_time`
+  echo "$transporttime_BG $transporttime_S $digi_time_TPC $digi_time_TRD $digi_time_REST"
+  echo "$transporttime_BG $transporttime_S $digi_time_TPC $digi_time_TRD $digi_time_REST" | awk -v nS="${nevS}" -v nB="${nevBG}" '//{digi=$3+$4+$5;noembed=($1*nS/(1.*nB) + $2) + digi;embed=($1 + $2) + digi; embedCachedIO=($2 + digi);print "NBG "nB " NSignal "nS" Ordinary-time "noembed," Embed-time "embed" Embed-time (cached optimal)  "embedCachedIO" Ratio ",noembed/embed" Ratio optimal"noembed/embedCachedIO }'
+fi
+
+if [[ "${scenario}" == "network" ]]; then
+  # Scenario where we fetch background events from the network!
+  do_transport_usecachedBG
+
+  digitize
+
+  # extract gain from embedding procedure and background reusage by analysing reported times:
+  transporttime_S=`awk '/walltime/{print $2}' sim_s.log_time`
+  digi_time_TPC=`awk '/walltime/{print $2}' digi_TPC.log_time`
+  digi_time_TRD=`awk '/walltime/{print $2}' digi_TRD.log_time`
+  digi_time_REST=`awk '/walltime/{print $2}' digi_REST.log_time`
+
+  echo "$transporttime_S $digi_time_TPC $digi_time_TRD $digi_time_REST"
+fi
 
 rm localhost*
