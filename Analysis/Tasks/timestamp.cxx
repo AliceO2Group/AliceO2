d3d7f1ed2543afcd14bd689c87c1b2471c699bf4
Evgeny Kryshen <evgeny.kryshen@cern.ch>
2020-07-16 16:02:59 +0300

    Read SOR orbit counters from CCDB (#3996)

--- Analysis/Tasks/timestamp.cxx
+++ Analysis/Tasks/timestamp.cxx
@@ -20,7 +20,7 @@
 #include <CCDB/BasicCCDBManager.h>
 #include "CommonDataFormat/InteractionRecord.h"
 #include "DetectorsRaw/HBFUtils.h"
-
+#include <map>
 using namespace o2::framework;
 using namespace o2::header;
 using namespace o2;
@@ -28,6 +28,7 @@ using namespace o2;
 struct TimestampTask {
   Produces<aod::Timestamps> ts_table;
   RunToTimestamp* converter = nullptr;
+  std::map<int, int>* mapStartOrbit = nullptr;
   Service<o2::ccdb::BasicCCDBManager> ccdb;
   Configurable<std::string> path{"ccdb-path", "Analysis/Core/RunToTimestamp", "path to the ccdb object"};
   Configurable<long> timestamp{"ccdb-timestamp", -1, "timestamp of the object"};
@@ -41,6 +42,10 @@ struct TimestampTask {
     } else {
       LOGF(fatal, "Cannot find run-number to timestamp converter in path '%s'.", path.value.data());
     }
+    mapStartOrbit = ccdb->get<std::map<int, int>>("Trigger/StartOrbit");
+    if (!mapStartOrbit) {
+      LOGF(fatal, "Cannot find map of SOR orbits in CCDB in path Trigger/StartOrbit");
+    }
   }
 
   void process(aod::BC const& bc)
@@ -48,8 +53,10 @@ struct TimestampTask {
     long timestamp = converter->getTimestamp(bc.runNumber());
     uint16_t currentBC = bc.globalBC() % o2::constants::lhc::LHCMaxBunches;
     uint32_t currentOrbit = bc.globalBC() / o2::constants::lhc::LHCMaxBunches;
+    uint16_t initialBC = 0; // exact bc number not relevant due to ms precision of timestamps
+    uint32_t initialOrbit = mapStartOrbit->at(bc.runNumber());
     InteractionRecord current(currentBC, currentOrbit);
-    InteractionRecord initial = o2::raw::HBFUtils::Instance().getFirstIR();
+    InteractionRecord initial(initialBC, initialOrbit);
     timestamp += (current - initial).bc2ns() / 1000000;
     ts_table(timestamp);
   }
