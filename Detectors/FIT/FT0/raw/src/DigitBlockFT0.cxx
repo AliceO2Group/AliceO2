ad0c1116fe6795bdc00db24e3c5e4d0381d44245
AllaMaevskaya <Alla.Maevskaia@cern.ch>
2020-07-18 11:58:02 +0300

    read raw data and convert to digits

---
+++ Detectors/FIT/FT0/raw/src/DigitBlockFT0.cxx
@@ -0,0 +1,17 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#include "FT0Raw/DigitBlockFT0.h"
+using namespace o2::ft0;
+
+ClassImp(DigitBlockFT0);
+
+int DigitBlockFT0::sEventID = 0;
+o2::ft0::LookUpTable DigitBlockFT0::sLookupTable = o2::ft0::LookUpTable::linear();
