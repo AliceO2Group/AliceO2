5434da86d27d8ad5c4b2a622c3d6a29f3bac7cc4
Iouri Belikov <iouri.belikov@cern.ch>
2020-07-22 16:29:25 +0200

    A macro generating noise maps out of clusters

---
+++ Detectors/ITSMFT/ITS/macros/Calib/MakeNoiseMapFromClusters.C
@@ -0,0 +1,71 @@
+#if !defined(__CLING__) || defined(__ROOTCLING__)
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+
+#include <TFile.h>
+#include <TTree.h>
+
+#include "ITSBase/GeometryTGeo.h"
+#include "DataFormatsITSMFT/CompCluster.h"
+#include "DataFormatsITSMFT/TopologyDictionary.h"
+
+#pragma link C++ class std::vector < std::map < int, int>> + ;
+
+#endif
+
+void MakeNoiseMapFromClusters(std::string dictfile = "ITSdictionary.bin", std::string input = "o2clus_its.root", std::string output = "noise.root")
+{
+  o2::itsmft::TopologyDictionary dict;
+  std::ifstream file(dictfile.c_str());
+  if (!file.good()) {
+    std::cerr << "Cannot open the dictionary file: " << dictfile << '\n';
+    return;
+  }
+  dict.readBinaryFile(dictfile);
+
+  TFile in(input.data());
+  if (!in.IsOpen()) {
+    std::cerr << "Can not open the input file " << input << '\n';
+    return;
+  }
+  auto clusTree = (TTree*)in.Get("o2sim");
+  if (!clusTree) {
+    std::cerr << "Can not get cluster tree\n";
+    return;
+  }
+  std::vector<o2::itsmft::CompClusterExt>* clusters = nullptr;
+  clusTree->SetBranchAddress("ITSClusterComp", &clusters);
+
+  std::vector<std::map<int, int>> noiseMap;
+  noiseMap.assign(24120, std::map<int, int>());
+
+  auto nevents = clusTree->GetEntries();
+  for (int n = 0; n < nevents; n++) {
+    clusTree->GetEntry(n);
+
+    for (const auto& c : *clusters) {
+      auto pattID = c.getPatternID();
+      auto npix = dict.getNpixels(pattID);
+
+      if (npix > 1)
+        continue;
+
+      auto id = c.getSensorID();
+      int row = c.getRow();
+      int col = c.getCol();
+      int key = row * 1024 + col;
+
+      noiseMap[id][key]++;
+    }
+  }
+
+  TFile out(output.data(), "new");
+  if (!out.IsOpen()) {
+    std::cerr << "Can not open the output file " << output << '\n';
+    return;
+  }
+  out.WriteObject(&noiseMap, "Noise");
+  out.Close();
+}
