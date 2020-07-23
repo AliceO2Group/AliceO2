496c1ccc7aea4de0b687cdceb023ee24d5c0fad4
Sean <hamiltonthomas@gmail.com>
2020-07-23 14:59:43 +0200

    clang formatting

--- DataFormats/Detectors/TRD/src/DataFormatsTRDLinkDef.h
+++ DataFormats/Detectors/TRD/src/DataFormatsTRDLinkDef.h
@@ -19,7 +19,7 @@
 #pragma link C++ struct o2::trd::HalfChamberHeader + ;
 #pragma link C++ struct o2::trd::MCMRawDataHeader + ;
 #pragma link C++ struct o2::trd::TrapRawTracklet + ;
-#pragma link C++ class  o2::trd::Tracklet64 + ;
+#pragma link C++ class o2::trd::Tracklet64 + ;
 #pragma link C++ class std::vector < o2::trd::TriggerRecord > +;
 
 #endif
--- Detectors/TRD/simulation/include/TRDSimulation/Tracklets2Raw.h
+++ Detectors/TRD/simulation/include/TRDSimulation/Tracklets2Raw.h
@@ -16,7 +16,6 @@
 #ifndef ALICEO2_TRD_TRACKLETS2RAW_H
 #define ALICEO2_TRD_TRACKLETS2RAW_H
 
-
 #include "DataFormatsTRD/TriggerRecord.h"
 #include "DataFormatsTRD/TrackletRawData.h"
 
@@ -28,27 +27,26 @@ namespace trd
 class Tracklets2Raw
 {
 
-    static constexpr int NChannels;
-    static constexpr int NLinksPerCRU=30;
-    static constexpr int NCRU=36;
-    
-    public: 
-        Tracklets2Raw() = default;
-        Tracklets2Raw(const std::string& outDir, const std::string& fileRawTrackletsName);
-        void readTracklets(const std::string& outDir, const std::string& fileRawTrackletsName);
+  static constexpr int NChannels;
+  static constexpr int NLinksPerCRU = 30;
+  static constexpr int NCRU = 36;
 
-        void converTracklets(o2::trd::TrackletRaw rawTracklets, gsl::span<2::trd::TriggerRecord const & mTriggerRecord);
+ public:
+  Tracklets2Raw() = default;
+  Tracklets2Raw(const std::string& outDir, const std::string& fileRawTrackletsName);
+  void readTracklets(const std::string& outDir, const std::string& fileRawTrackletsName);
 
+  void converTracklets(o2::trd::TrackletRaw rawTracklets, gsl::span < 2 ::trd::TriggerRecord const& mTriggerRecord);
 
-    private:
-        const o2::raw::HBFUtils& mSampler = o2::raw::HBFUtils::Instance();
-        o2::raw::RawFileWriter mWriter("TRD");
-        bool mOutputPerLink = true;
-        uint32_t mLinkId =0;
-        uint32_t mCRUId = 0;
-        uint32_t mEndPointId = 0;
-        uint32_t mFeeId =0;
+ private:
+  const o2::raw::HBFUtils& mSampler = o2::raw::HBFUtils::Instance();
+  o2::raw::RawFileWriter mWriter("TRD");
+  bool mOutputPerLink = true;
+  uint32_t mLinkId = 0;
+  uint32_t mCRUId = 0;
+  uint32_t mEndPointId = 0;
+  uint32_t mFeeId = 0;
 };
-} // end trd namespace
-} // end o2 namespace
+} // namespace trd
+} // namespace o2
 #endif
