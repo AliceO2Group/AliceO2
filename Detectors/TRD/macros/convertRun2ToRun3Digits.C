48f422309b55db1664c785393024e3eabbe8a933
Jason Barrella <brrjas002@myuct.ac.za>
2020-08-12 09:23:37 +0200

    minor improvements

--- Detectors/TRD/macros/convertRun2ToRun3Digits.C
+++ Detectors/TRD/macros/convertRun2ToRun3Digits.C
@@ -1,24 +1,17 @@
 #if !defined(__CINT__) || defined(__MAKECINT__)
 #include <TClonesArray.h>
-// #include <TEveLine.h>
-// #include <TEveManager.h>
-// #include <TEveElement.h>
 
 #include <AliRunLoader.h>
 #include <AliLoader.h>
 #include <AliDataLoader.h>
 #include <AliTreeLoader.h>
 #include <AliTRDarrayADC.h>
-// #include <AliTRDtrackletWord.h>
-// #include <AliTRDtrackletMCM.h>
 
 #include <iostream>
 
 #include "TH1F.h"
-// #include "AliTRDtrackletMCM.h"
-//#include "TRDDataFormat/TriggerRecord.h"
 #include "TRDBase/Digit.h"
-// #include "TRDBase/Tracklet.h"
+#include "TRDBase/Tracklet.h"
 
 #include <AliRawReaderRoot.h>
 #include <AliRawReaderDateOnline.h>
@@ -163,16 +156,12 @@ void convertRun2ToRun3Digits(TString qaOutPath = "",
 
         digitMan->GetDigits(det)->Expand();
 
-        AliTRDarrayADC* digits;
-        digits = digitMan->GetDigits(det);
-        digits->Expand();
-
-        // TODO: check actual number of rows, from geometry
-        // here: play it safe, assume 12 rows everywhere
         int nrows = AliTRDfeeParam::GetNrowC1();
         if (stack == 2) {
           nrows = AliTRDfeeParam::GetNrowC0();
         }
+
+        // cout << "det: " << det << " | " << "sector: " << sector << " | " << "stack: " << stack << " | " << "rows: " << nrows << endl;
 
         for (int row = 0; row < nrows; row++) {
           for (int col = 0; col < AliTRDfeeParam::GetNcol(); col++) {
@@ -182,13 +171,13 @@ void convertRun2ToRun3Digits(TString qaOutPath = "",
             // cout << "timebins: " << digitMan->GetDigits(det)->GetNtime() << endl;
 
             for (int timebin = 0; timebin < digitMan->GetDigits(det)->GetNtime(); timebin++) {
-              int adc = digitMan->GetDigitAmp(row, col, timebin, det);
 
-              if (digits->GetNtime() > 30) {
+              if (digitMan->GetDigits(det)->GetNtime() > 30) {
                 cout << "----!!! --- number of times is greater than 30" << endl;
               }
 
-              adctimes[timebin] = digits->GetData(row, col, timebin);
+              int adc = digitMan->GetDigitAmp(row, col, timebin, det);
+              adctimes[timebin] = adc;
 
               // this value seems to indicate no digit -> skip
               if (adc == -7169)
