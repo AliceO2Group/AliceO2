1c545c905b8dee94d3ba743763027bcd0ecd5b9e
Rafael Pezzi <rafael.pezzi@cern.ch>
2020-08-19 12:33:07 -0300

    Kalman gain formalism on forward tracking update

--- DataFormats/Reconstruction/src/TrackFwd.cxx
+++ DataFormats/Reconstruction/src/TrackFwd.cxx
@@ -252,43 +252,52 @@ bool TrackParCovFwd::update(const std::array<float, 2>& p, const std::array<floa
 {
   /// Kalman update step: computes new track parameters with a new cluster position and uncertainties
   /// The current track is expected to have been propagated to the cluster z position
-  /// Returns false in case of failure
 
-  // get new cluster parameters (m)
-  SMatrix5 clusterParam;
-  clusterParam(0) = p[0];
-  clusterParam(1) = p[1];
+  using SVector2 = ROOT::Math::SVector<double, 2>;
+  using SMatrix22 = ROOT::Math::SMatrix<double, 2>;
+  using SMatrix25 = ROOT::Math::SMatrix<double, 2, 5>;
+  using SMatrix52 = ROOT::Math::SMatrix<double, 5, 2>;
+  using SMatrix55Std = ROOT::Math::SMatrix<double, 5>;
 
-  // compute the actual parameter weight (W)
-  SMatrix55 paramWeight(mCovariances);
-  if (!(paramWeight.Invert())) {
-    std::cout << "runKalmanFilter ERROR: Determinant = 0\n";
-    return false;
-  }
+  SMatrix55 I = ROOT::Math::SMatrixIdentity();
+  SMatrix25 H_k;
+  SMatrix22 V_k;
+  SVector2 m_k(p[0], p[1]), r_k_kminus1;
+  V_k(0, 0) = cov[0];
+  V_k(1, 1) = cov[1];
+  H_k(0, 0) = 1.0;
+  H_k(1, 1) = 1.0;
 
-  // compute the new cluster weight (U)
-  SMatrix55 clusterWeight;
-  clusterWeight(0, 0) = 1. / cov[0];
-  clusterWeight(1, 1) = 1. / cov[1];
+  // Covariance of residuals
+  SMatrix22 invResCov = (V_k + ROOT::Math::Similarity(H_k, mCovariances));
+  invResCov.Invert();
 
-  // compute the new parameters covariance matrix ((W+U)^-1)
-  SMatrix55 newParamCov(paramWeight + clusterWeight);
-  if (!newParamCov.Invert()) {
-    std::cout << "runKalmanFilter ERROR: Determinant = 0\n";
-    return false;
-  }
-  mCovariances = newParamCov;
+  // Kalman Gain Matrix
+  SMatrix52 K_k = mCovariances * ROOT::Math::Transpose(H_k) * invResCov;
 
-  // compute the new parameters: (p' = ((W+U)^-1)U(m-p) + p)
-  // Parameters increment: p' - p = ((W+U)^-1)U(m-p)
-  SMatrix5 predict_residuals(clusterParam - mParameters); // m-p   -> residuals of prediction
-  SMatrix5 tmp(clusterWeight * predict_residuals);        // U(m-p)
-  SMatrix5 newParamDelta(newParamCov * tmp);              // ((W+U)^-1)U(m-p)
-  mParameters += newParamDelta;                           // ((W+U)^-1)U(m-p) + p
+  // Update Parameters
+  r_k_kminus1 = m_k - H_k * mParameters; // Residuals of prediction
+  mParameters = mParameters + K_k * r_k_kminus1;
 
-  // compute the additional addChi2 = ((p'-p)^t)W(p'-p) + ((p'-m)^t)U(p'-m)
-  SMatrix5 tmp2(mParameters - clusterParam); // (p'-m)
-  auto addChi2Track(ROOT::Math::Similarity(newParamDelta, paramWeight) + ROOT::Math::Similarity(tmp2, clusterWeight));
+  // Update covariances Matrix
+  SMatrix55Std updatedCov = (I - K_k * H_k) * mCovariances;
+  mCovariances(0, 0) = updatedCov(0, 0);
+  mCovariances(0, 1) = updatedCov(0, 1);
+  mCovariances(0, 2) = updatedCov(0, 2);
+  mCovariances(0, 3) = updatedCov(0, 3);
+  mCovariances(0, 4) = updatedCov(0, 4);
+  mCovariances(1, 1) = updatedCov(1, 1);
+  mCovariances(1, 2) = updatedCov(1, 2);
+  mCovariances(1, 3) = updatedCov(1, 3);
+  mCovariances(1, 4) = updatedCov(1, 4);
+  mCovariances(2, 2) = updatedCov(2, 2);
+  mCovariances(2, 3) = updatedCov(2, 3);
+  mCovariances(2, 4) = updatedCov(2, 4);
+  mCovariances(3, 3) = updatedCov(3, 3);
+  mCovariances(3, 4) = updatedCov(3, 4);
+  mCovariances(4, 4) = updatedCov(4, 4);
+
+  auto addChi2Track = ROOT::Math::Similarity(r_k_kminus1, invResCov);
   mTrackChi2 += addChi2Track;
 
   return true;
