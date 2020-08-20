afd4d98da8e0d54fa5a4638f8f346fe4bb40b1cc
AllaMaevskaya <Alla.Maevskaya@cern.ch>
2020-08-20 12:54:42 +0300

    FT0: add DigitsWriter to data reader (#4104)
    * add DigitsWriter to data reader
    
    * rename FT0DigitWriterSpec
    
    Co-authored-by: AllaMaevskaya <Alla.Maevskaia@cern.ch>

--- Detectors/FIT/FT0/workflow/src/ft0-flp-workflow.cxx
+++ Detectors/FIT/FT0/workflow/src/ft0-flp-workflow.cxx
@@ -8,8 +8,8 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-#include "FT0Workflow/FT0Workflow.h"
 #include "CommonUtils/ConfigurableParam.h"
+#include "FT0Workflow/FT0Workflow.h"
 
 using namespace o2::framework;
 
@@ -19,15 +19,33 @@ using namespace o2::framework;
 void customize(std::vector<o2::framework::ConfigParamSpec>& workflowOptions)
 {
   // option allowing to set parameters
-  workflowOptions.push_back(ConfigParamSpec{
-    "tcm-extended-mode", o2::framework::VariantType::Bool, false, {"in case of extended TCM mode (1 header + 1 TCMdata + 8 TCMdataExtended)"}});
+  workflowOptions.push_back(
+    ConfigParamSpec{"tcm-extended-mode",
+                    o2::framework::VariantType::Bool,
+                    false,
+                    {"in case of extended TCM mode (1 header + 1 TCMdata + 8 "
+                     "TCMdataExtended)"}});
 
-  workflowOptions.push_back(ConfigParamSpec{
-    "use-process", o2::framework::VariantType::Bool, false, {"enable processor for data taking/dumping"}});
-  workflowOptions.push_back(ConfigParamSpec{
-    "dump-blocks-process", o2::framework::VariantType::Bool, false, {"enable dumping of event blocks at processor side"}});
-  workflowOptions.push_back(ConfigParamSpec{
-    "dump-blocks-reader", o2::framework::VariantType::Bool, false, {"enable dumping of event blocks at reader side"}});
+  workflowOptions.push_back(
+    ConfigParamSpec{"use-process",
+                    o2::framework::VariantType::Bool,
+                    false,
+                    {"enable processor for data taking/dumping"}});
+  workflowOptions.push_back(
+    ConfigParamSpec{"dump-blocks-process",
+                    o2::framework::VariantType::Bool,
+                    false,
+                    {"enable dumping of event blocks at processor side"}});
+  workflowOptions.push_back(
+    ConfigParamSpec{"dump-blocks-reader",
+                    o2::framework::VariantType::Bool,
+                    false,
+                    {"enable dumping of event blocks at reader side"}});
+  workflowOptions.push_back(
+    ConfigParamSpec{"disable-root-output",
+                    o2::framework::VariantType::Bool,
+                    false,
+                    {"disable root-files output writers"}});
 }
 
 // ------------------------------------------------------------------
@@ -41,7 +59,9 @@ WorkflowSpec defineDataProcessing(ConfigContext const& configcontext)
   auto dumpProcessor = configcontext.options().get<bool>("dump-blocks-process");
   auto dumpReader = configcontext.options().get<bool>("dump-blocks-reader");
   auto isExtendedMode = configcontext.options().get<bool>("tcm-extended-mode");
-
+  auto disableRootOut =
+    configcontext.options().get<bool>("disable-root-output");
   LOG(INFO) << "WorkflowSpec FLPWorkflow";
-  return std::move(o2::fit::getFT0Workflow(isExtendedMode, useProcessor, dumpProcessor, dumpReader));
+  return std::move(o2::fit::getFT0Workflow(
+    isExtendedMode, useProcessor, dumpProcessor, dumpReader, disableRootOut));
 }
