ab765528cde3b498357ac6601bdbbaf4c42b3c15
David Rohr <drohr@jwdt.org>
2020-08-28 17:37:01 +0200

    GPU: Use double-pipeline within DPL

--- GPU/GPUTracking/Interface/GPUO2Interface.h
+++ GPU/GPUTracking/Interface/GPUO2Interface.h
@@ -31,6 +31,12 @@
 #include <vector>
 #include "GPUCommonDef.h"
 #include "GPUDataTypes.h"
+
+namespace std
+{
+class thread;
+} // namespace std
+
 namespace o2::tpc
 {
 struct ClusterNativeAccess;
@@ -66,18 +72,15 @@ class GPUO2Interface
   int Initialize(const GPUO2InterfaceConfiguration& config);
   void Deinitialize();
 
-  int RunTracking(GPUTrackingInOutPointers* data, GPUInterfaceOutputs* outputs = nullptr);
-  void Clear(bool clearOutputs);
+  int RunTracking(GPUTrackingInOutPointers* data, GPUInterfaceOutputs* outputs = nullptr, unsigned int iThread = 0);
+  void Clear(bool clearOutputs, unsigned int iThread = 0);
   void DumpEvent(int nEvent, GPUTrackingInOutPointers* data);
   void DumpSettings();
 
   void GetITSTraits(o2::its::TrackerTraits*& trackerTraits, o2::its::VertexerTraits*& vertexerTraits, o2::its::TimeFrame*& timeFrame);
 
   // Updates all calibration objects that are != nullptr in newCalib
-  int UpdateCalibration(const GPUCalibObjectsConst& newCalib, const GPUNewCalibValues& newVals);
-
-  bool GetParamContinuous() { return (mContinuous); }
-  void GetClusterErrors2(int row, float z, float sinPhi, float DzDs, short clusterState, float& ErrY2, float& ErrZ2) const;
+  int UpdateCalibration(const GPUCalibObjectsConst& newCalib, const GPUNewCalibValues& newVals, unsigned int iThread = 0);
 
   static std::unique_ptr<TPCPadGainCalib> getPadGainCalibDefault();
   static std::unique_ptr<TPCPadGainCalib> getPadGainCalib(const o2::tpc::CalDet<float>& in);
@@ -90,18 +93,24 @@ class GPUO2Interface
 
   const GPUO2InterfaceConfiguration& getConfig() const { return *mConfig; }
 
+  struct processingContext {
+    std::unique_ptr<GPUReconstruction> mRec;
+    GPUChainTracking* mChain = nullptr;
+    std::unique_ptr<GPUTrackingOutputs> mOutputRegions;
+  };
+
  private:
   GPUO2Interface(const GPUO2Interface&);
   GPUO2Interface& operator=(const GPUO2Interface&);
 
-  bool mInitialized = false;
   bool mContinuous = false;
 
-  std::unique_ptr<GPUReconstruction> mRec;              //!
-  GPUChainTracking* mChain = nullptr;                   //!
-  GPUChainITS* mChainITS = nullptr;                     //!
-  std::unique_ptr<GPUO2InterfaceConfiguration> mConfig; //!
-  std::unique_ptr<GPUTrackingOutputs> mOutputRegions;   //!
+  unsigned int mNContexts = 0;
+  std::unique_ptr<processingContext[]> mCtx;
+
+  std::unique_ptr<GPUO2InterfaceConfiguration> mConfig;
+  GPUChainITS* mChainITS = nullptr;
+  std::unique_ptr<std::thread> mPipelineThread;
 };
 } // namespace o2::gpu
 
