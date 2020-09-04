a741bd7b90b42b714d7fe14d378eb232b1308f20
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2020-09-04 09:53:18 +0200

    DPL: cleanups to compile with 1.4.22

--- Framework/Core/src/FrameworkGUIDevicesGraph.cxx
+++ Framework/Core/src/FrameworkGUIDevicesGraph.cxx
@@ -14,6 +14,7 @@
 #include "Framework/DeviceInfo.h"
 #include "Framework/LogParsingHelpers.h"
 #include "Framework/PaletteHelpers.h"
+#include "Framework/Logger.h"
 #include "FrameworkGUIDeviceInspector.h"
 #include "Framework/Logger.h"
 #include "../src/WorkflowHelpers.h"
@@ -31,11 +32,7 @@
 static inline ImVec2 operator+(const ImVec2& lhs, const ImVec2& rhs) { return ImVec2(lhs.x + rhs.x, lhs.y + rhs.y); }
 static inline ImVec2 operator-(const ImVec2& lhs, const ImVec2& rhs) { return ImVec2(lhs.x - rhs.x, lhs.y - rhs.y); }
 
-namespace o2
-{
-namespace framework
-{
-namespace gui
+namespace o2::framework::gui
 {
 
 struct NodeColor {
@@ -558,6 +555,4 @@ void showTopologyNodeGraph(WorkspaceGUIState& state,
   ImGui::End();
 }
 
-} // namespace gui
-} // namespace framework
-} // namespace o2
+} // namespace o2::framework::gui
