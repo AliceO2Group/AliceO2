aaf978e4c94f92ecb6101673703e3f1f9bdab2b9
David Dobrigkeit Chinellato <david.dobrigkeit.chinellato@cern.ch>
2020-09-08 19:44:16 +0200

    Tuning

--- Analysis/Tasks/lambdakzeroconsumer.cxx
+++ Analysis/Tasks/lambdakzeroconsumer.cxx
@@ -36,10 +36,35 @@ using namespace o2::framework;
 using namespace o2::framework::expressions;
 using std::array;
 
-struct lambdakzeroconsumer {
+struct lambdakzeroQA {
+    //Basic checks
     OutputObj<TH1F> hMassK0Short   {TH1F("hMassK0Short"   , "", 3000, 0.0, 3.0)};
     OutputObj<TH1F> hMassLambda    {TH1F("hMassLambda"    , "", 3000, 0.0, 3.0)};
     OutputObj<TH1F> hMassAntiLambda{TH1F("hMassAntiLambda", "", 3000, 0.0, 3.0)};
+    
+    OutputObj<TH1F> hV0Radius     {TH1F("hV0Radius",   "", 1000, 0.0, 100)};
+    OutputObj<TH1F> hV0CosPA      {TH1F("hV0CosPA",    "", 1000, 0.95, 1.0)};
+    OutputObj<TH1F> hDCAPosToPV   {TH1F("hDCAPosToPV", "", 1000, 0.0, 10.0)};
+    OutputObj<TH1F> hDCANegToPV   {TH1F("hDCANegToPV", "", 1000, 0.0, 10.0)};
+    OutputObj<TH1F> hDCAV0Dau     {TH1F("hDCAV0Dau", "", 1000, 0.0, 10.0)};
+
+    void process(aod::Collision const& collision, soa::Join<aod::V0s, aod::V0Data> const& fullV0s)
+    {
+        for (auto& v0 : fullV0s) {
+            hMassLambda->Fill(v0.MassAsLambdas());
+            hMassAntiLambda->Fill(v0.MassAsAntiLambdas());
+            hMassK0Short->Fill(v0.MassAsK0Shorts());
+            
+            hV0Radius->Fill(v0.V0Radii());
+            hV0CosPA->Fill(v0.V0CosPAs());
+            hDCAPosToPV->Fill(v0.DCAPosToPVs());
+            hDCANegToPV->Fill(v0.DCANegToPVs());
+            hDCAV0Dau->Fill(v0.DCAV0Daughters());
+        }
+    }
+};
+
+struct lambdakzeroconsumer {
     OutputObj<TH2F> h2dMassK0Short   {TH2F("h2dMassK0Short"   , "", 200,0,10, 200, 0.450, 0.550)};
     OutputObj<TH2F> h2dMassLambda    {TH2F("h2dMassLambda"    , "", 200,0,10, 200, 1.115-0.100,1.115+0.100)};
     OutputObj<TH2F> h2dMassAntiLambda{TH2F("h2dMassAntiLambda", "", 200,0,10, 200, 1.115-0.100,1.115+0.100)};
@@ -61,6 +86,7 @@ struct lambdakzeroconsumer {
 WorkflowSpec defineDataProcessing(ConfigContext const&)
 {
     return WorkflowSpec{
+        adaptAnalysisTask<lambdakzeroQA>("lf-lambdakzeroQA")
         adaptAnalysisTask<lambdakzeroconsumer>("lf-lambdakzeroconsumer")
     };
 }
