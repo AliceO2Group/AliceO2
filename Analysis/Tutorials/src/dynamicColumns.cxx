58baf30030be09aa8817e88f1e3dec05b8107eab
Anton Alkin <anton.alkin@cern.ch>
2020-09-09 10:40:24 +0200

    DPL Analysis: adapt Attach/Extend to new Join (#4307)

--- Analysis/Tutorials/src/dynamicColumns.cxx
+++ Analysis/Tutorials/src/dynamicColumns.cxx
@@ -28,7 +28,9 @@ struct ATask {
     auto table_with_extra_dynamic_columns = soa::Attach<aod::Tracks, aod::extension::P2<aod::track::P>>(tracks);
     for (auto& row : table_with_extra_dynamic_columns) {
       if (row.trackType() != 3) {
-        LOGF(info, "P^2 = %.3f", row.p2());
+        if (row.index() % 100 == 0) {
+          LOGF(info, "P^2 = %.3f", row.p2());
+        }
       }
     }
   }
