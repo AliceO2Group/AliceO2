3b056c650678543874e2d917d409f285b1b793f5
shahoian <ruben.shahoyan@cern.ch>
2020-09-14 00:08:02 +0200

    P.Vertex-tracks matching class + related workflows update
    At the moment the pr.vertex - track matching is done in time only (except vertex fit contributor TPC/ITS
    tracks), for every vertex list of global indices (VtxTrackIndex) is added, providing the source (global,
    or ITS or TPC track), ID in the source and bit flags for contributors and ambigous assignment.

--- Detectors/GlobalTrackingWorkflow/src/primary-vertexing-workflow.cxx
+++ Detectors/GlobalTrackingWorkflow/src/primary-vertexing-workflow.cxx
@@ -11,6 +11,9 @@
 #include "GlobalTrackingWorkflow/PrimaryVertexingSpec.h"
 #include "GlobalTrackingWorkflow/PrimaryVertexWriterSpec.h"
 #include "GlobalTrackingWorkflow/TrackTPCITSReaderSpec.h"
+#include "GlobalTrackingWorkflow/VertexTrackMatcherSpec.h"
+#include "ITSWorkflow/TrackReaderSpec.h"
+#include "TPCWorkflow/TrackReaderSpec.h"
 #include "FT0Workflow/RecPointReaderSpec.h"
 
 #include "CommonUtils/ConfigurableParam.h"
@@ -30,6 +33,7 @@ void customize(std::vector<ConfigParamSpec>& workflowOptions)
     {"disable-root-input", o2::framework::VariantType::Bool, false, {"disable root-files input reader"}},
     {"disable-root-output", o2::framework::VariantType::Bool, false, {"disable root-files output writer"}},
     {"validate-with-ft0", o2::framework::VariantType::Bool, false, {"use FT0 time for vertex validation"}},
+    {"disable-vertex-track-matching", o2::framework::VariantType::Bool, false, {"disable matching of vertex to non-contributor tracks"}},
     {"configKeyValues", VariantType::String, "", {"Semicolon separated key=value strings ..."}}};
 
   std::swap(workflowOptions, options);
@@ -50,6 +54,7 @@ WorkflowSpec defineDataProcessing(ConfigContext const& configcontext)
   auto disableRootInp = configcontext.options().get<bool>("disable-root-input");
   auto disableRootOut = configcontext.options().get<bool>("disable-root-output");
   auto validateWithFT0 = configcontext.options().get<bool>("validate-with-ft0");
+  auto disableMatching = configcontext.options().get<bool>("disable-vertex-track-matching");
 
   WorkflowSpec specs;
   if (!disableRootInp) {
@@ -59,8 +64,15 @@ WorkflowSpec defineDataProcessing(ConfigContext const& configcontext)
     }
   }
   specs.emplace_back(o2::vertexing::getPrimaryVertexingSpec(validateWithFT0, useMC));
+
+  if (!disableMatching) {
+    specs.emplace_back(o2::its::getITSTrackReaderSpec(false));
+    specs.emplace_back(o2::tpc::getTPCTrackReaderSpec(false));
+    specs.emplace_back(o2::vertexing::getVertexTrackMatcherSpec());
+  }
+
   if (!disableRootOut) {
-    specs.emplace_back(o2::vertexing::getPrimaryVertexWriterSpec(useMC));
+    specs.emplace_back(o2::vertexing::getPrimaryVertexWriterSpec(disableMatching, useMC));
   }
   return std::move(specs);
 }
