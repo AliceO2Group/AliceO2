ac9c3d96d4604a98c9eeeef47c0017e8511c3423
Sandro Wenzel <sandro.wenzel@cern.ch>
2020-09-22 12:16:36 +0200

    A view/adopted version of ConstMCTruthContainer
    New class ConstMCTruthContainerView.
    
    Same functionality as ConstMCTruthContainer but can simply adopt an existing buffer
    in form of a gsl::span. Useful to adopt labels that have been sent with DPL in shared memory.
    (This may finally be the only useful version.)

--- DataFormats/simulation/include/SimulationDataFormat/ConstMCTruthContainer.h
+++ DataFormats/simulation/include/SimulationDataFormat/ConstMCTruthContainer.h
@@ -115,4 +115,85 @@ struct is_specialization<o2::dataformats::ConstMCTruthContainer<T>, std::vector>
 };
 } // namespace o2::framework
 
+namespace o2
+{
+namespace dataformats
+{
+
+// A "view" label container without owning the storage (similar to gsl::span)
+template <typename TruthElement>
+class ConstMCTruthContainerView
+{
+ public:
+  ConstMCTruthContainerView(gsl::span<const char> const bufferview) : mStorage(bufferview){};
+
+  // const data access
+  // get individual const "view" container for a given data index
+  // the caller can't do modifications on this view
+  MCTruthHeaderElement const& getMCTruthHeader(uint32_t dataindex) const
+  {
+    return getHeaderStart()[dataindex];
+  }
+
+  gsl::span<const TruthElement> getLabels(uint32_t dataindex) const
+  {
+    if (dataindex >= getIndexedSize()) {
+      return gsl::span<const TruthElement>();
+    }
+    const auto start = getMCTruthHeader(dataindex).index;
+    const auto labelsptr = getLabelStart();
+    return gsl::span<const TruthElement>(&labelsptr[start], getSize(dataindex));
+  }
+
+  // return the number of original data indexed here
+  size_t getIndexedSize() const { return mStorage.size() >= sizeof(FlatHeader) ? getHeader().nofHeaderElements : 0; }
+
+  // return the number of labels  managed in this container
+  size_t getNElements() const { return mStorage.size() >= sizeof(FlatHeader) ? getHeader().nofTruthElements : 0; }
+
+ private:
+  gsl::span<const char> mStorage;
+
+  using FlatHeader = typename MCTruthContainer<TruthElement>::FlatHeader;
+
+  size_t getSize(uint32_t dataindex) const
+  {
+    // calculate size / number of labels from a difference in pointed indices
+    const auto size = (dataindex < getIndexedSize() - 1)
+                        ? getMCTruthHeader(dataindex + 1).index - getMCTruthHeader(dataindex).index
+                        : getNElements() - getMCTruthHeader(dataindex).index;
+    return size;
+  }
+
+  /// Restore internal vectors from a raw buffer
+  /// The two vectors are resized according to the information in the \a FlatHeader
+  /// struct at the beginning of the buffer. Data is copied to the vectors.
+  TruthElement const* const getLabelStart() const
+  {
+    auto* source = &(mStorage)[0];
+    auto flatheader = getHeader();
+    source += sizeof(FlatHeader);
+    const size_t headerSize = flatheader.sizeofHeaderElement * flatheader.nofHeaderElements;
+    source += headerSize;
+    return (TruthElement const* const)source;
+  }
+
+  FlatHeader const& getHeader() const
+  {
+    const auto* source = &(mStorage)[0];
+    const auto& flatheader = *reinterpret_cast<FlatHeader const*>(source);
+    return flatheader;
+  }
+
+  MCTruthHeaderElement const* const getHeaderStart() const
+  {
+    auto* source = &(mStorage)[0];
+    source += sizeof(FlatHeader);
+    return (MCTruthHeaderElement const* const)source;
+  }
+};
+
+} // namespace dataformats
+} // namespace o2
+
 #endif //O2_CONSTMCTRUTHCONTAINER_H
