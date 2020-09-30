b6c12e04b717fadddac13704044c7844b71b4ae5
Mario Kruger <mario.kruger@cern.ch>
2020-09-30 17:13:09 +0200

    HistHelpers: make histID template argument

--- Analysis/Tutorials/src/histHelpersTest.cxx
+++ Analysis/Tutorials/src/histHelpersTest.cxx
@@ -23,34 +23,37 @@ using namespace o2::experimental::histhelpers;
 struct HistHelpersTest {
 
   // some unique names for the histograms
-  enum HistNames : uint8_t {
-    Hist_pt = 0,
-    Hist_eta,
-    Hist_phi,
-    Hist_nClsTPC,
-    Hist_nCrossedRowsTPC,
-    Hist_Chi2PerClTPC,
-    Hist_nClsITS,
-    Hist_Chi2PerClITS,
+  enum HistNamesTest : uint8_t {
+    test_1d_TH1D,
+    test_2d_TH2F,
+    test_3d_TH3I,
+    test_1d_TH1D_Weight,
+    test_2d_TH2F_VarBinningY,
 
-    // for testing
-    Hist_test_1d_TH1D,
-    Hist_test_2d_TH2F,
-    Hist_test_3d_TH3I,
-    Hist_test_1d_TH1D_Builder,
-    Hist_test_2d_TH2F_Builder,
+    test_3d_THnI,
+    test_5d_THnSparseI,
 
-    Hist_test_3d_THnI,
-    Hist_test_5d_THnSparseI,
+    test_1d_TProfile,
+    test_1d_TProfile_Weight,
+    test_2d_TProfile2D,
+    test_3d_TProfile3D,
 
-    Hist_test_1d_TProfile,
-    Hist_test_2d_TProfile2D,
-    Hist_test_3d_TProfile3D,
-
-    Hist_test_7d_THnF_first,
-    Hist_test_7d_THnF_second,
-    Hist_test_8d_THnC_third,
-    LAST,
+    test_7d_THnF_first,
+    test_7d_THnF_second,
+    test_8d_THnC_third,
+  };
+  enum HistNamesKine : uint8_t {
+    pt,
+    eta,
+    phi,
+  };
+  enum HistNamesTPC : uint8_t {
+    tpcNClsFound,
+    tpcNClsCrossedRows,
+    tpcChi2NCl,
+  };
+  enum HistNamesITS : uint8_t {
+    itsChi2NCl,
   };
 
   OutputObj<HistArray> test{HistArray("Test"), OutputObjHandlingPolicy::QAObject};
@@ -63,23 +66,23 @@ struct HistHelpersTest {
   void init(o2::framework::InitContext&)
   {
     // add some plain and simple histograms
-    test->Add(Hist_test_1d_TH1D, TH1D("testHist_TH1", ";x", 100, 0., 50.));
-    test->Add(Hist_test_2d_TH2F, new TH2F("testHist_TH2", ";x;y", 100, -0.5, 0.5, 100, -0.5, 0.5));
-    test->Add(Hist_test_3d_TH3I, TH3I("testHist_TH3", ";x;y;z", 100, 0., 20., 100, 0., 20., 100, 0., 20.));
+    test->Add<test_1d_TH1D>(new TH1D("test_1d_TH1D", ";x", 100, 0., 50.));
+    test->Add<test_2d_TH2F>(new TH2F("test_2d_TH2F", ";x;y", 100, -0.5, 0.5, 100, -0.5, 0.5));
+    test->Add<test_3d_TH3I>(new TH3I("test_3d_TH3I", ";x;y;z", 100, 0., 20., 100, 0., 20., 100, 0., 20.));
 
     // alternatively use Hist to generate the histogram and add it to container afterwards
     Hist<TH1D> sameAsBefore;
     sameAsBefore.AddAxis("x", "x", 100, 0., 50.);
     // via Hist::Create() we can generate the actual root histogram with the requested axes
     // Parameters: the name and optionally the decision wether to call SumW2 in case we want to fill this histogram with weights
-    test->Add(Hist_test_1d_TH1D_Builder, sameAsBefore.Create("testHist_TH1_Builder", true));
+    test->Add<test_1d_TH1D_Weight>(sameAsBefore.Create("test_1d_TH1D_Weight", true));
 
     // this helper enables us to have combinations of flexible + fixed binning in 2d or 3d histograms
     // (which are not available via default root constructors)
     Hist<TH2F> sameButDifferent;
     sameButDifferent.AddAxis("x", "x", 100, -0.5, 0.5);
     sameButDifferent.AddAxis("y", "y", {-0.5, -0.48, -0.3, 0.4, 0.5}); // use variable binning for y axsis this time
-    test->Add(Hist_test_2d_TH2F_Builder, sameButDifferent.Create("testHist_TH2_Builder"));
+    test->Add<test_2d_TH2F_VarBinningY>(sameButDifferent.Create("test_2d_TH2F_VarBinningY"));
 
     // also for n dimensional histograms things become much simpler:
     std::vector<double> ptBins = {0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0,
@@ -99,42 +102,34 @@ struct HistHelpersTest {
     myHistogram.AddAxis(ptAxis);
     myHistogram.AddAxis(etaAxis);
     myHistogram.AddAxis("signed1Pt", "q/p_{T}", 200, -8, 8);
-    test->Add(Hist_test_3d_THnI, myHistogram.Create("testHist_THn"));
+    test->Add<test_3d_THnI>(myHistogram.Create("test_3d_THnI"));
 
-    Hist<THnSparseC> testSparseHist;
+    Hist<THnSparseI> testSparseHist;
     testSparseHist.AddAxis(ptAxis);
     testSparseHist.AddAxis(etaAxis);
     testSparseHist.AddAxis(phiAxis);
     testSparseHist.AddAxis(centAxis);
     testSparseHist.AddAxis(cutAxis);
-    test->Add(Hist_test_5d_THnSparseI, testSparseHist.Create("testHist_THnSparse"));
+    test->Add<test_5d_THnSparseI>(testSparseHist.Create("test_5d_THnSparseI"));
 
     Hist<TProfile> testProfile;
     testProfile.AddAxis(ptAxis);
-    test->Add(Hist_test_1d_TProfile, testProfile.Create("testProfile_TProfile"));
-    test->Get<TProfile>(Hist_test_1d_TProfile)->GetYaxis()->SetTitle("eta profile");
+    test->Add<test_1d_TProfile>(testProfile.Create("test_1d_TProfile"));
+    test->Get<TProfile>(test_1d_TProfile)->GetYaxis()->SetTitle("eta profile");
+
+    // now add same histogram but intended for weighted filling
+    test->Add<test_1d_TProfile_Weight>(testProfile.Create("test_1d_TProfile_Weight", true));
 
     Hist<TProfile2D> testProfile2d;
     testProfile2d.AddAxis(ptAxis);
     testProfile2d.AddAxis(etaAxis);
-    test->Add(Hist_test_2d_TProfile2D, testProfile2d.Create("testProfile_TProfile2D"));
+    test->Add<test_2d_TProfile2D>(testProfile2d.Create("test_2d_TProfile2D"));
 
     Hist<TProfile3D> testProfile3d;
     testProfile3d.AddAxis(ptAxis);
     testProfile3d.AddAxis(etaAxis);
     testProfile3d.AddAxis(phiAxis);
-    test->Add(Hist_test_3d_TProfile3D, testProfile3d.Create("testProfile_TProfile3D"));
-
-    // now add some more useful histograms
-    kine->Add(Hist_pt, TH1F("track-pt", "p_{T};p_{T} [GeV/c]", 100, 0., 50.));
-    kine->Add(Hist_eta, TH1F("track-eta", "#eta;#eta", 101, -1.0, 1.0));
-    kine->Add(Hist_phi, TH1F("track-phi", "#phi;#phi [rad]", 100, 0., 2 * M_PI));
-
-    tpc->Add(Hist_nCrossedRowsTPC, TH1F("tpc-crossedRows", "number of crossed TPC rows;# crossed rows TPC", 165, -0.5, 164.5));
-    tpc->Add(Hist_nClsTPC, TH1F("tpc-foundClusters", "number of found TPC clusters;# clusters TPC", 165, -0.5, 164.5));
-    tpc->Add(Hist_Chi2PerClTPC, TH1F("tpc-chi2PerCluster", "chi2 per cluster in TPC;chi2 / cluster TPC", 100, 0, 10));
-
-    its->Add(Hist_Chi2PerClITS, TH1F("its-chi2PerCluster", "chi2 per ITS cluster;chi2 / cluster ITS", 100, 0, 40));
+    test->Add<test_3d_TProfile3D>(testProfile3d.Create("test_3d_TProfile3D"));
 
     // we can also re-use axis definitions in case they are similar in many histograms:
     Hist<THnF> baseDimensions;
@@ -147,57 +142,71 @@ struct HistHelpersTest {
 
     Hist<THnF> firstHist = baseDimensions;
     firstHist.AddAxis("something", "v (m/s)", 10, -1, 1);
-    its->Add(Hist_test_7d_THnF_first, firstHist.Create("firstHist"));
+    test->Add<test_7d_THnF_first>(firstHist.Create("test_7d_THnF_first"));
 
     Hist<THnF> secondHist = baseDimensions;
     secondHist.AddAxis("somethingElse", "a (m/(s*s))", 10, -1, 1);
-    its->Add(Hist_test_7d_THnF_second, secondHist.Create("secondHist"));
+    test->Add<test_7d_THnF_second>(secondHist.Create("test_7d_THnF_second"));
 
     // or if we want to have the baseDimensions somewhere in between:
     Hist<THnC> thirdHist;
     thirdHist.AddAxis("myFirstDimension", "a (m/(s*s))", 10, -1, 1);
     thirdHist.AddAxes(baseDimensions);
     thirdHist.AddAxis("myLastDimension", "a (m/(s*s))", 10, -1, 1);
-    its->Add(Hist_test_8d_THnC_third, thirdHist.Create("thirdHist"));
+    test->Add<test_8d_THnC_third>(thirdHist.Create("test_8d_THnC_third"));
 
-    // we can also use the Hist heleper tool independent of the HistCollections:
+    // we can also use the Hist helper tool independent of the HistCollections:
     Hist<THnF> myHist;
     myHist.AddAxis(ptAxis);
     myHist.AddAxis(etaAxis);
     myHist.AddAxis(phiAxis);
     standaloneHist.setObject(myHist.Create("standaloneHist"));
+
+    // now add some more useful histograms
+    kine->Add<pt>(new TH1F("pt", "p_{T};p_{T} [GeV/c]", 100, 0., 5.));
+    kine->Add<eta>(new TH1F("eta", "#eta;#eta", 101, -1.0, 1.0));
+    kine->Add<phi>(new TH1F("phi", "#phi;#phi [rad]", 100, 0., 2 * M_PI));
+
+    tpc->Add<tpcNClsFound>(new TH1F("tpcNClsFound", "number of found TPC clusters;# clusters TPC", 165, -0.5, 164.5));
+    tpc->Add<tpcNClsCrossedRows>(new TH1F("tpcNClsCrossedRows", "number of crossed TPC rows;# crossed rows TPC", 165, -0.5, 164.5));
+    tpc->Add<tpcChi2NCl>(new TH1F("tpcChi2NCl", "chi2 per cluster in TPC;chi2 / cluster TPC", 100, 0, 10));
+
+    its->Add<itsChi2NCl>(new TH1F("itsChi2NCl", "chi2 per ITS cluster;chi2 / cluster ITS", 100, 0, 40));
   }
 
   void process(soa::Join<aod::Tracks, aod::TracksExtra>::iterator const& track)
   {
-    test->Fill(Hist_test_1d_TH1D, 1.);
-    test->Fill(Hist_test_2d_TH2F, 0.1, 0.3);
-    test->Fill(Hist_test_3d_TH3I, 10, 10, 15);
+    test->Fill<test_1d_TH1D>(20.);
+    test->Fill<test_2d_TH2F>(0.1, 0.3);
+    test->Fill<test_3d_TH3I>(10, 10, 15.5);
 
     // this time fill the 1d histogram with weight of 10:
-    test->FillWeight(Hist_test_1d_TH1D_Builder, 1., 10.);
-    test->Fill(Hist_test_2d_TH2F_Builder, 0.1, 0.3);
+    test->FillWeight<test_1d_TH1D_Weight>(20., 10.);
 
-    test->Fill(Hist_test_3d_THnI, 1., 0., 1.5);
-    test->Fill(Hist_test_5d_THnSparseI, 1., 0., 1.5, 30, 1);
+    test->Fill<test_2d_TH2F_VarBinningY>(0.1, 0.3);
+
+    test->Fill<test_3d_THnI>(1., 0., 1.5);
+    test->Fill<test_5d_THnSparseI>(1., 0., 1.5, 30, 1);
 
     // we can also directly access to the underlying histogram
     // for this the correct type has to be known (TH1, TH2, TH3, THn, THnSparse, TProfile, TProfile2D or TProfile3D)
-    test->Get<TH2>(Hist_test_2d_TH2F)->Fill(0.2, 0.2);
+    test->Get<TH2>(test_2d_TH2F)->Fill(0.2, 0.2);
 
-    test->Fill(Hist_test_1d_TProfile, track.pt(), track.eta());
-    test->Fill(Hist_test_2d_TProfile2D, track.pt(), track.eta(), track.phi());
-    test->Fill(Hist_test_3d_TProfile3D, track.pt(), track.eta(), track.phi(), track.tpcNClsFound());
+    test->Fill<test_1d_TProfile>(track.pt(), track.eta());
+    // now fill same histogram, but with random weight
+    test->FillWeight<test_1d_TProfile_Weight>(track.pt(), track.eta(), std::rand());
+    test->Fill<test_2d_TProfile2D>(track.pt(), track.eta(), track.phi());
+    test->Fill<test_3d_TProfile3D>(track.pt(), track.eta(), track.phi(), track.tpcNClsFound());
 
-    kine->Fill(Hist_pt, track.pt());
-    kine->Fill(Hist_eta, track.eta());
-    kine->Fill(Hist_phi, track.phi());
+    kine->Fill<pt>(track.pt());
+    kine->Fill<eta>(track.eta());
+    kine->Fill<phi>(track.phi());
 
-    tpc->Fill(Hist_nClsTPC, track.tpcNClsFound());
-    tpc->Fill(Hist_nCrossedRowsTPC, track.tpcNClsCrossedRows());
-    tpc->Fill(Hist_Chi2PerClTPC, track.itsChi2NCl());
+    tpc->Fill<tpcNClsFound>(track.tpcNClsFound());
+    tpc->Fill<tpcNClsCrossedRows>(track.tpcNClsCrossedRows());
+    tpc->Fill<tpcChi2NCl>(track.tpcChi2NCl());
 
-    its->Fill(Hist_Chi2PerClITS, track.tpcChi2NCl());
+    its->Fill<itsChi2NCl>(track.itsChi2NCl());
 
     double dummyArray[] = {track.pt(), track.eta(), track.phi()};
     standaloneHist->Fill(dummyArray);
