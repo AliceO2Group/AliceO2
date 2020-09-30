dfcdc09974c09d5eebd6bd1b8f9cb36124879d40
Anton Alkin <anton.alkin@cern.ch>
2020-09-30 07:27:00 +0200

    DPL Analysis: test for array and boolean column slicing (#4464)

--- Framework/Core/test/test_GroupSlicer.cxx
+++ Framework/Core/test/test_GroupSlicer.cxx
@@ -23,12 +23,14 @@ namespace o2::aod
 {
 namespace test
 {
+DECLARE_SOA_COLUMN(ID, id, int);
 DECLARE_SOA_COLUMN(Foo, foo, float);
 DECLARE_SOA_COLUMN(Bar, bar, float);
 DECLARE_SOA_COLUMN(EventProperty, eventProperty, float);
 } // namespace test
 DECLARE_SOA_TABLE(Events, "AOD", "EVTS",
                   o2::soa::Index<>,
+                  test::ID,
                   test::EventProperty,
                   test::Foo,
                   test::Bar);
@@ -56,13 +58,22 @@ DECLARE_SOA_TABLE(TrksU, "AOD", "TRKSU",
                   test::X,
                   test::Y,
                   test::Z);
+
+namespace test
+{
+DECLARE_SOA_COLUMN(Arr, arr, float[3]);
+DECLARE_SOA_COLUMN(Boo, boo, bool);
+} // namespace test
+
+DECLARE_SOA_TABLE(EventExtra, "AOD", "EVTSXTRA", test::Arr, test::Boo);
+
 } // namespace o2::aod
 BOOST_AUTO_TEST_CASE(GroupSlicerOneAssociated)
 {
   TableBuilder builderE;
   auto evtsWriter = builderE.cursor<aod::Events>();
   for (auto i = 0; i < 20; ++i) {
-    evtsWriter(0, 0.5f * i, 2.f * i, 3.f * i);
+    evtsWriter(0, i, 0.5f * i, 2.f * i, 3.f * i);
   }
   auto evtTable = builderE.finalize();
 
@@ -101,7 +112,7 @@ BOOST_AUTO_TEST_CASE(GroupSlicerSeveralAssociated)
   TableBuilder builderE;
   auto evtsWriter = builderE.cursor<aod::Events>();
   for (auto i = 0; i < 20; ++i) {
-    evtsWriter(0, 0.5f * i, 2.f * i, 3.f * i);
+    evtsWriter(0, i, 0.5f * i, 2.f * i, 3.f * i);
   }
   auto evtTable = builderE.finalize();
 
@@ -189,7 +200,7 @@ BOOST_AUTO_TEST_CASE(GroupSlicerMismatchedGroups)
   TableBuilder builderE;
   auto evtsWriter = builderE.cursor<aod::Events>();
   for (auto i = 0; i < 20; ++i) {
-    evtsWriter(0, 0.5f * i, 2.f * i, 3.f * i);
+    evtsWriter(0, i, 0.5f * i, 2.f * i, 3.f * i);
   }
   auto evtTable = builderE.finalize();
 
@@ -235,7 +246,7 @@ BOOST_AUTO_TEST_CASE(GroupSlicerMismatchedFilteredGroups)
   TableBuilder builderE;
   auto evtsWriter = builderE.cursor<aod::Events>();
   for (auto i = 0; i < 20; ++i) {
-    evtsWriter(0, 0.5f * i, 2.f * i, 3.f * i);
+    evtsWriter(0, i, 0.5f * i, 2.f * i, 3.f * i);
   }
   auto evtTable = builderE.finalize();
 
@@ -276,5 +287,60 @@ BOOST_AUTO_TEST_CASE(GroupSlicerMismatchedFilteredGroups)
       BOOST_CHECK_EQUAL(trk.eventId(), rows[count]);
     }
     ++count;
+  }
+}
+
+BOOST_AUTO_TEST_CASE(ArrowDirectSlicing)
+{
+  int counts[] = {5, 5, 5, 4, 1};
+  int offsets[] = {0, 5, 10, 15, 19, 20};
+  int ids[] = {0, 1, 2, 3, 4};
+
+  using BigE = soa::Join<aod::Events, aod::EventExtra>;
+
+  TableBuilder builderE;
+  auto evtsWriter = builderE.cursor<aod::Events>();
+  auto step = 0;
+  for (auto i = 0; i < 20; ++i) {
+    if (i >= offsets[step + 1])
+      ++step;
+    evtsWriter(0, ids[step], 0.5f * i, 2.f * i, 3.f * i);
+  }
+  auto evtTable = builderE.finalize();
+
+  TableBuilder builderEE;
+  auto evtsEWriter = builderEE.cursor<aod::EventExtra>();
+  for (auto i = 0; i < 20; ++i) {
+    float arr[3] = {0.1f * i, 0.2f * i, 0.3f * i};
+    evtsEWriter(0, arr, i % 2 == 0);
+  }
+  auto evtETable = builderEE.finalize();
+
+  aod::Events e{evtTable};
+  aod::EventExtra ee{evtETable};
+  BigE b_e{{evtTable, evtETable}};
+
+  std::vector<std::shared_ptr<arrow::ChunkedArray>> slices_array;
+  std::vector<std::shared_ptr<arrow::ChunkedArray>> slices_bool;
+  auto offset = 0;
+  for (auto i = 0u; i < 5; ++i) {
+    slices_array.emplace_back(evtETable->column(0)->Slice(offset, counts[i]));
+    slices_bool.emplace_back(evtETable->column(1)->Slice(offset, counts[i]));
+    offset += counts[i];
+    BOOST_REQUIRE_EQUAL(slices_array[i]->length(), counts[i]);
+    BOOST_REQUIRE_EQUAL(slices_bool[i]->length(), counts[i]);
+  }
+
+  std::vector<arrow::Datum> slices;
+  std::vector<uint64_t> offsts;
+  auto status = sliceByColumn("fID", b_e.asArrowTable(), 20, &slices, &offsts);
+  for (auto i = 0u; i < 5; ++i) {
+    auto tbl = arrow::util::get<std::shared_ptr<arrow::Table>>(slices[i].value);
+    auto ca = tbl->GetColumnByName("fArr");
+    auto cb = tbl->GetColumnByName("fBoo");
+    BOOST_REQUIRE_EQUAL(ca->length(), counts[i]);
+    BOOST_REQUIRE_EQUAL(cb->length(), counts[i]);
+    BOOST_CHECK(ca->Equals(slices_array[i]));
+    BOOST_CHECK(cb->Equals(slices_bool[i]));
   }
 }
