4ec16dcef22c186a56dc2561902d9e3ecfcf9a3a
Roberto Preghenella <preghenella@bo.infn.it>
2020-10-08 16:34:08 +0200

    add conet mode for raw decoding to digits
    add on-the-fly noise filter in TOF decoding for cosmic runs
    
    allowing filtering emtpy row in TOF digits
    
    add diagnostic and error in TOF decoding
    
    fix in TOF encoding (word counter)

--- Detectors/TOF/reconstruction/src/Decoder.cxx
+++ Detectors/TOF/reconstruction/src/Decoder.cxx
@@ -34,6 +34,7 @@ Decoder::Decoder()
     mBuffer[i] = nullptr;
     mCruIn[i] = false;
   }
+  clearCounts();
 }
 
 bool Decoder::open(const std::string name)
@@ -110,6 +111,11 @@ bool Decoder::close()
 void Decoder::clear()
 {
   reset();
+  if (mMaskNoiseRate > 0)
+    clearCounts();
+
+  mPatterns.clear();
+  mErrors.clear();
 }
 
 void Decoder::InsertDigit(int icrate, int itrm, int itdc, int ichain, int channel, uint32_t orbit, uint16_t bunchid, int time_ext, int tdc, int tot)
@@ -117,6 +123,8 @@ void Decoder::InsertDigit(int icrate, int itrm, int itdc, int ichain, int channe
   DigitInfo digitInfo;
 
   fromRawHit2Digit(icrate, itrm, itdc, ichain, channel, orbit, bunchid, time_ext + tdc, tot, digitInfo);
+  if (mMaskNoiseRate > 0)
+    mChannelCounts[digitInfo.channel]++;
 
   mHitDecoded++;
 
@@ -162,6 +170,8 @@ void Decoder::readTRM(int icru, int icrate, uint32_t orbit, uint16_t bunchid)
   for (int i = 0; i < nhits; i++) {
     fromRawHit2Digit(icrate, itrm, mUnion[icru]->packedHit.tdcID, mUnion[icru]->packedHit.chain, mUnion[icru]->packedHit.channel, orbit, bunchid,
                      time_ext + mUnion[icru]->packedHit.time, mUnion[icru]->packedHit.tot, digitInfo);
+    if (mMaskNoiseRate > 0)
+      mChannelCounts[digitInfo.channel]++;
 
     mHitDecoded++;
 
@@ -261,7 +271,7 @@ bool Decoder::decode() // return a vector of digits in a TOF readout window
         //read Orbit
         int orbit = mUnion[icru]->crateOrbit.orbitID;
         if (mVerbose)
-          printf("orbit ID      = %d\n", orbit);
+          printf("%d) orbit ID      = %d -- bunch ID = %d\n", icrate, orbit, bunchid);
         mUnion[icru]++;
         mIntegratedBytes[icru] += 4;
 
