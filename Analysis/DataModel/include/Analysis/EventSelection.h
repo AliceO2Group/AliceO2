21db5e604c191c730d503a169c4443b097bb257c
Evgeny Kryshen <evgeny.kryshen@cern.ch>
2020-10-14 12:36:55 +0300

    Added T0 timing and multiplicity

--- Analysis/DataModel/include/Analysis/EventSelection.h
+++ Analysis/DataModel/include/Analysis/EventSelection.h
@@ -19,6 +19,8 @@ namespace evsel
 {
 // TODO bool arrays are not supported? Storing in int32 for the moment
 DECLARE_SOA_COLUMN(Alias, alias, int32_t[kNaliases]);
+DECLARE_SOA_COLUMN(BBT0A, bbT0A, bool); // beam-beam time in T0A
+DECLARE_SOA_COLUMN(BBT0C, bbT0C, bool); // beam-beam time in T0C
 DECLARE_SOA_COLUMN(BBV0A, bbV0A, bool); // beam-beam time in V0A
 DECLARE_SOA_COLUMN(BBV0C, bbV0C, bool); // beam-beam time in V0C
 DECLARE_SOA_COLUMN(BGV0A, bgV0A, bool); // beam-gas time in V0A
@@ -30,9 +32,11 @@ DECLARE_SOA_COLUMN(BBFDC, bbFDC, bool); // beam-beam time in FDC
 DECLARE_SOA_COLUMN(BGFDA, bgFDA, bool); // beam-gas time in FDA
 DECLARE_SOA_COLUMN(BGFDC, bgFDC, bool); // beam-gas time in FDC
 DECLARE_SOA_DYNAMIC_COLUMN(SEL7, sel7, [](bool bbV0A, bool bbV0C, bool bbZNA, bool bbZNC) -> bool { return bbV0A && bbV0C && bbZNA && bbZNC; });
+DECLARE_SOA_DYNAMIC_COLUMN(SEL8, sel8, [](bool bbT0A, bool bbT0C, bool bbZNA, bool bbZNC) -> bool { return bbT0A && bbT0C && bbZNA && bbZNC; });
 } // namespace evsel
 DECLARE_SOA_TABLE(EvSels, "AOD", "EVSEL",
                   evsel::Alias,
+                  evsel::BBT0A, evsel::BBT0C,
                   evsel::BBV0A, evsel::BBV0C, evsel::BGV0A, evsel::BGV0C, evsel::BBZNA, evsel::BBZNC,
                   evsel::BBFDA, evsel::BBFDC, evsel::BGFDA, evsel::BGFDC,
                   evsel::SEL7<evsel::BBV0A, evsel::BBV0C, evsel::BBZNA, evsel::BBZNC>);
