e61e70311aaf4609110470c502daa59de15cf7ec
David Rohr <drohr@jwdt.org>
2020-10-18 20:58:32 +0200

    Add macro to test TPC refit

---
+++ macro/runTPCRefit.C
@@ -0,0 +1,105 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#if !defined(__CLING__) || defined(__ROOTCLING__)
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include "TSystem.h"
+
+#include "TROOT.h"
+
+#include "GPUO2InterfaceRefit.h"
+#include "TPCReconstruction/TPCFastTransformHelperO2.h"
+#include "DataFormatsParameters/GRPObject.h"
+#include "DataFormatsTPC/ClusterNative.h"
+#include "DataFormatsTPC/ClusterNativeHelper.h"
+#include "SimulationDataFormat/MCCompLabel.h"
+#include "SimulationDataFormat/MCTruthContainer.h"
+#include "DataFormatsTPC/Constants.h"
+#include "DataFormatsTPC/TrackTPC.h"
+#include "DetectorsBase/GeometryManager.h"
+#include "DetectorsBase/Propagator.h"
+#include "DetectorsCommonDataFormats/NameConf.h"
+#else
+#pragma cling load("libO2TPCReconstruction")
+#pragma cling load("libO2DataFormatsTPC")
+#endif
+
+using namespace o2::gpu;
+using namespace o2::tpc;
+using namespace o2::dataformats;
+using namespace o2::base;
+using namespace std;
+
+int runTPCRefit(TString trackFile = "tpctracks.root", TString clusterFile = "tpc-native-clusters.root")
+{
+  GeometryManager::loadGeometry();
+  Propagator::initFieldFromGRP(NameConf::getGRPFileName());
+  const auto grp = o2::parameters::GRPObject::loadFrom("o2sim_grp.root");
+  float bz = 5.00668f * grp->getL3Current() / 30000.;
+  std::unique_ptr<TPCFastTransform> trans = std::move(TPCFastTransformHelperO2::instance()->create(0));
+  auto* prop = Propagator::Instance();
+
+  ClusterNativeAccess clusterIndex;
+  std::vector<TrackTPC>* tracks = nullptr;
+  std::vector<TPCClRefElem>* trackHitRefs = nullptr;
+
+  TFile file(trackFile.Data());
+  auto tree = (TTree*)file.Get("tpcrec");
+  if (tree == nullptr) {
+    std::cout << "Error getting tree\n";
+    return 1;
+  }
+  tree->SetBranchAddress("TPCTracks", &tracks);
+  tree->SetBranchAddress("ClusRefs", &trackHitRefs);
+
+  ClusterNativeHelper::Reader tpcClusterReader{};
+  tpcClusterReader.init(clusterFile.Data());
+  std::unique_ptr<ClusterNative[]> clusterBuffer{};
+  o2::tpc::ClusterNativeHelper::ConstMCLabelContainerViewWithBuffer clusterMCBuffer;
+  const size_t maxEvent = tree->GetEntriesFast();
+  if (maxEvent != tpcClusterReader.getTreeSize()) {
+    std::cout << "Mismatch of entries in cluster and track file\n";
+    return 1;
+  }
+  for (unsigned int iEvent = 0; iEvent < maxEvent; ++iEvent) {
+    std::cout << "Event " << iEvent << " of " << maxEvent << "\n";
+    tree->GetEntry(iEvent);
+    memset(&clusterIndex, 0, sizeof(clusterIndex));
+    tpcClusterReader.read(iEvent);
+    int retVal = tpcClusterReader.fillIndex(clusterIndex, clusterBuffer, clusterMCBuffer);
+    if (retVal < 0) {
+      std::cout << "Error reading clusters (code " << retVal << ")\n";
+      return 1;
+    }
+    GPUTPCO2InterfaceRefit refit(&clusterIndex, trans.get(), bz, trackHitRefs->data(), nullptr, tracks, prop);
+    for (unsigned int i = 0; i < tracks->size(); i++) {
+      TrackTPC trk = (*tracks)[i];
+      std::cout << "\nTrack " << i << "\n";
+      trk.print();
+      std::cout << "Refitting as GPU track\n";
+      if (refit.RefitTrackAsGPU(trk, false, true)) {
+        std::cout << "Refit as GPU track failed\n";
+      } else {
+        trk.print();
+      }
+      trk = (*tracks)[i];
+      std::cout << "Refitting as TrackParCov track\n";
+      if (refit.RefitTrackAsTrackParCov(trk, false, true)) {
+        std::cout << "Refit as TrackParCov track failed\n";
+      } else {
+        trk.print();
+      }
+    }
+  }
+
+  return 0;
+}
