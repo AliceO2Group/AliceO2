a24747c2ad235c046f7adc0bddd1535fe9ad94b1
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2020-10-26 12:56:39 +0100

    DPL: add support for pre-schedule / post-schedule services

--- Framework/Core/include/Framework/ServiceSpec.h
+++ Framework/Core/include/Framework/ServiceSpec.h
@@ -65,6 +65,12 @@ using ServicePostForkChild = std::function<void(ServiceRegistry&)>;
 /// but before doing exec / starting the device.
 using ServicePostForkParent = std::function<void(ServiceRegistry&)>;
 
+/// Callback executed before each redeployment of the whole configuration
+using ServicePreSchedule = std::function<void(ServiceRegistry&, boost::program_options::variables_map const&)>;
+
+/// Callback executed after each redeployment of the whole configuration
+using ServicePostSchedule = std::function<void(ServiceRegistry&, boost::program_options::variables_map const&)>;
+
 /// Callback executed in the driver in order to process a metric.
 using ServiceMetricHandling = std::function<void(ServiceRegistry&,
                                                  std::vector<o2::framework::DeviceMetricsInfo>& metrics,
@@ -111,6 +117,10 @@ struct ServiceSpec {
   /// Callback executed after forking a given device in the driver,
   /// but before doing exec / starting the device.
   ServicePostForkParent postForkParent = nullptr;
+
+  /// Callback executed before and after we schedule a topology
+  ServicePreSchedule preSchedule = nullptr;
+  ServicePostSchedule postSchedule = nullptr;
 
   ///Callback executed after each metric is received by the driver.
   ServiceMetricHandling metricHandling = nullptr;
