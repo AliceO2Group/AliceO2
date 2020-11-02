34e98e51380f6b655151d4f62b2d1be7afd2d9a1
mkruegerGitHub <mkrueger@ikf.uni-frankfurt.de>
2020-11-02 08:52:56 +0100

    HistogramRegistry: add print function (#4737)
    - fix size calculation for ndim histograms
    - add print function to show registry content and size estimates
    - sort histograms in output file alphabetically by default

--- Framework/Core/src/HistogramRegistry.cxx
+++ Framework/Core/src/HistogramRegistry.cxx
@@ -34,4 +34,78 @@ const std::map<HistType, std::function<HistPtr(const HistogramSpec&)>> HistFacto
 
 #undef CALLB
 
+void HistogramRegistry::print(bool showAxisDetails)
+{
+  std::vector<double> fillFractions{0.1, 0.25, 0.5};
+  std::vector<double> totalSizes(fillFractions.size());
+
+  uint32_t nHistos{};
+  bool containsSparseHist{};
+  auto printHistInfo = [&](auto&& hist) {
+    if (hist) {
+      using T = std::decay_t<decltype(*hist)>;
+      bool isSparse{};
+      if (hist->InheritsFrom(THnSparse::Class())) {
+        isSparse = true;
+        containsSparseHist = true;
+      }
+      ++nHistos;
+      std::vector<double> sizes;
+      std::string sizeInfo{};
+      if (isSparse) {
+        std::transform(std::begin(fillFractions), std::end(fillFractions), std::back_inserter(sizes), [&hist](auto& fraction) { return HistFiller::getSize(hist, fraction); });
+        for (int i = 0; i < fillFractions.size(); ++i) {
+          sizeInfo += fmt::format("{:.2f} kB ({:.0f} %)", sizes[i] * 1024, fillFractions[i] * 100);
+          if (i != fillFractions.size() - 1) {
+            sizeInfo += ", ";
+          }
+        }
+      } else {
+        double size = HistFiller::getSize(hist);
+        sizes.resize(fillFractions.size(), size);
+        sizeInfo = fmt::format("{:.2f} kB", sizes[0] * 1024);
+      }
+      std::transform(totalSizes.begin(), totalSizes.end(), sizes.begin(), totalSizes.begin(), std::plus<double>());
+      LOGF(INFO, "Hist %03d: %-35s  %-19s [%s]", nHistos, hist->GetName(), hist->IsA()->GetName(), sizeInfo);
+
+      if (showAxisDetails) {
+        int nDim = 0;
+        if constexpr (std::is_base_of_v<THnBase, T>) {
+          nDim = hist->GetNdimensions();
+        } else if constexpr (std::is_base_of_v<TH1, T>) {
+          nDim = hist->GetDimension();
+        }
+        for (int d = 0; d < nDim; ++d) {
+          TAxis* axis = HistFactory::getAxis(d, hist);
+          LOGF(INFO, "- Axis %d: %-20s (%d bins)", d, axis->GetTitle(), axis->GetNbins());
+        }
+      }
+    }
+  };
+
+  std::string titleString{"======================== HistogramRegistry ========================"};
+  LOGF(INFO, "");
+  LOGF(INFO, "%s", titleString);
+  LOGF(INFO, "%s\"%s\"", std::string((int)(0.5 * titleString.size() - (1 + 0.5 * mName.size())), ' '), mName);
+  std::sort(mRegisteredNames.begin(), mRegisteredNames.end());
+  for (auto& curHistName : mRegisteredNames) {
+    std::visit(printHistInfo, mRegistryValue[getHistIndex(curHistName.data())]);
+  }
+  std::string totalSizeInfo{};
+  if (containsSparseHist) {
+    for (int i = 0; i < totalSizes.size(); ++i) {
+      totalSizeInfo += fmt::format("{:.2f} MB ({:.0f} %)", totalSizes[i], fillFractions[i] * 100);
+      if (i != totalSizes.size() - 1) {
+        totalSizeInfo += ", ";
+      }
+    }
+  } else {
+    totalSizeInfo = fmt::format("{:.2f} MB", totalSizes[0]);
+  }
+  LOGF(INFO, "%s", std::string(titleString.size(), '='), titleString);
+  LOGF(INFO, "Total: %d histograms, ca. %s", nHistos, totalSizeInfo);
+  LOGF(INFO, "%s", std::string(titleString.size(), '='), titleString);
+  LOGF(INFO, "");
+}
+
 } // namespace o2::framework
