9ce6972859cf8cb49309f14d288f7cacca85bc5b
Sylvain Chapeland <o2.syc@cern.ch>
2020-11-03 14:58:02 +0100

    removed obsolete DataBlock references

--- Utilities/DataSampling/src/DataSamplingReadoutAdapter.cxx
+++ Utilities/DataSampling/src/DataSamplingReadoutAdapter.cxx
@@ -11,8 +11,7 @@
 #include "Framework/DataProcessingHeader.h"
 #include "Headers/DataHeader.h"
 #include "Framework/DataSpecUtils.h"
-
-#include <Common/DataBlock.h>
+#include <atomic>
 
 using namespace o2::framework;
 
@@ -21,23 +20,22 @@ namespace o2::utilities
 
 using DataHeader = o2::header::DataHeader;
 
+static std::atomic<unsigned int> blockId = 0;
+
 InjectorFunction dataSamplingReadoutAdapter(OutputSpec const& spec)
 {
   return [spec](FairMQDevice& device, FairMQParts& parts, ChannelRetriever channelRetriever) {
     for (size_t i = 0; i < parts.Size() / 2; ++i) {
 
-      auto dbh = reinterpret_cast<DataBlockHeaderBase*>(parts.At(2 * i)->GetData());
-      assert(dbh->dataSize == parts.At(2 * i + 1)->GetSize());
-
       DataHeader dh;
       ConcreteDataTypeMatcher dataType = DataSpecUtils::asConcreteDataTypeMatcher(spec);
       dh.dataOrigin = dataType.origin;
       dh.dataDescription = dataType.description;
-      dh.subSpecification = DataSpecUtils::getOptionalSubSpec(spec).value_or(dbh->linkId);
-      dh.payloadSize = dbh->dataSize;
+      dh.subSpecification = DataSpecUtils::getOptionalSubSpec(spec).value_or(0xFF);
+      dh.payloadSize = parts.At(2 * i + 1)->GetSize();
       dh.payloadSerializationMethod = o2::header::gSerializationMethodNone;
 
-      DataProcessingHeader dph{dbh->blockId, 0};
+      DataProcessingHeader dph{++blockId, 0};
       o2::header::Stack headerStack{dh, dph};
       sendOnChannel(device, std::move(headerStack), std::move(parts.At(2 * i + 1)), spec, channelRetriever);
     }
