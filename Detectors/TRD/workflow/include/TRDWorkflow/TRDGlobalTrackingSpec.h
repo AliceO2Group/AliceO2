a882edb9083527b6412201883dc462e0ed0f3478
Ole Schmidt <ole.schmidt@cern.ch>
2020-11-04 16:55:23 +0100

    Add DPL TRD tracking workflow
    - added reader for TRD tracklets
    - added TRD tracking as DPL device
    - added dummy TRD track writer

---
+++ Detectors/TRD/workflow/include/TRDWorkflow/TRDGlobalTrackingSpec.h
@@ -0,0 +1,51 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// @file   TRDGlobalTrackingSpec.h
+
+#ifndef O2_TRD_GLOBALTRACKING
+#define O2_TRD_GLOBALTRACKING
+
+#include "Framework/DataProcessorSpec.h"
+#include "Framework/Task.h"
+#include "TStopwatch.h"
+
+#include "GPUO2Interface.h"
+#include "GPUTRDTracker.h"
+
+namespace o2
+{
+namespace trd
+{
+
+class TRDGlobalTracking : public o2::framework::Task
+{
+ public:
+  TRDGlobalTracking(bool useMC) : mUseMC(useMC) {}
+  ~TRDGlobalTracking() override = default;
+  void init(o2::framework::InitContext& ic) final;
+  void run(o2::framework::ProcessingContext& pc) final;
+  void endOfStream(o2::framework::EndOfStreamContext& ec) final;
+
+ private:
+  o2::gpu::GPUTRDTracker* mTracker{nullptr};          ///< TRD tracking engine
+  o2::gpu::GPUReconstruction* mRec{nullptr};          ///< GPU reconstruction pointer, handles memory for the tracker
+  o2::gpu::GPUChainTracking* mChainTracking{nullptr}; ///< TRD tracker is run in the tracking chain
+  bool mUseMC{false};                                 ///< MC flag
+  TStopwatch mTimer;
+};
+
+/// create a processor spec
+framework::DataProcessorSpec getTRDGlobalTrackingSpec(bool useMC);
+
+} // namespace trd
+} // namespace o2
+
+#endif /* O2_TRD_TRACKLETREADER */
