99acfb0bfdf5e7cf4bf5a29986577e8ee4728541
wiechula <Jens.Wiechula@cern.ch>
2020-11-12 20:51:20 +0100

    Code splitting and cleanup
    o move common raw data processing function for calibrations to separate
    file
    o clean up includes

--- Detectors/TPC/workflow/include/TPCWorkflow/TPCCalibPedestalSpec.h
+++ Detectors/TPC/workflow/include/TPCWorkflow/TPCCalibPedestalSpec.h
@@ -14,33 +14,26 @@
 /// @file   TPCCalibPedestalSpec.h
 /// @brief  TPC Pedestal calibration processor
 
-#include "DetectorsCalibration/Utils.h"
-#include "Framework/Task.h"
-#include "Framework/ConfigParamRegistry.h"
-#include "Framework/ControlService.h"
-#include "Framework/WorkflowSpec.h"
-
-#include "Framework/Logger.h"
-#include "Headers/DataHeader.h"
-#include "Framework/ConfigParamSpec.h"
-#include "Framework/CompletionPolicy.h"
-#include "Framework/CompletionPolicyHelpers.h"
-#include "DetectorsCalibration/Utils.h"
-#include "DPLUtils/RawParser.h"
-#include "Headers/DataHeader.h"
-#include "CommonUtils/ConfigurableParam.h"
-#include "TPCCalibration/CalibPedestal.h"
-#include "TPCReconstruction/RawReaderCRU.h"
 #include <vector>
 #include <string>
-#include "DetectorsRaw/RDHUtils.h"
-#include "TPCBase/RDHUtils.h"
-#include "TPCBase/CDBInterface.h"
+#include <chrono>
+
+#include "Framework/Task.h"
+#include "Framework/ControlService.h"
+#include "Framework/Logger.h"
+#include "Framework/ConfigParamRegistry.h"
+
+#include "Headers/DataHeader.h"
 #include "CCDB/CcdbApi.h"
+#include "DetectorsCalibration/Utils.h"
+
+#include "TPCBase/CDBInterface.h"
+#include "TPCCalibration/CalibPedestal.h"
+#include "TPCReconstruction/RawReaderCRU.h"
+#include "TPCWorkflow/CalibProcessingHelper.h"
 
 using namespace o2::framework;
 using namespace o2::tpc;
-using RDHUtils = o2::raw::RDHUtils;
 using clbUtils = o2::calibration::Utils;
 
 namespace o2
@@ -51,6 +44,8 @@ namespace calibration
 class TPCCalibPedestalDevice : public o2::framework::Task
 {
  public:
+  TPCCalibPedestalDevice(bool skipCalib) : mSkipCalib(skipCalib) {}
+
   void init(o2::framework::InitContext& ic) final
   {
     // set up ADC value filling
@@ -78,81 +73,8 @@ class TPCCalibPedestalDevice : public o2::framework::Task
       return;
     }
 
-    std::vector<InputSpec> filter = {{"check", ConcreteDataTypeMatcher{o2::header::gDataOriginTPC, "RAWDATA"}, Lifetime::Timeframe}};
-
-    for (auto const& ref : InputRecordWalker(pc.inputs(), filter)) {
-      const auto* dh = DataRefUtils::getHeader<o2::header::DataHeader*>(ref);
-
-      // ---| extract hardware information to do the processing |---
-      const auto subSpecification = dh->subSpecification;
-      rdh_utils::FEEIDType feeID = (rdh_utils::FEEIDType)dh->subSpecification;
-      rdh_utils::FEEIDType cruID, linkID, endPoint;
-
-      if (mUseOldSubspec) {
-        //---| old definition by Gvozden |---
-        cruID = (rdh_utils::FEEIDType)(subSpecification >> 16);
-        linkID = (rdh_utils::FEEIDType)((subSpecification + (subSpecification >> 8)) & 0xFF) - 1;
-        endPoint = (rdh_utils::FEEIDType)((subSpecification >> 8) & 0xFF) > 0;
-      } else {
-        //---| new definition by David |---
-        rdh_utils::getMapping(feeID, cruID, endPoint, linkID);
-      }
-
-      const auto globalLinkID = linkID + endPoint * 12;
-
-      // ---| update hardware information in the reader |---
-      auto& reader = mRawReader.getReaders()[0];
-      reader->forceCRU(cruID);
-      reader->setLink(globalLinkID);
-
-      LOGP(info, "Specifier: {}/{}/{}", dh->dataOrigin.as<std::string>(), dh->dataDescription.as<std::string>(), subSpecification);
-      LOGP(info, "Payload size: {}", dh->payloadSize);
-      LOGP(info, "CRU: {}; linkID: {}; endPoint: {}; globalLinkID: {}", cruID, linkID, endPoint, globalLinkID);
-
-      // TODO: exception handling needed?
-      const gsl::span<const char> raw = pc.inputs().get<gsl::span<char>>(ref);
-      o2::framework::RawParser parser(raw.data(), raw.size());
-
-      // TODO: it would be better to have external event handling and then moving the event processing functionality to CalibRawBase and RawReader to not repeat it in other places
-      rawreader::ADCRawData rawData;
-      rawreader::GBTFrame gFrame;
-
-      for (auto it = parser.begin(), end = parser.end(); it != end; ++it) {
-        // debugging stuff
-        /*
-        auto* rdhPtr = it.get_if<o2::header::RAWDataHeader>();
-        if (!rdhPtr) {
-          break;
-        }
-        const auto& rdh = *rdhPtr;
-        auto feeID = RDHUtils::getFEEID(rdh);
-        rdh_utils::FEEIDType cru, link, endPoint;
-        rdh_utils::getMapping(feeID, cru, endPoint, link);
-        LOGP(info, "feeID: {} -- CRU: {}; linkID: {}; endPoint: {}", feeID, cru, link, endPoint);
-        */
-
-        const auto size = it.size();
-        auto data = it.data();
-        //LOGP(info, "Data size: {}", size);
-
-        int iFrame = 0;
-        for (int i = 0; i < size; i += 16) {
-          gFrame.setFrameNumber(iFrame);
-          gFrame.setPacketNumber(iFrame / 508);
-          gFrame.readFromMemory(gsl::span<const o2::byte>(data + i, 16));
-
-          // extract the half words from the 4 32-bit words
-          gFrame.getFrameHalfWords();
-
-          gFrame.getAdcValues(rawData);
-          gFrame.updateSyncCheck(false);
-
-          ++iFrame;
-        }
-      }
-
-      reader->runADCDataCallback(rawData);
-    }
+    auto& reader = mRawReader.getReaders()[0];
+    calib_processing_helper::processRawData(pc.inputs(), reader, mUseOldSubspec);
 
     mCalibPedestal.incrementNEvents();
     LOGP(info, "Number of processed events: {} ({})", mCalibPedestal.getNumberOfProcessedEvents(), mMaxEvents);
@@ -174,7 +96,9 @@ class TPCCalibPedestalDevice : public o2::framework::Task
   {
     LOGP(info, "endOfStream");
     dumpCalibData();
-    sendOutput(ec.outputs());
+    if (!mSkipCalib) {
+      sendOutput(ec.outputs());
+    }
     ec.services().get<ControlService>().readyToQuit(QuitRequest::Me);
   }
 
@@ -187,12 +111,16 @@ class TPCCalibPedestalDevice : public o2::framework::Task
   bool mUseOldSubspec{false};
   bool mForceQuit{false};
   bool mDirectFileDump{false};
+  bool mSkipCalib{false};
 
   //____________________________________________________________________________
   void sendOutput(DataAllocator& output)
   {
     CDBStorage::MetaData_t md;
-    long timeStart = 0;
+
+    // perhaps should be changed to time of the run
+    const auto now = std::chrono::system_clock::now();
+    long timeStart = std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()).count();
     long timeEnd = 99999999999999;
 
     std::array<const CalDet<float>*, 2> data = {&mCalibPedestal.getPedestal(), &mCalibPedestal.getNoise()};
@@ -233,19 +161,21 @@ class TPCCalibPedestalDevice : public o2::framework::Task
 namespace framework
 {
 
-DataProcessorSpec getTPCCalibPedestalSpec(const std::string inputSpec)
+DataProcessorSpec getTPCCalibPedestalSpec(const std::string inputSpec, bool skipCalib)
 {
   using device = o2::calibration::TPCCalibPedestalDevice;
 
   std::vector<OutputSpec> outputs;
-  outputs.emplace_back(ConcreteDataTypeMatcher{clbUtils::gDataOriginCLB, clbUtils::gDataDescriptionCLBPayload});
-  outputs.emplace_back(ConcreteDataTypeMatcher{clbUtils::gDataOriginCLB, clbUtils::gDataDescriptionCLBInfo});
+  if (!skipCalib) {
+    outputs.emplace_back(ConcreteDataTypeMatcher{clbUtils::gDataOriginCLB, clbUtils::gDataDescriptionCLBPayload});
+    outputs.emplace_back(ConcreteDataTypeMatcher{clbUtils::gDataOriginCLB, clbUtils::gDataDescriptionCLBInfo});
+  }
 
   return DataProcessorSpec{
     "calib-tpc-pedestal",
     select(inputSpec.data()),
     outputs,
-    AlgorithmSpec{adaptFromTask<device>()},
+    AlgorithmSpec{adaptFromTask<device>(skipCalib)},
     Options{
       {"max-events", VariantType::Int, 100, {"maximum number of events to process"}},
       {"use-old-subspec", VariantType::Bool, false, {"use old subsecifiation definition"}},
