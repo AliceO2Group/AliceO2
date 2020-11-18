16052160a1b5aa2901974e27a26293ba349fa594
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2020-11-18 12:28:10 +0100

    [DCS] Add helper functions to generate (random) DCS data points.
    The main functions are `o2::dcs::generateRandomDataPoints` and
    `o2::dcs::expandAliases`, the latter being used to create easily large
    lists of data point aliases from basic patterns. For instance
    the compact description "DET/Crate[0..3]/Channel[000.042]/[iMon,vMon]"
    would be expanded to 344 (4x43x2) aliases.

---
+++ Detectors/DCS/src/AliasExpander.cxx
@@ -0,0 +1,145 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#include "DetectorsDCS/AliasExpander.h"
+#include <fmt/format.h>
+#include <sstream>
+
+namespace
+{
+
+std::vector<std::string> splitString(const std::string& src, char delim)
+{
+  std::stringstream ss(src);
+  std::string token;
+  std::vector<std::string> tokens;
+
+  while (std::getline(ss, token, delim)) {
+    if (!token.empty()) {
+      tokens.push_back(std::move(token));
+    }
+  }
+
+  return tokens;
+}
+
+std::vector<std::string> extractList(const std::string& slist)
+{
+  auto dots = slist.find(",");
+  if (dots == std::string::npos) {
+    return {};
+  }
+  return splitString(slist, ',');
+}
+
+std::vector<std::string> extractRange(std::string range)
+{
+  auto dots = range.find("..");
+  if (dots == std::string::npos) {
+    return extractList(range);
+  }
+
+  auto braceStart = range.find("{");
+  auto braceEnd = range.find("}");
+
+  if (
+    (braceStart != std::string::npos &&
+     braceEnd == std::string::npos) ||
+    (braceStart == std::string::npos &&
+     braceEnd != std::string::npos)) {
+    // incomplete custom pattern
+    return {};
+  }
+
+  std::string intFormat;
+  std::string sa, sb;
+
+  if (braceStart != std::string::npos &&
+      braceEnd != std::string::npos) {
+    intFormat = range.substr(braceStart, braceEnd - braceStart + 1);
+    range.erase(braceStart, braceEnd);
+    dots = range.find("..");
+    sa = range.substr(0, dots);
+    sb = range.substr(dots + 2);
+  } else {
+    sa = range.substr(0, dots);
+    sb = range.substr(dots + 2);
+    auto size = std::max(sa.size(), sb.size());
+    intFormat = "{:" + fmt::format("0{}d", size) + "}";
+  }
+
+  auto a = std::stoi(sa);
+  auto b = std::stoi(sb);
+  std::vector<std::string> result;
+
+  for (auto i = a; i <= b; i++) {
+    auto substituted = fmt::format(intFormat, i);
+    result.push_back(substituted);
+  }
+  return result;
+}
+} // namespace
+
+namespace o2::dcs
+{
+std::vector<std::string> expandAlias(const std::string& pattern)
+{
+  auto leftBracket = pattern.find("[");
+  auto rightBracket = pattern.find("]");
+
+  // no bracket at all -> return pattern simply
+  if (leftBracket == std::string::npos && rightBracket == std::string::npos) {
+    return {pattern};
+  }
+
+  // no matching bracket -> wrong pattern -> return nothing
+  if ((leftBracket == std::string::npos &&
+       rightBracket != std::string::npos) ||
+      (leftBracket != std::string::npos &&
+       rightBracket == std::string::npos)) {
+    return {};
+  }
+  auto rangeStr = pattern.substr(leftBracket + 1, rightBracket - leftBracket - 1);
+
+  auto range = extractRange(rangeStr);
+
+  // incorrect range -> return nothing
+  if (range.empty()) {
+    return {};
+  }
+
+  auto newPattern = pattern.substr(0, leftBracket) +
+                    "{:s}" +
+                    pattern.substr(rightBracket + 1);
+
+  std::vector<std::string> result;
+
+  for (auto r : range) {
+    auto substituted = fmt::format(newPattern, r);
+    result.emplace_back(substituted);
+  }
+
+  return o2::dcs::expandAliases(result);
+}
+
+std::vector<std::string> expandAliases(const std::vector<std::string>& patternedAliases)
+{
+  std::vector<std::string> result;
+
+  for (auto a : patternedAliases) {
+    auto e = expandAlias(a);
+    result.insert(result.end(), e.begin(), e.end());
+  }
+  // sort to get a predictable result
+  std::sort(result.begin(), result.end());
+
+  return result;
+}
+} // namespace o2::dcs
