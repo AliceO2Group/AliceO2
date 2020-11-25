3511abeaee8de78f6bb4e607ebec19f5bc3d060b
Roberto Preghenella <preghenella@bo.infn.it>
2020-11-25 16:56:38 +0100

    Initial concept of selective tracking with configurable param settings

---
+++ run/SimExamples/Selective_Tranport/run.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#
+# This is a simulation example showing how to run simulation and selectively
+# transport particles through the detector geometry according to configurable settings
+#
+# The simulation uses Pythia8 pp generator with default settings.
+# The specific settings of the simulation are defined in the sim.ini file.
+#
+# Only the parameters for the selective particle transport are adjusted, namely
+# ```
+# [Stack]
+# transportPrimary=external
+# transportPrimaryFileName=transportPDG.macro
+# transportPrimaryFuncName=transportPDG(321)
+# transportPrimaryInvert=false
+# ```
+#
+# `transportPrimary` defines the mode of selection, `external` means that the definition is loaded from a plugin macro
+# defined by the function call `transportPrimaryFuncName` from the file `transportPrimaryFileName`.
+# `transportPrimaryInvert` allows one to invert the definition, hence to inhibit transport for the selected particles.
+#
+
+set -x
+
+MODULES="PIPE ITS TPC"
+EVENTS=100
+NWORKERS=8
+
+o2-sim -j ${NWORKERS} -n ${EVENTS} -g pythia8 -m ${MODULES} -o step1 \
+       --configFile sim.ini \
+       > logstep1 2>&1
+
