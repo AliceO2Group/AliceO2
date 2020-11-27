aa1cdcdf7438a31d45fe7d1acc2b545d2643fb4e
Alla Maevskaya <Alla.Maevskaya@cern.ch>
2020-11-27 15:53:16 +0300

    fix unitest

--- DataFormats/Detectors/FIT/FT0/src/DataFormatsFT0LinkDef.h
+++ DataFormats/Detectors/FIT/FT0/src/DataFormatsFT0LinkDef.h
@@ -47,6 +47,6 @@
 #pragma link C++ class o2::ft0::CTFHeader + ;
 #pragma link C++ class o2::ft0::CompressedDigits + ;
 #pragma link C++ class o2::ft0::CTF + ;
-#pragma link C++ class o2::ctf::EncodedBlocks < o2::ft0::CTFHeader, 8, uint32_t> + ;
+#pragma link C++ class o2::ctf::EncodedBlocks < o2::ft0::CTFHeader, 9, uint32_t> + ;
 
 #endif
