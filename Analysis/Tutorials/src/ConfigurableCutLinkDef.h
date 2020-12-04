42c3a6d018fa98bf40c6098b8656601a995360a1
Anton Alkin <anton.alkin@cern.ch>
2020-12-04 11:40:29 +0100

    DPL Analysis: nested vector for struct configurable (#4897)

--- Analysis/Tutorials/src/ConfigurableCutLinkDef.h
+++ Analysis/Tutorials/src/ConfigurableCutLinkDef.h
@@ -11,5 +11,9 @@
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
-
+#pragma link C++ nestedclasses;
 #pragma link C++ class configurableCut + ;
+#pragma link C++ class std::vector < int> + ;
+#pragma link C++ class std::vector < float> + ;
+#pragma link C++ class std::vector < double> + ;
+#pragma link C++ class std::vector < bool> + ;
