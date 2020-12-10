5a763d13fc55e4427767d60e3da121749b08ea1e
Sandro Wenzel <sandro.wenzel@cern.ch>
2020-12-10 11:24:06 +0100

    Decouple VMC libs from o2-sim
    This is going back to loading VMC engines as real plugins **while**
    retaining the advantage of compiling the configuration templates
    and avoiding ROOT macros execution during startup.
    
    This brings the advantage that not all engine libs are loaded at
    startup simultaneously.

--- Detectors/gconfig/src/G4Config.cxx
+++ Detectors/gconfig/src/G4Config.cxx
@@ -17,18 +17,10 @@
 #include "TG4RunConfiguration.h"
 #include "TPythia6Decayer.h"
 #include "FairModule.h"
-#include <DetectorsPassive/Cave.h>
-#include "DetectorsBase/MaterialManager.h"
-#include "SimSetup/GlobalProcessCutSimParam.h"
 #include "SimConfig/G4Params.h"
 #include "Generators/DecayerPythia8.h"
 
 //using declarations here since SetCuts.C and g4Config.C are included within namespace
-// these are needed for SetCuts.C inclusion
-using o2::GlobalProcessCutSimParam;
-using o2::base::ECut;
-using o2::base::EProc;
-using o2::base::MaterialManager;
 // these are used in g4Config.C
 using std::cout;
 using std::endl;
@@ -40,13 +32,11 @@ namespace o2
 namespace g4config
 {
 #include "../g4Config.C"
-#include "../SetCuts.h"
 
 void G4Config()
 {
   LOG(INFO) << "Setting up G4 sim from library code";
   Config();
-  SetCuts();
 }
 } // namespace g4config
 } // namespace o2
