5886a6af71c5476b098e810012ba2e700852d50b
Jason Barrella <32719308+jbarrella@users.noreply.github.com>
2020-12-10 14:32:21 +0200

    TRD Tracklet Transformer Workflow (#4790)

---
+++ Detectors/TRD/workflow/src/TRDTrackletTransformerSpec.cxx
@@ -0,0 +1,73 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#include <gsl/span>
+
+#include "TRDWorkflow/TRDTrackletTransformerSpec.h"
+
+#include "DataFormatsTRD/TriggerRecord.h"
+#include "DataFormatsTRD/Tracklet64.h"
+#include "DataFormatsTRD/CalibratedTracklet.h"
+
+using namespace o2::framework;
+using SubSpecificationType = o2::framework::DataAllocator::SubSpecificationType;
+
+namespace o2
+{
+namespace trd
+{
+
+void TRDTrackletTransformerSpec::init(o2::framework::InitContext& ic)
+{
+  LOG(info) << "initializing tracklet transformer";
+}
+
+void TRDTrackletTransformerSpec::run(o2::framework::ProcessingContext& pc)
+{
+  LOG(info) << "running tracklet transformer";
+
+  auto tracklets = pc.inputs().get<gsl::span<Tracklet64>>("inTracklets");
+  // std::vector<TriggerRecord> triggerRec = pc.inputs().get<std::vector<TriggerRecord>>("triggerRecord");
+
+  std::vector<CalibratedTracklet> calibratedTracklets;
+  calibratedTracklets.reserve(tracklets.size());
+
+  // temporary. For testing
+  // for (int reci=0; reci < triggerRec.size(); reci++)
+  // {
+  //   LOG(info) << triggerRec[reci].getFirstEntry() << " | " << triggerRec[reci].getNumberOfObjects();
+  // }
+
+  LOG(info) << tracklets.size() << " tracklets found!";
+
+  for (const auto& tracklet : tracklets) {
+    CalibratedTracklet calibratedTracklet = mTransformer.transformTracklet(tracklet);
+    calibratedTracklets.push_back(calibratedTracklet);
+  }
+
+  pc.outputs().snapshot(Output{"TRD", "CTRACKLETS", 0, Lifetime::Timeframe}, calibratedTracklets);
+}
+
+o2::framework::DataProcessorSpec getTRDTrackletTransformerSpec()
+{
+  LOG(info) << "getting TRDTrackletTransformerSpec";
+  return DataProcessorSpec{
+    "TRDTRACKLETTRANSFORMER",
+    Inputs{
+      InputSpec{"inTracklets", "TRD", "TRACKLETS", 0},
+      //  InputSpec{"triggerRecord", "TRD", "TRKTRGRD", 0}
+    },
+    Outputs{OutputSpec{"TRD", "CTRACKLETS", 0, Lifetime::Timeframe}},
+    AlgorithmSpec{adaptFromTask<TRDTrackletTransformerSpec>()},
+    Options{}};
+}
+
+} //end namespace trd
+} //end namespace o2
---
+++ Detectors/TRD/workflow/src/TRDTrackletTransformerWorkflow.cxx
@@ -0,0 +1,48 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#include "TRDWorkflow/TRDTrackletTransformerSpec.h"
+#include "TRDWorkflow/TRDCalibratedTrackletWriterSpec.h"
+#include "TRDWorkflow/TRDTrackletReaderSpec.h"
+
+#include "CommonUtils/ConfigurableParam.h"
+#include "Framework/CompletionPolicy.h"
+
+using namespace o2::framework;
+
+void customize(std::vector<ConfigParamSpec>& workflowOptions)
+{
+  workflowOptions.push_back(ConfigParamSpec{
+    "root-in", VariantType::Int, 1, {"enable (1) or disable (0) input from ROOT file"}});
+  workflowOptions.push_back(ConfigParamSpec{
+    "root-out", VariantType::Int, 1, {"enable (1) or disable (0) output to ROOT file"}});
+}
+
+#include "Framework/runDataProcessing.h"
+
+WorkflowSpec defineDataProcessing(ConfigContext const& configcontext)
+{
+  int rootIn = configcontext.options().get<int>("root-in");
+  int rootOut = configcontext.options().get<int>("root-out");
+
+  WorkflowSpec spec;
+
+  if (rootIn) {
+    spec.emplace_back(o2::trd::getTRDTrackletReaderSpec(0));
+  }
+
+  spec.emplace_back(o2::trd::getTRDTrackletTransformerSpec());
+
+  if (rootOut) {
+    spec.emplace_back(o2::trd::getTRDCalibratedTrackletWriterSpec());
+  }
+
+  return spec;
+}
