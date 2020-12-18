99bb72d231ecf01ede8b385dfd7249836a6afa4a
Henrique Zanoli <hzanoli@gmail.com>
2020-12-18 10:36:05 +0100

    Unify Pt bins

--- Analysis/Tasks/PWGHF/qaTask.cxx
+++ Analysis/Tasks/PWGHF/qaTask.cxx
@@ -165,6 +165,22 @@ Feature CovarianceYZ("Cov_{yz}", "cm^{2}");
 Feature CovarianceZZ("Cov_{zz}", "cm^{2}");
 Feature VertexChi2("#Chi^{2}");
 
+std::vector<double> PtBins = {
+  0.01, 0.0101, 0.0102, 0.0103, 0.0104, 0.0105, 0.0106, 0.0107, 0.0108, 0.0109, 0.011,
+  0.0111, 0.0112, 0.0113, 0.0114, 0.0115, 0.0116, 0.0117, 0.0118, 0.0119, 0.012,
+  0.0121, 0.0122, 0.0123, 0.0124, 0.0125, 0.0126, 0.0127, 0.0128, 0.0129, 0.013,
+  0.0131, 0.0132, 0.0133, 0.0134, 0.0135, 0.0136, 0.0137, 0.0138, 0.0139, 0.014,
+  0.0141, 0.0142, 0.0143, 0.0144, 0.0145, 0.0146, 0.0147, 0.0148, 0.0149, 0.015,
+  0.0151, 0.0152, 0.0153, 0.0154, 0.0155, 0.0156, 0.0157, 0.0158, 0.0159, 0.016,
+  0.0161, 0.0162, 0.0163, 0.0164, 0.0165, 0.0166, 0.0167, 0.0168, 0.0169, 0.017,
+  0.0171, 0.0172, 0.0173, 0.0174, 0.0175, 0.0176, 0.0177, 0.0178, 0.0179, 0.018,
+  0.0181, 0.0182, 0.0183, 0.0184, 0.0185, 0.0186, 0.0187, 0.0188, 0.0189, 0.019,
+  0.0191, 0.0192, 0.0193, 0.0194, 0.0195, 0.0196, 0.0197, 0.0198, 0.0199, 0.02,
+  0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.12, 0.14, 0.155, 0.16, 0.165,
+  0.175, 0.18, 0.185, 0.2, 0.225, 0.25, 0.275, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6,
+  0.7, 0.8, 0.9, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.5, 3.0, 3.5, 4.0, 5.0, 6.0,
+  8.0, 10.0, 15.0, 20.0, 30.0, 50.0, 100.0};
+
 } // namespace o2::qa::features
 
 namespace qafeat = o2::qa::features;
@@ -261,9 +277,6 @@ struct QAGlobalObservables {
 
 /// Task to QA the kinematic properties of the tracks
 struct QATrackingKine {
-  o2fw::Configurable<int> nBinsPt{"nBinsPt", 100, "Number of bins for Pt"};
-  std::array<double, 2> ptRange = {0, 10.};
-
   o2fw::Configurable<int> nBinsPhi{"nBinsPhi", 100, "Number of bins for Phi"};
 
   o2fw::Configurable<int> nBinsEta{"nBinsEta", 100, "Number of bins for the eta histogram."};
@@ -274,7 +287,7 @@ struct QATrackingKine {
   void init(o2fw::InitContext&)
   {
     histos.add("tracking/pt", qafeat::MakeTitle({qafeat::Pt}).c_str(), o2fw::kTH1D,
-               {{nBinsPt, ptRange[0], ptRange[1]}});
+               {qafeat::PtBins});
     histos.add("tracking/eta", qafeat::MakeTitle({qafeat::Eta.NameRaw()}).c_str(), o2fw::kTH1D,
                {{nBinsEta, etaRange[0], etaRange[1]}});
     histos.add("tracking/phi", qafeat::MakeTitle({qafeat::Phi}).c_str(), o2fw::kTH1D, {{nBinsPhi, 0, 2 * M_PI}});
@@ -290,22 +303,6 @@ struct QATrackingKine {
 
 /// Task to evaluate the tracking resolution (Pt, Eta, Phi and impact parameter)
 struct QATrackingResolution {
-  std::vector<double> ptBins = {
-    0.01, 0.0101, 0.0102, 0.0103, 0.0104, 0.0105, 0.0106, 0.0107, 0.0108, 0.0109, 0.011, 
-    0.0111, 0.0112, 0.0113, 0.0114, 0.0115, 0.0116, 0.0117, 0.0118, 0.0119, 0.012, 
-    0.0121, 0.0122, 0.0123, 0.0124, 0.0125, 0.0126, 0.0127, 0.0128, 0.0129, 0.013, 
-    0.0131, 0.0132, 0.0133, 0.0134, 0.0135, 0.0136, 0.0137, 0.0138, 0.0139, 0.014, 
-    0.0141, 0.0142, 0.0143, 0.0144, 0.0145, 0.0146, 0.0147, 0.0148, 0.0149, 0.015, 
-    0.0151, 0.0152, 0.0153, 0.0154, 0.0155, 0.0156, 0.0157, 0.0158, 0.0159, 0.016, 
-    0.0161, 0.0162, 0.0163, 0.0164, 0.0165, 0.0166, 0.0167, 0.0168, 0.0169, 0.017, 
-    0.0171, 0.0172, 0.0173, 0.0174, 0.0175, 0.0176, 0.0177, 0.0178, 0.0179, 0.018, 
-    0.0181, 0.0182, 0.0183, 0.0184, 0.0185, 0.0186, 0.0187, 0.0188, 0.0189, 0.019, 
-    0.0191, 0.0192, 0.0193, 0.0194, 0.0195, 0.0196, 0.0197, 0.0198, 0.0199, 0.02, 
-    0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.12, 0.14, 0.155, 0.16, 0.165, 
-    0.175, 0.18, 0.185, 0.2, 0.225, 0.25, 0.275, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 
-    0.7, 0.8, 0.9, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.5, 3.0, 3.5, 4.0, 5.0, 6.0, 
-    8.0, 10.0, 15.0, 20.0, 30.0, 50.0, 100.0};
-
   o2fw::Configurable<bool> useOnlyPhysicsPrimary{"useOnlyPhysicsPrimary", true,
                                                  "Whether to use only physical primary particles for the resolution."};
 
@@ -326,7 +323,7 @@ struct QATrackingResolution {
 
   o2fw::Configurable<int> nBinsImpactParameter{"nBinsImpactParameter", 2000, "Number of bins for the Impact parameter"};
 
-  std::array<double, 2> impactParameterRange = {-500, 500};       // micrometer
+  std::array<double, 2> impactParameterRange = {-500, 500};         // micrometer
   std::array<double, 2> impactParameterResolutionRange = {0, 1000}; // micrometer
 
   // Registry of histograms
@@ -336,7 +333,7 @@ struct QATrackingResolution {
   {
     // Histogram axis definitions
 
-    o2fw::AxisSpec ptAxis{ptBins};
+    o2fw::AxisSpec ptAxis{qafeat::PtBins};
     o2fw::AxisSpec deltaPtAxis{nBinsDeltaPt, deltaPtRange[0], deltaPtRange[1]};
     o2fw::AxisSpec deltaPtRelativeAxis{nBinsDeltaPt, deltaPtRange[0], deltaPtRange[1]};
     o2fw::AxisSpec deltaPtAbsoluteRelativeAxis{nBinsDeltaPt, 0., deltaPtRange[1]};
@@ -494,22 +491,6 @@ struct QATrackingResolution {
 /// Task to QA the efficiency of a particular particle defined by particlePDG
 template <PDG_t particlePDG>
 struct QATrackingEfficiency {
-  std::vector<double> ptBins = {
-    0.01, 0.0101, 0.0102, 0.0103, 0.0104, 0.0105, 0.0106, 0.0107, 0.0108, 0.0109, 0.011, 
-    0.0111, 0.0112, 0.0113, 0.0114, 0.0115, 0.0116, 0.0117, 0.0118, 0.0119, 0.012, 
-    0.0121, 0.0122, 0.0123, 0.0124, 0.0125, 0.0126, 0.0127, 0.0128, 0.0129, 0.013, 
-    0.0131, 0.0132, 0.0133, 0.0134, 0.0135, 0.0136, 0.0137, 0.0138, 0.0139, 0.014, 
-    0.0141, 0.0142, 0.0143, 0.0144, 0.0145, 0.0146, 0.0147, 0.0148, 0.0149, 0.015, 
-    0.0151, 0.0152, 0.0153, 0.0154, 0.0155, 0.0156, 0.0157, 0.0158, 0.0159, 0.016, 
-    0.0161, 0.0162, 0.0163, 0.0164, 0.0165, 0.0166, 0.0167, 0.0168, 0.0169, 0.017, 
-    0.0171, 0.0172, 0.0173, 0.0174, 0.0175, 0.0176, 0.0177, 0.0178, 0.0179, 0.018, 
-    0.0181, 0.0182, 0.0183, 0.0184, 0.0185, 0.0186, 0.0187, 0.0188, 0.0189, 0.019, 
-    0.0191, 0.0192, 0.0193, 0.0194, 0.0195, 0.0196, 0.0197, 0.0198, 0.0199, 0.02, 
-    0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.12, 0.14, 0.155, 0.16, 0.165, 
-    0.175, 0.18, 0.185, 0.2, 0.225, 0.25, 0.275, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 
-    0.7, 0.8, 0.9, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.5, 3.0, 3.5, 4.0, 5.0, 6.0, 
-    8.0, 10.0, 15.0, 20.0, 30.0, 50.0, 100.0};
-
   o2fw::Configurable<int> nBinsEta{"nBinsEta", 30, "Number of bins for the pseudorapidity"};
   std::array<double, 2> etaRange = {-3, 3};
 
@@ -520,7 +501,7 @@ struct QATrackingEfficiency {
 
   void init(o2fw::InitContext&)
   {
-    o2fw::AxisSpec ptAxis{ptBins};
+    o2fw::AxisSpec ptAxis{qafeat::PtBins};
     o2fw::AxisSpec phiAxis{nBinsPhi, phiRange[0], phiRange[1]};
     o2fw::AxisSpec etaAxis{nBinsEta, etaRange[0], etaRange[1]};
 
