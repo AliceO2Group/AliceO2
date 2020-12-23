a9681a6d8df4e2b6b9e7b0cd7524925a8ca2f564
David Rohr <drohr@jwdt.org>
2020-12-23 12:16:43 +0100

    GPU: Exclude ITS Tracking / Vertexing parts from run time compilation

--- GPU/GPUTracking/Base/GPUReconstructionIncludesDevice.h
+++ GPU/GPUTracking/Base/GPUReconstructionIncludesDevice.h
@@ -95,13 +95,13 @@ using namespace GPUCA_NAMESPACE::gpu;
 // Files for ITS Track Fit
 #include "GPUITSFitterKernels.cxx"
 
-#if !defined(GPUCA_O2_LIB) && defined(__HIPCC__) && !defined(GPUCA_NO_ITS_TRAITS)
+#if !defined(GPUCA_O2_LIB) && defined(__HIPCC__) && !defined(GPUCA_NO_ITS_TRAITS) && !defined(GPUCA_GPUCODE_GENRTC)
 #include "VertexerTraitsHIP.hip.cxx"
 #include "ContextHIP.hip.cxx"
 #include "DeviceStoreVertexerHIP.hip.cxx"
 #include "ClusterLinesHIP.hip.cxx"
 #include "UtilsHIP.hip.cxx"
-#elif !defined(GPUCA_O2_LIB) && defined(__CUDACC__) && !defined(GPUCA_NO_ITS_TRAITS)
+#elif !defined(GPUCA_O2_LIB) && defined(__CUDACC__) && !defined(GPUCA_NO_ITS_TRAITS) && !defined(GPUCA_GPUCODE_GENRTC)
 #include "TrackerTraitsNV.cu"
 #include "VertexerTraitsGPU.cu"
 #include "Context.cu"
