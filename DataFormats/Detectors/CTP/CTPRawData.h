af05203711a79ded5439a4204f78a9344f03fb40
lietava <roman.lietava@cern.ch>
2021-01-03 14:51:35 +0100

    clang format fixed

--- DataFormats/Detectors/CTP/CTPRawData.h
+++ DataFormats/Detectors/CTP/CTPRawData.h
@@ -1,23 +1,21 @@
 #ifndef ALICEO2_CTPRAWDATA_H
 #define ALICEO2_CTPRAWDATA_H
 #include InteractionRecord.h
-namespace  o2
+namespace o2
 {
-    namespace ctp
-    {
-        struct CTPCRUData
-        {
-            static constexpr uint8_t NumberOfClasses=64;
-            static constexpr uint8_t NumberOfLMinputs=16;
-            static constexpr uint8_t NumberOfL0inputs=30;
-            static constexpr uint8_t NumberOfL1inputs=18;
-            InteractionRecord ir;
-            uint32_t InputsMask=0;
-            uint64_t TriggerClassMask=0;
+namespace ctp
+{
+struct CTPCRUData {
+  static constexpr uint8_t NumberOfClasses = 64;
+  static constexpr uint8_t NumberOfLMinputs = 16;
+  static constexpr uint8_t NumberOfL0inputs = 30;
+  static constexpr uint8_t NumberOfL1inputs = 18;
+  InteractionRecord ir;
+  uint32_t InputsMask = 0;
+  uint64_t TriggerClassMask = 0;
 
-            CTPRawData() = default;
-        }
-    }
-
+  CTPRawData() = default;
 }
+} // namespace ctp
 
+} // namespace o2
