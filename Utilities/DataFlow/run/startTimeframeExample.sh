b821240d44b54773f9edda84892fddbac97839b4
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2021-01-12 08:59:13 +0100

    Drop initial prototype of dataflow setup (#5032)

--- Utilities/DataFlow/run/startTimeframeExample.sh
+++
@@ -1,4 +0,0 @@
-xterm -geometry 80x25+0+0 -hold -e FakeTimeframeGeneratorDevice --id FakeTimeframeGeneratorDevice --mq-config confFakeTimeframe.json --output-channel-name output &
-xterm -geometry 80x25+0+0 -hold -e o2-timeframe-writer-device --id TimeframeWriterDevice --mq-config confFakeTimeframe.json --input-channel-name input --max-timeframes 1 --output-file data.o2tf &
-#xterm -geometry 80x25+0+0 -hold -e o2-timeframe-reader-device --id TimeframeReaderDevice --mq-config confFakeTimeframe.json --input-file data.o2tf --output-channel-name output &
-#xterm -geometry 80x25+0+0 -hold -e o2-timeframe-validator-device --id TimeframeValidatorDevice --mq-config confFakeTimeframe.json --input-channel-name input &
