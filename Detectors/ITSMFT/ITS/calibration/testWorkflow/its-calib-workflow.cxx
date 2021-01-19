1304d12d1911ed5a7ee6512ebcc07407203b1d11
Iouri Belikov <iouri.belikov@cern.ch>
2021-01-19 12:38:29 +0100

    Creating a skeleton for the ITS calibration in DPL

---
+++ Detectors/ITSMFT/ITS/calibration/testWorkflow/its-calib-workflow.cxx
@@ -0,0 +1,45 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#include "Framework/ConfigParamSpec.h"
+
+using namespace o2::framework;
+
+// we need to add workflow options before including Framework/runDataProcessing
+void customize(std::vector<ConfigParamSpec>& workflowOptions)
+{
+  // option allowing to set parameters
+  workflowOptions.push_back(
+    ConfigParamSpec{
+      "doNoise",
+      VariantType::Bool,
+      true,
+      {"Generate noisy-pixel maps"}});
+}
+
+// ------------------------------------------------------------------
+
+#include "Framework/runDataProcessing.h"
+#include "ITSCalibration/NoiseCalibratorSpec.h"
+
+WorkflowSpec defineDataProcessing(ConfigContext const& configcontext)
+{
+  WorkflowSpec specs;
+  auto doNoise = configcontext.options().get<bool>("doNoise");
+
+  LOG(INFO) << "ITS calibration workflow options";
+  LOG(INFO) << "Generate noisy-pixel maps: " << doNoise;
+
+  if (doNoise) {
+    specs.emplace_back(o2::its::getNoiseCalibratorSpec());
+  }
+
+  return specs;
+}
