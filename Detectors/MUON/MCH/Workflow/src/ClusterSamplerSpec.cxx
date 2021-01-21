49f2934488e52c6f0633c862bf24c3eb5ae865c3
pillot <pillot@subatech.in2p3.fr>
2021-01-21 18:26:42 +0100

    [MCH] introduce MCH ROFRecord.h + modify tracking workflows to process several ROF per TF (#5222)

--- Detectors/MUON/MCH/Workflow/src/ClusterSamplerSpec.cxx
+++ Detectors/MUON/MCH/Workflow/src/ClusterSamplerSpec.cxx
@@ -17,8 +17,8 @@
 
 #include <iostream>
 #include <fstream>
-
 #include <stdexcept>
+#include <vector>
 
 #include "Framework/CallbackService.h"
 #include "Framework/ConfigParamRegistry.h"
@@ -29,6 +29,7 @@
 #include "Framework/Task.h"
 #include "Framework/Logger.h"
 
+#include "DataFormatsMCH/ROFRecord.h"
 #include "MCHBase/ClusterBlock.h"
 #include "MCHBase/Digit.h"
 
@@ -52,7 +53,15 @@ class ClusterSamplerTask
     auto inputFileName = ic.options().get<std::string>("infile");
     mInputFile.open(inputFileName, ios::binary);
     if (!mInputFile.is_open()) {
-      throw invalid_argument("Cannot open input file" + inputFileName);
+      throw invalid_argument("cannot open input file" + inputFileName);
+    }
+    if (mInputFile.peek() == EOF) {
+      throw length_error("input file is empty");
+    }
+
+    mNEventsPerTF = ic.options().get<int>("nEventsPerTF");
+    if (mNEventsPerTF < 1) {
+      throw invalid_argument("number of events per time frame must be >= 1");
     }
 
     auto stop = [this]() {
@@ -66,28 +75,60 @@ class ClusterSamplerTask
   //_________________________________________________________________________________________________
   void run(framework::ProcessingContext& pc)
   {
-    /// send the clusters of the current event
+    /// send the clusters of the next events in the current TF
 
-    // get the number of clusters and associated digits
-    int nClusters(0);
+    static uint32_t event(0);
+
+    // reached eof
+    if (mInputFile.peek() == EOF) {
+      pc.services().get<ControlService>().endOfStream();
+      //pc.services().get<ControlService>().readyToQuit(QuitRequest::Me);
+      return;
+    }
+
+    // create the output messages
+    auto& rofs = pc.outputs().make<std::vector<ROFRecord>>(OutputRef{"rofs"});
+    auto& clusters = pc.outputs().make<std::vector<ClusterStruct>>(OutputRef{"clusters"});
+
+    // loop over the requested number of events (or until eof) and fill the messages
+    for (int iEvt = 0; iEvt < mNEventsPerTF && mInputFile.peek() != EOF; ++iEvt) {
+      int nClusters = readOneEvent(clusters);
+      rofs.emplace_back(o2::InteractionRecord{0, event++}, clusters.size() - nClusters, nClusters);
+    }
+  }
+
+ private:
+  //_________________________________________________________________________________________________
+  int readOneEvent(std::vector<ClusterStruct, o2::pmr::polymorphic_allocator<ClusterStruct>>& clusters)
+  {
+    /// fill the internal buffer with the clusters of the current event
+
+    // get the number of clusters
+    int nClusters(-1);
     mInputFile.read(reinterpret_cast<char*>(&nClusters), sizeof(int));
     if (mInputFile.fail()) {
-      pc.services().get<ControlService>().endOfStream();
-      return; // probably reached eof
+      throw length_error("invalid input");
     }
-    int nDigits(0);
+
+    // get the number of associated digits
+    int nDigits(-1);
     mInputFile.read(reinterpret_cast<char*>(&nDigits), sizeof(int));
+    if (mInputFile.fail()) {
+      throw length_error("invalid input");
+    }
 
     if (nClusters < 0 || nDigits < 0) {
-      throw length_error("incorrect message payload");
+      throw length_error("invalid input");
     }
-
-    // create the output message
-    auto clusters = pc.outputs().make<ClusterStruct>(Output{"MCH", "CLUSTERS", 0, Lifetime::Timeframe}, nClusters);
 
     // fill clusters in O2 format, if any
     if (nClusters > 0) {
-      mInputFile.read(reinterpret_cast<char*>(clusters.data()), clusters.size_bytes());
+      int clusterOffset = clusters.size();
+      clusters.resize(clusterOffset + nClusters);
+      mInputFile.read(reinterpret_cast<char*>(&clusters[clusterOffset]), nClusters * sizeof(ClusterStruct));
+      if (mInputFile.fail()) {
+        throw length_error("invalid input");
+      }
     } else {
       LOG(INFO) << "event is empty";
     }
@@ -95,11 +136,16 @@ class ClusterSamplerTask
     // skip the digits if any
     if (nDigits > 0) {
       mInputFile.seekg(nDigits * sizeof(Digit), std::ios::cur);
+      if (mInputFile.fail()) {
+        throw length_error("invalid input");
+      }
     }
+
+    return nClusters;
   }
 
- private:
   std::ifstream mInputFile{}; ///< input file
+  int mNEventsPerTF = 1;      ///< number of events per time frame
 };
 
 //_________________________________________________________________________________________________
@@ -108,9 +154,11 @@ o2::framework::DataProcessorSpec getClusterSamplerSpec()
   return DataProcessorSpec{
     "ClusterSampler",
     Inputs{},
-    Outputs{OutputSpec{"MCH", "CLUSTERS", 0, Lifetime::Timeframe}},
+    Outputs{OutputSpec{{"rofs"}, "MCH", "CLUSTERROFS", 0, Lifetime::Timeframe},
+            OutputSpec{{"clusters"}, "MCH", "CLUSTERS", 0, Lifetime::Timeframe}},
     AlgorithmSpec{adaptFromTask<ClusterSamplerTask>()},
-    Options{{"infile", VariantType::String, "", {"input filename"}}}};
+    Options{{"infile", VariantType::String, "", {"input filename"}},
+            {"nEventsPerTF", VariantType::Int, 1, {"number of events per time frame"}}}};
 }
 
 } // end namespace mch
