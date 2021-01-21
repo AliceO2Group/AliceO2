91b4df1c78f867c9a57b1b91b4bcb3f29785cfcf
David Rohr <drohr@jwdt.org>
2021-01-21 14:21:50 +0100

    GPU: Move utils/qa/display folders from Standalone to GPUTracking, since they are used by O2 meanwhile

--- GPU/GPUTracking/Standalone/qconfigoptions.h
+++ GPU/GPUTracking/Standalone/qconfigoptions.h
@@ -1,16 +1,1 @@
-// Copyright CERN and copyright holders of ALICE O2. This software is
-// distributed under the terms of the GNU General Public License v3 (GPL
-// Version 3), copied verbatim in the file "COPYING".
-//
-// See http://alice-o2.web.cern.ch/license for full licensing information.
-//
-// In applying this license CERN does not waive the privileges and immunities
-// granted to it by virtue of its status as an Intergovernmental Organization
-// or submit itself to any jurisdiction.
-
-/// \file qconfigoptions.h
-/// \author David Rohr
-
-#include "GPUSettings.h" // Usually we are included from GPUSettings.h, so this doesn't do anything.
-                         // But ROOT or qconfig.cpp will include us directly, so load the settings first.
-#include "GPUSettingsList.h"
+../qconfigoptions.h
