4c5c43efa6375263893259b6dd3a36003dc3bb95
Iouri Belikov <iouri.belikov@cern.ch>
2021-01-22 15:16:27 +0100

    Using the new noise calibration class inside the old macro

--- Detectors/ITSMFT/ITS/calibration/macros/MakeNoiseMapFromClusters.C
+++ Detectors/ITSMFT/ITS/calibration/macros/MakeNoiseMapFromClusters.C
@@ -2,19 +2,27 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <gsl/span>
 
 #include <TFile.h>
 #include <TTree.h>
 
+#include "ITSCalibration/NoiseCalibrator.h"
+
 #include "DataFormatsITSMFT/ClusterPattern.h"
 #include "DataFormatsITSMFT/CompCluster.h"
 #include "DataFormatsITSMFT/ROFRecord.h"
-#include "DataFormatsITSMFT/NoiseMap.h"
 
 #endif
 
-void MakeNoiseMapFromClusters(std::string input = "o2clus_its.root", std::string output = "noise.root", bool only1pix = false, float probT = 3e-6)
+void MakeNoiseMapFromClusters(std::string input = "o2clus_its.root", bool only1pix = false, float probT = 3e-6, std::string output = "noise.root")
 {
+  TFile out(output.data(), "new");
+  if (!out.IsOpen()) {
+    std::cerr << "The output file " << output << " already exists !";
+    return;
+  }
+
   TFile in(input.data());
   if (!in.IsOpen()) {
     std::cerr << "Can not open the input file " << input << '\n';
@@ -41,75 +49,16 @@ void MakeNoiseMapFromClusters(std::string input = "o2clus_its.root", std::string
   std::vector<o2::itsmft::ROFRecord>* rofVec = nullptr;
   clusTree->SetBranchAddress("ITSClustersROF", &rofVec);
 
-  o2::itsmft::NoiseMap noiseMap(24120);
+  o2::its::NoiseCalibrator calib(only1pix, probT);
 
-  long int nStrobes = 0;
   auto nevents = clusTree->GetEntries();
   for (int n = 0; n < nevents; n++) {
     clusTree->GetEntry(n);
-    auto pattIt = patternsPtr->cbegin();
-    for (const auto& rof : *rofVec) {
-      nStrobes++;
-      auto clustersInFrame = rof.getROFData(*clusters);
-      for (const auto& c : clustersInFrame) {
-        if (c.getPatternID() != o2::itsmft::CompCluster::InvalidPatternID)
-          continue;
-
-        o2::itsmft::ClusterPattern patt(pattIt);
-
-        auto id = c.getSensorID();
-        auto row = c.getRow();
-        auto col = c.getCol();
-        auto colSpan = patt.getColumnSpan();
-        auto rowSpan = patt.getRowSpan();
-
-        if ((rowSpan == 1) && (colSpan == 1)) {
-          noiseMap.increaseNoiseCount(id, row, col);
-          continue;
-        }
-
-        if (only1pix)
-          continue;
-
-        auto nBits = rowSpan * colSpan;
-        int ic = 0, ir = 0;
-        for (unsigned int i = 2; i < patt.getUsedBytes() + 2; i++) {
-          unsigned char tempChar = patt.getByte(i);
-          int s = 128; // 0b10000000
-          while (s > 0) {
-            if ((tempChar & s) != 0) {
-              noiseMap.increaseNoiseCount(id, row + ir, col + ic);
-            }
-            ic++;
-            s >>= 1;
-            if ((ir + 1) * ic == nBits) {
-              break;
-            }
-            if (ic == colSpan) {
-              ic = 0;
-              ir++;
-            }
-          }
-          if ((ir + 1) * ic == nBits) {
-            break;
-          }
-        }
-      }
-    }
+    calib.processTimeFrame(*clusters, *patternsPtr, *rofVec);
   }
+  calib.finalize();
 
-  noiseMap.applyProbThreshold(probT, nStrobes);
-
-  int fired = probT * nStrobes;
-  int ncalib = noiseMap.dumpAboveThreshold(fired);
-  std::cout << "Probalibity threshold: " << probT
-            << "  number of pixels: " << ncalib << '\n';
-
-  TFile out(output.data(), "new");
-  if (!out.IsOpen()) {
-    std::cerr << "Can not open the output file " << output << '\n';
-    return;
-  }
-  out.WriteObject(&noiseMap, "Noise");
+  const auto& map = calib.getNoiseMap();
+  out.WriteObject(&map, "Noise");
   out.Close();
 }
