6867586cc9aa2134ea5ace4140ab2c2b0cf84d94
Alexander Kalweit <Alexander.Philipp.Kalweit@cern.ch>
2021-01-23 21:33:01 +0100

    updating nuclei 3He task to filter and fulfill coding conventions

--- Analysis/Tasks/PWGLF/NucleiSpectraTask.cxx
+++ Analysis/Tasks/PWGLF/NucleiSpectraTask.cxx
@@ -44,6 +44,7 @@ struct NucleiSpecraTask {
     AxisSpec centAxis = {centBinning, "V0M (%)"};
 
     spectra.add("fCollZpos", "collision z position", HistType::kTH1F,{{600, -20., +20., "z position (cm)"}});
+    spectra.add("fKeepEvent", "skimming histogram", HistType::kTH1F,{{2, -0.5, +1.5, "true: keep event, false: reject event"}});
     spectra.add("fTPCsignal", "Specific energy loss", HistType::kTH2F, {{600, 0., 3, "#it{p} (GeV/#it{c})"}, {1400, 0, 1400, "d#it{E} / d#it{X} (a. u.)"}});
     spectra.add("fTPCcounts", "n-sigma TPC", HistType::kTH2F, {ptAxis, {200, -100., +100., "n#sigma_{He} (a. u.)"}});
   }
@@ -54,7 +55,9 @@ struct NucleiSpecraTask {
 
   Configurable<float> cfgCutVertex{"cfgCutVertex", 10.0f, "Accepted z-vertex range"};
   Configurable<float> cfgCutEta{"cfgCutEta", 0.8f, "Eta range for tracks"};
-  Configurable<float> nsigmacut{"nsigmacut", 3, "Value of the Nsigma cut"};
+  Configurable<float> nsigmacutLow{"nsigmacutLow", -30.0, "Value of the Nsigma cut"};
+  Configurable<float> nsigmacutHigh{"nsigmacutHigh", +3., "Value of the Nsigma cut"};
+
 
   Filter collisionFilter = nabs(aod::collision::posZ) < cfgCutVertex;
   Filter trackFilter = (nabs(aod::track::eta) < cfgCutEta) && (aod::track::isGlobalTrack == (uint8_t) true);
@@ -63,26 +66,43 @@ struct NucleiSpecraTask {
 
   void process(soa::Filtered<soa::Join<aod::Collisions, aod::EvSels>>::iterator const& collision, aod::BCsWithTimestamps const&, TrackCandidates const& tracks)
   {
-    
-    if (!collision.alias()[kINT7])
+    //
+    // collision process loop
+    //
+    bool keepEvent = kFALSE;
+    if (!collision.alias()[kINT7]) {
       return;
-    if (!collision.sel7())
+    }
+    if (!collision.sel7()) {
       return;
-
+    }
+    //
     spectra.fill(HIST("fCollZpos"), collision.posZ());
-
-
-    for (auto track : tracks) {
+    //
+    for (auto track : tracks) { // start loop over tracks
 
       TLorentzVector cutVector{};
-      cutVector.SetPtEtaPhiM(track.pt(), track.eta(), track.phi(), constants::physics::MassHelium3);
+      cutVector.SetPtEtaPhiM(track.pt()*2.0, track.eta(), track.phi(), constants::physics::MassHelium3);
       if (cutVector.Rapidity() < yMin + yBeam || cutVector.Rapidity() > yMax + yBeam) {
         continue;
       }
-
+      //
+      // fill QA histograms
+      //  
       spectra.fill(HIST("fTPCsignal"), track.tpcInnerParam(), track.tpcSignal());
-      spectra.fill(HIST("fTPCcounts"), fabs(track.pt()), track.tpcNSigmaHe());
-    }
+      spectra.fill(HIST("fTPCcounts"), track.tpcInnerParam(), track.tpcNSigmaHe());
+      //
+      // check offline-trigger (skimming) condidition
+      //
+      if (track.tpcNSigmaHe() > nsigmacutLow && track.tpcNSigmaHe() < nsigmacutHigh) {
+        keepEvent = kTRUE;
+      }
+
+    } // end loop over tracks
+    //
+    // fill trigger (skimming) results
+    //
+    spectra.fill(HIST("fKeepEvent"), keepEvent);
   }
 };
 
