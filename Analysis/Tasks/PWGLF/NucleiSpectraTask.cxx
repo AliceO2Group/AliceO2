1cd04c3a75cabe231731955e2e34bfe615b687d9
Alexander Kalweit <Alexander.Philipp.Kalweit@cern.ch>
2021-01-25 10:07:37 +0100

    clang fixes

--- Analysis/Tasks/PWGLF/NucleiSpectraTask.cxx
+++ Analysis/Tasks/PWGLF/NucleiSpectraTask.cxx
@@ -43,8 +43,8 @@ struct NucleiSpecraTask {
     AxisSpec ptAxis = {ptBinning, "#it{p}_{T} (GeV/#it{c})"};
     AxisSpec centAxis = {centBinning, "V0M (%)"};
 
-    spectra.add("fCollZpos", "collision z position", HistType::kTH1F,{{600, -20., +20., "z position (cm)"}});
-    spectra.add("fKeepEvent", "skimming histogram", HistType::kTH1F,{{2, -0.5, +1.5, "true: keep event, false: reject event"}});
+    spectra.add("fCollZpos", "collision z position", HistType::kTH1F, {{600, -20., +20., "z position (cm)"}});
+    spectra.add("fKeepEvent", "skimming histogram", HistType::kTH1F, {{2, -0.5, +1.5, "true: keep event, false: reject event"}});
     spectra.add("fTPCsignal", "Specific energy loss", HistType::kTH2F, {{600, 0., 3, "#it{p} (GeV/#it{c})"}, {1400, 0, 1400, "d#it{E} / d#it{X} (a. u.)"}});
     spectra.add("fTPCcounts", "n-sigma TPC", HistType::kTH2F, {ptAxis, {200, -100., +100., "n#sigma_{He} (a. u.)"}});
   }
@@ -57,7 +57,6 @@ struct NucleiSpecraTask {
   Configurable<float> cfgCutEta{"cfgCutEta", 0.8f, "Eta range for tracks"};
   Configurable<float> nsigmacutLow{"nsigmacutLow", -30.0, "Value of the Nsigma cut"};
   Configurable<float> nsigmacutHigh{"nsigmacutHigh", +3., "Value of the Nsigma cut"};
-
 
   Filter collisionFilter = nabs(aod::collision::posZ) < cfgCutVertex;
   Filter trackFilter = (nabs(aod::track::eta) < cfgCutEta) && (aod::track::isGlobalTrack == (uint8_t) true);
@@ -82,13 +81,13 @@ struct NucleiSpecraTask {
     for (auto track : tracks) { // start loop over tracks
 
       TLorentzVector cutVector{};
-      cutVector.SetPtEtaPhiM(track.pt()*2.0, track.eta(), track.phi(), constants::physics::MassHelium3);
+      cutVector.SetPtEtaPhiM(track.pt() * 2.0, track.eta(), track.phi(), constants::physics::MassHelium3);
       if (cutVector.Rapidity() < yMin + yBeam || cutVector.Rapidity() > yMax + yBeam) {
         continue;
       }
       //
       // fill QA histograms
-      //  
+      //
       spectra.fill(HIST("fTPCsignal"), track.tpcInnerParam(), track.tpcSignal());
       spectra.fill(HIST("fTPCcounts"), track.tpcInnerParam(), track.tpcNSigmaHe());
       //
