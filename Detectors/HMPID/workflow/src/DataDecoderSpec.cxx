3198d69a99f6c1f36a95abe39bf9079d650a3bb4
Antonio Franco <antonio.franco@cern.ch>
2021-01-25 14:48:05 +0100

    8

--- Detectors/HMPID/workflow/src/DataDecoderSpec.cxx
+++ Detectors/HMPID/workflow/src/DataDecoderSpec.cxx
@@ -155,9 +155,10 @@ void DataDecoderTask::decodeTF(framework::ProcessingContext& pc)
 
   for (auto it = parser.begin(), end = parser.end(); it != end; ++it) {
     uint32_t *theBuffer = (uint32_t *)it.raw();
+    std::cout << "Decode parser loop :"<< it.size() << " , " << it.offset();
     mDeco->setUpStream(theBuffer, it.size()+it.offset());
     mDeco->decodePageFast(&theBuffer);
-    std::cout << "Decode parser loop ..." ;
+
   }
   return;
 }
