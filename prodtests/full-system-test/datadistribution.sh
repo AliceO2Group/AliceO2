5db8f8610ec2e9866761c64ec52739492e089dd1
David Rohr <drohr@jwdt.org>
2021-01-27 12:06:20 +0100

    Fix double parameters in datadistribution script of full system test

--- prodtests/full-system-test/datadistribution.sh
+++ prodtests/full-system-test/datadistribution.sh
@@ -14,7 +14,7 @@ export TFRATE=$(awk "BEGIN {printf \"%.6f\",1/$TFDELAY}")
 
 ARGS_ALL="--session default --severity $SEVERITY --shm-segment-size $SHMSIZE --no-cleanup"
 
-StfBuilder --id stfb --session default --transport shmem \
+StfBuilder --id stfb --transport shmem \
   --dpl-channel-name dpl-chan --channel-config "name=dpl-chan,type=push,method=bind,address=ipc://@stfb-to-dpl,transport=shmem,rateLogging=1" \
   --data-source-dir ${TF_DIR} \
   --data-source-rate=0.01 \
@@ -23,5 +23,4 @@ StfBuilder --id stfb --session default --transport shmem \
   --data-source-headersize=1024 \
   --data-source-enable \
   --data-source-preread 5 \
-  --severity debug \
   ${ARGS_ALL}
