fc5b05eff28f59690072368a4f8c1038592004de
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2021-01-27 10:15:01 +0100

    DPL: introduce a MERGE_CONFIG phase
    * This way we guarantee that the workflow dumped is identical to the
    one actually run, also for what concerns reworked options.

--- Framework/Core/include/Framework/DriverInfo.h
+++ Framework/Core/include/Framework/DriverInfo.h
@@ -33,6 +33,8 @@ class ConfigContext;
 /// Possible states for the DPL Driver application
 ///
 /// INIT => Initial state where global initialization should happen
+/// MERGE_CONFIGS => Invoked to rework the configuration so that common
+///                  options are homogeneous between different invokations.
 /// SCHEDULE => Invoked whenever the topology or the resources associated
 ///             to it change.
 /// RUNNING => At least one device is running and processing data.
@@ -69,6 +71,7 @@ enum struct DriverState {
   MATERIALISE_WORKFLOW,
   IMPORT_CURRENT_WORKFLOW,
   DO_CHILD,
+  MERGE_CONFIGS,
   LAST
 };
 
