722f831fc06c4a3d3ab5b4025df79ee7adaed811
Chiara Zampolli <Chiara.Zampolli@cern.ch>
2021-02-08 23:16:31 +0100

    Aggregator
    changes to aggregator
    
    Fix logic in case of multiple slots
    
    Fix in OutputSpec
    
    Tool to run EPN + aggregator simulation
    
    Remove aggregator since raw-proxy can be used
    
    forgotten from the CMakeLists
    
    clang-format
    
    more info

---
+++ Detectors/Calibration/testMacros/runEPNsimulation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+iEPN=0
+xpos_start=100
+while [[ $iEPN -lt $1 ]]
+do
+    echo $iEPN
+    xpos=$((xpos_start+1000*$iEPN))
+    xterm -hold -geometry 150x41+$xpos+300 -e bash -c "o2-calibration-data-generator-workflow --lanes 7 --gen-slot $iEPN --gen-norm 3 --mean-latency 100000 --max-timeframes 500 -b | o2-dpl-output-proxy --channel-config "name=downstream,method=connect,address=tcp://localhost:30453,type=push,transport=zeromq" --dataspec downstream:TOF/CALIBDATA -b ; exec bash" &
+    ((iEPN = iEPN +1 ))
+done
