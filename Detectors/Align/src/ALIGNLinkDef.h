d91576bb6c14fe4316b111fb39b782df72b3c32b
Michael Lettrich <michael.lettrich@cern.ch>
2021-02-09 19:13:41 +0100

    [ALIGN] integrate into CMake

--- Detectors/Align/src/ALIGNLinkDef.h
+++ Detectors/Align/src/ALIGNLinkDef.h
@@ -1,32 +1,32 @@
-#ifdef __CINT__
+#ifdef __CLING__
 
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
 
 //#pragma link C++ class AliAlgAux;
-#pragma link C++ class o2::align::AliAlgDet+;
-#pragma link C++ class o2::align::AliAlgDetITS+;
-#pragma link C++ class o2::align::AliAlgDetTOF+;
-#pragma link C++ class o2::align::AliAlgDetTPC+;
-#pragma link C++ class o2::align::AliAlgDetTRD+;
-#pragma link C++ class o2::align::AliAlgDetHMPID+;
-#pragma link C++ class o2::align::AliAlgMPRecord+;
-#pragma link C++ class o2::align::AliAlgPoint+;
-#pragma link C++ class o2::align::AliAlgSens+;
-#pragma link C++ class o2::align::AliAlgSensITS+;
-#pragma link C++ class o2::align::AliAlgSensTOF+;
-#pragma link C++ class o2::align::AliAlgSensTPC+;
-#pragma link C++ class o2::align::AliAlgSensTRD+;
-#pragma link C++ class o2::align::AliAlgSensHMPID+;
-#pragma link C++ class o2::align::AliAlgSteer+;
-#pragma link C++ class o2::align::AliAlgTrack+;
-#pragma link C++ class o2::align::AliAlgVol+;
-#pragma link C++ class o2::align::AliAlgVtx+;
-#pragma link C++ class o2::align::AliAlgRes+;
-#pragma link C++ class o2::align::AliAlgResFast+;
-#pragma link C++ class o2::align::AliAlgConstraint+;
-#pragma link C++ class o2::align::AliAlgDOFStat+;
+#pragma link C++ class o2::align::AliAlgDet + ;
+#pragma link C++ class o2::align::AliAlgDetITS + ;
+#pragma link C++ class o2::align::AliAlgDetTOF + ;
+#pragma link C++ class o2::align::AliAlgDetTPC + ;
+#pragma link C++ class o2::align::AliAlgDetTRD + ;
+#pragma link C++ class o2::align::AliAlgDetHMPID + ;
+#pragma link C++ class o2::align::AliAlgMPRecord + ;
+#pragma link C++ class o2::align::AliAlgPoint + ;
+#pragma link C++ class o2::align::AliAlgSens + ;
+#pragma link C++ class o2::align::AliAlgSensITS + ;
+#pragma link C++ class o2::align::AliAlgSensTOF + ;
+#pragma link C++ class o2::align::AliAlgSensTPC + ;
+#pragma link C++ class o2::align::AliAlgSensTRD + ;
+#pragma link C++ class o2::align::AliAlgSensHMPID + ;
+#pragma link C++ class o2::align::AliAlgSteer + ;
+#pragma link C++ class o2::align::AliAlgTrack + ;
+#pragma link C++ class o2::align::AliAlgVol + ;
+#pragma link C++ class o2::align::AliAlgVtx + ;
+#pragma link C++ class o2::align::AliAlgRes + ;
+#pragma link C++ class o2::align::AliAlgResFast + ;
+#pragma link C++ class o2::align::AliAlgConstraint + ;
+#pragma link C++ class o2::align::AliAlgDOFStat + ;
 #pragma link C++ class o2::align::AliAlgAux;
 
 #endif
