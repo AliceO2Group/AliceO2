4114e65de028a307d60d0b5331d5d0e4e09b3861
Michael Lettrich <michael.lettrich@cern.ch>
2021-02-10 11:28:25 +0100

    [ALIGN] port AliAlgDOFStat

--- Detectors/Align/src/AlignLinkDef.h
+++ Detectors/Align/src/AlignLinkDef.h
@@ -26,7 +26,7 @@
 //#pragma link C++ class o2::align::AliAlgRes + ;
 //#pragma link C++ class o2::align::AliAlgResFast + ;
 //#pragma link C++ class o2::align::AliAlgConstraint + ;
-//#pragma link C++ class o2::align::AliAlgDOFStat + ;
+#pragma link C++ class o2::align::AliAlgDOFStat + ;
 #pragma link C++ class o2::align::AliAlgAux;
 
 #endif
