0ce407d7e1b51a0b9ce08e4fc88bd6c55413a4d1
swenzel <sandro.wenzel@cern.ch>
2021-02-12 12:46:41 +0100

    Full system test as a DAG pipeline
    This demonstrates how the full system test can be built
    and run as a DAG pipeline.
    
    We separate description of the workflow and runtime.
    
    One of the benefits coming with this are
    automatic parallel scheduling of QED + normal simulation and
    parallel RAW data creation (as long as this is possible in memory).
    
    Makes use of a recent development prototyped in O2DPG
    https://github.com/AliceO2Group/O2DPG/blob/master/MC/doc/WorkflowRunner.md
    
    The commit should be seen as a first demonstrator rather than a full conversion.

---
+++ prodtests/full-system-test/create_full_system_pipeline.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+#
+# A script producing a consistent full-system-test workflow.
+# Python used for convenient json handling but this is completely up to the workflow writer.
+# 
+from os import environ
+import json
+
+workflow={}
+workflow['stages'] = []
+
+taskcounter=0
+def Task(name='', needs=[], tf=-1, cmd='a', cwd='./', lab=[]):
+    global taskcounter
+    taskcounter = taskcounter + 1
+    t = { 'name': name, 'cmd': cmd, 'needs': needs, 'resources': { 'cpu': -1 , 'mem': -1 }, 'timeframe' : tf, 'labels' : lab, 'cwd' : cwd }
+    workflow['stages'].append(t)
+    return t
+
+# ---- qed transport task -------
+QED=Task(name='qedsim', lab=["QED", "SIM"], cwd='qed')
+QED['cmd']='o2-sim --seed $O2SIMSEED -j $NJOBS -n$NEventsQED -m PIPE ITS MFT FT0 FV0 FDD -g extgen --configKeyValues \"GeneratorExternal.fileName=$O2_ROOT/share/Generators/external/QEDLoader.C;QEDGenParam.yMin=-7;QEDGenParam.yMax=7;QEDGenParam.ptMin=0.001;QEDGenParam.ptMax=1.;Diamond.width[2]=6.\"'
+
+QED2HAD=Task(name='qed2had', lab=["QED", "SIM"], cwd='qed', needs=["qedsim"])
+QED2HAD['cmd']='PbPbXSec="8." ; awk "BEGIN {printf \\"%.2f\\",`grep xSectionQED qedgenparam.ini | cut -d\'=\' -f 2`/$PbPbXSec}" > qed2had.log'
+#echo "Obtained ratio of QED to hadronic x-sections = $QED2HAD" >> qedsim.log
+
+# --- signal sim and digitization ----
+SIM=Task(name="sim", lab=["SIM"])
+SIM['cmd']='o2-sim --seed $O2SIMSEED -n $NEvents --skipModules ZDC --configKeyValues "Diamond.width[2]=6." -g pythia8hi -j $NJOBS'
+
+DIGI1=Task(name="digi", lab=["DIGI"], needs=["sim", "qed2had"])
+DIGI1['cmd']='QED2HAD=`cat qed/qed2had.log`; echo ${QED2HAD}; o2-sim-digitizer-workflow -n $NEvents --simPrefixQED qed/o2sim --qed-x-section-ratio ${QED2HAD} ${NOMCLABELS} --firstOrbit 0 --firstBC 0 --skipDet TRD --tpc-lanes $((NJOBS < 36 ? NJOBS : 36)) --shm-segment-size $SHMSIZE ${GLOBALDPLOPT}'
+
+# the dependency on digi is because of collisioncontext
+DIGI2=Task(name='digiTRD', lab=["DIGI"], needs=["sim", "digi"])
+DIGI2['cmd']='o2-sim-digitizer-workflow -n $NEvents ${NOMCLABELS} --firstOrbit 0 --firstBC 0 --onlyDet TRD --shm-segment-size $SHMSIZE ${GLOBALDPLOPT} --incontext collisioncontext.root --configKeyValues "TRDSimParams.digithreads=${NJOBS}"'
+
+allrawtasknames=[]
+def RAWTask(name, command):
+    allrawtasknames.append(name)
+    return Task(name=name, cmd=command, lab=["RAW"], needs=["digi"])
+
+ITSRAW=RAWTask('itsraw', 'o2-its-digi2raw --file-for link --configKeyValues \"HBFUtils.nHBFPerTF=128;HBFUtils.orbitFirst=0\" -o raw/ITS')
+MFTRAW=RAWTask('mftraw', 'o2-mft-digi2raw --file-for link --configKeyValues \"HBFUtils.nHBFPerTF=128;HBFUtils.orbitFirst=0\" -o raw/MFT')
+FT0RAW=RAWTask('ft0raw', 'o2-ft0-digi2raw --file-per-link --configKeyValues \"HBFUtils.nHBFPerTF=128;HBFUtils.orbitFirst=0\" -o raw/FT0')
+FV0RAW=RAWTask('fv0raw', 'o2-fv0-digi2raw --file-per-link --configKeyValues \"HBFUtils.nHBFPerTF=128;HBFUtils.orbitFirst=0\" -o raw/FV0')
+FDDRAW=RAWTask('fddraw', 'o2-fdd-digit2raw --file-per-link --configKeyValues \"HBFUtils.nHBFPerTF=128;HBFUtils.orbitFirst=0\" -o raw/FDD')
+TPCRAW=RAWTask('tpcraw', 'o2-tpc-digits-to-rawzs  --file-for link --configKeyValues \"HBFUtils.nHBFPerTF=128;HBFUtils.orbitFirst=0\" -i tpcdigits.root -o raw/TPC')
+TOFRAW=RAWTask('tofraw', 'o2-tof-reco-workflow ${GLOBALDPLOPT} --tof-raw-file-for link --configKeyValues \"HBFUtils.nHBFPerTF=128;HBFUtils.orbitFirst=0\" --output-type raw --tof-raw-outdir raw/TOF')
+TOFRAW=RAWTask('midraw', 'o2-mid-digits-to-raw-workflow ${GLOBALDPLOPT} --mid-raw-outdir raw/MID --mid-raw-perlink  --configKeyValues \"HBFUtils.nHBFPerTF=128;HBFUtils.orbitFirst=0\"')
+EMCRAW=RAWTask('emcraw', 'o2-emcal-rawcreator --file-for link --configKeyValues \"HBFUtils.nHBFPerTF=128;HBFUtils.orbitFirst=0\" -o raw/EMC')
+PHSRAW=RAWTask('phsraw', 'o2-phos-digi2raw  --file-for link --configKeyValues \"HBFUtils.nHBFPerTF=128;HBFUtils.orbitFirst=0\" -o raw/PHS')
+CPVRAW=RAWTask('cpvraw', 'o2-cpv-digi2raw  --file-for link --configKeyValues \"HBFUtils.nHBFPerTF=128;HBFUtils.orbitFirst=0\" -o raw/CPV')
+
+# make configuration -> this depends on all previous raws
+Task('rawAllConfig', cmd='cat raw/*/*.cfg > rawAll.cfg', needs=allrawtasknames, lab=["RAW"])
+
+# now emit all possible versions of the dpl workflow (WITHGPU, NOGPU, AYNC)
+RECO_ENV={ "WITHGPU": { "CREATECTFDICT":"0",
+                        "GPUTYPE":"CUDA",
+                        "GPUMEMSIZE":"6000000000",
+                        "HOSTMEMSIZE":"1000000000",
+                        "SYNCMODE":"1",
+                        "CTFINPUT":"0",
+                        "SAVECTF":"0" },
+      "ASYNC": {
+          "CREATECTFDICT":"0",
+          "GPUTYPE":"CPU",
+          "SYNCMODE":"0",
+          "HOSTMEMSIZE":"$TPCTRACKERSCRATCHMEMORY",
+          "CTFINPUT":"1",
+          "SAVECTF":"0"
+      },
+      "NOGPU": {
+          "CREATECTFDICT":"1",
+          "GPUTYPE":"CPU",
+          "SYNCMODE":"0",
+          "HOSTMEMSIZE":"$TPCTRACKERSCRATCHMEMORY",
+          "CTFINPUT":"0",
+          "SAVECTF":"1"
+      }
+}
+
+for stage in [ "NOGPU", "WITHGPU", "ASYNC" ]:
+    t=Task(name='reco_' + stage, needs=['rawAllConfig'], lab=["RECO"])
+    t['env']=RECO_ENV[stage]
+    precommand=""
+    if stage=="NOGPU":
+        precommand="rm -f ctf_dictionary.root;"
+    t['cmd']=precommand + "$O2_ROOT/prodtests/full-system-test/dpl-workflow.sh"
+    
+def trimString(cmd):
+  return ' '.join(cmd.split())
+
+# insert taskwrapper stuff
+for s in workflow['stages']:
+  s['cmd']='. ${O2_ROOT}/share/scripts/jobutils.sh; taskwrapper ' + s['name']+'.log \'' + s['cmd'] + '\''
+
+# remove whitespaces etc
+for s in workflow['stages']:
+  s['cmd']=trimString(s['cmd'])
+
+# write workflow to json
+workflowfile='workflow.json'
+with open(workflowfile, 'w') as outfile:
+    json.dump(workflow, outfile, indent=2)
+
+exit (0)
---
+++ prodtests/full_system_test_pipeline.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+#
+# A workflow performing a full system test:
+# - simulation of digits
+# - creation of raw data
+# - reconstruction of raw data
+#
+# Note that this might require a production server to run.
+#
+# This script can use additional binary objects which can be optionally provided:
+# - matbud.root + ITSdictionary.bin
+#
+# authors: D. Rohr / S. Wenzel
+
+# include jobutils, which notably brings
+# --> the taskwrapper as a simple control and monitoring tool
+#     (look inside the jobutils.sh file for documentation)
+# --> utilities to query CPU count
+. ${O2_ROOT}/share/scripts/jobutils.sh
+
+export NEvents=${NEvents:-10} #550 for full TF (the number of PbPb events)
+export NEventsQED=${NEventsQED:-1000} #35000 for full TF
+export NCPUS=$(getNumberOfPhysicalCPUCores)
+echo "Found ${NCPUS} physical CPU cores"
+export NJOBS=${NJOBS:-"${NCPUS}"}
+export SHMSIZE=${SHMSIZE:-8000000000} # Size of shared memory for messages (use 128 GB for 550 event full TF)
+export TPCTRACKERSCRATCHMEMORY=${SHMSIZE:-4000000000} # Size of memory allocated by TPC tracker. (Use 24 GB for 550 event full TF)
+export ENABLE_GPU_TEST=${ENABLE_GPU_TEST:-0} # Run the full system test also on the GPU
+export NTIMEFRAMES=${NTIMEFRAMES:-1} # Number of time frames to process
+export TFDELAY=${TFDELAY:-100} # Delay in seconds between publishing time frames
+export NOMCLABELS="--disable-mc"
+export O2SIMSEED=${O2SIMSEED:--1}
+
+# allow skipping
+JOBUTILS_SKIPDONE=ON
+# enable memory monitoring (independent on whether DPL or not)
+JOBUTILS_MONITORMEM=ON
+# CPU monitoring JOBUTILS_MONITORCPU=ON
+
+# prepare some metrics file for the monitoring system
+METRICFILE=metrics.dat
+CONFIG="full_system_test_N${NEvents}"
+HOST=`hostname`
+
+# include header information such as tested alidist tag and O2 tag
+TAG="conf=${CONFIG},host=${HOST}${ALIDISTCOMMIT:+,alidist=$ALIDISTCOMMIT}${O2COMMIT:+,o2=$O2COMMIT}"
+echo "versions,${TAG} alidist=\"${ALIDISTCOMMIT}\",O2=\"${O2COMMIT}\" " > ${METRICFILE}
+
+ulimit -n 4096 # Make sure we can open sufficiently many files
+
+export GLOBALDPLOPT="-b" #  --monitoring-backend no-op:// is currently removed due to https://alice.its.cern.ch/jira/browse/O2-1887
+
+mkdir -p raw
+
+# create the full workflow as json
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+${DIR}/full-system-test/create_full_system_pipeline.py
+
+# create a visualization of the pipeline-workflow (in workflow.gv.pdf) and print the list of tasks
+${O2DPG_ROOT}/MC/bin/o2_dpg_workflow_runner.py -f workflow.json --visualize-workflow --list-tasks
+
+# run the workflow (not constraining in parallelism) until RAW creation is finished
+${O2DPG_ROOT}/MC/bin/o2_dpg_workflow_runner.py -f workflow.json --target-labels RAW
+
+# We run the workflow in both CPU-only and With-GPU mode
+STAGES="NOGPU"
+if [ $ENABLE_GPU_TEST != "0" ]; then
+  STAGES+=" WITHGPU"
+fi
+STAGES+=" ASYNC"
+for STAGE in $STAGES; do
+  logfile=reco_${STAGE}.log
+  export SHMSIZE
+  export NTIMEFRAMES
+  export TFDELAY
+  export GLOBALDPLOPT
+
+  # run each of the reco but in strictly serial mode
+  ${O2DPG_ROOT}/MC/bin/o2_dpg_workflow_runner.py -f workflow.json -tt reco_${STAGE} -jmax 1
+
+  # --- record interesting metrics to monitor ----
+  # boolean flag indicating if workflow completed successfully at all
+  RC=$?
+  SUCCESS=0
+  [ -f "${logfile}_done" ] && [ "$RC" = 0 ] && SUCCESS=1
+  echo "success_${STAGE},${TAG} value=${SUCCESS}" >> ${METRICFILE}
+
+  if [ "${SUCCESS}" = "1" ]; then
+    # runtime
+    walltime=`grep "#walltime" ${logfile}_time | awk '//{print $2}'`
+    echo "walltime_${STAGE},${TAG} value=${walltime}" >> ${METRICFILE}
+
+    # GPU reconstruction (also in CPU version) processing time
+    gpurecotime=`grep "tpc-tracker" reco_NOGPU.log | grep -e "Total Wall Time:" | awk '//{printf "%f", $6/1000000}'`
+    echo "gpurecotime_${STAGE},${TAG} value=${gpurecotime}" >> ${METRICFILE}
+
+    # memory
+    maxmem=`awk '/PROCESS MAX MEM/{print $5}' ${logfile}`  # in MB
+    avgmem=`awk '/PROCESS AVG MEM/{print $5}' ${logfile}`  # in MB
+    echo "maxmem_${STAGE},${TAG} value=${maxmem}" >> ${METRICFILE}
+    echo "avgmem_${STAGE},${TAG} value=${avgmem}" >> ${METRICFILE}
+
+    # some physics quantities
+    tpctracks=`grep "tpc-tracker" ${logfile} | grep -e "found.*track" | awk '//{print $4}'`
+    echo "tpctracks_${STAGE},${TAG} value=${tpctracks}" >> ${METRICFILE}
+    tpcclusters=`grep -e "Event has.*TPC Clusters" ${logfile} | awk '//{print $5}'`
+    echo "tpcclusters_${STAGE},${TAG} value=${tpcclusters}" >> ${METRICFILE}
+  fi
+done
