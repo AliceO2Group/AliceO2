938d6b878cfc2fec46ce671b1220f4f6de4376af
Matteo Concas <matteo.concas@cern.ch>
2021-02-15 10:18:22 +0100

    Use functor to check if we trick Clang13

--- Common/MathUtils/include/MathUtils/SMatrixGPU.h
+++ Common/MathUtils/include/MathUtils/SMatrixGPU.h
@@ -259,6 +259,14 @@ struct meta_matrix_dot<0> {
 
 namespace row_offsets_utils
 {
+template <unsigned int D>
+struct proxy_offset {
+  static constexpr int off0(int i) { return i == 0 ? 0 : off0(i - 1) + i; }
+  static constexpr int off2(int i, int j) { return j < i ? off0(i) + j : off0(j) + i; }
+  static constexpr int off1(int i) { return off2(i / D, i % D); }
+  int operator()(int i) const { return off1(i); }
+};
+
 template <int...>
 struct indices {
 };
@@ -352,13 +360,10 @@ class MatRepSymGPU
     kSize = D * (D + 1) / 2 // rows*columns
   };
 
-  static constexpr int off0(int i) { return i == 0 ? 0 : off0(i - 1) + i; }
-  static constexpr int off2(int i, int j) { return j < i ? off0(i) + j : off0(j) + i; }
-  static constexpr int off1(int i) { return off2(i / D, i % D); }
-
   static GPUdi() int off(int i)
   {
-    static constexpr auto v = row_offsets_utils::make<D * D>(off1);
+    row_offsets_utils::proxy_offset<D> proxy;
+    static constexpr auto v = row_offsets_utils::make<D * D>(proxy);
     return v[i];
   }
 
