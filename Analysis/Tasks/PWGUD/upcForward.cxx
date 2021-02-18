a3323b2360798a53556313ef70c791753d78d38a
amrit gautam <agautam@localhost.localdomain>
2021-02-18 13:16:33 -0500

    used clang-format to check the code format

--- Analysis/Tasks/PWGUD/upcForward.cxx
+++ Analysis/Tasks/PWGUD/upcForward.cxx
@@ -8,18 +8,24 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-#include "Framework/runDataProcessing.h"
-#include "Framework/AnalysisTask.h"
-#include "Framework/AnalysisDataModel.h"
+#include
+"Framework/runDataProcessing.h"
+#include
+  "Framework/AnalysisTask.h"
+#include
+  "Framework/AnalysisDataModel.h"
 
-#include "AnalysisDataModel/TrackSelectionTables.h"
-#include "AnalysisDataModel/EventSelection.h"
+#include
+  "AnalysisDataModel/TrackSelectionTables.h"
+#include
+  "AnalysisDataModel/EventSelection.h"
 
 #include <TH1D.h>
 #include <TH2D.h>
-#include "TLorentzVector.h"
-#include "AnalysisCore/TriggerAliases.h"
-using namespace std;
+#include
+  "TLorentzVector.h"
+#include
+  "AnalysisCore/TriggerAliases.h" using namespace std;
 using namespace o2;
 using namespace o2::framework;
 using namespace o2::framework::expressions;
@@ -27,12 +33,24 @@ using namespace o2::framework::expressions;
 #define mmuon 0.1057
 using MyEvents = soa::Join<aod::Collisions, aod::EvSels>;
 struct UPCForward {
-  OutputObj<TH1D> hMass{TH1D("hMass", ";#it{m_{#mu#mu}}, GeV/c^{2};", 500, 0, 10)};
-  OutputObj<TH1D> hPt{TH1D("hPt", ";#it{p_{T}}, GeV/c;", 500, 0., 1.)};
-  OutputObj<TH1D>hRap{TH1D("hRap", ";#it{y},..;", 500, -10., 10.)};
-  OutputObj<TH1D>hCharge{TH1D("hCharge", ";#it{charge},..;", 500, -10., 10.)};
-  OutputObj<TH1D>hTriggerChecker{TH1D("hTriggerChecker", ";#it{Trigger},..;", 30, 0., 30.)};
-  OutputObj<TH1D>hSelectionCounter{TH1D("hSelectionCounter", ";#it{Selection},..;", 30, 0., 30.)};
+  OutputObj<TH1D> hMass{TH1D(
+    "hMass",
+    ";#it{m_{#mu#mu}}, GeV/c^{2};", 500, 0, 10)};
+  OutputObj<TH1D> hPt{TH1D(
+    "hPt",
+    ";#it{p_{T}}, GeV/c;", 500, 0., 1.)};
+  OutputObj<TH1D> hRap{TH1D(
+    "hRap",
+    ";#it{y},..;", 500, -10., 10.)};
+  OutputObj<TH1D> hCharge{TH1D(
+    "hCharge",
+    ";#it{charge},..;", 500, -10., 10.)};
+  OutputObj<TH1D> hTriggerChecker{TH1D(
+    "hTriggerChecker",
+    ";#it{Trigger},..;", 30, 0., 30.)};
+  OutputObj<TH1D> hSelectionCounter{TH1D(
+    "hSelectionCounter",
+    ";#it{Selection},..;", 30, 0., 30.)};
 
   //Partition<aod::Muons> posMuon = o2::aod::muoncluster::charge> 0;
   //Partition<aod::Tracks> negMuon = o2::aod::muoncluster::charge< 0;
@@ -47,14 +65,15 @@ struct UPCForward {
 
   //void process(MyEvents::iterator const& col, aod::Muons const& tracksMuon, aod::BCs::iterator const& bc)//, aod::MuonCluster const&muoncluster)
   void process(aod::BC const& bc, aod::Muons const& tracksMuon)
-    //, soa::Filtered<soa::Join<aod::Tracks, aod::TracksExtra, aod::TrackSelection>> const& tracks)
+  //, soa::Filtered<soa::Join<aod::Tracks, aod::TracksExtra, aod::TrackSelection>> const& tracks)
   {
-    int iSelectionCounter=0;
+    int iSelectionCounter = 0;
     //bool checkV0 = col.bbV0A() || col.bbV0C() || col.bgV0A() || col.bgV0C(); //evgeny suggested that v0c will be fired by muon so
     hSelectionCounter->Fill(iSelectionCounter);
     iSelectionCounter++;
-    hSelectionCounter->GetXaxis()->SetBinLabel(iSelectionCounter,"NoSelection");
-/*    bool checkV0 = col.bbV0A() || col.bbV0C() || col.bgV0A() || col.bgV0C();
+    hSelectionCounter->GetXaxis()->SetBinLabel(iSelectionCounter,
+                                               "NoSelection");
+    /*    bool checkV0 = col.bbV0A() || col.bbV0C() || col.bgV0A() || col.bgV0C();
     if (checkV0) {
       return;
     }
@@ -71,12 +90,12 @@ struct UPCForward {
     //checking available triggers
     */
 
-  /*  for(int i; i<10;i++){
+    /*  for(int i; i<10;i++){
      cout <<"Trigger aliases are" <<col.alias()[i]<<endl;
    }*/
 
     //std::cout<<"checking triggers"<< col.alias()[kMUP11]<<"n";
-  /*  if (col.alias()[kMUP10]) {
+    /*  if (col.alias()[kMUP10]) {
       //std::cout<<"there is no CMUP10 triggered events";
       hTriggerChecker->Fill(0);
       hTriggerChecker->GetXaxis()->SetBinLabel(1,"CMUP10");
@@ -150,87 +169,80 @@ struct UPCForward {
     bool ispositive = kFALSE;
     bool isnegative = kFALSE;
 
-
     uint64_t classIndexMUP11 = 54; // 246392
     bool isMUP11fired = bc.triggerMask() & (1ull << classIndexMUP11);
     if (isMUP11fired) {
-       LOGF(info, "mup11 fired");
-       hTriggerChecker->Fill(8);
-       hTriggerChecker->GetXaxis()->SetBinLabel(9,"CMUP11_New");
-       hSelectionCounter->Fill(iSelectionCounter);
-       iSelectionCounter++;
-       hSelectionCounter->GetXaxis()->SetBinLabel(iSelectionCounter,"CMup11Trigger");
+      LOGF(info,
+           "mup11 fired");
+      hTriggerChecker->Fill(8);
+      hTriggerChecker->GetXaxis()->SetBinLabel(9,
+                                               "CMUP11_New");
+      hSelectionCounter->Fill(iSelectionCounter);
+      iSelectionCounter++;
+      hSelectionCounter->GetXaxis()->SetBinLabel(iSelectionCounter,
+                                                 "CMup11Trigger");
 
+      for (auto& muon : tracksMuon) {
+        //cout << aod::MuonCluster::TrackId<muon><<endl;
+        //cout<<"test of the iterator"<<muon.phi()<<endl;
+        //cout<<"id"<<muon.bcId()<<endl;
+        //auto muon1 = muon.begin()
+        //auto muon2 = muon1+1
 
-
-       for (auto &muon : tracksMuon){
-    //cout << aod::MuonCluster::TrackId<muon><<endl;
-    //cout<<"test of the iterator"<<muon.phi()<<endl;
-    //cout<<"id"<<muon.bcId()<<endl;
-    //auto muon1 = muon.begin()
-    //auto muon2 = muon1+1
-
-//commenting this out now to see if I we do not need these
-    /*if (muon.bcId() != col.bcId()) {
+        //commenting this out now to see if I we do not need these
+        /*if (muon.bcId() != col.bcId()) {
      continue;
    }*/
-//this code below is suggested by evgeny. this code is now hardcoded for run 246392 as we are not sure if trigger id is same for all the runs
+        //this code below is suggested by evgeny. this code is now hardcoded for run 246392 as we are not sure if trigger id is same for all the runs
 
+        hCharge->Fill(muon.charge());
 
+        muontracknumber++;
 
-      hCharge->Fill(muon.charge());
+        if (muon.charge() > 0) {
+          p1.SetXYZM(muon.px(), muon.py(), muon.pz(), mmuon);
+          ispositive = kTRUE;
+          //p1.SetXYZM(posMuon.px(), posMuon.py(), posMuon.pz(), mmuon);
+        }
 
-
-
-      muontracknumber++;
-
-
-      if (muon.charge()>0){
-      p1.SetXYZM(muon.px(), muon.py(), muon.pz(), mmuon);
-      ispositive =kTRUE;
-      //p1.SetXYZM(posMuon.px(), posMuon.py(), posMuon.pz(), mmuon);
-      }
-
-      if (muon.charge()<0){
-      p2.SetXYZM(muon.px(), muon.py(), muon.pz(), mmuon);
-      isnegative = kTRUE;
-      //p2.SetXYZM(negMuon.px(), negMuon.py(), negMuon.pz(), mmuon);
-      }
-    /*  if (posMuon.Charge() * negMuon.Charge()<=0){
+        if (muon.charge() < 0) {
+          p2.SetXYZM(muon.px(), muon.py(), muon.pz(), mmuon);
+          isnegative = kTRUE;
+          //p2.SetXYZM(negMuon.px(), negMuon.py(), negMuon.pz(), mmuon);
+        }
+        /*  if (posMuon.Charge() * negMuon.Charge()<=0){
         cout<< "there are no opposite charge pairs"
         continue;
       }*/
 
+        //cout<<"muontracknumber" <<muontracknumber<<endl;
+        //std::cout<<"there are some CMUP11 triggered events \n";
+      }
+      if (muontracknumber != 2) {
+        return;
+      }
+      hSelectionCounter->Fill(iSelectionCounter);
+      iSelectionCounter++;
+      hSelectionCounter->GetXaxis()->SetBinLabel(iSelectionCounter,
+                                                 "twotracks");
 
+      if (!ispositive || !isnegative) {
+        return;
+      }
 
-
-      //cout<<"muontracknumber" <<muontracknumber<<endl;
-      //std::cout<<"there are some CMUP11 triggered events \n";
-    }
-    if (muontracknumber!=2){
-      return;
-
-    }
-    hSelectionCounter->Fill(iSelectionCounter);
-    iSelectionCounter++;
-    hSelectionCounter->GetXaxis()->SetBinLabel(iSelectionCounter,"twotracks");
-
-    if (!ispositive || !isnegative){
-      return;
-    }
-
-    hSelectionCounter->Fill(iSelectionCounter);
-    iSelectionCounter++;
-    hSelectionCounter->GetXaxis()->SetBinLabel(iSelectionCounter,"oppositecharge");
-    p = p1 + p2;
-    //cout<< "printing values"<< p.M()<< p1.M() <<p2.M()<<endl;
-    hPt->Fill(p.Pt());
-    hRap->Fill(p.Rapidity());
-    hMass->Fill(p.M());
-  /*  if (tracks.size() != 2) {
+      hSelectionCounter->Fill(iSelectionCounter);
+      iSelectionCounter++;
+      hSelectionCounter->GetXaxis()->SetBinLabel(iSelectionCounter,
+                                                 "oppositecharge");
+      p = p1 + p2;
+      //cout<< "printing values"<< p.M()<< p1.M() <<p2.M()<<endl;
+      hPt->Fill(p.Pt());
+      hRap->Fill(p.Rapidity());
+      hMass->Fill(p.M());
+      /*  if (tracks.size() != 2) {
       return;
     }*/
-  /*  auto first = tracks.begin();
+      /*  auto first = tracks.begin();
     auto second = first + 1;
     if (first.charge() * second.charge() >= 0) {
       return;
@@ -252,14 +264,15 @@ struct UPCForward {
     //hSignalTPC1vsSignalTPC2->Fill(signalTPC1, signalTPC2);
     //if ((p.Pt() < 0.1) && (signalTPC1 + signalTPC2 < 140.)) {
       hMass->Fill(p.M());*/
-  //  }
+      //  }
 
-    }// end of cmup trigger
-  } //end of process
+    } // end of cmup trigger
+  }   //end of process
 };
 
 WorkflowSpec defineDataProcessing(ConfigContext const&)
 {
   return WorkflowSpec{
-    adaptAnalysisTask<UPCForward>("upc-forward")};
+    adaptAnalysisTask<UPCForward>(
+      "upc-forward")};
 }
