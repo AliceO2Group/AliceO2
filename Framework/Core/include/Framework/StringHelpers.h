a5e2235e2e229f68d79ec6895b6594ef8d9c3b4a
Mario Krüger <mkrueger@ikf.uni-frankfurt.de>
2021-02-23 09:40:38 +0100

    DPL Analysis: rudimentary support for configurable axes (#5502)
    * DPL Analysis: rudimentary support for configurable axes
    
    * readablity braces
    
    * avoid formatting differences between clang-format 8 and 11
    
    * remove meaningless const in return type
    
    * use auto return types
    
    Co-authored-by: Mario Krüger <mario.kruger@cern.ch>

--- Framework/Core/include/Framework/StringHelpers.h
+++ Framework/Core/include/Framework/StringHelpers.h
@@ -126,23 +126,23 @@ constexpr auto as_chars()
 }
 
 template <int N>
-constexpr const char* const get_str(const char (&str)[N])
+constexpr auto get_str(const char (&str)[N])
 {
   return str;
 }
 
 template <int N>
-constexpr const int get_size(const char (&str)[N])
+constexpr auto get_size(const char (&str)[N])
 {
   return N;
 }
 
-constexpr const char* const get_str(const std::string_view& str)
+constexpr auto get_str(const std::string_view& str)
 {
   return str.data();
 }
 
-constexpr const int get_size(const std::string_view& str)
+constexpr auto get_size(const std::string_view& str)
 {
   return str.size() + 1;
 }
