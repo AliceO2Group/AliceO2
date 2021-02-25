b653204663f58bfa21d344edc8511a289e6d5581
Pietro Cortese <pietro.cortese@cern.ch>
2021-02-25 10:57:11 +0100

    First raw 2 digit

---
+++ Detectors/ZDC/raw/include/ZDCRaw/RawReaderBase.h
@@ -0,0 +1,95 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+//
+//file RawReaderBase.h base class for RAW data reading
+//
+// Artur.Furs
+// afurs@cern.ch
+//
+//Main purpuse is to decode ZDC data blocks and push them to DigitBlockFT0 for process
+
+#ifndef ALICEO2_ZDC_RAWREADERBASE_H_
+#define ALICEO2_ZDC_RAWREADERBASE_H_
+#include <iostream>
+#include <vector>
+#include <map>
+
+#include <Rtypes.h>
+#include <CommonDataFormat/InteractionRecord.h>
+#include "Headers/RAWDataHeader.h"
+#include <Framework/Logger.h>
+#include "DataFormatsZDC/RawEventData.h"
+
+#include <gsl/span>
+namespace o2
+{
+namespace zdc
+{
+class RawReaderBase
+{
+ public:
+  RawReaderBase() = default;
+  ~RawReaderBase() = default;
+
+  //decoding binary data into data blocks
+  template <class DataBlockType>
+  size_t decodeBlocks(const gsl::span<const uint8_t> binaryPayload, std::vector<DataBlockType>& vecDataBlocks)
+  {
+    size_t srcPos = 0;
+    while (srcPos < binaryPayload.size()) { //checking element
+      DataBlockType dataBlock{};
+      dataBlock.decodeBlock(binaryPayload, srcPos);
+      srcPos += dataBlock.mSize;
+      if (dataBlock.isCorrect()) {
+        vecDataBlocks.push_back(dataBlock); //change to in-place construction? TODO
+      } else {
+        LOG(WARNING) << "INCORRECT DATA BLOCK! Byte position: " << srcPos - dataBlock.mSize << " | " << binaryPayload.size() << " | " << dataBlock.mSize;
+        dataBlock.print();
+      }
+    }
+    return srcPos;
+  }
+
+  //processing data blocks into digits
+  template <class DataBlockType>
+  void processBinaryData(gsl::span<const uint8_t> payload, int linkID)
+  {
+    std::vector<DataBlockType> vecDataBlocks;
+    auto srcPos = decodeBlocks(payload, vecDataBlocks);
+
+    for (auto& dataBlock : vecDataBlocks) {
+      auto intRec = dataBlock.getInteractionRecord();
+      auto [digitIter, isNew] = mMapDigits.try_emplace(intRec, intRec);
+      digitIter->second.template process<DataBlockType>(dataBlock, linkID);
+    }
+  }
+  /*
+  void process(int linkID, gsl::span<const uint8_t> payload)
+  {
+    static_cast<RawReader*>(this)->processDigits(linkID,payload);
+  }
+  */
+  //pop digits
+  template <class... DigitType>
+  int getDigits(std::vector<DigitType>&... vecDigit)
+  {
+    int digitCounter = mMapDigits.size();
+    for (auto& digit : mMapDigits) {
+      digit.second.pop(vecDigit...);
+    }
+    mMapDigits.clear();
+    return digitCounter;
+  }
+};
+
+} // namespace zdc
+} // namespace o2
+
+#endif
