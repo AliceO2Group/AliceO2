2f3ad165b91a58e6eff3e3bc7ab800edf1b2da11
shahoian <ruben.shahoyan@cern.ch>
2021-02-28 19:41:56 +0100

    GlobalTrackID sources masks and their manipulations

--- DataFormats/Detectors/Common/src/DetID.cxx
+++ DataFormats/Detectors/Common/src/DetID.cxx
@@ -39,23 +39,27 @@ constexpr int DetID::nDetectors;
 DetID::mask_t DetID::getMask(const std::string_view detList)
 {
   DetID::mask_t mask;
-  std::string ss(detList);
-  auto pos = ss.find(NONE);
-  if (pos != std::string::npos) {
-    ss.replace(pos, NONE.size(), "");
-  } else {
+  std::string ss(detList), sname{};
+  if (ss.find(NONE) != std::string::npos) {
+    return mask;
+  }
+  if (ss.find(ALL) != std::string::npos) {
+    mask.set();
+    return mask;
+  }
+  std::replace(ss.begin(), ss.end(), ' ', ',');
+  std::stringstream sss(ss);
+  while (getline(sss, sname, ',')) {
     for (auto id = DetID::First; id <= DetID::Last; id++) {
-      auto pos = ss.find(DetID::getName(id));
-      if (pos != std::string::npos) {
+      if (sname == getName(id)) {
         mask.set(id);
-        ss.replace(pos, strlen(DetID::getName(id)), "");
+        sname = "";
+        break;
       }
     }
-  }
-  // make sure that no names are left in the input strings
-  if (std::count_if(ss.begin(), ss.end(), [](unsigned char c) { return std::isalnum(c); })) {
-    LOG(ERROR) << "Ill-formed detectors list " << std::string(detList);
-    throw std::runtime_error("Ill-formed detectors list");
+    if (!sname.empty()) {
+      throw std::runtime_error(fmt::format("Wrong entry {:s} in detectors list {:s}", sname, detList));
+    }
   }
   return mask;
 }
@@ -68,14 +72,14 @@ DetID::DetID(const char* name) : mID(nameToID(name, First))
 }
 
 //_______________________________
-std::string DetID::getNames(DetID::mask_t mask)
+std::string DetID::getNames(DetID::mask_t mask, char delimiter)
 {
   // construct from the name
   std::string ns;
   for (auto id = DetID::First; id <= DetID::Last; id++) {
     if (mask[id]) {
       if (!ns.empty()) {
-        ns += ',';
+        ns += delimiter;
       }
       ns += getName(id);
     }
