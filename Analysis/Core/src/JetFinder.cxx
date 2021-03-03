db7f82f9ff7645db6ea82a8628035147ea426683
Timo Wilken <timo.wilken@cern.ch>
2021-03-03 13:17:43 +0100

    DO NOT MERGE Test clang-format auto-PR
    This is a PR with changes that should trip git-clang-format. I want to see if it
    will make a PR to my branch now.

--- Analysis/Core/src/JetFinder.cxx
+++ Analysis/Core/src/JetFinder.cxx
@@ -25,7 +25,7 @@ void JetFinder::setParams()
 
   //selGhosts =fastjet::SelectorRapRange(ghostEtaMin,ghostEtaMax) && fastjet::SelectorPhiRange(phiMin,phiMax);
   //ghostAreaSpec=fastjet::GhostedAreaSpec(selGhosts,ghostRepeatN,ghostArea,gridScatter,ktScatter,ghostktMean);
-  ghostAreaSpec = fastjet::GhostedAreaSpec(ghostEtaMax, ghostRepeatN, ghostArea, gridScatter, ktScatter, ghostktMean); //the first argument is rapidity not pseudorapidity, to be checked
+  ghostAreaSpec = fastjet  :: GhostedAreaSpec( ghostEtaMax , ghostRepeatN, ghostArea, gridScatter, ktScatter, ghostktMean); //the first argument is rapidity not pseudorapidity, to be checked
   jetDef = fastjet::JetDefinition(algorithm, jetR, recombScheme, strategy);
   areaDef = fastjet::AreaDefinition(areaType, ghostAreaSpec);
   selJets = fastjet::SelectorPtRange(jetPtMin, jetPtMax) && fastjet::SelectorEtaRange(jetEtaMin, jetEtaMax) && fastjet::SelectorPhiRange(jetPhiMin, jetPhiMax);
