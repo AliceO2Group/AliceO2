316824e271d3fbaf76c11bfee5b8b109a25869d5
David Rohr <drohr@jwdt.org>
2021-03-04 11:55:56 +0100

    Minor improvement for full system test script

--- prodtests/full-system-test/datadistribution.sh
+++ prodtests/full-system-test/datadistribution.sh
@@ -17,7 +17,7 @@ ARGS_ALL="--session default --severity $SEVERITY --shm-segment-size $SHMSIZE --n
 StfBuilder --id stfb --transport shmem \
   --dpl-channel-name dpl-chan --channel-config "name=dpl-chan,type=push,method=bind,address=ipc://@stfb-to-dpl,transport=shmem,rateLogging=1" \
   --data-source-dir ${TF_DIR} \
-  --data-source-rate=0.01 \
+  --data-source-rate=${TFRATE} \
   --data-source-repeat \
   --data-source-regionsize=${DDSHMSIZE} \
   --data-source-headersize=1024 \
