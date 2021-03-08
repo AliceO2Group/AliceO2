effebb5b6139ba8ebce247a531f73d4d01006c22
Fabio Catalano <fabio.catalano@cern.ch>
2021-03-08 11:13:18 +0100

    PWGHF: Add Dplus selector and MC matching (#5461)
    * Implement Dplus to piKpi selector and add MC matching in task
    
    * Fix for PR 5566
    
    * Address comment
    
    * Fix

--- Analysis/DataModel/include/AnalysisDataModel/HFCandidateSelectionTables.h
+++ Analysis/DataModel/include/AnalysisDataModel/HFCandidateSelectionTables.h
@@ -19,20 +19,20 @@ DECLARE_SOA_COLUMN(IsSelD0, isSelD0, int);
 DECLARE_SOA_COLUMN(IsSelD0bar, isSelD0bar, int);
 } // namespace hf_selcandidate_d0
 DECLARE_SOA_TABLE(HFSelD0Candidate, "AOD", "HFSELD0CAND", hf_selcandidate_d0::IsSelD0, hf_selcandidate_d0::IsSelD0bar);
-} // namespace o2::aod
 
-namespace o2::aod
+namespace hf_selcandidate_dplus
 {
+DECLARE_SOA_COLUMN(IsSelDplusToPiKPi, isSelDplusToPiKPi, int);
+} // namespace hf_selcandidate_dplus
+DECLARE_SOA_TABLE(HFSelDplusToPiKPiCandidate, "AOD", "HFSELDPLUSCAND", hf_selcandidate_dplus::IsSelDplusToPiKPi);
+
 namespace hf_selcandidate_lc
 {
 DECLARE_SOA_COLUMN(IsSelLcpKpi, isSelLcpKpi, int);
 DECLARE_SOA_COLUMN(IsSelLcpiKp, isSelLcpiKp, int);
 } // namespace hf_selcandidate_lc
 DECLARE_SOA_TABLE(HFSelLcCandidate, "AOD", "HFSELLCCAND", hf_selcandidate_lc::IsSelLcpKpi, hf_selcandidate_lc::IsSelLcpiKp);
-} // namespace o2::aod
 
-namespace o2::aod
-{
 namespace hf_selcandidate_jpsi
 {
 DECLARE_SOA_COLUMN(IsSelJpsiToEE, isSelJpsiToEE, int);
