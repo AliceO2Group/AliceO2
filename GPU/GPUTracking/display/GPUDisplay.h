794893c98354bc2a1571b60eb4fcd8dbe39f0102
David Rohr <drohr@jwdt.org>
2021-03-13 19:23:27 +0100

    GPU Display: Add option to change FOV

--- GPU/GPUTracking/display/GPUDisplay.h
+++ GPU/GPUTracking/display/GPUDisplay.h
@@ -294,6 +294,7 @@ class GPUDisplay
   bool mCamLookOrigin = false;
   bool mCamYUp = false;
   int mCameraMode = 0;
+  float mFOV = 45.f;
 
   float mAngleRollOrigin = -1e9;
   float mMaxClusterZ = 0;
