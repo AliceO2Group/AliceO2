4a9997cd00f77ecd028b845ae392c6f1f5a83cd0
Alla Maevskaya <Alla.Maevskaya@cern.ch>
2021-03-15 11:56:54 +0300

    support frame

--- Detectors/FIT/FT0/base/include/FT0Base/Geometry.h
+++ Detectors/FIT/FT0/base/include/FT0Base/Geometry.h
@@ -40,7 +40,7 @@ class Geometry
   static constexpr int Nchannels = 208;                     // number od channels
   static constexpr int NCellsA = 24;                        // number of radiatiors on A side
   static constexpr int NCellsC = 28;                        // number of radiatiors on C side
-  static constexpr float ZdetA = 335;                       // number of radiatiors on A side
+  static constexpr float ZdetA = 335.5;                       // number of radiatiors on A side
   static constexpr float ZdetC = 82;                        // number of radiatiors on C side
   static constexpr float ChannelWidth = 13.02;              // channel width in ps
   static constexpr float ChannelWidthInverse = 0.076804916; // channel width in ps inverse
