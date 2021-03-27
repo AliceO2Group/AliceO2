e1963a92a1ba452e2b8a0fd0e3cafd5846408b92
aferrero2707 <aferrero1975@gmail.com>
2021-03-27 15:06:29 +0100

    [MCH] updated TFbuilder helper script for calibration workflow

--- Detectors/MUON/MCH/Calibration/test/run-tfbuilder.sh
+++ Detectors/MUON/MCH/Calibration/test/run-tfbuilder.sh
@@ -1,1 +1,5 @@
-VS/run-tfbuilder.sh
+#! /bin/bash
+TRANSPORT=shmem
+#TRANSPORT=zeromq
+
+StfBuilder --id stf_builder-0 --transport $TRANSPORT --detector TPC --dpl-channel-name=dpl-chan --channel-config "name=dpl-chan,type=push,method=bind,address=ipc://@tf-builder-pipe-0,transport=shmem,rateLogging=1" --data-source-dir="$1" --data-source-enable --control=static #--max-built-stfs 10
