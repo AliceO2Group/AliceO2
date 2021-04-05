a3f978497c2406f18fa79eacf4e36f230bd068b6
Maurice <maurice.coquet@yahoo.com>
2021-04-05 15:22:53 +0200

    First version of MFT calibration : generating noise map and writing in CCDB

---
+++ Detectors/ITSMFT/MFT/calibration/testWorkflow/mft-calib-workflow.cxx
@@ -0,0 +1,43 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#include "Framework/ConfigParamSpec.h"
+#include "CommonUtils/ConfigurableParam.h"
+
+
+using namespace o2::framework;
+
+// we need to add workflow options before including Framework/runDataProcessing
+void customize(std::vector<ConfigParamSpec>& workflowOptions)
+{
+  // option allowing to set parameters
+    workflowOptions.push_back(ConfigParamSpec{"doNoise",VariantType::Bool,true,{"Generate noisy-pixel maps"}});
+}
+
+// ------------------------------------------------------------------
+
+#include "Framework/runDataProcessing.h"
+#include "MFTCalibration/NoiseCalibratorSpec.h"
+#include "MFTCalibration/NoiseCalibrator.h"
+
+WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
+{
+  WorkflowSpec specs;
+  auto doNoise = cfgc.options().get<bool>("doNoise");
+
+  LOG(INFO) << "ITS calibration workflow options";
+  LOG(INFO) << "Generate noisy-pixel maps: " << doNoise;
+
+  if (doNoise) {
+    specs.emplace_back(o2::mft::getNoiseCalibratorSpec());
+  }
+
+  return specs;
+}
