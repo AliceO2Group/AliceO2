5b0cba69a06e15419b0cbfb6e1f9afb40ecbbc43
Francesco Noferini <noferini@bo.infn.it>
2021-04-12 00:22:47 +0200

    fix for TOF cosmic calib (#5895)
    * fix for TOF cosmic calib
    
    * suppress unneeded dereferencing, [] instead of at()

--- Detectors/TOF/workflow/src/TOFClusterizerSpec.cxx
+++ Detectors/TOF/workflow/src/TOFClusterizerSpec.cxx
@@ -75,7 +75,7 @@ class TOFDPLClustererTask
     mTimer.Start(false);
     // get digit data
     auto digits = pc.inputs().get<gsl::span<o2::tof::Digit>>("tofdigits");
-    auto row = pc.inputs().get<std::vector<o2::tof::ReadoutWindowData>*>("readoutwin");
+    auto row = pc.inputs().get<gsl::span<o2::tof::ReadoutWindowData>>("readoutwin");
 
     const auto* dh = o2::header::get<o2::header::DataHeader*>(pc.inputs().getByPos(0).header);
     mClusterer.setFirstOrbit(dh->firstTForbit);
@@ -128,9 +128,9 @@ class TOFDPLClustererTask
       mCosmicProcessor.clear();
     }
 
-    for (int i = 0; i < row->size(); i++) {
-      //printf("# TOF readout window for clusterization = %d/%lu (N digits = %d)\n", i, row->size(), row->at(i).size());
-      auto digitsRO = row->at(i).getBunchChannelData(digits);
+    for (int i = 0; i < row.size(); i++) {
+      //printf("# TOF readout window for clusterization = %d/%lu (N digits = %d)\n", i, row.size(), row[i].size());
+      auto digitsRO = row[i].getBunchChannelData(digits);
       mReader.setDigitArray(&digitsRO);
 
       if (mIsCosmic) {
