424bebcffd709e01c801c312abecc897a82cc30b
Philip Hauer <philip.hauer@cern.ch>
2021-04-12 18:15:55 +0200

    Update on KrBoxClusterFinder (included Param-Class and a few updates)

--- Detectors/TPC/reconstruction/src/TPCReconstructionLinkDef.h
+++ Detectors/TPC/reconstruction/src/TPCReconstructionLinkDef.h
@@ -36,6 +36,8 @@
 #pragma link C++ class o2::tpc::HwClusterer + ;
 #pragma link C++ class o2::tpc::HwClustererParam + ;
 #pragma link C++ class o2::conf::ConfigurableParamHelper < o2::tpc::HwClustererParam > +;
+#pragma link C++ class o2::tpc::KrBoxClusterFinderParam + ;
+#pragma link C++ class o2::conf::ConfigurableParamHelper < o2::tpc::KrBoxClusterFinderParam > +;
 
 #pragma link C++ class o2::tpc::TPCFastTransformHelperO2 + ;
 
