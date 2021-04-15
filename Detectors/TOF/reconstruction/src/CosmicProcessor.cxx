54ac2e09b84cad26311e4c63204d003afd1f8ea8
Francesco Noferini <noferini@bo.infn.it>
2021-04-15 10:10:07 +0200

    TOF channel calibration using cosmics
    add on-the-fly tracking for cosmics crossing many strips in a TOF module
    
    add TOF calibration with cosmics
    
    update for calibration with cosmics
    
    templetization of TOF channel calibration to read different objects
    
    Improvements and fixes
    
    format
    
    Possibility to calibrate not only at the EoR.
    
    We need still to change the logic, because now it happens that
    too few pairs have enough entries for the fit, so in the end we
    close a time slot, but without being able to produce a CCDB
    entry.

--- Detectors/TOF/reconstruction/src/CosmicProcessor.cxx
+++ Detectors/TOF/reconstruction/src/CosmicProcessor.cxx
@@ -23,8 +23,25 @@ void CosmicProcessor::clear()
 {
   mCosmicInfo.clear();
   mCosmicInfo.reserve(5200);
+  mCosmicTrack.clear();
+  mCosmicTrack.reserve(1200);
+  mSizeTrack.clear();
+  mSizeTrack.reserve(1200);
+
+  mCosmicTrackTemp.reserve(200);
 
   memset(mCounters, 0, sizeof(int) * Geo::NCHANNELS);
+}
+//__________________________________________________
+void CosmicProcessor::processTrack()
+{
+  if (mCosmicTrack.size() > 1000) {
+    return;
+  }
+
+  // fit the track and check if is good
+  mSizeTrack.push_back(int(mCosmicTrackTemp.size()));
+  mCosmicTrack.insert(mCosmicTrack.end(), mCosmicTrackTemp.begin(), mCosmicTrackTemp.end());
 }
 //__________________________________________________
 void CosmicProcessor::process(DigitDataReader& reader, bool fill)
@@ -49,7 +66,12 @@ void CosmicProcessor::process(DigitDataReader& reader, bool fill)
   float thr = 5000000; // in ps
 
   int volID1[5], volID2[5];
-  float pos1[3], pos2[3];
+  float pos1[3], pos2[3], pos2or[3];
+
+  std::vector<int> mIcandClus;
+  mIcandClus.reserve(200);
+
+  bool trackFound = false;
 
   for (int i = 0; i < ndig; i++) {
     if (npair >= 150) {
@@ -71,6 +93,12 @@ void CosmicProcessor::process(DigitDataReader& reader, bool fill)
 
     float tm1 = (dig1.getIR().differenceInBC(ir0) * 1024 + tdc1) * Geo::TDCBIN; // in ps
 
+    mIcandClus.clear();
+    if (!trackFound) {
+      mCosmicTrackTemp.clear();
+      mCosmicTrackTemp.emplace_back(ch1, pos1[0], pos1[1], pos1[2], 0.0, tot1);
+    }
+
     for (int j = i + 1; j < ndig2; j++) {
       auto& dig2 = (*array)[j];
       int64_t bc2 = int64_t(dig2.getBC()) - bc1;
@@ -87,14 +115,19 @@ void CosmicProcessor::process(DigitDataReader& reader, bool fill)
       int tdc2 = int(dig2.getTDC()) - tdc1;
       float tot2 = dig2.getTOT() * 48.8E-3;
       Geo::getVolumeIndices(ch2, volID2);
-      Geo::getPos(volID2, pos2);
-      pos2[0] -= pos1[0];
-      pos2[1] -= pos1[1];
-      pos2[2] -= pos1[2];
+      Geo::getPos(volID2, pos2or);
+      pos2[0] = pos2or[0] - pos1[0];
+      pos2[1] = pos2or[1] - pos1[1];
+      pos2[2] = pos2or[2] - pos1[2];
 
       float dtime = (bc2 * 1024 + tdc2) * Geo::TDCBIN; // in ps
 
-      float l = sqrt(pos2[0] * pos2[0] + pos2[1] * pos2[1] + pos2[2] * pos2[2]);
+      float lt2 = pos2[0] * pos2[0] + pos2[1] * pos2[1];
+      float l = sqrt(lt2 + pos2[2] * pos2[2]);
+
+      if (!trackFound && lt2 < 40000 && fabs(dtime) < 10000) {
+        mCosmicTrackTemp.emplace_back(ch2, pos2or[0], pos2or[1], pos2or[2], dtime, tot2);
+      }
 
       if (l < 500) {
         continue;
@@ -112,6 +145,13 @@ void CosmicProcessor::process(DigitDataReader& reader, bool fill)
       npair++;
       mCosmicInfo.emplace_back(ch1, ch2, dtime, tot1, tot2, l, tm1, tm2);
     }
+    if (!trackFound && ndig < 1000 && mCosmicTrackTemp.size() > 4) {
+      trackFound = true;
+    }
+  }
+
+  if (trackFound) {
+    processTrack();
   }
 
   LOG(DEBUG) << "We had " << ndig << " digits in this event";
