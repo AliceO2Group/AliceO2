73b06d9280ac05e2931042faa6a92661a17cb3eb
Ole Schmidt <ole.schmidt@cern.ch>
2021-04-15 22:33:35 +0200

    More code cleanup

--- Detectors/TRD/base/include/TRDBase/SimParam.h
+++ Detectors/TRD/base/include/TRDBase/SimParam.h
@@ -8,6 +8,8 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
+/// \file SimParam.h
+
 #ifndef O2_TRD_SIMPARAM_H
 #define O2_TRD_SIMPARAM_H
 
@@ -18,22 +20,27 @@ namespace o2
 {
 namespace trd
 {
-////////////////////////////////////////////////////////////////////////////
-//                                                                        //
-// Class containing constant simulation parameters                        //
-//                                                                        //
-////////////////////////////////////////////////////////////////////////////
+
+/// \brief Constant parameters for the TRD simulation
+///
 class SimParam
 {
  public:
   enum {
-    kNPadsInPadResponse = 3 // Number of pads included in the pad response
+    kNPadsInPadResponse = 3 ///< Number of pads included in the pad response
   };
 
+  /// For a singleton class copying and assigning is not allowed
   SimParam(const SimParam&) = delete;
   SimParam& operator=(const SimParam&) = delete;
+
+  /// Returns an instance of this class. A SimParam object is created upon the first call of this function
   static SimParam* instance();
 
+  /// After setting a new gas mixture the parameters need to be re-evaluated
+  void reInit();
+
+  // Setters
   void setGasGain(float gasgain) { mGasGain = gasgain; }
   void setNoise(float noise) { mNoise = noise; }
   void setChipGain(float chipgain) { mChipGain = chipgain; }
@@ -50,6 +57,7 @@ class SimParam
   void setTimeStruct(bool flag = true) { mTimeStructOn = flag; }
   void setPadResponse(bool flag = true) { mPRFOn = flag; }
 
+  // Getters
   float getGasGain() const { return mGasGain; }
   float getNoise() const { return mNoise; }
   float getChipGain() const { return mChipGain; }
@@ -70,50 +78,44 @@ class SimParam
   const int getNumberOfPadsInPadResponse() const { return kNPadsInPadResponse; }
   inline double timeResponse(double) const;
   inline double crossTalk(double) const;
-  void reInit();
 
  protected:
-  static SimParam* mgInstance; //  Instance of this class (singleton implementation)
+  static SimParam* mgInstance; ///<  Instance of this class (singleton implementation)
 
-  // Digitization parameter
-  float mGasGain{4000.f}; //  Gas gain
-  float mNoise{1250.f};   //  Electronics noise
-  float mChipGain{12.4f}; //  Electronics gain
+  float mNoise{1250.f};                 ///< Electronics noise
+  float mChipGain{12.4f};               ///< Electronics gain
+  float mADCoutRange{1023.f};           ///< ADC output range (number of channels for the 10 bit ADC)
+  float mADCinRange{2000.f};            ///< ADC input range (2V)
+  int mADCbaseline{10};                 ///< ADC intrinsic baseline in ADC channel
+  bool mDiffusionOn{true};              ///< Switch for the diffusion
+  bool mElAttachOn{false};              ///< Switch for the electron attachment
+  float mElAttachProp{0.f};             ///< Propability for electron attachment (for 1m)
+  bool mTRFOn{true};                    ///< Switch for the time response
+  bool mCTOn{true};                     ///< Switch for cross talk
+  bool mPRFOn{true};                    ///<  Switch for the pad response
+  static constexpr int mNBinsMax = 200; ///< Maximum number of bins for integrated time response and cross talk
 
-  float mADCoutRange{1023.f}; //  ADC output range (number of channels); 10 bit ADC
-  float mADCinRange{2000.f};  //  ADC input range (input charge); 2V input range
-  int mADCbaseline{10};       //  ADC intrinsic baseline in ADC channel
-
-  bool mDiffusionOn{true}; //  Switch for the diffusion
-
-  bool mElAttachOn{false};  //  Switch for the electron attachment
-  float mElAttachProp{0.f}; //  Propability for electron attachment (for 1m)
-
-  static constexpr int mNBinsMax = 200;                              // maximum number of bins for integrated time response and cross talk
-  bool mTRFOn{true};                                                 //  Switch for the time response
-  std::array<float, mNBinsMax> mTRFsmp{};                            // Integrated time response
-  int mTRFbin{200};                                                  //  Number of bins for the TRF
-  float mTRFlo{-.4f};                                                //  Lower boundary of the TRF
-  float mTRFhi{3.58f};                                               //  Higher boundary of the TRF
-  float mInvTRFwid{static_cast<float>(mTRFbin) / (mTRFhi - mTRFlo)}; //  Inverse of the bin width of the integrated TRF
-
-  bool mCTOn{true};                      //  Switch for cross talk
-  std::array<float, mNBinsMax> mCTsmp{}; // Integrated cross talk
-
-  // The pad coupling factor
   // Use 0.46, instead of the theroetical value 0.3, since it reproduces better
   // the test beam data, even tough it is not understood why.
-  float mPadCoupling{.46f};
-  float mTimeCoupling{.4f}; //  Time coupling factor (image charge of moving ions); same number as for the TPC
-  bool mTimeStructOn{true}; //  Switch for cell time structure
+  float mPadCoupling{.46f}; ///< The pad coupling factor
+  float mTimeCoupling{.4f}; ///< Time coupling factor (image charge of moving ions); same number as for the TPC
+  bool mTimeStructOn{true}; ///< Switch for cell time structure
 
-  bool mPRFOn{true}; //  Switch for the pad response
+  /// The parameters below depend on the gas mixture and are re-evaluated upon a change
+  std::array<float, mNBinsMax> mTRFsmp{};                            ///< Integrated time response
+  std::array<float, mNBinsMax> mCTsmp{};                             ///< Integrated cross talk
+  int mTRFbin{200};                                                  ///<  Number of bins for the TRF and x-talk
+  float mTRFlo{-.4f};                                                ///<  Lower boundary of the TRF and x-talk
+  float mTRFhi{3.58f};                                               ///<  Higher boundary of the TRF and x-talk
+  float mInvTRFwid{static_cast<float>(mTRFbin) / (mTRFhi - mTRFlo)}; ///<  Inverse of the bin width of the integrated TRF and x-talk
+  float mGasGain{4000.f};                                            ///< Gas gain
 
  private:
-  // This is a singleton, constructor is private!
+  /// This is a singleton, constructor is private!
   SimParam();
   ~SimParam() = default;
 
+  /// Fill the arrays mTRDsmp and mCTsmp for the given gas mixture
   void sampleTRF();
 
   ClassDefNV(SimParam, 1); // The TRD simulation parameters
