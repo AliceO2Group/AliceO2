6e8a4f2b6f79dcd9522d670077d89b1c466f3707
Andreas Morsch <andreas.morsch@cern.ch>
2021-04-17 20:14:05 +0200

    FLUKA command lind configuration to switch to radiation studies

---
+++ Detectors/gconfig/include/SimSetup/FlukaParam.h
@@ -0,0 +1,34 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \author A+Morsch
+
+#include <string>
+#ifndef ALICEO2_EVENTGEN_FLUKAPARAM_H_
+#define ALICEO2_EVENTGEN_FLUKAPARAM_H_
+
+#include "CommonUtils/ConfigurableParam.h"
+#include "CommonUtils/ConfigurableParamHelper.h"
+
+namespace o2
+{
+/**
+ ** A parameter class/struct holding values for
+ ** FLUKA Transport Code
+ **/
+struct FlukaParam : public o2::conf::ConfigurableParamHelper<FlukaParam> {
+  bool  activationSimulation = false;              // whether FLUKA is used for activation studies
+  float activationHadronCut  = 0.003;              // hadron kinetic energy cut for activation studies
+  std::string scoringFile    = "";                 // input file for user scoring options 
+  O2ParamDef(FlukaParam, "FlukaParam");
+};
+} // end namespace o2
+
+#endif // ALICEO2_EVENTGEN_FLUKAPARAM_H_
