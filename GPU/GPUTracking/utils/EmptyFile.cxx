6f4a65a92e37e4c90cc393f6c50f37d9416b2d45
David Rohr <drohr@jwdt.org>
2021-05-03 20:34:05 +0200

    GPU: Use a common dummy file for header only libraries

--- GPU/GPUTracking/utils/EmptyFile.cxx
+++ GPU/GPUTracking/utils/EmptyFile.cxx
@@ -8,7 +8,7 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-/// \file GPUCommon.cxx
+/// \file EmptyFile.cxx
 /// \author David Rohr
 
-// Empty file, needed for AliRoot
+// Empty file, needed for header only libraries
