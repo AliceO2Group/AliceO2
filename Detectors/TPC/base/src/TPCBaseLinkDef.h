d6d47e564e2f3ab3676b7bc69e088ac2c572db20
wiechula <Jens.Wiechula@cern.ch>
2021-05-03 10:56:56 +0200

    add missing class

--- Detectors/TPC/base/src/TPCBaseLinkDef.h
+++ Detectors/TPC/base/src/TPCBaseLinkDef.h
@@ -30,6 +30,7 @@
 #pragma link C++ class std::vector < o2::tpc::CalDet < float>*> + ;
 #pragma link C++ class std::unordered_map < std::string, o2::tpc::CalDet < float>> + ;
 #pragma link C++ class o2::tpc::CDBInterface;
+#pragma link C++ class o2::tpc::CDBStorage;
 #pragma link C++ class o2::tpc::ContainerFactory;
 #pragma link C++ class o2::tpc::CRU;
 #pragma link C++ class o2::tpc::DigitPos;
