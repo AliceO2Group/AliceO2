32f000bd97d931c78af1bc6bd5d2919bd17e8613
Jan Fiete <jgrosseo@cern.ch>
2021-05-05 15:24:35 +0200

    Merging same and mixed event correlation analysis

--- Analysis/Tasks/PWGCF/correlations.cxx
+++ Analysis/Tasks/PWGCF/correlations.cxx
@@ -14,6 +14,7 @@
 #include <CCDB/BasicCCDBManager.h>
 #include "Framework/StepTHn.h"
 #include "Framework/HistogramRegistry.h"
+#include "Framework/RunningWorkflowInfo.h"
 
 #include "AnalysisDataModel/EventSelection.h"
 #include "AnalysisDataModel/TrackSelectionTables.h"
@@ -25,6 +26,17 @@
 #include <cmath>
 #include <TDirectory.h>
 #include <THn.h>
+
+namespace o2::aod
+{
+namespace hash
+{
+DECLARE_SOA_COLUMN(Bin, bin, int);
+} // namespace hash
+DECLARE_SOA_TABLE(Hashes, "AOD", "HASH", hash::Bin);
+
+using Hash = Hashes::iterator;
+} // namespace o2::aod
 
 using namespace o2;
 using namespace o2::framework;
@@ -55,6 +67,8 @@ struct CorrelationTask {
 
   O2_DEFINE_CONFIGURABLE(cfgEfficiencyTrigger, std::string, "", "CCDB path to efficiency object for trigger particles")
   O2_DEFINE_CONFIGURABLE(cfgEfficiencyAssociated, std::string, "", "CCDB path to efficiency object for associated particles")
+
+  O2_DEFINE_CONFIGURABLE(cfgNoMixedEvents, int, 5, "Number of mixed events per event")
 
   ConfigurableAxis axisVertex{"axisVertex", {7, -7, 7}, "vertex axis for histograms"};
   ConfigurableAxis axisDeltaPhi{"axisDeltaPhi", {72, -M_PI / 2, M_PI / 2 * 3}, "delta phi axis for histograms"};
@@ -142,31 +156,13 @@ struct CorrelationTask {
     }
   }
 
-  // Version with explicit nested loop
-  void process(soa::Filtered<soa::Join<aod::Collisions, aod::EvSels, aod::Cents>>::iterator const& collision, aod::BCsWithTimestamps const&, myTracks const& tracks)
+  template <typename TTarget, typename TCollision>
+  bool fillCollision(TTarget target, TCollision collision, float centrality)
   {
-    auto bc = collision.bc_as<aod::BCsWithTimestamps>();
-    if (cfgEfficiencyTrigger.value.empty() == false) {
-      cfg.mEfficiencyTrigger = ccdb->getForTimeStamp<THnT<float>>(cfgEfficiencyTrigger, bc.timestamp());
-      LOGF(info, "Loaded efficiency histogram for trigger particles from %s (%p)", cfgEfficiencyTrigger.value.c_str(), (void*)cfg.mEfficiencyTrigger);
-    }
-    if (cfgEfficiencyAssociated.value.empty() == false) {
-      cfg.mEfficiencyAssociated = ccdb->getForTimeStamp<THnT<float>>(cfgEfficiencyAssociated, bc.timestamp());
-      LOGF(info, "Loaded efficiency histogram for associated particles from %s (%p)", cfgEfficiencyAssociated.value.c_str(), (void*)cfg.mEfficiencyAssociated);
-    }
-
-    LOGF(info, "Tracks for collision: %d | Vertex: %.1f | INT7: %d | V0M: %.1f", tracks.size(), collision.posZ(), collision.sel7(), collision.centV0M());
-
-    if (std::abs(collision.posZ()) > cfgCutVertex) {
-      LOGF(warning, "Unexpected: Vertex %f outside of cut %f", collision.posZ(), cfgCutVertex);
-    }
-
-    const auto centrality = collision.centV0M();
-
     same->fillEvent(centrality, CorrelationContainer::kCFStepAll);
 
     if (!collision.alias()[kINT7] || !collision.sel7()) {
-      return;
+      return false;
     }
 
     same->fillEvent(centrality, CorrelationContainer::kCFStepTriggered);
@@ -174,30 +170,30 @@ struct CorrelationTask {
     // vertex range already checked as filter, but bitwise operations not yet supported
     // TODO (collision.flags() != 0) can be removed with next conversion (AliPhysics >= 20210305)
     if ((collision.flags() != 0) && ((collision.flags() & aod::collision::CollisionFlagsRun2::Run2VertexerTracks) != aod::collision::CollisionFlagsRun2::Run2VertexerTracks)) {
-      return;
+      return false;
     }
 
     same->fillEvent(centrality, CorrelationContainer::kCFStepVertex);
-
     same->fillEvent(centrality, CorrelationContainer::kCFStepReconstructed);
 
-    int bSign = 1; // TODO magnetic field from CCDB
+    return true;
+  }
 
+  template <typename TTarget, typename TTracks>
+  void fillCorrelations(TTarget target, TTracks tracks1, TTracks tracks2, float centrality, float posZ, int bSign)
+  {
     // Cache efficiency for particles (too many FindBin lookups)
     float* efficiencyAssociated = nullptr;
     if (cfg.mEfficiencyAssociated) {
-      efficiencyAssociated = new float[tracks.size()];
+      efficiencyAssociated = new float[tracks2.size()];
       int i = 0;
-      for (auto& track1 : tracks) {
-        efficiencyAssociated[i++] = getEfficiency(cfg.mEfficiencyAssociated, track1.eta(), track1.pt(), centrality, collision.posZ());
+      for (auto& track : tracks2) {
+        efficiencyAssociated[i++] = getEfficiency(cfg.mEfficiencyAssociated, track.eta(), track.pt(), centrality, posZ);
       }
     }
 
-    for (auto& track1 : tracks) {
+    for (auto& track1 : tracks1) {
       // LOGF(info, "Track %f | %f | %f  %d %d", track1.eta(), track1.phi(), track1.pt(), track1.isGlobalTrack(), track1.isGlobalTrackSDD());
-
-      registry.fill(HIST("yields"), centrality, track1.pt(), track1.eta());
-      registry.fill(HIST("etaphi"), centrality, track1.eta(), track1.phi());
 
       if (cfgTriggerCharge != 0 && cfgTriggerCharge * track1.sign() < 0) {
         continue;
@@ -205,14 +201,13 @@ struct CorrelationTask {
 
       float triggerWeight = 1.0;
       if (cfg.mEfficiencyTrigger) {
-        triggerWeight = getEfficiency(cfg.mEfficiencyTrigger, track1.eta(), track1.pt(), centrality, collision.posZ());
+        triggerWeight = getEfficiency(cfg.mEfficiencyTrigger, track1.eta(), track1.pt(), centrality, posZ);
       }
 
-      same->getTriggerHist()->Fill(CorrelationContainer::kCFStepReconstructed, track1.pt(), centrality, collision.posZ(), triggerWeight);
-      //mixed->getTriggerHist()->Fill(eventValues, CorrelationContainer::kCFStepReconstructed);
+      target->getTriggerHist()->Fill(CorrelationContainer::kCFStepReconstructed, track1.pt(), centrality, posZ, triggerWeight);
 
       int i = -1;
-      for (auto& track2 : tracks) {
+      for (auto& track2 : tracks2) {
         i++; // HACK
         if (track1 == track2) {
           continue;
@@ -250,18 +245,97 @@ struct CorrelationTask {
           deltaPhi += M_PI * 2;
         }
 
-        same->getPairHist()->Fill(CorrelationContainer::kCFStepReconstructed,
-                                  track1.eta() - track2.eta(), track2.pt(), track1.pt(), centrality, deltaPhi, collision.posZ(),
-                                  triggerWeight * associatedWeight);
-        //mixed->getPairHist()->Fill(values, CorrelationContainer::kCFStepReconstructed);
+        target->getPairHist()->Fill(CorrelationContainer::kCFStepReconstructed,
+                                    track1.eta() - track2.eta(), track2.pt(), track1.pt(), centrality, deltaPhi, posZ,
+                                    triggerWeight * associatedWeight);
       }
     }
 
     delete[] efficiencyAssociated;
   }
 
+  // Version with explicit nested loop
+  void processSame(soa::Filtered<soa::Join<aod::Collisions, aod::EvSels, aod::Cents>>::iterator const& collision, aod::BCsWithTimestamps const&, myTracks const& tracks)
+  {
+    auto bc = collision.bc_as<aod::BCsWithTimestamps>();
+    if (cfgEfficiencyTrigger.value.empty() == false) {
+      cfg.mEfficiencyTrigger = ccdb->getForTimeStamp<THnT<float>>(cfgEfficiencyTrigger, bc.timestamp());
+      LOGF(info, "Loaded efficiency histogram for trigger particles from %s (%p)", cfgEfficiencyTrigger.value.c_str(), (void*)cfg.mEfficiencyTrigger);
+    }
+    if (cfgEfficiencyAssociated.value.empty() == false) {
+      cfg.mEfficiencyAssociated = ccdb->getForTimeStamp<THnT<float>>(cfgEfficiencyAssociated, bc.timestamp());
+      LOGF(info, "Loaded efficiency histogram for associated particles from %s (%p)", cfgEfficiencyAssociated.value.c_str(), (void*)cfg.mEfficiencyAssociated);
+    }
+
+    LOGF(info, "Tracks for collision: %d | Vertex: %.1f | INT7: %d | V0M: %.1f", tracks.size(), collision.posZ(), collision.sel7(), collision.centV0M());
+
+    if (std::abs(collision.posZ()) > cfgCutVertex) {
+      LOGF(warning, "Unexpected: Vertex %f outside of cut %f", collision.posZ(), cfgCutVertex);
+    }
+
+    int bSign = 1; // TODO magnetic field from CCDB
+    const auto centrality = collision.centV0M();
+
+    if (fillCollision(same, collision, centrality) == false) {
+      return;
+    }
+
+    for (auto& track1 : tracks) {
+      registry.fill(HIST("yields"), centrality, track1.pt(), track1.eta());
+      registry.fill(HIST("etaphi"), centrality, track1.eta(), track1.phi());
+    }
+
+    fillCorrelations(same, tracks, tracks, centrality, collision.posZ(), bSign);
+  }
+
+  void processMixed(soa::Join<aod::Collisions, aod::Hashes, aod::EvSels, aod::Cents>& collisions, myTracks const& tracks)
+  {
+    // TODO loading of efficiency histogram missing here, because it will happen somehow in the CCDBConfigurable
+
+    int bSign = 1; // TODO magnetic field from CCDB
+
+    collisions.bindExternalIndices(&tracks);
+    auto tracksTuple = std::make_tuple(tracks);
+    AnalysisDataProcessorBuilder::GroupSlicer slicer(collisions, tracksTuple);
+
+    // Strictly upper categorised collisions, for cfgNoMixedEvents combinations per bin, skipping those in entry -1
+    for (auto& [collision1, collision2] : selfCombinations("fBin", cfgNoMixedEvents, -1, collisions, collisions)) {
+
+      LOGF(info, "Mixed collisions bin: %d pair: %d (%f), %d (%f)", collision1.bin(), collision1.index(), collision1.posZ(), collision2.index(), collision2.posZ());
+
+      // TODO in principle these should be already checked on hash level, because in this way we don't check collision 2
+      if (fillCollision(mixed, collision1, collision1.centV0M()) == false) {
+        continue;
+      }
+
+      auto it1 = slicer.begin();
+      auto it2 = slicer.begin();
+      for (auto& slice : slicer) {
+        if (slice.groupingElement().index() == collision1.index()) {
+          it1 = slice;
+          break;
+        }
+      }
+      for (auto& slice : slicer) {
+        if (slice.groupingElement().index() == collision2.index()) {
+          it2 = slice;
+          break;
+        }
+      }
+
+      auto tracks1 = std::get<myTracks>(it1.associatedTables());
+      tracks1.bindExternalIndices(&collisions);
+      auto tracks2 = std::get<myTracks>(it2.associatedTables());
+      tracks2.bindExternalIndices(&collisions);
+
+      // LOGF(info, "Tracks: %d and %d entries", tracks1.size(), tracks2.size());
+
+      fillCorrelations(mixed, tracks1, tracks2, collision1.centV0M(), collision1.posZ(), bSign);
+    }
+  }
+
   // Version with combinations
-  void process2(soa::Join<aod::Collisions, aod::Cents>::iterator const& collision, soa::Filtered<aod::Tracks> const& tracks)
+  void processWithCombinations(soa::Join<aod::Collisions, aod::Cents>::iterator const& collision, soa::Filtered<aod::Tracks> const& tracks)
   {
     LOGF(info, "Tracks for collision (Combination run): %d", tracks.size());
 
@@ -327,8 +401,84 @@ struct CorrelationTask {
   }
 };
 
+struct CorrelationHashTask {
+  // TODO sync configurables with main task
+  std::vector<float> vtxBins{0.0f};
+  std::vector<float> multBins{0.0f};
+
+  Produces<aod::Hashes> hashes;
+
+  void fillArray(int length, double* source, std::vector<float>& target)
+  {
+    // Expand binning from Configurable. Can we let some code in AxisSpec do this?
+
+    target.clear();
+    if (source[0] == VARIABLE_WIDTH) {
+      for (int i = 1; i < length; i++) {
+        target.push_back(source[i]);
+      }
+    } else {
+      for (int i = 0; i <= source[0]; i++) {
+        target.push_back(source[1] + (source[2] - source[1]) / source[0] * i);
+      }
+    }
+  }
+
+  void init(o2::framework::InitContext& initContext)
+  {
+    // get axis config from CorrelationTask
+    auto& workflows = initContext.services().get<RunningWorkflowInfo const>();
+    for (DeviceSpec device : workflows.devices) {
+      for (auto option : device.options) {
+        if (option.name == "axisVertex") {
+          fillArray(option.defaultValue.size(), option.defaultValue.get<double*>(), vtxBins);
+          LOGF(info, "Initialized vertex binning for mixing from configurable %s", option.name);
+        }
+        if (option.name == "axisMultiplicity") {
+          fillArray(option.defaultValue.size(), option.defaultValue.get<double*>(), multBins);
+          LOGF(info, "Initialized multiplicity binning for mixing from configurable %s", option.name);
+        }
+      }
+    }
+  }
+
+  // Calculate hash for an element based on 2 properties and their bins.
+  int getHash(std::vector<float> const& vtxBins, std::vector<float> const& multBins, float vtx, float mult)
+  {
+    // underflow
+    if (vtx < vtxBins[0]) {
+      return -1;
+    }
+    if (mult < multBins[0]) {
+      return -1;
+    }
+
+    for (int i = 1; i < vtxBins.size(); i++) {
+      if (vtx < vtxBins[i]) {
+        for (int j = 1; j < multBins.size(); j++) {
+          if (mult < multBins[j]) {
+            return i + j * (vtxBins.size() + 1);
+          }
+        }
+      }
+    }
+    // overflow
+    return -1;
+  }
+
+  void process(soa::Join<aod::Collisions, aod::Cents> const& collisions)
+  {
+    for (auto& collision : collisions) {
+      int hash = getHash(vtxBins, multBins, collision.posZ(), collision.centV0M());
+      LOGF(info, "Collision: %d (%f, %f) hash: %d", collision.index(), collision.posZ(), collision.centV0M(), hash);
+      hashes(hash);
+    }
+  }
+};
+
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
 {
   return WorkflowSpec{
-    adaptAnalysisTask<CorrelationTask>(cfgc)};
+    adaptAnalysisTask<CorrelationHashTask>(cfgc),
+    adaptAnalysisTask<CorrelationTask>(cfgc, Processes{&CorrelationTask::processSame, &CorrelationTask::processMixed})};
 }
