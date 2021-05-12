721ff950b5bfded5dcdec81601cb6c0165e3c01a
shahoian <ruben.shahoyan@cern.ch>
2021-05-12 19:09:40 +0200

    make sure MFT digi2raw creates 1 file per link if requested

--- Detectors/ITSMFT/MFT/simulation/src/digi2raw.cxx
+++ Detectors/ITSMFT/MFT/simulation/src/digi2raw.cxx
@@ -264,8 +264,8 @@ void setupLinks(o2::itsmft::MC2RawEncoder<MAP>& m2r, std::string_view outDir, st
           } else if (fileFor == "cru") {
             outFileLink = o2::utils::Str::concat_string(outDir, "/", outPrefix, "_cru", std::to_string(link->cruID), ".raw");
           } else if (fileFor == "link") {
-            outFileLink = o2::utils::Str::concat_string(outDir, "/", outPrefix, "_cru", std::to_string(cruID),
-                                                        "_link", std::to_string(linkID), "_ep", std::to_string(link->endPointID), ".raw");
+            outFileLink = o2::utils::Str::concat_string(outDir, "/", outPrefix, "_cru", std::to_string(link->cruID),
+                                                        "_link", std::to_string(linkID), "_ep", std::to_string(link->endPointID), "_feeid", std::to_string(link->feeID), ".raw");
           } else {
             throw std::runtime_error("invalid option provided for file grouping");
           }
