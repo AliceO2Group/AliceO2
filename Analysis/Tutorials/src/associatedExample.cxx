587ad57332abfb9020774220605a2df720218984
Paul <paul.buehler@oeaw.ac.at>
2021-05-14 17:19:18 +0200

    Cleaning and updates of tutorials

--- Analysis/Tutorials/src/associatedExample.cxx
+++ Analysis/Tutorials/src/associatedExample.cxx
@@ -7,7 +7,6 @@
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
-//
 ///
 /// \brief index columns are used to relate information in non-joinable tables.
 /// \author
