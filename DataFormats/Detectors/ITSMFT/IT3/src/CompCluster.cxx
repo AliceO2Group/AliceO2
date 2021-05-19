689703ad2ef728c5c5c1715d463f5d8526210cae
Maximiliano Puccio <mpuccio@users.noreply.github.com>
2021-05-19 15:58:42 +0200

    Cluster finder for ITS3 (#6129)
    * Adding clusteriser workflow for ITS3 and related changes
    
    Co-authored-by: Maximiliano Puccio <mpuccio@users.noreply.github.com>
    
    * First version of clusterfinder workflow ITS3 -- to be debugged
    
    Co-authored-by: Maximiliano Puccio <mpuccio@users.noreply.github.com>
    
    * First working version of ITS3 cluster finder
    
    Co-authored-by: Maximiliano Puccio <mpuccio@users.noreply.github.com>
    
    * Fixes for CheckClustersITS3 macro
    
    Co-authored-by: Maximiliano Puccio <mpuccio@users.noreply.github.com>
    
    * Introduce ITS3 compressed cluster format
    
    * Extend mask size in PixelData for upgrades
    
    * Use the same mask for ITS3 and ITS2
    
    * Fix warning and hopefully macro linking problem
    
    * Remove tabs?
    
    * Add copyright banner to TopologyDictionary.cxx
    
    * Add topology dictionary to the dictionary
    
    * Remove macro compilation for osx
    
    * Fix copy paste error in the CMakeLists
    
    * Add TopologyDictionry to LinkDef
    
    Co-authored-by: Alexander Kalweit <alexander.philipp.kalweit@cern.ch>

---
+++ DataFormats/Detectors/ITSMFT/IT3/src/CompCluster.cxx
@@ -0,0 +1,45 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file Cluster.cxx
+/// \brief Implementation of the ITSMFT cluster
+
+#include "DataFormatsITS3/CompCluster.h"
+#include <cassert>
+#include <iostream>
+
+using namespace o2::its3;
+
+std::ostream& operator<<(std::ostream& stream, const CompCluster& cl)
+{
+  stream << " row: " << cl.getRow() << " col: " << cl.getCol()
+         << " pattID " << cl.getPatternID() << " [flag: " << cl.getFlag() << "] ";
+  return stream;
+}
+
+std::ostream& operator<<(std::ostream& stream, const CompClusterExt& cl)
+{
+  stream << " chip: " << cl.getChipID() << ((const CompCluster&)cl);
+  return stream;
+}
+
+//______________________________________________________________________________
+void CompCluster::print() const
+{
+  // print itself
+  std::cout << *this << "\n";
+}
+
+//______________________________________________________________________________
+void CompClusterExt::print() const
+{
+  // print itself
+  std::cout << *this << "\n";
+}
