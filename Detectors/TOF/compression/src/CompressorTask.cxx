f94d7fb99267c0bdfde4f727f76e2d586f4c17c7
Roberto Preghenella <preghenella@bo.infn.it>
2021-05-20 14:16:55 +0200

    TOF: fix retrieval of integer output buffer CompressorTask.cxx (#6194)
    To fix retrieval of integer output buffer size output buffer size will be the sum of the command-line-specified buffer size and the value automatically calculated.
    Unless a negative value is used as the command-line-specified buffer size, in which case a fixed buffer size is used.

--- Detectors/TOF/compression/src/CompressorTask.cxx
+++ Detectors/TOF/compression/src/CompressorTask.cxx
@@ -38,7 +38,7 @@ void CompressorTask<RDH, verbose>::init(InitContext& ic)
   auto decoderVerbose = ic.options().get<bool>("tof-compressor-decoder-verbose");
   auto encoderVerbose = ic.options().get<bool>("tof-compressor-encoder-verbose");
   auto checkerVerbose = ic.options().get<bool>("tof-compressor-checker-verbose");
-  mOutputBufferSize = ic.options().get<bool>("tof-compressor-output-buffer-size");
+  mOutputBufferSize = ic.options().get<int>("tof-compressor-output-buffer-size");
 
   mCompressor.setDecoderCONET(decoderCONET);
   mCompressor.setDecoderVerbose(decoderVerbose);
@@ -102,7 +102,7 @@ void CompressorTask<RDH, verbose>::run(ProcessingContext& pc)
     headerOut.payloadSize = 0;
 
     /** initialise output message **/
-    auto bufferSize = mOutputBufferSize > 0 ? mOutputBufferSize : subspecBufferSize[subspec];
+    auto bufferSize = mOutputBufferSize >= 0 ? mOutputBufferSize + subspecBufferSize[subspec] : std::abs(mOutputBufferSize);
     auto payloadMessage = device->NewMessage(bufferSize);
     auto bufferPointer = (char*)payloadMessage->GetData();
 
