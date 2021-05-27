9454b545ee5893e45afe84a7eafd66df6b46ce3f
Matteo Concas <matteo.concas@cern.ch>
2021-05-27 14:47:12 +0200

    Flatten dir tree a bit

--- GPU/GPUbenchmark/macro/runGPUbenchmark.C
+++
@@ -1,9 +0,0 @@
-#if !defined(__CLING__) || defined(__ROOTCLING__)
-#include <GPUBenchmark/Steer/Kernels/Kernels.h>
-#endif
-
-void runGPUbenchmark()
-{
-  o2::benchmark::GPUbenchmark bm{};
-  bm.hello();
-}
