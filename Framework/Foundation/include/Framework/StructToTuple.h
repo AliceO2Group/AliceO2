ebea9e9d3a751ffd13e0e7dca363ed0041914c64
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2021-05-27 02:39:09 +0200

    DPL Foundation: do not use a tuple to store task fields.
    We can either apply directly the function, or extract base classes
    for each of the fields with this approach.

--- Framework/Foundation/include/Framework/StructToTuple.h
+++ Framework/Foundation/include/Framework/StructToTuple.h
@@ -11,8 +11,7 @@
 #define O2_FRAMEWORK_STRUCTTOTUPLE_H_
 
 #include <Framework/Traits.h>
-#include <tuple>
-#include <type_traits>
+#include <vector>
 
 namespace o2::framework
 {
@@ -32,29 +31,6 @@ std::false_type
 template <class T, typename... Args>
 struct is_braces_constructible : decltype(test<T, Args...>(0)) {
 };
-
-/// Helper function to convert a brace-initialisable struct to
-/// a tuple.
-template <class T>
-auto constexpr to_tuple(T&& object) noexcept
-{
-  using type = std::decay_t<T>;
-  if constexpr (is_braces_constructible<type, any_type, any_type, any_type, any_type>{}) {
-    auto&& [p0, p1, p2, p3] = object;
-    return std::make_tuple(p0, p1, p2, p3);
-  } else if constexpr (is_braces_constructible<type, any_type, any_type, any_type>{}) {
-    auto&& [p0, p1, p2] = object;
-    return std::make_tuple(p0, p1, p2);
-  } else if constexpr (is_braces_constructible<type, any_type, any_type>{}) {
-    auto&& [p0, p1] = object;
-    return std::make_tuple(p0, p1);
-  } else if constexpr (is_braces_constructible<type, any_type>{}) {
-    auto&& [p0] = object;
-    return std::make_tuple(p0);
-  } else {
-    return std::make_tuple();
-  }
-}
 
 #if __cplusplus >= 202002L
 struct UniversalType {
@@ -162,138 +138,6 @@ constexpr long brace_constructible_size()
   }
 }
 #endif
-
-/// Helper function to convert a brace-initialisable struct to
-/// a tuple.
-template <class T>
-auto constexpr to_tuple_refs(T&& object) noexcept
-{
-  using type = std::decay_t<T>;
-  constexpr unsigned long numElements = brace_constructible_size<T>();
-  if constexpr (numElements == 40) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p37, p38, p39] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p37, p38, p39);
-  } else if constexpr (numElements == 39) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p37, p38] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p37, p38);
-  } else if constexpr (numElements == 38) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p37] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p37);
-  } else if constexpr (numElements == 37) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36);
-  } else if constexpr (numElements == 36) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35);
-  } else if constexpr (numElements == 35) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34);
-  } else if constexpr (numElements == 34) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33);
-  } else if constexpr (numElements == 33) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32);
-  } else if constexpr (numElements == 32) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31);
-  } else if constexpr (numElements == 31) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30);
-  } else if constexpr (numElements == 30) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29);
-  } else if constexpr (numElements == 29) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28);
-  } else if constexpr (numElements == 28) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27);
-  } else if constexpr (numElements == 27) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26);
-  } else if constexpr (numElements == 26) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25);
-  } else if constexpr (numElements == 25) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24);
-  } else if constexpr (numElements == 24) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23);
-  } else if constexpr (numElements == 23) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22);
-  } else if constexpr (numElements == 22) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21);
-  } else if constexpr (numElements == 21) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20);
-  } else if constexpr (numElements == 20) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19);
-  } else if constexpr (numElements == 19) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18);
-  } else if constexpr (numElements == 18) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17);
-  } else if constexpr (numElements == 17) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16);
-  } else if constexpr (numElements == 16) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15);
-  } else if constexpr (numElements == 15) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14);
-  } else if constexpr (numElements == 14) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13);
-  } else if constexpr (numElements == 13) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12);
-  } else if constexpr (numElements == 12) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11);
-  } else if constexpr (numElements == 11) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10);
-  } else if constexpr (numElements == 10) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9);
-  } else if constexpr (numElements == 9) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7, p8] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7, p8);
-  } else if constexpr (numElements == 8) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6, p7] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6, p7);
-  } else if constexpr (numElements == 7) {
-    auto&& [p0, p1, p2, p3, p4, p5, p6] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5, p6);
-  } else if constexpr (numElements == 6) {
-    auto&& [p0, p1, p2, p3, p4, p5] = object;
-    return std::tie(p0, p1, p2, p3, p4, p5);
-  } else if constexpr (numElements == 5) {
-    auto&& [p0, p1, p2, p3, p4] = object;
-    return std::tie(p0, p1, p2, p3, p4);
-  } else if constexpr (numElements == 4) {
-    auto&& [p0, p1, p2, p3] = object;
-    return std::tie(p0, p1, p2, p3);
-  } else if constexpr (numElements == 3) {
-    auto&& [p0, p1, p2] = object;
-    return std::tie(p0, p1, p2);
-  } else if constexpr (numElements == 2) {
-    auto&& [p0, p1] = object;
-    return std::tie(p0, p1);
-  } else if constexpr (numElements == 1) {
-    auto&& [p0] = object;
-    return std::tie(p0);
-  } else {
-    return std::make_tuple();
-  }
-}
 
 template <typename L, class T>
 auto constexpr homogeneous_apply_refs(L l, T&& object) noexcept
