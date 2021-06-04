d0594616e42d182c65de928130f3f1f989103381
Pietro Cortese <pietro.cortese@cern.ch>
2021-06-04 09:37:10 +0200

    Digits reader

--- Detectors/ZDC/workflow/src/DigitReaderSpec.cxx
+++ Detectors/ZDC/workflow/src/DigitReaderSpec.cxx
@@ -18,9 +18,9 @@
 #include "Framework/ControlService.h"
 #include "Framework/Logger.h"
 #include "ZDCWorkflow/DigitReaderSpec.h"
-#include "DataFormatsZDC/ChannelData.h"
-#include "DataFormatsZDC/BCData.h"
 #include "DataFormatsZDC/OrbitData.h"
+#include "DataFormatsZDC/BCData.h"
+#include "DataFormatsZDC/ChannelData.h"
 #include "DataFormatsZDC/MCLabel.h"
 #include "SimulationDataFormat/MCTruthContainer.h"
 
@@ -34,7 +34,7 @@ namespace zdc
 void DigitReader::init(InitContext& ic)
 {
   auto filename = ic.options().get<std::string>("zdc-digit-infile");
-  mFile = std::make_unique<TFile>(filename.c_str());
+  mFile = std::make_unique<TFile>(filename.c_str(), "OLD");
   if (!mFile->IsOpen()) {
     LOG(ERROR) << "Cannot open the " << filename.c_str() << " file !";
     throw std::runtime_error("cannot open input digits file");
@@ -48,29 +48,32 @@ void DigitReader::init(InitContext& ic)
 
 void DigitReader::run(ProcessingContext& pc)
 {
-  std::vector<o2::zdc::ChannelData> digitsCh, *digitsChPtr = &digitsCh;
-  std::vector<o2::zdc::BCData> digitsBC, *digitsBCPtr = &digitsBC;
-  std::vector<o2::zdc::OrbitData> orbitData, *orbitDataPtr = &orbitData;
-  mTree->SetBranchAddress("ZDCDigitBC", &digitsBCPtr);
-  mTree->SetBranchAddress("ZDCDigitCh", &digitsChPtr);
-  mTree->SetBranchAddress("ZDCDigitOrbit", &orbitDataPtr);
 
+  std::vector<o2::zdc::OrbitData> zdcOrbitData, *zdcOrbitDataPtr = &zdcOrbitData;
+  std::vector<o2::zdc::BCData> zdcBCData, *zdcBCDataPtr = &zdcBCData;
+  std::vector<o2::zdc::ChannelData> zdcChData, *zdcChDataPtr = &zdcChData;
+
+  mTree->SetBranchAddress("ZDCDigitOrbit", &zdcOrbitDataPtr);
+  mTree->SetBranchAddress("ZDCDigitBC", &zdcBCDataPtr);
+  mTree->SetBranchAddress("ZDCDigitCh", &zdcChDataPtr);
   o2::dataformats::MCTruthContainer<o2::zdc::MCLabel> labels, *plabels = &labels;
   if (mUseMC) {
     mTree->SetBranchAddress("ZDCDigitLabels", &plabels);
   }
-  mTree->GetEntry(0);
-
-  LOG(INFO) << "ZDCDigitReader pushed " << digitsCh.size() << " channels in " << digitsBC.size() << " digits";
-
-  pc.outputs().snapshot(Output{"ZDC", "DIGITSBC", 0, Lifetime::Timeframe}, digitsBC);
-  pc.outputs().snapshot(Output{"ZDC", "DIGITSCH", 0, Lifetime::Timeframe}, digitsCh);
-  pc.outputs().snapshot(Output{"ZDC", "DIGITSPD", 0, Lifetime::Timeframe}, orbitData);
+  auto ent = mTree->GetReadEntry() + 1;
+  assert(ent < mTree->GetEntries()); // this should not happen
+  mTree->GetEntry(ent);
+  LOG(INFO) << "ZDCDigitReader pushed " << zdcOrbitData.size() << " orbits with " << zdcBCData.size() << " bcs and " << zdcChData.size() << " digits";
+  pc.outputs().snapshot(Output{"ZDC", "DIGITSPD", 0, Lifetime::Timeframe}, zdcOrbitData);
+  pc.outputs().snapshot(Output{"ZDC", "DIGITSBC", 0, Lifetime::Timeframe}, zdcBCData);
+  pc.outputs().snapshot(Output{"ZDC", "DIGITSCH", 0, Lifetime::Timeframe}, zdcChData);
   if (mUseMC) {
     pc.outputs().snapshot(Output{"ZDC", "DIGITSLBL", 0, Lifetime::Timeframe}, labels);
   }
-  pc.services().get<ControlService>().endOfStream();
-  pc.services().get<ControlService>().readyToQuit(QuitRequest::Me);
+  if (mTree->GetReadEntry() + 1 >= mTree->GetEntries()) {
+    pc.services().get<ControlService>().endOfStream();
+    pc.services().get<ControlService>().readyToQuit(QuitRequest::Me);
+  }
 }
 
 DataProcessorSpec getDigitReaderSpec(bool useMC)
@@ -82,7 +85,6 @@ DataProcessorSpec getDigitReaderSpec(bool useMC)
   if (useMC) {
     outputs.emplace_back("ZDC", "DIGITSLBL", 0, Lifetime::Timeframe);
   }
-
   return DataProcessorSpec{
     "zdc-digit-reader",
     Inputs{},
--- Detectors/ZDC/workflow/src/ZDCRecoWriterDPLSpec.cxx
+++ Detectors/ZDC/workflow/src/ZDCRecoWriterDPLSpec.cxx
@@ -8,7 +8,7 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-/// @file   FT0DigitWriterSpec.cxx
+/// @file   ZDCRecoWriterDPLSpec.cxx
 
 #include <vector>
 
