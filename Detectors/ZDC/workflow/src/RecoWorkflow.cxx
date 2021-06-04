40f91667a5e96d20eead77e08677b1f7577052a2
Pietro Cortese <pietro.cortese@cern.ch>
2021-06-04 09:37:10 +0200

    Digits reader

---
+++ Detectors/ZDC/workflow/src/RecoWorkflow.cxx
@@ -0,0 +1,38 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// @file   RecoWorkflow.cxx
+
+#include "FT0Workflow/RecoWorkflow.h"
+#include "FT0Workflow/DigitReaderSpec.h"
+#include "FT0Workflow/RecPointWriterSpec.h"
+#include "FT0Workflow/ReconstructionSpec.h"
+#include "FT0Workflow/FT0DataReaderDPLSpec.h"
+
+namespace o2
+{
+namespace zdc
+{
+
+framework::WorkflowSpec getRecoWorkflow(bool useMC, bool disableRootInp, bool disableRootOut)
+{
+  framework::WorkflowSpec specs;
+  if (!disableRootInp) {
+    specs.emplace_back(o2::zdc::getDigitReaderSpec(useMC));
+  }
+  specs.emplace_back(o2::zdc::getReconstructionSpec(useMC));
+  if (!disableRootOut) {
+    specs.emplace_back(o2::zdc::getRecPointWriterSpec(useMC));
+  }
+  return specs;
+}
+
+} // namespace zdc
+} // namespace o2
