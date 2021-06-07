f4239ec9939cb1ee131ff2b4efa88fac8d5299b9
Vít Kučera <vit.kucera@cern.ch>
2021-06-07 17:16:03 +0200

    Add MID and RICH in TrackSelectorPID (#6333)
    * Fix format and TPC in selector
    
    * Add MID table and functions in track selector
    
    * Add RICH in TrackSelectorPID
    
    * Add debugging messages
    
    * Define a single table for ALICE 3 PID track indices
    
    * Mute debugging messages
    
    * Accept only both accepted tracks for MID
    
    * Apply clang-format

--- Analysis/Tasks/PWGHF/HFJpsiToEECandidateSelector.cxx
+++ Analysis/Tasks/PWGHF/HFJpsiToEECandidateSelector.cxx
@@ -15,29 +15,31 @@
 /// \author Nima Zardoshti <nima.zardoshti@cern.ch>, CERN
 /// \author Vít Kučera <vit.kucera@cern.ch>, CERN
 
-#include "ALICE3Analysis/RICH.h"
-#include "AnalysisCore/TrackSelectorPID.h"
-#include "AnalysisDataModel/HFCandidateSelectionTables.h"
-#include "AnalysisDataModel/HFSecondaryVertex.h"
-#include "Framework/AnalysisTask.h"
 #include "Framework/runDataProcessing.h"
+#include "Framework/AnalysisTask.h"
+#include "AnalysisDataModel/HFSecondaryVertex.h"
+#include "AnalysisDataModel/HFCandidateSelectionTables.h"
+#include "AnalysisCore/TrackSelectorPID.h"
+#include "ALICE3Analysis/RICH.h"
+#include "ALICE3Analysis/MID.h"
 
 using namespace o2;
 using namespace o2::framework;
 using namespace o2::aod::hf_cand_prong2;
 using namespace o2::analysis::hf_cuts_jpsi_toee;
 
+/*
 namespace o2::aod
 {
 
-namespace indices
+namespace indices_rich
 {
 DECLARE_SOA_INDEX_COLUMN(Track, track);
 DECLARE_SOA_INDEX_COLUMN(RICH, rich);
-} // namespace indices
+} // namespace indices_rich
 
 DECLARE_SOA_INDEX_TABLE_USER(RICHTracksIndex, Tracks, "RICHTRK",
-                             indices::TrackId, indices::RICHId);
+                             indices_rich::TrackId, indices_rich::RICHId);
 } // namespace o2::aod
 
 struct RICHindexbuilder {
@@ -45,47 +47,70 @@ struct RICHindexbuilder {
   void init(o2::framework::InitContext&) {}
 };
 
+namespace o2::aod
+{
+
+namespace indices_mid
+{
+DECLARE_SOA_INDEX_COLUMN(Track, track);
+DECLARE_SOA_INDEX_COLUMN(MID, mid);
+} // namespace indices_mid
+
+DECLARE_SOA_INDEX_TABLE_USER(MIDTracksIndex, Tracks, "MIDTRK",
+                             indices_mid::TrackId, indices_mid::MIDId);
+} // namespace o2::aod
+
+struct MidIndexBuilder {
+  Builds<o2::aod::MIDTracksIndex> ind;
+  void init(o2::framework::InitContext&) {}
+};
+*/
+
+namespace o2::aod
+{
+
+namespace hf_track_index_alice3_pid
+{
+DECLARE_SOA_INDEX_COLUMN(Track, track); //!
+DECLARE_SOA_INDEX_COLUMN(RICH, rich);   //!
+DECLARE_SOA_INDEX_COLUMN(MID, mid);     //!
+} // namespace hf_track_index_alice3_pid
+
+DECLARE_SOA_INDEX_TABLE_USER(HfTrackIndexALICE3PID, Tracks, "HFTRKIDXA3PID", //!
+                             hf_track_index_alice3_pid::TrackId,
+                             hf_track_index_alice3_pid::RICHId,
+                             hf_track_index_alice3_pid::MIDId);
+} // namespace o2::aod
+
+struct Alice3PidIndexBuilder {
+  Builds<o2::aod::HfTrackIndexALICE3PID> index;
+  void init(o2::framework::InitContext&) {}
+};
+
 /// Struct for applying J/ψ → e+ e− selection cuts
 struct HFJpsiToEECandidateSelector {
   Produces<aod::HFSelJpsiToEECandidate> hfSelJpsiToEECandidate;
 
-  Configurable<double> d_pTCandMin{"d_pTCandMin", 0.,
-                                   "Lower bound of candidate pT"};
-  Configurable<double> d_pTCandMax{"d_pTCandMax", 50.,
-                                   "Upper bound of candidate pT"};
+  Configurable<double> d_pTCandMin{"d_pTCandMin", 0., "Lower bound of candidate pT"};
+  Configurable<double> d_pTCandMax{"d_pTCandMax", 50., "Upper bound of candidate pT"};
   // TPC
-  Configurable<double> d_pidTPCMinpT{"d_pidTPCMinpT", 0.15,
-                                     "Lower bound of track pT for TPC PID"};
-  Configurable<double> d_pidTPCMaxpT{"d_pidTPCMaxpT", 10.,
-                                     "Upper bound of track pT for TPC PID"};
+  Configurable<double> d_pidTPCMinpT{"d_pidTPCMinpT", 0.15, "Lower bound of track pT for TPC PID"};
+  Configurable<double> d_pidTPCMaxpT{"d_pidTPCMaxpT", 10., "Upper bound of track pT for TPC PID"};
   Configurable<double> d_nSigmaTPC{"d_nSigmaTPC", 3., "Nsigma cut on TPC only"};
-
-  Configurable<double> d_pidTOFMinpT{"d_pidTOFMinpT", 0.15,
-                                     "Lower bound of track pT for TOF PID"};
-  Configurable<double> d_pidTOFMaxpT{"d_pidTOFMaxpT", 5.,
-                                     "Upper bound of track pT for TOF PID"};
+  //Configurable<double> d_TPCNClsFindablePIDCut{"d_TPCNClsFindablePIDCut", 70., "Lower bound of TPC findable clusters for good PID"};
+  // TOF
+  Configurable<double> d_pidTOFMinpT{"d_pidTOFMinpT", 0.15, "Lower bound of track pT for TOF PID"};
+  Configurable<double> d_pidTOFMaxpT{"d_pidTOFMaxpT", 5., "Upper bound of track pT for TOF PID"};
   Configurable<double> d_nSigmaTOF{"d_nSigmaTOF", 3., "Nsigma cut on TOF only"};
+  Configurable<double> d_nSigmaTOFCombined{"d_nSigmaTOFCombined", 5., "Nsigma cut on TOF combined with TPC"};
+  // RICH
+  Configurable<double> d_pidRICHMinpT{"d_pidRICHMinpT", 0.15, "Lower bound of track pT for RICH PID"};
+  Configurable<double> d_pidRICHMaxpT{"d_pidRICHMaxpT", 10., "Upper bound of track pT for RICH PID"};
+  Configurable<double> d_nSigmaRICH{"d_nSigmaRICH", 3., "Nsigma cut on RICH only"};
 
-  Configurable<double> d_pidRICHMinpT{"d_pidRICHMinpT", 0.15,
-                                      "Lower bound of track pT for RICH PID"};
-  Configurable<double> d_pidRICHMaxpT{"d_pidRICHMaxpT", 10.,
-                                      "Upper bound of track pT for RICH PID"};
-  Configurable<double> d_nSigmaRICH{"d_nSigmaRICH", 3.,
-                                    "Nsigma cut on RICH only"};
-  Configurable<double> d_nSigmaTOFCombined{
-    "d_nSigmaTOFCombined", 5., "Nsigma cut on TOF combined with TPC"};
-  // Configurable<double>
-  // d_TPCNClsFindablePIDCut{"d_TPCNClsFindablePIDCut", 70., "Lower bound of TPC
-  // findable clusters for good PID"};
   // topological cuts
-  Configurable<std::vector<double>> pTBins{
-    "pTBins", std::vector<double>{hf_cuts_jpsi_toee::pTBins_v},
-    "pT bin limits"};
-  Configurable<LabeledArray<double>> cuts{
-    "Jpsi_to_ee_cuts",
-    {hf_cuts_jpsi_toee::cuts[0], npTBins, nCutVars, pTBinLabels,
-     cutVarLabels},
-    "Jpsi candidate selection per pT bin"};
+  Configurable<std::vector<double>> pTBins{"pTBins", std::vector<double>{hf_cuts_jpsi_toee::pTBins_v}, "pT bin limits"};
+  Configurable<LabeledArray<double>> cuts{"Jpsi_to_ee_cuts", {hf_cuts_jpsi_toee::cuts[0], npTBins, nCutVars, pTBinLabels, cutVarLabels}, "Jpsi candidate selection per pT bin"};
 
   /// Conjugate-independent topological cuts
   /// \param candidate is candidate
@@ -93,8 +118,7 @@ struct HFJpsiToEECandidateSelector {
   /// \param trackElectron is the track with the electron hypothesis
   /// \return true if candidate passes all cuts
   template <typename T1, typename T2>
-  bool selectionTopol(const T1& candidate, const T2& trackPositron,
-                      const T2& trackElectron)
+  bool selectionTopol(const T1& candidate, const T2& trackPositron, const T2& trackElectron)
   {
     auto candpT = candidate.pt();
     auto pTBin = findBin(pTBins, candpT);
@@ -108,104 +132,96 @@ struct HFJpsiToEECandidateSelector {
     }
 
     // cut on invariant mass
-    if (std::abs(InvMassJpsiToEE(candidate) -
-                 RecoDecay::getMassPDG(pdg::Code::kJpsi)) >
-        cuts->get(pTBin, "m")) {
+    if (std::abs(InvMassJpsiToEE(candidate) - RecoDecay::getMassPDG(pdg::Code::kJpsi)) > cuts->get(pTBin, "m")) {
       return false;
     }
 
     // cut on daughter pT
-    if (trackElectron.pt() < cuts->get(pTBin, "pT El") ||
-        trackPositron.pt() < cuts->get(pTBin, "pT El")) {
+    if (trackElectron.pt() < cuts->get(pTBin, "pT El") || trackPositron.pt() < cuts->get(pTBin, "pT El")) {
       return false;
     }
 
     // cut on daughter DCA - need to add secondary vertex constraint here
-    if (std::abs(trackElectron.dcaPrim0()) > cuts->get(pTBin, "DCA_xy") ||
-        std::abs(trackPositron.dcaPrim0()) > cuts->get(pTBin, "DCA_xy")) {
+    if (std::abs(trackElectron.dcaPrim0()) > cuts->get(pTBin, "DCA_xy") || std::abs(trackPositron.dcaPrim0()) > cuts->get(pTBin, "DCA_xy")) {
       return false;
     }
 
     // cut on daughter DCA - need to add secondary vertex constraint here
-    if (std::abs(trackElectron.dcaPrim1()) > cuts->get(pTBin, "DCA_z") ||
-        std::abs(trackPositron.dcaPrim1()) > cuts->get(pTBin, "DCA_z")) {
+    if (std::abs(trackElectron.dcaPrim1()) > cuts->get(pTBin, "DCA_z") || std::abs(trackPositron.dcaPrim1()) > cuts->get(pTBin, "DCA_z")) {
       return false;
     }
 
     // cut on chi2 point of closest approach
-    if (std::abs(candidate.chi2PCA()) > cuts->get(pTBin, "chi2PCA")){
-      return false;
-    }
-    return true;
-  }
-  template <typename T>
-  bool validRICHPID(const T& track)
-  {
-    if (track.pt() < d_pidRICHMinpT || track.pt() >= d_pidRICHMaxpT) {
+    if (std::abs(candidate.chi2PCA()) > cuts->get(pTBin, "chi2PCA")) {
       return false;
     }
     return true;
   }
 
-  using Trks = soa::Join<aod::BigTracksPID, aod::RICHTracksIndex>;
-  void process(aod::HfCandProng2 const& candidates, const Trks& tracks,
-               aod::RICHs const&)
+  using TracksPID = soa::Join<aod::BigTracksPID, aod::HfTrackIndexALICE3PID>;
+
+  void process(aod::HfCandProng2 const& candidates, TracksPID const&, aod::RICHs const&, aod::MIDs const&)
   {
     TrackSelectorPID selectorElectron(kElectron);
+    selectorElectron.setRangePtTPC(d_pidTPCMinpT, d_pidTPCMaxpT);
+    selectorElectron.setRangeNSigmaTPC(-d_nSigmaTPC, d_nSigmaTPC);
     selectorElectron.setRangePtTOF(d_pidTOFMinpT, d_pidTOFMaxpT);
     selectorElectron.setRangeNSigmaTOF(-d_nSigmaTOF, d_nSigmaTOF);
-    selectorElectron.setRangeNSigmaTOFCondTPC(-d_nSigmaTOFCombined,
-                                              d_nSigmaTOFCombined);
+    selectorElectron.setRangeNSigmaTOFCondTPC(-d_nSigmaTOFCombined, d_nSigmaTOFCombined);
+    selectorElectron.setRangePtRICH(d_pidRICHMinpT, d_pidRICHMaxpT);
+    selectorElectron.setRangeNSigmaRICH(-d_nSigmaRICH, d_nSigmaRICH);
+
+    TrackSelectorPID selectorMuon(kMuonMinus);
+
     // looping over 2-prong candidates
     for (auto& candidate : candidates) {
 
-      auto trackPos = candidate.index0_as<Trks>(); // positive daughter
-      auto trackNeg = candidate.index1_as<Trks>(); // negative daughter
+      auto trackPos = candidate.index0_as<TracksPID>(); // positive daughter
+      auto trackNeg = candidate.index1_as<TracksPID>(); // negative daughter
 
       if (!(candidate.hfflag() & 1 << DecayType::JpsiToEE)) {
         hfSelJpsiToEECandidate(0);
         continue;
       }
 
-      // track selection level need to add special cuts (additional cuts on
-      // decay length and d0 norm)
+      // track selection level need to add special cuts (additional cuts on decay length and d0 norm)
 
       if (!selectionTopol(candidate, trackPos, trackNeg)) {
         hfSelJpsiToEECandidate(0);
         continue;
       }
 
-      // track-level PID selection
-      if (selectorElectron.getStatusTrackPIDTOF(trackPos) ==
-            TrackSelectorPID::Status::PIDRejected ||
-          selectorElectron.getStatusTrackPIDTOF(trackNeg) ==
-            TrackSelectorPID::Status::PIDRejected) {
+      // track-level PID TPC selection
+      // FIXME: temporarily disabled for ALICE 3 development
+      //if (selectorElectron.getStatusTrackPIDTPC(trackPos) == TrackSelectorPID::Status::PIDRejected ||
+      //    selectorElectron.getStatusTrackPIDTPC(trackNeg) == TrackSelectorPID::Status::PIDRejected) {
+      //  hfSelJpsiToEECandidate(0);
+      //  continue;
+      //}
+
+      // track-level PID TOF selection
+      if (selectorElectron.getStatusTrackPIDTOF(trackPos) == TrackSelectorPID::Status::PIDRejected ||
+          selectorElectron.getStatusTrackPIDTOF(trackNeg) == TrackSelectorPID::Status::PIDRejected) {
         hfSelJpsiToEECandidate(0);
         continue;
       }
 
-      bool pidrichPos = validRICHPID(trackPos);
-      bool pidrichNeg = validRICHPID(trackNeg);
-      if (pidrichPos && pidrichNeg) {
-        //         LOGF(info, "both good rich tracks");
+      // track-level PID RICH selection
+      if (selectorElectron.getStatusTrackPIDRICH(trackPos) != TrackSelectorPID::Status::PIDAccepted ||
+          selectorElectron.getStatusTrackPIDRICH(trackNeg) != TrackSelectorPID::Status::PIDAccepted) {
+        hfSelJpsiToEECandidate(0);
+        continue;
       }
 
-      //       LOGF(info, "cut %f, val pos %f, val neg %f", d_nSigmaRICH,
-      //       trackPos.rich().richNsigmaEl(), trackNeg.rich().richNsigmaEl());
-      if (pidrichPos && pidrichNeg) {
-        bool selpidrichPos = abs(trackPos.rich().richNsigmaEl()) < d_nSigmaRICH;
-        bool selpidrichNeg = abs(trackNeg.rich().richNsigmaEl()) < d_nSigmaRICH;
-        //          LOGF(info, "pos %d, neg %d", selpidrichPos, selpidrichNeg);
-        bool selpidrich = selpidrichPos && selpidrichNeg;
-        if (selpidrich) {
-          //            LOGF(info, "both sel rich tracks");
-        }
-        if (!selpidrich) {
-          hfSelJpsiToEECandidate(0);
-          //            LOGF(info, "not selectedtracks");
-          continue;
-        }
+      // track-level muon PID MID selection
+      //LOGF(info, "Muon selection: Start");
+      if (selectorMuon.getStatusTrackPIDMID(trackPos) != TrackSelectorPID::Status::PIDAccepted ||
+          selectorMuon.getStatusTrackPIDMID(trackNeg) != TrackSelectorPID::Status::PIDAccepted) {
+        //LOGF(info, "Muon selection: Rejected");
+        hfSelJpsiToEECandidate(0);
+        continue;
       }
+      //LOGF(info, "Muon selection: Selected");
 
       hfSelJpsiToEECandidate(1);
     }
@@ -214,7 +230,9 @@ struct HFJpsiToEECandidateSelector {
 
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
 {
-  return WorkflowSpec{adaptAnalysisTask<RICHindexbuilder>(cfgc),
-                      adaptAnalysisTask<HFJpsiToEECandidateSelector>(
-                        cfgc, TaskName{"hf-jpsi-toee-candidate-selector"})};
+  return WorkflowSpec{
+    //adaptAnalysisTask<RICHindexbuilder>(cfgc),
+    //adaptAnalysisTask<MidIndexBuilder>(cfgc),
+    adaptAnalysisTask<Alice3PidIndexBuilder>(cfgc),
+    adaptAnalysisTask<HFJpsiToEECandidateSelector>(cfgc, TaskName{"hf-jpsi-toee-candidate-selector"})};
 }
