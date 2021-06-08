b8f0760951552c7adf40d12b795c40fc736c87ad
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2021-06-08 18:15:06 +0200

    Update Framework/Core/include/Framework/ServiceRegistryHelpers.h
    Co-authored-by: Matthias Richter <github@scieq.net>

--- Framework/Core/include/Framework/ServiceRegistryHelpers.h
+++ Framework/Core/include/Framework/ServiceRegistryHelpers.h
@@ -56,7 +56,7 @@ struct ServiceRegistryHelpers {
     // We need type elision as we do not want to know all the services in
     // advance
     static_assert(std::is_const_v<I> == true,
-                  "Service interface must be const");
+                  "Service interface must be const if service object is const");
     static_assert(std::is_base_of<I, C>::value == true,
                   "Registered service is not derived from declared interface");
     constexpr auto typeHash = TypeIdHelpers::uniqueId<I>();
