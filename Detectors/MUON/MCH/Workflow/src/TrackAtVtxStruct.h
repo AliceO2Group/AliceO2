71a09d8ac8fd0aedca14ce403b60236cab4500ea
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2021-06-15 14:49:37 +0200

    [MRRTF-126] Add MCH convenience reco workflow
    To e.g. ease inclusion in full system test
    
    The TrackFinder and TrackAtVertex workflow get a new option `--grp-file`
     to read field data from GRP by default.
    
    For debugging/expert usage and regain previous behaviour, you have to
    point `--grp-file` to a non-existing file.
    
    Also introduce `o2-mch-tracks-file-dumper` to get a textual dump of
     (intermediate) track binary files.

---
+++ Detectors/MUON/MCH/Workflow/src/TrackAtVtxStruct.h
@@ -0,0 +1,27 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#ifndef O2_MCH_TRACK_AT_VTX_STRUCT_H
+#define O2_MCH_TRACK_AT_VTX_STRUCT_H
+
+#include "MCHBase/TrackBlock.h"
+
+namespace o2::mch
+{
+struct TrackAtVtxStruct {
+  TrackParamStruct paramAtVertex{};
+  double dca = 0.;
+  double rAbs = 0.;
+  int mchTrackIdx = 0;
+};
+} // namespace o2::mch
+
+#endif
