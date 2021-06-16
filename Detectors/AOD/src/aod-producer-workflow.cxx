fc78990b99c6108d16e91d9fd68288507a8e6113
Nazar Burmasov <nazar.burmasov@cern.ch>
2021-06-16 14:49:50 +0300

    Switch to RecoContainer for information fetching  (#6170)
    * Remove a workaround for direct root-file writing
    
    * Switch to RecoContainer for data subscription.
    
    * Merge latest updates from #6120 and #6177
    
    * Fix CMakeLists
    
    * Fix BC IDs for FT0s, MC collisions and reconstructed vertices.
    
    * Add missing data request for FT0
    
    * Fix for a typo in FT0 RecPointReaderSpec.cxx
    
    * Revert "Fix for a typo in FT0 RecPointReaderSpec.cxx"
    
    This reverts commit 8b8389b7604d99310510c6b50ffe5590cc105f8f.
    
    * Remove unused code in collectBCs()
    
    * Merge changes from #6623: tracks sorting
    
    * Fix several typos in code
    
    * Rework converter to address commented issues
    
    * Cleanups and improvements according to suggestions

--- Detectors/AOD/src/aod-producer-workflow.cxx
+++ Detectors/AOD/src/aod-producer-workflow.cxx
@@ -8,18 +8,26 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-#include "AODProducerWorkflow/AODProducerWorkflow.h"
-#include "CommonUtils/ConfigurableParam.h"
+#include "AODProducerWorkflow/AODProducerWorkflowSpec.h"
 #include "Framework/CompletionPolicy.h"
+#include "ReconstructionDataFormats/GlobalTrackID.h"
+#include "CommonUtils/ConfigurableParam.h"
+#include "GlobalTrackingWorkflowHelpers/InputHelper.h"
+#include "DetectorsCommonDataFormats/DetID.h"
 
 using namespace o2::framework;
+using GID = o2::dataformats::GlobalTrackID;
+using DetID = o2::detectors::DetID;
 
 void customize(std::vector<ConfigParamSpec>& workflowOptions)
 {
   // option allowing to set parameters
   std::vector<o2::framework::ConfigParamSpec> options{
-    {"ignore-aod-writer", VariantType::Int, 0, {"Ignore DPL AOD writer and write tables directly into a file. 0 -- off, != 0 -- on"}},
-    {"configKeyValues", VariantType::String, "", {"Semicolon separated key=value strings"}}};
+    {"disable-root-input", o2::framework::VariantType::Bool, false, {"disable root-files input reader"}},
+    {"disable-root-output", o2::framework::VariantType::Bool, false, {"disable root-files output writer"}},
+    {"disable-mc", o2::framework::VariantType::Bool, false, {"disable MC propagation"}},
+    {"fill-svertices", o2::framework::VariantType::Bool, false, {"fill V0 table"}},
+    {"configKeyValues", VariantType::String, "", {"Semicolon separated key=value strings ..."}}};
 
   std::swap(workflowOptions, options);
 }
@@ -29,7 +37,21 @@ void customize(std::vector<ConfigParamSpec>& workflowOptions)
 WorkflowSpec defineDataProcessing(ConfigContext const& configcontext)
 {
   o2::conf::ConfigurableParam::updateFromString(configcontext.options().get<std::string>("configKeyValues"));
-  //  o2::conf::ConfigurableParam::updateFromString(configcontext.options().get<std::string>("ignore-aod-writer"));
-  int ignoreWriter = configcontext.options().get<int>("ignore-aod-writer");
-  return std::move(o2::aodproducer::getAODProducerWorkflow(ignoreWriter));
+  auto fillSVertices = configcontext.options().get<bool>("fill-svertices");
+  auto useMC = !configcontext.options().get<bool>("disable-mc");
+
+  GID::mask_t src = GID::getSourcesMask("ITS,MFT,TPC,ITS-TPC,ITS-TPC-TOF,TPC-TOF,FT0"); // asking only for currently processed sources
+  GID::mask_t dummy, srcClus = GID::includesDet(DetID::TOF, src) ? GID::getSourceMask(GID::TOF) : dummy;
+
+  WorkflowSpec specs;
+
+  specs.emplace_back(o2::aodproducer::getAODProducerWorkflowSpec(src, useMC, fillSVertices));
+
+  o2::globaltracking::InputHelper::addInputSpecs(configcontext, specs, srcClus, src, src, useMC, srcClus);
+  o2::globaltracking::InputHelper::addInputSpecsPVertex(configcontext, specs, useMC);
+  if (fillSVertices) {
+    o2::globaltracking::InputHelper::addInputSpecsSVertex(configcontext, specs);
+  }
+
+  return std::move(specs);
 }
