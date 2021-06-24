b925e311e0f7c21529a1470e7616b692a0dbf11e
Mario Krüger <mario.kruger@cern.ch>
2021-06-24 18:15:21 +0200

    rebase and update copyright notice

--- Analysis/Tutorials/src/histogramRegistry.cxx
+++ Analysis/Tutorials/src/histogramRegistry.cxx
@@ -18,6 +18,8 @@
 #include "Framework/HistogramRegistry.h"
 #include <TParameter.h>
 
+#include "Framework/StaticFor.h"
+
 using namespace o2;
 using namespace o2::framework;
 
@@ -175,9 +177,17 @@ struct RealisticExample {
     // clone single histograms
     spectra.addClone("sigmas", "cascades");
     spectra.addClone("neutral/pions", "strange/funny/particles");
+
+    // add some histograms for different trigger settings
+    spectra.add("INT7/pt", "pt", kTH1F, {ptAxis});
+    spectra.add("INT7/eta", "eta", kTH1F, {etaAxis});
+    spectra.add("INT7/phi", "phi", kTH1F, {phiAxis});
+    spectra.addClone("INT7/", "V0HM/");
+    spectra.addClone("INT7/", "EMC7/");
+    spectra.addClone("INT7/", "EMC8/");
   }
 
-  template <bool mode, typename T>
+  template <int mode, typename T>
   void fillHistos(const T& track)
   {
     static constexpr std::string_view subDir[] = {"before_cuts/", "after_cuts/"};
@@ -203,14 +213,27 @@ struct RealisticExample {
       spectra.fill(HIST("sigmas"), track.pt(), track.eta(), 50., 0.);
       spectra.fill(HIST("lambdas"), track.pt(), track.eta(), 50., 0.);
 
-      // fill histograms before and after cuts
-      fillHistos<false>(track);
+      // fill histograms before and after cuts with a helper function
+      fillHistos<0>(track);
       if (std::rand() > (RAND_MAX / 2)) {
-        fillHistos<true>(track);
+        fillHistos<1>(track);
       }
 
       spectra.fill(HIST("cascades"), track.pt(), track.eta(), 50., 0.);
       spectra.fill(HIST("strange/funny/particles"), track.pt(), track.eta(), 50., 0.);
+
+      // loop for filling multiple histograms with different naming patterns
+      static constexpr std::string_view triggers[] = {"INT7/", "V0HM/", "EMC7/", "EMC8/"};
+      static_for<0, 3>([&](auto i) {
+        constexpr int index = i.value;
+
+        if (std::rand() > (RAND_MAX / 2)) {
+          return; // dummy decision if trigger actually fired
+        }
+        spectra.fill(HIST(triggers[index]) + HIST("pt"), track.pt());
+        spectra.fill(HIST(triggers[index]) + HIST("eta"), track.eta());
+        spectra.fill(HIST(triggers[index]) + HIST("phi"), track.phi());
+      });
     }
   }
 };
