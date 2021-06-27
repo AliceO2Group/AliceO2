1a602f3f548e5745265993910f7234c6852ba102
Raymond Ehlers <raymond.ehlers@gmail.com>
2021-06-27 23:55:58 +0200

    Undo changes to the jet matching index

--- Analysis/Tasks/PWGJE/jetfinderhf.cxx
+++ Analysis/Tasks/PWGJE/jetfinderhf.cxx
@@ -93,7 +93,7 @@ struct JetFinderHFTask {
         }
         if (isHFJet) {
           jetsTable(collision, jet.eta(), jet.phi(), jet.pt(),
-                    jet.area(), jet.E(), jet.m(), -1);
+                    jet.area(), jet.E(), jet.m());
           for (const auto& constituent : jet.constituents()) {
             trackConstituents(jetsTable.lastIndex(), constituent.user_index());
           }
