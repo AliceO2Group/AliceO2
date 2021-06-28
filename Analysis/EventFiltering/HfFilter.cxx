c758a8dc0a3a4ab64c517e09aa3478437bd28158
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2021-06-28 14:42:03 +0200

    Add possibility to apply single-track selections to track used to build beauty candidates

--- Analysis/EventFiltering/HfFilter.cxx
+++ Analysis/EventFiltering/HfFilter.cxx
@@ -9,6 +9,11 @@
 // or submit itself to any jurisdiction.
 // O2 includes
 
+/// \file HfFilter.cxx
+/// \brief task for selection of events with HF signals
+///
+/// \author Fabrizio Grosa <fabrizio.grosa@cern.ch>, CERN
+
 #include "Framework/runDataProcessing.h"
 #include "Framework/AnalysisDataModel.h"
 #include "Framework/AnalysisTask.h"
@@ -19,36 +24,40 @@
 
 #include "AnalysisDataModel/HFSecondaryVertex.h"
 #include "AnalysisDataModel/HFCandidateSelectionTables.h"
-#include "AnalysisCore/TrackSelectorPID.h"
 
 #include <cmath>
 #include <string>
 
-namespace {
+namespace
+{
 
-  enum HfTriggers {
-    kHighPt = 0,
-    kBeauty,
-    kFemto,
-    kNtriggersHF
-  };
+enum HfTriggers {
+  kHighPt = 0,
+  kBeauty,
+  kFemto,
+  kNtriggersHF
+};
 
-  static const std::vector<std::string> HfTriggerNames{"highPt", "beauty", "femto"};
+static const std::vector<std::string> HfTriggerNames{"highPt", "beauty", "femto"};
 
-  static const float massPi = RecoDecay::getMassPDG(211);
-  static const float massK = RecoDecay::getMassPDG(321);
-  static const float massProton = RecoDecay::getMassPDG(2212);
-  static const float massD0 = RecoDecay::getMassPDG(421);
-  static const float massDPlus = RecoDecay::getMassPDG(411);
-  static const float massDs = RecoDecay::getMassPDG(431);
-  static const float massLc = RecoDecay::getMassPDG(4122);
-  static const float massBPlus = RecoDecay::getMassPDG(511);
-  static const float massB0 = RecoDecay::getMassPDG(521);
-  static const float massBs = RecoDecay::getMassPDG(531);
-  static const float massLb = RecoDecay::getMassPDG(5122);
+enum BeautyCandType {
+  kBeauty3Prong = 0, // combination of charm 2-prong and pion
+  kBeauty4Prong      // combination of charm 3-prong and pion
+};
+
+static const float massPi = RecoDecay::getMassPDG(211);
+static const float massK = RecoDecay::getMassPDG(321);
+static const float massProton = RecoDecay::getMassPDG(2212);
+static const float massD0 = RecoDecay::getMassPDG(421);
+static const float massDPlus = RecoDecay::getMassPDG(411);
+static const float massDs = RecoDecay::getMassPDG(431);
+static const float massLc = RecoDecay::getMassPDG(4122);
+static const float massBPlus = RecoDecay::getMassPDG(511);
+static const float massB0 = RecoDecay::getMassPDG(521);
+static const float massBs = RecoDecay::getMassPDG(531);
+static const float massLb = RecoDecay::getMassPDG(5122);
 
 } // namespace
-
 
 using namespace o2;
 using namespace o2::framework;
@@ -56,27 +65,60 @@ using namespace o2::framework::expressions;
 using namespace o2::aod::hf_cand;
 using namespace o2::aod::hf_cand_prong2;
 using namespace o2::aod::hf_cand_prong3;
+using namespace o2::analysis::hf_cuts_single_track;
 
 struct HfFilter {
 
   Produces<aod::HfFilters> tags;
 
-  Configurable<float> ptThreshold2Prong{"ptThreshold2Prong", 5., "pT treshold for high pT 2-prong candidates for kHighPt triggers in GeV/c"};
-  Configurable<float> ptThreshold3Prong{"ptThreshold3Prong", 5., "pT treshold for high pT 3-prong candidates for kHighPt triggers in GeV/c"};
+  // parameters for high-pT triggers
+  Configurable<float> pTThreshold2Prong{"pTThreshold2Prong", 5., "pT treshold for high pT 2-prong candidates for kHighPt triggers in GeV/c"};
+  Configurable<float> pTThreshold3Prong{"pTThreshold3Prong", 5., "pT treshold for high pT 3-prong candidates for kHighPt triggers in GeV/c"};
+
+  // parameters for beauty triggers
   Configurable<float> deltaMassBPlus{"deltaMassBPlus", 0.3, "invariant-mass delta with respect to the B+ mass"};
   Configurable<float> deltaMassB0{"deltaMassB0", 0.3, "invariant-mass delta with respect to the B0 mass"};
   Configurable<float> deltaMassBs{"deltaMassBs", 0.3, "invariant-mass delta with respect to the Bs mass"};
   Configurable<float> deltaMassLb{"deltaMassLb", 0.3, "invariant-mass delta with respect to the Lb mass"};
+  Configurable<std::vector<double>> pTBinsTrack{"pTBinsTrack", std::vector<double>{hf_cuts_single_track::pTBinsTrack_v}, "track pT bin limits for DCAXY pT-depentend cut"};
+  Configurable<LabeledArray<double>> cutsTrackBeauty3Prong{"cutsTrackBeauty3Prong", {hf_cuts_single_track::cutsTrack[0], npTBinsTrack, nCutVarsTrack, pTBinLabelsTrack, cutVarLabelsTrack}, "Single-track selections per pT bin for 3-prong beauty candidates"};
+  Configurable<LabeledArray<double>> cutsTrackBeauty4Prong{"cutsTrackBeauty4Prong", {hf_cuts_single_track::cutsTrack[0], npTBinsTrack, nCutVarsTrack, pTBinLabelsTrack, cutVarLabelsTrack}, "Single-track selections per pT bin for 4-prong beauty candidates"};
+
+  // array of single-track cuts for pion
+  std::array<LabeledArray<double>, 2> cutsSingleTrackBeauty;
 
   HistogramRegistry registry{"registry", {}, OutputObjHandlingPolicy::AnalysisObject, true, true};
 
   void init(o2::framework::InitContext&)
   {
+
+    cutsSingleTrackBeauty = {cutsTrackBeauty3Prong, cutsTrackBeauty4Prong};
+
     registry.add("fProcessedEvents", "HF - event filtered", HistType::kTH1F, {{4, -0.5, 4.5, "Event counter"}});
     std::array<std::string, 4> eventTitles = {"rejected", "w/ high-#it{p}_{T} candidate", "w/ beauty candidate", "w/ femto candidate"};
-    for(size_t iBin=0; iBin<eventTitles.size(); iBin++) {
-      registry.get<TH1>(HIST("fProcessedEvents"))->GetXaxis()->SetBinLabel(iBin+1, eventTitles[iBin].data());
+    for (size_t iBin = 0; iBin < eventTitles.size(); iBin++) {
+      registry.get<TH1>(HIST("fProcessedEvents"))->GetXaxis()->SetBinLabel(iBin + 1, eventTitles[iBin].data());
     }
+  }
+
+  /// Single-track cuts for 2-prongs or 3-prongs
+  /// \param track is a track
+  /// \return true if track passes all cuts
+  template <typename T>
+  bool isSelectedTrack(const T& track, const int candType)
+  {
+    auto pTBinTrack = findBin(pTBinsTrack, track.pt());
+    if (pTBinTrack == -1) {
+      return false;
+    }
+
+    if (std::abs(track.dcaPrim0()) < cutsSingleTrackBeauty[candType].get(pTBinTrack, "min_dcaxytoprimary")) {
+      return false; //minimum DCAxy
+    }
+    if (std::abs(track.dcaPrim0()) > cutsSingleTrackBeauty[candType].get(pTBinTrack, "max_dcaxytoprimary")) {
+      return false; //maximum DCAxy
+    }
+    return true;
   }
 
   void process(aod::Collision const& collision,
@@ -88,9 +130,9 @@ struct HfFilter {
     bool keepEvent[kNtriggersHF]{false};
     //
 
-    for (auto& cand2Prong : cand2Prongs) { // start loop over 2 prongs
+    for (const auto& cand2Prong : cand2Prongs) { // start loop over 2 prongs
 
-      if(!TESTBIT(cand2Prong.hfflag(), o2::aod::hf_cand_prong2::DecayType::D0ToPiK)) { // check if it's a D0
+      if (!TESTBIT(cand2Prong.hfflag(), o2::aod::hf_cand_prong2::DecayType::D0ToPiK)) { // check if it's a D0
         continue;
       }
 
@@ -101,36 +143,38 @@ struct HfFilter {
 
       auto pVec2Prong = RecoDecay::PVec(pVecPos, pVecNeg);
       auto pt2Prong = RecoDecay::Pt(pVec2Prong);
-      if(pt2Prong >= ptThreshold2Prong) {
+      if (pt2Prong >= pTThreshold2Prong) {
         keepEvent[kHighPt] = true;
       }
 
-      for (auto track : tracks) { // start loop over tracks
+      for (const auto& track : tracks) { // start loop over tracks
 
-        if(track.globalIndex() == trackPos.globalIndex() || track.globalIndex() == trackNeg.globalIndex()) {
+        if (track.globalIndex() == trackPos.globalIndex() || track.globalIndex() == trackNeg.globalIndex()) {
           continue;
         }
 
         std::array<float, 3> pVecThird = {track.px(), track.py(), track.pz()};
 
-        if(!keepEvent[kBeauty]) {
-          auto massCandB = RecoDecay::M(std::array{pVec2Prong, pVecThird}, std::array{massD0, massPi}); // TODO: retrieve D0-D0bar hypothesis to pair with proper signed track
-          if(abs(massCandB - massBPlus) <= deltaMassBPlus) {
-            keepEvent[kBeauty] = true;
+        if (!keepEvent[kBeauty]) {
+          if (isSelectedTrack(track, kBeauty3Prong)) {
+            auto massCandB = RecoDecay::M(std::array{pVec2Prong, pVecThird}, std::array{massD0, massPi}); // TODO: retrieve D0-D0bar hypothesis to pair with proper signed track
+            if (abs(massCandB - massBPlus) <= deltaMassBPlus) {
+              keepEvent[kBeauty] = true;
+            }
           }
         }
 
         // TODO: add momentum correlation with a track for femto
 
       } // end loop over tracks
-    } // end loop over 2-prong candidates
+    }   // end loop over 2-prong candidates
 
-    for (auto& cand3Prong : cand3Prongs) { // start loop over 2 prongs
+    for (const auto& cand3Prong : cand3Prongs) { // start loop over 3 prongs
 
       bool isDPlus = TESTBIT(cand3Prong.hfflag(), o2::aod::hf_cand_prong3::DecayType::DPlusToPiKPi);
       bool isDs = TESTBIT(cand3Prong.hfflag(), o2::aod::hf_cand_prong3::DecayType::DsToPiKK);
       bool isLc = TESTBIT(cand3Prong.hfflag(), o2::aod::hf_cand_prong3::DecayType::LcToPKPi);
-      if(!isDPlus && !isDs && !isLc) { // check if it's a D+, Ds+ or Lc+ 
+      if (!isDPlus && !isDs && !isLc) { // check if it's a D+, Ds+ or Lc+
         continue;
       }
 
@@ -145,28 +189,28 @@ struct HfFilter {
 
       auto pVec3Prong = RecoDecay::PVec(pVecFirst, pVecSecond, pVecThird);
       auto pt3Prong = RecoDecay::Pt(pVec3Prong);
-      if(pt3Prong >= ptThreshold3Prong) {
+      if (pt3Prong >= pTThreshold3Prong) {
         keepEvent[kHighPt] = true;
       }
 
-      for (auto track : tracks) { // start loop over tracks
+      for (const auto& track : tracks) { // start loop over tracks
 
-        if(track.globalIndex() == trackFirst.globalIndex() || track.globalIndex() == trackSecond.globalIndex() || track.globalIndex() == trackThird.globalIndex()) {
+        if (track.globalIndex() == trackFirst.globalIndex() || track.globalIndex() == trackSecond.globalIndex() || track.globalIndex() == trackThird.globalIndex()) {
           continue;
         }
 
         std::array<float, 3> pVecFourth = {track.px(), track.py(), track.pz()};
 
-        int iHypo=0;
+        int iHypo = 0;
         bool specieCharmHypos[3] = {isDPlus, isDs, isLc};
         float massCharmHypos[3] = {massDPlus, massDs, massLc};
         float massBeautyHypos[3] = {massB0, massBs, massLb};
         float deltaMassHypos[3] = {deltaMassB0, deltaMassBs, deltaMassLb};
-        if(track.signed1Pt() * sign3Prong < 0) {
-          while(!keepEvent[kBeauty] && iHypo < 3) {
-            if(specieCharmHypos[iHypo]) {
+        if (track.signed1Pt() * sign3Prong < 0 && isSelectedTrack(track, kBeauty4Prong)) {
+          while (!keepEvent[kBeauty] && iHypo < 3) {
+            if (specieCharmHypos[iHypo]) {
               auto massCandB = RecoDecay::M(std::array{pVec3Prong, pVecFourth}, std::array{massCharmHypos[iHypo], massPi});
-              if(abs(massCandB - massBeautyHypos[iHypo]) <= deltaMassHypos[iHypo]) {
+              if (abs(massCandB - massBeautyHypos[iHypo]) <= deltaMassHypos[iHypo]) {
                 keepEvent[kBeauty] = true;
               }
             }
@@ -177,17 +221,16 @@ struct HfFilter {
         // TODO: add momentum correlation with a track for femto
 
       } // end loop over tracks
-    } // end loop over 3-prong candidates
+    }   // end loop over 3-prong candidates
 
     tags(keepEvent[kHighPt], keepEvent[kBeauty], keepEvent[kFemto]);
 
-    if(!keepEvent[kHighPt] && !keepEvent[kBeauty] && !keepEvent[kFemto]) {
+    if (!keepEvent[kHighPt] && !keepEvent[kBeauty] && !keepEvent[kFemto]) {
       registry.get<TH1>(HIST("fProcessedEvents"))->Fill(1);
-    }
-    else {
-      for(int iTrigger=0; iTrigger<kNtriggersHF; iTrigger++) {
-        if(keepEvent[iTrigger]) {
-          registry.get<TH1>(HIST("fProcessedEvents"))->Fill(iTrigger+2);
+    } else {
+      for (int iTrigger = 0; iTrigger < kNtriggersHF; iTrigger++) {
+        if (keepEvent[iTrigger]) {
+          registry.get<TH1>(HIST("fProcessedEvents"))->Fill(iTrigger + 2);
         }
       }
     }
