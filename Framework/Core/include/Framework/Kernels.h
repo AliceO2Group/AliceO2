63873742fd9dda1b91abc3a40b6e08f38a94a91e
Anton Alkin <anton.alkin@cern.ch>
2021-06-30 13:43:26 +0200

    Rework slicing algorithm for merged AODs

--- Framework/Core/include/Framework/Kernels.h
+++ Framework/Core/include/Framework/Kernels.h
@@ -30,12 +30,13 @@ namespace o2::framework
 /// @a offset the offset in the original table at which the corresponding
 /// slice was split.
 template <typename T>
-auto sliceByColumn(char const* key,
-                   std::shared_ptr<arrow::Table> const& input,
-                   T fullSize,
-                   std::vector<arrow::Datum>* slices,
-                   std::vector<int32_t>* vals = nullptr,
-                   std::vector<uint64_t>* offsets = nullptr)
+auto sliceByColumn(
+  char const* key,
+  std::shared_ptr<arrow::Table> const& input,
+  T fullSize,
+  std::vector<arrow::Datum>* slices,
+  std::vector<int32_t>* vals = nullptr,
+  std::vector<uint64_t>* offsets = nullptr)
 {
   arrow::Datum value_counts;
   auto options = arrow::compute::CountOptions::Defaults();
@@ -47,7 +48,7 @@ auto sliceByColumn(char const* key,
   auto counts = static_cast<arrow::NumericArray<arrow::Int64Type>>(pair.field(1)->data());
 
   // create slices and offsets
-  auto offset = 0;
+  uint64_t offset = 0;
   auto count = 0;
 
   auto size = values.length();
@@ -57,37 +58,52 @@ auto sliceByColumn(char const* key,
     }
   }
 
-  auto makeSlice = [&](T count) {
-    slices->emplace_back(arrow::Datum{input->Slice(offset, count)});
+  auto makeSlice = [&](uint64_t offset_, T count) {
+    slices->emplace_back(arrow::Datum{input->Slice(offset_, count)});
     if (offsets) {
-      offsets->emplace_back(offset);
+      offsets->emplace_back(offset_);
     }
   };
 
-  auto current = 0;
   auto v = values.Value(0);
-  while (v - current >= 1) {
-    makeSlice(0);
-    ++current;
-  }
-
-  for (auto r = 0; r < size - 1; ++r) {
-    count = counts.Value(r);
-    makeSlice(count);
-    offset += count;
-    auto nextValue = values.Value(r + 1);
-    auto value = values.Value(r);
-    while (nextValue - value > 1) {
-      makeSlice(0);
-      ++value;
+  auto vprev = v;
+  auto vnext = v;
+  for (auto i = 0; i < size - 1; ++i) {
+    vprev = v;
+    v = values.Value(i);
+    count = counts.Value(i);
+    if (v >= 0) {
+      if (vprev < 0 || (v - vprev) == 1) {
+        makeSlice(offset, count);
+        offset += count;
+        continue;
+      } else {
+        while (v - vprev > 1) {
+          makeSlice(offset, 0);
+          ++vprev;
+        }
+        makeSlice(offset, count);
+        offset += count;
+        continue;
+      }
+    } else {
+      vnext = values.Value(i + 1);
+      while (vnext - vprev > 1) {
+        makeSlice(offset, 0);
+        ++vprev;
+      }
+      makeSlice(offset, count);
+      offset += count;
+      continue;
     }
   }
-  makeSlice(counts.Value(size - 1));
+
+  makeSlice(offset, counts.Value(size - 1));
   offset += counts.Value(size - 1);
 
   if (values.Value(size - 1) < fullSize - 1) {
     for (auto v = values.Value(size - 1) + 1; v < fullSize; ++v) {
-      makeSlice(0);
+      makeSlice(offset, 0);
     }
   }
 
