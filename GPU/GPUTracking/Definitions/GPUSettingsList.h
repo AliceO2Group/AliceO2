f9579463b0594f86ada25f1c2959f7e05c08b3a4
David Rohr <drohr@jwdt.org>
2021-07-08 11:16:54 +0200

    GPU: Add tpcIncreasedMinClustersPerRow option

--- GPU/GPUTracking/Definitions/GPUSettingsList.h
+++ GPU/GPUTracking/Definitions/GPUSettingsList.h
@@ -163,6 +163,7 @@ AddOption(disableTPCNoisyPadFilter, bool, false, "", 0, "Disables all TPC noisy 
 AddOption(createO2Output, char, 2, "", 0, "Create Track output in O2 format (2 = skip non-O2 output in GPU track format (reverts to =1 if QA is requested))")
 AddOption(clearO2OutputFromGPU, bool, false, "", 0, "Free the GPU memory used for O2 output after copying to host, prevents further O2 processing on the GPU")
 AddOption(ignoreNonFatalGPUErrors, bool, false, "", 0, "Continue running after having received non fatal GPU errors, e.g. abort due to overflow")
+AddOption(tpcIncreasedMinClustersPerRow, unsigned int, 0, "", 0, "Impose a minimum buffer size for the clustersPerRow during TPC clusterization")
 AddVariable(eventDisplay, GPUCA_NAMESPACE::gpu::GPUDisplayBackend*, nullptr)
 AddSubConfig(GPUSettingsProcessingRTC, rtc)
 AddHelp("help", 'h')
