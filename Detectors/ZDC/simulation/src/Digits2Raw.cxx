4aba1cbb976a66fd829865c86284b2da9e49f7bb
Pietro Cortese <pietro.cortese@cern.ch>
2021-07-19 13:31:38 +0200

    Avoid trivial assignment

--- Detectors/ZDC/simulation/src/Digits2Raw.cxx
+++ Detectors/ZDC/simulation/src/Digits2Raw.cxx
@@ -593,11 +593,10 @@ void Digits2Raw::emptyBunches(std::bitset<3564>& bunchPattern)
   for (int32_t ib = 0; ib < LHCMaxBunches; ib++) {
     int32_t mb = (ib + 31) % LHCMaxBunches; // beam gas from back of calorimeter (31 bc earlier than a colliding bunch)
     int32_t m1 = (ib + 1) % LHCMaxBunches;  // next bunch is colliding
-    int32_t cb = ib;                        // current bunch crossing
     int32_t p1 = (ib - 1) % LHCMaxBunches;  // previous bc is colliding (position +1)
     int32_t p2 = (ib - 2) % LHCMaxBunches;  // 2 bc after collision
     int32_t p3 = (ib - 3) % LHCMaxBunches;  // 3 bc after collision
-    if (bunchPattern[mb] || bunchPattern[m1] || bunchPattern[cb] || bunchPattern[p1] || bunchPattern[p2] || bunchPattern[p3]) {
+    if (bunchPattern[mb] || bunchPattern[m1] || bunchPattern[ib] || bunchPattern[p1] || bunchPattern[p2] || bunchPattern[p3]) {
       mEmpty[ib] = mNEmpty;
     } else {
       mNEmpty++;
