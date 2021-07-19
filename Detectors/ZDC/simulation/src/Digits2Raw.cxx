4b3e2d9986a38d8867708f184f52af2b6c9a7e7e
Pietro Cortese <pietro.cortese@cern.ch>
2021-07-19 13:33:08 +0200

    Better comments

--- Detectors/ZDC/simulation/src/Digits2Raw.cxx
+++ Detectors/ZDC/simulation/src/Digits2Raw.cxx
@@ -592,10 +592,10 @@ void Digits2Raw::emptyBunches(std::bitset<3564>& bunchPattern)
   mNEmpty = 0;
   for (int32_t ib = 0; ib < LHCMaxBunches; ib++) {
     int32_t mb = (ib + 31) % LHCMaxBunches; // beam gas from back of calorimeter (31 bc earlier than a colliding bunch)
-    int32_t m1 = (ib + 1) % LHCMaxBunches;  // next bunch is colliding
-    int32_t p1 = (ib - 1) % LHCMaxBunches;  // previous bc is colliding (position +1)
-    int32_t p2 = (ib - 2) % LHCMaxBunches;  // 2 bc after collision
-    int32_t p3 = (ib - 3) % LHCMaxBunches;  // 3 bc after collision
+    int32_t m1 = (ib + 1) % LHCMaxBunches;  // next bunch is colliding (position -1)
+    int32_t p1 = (ib - 1) % LHCMaxBunches;  // current bc is 1 bc after colliding (position +1)
+    int32_t p2 = (ib - 2) % LHCMaxBunches;  // current bc is 2 bc after colliding
+    int32_t p3 = (ib - 3) % LHCMaxBunches;  // current bc is 3 bc after colliding
     if (bunchPattern[mb] || bunchPattern[m1] || bunchPattern[ib] || bunchPattern[p1] || bunchPattern[p2] || bunchPattern[p3]) {
       mEmpty[ib] = mNEmpty;
     } else {
