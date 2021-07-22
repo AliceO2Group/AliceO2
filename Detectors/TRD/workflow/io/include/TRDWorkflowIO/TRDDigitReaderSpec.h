5cc46e78a687b881cd30816b743cc32738bce085
Ole Schmidt <ole.schmidt@cern.ch>
2021-07-22 19:49:04 +0200

    TRD digit reader cleanup + optional input dir

--- Detectors/TRD/workflow/io/include/TRDWorkflowIO/TRDDigitReaderSpec.h
+++ Detectors/TRD/workflow/io/include/TRDWorkflowIO/TRDDigitReaderSpec.h
@@ -29,23 +29,21 @@ namespace trd
 class TRDDigitReaderSpec : public o2::framework::Task
 {
  public:
-  TRDDigitReaderSpec(int channels, bool useMC) : mUseMC(useMC) {}
+  TRDDigitReaderSpec(bool useMC) : mUseMC(useMC) {}
   ~TRDDigitReaderSpec() override = default;
   void init(o2::framework::InitContext& ic) override;
   void run(o2::framework::ProcessingContext& pc) override;
 
  private:
-  int mState = 0;
   bool mUseMC = false;
   std::unique_ptr<TFile> mFile = nullptr;
-  std::string mInputFileName = "";
   std::string mDigitTreeName = "o2sim";
   std::string mDigitBranchName = "TRDDigit";
   std::string mTriggerRecordBranchName = "TriggerRecord";
   std::string mMCLabelsBranchName = "TRDMCLabels";
 };
 
-o2::framework::DataProcessorSpec getTRDDigitReaderSpec(int channels, bool useMC);
+o2::framework::DataProcessorSpec getTRDDigitReaderSpec(bool useMC);
 
 } // end namespace trd
 } // end namespace o2
