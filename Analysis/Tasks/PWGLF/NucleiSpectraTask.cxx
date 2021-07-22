bfdc9bfad37edf5386b511fe1ad1cdb7e3bd848c
akalweit <Alexander.Philipp.Kalweit@cern.ch>
2021-07-22 10:37:37 +0200

    Preparing nuclei-task for pilot testbeam and MC validation (#6698)

--- Analysis/Tasks/PWGLF/NucleiSpectraTask.cxx
+++ Analysis/Tasks/PWGLF/NucleiSpectraTask.cxx
@@ -25,6 +25,8 @@
 #include "Framework/HistogramRegistry.h"
 
 #include <TLorentzVector.h>
+#include <TMath.h>
+#include <TObjArray.h>
 
 #include <cmath>
 
@@ -32,7 +34,7 @@ using namespace o2;
 using namespace o2::framework;
 using namespace o2::framework::expressions;
 
-struct NucleiSpecraTask {
+struct NucleiSpectraTask {
 
   HistogramRegistry spectra{"spectra", {}, OutputObjHandlingPolicy::AnalysisObject, true, true};
 
@@ -44,24 +46,27 @@ struct NucleiSpecraTask {
     AxisSpec ptAxis = {ptBinning, "#it{p}_{T} (GeV/#it{c})"};
     AxisSpec centAxis = {centBinning, "V0M (%)"};
 
-    spectra.add("fCollZpos", "collision z position", HistType::kTH1F, {{600, -20., +20., "z position (cm)"}});
-    spectra.add("fKeepEvent", "skimming histogram", HistType::kTH1F, {{2, -0.5, +1.5, "true: keep event, false: reject event"}});
-    spectra.add("fTPCsignal", "Specific energy loss", HistType::kTH2F, {{600, -3., 3, "#it{p} (GeV/#it{c})"}, {1400, 0, 1400, "d#it{E} / d#it{X} (a. u.)"}});
-    spectra.add("fTPCcounts", "n-sigma TPC", HistType::kTH2F, {ptAxis, {200, -100., +100., "n#sigma_{He} (a. u.)"}});
+    spectra.add("histRecVtxZData", "collision z position", HistType::kTH1F, {{600, -20., +20., "z position (cm)"}});
+    spectra.add("histKeepEventData", "skimming histogram", HistType::kTH1F, {{2, -0.5, +1.5, "true: keep event, false: reject event"}});
+    spectra.add("histTpcSignalData", "Specific energy loss", HistType::kTH2F, {{600, -6., 6., "#it{p} (GeV/#it{c})"}, {1400, 0, 1400, "d#it{E} / d#it{X} (a. u.)"}});
+    spectra.add("histTofSignalData", "TOF signal", HistType::kTH2F, {{600, -6., 6., "#it{p} (GeV/#it{c})"}, {500, 0.0, 1.0, "#beta (TOF)"}});
+    spectra.add("histTpcNsigmaData", "n-sigma TPC", HistType::kTH2F, {ptAxis, {200, -100., +100., "n#sigma_{He} (a. u.)"}});
+    spectra.add("histDcaVsPtData", "dca vs Pt", HistType::kTH2F, {ptAxis, {400, -0.2, 0.2, "dca"}});
+    spectra.add("histInvMassData", "Invariant mass", HistType::kTH1F, {{600, 5.0, +15., "inv. mass GeV/c^{2}"}});
   }
 
-  Configurable<float> yMin{"yMin", -0.8, "Maximum rapidity"};
-  Configurable<float> yMax{"yMax", 0.8, "Minimum rapidity"};
+  Configurable<float> yMin{"yMin", -0.5, "Maximum rapidity"};
+  Configurable<float> yMax{"yMax", 0.5, "Minimum rapidity"};
 
   Configurable<float> cfgCutVertex{"cfgCutVertex", 10.0f, "Accepted z-vertex range"};
   Configurable<float> cfgCutEta{"cfgCutEta", 0.8f, "Eta range for tracks"};
-  Configurable<float> nsigmacutLow{"nsigmacutLow", -30.0, "Value of the Nsigma cut"};
-  Configurable<float> nsigmacutHigh{"nsigmacutHigh", +3., "Value of the Nsigma cut"};
+  Configurable<float> nsigmacutLow{"nsigmacutLow", -10.0, "Value of the Nsigma cut"};
+  Configurable<float> nsigmacutHigh{"nsigmacutHigh", +10.0, "Value of the Nsigma cut"};
 
   Filter collisionFilter = nabs(aod::collision::posZ) < cfgCutVertex;
   Filter trackFilter = (nabs(aod::track::eta) < cfgCutEta) && (aod::track::isGlobalTrack == (uint8_t) true);
 
-  using TrackCandidates = soa::Filtered<soa::Join<aod::Tracks, aod::TracksExtra, aod::pidTPCFullHe, aod::pidTOFFullHe, aod::TrackSelection>>;
+  using TrackCandidates = soa::Filtered<soa::Join<aod::Tracks, aod::TracksExtra, aod::TracksExtended, aod::pidTPCFullHe, aod::pidTOFFullHe, aod::TrackSelection>>;
 
   void process(soa::Filtered<soa::Join<aod::Collisions, aod::EvSels>>::iterator const& collision, TrackCandidates const& tracks)
   {
@@ -70,37 +75,76 @@ struct NucleiSpecraTask {
     //
     bool keepEvent = kFALSE;
     //
-    spectra.fill(HIST("fCollZpos"), collision.posZ());
+    spectra.fill(HIST("histRecVtxZData"), collision.posZ());
+    //
+    std::vector<TLorentzVector> posTracks;
+    std::vector<TLorentzVector> negTracks;
     //
     for (auto track : tracks) { // start loop over tracks
 
-      TLorentzVector cutVector{};
-      cutVector.SetPtEtaPhiM(track.pt() * 2.0, track.eta(), track.phi(), constants::physics::MassHelium3);
-      if (cutVector.Rapidity() < yMin || cutVector.Rapidity() > yMax) {
+      TLorentzVector lorentzVector{};
+      lorentzVector.SetPtEtaPhiM(track.pt() * 2.0, track.eta(), track.phi(), constants::physics::MassHelium3);
+      if (lorentzVector.Rapidity() < yMin || lorentzVector.Rapidity() > yMax) {
         continue;
       }
       //
       // fill QA histograms
       //
-      spectra.fill(HIST("fTPCsignal"), track.tpcInnerParam() * track.sign(), track.tpcSignal());
-      spectra.fill(HIST("fTPCcounts"), track.tpcInnerParam(), track.tpcNSigmaHe());
+      float nSigmaHe3 = track.tpcNSigmaHe();
+      nSigmaHe3 += 94.222101 * TMath::Exp(-0.905203 * track.tpcInnerParam());
+      //
+      spectra.fill(HIST("histTpcSignalData"), track.tpcInnerParam() * track.sign(), track.tpcSignal());
+      spectra.fill(HIST("histTpcNsigmaData"), track.tpcInnerParam(), nSigmaHe3);
       //
       // check offline-trigger (skimming) condidition
       //
-      if (track.tpcNSigmaHe() > nsigmacutLow && track.tpcNSigmaHe() < nsigmacutHigh) {
+      if (nSigmaHe3 > nsigmacutLow && nSigmaHe3 < nsigmacutHigh) {
         keepEvent = kTRUE;
+        if (track.sign() < 0) {
+          spectra.fill(HIST("histDcaVsPtData"), track.pt(), track.dcaXY());
+        }
+        //
+        // store tracks for invariant mass calculation
+        //
+        if (track.sign() < 0) {
+          negTracks.push_back(lorentzVector);
+        }
+        if (track.sign() > 0) {
+          posTracks.push_back(lorentzVector);
+        }
+        //
+        // calculate beta
+        //
+        if (!track.hasTOF()) {
+          continue;
+        }
+        Float_t tofTime = track.tofSignal();
+        Float_t tofLength = track.length();
+        Float_t beta = tofLength / (TMath::C() * 1e-10 * tofTime);
+        spectra.fill(HIST("histTofSignalData"), track.tpcInnerParam() * track.sign(), beta);
       }
 
     } // end loop over tracks
     //
     // fill trigger (skimming) results
     //
-    spectra.fill(HIST("fKeepEvent"), keepEvent);
+    spectra.fill(HIST("histKeepEventData"), keepEvent);
+    //
+    // calculate invariant mass
+    //
+    for (Int_t iPos = 0; iPos < posTracks.size(); iPos++) {
+      TLorentzVector& vecPos = posTracks[iPos];
+      for (Int_t jNeg = 0; jNeg < negTracks.size(); jNeg++) {
+        TLorentzVector& vecNeg = negTracks[jNeg];
+        TLorentzVector vecMother = vecPos + vecNeg;
+        spectra.fill(HIST("histInvMassData"), vecMother.M());
+      }
+    }
   }
 };
 
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
 {
   return WorkflowSpec{
-    adaptAnalysisTask<NucleiSpecraTask>(cfgc, TaskName{"nuclei-spectra"})};
+    adaptAnalysisTask<NucleiSpectraTask>(cfgc, TaskName{"nuclei-spectra"})};
 }
