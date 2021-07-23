71e9c12e866e3b3970ea77d52f1e70e021c0890b
a-mathis <andreas.mathis@cern.ch>
2021-07-23 14:09:24 +0200

    [o2femtodream] Analysis task for track-track femto (#6686)
    - Introducing
      o PairCleaner - clean up clones
      o CutCulator - resolves the bit-wise encoding of syst. vars.
      o further improvements and bug fixes
      o plenty of documentation
    
    - so long, and thanks for all the fish!

--- Analysis/Tasks/PWGCF/FemtoDream/include/FemtoDream/FemtoDreamCollisionSelection.h
+++ Analysis/Tasks/PWGCF/FemtoDream/include/FemtoDream/FemtoDreamCollisionSelection.h
@@ -19,23 +19,28 @@
 #include "AnalysisCore/TriggerAliases.h"
 #include "Framework/HistogramRegistry.h"
 #include "Framework/Logger.h"
-#include <Rtypes.h>
 
 #include <string>
 #include <iostream>
 
 using namespace o2::framework;
 
-namespace o2::analysis
-{
-namespace femtoDream
+namespace o2::analysis::femtoDream
 {
 
 /// \class FemtoDreamCollisionSelection
-/// \brief Cut class to contain and execute all cuts applied to events
+/// \brief Small selection class to check whether a given collision fulfills the specified selections
 class FemtoDreamCollisionSelection
 {
  public:
+  /// Destructor
+  virtual ~FemtoDreamCollisionSelection() = default;
+
+  /// Pass the selection criteria to the class
+  /// \param zvtxMax Maximal value of the z-vertex
+  /// \param checkTrigger whether or not to check for the trigger alias
+  /// \param trig Requested trigger alias
+  /// \param checkOffline whether or not to check for offline selection criteria
   void setCuts(float zvtxMax, bool checkTrigger, int trig, bool checkOffline)
   {
     mCutsSet = true;
@@ -46,6 +51,7 @@ class FemtoDreamCollisionSelection
   }
 
   /// Initializes histograms for the task
+  /// \param registry Histogram registry to be passed
   void init(HistogramRegistry* registry)
   {
     if (!mCutsSet) {
@@ -56,67 +62,69 @@ class FemtoDreamCollisionSelection
     mHistogramRegistry->add("Event/MultV0M", "; vMultV0M; Entries", kTH1F, {{1000, 0, 1000}});
   }
 
+  /// Print some debug information
   void printCuts()
   {
     std::cout << "Debug information for FemtoDreamCollisionSelection \n Max. z-vertex: " << mZvtxMax << "\n Check trigger: " << mCheckTrigger << "\n Trigger: " << mTrigger << "\n Check offline: " << mCheckOffline << "\n";
   }
 
+  /// Check whether the collisions fulfills the specified selections
+  /// \tparam T type of the collision
+  /// \param col Collision
+  /// \return whether or not the collisions fulfills the specified selections
   template <typename T>
-  bool isSelected(T const& col);
+  bool isSelected(T const& col)
+  {
+    if (std::abs(col.posZ()) > mZvtxMax) {
+      return false;
+    }
 
+    if (mCheckTrigger && col.alias()[mTrigger] != 1) {
+      return false;
+    }
+
+    if (mCheckOffline && col.sel7() != 1) {
+      return false;
+    }
+
+    return true;
+  }
+
+  /// Some basic QA of the event
+  /// \tparam T type of the collision
+  /// \param col Collision
   template <typename T>
-  void fillQA(T const& col);
+  void fillQA(T const& col)
+  {
+    if (mHistogramRegistry) {
+      mHistogramRegistry->fill(HIST("Event/zvtxhist"), col.posZ());
+      mHistogramRegistry->fill(HIST("Event/MultV0M"), col.multV0M());
+    }
+  }
 
   /// \todo to be implemented!
+  /// Compute the sphericity of an event
+  /// Important here is that the filter on tracks does not interfere here!
+  /// In Run 2 we used here global tracks within |eta| < 0.8
+  /// \tparam T1 type of the collision
+  /// \tparam T2 type of the tracks
+  /// \param col Collision
+  /// \param tracks All tracks
+  /// \return value of the sphericity of the event
   template <typename T1, typename T2>
-  float computeSphericity(T1 const& col, T2 const& tracks);
+  float computeSphericity(T1 const& col, T2 const& tracks)
+  {
+    return 2.f;
+  }
 
  private:
-  bool mCutsSet = false;           ///< Protection against running without cuts
-  float mZvtxMax = 999.f;          ///< Maximal deviation from nominal z-vertex (cm)
-  bool mCheckTrigger = false;      ///< Check for trigger
-  triggerAliases mTrigger = kINT7; ///< Trigger to check for
-  bool mCheckOffline = false;      ///< Check for offline criteria (might change)
-
   HistogramRegistry* mHistogramRegistry = nullptr; ///< For QA output
-
-  ClassDefNV(FemtoDreamCollisionSelection, 1);
+  bool mCutsSet = false;                           ///< Protection against running without cuts
+  bool mCheckTrigger = false;                      ///< Check for trigger
+  bool mCheckOffline = false;                      ///< Check for offline criteria (might change)
+  triggerAliases mTrigger = kINT7;                 ///< Trigger to check for
+  float mZvtxMax = 999.f;                          ///< Maximal deviation from nominal z-vertex (cm)
 };
-
-template <typename T>
-void FemtoDreamCollisionSelection::fillQA(T const& col)
-{
-  if (mHistogramRegistry) {
-    mHistogramRegistry->fill(HIST("Event/zvtxhist"), col.posZ());
-    mHistogramRegistry->fill(HIST("Event/MultV0M"), col.multV0M());
-  }
-}
-
-template <typename T>
-bool FemtoDreamCollisionSelection::isSelected(T const& col)
-{
-  if (std::abs(col.posZ()) > mZvtxMax) {
-    return false;
-  }
-
-  if (mCheckTrigger && col.alias()[mTrigger] != 1) {
-    return false;
-  }
-
-  if (mCheckOffline && col.sel7() != 1) {
-    return false;
-  }
-
-  return true;
-}
-
-template <typename T1, typename T2>
-float FemtoDreamCollisionSelection::computeSphericity(T1 const& col, T2 const& tracks)
-{
-  return 2.f;
-}
-
-} /* namespace femtoDream */
-} /* namespace o2::analysis */
+} // namespace o2::analysis::femtoDream
 
 #endif /* ANALYSIS_TASKS_PWGCF_FEMTODREAM_FEMTODREAMCOLLISIONSELECTION_H_ */
--- Analysis/Tasks/PWGCF/FemtoDream/include/FemtoDream/FemtoDreamObjectSelection.h
+++ Analysis/Tasks/PWGCF/FemtoDream/include/FemtoDream/FemtoDreamObjectSelection.h
@@ -10,7 +10,7 @@
 // or submit itself to any jurisdiction.
 
 /// \file FemtoDreamObjectSelection.h
-/// \brief FemtoDreamObjectSelection - Partent class of all selections
+/// \brief FemtoDreamObjectSelection - Parent class of all selections
 /// \author Andi Mathis, TU München, andreas.mathis@ph.tum.de
 
 #ifndef ANALYSIS_TASKS_PWGCF_FEMTODREAM_FEMTODREAMOBJECTSELECTION_H_
@@ -20,10 +20,6 @@
 
 #include "ReconstructionDataFormats/PID.h"
 #include "Framework/HistogramRegistry.h"
-#include <Rtypes.h>
-
-#include <string>
-#include <cmath>
 
 using namespace o2::framework;
 
@@ -34,66 +30,80 @@ namespace femtoDream
 
 /// \class FemtoDreamObjectSelection
 /// \brief Cut class to contain and execute all cuts applied to tracks
-
-template <class T1, class T2>
+/// \todo In principle all cuts that fulfill the getMinimalSelection are done implicitly and can be removed from the vector containing all cuts
+/// \tparam selValDataType Data type used for the selection (float/int/bool/...)
+/// \tparam selVariable Variable used for the selection
+template <class selValDataType, class selVariable>
 class FemtoDreamObjectSelection
 {
  public:
+  /// Destructor
   virtual ~FemtoDreamObjectSelection() = default;
 
-  /// Initializes histograms for the task
-  void init(HistogramRegistry* registry)
-  {
-    mHistogramRegistry = registry;
-  }
-
-  void fillSelectionHistogram(const char* name)
+  /// The selection criteria employed in the child class are written to a histogram
+  /// \tparam part Type of the particle, used as a prefix for the folder in the QAResults.root
+  template <o2::aod::femtodreamparticle::ParticleType part>
+  void fillSelectionHistogram()
   {
     int nBins = mSelections.size();
-    mHistogramRegistry->add(name, "; Cut; Value", kTH1F, {{nBins, 0, static_cast<double>(nBins)}});
-    auto hist = mHistogramRegistry->get<TH1>(HIST("TrackCuts/cuthist"));
+    mHistogramRegistry->add((static_cast<std::string>(o2::aod::femtodreamparticle::ParticleTypeName[part]) + "/cuthist").c_str(), "; Cut; Value", kTH1F, {{nBins, 0, static_cast<double>(nBins)}});
+    auto hist = mHistogramRegistry->get<TH1>(HIST(o2::aod::femtodreamparticle::ParticleTypeName[part]) + HIST("/cuthist"));
     for (size_t i = 0; i < mSelections.size(); ++i) {
       hist->GetXaxis()->SetBinLabel(i + 1, Form("%u", mSelections.at(i).getSelectionVariable()));
       hist->SetBinContent(i + 1, mSelections.at(i).getSelectionValue());
     }
   }
 
+  /// Pass the Configurable of selection values in the analysis task to the selection class
+  /// \tparam T Type of the configurable passed to the function
+  /// \param selVals o2 configurable containing the values employed for the selection
+  /// \param selVar Variable to be employed for the selection
+  /// \param selType Type of the selection to be employed
   template <typename T>
-  void setSelection(T& selVals, T2 selVar, femtoDreamSelection::SelectionType selType)
+  void setSelection(T& selVals, selVariable selVar, femtoDreamSelection::SelectionType selType)
   {
-    std::vector<T1> tmpSelVals = selVals; // necessary due to some features of the Configurable
-    std::vector<FemtoDreamSelection<T1, T2>> tempVec;
-    for (const T1 selVal : tmpSelVals) {
-      tempVec.push_back(FemtoDreamSelection<T1, T2>(selVal, selVar, selType));
+    std::vector<selValDataType> tmpSelVals = selVals; // necessary due to some features of the Configurable
+    std::vector<FemtoDreamSelection<selValDataType, selVariable>> tempVec;
+    for (const selValDataType selVal : tmpSelVals) {
+      tempVec.push_back(FemtoDreamSelection<selValDataType, selVariable>(selVal, selVar, selType));
     }
     setSelection(tempVec);
   }
 
-  void setSelection(std::vector<FemtoDreamSelection<T1, T2>>& sels)
+  /// Pass an std::vector of selection values to the selection class
+  /// \param sels std::vector containing FemtoDreamSelections
+  void setSelection(std::vector<FemtoDreamSelection<selValDataType, selVariable>>& sels)
   {
+    /// First the selection is sorted so that the most open cuts are conducted first
     switch (sels.at(0).getSelectionType()) {
       case (femtoDreamSelection::SelectionType::kUpperLimit):
       case (femtoDreamSelection::SelectionType::kAbsUpperLimit):
-        std::sort(sels.begin(), sels.end(), [](FemtoDreamSelection<T1, T2> a, FemtoDreamSelection<T1, T2> b) {
+        std::sort(sels.begin(), sels.end(), [](FemtoDreamSelection<selValDataType, selVariable> a, FemtoDreamSelection<selValDataType, selVariable> b) {
           return a.getSelectionValue() > b.getSelectionValue();
         });
         break;
       case (femtoDreamSelection::SelectionType::kLowerLimit):
       case (femtoDreamSelection::SelectionType::kAbsLowerLimit):
       case (femtoDreamSelection::SelectionType::kEqual):
-        std::sort(sels.begin(), sels.end(), [](FemtoDreamSelection<T1, T2> a, FemtoDreamSelection<T1, T2> b) {
+        std::sort(sels.begin(), sels.end(), [](FemtoDreamSelection<selValDataType, selVariable> a, FemtoDreamSelection<selValDataType, selVariable> b) {
           return a.getSelectionValue() < b.getSelectionValue();
         });
         break;
     }
+
+    /// Then, the sorted selections are added to the overall container of cuts
     for (const auto sel : sels) {
       mSelections.push_back(sel);
     }
   }
 
-  T1 getMinimalSelection(T2 selVar, femtoDreamSelection::SelectionType selType)
+  /// Retrieve the most open selection of a given selection variable
+  /// \param selVar Selection variable under consideration
+  /// \param selType Type of the selection variable
+  /// \return The most open selection of the selection variable given to the class
+  selValDataType getMinimalSelection(selVariable selVar, femtoDreamSelection::SelectionType selType)
   {
-    T1 minimalSel;
+    selValDataType minimalSel;
     switch (selType) {
       case (femtoDreamSelection::SelectionType::kUpperLimit):
       case (femtoDreamSelection::SelectionType::kAbsUpperLimit):
@@ -128,27 +138,52 @@ class FemtoDreamObjectSelection
     return minimalSel;
   }
 
+  /// The total number of different selections
+  /// \return Total number of selections
   size_t getNSelections()
   {
     return mSelections.size();
   }
 
-  size_t getNSelections(T2 selVar)
+  /// The number of selection of an individual variable
+  /// \param selVar Selection variable under consideration
+  /// \return Number of selection of the individual variable
+  size_t getNSelections(selVariable selVar)
   {
-    size_t counter = 0;
+    return getSelections(selVar).size();
+  }
+
+  /// Obtain the selections of an individual variable
+  /// \param selVar Selection variable under consideration
+  /// \return All selections of the individual variable
+  std::vector<FemtoDreamSelection<selValDataType, selVariable>> getSelections(selVariable selVar)
+  {
+    std::vector<FemtoDreamSelection<selValDataType, selVariable>> selValVec;
     for (auto it : mSelections) {
       if (it.getSelectionVariable() == selVar) {
-        ++counter;
+        selValVec.push_back(it);
       }
     }
-    return counter;
+    return selValVec;
+  }
+
+  /// Retrieve all the different selection variables
+  /// \return std::vector containing all the different selection variables
+  std::vector<selVariable> getSelectionVariables()
+  {
+    std::vector<selVariable> selVarVec;
+    for (auto it : mSelections) {
+      auto selVar = it.getSelectionVariable();
+      if (std::none_of(selVarVec.begin(), selVarVec.end(), [selVar](selVariable a) { return a == selVar; })) {
+        selVarVec.push_back(selVar);
+      }
+    }
+    return selVarVec;
   }
 
  protected:
-  std::vector<FemtoDreamSelection<T1, T2>> mSelections;
-  HistogramRegistry* mHistogramRegistry; ///< For QA output
-
-  ClassDefNV(FemtoDreamObjectSelection, 1);
+  HistogramRegistry* mHistogramRegistry;                                     ///< For QA output
+  std::vector<FemtoDreamSelection<selValDataType, selVariable>> mSelections; ///< Vector containing all selections
 };
 
 } // namespace femtoDream
