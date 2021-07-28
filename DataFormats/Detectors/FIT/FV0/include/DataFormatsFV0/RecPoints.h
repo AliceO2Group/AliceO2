67b7e8daa712e160340c65e8fa3cb75a616ffc2a
Maciej Slupecki <maciej.slupecki@gmail.com>
2021-07-28 20:14:43 +0200

    [FV0] Reconstruction skeleton (#6733)
    * [FV0] Reconstruction skeleton - mainly forwards the digits to rec points + experimental collision time calculation
    
    * RecPoints revision, following Ruben's comments and cleanup
    
    Co-authored-by: Maciej Slupecki <maciej.slupecki@cern.ch>

---
+++ DataFormats/Detectors/FIT/FV0/include/DataFormatsFV0/RecPoints.h
@@ -0,0 +1,94 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file RecPoints.h
+/// \brief Definition of the FV0 RecPoints class
+
+#ifndef ALICEO2_FV0_RECPOINTS_H
+#define ALICEO2_FV0_RECPOINTS_H
+
+#include "CommonDataFormat/InteractionRecord.h"
+#include "CommonDataFormat/TimeStamp.h"
+#include "DataFormatsFV0/ChannelData.h"
+#include "CommonDataFormat/RangeReference.h"
+#include "DataFormatsFV0/BCData.h"
+#include <array>
+#include "Rtypes.h"
+#include <TObject.h>
+#include <gsl/span>
+
+namespace o2
+{
+namespace fv0
+{
+struct ChannelDataFloat {
+
+  int channel = -1;       // channel Id
+  double time = -20000;   // time in ps, 0 at the LHC clk center
+  double charge = -20000; // charge [channels]
+  int adcId = -1;         // QTC chain
+
+  ChannelDataFloat() = default;
+  ChannelDataFloat(int Channel, double Time, double Charge, int AdcId)
+  {
+    channel = Channel;
+    time = Time;
+    charge = Charge;
+    adcId = AdcId;
+  }
+
+  void print() const;
+
+  ClassDefNV(ChannelDataFloat, 1);
+};
+
+class RecPoints
+{
+
+ public:
+  enum TimeTypeIndex : int { TimeFirst,
+                             TimeGlobalMean,
+                             TimeSelectedMean };
+  RecPoints() = default;
+  RecPoints(const std::array<short, 3>& collisiontime, int first, int ne,
+            o2::InteractionRecord iRec, o2::fv0::Triggers triggers)
+    : mCollisionTimePs(collisiontime)
+  {
+    mRef.setFirstEntry(first);
+    mRef.setEntries(ne);
+    mIntRecord = iRec;
+    mTriggers = triggers;
+  }
+  ~RecPoints() = default;
+
+  float getCollisionTime(TimeTypeIndex type) const { return mCollisionTimePs[type]; }
+  float getCollisionFirstTime() const { return getCollisionTime(TimeFirst); }
+  float getCollisionGlobalMeanTime() const { return getCollisionTime(TimeGlobalMean); }
+  float getCollisionSelectedMeanTime() const { return getCollisionTime(TimeSelectedMean); }
+  bool isValidTime(TimeTypeIndex type) const { return getCollisionTime(type) < sDummyCollissionTime; }
+  void setCollisionTime(Float_t time, TimeTypeIndex type) { mCollisionTimePs[type] = time; }
+
+  o2::fv0::Triggers getTrigger() const { return mTriggers; }
+  o2::InteractionRecord getInteractionRecord() const { return mIntRecord; };
+  gsl::span<const ChannelDataFloat> getBunchChannelData(const gsl::span<const ChannelDataFloat> tfdata) const;
+  short static constexpr sDummyCollissionTime = 32767;
+
+ private:
+  o2::dataformats::RangeReference<int, int> mRef;
+  o2::InteractionRecord mIntRecord;
+  o2::fv0::Triggers mTriggers;                                                                                // pattern of triggers  in this BC
+  std::array<short, 3> mCollisionTimePs = {sDummyCollissionTime, sDummyCollissionTime, sDummyCollissionTime}; // in picoseconds
+
+  ClassDefNV(RecPoints, 1);
+};
+} // namespace fv0
+} // namespace o2
+#endif
---
+++ Detectors/FIT/FV0/reconstruction/include/FV0Reconstruction/BaseRecoTask.h
@@ -0,0 +1,57 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file BaseRecoTask.h
+/// \brief Definition of the FV0 reconstruction task
+#ifndef ALICEO2_FV0_BASERECOTASK_H
+#define ALICEO2_FV0_BASERECOTASK_H
+
+#include "DataFormatsFV0/BCData.h"
+#include "DataFormatsFV0/ChannelData.h"
+#include "DataFormatsFV0/RecPoints.h"
+#include "CommonDataFormat/InteractionRecord.h"
+#include "CommonDataFormat/TimeStamp.h"
+//#include "FV0Calibration/FT0ChannelTimeCalibrationObject.h"
+#include "FV0Base/Constants.h"
+#include <gsl/span>
+#include <bitset>
+#include <vector>
+#include <array>
+#include <TGraph.h>
+
+namespace o2
+{
+namespace fv0
+{
+class BaseRecoTask
+{
+  //  using offsetCalib = o2::fv0::FV0ChannelTimeCalibrationObject;
+
+ public:
+  BaseRecoTask() = default;
+  ~BaseRecoTask() = default;
+  o2::fv0::RecPoints process(o2::fv0::BCData const& bcd,
+                             gsl::span<const o2::fv0::ChannelData> inChData,
+                             gsl::span<o2::fv0::ChannelDataFloat> outChData);
+  void FinishTask();
+  //  void SetChannelOffset(o2::fv0::FT0ChannelTimeCalibrationObject* caliboffsets) { mCalibOffset = caliboffsets; };
+  //  void SetSlew(std::array<TGraph, Constants::nFv0Channels>* calibslew) { mCalibSlew = calibslew; };
+  //  int getOffset(int channel, int amp);
+
+ private:
+  //  o2::fv0::FV0ChannelTimeCalibrationObject* mCalibOffset;
+  //  std::array<TGraph, Constants::nFv0Channels>* mCalibSlew;
+
+  ClassDefNV(BaseRecoTask, 3);
+};
+} // namespace fv0
+} // namespace o2
+#endif
