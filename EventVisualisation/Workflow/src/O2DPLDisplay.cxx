4ed41152e048be1f6518f8ad27bd83d1cd2f4dbf
jmyrcha <julian.myrcha@cern.ch>
2021-07-30 21:46:22 +0200

    add parameters to eve workflow (#6767)
    * add parameters to eve workflow
    
    * removed dependency from gpu
    added parameters for maximum number of tracks and host filtering
    
    * reformating
    
    * reformating
    
    * fixed fullCl warning

--- EventVisualisation/Workflow/src/O2DPLDisplay.cxx
+++ EventVisualisation/Workflow/src/O2DPLDisplay.cxx
@@ -9,40 +9,49 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
+/// \file
+/// \author Julian Myrcha
+
 #include "EveWorkflow/O2DPLDisplay.h"
+#include "EveWorkflow/EveConfiguration.h"
 #include "EveWorkflow/FileProducer.h"
+#include "EveWorkflow/EveWorkflowHelper.h"
 #include "EventVisualisationDataConverter/VisualisationTrack.h"
 #include "EventVisualisationDataConverter/VisualisationEvent.h"
-#include "Framework/ConfigParamSpec.h"
+
+#include "DetectorsBase/GeometryManager.h"
 #include "DataFormatsParameters/GRPObject.h"
 #include "DataFormatsGlobalTracking/RecoContainer.h"
-#include "DetectorsCommonDataFormats/NameConf.h"
-#include "DetectorsBase/GeometryManager.h"
-#include "TRDBase/GeometryFlat.h"
-#include "TRDBase/Geometry.h"
-#include "TOFBase/Geo.h"
-#include "ITSBase/GeometryTGeo.h"
-#include "DetectorsBase/Propagator.h"
-#include "GPUO2InterfaceDisplay.h"
-#include "GPUO2InterfaceConfiguration.h"
-#include "TPCFastTransform.h"
-#include "TPCReconstruction/TPCFastTransformHelperO2.h"
-#include "GlobalTrackingWorkflowHelpers/InputHelper.h"
 #include "DataFormatsTPC/WorkflowHelper.h"
-#include "DataFormatsTRD/RecoInputContainer.h"
-#include "GPUWorkflowHelper/GPUWorkflowHelper.h"
 #include "DataFormatsITSMFT/TopologyDictionary.h"
+#include "DetectorsCommonDataFormats/NameConf.h"
+#include "DetectorsBase/Propagator.h"
+#include "Framework/ConfigParamSpec.h"
+#include "GlobalTrackingWorkflowHelpers/InputHelper.h"
+#include "ITSBase/GeometryTGeo.h"
+#include "TRDBase/GeometryFlat.h"
+#include "TOFBase/Geo.h"
+#include "TPCFastTransform.h"
+#include "TRDBase/Geometry.h"
 
+#include <unistd.h>
+#include <climits>
+
+using namespace o2::event_visualisation;
 using namespace o2::framework;
 using namespace o2::dataformats;
 using namespace o2::globaltracking;
-using namespace o2::gpu;
 using namespace o2::tpc;
 using namespace o2::trd;
 
 void customize(std::vector<ConfigParamSpec>& workflowOptions)
 {
   std::vector<o2::framework::ConfigParamSpec> options{
+    {"jsons-folder", VariantType::String, "jsons", {"name of the folder to store json files"}},
+    {"eve-hostname", VariantType::String, "", {"name of the host allowed to produce files (empty means no limit)"}},
+    {"number-of_files", VariantType::Int, 300, {"maximum number of json files in folder"}},
+    {"number-of_tracks", VariantType::Int, -1, {"maximum number of track stored in json file (-1 means no limit)"}},
+    {"time-interval", VariantType::Int, 5000, {"time interval in milliseconds between stored files"}},
     {"enable-mc", o2::framework::VariantType::Bool, false, {"enable visualization of MC data"}},
     {"disable-mc", o2::framework::VariantType::Bool, false, {"disable visualization of MC data"}}, // for compatibility, overrides enable-mc
     {"display-clusters", VariantType::String, "ITS,TPC,TRD,TOF", {"comma-separated list of clusters to display"}},
@@ -53,21 +62,17 @@ void customize(std::vector<ConfigParamSpec>& workflowOptions)
 
   std::swap(workflowOptions, options);
 }
-
 #include "Framework/runDataProcessing.h"
 
-void O2GPUDPLDisplaySpec::init(InitContext& ic)
+void O2DPLDisplaySpec::init(InitContext& ic)
 {
   const auto grp = o2::parameters::GRPObject::loadFrom();
   o2::base::GeometryManager::loadGeometry();
   o2::base::Propagator::initFieldFromGRP();
-  mConfig.reset(new GPUO2InterfaceConfiguration);
+  mConfig.reset(new EveConfiguration);
   mConfig->configGRP.solenoidBz = 5.00668f * grp->getL3Current() / 30000.;
   mConfig->configGRP.continuousMaxTimeBin = grp->isDetContinuousReadOut(o2::detectors::DetID::TPC) ? -1 : 0; // Number of timebins in timeframe if continuous, 0 otherwise
   mConfig->ReadConfigurableParam();
-
-  mFastTransform = std::move(TPCFastTransformHelperO2::instance()->create(0));
-  mConfig->configCalib.fastTransform = mFastTransform.get();
 
   auto gm = o2::trd::Geometry::instance();
   gm->createPadPlaneArray();
@@ -77,26 +82,19 @@ void O2GPUDPLDisplaySpec::init(InitContext& ic)
 
   mITSDict = std::make_unique<o2::itsmft::TopologyDictionary>();
   mConfig->configCalib.itsPatternDict = mITSDict.get();
-
   mConfig->configProcessing.runMC = mUseMC;
 
   o2::tof::Geo::Init();
 
-  o2::its::GeometryTGeo::Instance()->fillMatrixCache(o2::math_utils::bit2Mask(o2::math_utils::TransformType::T2GRot, o2::math_utils::TransformType::T2G, o2::math_utils::TransformType::L2G, o2::math_utils::TransformType::T2L));
-
-  //mDisplay.reset(new GPUO2InterfaceDisplay(mConfig.get()));
+  o2::its::GeometryTGeo::Instance()->fillMatrixCache(
+    o2::math_utils::bit2Mask(o2::math_utils::TransformType::T2GRot,
+                             o2::math_utils::TransformType::T2G,
+                             o2::math_utils::TransformType::L2G,
+                             o2::math_utils::TransformType::T2L));
 }
-
-struct GPUWorkflowHelper::tmpDataContainer {
-  std::vector<o2::BaseCluster<float>> ITSClustersArray;
-  std::vector<int> tpcLinkITS, tpcLinkTRD, tpcLinkTOF;
-  std::vector<const o2::track::TrackParCov*> globalTracks;
-  std::vector<float> globalTrackTimes;
-};
 
 using GID = o2::dataformats::GlobalTrackID;
 using PNT = std::array<float, 3>;
-
 std::vector<PNT> getTrackPoints(const o2::track::TrackPar& trc, float minR, float maxR, float maxStep)
 
 {
@@ -132,17 +130,32 @@ std::vector<PNT> getTrackPoints(const o2::track::TrackPar& trc, float minR, floa
   return pnts;
 }
 
-void O2GPUDPLDisplaySpec::run(ProcessingContext& pc)
+void O2DPLDisplaySpec::run(ProcessingContext& pc)
 {
+  if (!this->mEveHostNameMatch) {
+    return;
+  }
+
+  // filtering out any run which occur before reaching next time interval
+  std::chrono::time_point<std::chrono::high_resolution_clock> currentTime = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> elapsed = currentTime - this->mTimeStamp;
+  if (elapsed < this->mTimeInteval) {
+    return; // skip this run - it is too often
+  }
+  this->mTimeStamp = currentTime;
+
   o2::globaltracking::RecoContainer recoData;
   recoData.collectData(pc, *mDataRequest);
-  GPUTrackingInOutPointers ptrs;
-  auto cnt = GPUWorkflowHelper::fillIOPtr(ptrs, recoData, mUseMC, &(mConfig->configCalib), mClMask, mTrkMask, mTrkMask);
+  auto cnt = EveWorkflowHelper::compute(recoData, &(mConfig->configCalib), mClMask, mTrkMask, mTrkMask);
 
   std::cout << cnt->globalTracks.size() << std::endl;
   o2::event_visualisation::VisualisationEvent vEvent;
+  int trackCount = cnt->globalTracks.size(); // how many tracks should be stored
+  if (this->mNumberOfTracks != -1 && this->mNumberOfTracks < trackCount) {
+    trackCount = this->mNumberOfTracks; // less than available
+  }
 
-  for (int i = 0; i < cnt->globalTracks.size(); i++) {
+  for (unsigned int i = 0; i < trackCount; i++) {
     auto tr = cnt->globalTracks[i];
     std::cout << tr->getX() << std::endl;
     float rMax = 385;
@@ -170,11 +183,11 @@ void O2GPUDPLDisplaySpec::run(ProcessingContext& pc)
     }
   }
 
-  FileProducer producer("./jsons");
+  FileProducer producer(this->mJsonPath, this->mNumberOfFiles);
   vEvent.toFile(producer.newFileName());
 }
 
-void O2GPUDPLDisplaySpec::endOfStream(EndOfStreamContext& ec)
+void O2DPLDisplaySpec::endOfStream(EndOfStreamContext& ec)
 {
 }
 
@@ -183,8 +196,17 @@ WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
   WorkflowSpec specs;
 
   o2::conf::ConfigurableParam::updateFromString(cfgc.options().get<std::string>("configKeyValues"));
-
   bool useMC = cfgc.options().get<bool>("enable-mc") && !cfgc.options().get<bool>("disable-mc");
+  std::string jsonFolder = cfgc.options().get<std::string>("jsons-folder");
+  std::string eveHostName = cfgc.options().get<std::string>("eve-hostname");
+  char hostname[HOST_NAME_MAX];
+  gethostname(hostname, HOST_NAME_MAX);
+  bool eveHostNameMatch = eveHostName.empty() || eveHostName == hostname;
+
+  std::chrono::milliseconds timeInterval(cfgc.options().get<int>("time-interval"));
+  int numberOfFiles = cfgc.options().get<int>("number-of_files");
+  int numberOfTracks = cfgc.options().get<int>("number-of_tracks");
+
   GlobalTrackID::mask_t srcTrk = GlobalTrackID::getSourcesMask(cfgc.options().get<std::string>("display-tracks"));
   GlobalTrackID::mask_t srcCl = GlobalTrackID::getSourcesMask(cfgc.options().get<std::string>("display-clusters"));
   if (!srcTrk.any() && !srcCl.any()) {
@@ -199,10 +221,10 @@ WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
   }
 
   specs.emplace_back(DataProcessorSpec{
-    "o2-gpu-display",
+    "o2-eve-display",
     dataRequest->inputs,
     {},
-    AlgorithmSpec{adaptFromTask<O2GPUDPLDisplaySpec>(useMC, srcTrk, srcCl, dataRequest)}});
+    AlgorithmSpec{adaptFromTask<O2DPLDisplaySpec>(useMC, srcTrk, srcCl, dataRequest, jsonFolder, timeInterval, numberOfFiles, numberOfTracks, eveHostNameMatch)}});
 
   return std::move(specs);
 }
