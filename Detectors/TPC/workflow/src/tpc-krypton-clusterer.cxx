2fb251526ebc82dc399cb8a41b0dc448add9ef10
wiechula <Jens.Wiechula@cern.ch>
2021-08-02 21:53:12 +0200

    TPC: generalize cluster QC
    * move Krcluster to DataFormatsTPC
    * modify cluster QC to accept ClusterNative and KrCluster as input

--- Detectors/TPC/workflow/src/tpc-krypton-clusterer.cxx
+++ Detectors/TPC/workflow/src/tpc-krypton-clusterer.cxx
@@ -24,7 +24,7 @@
 #include "CommonUtils/ConfigurableParam.h"
 
 #include "DataFormatsTPC/TPCSectorHeader.h"
-#include "TPCReconstruction/KrCluster.h"
+#include "DataFormatsTPC/KrCluster.h"
 #include "TPCBase/Sector.h"
 #include "TPCWorkflow/KryptonClustererSpec.h"
 
