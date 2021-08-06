56695adaec6dbfa17c4764d1079f6a0e05586387
Ole Schmidt <ole.schmidt@cern.ch>
2021-08-06 17:53:40 +0200

    FST: disable MC in TRD tracking workflow

--- prodtests/full-system-test/dpl-workflow.sh
+++ prodtests/full-system-test/dpl-workflow.sh
@@ -138,7 +138,7 @@ WORKFLOW+="o2-tpcits-match-workflow $ARGS_ALL --disable-root-input --disable-roo
 WORKFLOW+="o2-ft0-reco-workflow $ARGS_ALL --disable-root-input --disable-root-output $DISABLE_MC | "
 WORKFLOW+="o2-tof-reco-workflow $ARGS_ALL --input-type $TOF_INPUT --output-type $TOF_OUTPUT --disable-root-input --disable-root-output $DISABLE_MC | "
 WORKFLOW+="o2-trd-tracklet-transformer $ARGS_ALL --disable-root-input --disable-root-output $TRD_TRANSFORMER_CONFIG --pipeline TRDTRACKLETTRANSFORMER:$N_TRDTRK | "
-WORKFLOW+="o2-trd-global-tracking $ARGS_ALL --disable-root-input --disable-root-output $TRD_CONFIG | "
+WORKFLOW+="o2-trd-global-tracking $ARGS_ALL --disable-root-input --disable-root-output $DISABLE_MC $TRD_CONFIG | "
 
 # Workflows disabled in sync mode
 if [ $SYNCMODE == 0 ]; then
