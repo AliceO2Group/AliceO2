494c1504f213c7059469061b2939c9de4844a505
nicolovalle <35177278+nicolovalle@users.noreply.github.com>
2021-08-15 21:55:35 +0200

    Muon chambers bitmap (#6832)
    * Muon chambers bitmap
    
    * clang corrections
    
    Co-authored-by: Nicolo Valle <nicolo.valle@cern.ch>

--- Analysis/DataModel/include/AnalysisDataModel/ReducedInfoTables.h
+++ Analysis/DataModel/include/AnalysisDataModel/ReducedInfoTables.h
@@ -238,7 +238,9 @@ DECLARE_SOA_TABLE(ReducedMuons, "AOD", "RTMUON", //!
 DECLARE_SOA_TABLE(ReducedMuonsExtra, "AOD", "RTMUONEXTRA", //!
                   fwdtrack::NClusters, fwdtrack::PDca, fwdtrack::RAtAbsorberEnd,
                   fwdtrack::Chi2, fwdtrack::Chi2MatchMCHMID, fwdtrack::Chi2MatchMCHMFT,
-                  fwdtrack::MatchScoreMCHMFT, fwdtrack::MatchMFTTrackID, fwdtrack::MatchMCHTrackID);
+                  fwdtrack::MatchScoreMCHMFT, fwdtrack::MatchMFTTrackID, fwdtrack::MatchMCHTrackID,
+                  fwdtrack::MCHBitMap);
+
 // Muon covariance, TODO: the rest of the matrix should be added when needed
 DECLARE_SOA_TABLE(ReducedMuonsCov, "AOD", "RTMUONCOV",
                   fwdtrack::X, fwdtrack::Y, fwdtrack::Z, reducedmuon::RawPhi, fwdtrack::Tgl, fwdtrack::Signed1Pt,
