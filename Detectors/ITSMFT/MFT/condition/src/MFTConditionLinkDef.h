93f6fb61b0c8510cc3184c007eaef24a45f13b18
Satoshi Yano <satoshi.yano@cern.ch>
2021-08-16 10:00:03 +0900

    MFT DCS Processor

---
+++ Detectors/ITSMFT/MFT/condition/src/MFTConditionLinkDef.h
@@ -0,0 +1,10 @@
+#ifdef __CLING__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ struct o2::mft::MFTDCSinfo + ;
+#pragma link C++ class std::unordered_map < o2::dcs::DataPointIdentifier, o2::mft::MFTDCSinfo> + ;
+
+#endif
