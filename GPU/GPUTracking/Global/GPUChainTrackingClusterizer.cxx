a59f6c48f6f9eeffadff0192c97a0ae334d7c9f5
David Rohr <drohr@jwdt.org>
2021-08-19 15:12:55 +0200

    GPU: Increase buffer extension factor for low occupancy TPC data with noise

--- GPU/GPUTracking/Global/GPUChainTrackingClusterizer.cxx
+++ GPU/GPUTracking/Global/GPUChainTrackingClusterizer.cxx
@@ -385,7 +385,7 @@ int GPUChainTracking::RunTPCClusterizer(bool synchronizeOutput)
     unsigned int threshold = 30000000 / 256 * mIOPtrs.settingsTF->nHBFPerTF;
     mRec->MemoryScalers()->nTPCHits = std::max<unsigned int>(nHitsBase, std::min<unsigned int>(threshold, nHitsBase * 3)); // Increase the buffer size for low occupancy data to compensate for noisy pads creating exceiive clusters
     if (nHitsBase < threshold) {
-      float maxFactor = mRec->MemoryScalers()->nTPCHits < threshold ? 2.0 : 1.5;
+      float maxFactor = mRec->MemoryScalers()->nTPCHits < threshold ? 2.25 : 1.75;
       mRec->MemoryScalers()->temporaryFactor *= std::min(maxFactor, (float)threshold / nHitsBase);
       tpcHitLowOccupancyScalingFactor = std::min(3.f, (float)threshold / nHitsBase);
     }
