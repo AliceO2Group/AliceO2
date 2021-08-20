f927c2faebea4cc5840426e2315340b502244b91
Stefan Heckel <sheckel@cern.ch>
2021-08-20 16:18:25 +0200

    TPC QC: Update PID task, add histograms
    o Add 1D and 2D histos for MIPs and electrons

--- Detectors/TPC/qc/src/PID.cxx
+++ Detectors/TPC/qc/src/PID.cxx
@@ -17,6 +17,7 @@
 #include "TStyle.h"
 #include "TFile.h"
 #include "TCanvas.h"
+#include "TMathBase.h"
 
 //o2 includes
 #include "DataFormatsTPC/dEdxInfo.h"
@@ -31,12 +32,14 @@ using namespace o2::tpc::qc;
 //______________________________________________________________________________
 void PID::initializeHistograms()
 {
-  mHist1D.emplace_back("hNClusters", "; # of clusters; counts", 160, 0, 160); //| mHist1D[0]
-  mHist1D.emplace_back("hdEdxTot", "; dEdxTot (a.u.); counts", 200, 0, 200);  //| mHist1D[1]
-  mHist1D.emplace_back("hdEdxMax", "; dEdxMax (a.u.); counts", 200, 0, 200);  //| mHist1D[2]
-  mHist1D.emplace_back("hPhi", "; #phi (rad); counts", 180, 0., 2 * M_PI);    //| mHist1D[3]
-  mHist1D.emplace_back("hTgl", "; tan#lambda; counts", 60, -2, 2);            //| mHist1D[4]
-  mHist1D.emplace_back("hSnp", "; sin p; counts", 60, -2, 2);                 //| mHist1D[5]
+  mHist1D.emplace_back("hNClusters", "; # of clusters; counts", 160, 0, 160);                        //| mHist1D[0]
+  mHist1D.emplace_back("hdEdxTot", "; dEdxTot (a.u.); counts", 200, 0, 200);                         //| mHist1D[1]
+  mHist1D.emplace_back("hdEdxMax", "; dEdxMax (a.u.); counts", 200, 0, 200);                         //| mHist1D[2]
+  mHist1D.emplace_back("hPhi", "; #phi (rad); counts", 180, 0., 2 * M_PI);                           //| mHist1D[3]
+  mHist1D.emplace_back("hTgl", "; tan#lambda; counts", 60, -2, 2);                                   //| mHist1D[4]
+  mHist1D.emplace_back("hSnp", "; sin p; counts", 60, -2, 2);                                        //| mHist1D[5]
+  mHist1D.emplace_back("hdEdxMips", "dEdx (a.u.) of MIPs; dEdx of MIPs (a.u.); counts", 25, 35, 60); //| mHist1D[6]
+  mHist1D.emplace_back("hdEdxEles", "dEdx (a.u.) of electrons; dEdx (a.u.); counts", 30, 70, 100);   //| mHist1D[7]
 
   mHist2D.emplace_back("hdEdxVsPhi", "dEdx (a.u.) vs #phi (rad); #phi (rad); dEdx (a.u.)", 180, 0., 2 * M_PI, 300, 0, 300); //| mHist2D[0]
   mHist2D.emplace_back("hdEdxVsTgl", "dEdx (a.u.) vs tan#lambda; tan#lambda; dEdx (a.u.)", 60, -2, 2, 300, 0, 300);         //| mHist2D[1]
@@ -46,8 +49,11 @@ void PID::initializeHistograms()
   if (logPtBinning.size() > 0) {
     mHist2D.emplace_back("hdEdxVsp", "dEdx (a.u.) vs p (G#it{e}V/#it{c}); p (G#it{e}V/#it{c}); dEdx (a.u.)", logPtBinning.size() - 1, logPtBinning.data(), 300, 0, 300); //| mHist2D[3]
   }
-  //mHist2D.emplace_back("hdedxVsphiMIPA","; #phi (rad); dedx (a.u.)", 180,0.,2*M_PI,25,35,60);  //| mHist2D[4]
-  //mHist2D.emplace_back("hdedxVsphiMIPC","; #phi (rad); dedx (a.u.)", 180,0.,2*M_PI,25,35,60);  //| mHist2D[5]
+
+  mHist2D.emplace_back("hdEdxVsPhiMipsAside", "dEdx (a.u.) vs #phi (rad) of MIPs (A side); #phi (rad); dEdx (a.u.)", 180, 0., 2 * M_PI, 25, 35, 60);       //| mHist2D[4]
+  mHist2D.emplace_back("hdEdxVsPhiMipsCside", "dEdx (a.u.) vs #phi (rad) of MIPs (C side); #phi (rad); dEdx (a.u.)", 180, 0., 2 * M_PI, 25, 35, 60);       //| mHist2D[5]
+  mHist2D.emplace_back("hdEdxVsPhiElesAside", "dEdx (a.u.) vs #phi (rad) of electrons (A side); #phi (rad); dEdx (a.u.)", 180, 0., 2 * M_PI, 30, 70, 100); //| mHist2D[6]
+  mHist2D.emplace_back("hdEdxVsPhiElesCside", "dEdx (a.u.) vs #phi (rad) of electrons (C side); #phi (rad); dEdx (a.u.)", 180, 0., 2 * M_PI, 30, 70, 100); //| mHist2D[7]
 }
 
 //______________________________________________________________________________
@@ -72,6 +78,9 @@ bool PID::processTrack(const o2::tpc::TrackTPC& track)
   const auto tgl = track.getTgl();
   const auto snp = track.getSnp();
   const auto nclusters = track.getNClusterReferences();
+  const auto eta = track.getEta();
+
+  double absEta = TMath::Abs(eta);
 
   // ===| histogram filling |===
   mHist1D[0].Fill(nclusters);
@@ -85,6 +94,26 @@ bool PID::processTrack(const o2::tpc::TrackTPC& track)
   mHist2D[1].Fill(tgl, dEdxTot);
   mHist2D[2].Fill(nclusters, dEdxTot);
   mHist2D[3].Fill(p, dEdxTot);
+
+  // ===| cuts and  histogram filling for MIPs |===
+  if (p > 0.4 && p < 0.55 && absEta < 1. && nclusters > 80 && dEdxTot > 35 && dEdxTot < 60) {
+    mHist1D[6].Fill(dEdxTot);
+    if (eta > 0.) {
+      mHist2D[4].Fill(phi, dEdxTot);
+    } else {
+      mHist2D[5].Fill(phi, dEdxTot);
+    }
+  }
+
+  // ===| cuts and  histogram filling for electrons |===
+  if (p > 0.32 && p < 0.38 && absEta < 1. && nclusters > 80 && dEdxTot > 70 && dEdxTot < 100) {
+    mHist1D[7].Fill(dEdxTot);
+    if (eta > 0.) {
+      mHist2D[6].Fill(phi, dEdxTot);
+    } else {
+      mHist2D[7].Fill(phi, dEdxTot);
+    }
+  }
 
   return true;
 }
