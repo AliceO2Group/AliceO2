1416bfbf4b177ee3900b6fd6fde31653a56eaf24
shahoian <ruben.shahoyan@cern.ch>
2021-09-03 10:54:54 +0200

    macro to upload CTP orbit reset time object

---
+++ macro/CreateCTPOrbitResetObject.C
@@ -0,0 +1,24 @@
+#if !defined(__CLING__) || defined(__ROOTCLING__)
+#include "CCDB/CcdbApi.h"
+#include "Framework/Logger.h"
+#include <array>
+#endif
+
+// create and upload orbit reset object to CCDB
+constexpr long DummyTime = -1;
+
+void CreateCTPOrbitResetObject(const std::string& ccdbHost = "http://ccdb-test.cern.ch:8080", long t = DummyTime, long tmin = 0, long tmax = -1)
+{
+
+  if (t == DummyTime) {
+    t = std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::system_clock::now()).time_since_epoch().count();
+  }
+  std::array<long, 1> rt{t};
+  const std::string objName{"CTP/OrbitReset"};
+  o2::ccdb::CcdbApi api;
+  api.init(ccdbHost.c_str());   // or http://localhost:8080 for a local installation
+  map<string, string> metadata; // can be empty
+  metadata["comment"] = "CTP Orbit reset";
+  api.storeAsTFileAny(&rt, objName, metadata, tmin, tmax);
+  LOGP(INFO, "Uploaded CTP Oribt reset time {} to {}", t, objName);
+}
