8ee6439a65cae0e9feb797a0dd48fe623fb691b4
rpezzi <rafael.pezzi@cern.ch>
2021-09-08 11:29:49 +0200

    Fix another typo

--- Detectors/ITSMFT/MFT/condition/testWorkflow/mft-dcs-sim-workflow.cxx
+++ Detectors/ITSMFT/MFT/condition/testWorkflow/mft-dcs-sim-workflow.cxx
@@ -26,10 +26,10 @@ o2::framework::WorkflowSpec defineDataProcessing(o2::framework::ConfigContext co
   // for MFT
   // for test, we use less DPs that official ones
 
-  dphints.emplace_back(o2::dcs::test::DataPointHint<double>{"mft_main/MFT_PSU_Zone/H[0..1]D[0..4]F[0..1]Z[0..3]/Monitoirng/Current/Analog", 0.05, 0.3});
-  dphints.emplace_back(o2::dcs::test::DataPointHint<double>{"mft_main/MFT_PSU_Zone/H[0..1]D[0..4]F[0..1]Z[0..3]/Monitoirng/Current/Digital", 0.1, 2.0});
-  dphints.emplace_back(o2::dcs::test::DataPointHint<double>{"mft_main/MFT_PSU_Zone/H[0..1]D[0..4]F[0..1]Z[0..3]/Monitoirng/Current/BackBias", 0.0, 1.0});
-  dphints.emplace_back(o2::dcs::test::DataPointHint<double>{"mft_main/MFT_PSU_Zone/H[0..1]D[0..4]F[0..1]Z[0..3]/Monitoirng/Voltage/BackBias", 0, 3.});
+  dphints.emplace_back(o2::dcs::test::DataPointHint<double>{"mft_main/MFT_PSU_Zone/H[0..1]D[0..4]F[0..1]Z[0..3]/Monitoring/Current/Analog", 0.05, 0.3});
+  dphints.emplace_back(o2::dcs::test::DataPointHint<double>{"mft_main/MFT_PSU_Zone/H[0..1]D[0..4]F[0..1]Z[0..3]/Monitoring/Current/Digital", 0.1, 2.0});
+  dphints.emplace_back(o2::dcs::test::DataPointHint<double>{"mft_main/MFT_PSU_Zone/H[0..1]D[0..4]F[0..1]Z[0..3]/Monitoring/Current/BackBias", 0.0, 1.0});
+  dphints.emplace_back(o2::dcs::test::DataPointHint<double>{"mft_main/MFT_PSU_Zone/H[0..1]D[0..4]F[0..1]Z[0..3]/Monitoring/Voltage/BackBias", 0, 3.});
 
   o2::framework::WorkflowSpec specs;
   specs.emplace_back(o2::dcs::test::getDCSRandomDataGeneratorSpec(dphints, "MFT"));
