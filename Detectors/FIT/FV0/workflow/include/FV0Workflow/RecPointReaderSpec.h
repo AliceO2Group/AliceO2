18edfbc25c87494d9e1067531a322d293a64d6d9
shahoian <ruben.shahoyan@cern.ch>
2021-09-10 15:03:13 +0200

    Add FV0 RecPoint reader

---
+++ Detectors/FIT/FV0/workflow/include/FV0Workflow/RecPointReaderSpec.h
@@ -0,0 +1,65 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// @file   RecPointReaderSpec.h
+
+#ifndef O2_FV0_RECPOINTREADER
+#define O2_FV0_RECPOINTREADER
+
+#include "TFile.h"
+#include "TTree.h"
+
+#include "Framework/RootSerializationSupport.h"
+#include "Framework/DataProcessorSpec.h"
+#include "Framework/Task.h"
+#include "DataFormatsFV0/RecPoints.h"
+
+using namespace o2::framework;
+
+namespace o2
+{
+namespace fv0
+{
+
+class RecPointReader : public Task
+{
+ public:
+  RecPointReader(bool useMC = true);
+  ~RecPointReader() override = default;
+  void init(InitContext& ic) final;
+  void run(ProcessingContext& pc) final;
+
+ private:
+  void connectTree(const std::string& filename);
+
+  std::unique_ptr<TFile> mFile;
+  std::unique_ptr<TTree> mTree;
+
+  bool mUseMC = true; // use MC truth
+  o2::header::DataOrigin mOrigin = o2::header::gDataOriginFV0;
+
+  std::vector<o2::fv0::RecPoints>* mRecPoints = nullptr;
+  std::vector<o2::fv0::ChannelDataFloat>* mChannelData = nullptr;
+
+  std::string mInputFileName = "o2reco_fv0.root";
+  std::string mRecPointTreeName = "o2sim";
+  std::string mRecPointBranchName = "FV0Cluster";
+  std::string mChannelDataBranchName = "FV0RecChData";
+};
+
+/// create a processor spec
+/// read simulated ITS digits from a root file
+framework::DataProcessorSpec getRecPointReaderSpec(bool useMC);
+
+} // namespace fv0
+} // namespace o2
+
+#endif /* O2_FV0_RECPOINTREADER */
---
+++ Detectors/FIT/FV0/workflow/src/RecPointReaderSpec.cxx
@@ -0,0 +1,99 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// @file   RecPointReaderSpec.cxx
+
+#include <vector>
+
+#include "Framework/ConfigParamRegistry.h"
+#include "Framework/ControlService.h"
+#include "Framework/Logger.h"
+#include "FV0Workflow/RecPointReaderSpec.h"
+#include "DetectorsCommonDataFormats/NameConf.h"
+
+using namespace o2::framework;
+using namespace o2::fv0;
+
+namespace o2
+{
+namespace fv0
+{
+
+RecPointReader::RecPointReader(bool useMC)
+{
+  mUseMC = useMC;
+  if (useMC) {
+    LOG(WARNING) << "FV0 RecPoint reader at the moment does not process MC";
+  }
+}
+
+void RecPointReader::init(InitContext& ic)
+{
+  mInputFileName = o2::utils::Str::concat_string(o2::utils::Str::rectifyDirectory(ic.options().get<std::string>("input-dir")),
+                                                 ic.options().get<std::string>("fv0-recpoints-infile"));
+  connectTree(mInputFileName);
+}
+
+void RecPointReader::run(ProcessingContext& pc)
+{
+  auto ent = mTree->GetReadEntry() + 1;
+  assert(ent < mTree->GetEntries()); // this should not happen
+  mTree->GetEntry(ent);
+
+  LOG(INFO) << "FV0 RecPointReader pushes " << mRecPoints->size() << " recpoints with " << mChannelData->size() << " channels at entry " << ent;
+  pc.outputs().snapshot(Output{mOrigin, "RECPOINTS", 0, Lifetime::Timeframe}, *mRecPoints);
+  pc.outputs().snapshot(Output{mOrigin, "RECCHDATA", 0, Lifetime::Timeframe}, *mChannelData);
+
+  if (mTree->GetReadEntry() + 1 >= mTree->GetEntries()) {
+    pc.services().get<ControlService>().endOfStream();
+    pc.services().get<ControlService>().readyToQuit(QuitRequest::Me);
+  }
+}
+
+void RecPointReader::connectTree(const std::string& filename)
+{
+  mTree.reset(nullptr); // in case it was already loaded
+  mFile.reset(TFile::Open(filename.c_str()));
+  assert(mFile && !mFile->IsZombie());
+  mTree.reset((TTree*)mFile->Get(mRecPointTreeName.c_str()));
+  assert(mTree);
+
+  mTree->SetBranchAddress(mRecPointBranchName.c_str(), &mRecPoints);
+  mTree->SetBranchAddress(mChannelDataBranchName.c_str(), &mChannelData);
+  if (mUseMC) {
+    LOG(WARNING) << "MC-truth is not supported for FV0 recpoints currently";
+    mUseMC = false;
+  }
+
+  LOG(INFO) << "Loaded FV0 RecPoints tree from " << filename << " with " << mTree->GetEntries() << " entries";
+}
+
+DataProcessorSpec getRecPointReaderSpec(bool useMC)
+{
+  std::vector<OutputSpec> outputSpec;
+  outputSpec.emplace_back(o2::header::gDataOriginFV0, "RECPOINTS", 0, Lifetime::Timeframe);
+  outputSpec.emplace_back(o2::header::gDataOriginFV0, "RECCHDATA", 0, Lifetime::Timeframe);
+  if (useMC) {
+    LOG(WARNING) << "MC-truth is not supported for FV0 recpoints currently";
+  }
+
+  return DataProcessorSpec{
+    "fv0-recpoints-reader",
+    Inputs{},
+    outputSpec,
+    AlgorithmSpec{adaptFromTask<RecPointReader>()},
+    Options{
+      {"fv0-recpoints-infile", VariantType::String, "o2reco_fv0.root", {"Name of the input file"}},
+      {"input-dir", VariantType::String, "none", {"Input directory"}}}};
+}
+
+} // namespace fv0
+} // namespace o2
