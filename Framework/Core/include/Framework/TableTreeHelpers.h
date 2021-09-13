fb7a97c70c54d5575ce0c9b3e5cab3838aa96867
Anton Alkin <anton.alkin@cern.ch>
2021-09-13 18:15:04 +0200

    Rewrite TableToTree to be more generic

--- Framework/Core/include/Framework/TableTreeHelpers.h
+++ Framework/Core/include/Framework/TableTreeHelpers.h
@@ -22,97 +22,205 @@ namespace o2::framework
 {
 
 // -----------------------------------------------------------------------------
-// TableToTree allows to save the contents of a given arrow::Table to a TTree
-//  BranchIterator is used by TableToTree
+// TableToTree allows to save the contents of a given arrow::Table into
+// a TTree
+// ColumnToBranch is used by GenericTableToTree
 //
 // To write the contents of a table ta to a tree tr on file f do:
-//  . TableToTree t2t(ta,f,treename);
-//  . t2t.addBranch(coumn1); t2t.addBranch(coumn1); ...
-//    OR
-//    t2t.addAllBranches();
+//  . GenericTableToTree t2t(ta, f,treename);
+//  . t2t.addBranches();
+//    OR t2t.addBranch(column.get(), field.get()), ...;
 //  . t2t.process();
 //
 // .............................................................................
-class BranchIterator
+class ColumnToBranchBase
 {
+ public:
+  ColumnToBranchBase(arrow::ChunkedArray* column, arrow::Field* field, int size = 1);
+  virtual ~ColumnToBranchBase() = default;
+  void at(int64_t* pos)
+  {
+    mCurrentPos = pos;
+    resetBuffer();
+  }
+
+ protected:
+  std::string mBranchName;
+
+  virtual void resetBuffer() = 0;
+  virtual void nextChunk() = 0;
+
+  arrow::ChunkedArray* mColumn = nullptr;
+
+  int64_t const* mCurrentPos = nullptr;
+  mutable int mFirstIndex = 0;
+  mutable int mCurrentChunk = 0;
+
+  int listSize = 1;
+};
+
+template <typename T>
+struct ROOTTypeString {
+  static constexpr char const* str = "/E";
+};
+
+template <>
+struct ROOTTypeString<bool> {
+  static constexpr char const* str = "/O";
+};
+
+template <>
+struct ROOTTypeString<uint8_t> {
+  static constexpr char const* str = "/b";
+};
+
+template <>
+struct ROOTTypeString<uint16_t> {
+  static constexpr char const* str = "/s";
+};
+
+template <>
+struct ROOTTypeString<uint32_t> {
+  static constexpr char const* str = "/i";
+};
+
+template <>
+struct ROOTTypeString<uint64_t> {
+  static constexpr char const* str = "/l";
+};
+
+template <>
+struct ROOTTypeString<int8_t> {
+  static constexpr char const* str = "/B";
+};
+
+template <>
+struct ROOTTypeString<int16_t> {
+  static constexpr char const* str = "/S";
+};
+
+template <>
+struct ROOTTypeString<int32_t> {
+  static constexpr char const* str = "/I";
+};
+
+template <>
+struct ROOTTypeString<int64_t> {
+  static constexpr char const* str = "/L";
+};
+
+template <>
+struct ROOTTypeString<float> {
+  static constexpr char const* str = "/F";
+};
+
+template <>
+struct ROOTTypeString<double> {
+  static constexpr char const* str = "/D";
+};
+
+template <typename T>
+constexpr auto ROOTTypeString_t = ROOTTypeString<T>::str;
+
+template <typename T>
+class ColumnToBranch : public ColumnToBranchBase
+{
+ public:
+  ColumnToBranch(TTree* tree, arrow::ChunkedArray* column, arrow::Field* field, int size = 1)
+    : ColumnToBranchBase(column, field, size)
+  {
+    if constexpr (std::is_pointer_v<T>) {
+      mLeaflist = mBranchName + "[" + std::to_string(listSize) + "]" + ROOTTypeString_t<std::remove_pointer_t<T>>;
+    } else {
+      mLeaflist = mBranchName + ROOTTypeString_t<T>;
+    }
+    mBranch = tree->GetBranch(mBranchName.c_str());
+    if (mBranch == nullptr) {
+      mBranch = tree->Branch(mBranchName.c_str(), (char*)nullptr, mLeaflist.c_str());
+    }
+    if constexpr (std::is_same_v<bool, std::remove_pointer_t<T>>) {
+      mCurrent = new bool[listSize];
+      mLast = mCurrent + listSize;
+      accessChunk(0);
+    } else {
+      accessChunk(0);
+    }
+  }
 
  private:
-  std::string mBranchName;    // branch name
-  arrow::ArrayVector mChunks; // chunks
-  Int_t mNumberChuncs;        // number of chunks
-  Int_t mCounterChunk;        // chunk counter
-  Int_t mNumberRows;          // number of rows
-  Int_t mCounterRow;          // row counter
+  std::string mLeaflist;
 
-  // data buffers for each data type
-  bool mStatus = false;
-  arrow::Field* mField;
-  arrow::Type::type mFieldType;
-  arrow::Type::type mElementType;
-  int32_t mNumberElements;
-  std::string mLeaflistString;
+  void resetBuffer() override
+  {
+    if constexpr (std::is_same_v<bool, std::remove_pointer_t<T>>) {
+      if (O2_BUILTIN_UNLIKELY((*mCurrentPos - mFirstIndex) * listSize >= getCurrentArray()->length())) {
+        nextChunk();
+      }
+    } else {
+      if (O2_BUILTIN_UNLIKELY((mCurrent + (*mCurrentPos - mFirstIndex) * listSize) >= mLast)) {
+        nextChunk();
+      }
+    }
+    accessChunk(*mCurrentPos);
+    mBranch->SetAddress((void*)(mCurrent + (*mCurrentPos - mFirstIndex) * listSize));
+  }
 
-  TBranch* mBranchPtr = nullptr;
+  auto getCurrentArray() const
+  {
+    if (listSize > 1) {
+      return std::static_pointer_cast<o2::soa::arrow_array_for_t<std::remove_pointer_t<T>>>(std::static_pointer_cast<arrow::FixedSizeListArray>(mColumn->chunk(mCurrentChunk))->values());
+    } else {
+      return std::static_pointer_cast<o2::soa::arrow_array_for_t<std::remove_pointer_t<T>>>(mColumn->chunk(mCurrentChunk));
+    }
+  }
 
-  char* mBranchBuffer = nullptr;
-  void* mValueBuffer = nullptr;
+  void nextChunk() override
+  {
+    mFirstIndex += getCurrentArray()->length();
+    ++mCurrentChunk;
+  }
 
-  std::shared_ptr<arrow::BooleanArray> mArray_o = nullptr;
-  //bool mBoolValueHolder;
-  bool* mVariable_o = nullptr;
+  void accessChunk(int at)
+  {
+    auto array = getCurrentArray();
+    if constexpr (std::is_same_v<bool, std::remove_pointer_t<T>>) {
+      for (auto i = 0; i < listSize; ++i) {
+        mCurrent[i] = (bool)array->Value((at - mFirstIndex) * listSize + i);
+      }
+    } else {
+      mCurrent = (std::remove_pointer_t<T>*)array->raw_values();
+      mLast = mCurrent + array->length() * listSize;
+    }
+  }
 
-  uint8_t* mVariable_ub = nullptr;
-  uint16_t* mVariable_us = nullptr;
-  uint32_t* mVariable_ui = nullptr;
-  uint64_t* mVariable_ul = nullptr;
-  int8_t* mVariable_b = nullptr;
-  int16_t* mVariable_s = nullptr;
-  int32_t* mVariable_i = nullptr;
-  int64_t* mVariable_l = nullptr;
-  float* mVariable_f = nullptr;
-  double* mVariable_d = nullptr;
-
-  // initialize a branch
-  bool initBranch(TTree* tree);
-
-  // initialize chunk ib
-  bool initDataBuffer(Int_t ib);
-
- public:
-  BranchIterator(TTree* tree, std::shared_ptr<arrow::ChunkedArray> col, std::shared_ptr<arrow::Field> field);
-  ~BranchIterator();
-
-  // has the iterator been properly initialized
-  bool getStatus();
-
-  // fills buffer with next value
-  // returns false if end of buffer reached
-  bool push();
+  mutable std::remove_pointer_t<T>* mCurrent = nullptr;
+  mutable std::remove_pointer_t<T>* mLast = nullptr;
+  TBranch* mBranch = nullptr;
 };
 
 class TableToTree
 {
+ public:
+  TableToTree(std::shared_ptr<arrow::Table> table, TFile* file, const char* treename);
+
+  TTree* process();
+  void addBranch(std::shared_ptr<arrow::ChunkedArray> column, std::shared_ptr<arrow::Field> field);
+  void addAllBranches();
 
  private:
-  TTree* mTreePtr;
+  arrow::Table* mTable;
+  int64_t mRows = 0;
+  TTree* mTree = nullptr;
+  std::vector<std::unique_ptr<ColumnToBranchBase>> mColumnReaders;
+};
 
-  // a list of BranchIterator
-  std::vector<std::unique_ptr<BranchIterator>> mBranchIterators;
-
-  // table to convert
-  std::shared_ptr<arrow::Table> mTable;
-
+class GenericTreeToTable
+{
  public:
-  TableToTree(std::shared_ptr<arrow::Table> table,
-              TFile* file,
-              const char* treename);
-
-  // add branches
-  bool addBranch(std::shared_ptr<arrow::ChunkedArray> col, std::shared_ptr<arrow::Field> field);
-  bool addAllBranches();
-
-  // write table to tree
-  TTree* process();
+ private:
+  std::shared_ptr<arrow::Table> mTable;
+  std::vector<std::shared_ptr<arrow::Field>> mFields;
+  std::string mTableLabel;
 };
 
 class TreeToTable
