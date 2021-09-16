2a1eef58ccda5388e46cb6b84d1bad38c026e645
Jan Fiete <jgrosseo@cern.ch>
2021-09-16 14:29:56 +0200

    add comments

--- Framework/Core/include/Framework/AnalysisDataModel.h
+++ Framework/Core/include/Framework/AnalysisDataModel.h
@@ -334,27 +334,27 @@ namespace fwdtrack
 {
 // FwdTracks and MFTTracks Columns definitions
 DECLARE_SOA_INDEX_COLUMN(Collision, collision);                                              //!
-DECLARE_SOA_COLUMN(TrackType, trackType, uint8_t);                                           //! TODO change to ForwardTrackTypeEnum when enums are supported
-DECLARE_SOA_COLUMN(X, x, float);                                                             //! TrackParFwd parameters: x, y, z, phi, tan(lamba), q/pt
-DECLARE_SOA_COLUMN(Y, y, float);                                                             //!
-DECLARE_SOA_COLUMN(Z, z, float);                                                             //!
-DECLARE_SOA_COLUMN(Phi, phi, float);                                                         //!
-DECLARE_SOA_COLUMN(Tgl, tgl, float);                                                         //!
-DECLARE_SOA_COLUMN(Signed1Pt, signed1Pt, float);                                             //!
-DECLARE_SOA_COLUMN(NClusters, nClusters, int8_t);                                            //!
-DECLARE_SOA_COLUMN(Chi2, chi2, float);                                                       //!
+DECLARE_SOA_COLUMN(TrackType, trackType, uint8_t);                                           //! Type of track. See enum ForwardTrackTypeEnum
+DECLARE_SOA_COLUMN(X, x, float);                                                             //! TrackParFwd parameter x
+DECLARE_SOA_COLUMN(Y, y, float);                                                             //! TrackParFwd parameter y
+DECLARE_SOA_COLUMN(Z, z, float);                                                             //! TrackParFwd propagation parameter z
+DECLARE_SOA_COLUMN(Phi, phi, float);                                                         //! TrackParFwd parameter phi; (i.e. pt pointing direction)
+DECLARE_SOA_COLUMN(Tgl, tgl, float);                                                         //! TrackParFwd parameter tan(\lamba); (\lambda = 90 - \theta_{polar})
+DECLARE_SOA_COLUMN(Signed1Pt, signed1Pt, float);                                             //! TrackParFwd parameter: charged inverse transverse momentum; (q/pt)
+DECLARE_SOA_COLUMN(NClusters, nClusters, int8_t);                                            //! Number of clusters
+DECLARE_SOA_COLUMN(Chi2, chi2, float);                                                       //! Track chi^2
 DECLARE_SOA_COLUMN(PDca, pDca, float);                                                       //! PDca for MUONStandalone
 DECLARE_SOA_COLUMN(RAtAbsorberEnd, rAtAbsorberEnd, float);                                   //! RAtAbsorberEnd for MUONStandalone tracks and GlobalMuonTrackstracks
 DECLARE_SOA_COLUMN(Chi2MatchMCHMID, chi2MatchMCHMID, float);                                 //! MCH-MID Match Chi2 for MUONStandalone tracks
 DECLARE_SOA_COLUMN(Chi2MatchMCHMFT, chi2MatchMCHMFT, float);                                 //! MCH-MFT Match Chi2 for GlobalMuonTracks
 DECLARE_SOA_COLUMN(MatchScoreMCHMFT, matchScoreMCHMFT, float);                               //! MCH-MFT Machine Learning Matching Score for GlobalMuonTracks
-DECLARE_SOA_SELF_INDEX_COLUMN_FULL(MCHTrack, matchMCHTrack, int, "FwdTracks_MatchMCHTrack"); //! ID of matching MCH track for GlobalMuonTracks
+DECLARE_SOA_SELF_INDEX_COLUMN_FULL(MCHTrack, matchMCHTrack, int, "FwdTracks_MatchMCHTrack"); //! Index of matching MCH track for GlobalMuonTracks and GlobalForwardTracks
 DECLARE_SOA_COLUMN(MCHBitMap, MchBitMap, uint16_t);                                          //! Fired muon trackig chambers bitmap
 DECLARE_SOA_COLUMN(MIDBitMap, midBitMap, uint8_t);                                           //! MID bitmap: non-bending plane (4bit), bending plane (4bit)
 DECLARE_SOA_COLUMN(MIDBoards, midBoards, uint32_t);                                          //! Local boards on each MID plane (8 bits per plane)
 DECLARE_SOA_COLUMN(TrackTime, trackTime, float);                                             //! Estimated time of the track in ns wrt collision().bc() or ambiguoustrack.bcSlice()[0]
 DECLARE_SOA_COLUMN(TrackTimeRes, trackTimeRes, float);                                       //! Resolution of the track time in ns
-DECLARE_SOA_DYNAMIC_COLUMN(Sign, sign,                                                       //!
+DECLARE_SOA_DYNAMIC_COLUMN(Sign, sign,                                                       //! Sign of the track eletric charge
                            [](float signed1Pt) -> short { return (signed1Pt > 0) ? 1 : -1; });
 DECLARE_SOA_EXPRESSION_COLUMN(Eta, eta, float, //!
                               -1.f * nlog(ntan(0.25f * static_cast<float>(M_PI) - 0.5f * natan(aod::fwdtrack::tgl))));
