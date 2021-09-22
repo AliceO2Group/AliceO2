f1fcc8055a60f058be88b26f48002e69007ebdbb
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2021-09-22 15:28:27 +0200

    DPL: more verbosity reduction for GUI metrics

--- Framework/Core/src/CommonServices.cxx
+++ Framework/Core/src/CommonServices.cxx
@@ -483,7 +483,7 @@ auto flushMetrics(ServiceRegistry& registry, DataProcessingStats& stats) -> void
   for (size_t si = 0; si < stats.statesSize.load(); ++si) {
     auto value = std::atomic_load_explicit(&stats.relayerState[si], std::memory_order_relaxed);
     std::atomic_thread_fence(std::memory_order_acquire);
-    monitoring.send({value, fmt::format("data_relayer/{}", si)});
+    monitoring.send({value, fmt::format("data_relayer/{}", si, Verbosity::Debug)});
   }
   relayer.sendContextState();
   monitoring.flushBuffer();
