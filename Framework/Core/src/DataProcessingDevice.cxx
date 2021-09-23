33c324de88f52d2a0c6705a6f630c996a121e373
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2021-09-23 14:31:43 +0200

    DPL: disable parallel processing for analysis (#7100)
    This is needed because otherwise the accounting will not be able to
    distinguish between real messages and shallow copies.

--- Framework/Core/src/DataProcessingDevice.cxx
+++ Framework/Core/src/DataProcessingDevice.cxx
@@ -540,6 +540,10 @@ void DataProcessingDevice::fillContext(DataProcessorContext& context, DeviceCont
   /// We must make sure there is no optional
   /// if we want to optimize the forwarding
   for (auto& input : mSpec.inputs) {
+    if (strncmp(DataSpecUtils::asConcreteOrigin(input.matcher).str, "AOD", 3) == 0) {
+      context.canForwardEarly = false;
+      break;
+    }
     if (input.matcher.lifetime == Lifetime::Optional) {
       context.canForwardEarly = false;
       break;
