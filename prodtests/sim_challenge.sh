c1c0379b8d3e5ffa3149d2de51a867d2ade4473b
shahoian <ruben.shahoyan@cern.ch>
2021-09-25 19:26:15 +0200

    Add q/pt=20 cuttoff for TPC reco in sim_challenge
    In order to roughly emulate the async. reconstruction

--- prodtests/sim_challenge.sh
+++ prodtests/sim_challenge.sh
@@ -126,7 +126,7 @@ if [ "$doreco" == "1" ]; then
 
   echo "Running TPC reco flow"
   #needs TPC digitized data
-  taskwrapper tpcreco.log o2-tpc-reco-workflow $gloOpt --input-type digits --output-type clusters,tracks
+  taskwrapper tpcreco.log o2-tpc-reco-workflow $gloOpt --input-type digits --output-type clusters,tracks  --configKeyValues "GPU_rec.maxTrackQPt=20"
   echo "Return status of tpcreco: $?"
 
   echo "Running ITS reco flow"
