cf99a15ed73d76e3216717a7bc81cc86a8823b34
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2021-10-02 12:15:42 +0200

    MCH elecmap.py can now generate cru.map and fec.map

--- Detectors/MUON/MCH/Raw/ElecMap/src/gen.sh
+++ Detectors/MUON/MCH/Raw/ElecMap/src/gen.sh
@@ -1,11 +1,34 @@
 #!/bin/sh
 
-for chamber in CH5R CH5L CH6R CH7R CH7L CH8L CH8R CH9L CH9R CH10R; do
+CRED="/Users/laurent/MCH Mapping-0a71181801a9.json"
+CHAMBERS="CH1R CH1L CH5R CH5L CH6R CH7R CH7L CH8L CH8R CH9L CH9R CH10R"
 
-  # srcdir=/Users/laurent/ownCloud/archive/2019/MRRTF/raw-data-encdec
-  # ./elecmap.py -i ${srcdir}/Mapping-${chamber}.xlsx -c ${chamber}
-  echo "Generating ${chamber}"
-  ./elecmap.py -gs "MCH Electronic Mapping" -s ${chamber} \
-  --credentials=/Users/laurent/alice/mch/MCH\ Mapping-0a71181801a9.json -c \
-  ${chamber}
+for chamber in $CHAMBERS; do
+   echo "Generating code for ${chamber}"
+   ./elecmap.py -gs "MCH Electronic Mapping" -s ${chamber} \
+   --credentials="$CRED" -c \
+   ${chamber}
 done
+
+rm cru.map
+for chamber in $CHAMBERS; do
+  echo "Generating cru map for ${chamber}"
+  ./elecmap.py -gs "MCH Electronic Mapping" -s ${chamber} \
+  --credentials="$CRED" \
+  --cru_map ${chamber}.cru.map
+  cat ${chamber}.cru.map >> cru.map
+done
+
+rm fec.map
+for chamber in $CHAMBERS; do
+  echo "Generating fec map for ${chamber}"
+  ./elecmap.py -gs "MCH Electronic Mapping" -s ${chamber} \
+  --credentials="$CRED" \
+  --fec_map ${chamber}.fec.map
+  cat ${chamber}.fec.map >> fec.map
+done
+
+
+
+
+
