da2b4997e2faa07576dff60151b43e0f4acfb012
Chiara Zampolli <Chiara.Zampolli@cern.ch>
2021-10-05 16:05:41 +0200

    ITSTPC QC
    Fixes
    
    Selection cuts
    
    fixes
    
    DPL for ITSTPC matching QC
    
    clang-format

---
+++ Detectors/GlobalTracking/include/GlobalTracking/MatchITSTPCQC.h
@@ -0,0 +1,105 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file MatchTPCITS.h
+/// \brief Class to perform TPC ITS matching
+/// \author ruben.shahoyan@cern.ch
+
+#ifndef ALICEO2_GLOBTRACKING_MATCHTPCITS_QC_
+#define ALICEO2_GLOBTRACKING_MATCHTPCITS_QC_
+
+#include <TH1F.h>
+#include <TH2F.h>
+#include <TObjArray.h>
+#include "DataFormatsGlobalTracking/RecoContainer.h"
+#include "Framework/ProcessingContext.h"
+
+namespace o2
+{
+
+namespace globaltracking
+{
+
+using GID = o2::dataformats::GlobalTrackID;
+
+class MatchITSTPCQC
+{
+ public:
+  MatchITSTPCQC() = default;
+  ~MatchITSTPCQC();
+
+  bool init();
+  void run(o2::framework::ProcessingContext& ctx);
+  void finalize();
+  void reset();
+  TH1F* getHistoPtTPC() const { return mPtTPC; }
+  TH1F* getHistoFractionITSTPCmatch() const { return mFractionITSTPCmatch; }
+  TH1F* getHistoPt() const { return mPt; }
+  TH1F* getHistoEta() const { return mEta; }
+  TH1F* getHistoChi2Matching() const { return mChi2Matching; }
+  TH1F* getHistoChi2Refit() const { return mChi2Refit; }
+  TH2F* getHistoTimeResVsPt() const { return mTimeResVsPt; }
+  void getTH1FHistos(TObjArray& objarTH1F);
+  void getTH2FHistos(TObjArray& objarTH2F);
+  void setSources(GID::mask_t src) { mSrc = src; }
+  void setUseMC(bool b) { mUseMC = b; }
+  void deleteHistograms();
+
+  // track selection
+  bool selectTrack(o2::tpc::TrackTPC const& track);
+  void setPtCut(float v) { mPtCut = v; }
+  void setEtaCut(float v) { mEtaCut = v; }
+  void setMinNTPCClustersCut(float v) { mNTPCClustersCut = v; }
+  void setMinDCAtoBeamPipeCut(std::array<float, 2> v)
+  {
+    setMinDCAtoBeamPipeDistanceCut(v[0]);
+    setMinDCAtoBeamPipeYCut(v[1]);
+  }
+  void setMinDCAtoBeamPipeDistanceCut(float v) { mDCACut = v; }
+  void setMinDCAtoBeamPipeYCut(float v) { mDCACutY = v; }
+
+ private:
+  std::shared_ptr<o2::globaltracking::DataRequest> mDataRequest;
+  o2::globaltracking::RecoContainer mRecoCont;
+  GID::mask_t mSrc = GID::getSourcesMask("TPC,ITS-TPC");
+  GID::mask_t mAllowedSources = GID::getSourcesMask("TPC,ITS-TPC");
+  // TPC
+  gsl::span<const o2::tpc::TrackTPC> mTPCTracks;
+  // ITS-TPC
+  gsl::span<const o2::dataformats::TrackTPCITS> mITSTPCTracks;
+  bool mUseMC = false;
+  float mBz = 0; ///< nominal Bz
+
+  TH1F* mPtTPC = nullptr;
+  TH1F* mFractionITSTPCmatch = nullptr;
+  TH1F* mPt = nullptr;
+  TH1F* mEta = nullptr;
+  TH1F* mChi2Matching = nullptr;
+  TH1F* mChi2Refit = nullptr;
+  TH2F* mTimeResVsPt = nullptr;
+
+  float mNTPCTracksInv = 1.f;
+  float mNITSTPCTracksInv = 1.f;
+
+  // cut values
+  float mPtCut = 0.1f;
+  float mEtaCut = 1.4f;
+  int32_t mNTPCClustersCut = 40;
+  float mDCACut = 100.f;
+  float mDCACutY = 10.f;
+
+  ClassDefNV(MatchITSTPCQC, 1);
+};
+
+} // namespace globaltracking
+} // namespace o2
+
+#endif
