6b050b1a1077f7d453f8d0d2d49fe2b8dc5dbb8b
shahoian <ruben.shahoyan@cern.ch>
2021-10-08 01:53:21 +0200

    Fix CTP digit reader

--- Detectors/CTP/workflowIO/src/DigitReaderSpec.cxx
+++ Detectors/CTP/workflowIO/src/DigitReaderSpec.cxx
@@ -10,122 +10,99 @@
 // or submit itself to any jurisdiction.
 
 #include "CTPWorkflowIO/DigitReaderSpec.h"
-#include "Framework/ConfigParamRegistry.h"
-#include "Framework/ControlService.h"
+
+#include "TFile.h"
+#include "TTree.h"
+#include "DataFormatsCTP/Digits.h"
 #include "Headers/DataHeader.h"
-#include "DPLUtils/RootTreeReader.h"
-#include "DPLUtils/MakeRootTreeWriterSpec.h"
-#include "Framework/DataSpecUtils.h"
+#include "DetectorsCommonDataFormats/DetID.h"
 #include "DetectorsCommonDataFormats/NameConf.h"
-#include <memory>
-#include <utility>
+#include "Framework/DataProcessorSpec.h"
+#include "Framework/Task.h"
+#include "Framework/ControlService.h"
+#include "Framework/ConfigParamRegistry.h"
+#include "Framework/Logger.h"
+#include <vector>
 
 using namespace o2::framework;
 
 namespace o2
 {
-
 namespace ctp
 {
 
-struct ProcessAttributes {
-  std::shared_ptr<RootTreeReader> reader;
-  std::string datatype;
-  bool terminateOnEod;
-  bool finished;
+class DigitReader : public Task
+{
+ public:
+  DigitReader() = delete;
+  DigitReader(bool useMC);
+  ~DigitReader() override = default;
+  void init(InitContext& ic) final;
+  void run(ProcessingContext& pc) final;
+
+ protected:
+  void connectTree(const std::string& filename);
+
+  std::vector<o2::ctp::CTPDigit> mDigits, *mDigitsPtr = &mDigits;
+  std::unique_ptr<TFile> mFile;
+  std::unique_ptr<TTree> mTree;
+
+  bool mUseMC = false; // use MC truth
+  std::string mDigTreeName = "o2sim";
+  std::string mDigitBranchName = "CTPDigits";
 };
 
-DataProcessorSpec getDigitsReaderSpec(bool propagateMC)
+DigitReader::DigitReader(bool useMC)
 {
-  if (propagateMC) {
-    LOG(WARNING) << "MC truth not implemented for CTP, continouing wothout MC";
-    propagateMC = false;
+  if (useMC) {
+    LOG(INFO) << "CTP does not support MC truth at the moment";
   }
-  auto initFunction = [propagateMC](InitContext& ic) {
-    // get the option from the init context
-    auto filename = o2::utils::Str::concat_string(o2::utils::Str::rectifyDirectory(ic.options().get<std::string>("input-dir")),
-                                                  ic.options().get<std::string>("infile"));
-    auto treename = ic.options().get<std::string>("treename");
-    auto nofEvents = ic.options().get<int>("nevents");
-    auto publishingMode = nofEvents == -1 ? RootTreeReader::PublishingMode::Single : RootTreeReader::PublishingMode::Loop;
-    auto processAttributes = std::make_shared<ProcessAttributes>();
-    {
-      processAttributes->terminateOnEod = ic.options().get<bool>("terminate-on-eod");
-      processAttributes->finished = false;
-      processAttributes->datatype = "CTPDigit";
-      constexpr auto persistency = Lifetime::Timeframe;
-      o2::header::DataHeader::SubSpecificationType subSpec = 0;
-      if (propagateMC) {
-        //processAttributes->reader = std::make_shared<RootTreeReader>(treename.c_str(), // tree name
-        //filename.c_str(), // input file name
-        //nofEvents,        // number of entries to publish
-        //publishingMode,
-        //Output{"CTP", "DIGITS", subSpec, persistency},
-        //"CTPDigit", // name of data branch
-        //Output{"CTP", "DIGITSMCTR", subSpec, persistency},"CPVDigitMCTruth");
-      }
-      processAttributes->reader = std::make_shared<RootTreeReader>(treename.c_str(), // tree name
-                                                                   filename.c_str(), // input file name
-                                                                   nofEvents,        // number of entries to publish
-                                                                   publishingMode,
-                                                                   Output{"CTP", "DIGITS", subSpec, persistency},
-                                                                   "CTPDigit"); // name of data branch
-    }
+}
 
-    auto processFunction = [processAttributes, propagateMC](ProcessingContext& pc) { // false for propagateMC
-      if (processAttributes->finished) {
-        return;
-      }
+void DigitReader::init(InitContext& ic)
+{
+  auto filename = o2::utils::Str::concat_string(o2::utils::Str::rectifyDirectory(ic.options().get<std::string>("input-dir")),
+                                                ic.options().get<std::string>("ctp-digit-infile"));
+  connectTree(filename);
+}
 
-      auto publish = [&processAttributes, &pc, propagateMC]() { // false for propgateMC
-        //o2::cpv::CPVBlockHeader cpvheader(true);
-        //if (processAttributes->reader->next()) {
-        //(*processAttributes->reader)(pc, cpvheader);
-        //} else {
-        //processAttributes->reader.reset();
-        //return false;
-        //}
-        return true;
-      };
+void DigitReader::run(ProcessingContext& pc)
+{
+  auto ent = mTree->GetReadEntry() + 1;
+  assert(ent < mTree->GetEntries()); // this should not happen
 
-      bool active(true);
-      if (!publish()) {
-        active = false;
-        // Send dummy header with no payload option
-        //o2::cpv::CPVBlockHeader dummyheader(false);
-        //pc.outputs().snapshot(OutputRef{"output", 0, {dummyheader}}, 0);
-        //pc.outputs().snapshot(OutputRef{"outputTR", 0, {dummyheader}}, 0);
-        //if (propagateMC) {
-        //pc.outputs().snapshot(OutputRef{"outputMC", 0, {dummyheader}}, 0);
-        //}
-      }
-      if ((processAttributes->finished = (active == false)) && processAttributes->terminateOnEod) {
-        pc.services().get<ControlService>().endOfStream();
-        pc.services().get<ControlService>().readyToQuit(framework::QuitRequest::Me);
-      }
-    };
-    return processFunction;
-  };
-
-  std::vector<OutputSpec> outputSpecs;
-  outputSpecs.emplace_back(OutputSpec{{"output"}, "CTP", "DIGITS", 0, Lifetime::Timeframe});
-  if (propagateMC) {
-    outputSpecs.emplace_back(OutputSpec{{"outputMC"}, "CTP", "DIGITSMCTR", 0, Lifetime::Timeframe});
+  mTree->GetEntry(ent);
+  LOG(INFO) << "DigitReader pushes " << mDigits.size() << " digits at entry " << ent;
+  pc.outputs().snapshot(Output{"CTP", "DIGITS", 0, Lifetime::Timeframe}, mDigits);
+  if (mTree->GetReadEntry() + 1 >= mTree->GetEntries()) {
+    pc.services().get<ControlService>().endOfStream();
+    pc.services().get<ControlService>().readyToQuit(QuitRequest::Me);
   }
+}
 
+void DigitReader::connectTree(const std::string& filename)
+{
+  mTree.reset(nullptr); // in case it was already loaded
+  mFile.reset(TFile::Open(filename.c_str()));
+  assert(mFile && !mFile->IsZombie());
+  mTree.reset((TTree*)mFile->Get(mDigTreeName.c_str()));
+  assert(mTree);
+  mTree->SetBranchAddress(mDigitBranchName.c_str(), &mDigitsPtr);
+  LOG(INFO) << "Loaded tree from " << filename << " with " << mTree->GetEntries() << " entries";
+}
+
+DataProcessorSpec getDigitsReaderSpec(bool useMC)
+{
   return DataProcessorSpec{
     "ctp-digit-reader",
-    Inputs{}, // no inputs
-    outputSpecs,
-    AlgorithmSpec(initFunction),
+    Inputs{},
+    Outputs{{"CTP", "DIGITS", 0, Lifetime::Timeframe}},
+    AlgorithmSpec{adaptFromTask<DigitReader>(useMC)},
     Options{
-      {"infile", VariantType::String, "ctpdigits.root", {"Name of the input file"}},
-      {"input-dir", VariantType::String, "none", {"Input directory"}},
-      {"treename", VariantType::String, "o2sim", {"Name of input tree"}},
-      {"nevents", VariantType::Int, -1, {"number of events to run, -1: inf loop"}},
-      {"terminate-on-eod", VariantType::Bool, true, {"terminate on end-of-data"}},
-    }};
+      {"ctp-digit-infile", VariantType::String, "ctpdigits.root", {"Name of the input digit file"}},
+      {"input-dir", VariantType::String, "none", {"Input directory"}}}};
 }
+
 } // namespace ctp
 
 } // namespace o2
