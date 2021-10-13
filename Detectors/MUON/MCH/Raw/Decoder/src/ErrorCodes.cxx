575fa4a57f6744295b9517fefabd8befec499673
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2021-10-13 14:53:03 +0200

    MCH: prevent decoder crashes due to unknown link id error

---
+++ Detectors/MUON/MCH/Raw/Decoder/src/ErrorCodes.cxx
@@ -0,0 +1,73 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#include "MCHRawDecoder/ErrorCodes.h"
+
+namespace o2
+{
+namespace mch
+{
+namespace raw
+{
+
+void append(const char* msg, std::string& to)
+{
+  std::string s = to.size() ? "& " + to : "";
+  s += msg;
+  if (to.size()) {
+    to += "& ";
+  }
+  to += msg;
+}
+
+std::string errorCodeAsString(uint32_t ec)
+{
+  std::string msg;
+
+  if (ec & ErrorParity) {
+    append("Parity", msg);
+  }
+  if (ec & ErrorHammingCorrectable) {
+    append("Hamming Correctable", msg);
+  }
+  if (ec & ErrorHammingUncorrectable) {
+    append("Hamming Uncorrectable", msg);
+  }
+  if (ec & ErrorBadClusterSize) {
+    append("Cluster Size", msg);
+  }
+  if (ec & ErrorBadPacketType) {
+    append("Bad Packet Type", msg);
+  }
+  if (ec & ErrorBadHeartBeatPacket) {
+    append("Bad HeartBeat Packet", msg);
+  }
+  if (ec & ErrorBadIncompleteWord) {
+    append("Bad Incomplete Word", msg);
+  }
+  if (ec & ErrorTruncatedData) {
+    append("Truncated Data", msg);
+  }
+  if (ec & ErrorBadELinkID) {
+    append("Bad E-Link ID", msg);
+  }
+  if (ec & ErrorBadLinkID) {
+    append("Bad Link ID", msg);
+  }
+  if (ec & ErrorUnknownLinkID) {
+    append("Unknown Link ID", msg);
+  }
+  return msg;
+}
+
+} // namespace raw
+} // namespace mch
+} // namespace o2
