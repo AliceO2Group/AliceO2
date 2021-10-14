18112bcbfa5521e07eb22db1be14f7987fe45f75
Matteo Concas <matteo.concas@cern.ch>
2021-10-14 21:04:41 +0200

    Merge pull request #6773 from mconcas/improve-gpu-benchmark
    Improve GPU benchmark

--- GPU/GPUbenchmark/Shared/Utils.h
+++ GPU/GPUbenchmark/Shared/Utils.h
@@ -15,11 +15,17 @@
 #ifndef GPU_BENCHMARK_UTILS_H
 #define GPU_BENCHMARK_UTILS_H
 
+#if defined(__HIPCC__)
+#include "hip/hip_runtime.h"
+#endif
+
 #include <iostream>
+#include <sstream>
 #include <iomanip>
 #include <typeinfo>
 #include <boost/program_options.hpp>
 #include <vector>
+#include <string>
 #include <TTree.h>
 #include <TFile.h>
 
@@ -27,12 +33,20 @@
 #define KRED "\x1B[31m"
 #define KGRN "\x1B[32m"
 #define KYEL "\x1B[33m"
-#define KBLU "\x1B[34m"
+#define configLU "\x1B[34m"
 #define KMAG "\x1B[35m"
 #define KCYN "\x1B[36m"
 #define KWHT "\x1B[37m"
 
 #define GB (1024 * 1024 * 1024)
+
+#define failed(...)                       \
+  printf("%serror: ", KRED);              \
+  printf(__VA_ARGS__);                    \
+  printf("\n");                           \
+  printf("error: TEST FAILED\n%s", KNRM); \
+  exit(EXIT_FAILURE);
+#endif
 
 enum class Test {
   Read,
@@ -40,84 +54,191 @@ enum class Test {
   Copy
 };
 
+inline std::ostream& operator<<(std::ostream& os, Test test)
+{
+  switch (test) {
+    case Test::Read:
+      os << "read";
+      break;
+    case Test::Write:
+      os << "write";
+      break;
+    case Test::Copy:
+      os << "copy";
+      break;
+  }
+  return os;
+}
+
 enum class Mode {
   Sequential,
   Concurrent
 };
 
-enum class SplitLevel {
-  Blocks,
-  Threads
+inline std::ostream& operator<<(std::ostream& os, Mode mode)
+{
+  switch (mode) {
+    case Mode::Sequential:
+      os << "sequential";
+      break;
+    case Mode::Concurrent:
+      os << "concurrent";
+      break;
+  }
+  return os;
+}
+
+enum class KernelConfig {
+  Single,
+  Multi,
+  All
+};
+
+inline std::ostream& operator<<(std::ostream& os, KernelConfig config)
+{
+  switch (config) {
+    case KernelConfig::Single:
+      os << "single";
+      break;
+    case KernelConfig::Multi:
+      os << "multiple";
+      break;
+    case KernelConfig::All:
+      os << "all";
+      break;
+  }
+  return os;
+}
+
+template <class T>
+inline std::string getType()
+{
+  if (typeid(T).name() == typeid(char).name()) {
+    return std::string{"char"};
+  }
+  if (typeid(T).name() == typeid(size_t).name()) {
+    return std::string{"unsigned_long"};
+  }
+  if (typeid(T).name() == typeid(int).name()) {
+    return std::string{"int"};
+  }
+  if (typeid(T).name() == typeid(int4).name()) {
+    return std::string{"int4"};
+  }
+  return std::string{"unknown"};
+}
+
+inline std::string getTestName(Mode mode, Test test, KernelConfig blocks)
+{
+  std::string tname;
+  tname += (mode == Mode::Sequential) ? "seq_" : "conc_";
+  tname += (test == Test::Read) ? "read_" : (test == Test::Write) ? "write_"
+                                                                  : "copy_";
+  tname += (blocks == KernelConfig::Single) ? "SB" : "MB";
+  return tname;
+}
+
+template <class chunk_t>
+inline chunk_t* getPartPtr(chunk_t* scratchPtr, float chunkReservedGB, int partNumber)
+{
+  return reinterpret_cast<chunk_t*>(reinterpret_cast<char*>(scratchPtr) + static_cast<size_t>(GB * chunkReservedGB) * partNumber);
+}
+
+// Return pointer to custom offset (GB)
+template <class chunk_t>
+inline chunk_t* getCustomPtr(chunk_t* scratchPtr, int partNumber)
+{
+  return reinterpret_cast<chunk_t*>(reinterpret_cast<char*>(scratchPtr) + static_cast<size_t>(GB * partNumber));
+}
+
+inline float computeThroughput(Test test, float result, float chunkSizeGB, int ntests)
+{
+  // https://docs.nvidia.com/cuda/cuda-c-best-practices-guide/index.html
+  // Eff_bandwidth (GB/s) = (B_r + B_w) / (~1e9 * Time (s))
+
+  return 1e3 * chunkSizeGB * ntests / result;
+}
+
+template <class chunk_t>
+inline size_t getBufferCapacity(int chunkReservedGB)
+{
+  return static_cast<size_t>(GB * chunkReservedGB / sizeof(chunk_t));
+}
+
+// LCG: https://rosettacode.org/wiki/Linear_congruential_generator
+class LCGRnd
+{
+ public:
+  __host__ __device__ void seed(unsigned int s) { mSeed = s; }
+
+ protected:
+  __host__ __device__ LCGRnd() : mSeed{0}, mA{0}, mC{0}, mM(2147483648) {}
+  __host__ __device__ int rnd() { return (mSeed = (mA * mSeed + mC) % mM); }
+
+  int mA, mC;
+  unsigned int mM, mSeed;
+};
+
+class BSDRnd : public LCGRnd
+{
+ public:
+  __host__ __device__ BSDRnd()
+  {
+    mA = 1103515245;
+    mC = 12345;
+  }
+  __host__ __device__ int rnd() { return LCGRnd::rnd(); }
 };
 
 namespace o2
 {
 namespace benchmark
 {
-
 struct benchmarkOpts {
   benchmarkOpts() = default;
 
   int deviceId = 0;
   std::vector<Test> tests = {Test::Read, Test::Write, Test::Copy};
   std::vector<Mode> modes = {Mode::Sequential, Mode::Concurrent};
-  std::vector<SplitLevel> pools = {SplitLevel::Blocks, SplitLevel::Threads};
+  std::vector<KernelConfig> pools = {KernelConfig::Single, KernelConfig::Multi};
+  std::vector<std::string> dtypes = {"char", "int", "ulong"};
+  std::vector<std::pair<int, int>> testChunks;
   float chunkReservedGB = 1.f;
-  int nRegions = 2;
+  float threadPoolFraction = 1.f;
   float freeMemoryFractionToAllocate = 0.95f;
   int kernelLaunches = 1;
   int nTests = 1;
+  int streams = 8;
+  std::string outFileName = "benchmark_result";
 };
 
-template <class T>
+template <class chunk_t>
 struct gpuState {
   int getMaxChunks()
   {
     return (double)scratchSize / (chunkReservedGB * GB);
   }
 
-  void computeScratchPtrs()
+  size_t getChunkCapacity()
   {
-    partAddrOnHost.resize(getMaxChunks());
-    for (size_t iBuffAddress{0}; iBuffAddress < getMaxChunks(); ++iBuffAddress) {
-      partAddrOnHost[iBuffAddress] = reinterpret_cast<T*>(reinterpret_cast<char*>(scratchPtr) + static_cast<size_t>(GB * chunkReservedGB) * iBuffAddress);
-    }
-  }
-
-  size_t getPartitionCapacity()
-  {
-    return static_cast<size_t>(GB * chunkReservedGB / sizeof(T));
-  }
-
-  std::vector<T*> getScratchPtrs()
-  {
-    return partAddrOnHost;
-  }
-
-  std::vector<std::vector<T>>& getHostBuffers()
-  {
-    return gpuBuffersHost;
+    return getBufferCapacity<chunk_t>(chunkReservedGB);
   }
 
   int getNKernelLaunches() { return iterations; }
+  int getStreamsPoolSize() { return streams; }
 
   // Configuration
   size_t nMaxThreadsPerDimension;
   int iterations;
+  int streams;
 
   float chunkReservedGB; // Size of each partition (GB)
 
   // General containers and state
-  T* scratchPtr;                              // Pointer to scratch buffer
-  size_t scratchSize;                         // Size of scratch area (B)
-  std::vector<T*> partAddrOnHost;             // Pointers to scratch partitions on host vector
-  std::vector<std::vector<T>> gpuBuffersHost; // Host-based vector-ized data
-  T* deviceReadResultsPtr;                    // Results of the read test (single variable) on GPU
-  std::vector<T> hostReadResultsVector;       // Results of the read test (single variable) on host
-  T* deviceWriteResultsPtr;                   // Results of the write test (single variable) on GPU
-  std::vector<T> hostWriteResultsVector;      // Results of the write test (single variable) on host
-  T* deviceCopyInputsPtr;                     // Inputs of the copy test (single variable) on GPU
-  std::vector<T> hostCopyInputsVector;        // Inputs of the copy test (single variable) on host
+  chunk_t* scratchPtr;                         // Pointer to scratch buffer
+  size_t scratchSize;                          // Size of scratch area (B)
+  std::vector<chunk_t*> partAddrOnHost;        // Pointers to scratch partitions on host vector
+  std::vector<std::pair<int, int>> testChunks; // Vector of definitions for arbitrary chunks
 
   // Static info
   size_t totalMemory;
@@ -131,14 +252,16 @@ class ResultWriter
  public:
   explicit ResultWriter(const std::string resultsTreeFilename = "benchmark_results.root");
   ~ResultWriter() = default;
-  void storeBenchmarkEntry(int chunk, float entry);
+  void storeBenchmarkEntry(Test test, int chunk, float entry, float chunkSizeGB, int nLaunches);
   void addBenchmarkEntry(const std::string bName, const std::string type, const int nChunks);
   void snapshotBenchmark();
   void saveToFile();
 
  private:
-  std::vector<float> mBenchmarkResults;
-  std::vector<TTree*> mBenchmarkTrees;
+  std::vector<float> mTimeResults;
+  std::vector<TTree*> mTimeTrees;
+  std::vector<float> mThroughputResults;
+  std::vector<TTree*> mThroughputTrees;
   TFile* mOutfile;
 };
 
@@ -149,26 +272,36 @@ inline ResultWriter::ResultWriter(const std::string resultsTreeFilename)
 
 inline void ResultWriter::addBenchmarkEntry(const std::string bName, const std::string type, const int nChunks)
 {
-  mBenchmarkTrees.emplace_back(new TTree((bName + "_" + type).data(), (bName + "_" + type).data()));
-  mBenchmarkResults.clear();
-  mBenchmarkResults.resize(nChunks);
-  mBenchmarkTrees.back()->Branch("elapsed", &mBenchmarkResults);
+  mTimeTrees.emplace_back(new TTree((bName + "_" + type).data(), (bName + "_" + type).data()));
+  mTimeResults.clear();
+  mTimeResults.resize(nChunks);
+  mTimeTrees.back()->Branch("elapsed", &mTimeResults);
+
+  mThroughputTrees.emplace_back(new TTree((bName + "_" + type + "_TP").data(), (bName + "_" + type + "_TP").data()));
+  mThroughputResults.clear();
+  mThroughputResults.resize(nChunks);
+  mThroughputTrees.back()->Branch("throughput", &mThroughputResults);
 }
 
-inline void ResultWriter::storeBenchmarkEntry(int chunk, float entry)
+inline void ResultWriter::storeBenchmarkEntry(Test test, int chunk, float entry, float chunkSizeGB, int nLaunches)
 {
-  mBenchmarkResults[chunk] = entry;
+  mTimeResults[chunk] = entry;
+  mThroughputResults[chunk] = computeThroughput(test, entry, chunkSizeGB, nLaunches);
 }
 
 inline void ResultWriter::snapshotBenchmark()
 {
-  mBenchmarkTrees.back()->Fill();
+  mTimeTrees.back()->Fill();
+  mThroughputTrees.back()->Fill();
 }
 
 inline void ResultWriter::saveToFile()
 {
   mOutfile->cd();
-  for (auto t : mBenchmarkTrees) {
+  for (auto t : mTimeTrees) {
+    t->Write();
+  }
+  for (auto t : mThroughputTrees) {
     t->Write();
   }
   mOutfile->Close();
@@ -176,11 +309,3 @@ inline void ResultWriter::saveToFile()
 
 } // namespace benchmark
 } // namespace o2
-
-#define failed(...)                       \
-  printf("%serror: ", KRED);              \
-  printf(__VA_ARGS__);                    \
-  printf("\n");                           \
-  printf("error: TEST FAILED\n%s", KNRM); \
-  exit(EXIT_FAILURE);
-#endif
--- GPU/GPUbenchmark/macro/showBenchmarks.C
+++ GPU/GPUbenchmark/macro/showBenchmarks.C
@@ -3,6 +3,7 @@
 #include <TFile.h>
 #include <TH1F.h>
 #include <TTree.h>
+#include <TKey.h>
 #include <TBranch.h>
 #include <TCanvas.h>
 #include <TGraphErrors.h>
@@ -13,9 +14,9 @@
 
 int nBins{500};
 float minHist{0.f}, maxHist{1e4};
-void showBenchmarks(const TString fileName = "0_benchmark_results.root")
+void showBenchmarks(const bool times = false, const TString fileName = "0_benchmark_result.root")
 {
-  auto f = TFile::Open(fileName.Data(), "read");
+  auto f = TFile::Open(fileName.Data(), "UPDATE");
   std::unordered_map<std::string, TTree*> um_trees;
   std::vector<std::vector<TH1F*>> histograms;
   std::vector<TGraphErrors*> results;
@@ -40,40 +41,82 @@ void showBenchmarks(const TString fileName = "0_benchmark_results.root")
               if (keyPair.first.find(type) != std::string::npos) {
                 for (auto& pattern : patterns) {
                   if (keyPair.first.find(pattern) != std::string::npos) {
-                    // Single Tree entry, we know test, type, mode, pattern
-                    std::vector<float>* measures = 0;
-                    TBranch* elapsed;
-                    keyPair.second->SetBranchAddress("elapsed", &measures, &elapsed);
-                    elapsed->GetEntry(keyPair.second->LoadTree(0));
-                    auto nChunk = measures->size();
-                    histograms.emplace_back(nChunk);
-                    for (int iHist{0}; iHist < (int)nChunk; ++iHist) {
-                      histograms.back()[iHist] = new TH1F(Form("Chunk_%d_%s", iHist, keyPair.first.c_str()), Form("Chunk_%d_%s;ms", iHist, keyPair.first.c_str()), 500, 0, 1e4);
-                    }
-                    for (size_t iEntry(0); iEntry < (size_t)keyPair.second->GetEntriesFast(); ++iEntry) {
-                      auto tentry = keyPair.second->LoadTree(iEntry);
-                      elapsed->GetEntry(tentry);
-                      for (int iHist{0}; iHist < (int)nChunk; ++iHist) {
-                        histograms.back()[iHist]->Fill((*measures)[iHist]);
+                    if ((keyPair.first.find("TP") == std::string::npos)) {
+                      if (times) {
+                        // Single Tree entry, we know test, type, mode, pattern
+                        std::vector<float>* measures = 0;
+                        TBranch* elapsed;
+                        keyPair.second->SetBranchAddress("elapsed", &measures, &elapsed);
+                        elapsed->GetEntry(keyPair.second->LoadTree(0));
+                        auto nChunk = measures->size();
+                        histograms.emplace_back(nChunk);
+                        for (int iHist{0}; iHist < (int)nChunk; ++iHist) {
+                          histograms.back()[iHist] = new TH1F(Form("Chunk_%d_%s", iHist, keyPair.first.c_str()), Form("Chunk_%d_%s;ms", iHist, keyPair.first.c_str()), 1000, 0, 1e4);
+                        }
+                        for (size_t iEntry(0); iEntry < (size_t)keyPair.second->GetEntriesFast(); ++iEntry) {
+                          auto tentry = keyPair.second->LoadTree(iEntry);
+                          elapsed->GetEntry(tentry);
+                          for (int iHist{0}; iHist < (int)nChunk; ++iHist) {
+                            histograms.back()[iHist]->Fill((*measures)[iHist]);
+                          }
+                        }
+
+                        std::vector<float> xCoord(nChunk), exCoord(nChunk), yCoord(nChunk), eyCoord(nChunk);
+
+                        for (size_t i{0}; i < nChunk; ++i) {
+                          xCoord[i] = (float)i;
+                          yCoord[i] = histograms.back()[i]->GetMean();
+                          eyCoord[i] = histograms.back()[i]->GetRMS();
+                          exCoord[i] = 0.f;
+                        }
+                        TCanvas* c = new TCanvas(Form("c%s", keyPair.first.c_str()), Form("%s", keyPair.first.c_str()));
+                        c->cd();
+                        TGraphErrors* g = new TGraphErrors(nChunk, xCoord.data(), yCoord.data(), exCoord.data(), eyCoord.data());
+                        g->GetYaxis()->SetRangeUser(0, 5000);
+                        g->GetXaxis()->SetRangeUser(-2.f, nChunk);
+                        g->SetTitle(Form("%s, N_{test}=%d;chunk_id;elapsed (s)", keyPair.first.c_str(), (int)keyPair.second->GetEntriesFast()));
+                        g->SetFillColor(kBlue);
+                        g->SetFillStyle(3001);
+                        g->Draw("AB");
                       }
-                    }
+                    } else { // TP plots //
+                      // Single Tree entry, we know test, type, mode, pattern
+                      std::vector<float>* measures = 0;
+                      TBranch* throughput;
+                      keyPair.second->SetBranchAddress("throughput", &measures, &throughput);
+                      throughput->GetEntry(keyPair.second->LoadTree(0));
+                      auto nChunk = measures->size();
+                      histograms.emplace_back(nChunk);
+                      for (int iHist{0}; iHist < (int)nChunk; ++iHist) {
+                        histograms.back()[iHist] = new TH1F(Form("Chunk_%d_%s", iHist, keyPair.first.c_str()), Form("Chunk_%d_%s;GB/s", iHist, keyPair.first.c_str()), 1000, 0, 1e3);
+                      }
+                      for (size_t iEntry(0); iEntry < (size_t)keyPair.second->GetEntriesFast(); ++iEntry) {
+                        auto tentry = keyPair.second->LoadTree(iEntry);
+                        throughput->GetEntry(tentry);
+                        for (int iHist{0}; iHist < (int)nChunk; ++iHist) {
+                          histograms.back()[iHist]->Fill((*measures)[iHist]);
+                        }
+                      }
 
-                    std::vector<float> xCoord(nChunk), exCoord(nChunk), yCoord(nChunk), eyCoord(nChunk);
+                      std::vector<float> xCoord(nChunk), exCoord(nChunk), yCoord(nChunk), eyCoord(nChunk);
 
-                    for (size_t i{0}; i < nChunk; ++i) {
-                      xCoord[i] = (float)i;
-                      yCoord[i] = histograms.back()[i]->GetMean();
-                      eyCoord[i] = histograms.back()[i]->GetRMS();
-                      exCoord[i] = 0.f;
+                      for (size_t i{0}; i < nChunk; ++i) {
+                        xCoord[i] = (float)i;
+                        yCoord[i] = histograms.back()[i]->GetMean();
+                        eyCoord[i] = histograms.back()[i]->GetRMS();
+                        exCoord[i] = 0.f;
+                      }
+                      TCanvas* c = new TCanvas(Form("c%s", keyPair.first.c_str()), Form("%s", keyPair.first.c_str()));
+                      c->cd();
+                      TGraphErrors* g = new TGraphErrors(nChunk, xCoord.data(), yCoord.data(), exCoord.data(), eyCoord.data());
+                      g->GetYaxis()->SetRangeUser(0, 150);
+                      g->GetXaxis()->SetRangeUser(-2.f, nChunk);
+                      g->SetTitle(Form("%s, N_{test}=%d;chunk_id;throughput (GB/s)", keyPair.first.c_str(), (int)keyPair.second->GetEntriesFast()));
+                      g->SetFillColor(kBlue);
+                      g->SetFillStyle(3001);
+                      g->Draw("AB");
+                      g->Write();
                     }
-                    TCanvas* c = new TCanvas(Form("c%s", keyPair.first.c_str()), Form("%s", keyPair.first.c_str()));
-                    c->cd();
-                    TGraphErrors* g = new TGraphErrors(nChunk, xCoord.data(), yCoord.data(), exCoord.data(), eyCoord.data());
-                    g->GetYaxis()->SetRangeUser(0, 5000);
-                    g->GetXaxis()->SetRangeUser(-2.f, nChunk);
-                    g->SetTitle(Form("%s, N_{test}=%d;chunk_id;elapsed (ms)", keyPair.first.c_str(), (int)keyPair.second->GetEntriesFast()));
-                    g->SetFillColor(40);
-                    g->Draw("AB");
                   }
                 }
               }
@@ -83,4 +126,5 @@ void showBenchmarks(const TString fileName = "0_benchmark_results.root")
       }
     }
   }
+  f->Close();
 }
