1c63df74f9b05e4f7fe743eb2e295bcf43c3bd59
shahoian <ruben.shahoyan@cern.ch>
2021-10-20 13:23:37 +0200

    fix default name in the BC pattern gen. macro

--- macro/CreateBCPattern.C
+++ macro/CreateBCPattern.C
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-void CreateBCPattern(const std::string& outFileName = "bcPattern.root", const string& objName = "ccdb_object")
+void CreateBCPattern(const std::string& outFileName = "bcPattern.root", const string& objName = "")
 {
   // example of interacting BC pattern creation
 
@@ -43,7 +43,7 @@ void CreateBCPattern(const std::string& outFileName = "bcPattern.root", const st
   pattern.print();
 
   if (!outFileName.empty()) {
-    std::string nm = objName.empty() ? objName : o2::BunchFilling::Class()->GetName();
+    std::string nm = !objName.empty() ? objName : o2::BunchFilling::Class()->GetName();
     LOG(INFO) << "Storing pattern with name " << nm << " in a file " << outFileName;
     TFile outf(outFileName.c_str(), "update");
     outf.WriteObjectAny(&pattern, pattern.Class(), nm.c_str());
