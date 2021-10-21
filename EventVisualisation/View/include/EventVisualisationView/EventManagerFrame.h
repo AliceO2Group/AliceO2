de79cc1d663056420c57419c922c2da5ac0d8f0d
Julian Myrcha <jmyrcha@cern.ch>
2021-10-21 20:06:25 +0200

    o2-eve: adding maximum allowed memory option

--- EventVisualisation/View/include/EventVisualisationView/EventManagerFrame.h
+++ EventVisualisation/View/include/EventVisualisationView/EventManagerFrame.h
@@ -39,6 +39,7 @@ class EventManagerFrame : public TGMainFrame
   bool inTick = false;
   bool setInTick();   // try set inTick, return true if set, false if already set
   void clearInTick(); // safely clears inTick
+  void checkMemory(); // check memory used end exit(-1) if it is too much
   static TGTextButton* makeButton(TGCompositeFrame* p, const char* txt, Int_t width = 0,
                                   Int_t lo = 0, Int_t ro = 0, Int_t to = 0, Int_t bo = 0);
 
