128a2958e0de3917f23854aeb7ccc24bcd9de3d6
David Rohr <drohr@jwdt.org>
2021-10-25 16:25:38 +0200

    Add pass-through input mode to tpc-reco-workflow

--- Detectors/TPC/workflow/src/RecoWorkflow.cxx
+++ Detectors/TPC/workflow/src/RecoWorkflow.cxx
@@ -67,6 +67,7 @@ template <typename T>
 using BranchDefinition = MakeRootTreeWriterSpec::BranchDefinition<T>;
 
 const std::unordered_map<std::string, InputType> InputMap{
+  {"pass-through", InputType::PassThrough},
   {"digitizer", InputType::Digitizer},
   {"digits", InputType::Digits},
   {"clustershardware", InputType::ClustersHardware},
@@ -121,7 +122,7 @@ framework::WorkflowSpec getWorkflow(CompletionPolicyData* policyData, std::vecto
     zsOnTheFly = false;
     propagateMC = false;
   }
-  if (inputType == InputType::ClustersHardware || inputType == InputType::Clusters) {
+  if (inputType == InputType::PassThrough || inputType == InputType::ClustersHardware || inputType == InputType::Clusters) {
     caClusterer = false;
   }
   if (!caClusterer) {
@@ -161,7 +162,7 @@ framework::WorkflowSpec getWorkflow(CompletionPolicyData* policyData, std::vecto
     return false;
   }};
 
-  if (!disableRootInput) {
+  if (!disableRootInput || inputType == InputType::PassThrough) {
     // The OutputSpec of the PublisherSpec is configured depending on the input
     // type. Note that the configuration of the dispatch trigger in the main file
     // needs to be done in accordance. This means, if a new input option is added
@@ -235,6 +236,10 @@ framework::WorkflowSpec getWorkflow(CompletionPolicyData* policyData, std::vecto
   bool outRaw = inputType == InputType::Digits && isEnabled(OutputType::ZSRaw) && !isEnabled(OutputType::DisableWriter);
   //bool runZSDecode = inputType == InputType::ZSRaw;
   bool zsToDigit = inputType == InputType::ZSRaw && isEnabled(OutputType::Digits);
+
+  if (inputType == InputType::PassThrough) {
+    runTracker = runHWDecoder = runClusterer = runClusterEncoder = zsToDigit = false;
+  }
 
   WorkflowSpec parallelProcessors;
   //////////////////////////////////////////////////////////////////////////////////////////////
@@ -398,7 +403,7 @@ framework::WorkflowSpec getWorkflow(CompletionPolicyData* policyData, std::vecto
                                    BranchDefinition<std::vector<char>>{InputSpec{"mc", ConcreteDataTypeMatcher{"TPC", "CLNATIVEMCLBL"}},
                                                                        "TPCClusterNativeMCTruth",
                                                                        "mcbranch", fillLabels},
-                                   (caClusterer || decompressTPC) && !isEnabled(OutputType::SendClustersPerSector)));
+                                   (caClusterer || decompressTPC || inputType == InputType::PassThrough) && !isEnabled(OutputType::SendClustersPerSector)));
   }
 
   if (zsOnTheFly) {
