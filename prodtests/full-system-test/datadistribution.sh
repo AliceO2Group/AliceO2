63fecb29e536f67a84530c8bb38423835ab6df9c
David Rohr <drohr@jwdt.org>
2021-10-26 14:32:10 +0200

    FST: Add INPUT_FILE_LIST and INPUT_FILE_COPY_CMD options

--- prodtests/full-system-test/datadistribution.sh
+++ prodtests/full-system-test/datadistribution.sh
@@ -3,25 +3,30 @@
 MYDIR="$(dirname $(readlink -f $0))"
 source $MYDIR/setenv.sh
 
-if [ `which StfBuilder 2> /dev/null | wc -l` == "0" ]; then
+if [[ `which StfBuilder 2> /dev/null | wc -l` == "0" ]]; then
   eval "`alienv shell-helper`"
   alienv --no-refresh load DataDistribution/latest
 fi
 
 # For benchmark only, do NOT copy&paste!
-export DATADIST_SHM_DELAY=30
+[[ $NUMAGPUIDS == 1 ]] && export DATADIST_SHM_DELAY=30
 
-if [ ! -z $DD_STARTUP_DELAY ]; then
+if [[ ! -z $DD_STARTUP_DELAY ]]; then
   sleep $DD_STARTUP_DELAY
 fi
 
-if [ -z $INPUT_FILE_LIST ]; then
+if [[ -z $INPUT_FILE_LIST ]]; then
   DD_INPUT_CMD="--data-source-dir ./raw/timeframe"
 else
-  DD_INPUT_CMD="--data-source-copy-cmd=\"XrdSecPROTOCOL=sss,unix xrdcp -N root://eosaliceo2.cern.ch/?src ?dst\" --data-source-file-list $INPUT_FILE_LIST"
+  DD_INPUT_CMD="--data-source-file-list $INPUT_FILE_LIST"
+  if [[ -z $INPUT_FILE_COPY_CMD ]]; then
+    DD_INPUT_CMD+=" --data-source-copy-cmd \"XrdSecPROTOCOL=sss,unix xrdcp -N root://eosaliceo2.cern.ch/?src ?dst\""
+  else
+    DD_INPUT_CMD+=" --data-source-copy-cmd \"$INPUT_FILE_COPY_CMD\""
+  fi
 fi
 
-if [ $NTIMEFRAMES != -1 ]; then
+if [[ $NTIMEFRAMES != -1 ]]; then
   export DATADIST_FILE_READ_COUNT=$NTIMEFRAMES
   DD_INPUT_CMD+=" --data-source-repeat"
 fi
