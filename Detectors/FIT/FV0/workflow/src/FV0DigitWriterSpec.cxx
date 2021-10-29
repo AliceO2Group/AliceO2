d112b93805be46046a9082d249d4f8e3a3bc5041
Sebastian Bysiak <sebastian.bysiak@gmail.com>
2021-10-29 06:07:22 +0200

    Add o2-fv0/fdd-digits-writer-workflow(s)

---
+++ Detectors/FIT/FV0/workflow/src/FV0DigitWriterSpec.cxx
@@ -0,0 +1,72 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// @file   FV0DigitWriterSpec.cxx
+#include "FV0Workflow/FV0DigitWriterSpec.h"
+
+namespace o2
+{
+namespace fv0
+{
+
+template <typename T>
+using BranchDefinition = framework::MakeRootTreeWriterSpec::BranchDefinition<T>;
+
+o2::framework::DataProcessorSpec getFV0DigitWriterSpec(bool mctruth, bool trigInp)
+{
+  using InputSpec = framework::InputSpec;
+  using MakeRootTreeWriterSpec = framework::MakeRootTreeWriterSpec;
+  // Spectators for logging
+  auto logger = [](std::vector<o2::fv0::BCData> const& vecDigits) {
+    LOG(INFO) << "FV0DigitWriter pulled " << vecDigits.size() << " digits";
+  };
+  // the callback to be set as hook for custom action when the writer is closed
+  auto finishWriting = [](TFile* outputfile, TTree* outputtree) {
+    const auto* brArr = outputtree->GetListOfBranches();
+    int64_t nent = 0;
+    for (const auto* brc : *brArr) {
+      int64_t n = ((const TBranch*)brc)->GetEntries();
+      if (nent && (nent != n)) {
+        LOG(ERROR) << "Branches have different number of entries";
+      }
+      nent = n;
+    }
+    outputtree->SetEntries(nent);
+    outputtree->Write();
+    outputfile->Close();
+  };
+
+  auto labelsdef = BranchDefinition<o2::dataformats::MCTruthContainer<o2::fv0::MCLabel>>{InputSpec{"labelinput", "FV0", "DIGITSMCTR"},
+                                                                                         "FV0DIGITSMCTR", mctruth ? 1 : 0};
+  if (trigInp) {
+    return MakeRootTreeWriterSpec("FV0DigitWriter",
+                                  "fv0digits.root",
+                                  "o2sim",
+                                  MakeRootTreeWriterSpec::CustomClose(finishWriting),
+                                  BranchDefinition<std::vector<o2::fv0::BCData>>{InputSpec{"digitBCinput", "FV0", "DIGITSBC"}, "FV0DigitBC", "fv0-digits-branch-name", 1,
+                                                                                 logger},
+                                  BranchDefinition<std::vector<o2::fv0::ChannelData>>{InputSpec{"digitChinput", "FV0", "DIGITSCH"}, "FV0DigitCh", "fv0-chhdata-branch-name"},
+                                  BranchDefinition<std::vector<o2::fv0::DetTrigInput>>{InputSpec{"digitTrinput", "FV0", "TRIGGERINPUT"}, "TRIGGERINPUT", "fv0-triggerinput-branch-name"},
+                                  std::move(labelsdef))();
+  } else {
+    return MakeRootTreeWriterSpec("FV0DigitWriterRaw",
+                                  "o2_fv0digits.root",
+                                  "o2sim",
+                                  MakeRootTreeWriterSpec::CustomClose(finishWriting),
+                                  BranchDefinition<std::vector<o2::fv0::BCData>>{InputSpec{"digitBCinput", "FV0", "DIGITSBC"}, "FV0DigitBC", "fv0-digits-branch-name", 1,
+                                                                                 logger},
+                                  BranchDefinition<std::vector<o2::fv0::ChannelData>>{InputSpec{"digitChinput", "FV0", "DIGITSCH"}, "FV0DigitCh", "fv0-chhdata-branch-name"},
+                                  std::move(labelsdef))();
+  }
+}
+
+} // end namespace fv0
+} // end namespace o2
