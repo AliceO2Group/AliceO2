9dfce4f74799908eeeace5c6989131a948850763
noferini <noferini@bo.infn.it>
2021-11-03 10:02:01 +0100

    fix in tof latency window

--- Detectors/GlobalTracking/src/MatchTOF.cxx
+++ Detectors/GlobalTracking/src/MatchTOF.cxx
@@ -674,7 +674,7 @@ void MatchTOF::doMatching(int sec)
           foundCluster = true;
           // set event indexes (to be checked)
           int eventIndexTOFCluster = mTOFClusSectIndexCache[indices[0]][itof];
-          mMatchedTracksPairs.emplace_back(cacheTrk[itrk], eventIndexTOFCluster, mTOFClusWork[cacheTOF[itof]].getTime(), chi2, trkLTInt[iPropagation], mTrackGid[type][cacheTrk[itrk]], type); // TODO: check if this is correct!
+          mMatchedTracksPairs.emplace_back(cacheTrk[itrk], eventIndexTOFCluster, mTOFClusWork[cacheTOF[itof]].getTime(), chi2, trkLTInt[iPropagation], mTrackGid[type][cacheTrk[itrk]], type, trefTOF.getTime() - (minTrkTime + maxTrkTime) * 0.5); // TODO: check if this is correct!
         }
       }
     }
