934d334eca19d3821168a8f9c172084c120183ea
David Rohr <drohr@jwdt.org>
2021-11-04 18:05:37 +0100

    FST: explicit EOS not needed any more in readout-proxy

--- prodtests/full-system-test/dpl-workflow.sh
+++ prodtests/full-system-test/dpl-workflow.sh
@@ -313,7 +313,7 @@ elif [ $RAWTFINPUT == 1 ]; then
   add_W o2-raw-tf-reader-workflow "--delay $TFDELAY --loop $TFLOOP $NTIMEFRAMES_CMD --input-data ${TFName} ${INPUT_FILE_COPY_CMD+--copy-cmd} ${INPUT_FILE_COPY_CMD} --onlyDet $WORKFLOW_DETECTORS"
 elif [ $EXTINPUT == 1 ]; then
   PROXY_CHANNEL="name=readout-proxy,type=pull,method=connect,address=ipc://@$INRAWCHANNAME,transport=shmem,rateLogging=$EPNSYNCMODE"
-  PROXY_INSPEC="dd:FLP/DISTSUBTIMEFRAME/0;eos:***/INFORMATION"
+  PROXY_INSPEC="dd:FLP/DISTSUBTIMEFRAME/0"
   PROXY_IN_N=0
   for i in `echo "$WORKFLOW_DETECTORS" | sed "s/,/ /g"`; do
     if has_detector_flp_processing $i; then
