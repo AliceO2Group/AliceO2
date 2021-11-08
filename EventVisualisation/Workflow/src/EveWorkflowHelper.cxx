26329e3358ba79614f0ddc2bb53dc5503da7b1ea
Julian Myrcha <julian.myrcha@cern.ch>
2021-11-08 20:34:24 +0100

    o2-eve-display: trd show tracks, tracks limited to detectors with clusters

--- EventVisualisation/Workflow/src/EveWorkflowHelper.cxx
+++ EventVisualisation/Workflow/src/EveWorkflowHelper.cxx
@@ -112,6 +112,7 @@ void EveWorkflowHelper::draw(const std::string& jsonPath, int numberOfFiles, int
 std::vector<PNT> EveWorkflowHelper::getTrackPoints(const o2::track::TrackPar& trc, float minR, float maxR, float maxStep, float minZ, float maxZ)
 {
   // adjust minR according to real track start from track starting point
+  auto maxR2 = maxR * maxR;
   float rMin = std::sqrt(trc.getX() * trc.getX() + trc.getY() * trc.getY());
   if (rMin > minR) {
     minR = rMin;
@@ -149,13 +150,19 @@ std::vector<PNT> EveWorkflowHelper::getTrackPoints(const o2::track::TrackPar& tr
     if (xyz.Z() > maxZ) {
       return pnts;
     }
+    if (xyz.X() * xyz.X() + xyz.Y() * xyz.Y() > maxR2) {
+      return pnts;
+    }
     pnts.emplace_back(PNT{xyz.X(), xyz.Y(), xyz.Z()});
   }
   return pnts;
 }
 
-void EveWorkflowHelper::addTrackToEvent(const o2::track::TrackParCov& tr, GID gid, float trackTime, float dz)
+void EveWorkflowHelper::addTrackToEvent(const o2::track::TrackParCov& tr, GID gid, float trackTime, float dz, GID::Source source)
 {
+  if (source == GID::NSources) {
+    source = (o2::dataformats::GlobalTrackID::Source)gid.getSource();
+  }
   auto vTrack = mEvent.addTrack({.time = trackTime,
                                  .charge = tr.getCharge(),
                                  .PID = tr.getPID(),
@@ -164,8 +171,8 @@ void EveWorkflowHelper::addTrackToEvent(const o2::track::TrackParCov& tr, GID gi
                                  .theta = tr.getTheta(),
                                  .eta = tr.getEta(),
                                  .gid = gid.asString(),
-                                 .source = (o2::dataformats::GlobalTrackID::Source)gid.getSource()});
-  auto pnts = getTrackPoints(tr, minmaxR[gid.getSource()].first, minmaxR[gid.getSource()].second, 4);
+                                 .source = source});
+  auto pnts = getTrackPoints(tr, minmaxR[source].first, minmaxR[source].second, 4, minmaxZ[source].first, minmaxZ[source].second);
 
   for (size_t ip = 0; ip < pnts.size(); ip++) {
     vTrack->addPolyPoint(pnts[ip][0], pnts[ip][1], pnts[ip][2] + dz);
@@ -196,11 +203,11 @@ void EveWorkflowHelper::prepareMFTClusters(const o2::itsmft::TopologyDictionary&
   }
 }
 
-void EveWorkflowHelper::drawITSTPC(GID gid, float trackTime)
+void EveWorkflowHelper::drawITSTPC(GID gid, float trackTime, GID::Source source)
 {
   // LOG(INFO) << "EveWorkflowHelper::drawITSTPC " << gid;
   const auto& track = mRecoCont.getTPCITSTrack(gid);
-  addTrackToEvent(track, gid, trackTime, 0.);
+  addTrackToEvent(track, gid, trackTime, 0., source);
   drawITSClusters(track.getRefITS(), trackTime);
   drawTPCClusters(track.getRefTPC(), trackTime * mMUS2TPCTimeBins);
 }
@@ -225,9 +232,9 @@ void EveWorkflowHelper::drawTPCTRD(GID gid, float trackTime)
 
 void EveWorkflowHelper::drawITSTPCTRD(GID gid, float trackTime)
 {
-  // LOG(INFO) << "EveWorkflowHelper::drawITSTPCTRD " << gid;
+  //LOG(INFO) << "EveWorkflowHelper::drawITSTPCTRD " << gid;
   const auto& itsTpcTrdTrack = mRecoCont.getITSTPCTRDTrack<o2::trd::TrackTRD>(gid);
-  drawITSTPC(itsTpcTrdTrack.getRefGlobalTrackId(), trackTime);
+  drawITSTPC(itsTpcTrdTrack.getRefGlobalTrackId(), trackTime, GID::ITSTPCTRD);
   drawTRDClusters(itsTpcTrdTrack, trackTime);
 }
 
@@ -355,7 +362,8 @@ void EveWorkflowHelper::drawTPC(GID gid, float trackTime)
                                  .eta = tr.getEta(),
                                  .gid = gid.asString(),
                                  .source = GID::TPC});
-  auto pnts = getTrackPoints(tr, minmaxR[gid.getSource()].first, minmaxR[gid.getSource()].second, 4, -250, 250);
+  auto source = gid.getSource();
+  auto pnts = getTrackPoints(tr, minmaxR[source].first, minmaxR[source].second, 4, minmaxZ[source].first, minmaxZ[source].second);
   float dz = 0.0;
   for (size_t ip = 0; ip < pnts.size(); ip++) {
     vTrack->addPolyPoint(pnts[ip][0], pnts[ip][1], pnts[ip][2] + dz);
@@ -375,7 +383,8 @@ void EveWorkflowHelper::drawITS(GID gid, float trackTime)
                                  .eta = tr.getEta(),
                                  .gid = gid.asString(),
                                  .source = GID::ITS});
-  auto pnts = getTrackPoints(tr, minmaxR[gid.getSource()].first, minmaxR[gid.getSource()].second, 1.0, -250, 250);
+  auto source = gid.getSource();
+  auto pnts = getTrackPoints(tr, minmaxR[source].first, minmaxR[source].second, 1.0, minmaxZ[source].first, minmaxZ[source].second);
   float dz = 0.0;
   for (size_t ip = 0; ip < pnts.size(); ip++) {
     vTrack->addPolyPoint(pnts[ip][0], pnts[ip][1], pnts[ip][2] + dz);
