fe70e646a4e5d66ad0e9f4dcd761414bb7d66936
Nazar Burmasov <nazar.burmasov@cern.ch>
2021-11-09 13:29:38 +0300

    Add metadata to AOD (#7503)
    * Add metadata to AOD
    
    * pass production tags using separate configurables
    
    * set data type

--- Detectors/AOD/include/AODProducerWorkflow/AODProducerWorkflowSpec.h
+++ Detectors/AOD/include/AODProducerWorkflow/AODProducerWorkflowSpec.h
@@ -34,6 +34,7 @@
 #include "ReconstructionDataFormats/VtxTrackIndex.h"
 #include "SimulationDataFormat/MCCompLabel.h"
 #include "Steer/MCKinematicsReader.h"
+#include "TMap.h"
 #include "TStopwatch.h"
 
 #include <boost/functional/hash.hpp>
@@ -191,7 +192,7 @@ typedef boost::unordered_map<Triplet_t, int, TripletHash, TripletEqualTo> Triple
 class AODProducerWorkflowDPL : public Task
 {
  public:
-  AODProducerWorkflowDPL(GID::mask_t src, std::shared_ptr<DataRequest> dataRequest, bool useMC = true) : mInputSources(src), mDataRequest(dataRequest), mUseMC(useMC) {}
+  AODProducerWorkflowDPL(GID::mask_t src, std::shared_ptr<DataRequest> dataRequest, std::string resFile, bool useMC = true) : mInputSources(src), mDataRequest(dataRequest), mResFile{resFile}, mUseMC(useMC) {}
   ~AODProducerWorkflowDPL() override = default;
   void init(InitContext& ic) final;
   void run(ProcessingContext& pc) final;
@@ -207,6 +208,11 @@ class AODProducerWorkflowDPL : public Task
   int mRunNumber{-1};
   int mTruncate{1};
   int mRecoOnly{0};
+  TString mResFile{"AO2D"};
+  TString mLPMProdTag{""};
+  TString mAnchorPass{""};
+  TString mAnchorProd{""};
+  TString mRecoPass{""};
   TStopwatch mTimer;
 
   // unordered map connects global indices and table indices of barrel tracks
@@ -215,6 +221,9 @@ class AODProducerWorkflowDPL : public Task
   int mTableTrID{0};
 
   TripletsMap_t mToStore;
+
+  // MC production metadata holder
+  TMap mMetaData;
 
   std::shared_ptr<DataRequest> mDataRequest;
 
@@ -355,7 +364,7 @@ class AODProducerWorkflowDPL : public Task
 };
 
 /// create a processor spec
-framework::DataProcessorSpec getAODProducerWorkflowSpec(GID::mask_t src, bool useMC);
+framework::DataProcessorSpec getAODProducerWorkflowSpec(GID::mask_t src, bool useMC, std::string resFile);
 
 } // namespace o2::aodproducer
 
