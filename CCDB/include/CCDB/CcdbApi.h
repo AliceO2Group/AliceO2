1860355b1d05a3d6f40d8322bd6ac35c953b9e11
Barthelemy <barthelemy.von.haller@cern.ch>
2021-11-18 13:46:29 +0100

    Add missing doxy

--- CCDB/include/CCDB/CcdbApi.h
+++ CCDB/include/CCDB/CcdbApi.h
@@ -180,6 +180,13 @@ class CcdbApi //: public DatabaseInterface
    */
   void deleteObject(std::string const& path, long timestamp = -1) const;
 
+  /**
+   * Update the metadata of the object defined by the provided timestamp, and id if provided.
+   * @param path Path to the object to update
+   * @param metadata The metadata to update
+   * @param timestamp The timestamp to select the object
+   * @param id The id, if any, to select the object
+   */
   void updateMetadata(std::string const& path, std::map<std::string, std::string> const& metadata, long timestamp, std::string const& id = "");
 
   /**
