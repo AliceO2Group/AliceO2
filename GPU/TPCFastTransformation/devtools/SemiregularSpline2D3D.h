5cd140ee3525bf7c1f78750c735737f3c5c18401
wiechula <11199190+wiechula@users.noreply.github.com>
2021-11-19 08:30:15 +0100

    Add missing protection for ROOTCLING (#7657)

--- GPU/TPCFastTransformation/devtools/SemiregularSpline2D3D.h
+++ GPU/TPCFastTransformation/devtools/SemiregularSpline2D3D.h
@@ -23,7 +23,7 @@
 #include "RegularSpline1D.h"
 #include "FlatObject.h"
 
-#if !defined(__CINT__) && !defined(__ROOTCINT__) && !defined(GPUCA_GPUCODE) && !defined(GPUCA_NO_VC)
+#if !defined(__CINT__) && !defined(__ROOTCINT__) && !defined(__ROOTCLING__) && !defined(GPUCA_GPUCODE) && !defined(GPUCA_NO_VC)
 #include <Vc/Vc>
 #include <Vc/SimdArray>
 #endif
@@ -396,7 +396,7 @@ inline void SemiregularSpline2D3D::getSplineVec(const float* correctedData, floa
   // Same as getSpline, but using vectorized calculation.
   // \param correctedData should be at least 128-bit aligned
 
-#if !defined(__CINT__) && !defined(__ROOTCINT__) && !defined(GPUCA_GPUCODE) && !defined(GPUCA_NO_VC)
+#if !defined(__CINT__) && !defined(__ROOTCINT__) && !defined(__ROOTCLING__) && !defined(GPUCA_GPUCODE) && !defined(GPUCA_NO_VC)
   //&& !defined(__CLING__)
   /*
 	Idea: There are 16 knots important for (u, v).
