c3eeb86744bcbd2717536560a8c6f4b50daf82fc
iravasen <ivan.ravasenga@cern.ch>
2021-11-19 14:08:53 +0100

    Adding TreeMaker to ITS framework

---
+++ Detectors/ITSMFT/ITS/workflow/src/its-tree-maker-workflow.cxx
@@ -0,0 +1,27 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#include "ITSWorkflow/TreeMakerWorkflow.h"
+#include "ITSWorkflow/TreeMaker.h"
+#include "CommonUtils/ConfigurableParam.h"
+#include "ITStracking/TrackingConfigParam.h"
+#include "ITStracking/Configuration.h"
+
+using namespace o2::framework;
+
+#include "Framework/runDataProcessing.h"
+#include "Framework/Logger.h"
+
+WorkflowSpec defineDataProcessing(ConfigContext const& configcontext)
+{
+    LOG(INFO) << "Initializing O2 Tree Maker :-))))))";
+
+    return std::move(o2::its::tree_maker_workflow::getWorkflow());
+}
