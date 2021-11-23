6978e0548a5a6d726604ae4d7bee9d3acf0ddf85
shahoian <ruben.shahoyan@cern.ch>
2021-11-23 19:25:05 +0100

    Add trigger slot to FV0 CTF

--- DataFormats/Detectors/FIT/FV0/src/DataFormatsFV0LinkDef.h
+++ DataFormats/Detectors/FIT/FV0/src/DataFormatsFV0LinkDef.h
@@ -33,7 +33,7 @@
 
 #pragma link C++ class o2::fv0::CTFHeader + ;
 #pragma link C++ class o2::fv0::CTF + ;
-#pragma link C++ class o2::ctf::EncodedBlocks < o2::fv0::CTFHeader, 6, uint32_t> + ;
+#pragma link C++ class o2::ctf::EncodedBlocks < o2::fv0::CTFHeader, 7, uint32_t> + ;
 
 #pragma link C++ class o2::fv0::RecPoints + ;
 #pragma link C++ class vector < o2::fv0::RecPoints> + ;
