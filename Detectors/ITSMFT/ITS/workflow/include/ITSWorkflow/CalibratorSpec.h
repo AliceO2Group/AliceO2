5b5f46a203da542b754181efef2295b50ed81f98
iravasen <ivan.ravasenga@cern.ch>
2021-12-02 12:03:24 +0100

    Adding threshold code into new implementation

--- Detectors/ITSMFT/ITS/workflow/include/ITSWorkflow/CalibratorSpec.h
+++ Detectors/ITSMFT/ITS/workflow/include/ITSWorkflow/CalibratorSpec.h
@@ -18,8 +18,9 @@
 #include "DataFormatsITSMFT/CompCluster.h"
 #include "DataFormatsITSMFT/NoiseMap.h"
 #include "gsl/span"
+
 // ROOT includes
-#include "TH2D.h"
+#include "TH2F.h"
 
 #include <ITSBase/GeometryTGeo.h>
 
@@ -44,9 +45,9 @@ class ITSCalibrator : public Task
         ~ITSCalibrator(); // override = default;
 
         using ChipPixelData = o2::itsmft::ChipPixelData;
-		o2::itsmft::ChipMappingITS mp;
+        o2::itsmft::ChipMappingITS mp;
 
-		void init(InitContext& ic) final;
+        void init(InitContext& ic) final;
         void run(ProcessingContext& pc) final;
         void endOfStream(EndOfStreamContext& ec) final;
 
@@ -60,27 +61,50 @@ class ITSCalibrator : public Task
         std::ofstream thrfile;
 
         //detector information
-        static constexpr int NCols = 1024; //column number in Alpide chip
-        static constexpr int NRows = 512;  //row number in Alpide chip
-        static constexpr int NLayer = 7;   //layer number in ITS detector
-        static constexpr int NLayerIB = 3;
+        static constexpr short int NCols = 1024; //column number in Alpide chip
+        static constexpr short int NRows = 512;  //row number in Alpide chip
+        static constexpr short int NLayer = 7;   //layer number in ITS detector
+        static constexpr short int NLayerIB = 3;
 
-        const int NSubStave[NLayer] = { 1, 1, 1, 2, 2, 2, 2 };
-        const int NStaves[NLayer] = { 12, 16, 20, 24, 30, 42, 48 };
-        const int nHicPerStave[NLayer] = { 1, 1, 1, 8, 8, 14, 14 };
-        const int nChipsPerHic[NLayer] = { 9, 9, 9, 14, 14, 14, 14 };
-        //const int ChipBoundary[NLayer + 1] = { 0, 108, 252, 432, 3120, 6480, 14712, 24120 };
-        const int StaveBoundary[NLayer + 1] = { 0, 12, 28, 48, 72, 102, 144, 192 };
-        const int ReduceFraction = 1; //TODO: move to Config file to define this number
+        const short int nRUs = o2::itsmft::ChipMappingITS::getNRUs();
+
+        // The number of charges
+        const short int nCharge = 50;
+        // The number of injections per charge
+        const short int nInj = 50;
+
+        // Charge injected ranges from 1 - 50 in steps of 1
+        short int* x = new short int[nInj];
+
+        const short int NSubStave[NLayer] = { 1, 1, 1, 2, 2, 2, 2 };
+        const short int NStaves[NLayer] = { 12, 16, 20, 24, 30, 42, 48 };
+        const short int nHicPerStave[NLayer] = { 1, 1, 1, 8, 8, 14, 14 };
+        const short int nChipsPerHic[NLayer] = { 9, 9, 9, 14, 14, 14, 14 };
+        //const short int ChipBoundary[NLayer + 1] = { 0, 108, 252, 432, 3120, 6480, 14712, 24120 };
+        const short int StaveBoundary[NLayer + 1] = { 0, 12, 28, 48, 72, 102, 144, 192 };
+        const short int ReduceFraction = 1; //TODO: move to Config file to define this number
 
         std::array<bool, NLayer> mEnableLayers = { false };
 
         // Hash tables to store the hit and threshold information per pixel
-        std::map< int, int > currentRow;
-        std::map< int, std::vector< std::vector<int>> > pixelHits;
-        std::map< int, TH2D* > thresholds;
+        std::map< short int, short int > currentRow;
+        std::map< short int, std::vector< std::vector<short int>> > pixelHits;
+        std::map< short int, TH2F* > thresholds;
+        //std::unordered_map<unsigned int, int> mHitPixelID_Hash[7][48][2][14][14]; //layer, stave, substave, hic, chip
 
-        std::unordered_map<unsigned int, int> mHitPixelID_Hash[7][48][2][14][14]; //layer, stave, substave, hic, chip
+        // Some private helper functions
+        // Helper functions related to the running over data
+        void reset_row_hitmap(const short int&, const short int&);
+        void init_chip_data(const short int&);
+        void extract_thresh_row(const short int&, const short int&);
+        void update_output(const short int&);
+
+        // Helper functions related to threshold extraction
+        void get_row_col_arr(const short int&, short int&, float**, short int&, float**);
+        bool FindUpperLower (const short int*, const short int*, const short int&, short int&, short int&);
+        float FindStart(const short int*, const short int*, const short int&);
+        bool GetThreshold(const short int*, const short int*, const short int&, float*, float*, float*);
+        bool GetThresholdAlt(const short int*, const short int*, const short int&, float*, float*, float*);
 
         std::string mSelfName;
         std::string mDictName;
