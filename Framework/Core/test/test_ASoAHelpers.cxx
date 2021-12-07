bb2fd4ac68c106a1f59bcb6677d037d748db69d0
Maja Kabus <mkabus@cern.ch>
2021-12-07 11:10:17 +0100

    DPL Analysis: event mixing: allow for creating empty combinations (#7780)

--- Framework/Core/test/test_ASoAHelpers.cxx
+++ Framework/Core/test/test_ASoAHelpers.cxx
@@ -224,7 +224,7 @@ BOOST_AUTO_TEST_CASE(CombinationsGeneratorConstruction)
   BOOST_REQUIRE_EQUAL(*(static_cast<test::X>(std::get<1>(*(combItFiltered))).getIterator().mCurrentPos), 5);
   BOOST_REQUIRE_EQUAL(static_cast<test::X>(std::get<1>(*(combItFiltered))).getIterator().mCurrentChunk, 0);
 
-  auto comb2Filter = combinations(CombinationsStrictlyUpperIndexPolicy(testsA, testsA), filter, testsA, testsA);
+  auto comb2Filter = combinations(CombinationsStrictlyUpperIndexPolicy<TestA, TestA>(), filter, testsA, testsA);
 
   static_assert(std::is_same_v<decltype(comb2Filter.begin()), CombinationsGenerator<CombinationsStrictlyUpperIndexPolicy<Filtered<TestA>, Filtered<TestA>>>::CombinationsIterator>, "Wrong iterator type");
   static_assert(std::is_same_v<decltype(*(comb2Filter.begin())), CombinationsStrictlyUpperIndexPolicy<Filtered<TestA>, Filtered<TestA>>::CombinationType&>, "Wrong combination type");
@@ -432,7 +432,7 @@ BOOST_AUTO_TEST_CASE(Combinations)
   count = 0;
   i = 4;
   j = 5;
-  for (auto& [t0, t1] : combinations(CombinationsStrictlyUpperIndexPolicy(testsA, testsA), pairsFilter, testsA, testsA)) {
+  for (auto& [t0, t1] : combinations(CombinationsStrictlyUpperIndexPolicy<TestA, TestA>(), pairsFilter, testsA, testsA)) {
     BOOST_CHECK_EQUAL(t0.x(), i);
     BOOST_CHECK_EQUAL(t1.x(), j);
     count++;
@@ -1349,4 +1349,33 @@ BOOST_AUTO_TEST_CASE(CombinationsHelpers)
     count++;
   }
   BOOST_CHECK_EQUAL(count, expectedStrictlyUpperTriples.size());
+}
+
+BOOST_AUTO_TEST_CASE(ConstructorsWithoutTables)
+{
+  using TestA = o2::soa::Table<o2::soa::Index<>, test::X, test::Y>;
+
+  int count = 0;
+  for (auto& [t0, t1] : pairCombinations<TestA>()) {
+    count++;
+  }
+  BOOST_CHECK_EQUAL(count, 0);
+
+  count = 0;
+  for (auto& [t0, t1, t2] : tripleCombinations<TestA>()) {
+    count++;
+  }
+  BOOST_CHECK_EQUAL(count, 0);
+
+  count = 0;
+  for (auto& [c0, c1] : selfPairCombinations<int, TestA>("y", 2, -1)) {
+    count++;
+  }
+  BOOST_CHECK_EQUAL(count, 0);
+
+  count = 0;
+  for (auto& [c0, c1, c2] : selfTripleCombinations<int, TestA>("y", 2, -1)) {
+    count++;
+  }
+  BOOST_CHECK_EQUAL(count, 0);
 }
