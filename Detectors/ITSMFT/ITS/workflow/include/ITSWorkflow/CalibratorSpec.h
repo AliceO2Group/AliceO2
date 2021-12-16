4248b96940871d59ce3c1936696c71e128fa5d44
iravasen <ivan.ravasenga@cern.ch>
2021-12-16 17:42:37 +0100

    Implementing averaging and runtypes

--- Detectors/ITSMFT/ITS/workflow/include/ITSWorkflow/CalibratorSpec.h
+++ Detectors/ITSMFT/ITS/workflow/include/ITSWorkflow/CalibratorSpec.h
@@ -37,6 +37,22 @@ namespace o2
 {
 namespace its
 {
+
+typedef struct threshold_obj {
+    public:
+
+        threshold_obj(short _row, short _col, short _threshold, short _noise, bool _success) :
+            row(_row), col(_col), threshold(_threshold), noise(_noise), success(_success) { };
+
+        short int row = -1, col = -1;
+
+        // threshold * 10 saved as short int to save memory
+        short int threshold = -1, noise = -1;
+
+        // Whether or not the fit is good
+        bool success = false;
+};
+
 template <class Mapping>
 class ITSCalibrator : public Task
 {
@@ -70,13 +86,20 @@ class ITSCalibrator : public Task
 
         const short int nRUs = o2::itsmft::ChipMappingITS::getNRUs();
 
-        // The number of charges
-        const short int nCharge = 50;
-        // The number of injections per charge
+        // The number of injections per data value
         const short int nInj = 50;
 
-        // Charge injected ranges from 1 - 50 in steps of 1
-        short int* x = new short int[nInj];
+        // Number of charges in a threshold scan (from 0 - 50 inclusive)
+        const short int nCharge = 51;
+        // Number of points in a VCASN tuning (from 30 - 70 inclusive)
+        const short int nVCASN = 41;
+        // Number of in a ITHR tuning (from 30 to 100 inclusive)
+        const short int nITHR = 71;
+        // Refernce to one of the above values; updated during runtime
+        const short int & nRange;
+
+        // The x-axis of the correct data fit chosen above
+        short int* x;
 
         const short int NSubStave[NLayer] = { 1, 1, 1, 2, 2, 2, 2 };
         const short int NStaves[NLayer] = { 12, 16, 20, 24, 30, 42, 48 };
@@ -91,8 +114,13 @@ class ITSCalibrator : public Task
         // Hash tables to store the hit and threshold information per pixel
         std::map< short int, short int > currentRow;
         std::map< short int, std::vector< std::vector<short int>> > pixelHits;
-        std::map< short int, TH2F* > thresholds;
+        //std::map< short int, TH2F* > thresholds;
+        // Unordered vector for saving info to the output
+        std::map< short int, std::vector<threshold_obj> > thresholds;
         //std::unordered_map<unsigned int, int> mHitPixelID_Hash[7][48][2][14][14]; //layer, stave, substave, hic, chip
+
+        // DCS formatted data object for VCASN and ITHR tuning
+        o2::dcs::DCSconfigObject_t tuning;
 
         // Some private helper functions
         // Helper functions related to the running over data
@@ -100,16 +128,21 @@ class ITSCalibrator : public Task
         void init_chip_data(const short int&);
         void extract_thresh_row(const short int&, const short int&);
         void update_output(const short int&);
+        void set_run_type(const short int&);
 
         // Helper functions related to threshold extraction
         void get_row_col_arr(const short int&, float**, float**);
         bool FindUpperLower (const short int*, const short int*, const short int&, short int&, short int&);
-        float FindStart(const short int*, const short int*, const short int&);
-        bool GetThreshold(const short int*, const short int*, const short int&,
-                          float*, float*, float*);//, bool, TGraph**);
-        bool GetThreshold_Derivative(const short int*, const short int*, const short int&,
-                                     float*, float*, bool, TH1F**, float*);
+        bool GetThreshold(const short int*, const short int*, const short int&, float*, float*, float*);
+        bool GetThreshold_Fit(const short int*, const short int*, const short int&, float*, float*);
+        bool GetThreshold_Derivative(const short int*, const short int*, const short int&, float*, float*);
         bool GetThreshold_Hitcounting(const short int*, const short int*, const short int&, float*);
+        float find_average(const std::vector<threshold_obj>&);
+        void save_threshold(const short int&, const short int&, const short int&, float*, float*, bool);
+
+        // Helper functions for writing to the database
+        void add_db_entry(const short int&, const std::string&, const short int&, bool);
+        void add_db_entry(const short int&, const threshold_obj&);
 
         std::string mSelfName;
         std::string mDictName;
@@ -119,18 +152,12 @@ class ITSCalibrator : public Task
 
         int16_t partID = 0;
 
+        short int run_type = -1;
+
+        // Get threshold method (fit == 1, derivative == 0, or hitcounting == 2)
+        char fit_type = -1;
+
         int mTimeFrameId = 0;
-        //std::unique_ptr<RawPixelDecoder<Mapping>> mDecoder;
-        ChipPixelData* mChipDataBuffer = nullptr;
-        int mHitNumberOfChip[7][48][2][14][14] = { { { { { 0 } } } } }; //layer, stave, substave, hic, chip
-        std::vector<ChipPixelData> mChipsBuffer;  // currently processed ROF's chips data
-
-        //RawPixelDecoder<ChipMappingITS>* mRawPixelDecoder;
-        //std::unique_ptr<ChipPixelData<Mapping>> mChipPixelData;
-        //int mHitNumberOfChip[7][48][2][14][14] = { { { { { 0 } } } } }; //layer, stave, substave, hic, chip
-
-        o2::its::GeometryTGeo* mGeom;
-        std::string mGeomPath;
 
         //output file (temp solution)
         std::ofstream outfile;
