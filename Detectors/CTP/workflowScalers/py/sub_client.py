3b48fc39d6e3e57ffd6180d9f2e3b8147a05cd1a
lietava <roman.lietava@cern.ch>
2022-01-06 15:58:43 +0100

    dev: o2-ctp-proxy for counters

---
+++ Detectors/CTP/workflowScalers/py/sub_client.py
@@ -0,0 +1,38 @@
+import sys
+import zmq
+
+port = "5556"
+if len(sys.argv) > 1:
+    port =  sys.argv[1]
+    int(port)
+    
+if len(sys.argv) > 2:
+    port1 =  sys.argv[2]
+    int(port1)
+
+# Socket to talk to server
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+
+print("Collecting updates from weather server...")
+socket.connect ("tcp://localhost:%s" % port)
+
+if len(sys.argv) > 2:
+    socket.connect ("tcp://localhost:%s" % port1)
+# Subscribe to zipcode, default is NYC, 10001
+topicfilter = "10001"
+socket.setsockopt_string(zmq.SUBSCRIBE, topicfilter)
+
+# Process 5 updates
+total_value = 0
+#for update_nbr in range (5):
+while(1):
+    string = socket.recv_multipart()
+    #string = stringr.decode()
+    print("string:",string)
+    #topic, messagedata = string.split()
+    #total_value += int(messagedata)
+    #print(topic, messagedata)
+
+print("Average messagedata value for topic '%s' was %dF" % (topicfilter, total_value / update_nbr))
+      
