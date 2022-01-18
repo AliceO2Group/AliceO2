9d38388379c0c960699f7b90f561787706f33ef3
Giacomo Volpe <gvolpe@pcgvolpe.ba.infn.it>
2022-01-18 15:59:33 +0100

    Applying coding convenctions 2

--- Detectors/HMPID/simulation/src/HMPIDDigitizer.cxx
+++ Detectors/HMPID/simulation/src/HMPIDDigitizer.cxx
@@ -64,7 +64,7 @@ void HMPIDDigitizer::reset()
 void HMPIDDigitizer::process(std::vector<o2::hmpid::HitType> const& hits, std::vector<o2::hmpid::Digit>& digits)
 {
   printf("*******************In digitizer process**********************************");
-  
+
   for (auto& hit : hits) {
     int chamber, pc, px, py;
     float totalQ;
