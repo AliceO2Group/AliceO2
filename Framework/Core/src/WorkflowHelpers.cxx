721f8841f12007d85dcc6a5d6de3d677b9b4096a
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2022-01-18 22:44:34 +0100

    DPL: late add of CTP output

--- Framework/Core/src/WorkflowHelpers.cxx
+++ Framework/Core/src/WorkflowHelpers.cxx
@@ -194,7 +194,7 @@ void WorkflowHelpers::injectServiceDevices(WorkflowSpec& workflow, ConfigContext
 
   DataProcessorSpec ccdbBackend{
     .name = "internal-dpl-ccdb-backend",
-    .outputs = {{"CTP", "OrbitReset", 0}},
+    .outputs = {},
     .algorithm = CCDBHelpers::fetchFromCCDB(),
     .options = {{"condition-backend", VariantType::String, "http://alice-ccdb.cern.ch", {"URL for CCDB"}},
                 {"condition-not-before", VariantType::Int64, 0ll, {"do not fetch from CCDB objects created before provide timestamp"}},
@@ -460,6 +460,7 @@ void WorkflowHelpers::injectServiceDevices(WorkflowSpec& workflow, ConfigContext
   }
 
   if (ccdbBackend.outputs.empty() == false) {
+    ccdbBackend.outputs.push_back(OutputSpec{"CTP", "OrbitReset", 0});
     bool hasDISTSTF = false;
     InputSpec matcher{"dstf", "FLP", "DISTSUBTIMEFRAME"};
     ConcreteDataMatcher dstf{"FLP", "DISTSUBTIMEFRAME", 0};
