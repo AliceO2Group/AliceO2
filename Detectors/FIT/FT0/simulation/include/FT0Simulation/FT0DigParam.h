00597e1f950aa37f8a578a8f16bab71d51339ce3
Maciej Slupecki <maciej.slupecki@cern.ch>
2022-01-21 20:16:14 +0100

    [FT0] Cleanup: removed obsolete LookUpTable, DigitsTemp, unused ReadRaw and Digits2Raw (replaced by test-raw2digit.cxx) and digi2raw superceeded by digit2raw

--- Detectors/FIT/FT0/simulation/include/FT0Simulation/FT0DigParam.h
+++ Detectors/FIT/FT0/simulation/include/FT0Simulation/FT0DigParam.h
@@ -9,32 +9,33 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-#ifndef ALICEO2_FT0_DIGITIZATION_PARAMETERS
-#define ALICEO2_FT0_DIGITIZATION_PARAMETERS
+#ifndef ALICEO2_FT0_DIG_PARAM
+#define ALICEO2_FT0_DIG_PARAM
+
 #include <FT0Base/Geometry.h>
 #include <CommonUtils/ConfigurableParamHelper.h>
 
 namespace o2::ft0
 {
 
-struct DigitizationParameters
-  : o2::conf::ConfigurableParamHelper<DigitizationParameters> {
-  float mBunchWidth = 25;                   //ns
+struct FT0DigParam
+  : o2::conf::ConfigurableParamHelper<FT0DigParam> {
+  float mBunchWidth = 25;                   // ns
   float mChannelWidthInverse = 0.076804916; // channel width in ps inverse
 
-  float mMCPs = Geometry::Nchannels; //number of MCPs
+  float mMCPs = Geometry::Nchannels; // number of MCPs
   float mCFD_trsh = 3.;              // [mV]
   float mAmp_trsh = 100;             // [ph.e]
   float mAmpRecordLow = -4;          // integrate charge from
   float mAmpRecordUp = 15;           // to [ns]
-  float mC_side_cable_cmps = 2.86;   //ns
-  float mA_side_cable_cmps = 11.110; //ns
+  float mC_side_cable_cmps = 2.86;   // ns
+  float mA_side_cable_cmps = 11.110; // ns
   int mtrg_central_trh = 600.;       // channels
   int mtrg_semicentral_trh = 300.;   // channels
 
-  float mMip_in_V = 7;       //MIP to mV
+  float mMip_in_V = 7;       // MIP to mV
   float mPe_in_mip = 0.004;  // invserse Np.e. in MIP 1./250.
-  float mCfdShift = 1.66;    //ns
+  float mCfdShift = 1.66;    // ns
   float mCFDShiftPos = 1.47; //// shift positive part of CFD signal; distance between 0.3 of max amplitude  to max
   float mCFDdeadTime = 15.6; // ns
   float mCharge2amp = 0.22;
@@ -44,10 +45,10 @@ struct DigitizationParameters
   static constexpr float mAmpThresholdForReco = 14;    // only channels with amplitude higher will participate in calibration and collision time
   static constexpr short mTimeThresholdForReco = 1000; // only channels with time below will participate in calibration and collision time
 
-  static constexpr float mMV_2_Nchannels = 2.2857143;          //amplitude channel 7 mV ->16channels
-  static constexpr float mMV_2_NchannelsInverse = 0.437499997; //inverse amplitude channel 7 mV ->16channels
+  static constexpr float mMV_2_Nchannels = 2.2857143;          // amplitude channel 7 mV ->16channels
+  static constexpr float mMV_2_NchannelsInverse = 0.437499997; // inverse amplitude channel 7 mV ->16channels
 
-  O2ParamDef(DigitizationParameters, "DigitizationParameters");
+  O2ParamDef(FT0DigParam, "FT0DigParam");
 };
 } // namespace o2::ft0
 #endif
--- Detectors/FIT/FT0/simulation/src/FT0SimulationLinkDef.h
+++ Detectors/FIT/FT0/simulation/src/FT0SimulationLinkDef.h
@@ -19,7 +19,5 @@
 #pragma link C++ class o2::ft0::Digitizer + ;
 #pragma link C++ class o2::ft0::FT0DigParam + ;
 #pragma link C++ class o2::base::DetImpl < o2::ft0::Detector> + ;
-#pragma link C++ class o2::ft0::Digits2Raw + ;
 
 #endif
-
