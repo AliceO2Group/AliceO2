cbed4d16e477be8db5567da5c87d1715a48784c9
David Rohr <drohr@jwdt.org>
2022-01-28 22:56:03 +0100

    GPU TRD: Enable GPU compilation for more of the GPUTRDTrack constructors (TPS and ITSTPC track are now fully GPU compatible)

--- GPU/GPUTracking/TRDTracking/GPUTRDInterfaces.h
+++ GPU/GPUTracking/TRDTracking/GPUTRDInterfaces.h
@@ -192,7 +192,7 @@ class propagatorInterface<o2::base::Propagator>
 #include "GPUTPCGMPropagator.h"
 #include "GPUParam.h"
 #include "GPUDef.h"
-#ifdef GPUCA_O2_LIB
+#ifdef GPUCA_HAVE_O2HEADERS
 #include "DataFormatsTPC/TrackTPC.h"
 #include "ReconstructionDataFormats/TrackTPCITS.h"
 #endif
@@ -237,8 +237,8 @@ class trackInterface<GPUTPCGMTrackParam> : public GPUTPCGMTrackParam
     }
   };
 #endif
-#if defined(GPUCA_O2_LIB) && !defined(GPUCA_GPUCODE)
-  trackInterface<GPUTPCGMTrackParam>(const o2::dataformats::TrackTPCITS& param) : GPUTPCGMTrackParam(), mAlpha(param.getParamOut().getAlpha())
+#if defined(GPUCA_HAVE_O2HEADERS)
+  GPUd() trackInterface<GPUTPCGMTrackParam>(const o2::dataformats::TrackTPCITS& param) : GPUTPCGMTrackParam(), mAlpha(param.getParamOut().getAlpha())
   {
     SetX(param.getParamOut().getX());
     SetPar(0, param.getParamOut().getY());
@@ -250,7 +250,7 @@ class trackInterface<GPUTPCGMTrackParam> : public GPUTPCGMTrackParam
       SetCov(i, param.getParamOut().getCov()[i]);
     }
   }
-  trackInterface<GPUTPCGMTrackParam>(const o2::tpc::TrackTPC& param) : GPUTPCGMTrackParam(), mAlpha(param.getParamOut().getAlpha())
+  GPUd() trackInterface<GPUTPCGMTrackParam>(const o2::tpc::TrackTPC& param) : GPUTPCGMTrackParam(), mAlpha(param.getParamOut().getAlpha())
   {
     SetX(param.getParamOut().getX());
     SetPar(0, param.getParamOut().getY());
