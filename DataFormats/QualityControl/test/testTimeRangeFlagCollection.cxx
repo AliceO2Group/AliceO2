fb6ca80da7206502fd33d4232e1d22f071d19781
Piotr Konopka <piotr.jan.konopka@cern.ch>
2022-01-31 16:49:14 +0100

    [QC-725] CSV I/O for TRFCollections

--- DataFormats/QualityControl/test/testTimeRangeFlagCollection.cxx
+++ DataFormats/QualityControl/test/testTimeRangeFlagCollection.cxx
@@ -15,23 +15,26 @@
 
 // boost includes
 #include <boost/test/unit_test.hpp>
-
+// STL
+#include <sstream>
 // o2 includes
 #include "DataFormatsQualityControl/TimeRangeFlagCollection.h"
 #include "DataFormatsQualityControl/TimeRangeFlag.h"
 
 using namespace o2::quality_control;
 
-BOOST_AUTO_TEST_CASE(test_TimeRangeFlagCollection)
+BOOST_AUTO_TEST_CASE(test_TimeRangeFlagCollection_Methods)
 {
   TimeRangeFlag trf1{12, 34, FlagReasonFactory::ProcessingError(), "comment", "source"};
   TimeRangeFlag trf2{10, 34, FlagReasonFactory::ProcessingError(), "comment", "source"};
 
-  TimeRangeFlagCollection trfc1{"Raw data checks", "TOF"};
+  TimeRangeFlagCollection trfc1{"Raw data checks", "TOF", {10, 20000}};
   trfc1.insert(trf1); // by copy
   trfc1.insert(trf2);
   trfc1.insert({50, 77, FlagReasonFactory::Invalid()}); // by move
   BOOST_CHECK_EQUAL(trfc1.size(), 3);
+  BOOST_CHECK_EQUAL(trfc1.getStart(), 10);
+  BOOST_CHECK_EQUAL(trfc1.getEnd(), 20000);
 
   TimeRangeFlagCollection trfc2{"Reco checks", "TOF"};
   trfc2.insert({50, 77, FlagReasonFactory::Invalid()}); // this is a duplicate to an entry in trfc1
@@ -67,5 +70,60 @@ BOOST_AUTO_TEST_CASE(test_TimeRangeFlagCollection)
   // iterating
   for (const auto& trf : trfc3) {
     (void)trf;
+  }
+}
+
+BOOST_AUTO_TEST_CASE(test_TimeRangeFlagCollection_IO)
+{
+  {
+    TimeRangeFlagCollection trfc1{"xyz", "TST"};
+
+    std::stringstream store;
+    trfc1.streamTo(store);
+
+    TimeRangeFlagCollection trfc2{"xyz", "TST"};
+    trfc2.streamFrom(store);
+
+    BOOST_CHECK_EQUAL(trfc2.size(), 0);
+  }
+  {
+    TimeRangeFlagCollection trfc1{"xyz", "TST"};
+    trfc1.insert({50, 77, FlagReasonFactory::Invalid(), "a comment", "a source"});
+    trfc1.insert({51, 77, FlagReasonFactory::Invalid()});
+    trfc1.insert({1234, 3434, FlagReasonFactory::LimitedAcceptance()});
+    trfc1.insert({50, 77, FlagReasonFactory::LimitedAcceptance()});
+    trfc1.insert({43434, 63421, FlagReasonFactory::NotBadFlagExample()});
+
+    std::stringstream store;
+    trfc1.streamTo(store);
+
+    TimeRangeFlagCollection trfc2{"xyz", "TST"};
+    trfc2.streamFrom(store);
+
+    BOOST_REQUIRE_EQUAL(trfc1.size(), trfc2.size());
+    for (auto it1 = trfc1.begin(), it2 = trfc2.begin(); it1 != trfc1.end() && it2 != trfc2.end(); ++it1, ++it2) {
+      BOOST_CHECK_EQUAL(*it1, *it2);
+    }
+  }
+  {
+    std::stringstream store;
+    store << "start,end,flag_id,invalid,header,format\n";
+    store << R"(123,345,11,"fdsa",1,"comment","source")";
+    TimeRangeFlagCollection trfc1{"A", "TST"};
+    BOOST_CHECK_THROW(trfc1.streamFrom(store), std::runtime_error);
+  }
+  {
+    std::stringstream store;
+    store << "start,end,flag_id,flag_name,flag_bad,comment,source\n";
+    store << R"(123,345,11,"fdsa",1,"comment","source","toomanycolumns")" << '\n';
+    store << R"(123,345,11,"fdsa",1)" << '\n';
+    store << R"(123,,11,"fdsa",1,"comment","source")" << '\n';
+    store << R"(,345,11,"fdsa",1,"comment","source")" << '\n';
+    store << R"(123,345,,"fdsa",1,"comment","source")" << '\n';
+    store << R"(123,345,11,"",1,"comment","source")" << '\n';
+    store << R"(123,345,11,"fdsa",,"comment","source")" << '\n';
+    TimeRangeFlagCollection trfc1{"A", "TST"};
+    BOOST_CHECK_NO_THROW(trfc1.streamFrom(store));
+    BOOST_CHECK_EQUAL(trfc1.size(), 0);
   }
 }
