24547a1d0384ea069a3202434ff305921efa33d1
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2022-02-02 12:45:31 +0100

    DPL: timeout on STOP transition

--- Framework/Core/src/WorkflowCustomizationHelpers.cxx
+++ Framework/Core/src/WorkflowCustomizationHelpers.cxx
@@ -36,42 +36,42 @@ namespace o2::framework
 
 std::vector<ConfigParamSpec> WorkflowCustomizationHelpers::requiredWorkflowOptions()
 {
-  return std::vector<ConfigParamSpec>{{ConfigParamSpec{"readers", VariantType::Int64, 1ll, {"number of parallel readers to use"}},
-                                       ConfigParamSpec{"spawners", VariantType::Int64, 1ll, {"number of parallel spawners to use"}},
-                                       ConfigParamSpec{"pipeline", VariantType::String, "", {"override default pipeline size"}},
-                                       ConfigParamSpec{"clone", VariantType::String, "", {"clone processors from a template"}},
-                                       ConfigParamSpec{"labels", VariantType::String, "", {"add labels to dataprocessors"}},
-                                       ConfigParamSpec{"workflow-suffix", VariantType::String, "", {"suffix to add to all dataprocessors"}},
+  return {{{"readers", VariantType::Int64, 1ll, {"number of parallel readers to use"}},
+           {"spawners", VariantType::Int64, 1ll, {"number of parallel spawners to use"}},
+           {"pipeline", VariantType::String, "", {"override default pipeline size"}},
+           {"clone", VariantType::String, "", {"clone processors from a template"}},
+           {"labels", VariantType::String, "", {"add labels to dataprocessors"}},
+           {"workflow-suffix", VariantType::String, "", {"suffix to add to all dataprocessors"}},
 
-                                       // options for TF rate limiting
-                                       ConfigParamSpec{"timeframes-rate-limit-ipcid", VariantType::String, "-1", {"Suffix for IPC channel for metrix-feedback, -1 = disable"}},
+           // options for TF rate limiting
+           {"timeframes-rate-limit-ipcid", VariantType::String, "-1", {"Suffix for IPC channel for metrix-feedback, -1 = disable"}},
 
-                                       // options for AOD rate limiting
-                                       ConfigParamSpec{"aod-memory-rate-limit", VariantType::Int64, 0LL, {"Rate limit AOD processing based on memory"}},
+           // options for AOD rate limiting
+           {"aod-memory-rate-limit", VariantType::Int64, 0LL, {"Rate limit AOD processing based on memory"}},
 
-                                       // options for AOD writer
-                                       ConfigParamSpec{"aod-writer-json", VariantType::String, "", {"Name of the json configuration file"}},
-                                       ConfigParamSpec{"aod-writer-resfile", VariantType::String, "", {"Default name of the output file"}},
-                                       ConfigParamSpec{"aod-writer-resmode", VariantType::String, "RECREATE", {"Creation mode of the result files: NEW, CREATE, RECREATE, UPDATE"}},
-                                       ConfigParamSpec{"aod-writer-ntfmerge", VariantType::Int, -1, {"Number of time frames to merge into one file"}},
-                                       ConfigParamSpec{"aod-writer-keep", VariantType::String, "", {"Comma separated list of ORIGIN/DESCRIPTION/SUBSPECIFICATION:treename:col1/col2/..:filename"}},
+           // options for AOD writer
+           {"aod-writer-json", VariantType::String, "", {"Name of the json configuration file"}},
+           {"aod-writer-resfile", VariantType::String, "", {"Default name of the output file"}},
+           {"aod-writer-resmode", VariantType::String, "RECREATE", {"Creation mode of the result files: NEW, CREATE, RECREATE, UPDATE"}},
+           {"aod-writer-ntfmerge", VariantType::Int, -1, {"Number of time frames to merge into one file"}},
+           {"aod-writer-keep", VariantType::String, "", {"Comma separated list of ORIGIN/DESCRIPTION/SUBSPECIFICATION:treename:col1/col2/..:filename"}},
 
-                                       ConfigParamSpec{"fairmq-rate-logging", VariantType::Int, 0, {"Rate logging for FairMQ channels"}},
-                                       ConfigParamSpec{"fairmq-recv-buffer-size", VariantType::Int, 4, {"recvBufferSize option for FairMQ channels"}},
-                                       ConfigParamSpec{"fairmq-send-buffer-size", VariantType::Int, 4, {"sendBufferSize option for FairMQ channels"}},
-                                       /// Find out a place where we can write the sockets
-                                       ConfigParamSpec{"fairmq-ipc-prefix", VariantType::String, defaultIPCFolder(), {"Prefix for FairMQ channels location"}},
+           {"fairmq-rate-logging", VariantType::Int, 0, {"Rate logging for FairMQ channels"}},
+           {"fairmq-recv-buffer-size", VariantType::Int, 4, {"recvBufferSize option for FairMQ channels"}},
+           {"fairmq-send-buffer-size", VariantType::Int, 4, {"sendBufferSize option for FairMQ channels"}},
+           /// Find out a place where we can write the sockets
+           {"fairmq-ipc-prefix", VariantType::String, defaultIPCFolder(), {"Prefix for FairMQ channels location"}},
 
-                                       ConfigParamSpec{"forwarding-policy", VariantType::String, "dangling", {"Which messages to forward."
-                                                                                                              " *dangling*: dangling outputs,"
-                                                                                                              " all: all messages,"
-                                                                                                              " none: no forwarding - it will complain if you try to create dangling outputs"}},
-                                       ConfigParamSpec{"forwarding-destination",
-                                                       VariantType::String,
-                                                       "drop",
-                                                       {"Destination for forwarded messages."
-                                                        " drop: simply drop them,"
-                                                        " file: write to file,"
-                                                        " fairmq: send to output proxy"}}}};
+           {"forwarding-policy", VariantType::String, "dangling", {"Which messages to forward."
+                                                                   " *dangling*: dangling outputs,"
+                                                                   " all: all messages,"
+                                                                   " none: no forwarding - it will complain if you try to create dangling outputs"}},
+           {"forwarding-destination",
+            VariantType::String,
+            "drop",
+            {"Destination for forwarded messages."
+             " drop: simply drop them,"
+             " file: write to file,"
+             " fairmq: send to output proxy"}}}};
 }
 } // namespace o2::framework
