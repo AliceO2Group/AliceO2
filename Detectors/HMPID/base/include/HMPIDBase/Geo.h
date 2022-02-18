3620821d510a289bbd388eefde61faca267e0b59
David Rohr <drohr@jwdt.org>
2022-02-18 11:55:06 +0100

    Fix typo in raw file names, and do not use lnk prefix after crorc number

--- Detectors/HMPID/base/include/HMPIDBase/Geo.h
+++ Detectors/HMPID/base/include/HMPIDBase/Geo.h
@@ -147,20 +147,20 @@ class ReadOut
     LinkAddr Id;
     uint32_t LinkUId;
   };
-  static constexpr Lnk mEq[Geo::MAXEQUIPMENTS] = {{0, 222, 0, 160, "alio-cr1-flp160"},
-                                                  {1, 222, 1, 160, "alio-cr1-flp160"},
-                                                  {2, 222, 2, 160, "alio-cr1-flp160"},
-                                                  {3, 222, 3, 160, "alio-cr1-flp160"},
-                                                  {4, 221, 0, 160, "alio-cr1-flp160"},
-                                                  {5, 221, 1, 160, "alio-cr1-flp160"},
-                                                  {8, 221, 2, 160, "alio-cr1-flp160"},
-                                                  {9, 221, 3, 160, "alio-cr1-flp160"},
-                                                  {6, 51, 0, 161, "alio-cr1-flp161"},
-                                                  {7, 51, 1, 161, "alio-cr1-flp161"},
-                                                  {10, 51, 2, 161, "alio-cr1-flp161"},
-                                                  {11, 288, 0, 161, "alio-cr1-flp161"},
-                                                  {12, 288, 1, 161, "alio-cr1-flp161"},
-                                                  {13, 288, 2, 161, "alio-cr1-flp161"}};
+  static constexpr Lnk mEq[Geo::MAXEQUIPMENTS] = {{0, 222, 0, 160, "alio2-cr1-flp160"},
+                                                  {1, 222, 1, 160, "alio2-cr1-flp160"},
+                                                  {2, 222, 2, 160, "alio2-cr1-flp160"},
+                                                  {3, 222, 3, 160, "alio2-cr1-flp160"},
+                                                  {4, 221, 0, 160, "alio2-cr1-flp160"},
+                                                  {5, 221, 1, 160, "alio2-cr1-flp160"},
+                                                  {8, 221, 2, 160, "alio2-cr1-flp160"},
+                                                  {9, 221, 3, 160, "alio2-cr1-flp160"},
+                                                  {6, 51, 0, 161, "alio2-cr1-flp161"},
+                                                  {7, 51, 1, 161, "alio2-cr1-flp161"},
+                                                  {10, 51, 2, 161, "alio2-cr1-flp161"},
+                                                  {11, 288, 0, 161, "alio2-cr1-flp161"},
+                                                  {12, 288, 1, 161, "alio2-cr1-flp161"},
+                                                  {13, 288, 2, 161, "alio2-cr1-flp161"}};
 
   static inline int FeeId(unsigned int idx) { return (idx >= Geo::MAXEQUIPMENTS) ? -1 : mEq[idx].Id.Fee; };
   static inline int CruId(unsigned int idx) { return (idx >= Geo::MAXEQUIPMENTS) ? -1 : mEq[idx].Id.Cru; };
