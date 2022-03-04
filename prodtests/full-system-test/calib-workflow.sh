dc312749e49937141445c4c34f2fab40445c71c5
Ole Schmidt <ole.schmidt@cern.ch>
2022-03-04 16:22:13 +0100

    Fix return error code in case of missing detectors

--- prodtests/full-system-test/calib-workflow.sh
+++ prodtests/full-system-test/calib-workflow.sh
@@ -6,3 +6,5 @@ if [[ -z "$WORKFLOW" ]] || [[ -z "$MYDIR" ]]; then
 fi
 
 has_detector_calib TPC && has_detectors TPC ITS TRD TOF && add_W o2-tpc-scdcalib-interpolation-workflow "$DISABLE_ROOT_OUTPUT --disable-root-input --pipeline $(get_N tpc-track-interpolation TPC REST)" "$ITSMFT_FILES"
+
+true # everything OK up to this point, so the script should return 0 (it is !=0 already if a has_detector check fails)
