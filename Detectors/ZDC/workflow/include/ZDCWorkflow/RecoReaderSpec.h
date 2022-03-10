9267b7c3c7b4fc65b2f60096c3d3bf5a7614e63e
Pietro Cortese <pietro.cortese@cern.ch>
2022-03-10 15:22:25 +0100

    WIP

---
+++ Detectors/ZDC/workflow/include/ZDCWorkflow/RecoReaderSpec.h
@@ -0,0 +1,45 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#ifndef O2_ZDC_RECOREADERSPEC_H
+#define O2_ZDC_RECOREADERSPEC_H
+
+#include "TFile.h"
+#include "TTree.h"
+#include "Framework/DataProcessorSpec.h"
+#include "Framework/Task.h"
+
+namespace o2
+{
+namespace zdc
+{
+
+class RecoReader : public framework::Task
+{
+ public:
+  RecoReader() {}
+  ~RecoReader() override = default;
+  void init(framework::InitContext& ic) final;
+  void run(framework::ProcessingContext& pc) final;
+
+ private:
+  std::unique_ptr<TTree> mTree;
+  std::unique_ptr<TFile> mFile;
+};
+
+/// create a processor spec
+/// read reconstructed ZDC data from a root file
+framework::DataProcessorSpec getRecoReaderSpec();
+
+} // end namespace zdc
+} // end namespace o2
+
+#endif // O2_ZDC_RECOREADERSPEC_H
---
+++ Detectors/ZDC/workflow/src/RecoReaderSpec.cxx
@@ -0,0 +1,102 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// @file   RecoReaderSpec.cxx
+
+#include <vector>
+
+#include "TTree.h"
+
+#include "Framework/ConfigParamRegistry.h"
+#include "Framework/ControlService.h"
+#include "Framework/Logger.h"
+#include "ZDCWorkflow/RecoReaderSpec.h"
+#include "DataFormatsZDC/OrbitData.h"
+#include "DataFormatsZDC/BCData.h"
+#include "DataFormatsZDC/ChannelData.h"
+#include "DataFormatsZDC/MCLabel.h"
+#include "SimulationDataFormat/MCTruthContainer.h"
+#include "CommonUtils/NameConf.h"
+
+using namespace o2::framework;
+
+namespace o2
+{
+namespace zdc
+{
+
+void RecoReader::init(InitContext& ic)
+{
+  auto filename = o2::utils::Str::concat_string(o2::utils::Str::rectifyDirectory(ic.options().get<std::string>("input-dir")),
+                                                ic.options().get<std::string>("zdc-reco-infile"));
+  mFile = std::make_unique<TFile>(filename.c_str());
+  if (!mFile->IsOpen()) {
+    LOG(error) << "Cannot open the " << filename.c_str() << " file !";
+    throw std::runtime_error("cannot open input digits file");
+  }
+  mTree.reset((TTree*)mFile->Get("o2rec"));
+  if (!mTree) {
+    LOG(error) << "Did not find o2sim tree in " << filename.c_str();
+    throw std::runtime_error("Did not find o2rec tree in ZDC reco file");
+  }
+}
+
+void RecoReader::run(ProcessingContext& pc)
+{
+
+  std::vector<o2::zdc::OrbitData> zdcOrbitData, *zdcOrbitDataPtr = &zdcOrbitData;
+  std::vector<o2::zdc::BCData> zdcBCData, *zdcBCDataPtr = &zdcBCData;
+  std::vector<o2::zdc::ChannelData> zdcChData, *zdcChDataPtr = &zdcChData;
+
+  mTree->SetBranchAddress("ZDCDigitOrbit", &zdcOrbitDataPtr);
+  mTree->SetBranchAddress("ZDCDigitBC", &zdcBCDataPtr);
+  mTree->SetBranchAddress("ZDCDigitCh", &zdcChDataPtr);
+  o2::dataformats::MCTruthContainer<o2::zdc::MCLabel> labels, *plabels = &labels;
+  if (mUseMC) {
+    mTree->SetBranchAddress("ZDCDigitLabels", &plabels);
+  }
+  auto ent = mTree->GetReadEntry() + 1;
+  assert(ent < mTree->GetEntries()); // this should not happen
+  mTree->GetEntry(ent);
+  LOG(info) << "ZDCRecoReader pushed " << zdcOrbitData.size() << " orbits with " << zdcBCData.size() << " bcs and " << zdcChData.size() << " digits";
+  pc.outputs().snapshot(Output{"ZDC", "DIGITSPD", 0, Lifetime::Timeframe}, zdcOrbitData);
+  pc.outputs().snapshot(Output{"ZDC", "DIGITSBC", 0, Lifetime::Timeframe}, zdcBCData);
+  pc.outputs().snapshot(Output{"ZDC", "DIGITSCH", 0, Lifetime::Timeframe}, zdcChData);
+  if (mUseMC) {
+    pc.outputs().snapshot(Output{"ZDC", "DIGITSLBL", 0, Lifetime::Timeframe}, labels);
+  }
+  if (mTree->GetReadEntry() + 1 >= mTree->GetEntries()) {
+    pc.services().get<ControlService>().endOfStream();
+    pc.services().get<ControlService>().readyToQuit(QuitRequest::Me);
+  }
+}
+
+DataProcessorSpec getRecoReaderSpec(bool useMC)
+{
+  std::vector<OutputSpec> outputs;
+  outputs.emplace_back("ZDC", "DIGITSBC", 0, Lifetime::Timeframe);
+  outputs.emplace_back("ZDC", "DIGITSCH", 0, Lifetime::Timeframe);
+  outputs.emplace_back("ZDC", "DIGITSPD", 0, Lifetime::Timeframe);
+  if (useMC) {
+    outputs.emplace_back("ZDC", "DIGITSLBL", 0, Lifetime::Timeframe);
+  }
+  return DataProcessorSpec{
+    "zdc-reco-reader",
+    Inputs{},
+    outputs,
+    AlgorithmSpec{adaptFromTask<RecoReader>(useMC)},
+    Options{
+      {"zdc-reco-infile", VariantType::String, "zdcreco.root", {"Name of the input file"}},
+      {"input-dir", VariantType::String, "none", {"Input directory"}}}};
+}
+
+} // namespace zdc
+} // namespace o2
