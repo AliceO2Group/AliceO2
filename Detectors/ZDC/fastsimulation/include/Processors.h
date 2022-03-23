1d02fe13193e41c8c15a8b21367102be4d2dcca9
SwirtaB <49533902+SwirtaB@users.noreply.github.com>
2022-03-23 17:05:39 +0100

    Fixed
    copyright notice
    o2checkcode errors

--- Detectors/ZDC/fastsimulation/include/Processors.h
+++ Detectors/ZDC/fastsimulation/include/Processors.h
@@ -1,13 +1,18 @@
-/**
- * @file Processors.h
- * @author SwirtaB
- * @brief
- * @version 0.1
- * @date 2022-03-19
- *
- * @copyright Copyright (c) 2022
- *
- */
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// @file   Processors.h
+/// @author SwirtaB
+///
 
 #ifndef O2_ZDC_FAST_SIMULATIONS_PROCESSORS_H
 #define O2_ZDC_FAST_SIMULATIONS_PROCESSORS_H
--- Detectors/ZDC/fastsimulation/src/Processors.cxx
+++ Detectors/ZDC/fastsimulation/src/Processors.cxx
@@ -1,13 +1,18 @@
-/**
- * @file FastSimReaders.cxx
- * @author SwirtaB
- * @brief
- * @version 0.1
- * @date 2022-03-18
- *
- * @copyright Copyright (c) 2022
- *
- */
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// @file   Processors.cxx
+/// @author SwirtaB
+///
 
 #include "Processors.h"
 
@@ -52,14 +57,15 @@ std::array<long, 5> o2::zdc::fastsim::processors::calculateChannels(const Ort::V
   for (int i = 0; i < 44; i++) {
     for (int j = 0; j < 44; j++) {
       if (i % 2 == j % 2) {
-        if (i < 22 && j < 22)
+        if (i < 22 && j < 22) {
           channels[0] += std::expm1(flattedImageVector[j + i * 44]);
-        else if (i < 22 && j >= 22)
+        } else if (i < 22 && j >= 22) {
           channels[1] += std::expm1(flattedImageVector[j + i * 44]);
-        else if (i >= 22 && j < 22)
+        } else if (i >= 22 && j < 22) {
           channels[2] += std::expm1(flattedImageVector[j + i * 44]);
-        else if (i >= 22 && j >= 22)
+        } else if (i >= 22 && j >= 22) {
           channels[3] += std::expm1(flattedImageVector[j + i * 44]);
+        }
       } else {
         channels[4] += std::expm1(flattedImageVector[j + i * 44]);
       }
