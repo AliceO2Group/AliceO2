fc02bbee9e063e78b6d140b1614442cc427e61ec
David Rohr <drohr@jwdt.org>
2022-03-27 17:22:51 +0200

    GPU Vulkan Display: Can steer with ENV variable whether to throw on error in validation layer

--- GPU/GPUTracking/display/GPUDisplayBackendVulkan.cxx
+++ GPU/GPUTracking/display/GPUDisplayBackendVulkan.cxx
@@ -350,17 +350,22 @@ void GPUDisplayBackendVulkan::createDevice()
   const std::vector<const char*> reqValidationLayers = {
     "VK_LAYER_KHRONOS_validation"};
   auto debugCallback = [](VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData) -> VkBool32 {
+    static bool throwOnError = getenv("GPUCA_VULKAN_VALIDATION_THROW") && atoi(getenv("GPUCA_VULKAN_VALIDATION_THROW"));
     switch (messageSeverity) {
       case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT:
         // GPUInfo("%s", pCallbackData->pMessage);
         break;
       case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
         GPUWarning("%s", pCallbackData->pMessage);
-        // throw std::logic_error("break_on_validation_warning");
+        if (throwOnError) {
+          throw std::logic_error("break_on_validation_warning");
+        }
         break;
       case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
         GPUError("%s", pCallbackData->pMessage);
-        // throw std::logic_error("break_on_validation_error");
+        if (throwOnError) {
+          throw std::logic_error("break_on_validation_error");
+        }
         break;
       case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:
       default:
