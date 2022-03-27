6ff636b32addf8dc75802b2972e25bfa9011c923
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2022-03-27 23:08:58 +0200

    DPL: propagate startTime from input

--- Framework/Core/src/LifetimeHelpers.cxx
+++ Framework/Core/src/LifetimeHelpers.cxx
@@ -484,6 +484,7 @@ ExpirationHandler::Handler LifetimeHelpers::enumerate(ConcreteDataMatcher const&
 
     variables.put({data_matcher::FIRSTTFORBIT_POS, dh.firstTForbit});
     variables.put({data_matcher::TFCOUNTER_POS, dh.tfCounter});
+    variables.put({data_matcher::STARTTIME_POS, dph.startTime});
     variables.put({data_matcher::CREATIONTIME_POS, dph.creation});
 
     auto&& transport = rawDeviceService.device()->GetChannel(sourceChannel, 0).Transport();
