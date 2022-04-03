bd9144a82eee9284900ea22d66f6c5689c117aa2
shahoian <ruben.shahoyan@cern.ch>
2022-04-03 02:17:40 +0200

    Optionally check cached ccdb objects only every N TFs
    with --condition-tf-per-query N (def=1) the validity of cached objects for TF timestamp will
    be checked every N TFs. If N <= 0, never check validity once they are cached.

--- Framework/Core/src/WorkflowHelpers.cxx
+++ Framework/Core/src/WorkflowHelpers.cxx
@@ -234,6 +234,7 @@ void WorkflowHelpers::injectServiceDevices(WorkflowSpec& workflow, ConfigContext
                 {"condition-not-before", VariantType::Int64, 0ll, {"do not fetch from CCDB objects created before provide timestamp"}},
                 {"condition-not-after", VariantType::Int64, 3385078236000ll, {"do not fetch from CCDB objects created after the timestamp"}},
                 {"condition-remap", VariantType::String, "", {"remap condition path in CCDB based on the provided string."}},
+                {"condition-tf-per-query", VariantType::Int64, 1ll, {"check condition validity per requested number of TFs, fetch only once if <0"}},
                 {"orbit-offset-enumeration", VariantType::Int64, 0ll, {"initial value for the orbit"}},
                 {"orbit-multiplier-enumeration", VariantType::Int64, 0ll, {"multiplier to get the orbit from the counter"}},
                 {"start-value-enumeration", VariantType::Int64, 0ll, {"initial value for the enumeration"}},
