6704405779547550e5f4c0b69a9179e185605589
Antonio Franco <antonio.franco@cern.ch>
2022-04-07 14:20:41 +0200

    Get the Run_Number into Ped_Tag

--- Detectors/HMPID/workflow/src/PedestalsCalculationSpec.cxx
+++ Detectors/HMPID/workflow/src/PedestalsCalculationSpec.cxx
@@ -90,6 +90,10 @@ void PedestalsCalculationTask::init(framework::InitContext& ic)
   }
 
   mPedestalTag = ic.options().get<std::string>("pedestals-tag");
+  if(mPedestalTag == "run_number") { // if the Tag is run_number, then substitute the Tag with RN
+    const std::string NAStr = "NA";
+    mPedestalTag = pc.services().get<RawDeviceService>().device()->fConfig->GetProperty<std::string>("runNumber", NAStr); 
+  }
   mFastAlgorithm = ic.options().get<bool>("fast-decode");
 
   mWriteToDCSDB = ic.options().get<bool>("use-dcsccdb");
