c46a483045cfe700354cadee41b96cb69ace3f84
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2022-04-12 11:25:53 +0200

    DPL: provide per channel metric when backpressure happens

--- Framework/Core/src/DataProcessingDevice.cxx
+++ Framework/Core/src/DataProcessingDevice.cxx
@@ -1245,6 +1245,8 @@ void DataProcessingDevice::handleData(DataProcessorContext& context, InputChanne
             case DataRelayer::Backpressured:
               if (info.normalOpsNotified == true && info.backpressureNotified == false) {
                 LOGP(alarm, "Backpressure on channel {}. Waiting.", info.channel->GetName());
+                auto& monitoring = context.registry->get<o2::monitoring::Monitoring>();
+                monitoring.send(o2::monitoring::Metric{1, fmt::format("backpressure_{}", info.channel->GetName())});
                 info.backpressureNotified = true;
                 info.normalOpsNotified = false;
               }
@@ -1255,6 +1257,8 @@ void DataProcessingDevice::handleData(DataProcessorContext& context, InputChanne
             case DataRelayer::WillRelay:
               if (info.normalOpsNotified == false && info.backpressureNotified == true) {
                 LOGP(info, "Back to normal on channel {}.", info.channel->GetName());
+                auto& monitoring = context.registry->get<o2::monitoring::Monitoring>();
+                monitoring.send(o2::monitoring::Metric{0, fmt::format("backpressure_{}", info.channel->GetName())});
                 info.normalOpsNotified = true;
                 info.backpressureNotified = false;
               }
