3b0bccb6c137bd9e43df0de1e9a97af2d3cf8d87
Ole Schmidt <ole.schmidt@cern.ch>
2022-04-26 17:30:30 +0200

    Enable TRD vDrift calib for data taking with beam

--- prodtests/full-system-test/calib-workflow.sh
+++ prodtests/full-system-test/calib-workflow.sh
@@ -8,6 +8,7 @@ fi
 if [[ $BEAMTYPE != "cosmic" ]]; then
   has_detector_calib TPC && has_detectors TPC ITS TRD TOF && add_W o2-tpc-scdcalib-interpolation-workflow "$DISABLE_ROOT_OUTPUT --disable-root-input --pipeline $(get_N tpc-track-interpolation TPC REST)" "$ITSMFT_FILES"
   has_detector_calib ITS && has_detectors ITS && has_detectors_reco ITS && has_detector_matching PRIMVTX && [[ ! -z "$VERTEXING_SOURCES" ]] && add_W o2-calibration-mean-vertex-calibration-workflow
+  has_detector_calib TRD && has_detector ITS TPC TRD && add_W o2-calibration-trd-vdrift-exb
 fi
 
 true # everything OK up to this point, so the script should return 0 (it is !=0 already if a has_detector check fails)
