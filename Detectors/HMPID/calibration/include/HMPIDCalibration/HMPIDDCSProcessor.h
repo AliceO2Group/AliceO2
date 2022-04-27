bc37e31047e27e82920c5e54f6c2cca78a331821
eflatlan <99878253+eflatlan@users.noreply.github.com>
2022-04-27 16:43:01 +0200

    Add files via upload

--- Detectors/HMPID/calibration/include/HMPIDCalibration/HMPIDDCSProcessor.h
+++ Detectors/HMPID/calibration/include/HMPIDCalibration/HMPIDDCSProcessor.h
@@ -89,7 +89,80 @@ class HMPIDDCSProcessor{
 
 	
 		// procTrans	 ===================================================================================================
-		double DefaultEMean();					//just set a refractive index for C6F14 at ephot=6.675 eV @ T=25 C
+		double DefaultEMean();					//just set a refractive index for C6F14 at ephot=6.675 eV @ T=25 C 		
+
+		
+		double ProcTrans();
+	
+		// Process Datapoints:  ===================================================================================================
+		//void initTempArr();						// initialize tOut/tIn arrays  pTin[3*iCh+iRad]	= new TF1		
+		//void init(const std::vector<DPID>& pids);
+
+		void process(const gsl::span<const DPCOM> dps); 		// process DPs, fetch IDs and call relevant fill-method
+
+		void fillChamberPressures(const DPCOM& dpcom);			// fill element[0-6] in chamber-pressure vector
+
+		void fillEnvironmentPressure(const DPCOM& dpcom);		// fill environment-pressure vector 
+	
+		// HV in each chamber_section = 7*3 --> will result in Q_thre  
+		void fillHV(const DPCOM& dpcom); 				// fill element[0-20] in HV vector
+		
+		// Temp in (T1) and out (T2), in each chamber_radiator = 7*3 :  
+		void fill_InTemperature(const DPCOM& dpcom);			// fill element[0-20] in tempIn vector
+		void fill_OutTemperature(const DPCOM& dpcom); 			// fill element[0-20] in tempOut vector
+
+		uint64_t processFlags(const uint64_t flags, const char* pid);
+		//Bool_t stDcsStore=kFALSE;
+
+
+
+		 // finalize DPs, after run is finished  ===================================================================================================
+		 void finalizeEnvPressure(); 
+		 void finalizeHV_Entry(Int_t iCh,Int_t iSec); 
+		 void finalizeChPressureEntry(Int_t iCh); 
+		 void finalizeTempOutEntry(Int_t iCh,Int_t iRad); 
+		 void finalizeTempInEntry(Int_t iCh,Int_t iRad); 
+		 void finalize(); 
+
+
+
+
+
+		// convert char or substring to int (i.e. fetch int in string/char) 
+		int subStringToInt(std::string inputString, std::size_t startIndex, std::size_t endIndex);
+		
+		// determine if string of DP has substring corresponding to HMPID-IR or other HMPID specifications: 
+		void processIR(DPCOM dp);    // if it mathces IR_ID = "HMP_DET/HMP_INFR"
+		void processHMPID(DPCOM dp); // if it matches HMPID_ID = "HMP_DET", but not IR_ID
+		
+		
+			const auto& getTimeQThresh() const { return mTimeQThresh;}
+			const auto& getTimeArNmean() const { return mTimeArNmean;}
+		//end  ProcDCS:
+			
+		  void setStartValidity(long t) { mStartValidity = t; }
+		  void useVerboseMode() { mVerbose = true; }
+           
+	 private:
+		// DCS-CCDB ====================================================================================================
+
+		std::unordered_map<DPID, bool> mPids;
+
+	 	long mFirstTime;         // time when a CCDB object was stored first
+	  	long mStartValidity = 0; // TF index for processing, used to store CCDB object
+	  	bool mFirstTimeSet = false; 	
+	
+		bool mVerbose = false;		
+
+		CcdbObjectInfo mccdbREF_INDEX_Info;
+		const std::array<TF1,43> mRefIndex;//TF1 mRefIndex[43];
+	
+		CcdbObjectInfo mccdbCHARGE_CUT_Info;
+		const std::array<TF1,42> mChargeCut;//TF1 mChargeCut[42];
+
+
+// ProcTrans private variables	
+//====================================================================================		
      		double  sEnergProb=0, sProb=0; 				// energy probaility, probability
      		// Double_t tRefCR5 = 19. ;                             // mean temperature of CR5 where the system is in place
      		double  eMean = 0;					// initialize eMean (Photon energy mean) to 0
@@ -115,31 +188,41 @@ class HMPIDDCSProcessor{
 		double  aTransGap;	       // evaluate 80 cm of thickness Gap (low density CH4) transparency 
 		double  aCsIQE; 	       // evaluate CsI quantum efficiency
 		double  aTotConvolution;      // evaluate total convolution of all material optical properties
- 		
 
+
+
+		// Timestamps and TimeRanges ======================================================================================
+		// timestamps of last and first HV-datapoint entry in 
+		// 1d-array of vectors of HV
+		uint64_t hvFirstTime, hvLastTime; 
+
+		uint64_t pChFirstTime, pChLastTime; 	// chamberprssure timestamps
+		uint64_t pEnvFirstTime, pEnvLastTime;   // envPressure timestamps
 		
-		double ProcTrans();
 	
-		// Process Datapoints:  ===================================================================================================
-		//void initTempArr();						// initialize tOut/tIn arrays  pTin[3*iCh+iRad]	= new TF1		
-		//void init(const std::vector<DPID>& pids);
 
-		void process(const gsl::span<const DPCOM> dps); 		// process DPs, fetch IDs and call relevant fill-method
 
-		void fillChamberPressures(const DPCOM& dpcom);			// fill element[0-6] in chamber-pressure vector
+		TimeRange mTimeQThresh; // Timerange for QThresh (ChargeCut)
+					// min, max of timestamps {envP, chP, HV}
 
-		void fillEnvironmentPressure(const DPCOM& dpcom);		// fill environment-pressure vector 
-	
-		// HV in each chamber_section = 7*3 --> will result in Q_thre  
-		void fillHV(const DPCOM& dpcom); 				// fill element[0-20] in HV vector
-		
-		// Temp in (T1) and out (T2), in each chamber_radiator = 7*3 :  
-		void fill_InTemperature(const DPCOM& dpcom);			// fill element[0-20] in tempIn vector
-		void fill_OutTemperature(const DPCOM& dpcom); 			// fill element[0-20] in tempOut vector
 
-		uint64_t processFlags(const uint64_t flags, const char* pid);
-		//Bool_t stDcsStore=kFALSE;
+ 		// indexes for getting chamber-numbers etc ==================================================================================================
+		std::size_t startI_chamberPressure = 14, endI_chamberPressure = 6;
+		std::size_t startI_chamberHV = 14, endI_chamberHV = 6;
+		std::size_t startI_sectorHV = 38, endI_sectorHV = 51;
+		std::size_t startI_chamberTemp = 14, endI_chamberTemp = 22;
+		std::size_t startI_radiatorTemp = 51;
+ 
 
+		uint64_t tempFirstTime, tempLastTime; 
+		uint64_t tOutFirstTime, tOutLastTime; 
+		uint64_t procTransFirstTime, procTransLastTime; 
+
+		TimeRange mTimeArNmean;	// Timerange for arNmean (RefIndex)
+					// min, max of timestamps {Tin, Tout, ProcTrans}
+
+// private variables
+// ======================================================================================		
 		Double_t xP,yP;
 		TF2 *thr = new TF2("RthrCH4"  ,"3*10^(3.01e-3*x-4.72)+170745848*exp(-y*0.0162012)"             ,2000,3000,900,1200); 
 	
@@ -175,79 +258,6 @@ class HMPIDDCSProcessor{
 		// HV 
 		std::vector<DPCOM> dpcomHV[42];   //  HV vector [0..41]; 7 chambers * 6 sectors
 		Int_t cntHV=0;			  // cnt HV entries in element i[0..41];  i = iCh*6 + iSec
-
-		 // finalize DPs, after run is finished  ===================================================================================================
-		 void finalizeEnvPressure(); 
-		 void finalizeHV_Entry(Int_t iCh,Int_t iSec); 
-		 void finalizeChPressureEntry(Int_t iCh); 
-		 void finalizeTempOutEntry(Int_t iCh,Int_t iRad); 
-		 void finalizeTempInEntry(Int_t iCh,Int_t iRad); 
-		 void finalize(); 
-
-
-
-		// indexes for getting chamber-numbers etc ==================================================================================================
-		std::size_t startI_chamberPressure = 14, endI_chamberPressure = 6;
-		std::size_t startI_chamberHV = 14, endI_chamberHV = 6;
-		std::size_t startI_sectorHV = 38, endI_sectorHV = 51;
-		std::size_t startI_chamberTemp = 14, endI_chamberTemp = 22;
-		std::size_t startI_radiatorTemp = 51;
-
-		// convert char or substring to int (i.e. fetch int in string/char) 
-		int subStringToInt(std::string inputString, std::size_t startIndex, std::size_t endIndex);
-		
-		// determine if string of DP has substring corresponding to HMPID-IR or other HMPID specifications: 
-		void processIR(DPCOM dp);    // if it mathces IR_ID = "HMP_DET/HMP_INFR"
-		void processHMPID(DPCOM dp); // if it matches HMPID_ID = "HMP_DET", but not IR_ID
-		
-		
-			const auto& getTimeQThresh() const { return mTimeQThresh;}
-			const auto& getTimeArNmean() const { return mTimeArNmean;}
-		//end  ProcDCS:
-			
-		  void setStartValidity(long t) { mStartValidity = t; }
-		  void useVerboseMode() { mVerbose = true; }
-            private:
-		// DCS-CCDB ====================================================================================================
-
-		std::unordered_map<DPID, bool> mPids;
-
-	 	long mFirstTime;         // time when a CCDB object was stored first
-	  	long mStartValidity = 0; // TF index for processing, used to store CCDB object
-	  	bool mFirstTimeSet = false; 	
-	
-		bool mVerbose = false;		
-
-		CcdbObjectInfo mccdbREF_INDEX_Info;
-		const std::array<TF1,43> mRefIndex;//TF1 mRefIndex[43];
-	
-		CcdbObjectInfo mccdbCHARGE_CUT_Info;
-		const std::array<TF1,42> mChargeCut;//TF1 mChargeCut[42];
-	
-		
-		// Timestamps and TimeRanges ======================================================================================
-		// timestamps of last and first HV-datapoint entry in 
-		// 1d-array of vectors of HV
-		uint64_t hvFirstTime, hvLastTime; 
-
-		uint64_t pChFirstTime, pChLastTime; 	// chamberprssure timestamps
-		uint64_t pEnvFirstTime, pEnvLastTime;   // envPressure timestamps
-		
-	
-
-
-		TimeRange mTimeQThresh; // Timerange for QThresh (ChargeCut)
-					// min, max of timestamps {envP, chP, HV}
- 
- 
-		uint64_t tempFirstTime, tempLastTime; 
-		uint64_t tOutFirstTime, tOutLastTime; 
-		uint64_t procTransFirstTime, procTransLastTime; 
-
-		TimeRange mTimeArNmean;	// Timerange for arNmean (RefIndex)
-					// min, max of timestamps {Tin, Tout, ProcTrans}
-
-
 		
 
 	
