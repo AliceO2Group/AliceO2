f76eb23c3fa8d81c5bfea9071d047ef548059fd4
Piotr Konopka <piotr.jan.konopka@cern.ch>
2022-04-27 14:08:32 +0200

    [QC-751] Remove obsolete QC flags (#8651)

--- DataFormats/QualityControl/test/testTimeRangeFlag.cxx
+++ DataFormats/QualityControl/test/testTimeRangeFlag.cxx
@@ -23,17 +23,17 @@ using namespace o2::quality_control;
 
 BOOST_AUTO_TEST_CASE(test_TimeRangeFlag)
 {
-  TimeRangeFlag trf1{12, 34, FlagReasonFactory::ProcessingError(), "comment", "source"};
+  TimeRangeFlag trf1{12, 34, FlagReasonFactory::BadTracking(), "comment", "source"};
 
   BOOST_CHECK_EQUAL(trf1.getStart(), 12);
   BOOST_CHECK_EQUAL(trf1.getEnd(), 34);
-  BOOST_CHECK_EQUAL(trf1.getFlag(), FlagReasonFactory::ProcessingError());
+  BOOST_CHECK_EQUAL(trf1.getFlag(), FlagReasonFactory::BadTracking());
   BOOST_CHECK_EQUAL(trf1.getComment(), "comment");
   BOOST_CHECK_EQUAL(trf1.getSource(), "source");
 
-  BOOST_CHECK_THROW((TimeRangeFlag{12, 0, FlagReasonFactory::ProcessingError()}), std::runtime_error);
+  BOOST_CHECK_THROW((TimeRangeFlag{12, 0, FlagReasonFactory::BadTracking()}), std::runtime_error);
 
-  TimeRangeFlag trf2{10, 34, FlagReasonFactory::ProcessingError(), "comment", "source"};
+  TimeRangeFlag trf2{10, 34, FlagReasonFactory::BadTracking(), "comment", "source"};
 
   BOOST_CHECK(trf1 > trf2);
   BOOST_CHECK(!(trf1 < trf2));
