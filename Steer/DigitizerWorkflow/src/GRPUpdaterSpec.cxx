c6d508f9525c02a3ef1d961b1acb780a598e97d1
shahoian <ruben.shahoyan@cern.ch>
2022-05-04 01:30:18 +0200

    Produce in o2-sim full triplet of GRPs, update GRPECS/LHCIF in GRPUpdater

--- Steer/DigitizerWorkflow/src/GRPUpdaterSpec.cxx
+++ Steer/DigitizerWorkflow/src/GRPUpdaterSpec.cxx
@@ -17,6 +17,8 @@
 #include "Framework/Task.h"
 #include "Framework/Lifetime.h"
 #include "DataFormatsParameters/GRPObject.h"
+#include "DataFormatsParameters/GRPECSObject.h"
+#include <DataFormatsParameters/GRPLHCIFData.h>
 #include "CommonUtils/NameConf.h"
 #include <TFile.h>
 #include <FairLogger.h>
@@ -43,10 +45,9 @@ static std::vector<o2::detectors::DetID> sDetList;
 
 class GRPDPLUpdatedTask
 {
-  using GRP = o2::parameters::GRPObject;
 
  public:
-  GRPDPLUpdatedTask(const std::string& grpfilename) : mGRPFileName{grpfilename} {}
+  GRPDPLUpdatedTask(const std::string& prefix) : mPrefix{prefix} {}
 
   void init(framework::InitContext& ic)
   {
@@ -56,14 +57,14 @@ class GRPDPLUpdatedTask
   void run(framework::ProcessingContext& pc)
   {
     const std::string grpName{o2::base::NameConf::CCDBOBJECT};
-
+    auto grpFileName = o2::base::NameConf::getGRPFileName(mPrefix);
+    auto grpECSFileName = o2::base::NameConf::getGRPECSFileName(mPrefix);
     // a standardized semaphore convention --> taking the current execution path should be enough
     // (the user enables this via O2_USEGRP_SEMA environment)
-    bool use_sema = false;
     boost::interprocess::named_semaphore* sem = nullptr;
     std::string semhashedstring;
     try {
-      const auto semname = std::filesystem::current_path().string() + mGRPFileName;
+      const auto semname = std::filesystem::current_path().string() + grpECSFileName;
       std::hash<std::string> hasher;
       semhashedstring = "alice_grp_" + std::to_string(hasher(semname)).substr(0, 16);
       sem = new boost::interprocess::named_semaphore(boost::interprocess::open_or_create_t{}, semhashedstring.c_str(), 1);
@@ -87,30 +88,9 @@ class GRPDPLUpdatedTask
           delete sem;
         }
       };
-
-      TFile flGRP(mGRPFileName.c_str(), "update");
-      if (flGRP.IsZombie()) {
-        LOG(error) << "Failed to open in update mode " << mGRPFileName;
-        postSem();
-        return;
-      }
-      std::unique_ptr<GRP> grp(static_cast<GRP*>(flGRP.GetObjectChecked(grpName.c_str(), GRP::Class())));
-      for (auto det : sDetList) { // get readout mode data from different detectors
-        auto roMode = pc.inputs().get<o2::parameters::GRPObject::ROMode>(det.getName());
-        if (!(roMode & o2::parameters::GRPObject::PRESENT)) {
-          LOG(error) << "Detector " << det.getName() << " is read out while processor set ABSENT";
-          continue;
-        }
-        grp->setDetROMode(det, roMode);
-      }
-      grp->setIsMC(true);
-      grp->setFirstOrbit(o2::raw::HBFUtils::Instance().orbitFirst);
-      grp->setNHBFPerTF(o2::raw::HBFUtils::Instance().nHBFPerTF);
-      LOG(info) << "Updated GRP in " << mGRPFileName << " for detectors RO mode and 1st orbit of the run";
-      grp->print();
-      flGRP.WriteObjectAny(grp.get(), grp->Class(), grpName.c_str());
-      flGRP.Close();
-
+      updateECSData<o2::parameters::GRPECSObject>(grpECSFileName, pc);
+      updateECSData<o2::parameters::GRPObject>(grpFileName, pc); // RS FIXME: suppress once we completely switch to GRPs triplet
+      updateLHCIFData(pc);
       postSem();
     } catch (boost::interprocess::interprocess_exception e) {
       LOG(error) << "Caught semaphore exception " << e.what();
@@ -119,26 +99,72 @@ class GRPDPLUpdatedTask
   }
 
  private:
-  std::string mGRPFileName = "o2sim_grp.root";
+  template <typename GRP>
+  void updateECSData(const std::string& grpFileName, framework::ProcessingContext& pc)
+  {
+    const std::string grpName{o2::base::NameConf::CCDBOBJECT};
+    TFile flGRP(grpFileName.c_str(), "update");
+    if (flGRP.IsZombie()) {
+      LOG(error) << "Failed to open in update mode " << grpFileName;
+      return;
+    }
+    std::unique_ptr<GRP> grp(static_cast<GRP*>(flGRP.GetObjectChecked(grpName.c_str(), GRP::Class())));
+    for (auto det : sDetList) { // get readout mode data from different detectors
+      auto roMode = pc.inputs().get<typename GRP::ROMode>(det.getName());
+      if (!(roMode & GRP::PRESENT)) {
+        LOG(error) << "Detector " << det.getName() << " is read out while processor set ABSENT";
+        continue;
+      }
+      grp->setDetROMode(det, roMode);
+    }
+    grp->setIsMC(true);
+    grp->setNHBFPerTF(o2::raw::HBFUtils::Instance().nHBFPerTF);
+    LOG(info) << "Updated " << grpFileName << " with detectors RO modes";
+    grp->print();
+    flGRP.WriteObjectAny(grp.get(), grp->Class(), grpName.c_str());
+    flGRP.Close();
+  }
+
+  void updateLHCIFData(framework::ProcessingContext& pc)
+  {
+    using GRPLHCIF = o2::parameters::GRPLHCIFData;
+    auto grpFileName = o2::base::NameConf::getGRPLHCIFFileName(mPrefix);
+    const std::string grpName{o2::base::NameConf::CCDBOBJECT};
+    if (!std::filesystem::exists(grpFileName)) {
+      LOGP(info, "GRPLHCIF file {} is absent, abandon setting bunch-filling", grpFileName);
+      return;
+    }
+    TFile flGRP(grpFileName.c_str(), "update");
+    if (flGRP.IsZombie()) {
+      LOG(fatal) << "Failed to open in update mode " << grpFileName;
+    }
+    std::unique_ptr<GRPLHCIF> grp(static_cast<GRPLHCIF*>(flGRP.GetObjectChecked(grpName.c_str(), GRPLHCIF::Class())));
+    grp->setBunchFillingWithTime(grp->getBeamEnergyPerZTime(), pc.inputs().get<o2::BunchFilling>("bunchfilling")); // borrow the time from the existing entry
+    flGRP.WriteObjectAny(grp.get(), grp->Class(), grpName.c_str());
+    flGRP.Close();
+    LOG(info) << "Updated " << grpFileName << " with bunch filling";
+  }
+
+  std::string mPrefix = "o2sim";
 };
 
 /// create the processor spec
-o2::framework::DataProcessorSpec getGRPUpdaterSpec(const std::string& grpfilename, const std::vector<o2::detectors::DetID>& detList)
+o2::framework::DataProcessorSpec getGRPUpdaterSpec(const std::string& prefix, const std::vector<o2::detectors::DetID>& detList)
 {
   sDetList = detList;
 
   // prepare specs
   std::vector<InputSpec> inputs;
   for (const auto det : detList) {
-    inputs.emplace_back(InputSpec{det.getName(), det.getDataOrigin(), "ROMode",
-                                  static_cast<SubSpecificationType>(0), Lifetime::Timeframe});
+    inputs.emplace_back(InputSpec{det.getName(), det.getDataOrigin(), "ROMode", 0, Lifetime::Timeframe});
   }
+  inputs.emplace_back(InputSpec{"bunchfilling", "SIM", "BUNCHFILLING", 0, Lifetime::Timeframe});
 
   return DataProcessorSpec{
     "GRPUpdater",
     inputs, // input status from each detector
     {},     // no output
-    AlgorithmSpec{adaptFromTask<GRPDPLUpdatedTask>(grpfilename)},
+    AlgorithmSpec{adaptFromTask<GRPDPLUpdatedTask>(prefix)},
     Options{/* for the moment no options */}};
 }
 
