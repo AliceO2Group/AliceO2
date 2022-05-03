5a75d7437b6a0f49364b0c7419bc6ba08a18e672
eflatlan <99878253+eflatlan@users.noreply.github.com>
2022-05-03 11:08:11 +0200

    remove unnecessary vars

--- Detectors/HMPID/calibration/checkHMPIDAliasStrings.C
+++ Detectors/HMPID/calibration/checkHMPIDAliasStrings.C
@@ -29,44 +29,29 @@ int makeHMPIDCCDBEntryForDCS(const std::string url = "http://localhost:8080")
 {
 // check if IR or other HMPID specifciation
 			 
-  //  std::string url(argv[0]);
-  // macro to populate CCDB for HMPID with the configuration for DCS
-  std::unordered_map<DPID, std::string> dpid2DataDesc;
-  std::vector<std::string> aliases; // vector of strings that will hold DataPoints identifiers
   
   // string for DPs of pressures, HV and temperatures =============================================================
-  std::vector<std::string> tempInString, tempOutString, chamberPressureString, highVoltageString;
 
-   aliases.push_back("HMP_DET/HMP_ENV/HMP_ENV_PENV.actual.value"); // environment pressure 
       for(int iCh = 0; iCh < 1; iCh++)
       {
 
-           chamberPressureString.push_back( Form("HMP_DET/HMP_MP%i/HMP_MP%i_GAS/HMP_MP%i_GAS_PMWPC.actual.value",iCh,iCh,iCh)); 
 	   auto alias = (std::string) Form("HMP_DET/HMP_MP%i/HMP_MP%i_GAS/HMP_MP%i_GAS_PMWPC.actual.value",iCh,iCh,iCh);
            auto detector_id = alias.substr(0, 7);
 	   std::cout << detector_id << std::endl;
 	   //processHMPID(Form("HMP_DET/HMP_MP%i/HMP_MP%i_GAS/HMP_MP%i_GAS_PMWPC.actual.value",iCh,iCh,iCh));
            for(int iRad = 0; iRad < 1; iRad++)
            {  
-               tempOutString.push_back(Form("HMP_DET/HMP_MP%i/HMP_MP%i_LIQ_LOOP.actual.sensors.Rad%iOut_Temp",iCh,iCh,iRad));
 		//processHMPID(Form("HMP_DET/HMP_MP%i/HMP_MP%i_LIQ_LOOP.actual.sensors.Rad%iOut_Temp",iCh,iCh,iRad));
-               tempInString.push_back(Form("HMP_DET/HMP_MP%i/HMP_MP%i_LIQ_LOOP.actual.sensors.Rad%iIn_Temp",iCh,iCh,iRad)); 
            }        
            for(int iSec = 0; iSec < 1; iSec++)
            {  
 		//processHMPID(Form("HMP_DET/HMP_MP%i/HMP_MP%i_PW/HMP_MP%i_SEC%i/HMP_MP%i_SEC%i_HV.actual.vMon",iCh,iCh,iCh,iSec,iCh,iSec));
-  		
-               highVoltageString.push_back(Form("HMP_DET/HMP_MP%i/HMP_MP%i_PW/HMP_MP%i_SEC%i/HMP_MP%i_SEC%i_HV.actual.vMon",iCh,iCh,iCh,iSec,iCh,iSec)); 
            } 
       }
-      aliases.insert(aliases.end(), chamberPressureString.begin(), chamberPressureString.end()); 
-      aliases.insert(aliases.end(), tempOutString.begin(), tempOutString.end()); 
-      aliases.insert(aliases.end(), tempInString.begin(), tempInString.end()); 
-      aliases.insert(aliases.end(), highVoltageString.begin(), highVoltageString.end()); 
-  
+
   
      // string for DPs of Refractive Index Parameters =============================================================
-      std::vector<std::string> waveLenghtString, argonReferenceString, argonCellString, c6f14CellString, c6f14ReferenceString;
+    
       std::string wLen = "waveLenght", argRef = "argonReference",
       argCell = "argonCell", c6f14Cell= "c6f14Cell", c6f14Ref = "c6f14Reference"; 
 
@@ -74,33 +59,13 @@ int makeHMPIDCCDBEntryForDCS(const std::string url = "http://localhost:8080")
       for(int i = 29; i < 30; i++)
       {
 	temp = Form("HMP_DET/HMP_INFR/HMP_INFR_TRANPLANT/HMP_INFR_TRANPLANT_MEASURE.mesure%i.",i);
-        waveLenghtString.push_back(temp+wLen);processIR(temp+wLen);
-        argonReferenceString.push_back(temp+argRef);processIR(temp+argRef);
-        argonCellString.push_back(temp+argCell);processIR(temp+argCell);
-        c6f14CellString.push_back(temp+c6f14Cell);processIR(temp+c6f14Cell);
-       c6f14ReferenceString.push_back(temp+c6f14Ref);processIR(temp+c6f14Ref);
+        processIR(temp+wLen);
+        processIR(temp+argRef);
+        processIR(temp+argCell);
+       processIR(temp+c6f14Cell);
+       processIR(temp+c6f14Ref);
  
       }  
-      aliases.insert(aliases.end(), waveLenghtString.begin(), waveLenghtString.end()); 
-      aliases.insert(aliases.end(), argonReferenceString.begin(), argonReferenceString.end()); 
-      aliases.insert(aliases.end(), argonCellString.begin(), argonCellString.end()); 
-      aliases.insert(aliases.end(), c6f14CellString.begin(), c6f14CellString.end()); 
-      aliases.insert(aliases.end(), c6f14ReferenceString.begin(), c6f14ReferenceString.end()); 
-
-  std::vector<std::string> expaliases = o2::dcs::expandAliases(aliases);
-
-  DPID dpidtmp;
-  for (size_t i = 0; i < expaliases.size(); ++i) {
-    DPID::FILL(dpidtmp, expaliases[i], o2::dcs::DeliveryType::DPVAL_DOUBLE);
-    dpid2DataDesc[dpidtmp] = "HMPIDDATAPOINTS";
-  }
-
-  o2::ccdb::CcdbApi api;
-  api.init(url); // or http://localhost:8080 for a local installation
-  std::map<std::string, std::string> md;
-  long ts = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-  api.storeAsTFileAny(&dpid2DataDesc, "HMPID/Config/DCSDPconfig", md, ts, 99999999999999);
-
   return 0;
 }
 
