c666a06e7f5360f6756a1dba14bbd2d12bab1219
Chiara Zampolli <Chiara.Zampolli@cern.ch>
2022-05-06 09:52:14 +0200

    PHS + EMC + aggregator

--- prodtests/full-system-test/aggregator-workflow.sh
+++ prodtests/full-system-test/aggregator-workflow.sh
@@ -12,6 +12,9 @@ source $O2DPG_ROOT/DATA/common/getCommonArgs.sh
 # Set up calibrations
 source $O2DPG_ROOT/DATA/common/setenv_calib.sh
 
+# CCDB destination for uploads
+[[ -z ${CCDBPATH+x} ]] && CCDBPATH="http://o2-ccdb.internal"
+
 # Adding calibrations
 EXTRA_WORKFLOW=
 
@@ -19,6 +22,11 @@ echo "CALIB_PRIMVTX_MEANVTX = $CALIB_PRIMVTX_MEANVTX" 1>&2
 echo "CALIB_TOF_LHCPHASE = $CALIB_TOF_LHCPHASE" 1>&2
 echo "CALIB_TOF_CHANNELOFFSETS = $CALIB_TOF_CHANNELOFFSETS" 1>&2
 echo "CALIB_TOF_DIAGNOSTICS = $CALIB_TOF_DIAGNOSTICS" 1>&2
+echo "CALIB_EMC_CHANNELCALIB = $CALIB_EMC_CHANNELCALIB" 1>&2
+echo "CALIB_PHS_ENERGYCALIB = $CALIB_PHS_ENERGYCALIB" 1>&2
+echo "CALIB_PHS_BADMAPCALIB = $CALIB_PHS_BADMAPCALIB" 1>&2
+echo "CALIB_PHS_TURNONCALIB = $CALIB_PHS_TURNONCALIB" 1>&2
+echo "CALIB_PHS_RUNBYRUNCALIB = $CALIB_PHS_RUNBYRUNCALIB" 1>&2
 
 # PrimVertex
 if [[ $CALIB_PRIMVTX_MEANVTX == 1 ]]; then
@@ -38,8 +46,28 @@ if [[ $CALIB_TOF_DIAGNOSTICS == 1 ]]; then
     EXTRA_WORKFLOW+="o2-calibration-tof-diagnostic-workflow $ARGS_ALL --tf-per-slot 26400 | "
 fi
 
-WORKFLOW="o2-dpl-raw-proxy $ARGS_ALL --proxy-name aggregator-proxy --dataspec \"$CALIBDATASPEC\" --channel-config \"name=aggregator-proxy,method=bind,type=pull,rateLogging=1,transport=zeromq,address=tcp://localhost:30453\" |"
+# EMC
+if [[ $CALIB_EMC_CHANNELCALIB == 1 ]]; then
+    EXTRA_WORKFLOW+="o2-calibration-emcal-channel-calib-workflow --calibMode timeCalib | "
+fi
+
+# PHS
+if [[ $CALIB_PHS_ENERGYCALIB == 1 ]]; then
+    EXTRA_WORKFLOW+="o2-phos-calib-workflow --energy | "
+fi
+if [[ $CALIB_PHS_BADMAPCALIB == 1 ]]; then
+    EXTRA_WORKFLOW+="o2-phos-calib-workflow --badmap --mode 0 | "
+fi
+if [[ $CALIB_PHS_TURNONCALIB == 1 ]]; then
+    EXTRA_WORKFLOW+="o2-phos-calib-workflow --turnon | "
+fi
+if [[ $CALIB_PHS_RUNBYRUNCALIB == 1 ]]; then
+    EXTRA_WORKFLOW+="o2-phos-calib-workflow --runbyrun | "
+fi
+
+WORKFLOW="o2-dpl-raw-proxy $ARGS_ALL --proxy-name aggregator-proxy --dataspec \"$CALIBDATASPEC\" --channel-config \"name=aggregator-proxy,method=bind,type=pull,rateLogging=1,transport=zeromq,address=tcp://localhost:30453\" | "
 WORKFLOW+=$EXTRA_WORKFLOW
+if [[ $CCDBPATH != "none" ]]; then WORKFLOW+="o2-calibration-ccdb-populator-workflow --ccdb-path $CCDBPATH | "; fi
 WORKFLOW+="o2-dpl-run $ARGS_ALL $GLOBALDPLOPT"
 
 if [ $WORKFLOWMODE == "print" ]; then
