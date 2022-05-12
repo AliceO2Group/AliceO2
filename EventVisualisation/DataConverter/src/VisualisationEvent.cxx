40862e2f6d2df61177ee0712538073e67062ce9b
jmyrcha <julian.myrcha@cern.ch>
2022-05-12 12:48:53 +0200

    o2-eve: calorimeters display, fixes in json serialisation (#8755)
    * o2-eve: calorimeters display, fixes in json serialisation
    
    * reformatting
    
    * fixed merge errors

--- EventVisualisation/DataConverter/src/VisualisationEvent.cxx
+++ EventVisualisation/DataConverter/src/VisualisationEvent.cxx
@@ -13,17 +13,12 @@
 /// \file    VisualisationEvent.cxx
 /// \author  Jeremi Niedziela
 /// \author  Maciej Grochowicz
+/// \author julian.myrcha@cern.ch
 ///
 
 #include "EventVisualisationDataConverter/VisualisationEvent.h"
-#include "rapidjson/document.h"
-#include "rapidjson/writer.h"
-#include "rapidjson/prettywriter.h"
-#include "rapidjson/stringbuffer.h"
 
 #include <string>
-#include <iostream>
-#include <iomanip>
 #include <limits>
 #include <algorithm>
 #include "FairLogger.h"
@@ -35,7 +30,6 @@ namespace o2
 {
 namespace event_visualisation
 {
-constexpr int JSON_FILE_VERSION = 1;
 
 VisualisationEvent::GIDVisualisation VisualisationEvent::mVis = [] {
   VisualisationEvent::GIDVisualisation res;
@@ -59,6 +53,9 @@ VisualisationEvent::GIDVisualisation VisualisationEvent::mVis = [] {
     }
     if (filter == o2::event_visualisation::EVisualisationGroup::TRD) {
       res.contains[o2::dataformats::GlobalTrackID::TRD][filter] = true;
+      res.contains[o2::dataformats::GlobalTrackID::TPCTRD][filter] = true;
+      res.contains[o2::dataformats::GlobalTrackID::ITSTPCTRD][filter] = true;
+      res.contains[o2::dataformats::GlobalTrackID::ITSTPCTRDTOF][filter] = true;
     }
     if (filter == o2::event_visualisation::EVisualisationGroup::TOF) {
       res.contains[o2::dataformats::GlobalTrackID::ITSTPCTRDTOF][filter] = true;
@@ -81,6 +78,12 @@ VisualisationEvent::GIDVisualisation VisualisationEvent::mVis = [] {
       res.contains[o2::dataformats::GlobalTrackID::MFTMCH][filter] = true;
       res.contains[o2::dataformats::GlobalTrackID::MFTMCHMID][filter] = true;
     }
+    if (filter == o2::event_visualisation::EVisualisationGroup::EMC) {
+      res.contains[o2::dataformats::GlobalTrackID::EMC][filter] = true;
+    }
+    if (filter == o2::event_visualisation::EVisualisationGroup::PHS) {
+      res.contains[o2::dataformats::GlobalTrackID::PHS][filter] = true;
+    }
   }
   return res;
 }();
@@ -96,49 +99,6 @@ VisualisationEvent::VisualisationEvent(VisualisationEventVO vo)
   this->mCollisionTime = vo.collisionTime;
   this->mMinTimeOfTracks = numeric_limits<float>::max();
   this->mMaxTimeOfTracks = numeric_limits<float>::min();
-}
-
-std::string VisualisationEvent::toJson()
-{
-  Document tree(kObjectType);
-  Document::AllocatorType& allocator = tree.GetAllocator();
-
-  // compatibility verification
-  tree.AddMember("fileVersion", rapidjson::Value().SetInt(JSON_FILE_VERSION), allocator);
-  tree.AddMember("runNumber", rapidjson::Value().SetInt(this->mRunNumber), allocator);
-  tree.AddMember("collisionTime", rapidjson::Value().SetString(this->mCollisionTime.c_str(), this->mCollisionTime.size()), allocator);
-  tree.AddMember("workflowVersion", rapidjson::Value().SetFloat(this->mWorkflowVersion), allocator);
-  tree.AddMember("workflowParameters", rapidjson::Value().SetString(this->mWorkflowParameters.c_str(), this->mWorkflowParameters.size()), allocator);
-  // Tracks
-  tree.AddMember("trackCount", rapidjson::Value().SetInt(this->getTrackCount()), allocator);
-
-  Value jsonTracks(kArrayType);
-  for (size_t i = 0; i < this->getTrackCount(); i++) {
-    jsonTracks.PushBack(this->mTracks[i].jsonTree(allocator), allocator);
-  }
-  tree.AddMember("mTracks", jsonTracks, allocator);
-
-  // Clusters
-  rapidjson::Value clusterCount(rapidjson::kNumberType);
-  clusterCount.SetInt(this->getClusterCount());
-  tree.AddMember("clusterCount", clusterCount, allocator);
-  Value jsonClusters(kArrayType);
-  for (size_t i = 0; i < this->getClusterCount(); i++) {
-    jsonClusters.PushBack(this->mClusters[i].jsonTree(allocator), allocator);
-  }
-  tree.AddMember("mClusters", jsonClusters, allocator);
-
-  // stringify
-  rapidjson::StringBuffer buffer;
-  rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(buffer);
-  tree.Accept(writer);
-  std::string json_str = std::string(buffer.GetString(), buffer.GetSize());
-  return json_str;
-}
-
-VisualisationEvent::VisualisationEvent(std::string fileName)
-{
-  this->fromFile(fileName);
 }
 
 VisualisationEvent::VisualisationEvent(const VisualisationEvent& source, EVisualisationGroup filter, float minTime, float maxTime)
@@ -159,88 +119,11 @@ VisualisationEvent::VisualisationEvent(const VisualisationEvent& source, EVisual
       this->mClusters.push_back(*it);
     }
   }
-}
-
-void VisualisationEvent::fromJson(std::string json)
-{
-  mTracks.clear();
-  mClusters.clear();
-
-  rapidjson::Document tree;
-  tree.Parse(json.c_str());
-
-  auto version = 1;
-  if (tree.HasMember("fileVersion")) {
-    rapidjson::Value& jsonFileVersion = tree["fileVersion"];
-    version = jsonFileVersion.GetInt();
+  for (auto it = source.mCalo.begin(); it != source.mCalo.end(); ++it) {
+    if (VisualisationEvent::mVis.contains[it->getSource()][filter]) {
+      this->mCalo.push_back(*it);
+    }
   }
-
-  o2::header::DataHeader::RunNumberType runNumber = 0;
-  if (tree.HasMember("runNumber")) {
-    rapidjson::Value& jsonRunNumber = tree["runNumber"];
-    runNumber = jsonRunNumber.GetInt();
-  }
-  this->setRunNumber(runNumber);
-
-  auto collisionTime = "not specified";
-  if (tree.HasMember("collisionTime")) {
-    rapidjson::Value& jsonCollisionTime = tree["collisionTime"];
-    collisionTime = jsonCollisionTime.GetString();
-  }
-  this->setCollisionTime(collisionTime);
-
-  rapidjson::Value& trackCount = tree["trackCount"];
-  this->mTracks.reserve(trackCount.GetInt());
-  rapidjson::Value& jsonTracks = tree["mTracks"];
-  for (auto& v : jsonTracks.GetArray()) {
-    mTracks.emplace_back(v);
-  }
-  this->mMinTimeOfTracks = numeric_limits<float>::max();
-  this->mMaxTimeOfTracks = numeric_limits<float>::min();
-  for (auto& v : this->mTracks) {
-    this->mMinTimeOfTracks = min(this->mMinTimeOfTracks, v.getTime());
-    this->mMaxTimeOfTracks = max(this->mMaxTimeOfTracks, v.getTime());
-  }
-
-  rapidjson::Value& clusterCount = tree["clusterCount"];
-  this->mClusters.reserve(clusterCount.GetInt());
-  rapidjson::Value& jsonClusters = tree["mClusters"];
-  for (auto& v : jsonClusters.GetArray()) {
-    mClusters.emplace_back(v);
-  }
-}
-
-void VisualisationEvent::toFile(std::string fileName)
-{
-  std::string json = toJson();
-  std::ofstream out(fileName);
-  out << json;
-  out.close();
-}
-
-std::string VisualisationEvent::fileNameIndexed(const std::string fileName, const int index)
-{
-  std::stringstream buffer;
-  buffer << fileName << std::setfill('0') << std::setw(3) << index << ".json";
-  return buffer.str();
-}
-
-bool VisualisationEvent::fromFile(std::string fileName)
-{
-  if (FILE* file = fopen(fileName.c_str(), "r")) {
-    fclose(file); // file exists
-  } else {
-    return false;
-  }
-  std::ifstream inFile;
-  inFile.open(fileName);
-
-  std::stringstream strStream;
-  strStream << inFile.rdbuf(); //read the file
-  inFile.close();
-  std::string str = strStream.str(); //str holds the content of the file
-  fromJson(str);
-  return true;
 }
 
 VisualisationEvent::VisualisationEvent()
