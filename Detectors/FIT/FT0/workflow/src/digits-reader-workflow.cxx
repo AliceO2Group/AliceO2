551b226a59beee54006f5dcfc9662b02ae1b6e9b
shahoian <ruben.shahoyan@cern.ch>
2022-05-19 13:51:04 +0200

    Use HBFUtilsInitializer in FIT digit reader workflows

--- Detectors/FIT/FT0/workflow/src/digits-reader-workflow.cxx
+++ Detectors/FIT/FT0/workflow/src/digits-reader-workflow.cxx
@@ -15,13 +15,20 @@
 /// \author ruben.shahoyan@cern.ch
 
 #include "Framework/CallbackService.h"
+#include "Framework/CallbacksPolicy.h"
 #include "Framework/ControlService.h"
 #include "Framework/ConfigParamRegistry.h"
 #include "Framework/Task.h"
 #include "FT0Workflow/DigitReaderSpec.h"
+#include "DetectorsRaw/HBFUtilsInitializer.h"
 #include "CommonUtils/ConfigurableParam.h"
 
 using namespace o2::framework;
+
+void customize(std::vector<o2::framework::CallbacksPolicy>& policies)
+{
+  o2::raw::HBFUtilsInitializer::addNewTimeSliceCallback(policies);
+}
 
 // we need to add workflow options before including Framework/runDataProcessing
 void customize(std::vector<ConfigParamSpec>& workflowOptions)
@@ -32,6 +39,7 @@ void customize(std::vector<ConfigParamSpec>& workflowOptions)
   options.push_back(ConfigParamSpec{"disable-trigger-input", o2::framework::VariantType::Bool, false, {"Disable trigger input DPL channel"}});
   std::string keyvaluehelp("Semicolon separated key=value strings");
   options.push_back(ConfigParamSpec{"configKeyValues", VariantType::String, "", {keyvaluehelp}});
+  o2::raw::HBFUtilsInitializer::addConfigOption(options);
   std::swap(workflowOptions, options);
 }
 
@@ -43,5 +51,9 @@ WorkflowSpec defineDataProcessing(const ConfigContext& ctx)
   o2::conf::ConfigurableParam::updateFromString(ctx.options().get<std::string>("configKeyValues"));
   DataProcessorSpec producer = o2::ft0::getDigitReaderSpec(ctx.options().get<bool>("disable-mc"), ctx.options().get<bool>("disable-trigger-input"));
   specs.push_back(producer);
+
+  // configure dpl timer to inject correct firstTFOrbit: start from the 1st orbit of TF containing 1st sampled orbit
+  o2::raw::HBFUtilsInitializer hbfIni(ctx, specs);
+
   return specs;
 }
