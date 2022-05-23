4c47a1d729a1f28d20e2fb31abf2be4df2587652
Matteo Concas <matteo.concas@cern.ch>
2022-05-23 19:15:47 +0200

    Improve TimeFrameGPU allocations

--- Detectors/ITSMFT/ITS/tracking/GPU/ITStrackingGPU/TrackerTraitsGPU.h
+++ Detectors/ITSMFT/ITS/tracking/GPU/ITStrackingGPU/TrackerTraitsGPU.h
@@ -22,33 +22,6 @@ namespace o2
 {
 namespace its
 {
-namespace gpu
-{
-template <int NLayers>
-struct StaticTrackingParameters {
-  StaticTrackingParameters<NLayers>& operator=(const StaticTrackingParameters<NLayers>& t) = default;
-  // int CellMinimumLevel();
-  /// General parameters
-  int ClusterSharing = 0;
-  int MinTrackLength = NLayers;
-  float NSigmaCut = 5;
-  float PVres = 1.e-2f;
-  int DeltaROF = 0;
-  int ZBins{256};
-  int PhiBins{128};
-  /// Trackleting cuts
-  float TrackletMaxDeltaPhi = 0.3f;
-  float TrackletMaxDeltaZ[NLayers - 1] = {0.1f, 0.1f, 0.3f, 0.3f, 0.3f, 0.3f};
-  /// Cell finding cuts
-  // float CellMaxDeltaTanLambda = 0.025f;
-  // float CellMaxDCA[NLayers - 2] = {0.05f, 0.04f, 0.05f, 0.2f, 0.4f};
-  // float CellMaxDeltaPhi = 0.14f;
-  // float CellMaxDeltaZ[NLayers - 2] = {0.2f, 0.4f, 0.5f, 0.6f, 3.0f};
-  // /// Neighbour finding cuts
-  // float NeighbourMaxDeltaCurvature[NLayers - 3] = {0.008f, 0.0025f, 0.003f, 0.0035f};
-  // float NeighbourMaxDeltaN[NLayers - 3] = {0.002f, 0.0090f, 0.002f, 0.005f};
-};
-} // namespace gpu
 
 template <int NLayers = 7>
 class TrackerTraitsGPU : public TrackerTraits
