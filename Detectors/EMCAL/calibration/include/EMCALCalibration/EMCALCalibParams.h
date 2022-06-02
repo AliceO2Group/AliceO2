039d0e2c2752f0f135e6ad461d6dfc4b78744bd5
Joshua Koenig <joshua.konig@cern.ch>
2022-06-02 12:04:46 -0400

    [EMCAL-566] Replacing algorithm::sum with own counter

--- Detectors/EMCAL/calibration/include/EMCALCalibration/EMCALCalibParams.h
+++ Detectors/EMCAL/calibration/include/EMCALCalibration/EMCALCalibParams.h
@@ -30,12 +30,13 @@ namespace emcal
 struct EMCALCalibParams : public o2::conf::ConfigurableParamHelper<EMCALCalibParams> {
 
   unsigned int minNEvents = 1e6;              ///< minimum number of events to trigger the calibration
-  unsigned int minNEntries = 1e5;             ///< minimum number of entries to trigger the calibration
+  unsigned int minNEntries = 1e6;             ///< minimum number of entries to trigger the calibration
   bool useNEventsForCalib = true;             ///< use the minimum number of events to trigger the calibration
   std::string calibType = "time";             ///< type of calibration to run
   std::string localRootFilePath = "";         ///< path to local root file in order to store the calibration histograms (off by default, only to be used for testing)
   bool useScaledHistoForBadChannelMap = true; ///< use the scaled histogram for the bad channel map
   bool enableTestMode = false;                ///< enable test mode for calibration
+  float minCellEnergyForTimeCalib = 0.5;      ///< minimum cell energy to enter the time calibration (typical minimum seed energy for clusters), time resolution gets better with rising energy
 
   O2ParamDef(EMCALCalibParams, "EMCALCalibParams");
 };
