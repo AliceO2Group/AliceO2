72f557dd8493cad8abba3852d8d6beedf1aa2f2a
shahoian <ruben.shahoyan@cern.ch>
2022-06-05 03:39:43 +0200

    Account for the physics triggers embedded into the ITS/MFT raw data
    The trigger headers with noData=1 and internal=0 are interpreted as external physics triggers,
    there could be multiple such triggers anywhere on the RDH page except interleaved with payload
    words. They will be extracted to separate vector<GBTTrigger> and sent as <DET>/PHYSTRIG/0 output.
    At the moment it is not used and is not written into the digit or cluster tree.

--- Detectors/ITSMFT/common/reconstruction/include/ITSMFTReconstruction/RawPixelDecoder.h
+++ Detectors/ITSMFT/common/reconstruction/include/ITSMFTReconstruction/RawPixelDecoder.h
@@ -28,6 +28,7 @@
 #include "ITSMFTReconstruction/PixelReader.h"
 #include "DataFormatsITSMFT/ROFRecord.h"
 #include "ITSMFTReconstruction/PixelData.h"
+#include "ITSMFTReconstruction/GBTWord.h"
 
 namespace o2
 {
@@ -107,6 +108,9 @@ class RawPixelDecoder final : public PixelReader
   void setRawDumpDirectory(const std::string& s) { mRawDumpDirectory = s; }
   auto getRawDumpDirectory() const { return mRawDumpDirectory; }
 
+  std::vector<GBTTrigger>& getExternalTriggers() { return mExtTriggers; }
+  const std::vector<GBTTrigger>& getExternalTriggers() const { return mExtTriggers; }
+
   struct LinkEntry {
     int entry = -1;
   };
@@ -125,6 +129,7 @@ class RawPixelDecoder final : public PixelReader
   std::vector<RUDecodeData> mRUDecodeVec;                   // set of active RUs
   std::array<short, Mapping::getNRUs()> mRUEntry;           // entry of the RU with given SW ID in the mRUDecodeVec
   std::vector<ChipPixelData*> mOrderedChipsPtr;             // special ordering helper used for the MFT (its chipID is not contiguous in RU)
+  std::vector<GBTTrigger> mExtTriggers;                     // external triggers
   std::string mSelfName{};                                  // self name
   std::string mRawDumpDirectory;                            // destination directory for dumps
   header::DataOrigin mUserDataOrigin = o2::header::gDataOriginInvalid; // alternative user-provided data origin to pick
@@ -143,6 +148,7 @@ class RawPixelDecoder final : public PixelReader
   uint32_t mNLinksDone = 0;                       // number of links reached end of data
   size_t mNChipsFired = 0;                        // global counter
   size_t mNPixelsFired = 0;                       // global counter
+  size_t mNExtTriggers = 0;                       // global counter
   size_t mInstanceID = 0;                         // pipeline instance
   size_t mNInstances = 1;                         // total number of pipelines
   TStopwatch mTimerTFStart;
