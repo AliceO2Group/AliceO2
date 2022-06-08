5c5b00b751cb0522bdb6c09b4c36d17bc13d54bd
Bhawani Singh <bhsingh@cern.ch>
2022-06-08 16:57:12 +0200

    TPCQC: Tracks Task Updated with maps

--- Detectors/TPC/qc/macro/runTracks.C
+++ Detectors/TPC/qc/macro/runTracks.C
@@ -45,11 +45,11 @@ void runTracks(std::string outputFileName = "tpcQcTracks", std::string_view inpu
 
   // ===| create Tracks object |=====================================================
   qc::Tracks tracksQC;
-
+  // set track cutss defaults are (eta = 1.0, nCluster = 60, dEdxTot  = 20)
+  tracksQC.setTrackCuts(1., 60, 20.);
   tracksQC.initializeHistograms();
   gStyle->SetPalette(kCividis);
-  qc::helpers::setStyleHistogram1D(tracksQC.getHistograms1D());
-  qc::helpers::setStyleHistogram2D(tracksQC.getHistograms2D());
+  qc::helpers::setStyleHistogramsInMap(tracksQC.getMapHist());
 
   // ===| event loop |============================================================
   for (int i = 0; i < tree->GetEntriesFast(); ++i) {
@@ -63,41 +63,92 @@ void runTracks(std::string outputFileName = "tpcQcTracks", std::string_view inpu
   }
 
   // ===| get histograms |=======================================================
-  auto& histos1D = tracksQC.getHistograms1D();
-  auto& histos2D = tracksQC.getHistograms2D();
+  auto& mMapOfHisto = tracksQC.getMapHist();
+
+  // 1d hitograms
+  auto& hNClustersBeforeCuts = mMapOfHisto["hNClustersBeforeCuts"];
+  auto& hNClustersAfterCuts = mMapOfHisto["hNClustersAfterCuts"];
+  auto& hEta = mMapOfHisto["hEta"];
+  auto& hPhiAside = mMapOfHisto["hPhiAside"];
+  auto& hPhiCside = mMapOfHisto["hPhiCside"];
+  auto& hPt = mMapOfHisto["hPt"];
+  auto& hSign = mMapOfHisto["hSign"];
+  auto& hEtaNeg = mMapOfHisto["hEtaNeg"];
+  auto& hEtaPos = mMapOfHisto["hEtaPos"];
+  auto& hPhiAsideNeg = mMapOfHisto["hPhiAsideNeg"];
+  auto& hPhiAsidePos = mMapOfHisto["hPhiAsidePos"];
+  auto& hPhiCsideNeg = mMapOfHisto["hPhiCsideNeg"];
+  auto& hPhiCsidePos = mMapOfHisto["hPhiCsidePos"];
+  auto& hPtNeg = mMapOfHisto["hPtNeg"];
+  auto& hPtPos = mMapOfHisto["hPtPos"];
+  auto& hEtaBeforeCuts = mMapOfHisto["hEtaBeforeCuts"];
+  auto& hPtBeforeCuts = mMapOfHisto["hPtBeforeCuts"];
+  auto& hQOverPt = mMapOfHisto["hQOverPt"];
+  auto& hPhiBothSides = mMapOfHisto["hPhiBothSides"];
+  // 2d histograms
+  auto& h2DNClustersEta = mMapOfHisto["h2DNClustersEta"];
+  auto& h2DNClustersPhiAside = mMapOfHisto["h2DNClustersPhiAside"];
+  auto& h2DNClustersPhiCside = mMapOfHisto["h2DNClustersPhiCside"];
+  auto& h2DNClustersPt = mMapOfHisto["h2DNClustersPt"];
+  auto& h2DEtaPhi = mMapOfHisto["h2DEtaPhi"];
+  auto& h2DEtaPhiNeg = mMapOfHisto["h2DEtaPhiNeg"];
+  auto& h2DEtaPhiPos = mMapOfHisto["h2DEtaPhiPos"];
+  auto& h2DNClustersEtaBeforeCuts = mMapOfHisto["h2DNClustersEtaBeforeCuts"];
+  auto& h2DNClustersPtBeforeCuts = mMapOfHisto["h2DNClustersPtBeforeCuts"];
+  auto& h2DEtaPhiBeforeCuts = mMapOfHisto["h2DEtaPhiBeforeCuts"];
+  auto& h2DQOverPtPhiAside = mMapOfHisto["h2DQOverPtPhiAside"];
+  auto& h2DQOverPtPhiCside = mMapOfHisto["h2DQOverPtPhiCside"];
+
+  // 1d histograms
+  auto& hEtaRatio = mMapOfHisto["hEtaRatio"];
+  auto& hPhiAsideRatio = mMapOfHisto["hPhiAsideRatio"];
+  auto& hPhiCsideRatio = mMapOfHisto["hPhiCsideRatio"];
+  auto& hPtRatio = mMapOfHisto["hPtRatio"];
 
   // ===| create canvases |======================================================
   auto* c1 = new TCanvas("c1", "eta_phi_pt", 1200, 600);
   c1->Divide(3, 2);
   c1->cd(1);
-  histos2D[6].Draw();
+  h2DEtaPhiPos->Draw();
   c1->cd(2);
-  histos1D[2].Draw();
+  hEta->Draw();
   c1->cd(3);
-  histos1D[4].Draw();
+  hPhiCside->Draw();
   c1->cd(4);
-  histos2D[5].Draw();
+  h2DEtaPhiNeg->Draw();
   c1->cd(5);
-  histos1D[5].Draw();
+  hPt->Draw();
   gPad->SetLogy();
   c1->cd(6);
-  histos1D[3].Draw();
+  hPhiAside->Draw();
 
   auto* c2 = new TCanvas("c2", "nClustersPerTrack_details", 1200, 300);
   c2->Divide(3, 1);
   c2->cd(1);
-  histos2D[0].Draw();
+  h2DNClustersEta->Draw();
   c2->cd(2);
-  histos2D[2].Draw();
+  h2DNClustersPhiCside->Draw();
   c2->cd(3);
-  histos2D[1].Draw();
+  h2DNClustersPhiAside->Draw();
 
   auto* c3 = new TCanvas("c3", "nClustersPerTrack_cuts", 800, 300);
   c3->Divide(2, 1);
   c3->cd(1);
-  histos1D[0].Draw();
+  hNClustersBeforeCuts->Draw();
   c3->cd(2);
-  histos1D[1].Draw();
+  hNClustersAfterCuts->Draw();
+
+  // ratio plots
+  auto* c4 = new TCanvas("c4", "ratio", 800, 300);
+  c4->Divide(2, 2);
+  c4->cd(1);
+  hEtaRatio->Draw();
+  c4->cd(2);
+  hPhiAsideRatio->Draw();
+  c4->cd(3);
+  hPhiCsideRatio->Draw();
+  c4->cd(4);
+  hPtRatio->Draw();
 
   if (outputFileName.find(".root") != std::string::npos) {
     outputFileName.resize(outputFileName.size() - 5);
@@ -109,10 +160,12 @@ void runTracks(std::string outputFileName = "tpcQcTracks", std::string_view inpu
   f->WriteObject(c1, "eta_phi_pt");
   f->WriteObject(c2, "nClustersPerTrack_details");
   f->WriteObject(c3, "nClustersPerTrack_cuts");
+  f->WriteObject(c4, "RatioTrack_cuts");
   f->Close();
   delete c1;
   delete c2;
   delete c3;
+  delete c4;
 
   //===| dump histograms to a file |=============================================
   std::string histFile = outputFileName + ".root";
