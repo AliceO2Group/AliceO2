97d929a8c80c3069cfed5e772c20bc857b3143df
Chiara Zampolli <Chiara.Zampolli@cern.ch>
2022-06-09 13:52:36 +0200

    store vectors for EnvVars, Collimators, change a bit the policy

--- Detectors/GRP/workflows/src/GRPDCSDPsSpec.cxx
+++ Detectors/GRP/workflows/src/GRPDCSDPsSpec.cxx
@@ -117,6 +117,7 @@ void GRPDCSDPsDataProcessor::init(o2::framework::InitContext& ic)
 
 void GRPDCSDPsDataProcessor::run(o2::framework::ProcessingContext& pc)
 {
+  mLHCIFupdated = false;
   TStopwatch sw;
   auto startValidity = DataRefUtils::getHeader<DataProcessingHeader*>(pc.inputs().getFirstValid(true))->creation;
   auto dps = pc.inputs().get<gsl::span<DPCOM>>("input");
@@ -128,18 +129,28 @@ void GRPDCSDPsDataProcessor::run(o2::framework::ProcessingContext& pc)
   mProcessor->process(dps);
   Duration elapsedTime = timeNow - mTimer; // in seconds
   if (elapsedTime.count() >= mDPsUpdateInterval || mProcessor->isLHCIFInfoUpdated()) {
+    // after enough time or after something changed, we store the LHCIF part of the DPs:
+    LOG(debug) << "enough time passed (" << elapsedTime.count() << " s), sending to CCDB";
+    mProcessor->updateLHCIFInfoCCDB();
     sendLHCIFDPsoutput(pc.outputs());
-    mProcessor->resetLHCIFDPs();
+    mProcessor->resetAndKeepLastLHCIFDPs();
+    mLHCIFupdated = true;
+  }
+  if (elapsedTime.count() >= mDPsUpdateInterval) {
+    // after enough time, we store:
+    // collimators:
+    mProcessor->updateCollimatorsCCDB();
+    sendCollimatorsDPsoutput(pc.outputs());
+    mProcessor->resetAndKeepLast(mProcessor->getCollimatorsObj().mCollimators);
+    // env vars:
+    mProcessor->updateEnvVarsCCDB();
+    sendEnvVarsDPsoutput(pc.outputs());
+    mProcessor->resetAndKeepLast(mProcessor->getEnvVarsObj().mEnvVars);
     mTimer = timeNow;
+    mProcessor->resetPIDs();
   }
   if (mProcessor->isMagFieldUpdated()) {
     sendMagFieldDPsoutput(pc.outputs());
-  }
-  if (mProcessor->isCollimatorsUpdated()) {
-    sendCollimatorsDPsoutput(pc.outputs());
-  }
-  if (mProcessor->isEnvVarsUpdated()) {
-    sendEnvVarsDPsoutput(pc.outputs());
   }
   sw.Stop();
   if (mReportTiming) {
@@ -152,13 +163,17 @@ void GRPDCSDPsDataProcessor::endOfStream(o2::framework::EndOfStreamContext& ec)
 {
 
   LOG(info) << " ********** End of Stream **********";
-  mProcessor->updateLHCIFInfoCCDB();
-  sendLHCIFDPsoutput(ec.outputs());
-  /*
-  sendMagFieldDPsoutput(ec.outputs());
+  // we force writing to CCDB the entries for which we accumulate values in vectors (we don't do it for the B field
+  // because this is updated every time on change of any of the 4 DPs related to it)
+  if (!mLHCIFupdated) { // the last TF did not update the LHCIF CCDB entry, let's force it
+    mProcessor->updateLHCIFInfoCCDB();
+    sendLHCIFDPsoutput(ec.outputs());
+  }
+  mProcessor->updateCollimatorsCCDB();
   sendCollimatorsDPsoutput(ec.outputs());
+
+  mProcessor->updateEnvVarsCCDB();
   sendEnvVarsDPsoutput(ec.outputs());
-  */
 }
 
 //________________________________________________________________
