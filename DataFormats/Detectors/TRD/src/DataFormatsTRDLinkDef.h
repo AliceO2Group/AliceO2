f769176c3a69ec67679bb4b8cf48a613912b983c
Ole Schmidt <ole.schmidt@cern.ch>
2022-06-11 20:48:56 +0200

    TRD HelperMethods and small typo correction in TRD DCS processor spec (#9143)

--- DataFormats/Detectors/TRD/src/DataFormatsTRDLinkDef.h
+++ DataFormats/Detectors/TRD/src/DataFormatsTRDLinkDef.h
@@ -32,6 +32,7 @@
 #pragma link C++ class o2::trd::AngularResidHistos + ;
 #pragma link C++ class o2::trd::CalVdriftExB + ;
 #pragma link C++ class o2::trd::CompressedDigit + ;
+#pragma link C++ class o2::trd::HelperMethods + ;
 #pragma link C++ class std::vector < o2::trd::Tracklet64> + ;
 #pragma link C++ class std::vector < o2::trd::CalibratedTracklet> + ;
 #pragma link C++ class std::vector < o2::trd::TrackTriggerRecord> + ;
