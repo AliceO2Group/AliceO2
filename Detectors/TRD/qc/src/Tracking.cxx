2b7a93de1a18e425afed3a07567f10a8469ba063
Felix Schlepper <felix.schlepper@cern.ch>
2022-06-14 07:39:14 +0000

    Added PID parameters to TRD tracking QC (#9121)
    and add track Phi and Eta angles

--- Detectors/TRD/qc/src/Tracking.cxx
+++ Detectors/TRD/qc/src/Tracking.cxx
@@ -55,9 +55,12 @@ void Tracking::checkTrack(const TrackTRD& trkTrd, bool isTPCTRD)
   TrackQC qcStruct;
   qcStruct.type = isTPCTRD ? 0 : 1;
   qcStruct.nTracklets = trkTrd.getNtracklets();
+  qcStruct.nLayers = trkTrd.getNlayersFindable();
   qcStruct.chi2 = trkTrd.getChi2();
   qcStruct.reducedChi2 = trkTrd.getReducedChi2();
   qcStruct.pt = trkTrd.getPt();
+  qcStruct.ptSigma2 = trkTrd.getSigma1Pt2();
+  qcStruct.pid = trkTrd.getPID();
 
   LOGF(debug, "Got track with %i tracklets and ID %i", trkTrd.getNtracklets(), trkTrd.getRefGlobalTrackId());
   const auto& trkSeed = isTPCTRD ? mTracksTPC[trkTrd.getRefGlobalTrackId()].getParamOut() : mTracksITSTPC[trkTrd.getRefGlobalTrackId()].getParamOut();
@@ -101,6 +104,8 @@ void Tracking::checkTrack(const TrackTRD& trkTrd, bool isTPCTRD)
     qcStruct.trackSnp[iLayer] = trk.getSnp();
     qcStruct.trackTgl[iLayer] = trk.getTgl();
     qcStruct.trackQpt[iLayer] = trk.getQ2Pt();
+    qcStruct.trackPhi[iLayer] = trk.getPhi();
+    qcStruct.trackEta[iLayer] = trk.getEta();
     qcStruct.trackletYraw[iLayer] = mTrackletsCalib[trkltId].getY();
     qcStruct.trackletZraw[iLayer] = mTrackletsCalib[trkltId].getZ();
     qcStruct.trackletY[iLayer] = trkltPosUp[0];
@@ -112,6 +117,11 @@ void Tracking::checkTrack(const TrackTRD& trkTrd, bool isTPCTRD)
     qcStruct.trackletRob[iLayer] = mTrackletsRaw[trkltId].getROB();
     qcStruct.trackletMcm[iLayer] = mTrackletsRaw[trkltId].getMCM();
     qcStruct.trackletChi2[iLayer] = chi2trklt;
+    qcStruct.trackletCharges[iLayer] = {
+      mTrackletsRaw[trkltId].getQ0(),
+      mTrackletsRaw[trkltId].getQ1(),
+      mTrackletsRaw[trkltId].getQ2(),
+    };
   }
   mTrackQC.push_back(qcStruct);
 }
