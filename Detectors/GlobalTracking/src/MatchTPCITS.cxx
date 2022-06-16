AliceO2Group/AliceO2

commit eca4cfaa8a4d053825d4ab4c8054d4379aec7d21

Fix a bunch of pedantic warnings (#8908)
* Fix some ITS related files

* Add MatchITSTPCQC and MatchTPCITS

* Add ClustererTask CookedTracker FastMultEst RecoGeomHelper

* Add Detector

* Change construction order rather than datamember settings

* Remove unused variables

* Fix test, re-implementing Hit addition

* Fix initialisation order in NoisCalibratorSpec

* Fix wrong conversion to int

* Update checkStack.cxx

Detectors/GlobalTracking/src/MatchTPCITS.cxx
@@ -569,7 +569,7 @@ bool MatchTPCITS::prepareITSData()
     float tMin = nBC * o2::constants::lhc::LHCBunchSpacingMUS;
     float tMax = (nBC + mITSROFrameLengthInBC) * o2::constants::lhc::LHCBunchSpacingMUS;
     if (!mITSTriggered) {
-      auto irofCont = nBC / mITSROFrameLengthInBC;
+      size_t irofCont = nBC / mITSROFrameLengthInBC;
       if (mITSTrackROFContMapping.size() <= irofCont) { // there might be gaps in the non-empty rofs, this will map continuous ROFs index to non empty ones
         mITSTrackROFContMapping.resize((1 + irofCont / 128) * 128, 0);
       }
@@ -1392,7 +1392,7 @@ bool MatchTPCITS::refitABTrack(int iITSAB, const TPCABSeed& seed)
   // refit track outward in the ITS
   const auto& itsClRefs = mABTrackletRefs[iITSAB];
   int nclRefit = 0, ncl = itsClRefs.getNClusters();
-  uint16_t patt = 0;
+
   float chi2 = 0.f;
   // NOTE: the ITS cluster absolute indices are stored from inner to outer layers
   for (int icl = itsClRefs.getFirstEntry(); icl < itsClRefs.getEntriesBound(); icl++) {
@@ -1465,7 +1465,6 @@ bool MatchTPCITS::refitABTrack(int iITSAB, const TPCABSeed& seed)
 bool MatchTPCITS::refitTPCInward(o2::track::TrackParCov& trcIn, float& chi2, float xTgt, int trcID, float timeTB) const
 {
   // inward refit
-  constexpr float TolSNP = 0.99;
   const auto& tpcTrOrig = mTPCTracksArray[trcID];
 
   trcIn = tpcTrOrig.getOuterParam();
@@ -1625,7 +1624,6 @@ void MatchTPCITS::runAfterBurner()
   mTimer[SWABWinners].Start(false);
   int nwin = 0;
   // select winners
-  int iter = 0;
   struct SID {
     int seedID = -1;
     float chi2 = 1e9;
@@ -1664,7 +1662,6 @@ void MatchTPCITS::runAfterBurner()
       continue;
     }
     auto bestID = ABSeed.getBestLinkID();
-    const auto& link = ABSeed.getLink(bestID); // RS FIXME TMP
     if (ABSeed.checkLinkHasUsedClusters(bestID, mABClusterLinkIndex)) {
       ABSeed.setNeedAlternative(); // flag for later processing
       //RSTMP      LOG(info) << "Iter: " << iter << " seed has used clusters " << i << "[" << candAB[i].seedID << "/" << candAB[i].chi2 << "]"  << " last lr: " << int(ABSeed.lowestLayer) << " Ncont: " << int(link.nContLayers);;
@@ -1804,7 +1801,7 @@ int MatchTPCITS::followABSeed(const o2::track::TrackParCov& seed, const ITSChipC
   o2::math_utils::IntervalXYf_t trcLinPar; // line parameters for B OFF data
   float sna, csa;
   // approximate errors
-  float errY = std::sqrt(seedC.getSigmaY2() + mParams->err2ABExtraY), errYFrac = errY * mRGHelper.ladderWidthInv(), errPhi = errY * lr.rInv;
+  float errY = std::sqrt(seedC.getSigmaY2() + mParams->err2ABExtraY), errYFrac = errY * mRGHelper.ladderWidthInv();
   if (mFieldON) {
     seedC.getCircleParams(propagator->getNominalBz(), trcCircle, sna, csa);
   } else {
@@ -1826,7 +1823,6 @@ int MatchTPCITS::followABSeed(const o2::track::TrackParCov& seed, const ITSChipC
     // coordinates xCross,yCross,zCross for this central chipIDguess, although we are going to check also neighbours
     float t = 1e9, xCross, yCross;
     const auto& chipC = lad.chips[chipIDguess];
-    bool res = mFieldON ? chipC.xyEdges.circleCrossParam(trcCircle, t) : chipC.xyEdges.lineCrossParam(trcLinPar, t);
     chipC.xyEdges.eval(t, xCross, yCross);
     float dx = xCross - xCurr, dy = yCross - yCurr, dst2 = dx * dx + dy * dy, dst = sqrtf(dst2);
     // Z-step sign depends on radius decreasing or increasing during the propagation
@@ -1834,7 +1830,7 @@ int MatchTPCITS::followABSeed(const o2::track::TrackParCov& seed, const ITSChipC
 
     for (int ich = -1; ich < 2; ich++) {
       int chipID = chipIDguess + ich;
-      if (chipID < 0 || chipID >= lad.chips.size()) {
+      if (chipID < 0 || chipID >= static_cast<int>(lad.chips.size())) {
         continue;
       }
       if (lad.chips[chipID].zRange.isOutside(zCross, mParams->nABSigmaZ * errZ)) {
@@ -1858,7 +1854,6 @@ int MatchTPCITS::followABSeed(const o2::track::TrackParCov& seed, const ITSChipC
         //trcLC.print();
         break; // the chips of the ladder are practically on the same X and alpha
       }
-      int cntc = 0;
       for (auto clID : chipSelClusters) {
         const auto& cls = mITSClustersArray[clID];
         auto chi2 = trcLC.getPredictedChi2(cls);

DataFormats/Detectors/ITSMFT/common/src/ClusterPattern.cxx
@@ -73,7 +73,7 @@ std::ostream& operator<<(std::ostream& os, const ClusterPattern& pattern)
   unsigned char tempChar = 0;
   int s = 0;
   int ic = 0;
-  for (unsigned int i = 2; i < pattern.getUsedBytes() + 2; i++) {
+  for (int i = 2; i < pattern.getUsedBytes() + 2; i++) {
     tempChar = pattern.mBitmap[i];
     s = 128; // 0b10000000
     while (s > 0) {
@@ -126,7 +126,7 @@ int ClusterPattern::getCOG(int rowSpan, int colSpan, const unsigned char patt[Ma
   if (nBits % 8 != 0) {
     nBytes++;
   }
-  for (unsigned int i = 0; i < nBytes; i++) {
+  for (int i = 0; i < nBytes; i++) {
     unsigned char tempChar = patt[i];
     int s = 128; // 0b10000000
     while (s > 0) {

DataFormats/Detectors/ITSMFT/common/src/ClusterTopology.cxx
@@ -76,11 +76,14 @@ unsigned int ClusterTopology::hashFunction(const void* key, int len)
   switch (len) {
     case 3:
       h ^= data[2] << 16;
+      break;
     case 2:
       h ^= data[1] << 8;
+      break;
     case 1:
       h ^= data[0];
       h *= m;
+      break;
   };
   // Do a few final mixes of the hash to ensure the last few
   // bytes are well-incorporated.

DataFormats/Reconstruction/include/ReconstructionDataFormats/Vertex.h
@@ -122,7 +122,7 @@ class Vertex : public VertexBase
   GPUhdDefault() Vertex() = default;
   GPUhdDefault() ~Vertex() = default;
   GPUd() Vertex(const math_utils::Point3D<float>& pos, const gpu::gpustd::array<float, kNCov>& cov, ushort nCont, float chi2)
-    : VertexBase(pos, cov), mNContributors(nCont), mChi2(chi2)
+    : VertexBase(pos, cov), mChi2(chi2), mNContributors(nCont)
   {
   }
 

DataFormats/simulation/include/SimulationDataFormat/TrackReference.h
@@ -183,7 +183,8 @@ class TrackReference
 // already fetched from VMC
 inline TrackReference::TrackReference(float x, float y, float z, float px, float py, float pz, float l, float tof,
                                       int trackID, int detlabel)
-  : mX(x),
+  : mTrackNumber(trackID),
+    mX(x),
     mY(y),
     mZ(z),
     mPX(px),
@@ -191,7 +192,6 @@ inline TrackReference::TrackReference(float x, float y, float z, float px, float
     mPZ(pz),
     mTrackLength(l),
     mTof(tof),
-    mTrackNumber(trackID),
     mDetectorId(detlabel)
 {
 }

Detectors/GlobalTracking/src/MatchITSTPCQC.cxx
@@ -99,7 +99,6 @@ bool MatchITSTPCQC::init()
   Double_t xlogminPt = TMath::Log10(xminPt);
   Double_t xlogmaxPt = TMath::Log10(xmaxPt);
   Double_t dlogxPt = (xlogmaxPt - xlogminPt) / nbinsPt;
-  Double_t yPt = (xlogmaxPt - xlogminPt) / nbinsPt;
   for (int i = 0; i <= nbinsPt; i++) {
     Double_t xlogPt = xlogminPt + i * dlogxPt;
     xbinsPt[i] = TMath::Exp(TMath::Log(10) * xlogPt);
@@ -175,7 +174,8 @@ void MatchITSTPCQC::run(o2::framework::ProcessingContext& ctx)
   // cache selection for TPC tracks
   std::vector<bool> isTPCTrackSelectedEntry(mTPCTracks.size(), false);
   TrackCuts cuts;
-  for (auto itrk = 0; itrk < mTPCTracks.size(); ++itrk) {
+
+  for (size_t itrk = 0; itrk < mTPCTracks.size(); ++itrk) {
     auto const& trkTpc = mTPCTracks[itrk];
     // if (selectTrack(trkTpc)) {
     //   isTPCTrackSelectedEntry[itrk] = true;
@@ -189,7 +189,7 @@ void MatchITSTPCQC::run(o2::framework::ProcessingContext& ctx)
   // numerator + eta, chi2...
   if (mUseMC) {
     mMapLabels.clear();
-    for (auto itrk = 0; itrk < mITSTPCTracks.size(); ++itrk) {
+    for (int itrk = 0; itrk < static_cast<int>(mITSTPCTracks.size()); ++itrk) {
       auto const& trk = mITSTPCTracks[itrk];
       auto idxTrkTpc = trk.getRefTPC().getIndex();
       if (isTPCTrackSelectedEntry[idxTrkTpc] == true) {
@@ -260,7 +260,7 @@ void MatchITSTPCQC::run(o2::framework::ProcessingContext& ctx)
     mMapTPCLabels.clear();
     // filling the map where we store for each MC label, the track id of the reconstructed
     // track with the highest number of TPC clusters
-    for (auto itrk = 0; itrk < mTPCTracks.size(); ++itrk) {
+    for (int itrk = 0; itrk < static_cast<int>(mTPCTracks.size()); ++itrk) {
       auto const& trk = mTPCTracks[itrk];
       if (isTPCTrackSelectedEntry[itrk] == true) {
         auto lbl = mRecoCont.getTrackMCLabel({(unsigned int)(itrk), GID::Source::TPC});
@@ -301,7 +301,7 @@ void MatchITSTPCQC::run(o2::framework::ProcessingContext& ctx)
     }
   } else {
     // if we are in data, we loop over all tracks (no check on the label)
-    for (int itrk = 0; itrk < mTPCTracks.size(); ++itrk) {
+    for (size_t itrk = 0; itrk < mTPCTracks.size(); ++itrk) {
       auto const& trk = mTPCTracks[itrk];
       if (isTPCTrackSelectedEntry[itrk] == true) {
         mPtTPC->Fill(trk.getPt());
@@ -376,8 +376,6 @@ void MatchITSTPCQC::finalize()
     }
   }
 
-  float scaleFactTPC = 1. / mNTPCSelectedTracks;
-  float scaleFactITSTPC = 1. / mNITSTPCSelectedTracks;
   /*
   mPtTPC->Scale(scaleFactTPC);
   mPt->Scale(scaleFactITSTPC);

Detectors/ITSMFT/ITS/QC/TestDataReaderWorkflow/include/ITSQCDataReaderWorkflow/TestDataReader.h
@@ -85,7 +85,7 @@ class TestDataReader : public Task
 
   ChipPixelData* mChipData = nullptr;
   std::string mInputName = "Split9.bin";
-  int mIndexPush;
+  size_t mIndexPush;
   //  int mPixelSize;
   std::vector<int> mNDigits;
   std::vector<std::string> mFolderNames;
@@ -106,7 +106,7 @@ class TestDataReader : public Task
   int mEventRegistered;
   int mTotalPixelSize;
   static constexpr int sNError = 11;
-  //			unsigned int Error[sNError];
+  // unsigned int Error[sNError];
   std::array<unsigned int, sNError> mErrors;
   std::vector<std::array<unsigned int, sNError>> mErrorsVec;
   std::vector<std::array<unsigned int, sNError>> mErrorsVecTest;
@@ -119,7 +119,7 @@ class TestDataReader : public Task
   int mIndexPushEx;
   int mFileRemain;
   int mFileInfo;
-  //Immediate Injection Variables//
+  // Immediate Injection Variables//
 
   int mNewFileInj;
   //  int mNewFileInjAction;

Detectors/ITSMFT/ITS/QC/TestDataReaderWorkflow/src/TestDataReader.cxx
@@ -74,7 +74,7 @@ void TestDataReader::init(InitContext& ic)
   mFolderNames = GetFName(mWorkDir);
 
   cout << "NFolder = " << mFolderNames.size() << endl;
-  for (int i = 0; i < mFolderNames.size(); i++) {
+  for (size_t i = 0; i < mFolderNames.size(); i++) {
 
     cout << "FDN = " << mFolderNames[i] << endl;
 
@@ -82,7 +82,7 @@ void TestDataReader::init(InitContext& ic)
 
     cout << "FDN File Size = " << mFileNames[i].size() << endl;
 
-    for (int j = 0; j < mFileNames[i].size(); j++) {
+    for (size_t j = 0; j < mFileNames[i].size(); j++) {
 
       cout << "FDN File = " << mFileNames[i][j] << endl;
     }
@@ -91,12 +91,11 @@ void TestDataReader::init(InitContext& ic)
     mErrors[i] = 0;
   }
 
-  //		mEventPerPush = 3000;
+  // mEventPerPush = 3000;
   mEventRegistered = 0;
   mTotalPixelSize = 0;
 
-  //	GetFileName("infile");
-
+  //("infile");
   //
 
   const Int_t numOfChips = o2::itsmft::ChipMappingITS::getNChips();
@@ -114,9 +113,9 @@ void TestDataReader::run(ProcessingContext& pc)
   // Keep checking new folders (and files in the folders)
   // If found, process them one by one
 
-  //Defining all local variables
+  // Defining all local variables
   int j = 0;
-  int NEventPre;
+  int NEventPre = 0;
   int NEvent;
   double PercentDone = 0;
   int ErrorDetcted;
@@ -130,7 +129,7 @@ void TestDataReader::run(ProcessingContext& pc)
   // Get folders in working directory, put all files in a vector
   mNowFolderNames = GetFName(mWorkDir);
   cout << "Now NFolder = " << mNowFolderNames.size() << endl;
-  for (int i = 0; i < mNowFolderNames.size(); i++) {
+  for (size_t i = 0; i < mNowFolderNames.size(); i++) {
     mNowFileNames.push_back(GetFName(mNowFolderNames[i]));
   }
 
@@ -180,7 +179,7 @@ void TestDataReader::run(ProcessingContext& pc)
 
   LOG(debug) << "Get IN LOOP";
   // Checking for new files, extract them into new vector
-  for (int i = 0; i < mFolderNames.size(); i++) {
+  for (size_t i = 0; i < mFolderNames.size(); i++) {
     std::set_difference(mNowFileNames[i].begin(), mNowFileNames[i].end(), mFileNames[i].begin(), mFileNames[i].end(), std::inserter(mDiffFileNamePush, mDiffFileNamePush.begin()));
     mDiffFileNames.push_back(mDiffFileNamePush);
     cout << "Difference File Size Between New and Initial Runs " << mDiffFileNames[i].size() << endl;
@@ -190,8 +189,8 @@ void TestDataReader::run(ProcessingContext& pc)
 
   LOG(debug) << "DONE GRABING Existing";
 
-  //Getting the new files from new folders that does not exist in the previous cycle
-  for (int i = mFolderNames.size(); i < mNowFolderNames.size(); i++) {
+  // Getting the new files from new folders that does not exist in the previous cycle
+  for (size_t i = mFolderNames.size(); i < mNowFolderNames.size(); i++) {
     mDiffFileNames.push_back(mNowFileNames[i]);
     cout << "New File Size Between New and Initial Runs " << mDiffFileNames[i].size() << endl;
   }
@@ -204,15 +203,15 @@ void TestDataReader::run(ProcessingContext& pc)
 
   LOG(debug) << "Start Loop";
 
-  //Start Decoding New Files by loop through the new file vector
+  // Start Decoding New Files by loop through the new file vector
 
-  for (int i = 0; i < mNowFolderNames.size(); i++) {
+  for (size_t i = 0; i < mNowFolderNames.size(); i++) {
 
     LOG(debug) << "i = " << i << "    mDiffFileNames[i].size() = " << mDiffFileNames[i].size();
 
-    //Getting the folder name ID
+    // Getting the folder name ID
 
-    int pos = mNowFolderNames[i].find_last_of("/");
+    size_t pos = mNowFolderNames[i].find_last_of("/");
 
     if (pos != string::npos) {
       mRunID = mNowFolderNames[i].substr(pos + 1);
@@ -220,7 +219,7 @@ void TestDataReader::run(ProcessingContext& pc)
 
     LOG(debug) << "FileDone = " << mFileDone << endl;
 
-    //Reading files one by one
+    // Reading files one by one
 
     if (mDiffFileNames[i].size() > 0 && mFileDone == 1) {
 
@@ -229,11 +228,11 @@ void TestDataReader::run(ProcessingContext& pc)
       cout << "RunID = " << mRunID << endl;
       cout << "File Location = " << mDiffFileNames[i][0] << endl;
 
-      //Getting the RunID
+      // Getting the RunID
       size_t last_index1 = mRunID.find_last_not_of("0123456789");
       string RunIDS = mRunID.substr(last_index1 + 1);
 
-      //Getting the FileID
+      // Getting the FileID
       string FileIDS;
       pos = mDiffFileNames[i][0].find_last_of("/");
       if (pos != string::npos) {
@@ -245,21 +244,21 @@ void TestDataReader::run(ProcessingContext& pc)
       size_t last_index2 = FileIDS.find_last_not_of("0123456789");
       FileIDS = FileIDS.substr(last_index2 + 1);
 
-      //Extracting the RunID and File to integer in order to make it possible to inject to the QC
+      // Extracting the RunID and File to integer in order to make it possible to inject to the QC
       mRunNumber = std::stoi(RunIDS);
       mFileID = std::stoi(FileIDS);
 
       ofstream fout(Form("ErrorData/ErrorLogRun%d_File%d.dat", mRunNumber, mFileID));
       fout << " START OF ERROR REPORT For Run " << mRunNumber << "  File " << mFileID << endl;
 
-      //Reading the first file (Because it is one by one)
+      // Reading the first file (Because it is one by one)
       mInputName = mDiffFileNames[i][0];
 
       mEventRegistered = 0;
       LOG(debug) << "mInputName = " << mInputName;
 
-      //Inject fake thing digits for the QC to update immediately
-      //mDigitsTest is the fake digit for updating the QC immediately on the QC GUI
+      // Inject fake thing digits for the QC to update immediately
+      // mDigitsTest is the fake digit for updating the QC immediately on the QC GUI
 
       if (mNewFileInj == 1) {
         cout << "New File Injected, Now Updating the Canvas and Light" << endl;
@@ -284,7 +283,7 @@ void TestDataReader::run(ProcessingContext& pc)
         break;
       }
 
-      //DONE Inhection//
+      // DONE Inhection//
 
       o2::itsmft::RawPixelReader<o2::itsmft::ChipMappingITS> mRawReader;
       mRawReader.setPadding128(true); // payload GBT words are padded to 16B
@@ -305,7 +304,7 @@ void TestDataReader::run(ProcessingContext& pc)
         if (NChip < NChipMax) {
           break;
         }
-        //	cout << "Pass Chip" << endl;
+        // cout << "Pass Chip" << endl;
 
         const auto* ruInfo = rawErrorReader.getCurrRUDecodeData()->ruInfo;
         const auto& statRU = rawErrorReader.getRUDecodingStatSW(ruInfo->idSW);
@@ -340,7 +339,7 @@ void TestDataReader::run(ProcessingContext& pc)
           mTotalPixelSize = 0;
         }
 
-        //Printing the Event Number Processed
+        // Printing the Event Number Processed
 
         if (NEvent % 1000000 == 0 && TimePrint == 0) {
           cout << "Event Number = " << NEvent << endl;
@@ -453,22 +452,23 @@ void TestDataReader::run(ProcessingContext& pc)
 
   LOG(debug) << "mIndexPush After = " << mIndexPush;
 
-  /*
-			   LOG(debug) << "Before:  " << "mIndexPush = " << mIndexPush << "     mDigits.size() = " <<  mDigits.size(); 
-			   while(mIndexPush < mDigits.size()){
-			//	LOG(debug) << "mDigits.size() = " << mDigits.size();
-			pc.outputs().snapshot(Output{ "ITS", "DIGITS", 0, Lifetime::Timeframe }, mDigits[mIndexPush++]);
-			if(mIndexPush%100000==0) 	LOG(debug) << "mIndexPush = " << mIndexPush << "    Chip ID Pushing " << mDigits[mIndexPush].getChipIndex();
-			}
-			//pc.services().get<ControlService>().readyToQuit(QuitRequest::All);
-			LOG(debug) << "After:  " << "mIndexPush = " << mIndexPush << "     mDigits.size() = " <<  mDigits.size(); 
-			*/
+  // LOG(debug) << "Before:  "
+  //            << "mIndexPush = " << mIndexPush << "     mDigits.size() = " << mDigits.size();
+  // while (mIndexPush < mDigits.size()) {
+  // LOG(debug) << "mDigits.size() = " << mDigits.size();
+  //   pc.outputs().snapshot(Output{"ITS", "DIGITS", 0, Lifetime::Timeframe}, mDigits[mIndexPush++]);
+  //   if (mIndexPush % 100000 == 0)
+  //     LOG(debug) << "mIndexPush = " << mIndexPush << "    Chip ID Pushing " << mDigits[mIndexPush].getChipIndex();
+  // }
+  // // pc.services().get<ControlService>().readyToQuit(QuitRequest::All);
+  // LOG(debug) << "After:  "
+  //            << "mIndexPush = " << mIndexPush << "     mDigits.size() = " << mDigits.size();
 
   mFolderNames.clear();
-  //	mFileNames.clear();
+  // mFileNames.clear();
   NewNextFold.clear();
   mFolderNames = mNowFolderNames;
-  //	mFileNames = mNowFileNames;
+  // mFileNames = mNowFileNames;
 
   mNowFolderNames.clear();
   mNowFileNames.clear();
@@ -479,7 +479,7 @@ void TestDataReader::run(ProcessingContext& pc)
 
   cout << "Resetting Pushing Things" << endl;
 
-  //Resetting a New File //
+  // Resetting a New File //
   if (mIndexPush > mDigits.size() - 5) {
     mDigits.clear();
     mIndexPush = 0;
@@ -491,9 +491,9 @@ void TestDataReader::run(ProcessingContext& pc)
     mErrorsVec.clear();
   }
 
-  //if(mNewFileInjAction == 1){
-  //		mNewFileInjAction = 0;
-  //	}
+  // if(mNewFileInjAction == 1){
+  // mNewFileInjAction = 0;
+  // }
 
   cout << "Start Sleeping" << endl;
   cout << " " << endl;
@@ -519,12 +519,12 @@ std::vector<string> TestDataReader::GetFName(std::string folder)
   strcpy(cstr, folder.c_str());
   dirp = opendir(cstr);
   std::vector<string> names;
-  //string search_path = folder + "/*";
+  // string search_path = folder + "/*";
   if (dirp) {
     struct dirent* directory;
     while ((directory = readdir(dirp)) != nullptr) {
 
-      //printf("%s\n", directory->d_name);
+      // printf("%s\n", directory->d_name);
 
       if (!(!strcmp(directory->d_name, ".") || !strcmp(directory->d_name, ".."))) {
         names.push_back(folder + "/" + directory->d_name);
@@ -551,18 +551,18 @@ DataProcessorSpec getTestDataReaderSpec()
       OutputSpec{"ITS", "File", 0, Lifetime::Timeframe},
       OutputSpec{"ITS", "Finish", 0, Lifetime::Timeframe},
       /*
-						   OutputSpec{ "TST", "Error0", 0, Lifetime::Timeframe },
-						   OutputSpec{ "TST", "Error1", 0, Lifetime::Timeframe },
-						   OutputSpec{ "TST", "Error2", 0, Lifetime::Timeframe },
-						   OutputSpec{ "TST", "Error3", 0, Lifetime::Timeframe },
-						   OutputSpec{ "TST", "Error4", 0, Lifetime::Timeframe },
-						   OutputSpec{ "TST", "Error5", 0, Lifetime::Timeframe },
-						   OutputSpec{ "TST", "Error6", 0, Lifetime::Timeframe },
-						   OutputSpec{ "TST", "Error7", 0, Lifetime::Timeframe },
-						   OutputSpec{ "TST", "Error8", 0, Lifetime::Timeframe },
-						   OutputSpec{ "TST", "Error9", 0, Lifetime::Timeframe },
-						   */
-      //		OutputSpec{ "TST", "TEST3", 0, Lifetime::Timeframe },
+               OutputSpec{ "TST", "Error0", 0, Lifetime::Timeframe },
+               OutputSpec{ "TST", "Error1", 0, Lifetime::Timeframe },
+               OutputSpec{ "TST", "Error2", 0, Lifetime::Timeframe },
+               OutputSpec{ "TST", "Error3", 0, Lifetime::Timeframe },
+               OutputSpec{ "TST", "Error4", 0, Lifetime::Timeframe },
+               OutputSpec{ "TST", "Error5", 0, Lifetime::Timeframe },
+               OutputSpec{ "TST", "Error6", 0, Lifetime::Timeframe },
+               OutputSpec{ "TST", "Error7", 0, Lifetime::Timeframe },
+               OutputSpec{ "TST", "Error8", 0, Lifetime::Timeframe },
+               OutputSpec{ "TST", "Error9", 0, Lifetime::Timeframe },
+               */
+      // OutputSpec{ "TST", "TEST3", 0, Lifetime::Timeframe },
     },
     AlgorithmSpec{adaptFromTask<TestDataReader>()},
   };

commit 07fcc505af6cdc6067862ac30f59a0b727859350

[O2-2679] Fix bug in TPCITS Afterburner
Bug was introduced in #8908 by me.
It affects also: https://alice.its.cern.ch/jira/browse/O2-2679

@shahor02

Detectors/GlobalTracking/src/MatchTPCITS.cxx
@@ -1823,6 +1823,11 @@ int MatchTPCITS::followABSeed(const o2::track::TrackParCov& seed, const ITSChipC
     // coordinates xCross,yCross,zCross for this central chipIDguess, although we are going to check also neighbours
     float t = 1e9, xCross, yCross;
     const auto& chipC = lad.chips[chipIDguess];
+    if (mFieldON) {
+      chipC.xyEdges.circleCrossParam(trcCircle, t);
+    } else {
+      chipC.xyEdges.lineCrossParam(trcLinPar, t);
+    }
     chipC.xyEdges.eval(t, xCross, yCross);
     float dx = xCross - xCurr, dy = yCross - yCurr, dst2 = dx * dx + dy * dy, dst = sqrtf(dst2);
     // Z-step sign depends on radius decreasing or increasing during the propagation