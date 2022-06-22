4e5506649ea3a7787cd2a8f5cf7be8ae2ea60e27
Pietro Cortese <pietro.cortese@cern.ch>
2022-06-22 17:46:15 +0200

    Fix whitespace

--- Detectors/ZDC/macro/CreateBaselineCalib.C
+++ Detectors/ZDC/macro/CreateBaselineCalib.C
@@ -33,36 +33,36 @@ void CreateBaselineCalib(long tmin = 0, long tmax = -1, std::string ccdbHost = "
   // Default object has not valid data = -std::numeric_limits<float>::infinity()
   // that makes sure the pedestal are not used
 
-//   conf.setCalib(IdZNAC, -std::numeric_limits<float>::infinity());
-//   conf.setCalib(IdZNA1, -std::numeric_limits<float>::infinity());
-//   conf.setCalib(IdZNA2, -std::numeric_limits<float>::infinity());
-//   conf.setCalib(IdZNA3, -std::numeric_limits<float>::infinity());
-//   conf.setCalib(IdZNA4, -std::numeric_limits<float>::infinity());
-//   conf.setCalib(IdZNASum, -std::numeric_limits<float>::infinity());
-// 
-//   conf.setCalib(IdZPAC, -std::numeric_limits<float>::infinity());
-//   conf.setCalib(IdZPA1, -std::numeric_limits<float>::infinity());
-//   conf.setCalib(IdZPA2, -std::numeric_limits<float>::infinity());
-//   conf.setCalib(IdZPA3, -std::numeric_limits<float>::infinity());
-//   conf.setCalib(IdZPA4, -std::numeric_limits<float>::infinity());
-//   conf.setCalib(IdZPASum, -std::numeric_limits<float>::infinity());
-// 
-//   conf.setCalib(IdZEM1, -std::numeric_limits<float>::infinity());
-//   conf.setCalib(IdZEM2, -std::numeric_limits<float>::infinity());
-// 
-//   conf.setCalib(IdZNCC, -std::numeric_limits<float>::infinity());
-//   conf.setCalib(IdZNC1, -std::numeric_limits<float>::infinity());
-//   conf.setCalib(IdZNC2, -std::numeric_limits<float>::infinity());
-//   conf.setCalib(IdZNC3, -std::numeric_limits<float>::infinity());
-//   conf.setCalib(IdZNC4, -std::numeric_limits<float>::infinity());
-//   conf.setCalib(IdZNCSum, -std::numeric_limits<float>::infinity());
-// 
-//   conf.setCalib(IdZPCC, -std::numeric_limits<float>::infinity());
-//   conf.setCalib(IdZPC1, -std::numeric_limits<float>::infinity());
-//   conf.setCalib(IdZPC2, -std::numeric_limits<float>::infinity());
-//   conf.setCalib(IdZPC3, -std::numeric_limits<float>::infinity());
-//   conf.setCalib(IdZPC4, -std::numeric_limits<float>::infinity());
-//   conf.setCalib(IdZPCSum, -std::numeric_limits<float>::infinity());
+  // conf.setCalib(IdZNAC, -std::numeric_limits<float>::infinity());
+  // conf.setCalib(IdZNA1, -std::numeric_limits<float>::infinity());
+  // conf.setCalib(IdZNA2, -std::numeric_limits<float>::infinity());
+  // conf.setCalib(IdZNA3, -std::numeric_limits<float>::infinity());
+  // conf.setCalib(IdZNA4, -std::numeric_limits<float>::infinity());
+  // conf.setCalib(IdZNASum, -std::numeric_limits<float>::infinity());
+  //
+  // conf.setCalib(IdZPAC, -std::numeric_limits<float>::infinity());
+  // conf.setCalib(IdZPA1, -std::numeric_limits<float>::infinity());
+  // conf.setCalib(IdZPA2, -std::numeric_limits<float>::infinity());
+  // conf.setCalib(IdZPA3, -std::numeric_limits<float>::infinity());
+  // conf.setCalib(IdZPA4, -std::numeric_limits<float>::infinity());
+  // conf.setCalib(IdZPASum, -std::numeric_limits<float>::infinity());
+  //
+  // conf.setCalib(IdZEM1, -std::numeric_limits<float>::infinity());
+  // conf.setCalib(IdZEM2, -std::numeric_limits<float>::infinity());
+  //
+  // conf.setCalib(IdZNCC, -std::numeric_limits<float>::infinity());
+  // conf.setCalib(IdZNC1, -std::numeric_limits<float>::infinity());
+  // conf.setCalib(IdZNC2, -std::numeric_limits<float>::infinity());
+  // conf.setCalib(IdZNC3, -std::numeric_limits<float>::infinity());
+  // conf.setCalib(IdZNC4, -std::numeric_limits<float>::infinity());
+  // conf.setCalib(IdZNCSum, -std::numeric_limits<float>::infinity());
+  //
+  // conf.setCalib(IdZPCC, -std::numeric_limits<float>::infinity());
+  // conf.setCalib(IdZPC1, -std::numeric_limits<float>::infinity());
+  // conf.setCalib(IdZPC2, -std::numeric_limits<float>::infinity());
+  // conf.setCalib(IdZPC3, -std::numeric_limits<float>::infinity());
+  // conf.setCalib(IdZPC4, -std::numeric_limits<float>::infinity());
+  // conf.setCalib(IdZPCSum, -std::numeric_limits<float>::infinity());
 
   conf.print();
 
