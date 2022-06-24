918c56a3f7041104d4ecd3d26e1755e06923cbef
Pietro Cortese <pietro.cortese@cern.ch>
2022-06-24 14:23:07 +0200

    clang-format

--- Detectors/ZDC/macro/InspectCCDBFile.C
+++ Detectors/ZDC/macro/InspectCCDBFile.C
@@ -34,91 +34,71 @@ void InspectCCDBFile() {
   TIter nextkey(gDirectory->GetListOfKeys());
   TKey *key;
   while ((key = (TKey *)nextkey())) {
+    // clang-format off
     TString cn = key->GetClassName();
     if (cn.EqualTo("vector<Long64_t>")) {
-      vector<Long64_t> *ob = (vector<Long64_t> *)key->ReadObj();
-      printf("%s %s %d %s\n", "OBJ", key->GetName(), key->GetCycle(),
-             key->GetTitle());
-      for (auto val : *ob) {
+      vector<Long64_t>* ob = (vector<Long64_t>*)key->ReadObj();
+      printf("%s %s %d %s\n", "OBJ", key->GetName(), key->GetCycle(), key->GetTitle());
+      for (auto val : *ob){
         printf("%lld\n", val);
       }
-      // ob->print();
+      //ob->print();
     } else if (cn.EqualTo("o2::zdc::ModuleConfig")) {
-      o2::zdc::ModuleConfig *ob = (o2::zdc::ModuleConfig *)key->ReadObj();
-      printf("%s %s %d %s @ %s\n", "OBJ", key->GetName(), key->GetCycle(),
-             key->GetTitle(), o2::zdc::CCDBPathConfigModule.data());
+      o2::zdc::ModuleConfig* ob = (o2::zdc::ModuleConfig*)key->ReadObj();
+      printf("%s %s %d %s @ %s\n", "OBJ", key->GetName(), key->GetCycle(), key->GetTitle(), o2::zdc::CCDBPathConfigModule.data());
       ob->print();
     } else if (cn.EqualTo("o2::zdc::SimCondition")) {
       // o2::zdc::SimCondition *ob=(o2::zdc::SimCondition *)key->ReadObj();
-      o2::zdc::SimCondition *ob =
-          (o2::zdc::SimCondition *)gFile->GetObjectUnchecked("ccdb_object");
-      printf("%s %s %d %s @ %s\n", "OBJ", key->GetName(), key->GetCycle(),
-             key->GetTitle(), o2::zdc::CCDBPathConfigSim.data());
+      o2::zdc::SimCondition* ob = (o2::zdc::SimCondition*)gFile->GetObjectUnchecked("ccdb_object");
+      printf("%s %s %d %s @ %s\n", "OBJ", key->GetName(), key->GetCycle(), key->GetTitle(), o2::zdc::CCDBPathConfigSim.data());
       ob->print();
     } else if (cn.EqualTo("o2::zdc::RecoConfigZDC")) {
-      o2::zdc::RecoConfigZDC *ob = (o2::zdc::RecoConfigZDC *)key->ReadObj();
-      printf("%s %s %d %s @ %s\n", "OBJ", key->GetName(), key->GetCycle(),
-             key->GetTitle(), o2::zdc::CCDBPathRecoConfigZDC.data());
+      o2::zdc::RecoConfigZDC* ob = (o2::zdc::RecoConfigZDC*)key->ReadObj();
+      printf("%s %s %d %s @ %s\n", "OBJ", key->GetName(), key->GetCycle(), key->GetTitle(), o2::zdc::CCDBPathRecoConfigZDC.data());
       ob->print();
     } else if (cn.EqualTo("o2::zdc::ZDCTDCCorr")) {
-      o2::zdc::ZDCTDCCorr *ob = (o2::zdc::ZDCTDCCorr *)key->ReadObj();
-      printf("%s %s %d %s @ %s\n", "OBJ", key->GetName(), key->GetCycle(),
-             key->GetTitle(), o2::zdc::CCDBPathTDCCorr.data());
+      o2::zdc::ZDCTDCCorr* ob = (o2::zdc::ZDCTDCCorr*)key->ReadObj();
+      printf("%s %s %d %s @ %s\n", "OBJ", key->GetName(), key->GetCycle(), key->GetTitle(), o2::zdc::CCDBPathTDCCorr.data());
       ob->print();
     } else if (cn.EqualTo("o2::zdc::ZDCTDCParam")) {
-      o2::zdc::ZDCTDCParam *ob = (o2::zdc::ZDCTDCParam *)key->ReadObj();
-      printf("%s %s %d %s @ %s\n", "OBJ", key->GetName(), key->GetCycle(),
-             key->GetTitle(), o2::zdc::CCDBPathTDCCalib.data());
+      o2::zdc::ZDCTDCParam* ob = (o2::zdc::ZDCTDCParam*)key->ReadObj();
+      printf("%s %s %d %s @ %s\n", "OBJ", key->GetName(), key->GetCycle(), key->GetTitle(), o2::zdc::CCDBPathTDCCalib.data());
       ob->print();
     } else if (cn.EqualTo("o2::zdc::ZDCEnergyParam")) {
-      o2::zdc::ZDCEnergyParam *ob = (o2::zdc::ZDCEnergyParam *)key->ReadObj();
-      printf("%s %s %d %s @ %s\n", "OBJ", key->GetName(), key->GetCycle(),
-             key->GetTitle(), o2::zdc::CCDBPathEnergyCalib.data());
+      o2::zdc::ZDCEnergyParam* ob = (o2::zdc::ZDCEnergyParam*)key->ReadObj();
+      printf("%s %s %d %s @ %s\n", "OBJ", key->GetName(), key->GetCycle(), key->GetTitle(), o2::zdc::CCDBPathEnergyCalib.data());
       ob->print();
     } else if (cn.EqualTo("o2::zdc::ZDCTowerParam")) {
-      o2::zdc::ZDCTowerParam *ob = (o2::zdc::ZDCTowerParam *)key->ReadObj();
-      printf("%s %s %d %s @ %s\n", "OBJ", key->GetName(), key->GetCycle(),
-             key->GetTitle(), o2::zdc::CCDBPathTowerCalib.data());
+      o2::zdc::ZDCTowerParam* ob = (o2::zdc::ZDCTowerParam*)key->ReadObj();
+      printf("%s %s %d %s @ %s\n", "OBJ", key->GetName(), key->GetCycle(), key->GetTitle(), o2::zdc::CCDBPathTowerCalib.data());
       ob->print();
     } else if (cn.EqualTo("o2::zdc::InterCalibConfig")) {
-      o2::zdc::InterCalibConfig *ob =
-          (o2::zdc::InterCalibConfig *)key->ReadObj();
-      printf("%s %s %d %s @ %s\n", "OBJ", key->GetName(), key->GetCycle(),
-             key->GetTitle(), o2::zdc::CCDBPathInterCalibConfig.data());
+      o2::zdc::InterCalibConfig* ob = (o2::zdc::InterCalibConfig*)key->ReadObj();
+      printf("%s %s %d %s @ %s\n", "OBJ", key->GetName(), key->GetCycle(), key->GetTitle(), o2::zdc::CCDBPathInterCalibConfig.data());
       ob->print();
     } else if (cn.EqualTo("o2::zdc::BaselineCalibConfig")) {
-      o2::zdc::BaselineCalibConfig *ob =
-          (o2::zdc::BaselineCalibConfig *)key->ReadObj();
-      printf("%s %s %d %s @ %s\n", "OBJ", key->GetName(), key->GetCycle(),
-             key->GetTitle(), o2::zdc::CCDBPathBaselineCalibConfig.data());
+      o2::zdc::BaselineCalibConfig* ob = (o2::zdc::BaselineCalibConfig*)key->ReadObj();
+      printf("%s %s %d %s @ %s\n", "OBJ", key->GetName(), key->GetCycle(), key->GetTitle(), o2::zdc::CCDBPathBaselineCalibConfig.data());
       ob->print();
     } else if (cn.EqualTo("o2::zdc::BaselineParam")) {
-      o2::zdc::BaselineParam *ob = (o2::zdc::BaselineParam *)key->ReadObj();
-      printf("%s %s %d %s @ %s\n", "OBJ", key->GetName(), key->GetCycle(),
-             key->GetTitle(), o2::zdc::CCDBPathBaselineCalib.data());
+      o2::zdc::BaselineParam* ob = (o2::zdc::BaselineParam*)key->ReadObj();
+      printf("%s %s %d %s @ %s\n", "OBJ", key->GetName(), key->GetCycle(), key->GetTitle(), o2::zdc::CCDBPathBaselineCalib.data());
       ob->print();
     } else if (cn.EqualTo("o2::zdc::WaveformCalibConfig")) {
-      o2::zdc::WaveformCalibConfig *ob =
-          (o2::zdc::WaveformCalibConfig *)key->ReadObj();
-      printf("%s %s %d %s @ %s\n", "OBJ", key->GetName(), key->GetCycle(),
-             key->GetTitle(), o2::zdc::CCDBPathWaveformCalibConfig.data());
+      o2::zdc::WaveformCalibConfig* ob = (o2::zdc::WaveformCalibConfig*)key->ReadObj();
+      printf("%s %s %d %s @ %s\n", "OBJ", key->GetName(), key->GetCycle(), key->GetTitle(), o2::zdc::CCDBPathWaveformCalibConfig.data());
       ob->print();
     } else if (cn.EqualTo("o2::zdc::WaveformCalibParam")) {
-      o2::zdc::WaveformCalibParam *ob =
-          (o2::zdc::WaveformCalibParam *)gFile->GetObjectUnchecked(
-              "ccdb_object");
-      // o2::zdc::WaveformCalibParam *ob=(o2::zdc::WaveformCalibParam
-      // *)key->ReadObj();
-      printf("%s %s %d %s @ %s\n", "OBJ", key->GetName(), key->GetCycle(),
-             key->GetTitle(), o2::zdc::CCDBPathWaveformCalib.data());
+      o2::zdc::WaveformCalibParam* ob = (o2::zdc::WaveformCalibParam*)gFile->GetObjectUnchecked("ccdb_object");
+      // o2::zdc::WaveformCalibParam *ob=(o2::zdc::WaveformCalibParam *)key->ReadObj();
+      printf("%s %s %d %s @ %s\n", "OBJ", key->GetName(), key->GetCycle(), key->GetTitle(), o2::zdc::CCDBPathWaveformCalib.data());
       ob->print();
       ob->saveDebugHistos("InspectCCDBFile_WaveformCalibParam.root");
     } else {
-      printf("%s %s %d %s\n", key->GetClassName(), key->GetName(),
-             key->GetCycle(), key->GetTitle());
+      printf("%s %s %d %s\n", key->GetClassName(), key->GetName(), key->GetCycle(), key->GetTitle());
     }
+    // clang-format on
   }
-
   //   TObject *ob = (TObject*)gDirectory->Get("ccdb_object");
   //   if(ob == nullptr){
   //     printf("Object not found\n");
