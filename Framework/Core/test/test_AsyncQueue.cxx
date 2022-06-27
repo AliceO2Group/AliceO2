748f30c6d65fbcd428eabf2e8df3433ae8c9b79f
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2022-06-27 11:57:51 +0200

    DPL: improve async queue to support oldest possible timeframe
    Do not schedule tasks before their associated oldest possible
    timeframe is reached.

--- Framework/Core/test/test_AsyncQueue.cxx
+++ Framework/Core/test/test_AsyncQueue.cxx
@@ -20,28 +20,104 @@
 /// in a given run.
 BOOST_AUTO_TEST_CASE(TestDebouncing)
 {
-  o2::framework::AsyncQueue queue;
-  auto taskId = o2::framework::AsyncQueueHelpers::create(queue, {.name = "test", .score = 10});
+  using namespace o2::framework;
+  AsyncQueue queue;
+  auto taskId = AsyncQueueHelpers::create(queue, {.name = "test", .score = 10});
   // Push two tasks on the queue with the same id
   auto count = 0;
-  o2::framework::AsyncQueueHelpers::post(
-    queue, taskId, [&count]() { count += 1; }, 10);
-  o2::framework::AsyncQueueHelpers::post(
-    queue, taskId, [&count]() { count += 2; }, 20);
-  o2::framework::AsyncQueueHelpers::run(queue);
+  AsyncQueueHelpers::post(
+    queue, taskId, [&count]() { count += 1; }, TimesliceId{0}, 10);
+  AsyncQueueHelpers::post(
+    queue, taskId, [&count]() { count += 2; }, TimesliceId{1}, 20);
+  AsyncQueueHelpers::run(queue, TimesliceId{2});
   BOOST_CHECK_EQUAL(count, 2);
 }
 
 // Test task oridering. The tasks with the highest priority should be executed first.
 BOOST_AUTO_TEST_CASE(TestPriority)
 {
-  o2::framework::AsyncQueue queue;
-  auto taskId1 = o2::framework::AsyncQueueHelpers::create(queue, {.name = "test1", .score = 10});
-  auto taskId2 = o2::framework::AsyncQueueHelpers::create(queue, {.name = "test2", .score = 20});
+  using namespace o2::framework;
+  AsyncQueue queue;
+  auto taskId1 = AsyncQueueHelpers::create(queue, {.name = "test1", .score = 10});
+  auto taskId2 = AsyncQueueHelpers::create(queue, {.name = "test2", .score = 20});
   // Push two tasks on the queue with the same id
   auto count = 0;
-  o2::framework::AsyncQueueHelpers::post(queue, taskId1, [&count]() { count += 10; });
-  o2::framework::AsyncQueueHelpers::post(queue, taskId2, [&count]() { count /= 10; });
-  o2::framework::AsyncQueueHelpers::run(queue);
+  AsyncQueueHelpers::post(
+    queue, taskId1, [&count]() { count += 10; }, TimesliceId{0});
+  AsyncQueueHelpers::post(
+    queue, taskId2, [&count]() { count /= 10; }, TimesliceId{0});
+  AsyncQueueHelpers::run(queue, TimesliceId{2});
   BOOST_CHECK_EQUAL(count, 10);
+}
+
+// Make sure we execute tasks only up to the timeslice provided to run.
+BOOST_AUTO_TEST_CASE(TestOldestTimeslice)
+{
+  using namespace o2::framework;
+  AsyncQueue queue;
+  auto taskId1 = AsyncQueueHelpers::create(queue, {.name = "test1", .score = 10});
+  auto taskId2 = AsyncQueueHelpers::create(queue, {.name = "test2", .score = 20});
+  // Push two tasks on the queue with the same id
+  auto count = 0;
+  AsyncQueueHelpers::post(
+    queue, taskId1, [&count]() { count += 10; }, TimesliceId{1});
+  AsyncQueueHelpers::post(
+    queue, taskId2, [&count]() { count += 20; }, TimesliceId{0});
+  AsyncQueueHelpers::run(queue, TimesliceId{1});
+  BOOST_CHECK_EQUAL(count, 20);
+  AsyncQueueHelpers::run(queue, TimesliceId{1});
+  BOOST_CHECK_EQUAL(count, 20);
+  AsyncQueueHelpers::run(queue, TimesliceId{2});
+  BOOST_CHECK_EQUAL(count, 30);
+}
+
+// Make sure we execute tasks only up to the timeslice provided to run.
+BOOST_AUTO_TEST_CASE(TestOldestTimesliceWithBounce)
+{
+  using namespace o2::framework;
+  AsyncQueue queue;
+  auto taskId1 = AsyncQueueHelpers::create(queue, {.name = "test1", .score = 10});
+  auto taskId2 = AsyncQueueHelpers::create(queue, {.name = "test2", .score = 20});
+  // Push two tasks on the queue with the same id
+  auto count = 0;
+  AsyncQueueHelpers::post(
+    queue, taskId1, [&count]() { count += 10; }, TimesliceId{1});
+  AsyncQueueHelpers::post(
+    queue, taskId2, [&count]() { count += 20; }, TimesliceId{0}, 10);
+  AsyncQueueHelpers::post(
+    queue, taskId2, [&count]() { count += 30; }, TimesliceId{0}, 20);
+  AsyncQueueHelpers::run(queue, TimesliceId{0});
+  BOOST_CHECK_EQUAL(count, 0);
+  BOOST_CHECK_EQUAL(queue.tasks.size(), 3);
+  AsyncQueueHelpers::run(queue, TimesliceId{1});
+  BOOST_CHECK_EQUAL(count, 30);
+  BOOST_CHECK_EQUAL(queue.tasks.size(), 1);
+  AsyncQueueHelpers::run(queue, TimesliceId{2});
+  BOOST_CHECK_EQUAL(count, 40);
+  BOOST_CHECK_EQUAL(queue.tasks.size(), 0);
+}
+
+// Make sure we execute tasks only up to the timeslice provided to run.
+BOOST_AUTO_TEST_CASE(TestOldestTimeslicePerTimeslice)
+{
+  using namespace o2::framework;
+  AsyncQueue queue;
+  auto taskId1 = AsyncQueueHelpers::create(queue, {.name = "test1", .score = 10});
+  // Push two tasks on the queue with the same id
+  auto count = 0;
+  AsyncQueueHelpers::post(
+    queue, taskId1, [&count]() { count += 10; }, TimesliceId{0});
+  BOOST_CHECK_EQUAL(queue.tasks.size(), 1);
+  AsyncQueueHelpers::run(queue, TimesliceId{0});
+  BOOST_CHECK_EQUAL(queue.tasks.size(), 1);
+  BOOST_CHECK_EQUAL(count, 0);
+  AsyncQueueHelpers::post(
+    queue, taskId1, [&count]() { count += 20; }, TimesliceId{1});
+  BOOST_CHECK_EQUAL(queue.tasks.size(), 2);
+  AsyncQueueHelpers::run(queue, TimesliceId{1});
+  BOOST_CHECK_EQUAL(queue.tasks.size(), 1);
+  BOOST_CHECK_EQUAL(count, 10);
+  AsyncQueueHelpers::run(queue, TimesliceId{2});
+  BOOST_CHECK_EQUAL(queue.tasks.size(), 0);
+  BOOST_CHECK_EQUAL(count, 30);
 }
