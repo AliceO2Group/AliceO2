763b89c2ff7572eb878eebb3f5af1ea5a50b462f
Giacomo Volpe <gvolpe@pcgvolpe.ba.infn.it>
2022-07-07 12:00:00 +0200

    Copyright headers

--- Detectors/HMPID/calibration/include/HMPIDCalibration/HMPIDDCSProcessor.h
+++ Detectors/HMPID/calibration/include/HMPIDCalibration/HMPIDDCSProcessor.h
@@ -1,6 +1,6 @@
 // Copyright 2019-2020 CERN and copyright holders of ALICE O2.
-// See https://alice-o2.web.cern.ch/copyright for details of the copyright
-// holders. All rights not expressly granted are reserved.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
 //
 // This software is distributed under the terms of the GNU General Public
 // License v3 (GPL Version 3), copied verbatim in the file "COPYING".
--- Detectors/HMPID/calibration/testWorkflow/HMPIDDCSDataProcessorSpec.h
+++ Detectors/HMPID/calibration/testWorkflow/HMPIDDCSDataProcessorSpec.h
@@ -1,6 +1,6 @@
 // Copyright 2019-2020 CERN and copyright holders of ALICE O2.
-// See https://alice-o2.web.cern.ch/copyright for details of the copyright
-// holders. All rights not expressly granted are reserved.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
 //
 // This software is distributed under the terms of the GNU General Public
 // License v3 (GPL Version 3), copied verbatim in the file "COPYING".
@@ -8,9 +8,6 @@
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
-
-/// @file   DCSHMPIDDataProcessorSpec.h
-/// @brief  HMPID Processor for DCS Data Points
 
 #ifndef O2_HMPID_DATAPROCESSOR_H
 #define O2_HMPID_DATAPROCESSOR_H
