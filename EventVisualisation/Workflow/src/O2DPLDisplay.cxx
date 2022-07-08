51b52ac976a02ddf5f3f3e4914acad0a0a3874e8
Julian Myrcha <jmyrcha@cern.ch>
2022-07-08 13:24:37 +0200

    o2-eve-export-workflow: change defaults for json file location and number of stored files

--- EventVisualisation/Workflow/src/O2DPLDisplay.cxx
+++ EventVisualisation/Workflow/src/O2DPLDisplay.cxx
@@ -50,10 +50,10 @@ void customize(std::vector<o2::framework::CallbacksPolicy>& policies)
 void customize(std::vector<ConfigParamSpec>& workflowOptions)
 {
   std::vector<o2::framework::ConfigParamSpec> options{
-    {"jsons-folder", VariantType::String, "jsons", {"name of the folder to store json files"}},
+    {"jsons-folder", VariantType::String, "/scratch/services/ed/jsons", {"name of the folder to store json files"}},
     {"eve-hostname", VariantType::String, "", {"name of the host allowed to produce files (empty means no limit)"}},
     {"eve-dds-collection-index", VariantType::Int, -1, {"number of dpl collection allowed to produce files (-1 means no limit)"}},
-    {"number-of_files", VariantType::Int, 300, {"maximum number of json files in folder"}},
+    {"number-of_files", VariantType::Int, 150, {"maximum number of json files in folder"}},
     {"number-of_tracks", VariantType::Int, -1, {"maximum number of track stored in json file (-1 means no limit)"}},
     {"time-interval", VariantType::Int, 5000, {"time interval in milliseconds between stored files"}},
     {"disable-mc", VariantType::Bool, false, {"disable visualization of MC data"}},
