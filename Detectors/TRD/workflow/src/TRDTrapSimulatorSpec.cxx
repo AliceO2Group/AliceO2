20c560ddaf6f3bd63762fff188b6c8bdac9f3a4c
Ole Schmidt <ole.schmidt@cern.ch>
2022-07-10 18:20:09 +0200

    TRD TRAP report rejected digits

--- Detectors/TRD/workflow/src/TRDTrapSimulatorSpec.cxx
+++ Detectors/TRD/workflow/src/TRDTrapSimulatorSpec.cxx
@@ -251,6 +251,7 @@ void TRDDPLTrapSimulatorTask::run(o2::framework::ProcessingContext& pc)
   auto parallelTime = std::chrono::high_resolution_clock::now() - timeParallelStart;
 
   // accumulate results and add MC labels
+  int nDigitsRejected = 0;
   for (size_t iTrig = 0; iTrig < triggerRecords.size(); ++iTrig) {
     if (mUseMC) {
       int currDigitIndex = 0; // counter for all digits which are associated to tracklets
@@ -292,6 +293,7 @@ void TRDDPLTrapSimulatorTask::run(o2::framework::ProcessingContext& pc)
       triggerRecords[iTrig].setDigitRange(digitsOut.size() - triggerRecords[iTrig].getNumberOfDigits(), triggerRecords[iTrig].getNumberOfDigits());
     } else {
       // digits are not kept, we just need to update the trigger record
+      nDigitsRejected += triggerRecords[iTrig].getNumberOfDigits();
       triggerRecords[iTrig].setDigitRange(digitsOut.size(), 0);
     }
   }
@@ -299,6 +301,7 @@ void TRDDPLTrapSimulatorTask::run(o2::framework::ProcessingContext& pc)
   auto processingTime = std::chrono::high_resolution_clock::now() - timeProcessingStart;
 
   LOG(info) << "Trap simulator found " << tracklets.size() << " tracklets from " << digits.size() << " Digits.";
+  LOG(info) << "In total " << nDigitsRejected << " digits were rejected due to digit downscaling factor of " << mDigitDownscaling;
   if (mUseMC) {
     LOG(info) << "In total " << lblTracklets.getNElements() << " MC labels are associated to the " << lblTracklets.getIndexedSize() << " tracklets";
   }
