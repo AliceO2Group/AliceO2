d0f7cb87b0ce476d8054fd047ce59c07f60779b4
Alexander Bigot <alexander@localhost.com>
2022-07-13 15:24:58 +0200

    Clang formatting

--- Detectors/ITSMFT/common/simulation/src/AlpideSimResponse.cxx
+++ Detectors/ITSMFT/common/simulation/src/AlpideSimResponse.cxx
@@ -37,7 +37,7 @@ void AlpideSimResponse::initData(int tableNumber)
    */
   if (tableNumber == 0) // 0V back bias
   {
-    const std::string newDataPath = mDataPath + "Vbb-0.0V"; 
+    const std::string newDataPath = mDataPath + "Vbb-0.0V";
     setDataPath(newDataPath);                               // setting the new data path
   } else if (tableNumber == 1)                              // -3V back bias
   {
