ffe5b8fa8e6a8784a317279ac9a57e458af840a1
Andreas Molander <andreas.molander@cern.ch>
2022-07-19 16:36:05 +0200

    FIT: Add DCS configuration workflows
    Add workflows for processing bad channel maps for FT0, FV0 and FDD

---
+++ Detectors/FIT/FV0/dcsmonitoring/workflow/FV0DCSConfigProcessorSpec.h
@@ -0,0 +1,54 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file FV0DCSConfigProcessorSpec.cxx
+/// \brief FV0 processor spec for DCS configurations
+///
+/// \author Andreas Molander <andreas.molander@cern.ch>, University of Jyvaskyla, Finland
+
+#ifndef O2_FV0_DCSCONFIGPROCESSOR_H
+#define O2_FV0_DCSCONFIGPROCESSOR_H
+
+#include "FITDCSMonitoring/FITDCSConfigProcessorSpec.h"
+#include "DetectorsCalibration/Utils.h"
+#include "Framework/WorkflowSpec.h"
+#include "Headers/DataHeader.h"
+
+#include <string>
+#include <vector>
+
+namespace o2
+{
+
+namespace framework
+{
+
+DataProcessorSpec getFV0DCSConfigProcessorSpec()
+{
+  o2::header::DataDescription ddBChM = "FV0_BCHM";
+  std::vector<OutputSpec> outputs;
+  outputs.emplace_back(ConcreteDataTypeMatcher{o2::calibration::Utils::gDataOriginCDBPayload, ddBChM}, Lifetime::Sporadic);
+  outputs.emplace_back(ConcreteDataTypeMatcher{o2::calibration::Utils::gDataOriginCDBWrapper, ddBChM}, Lifetime::Sporadic);
+
+  return DataProcessorSpec{
+    "fv0-dcs-config-processor",
+    Inputs{{"inputConfig", o2::header::gDataOriginFV0, "DCS_CONFIG_FILE", Lifetime::Timeframe},
+           {"inputConfigFileName", o2::header::gDataOriginFV0, "DCS_CONFIG_NAME", Lifetime::Timeframe}},
+    outputs,
+    AlgorithmSpec{adaptFromTask<o2::fit::FITDCSConfigProcessor>("FV0", ddBChM)},
+    Options{{"use-verbose-mode", VariantType::Bool, false, {"Use verbose mode"}},
+            {"filename-bchm", VariantType::String, "FV0-badchannels.txt", {"Bad channel map file name"}}}};
+}
+
+} // namespace framework
+} // namespace o2
+
+#endif // O2_FV0_DCSCONFIGPROCESSOR_H
