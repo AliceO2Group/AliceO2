a3a44b7b4c2c1bc2eeff3ab143523d7ffd66526f
lietava <roman.lietava@cern.ch>
2022-07-27 14:33:42 +0200

    fix: adding ctpreadout-create node

--- Detectors/AOD/src/AODProducerWorkflowSpec.cxx
+++ Detectors/AOD/src/AODProducerWorkflowSpec.cxx
@@ -2156,7 +2156,8 @@ DataProcessorSpec getAODProducerWorkflowSpec(GID::mask_t src, bool enableSV, boo
       ConfigParamSpec{"anchor-pass", VariantType::String, "", {"AnchorPassName"}},
       ConfigParamSpec{"anchor-prod", VariantType::String, "", {"AnchorProduction"}},
       ConfigParamSpec{"reco-pass", VariantType::String, "", {"RecoPassName"}},
-      ConfigParamSpec{"reco-mctracks-only", VariantType::Int, 0, {"Store only reconstructed MC tracks and their mothers/daughters. 0 -- off, != 0 -- on"}}}};
+      ConfigParamSpec{"reco-mctracks-only", VariantType::Int, 0, {"Store only reconstructed MC tracks and their mothers/daughters. 0 -- off, != 0 -- on"}},
+      ConfigParamSpec{"ctpreadout-create",VariantType::Int, 0,{"Create CTP digits from detector readout and CTP inputs. !=1 -- off, 1 -- on"}}}};
 }
 
 } // namespace o2::aodproducer
