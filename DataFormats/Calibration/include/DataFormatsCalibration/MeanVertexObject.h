0bad42ef38f95ece230e983a282807ea1abf4dac
Chiara Zampolli <Chiara.Zampolli@cern.ch>
2022-08-01 23:07:32 +0200

    Fix compilation and typo

--- DataFormats/Calibration/include/DataFormatsCalibration/MeanVertexObject.h
+++ DataFormats/Calibration/include/DataFormatsCalibration/MeanVertexObject.h
@@ -28,14 +28,14 @@ class MeanVertexObject : public VertexBase
  public:
   MeanVertexObject(float x, float y, float z, float sigmax, float sigmay, float sigmaz, float slopeX, float slopeY)
   {
-    math_utils::Point3D<float> p(x, y, z);
     gpu::gpustd::array<float, kNCov> cov;
     cov[CovElems::kCovXX] = sigmax;
     cov[CovElems::kCovYY] = sigmay;
     cov[CovElems::kCovZZ] = sigmaz;
-    VertexBase(p, cov);
+    setXYZ(x, y, z);
+    setCov(cov);
     mSlopeX = slopeX;
-    mSlopeX = slopeY;
+    mSlopeY = slopeY;
   }
   MeanVertexObject(std::array<float, 3> pos, std::array<float, 3> sigma, float slopeX, float slopeY)
   {
@@ -44,9 +44,10 @@ class MeanVertexObject : public VertexBase
     cov[CovElems::kCovXX] = sigma[0];
     cov[CovElems::kCovYY] = sigma[1];
     cov[CovElems::kCovZZ] = sigma[2];
-    VertexBase(p, cov);
+    setPos(p);
+    setCov(cov);
     mSlopeX = slopeX;
-    mSlopeX = slopeY;
+    mSlopeY = slopeY;
   }
   MeanVertexObject() = default;
   ~MeanVertexObject() = default;
@@ -55,33 +56,8 @@ class MeanVertexObject : public VertexBase
   MeanVertexObject& operator=(MeanVertexObject& other) = default;
   MeanVertexObject& operator=(MeanVertexObject&& other) = default;
 
-  void set(int icoord, float val)
-  {
-    if (icoord == 0) {
-      setX(val);
-    } else if (icoord == 1) {
-      setY(val);
-    } else if (icoord == 2) {
-      setZ(val);
-    } else {
-      LOG(fatal) << "Coordinate out of bound to set vtx " << icoord << ", should be in [0, 2]";
-    }
-  }
-  math_utils::Point3D<float>& getPos() { return getXYZ(); }
-  math_utils::Point3D<float> getPos() const { return getXYZ(); }
-
-  void setSigma(int icoord, float val)
-  {
-    if (icoord == 0) {
-      setSigmaX2(val);
-    } else if (icoord == 1) {
-      setSigmaY2(val);
-    } else if (icoord == 2) {
-      setSigmaZ2(val);
-    } else {
-      LOG(fatal) << "Coordinate out of bound to set sigma via MeanVtx " << icoord << ", should be in [0, 2]";
-    }
-  }
+  void set(int icoord, float val);
+  void setSigma(int icoord, float val);
   void setSigmaX(float val) { setSigmaX2(val); }
   void setSigmaY(float val) { setSigmaY2(val); }
   void setSigmaZ(float val) { setSigmaZ2(val); }
@@ -91,14 +67,15 @@ class MeanVertexObject : public VertexBase
     setSigmaY2(val[1]);
     setSigmaZ2(val[2]);
   }
+  void setSlopeX(float val) { mSlopeX = val; }
+  void setSlopeY(float val) { mSlopeY = val; }
 
+  math_utils::Point3D<float>& getPos() { return getXYZ(); }
+  math_utils::Point3D<float> getPos() const { return getXYZ(); }
   float getSigmaX() const { return getSigmaX2(); }
   float getSigmaY() const { return getSigmaY2(); }
   float getSigmaZ() const { return getSigmaZ2(); }
   const gpu::gpustd::array<float, kNCov>& getSigma() const { return getCov(); }
-
-  void setSlopeX(float val) { mSlopeX = val; }
-  void setSlopeY(float val) { mSlopeY = val; }
 
   float getSlopeX() const { return mSlopeX; }
   float getSlopeY() const { return mSlopeY; }
@@ -113,6 +90,7 @@ class MeanVertexObject : public VertexBase
   {
     VertexBase v = *this;
     v.setXYZ(getXAtZ(z), getYAtZ(z), z);
+    return v;
   }
 
   const VertexBase& getMeanVertex() const
