e392bfacacf0fdb3addd90c02f8ed9e7b8b52cfc
Joshua Koenig <joshua.konig@cern.ch>
2022-08-10 10:19:45 +0200

    [EMCAL-566] Read calibration params from ccdb
    - Added functionality to read calib params from ccdb in he calibratorSpec
    - Object is already uploaded to ccdb (O2-3116)
    - This will overwrite the default values in the CalibParams but can be
    overwritten if secified via the configurableParams
    - By default, the object is taken from the ccdb, one can switch it of by
    adding the workflow argument --no-loadCalibParamsFromCCDB
    
    addition

--- Detectors/EMCAL/calibration/testWorkflow/emc-channel-calib-workflow.cxx
+++ Detectors/EMCAL/calibration/testWorkflow/emc-channel-calib-workflow.cxx
@@ -36,7 +36,8 @@ using namespace o2::emcal;
 void customize(std::vector<ConfigParamSpec>& workflowOptions)
 {
   std::vector<ConfigParamSpec> options{
-    {"configKeyValues", VariantType::String, "", {"Semicolon separated key=value strings"}}};
+    {"configKeyValues", VariantType::String, "", {"Semicolon separated key=value strings"}},
+    {"no-loadCalibParamsFromCCDB", VariantType::Bool, false, {"if true, load the EMCalCalibParams from the ccdb. If false, use the default values given in the struct"}}};
 
   std::swap(workflowOptions, options);
 }
@@ -45,12 +46,15 @@ void customize(std::vector<ConfigParamSpec>& workflowOptions)
 
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
 {
-  WorkflowSpec specs;
-  specs.emplace_back(getEMCALChannelCalibDeviceSpec());
 
   o2::conf::ConfigurableParam::updateFromString(cfgc.options().get<std::string>("configKeyValues"));
+  bool loadCalibParamsFromCCDB = !cfgc.options().get<bool>("no-loadCalibParamsFromCCDB");
+
+  WorkflowSpec specs;
+  specs.emplace_back(getEMCALChannelCalibDeviceSpec(loadCalibParamsFromCCDB));
 
   // configure dpl timer to inject correct firstTForbit: start from the 1st orbit of TF containing 1st sampled orbit
   // o2::raw::HBFUtilsInitializer hbfIni(cfgc, specs);
+
   return specs;
 }
