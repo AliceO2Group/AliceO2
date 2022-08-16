eb0b78885a42ab2c70dccffab213f313f292ed32
Dmitri Peresunko <Dmitri.Peresunko@cern.ch>
2022-08-16 17:24:14 +0300

    Fix memory leak in calibration

--- Detectors/PHOS/calib/src/PHOSRunbyrunCalibDevice.cxx
+++ Detectors/PHOS/calib/src/PHOSRunbyrunCalibDevice.cxx
@@ -11,31 +11,38 @@
 
 #include "PHOSCalibWorkflow/PHOSRunbyrunCalibDevice.h"
 #include "Framework/ConfigParamRegistry.h"
+#include "Framework/DataTakingContext.h"
 #include "CCDB/CcdbApi.h"
 #include "CCDB/CcdbObjectInfo.h"
 #include "DetectorsCalibration/Utils.h"
 #include "Framework/ControlService.h"
 
 #include "FairLogger.h"
+#include <filesystem>
 
 using namespace o2::phos;
 
 void PHOSRunbyrunCalibDevice::init(o2::framework::InitContext& ic)
 {
   o2::base::GRPGeomHelper::instance().setRequest(mCCDBRequest);
-  // int slotL = ic.options().get<int>("tf-per-slot");
-  // int delay = ic.options().get<int>("max-delay");
   mCalibrator.reset(new PHOSRunbyrunCalibrator());
 
   // mCalibrator->setSlotLength(slotL);
   // mCalibrator->setMaxSlotsDelay(delay);
   mCalibrator->setUpdateAtTheEndOfRunOnly();
 }
+
 void PHOSRunbyrunCalibDevice::run(o2::framework::ProcessingContext& pc)
 {
+
   o2::base::GRPGeomHelper::instance().checkUpdates(pc);
-  if (mRunStartTime == 0) {
-    mRunStartTime = pc.services().get<o2::framework::TimingInfo>().creation; // approximate time in ms
+  auto crTime = pc.services().get<o2::framework::TimingInfo>().creation;
+  if (mRunStartTime == 0 || crTime < mRunStartTime) {
+    mRunStartTime = crTime;
+    if (mWriteRootOutput) {
+      mHistoFileMetaData = std::make_unique<o2::dataformats::FileMetaData>();
+      mHistoFileMetaData->setDataTakingContext(pc.services().get<DataTakingContext>());
+    }
   }
   auto tfcounter = o2::header::get<o2::header::DataHeader*>(pc.inputs().get("clusters").header)->tfCounter;
   auto clusters = pc.inputs().get<gsl::span<Cluster>>("clusters");
@@ -47,14 +54,41 @@ void PHOSRunbyrunCalibDevice::run(o2::framework::ProcessingContext& pc)
 void PHOSRunbyrunCalibDevice::endOfStream(o2::framework::EndOfStreamContext& ec)
 {
   mCalibrator->checkSlotsToFinalize(o2::calibration::INFINITE_TF);
-  mCalibrator->endOfStream();
+  if (mWriteRootOutput) {
+    std::string runNumber = mHistoFileMetaData->run;
+    std::string histoFileName = mOutputDir + fmt::format("PHOS_RunByRunCalib_{}.root", runNumber);
+    TFile histoFileOut(histoFileName.c_str(), "recreate");
+    mCalibrator->writeHistos();
+    histoFileOut.Close();
+
+    // write metaFile data
+    if (mMetaFileDir.compare("/dev/null")) {
+      mHistoFileMetaData->fillFileData(histoFileName);
+      mHistoFileMetaData->type = "calib";
+      mHistoFileMetaData->priority = "high";
+
+      std::string metaFileNameTmp = mMetaFileDir + fmt::format("PHOS_RunByRunCalib_{}.tmp", runNumber);
+      std::string metaFileName = mMetaFileDir + fmt::format("PHOS_RunByRunCalib_{}.done", runNumber);
+      try {
+        std::ofstream metaFileOut(metaFileNameTmp);
+        metaFileOut << *mHistoFileMetaData.get();
+        metaFileOut.close();
+        std::filesystem::rename(metaFileNameTmp, metaFileName);
+      } catch (std::exception const& e) {
+        LOG(error) << "Failed to store PHOS meta data file " << metaFileName << ", reason: " << e.what();
+      }
+      LOG(info) << "Stored metadate file " << metaFileName << ".done";
+    } else {
+      LOG(info) << "Did not store metafile as meta-dir=" << mMetaFileDir;
+    }
+  }
   mRunByRun = mCalibrator->getCalibration();
   if (checkFitResult()) {
     LOG(info) << "End of stream reached, sending output to CCDB";
     // prepare all info to be sent to CCDB
     auto flName = o2::ccdb::CcdbApi::generateFileName("Runbyrun");
     std::map<std::string, std::string> md;
-    o2::ccdb::CcdbObjectInfo info("PHS/Calib/Runbyrun", "Runbyrun", flName, md, mRunStartTime, o2::ccdb::CcdbObjectInfo::INFINITE_TIMESTAMP);
+    o2::ccdb::CcdbObjectInfo info("PHS/Calib/Runbyrun", "Runbyrun", flName, md, mRunStartTime - o2::ccdb::CcdbObjectInfo::MINUTE, mRunStartTime + o2::ccdb::CcdbObjectInfo::YEAR);
     info.setMetaData(md);
     auto image = o2::ccdb::CcdbApi::createObjectImage(&mRunByRun, &info);
 
@@ -63,9 +97,8 @@ void PHOSRunbyrunCalibDevice::endOfStream(o2::framework::EndOfStreamContext& ec)
               << " bytes, valid for " << info.getStartValidityTimestamp()
               << " : " << info.getEndValidityTimestamp();
 
-    header::DataHeader::SubSpecificationType subSpec{(header::DataHeader::SubSpecificationType)0};
-    ec.outputs().snapshot(Output{o2::calibration::Utils::gDataOriginCDBPayload, "PHOS_Runbyrun", subSpec}, *image.get());
-    ec.outputs().snapshot(Output{o2::calibration::Utils::gDataOriginCDBWrapper, "PHOS_Runbyrun", subSpec}, info);
+    ec.outputs().snapshot(Output{o2::calibration::Utils::gDataOriginCDBPayload, "PHOS_Runbyrun", 0}, *image.get());
+    ec.outputs().snapshot(Output{o2::calibration::Utils::gDataOriginCDBWrapper, "PHOS_Runbyrun", 0}, info);
   } else {
     LOG(error) << "Incorrect fit results: " << mRunByRun[0] << "+-" << mRunByRun[1] << ", "
                << mRunByRun[2] << "+-" << mRunByRun[3] << ", "
@@ -89,13 +122,13 @@ bool PHOSRunbyrunCalibDevice::checkFitResult()
   return res;
 }
 
-o2::framework::DataProcessorSpec o2::phos::getPHOSRunbyrunCalibDeviceSpec(bool useCCDB)
+o2::framework::DataProcessorSpec o2::phos::getPHOSRunbyrunCalibDeviceSpec(bool useCCDB, const std::string& outputDir, const std::string& metaFileDir, bool writeRootOutput)
 {
 
   std::vector<OutputSpec> outputs;
   outputs.emplace_back("PHS", "RUNBYRUNHISTOS", 0, o2::framework::Lifetime::Timeframe);
-  outputs.emplace_back(ConcreteDataTypeMatcher{o2::calibration::Utils::gDataOriginCDBPayload, "PHOS_Runbyrun"}, Lifetime::Sporadic);
-  outputs.emplace_back(ConcreteDataTypeMatcher{o2::calibration::Utils::gDataOriginCDBWrapper, "PHOS_Runbyrun"}, Lifetime::Sporadic);
+  outputs.emplace_back(o2::calibration::Utils::gDataOriginCDBPayload, "PHOS_Runbyrun", 0, Lifetime::Sporadic);
+  outputs.emplace_back(o2::calibration::Utils::gDataOriginCDBWrapper, "PHOS_Runbyrun", 0, Lifetime::Sporadic);
 
   std::vector<InputSpec> inputs;
   inputs.emplace_back("clusters", "PHS", "CLUSTERS");
@@ -111,6 +144,6 @@ o2::framework::DataProcessorSpec o2::phos::getPHOSRunbyrunCalibDeviceSpec(bool u
     "calib-phos-runbyrun",
     inputs,
     outputs,
-    AlgorithmSpec{adaptFromTask<PHOSRunbyrunCalibDevice>(ccdbRequest)},
+    AlgorithmSpec{adaptFromTask<PHOSRunbyrunCalibDevice>(ccdbRequest, outputDir, metaFileDir, writeRootOutput)},
     Options{}};
 }
