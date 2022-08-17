0988f93cf66c94e2822dd061950a8d6289af65e2
wiechula <Jens.Wiechula@cern.ch>
2022-08-17 11:37:21 +0200

    Clusters sorting; run, firstOrbit, tfCounter branches

--- Detectors/TPC/reconstruction/macro/findKrBoxCluster.C
+++ Detectors/TPC/reconstruction/macro/findKrBoxCluster.C
@@ -13,26 +13,33 @@
 /// \brief This macro retrieves clusters from Krypton and X-Ray runs, input tpcdigits.root
 /// \author Philip Hauer <philip.hauer@cern.ch>
 
-#if !defined(__CLING__) || defined(__ROOTCLING__)
+//#if !defined(__CLING__) || defined(__ROOTCLING__)
 #include "TCanvas.h"
 #include "TFile.h"
-#include "TTree.h"
+#include "TChain.h"
+#include "TGrid.h"
 
 #include "DataFormatsTPC/KrCluster.h"
 #include "TPCReconstruction/KrBoxClusterFinder.h"
 #include "DataFormatsTPC/Digit.h"
+#include "TPCBase/Utils.h"
 
 #include <array>
 #include <iostream>
 #include <tuple>
 #include <vector>
-#endif
+//#endif
 
-void findKrBoxCluster(int lastTimeBin = 1000, int run = -1, int time = -1, std::string_view gainMapFile = "", std::string inputFile = "tpcdigits.root", std::string outputFile = "BoxClusters.root")
+void findKrBoxCluster(std::string inputFile = "tpcdigits.root", std::string outputFile = "BoxClusters.root", std::string_view gainMapFile = "")
 {
+  int lastTimeBin = 1000;
+  ULong_t runRead = -1;
+  uint32_t run = 0;
+  uint32_t firstOrbit = 0;
+  uint32_t tfCounter = 0;
+
   // Read the digits:
-  TFile* file = new TFile(inputFile.c_str());
-  TTree* tree = (TTree*)file->Get("o2sim");
+  TChain* tree = o2::tpc::utils::buildChain(fmt::format("cat {}", inputFile), "o2sim", "o2sim");
   Long64_t nEntries = tree->GetEntries();
   std::cout << "The Tree has " << nEntries << " Entries." << std::endl;
 
@@ -46,13 +53,17 @@ void findKrBoxCluster(int lastTimeBin = 1000, int run = -1, int time = -1, std::
   // Create a Branch for each sector:
   tClusters->Branch("cls", &clusters);
   tClusters->Branch("run", &run);
-  tClusters->Branch("time", &time);
+  tClusters->Branch("firstOrbit", &firstOrbit);
+  tClusters->Branch("tfCounter", &tfCounter);
 
   std::array<std::vector<o2::tpc::Digit>*, 36> digitizedSignal;
   for (size_t iSec = 0; iSec < digitizedSignal.size(); ++iSec) {
     digitizedSignal[iSec] = nullptr;
     tree->SetBranchAddress(Form("TPCDigit_%zu", iSec), &digitizedSignal[iSec]);
   }
+  tree->SetBranchAddress("run", &runRead);
+  tree->SetBranchAddress("firstOrbit", &firstOrbit);
+  tree->SetBranchAddress("tfCounter", &tfCounter);
 
   if (gainMapFile.size()) {
     clFinder->loadGainMapFromFile(gainMapFile);
@@ -67,13 +78,29 @@ void findKrBoxCluster(int lastTimeBin = 1000, int run = -1, int time = -1, std::
   for (int iEvent = 0; iEvent < nEntries; ++iEvent) {
     std::cout << iEvent + 1 << "/" << nEntries << std::endl;
     tree->GetEntry(iEvent);
+    run = uint32_t(runRead);
 
     for (int i = 0; i < 36; i++) {
       auto sector = digitizedSignal[i];
       if (sector->size() == 0) {
         continue;
       }
-      std::cout << "Processing sector " << i << "\n";
+      auto& digits = *sector;
+      std::sort(digits.begin(), digits.end(), [](const auto& a, const auto& b) {
+        if (a.getTimeStamp() < b.getTimeStamp()) {
+          return true;
+        }
+        if (a.getTimeStamp() == b.getTimeStamp()) {
+          if (a.getRow() < b.getRow()) {
+            return true;
+          } else if (a.getRow() == b.getRow()) {
+            return a.getPad() < b.getPad();
+          }
+        }
+        return false;
+      });
+
+      // std::cout << "Processing sector " << i << "\n";
 
       clFinder->loopOverSector(*sector, i);
     }
