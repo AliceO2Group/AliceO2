518095a213419d90c443fcfca45f36865548e3fc
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2022-08-26 14:07:02 +0200

    DPL: drop all messages when cleanup property increases
    This is an attempt at doing what we discussed. Frankly speaking
    I am more and more convinced this is a hack (if it works at all)
    and we should either have a proper DRAIN state, or we discard messages
    based on the run number.

--- Framework/Core/src/DataProcessingDevice.cxx
+++ Framework/Core/src/DataProcessingDevice.cxx
@@ -125,6 +125,27 @@ DataProcessingDevice::DataProcessingDevice(RunningDeviceRef running, ServiceRegi
     mServiceRegistry{registry},
     mProcessingPolicies{policies}
 {
+  GetConfig()->Subscribe<std::string>("dpl", [&cleanupCount = mCleanupCount, &registry = mServiceRegistry](const std::string& key, std::string value) {
+    if (key == "cleanup") {
+      int64_t newCleanupCount = std::stoll(value);
+      if (newCleanupCount <= cleanupCount) {
+        return;
+      }
+      cleanupCount = newCleanupCount;
+      auto ref = ServiceRegistryRef{registry, ServiceRegistry::globalDeviceSalt()};
+      auto& deviceState = ref.get<DeviceState>();
+      for (auto& info : deviceState.inputChannelInfos) {
+        FairMQParts parts;
+        while (info.channel->Receive(parts, 0)) {
+          LOGP(debug, "Dropping {} parts", parts.Size());
+          if (parts.Size() == 0) {
+            break;
+          }
+        }
+      }
+    }
+  });
+
   std::function<void(const fair::mq::State)> stateWatcher = [this, &registry = mServiceRegistry](const fair::mq::State state) -> void {
     auto ref = ServiceRegistryRef{registry, ServiceRegistry::globalDeviceSalt()};
     auto& deviceState = ref.get<DeviceState>();
