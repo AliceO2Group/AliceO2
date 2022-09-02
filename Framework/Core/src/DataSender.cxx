7ecc54d78803eac6cdfa64b0ad6ed4b60927155b
Drew Mason-Laurence <drew@zowlyfon.net>
2022-09-02 11:26:48 +0200

    Sending Policy add Services Registry (#9742)

--- Framework/Core/src/DataSender.cxx
+++ Framework/Core/src/DataSender.cxx
@@ -79,7 +79,7 @@ std::unique_ptr<fair::mq::Message> DataSender::create(RouteIndex routeIndex)
 
 void DataSender::send(fair::mq::Parts& parts, ChannelIndex channelIndex)
 {
-  mPolicy.send(mProxy, parts, channelIndex);
+  mPolicy.send(mProxy, parts, channelIndex, mRegistry);
 }
 
 } // namespace o2::framework
