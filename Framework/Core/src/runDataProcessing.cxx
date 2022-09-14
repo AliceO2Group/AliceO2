74337028db0d96372e3a2a97483df5448e3f3af7
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2022-09-14 22:01:53 +0200

    DPL: add back Pss metrics when resources-monitoring is enabled

--- Framework/Core/src/runDataProcessing.cxx
+++ Framework/Core/src/runDataProcessing.cxx
@@ -1128,7 +1128,7 @@ int doChild(int argc, char** argv, ServiceRegistry& serviceRegistry,
       serviceRegistry.declareService(service, *deviceState.get(), r.fConfig);
     }
     if (ResourcesMonitoringHelper::isResourcesMonitoringEnabled(spec.resourceMonitoringInterval)) {
-      serviceRegistry.get<Monitoring>().enableProcessMonitoring(spec.resourceMonitoringInterval);
+      serviceRegistry.get<Monitoring>().enableProcessMonitoring(spec.resourceMonitoringInterval, {PmMeasurement::Cpu, PmMeasurement::Mem, PmMeasurement::Smaps});
     }
   };
 
