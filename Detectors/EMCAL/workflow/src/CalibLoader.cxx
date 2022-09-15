a7fe6248f2b5074a5e9235b5ecb1e1facbecee04
Markus Fasel <markus.fasel@cern.ch>
2022-09-15 18:25:59 +0200

    [EMCAL-830] Adapt CalibLoader to fully rely on CCDB-backend
    - Remove static loading of calibration objects from file in
      favour of condition-remap from the framework
    - Remove ManagedObject as it is no longer needed due to
      ownership handling in the CCDB backend
    - Add checkUpdate function triggering the request of the
      calibration objects from the ProcessingContext

--- Detectors/EMCAL/workflow/src/CalibLoader.cxx
+++ Detectors/EMCAL/workflow/src/CalibLoader.cxx
@@ -10,7 +10,6 @@
 // or submit itself to any jurisdiction.
 
 #include <filesystem>
-#include <memory>
 #include <fairlogger/Logger.h>
 #include "EMCALCalib/CalibDB.h"
 #include "EMCALCalib/BadChannelMap.h"
@@ -18,82 +17,65 @@
 #include "EMCALCalib/GainCalibrationFactors.h"
 #include "EMCALWorkflow/CalibLoader.h"
 #include "Framework/CCDBParamSpec.h"
+#include "Framework/InputRecord.h"
 #include "TFile.h"
 
 using namespace o2::emcal;
 
 void CalibLoader::defineInputSpecs(std::vector<o2::framework::InputSpec>& inputs)
 {
-  if (hasBadChannelMap() && !hasLocalBadChannelMap()) {
+  if (hasBadChannelMap()) {
     inputs.push_back({"badChannelMap", o2::header::gDataOriginEMC, "BADCHANNELMAP", 0, o2::framework::Lifetime::Condition, o2::framework::ccdbParamSpec(CalibDB::getCDBPathBadChannelMap())});
   }
-  if (hasTimeCalib() && !hasLocalTimeCalib()) {
+  if (hasTimeCalib()) {
     inputs.push_back({"timeCalibParams", o2::header::gDataOriginEMC, "TIMECALIBPARAMS", 0, o2::framework::Lifetime::Condition, o2::framework::ccdbParamSpec(CalibDB::getCDBPathTimeCalibrationParams())});
   }
-  if (hasGainCalib() && !hasLocalGainCalib()) {
+  if (hasGainCalib()) {
     inputs.push_back({"gainCalibParams", o2::header::gDataOriginEMC, "GAINCALIBPARAMS", 0, o2::framework::Lifetime::Condition, o2::framework::ccdbParamSpec(CalibDB::getCDBPathGainCalibrationParams())});
+  }
+}
+
+void CalibLoader::checkUpdates(o2::framework::ProcessingContext& ctx)
+{
+  if (hasBadChannelMap()) {
+    ctx.inputs().get<o2::emcal::BadChannelMap*>("badChannelMap");
+  }
+  if (hasTimeCalib()) {
+    ctx.inputs().get<o2::emcal::TimeCalibrationParams*>("timeCalibParams");
+  }
+  if (hasGainCalib()) {
+    ctx.inputs().get<o2::emcal::GainCalibrationFactors*>("gainCalibParams");
   }
 }
 
 bool CalibLoader::finalizeCCDB(o2::framework::ConcreteDataMatcher& matcher, void* obj)
 {
   if (matcher == o2::framework::ConcreteDataMatcher("EMC", "BADCHANNELMAP", 0)) {
-    if (hasBadChannelMap() && !hasLocalBadChannelMap()) {
+    if (hasBadChannelMap()) {
       LOG(info) << "New bad channel map loaded";
-      mBadChannelMap = ManagedObject<o2::emcal::BadChannelMap>(reinterpret_cast<o2::emcal::BadChannelMap*>(obj), false);
+      mBadChannelMap = reinterpret_cast<o2::emcal::BadChannelMap*>(obj);
     } else {
       LOG(error) << "New bad channel map available even though bad channel calibration was not enabled, not loading";
     }
     return true;
   }
   if (matcher == o2::framework::ConcreteDataMatcher("EMC", "TIMECALIBPARAMS", 0)) {
-    if (hasTimeCalib() && !hasLocalTimeCalib()) {
+    if (hasTimeCalib()) {
       LOG(info) << "New time calibration paramset loaded";
-      mTimeCalibParams = ManagedObject<o2::emcal::TimeCalibrationParams>(reinterpret_cast<o2::emcal::TimeCalibrationParams*>(obj), false);
+      mTimeCalibParams = reinterpret_cast<o2::emcal::TimeCalibrationParams*>(obj);
     } else {
       LOG(error) << "New time calibration paramset available even though time calibration was not enabled, not loading";
     }
     return true;
   }
   if (matcher == o2::framework::ConcreteDataMatcher("EMC", "GAINCALIBPARAMS", 0)) {
-    if (hasGainCalib() && !hasLocalGainCalib()) {
+    if (hasGainCalib()) {
       LOG(info) << "New gain calibration paramset loaded";
-      mGainCalibParams = ManagedObject<o2::emcal::GainCalibrationFactors>(reinterpret_cast<o2::emcal::GainCalibrationFactors*>(obj), false);
+      mGainCalibParams = reinterpret_cast<o2::emcal::GainCalibrationFactors*>(obj);
     } else {
       LOG(error) << "New gain calibration paramset available even though the gain calibration was not enabled, not loading";
     }
     return true;
   }
   return false;
-}
-
-void CalibLoader::static_load()
-{
-  if (hasLocalBadChannelMap()) {
-    if (std::filesystem::exists(std::filesystem::path(mPathBadChannelMap))) {
-      LOG(info) << "Static load: Bad channel map taken from file " << mPathBadChannelMap;
-      std::unique_ptr<TFile> reader(TFile::Open(mPathBadChannelMap.data(), "READ"));
-      mBadChannelMap = ManagedObject<o2::emcal::BadChannelMap>(new o2::emcal::BadChannelMap(*(reader->Get<o2::emcal::BadChannelMap>("ccdb_object"))), true);
-    } else {
-      LOG(error) << "Static load: Path to bad channel map (" << mPathBadChannelMap << ") not existing";
-    }
-  }
-  if (hasLocalTimeCalib()) {
-    if (std::filesystem::exists(std::filesystem::path(mPathTimeCalib))) {
-      LOG(info) << "Static load: Time calibration coefficients taken from file " << mPathTimeCalib;
-      std::unique_ptr<TFile> reader(TFile::Open(mPathTimeCalib.data(), "READ"));
-      mTimeCalibParams = ManagedObject<o2::emcal::TimeCalibrationParams>(new o2::emcal::TimeCalibrationParams(*(reader->Get<o2::emcal::TimeCalibrationParams>("ccdb_object"))), true);
-    } else {
-      LOG(error) << "Static load: Path to time calibration params (" << mPathTimeCalib << ") not existing";
-    }
-  }
-  if (hasLocalGainCalib()) {
-    if (std::filesystem::exists(std::filesystem::path(mPathGainCalib))) {
-      LOG(info) << "Static load: Gain calibration coefficients taken from file " << mPathGainCalib;
-      std::unique_ptr<TFile> reader(TFile::Open(mPathGainCalib.data(), "READ"));
-      mGainCalibParams = ManagedObject<o2::emcal::GainCalibrationFactors>(new o2::emcal::GainCalibrationFactors(*(reader->Get<o2::emcal::GainCalibrationFactors>("ccdb_object"))), true);
-    } else {
-      LOG(error) << "Static load: Path to gain calibration params (" << mPathGainCalib << ") not existing";
-    }
-  }
 }
