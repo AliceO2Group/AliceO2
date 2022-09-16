e8c1231f5bc7d3a7b46993513bf36c8c0e5750cf
wiechula <Jens.Wiechula@cern.ch>
2022-09-16 15:15:40 +0200

    Allow setting of pad IT params

--- Detectors/TPC/reconstruction/src/IonTailCorrection.cxx
+++ Detectors/TPC/reconstruction/src/IonTailCorrection.cxx
@@ -17,7 +17,10 @@
 #include <cmath>
 #include <vector>
 #include <algorithm>
+#include <filesystem>
 
+#include "TPCBase/Utils.h"
+#include "TPCBase/CRU.h"
 #include "TPCReconstruction/IonTailCorrection.h"
 #include "TPCReconstruction/IonTailCorrectionSettings.h"
 
@@ -28,89 +31,98 @@ IonTailCorrection::IonTailCorrection()
   const auto& settings = IonTailCorrectionSettings::Instance();
   mKTime = settings.kTime;
   mITMultFactor = settings.ITMultFactor;
+  LOGP(info, "IT settings: kTime = {}, ITMultFactor = {}, padITCorrFile = {}",
+       settings.kTime, settings.ITMultFactor, settings.padITCorrFile);
+
+  if (!settings.padITCorrFile.empty()) {
+    loadITPadValuesFromFile(settings.padITCorrFile);
+  }
 }
 
 void IonTailCorrection::filterDigitsDirect(std::vector<Digit>& digits)
 {
+  using Streamer = o2::utils::DebugStreamer;
+
+  if (digits.size() == 0) {
+    return;
+  }
+
   // make sure we process the digits pad-by-pad in increasing time order
   sortDigitsOneSectorPerPad(digits);
+
   int lastRow = -1;
   int lastPad = -1;
-  int lastTime = digits.front().getTimeStamp() - 1;
-
+  int lastTime = digits.front().getTimeStamp();
   float cumul = 0;
 
-  // TODO: for now hard-coded, make pad-by-pad, if available
-  float kAmp = std::abs(mITMultFactor) * 0.1276; // TODO: replace with pad-by-pad value
+  // default IT parameters are replaced below,
+  // in case pad-by-pad values were set up
+  float kAmp = std::abs(mITMultFactor) * 0.1276;
   if (mITMultFactor < 0) {
     kAmp = kAmp / (1 + kAmp);
   }
-  const float kTime = mKTime; // TODO: replace with pad-by-pad value
-  const float tailSlopeUnit = std::exp(-kTime);
+
+  float kTime = mKTime;
+  float tailSlopeUnit = std::exp(-kTime);
 
   for (auto& digit : digits) {
+    const auto sector = CRU(digit.getCRU()).sector();
     const auto row = digit.getRow();
     const auto pad = digit.getPad();
-    auto time = digit.getTimeStamp();
-    auto charge = digit.getChargeFloat();
+    const auto time = digit.getTimeStamp();
+
+    if (mFraction) {
+      kAmp = mFraction->getValue(sector, row, pad) * std::abs(mITMultFactor);
+      if (mITMultFactor < 0) {
+        kAmp = kAmp / (1 + kAmp);
+      }
+    }
+    if (mExpLambda) {
+      tailSlopeUnit = mExpLambda->getValue(sector, row, pad);
+      kTime = -std::log(tailSlopeUnit);
+    }
 
     // reset charge cumulation if pad has changed
     if (row != lastRow || pad != lastPad) {
       cumul = 0;
-      lastTime = time - 1;
+      lastTime = time;
     }
-    auto origCharge = charge;
-    charge = origCharge + mSign * kAmp * (1 - tailSlopeUnit) * cumul;
 
     // cumulate charge also over missing time stamps
-    while (lastTime < time) {
-      ++lastTime;
+    while (lastTime + 1 < time) {
+      auto origCuml = cumul;
+      cumul *= tailSlopeUnit;
 
-      using Streamer = o2::utils::DebugStreamer;
       if (Streamer::checkStream(o2::utils::StreamFlags::streamITCorr)) {
-        mStreamer.setStreamer("debug_ITCorr", "UPDATE");
-        int cru = digit.getCRU();
-        auto rowTmp = row;
-        auto padTmp = pad;
-        auto kAmpTmp = kAmp;
-        auto kTimeTmp = kTime;
-        auto tailSlopeUnitTmp = tailSlopeUnit;
-        auto chargeTmp = origCharge + mSign * kAmp * (1 - tailSlopeUnit) * cumul;
-
-        mStreamer.getStreamer()
-          << mStreamer.getUniqueTreeName("itCorr").data()
-          << "cru=" << cru
-          << "row=" << rowTmp
-          << "pad=" << padTmp
-          << "time=" << lastTime
-
-          << "kAmp=" << kAmpTmp
-          << "kTime=" << kTimeTmp
-          << "tailSlopeUnit=" << tailSlopeUnitTmp
-          << "itMultFactor=" << mITMultFactor
-
-          << "origCharge=" << origCharge
-          << "charge=" << chargeTmp
-          << "\n";
+        streamData(digit.getCRU(), row, pad, time, lastTime, kAmp, kTime, tailSlopeUnit, origCuml, cumul, 0, 0);
       }
 
-      cumul += origCharge;
-      cumul *= tailSlopeUnit;
+      ++lastTime;
 
       if (cumul < 0.1) {
         cumul = 0;
         break;
       }
-      origCharge = 0;
     }
 
+    const auto origCharge = digit.getChargeFloat();
+    const auto charge = origCharge + mSign * kAmp * (1 - tailSlopeUnit) * cumul;
     digit.setCharge(charge);
+
+    const auto origCuml = cumul;
+    cumul += origCharge;
+    cumul *= tailSlopeUnit;
+
+    if (Streamer::checkStream(o2::utils::StreamFlags::streamITCorr)) {
+      streamData(digit.getCRU(), row, pad, time, lastTime, kAmp, kTime, tailSlopeUnit, origCuml, cumul, origCharge, charge);
+    }
 
     lastRow = row;
     lastPad = pad;
     lastTime = time;
   }
 
+  // sort back
   sortDigitsOneSectorPerTimeBin(digits);
 }
 
@@ -148,4 +160,45 @@ void IonTailCorrection::sortDigitsOneSectorPerTimeBin(std::vector<Digit>& digits
     }
     return false;
   });
+}
+
+void IonTailCorrection::loadITPadValuesFromFile(std::string_view itParamFile)
+{
+  if (!std::filesystem::exists(itParamFile)) {
+    LOGP(fatal, "Could not find IF param file {}", itParamFile);
+  }
+
+  auto calDets = utils::readCalPads(itParamFile, "fraction,expLambda");
+  if (!calDets[0]) {
+    LOGP(fatal, "Could not read IT fraction object from file {}", itParamFile);
+  }
+  if (!calDets[1]) {
+    LOGP(fatal, "Could not read IT expLambda object from file {}", itParamFile);
+  }
+
+  mFraction.reset(calDets[0]);
+  mExpLambda.reset(calDets[1]);
+
+  LOGP(info, "Loaded ion tail correction values from file {}", itParamFile);
+}
+
+void IonTailCorrection::streamData(int cru, int row, int pad, int time, int lastTime, float kAmp, float kTime, float tailSlopeUnit, float origCumul, float cumul, float origCharge, float charge)
+{
+  mStreamer.setStreamer("debug_ITCorr", "UPDATE");
+
+  mStreamer.getStreamer()
+    << "itCorr"
+    << "cru=" << cru
+    << "row=" << row
+    << "pad=" << pad
+    << "time=" << lastTime
+
+    << "kAmp=" << kAmp
+    << "kTime=" << kTime
+    << "tailSlopeUnit=" << tailSlopeUnit
+    << "itMultFactor=" << mITMultFactor
+
+    << "origCharge=" << origCharge
+    << "charge=" << charge
+    << "\n";
 }
