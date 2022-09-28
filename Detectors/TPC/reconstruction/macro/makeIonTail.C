07d9f489cbf4a4e21e90bc30c4d8795d1f452228
wiechula <Jens.Wiechula@cern.ch>
2022-09-28 08:56:16 +0200

    Add detection / counting of digits from saturated signals

--- Detectors/TPC/reconstruction/macro/makeIonTail.C
+++ Detectors/TPC/reconstruction/macro/makeIonTail.C
@@ -15,6 +15,7 @@
 
 #include <algorithm>
 #include <memory>
+#include <numeric>
 #include <vector>
 #include <string_view>
 #include <fmt/format.h>
@@ -28,15 +29,16 @@
 #include "TPCBase/Utils.h"
 #include "TPCReconstruction/IonTailCorrection.h"
 #include "TPCBase/CRUCalibHelpers.h"
+#include "CommonUtils/TreeStreamRedirector.h"
 
 using namespace o2::tpc;
+size_t digitsInSaturateion(std::vector<Digit>& digits, bool correctCharge = false, CalPad* pedestals = nullptr, o2::utils::TreeStreamRedirector* stream = nullptr);
 
-void makeIonTail(std::string_view inputFile = "tpcdigits.root", float itMultFactor = 1.f, float kTime = 0.0515, float threshold = -100.f, bool suppressBelowThreshold = false, bool enableDebugStream = false, bool roundTo12b = true, std::string_view itParamFile = "")
+void makeIonTail(std::string_view inputFile = "tpcdigits.root", float itMultFactor = 1.f, float kTime = 0.0515, float threshold = -100.f, bool suppressBelowThreshold = false, bool removeSaturation = false, bool enableDebugStream = false, bool roundTo12b = true, std::string_view itParamFile = "", std::string_view pedetalFile = "")
 {
   if (enableDebugStream) {
     o2::conf::ConfigurableParam::setValue<int>("DebugStreamerParam", "StreamLevel", o2::utils::StreamFlags::streamITCorr);
   }
-
   o2::conf::ConfigurableParam::setValue<float>("TPCITCorr", "ITMultFactor", itMultFactor);
   o2::conf::ConfigurableParam::setValue<float>("TPCITCorr", "kTime", kTime);
   o2::conf::ConfigurableParam::setValue("TPCITCorr.padITCorrFile", itParamFile.data());
@@ -49,9 +51,9 @@ void makeIonTail(std::string_view inputFile = "tpcdigits.root", float itMultFact
     suppressed += ".12b";
   }
 
-  auto outputFile = fmt::format("tpcdigits.itCorr.{:.2f}.{:.4f}{}.root", itMultFactor, kTime, suppressed);
+  auto outputFile = fmt::format("tpcdigits.itCorr.{:.2f}.{:.4f}{}{}.root", itMultFactor, kTime, suppressed, removeSaturation ? ".rs" : "");
   if (itParamFile.size()) {
-    outputFile = fmt::format("tpcdigits.itCorr.paramFile{}.root", suppressed);
+    outputFile = fmt::format("tpcdigits.itCorr.{:.2f}.paramFile{}{}.root", itMultFactor, suppressed, removeSaturation ? ".rs" : "");
   }
 
   TChain* tree = o2::tpc::utils::buildChain(fmt::format("ls {}", inputFile), "o2sim", "o2sim");
@@ -71,7 +73,18 @@ void makeIonTail(std::string_view inputFile = "tpcdigits.root", float itMultFact
     tOut.Branch(Form("TPCDigit_%zu", iSec), &digitizedSignal[iSec]);
   }
 
+  // pedestals
+  CalPad* pedestals = nullptr;
+  if (!pedetalFile.empty()) {
+    auto calPads = utils::readCalPads(pedetalFile, "Pedestals");
+    pedestals = calPads[0];
+  }
+  // debug stream
+  o2::utils::TreeStreamRedirector pcstream("DigitFilterDebug.root", "recreate");
+
   IonTailCorrection itCorr;
+
+  size_t saturatedSignals = 0;
 
   for (Long64_t iEvent = 0; iEvent < nEntries; ++iEvent) {
     tree->GetEntry(iEvent);
@@ -84,6 +97,11 @@ void makeIonTail(std::string_view inputFile = "tpcdigits.root", float itMultFact
         }
       }
       originalDigits += digits->size();
+
+      // count digits in threshold
+      const auto satSignalsSec = digitsInSaturateion(*digits, removeSaturation, pedestals, &pcstream);
+      saturatedSignals += satSignalsSec;
+
       itCorr.filterDigitsDirect(*digits);
       if (suppressBelowThreshold) {
         digits->erase(std::remove_if(digits->begin(), digits->end(),
@@ -95,19 +113,133 @@ void makeIonTail(std::string_view inputFile = "tpcdigits.root", float itMultFact
       } else {
         digitsAboveThreshold += std::count_if(digits->begin(), digits->end(), [threshold](const auto& digit) { return digit.getChargeFloat() >= threshold; });
       }
+
+      fmt::print("Found {} saturated signals in sector {}\n", satSignalsSec, iSector);
     }
 
     tOut.Fill();
   }
 
-  const float digitFraction = float(digitsAboveThreshold) / float(originalDigits);
-  std::string addInfo = fmt::format("itMultFactor {} and kTime {}", itMultFactor, kTime);
+  const float digitFraction = float(digitsAboveThreshold) / float(originalDigits) * 100;
+  const float saturationFraction = float(saturatedSignals) / float(originalDigits) * 100;
+
+  std::string addInfo = fmt::format("itMultFactor {}, kTime {} and saturation signal removal {}", itMultFactor, kTime, removeSaturation);
   if (itParamFile.size()) {
     addInfo = fmt::format("IT parameters loaded from {}", itParamFile);
   }
-  LOGP(info, "Found {} / {}  = {:.2f} digits with charge >= {} in {} entries with {}",
-       digitsAboveThreshold, originalDigits, digitFraction, threshold, nEntries, addInfo);
+  LOGP(info, "Found {} / {}  = {:.2f}% digits with charge >= {} and {} / {} = {:.2f}% saturated signals in {} entries with {}",
+       digitsAboveThreshold, originalDigits, digitFraction, threshold, saturatedSignals, originalDigits, saturationFraction, nEntries, addInfo);
 
   fOut->Write();
   fOut->Close();
+}
+
+size_t digitsInSaturateion(std::vector<Digit>& digits, bool correctCharge /*= false*/, CalPad* pedestals /*= nullptr*/, o2::utils::TreeStreamRedirector* stream /*= nullptr*/)
+{
+  int lastRow = -1;
+  int lastPad = -1;
+  int lastTime = -1;
+  float lastCharge = 0;
+  float refSat = 90;
+  float upperSat = 30;
+  float lowerSat = 40;
+  float initSat = 3;
+  size_t nAboveThreshold = 0;
+  bool checkSaturation = false;
+  size_t nSaturatedSignals = 0;
+  const int nMean = 3;
+  int posMean = 0;
+  int nPosMean = 0;
+  float satVals[nMean] = {0};
+  bool isSaturated = false;
+
+  IonTailCorrection::sortDigitsOneSectorPerPad(digits);
+
+  for (auto& digit : digits) {
+    const auto sector = CRU(digit.getCRU()).sector();
+    auto row = digit.getRow();
+    auto pad = digit.getPad();
+    auto time = digit.getTimeStamp();
+    float pedestal = 0;
+    float threshold = 920;
+    if (pedestals) {
+      pedestal = pedestals->getValue(sector, row, pad);
+      threshold = 1020.f - pedestal;
+      // fmt::print("threshold: {}\n", threshold);
+    }
+
+    // reset charge cumulation if pad has changed
+    if (row != lastRow || pad != lastPad) {
+      lastTime = time;
+      nAboveThreshold = 0;
+      refSat = 90;
+      upperSat = 30;
+      lowerSat = 40;
+      initSat = 5;
+      nPosMean = 0;
+      posMean = 0;
+      isSaturated = false;
+    }
+
+    bool isInLimit = false;
+    auto origCharge = digit.getChargeFloat();
+    if (time == lastTime + 1) {
+      // fmt::print("checkSaturation: origCharge = {}, refSat = {}, lastCharge = {}\n", origCharge, refSat, lastCharge);
+      if (origCharge > threshold) {
+        checkSaturation = true;
+        isInLimit = true;
+      }
+      if (checkSaturation && (origCharge < refSat + upperSat) && (origCharge > refSat - upperSat) && std::abs(origCharge - lastCharge) < initSat) {
+        ++nAboveThreshold;
+        if (nAboveThreshold > 2) {
+          ++nSaturatedSignals;
+          isSaturated = true;
+          satVals[posMean++] = origCharge;
+          posMean %= nMean;
+          if (nPosMean < nMean) {
+            ++nPosMean;
+          }
+          refSat = std::accumulate(satVals, satVals + nPosMean, 0.f) / float(nPosMean);
+          upperSat = 5;
+          lowerSat = 5;
+          initSat = lowerSat;
+        }
+      }
+    } else {
+      isSaturated = false;
+      checkSaturation = false;
+      nAboveThreshold = 0;
+      refSat = 90;
+      upperSat = 30;
+      lowerSat = 40;
+      initSat = 5;
+      nPosMean = 0;
+      posMean = 0;
+    }
+    if (isSaturated || isInLimit) {
+      if (stream) {
+        int sec = sector;
+        (*stream) << "satSig"
+                  << "sector=" << sec
+                  << "row=" << row
+                  << "pad=" << pad
+                  << "time=" << time
+                  << "charge=" << origCharge
+                  << "lastCharge=" << lastCharge
+                  << "refSat=" << refSat
+                  << "pedestal=" << pedestal
+                  << "isInLimit=" << isInLimit
+                  << "\n";
+      }
+    }
+    if (correctCharge && isSaturated) {
+      digit.setCharge(origCharge - refSat);
+      // digit.setCharge(-origCharge - 10);
+    }
+    lastRow = row;
+    lastPad = pad;
+    lastCharge = origCharge;
+    lastTime = time;
+  }
+  return nSaturatedSignals;
 }
