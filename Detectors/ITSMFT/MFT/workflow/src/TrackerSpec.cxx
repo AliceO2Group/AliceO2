555295384a75f36ef22da3cf1dae8bb3c95db464
Rafael Pezzi <rafael.pezzi@cern.ch>
2022-10-07 12:33:26 +0200

    Multithreaded MFT tracker

--- Detectors/ITSMFT/MFT/workflow/src/TrackerSpec.cxx
+++ Detectors/ITSMFT/MFT/workflow/src/TrackerSpec.cxx
@@ -20,6 +20,7 @@
 #include "MFTBase/GeometryTGeo.h"
 
 #include <vector>
+#include <future>
 
 #include "TGeoGlobalMagField.h"
 
@@ -52,13 +53,14 @@ void TrackerDPL::init(InitContext& ic)
     mTimer[sw].Reset();
   }
 
-  mTimer[SWTot].Start(false);
   // tracking configuration parameters
   auto& trackingParam = MFTTrackingParam::Instance(); // to avoi loading interpreter during the run
 }
 
 void TrackerDPL::run(ProcessingContext& pc)
 {
+  mTimer[SWTot].Start(false);
+
   updateTimeDependentParams(pc);
   gsl::span<const unsigned char> patterns = pc.inputs().get<gsl::span<unsigned char>>("patterns");
   auto compClusters = pc.inputs().get<const std::vector<o2::itsmft::CompClusterExt>>("compClusters");
@@ -90,81 +92,213 @@ void TrackerDPL::run(ProcessingContext& pc)
   std::vector<o2::mft::TrackLTFL> tracksL;
   auto& allTracksMFT = pc.outputs().make<std::vector<o2::mft::TrackMFT>>(Output{"MFT", "TRACKS", 0, Lifetime::Timeframe});
 
-  std::uint32_t roFrame = 0;
+  std::uint32_t roFrameId = 0;
+  auto nROFs = rofs.size();
+  auto rofsPerWorker = nROFs / mNThreads;
 
-  auto runTracking = [&, this](auto& tracker, auto& event, auto& tempTracks) {
-    // tracking configuration parameters
+  auto loadData = [&, this](auto& trackerVec, auto& roFrameDataVec) {
     auto& trackingParam = MFTTrackingParam::Instance();
-
-    // snippet to convert found tracks to final output tracks with separate cluster indices
-    auto copyTracks = [&event](auto& new_tracks, auto& allTracks, auto& allClusIdx) {
-      for (auto& trc : new_tracks) {
-        trc.setExternalClusterIndexOffset(allClusIdx.size());
-        int ncl = trc.getNumberOfPoints();
-        for (int ic = 0; ic < ncl; ic++) {
-          auto externalClusterID = trc.getExternalClusterIndex(ic);
-          allClusIdx.push_back(externalClusterID);
-        }
-        allTracks.emplace_back(trc);
-      }
-    };
-
+    auto& tracker = trackerVec[0]; // Use first tracker to load the data: serial operation
     gsl::span<const unsigned char>::iterator pattIt = patterns.begin();
+
+    auto iROF = 0;
+
     for (auto& rof : rofs) {
-      mTimer[SWLoadData].Start(false);
-      int nclUsed = ioutils::loadROFrameData(rof, event, compClusters, pattIt, mDict, labels, tracker.get());
-      mTimer[SWLoadData].Stop();
-      int ntracksROF = 0, firstROFTrackEntry = allTracksMFT.size();
-      if (nclUsed) {
+      int worker = std::min(int(iROF / rofsPerWorker), mNThreads - 1);
+      // std::cout << "iROF = " << iROF << " ; nROFs = " << nROFs << " ; worker = " << worker << std::endl;
+      auto& roFrameData = roFrameDataVec[worker].emplace_back();
+      int nclUsed = ioutils::loadROFrameData(rof, roFrameData, compClusters, pattIt, mDict, labels, tracker.get());
 
-        mTimer[SWLoadData].Start(false);
-        event.setROFrameId(roFrame);
-        event.initialize(trackingParam.FullClusterScan);
-        LOG(debug) << "ROframe: " << roFrame << ", clusters loaded : " << nclUsed;
-        tracker->setROFrame(roFrame);
-        tracker->clearTracks();
-        mTimer[SWLoadData].Stop();
+      roFrameData.initialize(trackingParam.FullClusterScan);
+      LOG(debug) << "ROframeId: " << roFrameId << ", clusters loaded : " << nclUsed;
+      iROF++;
+    }
+  };
 
-        mTimer[SWFindLTFTracks].Start(false);
-        tracker->findLTFTracks(event);
-        mTimer[SWFindLTFTracks].Stop();
+  auto launchLTF = [](auto* tracker, auto* workerROFs) {
+    for (auto& rofData : *workerROFs) {
+      tracker->findLTFTracks(rofData);
+    }
+    return true;
+  };
 
-        mTimer[SWFindCATracks].Start(false);
-        tracker->findCATracks(event);
-        mTimer[SWFindCATracks].Stop();
+  auto launchCA = [](auto* tracker, auto* workerROFs) {
+    for (auto& rofData : *workerROFs) {
+      tracker->findCATracks(rofData);
+    }
+    return true;
+  };
 
-        mTimer[SWFitTracks].Start(false);
-        tracker->fitTracks(event);
-        mTimer[SWFitTracks].Stop();
+  auto launchFitter = [](auto* tracker, auto* workerROFs) {
+    for (auto& rofData : *workerROFs) {
+      tracker->fitTracks(rofData);
+    }
+    return true;
+  };
 
-        tempTracks.swap(event.getTracks());
-        ntracks += tempTracks.size();
+  auto runLTFTrackFinder = [&, this](auto& trackerVec, auto& roFrameDataVec) {
+    std::vector<std::future<bool>> finder;
+    for (int i = 0; i < mNThreads; i++) {
+      auto& tracker = trackerVec[i];
+      auto& workerData = roFrameDataVec[i];
+      auto f = std::async(std::launch::async, launchLTF, tracker.get(), &workerData);
+      finder.push_back(std::move(f));
+    }
 
-        if (mUseMC) {
-          mTimer[SWComputeLabels].Start(false);
-          tracker->computeTracksMClabels(tempTracks);
-          trackLabels.swap(tracker->getTrackLabels());
-          std::copy(trackLabels.begin(), trackLabels.end(), std::back_inserter(allTrackLabels));
-          trackLabels.clear();
-          mTimer[SWComputeLabels].Stop();
-        }
+    for (int i = 0; i < mNThreads; i++) {
+      finder[i].wait();
+    }
+  };
 
-        LOG(debug) << "Found MFT tracks: " << tracks.size();
-        ntracksROF = tempTracks.size();
-        copyTracks(tempTracks, allTracksMFT, allClusIdx);
+  auto runCATrackFinder = [&, this](auto& trackerVec, auto& roFrameDataVec) {
+    std::vector<std::future<bool>> finder;
+    for (int i = 0; i < mNThreads; i++) {
+      auto& tracker = trackerVec[i];
+      auto& workerData = roFrameDataVec[i];
+      auto f = std::async(std::launch::async, launchCA, tracker.get(), &workerData);
+      finder.push_back(std::move(f));
+    }
+
+    for (int i = 0; i < mNThreads; i++) {
+      finder[i].wait();
+    }
+  };
+
+  auto runTrackFitter = [&, this](auto& trackerVec, auto& roFrameDataVec) {
+    std::vector<std::future<bool>> fitter;
+    for (int i = 0; i < mNThreads; i++) {
+      auto& tracker = trackerVec[i];
+      auto& workerData = roFrameDataVec[i];
+      auto f = std::async(std::launch::async, launchFitter, tracker.get(), &workerData);
+      fitter.push_back(std::move(f));
+    }
+
+    for (int i = 0; i < mNThreads; i++) {
+      fitter[i].wait();
+    }
+  };
+
+  // snippet to convert found tracks to final output tracks with separate cluster indices
+  auto copyTracks = [](auto& new_tracks, auto& allTracks, auto& allClusIdx) {
+    for (auto& trc : new_tracks) {
+      trc.setExternalClusterIndexOffset(allClusIdx.size());
+      int ncl = trc.getNumberOfPoints();
+      for (int ic = 0; ic < ncl; ic++) {
+        auto externalClusterID = trc.getExternalClusterIndex(ic);
+        allClusIdx.push_back(externalClusterID);
       }
-      rof.setFirstEntry(firstROFTrackEntry);
-      rof.setNEntries(ntracksROF);
-      roFrame++;
+      allTracks.emplace_back(trc);
     }
   };
 
   if (mFieldOn) {
-    o2::mft::ROframe<TrackLTF> event(0);
-    runTracking(mTracker, event, tracks);
+
+    std::vector<std::vector<o2::mft::ROframe<TrackLTF>>> roFrameVec(mNThreads); // One vector of ROFrames per thread
+    std::vector<std::vector<TrackLTF>> tracksROFVec(mNThreads);
+    for (auto& rof : roFrameVec) {
+      rof.reserve(rofsPerWorker * 2);
+    }
+
+    mTimer[SWLoadData].Start(false);
+    loadData(mTrackerVec, roFrameVec);
+    mTimer[SWLoadData].Stop();
+
+    mTimer[SWFindLTFTracks].Start(false);
+    runLTFTrackFinder(mTrackerVec, roFrameVec);
+    mTimer[SWFindLTFTracks].Stop();
+
+    mTimer[SWFindCATracks].Start(false);
+    runCATrackFinder(mTrackerVec, roFrameVec);
+    mTimer[SWFindCATracks].Stop();
+
+    mTimer[SWFitTracks].Start(false);
+    runTrackFitter(mTrackerVec, roFrameVec);
+    mTimer[SWFitTracks].Stop();
+
+    if (mUseMC) {
+      mTimer[SWComputeLabels].Start(false);
+      auto& tracker = mTrackerVec[0];
+
+      for (int i = 0; i < mNThreads; i++) {
+        for (auto& rofData : roFrameVec[i]) {
+          tracker->computeTracksMClabels(rofData.getTracks());
+          trackLabels.swap(tracker->getTrackLabels());
+          std::copy(trackLabels.begin(), trackLabels.end(), std::back_inserter(allTrackLabels));
+          trackLabels.clear();
+        }
+      }
+      mTimer[SWComputeLabels].Stop();
+    }
+
+    auto rof = rofs.begin();
+
+    for (int i = 0; i < mNThreads; i++) {
+      for (auto& rofData : roFrameVec[i]) {
+        int ntracksROF = 0, firstROFTrackEntry = allTracksMFT.size();
+        tracks.swap(rofData.getTracks());
+        ntracksROF = tracks.size();
+        copyTracks(tracks, allTracksMFT, allClusIdx);
+
+        rof->setFirstEntry(firstROFTrackEntry);
+        rof->setNEntries(ntracksROF);
+        *rof++;
+        roFrameId++;
+      }
+    }
+
   } else {
-    o2::mft::ROframe<TrackLTFL> event(0);
-    runTracking(mTrackerL, event, tracksL);
+    std::vector<std::vector<o2::mft::ROframe<TrackLTFL>>> roFrameVec(mNThreads); // One vector of ROFrames per thread
+    std::vector<std::vector<TrackLTFL>> tracksROFVec(mNThreads);
+    for (auto& rof : roFrameVec) {
+      rof.reserve(rofsPerWorker);
+    }
+
+    mTimer[SWLoadData].Start(false);
+    loadData(mTrackerLVec, roFrameVec);
+    mTimer[SWLoadData].Stop();
+
+    mTimer[SWFindLTFTracks].Start(false);
+    runLTFTrackFinder(mTrackerLVec, roFrameVec);
+    mTimer[SWFindLTFTracks].Stop();
+
+    mTimer[SWFindCATracks].Start(false);
+    runCATrackFinder(mTrackerLVec, roFrameVec);
+    mTimer[SWFindCATracks].Stop();
+
+    mTimer[SWFitTracks].Start(false);
+    runTrackFitter(mTrackerLVec, roFrameVec);
+    mTimer[SWFitTracks].Stop();
+
+    if (mUseMC) {
+      mTimer[SWComputeLabels].Start(false);
+      auto& tracker = mTrackerLVec[0];
+
+      for (int i = 0; i < mNThreads; i++) {
+        for (auto& rofData : roFrameVec[i]) {
+          tracker->computeTracksMClabels(rofData.getTracks());
+          trackLabels.swap(tracker->getTrackLabels());
+          std::copy(trackLabels.begin(), trackLabels.end(), std::back_inserter(allTrackLabels));
+          trackLabels.clear();
+        }
+      }
+      mTimer[SWComputeLabels].Stop();
+    }
+
+    auto rof = rofs.begin();
+
+    for (int i = 0; i < mNThreads; i++) {
+      for (auto& rofData : roFrameVec[i]) {
+        int ntracksROF = 0, firstROFTrackEntry = allTracksMFT.size();
+        tracksL.swap(rofData.getTracks());
+        ntracksROF = tracks.size();
+        copyTracks(tracks, allTracksMFT, allClusIdx);
+
+        rof->setFirstEntry(firstROFTrackEntry);
+        rof->setNEntries(ntracksROF);
+        *rof++;
+        roFrameId++;
+      }
+    }
   }
 
   LOG(info) << "MFTTracker pushed " << allTracksMFT.size() << " tracks";
@@ -173,12 +307,12 @@ void TrackerDPL::run(ProcessingContext& pc)
     pc.outputs().snapshot(Output{"MFT", "TRACKSMCTR", 0, Lifetime::Timeframe}, allTrackLabels);
     pc.outputs().snapshot(Output{"MFT", "TRACKSMC2ROF", 0, Lifetime::Timeframe}, mc2rofs);
   }
+
+  mTimer[SWTot].Stop();
 }
 
 void TrackerDPL::endOfStream(EndOfStreamContext& ec)
 {
-  mTimer[SWTot].Stop();
-
   for (int i = 0; i < NStopWatches; i++) {
     LOGF(info, "Timing %18s: Cpu: %.3e s; Real: %.3e s in %d slots", TimerName[i], mTimer[i].CpuTime(), mTimer[i].RealTime(), mTimer[i].Counter() - 1);
   }
@@ -206,16 +340,20 @@ void TrackerDPL::updateTimeDependentParams(ProcessingContext& pc)
     if (Bz == 0 || trackingParam.forceZeroField) {
       LOG(info) << "Starting MFT Linear tracker: Field is off!";
       mFieldOn = false;
-      mTrackerL = std::make_unique<o2::mft::Tracker<TrackLTFL>>(mUseMC);
-      mTrackerL->initConfig(trackingParam, true);
-      mTrackerL->initialize(trackingParam.FullClusterScan);
+      for (auto i = 0; i < mNThreads; i++) {
+        auto& tracker = mTrackerLVec.emplace_back(std::make_unique<o2::mft::Tracker<TrackLTFL>>(mUseMC));
+        tracker->initConfig(trackingParam, !i);
+        tracker->initialize(trackingParam.FullClusterScan);
+      }
     } else {
-      LOG(info) << "Starting MFT tracker: Field is on!";
+      LOG(info) << "Starting MFT tracker: Field is on! Bz = " << Bz;
       mFieldOn = true;
-      mTracker = std::make_unique<o2::mft::Tracker<TrackLTF>>(mUseMC);
-      mTracker->setBz(Bz);
-      mTracker->initConfig(trackingParam, true);
-      mTracker->initialize(trackingParam.FullClusterScan);
+      for (auto i = 0; i < mNThreads; i++) {
+        auto& tracker = mTrackerVec.emplace_back(std::make_unique<o2::mft::Tracker<TrackLTF>>(mUseMC));
+        tracker->setBz(Bz);
+        tracker->initConfig(trackingParam, !i);
+        tracker->initialize(trackingParam.FullClusterScan);
+      }
     }
   }
 }
@@ -232,7 +370,7 @@ void TrackerDPL::finaliseCCDB(ConcreteDataMatcher& matcher, void* obj)
   }
 }
 
-DataProcessorSpec getTrackerSpec(bool useMC)
+DataProcessorSpec getTrackerSpec(bool useMC, int nThreads)
 {
   std::vector<InputSpec> inputs;
   inputs.emplace_back("compClusters", "MFT", "COMPCLUSTERS", 0, Lifetime::Timeframe);
@@ -263,7 +401,7 @@ DataProcessorSpec getTrackerSpec(bool useMC)
     "mft-tracker",
     inputs,
     outputs,
-    AlgorithmSpec{adaptFromTask<TrackerDPL>(ggRequest, useMC)},
+    AlgorithmSpec{adaptFromTask<TrackerDPL>(ggRequest, useMC, nThreads)},
     Options{}};
 }
 
