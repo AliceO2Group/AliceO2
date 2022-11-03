36aa79c7c64da4c350a65874c5e9747eb79d9e70
Markus Fasel <markus.fasel@cern.ch>
2022-11-03 20:10:40 +0100

    [FOCAL] Add stand-alone decoder of pad data

---
+++ Detectors/FOCAL/reconstruction/run/rawReaderPadRootify.cxx
@@ -0,0 +1,269 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file rawReaderFileNew.cxx
+/// \author Markus Fasel <markus.fasel@cern.ch>, Oak Ridge National Laboratory
+
+#include <iostream>
+#include <boost/program_options.hpp>
+#include <gsl/span>
+#include <fairlogger/Logger.h>
+
+#include <TFile.h>
+#include <TTree.h>
+
+#include "CommonConstants/Triggers.h"
+#include "DetectorsRaw/RawFileReader.h"
+#include "DetectorsRaw/RDHUtils.h"
+#include "FOCALReconstruction/PadWord.h"
+#include "FOCALReconstruction/PadDecoder.h"
+#include "Headers/RDHAny.h"
+
+namespace bpo = boost::program_options;
+
+// Data for tree
+struct PadTreeData {
+  static constexpr int NASICS = 20;
+  static constexpr int NCHANNELS = 72;
+  static constexpr int WINDUR = 20;
+  static constexpr int NTRIGGER = 8;
+
+  int mBCid;
+  int mOrbit;
+  unsigned int mHeader0[NASICS];
+  unsigned int mHeader1[NASICS];
+  int mASICNum[NASICS];
+  int mADC[NASICS][NCHANNELS];
+  int mTOA[NASICS][NCHANNELS];
+  int mTOT[NASICS][NCHANNELS];
+  int mCalib0[NASICS];
+  int mCalib1[NASICS];
+  int mTriggerhead0[NASICS][WINDUR];
+  int mTriggerhead1[NASICS][WINDUR];
+  int mTriggerdata[NASICS][NTRIGGER][WINDUR];
+
+  TTree* mTree = nullptr;
+
+  void connectTree(TTree* padtree)
+  {
+    mTree = padtree;
+    mTree->Branch("ORBIT", &mOrbit, "ORBIT/I");
+    mTree->Branch("BCID", &mBCid, "BCID/I");
+    mTree->Branch("HEADER0", &mHeader0, "HEADER0[20]/i");
+    mTree->Branch("HEADER1", &mHeader1, "HEADER1[20]/i");
+    mTree->Branch("ASIC", &mASICNum, "ASICNum[20]/I");
+    mTree->Branch("ADC", &mADC, "ADC[20][72]/I");
+    mTree->Branch("TOA", &mTOA, "TOA[20][72]/I");
+    mTree->Branch("TOT", &mTOT, "TOT[20][72]/I");
+    mTree->Branch("CALIB0", &mCalib0, "CALIB0[20]/I");
+    mTree->Branch("CALIB1", &mCalib1, "CALIB1[20]/I");
+    mTree->Branch("TRIGHEADER0", &mTriggerhead0, "TRIGHEADER0[20][20]/I");
+    mTree->Branch("TRIGHEADER1", &mTriggerhead1, "TRIGHEADER1[20][20]/I");
+    mTree->Branch("TRIGDATA", &mTriggerdata, "TRIGDATA[20][8][20]/I");
+  }
+
+  void reset()
+  {
+    mBCid = 0;
+    mOrbit = 0;
+    memset(mHeader0, 0, sizeof(unsigned int) * 20);
+    memset(mHeader1, 0, sizeof(unsigned int) * 20);
+    memset(mASICNum, 0, sizeof(int) * 20);
+    memset(mADC, 0, sizeof(int) * 20 * 72);
+    memset(mTOA, 0, sizeof(int) * 20 * 72);
+    memset(mTOT, 0, sizeof(int) * 20 * 72);
+    memset(mCalib0, 0, sizeof(int) * 20);
+    memset(mCalib1, 0, sizeof(int) * 20);
+    memset(mTriggerhead0, 0, sizeof(int) * 20 * 20);
+    memset(mTriggerhead1, 0, sizeof(int) * 20 * 20);
+    memset(mTriggerdata, 0, sizeof(int) * 20 * 8 * 20);
+  }
+
+  void setInteractionRecord(const o2::InteractionRecord& ir)
+  {
+    mBCid = ir.bc;
+    mOrbit = ir.orbit;
+  }
+
+  void fill(const o2::focal::PadData& data)
+  {
+    for (int iasic = 0; iasic < NASICS; iasic++) {
+      auto& asicdata = data.getDataForASIC(iasic);
+      auto& asicraw = asicdata.getASIC();
+      mASICNum[iasic] = iasic;
+      mHeader0[iasic] = asicraw.getFirstHeader().mData;
+      mHeader1[iasic] = asicraw.getSecondHeader().mData;
+      mCalib0[iasic] = asicraw.getFirstCalib().mADC;
+      mCalib1[iasic] = asicraw.getSecondCalib().mADC;
+      for (auto ichannel = 0; ichannel < NCHANNELS; ichannel++) {
+        mADC[iasic][ichannel] = asicraw.getChannel(ichannel).getADC();
+        mTOT[iasic][ichannel] = asicraw.getChannel(ichannel).getTOT();
+        mTOA[iasic][ichannel] = asicraw.getChannel(ichannel).getTOA();
+      }
+      auto triggerdata = asicdata.getTriggerWords();
+      for (auto iwin = 0; iwin < WINDUR; iwin++) {
+        mTriggerhead0[iasic][iwin] = triggerdata[iwin].mHeader;
+        mTriggerhead1[iasic][iwin] = triggerdata[iwin].mHeader;
+        mTriggerdata[iasic][0][iwin] = triggerdata[iwin].mTrigger0;
+        mTriggerdata[iasic][1][iwin] = triggerdata[iwin].mTrigger1;
+        mTriggerdata[iasic][2][iwin] = triggerdata[iwin].mTrigger2;
+        mTriggerdata[iasic][3][iwin] = triggerdata[iwin].mTrigger3;
+        mTriggerdata[iasic][4][iwin] = triggerdata[iwin].mTrigger4;
+        mTriggerdata[iasic][5][iwin] = triggerdata[iwin].mTrigger5;
+        mTriggerdata[iasic][6][iwin] = triggerdata[iwin].mTrigger6;
+        mTriggerdata[iasic][7][iwin] = triggerdata[iwin].mTrigger7;
+      }
+    }
+  }
+
+  void fillTree()
+  {
+    mTree->Fill();
+  }
+};
+
+void convertPadData(gsl::span<const char> padrawdata, const o2::InteractionRecord& currentir, PadTreeData& rootified)
+{
+  auto payloadsizeGBT = padrawdata.size() * sizeof(char) / sizeof(o2::focal::PadGBTWord);
+  auto gbtdata = gsl::span<const o2::focal::PadGBTWord>(reinterpret_cast<const o2::focal::PadGBTWord*>(padrawdata.data()), payloadsizeGBT);
+  o2::focal::PadDecoder decoder;
+
+  constexpr std::size_t EVENTSIZEPADGBT = 1180;
+  int nevents = gbtdata.size() / EVENTSIZEPADGBT;
+  for (int iev = 0; iev < nevents; iev++) {
+    decoder.reset();
+    rootified.reset();
+    decoder.decodeEvent(gbtdata.subspan(iev * EVENTSIZEPADGBT, EVENTSIZEPADGBT));
+    rootified.setInteractionRecord(currentir);
+    rootified.fill(decoder.getData());
+    rootified.fillTree();
+  }
+}
+
+int main(int argc, char** argv)
+{
+  bpo::variables_map vm;
+  bpo::options_description opt_general("Usage:\n  " + std::string(argv[0]) +
+                                       " <cmds/options>\n"
+                                       "  Tool will decode the DDLx data for EMCAL 0\n"
+                                       "Commands / Options");
+  bpo::options_description opt_hidden("");
+  bpo::options_description opt_all;
+  bpo::positional_options_description opt_pos;
+
+  try {
+    auto add_option = opt_general.add_options();
+    add_option("help,h", "Print this help message");
+    add_option("verbose,v", bpo::value<uint32_t>()->default_value(0), "Select verbosity level [0 = no output]");
+    add_option("version", "Print version information");
+    add_option("input-file,i", bpo::value<std::string>()->required(), "Specifies input file.");
+    add_option("output-file,o", bpo::value<std::string>()->default_value("FOCALPadData.root"), "Output file for rootified data");
+    add_option("debug,d", bpo::value<uint32_t>()->default_value(0), "Select debug output level [0 = no debug output]");
+
+    opt_all.add(opt_general).add(opt_hidden);
+    bpo::store(bpo::command_line_parser(argc, argv).options(opt_all).positional(opt_pos).run(), vm);
+
+    if (vm.count("help") || argc == 1) {
+      std::cout << opt_general << std::endl;
+      exit(0);
+    }
+
+    if (vm.count("version")) {
+      // std::cout << GitInfo();
+      exit(0);
+    }
+
+    bpo::notify(vm);
+  } catch (bpo::error& e) {
+    std::cerr << "ERROR: " << e.what() << std::endl
+              << std::endl;
+    std::cerr << opt_general << std::endl;
+    exit(1);
+  } catch (std::exception& e) {
+    std::cerr << e.what() << ", application will now exit" << std::endl;
+    exit(2);
+  }
+
+  auto rawfilename = vm["input-file"].as<std::string>();
+  auto rootfilename = vm["output-file"].as<std::string>();
+
+  o2::raw::RawFileReader reader;
+  reader.setDefaultDataOrigin(o2::header::gDataOriginFOC);
+  reader.setDefaultDataDescription(o2::header::gDataDescriptionRawData);
+  reader.setDefaultReadoutCardType(o2::raw::RawFileReader::RORC);
+  reader.addFile(rawfilename);
+  reader.init();
+
+  std::unique_ptr<TFile> rootfilewriter(TFile::Open(rootfilename.data(), "RECREATE"));
+  rootfilewriter->cd();
+  TTree* padtree = new TTree("PadData", "PadData");
+  PadTreeData rootified;
+  rootified.connectTree(padtree);
+
+  while (1) {
+    int tfID = reader.getNextTFToRead();
+    if (tfID >= reader.getNTimeFrames()) {
+      LOG(info) << "nothing left to read after " << tfID << " TFs read";
+      break;
+    }
+    std::vector<char> rawtf; // where to put extracted data
+    for (int il = 0; il < reader.getNLinks(); il++) {
+      auto& link = reader.getLink(il);
+
+      auto sz = link.getNextTFSize(); // size in bytes needed for the next TF of this link
+      rawtf.resize(sz);
+      link.readNextTF(rawtf.data());
+      gsl::span<char> dataBuffer(rawtf);
+
+      // Parse
+      std::vector<char> hbfbuffer;
+      int currentpos = 0;
+      o2::InteractionRecord currentir;
+      while (currentpos < dataBuffer.size()) {
+        auto rdh = reinterpret_cast<const o2::header::RDHAny*>(dataBuffer.data() + currentpos);
+        o2::raw::RDHUtils::printRDH(rdh);
+        if (o2::raw::RDHUtils::getMemorySize(rdh) == o2::raw::RDHUtils::getHeaderSize(rdh)) {
+          auto trigger = o2::raw::RDHUtils::getTriggerType(rdh);
+          if (trigger & o2::trigger::SOT || trigger & o2::trigger::HB) {
+            if (o2::raw::RDHUtils::getStop(rdh)) {
+              LOG(debug) << "Stop bit received - processing payload";
+              convertPadData(hbfbuffer, currentir, rootified);
+              hbfbuffer.clear();
+            } else {
+              LOG(debug) << "New HBF or Timeframe";
+              hbfbuffer.clear();
+              currentir.bc = o2::raw::RDHUtils::getTriggerBC(rdh);
+              currentir.orbit = o2::raw::RDHUtils::getTriggerOrbit(rdh);
+            }
+          }
+          currentpos += o2::raw::RDHUtils::getOffsetToNext(rdh);
+          continue;
+        }
+
+        // non-0 payload size:
+        auto payloadsize = o2::raw::RDHUtils::getMemorySize(rdh) - o2::raw::RDHUtils::getHeaderSize(rdh);
+        int endpoint = static_cast<int>(o2::raw::RDHUtils::getEndPointID(rdh));
+        LOG(debug) << "Next RDH: ";
+        LOG(debug) << "Found endpoint              " << endpoint;
+        LOG(debug) << "Found trigger BC:           " << o2::raw::RDHUtils::getTriggerBC(rdh);
+        LOG(debug) << "Found trigger Oribt:        " << o2::raw::RDHUtils::getTriggerOrbit(rdh);
+        LOG(debug) << "Found payload size:         " << payloadsize;
+        LOG(debug) << "Found offset to next:       " << o2::raw::RDHUtils::getOffsetToNext(rdh);
+        LOG(debug) << "Stop bit:                   " << (o2::raw::RDHUtils::getStop(rdh) ? "yes" : "no");
+        LOG(debug) << "Number of GBT words:        " << (payloadsize * sizeof(char) / sizeof(o2::focal::PadGBTWord));
+        auto page_payload = dataBuffer.subspan(currentpos + o2::raw::RDHUtils::getHeaderSize(rdh), payloadsize);
+        std::copy(page_payload.begin(), page_payload.end(), std::back_inserter(hbfbuffer));
+        currentpos += o2::raw::RDHUtils::getOffsetToNext(rdh);
+      }
+    }
+    reader.setNextTFToRead(++tfID);
+  }
+}
---
+++ Detectors/FOCAL/reconstruction/src/PadDecoder.cxx
@@ -0,0 +1,62 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#include <cstdint>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+
+#include <gsl/span>
+
+#include <fairlogger/Logger.h>
+#include "FOCALReconstruction/PadDecoder.h"
+#include "FOCALReconstruction/PadWord.h"
+
+using namespace o2::focal;
+
+void PadDecoder::reset()
+{
+  mData.reset();
+}
+
+void PadDecoder::decodeEvent(gsl::span<const PadGBTWord> gbtdata)
+{
+  LOG(debug) << "decoding pad data of size " << gbtdata.size() << "  GBT words - " << gbtdata.size() * sizeof(PadGBTWord) / sizeof(uint64_t) << " 64 bit words";
+  // first 39 GBT words : ASIC data
+  // Other words: Trigger data
+  std::size_t asicsize = 39 * PadData::NASICS;
+  auto asicwords = gbtdata.subspan(0, asicsize);
+  auto triggerwords = gbtdata.subspan(39, gbtdata.size() - asicsize);
+  for (int iasic = 0; iasic < PadData::NASICS; iasic++) {
+    // First part: ASIC words
+    auto& asicdata = mData[iasic].getASIC();
+    auto wordsthisAsic = asicwords.subspan(iasic * 39, 39);
+    auto headerwords = wordsthisAsic[0].getASICData<ASICHeader>();
+    asicdata.setFirstHeader(headerwords[0]);
+    asicdata.setSecondHeader(headerwords[1]);
+    int nchannels = 0;
+    for (auto& datawords : wordsthisAsic.subspan(1, 36)) {
+      for (auto& channelword : datawords.getASICData<ASICChannel>()) {
+        asicdata.setChannel(channelword, nchannels);
+        nchannels++;
+      }
+    }
+    asicdata.setCMNs(wordsthisAsic[37].getASICData<ASICChannel>());
+    asicdata.setCalibs(wordsthisAsic[38].getASICData<ASICChannel>());
+
+    // Second part: Trigger words
+    auto wordsTriggerThisAsic = triggerwords.subspan(iasic * mWin_dur, mWin_dur);
+    auto& asiccont = mData[iasic];
+    for (const auto trgword : wordsTriggerThisAsic) {
+      asiccont.appendTriggerWord(trgword.getTriggerData());
+    }
+  }
+}
