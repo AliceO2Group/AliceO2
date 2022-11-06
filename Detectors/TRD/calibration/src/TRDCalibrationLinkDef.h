e7b1cbbb29bf859458df0534debd40a71185c7da
Ole Schmidt <ole.schmidt@cern.ch>
2022-11-06 15:05:20 +0100

    TRD calib simplify aggregators for which input and container types are identical

--- Detectors/TRD/calibration/src/TRDCalibrationLinkDef.h
+++ Detectors/TRD/calibration/src/TRDCalibrationLinkDef.h
@@ -17,10 +17,10 @@
 
 #pragma link C++ class o2::trd::CalibratorVdExB + ;
 #pragma link C++ class o2::calibration::TimeSlot < o2::trd::AngularResidHistos> + ;
-#pragma link C++ class o2::calibration::TimeSlotCalibration < o2::trd::AngularResidHistos, o2::trd::AngularResidHistos> + ;
+#pragma link C++ class o2::calibration::TimeSlotCalibration < o2::trd::AngularResidHistos> + ;
 #pragma link C++ class o2::trd::CalibratorNoise + ;
 #pragma link C++ class o2::calibration::TimeSlot < o2::trd::PadAdcInfo> + ;
-#pragma link C++ class o2::calibration::TimeSlotCalibration < o2::trd::PadAdcInfo, o2::trd::PadAdcInfo> + ;
+#pragma link C++ class o2::calibration::TimeSlotCalibration < o2::trd::PadAdcInfo> + ;
 #pragma link C++ class o2::trd::TrackBasedCalib + ;
 #pragma link C++ class o2::trd::KrClusterFinder + ;
 #pragma link C++ class std::unordered_map < o2::dcs::DataPointIdentifier, o2::trd::TRDDCSMinMaxMeanInfo> + ;
