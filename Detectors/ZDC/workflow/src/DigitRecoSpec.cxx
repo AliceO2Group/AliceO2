019b555a0ad97caec050cc8d369d1497b86e1c99
Pietro Cortese <pietro.cortese@cern.ch>
2022-11-13 12:05:22 +0100

    Improved formatting

--- Detectors/ZDC/workflow/src/DigitRecoSpec.cxx
+++ Detectors/ZDC/workflow/src/DigitRecoSpec.cxx
@@ -262,7 +262,6 @@ void DigitRecoSpec::run(ProcessingContext& pc)
   }
   // TODO: rate information for all channels
   // TODO: summary of reconstruction to be collected by DQM?
-  ZDCTDCDataErr::print();
   pc.outputs().snapshot(Output{"ZDC", "BCREC", 0, Lifetime::Timeframe}, recEvent.mRecBC);
   pc.outputs().snapshot(Output{"ZDC", "ENERGY", 0, Lifetime::Timeframe}, recEvent.mEnergy);
   pc.outputs().snapshot(Output{"ZDC", "TDCDATA", 0, Lifetime::Timeframe}, recEvent.mTDCData);
