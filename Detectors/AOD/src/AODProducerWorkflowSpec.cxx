68c3f3641e5d7c6985cb4a479a77cdbec09ae915
Felix Schlepper <f3sch.git@outlook.com>
2022-11-28 08:50:38 +0100

    AOD: AODProducerWorkflowSpec: Add pattern information for TRD track
    This adds two bits of information to trdPattern (uint8_t) of extraInfo.
    
    Since only the lower 6 bits of the pattern is used used currently, we
    can add some track information "for free".
    
    Bit 6 describes whether a neighboring tracklet could be found during
    matching. This indicates a possible splitting of tracklet information.
    
    Bit 7 describes whether a tracklet crossed a padrow. This indicates a
    loss of charge information.
    
    Both bits can be used for tighter track selection and give some
    information about for example the trdSignal (e.g. PID).
    
    Signed-off-by: Felix Schlepper <f3sch.git@outlook.com>

--- Detectors/AOD/src/AODProducerWorkflowSpec.cxx
+++ Detectors/AOD/src/AODProducerWorkflowSpec.cxx
@@ -1204,6 +1204,12 @@ uint8_t AODProducerWorkflowDPL::getTRDPattern(const o2::trd::TrackTRD& track)
       pattern |= 0x1 << il;
     }
   }
+  if (track.getHasNeighbor()) {
+    pattern |= 0x1 << 6;
+  }
+  if (track.getHasPadrowCrossing()) {
+    pattern |= 0x1 << 7;
+  }
   return pattern;
 }
 
