06c5defccc5bee27eb7920b114fab3a0ccbca024
fmazzasc <fmazzasc@cern.ch>
2022-12-05 19:40:57 +0100

    Clear typo

--- Detectors/StrangenessTracking/tracking/src/StrangenessTracker.cxx
+++ Detectors/StrangenessTracking/tracking/src/StrangenessTracker.cxx
@@ -1,7 +1,7 @@
 // Copyright 2019-2020 CERN and copyright holders of ALICE O2.
 // See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
 // All rights not expressly granted are reserved.
-// StrangenessTracker
+//
 // This software is distributed under the terms of the GNU General Public
 // License v3 (GPL Version 3), copied verbatim in the file "COPYING".
 //
