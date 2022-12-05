AliceO2Group/AliceO2

commit abdeb25d3ded471cb845414a5025295af6434d0e

TRD: calibration: vDrift and ExB (#10053)
* TRD: CalibratorVdExB: Reuse previous fit result

This allows without interacting with the ccdb reusing the previous
fit result.
In the next patch I will a mechanism to skip updates for chambers
which have too few entries in a slot.

Signed-off-by: Felix Schlepper <felix.schlepper@cern.ch>

* TRD: CalibratorVdExB: Require minimum amount of entries in profile refit

This patch allows to specify an addition value via
`--min-entries-chamber` which set an requirement on the minimum amount
of entries required for refitting an updating the previous values.

Due to name clash, I renamed `--min-entries` to `--min-entries-total`
clarifying the distinction.

The next patch will finally allow pulling the initial fit values from the
ccdb e.g. the previous time the calibrator ran.

Signed-off-by: Felix Schlepper <felix.schlepper@cern.ch>

* TRD: CalibratorVdExB: Query if fit values from previous Run can be used.

If there was a run within the last ~hour, we can reuse the ccdb object
since object is still valid for the current time period.
Else we start from scratch.

Signed-off-by: Felix Schlepper <felix.schlepper@cern.ch>

* TRD: CalibratorVdExB: Made anodeplane a constant

Signed-off-by: Felix Schlepper <felix.schlepper@cern.ch>

* TRD: CalibratorVdExB: Clang-format on all touched files

This is just a minor patch on all files the previous patches touched.
Mostly comment alignment which IMHO are nicer.
Also some minor touch-ups on comments to allow doxygen to pickup as
proper documentation.

Signed-off-by: Felix Schlepper <felix.schlepper@cern.ch>
Signed-off-by: Felix Schlepper <f3sch.git@outlook.com>

* TRD: CalibratorVdExB: Reuse Fitter

This avoids instantiating a similar object all the time.
Now it is done once during the init and then just reused.
Hopefully, this provides a small performance boost.

Signed-off-by: Felix Schlepper <felix.schlepper@cern.ch>

Detectors/TRD/calibration/include/TRDCalibration/CalibratorVdExB.h
@@ -25,6 +25,7 @@
 
 #include "Rtypes.h"
 #include "TProfile.h"
+#include "Fit/Fitter.h"
 
 #include <array>
 #include <cstdlib>
@@ -38,11 +39,12 @@ struct FitFunctor {
   double operator()(const double* par) const;
   double calculateDeltaAlphaSim(double vdFit, double laFit, double impactAng) const;
   std::array<std::unique_ptr<TProfile>, constants::MAXCHAMBER> profiles; ///< profile histograms for each TRD chamber
-  double vdPreCorr;                                                      // TODO: these values should eventually be taken from CCDB
-  double laPreCorr;                                                      // TODO: these values should eventually be taken from CCDB
+  std::array<double, constants::MAXCHAMBER> vdPreCorr;                   ///< vDrift from previous Run
+  std::array<double, constants::MAXCHAMBER> laPreCorr;                   ///< LorentzAngle from previous Run
   int currDet;                                                           ///< the current TRD chamber number
-  float lowerBoundAngleFit;
-  float upperBoundAngleFit;
+  float lowerBoundAngleFit;                                              ///< lower bound for fit angle
+  float upperBoundAngleFit;                                              ///< upper bound for fit angle
+  double mAnodePlane;                                                    ///< distance of the TRD anode plane from the drift cathodes in m
 };
 
 class CalibratorVdExB final : public o2::calibration::TimeSlotCalibration<o2::trd::AngularResidHistos>
@@ -54,10 +56,10 @@ class CalibratorVdExB final : public o2::calibration::TimeSlotCalibration<o2::tr
     LA,
     VD
   };
-  CalibratorVdExB(size_t nMin = 40'000, bool enableOut = false) : mMinEntries(nMin), mEnableOutput(enableOut) {}
+  CalibratorVdExB(bool enableOut = false) : mEnableOutput(enableOut) {}
   ~CalibratorVdExB() final = default;
 
-  bool hasEnoughData(const Slot& slot) const final { return slot.getContainer()->getNEntries() >= mMinEntries; }
+  bool hasEnoughData(const Slot& slot) const final { return slot.getContainer()->getNEntries() >= mMinEntriesTotal; }
   void initOutput() final;
   void finalizeSlot(Slot& slot) final;
   Slot& emplaceNewSlot(bool front, TFType tStart, TFType tEnd) final;
@@ -67,14 +69,21 @@ class CalibratorVdExB final : public o2::calibration::TimeSlotCalibration<o2::tr
 
   void initProcessing();
 
+  /// Initialize the fit values once with the previous valid ones if they are
+  /// available.
+  void retrievePrev(o2::framework::ProcessingContext& pc);
+
  private:
-  bool mInitDone{false}; ///< flag to avoid creating the TProfiles multiple times
-  size_t mMinEntries; ///< minimum total number of angular deviations (on average ~3 entries per bin for each TRD chamber)
-  bool mEnableOutput; //< enable output of calibration fits and tprofiles in a root file instead of the ccdb
-  FitFunctor mFitFunctor; ///< used for minimization procedure
+  bool mInitDone{false};                             ///< flag to avoid creating the TProfiles multiple times
+  size_t mMinEntriesTotal;                           ///< minimum total number of angular deviations (on average ~3 entries per bin for each TRD chamber)
+  size_t mMinEntriesChamber;                         ///< minimum number of angular deviations per chamber for accepting refitted value (~3 per bin)
+  bool mEnableOutput;                                ///< enable output of calibration fits and tprofiles in a root file instead of the ccdb
+  FitFunctor mFitFunctor;                            ///< used for minimization procedure
   std::vector<o2::ccdb::CcdbObjectInfo> mInfoVector; ///< vector of CCDB infos; each element is filled with CCDB description of accompanying CCDB calibration object
   std::vector<o2::trd::CalVdriftExB> mObjectVector;  ///< vector of CCDB calibration objects; the extracted vDrift and ExB per chamber for given slot
-  ClassDefOverride(CalibratorVdExB, 2);
+  ROOT::Fit::Fitter mFitter;                         ///< Fitter object will be reused across slots
+  double mParamsStart[2];                            ///< Start fit parameter
+  ClassDefOverride(CalibratorVdExB, 3);
 };
 
 } // namespace trd

Detectors/TRD/calibration/src/CalibratorVdExB.cxx
@@ -14,17 +14,24 @@
 /// \author Ole Schmidt
 
 #include "TRDCalibration/CalibratorVdExB.h"
-#include "Fit/Fitter.h"
+#include "TRDCalibration/CalibrationParams.h"
+#include "Framework/ProcessingContext.h"
+#include "Framework/TimingInfo.h"
+#include "Framework/InputRecord.h"
+#include "DataFormatsTRD/Constants.h"
+#include "TRDBase/GeometryBase.h"
 #include "TStopwatch.h"
 #include "CCDB/CcdbApi.h"
 #include "CCDB/BasicCCDBManager.h"
-#include <string>
-#include <map>
-#include <memory>
 #include "CommonUtils/NameConf.h"
 #include "CommonUtils/MemFileHelper.h"
 #include <TFile.h>
 #include <TTree.h>
+
+#include <string>
+#include <map>
+#include <memory>
+#include <ctime>
 
 using namespace o2::trd::constants;
 
@@ -33,9 +40,6 @@ namespace o2::trd
 
 double FitFunctor::calculateDeltaAlphaSim(double vdFit, double laFit, double impactAng) const
 {
-
-  double trdAnodePlane = .0335; // distance of the TRD anode plane from the drift cathodes in [m]
-
   auto xDir = TMath::Cos(impactAng);
   auto yDir = TMath::Sin(impactAng);
   double slope = (TMath::Abs(xDir) < 1e-7) ? 1e7 : yDir / xDir;
@@ -43,20 +47,20 @@ double FitFunctor::calculateDeltaAlphaSim(double vdFit, double laFit, double imp
   double lorentzSlope = (TMath::Abs(laTan) < 1e-7) ? 1e7 : 1. / laTan;
 
   // hit point of incoming track with anode plane
-  double xAnodeHit = trdAnodePlane / slope;
-  double yAnodeHit = trdAnodePlane;
+  double xAnodeHit = mAnodePlane / slope;
+  double yAnodeHit = mAnodePlane;
 
   // hit point at anode plane of Lorentz angle shifted cluster from the entrance -> independent of true drift velocity
-  double xLorentzAnodeHit = trdAnodePlane / lorentzSlope;
-  double yLorentzAnodeHit = trdAnodePlane;
+  double xLorentzAnodeHit = mAnodePlane / lorentzSlope;
+  double yLorentzAnodeHit = mAnodePlane;
 
   // cluster location within drift cell of cluster from entrance after drift velocity ratio is applied
   double xLorentzDriftHit = xLorentzAnodeHit;
-  double yLorentzDriftHit = trdAnodePlane - trdAnodePlane * (vdPreCorr / vdFit);
+  double yLorentzDriftHit = mAnodePlane - mAnodePlane * (vdPreCorr[currDet] / vdFit);
 
   // reconstructed hit of first cluster at chamber entrance after pre Lorentz angle correction
-  double xLorentzDriftHitPreCorr = xLorentzAnodeHit - (trdAnodePlane - yLorentzDriftHit) * TMath::Tan(laPreCorr);
-  double yLorentzDriftHitPreCorr = trdAnodePlane - trdAnodePlane * (vdPreCorr / vdFit);
+  double xLorentzDriftHitPreCorr = xLorentzAnodeHit - (mAnodePlane - yLorentzDriftHit) * TMath::Tan(laPreCorr[currDet]);
+  double yLorentzDriftHitPreCorr = mAnodePlane - mAnodePlane * (vdPreCorr[currDet] / vdFit);
 
   double impactAngleSim = TMath::ATan2(yAnodeHit, xAnodeHit);
 
@@ -101,14 +105,61 @@ void CalibratorVdExB::initProcessing()
   if (mInitDone) {
     return;
   }
+
   mFitFunctor.lowerBoundAngleFit = 80 * TMath::DegToRad();
   mFitFunctor.upperBoundAngleFit = 100 * TMath::DegToRad();
-  mFitFunctor.vdPreCorr = 1.546;    // TODO: will be taken from CCDB in the future
-  mFitFunctor.laPreCorr = 0.;       // TODO: will be taken from CCDB in the future
+  mFitFunctor.mAnodePlane = GeometryBase::camHght() / (2.f * 100.f);
   for (int iDet = 0; iDet < MAXCHAMBER; ++iDet) {
     mFitFunctor.profiles[iDet] = std::make_unique<TProfile>(Form("profAngleDiff_%i", iDet), Form("profAngleDiff_%i", iDet), NBINSANGLEDIFF, -MAXIMPACTANGLE, MAXIMPACTANGLE);
   }
+
+  mFitter.SetFCN<FitFunctor>(2, mFitFunctor, mParamsStart);
+  mFitter.Config().ParSettings(ParamIndex::LA).SetLimits(-0.7, 0.7);
+  mFitter.Config().ParSettings(ParamIndex::LA).SetStepSize(.01);
+  mFitter.Config().ParSettings(ParamIndex::VD).SetLimits(0.01, 3.);
+  mFitter.Config().ParSettings(ParamIndex::VD).SetStepSize(.01);
+  ROOT::Math::MinimizerOptions opt;
+  opt.SetMinimizerType("Minuit2");
+  opt.SetMinimizerAlgorithm("Migrad");
+  opt.SetPrintLevel(0);
+  opt.SetMaxFunctionCalls(1'000);
+  opt.SetTolerance(.001);
+  mFitter.Config().SetMinimizerOptions(opt);
+
   mInitDone = true;
+}
+
+void CalibratorVdExB::retrievePrev(o2::framework::ProcessingContext& pc)
+{
+  static bool doneOnce = false;
+  if (!doneOnce) {
+    doneOnce = true;
+    mFitFunctor.vdPreCorr.fill(constants::VDRIFTDEFAULT);
+    mFitFunctor.laPreCorr.fill(constants::EXBDEFAULT);
+    // We either get a pointer to a valid object from the last ~hour or to the default object
+    // which is always present. The first has precedence over the latter.
+    auto dataCalVdriftExB = pc.inputs().get<o2::trd::CalVdriftExB*>("calvdexb");
+    std::string msg = "Default Object";
+    // We check if the object we got is the default one by comparing it to the defaults.
+    for (int iDet = 0; iDet < MAXCHAMBER; ++iDet) {
+      if (dataCalVdriftExB->getVdrift(iDet) != constants::VDRIFTDEFAULT ||
+          dataCalVdriftExB->getExB(iDet) != constants::EXBDEFAULT) {
+        msg = "Previous Object";
+        break;
+      }
+    }
+    LOG(info) << "Calibrator: From CCDB retrieved " << msg;
+
+    // Here we set each entry regardless if it is the default or not.
+    for (int iDet = 0; iDet < MAXCHAMBER; ++iDet) {
+      mFitFunctor.laPreCorr[iDet] = dataCalVdriftExB->getExB(iDet);
+      mFitFunctor.vdPreCorr[iDet] = dataCalVdriftExB->getVdrift(iDet);
+    }
+
+    auto& params = TRDCalibParams::Instance();
+    mMinEntriesChamber = params.minEntriesChamber;
+    mMinEntriesTotal = params.minEntriesTotal;
+  }
 }
 
 void CalibratorVdExB::finalizeSlot(Slot& slot)
@@ -116,42 +167,39 @@ void CalibratorVdExB::finalizeSlot(Slot& slot)
   // do actual calibration for the data provided in the given slot
   TStopwatch timer;
   timer.Start();
-  std::array<float, MAXCHAMBER> laFitResults{};
-  std::array<float, MAXCHAMBER> vdFitResults{};
-  auto residHists = slot.getContainer();
   initProcessing();
+  auto laFitResults = mFitFunctor.laPreCorr;
+  auto vdFitResults = mFitFunctor.vdPreCorr;
+  auto residHists = slot.getContainer();
   for (int iDet = 0; iDet < MAXCHAMBER; ++iDet) {
+    int sumEntries = 0;
     mFitFunctor.profiles[iDet]->Reset();
     mFitFunctor.currDet = iDet;
     for (int iBin = 0; iBin < NBINSANGLEDIFF; ++iBin) {
       // fill profiles
       auto angleDiffSum = residHists->getHistogramEntry(iDet * NBINSANGLEDIFF + iBin);
       auto nEntries = residHists->getBinCount(iDet * NBINSANGLEDIFF + iBin);
+      sumEntries += nEntries;
       if (nEntries > 0) {
         mFitFunctor.profiles[iDet]->Fill(2 * iBin - MAXIMPACTANGLE, angleDiffSum / nEntries, nEntries);
       }
     }
-    ROOT::Fit::Fitter fitter;
-    double paramsStart[2];
-    paramsStart[ParamIndex::LA] = 0. * TMath::DegToRad();
-    paramsStart[ParamIndex::VD] = 1.;
-    fitter.SetFCN<FitFunctor>(2, mFitFunctor, paramsStart);
-    fitter.Config().ParSettings(ParamIndex::LA).SetLimits(-0.7, 0.7);
-    fitter.Config().ParSettings(ParamIndex::LA).SetStepSize(.01);
-    fitter.Config().ParSettings(ParamIndex::VD).SetLimits(0., 3.);
-    fitter.Config().ParSettings(ParamIndex::VD).SetStepSize(.01);
-    ROOT::Math::MinimizerOptions opt;
-    opt.SetMinimizerType("Minuit2");
-    opt.SetMinimizerAlgorithm("Migrad");
-    opt.SetPrintLevel(0);
-    opt.SetMaxFunctionCalls(1'000);
-    opt.SetTolerance(.001);
-    fitter.Config().SetMinimizerOptions(opt);
-    fitter.FitFCN();
-    auto fitResult = fitter.Result();
+    // Check if we have the minimum amount of entries
+    if (sumEntries < mMinEntriesChamber) {
+      LOGF(debug, "Chamber %d did not reach minimum amount of entries for refit", iDet);
+      continue;
+    }
+    // Reset Start Parameter
+    mParamsStart[ParamIndex::LA] = 0.0;
+    mParamsStart[ParamIndex::VD] = 1.0;
+    mFitter.FitFCN();
+    auto fitResult = mFitter.Result();
     laFitResults[iDet] = fitResult.Parameter(ParamIndex::LA);
     vdFitResults[iDet] = fitResult.Parameter(ParamIndex::VD);
     LOGF(debug, "Fit result for chamber %i: vd=%f, la=%f", iDet, vdFitResults[iDet], laFitResults[iDet] * TMath::RadToDeg());
+    // Update fit values for next fit
+    mFitFunctor.laPreCorr[iDet] = laFitResults[iDet];
+    mFitFunctor.vdPreCorr[iDet] = vdFitResults[iDet];
   }
   timer.Stop();
   LOGF(info, "Done fitting angular residual histograms. CPU time: %f, real time: %f", timer.CpuTime(), timer.RealTime());
@@ -186,8 +234,8 @@ void CalibratorVdExB::finalizeSlot(Slot& slot)
   // assemble CCDB object
   CalVdriftExB calObject;
   for (int iDet = 0; iDet < MAXCHAMBER; ++iDet) {
-    // OS: what about chambers for which we had no data in this slot? should we use the initial parameters or something else?
-    // maybe it is better not to overwrite an older result if we don't have anything better?
+    // For Chambers which did not have the minimum amount of entries in this slot e.g. missing, empty chambers.
+    // We will reuse the prevoius one. This would have been read either from the ccdb or come from a previous successful fit.
     calObject.setVdrift(iDet, vdFitResults[iDet]);
     calObject.setExB(iDet, laFitResults[iDet]);
   }

commit c81e5ced3bfcce82755682295839d79603818ba5

TRD: CalibratorVdExB: Fix minimal entries init
Fixes a bug, which was introduced in PR #10053, where both field for
minimal entries were not properly initialized. initProcessing would only
be called once hasEnoughData evaluates to true, hence minEntriesTotal
would never be properly set. While running locally these probably init
to 0. However this caused problems while running on the grid.

Signed-off-by: Felix Schlepper <f3sch.git@outlook.com>

Detectors/TRD/calibration/include/TRDCalibration/CalibratorVdExB.h
@@ -22,6 +22,7 @@
 #include "DataFormatsTRD/AngularResidHistos.h"
 #include "CCDB/CcdbObjectInfo.h"
 #include "DataFormatsTRD/CalVdriftExB.h"
+#include "TRDCalibration/CalibrationParams.h"
 
 #include "Rtypes.h"
 #include "TProfile.h"
@@ -85,19 +86,20 @@ class CalibratorVdExB final : public o2::calibration::TimeSlotCalibration<o2::tr
   void retrievePrev(o2::framework::ProcessingContext& pc);
 
  private:
-  bool mInitDone{false};                             ///< flag to avoid creating the TProfiles multiple times
-  size_t mMinEntriesTotal;                           ///< minimum total number of angular deviations (on average ~3 entries per bin for each TRD chamber)
-  size_t mMinEntriesChamber;                         ///< minimum number of angular deviations per chamber for accepting refitted value (~3 per bin)
-  bool mEnableOutput;                                ///< enable output of calibration fits and tprofiles in a root file instead of the ccdb
-  std::unique_ptr<TFile> mOutFile{nullptr};          ///< output file
-  std::unique_ptr<TTree> mOutTree{nullptr};          ///< output tree
-  FitFunctor mFitFunctor;                            ///< used for minimization procedure
-  std::vector<o2::ccdb::CcdbObjectInfo> mInfoVector; ///< vector of CCDB infos; each element is filled with CCDB description of accompanying CCDB calibration object
-  std::vector<o2::trd::CalVdriftExB> mObjectVector;  ///< vector of CCDB calibration objects; the extracted vDrift and ExB per chamber for given slot
-  ROOT::Fit::Fitter mFitter;                         ///< Fitter object will be reused across slots
-  double mParamsStart[2];                            ///< Start fit parameter
+  bool mInitDone{false};                                     ///< flag to avoid creating the TProfiles multiple times
+  const TRDCalibParams& mParams{TRDCalibParams::Instance()}; ///< reference to calibration parameters
+  size_t mMinEntriesTotal{mParams.minEntriesChamber};        ///< minimum total number of angular deviations (on average ~3 entries per bin for each TRD chamber)
+  size_t mMinEntriesChamber{mParams.minEntriesTotal};        ///< minimum number of angular deviations per chamber for accepting refitted value (~3 per bin)
+  bool mEnableOutput;                                        ///< enable output of calibration fits and tprofiles in a root file instead of the ccdb
+  std::unique_ptr<TFile> mOutFile{nullptr};                  ///< output file
+  std::unique_ptr<TTree> mOutTree{nullptr};                  ///< output tree
+  FitFunctor mFitFunctor;                                    ///< used for minimization procedure
+  std::vector<o2::ccdb::CcdbObjectInfo> mInfoVector;         ///< vector of CCDB infos; each element is filled with CCDB description of accompanying CCDB calibration object
+  std::vector<o2::trd::CalVdriftExB> mObjectVector;          ///< vector of CCDB calibration objects; the extracted vDrift and ExB per chamber for given slot
+  ROOT::Fit::Fitter mFitter;                                 ///< Fitter object will be reused across slots
+  double mParamsStart[2];                                    ///< Start fit parameter
 
-  ClassDefOverride(CalibratorVdExB, 4);
+  ClassDefOverride(CalibratorVdExB, 5);
 };
 
 } // namespace trd

Detectors/TRD/calibration/src/CalibratorVdExB.cxx
@@ -14,7 +14,6 @@
 /// \author Ole Schmidt
 
 #include "TRDCalibration/CalibratorVdExB.h"
-#include "TRDCalibration/CalibrationParams.h"
 #include "Framework/ProcessingContext.h"
 #include "Framework/TimingInfo.h"
 #include "Framework/InputRecord.h"
@@ -134,11 +133,6 @@ void CalibratorVdExB::initProcessing()
       mOutTree->Branch(fmt::format("residuals_{:d}", iDet).c_str(), mFitFunctor.profiles[iDet].get());
     }
   }
-
-  // get parameters
-  auto& params = TRDCalibParams::Instance();
-  mMinEntriesChamber = params.minEntriesChamber;
-  mMinEntriesTotal = params.minEntriesTotal;
 
   mInitDone = true;
 }