7d032e326c259bef4b00e162909b641bd6e03893
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2022-12-15 08:59:00 +0100

    DPL: provide helpers to navigate O2 Data Model

---
+++ Framework/Core/test/test_O2DataModelHelpers.cxx
@@ -0,0 +1,103 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#define BOOST_TEST_MODULE Test O2DataModelHelpers
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_DYN_LINK
+
+#include "Framework/O2DataModelHelpers.h"
+#include "Framework/DataProcessingHeader.h"
+#include "Headers/DataHeader.h"
+#include "Headers/Stack.h"
+#include <fairmq/TransportFactory.h>
+#include <boost/test/unit_test.hpp>
+
+using namespace o2::framework;
+
+BOOST_AUTO_TEST_CASE(TestNoWait)
+{
+  o2::header::DataHeader dh1;
+  dh1.dataDescription = "CLUSTERS";
+  dh1.dataOrigin = "TPC";
+  dh1.subSpecification = 0;
+  dh1.splitPayloadIndex = 0;
+  dh1.splitPayloadParts = 0;
+
+  o2::header::DataHeader dh2;
+  dh2.dataDescription = "CLUSTERS";
+  dh2.dataOrigin = "ITS";
+  dh2.subSpecification = 0;
+  dh2.splitPayloadIndex = 0;
+  dh2.splitPayloadParts = 0;
+
+  DataProcessingHeader dph1{0, 1};
+  DataProcessingHeader dph2{0, 1};
+  auto transport = fair::mq::TransportFactory::CreateTransportFactory("zeromq");
+  std::array<fair::mq::MessagePtr, 2> messages;
+  auto channelAlloc = o2::pmr::getTransportAllocator(transport.get());
+  fair::mq::Parts inputs {
+    o2::pmr::getMessage(o2::header::Stack{channelAlloc, dh1, dph1}),
+    transport->CreateMessage(1000),
+    o2::pmr::getMessage(o2::header::Stack{channelAlloc, dh2, dph2}),
+    transport->CreateMessage(1000)
+  };
+  // Check if any header has dataDescription == "CLUSTERS"
+  BOOST_CHECK(O2DataModelHelpers::all_headers_matching(inputs, [](auto const& header) {
+    return header != nullptr && header->dataDescription == o2::header::DataDescription("CLUSTERS");
+  }));
+
+  BOOST_CHECK(O2DataModelHelpers::any_header_matching(inputs, [](auto const& header) {
+    return header != nullptr && header->dataOrigin == o2::header::DataOrigin("ITS");
+  }));
+
+  BOOST_CHECK(O2DataModelHelpers::any_header_matching(inputs, [](auto const& header) {
+    return header != nullptr && header->dataOrigin == o2::header::DataOrigin("TPC");
+  }));
+
+  dh2.splitPayloadParts = 2;
+  o2::header::DataHeader dh3;
+  dh3.dataDescription = "TRACKS";
+  dh3.dataOrigin = "ITS";
+  dh3.subSpecification = 0;
+  dh3.splitPayloadIndex = 0;
+  dh3.splitPayloadParts = 0;
+  DataProcessingHeader dph3{0, 1};
+
+  fair::mq::Parts inputs2 {
+    o2::pmr::getMessage(o2::header::Stack{channelAlloc, dh1, dph1}),
+    transport->CreateMessage(1000),
+    o2::pmr::getMessage(o2::header::Stack{channelAlloc, dh2, dph2}),
+    transport->CreateMessage(1000),
+    transport->CreateMessage(1000),
+    o2::pmr::getMessage(o2::header::Stack{channelAlloc, dh3, dph3}),
+    transport->CreateMessage(1000),
+  };
+
+  BOOST_CHECK(O2DataModelHelpers::all_headers_matching(inputs, [](auto const& header) {
+    return header != nullptr && header->dataDescription == o2::header::DataDescription("CLUSTERS");
+  }));
+
+  BOOST_CHECK(O2DataModelHelpers::any_header_matching(inputs, [](auto const& header) {
+    return header != nullptr && header->dataOrigin == o2::header::DataOrigin("ITS");
+  }));
+
+  BOOST_CHECK(O2DataModelHelpers::any_header_matching(inputs, [](auto const& header) {
+    return header != nullptr && header->dataDescription == o2::header::DataDescription("TRACKS");
+  }) == false);
+
+  BOOST_CHECK(O2DataModelHelpers::any_header_matching(inputs2, [](auto const& header) {
+    return header != nullptr && header->dataDescription == o2::header::DataDescription("TRACKS");
+  }));
+
+  BOOST_CHECK(O2DataModelHelpers::all_headers_matching(inputs2, [](auto const& header) {
+    return header != nullptr && header->subSpecification == 0;
+  }));
+}
