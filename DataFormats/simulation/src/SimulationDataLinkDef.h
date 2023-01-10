741c0cda6ca539d55a64dbc4c63725cd7cbcb20a
Benedikt Volkel <benedikt.volkel@cern.ch>
2023-01-10 15:59:48 +0100

    Refine and add utilities for MCGenStatus
    * abort if HepMC and native generator status are
      not encoded properly
    
    * however, encode automatically if not yet done
      in GeneratorTGenerator::importParticles
    
    * add helper function (static method)
      o2::mcutil::MCGenHelper::encodeParticleStatusAndTracking
      which can be used whenever TParticles are pushed to
      Generator::mParticles manually or
      when a class overrides Generator::importParticles()
    
    * override FairPrimaryGenerator::AddTrack to have full control
      over tracks that are added by non-O2 generators
      (e.g. the BoxGen we are using is FairBoxGenerator)
    
    * some minor updates

--- DataFormats/simulation/src/SimulationDataLinkDef.h
+++ DataFormats/simulation/src/SimulationDataLinkDef.h
@@ -45,7 +45,7 @@
 #pragma link C++ class o2::BasicXYZQHit < double, double, int> + ;
 #pragma link C++ struct o2::dataformats::MCTruthHeaderElement + ;
 #pragma link C++ class o2::dataformats::MCTruthContainer < long> + ;
-#pragma link C++ class o2::dataformats::MCTruthContainer < o2::MCCompLabel > -;
+#pragma link C++ class o2::dataformats::MCTruthContainer < o2::MCCompLabel> - ;
 #pragma link C++ class std::vector < o2::dataformats::MCTruthContainer < o2::MCCompLabel>> + ;
 #pragma link C++ class std::vector < o2::MCCompLabel> + ;
 #pragma link C++ class std::vector < o2::MCEventLabel> + ;
@@ -71,6 +71,7 @@
 #pragma link C++ class o2::dataformats::IOMCTruthContainerView + ;
 
 #pragma link C++ class o2::mcutils::MCTrackNavigator + ;
+#pragma link C++ class o2::mcutils::MCGenHelper + ;
 #pragma link C++ class o2::O2DatabasePDG + ;
 
 #endif
