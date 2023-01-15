0c848072fbd44bd68f536770e2ea97c6f04ba333
lietava <roman.lietava@cern.ch>
2023-01-15 11:57:13 +0100

    fix: copyright added

--- Detectors/CTP/macro/TestConfig.C
+++ Detectors/CTP/macro/TestConfig.C
@@ -1,10 +1,21 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
 #if !defined(__CLING__) || defined(__ROOTCLING__)
 #include <CCDB/BasicCCDBManager.h>
 #include <DataFormatsCTP/Configuration.h>
 #endif
 using namespace o2::ctp;
 
-void TestConfig(bool test = 1)
+void TestConfig(bool test = 0)
 {
   if (test == 0) {
     return;
