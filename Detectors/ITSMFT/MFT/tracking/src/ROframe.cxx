26ffcfae70d2882366cbef0b91e4a2fb98a3257e
shahoian <ruben.shahoyan@cern.ch>
2023-01-27 00:41:06 +0100

    Revert "Upgrade MFT track finder"
    This reverts commit 78ed333a727b2fc3260e92e7af9b31114fcf8306.

--- Detectors/ITSMFT/MFT/tracking/src/ROframe.cxx
+++ Detectors/ITSMFT/MFT/tracking/src/ROframe.cxx
@@ -31,6 +31,51 @@ Int_t ROframe<T>::getTotalClusters() const
   return Int_t(totalClusters);
 }
 
+template <typename T>
+void ROframe<T>::initialize(bool fullClusterScan)
+{
+  if (!fullClusterScan) {
+    sortClusters();
+  }
+}
+
+template <typename T>
+void ROframe<T>::sortClusters()
+{
+  Int_t nClsInLayer, binPrevIndex, clsMinIndex, clsMaxIndex, jClsLayer;
+  // sort the clusters in R-Phi
+  for (Int_t iLayer = 0; iLayer < constants::mft::LayersNumber; ++iLayer) {
+    if (mClusters[iLayer].size() == 0) {
+      continue;
+    }
+    // sort clusters in layer according to the bin index
+    sort(mClusters[iLayer].begin(), mClusters[iLayer].end(),
+         [](Cluster& c1, Cluster& c2) { return c1.indexTableBin < c2.indexTableBin; });
+    // find the cluster local index range in each bin
+    // index = element position in the vector
+    nClsInLayer = mClusters[iLayer].size();
+    binPrevIndex = mClusters[iLayer].at(0).indexTableBin;
+    clsMinIndex = 0;
+    for (jClsLayer = 1; jClsLayer < nClsInLayer; ++jClsLayer) {
+      if (mClusters[iLayer].at(jClsLayer).indexTableBin == binPrevIndex) {
+        continue;
+      }
+
+      clsMaxIndex = jClsLayer - 1;
+
+      mClusterBinIndexRange[iLayer][binPrevIndex] = std::pair<Int_t, Int_t>(clsMinIndex, clsMaxIndex);
+
+      binPrevIndex = mClusters[iLayer].at(jClsLayer).indexTableBin;
+      clsMinIndex = jClsLayer;
+    } // clusters
+
+    // last cluster
+    clsMaxIndex = jClsLayer - 1;
+
+    mClusterBinIndexRange[iLayer][binPrevIndex] = std::pair<Int_t, Int_t>(clsMinIndex, clsMaxIndex);
+  } // layers
+}
+
 template class ROframe<o2::mft::TrackLTF>;
 template class ROframe<o2::mft::TrackLTFL>;
 
