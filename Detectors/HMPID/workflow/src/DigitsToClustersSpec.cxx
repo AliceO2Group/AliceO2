1b1393d29b816db0f7c53a5e79f59d8d2439053e
Giacomo Volpe <giacomo.volpe@cern.ch>
2023-01-27 18:39:37 +0100

    Clusterization workflow updeated

--- Detectors/HMPID/workflow/src/DigitsToClustersSpec.cxx
+++ Detectors/HMPID/workflow/src/DigitsToClustersSpec.cxx
@@ -9,42 +9,33 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-/// \file   DigitsToRootSpec.cxx
-/// \author Antonio Franco - INFN Bari
-/// \version 1.0
-/// \date 01 feb 2021
-/// \brief Implementation of a data processor to Clusterize the Digits
-///
-
-#include <random>
-#include <iostream>
-#include <fstream>
-#include <stdexcept>
-#include <array>
-#include <functional>
-#include <vector>
+/// \file DigitsToClustersSpec.cxx
+/// \brief Implementation of clusterization for HMPID; read upstream/from file write upstream/to file
 
 #include "HMPIDWorkflow/DigitsToClustersSpec.h"
+
+#include <array>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <random>
+#include <stdexcept>
+#include <vector>
+
 #include "Framework/CallbackService.h"
 #include "Framework/ConfigParamRegistry.h"
 #include "Framework/ControlService.h"
-#include "Framework/DataProcessorSpec.h"
-#include "Framework/Lifetime.h"
-#include "Framework/Output.h"
-#include "Framework/Task.h"
-#include "Framework/WorkflowSpec.h"
-#include "Framework/Logger.h"
 #include "Framework/DataRefUtils.h"
 #include "Framework/InputRecordWalker.h"
+#include "Framework/Lifetime.h"
+#include "Framework/Logger.h"
+#include "Framework/Output.h"
 
-#include "Headers/RAWDataHeader.h"
-#include "DetectorsRaw/RDHUtils.h"
 #include "DPLUtils/DPLRawParser.h"
+#include "DetectorsRaw/RDHUtils.h"
+#include "Headers/RAWDataHeader.h"
 
-#include "DataFormatsHMP/Digit.h"
-#include "DataFormatsHMP/Trigger.h"
-#include "DataFormatsHMP/Cluster.h"
-#include "HMPIDBase/Geo.h"
+#include "CommonUtils/NameConf.h" // ef : o2::utils::Str
 
 namespace o2
 {
@@ -56,8 +47,11 @@ using namespace o2::header;
 using namespace o2::framework;
 using RDH = o2::header::RDHAny;
 
-// Splits a string in float array for string delimiter, TODO: Move this in a HMPID common library
-void DigitsToClustersTask::strToFloatsSplit(std::string s, std::string delimiter, float* res, int maxElem)
+// Splits a string in float array for string delimiter, TODO: Move this in a
+// HMPID common library
+void DigitsToClustersTask::strToFloatsSplit(std::string s,
+                                            std::string delimiter, float* res,
+                                            int maxElem)
 {
   int index = 0;
   size_t pos_start = 0;
@@ -73,71 +67,209 @@ void DigitsToClustersTask::strToFloatsSplit(std::string s, std::string delimiter
     }
   }
   res[index++] = (std::stof(s.substr(pos_start)));
+  return;
 }
 
 //=======================
 //
 void DigitsToClustersTask::init(framework::InitContext& ic)
 {
-  LOG(info) << "[HMPID Clusterization - init() ] ";
+  LOG(info) << "[HMPID Clusterization - init() ; mReadFile = ] "
+            << mReadFile;
   mSigmaCutPar = ic.options().get<std::string>("sigma-cut");
+
   if (mSigmaCutPar != "") {
     strToFloatsSplit(mSigmaCutPar, ",", mSigmaCut, 7);
   }
 
-  mDigitsReceived = 0;
-  mRec = new o2::hmpid::Clusterer();
+  mDigitsReceived, mClustersReceived = 0;
+
+  mRec.reset(new o2::hmpid::Clusterer()); // ef: changed to smart-pointer
+
   mExTimer.start();
+
+  // specify location and filename for output in case of writing to file
+  if (mReadFile) {
+    // Build the file name
+    const auto filename = o2::utils::Str::concat_string(
+      o2::utils::Str::rectifyDirectory(
+        ic.options().get<std::string>("input-dir")),
+      ic.options().get<std::string>("hmpid-digit-infile"));
+    initFileIn(filename);
+  }
 }
 
 void DigitsToClustersTask::run(framework::ProcessingContext& pc)
 {
-  LOG(debug) << "[HMPID DClusterization - run() ] Enter ...";
-  auto triggers = pc.inputs().get<gsl::span<o2::hmpid::Trigger>>("intrecord");
-  auto digits = pc.inputs().get<gsl::span<o2::hmpid::Digit>>("digits");
+  // outputs
   std::vector<o2::hmpid::Cluster> clusters;
   std::vector<o2::hmpid::Trigger> clusterTriggers;
+  LOG(info) << "[HMPID DClusterization - run() ] Enter ...";
+  clusters.clear();
+  clusterTriggers.clear();
 
-  for (const auto& trig : triggers) {
-    if (trig.getNumberOfObjects()) {
-      gsl::span<const o2::hmpid::Digit> trigDigits{digits.data() + trig.getFirstEntry(), size_t(trig.getNumberOfObjects())};
-      size_t clStart = clusters.size();
-      mRec->Dig2Clu(trigDigits, clusters, mSigmaCut, true);
-      clusterTriggers.emplace_back(trig.getIr(), clStart, clusters.size() - clStart);
+  //===============mReadFromFile=============================================
+  if (mReadFile) {
+    LOG(info) << "[HMPID DClusterization - run() ] Entries  = " << mTree->GetEntries();
+
+    // check if more entries in tree
+    if (mTree->GetReadEntry() + 1 >= mTree->GetEntries()) {
+
+      pc.services().get<ControlService>().endOfStream();
+      pc.services().get<ControlService>().readyToQuit(QuitRequest::Me);
+      mExTimer.stop();
+      mExTimer.logMes("End Clusterization !  digits = " +
+                      std::to_string(mDigitsReceived));
+    } else {
+      auto entry = mTree->GetReadEntry() + 1;
+      assert(entry < mTree->GetEntries());
+
+      mTree->GetEntry(entry);
+
+      // =============== create clusters =====================
+      for (const auto& trig : *mTriggersFromFilePtr) {
+        if (trig.getNumberOfObjects()) {
+          gsl::span<const o2::hmpid::Digit> trigDigits{
+            mDigitsFromFilePtr->data() + trig.getFirstEntry(),
+            size_t(trig.getNumberOfObjects())};
+          size_t clStart = clusters.size();
+          mRec->Dig2Clu(trigDigits, clusters, mSigmaCut, true);
+          clusterTriggers.emplace_back(trig.getIr(), clStart,
+                                       clusters.size() - clStart);
+        }
+      }
+
+      LOGP(info, "Received {} triggers with {} digits -> {} triggers with {} clusters",
+           mTriggersFromFilePtr->size(), mDigitsFromFilePtr->size(), clusterTriggers.size(),
+           clusters.size());
+      mDigitsReceived += mDigitsFromFilePtr->size();
+    } // <end else of num entries>
+  }   //===============  <end mReadFromFile>
+
+  else { // =========  if readfromStream==============================================
+
+    auto triggers = pc.inputs().get<gsl::span<o2::hmpid::Trigger>>("intrecord");
+    auto digits = pc.inputs().get<gsl::span<o2::hmpid::Digit>>("digits");
+
+    for (const auto& trig : triggers) {
+      if (trig.getNumberOfObjects()) {
+        gsl::span<const o2::hmpid::Digit> trigDigits{
+          digits.data() + trig.getFirstEntry(),
+          size_t(trig.getNumberOfObjects())};
+        size_t clStart = clusters.size();
+        mRec->Dig2Clu(trigDigits, clusters, mSigmaCut, true);
+        clusterTriggers.emplace_back(trig.getIr(), clStart,
+                                     clusters.size() - clStart);
+      }
     }
-  }
-  LOGP(info, "Received {} triggers with {} digits -> {} triggers with {} clusters", triggers.size(), digits.size(), clusterTriggers.size(), clusters.size());
-  mDigitsReceived += digits.size();
+    mDigitsReceived += digits.size();
+    /*LOGP(info, "Received {} triggers with {} digits -> {} triggers with {} clusters",
+         triggers.size(), digits.size(), clusterTriggers.size(),
+         clusters.size());
+       */
+  } //========= <end readfromStream>
+    //=====================================================================================
 
-  pc.outputs().snapshot(o2::framework::Output{"HMP", "CLUSTERS", 0, o2::framework::Lifetime::Timeframe}, clusters);
-  pc.outputs().snapshot(o2::framework::Output{"HMP", "INTRECORDS1", 0, o2::framework::Lifetime::Timeframe}, clusterTriggers);
+  mClustersReceived += clusters.size();
 
-  mExTimer.elapseMes("Clusterization of Digits received = " + std::to_string(mDigitsReceived));
+  pc.outputs().snapshot(
+    o2::framework::Output{"HMP", "CLUSTERS", 0,
+                          o2::framework::Lifetime::Timeframe},
+    clusters);
+  pc.outputs().snapshot(
+    o2::framework::Output{"HMP", "INTRECORDS1", 0,
+                          o2::framework::Lifetime::Timeframe},
+    clusterTriggers);
+
+  mExTimer.elapseMes("Clusterization of Digits received = " +
+                     std::to_string(mDigitsReceived));
+  mExTimer.elapseMes("Clusterization of Clusters received = " +
+                     std::to_string(mDigitsReceived));
 }
 
 void DigitsToClustersTask::endOfStream(framework::EndOfStreamContext& ec)
 {
+
   mExTimer.stop();
-  mExTimer.logMes("End Clusterization !  digits = " + std::to_string(mDigitsReceived));
+  mExTimer.logMes("End Clusterization !  digits = " +
+                  std::to_string(mDigitsReceived));
 }
 
-//_________________________________________________________________________________________________
-o2::framework::DataProcessorSpec getDigitsToClustersSpec(std::string inputSpec)
+void DigitsToClustersTask::initFileIn(const std::string& filename)
 {
-  std::vector<o2::framework::InputSpec> inputs;
-  inputs.emplace_back("digits", o2::header::gDataOriginHMP, "DIGITS", 0, Lifetime::Timeframe);
-  inputs.emplace_back("intrecord", o2::header::gDataOriginHMP, "INTRECORDS", 0, Lifetime::Timeframe);
+  // Create the TFIle
+  mFile = std::make_unique<TFile>(filename.c_str(), "OLD");
+  if (!mFile) {
+    LOG(error)
+      << "HMPID DigitToClusterSpec::init() : Did not find any digits file ";
+    return;
+  }
+  assert(mFile && !mFile->IsZombie());
+  mTree.reset((TTree*)mFile->Get("o2sim"));
 
+  if (!mTree) {
+    mTree.reset((TTree*)mFile->Get("o2hmp"));
+  }
+
+  if (!mTree) {
+    LOG(error)
+      << "HMPID DigitToClusterSpec::init() : Did not find o2sim tree in "
+      << filename.c_str();
+    throw std::runtime_error(
+      "HMPID DigitToClusterSpec::init() : Did not find "
+      "o2sim file in digits tree");
+  }
+
+  if (mTree->GetBranchStatus("HMPDigit"))
+    mTree->SetBranchAddress("HMPDigit", &mDigitsFromFilePtr);
+  else if (mTree->GetBranchStatus("HMPIDDigits"))
+    mTree->SetBranchAddress("HMPIDDigits", &mDigitsFromFilePtr);
+  else {
+    LOG(error)
+      << "HMPID DigitToClusterSpec::init() : Did not find any branch ";
+  }
+  mTree->SetBranchAddress("InteractionRecords", &mTriggersFromFilePtr);
+  mTree->Print("toponly");
+}
+
+//_______________________________________________________________________________________________
+o2::framework::DataProcessorSpec
+  getDigitsToClustersSpec(std::string inputSpec, bool readFile, bool writeFile)
+
+{
+
+  // define inputs if reading from stream:
+  std::vector<o2::framework::InputSpec> inputs;
+  if (!readFile) {
+    inputs.emplace_back("digits", o2::header::gDataOriginHMP, "DIGITS", 0,
+                        Lifetime::Timeframe);
+    inputs.emplace_back("intrecord", o2::header::gDataOriginHMP, "INTRECORDS",
+                        0, Lifetime::Timeframe);
+  }
+
+  // define outputs
+
+  // outputs are streamed, and optionally stored in a root-file if the --write-to-file
+  // option in digits-to-clusters-workflow.cxx is passed
   std::vector<o2::framework::OutputSpec> outputs;
-  outputs.emplace_back("HMP", "CLUSTERS", 0, o2::framework::Lifetime::Timeframe);
-  outputs.emplace_back("HMP", "INTRECORDS1", 0, o2::framework::Lifetime::Timeframe);
+
+  outputs.emplace_back("HMP", "CLUSTERS", 0,
+                       o2::framework::Lifetime::Timeframe);
+  outputs.emplace_back("HMP", "INTRECORDS1", 0,
+                       o2::framework::Lifetime::Timeframe);
 
   return DataProcessorSpec{
-    "HMP-Clusterization",
-    inputs,
-    outputs,
-    AlgorithmSpec{adaptFromTask<DigitsToClustersTask>()},
-    Options{{"sigma-cut", VariantType::String, "", {"sigmas as comma separated list"}}}};
+    "HMP-Clusterization", inputs, outputs,
+    AlgorithmSpec{adaptFromTask<DigitsToClustersTask>(readFile)},
+    Options{{"sigma-cut",
+             VariantType::String,
+             "",
+             {"sigmas as comma separated list"}},
+            {"hmpid-digit-infile",
+             VariantType::String,
+             "hmpiddigits.root",
+             {"Name of the input file"}},
+            {"input-dir", VariantType::String, "./", {"Input directory"}}}};
 }
 
 } // namespace hmpid
--- Detectors/HMPID/workflow/src/digits-to-clusters-workflow.cxx
+++ Detectors/HMPID/workflow/src/digits-to-clusters-workflow.cxx
@@ -9,26 +9,23 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-/// \file   digits-to-cluster-workflow.cxx
-/// \author Antonio Franco - INFN Bari
-/// \version 1.0
-/// \date 22 nov 2021
-///
+/// \file digits-to-clusters-workflow.h
+/// \brief Workflow for clusterization for HMPID; read upstream/from file write upstream/to file
 
-#include "Framework/WorkflowSpec.h"
-#include "Framework/DataSpecUtils.h"
-#include "Framework/CallbackService.h"
-#include "Framework/ControlService.h"
-#include "Framework/Task.h"
-#include "Framework/CompletionPolicy.h"
-#include "Framework/CompletionPolicyHelpers.h"
-#include "Framework/DispatchPolicy.h"
-#include "Framework/ConfigParamSpec.h"
-#include "Framework/Variant.h"
 #include "CommonUtils/ConfigurableParam.h"
 #include "CommonUtils/NameConf.h"
 #include "DetectorsRaw/HBFUtilsInitializer.h"
+#include "Framework/CallbackService.h"
 #include "Framework/CallbacksPolicy.h"
+#include "Framework/CompletionPolicy.h"
+#include "Framework/CompletionPolicyHelpers.h"
+#include "Framework/ConfigParamSpec.h"
+#include "Framework/ControlService.h"
+#include "Framework/DataSpecUtils.h"
+#include "Framework/DispatchPolicy.h"
+#include "Framework/Task.h"
+#include "Framework/Variant.h"
+#include "Framework/WorkflowSpec.h"
 
 void customize(std::vector<o2::framework::CallbacksPolicy>& policies)
 {
@@ -40,19 +37,38 @@ void customize(std::vector<o2::framework::CompletionPolicy>& policies)
 {
   using o2::framework::CompletionPolicy;
   using o2::framework::CompletionPolicyHelpers;
-  policies.push_back(o2::framework::CompletionPolicyHelpers::defineByName("clusters-hmpid-write", CompletionPolicy::CompletionOp::Consume));
+  policies.push_back(o2::framework::CompletionPolicyHelpers::defineByName(
+    "clusters-hmpid-write", CompletionPolicy::CompletionOp::Consume));
 }
 
 // we need to add workflow options before including Framework/runDataProcessing
 void customize(std::vector<o2::framework::ConfigParamSpec>& workflowOptions)
 {
   std::string keyvaluehelp("Semicolon separated key=value strings ...");
-  workflowOptions.push_back(o2::framework::ConfigParamSpec{"configKeyValues", o2::framework::VariantType::String, "", {keyvaluehelp}});
+  workflowOptions.push_back(
+    o2::framework::ConfigParamSpec{"configKeyValues",
+                                   o2::framework::VariantType::String,
+                                   "",
+                                   {keyvaluehelp}});
+  workflowOptions.push_back(
+    o2::framework::ConfigParamSpec{"read-from-file",
+                                   o2::framework::VariantType::Bool,
+                                   false,
+                                   {"read upstream by default"}});
+  workflowOptions.push_back(
+    o2::framework::ConfigParamSpec{"write-to-file",
+                                   o2::framework::VariantType::Bool,
+                                   false,
+                                   {"read upstream by default"}});
+
   o2::raw::HBFUtilsInitializer::addConfigOption(workflowOptions);
 }
 
 #include "Framework/runDataProcessing.h"
 #include "HMPIDWorkflow/DigitsToClustersSpec.h"
+#include "HMPIDWorkflow/ClustersWriterSpec.h"
+
+//#include "HMPIDWorkflow/HMPIDDigitizerSpec.h"
 
 using namespace o2;
 using namespace o2::framework;
@@ -60,8 +76,22 @@ using namespace o2::framework;
 WorkflowSpec defineDataProcessing(const ConfigContext& configcontext)
 {
   WorkflowSpec specs;
-  o2::conf::ConfigurableParam::updateFromString(configcontext.options().get<std::string>("configKeyValues"));
-  DataProcessorSpec consumer = o2::hmpid::getDigitsToClustersSpec();
+  o2::conf::ConfigurableParam::updateFromString(
+    configcontext.options().get<std::string>("configKeyValues"));
+  auto mFromFile = configcontext.options().get<bool>(
+    "read-from-file"); // read upstream by default
+  auto mToFile = configcontext.options().get<bool>(
+    "write-to-file"); // write upstream by default
+
+  DataProcessorSpec consumer =
+    o2::hmpid::getDigitsToClustersSpec("HMP/DIGITS", mFromFile, mToFile);
+
   specs.push_back(consumer);
+
+  if (mToFile) { // Write to File
+    DataProcessorSpec consumerClusterToRoot = o2::hmpid::getClustersToRootWriter();
+    specs.push_back(consumerClusterToRoot);
+  }
+
   return specs;
 }
