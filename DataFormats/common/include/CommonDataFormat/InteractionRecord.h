6f40b420a7fd2eac2063c33c6475fc819c5d77c3
dstocco <diego.stocco@cern.ch>
2023-02-02 11:17:24 +0100

    Add default copy assignment to prevent compiler warnings

--- DataFormats/common/include/CommonDataFormat/InteractionRecord.h
+++ DataFormats/common/include/CommonDataFormat/InteractionRecord.h
@@ -46,6 +46,7 @@ struct InteractionRecord {
   }
 
   InteractionRecord(const InteractionRecord& src) = default;
+  InteractionRecord& operator=(const InteractionRecord& src) = default;
 
   void clear()
   {
