9ea85cc8077f015bdf01959288d3b31e51ecdbf5
David Rohr <drohr@jwdt.org>
2023-02-04 00:00:34 +0100

    dpl-workflow: Add optional TOF digit writer/reader

--- prodtests/full-system-test/dpl-workflow.sh
+++ prodtests/full-system-test/dpl-workflow.sh
@@ -213,6 +213,8 @@ fi
 
 if ! has_detector_reco TOF; then
   TOF_OUTPUT=digits
+elif has_detector_reco TOF && ! has_detector_from_global_reader TOF; then
+  TOF_OUTPUT+=",digits"
 fi
 
 if has_detector_calib PHS && workflow_has_parameter CALIB; then
@@ -263,8 +265,9 @@ if [[ ! -z $WORKFLOW_DETECTORS_USE_GLOBAL_READER ]]; then
   add_W o2-global-track-cluster-reader "--cluster-types $WORKFLOW_DETECTORS_USE_GLOBAL_READER --track-types $WORKFLOW_DETECTORS_USE_GLOBAL_READER $GLOBAL_READER_OPTIONS $DISABLE_MC --hbfutils-config o2_tfidinfo.root"
   has_detector FV0 && has_detector_from_global_reader FV0 && add_W o2-fv0-digit-reader-workflow "$DISABLE_MC --hbfutils-config o2_tfidinfo.root --fv0-digit-infile o2_fv0digits.root"
   has_detector MID && has_detector_from_global_reader MID && add_W o2-mid-digits-reader-workflow "$DISABLE_MC --hbfutils-config o2_tfidinfo.root --mid-digit-infile mid-digits-decoded.root"
-  has_detector MCH && has_detector_from_global_reader MCH && add_W o2-mch-digits-reader-workflow " --hbfutils-config o2_tfidinfo.root --infile mchdigits.root"
-  has_detector TRD && has_detector_from_global_reader TRD && add_W o2-trd-digit-reader-workflow " --hbfutils-config o2_tfidinfo.root"
+  has_detector MCH && has_detector_from_global_reader MCH && add_W o2-mch-digits-reader-workflow "--hbfutils-config o2_tfidinfo.root --infile mchdigits.root"
+  has_detector TRD && has_detector_from_global_reader TRD && add_W o2-trd-digit-reader-workflow "--hbfutils-config o2_tfidinfo.root"
+  has_detector TOF && has_detector_from_global_reader TOF && add_W o2-tof-reco-workflow "--input-type digits --output-type NONE --hbfutils-config o2_tfidinfo.root"
 fi
 
 if [[ ! -z $INPUT_DETECTOR_LIST ]]; then
