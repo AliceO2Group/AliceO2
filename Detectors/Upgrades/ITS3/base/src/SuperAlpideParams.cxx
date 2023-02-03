da3cc4b6eb04e83010ddf1c05e975f873d3ea957
Felix Schlepper <f3sch.git@outlook.com>
2023-02-03 15:56:06 +0100

    ITS3: Base: Make ALPIDE pixel size configurable
    v1->v2: Changed Name from Alpide to SuperAlpide to be more consistent
    
    Signed-off-by: Felix Schlepper <f3sch.git@outlook.com>

--- Detectors/Upgrades/ITS3/base/src/SuperAlpideParams.cxx
+++ Detectors/Upgrades/ITS3/base/src/SuperAlpideParams.cxx
@@ -9,15 +9,5 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-#ifdef __CLING__
-
-#pragma link off all globals;
-#pragma link off all classes;
-#pragma link off all functions;
-
-#pragma link C++ class o2::its3::SegmentationSuperAlpide + ;
-#pragma link C++ class o2::its3::MisalignmentParameter + ;
-#pragma link C++ class o2::its3::SuperAlpideParams + ;
-#pragma link C++ class o2::conf::ConfigurableParamHelper < o2::its3::SuperAlpideParams> + ;
-
-#endif
+#include "ITS3Base/SuperAlpideParams.h"
+O2ParamImpl(o2::its3::SuperAlpideParams);
