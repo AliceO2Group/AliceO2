0823eeeacddcd9e10774cc2c8be58b8fd501be8b
Felix Schlepper <f3sch.git@outlook.com>
2023-02-28 11:41:28 +0100

    TRD: PID: fixups
    This commit renames the factory function to better reflect its purpose.
    Additionally, z-row merging and charge correction have been added to the codebase to improve functionality.
    Pytorch policy and LQND policy have been added as new policies.
    The README has also been added to provide additional explanation of the code.
    As part of this update, the pid policy map and pidvalue alias have been
    removed.
    A print overload has been added for the policy enum to improve readability.
    Various minor fixes have also been made to improve overall code quality.
    Also included are various changes to the class layout and the ccdb object for LUTs.
    
    Signed-off-by: Felix Schlepper <f3sch.git@outlook.com>

--- Detectors/TRD/pid/src/PIDBase.cxx
+++ Detectors/TRD/pid/src/PIDBase.cxx
@@ -14,31 +14,72 @@
 
 #include "TRDPID/PIDBase.h"
 #include "DataFormatsTRD/PID.h"
+#include "Framework/Logger.h"
+
 #ifdef TRDPID_WITH_ONNX
 #include "TRDPID/ML.h"
 #endif
+#include "TRDPID/LQND.h"
 #include "TRDPID/Dummy.h"
-#include "Framework/Logger.h"
-#include "fmt/format.h"
 
 namespace o2
 {
 namespace trd
 {
 
-std::unique_ptr<PIDBase> getTRDPIDBase(PIDPolicy policy)
+std::array<float, constants::NCHARGES> PIDBase::getCharges(const Tracklet64& tracklet, const int layer, const TrackTRD& trk, const o2::globaltracking::RecoContainer& input, float snp, float tgl) const noexcept
 {
-  auto policyInt = static_cast<unsigned int>(policy);
-  LOG(info) << "Creating PID policy. Loading model " << PIDPolicyEnum[policyInt];
+  // Check z-row merging needs to be performed to recover full charge information
+  if (trk.getIsCrossingNeighbor(layer) && trk.getHasNeighbor()) { // tracklet needs correction
+    for (const auto& trklt : input.getTRDTracklets()) {           // search for nearby tracklet
+      if (std::abs(tracklet.getPadCol() - trklt.getPadCol()) <= 1 && std::abs(tracklet.getPadRow() - trklt.getPadRow()) == 1) {
+        if (tracklet.getTrackletWord() == trklt.getTrackletWord()) { // skip original tracklet
+          continue;
+        }
+
+        // Add charge information
+        const auto [aQ0, aQ1, aQ2] = correctCharges(tracklet, snp, tgl);
+        const auto [bQ0, bQ1, bQ2] = correctCharges(tracklet, snp, tgl);
+        return {aQ0 + bQ0, aQ1 + bQ1, aQ2 + bQ2};
+      }
+    }
+  }
+
+  return correctCharges(tracklet, snp, tgl);
+}
+
+std::array<float, constants::NCHARGES> PIDBase::correctCharges(const Tracklet64& trklt, float snp, float tgl) const noexcept
+{
+  auto tphi = snp / std::sqrt((1.f - snp) + (1.f + snp));
+  auto trackletLength = std::sqrt(1.f + tphi * tphi + tgl * tgl);
+  const float correction = mLocalGain->getValue(trklt.getHCID() / 2, trklt.getPadCol(), trklt.getPadRow()) * trackletLength;
+  return {
+    trklt.getQ0() / correction,
+    trklt.getQ1() / correction,
+    trklt.getQ2() / correction,
+  };
+}
+
+std::unique_ptr<PIDBase> getTRDPIDPolicy(PIDPolicy policy)
+{
+  LOG(info) << "Creating PID policy. Loading model " << policy;
   switch (policy) {
-#ifdef TRDPID_WITH_ONNX
-    case PIDPolicy::Test:
-      return std::make_unique<XGB>(PIDPolicy::Test);
+    case PIDPolicy::LQ1D:
+      return std::make_unique<LQ1D>(PIDPolicy::LQ1D);
+    case PIDPolicy::LQ2D:
+      return std::make_unique<LQ2D>(PIDPolicy::LQ2D);
+    case PIDPolicy::LQ3D:
+      return std::make_unique<LQ3D>(PIDPolicy::LQ3D);
+#ifdef TRDPID_WITH_ONNX // Add all policies that use ONNX in this ifdef
+    case PIDPolicy::XGB:
+      return std::make_unique<XGB>(PIDPolicy::XGB);
+    case PIDPolicy::PY:
+      return std::make_unique<PY>(PIDPolicy::PY);
 #endif
     case PIDPolicy::Dummy:
       return std::make_unique<Dummy>(PIDPolicy::Dummy);
     default:
-      throw std::invalid_argument(fmt::format("Cannot create this PID policy {}({})", PIDPolicyEnum[policyInt], policyInt));
+      return nullptr;
   }
   return nullptr; // cannot be reached
 }
