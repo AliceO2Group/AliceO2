dd83cac9afa8a8b4dddbb7821f6d045ecf3d1201
Luca Barioglio <luca.barioglio@cern.ch>
2023-03-03 08:59:50 +0100

    Add copyright statement

--- Detectors/Upgrades/ITS3/macros/test/CreateDictionariesITS3.C
+++ Detectors/Upgrades/ITS3/macros/test/CreateDictionariesITS3.C
@@ -1,5 +1,16 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
 /// \file CreateDictionaries.C
-/// Macros to test the generation of a dictionary of topologies. Three dictionaries are generated: one with signal-cluster only, one with noise-clusters only and one with all the clusters.
+/// \brief Macros to test the generation of a dictionary of topologies. Three dictionaries are generated: one with signal-cluster only, one with noise-clusters only and one with all the clusters.
 
 #if !defined(__CLING__) || defined(__ROOTCLING__)
 
