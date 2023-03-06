27901705d55861a79aeb7d83e113f4958f44e693
Markus Fasel <markus.fasel@cern.ch>
2023-03-06 14:51:01 +0100

    Stream operators for RDH v6 and v7
    - Add streaming operators for RDH v6
      and v7
    - Move streaming operators from
      EMCALReconstruction to DetectorsRaw

--- Detectors/EMCAL/reconstruction/include/EMCALReconstruction/RawHeaderStream.h
+++ Detectors/EMCAL/reconstruction/include/EMCALReconstruction/RawHeaderStream.h
@@ -8,32 +8,8 @@
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
-
-/// \file RawHeaderStream.h
-/// \brief Input stream operators for raw header 4 and 5 from binary file
-///
-/// Helpers to define input stream operator for raw headers v4 and v5 from
-/// binary file input stream, used in RawReaderFile
-
 #ifndef ALICEO2_EMCAL_RAWHEADERSTREAM_H
 #define ALICEO2_EMCAL_RAWHEADERSTREAM_H
-
-#include <iosfwd>
-#include "Headers/RAWDataHeader.h"
-
-namespace o2
-{
-
-namespace emcal
-{
-
-std::istream& operator>>(std::istream& stream, o2::header::RAWDataHeaderV4& header);
-std::istream& operator>>(std::istream& stream, o2::header::RAWDataHeaderV5& header);
-
-std::ostream& operator<<(std::ostream& stream, const o2::header::RAWDataHeaderV4& header);
-std::ostream& operator<<(std::ostream& stream, const o2::header::RAWDataHeaderV5& header);
-
-} // namespace emcal
-
-} // namespace o2
-#endif
+// Temporary file, needed for compatibility in the QC package
+#include "DetectorsRaw/RawHeaderStream.h"
+#endif // ALICEO2_EMCAL_RAWHEADERSTREAM_H
