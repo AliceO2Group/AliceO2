4d264ffb2f4cf04aedaf720d013313115c4ea1c4
David Rohr <drohr@jwdt.org>
2023-03-08 14:57:26 +0100

    FST: Add support for MI100 GPU via env variable EPN_NODE_MI100=1

--- prodtests/full-system-test/start_tmux.sh
+++ prodtests/full-system-test/start_tmux.sh
@@ -27,10 +27,13 @@ source $GEN_TOPO_MYDIR/setenv.sh || { echo "setenv.sh failed" 1>&2 && exit 1; }
 
 if [[ "0$FST_TMUX_NO_EPN" != "01" ]]; then
   # This sets up the hardcoded configuration to run the full system workflow on the EPN
+  if [[ "0$EPN_NODE_MI100" == "01" && -z $EPN_GLOBAL_SCALING ]]; then
+    export EPN_GLOBAL_SCALING="3 / 2"
+  fi
   export NGPUS=4
   export GPUTYPE=HIP
-  export SHMSIZE=$(( 112 << 30 )) # Please keep these defaults in sync with those in shm-tool.sh
-  export DDSHMSIZE=$(( 112 << 10 ))
+  export SHMSIZE=$(( (112 << 30) * ${EPN_GLOBAL_SCALING:-1} )) # Please keep these defaults in sync with those in shm-tool.sh
+  export DDSHMSIZE=$(( (112 << 10) * ${EPN_GLOBAL_SCALING:-1} ))
   export GPUMEMSIZE=$(( 24 << 30 ))
   export NUMAGPUIDS=1
   export EPNPIPELINES=1
@@ -38,7 +41,7 @@ if [[ "0$FST_TMUX_NO_EPN" != "01" ]]; then
   export ALL_EXTRA_CONFIG="$ALL_EXTRA_CONFIG;NameConf.mCCDBServer=${DPL_CONDITION_BACKEND};"
   export GEN_TOPO_QC_OVERRIDE_CCDB_SERVER="http://localhost:8084"
   NUM_DPL_WORKFLOWS=2
-  if [[ `lspci | grep "Vega 20" | wc -l` != "8" ]]; then
+  if [[ `lspci | grep "Vega 20\|Arcturus GL-XL" | wc -l` != "8" ]]; then
     echo "Could not detect 8 EPN GPUs, aborting" 1>&2
     exit 1
   fi
@@ -147,5 +150,6 @@ else
 fi
 
 if [[ -z $SHM_MANAGER_SHMID ]]; then
+  echo "Cleaning up SHM"
   rm -f /dev/shm/*fmq*
 fi
