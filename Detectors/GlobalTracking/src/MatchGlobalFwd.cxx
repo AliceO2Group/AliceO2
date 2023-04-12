95368c2f616b54bb7789f8f1af759acd215dcf28
Robin Caron <45421914+robincaron13@users.noreply.github.com>
2023-04-12 13:35:09 +0200

    Re-indexing "working" copies of MCH tracks (and correct ROF entries for MFT tracks) (#10867)
    * add re-indexing of MCH tracks for the matching
    
    * correct ROF entries to avoid zero mMFTWork.size()
    
    * removes previous changes for getting ROF entries
    
    * remove printer info

--- Detectors/GlobalTracking/src/MatchGlobalFwd.cxx
+++ Detectors/GlobalTracking/src/MatchGlobalFwd.cxx
@@ -151,6 +151,8 @@ bool MatchGlobalFwd::prepareMCHData()
     return false;
   }
   mMCHWork.reserve(mMCHTracks.size());
+  mMCHID2Work.clear();
+  mMCHID2Work.resize(mMCHTracks.size(), -1);
   static int BCDiffErrCount = 0;
   constexpr int MAXBCDiffErrCount = 2;
 
@@ -173,6 +175,7 @@ bool MatchGlobalFwd::prepareMCHData()
     for (int it = rofRec.getFirstIdx(); it < trlim; it++) {
       auto& trcOrig = mMCHTracks[it];
       int nWorkTracks = mMCHWork.size();
+      mMCHID2Work[it] = nWorkTracks;
       // working copy MCH track propagated to matching plane and converted to the forward track format
       o2::mch::TrackParam tempParam(trcOrig.getZ(), trcOrig.getParameters(), trcOrig.getCovariances());
       if (!o2::mch::TrackExtrap::extrapToVertexWithoutBranson(tempParam, mMatchingPlaneZ)) {
@@ -202,7 +205,8 @@ bool MatchGlobalFwd::processMCHMIDMatches()
     for (const auto& MIDMatch : mMCHMIDMatches) {
       const auto& MCHId = MIDMatch.getMCHRef().getIndex();
       const auto& MIDId = MIDMatch.getMIDRef().getIndex();
-      auto& thisMuonTrack = mMCHWork[MCHId];
+      auto& thisMuonTrack = mMCHWork[mMCHID2Work[MCHId]];
+      LOG(debug) << " MCHId: " << MCHId << " --> mMCHID2Work[MCHId]:" << mMCHID2Work[MCHId];
       const auto& IR = MIDMatch.getIR();
       int nBC = IR.differenceInBC(mStartIR);
       float tMin = nBC * o2::constants::lhc::LHCBunchSpacingMUS;
@@ -251,7 +255,6 @@ bool MatchGlobalFwd::prepareMFTData()
 
   for (int irof = 0; irof < nROFs; irof++) {
     const auto& rofRec = mMFTTrackROFRec[irof];
-
     int nBC = rofRec.getBCData().differenceInBC(mStartIR);
     if (nBC < 0) {
       if (BCDiffErrCount++ < MAXBCDiffErrCount) {
@@ -306,7 +309,7 @@ void MatchGlobalFwd::loadMatches()
     auto MCHId = match.getMCHTrackID();
     LOG(debug) << "     ==> MFTId = " << MFTId << " MCHId =  " << MCHId << std::endl;
 
-    auto& thisMCHTrack = mMCHWork[MCHId];
+    auto& thisMCHTrack = mMCHWork[mMCHID2Work[MCHId]];
     thisMCHTrack.setMatchInfo(match);
     mMatchedTracks.emplace_back(thisMCHTrack);
     if (mMCTruthON) {
@@ -332,6 +335,8 @@ void MatchGlobalFwd::doMatching()
   // ROFrame of first MFT track
   auto firstMFTTrackIdInROF = 0;
   auto MFTROFId = mMFTWork.front().roFrame;
+  LOG(debug) << "(*) nMCHROFs: " << nMCHROFs << ", mMFTTracks.size(): " << mMFTTracks.size() << " MFTROFId: " << MFTROFId << ",  mMFTTrackROFRec.size(): " << mMFTTrackROFRec.size();
+
   while ((firstMFTTrackIdInROF < mMFTTracks.size()) && (MFTROFId < mMFTTrackROFRec.size())) {
     auto MFTROFId = mMFTWork[firstMFTTrackIdInROF].roFrame;
     const auto& thisMFTBracket = mMFTROFTimes[MFTROFId];
@@ -502,7 +507,7 @@ void MatchGlobalFwd::doMCMatching()
   // loop over all MCH tracks
   for (auto MCHId = 0; MCHId < mMCHWork.size(); MCHId++) {
     auto& thisMCHTrack = mMCHWork[MCHId];
-    const o2::MCCompLabel& thisMCHLabel = mMCHTrkLabels[MCHId];
+    const o2::MCCompLabel& thisMCHLabel = mMCHTrkLabels[mMCHID2Work[MCHId]];
 
     LOG(debug) << "   MCH Track # " << MCHId << " Label: " << thisMCHLabel;
     if (!((thisMCHLabel).isSet())) {
