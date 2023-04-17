cc1a73bcae2b9d8047e889fedf1c02cc15334777
Mesut Arslandok <mesut.arslandok@cern.ch>
2023-04-17 14:10:01 +0200

    ATO-609: Additional ITS material added (to be MOVED in ITS code)

--- Detectors/Passive/src/Cave.cxx
+++ Detectors/Passive/src/Cave.cxx
@@ -31,6 +31,7 @@
 #include "TGeoManager.h"
 #include "TGeoVolume.h"
 #include "TGeoPgon.h"
+#include "TGeoPcon.h"
 #include "TGeoTube.h"
 #include "TGeoCompositeShape.h"
 using namespace o2::passive;
@@ -49,13 +50,35 @@ void Cave::createMaterials()
   Float_t zAir[4] = {6., 7., 8., 18.};
   Float_t wAir[4] = {0.000124, 0.755267, 0.231781, 0.012827};
   Float_t dAir = 1.20479E-3 * 960. / 1014.;
-
   //
+  // Water
+  Float_t aWater[2] = {1.00794, 15.9994};
+  Float_t zWater[2] = {1., 8.};
+  Float_t wWater[2] = {0.111894, 0.888106};
+  //
+  // Polymer
+  //
+  Float_t apoly[2] = {12.01, 1.};
+  Float_t zpoly[2] = {6., 1.};
+  Float_t wpoly[2] = {0.857, .143};
+
+  // Air
   matmgr.Mixture("CAVE", 2, "Air", aAir, zAir, dAir, 4, wAir);
   matmgr.Mixture("CAVE", 3, "Air_NF", aAir, zAir, dAir, 4, wAir);
-  //
   matmgr.Medium("CAVE", 2, "Air", 2, 0, isxfld, sxmgmx, 10, -1, -0.1, 0.1, -10);
   matmgr.Medium("CAVE", 3, "Air_NF", 3, 0, 0, sxmgmx, 10, -1, -0.1, 0.1, -10);
+  // Copper
+  matmgr.Material("CAVE", 10, "COPPER", 63.55, 29, 8.96, 1.43, 85.6 / 8.96);
+  matmgr.Medium("CAVE", 10, "COPPER", 10, 0, 0, sxmgmx, 10, -1, -0.1, 0.1, -10);
+  // Carbon
+  matmgr.Material("CAVE", 13, "Carbon", 12.01, 6., 1.75, 24.4, 49.9);
+  matmgr.Medium("CAVE", 13, "Carbon", 13, 0, 0, sxmgmx, 10, -1, -0.1, 0.1, -10);
+  // Water
+  matmgr.Mixture("CAVE", 11, "Water", aWater, zWater, 1., 2, wWater);
+  matmgr.Medium("CAVE", 11, "Water", 11, 0, 0, sxmgmx, 10, -1, -0.1, 0.1, -10);
+  // Polymer
+  matmgr.Mixture("CAVE", 12, "Polymer", apoly, zpoly, 0.9, 2, wpoly);
+  matmgr.Medium("CAVE", 12, "Polymer", 12, 0, 0, sxmgmx, 10, -1, -0.1, 0.1, -10);
 }
 
 void Cave::ConstructGeometry()
@@ -63,7 +86,10 @@ void Cave::ConstructGeometry()
   createMaterials();
   auto& matmgr = o2::base::MaterialManager::Instance();
   auto kMedAir = gGeoManager->GetMedium("CAVE_Air");
-
+  auto kMedCu = gGeoManager->GetMedium("CAVE_COPPER");
+  auto kMedWater = gGeoManager->GetMedium("CAVE_Water");
+  auto kMedPolymer = gGeoManager->GetMedium("CAVE_Polymer");
+  auto kMedCarbon = gGeoManager->GetMedium("CAVE_Carbon");
   Float_t dALIC[3];
 
   if (mHasZDC) {
@@ -91,6 +117,103 @@ void Cave::ConstructGeometry()
   TGeoCompositeShape* shCaveTR = new TGeoCompositeShape("shCaveTR", "shCaveTR1-shCaveTR2:transTR2");
   TGeoVolume* voBarrel = new TGeoVolume("barrel", shCaveTR, kMedAir);
   cavevol->AddNode(voBarrel, 1, new TGeoTranslation(0., -30., 0.));
+
+  // ITS services
+  // Inner Barrel
+  // Mother
+
+  TGeoPcon* shITSIB = new TGeoPcon(0., 360., 3);
+  auto drCC = 0.2;
+  auto drSE = 0.462;
+  auto drMo1 = drCC + 1.8 * 0.462;
+  auto drMo2 = drCC + 0.462 / 1.8;
+  auto r2 = 13.3;
+  auto r1 = r2 - drMo1;
+  auto r4 = 43.63;
+  auto r3 = r4 - drMo2;
+  auto z1 = 35.;
+  auto z2 = 44.;
+  auto z3 = 253.45;
+  shITSIB->DefineSection(0, z1, r1, r2);
+  shITSIB->DefineSection(1, z2, r1, r2);
+  shITSIB->DefineSection(2, z3, r3, r4);
+  TGeoVolume* voITSIB = new TGeoVolume("ITSIB", shITSIB, kMedCarbon);
+  voBarrel->AddNode(voITSIB, 1, new TGeoTranslation(0., 30., 0.));
+  // Copper
+  r2 -= drCC;
+  r4 -= drCC;
+  TGeoPcon* shITSIBcu = new TGeoPcon(0., 360., 3);
+  shITSIBcu->DefineSection(0, z1, r1, r2);
+  shITSIBcu->DefineSection(1, z2, r1, r2);
+  shITSIBcu->DefineSection(2, z3, r3, r4);
+  TGeoVolume* voITSIBcu = new TGeoVolume("ITSIBcu", shITSIBcu, kMedCu);
+  voITSIB->AddNode(voITSIBcu, 1, new TGeoTranslation(0., 0., 0.));
+  // Polymer
+  auto drCu = 0.018;
+  r2 -= drCu * 1.8;
+  r4 -= drCu / 1.8;
+  TGeoPcon* shITSIBpo = new TGeoPcon(0., 360., 3);
+  shITSIBpo->DefineSection(0, z1, r1, r2);
+  shITSIBpo->DefineSection(1, z2, r1, r2);
+  shITSIBpo->DefineSection(2, z3, r3, r4);
+  TGeoVolume* voITSIBpo = new TGeoVolume("ITSIBpo", shITSIBpo, kMedPolymer);
+  voITSIBcu->AddNode(voITSIBpo, 1, new TGeoTranslation(0., 0., 0.));
+  // Water
+  auto drPo = 0.42;
+  r2 -= drPo * 1.8;
+  r4 -= drPo / 1.8;
+  TGeoPcon* shITSIBwa = new TGeoPcon(0., 360., 3);
+  shITSIBwa->DefineSection(0, z1, r1, r2);
+  shITSIBwa->DefineSection(1, z2, r1, r2);
+  shITSIBwa->DefineSection(2, z3, r3, r4);
+  TGeoVolume* voITSIBwa = new TGeoVolume("ITSIBwa", shITSIBwa, kMedWater);
+  voITSIBpo->AddNode(voITSIBwa, 1, new TGeoTranslation(0., 0., 0.));
+  // Outer Barrel
+  TGeoPcon* shITSOB = new TGeoPcon(0., 360., 3);
+  auto drMo = 4.42;
+  r2 = 47.3;
+  r1 = r2 - drMo;
+  // r4 = 55.25;
+  r4 = r2;
+  r3 = r4 - drMo;
+  z1 = 83.;
+  z2 = 117.28;
+  z3 = 248.0;
+  shITSOB->DefineSection(0, z1, r1, r2);
+  shITSOB->DefineSection(1, z2, r1, r2);
+  shITSOB->DefineSection(2, z3, r3, r4);
+  TGeoVolume* voITSOB = new TGeoVolume("ITSOB", shITSOB, kMedCarbon);
+  voBarrel->AddNode(voITSOB, 1, new TGeoTranslation(0., 30., 0.));
+  drCC = 0.25;
+  // Copper
+  r2 -= drCC;
+  r4 -= drCC;
+  TGeoPcon* shITSOBcu = new TGeoPcon(0., 360., 3);
+  shITSOBcu->DefineSection(0, z1, r1, r2);
+  shITSOBcu->DefineSection(1, z2, r1, r2);
+  shITSOBcu->DefineSection(2, z3, r3, r4);
+  TGeoVolume* voITSOBcu = new TGeoVolume("ITSOBcu", shITSOBcu, kMedCu);
+  voITSOB->AddNode(voITSOBcu, 1, new TGeoTranslation(0., 0., 0.));
+  // Polymer
+  drCu = 0.23;
+  r2 -= drCu;
+  r4 -= drCu;
+  TGeoPcon* shITSOBpo = new TGeoPcon(0., 360., 3);
+  shITSOBpo->DefineSection(0, z1, r1, r2);
+  shITSOBpo->DefineSection(1, z2, r1, r2);
+  shITSOBpo->DefineSection(2, z3, r3, r4);
+  TGeoVolume* voITSOBpo = new TGeoVolume("ITSOBpo", shITSOBpo, kMedPolymer);
+  voITSOBcu->AddNode(voITSOBpo, 1, new TGeoTranslation(0., 0., 0.));
+  // Water
+  drPo = 3.8;
+  r2 -= drPo;
+  r4 -= drPo;
+  TGeoPcon* shITSOBwa = new TGeoPcon(0., 360., 3);
+  shITSOBwa->DefineSection(0, z1, r1, r2);
+  shITSOBwa->DefineSection(1, z2, r1, r2);
+  shITSOBwa->DefineSection(2, z3, r3, r4);
+  TGeoVolume* voITSOBwa = new TGeoVolume("ITSOBwa", shITSOBwa, kMedWater);
+  voITSOBpo->AddNode(voITSOBwa, 1, new TGeoTranslation(0., 0., 0.));
 
   // mother volune for RB24 side (FDD, Compensator)
   const Float_t kRB24CL = 2. * 597.9;
