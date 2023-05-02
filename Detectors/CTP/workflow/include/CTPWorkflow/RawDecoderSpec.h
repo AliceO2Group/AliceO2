f2ea9b7169c2b7bcd2e2d0c15ef3badc37dea8de
lietava <roman.lietava@cern.ch>
2023-05-02 20:16:08 +0200

    dev: creating decoding method which can be used also in QC ctp

--- Detectors/CTP/workflow/include/CTPWorkflow/RawDecoderSpec.h
+++ Detectors/CTP/workflow/include/CTPWorkflow/RawDecoderSpec.h
@@ -14,9 +14,10 @@
 
 #include <vector>
 #include <deque>
-
+#include "DPLUtils/DPLRawParser.h"
 #include "Framework/DataProcessorSpec.h"
 #include "Framework/Task.h"
+#include "Framework/InputRecord.h"
 #include "DataFormatsCTP/Digits.h"
 #include "DataFormatsCTP/LumiInfo.h"
 #include "DataFormatsCTP/TriggerOffsetsParam.h"
@@ -53,7 +54,7 @@ class RawDecoderSpec : public framework::Task
   void run(framework::ProcessingContext& ctx) final;
   static void makeGBTWordInverse(std::vector<gbtword80_t>& diglets, gbtword80_t& GBTWord, gbtword80_t& remnant, uint32_t& size_gbt, uint32_t Npld);
   int addCTPDigit(uint32_t linkCRU, uint32_t triggerOrbit, gbtword80_t& diglet, gbtword80_t& pldmask, std::map<o2::InteractionRecord, CTPDigit>& digits);
-
+  int decodeRaw(o2::framework::InputRecord& inputs, std::map<o2::InteractionRecord, CTPDigit>& digits, std::vector<LumiInfo>& lumiPointsHBF1);
  protected:
  private:
   static constexpr uint32_t TF_TRIGGERTYPE_MASK = 0x800;
