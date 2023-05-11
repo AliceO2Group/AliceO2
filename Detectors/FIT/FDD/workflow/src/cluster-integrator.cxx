78ef41b2cca89831b287a792fa20fa3bafa21eea
Matthias Kleiner <Matthias.Kleiner@cern.ch>
2023-05-11 12:01:28 +0200

    FDD: adding workflows for integrated currents
    For further informations see: https://github.com/AliceO2Group/AliceO2/pull/10663

---
+++ Detectors/FIT/FDD/workflow/src/cluster-integrator.cxx
@@ -0,0 +1,44 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#include "FITWorkflow/FITIntegrateClusterSpec.h"
+#include "DataFormatsFDD/RecPoint.h"
+#include "FITWorkflow/FITIntegrateClusterWriterSpec.h"
+#include "CommonUtils/ConfigurableParam.h"
+#include "Framework/ConfigParamSpec.h"
+
+using namespace o2::framework;
+
+void customize(std::vector<o2::framework::ConfigParamSpec>& workflowOptions)
+{
+  std::vector<ConfigParamSpec> options{
+    ConfigParamSpec{"configKeyValues", VariantType::String, "", {"Semicolon separated key=value strings"}},
+    {"min-NChan", VariantType::Int, 0, {"Minimum NChan signal required to avoid noise"}},
+    {"min-Ampl", VariantType::Int, 0, {"Minimum Ampl signal required to avoid noise"}},
+    {"disable-root-output", VariantType::Bool, false, {"disable root-files output writers"}}};
+  std::swap(workflowOptions, options);
+}
+
+#include "Framework/runDataProcessing.h"
+
+WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
+{
+  WorkflowSpec wf;
+  o2::conf::ConfigurableParam::updateFromString(cfgc.options().get<std::string>("configKeyValues"));
+  const bool disableWriter = cfgc.options().get<bool>("disable-root-output");
+  const int minNChan = cfgc.options().get<int>("min-NChan");
+  const int minAmpl = cfgc.options().get<int>("min-Ampl");
+  wf.emplace_back(o2::fit::getFITIntegrateClusterSpec<o2::fdd::RecPoint>(disableWriter, minNChan, minAmpl));
+  if (!disableWriter) {
+    wf.emplace_back(o2::fit::getFITIntegrateClusterWriterSpec<o2::fdd::RecPoint>());
+  }
+  return wf;
+}
