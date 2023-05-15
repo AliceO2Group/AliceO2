0a6486f6242114370cae5528625fdc6a6b940ef3
Anton Alkin <anton.alkin@cern.ch>
2023-05-15 14:10:43 +0900

    fixup

--- Framework/Core/src/ArrowTableSlicingCache.cxx
+++ Framework/Core/src/ArrowTableSlicingCache.cxx
@@ -39,6 +39,9 @@ std::pair<int64_t, int64_t> SliceInfoPtr::getSliceFor(int value) const
 
 gsl::span<const int64_t> SliceInfoUnsortedPtr::getSliceFor(int value) const
 {
+  if (values.empty()) {
+    return {};
+  }
   if (value > values[values.size() - 1]) {
     return {};
   }
