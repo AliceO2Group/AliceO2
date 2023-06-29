50e07ae71731a018833f4768f743969774e8219b
Tom Dietel <tom@dietel.net>
2023-06-29 10:38:56 +0200

    Fix formatting

--- Detectors/TRD/qc/src/TRDQCLinkDef.h
+++ Detectors/TRD/qc/src/TRDQCLinkDef.h
@@ -22,7 +22,7 @@
 
 #pragma link C++ class o2::trd::RawDataManager + ;
 #pragma link C++ class o2::trd::RawDataSpan + ;
-#pragma link C++ class o2::trd::ChamberSpacePoint +;
+#pragma link C++ class o2::trd::ChamberSpacePoint + ;
 #pragma link C++ class o2::trd::CoordinateTransformer + ;
 #pragma link C++ class o2::trd::RawDisplay + ;
 #pragma link C++ class o2::trd::MCMDisplay + ;
