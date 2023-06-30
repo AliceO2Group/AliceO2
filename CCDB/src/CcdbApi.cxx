a3fd1b585d92e722abeff775695a0e68e094c416
Michal <trzebuniak.michal@gmail.com>
2023-06-30 13:18:13 +0200

    Replaced deprecated formpost with mime in CCDBApi

--- CCDB/src/CcdbApi.cxx
+++ CCDB/src/CcdbApi.cxx
@@ -316,15 +316,11 @@ int CcdbApi::storeAsBinaryFile(const char* buffer, size_t size, const std::strin
   checkMetadataKeys(metadata);
 
   if (curl != nullptr) {
-    struct curl_httppost* formpost = nullptr;
-    struct curl_httppost* lastptr = nullptr;
-    curl_formadd(&formpost,
-                 &lastptr,
-                 CURLFORM_COPYNAME, "send",
-                 CURLFORM_BUFFER, filename.c_str(),
-                 CURLFORM_BUFFERPTR, buffer, //.Buffer(),
-                 CURLFORM_BUFFERLENGTH, size,
-                 CURLFORM_END);
+    auto mime = curl_mime_init(curl);
+    auto field = curl_mime_addpart(mime);
+    curl_mime_name(field, "send");
+    curl_mime_filedata(field, filename.c_str());
+    curl_mime_data(field, buffer, size);
 
     struct curl_slist* headerlist = nullptr;
     static const char buf[] = "Expect:";
@@ -332,8 +328,8 @@ int CcdbApi::storeAsBinaryFile(const char* buffer, size_t size, const std::strin
 
     curlSetSSLOptions(curl);
 
+    curl_easy_setopt(curl, CURLOPT_MIMEPOST, mime);
     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headerlist);
-    curl_easy_setopt(curl, CURLOPT_HTTPPOST, formpost);
     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
     curl_easy_setopt(curl, CURLOPT_USERAGENT, mUniqueAgentID.c_str());
 
@@ -357,10 +353,10 @@ int CcdbApi::storeAsBinaryFile(const char* buffer, size_t size, const std::strin
     /* always cleanup */
     curl_easy_cleanup(curl);
 
-    /* then cleanup the formpost chain */
-    curl_formfree(formpost);
     /* free slist */
     curl_slist_free_all(headerlist);
+    /* free mime */
+    curl_mime_free(mime);
   } else {
     LOGP(alarm, "curl initialization failure");
     returnValue = -2;
