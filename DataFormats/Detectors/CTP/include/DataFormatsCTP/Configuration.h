0d29b9425605bba0bf59cecc7b1e54a07fca85de
Roman Lietava <roman.lietava@cern.ch>
2023-07-02 12:46:08 +0200

    ctpdev: CTP RunManager class in separate file (#11584)
    * 1 RunManager class to separate file. 2 Configuration improvements
    
    * clang
    
    * fix:typo in define
    
    * fixxing RunManager.h dependences
    
    * fixxing RunManager.h dependences

--- DataFormats/Detectors/CTP/include/DataFormatsCTP/Configuration.h
+++ DataFormats/Detectors/CTP/include/DataFormatsCTP/Configuration.h
@@ -151,6 +151,7 @@ class CTPConfiguration
                     UNKNOWN };
   int loadConfigurationRun3(const std::string& ctpconfiguartion);
   void printStream(std::ostream& stream) const;
+  void setRunNumber(uint32_t runnumber) { mRunNumber = runnumber; }
   std::vector<CTPInput>& getCTPInputs() { return mInputs; }
   std::vector<CTPClass>& getCTPClasses() { return mCTPClasses; }
   const std::vector<CTPInput>& getCTPInputs() const { return mInputs; }      // Read-only interface
@@ -194,50 +195,7 @@ class CTPConfiguration
   int processConfigurationLineRun3v2(std::string& line, int& level, std::map<int, std::vector<int>>& descInputsIndex);
   ClassDefNV(CTPConfiguration, 6);
 };
-// Run Manager
-struct CTPActiveRun {
-  CTPActiveRun() = default;
-  long timeStart;
-  long timeStop;
-  CTPConfiguration cfg;
-  CTPRunScalers scalers;
-};
-class CTPRunManager
-{
- public:
-  CTPRunManager() = default;
-  void init();
-  int loadRun(const std::string& cfg);
-  int startRun(const std::string& cfg);
-  int stopRun(uint32_t irun, long timeStamp);
-  int addScalers(uint32_t irun, std::time_t time);
-  int processMessage(std::string& topic, const std::string& message);
-  void printActiveRuns() const;
-  int saveRunScalersToCCDB(int i);
-  int saveRunConfigToCCDB(CTPConfiguration* cfg, long timeStart);
-  static CTPConfiguration getConfigFromCCDB(long timestamp, std::string run);
-  CTPRunScalers getScalersFromCCDB(long timestamp, std::string, bool& ok);
-  int loadScalerNames();
-  // void setCCDBPathConfig(std::string path) { mCCDBPathCTPConfig = path;};
-  void setCCDBPathScalers(std::string path) { mCCDBPathCTPScalers = path; };
-  static void setCCDBHost(std::string host) { mCCDBHost = host; };
-  void printCounters();
-
- private:
-  /// Database constants
-  // std::string mCCDBHost = "http://ccdb-test.cern.ch:8080";
-  static std::string mCCDBHost;
-  std::string mCCDBPathCTPScalers = "CTP/Calib/Scalers";
-  std::array<CTPActiveRun*, NRUNS> mActiveRuns;
-  std::array<std::uint32_t, NRUNS> mActiveRunNumbers;
-  std::array<uint32_t, CTPRunScalers::NCOUNTERS> mCounters;
-  std::map<std::string, uint32_t> mScalerName2Position;
-  std::map<uint32_t, CTPActiveRun*> mRunsLoaded;
-  int mEOX = 0; // redundancy check
-  int mNew = 1; // 1 - no CCDB: used for QC
-  ClassDefNV(CTPRunManager, 5);
-};
-
 } // namespace ctp
 } // namespace o2
+#include "DataFormatsCTP/RunManager.h"
 #endif //_CTP_CONFIGURATION_H_
