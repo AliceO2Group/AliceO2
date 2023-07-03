3709a0fe224146efcf732d6e7fd2dc41d062aa82
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2023-07-03 14:14:11 +0200

    DPL: update the nextTimeslice correctly in case of dropped data
    This should not happen, but in case it does, it will not stop data
    taking.

--- Framework/Core/src/CommonServices.cxx
+++ Framework/Core/src/CommonServices.cxx
@@ -481,6 +481,13 @@ o2::framework::ServiceSpec CommonServices::decongestionSpec()
       LOGP(debug, "Broadcasting oldest possible output {} due to {} ({})", oldestPossibleOutput.timeslice.value,
            oldestPossibleOutput.slot.index == -1 ? "channel" : "slot",
            oldestPossibleOutput.slot.index == -1 ? oldestPossibleOutput.channel.value : oldestPossibleOutput.slot.index);
+      if (decongestion->orderedCompletionPolicyActive) {
+        auto oldNextTimeslice = decongestion->nextTimeslice;
+        decongestion->nextTimeslice = std::max(decongestion->nextTimeslice, (int64_t)oldestPossibleOutput.timeslice.value);
+        if (oldNextTimeslice != decongestion->nextTimeslice) {
+          LOGP(error, "Some Lifetime::Timeframe data got dropped starting at {}", oldNextTimeslice);
+        }
+      }
       DataProcessingHelpers::broadcastOldestPossibleTimeslice(proxy, oldestPossibleOutput.timeslice.value);
 
       for (int fi = 0; fi < proxy.getNumForwardChannels(); fi++) {
@@ -543,6 +550,13 @@ o2::framework::ServiceSpec CommonServices::decongestionSpec()
             }
           }
           decongestion.lastTimeslice = oldestPossibleOutput.timeslice.value;
+          if (decongestion.orderedCompletionPolicyActive) {
+            int64_t oldNextTimeslice = decongestion.nextTimeslice;
+            decongestion.nextTimeslice = std::max(decongestion.nextTimeslice, (int64_t)oldestPossibleOutput.timeslice.value);
+            if (oldNextTimeslice != decongestion.nextTimeslice) {
+              LOGP(error, "Some Lifetime::Timeframe data got dropped starting at {}", oldNextTimeslice);
+            }
+          }
         },
         TimesliceId{oldestPossibleTimeslice}, -1); },
     .kind = ServiceKind::Serial};
@@ -892,7 +906,7 @@ o2::framework::ServiceSpec CommonServices::dataProcessingStats()
     },
     .postProcessing = [](ProcessingContext& context, void* service) {
       auto* stats = (DataProcessingStats*)service;
-      stats->updateStats({(short)ProcessingStatsId::PERFORMED_COMPUTATIONS, DataProcessingStats::Op::Add, 1}); 
+      stats->updateStats({(short)ProcessingStatsId::PERFORMED_COMPUTATIONS, DataProcessingStats::Op::Add, 1});
       flushMetrics(context.services(), *stats); },
     .preDangling = [](DanglingContext& context, void* service) {
        auto* stats = (DataProcessingStats*)service;
