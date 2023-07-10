d07761070281c1a94c79dc92a63d467002488412
Ole Schmidt <ole.schmidt@cern.ch>
2023-07-10 10:12:11 +0200

    Apply MFT tracker high mult protection always in synch mode

--- prodtests/full-system-test/dpl-workflow.sh
+++ prodtests/full-system-test/dpl-workflow.sh
@@ -103,9 +103,8 @@ if [[ $SYNCMODE == 1 ]]; then
   [[ ! -z ${CUT_RANDOM_FRACTION_ITS:-} ]] && ITS_CONFIG_KEY+="fastMultConfig.cutRandomFraction=$CUT_RANDOM_FRACTION_ITS;"
   if has_detector_reco ITS; then
     [[ $RUNTYPE == "COSMICS" ]] && MFT_CONFIG_KEY+="MFTTracking.irFramesOnly=1;"
-  else
-    MFT_CONFIG_KEY+="MFTTracking.cutMultClusLow=0;MFTTracking.cutMultClusHigh=2000;"
   fi
+  MFT_CONFIG_KEY+="MFTTracking.cutMultClusLow=0;MFTTracking.cutMultClusHigh=2000;"
 
   PVERTEXING_CONFIG_KEY+="pvertexer.meanVertexExtraErrConstraint=0.3;" # for calibration relax the constraint
   if [[ $SYNCRAWMODE == 1 ]]; then # add extra tolerance in sync mode to account for eventual time misalignment
