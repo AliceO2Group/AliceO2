f5bd47dc69de547044feb2a8304c759f32797374
iravasen <ivan.ravasenga@cern.ch>
2023-08-21 17:35:04 +0200

    ITS workflows to generate and parse ADAPOS data for strobe length: first basic workflows

---
+++ Detectors/ITSMFT/ITS/workflow/src/DCSAdaposParserSpec.cxx
@@ -0,0 +1,217 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// @file   DCSAdaposParserSpec.cxx
+
+#include "ITSWorkflow/DCSAdaposParserSpec.h"
+
+namespace o2
+{
+namespace its
+{
+
+//////////////////////////////////////////////////////////////////////////////
+// Default constructor
+ITSDCSAdaposParser::ITSDCSAdaposParser()
+{
+  this->mSelfName = o2::utils::Str::concat_string(ChipMappingITS::getName(), "ITSDCSAdaposParser");
+}
+
+//////////////////////////////////////////////////////////////////////////////
+void ITSDCSAdaposParser::init(InitContext& ic)
+{
+  LOGF(info, "ITSDCSAdaposParser init...", mSelfName);
+
+  this->mCcdbUrl = ic.options().get<std::string>("ccdb-url");
+
+  this->mVerboseOutput = ic.options().get<bool>("verbose");
+
+  return;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// Main running function
+// Get Data from ADAPOS and prepare CCDB objects
+void ITSDCSAdaposParser::run(ProcessingContext& pc)
+{
+
+  // Retrieve adapos data and process them
+  auto dps = pc.inputs().get<gsl::span<DPCOM>>("input");
+  process(dps);
+
+  if (doStrobeUpload && dps.size() > 0) {
+    // upload to ccdb
+    pushToCCDB(pc);
+  }
+
+  if (mTF % 50 == 0) { // clean memory after some TFs
+    mDPstrobe.clear();
+  }
+
+  mTF++;
+
+  return;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// Function to process DPs
+void ITSDCSAdaposParser::process(const gsl::span<const DPCOM> dps)
+{
+
+  // first we check which DPs are missing - if some are, it means that
+  // the delta map was sent
+  if (mVerboseOutput) {
+    LOG(info) << "\n\n\nProcessing new TF\n-----------------";
+  }
+
+  // Process all DPs, one by one
+  for (const auto& it : dps) {
+    processDP(it);
+  }
+
+  /**************************************
+     decide whether to upload or not the object to ccdb. The logic for strobe length is:
+     - if the strobe length it's the first value filled in mDPstrobe, then upload it to CCDB (only at the first call of the wf)
+     - if the strobe length it's the second value filled, compare it with the previous one: if different, upload the most recent
+     - the logic continue... memory is cleaned every 50 TFs (random number of TFs).
+  ***************************************/
+  auto mapel = mDPstrobe.begin();
+  if (!mDPstrobe.size()) {
+    doStrobeUpload = false;
+    return;
+  }
+
+  if (mapel->second.size() == 1 && isFirstCheck) {
+    mStrobeToUpload = mapel->second[0].payload_pt1;
+    doStrobeUpload = true;
+    isFirstCheck = false;
+  } else if (mapel->second.size() == 1 && mapel->second[0].payload_pt1 != mStrobeToUpload && !isFirstCheck) { // in case the new value to be uploaded is the first on the list (we erase memory every 50 TFs)
+    mStrobeToUpload = mapel->second[0].payload_pt1;
+    doStrobeUpload = true;
+  } else if (mapel->second.size() > 1) {
+    if (mapel->second.rbegin()[0].payload_pt1 == mapel->second.rbegin()[1].payload_pt1) {
+      doStrobeUpload = false;
+    } else {
+      mStrobeToUpload = mapel->second.rbegin()[0].payload_pt1; // take the last
+      doStrobeUpload = true;
+    }
+  } else {
+    doStrobeUpload = false;
+  }
+
+  return;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// Process single DPs
+void ITSDCSAdaposParser::processDP(const DPCOM& dpcom)
+{
+  auto& dpid = dpcom.id;
+  const auto& type = dpid.get_type();
+  auto& val = dpcom.data;
+  auto flags = val.get_flags();
+
+  if (mVerboseOutput) {
+    LOG(info) << "Processing DP = " << dpcom << ", with value = " << o2::dcs::getValue<int>(dpcom);
+  }
+
+  if (o2::dcs::getValue<int>(dpcom) < 190) {
+    if (mVerboseOutput) {
+      LOG(info) << "Value is < 190 BCs, skipping it";
+    }
+    return;
+  }
+  // Checking if the DP is updated with time stamp information
+  auto etime = val.get_epoch_time();
+
+  if (o2::dcs::getValue<int>(dpcom) > 189) { // Discard strobe length lower than this: thr scan
+    mDPstrobe[dpid].push_back(val);
+  }
+}
+
+//////////////////////////////////////////////////////////////////////////////
+void ITSDCSAdaposParser::pushToCCDB(ProcessingContext& pc)
+{
+  // Timestamps for CCDB entry
+  long tstart = 0, tend = 0;
+  // retireve run start/stop times from CCDB
+  o2::ccdb::CcdbApi api;
+  api.init("http://alice-ccdb.cern.ch");
+  // Initialize empty metadata object for search
+  std::map<std::string, std::string> metadata;
+
+  tstart = o2::ccdb::getCurrentTimestamp();
+  tend = tstart + 365L * 2 * 24 * 3600 * 1000; // valid two years by default
+
+  // Create metadata for database object
+  metadata = {{"comment", "uploaded by flp199 (ADAPOS data)"}};
+
+  std::string path("ITS/Config/AlpideParam");
+
+  std::string filename = "o2-itsmft-DPLAlpideParam<0>_" + std::to_string(tstart) + ".root";
+  o2::ccdb::CcdbObjectInfo info(path, "dplalpideparam", filename, metadata, tstart, tend);
+  // Define the dpl alpide param and set the strobe length to ship
+  auto& dplAlpideParams = o2::itsmft::DPLAlpideParam<o2::detectors::DetID::ITS>::Instance();
+  int* sl = (int*)&dplAlpideParams.roFrameLengthInBC;
+  *sl = (int)mStrobeToUpload + 8; // +8 is because the strobe length of ALPIDE (sent via ADAPOS) is 200ns shorted than the external trigger strobe length.
+  auto class_name = o2::utils::MemFileHelper::getClassName(dplAlpideParams);
+
+  auto image = o2::ccdb::CcdbApi::createObjectImage(&dplAlpideParams, &info);
+  info.setFileName(filename);
+
+  // Send to ccdb-populator wf
+  LOG(info) << "Class Name: " << class_name << " | File Name: " << filename
+            << "\nSending to ccdb-populator the object " << info.getPath() << "/" << info.getFileName()
+            << " of size " << image->size() << " bytes, valid for "
+            << info.getStartValidityTimestamp() << " : "
+            << info.getEndValidityTimestamp();
+
+  if (mCcdbUrl.empty()) {
+
+    pc.outputs().snapshot(Output{o2::calibration::Utils::gDataOriginCDBPayload, "ITSALPIDEPARAM", 0}, *image);
+    pc.outputs().snapshot(Output{o2::calibration::Utils::gDataOriginCDBWrapper, "ITSALPIDEPARAM", 0}, info);
+
+  } else { // if url is specified, send object to ccdb from THIS wf
+
+    LOG(info) << "Sending object " << info.getFileName() << " to " << mCcdbUrl << "/browse/"
+              << info.getPath() << " from the ITS ADAPOS parser workflow";
+    o2::ccdb::CcdbApi mApi;
+    mApi.init(mCcdbUrl);
+    mApi.storeAsBinaryFile(
+      &image->at(0), image->size(), info.getFileName(), info.getObjectType(), info.getPath(),
+      info.getMetaData(), info.getStartValidityTimestamp(), info.getEndValidityTimestamp());
+  }
+
+  return;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+DataProcessorSpec getITSDCSAdaposParserSpec()
+{
+  o2::header::DataOrigin detOrig = o2::header::gDataOriginITS;
+  std::vector<InputSpec> inputs;
+  inputs.emplace_back("input", "ITS", "ITSDATAPOINTS");
+
+  std::vector<OutputSpec> outputs;
+  outputs.emplace_back(ConcreteDataTypeMatcher{o2::calibration::Utils::gDataOriginCDBPayload, "ITSALPIDEPARAM"}, Lifetime::Sporadic);
+  outputs.emplace_back(ConcreteDataTypeMatcher{o2::calibration::Utils::gDataOriginCDBWrapper, "ITSALPIDEPARAM"}, Lifetime::Sporadic);
+
+  return DataProcessorSpec{
+    "its-adapos-parser",
+    inputs,
+    outputs,
+    AlgorithmSpec{adaptFromTask<o2::its::ITSDCSAdaposParser>()},
+    Options{
+      {"verbose", VariantType::Bool, false, {"Use verbose output mode"}},
+      {"ccdb-url", VariantType::String, "", {"CCDB url, default is empty (i.e. send output to CCDB populator workflow)"}}}};
+}
+} // namespace its
+} // namespace o2
---
+++ Detectors/ITSMFT/ITS/workflow/src/DCSGeneratorSpec.cxx
@@ -0,0 +1,149 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#include "ITSWorkflow/DCSGeneratorSpec.h"
+#include "DetectorsDCS/DataPointCompositeObject.h"
+#include "DetectorsDCS/DataPointGenerator.h"
+#include "DetectorsDCS/DataPointCreator.h"
+#include "Framework/ConfigParamRegistry.h"
+#include "Framework/ControlService.h"
+#include "Framework/DeviceSpec.h"
+#include "Framework/Logger.h"
+#include "Framework/Task.h"
+#include <TDatime.h>
+#include <random>
+#include <variant>
+#include <string>
+#include <algorithm>
+
+using namespace o2::its;
+
+namespace
+{
+std::vector<o2::dcs::DataPointCompositeObject> generate(std::vector<std::string> aliases, int val)
+{
+  std::vector<o2::dcs::DataPointCompositeObject> dataPoints;
+
+  for (auto alias : aliases) {
+    dataPoints.emplace_back(o2::dcs::createDataPointCompositeObject(alias, val, 1, 0));
+  }
+
+  return dataPoints;
+}
+
+//______________________________________________________________________________________________________________
+// class ITSDCSDataGenerator
+class ITSDCSDataGenerator : public o2::framework::Task
+{
+ public:
+  ITSDCSDataGenerator(o2::header::DataDescription description);
+
+  void init(o2::framework::InitContext& ic) final;
+
+  void run(o2::framework::ProcessingContext& pc) final;
+
+  void fillAliasesStrobeDuration();
+
+ private:
+  uint64_t mMaxTF = 1e6;
+  uint64_t mTFs = 0;
+  uint64_t mMaxCyclesNoFullMap;
+  uint64_t changeAfterTF = 0;
+  int valueA = 190;
+  int valueB = 190;
+  bool isStrobeDurationData = false;
+  std::vector<std::string> mAliases;
+  std::vector<o2::dcs::test::HintType> mDataPointHints;
+  o2::header::DataDescription mDataDescription;
+};
+
+ITSDCSDataGenerator::ITSDCSDataGenerator(o2::header::DataDescription description) : mDataDescription(description) {}
+
+void ITSDCSDataGenerator::fillAliasesStrobeDuration()
+{
+
+  // Aliases in this case are in the format: ITS_L0_00_STROBE
+  // Here we fill them for every stave in mAliases
+  int nStaves[] = {12, 16, 20, 24, 30, 42, 48};
+  for (int iL = 0; iL < 7; iL++) {
+    for (int iS = 0; iS < nStaves[iL]; iS++) {
+      std::string stv = iS > 9 ? std::to_string(iS) : std::string(1, '0').append(std::to_string(iS));
+      mAliases.push_back("ITS_L" + std::to_string(iL) + "_" + stv + "_STROBE");
+    }
+  }
+}
+
+void ITSDCSDataGenerator::init(o2::framework::InitContext& ic)
+{
+  mMaxTF = ic.options().get<int64_t>("max-timeframes");
+  mMaxCyclesNoFullMap = ic.options().get<int64_t>("max-cycles-no-full-map");
+  isStrobeDurationData = ic.options().get<bool>("generate-strobe-duration-data");
+  changeAfterTF = ic.options().get<int64_t>("change-after-n-timeframes");
+  valueA = ic.options().get<int>("value-a");
+  valueB = ic.options().get<int>("value-b");
+
+  if (isStrobeDurationData) {
+    fillAliasesStrobeDuration();
+  }
+}
+
+void ITSDCSDataGenerator::run(o2::framework::ProcessingContext& pc)
+{
+  auto input = pc.inputs().begin();
+  uint64_t tfid = o2::header::get<o2::framework::DataProcessingHeader*>((*input).header)->startTime;
+  if (tfid >= mMaxTF) {
+    LOG(info) << "ITS DCS Data generator reached TF " << tfid << ", stopping";
+    pc.services().get<o2::framework::ControlService>().endOfStream();
+    pc.services().get<o2::framework::ControlService>().readyToQuit(o2::framework::QuitRequest::Me);
+  }
+
+  // generate data simulating ADAPOS
+  bool doGen = mTFs % mMaxCyclesNoFullMap == 0;
+  std::vector<o2::dcs::DataPointCompositeObject> dpcoms;
+  if (doGen) {
+    dpcoms = generate(mAliases, mTFs > changeAfterTF ? valueB : valueA);
+  }
+
+  LOG(info) << "TF " << tfid << " has generated " << dpcoms.size() << " DPs";
+  auto& timingInfo = pc.services().get<o2::framework::TimingInfo>();
+  auto timeNow = std::chrono::system_clock::now();
+  timingInfo.creation = std::chrono::duration_cast<std::chrono::milliseconds>(timeNow.time_since_epoch()).count(); // in ms
+
+  pc.outputs().snapshot(Output{"ITS", mDataDescription, 0, Lifetime::Timeframe}, dpcoms);
+  mTFs++;
+}
+} // namespace
+
+namespace o2::its
+{
+o2::framework::DataProcessorSpec getITSDCSDataGeneratorSpec(const char* detName)
+{
+  std::string desc{detName};
+  desc += "DATAPOINTS";
+
+  o2::header::DataDescription dd;
+
+  dd.runtimeInit(desc.c_str(), desc.size());
+
+  return DataProcessorSpec{
+    "its-dcs-data-generator",
+    Inputs{},
+    Outputs{{{"outputDCS"}, "ITS", dd}},
+    AlgorithmSpec{adaptFromTask<ITSDCSDataGenerator>(dd)},
+    Options{
+      {"change-after-n-timeframes", VariantType::Int64, 99999999999ll, {"change value generated after n timeframes: do not change val by default"}},
+      {"value-a", VariantType::Int, 0, {"First value to be generated, will change to value-b after nTF = change-after-n-timeframes"}},
+      {"value-b", VariantType::Int, 1, {"Second  value to be generated, will be after value-a once nTF = change-after-n-timeframes has been reached"}},
+      {"max-timeframes", VariantType::Int64, 99999999999ll, {"max TimeFrames to generate"}},
+      {"max-cycles-no-full-map", VariantType::Int64, 6000ll, {"max num of cycles between the sending of 2 full maps"}},
+      {"generate-strobe-duration-data", VariantType::Bool, false, {"enable generation of DCS data containing the strobe duration in BCs"}}}};
+}
+} // namespace o2::its
