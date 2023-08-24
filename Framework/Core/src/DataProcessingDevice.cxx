e8bb6b49a9c959ab7b972a52d35eb7f7bdeb996c
Piotr Konopka <piotr.jan.konopka@cern.ch>
2023-08-24 08:58:27 +0200

    Revert "DPL: handle spurious messages when in READY state (#11799)" (#11808)
    This reverts commit 44b555989a61a5a17252e3acf87556b9a23a6460.
    
    It seems to break multinode QC test which intermittently time-outs when exiting a workflow with a proxy

--- Framework/Core/src/DataProcessingDevice.cxx
+++ Framework/Core/src/DataProcessingDevice.cxx
@@ -1494,10 +1494,6 @@ void DataProcessingDevice::doPrepare(ServiceRegistryRef ref)
     if (info.channel == nullptr) {
       continue;
     }
-    // Only poll DPL channels for now.
-    if (info.channelType != ChannelAccountingType::DPL) {
-      continue;
-    }
     auto& socket = info.channel->GetSocket();
     // If we have pending events from a previous iteration,
     // we do receive in any case.
