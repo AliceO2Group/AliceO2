93cb973febc5b01b7fa3469341b22a22ed81c4d6
David Rohr <drohr@jwdt.org>
2023-08-28 20:48:18 +0200

    GPU TPC: Add extra systematic cluster errors at IFC

--- GPU/GPUTracking/SliceTracker/GPUTPCTrackletConstructor.cxx
+++ GPU/GPUTracking/SliceTracker/GPUTPCTrackletConstructor.cxx
@@ -203,6 +203,9 @@ GPUdic(2, 1) void GPUTPCTrackletConstructor::UpdateTracklet(int /*nBlocks*/, int
         tracker.GetErrors2Seeding(iRow, tParam.GetZ(), sinPhi, tParam.GetDzDs(), err2Y, err2Z);
 
         if (r.mNHits >= 10) {
+          const float sErr2 = tracker.Param().GetSystematicClusterErrorIFC2(x, tParam.GetZ(), tracker.ISlice() > 18);
+          err2Y += sErr2;
+          err2Z += sErr2;
           const float kFactor = tracker.Param().rec.tpc.hitPickUpFactor * tracker.Param().rec.tpc.hitPickUpFactor * 3.5f * 3.5f;
           float sy2 = kFactor * (tParam.Err2Y() + err2Y);
           float sz2 = kFactor * (tParam.Err2Z() + err2Z);
@@ -298,6 +301,11 @@ GPUdic(2, 1) void GPUTPCTrackletConstructor::UpdateTracklet(int /*nBlocks*/, int
 
       float err2Y, err2Z;
       tracker.GetErrors2Seeding(iRow, *((MEM_LG2(GPUTPCTrackParam)*)&tParam), err2Y, err2Z);
+      if (r.mNHits >= 10) {
+        const float sErr2 = tracker.Param().GetSystematicClusterErrorIFC2(x, tParam.GetZ(), tracker.ISlice() > 18);
+        err2Y += sErr2;
+        err2Z += sErr2;
+      }
       if (CAMath::Abs(yUncorrected) < x * MEM_GLOBAL(GPUTPCRow)::getTPCMaxY1X()) { // search for the closest hit
         const float kFactor = tracker.Param().rec.tpc.hitPickUpFactor * tracker.Param().rec.tpc.hitPickUpFactor * 7.0f * 7.0f;
         const float maxWindow2 = tracker.Param().rec.tpc.hitSearchArea2;
