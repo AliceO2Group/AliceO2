af94159ca7079a2fcf3de9b30bf64d8908525067
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2023-09-01 06:41:57 +0200

    Hide clang thread analyser code to avoid clash with older version in (#11847)

--- Framework/Foundation/include/Framework/ThreadSafetyAnalysis.h
+++ Framework/Foundation/include/Framework/ThreadSafetyAnalysis.h
@@ -21,72 +21,72 @@
 
 // Assume that we only need this is someone else is not using the
 // same clang provided macros
-#ifndef THREAD_ANNOTATION_ATTRIBUTE__
+#ifndef O2_DPL_THREAD_ANNOTATION_ATTRIBUTE__
 #if defined(__clang__) && (!defined(SWIG)) && defined(_LIBCPP_ENABLE_THREAD_SAFETY_ANNOTATIONS)
-#define THREAD_ANNOTATION_ATTRIBUTE__(x) __attribute__((x))
+#define O2_DPL_THREAD_ANNOTATION_ATTRIBUTE__(x) __attribute__((x))
 #else
-#define THREAD_ANNOTATION_ATTRIBUTE__(x) // no-op
+#define O2_DPL_THREAD_ANNOTATION_ATTRIBUTE__(x) // no-op
 #endif
 
-#define CAPABILITY(x) \
-  THREAD_ANNOTATION_ATTRIBUTE__(capability(x))
+#define O2_DPL_CAPABILITY(x) \
+  O2_DPL_THREAD_ANNOTATION_ATTRIBUTE__(capability(x))
 
-#define SCOPED_CAPABILITY \
-  THREAD_ANNOTATION_ATTRIBUTE__(scoped_lockable)
+#define O2_DPL_SCOPED_CAPABILITY \
+  O2_DPL_THREAD_ANNOTATION_ATTRIBUTE__(scoped_lockable)
 
-#define GUARDED_BY(x) \
-  THREAD_ANNOTATION_ATTRIBUTE__(guarded_by(x))
+#define O2_DPL_GUARDED_BY(x) \
+  O2_DPL_THREAD_ANNOTATION_ATTRIBUTE__(guarded_by(x))
 
-#define PT_GUARDED_BY(x) \
-  THREAD_ANNOTATION_ATTRIBUTE__(pt_guarded_by(x))
+#define O2_DPL_PT_GUARDED_BY(x) \
+  O2_DPL_THREAD_ANNOTATION_ATTRIBUTE__(pt_guarded_by(x))
 
-#define ACQUIRED_BEFORE(...) \
-  THREAD_ANNOTATION_ATTRIBUTE__(acquired_before(__VA_ARGS__))
+#define O2_DPL_ACQUIRED_BEFORE(...) \
+  O2_DPL_THREAD_ANNOTATION_ATTRIBUTE__(acquired_before(__VA_ARGS__))
 
-#define ACQUIRED_AFTER(...) \
-  THREAD_ANNOTATION_ATTRIBUTE__(acquired_after(__VA_ARGS__))
+#define O2_DPL_ACQUIRED_AFTER(...) \
+  O2_DPL_THREAD_ANNOTATION_ATTRIBUTE__(acquired_after(__VA_ARGS__))
 
-#define REQUIRES(...) \
-  THREAD_ANNOTATION_ATTRIBUTE__(requires_capability(__VA_ARGS__))
+#define O2_DPL_REQUIRES(...) \
+  O2_DPL_THREAD_ANNOTATION_ATTRIBUTE__(requires_capability(__VA_ARGS__))
 
-#define REQUIRES_SHARED(...) \
-  THREAD_ANNOTATION_ATTRIBUTE__(requires_shared_capability(__VA_ARGS__))
+#define O2_DPL_REQUIRES_SHARED(...) \
+  O2_DPL_THREAD_ANNOTATION_ATTRIBUTE__(requires_shared_capability(__VA_ARGS__))
 
-#define ACQUIRE(...) \
-  THREAD_ANNOTATION_ATTRIBUTE__(acquire_capability(__VA_ARGS__))
+#define O2_DPL_ACQUIRE(...) \
+  O2_DPL_THREAD_ANNOTATION_ATTRIBUTE__(acquire_capability(__VA_ARGS__))
 
-#define ACQUIRE_SHARED(...) \
-  THREAD_ANNOTATION_ATTRIBUTE__(acquire_shared_capability(__VA_ARGS__))
+#define O2_DPL_ACQUIRE_SHARED(...) \
+  O2_DPL_THREAD_ANNOTATION_ATTRIBUTE__(acquire_shared_capability(__VA_ARGS__))
 
-#define RELEASE(...) \
-  THREAD_ANNOTATION_ATTRIBUTE__(release_capability(__VA_ARGS__))
+#define O2_DPL_RELEASE(...) \
+  O2_DPL_THREAD_ANNOTATION_ATTRIBUTE__(release_capability(__VA_ARGS__))
 
-#define RELEASE_SHARED(...) \
-  THREAD_ANNOTATION_ATTRIBUTE__(release_shared_capability(__VA_ARGS__))
+#define O2_DPL_RELEASE_SHARED(...) \
+  O2_DPL_THREAD_ANNOTATION_ATTRIBUTE__(release_shared_capability(__VA_ARGS__))
 
-#define RELEASE_GENERIC(...) \
-  THREAD_ANNOTATION_ATTRIBUTE__(release_generic_capability(__VA_ARGS__))
+#define O2_DPL_RELEASE_GENERIC(...) \
+  O2_DPL_THREAD_ANNOTATION_ATTRIBUTE__(release_generic_capability(__VA_ARGS__))
 
-#define TRY_ACQUIRE(...) \
-  THREAD_ANNOTATION_ATTRIBUTE__(try_acquire_capability(__VA_ARGS__))
+#define O2_DPL_TRY_ACQUIRE(...) \
+  O2_DPL_THREAD_ANNOTATION_ATTRIBUTE__(try_acquire_capability(__VA_ARGS__))
 
-#define TRY_ACQUIRE_SHARED(...) \
-  THREAD_ANNOTATION_ATTRIBUTE__(try_acquire_shared_capability(__VA_ARGS__))
+#define O2_DPL_TRY_ACQUIRE_SHARED(...) \
+  O2_DPL_THREAD_ANNOTATION_ATTRIBUTE__(try_acquire_shared_capability(__VA_ARGS__))
 
-#define EXCLUDES(...) \
-  THREAD_ANNOTATION_ATTRIBUTE__(locks_excluded(__VA_ARGS__))
+#define O2_DPL_EXCLUDES(...) \
+  O2_DPL_THREAD_ANNOTATION_ATTRIBUTE__(locks_excluded(__VA_ARGS__))
 
-#define ASSERT_CAPABILITY(x) \
-  THREAD_ANNOTATION_ATTRIBUTE__(assert_capability(x))
+#define O2_DPL_ASSERT_CAPABILITY(x) \
+  O2_DPL_THREAD_ANNOTATION_ATTRIBUTE__(assert_capability(x))
 
-#define ASSERT_SHARED_CAPABILITY(x) \
-  THREAD_ANNOTATION_ATTRIBUTE__(assert_shared_capability(x))
+#define O2_DPL_ASSERT_SHARED_CAPABILITY(x) \
+  O2_DPL_THREAD_ANNOTATION_ATTRIBUTE__(assert_shared_capability(x))
 
-#define RETURN_CAPABILITY(x) \
-  THREAD_ANNOTATION_ATTRIBUTE__(lock_returned(x))
+#define O2_DPL_RETURN_CAPABILITY(x) \
+  O2_DPL_THREAD_ANNOTATION_ATTRIBUTE__(lock_returned(x))
 
-#define NO_THREAD_SAFETY_ANALYSIS \
-  THREAD_ANNOTATION_ATTRIBUTE__(no_thread_safety_analysis)
+#define O2_DPL_NO_THREAD_SAFETY_ANALYSIS \
+  O2_DPL_THREAD_ANNOTATION_ATTRIBUTE__(no_thread_safety_analysis)
 
-#endif // THREAD_ANNOTATION_ATTRIBUTE__
+#endif // O2_DPL_THREAD_ANNOTATION_ATTRIBUTE__
 #endif // O2_FRAMEWORK_THREADSAFETYANALYSIS_H_
