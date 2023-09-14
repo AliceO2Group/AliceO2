805893d5933b8ea37735b60bc4a46c6373505333
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2023-09-14 09:22:26 +0200

    Get rid of abseil dependency

--- dependencies/O2Dependencies.cmake
+++ dependencies/O2Dependencies.cmake
@@ -173,9 +173,6 @@ find_package(FastJet)
 find_package(FFTW3f CONFIG)
 set_package_properties(FFTW3f PROPERTIES TYPE REQUIRED)
 
-find_package(absl CONFIG)
-set_package_properties(absl PROPERTIES TYPE REQUIRED)
-
 find_package(Vtune)
 set_package_properties(Vtune PROPERTIES TYPE OPTIONAL)
 
