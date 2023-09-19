2e413a322b29d161384dc7b935d9ffe52ac3dcf0
Christian Holm Christensen <cholmcc@gmail.com>
2023-09-19 15:10:00 +0200

    Fixes for copyright stuff - sigh!

--- Generators/include/Generators/GeneratorTParticle.h
+++ Generators/include/Generators/GeneratorTParticle.h
@@ -1,18 +1,14 @@
-// -*- C++ -*-
-// Copyright 2023 CERN and copyright holders of ALICE O2.
-//
-// See https://alice-o2.web.cern.ch/copyright for details of the
-// copyright holders.  All rights not expressly granted are reserved.
-//
-// This software is distributed under the terms of the GNU General
-// Public License v3 (GPL Version 3), copied verbatim in the file
-// "COPYING".
-//
-// In applying this license CERN does not waive the privileges and
-// immunities granted to it by virtue of its status as an
-// Intergovernmental Organization or submit itself to any
-// jurisdiction.
-//
+// Copyright 2023-2099 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//  
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//  
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
 /// @author: Christian Holm Christensen <cholm@nbi.dk>
 #ifndef ALICEO2_GENERATORTPARTICLE_H_
 #define ALICEO2_GENERATORTPARTICLE_H_
@@ -110,6 +106,9 @@ TClonesArray of @c TParticle objects */
 } // namespace eventgen
 } // namespace o2
 #endif
+// Local Variables:
+//  mode: C++
+// End:
 //
 // EOF
 //
