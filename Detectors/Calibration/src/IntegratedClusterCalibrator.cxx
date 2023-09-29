310ed96bd49c8893bee90449aa2028ecce222c73
Matthias Kleiner <Matthias.Kleiner@cern.ch>
2023-09-29 16:41:52 +0200

    TPC: Adding workflow for integrated DCAs for corrections and QC
    - DCAs are integrated in phi bins over all tgl and in tgl bins
    integrated over phi
    - DCA integrated per side is also stored
    - RMS and number of tracks are stored per bin
    - Multithreading for DCA calculation of tracks
    - Additional information with per track DCAs can be written with debug
    streamer
    - Adding start time as meta data to integrated currents
    - Adding DCAs for ITS-TPC matched tracks
    - Adding collision vertex
    - Adding nearest vertex to tracks which have no vertex assigned
    - Adding DCAz from time bin delta
    
    Adding ITS-TPC matching efficiency
    
    Adding: sqrt(chi2Match), MIP/dedx, sqrt(chi2), nCl

--- Detectors/Calibration/src/IntegratedClusterCalibrator.cxx
+++ Detectors/Calibration/src/IntegratedClusterCalibrator.cxx
@@ -165,6 +165,7 @@ void IntegratedClusterCalibrator<DataT>::finalizeSlot(o2::calibration::TimeSlot<
   LOGP(info, "Finalizing slot {} <= TF <= {}", startTF, endTF);
 
   auto& integratedClusters = *slot.getContainer();
+  integratedClusters.setStartTime(slot.getStartTimeMS());
   if (mDebug) {
     integratedClusters.dumpToFile(fmt::format("IntegratedClusters_TF_{}_{}_TS_{}_{}.root", startTF, endTF, slot.getStartTimeMS(), slot.getEndTimeMS()).data());
   }
@@ -196,8 +197,218 @@ template class IntegratedClusterCalibrator<o2::fit::IFV0C>;
 template class IntegratedClusters<o2::tpc::ITPCC>;
 template class IntegratedClusterCalibrator<o2::tpc::ITPCC>;
 
+template class IntegratedClusters<o2::tpc::TimeSeriesITSTPC>;
+template class IntegratedClusterCalibrator<o2::tpc::TimeSeriesITSTPC>;
+
 template class IntegratedClusters<o2::fit::IFDDC>;
 template class IntegratedClusterCalibrator<o2::fit::IFDDC>;
 
 } // end namespace calibration
+
+void tpc::TimeSeriesITSTPC::dumpToTree(const char* outFileName, const int nHBFPerTF)
+{
+  TFile f(outFileName, "RECREATE");
+  TTree tree("timeSeries", "timeSeries");
+  TimeSeries::dumpToTree(outFileName, tree, mTSTPC, "TPC_", nHBFPerTF);
+  TimeSeries::dumpToTree(outFileName, tree, mTSITSTPC, "ITSTPC_", nHBFPerTF);
+
+  // adding matching efficiency
+  const auto nValues = mTSTPC.mDCAr_A_Median.size();
+  const int nPointsPerTF = mTSTPC.getNBins();
+  const int nTotPoints = nValues / nPointsPerTF;
+
+  const int nBinTypes = 4;
+  for (int type = 0; type < nBinTypes; ++type) {
+    std::array<int, nBinTypes> nBinsAllTypes{mTSTPC.mNBinsPhi, mTSTPC.mNBinsTgl, mTSTPC.mNBinsqPt, 1};
+    std::array<std::string, nBinTypes> nBinsAllTypesNames{"phi", "tgl", "qPt", "int"};
+    int nBins = nBinsAllTypes[type];
+
+    std::vector<std::vector<float>> matching_eff_A(nBins);
+    std::vector<std::vector<float>> matching_eff_C(nBins);
+    std::vector<std::vector<float>> chi2Match_A(nBins);
+    std::vector<std::vector<float>> chi2Match_C(nBins);
+
+    std::vector<TBranch*> listBranches;
+    listBranches.reserve(4 * nBins);
+    for (int i = 0; i < nBins; ++i) {
+      const std::string brSuf = (type == nBinTypes - 1) ? nBinsAllTypesNames[type] : fmt::format("{}_{}", nBinsAllTypesNames[type], i);
+      listBranches.emplace_back(tree.Branch(fmt::format("mITSTPC_A_MatchEff_{}", brSuf).data(), &matching_eff_A[i]));
+      listBranches.emplace_back(tree.Branch(fmt::format("mITSTPC_C_MatchEff_{}", brSuf).data(), &matching_eff_C[i]));
+      listBranches.emplace_back(tree.Branch(fmt::format("mITSTPC_A_Chi2Match_{}", brSuf).data(), &chi2Match_A[i]));
+      listBranches.emplace_back(tree.Branch(fmt::format("mITSTPC_C_Chi2Match_{}", brSuf).data(), &chi2Match_C[i]));
+    }
+
+    // alloc memory
+    for (int i = 0; i < nBins; ++i) {
+      matching_eff_A[i].resize(nTotPoints);
+      matching_eff_C[i].resize(nTotPoints);
+      chi2Match_A[i].resize(nTotPoints);
+      chi2Match_C[i].resize(nTotPoints);
+    }
+
+    for (int j = 0; j < nTotPoints; ++j) {
+      for (int i = 0; i < nBins; ++i) {
+        int idx = mTSTPC.getIndexPhi(i, j); // phi bins
+        if (type == 1) {
+          idx = mTSTPC.getIndexTgl(i, j); // j * nPointsPerTF + i + mTSTPC.mNBinsPhi; // tgl bins
+        } else if (type == 2) {
+          idx = mTSTPC.getIndexqPt(i, j); // j * nPointsPerTF + mTSTPC.mNBinsPhi + mTSTPC.mNBinsTgl; // qPt
+        } else if (type == 3) {
+          idx = mTSTPC.getIndexInt(j);
+        }
+        matching_eff_A[i][j] = mITSTPC_A_MatchEff[idx];
+        matching_eff_C[i][j] = mITSTPC_C_MatchEff[idx];
+        chi2Match_A[i][j] = mITSTPC_A_Chi2Match[idx];
+        chi2Match_C[i][j] = mITSTPC_C_Chi2Match[idx];
+      }
+    }
+    // fill branches
+    for (auto br : listBranches) {
+      br->Fill();
+    }
+  }
+  tree.SetEntries(1);
+  f.WriteObject(&tree, "timeSeries");
+}
+
+void tpc::TimeSeries::dumpToTree(const char* outFileName, TTree& tree, const tpc::TimeSeries& idc, const char* prefix, const int nHBFPerTF)
+{
+  const auto nValues = idc.mDCAr_A_Median.size();
+  const int nPointsPerTF = idc.mNBinsPhi + idc.mNBinsTgl + 1;
+  const int nTotPoints = nValues / nPointsPerTF;
+
+  const int nBinTypes = 4;
+  for (int type = 0; type < nBinTypes; ++type) {
+    std::array<int, nBinTypes> nBinsAllTypes{idc.mNBinsPhi, idc.mNBinsTgl, idc.mNBinsqPt, 1};
+    std::array<std::string, nBinTypes> nBinsAllTypesNames{"phi", "tgl", "qPt", "int"};
+    int nBins = nBinsAllTypes[type];
+
+    std::vector<std::vector<float>> dcar_A_Median(nBins);
+    std::vector<std::vector<float>> dcar_C_Median(nBins);
+    std::vector<std::vector<float>> dcar_A_WeightedMean(nBins);
+    std::vector<std::vector<float>> dcar_C_WeightedMean(nBins);
+    std::vector<std::vector<float>> dcar_A_RMS(nBins);
+    std::vector<std::vector<float>> dcar_C_RMS(nBins);
+    std::vector<std::vector<float>> dcaz_A_Median(nBins);
+    std::vector<std::vector<float>> dcaz_C_Median(nBins);
+    std::vector<std::vector<float>> dcaz_A_WeightedMean(nBins);
+    std::vector<std::vector<float>> dcaz_C_WeightedMean(nBins);
+    std::vector<std::vector<float>> dcaz_A_RMS(nBins);
+    std::vector<std::vector<float>> dcaz_C_RMS(nBins);
+    std::vector<std::vector<float>> nTracksDCAr_A(nBins);
+    std::vector<std::vector<float>> nTracksDCAr_C(nBins);
+    std::vector<std::vector<float>> nTracksDCAz_A(nBins);
+    std::vector<std::vector<float>> nTracksDCAz_C(nBins);
+    std::vector<std::vector<float>> mMIPdEdxRatioA(nBins);
+    std::vector<std::vector<float>> mMIPdEdxRatioC(nBins);
+    std::vector<std::vector<float>> mTPCChi2A(nBins);
+    std::vector<std::vector<float>> mTPCChi2C(nBins);
+    std::vector<std::vector<float>> mTPCNClA(nBins);
+    std::vector<std::vector<float>> mTPCNClC(nBins);
+
+    std::vector<TBranch*> listBranches;
+    listBranches.reserve(22 * nBins);
+    for (int i = 0; i < nBins; ++i) {
+      const std::string brSuf = (type == nBinTypes - 1) ? nBinsAllTypesNames[type] : fmt::format("{}_{}", nBinsAllTypesNames[type], i);
+      listBranches.emplace_back(tree.Branch(fmt::format("{}mDCAr_A_Median_{}", prefix, brSuf).data(), &dcar_A_Median[i]));
+      listBranches.emplace_back(tree.Branch(fmt::format("{}mDCAr_C_Median_{}", prefix, brSuf).data(), &dcar_C_Median[i]));
+      listBranches.emplace_back(tree.Branch(fmt::format("{}mDCAr_A_WeightedMean_{}", prefix, brSuf).data(), &dcar_A_WeightedMean[i]));
+      listBranches.emplace_back(tree.Branch(fmt::format("{}mDCAr_C_WeightedMean_{}", prefix, brSuf).data(), &dcar_C_WeightedMean[i]));
+      listBranches.emplace_back(tree.Branch(fmt::format("{}mDCAr_A_RMS_{}", prefix, brSuf).data(), &dcar_A_RMS[i]));
+      listBranches.emplace_back(tree.Branch(fmt::format("{}mDCAr_C_RMS_{}", prefix, brSuf).data(), &dcar_C_RMS[i]));
+      listBranches.emplace_back(tree.Branch(fmt::format("{}mDCAz_A_Median_{}", prefix, brSuf).data(), &dcaz_A_Median[i]));
+      listBranches.emplace_back(tree.Branch(fmt::format("{}mDCAz_C_Median_{}", prefix, brSuf).data(), &dcaz_C_Median[i]));
+      listBranches.emplace_back(tree.Branch(fmt::format("{}mDCAz_A_WeightedMean_{}", prefix, brSuf).data(), &dcaz_A_WeightedMean[i]));
+      listBranches.emplace_back(tree.Branch(fmt::format("{}mDCAz_C_WeightedMean_{}", prefix, brSuf).data(), &dcaz_C_WeightedMean[i]));
+      listBranches.emplace_back(tree.Branch(fmt::format("{}mDCAz_A_RMS_{}", prefix, brSuf).data(), &dcaz_A_RMS[i]));
+      listBranches.emplace_back(tree.Branch(fmt::format("{}mDCAz_C_RMS_{}", prefix, brSuf).data(), &dcaz_C_RMS[i]));
+      listBranches.emplace_back(tree.Branch(fmt::format("{}mDCAr_A_NTracks_{}", prefix, brSuf).data(), &nTracksDCAr_A[i]));
+      listBranches.emplace_back(tree.Branch(fmt::format("{}mDCAr_C_NTracks_{}", prefix, brSuf).data(), &nTracksDCAr_C[i]));
+      listBranches.emplace_back(tree.Branch(fmt::format("{}mDCAz_A_NTracks_{}", prefix, brSuf).data(), &nTracksDCAz_A[i]));
+      listBranches.emplace_back(tree.Branch(fmt::format("{}mDCAz_C_NTracks_{}", prefix, brSuf).data(), &nTracksDCAz_C[i]));
+      listBranches.emplace_back(tree.Branch(fmt::format("{}mMIPdEdxRatioA_{}", prefix, brSuf).data(), &mMIPdEdxRatioA[i]));
+      listBranches.emplace_back(tree.Branch(fmt::format("{}mMIPdEdxRatioC_{}", prefix, brSuf).data(), &mMIPdEdxRatioC[i]));
+      listBranches.emplace_back(tree.Branch(fmt::format("{}mTPCChi2A_{}", prefix, brSuf).data(), &mTPCChi2A[i]));
+      listBranches.emplace_back(tree.Branch(fmt::format("{}mTPCChi2C_{}", prefix, brSuf).data(), &mTPCChi2C[i]));
+      listBranches.emplace_back(tree.Branch(fmt::format("{}mTPCNClA_{}", prefix, brSuf).data(), &mTPCNClA[i]));
+      listBranches.emplace_back(tree.Branch(fmt::format("{}mTPCNClC_{}", prefix, brSuf).data(), &mTPCNClC[i]));
+    }
+
+    // alloc memory
+    for (int i = 0; i < nBins; ++i) {
+      dcar_A_Median[i].resize(nTotPoints);
+      dcar_C_Median[i].resize(nTotPoints);
+      dcar_A_WeightedMean[i].resize(nTotPoints);
+      dcar_C_WeightedMean[i].resize(nTotPoints);
+      dcar_A_RMS[i].resize(nTotPoints);
+      dcar_C_RMS[i].resize(nTotPoints);
+      dcaz_A_Median[i].resize(nTotPoints);
+      dcaz_C_Median[i].resize(nTotPoints);
+      dcaz_A_WeightedMean[i].resize(nTotPoints);
+      dcaz_C_WeightedMean[i].resize(nTotPoints);
+      dcaz_A_RMS[i].resize(nTotPoints);
+      dcaz_C_RMS[i].resize(nTotPoints);
+      nTracksDCAr_A[i].resize(nTotPoints);
+      nTracksDCAr_C[i].resize(nTotPoints);
+      nTracksDCAz_A[i].resize(nTotPoints);
+      nTracksDCAz_C[i].resize(nTotPoints);
+      mMIPdEdxRatioA[i].resize(nTotPoints);
+      mMIPdEdxRatioC[i].resize(nTotPoints);
+      mTPCChi2A[i].resize(nTotPoints);
+      mTPCChi2C[i].resize(nTotPoints);
+      mTPCNClA[i].resize(nTotPoints);
+      mTPCNClC[i].resize(nTotPoints);
+    }
+
+    std::vector<double> time(nTotPoints);
+    if (type == 0) {
+      listBranches.emplace_back(tree.Branch("time", &time));
+    }
+
+    for (int j = 0; j < nTotPoints; ++j) {
+      for (int i = 0; i < nBins; ++i) {
+        int idx = idc.getIndexPhi(i, j);
+        if (type == 1) {
+          idx = idc.getIndexTgl(i, j);
+        } else if (type == 2) {
+          idx = idc.getIndexqPt(i, j);
+        } else if (type == 3) {
+          idx = idc.getIndexInt(j);
+        }
+        dcar_A_Median[i][j] = idc.mDCAr_A_Median[idx];
+        dcar_C_Median[i][j] = idc.mDCAr_C_Median[idx];
+        dcar_A_WeightedMean[i][j] = idc.mDCAr_A_WeightedMean[idx];
+        dcar_C_WeightedMean[i][j] = idc.mDCAr_C_WeightedMean[idx];
+        dcar_A_RMS[i][j] = idc.mDCAr_A_RMS[idx];
+        dcar_C_RMS[i][j] = idc.mDCAr_C_RMS[idx];
+        dcaz_A_Median[i][j] = idc.mDCAz_A_Median[idx];
+        dcaz_C_Median[i][j] = idc.mDCAz_C_Median[idx];
+        dcaz_A_WeightedMean[i][j] = idc.mDCAz_A_WeightedMean[idx];
+        dcaz_C_WeightedMean[i][j] = idc.mDCAz_C_WeightedMean[idx];
+        dcaz_A_RMS[i][j] = idc.mDCAz_A_RMS[idx];
+        dcaz_C_RMS[i][j] = idc.mDCAz_C_RMS[idx];
+        nTracksDCAr_A[i][j] = idc.mDCAr_A_NTracks[idx];
+        nTracksDCAr_C[i][j] = idc.mDCAr_C_NTracks[idx];
+        nTracksDCAz_A[i][j] = idc.mDCAz_A_NTracks[idx];
+        nTracksDCAz_C[i][j] = idc.mDCAz_C_NTracks[idx];
+        mMIPdEdxRatioA[i][j] = idc.mMIPdEdxRatioA[idx];
+        mMIPdEdxRatioC[i][j] = idc.mMIPdEdxRatioC[idx];
+        mTPCChi2A[i][j] = idc.mTPCChi2A[idx];
+        mTPCChi2C[i][j] = idc.mTPCChi2C[idx];
+        mTPCNClA[i][j] = idc.mTPCNClA[idx];
+        mTPCNClC[i][j] = idc.mTPCNClC[idx];
+      }
+      // add time only once
+      if (type == 0) {
+        time[j] = idc.mTimeMS + j * nHBFPerTF * o2::constants::lhc::LHCOrbitMUS / 1000;
+      }
+    }
+    // fill branches
+    for (auto br : listBranches) {
+      br->Fill();
+    }
+  }
+  tree.SetEntries(1);
+}
+
 } // end namespace o2
