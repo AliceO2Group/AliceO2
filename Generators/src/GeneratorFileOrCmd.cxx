fba7f8c01aa8a36e6a190a5fa6dbbbb671fdce36
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2023-10-09 11:57:22 +0200

    Update Generators/src/GeneratorFileOrCmd.cxx

--- Generators/src/GeneratorFileOrCmd.cxx
+++ Generators/src/GeneratorFileOrCmd.cxx
@@ -9,7 +9,6 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-/// \author R+Preghenella - August 2017
 
 #include "SimulationDataFormat/MCUtils.h"
 #include "Generators/GeneratorFileOrCmd.h"
