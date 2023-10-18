ef711e598050dc6f80515a8a2feb994f38968ca4
Fabio Catalano <fabio.catalano@cern.ch>
2023-10-18 16:05:57 +0200

    Fix typo in subGenerator check

--- Generators/src/Generator.cxx
+++ Generators/src/Generator.cxx
@@ -222,7 +222,7 @@ Bool_t
 
 void Generator::addSubGenerator(int subGeneratorId, std::string const& subGeneratorDescription)
 {
-  if (mSubGeneratorId < 0) {
+  if (subGeneratorId < 0) {
     LOG(fatal) << "Sub-generator IDs must be >= 0, instead, passed value is " << subGeneratorId;
   }
   mSubGeneratorsIdToDesc.insert({subGeneratorId, subGeneratorDescription});
