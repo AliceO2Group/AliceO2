d905b8c81290b0a676a37b831d98e8d6cb0f320d
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2023-10-27 15:06:35 +0200

    Ignore checking for FairMQ version (#12156)
    * Version is way too obsolete in any case.
    * Anything which is not the version in alidist is unsupported in any case.
    * It breaks whenever git is not able to determine a valid version.

--- dependencies/O2Dependencies.cmake
+++ dependencies/O2Dependencies.cmake
@@ -88,7 +88,7 @@ set_package_properties(Microsoft.GSL
                        TYPE REQUIRED
                        PURPOSE "Mainly for its span")
 
-find_package(FairMQ 1.4.41 CONFIG)
+find_package(FairMQ CONFIG)
 set_package_properties(FairMQ PROPERTIES TYPE REQUIRED)
 
 # find_package(protobuf CONFIG)
