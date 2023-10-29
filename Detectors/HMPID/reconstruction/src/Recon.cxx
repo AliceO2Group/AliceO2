c3dd00ad2eb43bc5ea8c55105549b6519ee8371c
Ruben Shahoyan <shahor02@users.noreply.github.com>
2023-10-29 21:36:37 +0100

    fix braces

--- Detectors/HMPID/reconstruction/src/Recon.cxx
+++ Detectors/HMPID/reconstruction/src/Recon.cxx
@@ -87,8 +87,9 @@ void Recon::ckovAngle(o2::dataformats::MatchInfoHMP* match, const std::vector<o2
     }
 
     chId = cluster.ch();
-    if (cluster.q() > 2 * fParam->qCut() || cluster.size() > 4)
+    if (cluster.q() > 2 * fParam->qCut() || cluster.size() > 4) {
       continue;
+    }
     double thetaCer, phiCer;
     if (findPhotCkov(cluster.x(), cluster.y(), thetaCer, phiCer)) { // find ckov angle for this  photon candidate
       fPhotCkov[fPhotCnt] = thetaCer;                               // actual theta Cerenkov (in TRS)
