1898143f56df236733de0000de83d6789ee8b113
shahoian <ruben.shahoyan@cern.ch>
2023-11-14 18:19:48 +0100

    Optionally remap PIDforTracking assingments
    e.g.
    PIDREMAP="GPU_rec_tpc.PID_remap[0]=2;GPU_rec_tpc.PID_remap[2]=-1;GPU_rec_tpc.PID_remap[3]=2;GPU_rec_tpc.PID_remap[5]=4;GPU_rec_tpc.PID_remap[6]=4;"
    will convert PID response for
    electron (0) to pion (2)
    kaon (3) to pion
    deuteron (5) to proton (4)
    Triron (6) to proton (4)
    and leave the rest unchanged. Negative value (like GPU_rec_tpc.PID_remap[2]=-1) leave PID unchanged.

--- GPU/GPUTracking/Definitions/GPUSettingsList.h
+++ GPU/GPUTracking/Definitions/GPUSettingsList.h
@@ -132,6 +132,7 @@ AddOptionRTC(mergerReadFromTrackerDirectly, char, 1, "", 0, "Forward data direct
 AddOptionRTC(dropSecondaryLegsInOutput, char, 1, "", 0, "Do not store secondary legs of looping track in TrackTPC")
 AddOptionRTC(enablePID, char, 1, "", 0, "Enable PID response")
 AddOptionRTC(PID_useNsigma, char, 1, "", 0, "Use nSigma instead of absolute distance in PID response")
+AddOptionArrayRTC(PID_remap, char, 9, (0, 1, 2, 3, 4, 5, 6, 7, 8), "", 0, "Remap Ipid to PID_reamp[Ipid] (no remap if<0)")
 AddHelp("help", 'h')
 EndConfig()
 
