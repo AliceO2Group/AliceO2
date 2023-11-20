0492dd1a1db53c329a0560a9108b2446e4ef1282
Vít Kučera <vit.kucera@cern.ch>
2023-11-20 18:50:56 +0100

    Replace values with aliases

--- Common/Constants/include/CommonConstants/PhysicsConstants.h
+++ Common/Constants/include/CommonConstants/PhysicsConstants.h
@@ -21,20 +21,15 @@
 namespace o2::constants::physics
 {
 // particles masses
-constexpr float MassPhoton = 0.0;
-constexpr float MassMuon = 0.105658;
-constexpr float MassPionCharged = 0.139570;
-constexpr float MassPionNeutral = 0.134976;
-constexpr float MassKaonCharged = 0.493677;
-constexpr float MassKaonNeutral = 0.497648;
-constexpr float MassLambda = 1.115683;
-constexpr float MassDeuteron = 1.8756129;
-constexpr float MassTriton = 2.8089211;
-constexpr float MassHelium3 = 2.8083916;
-constexpr float MassAlpha = 3.7273794;
-constexpr float MassHyperTriton = 2.992;
-constexpr float MassHyperhydrog4 = 3.931;
-constexpr float MassHyperhelium4 = 3.9218;
+constexpr float MassPhoton = MassGamma;
+constexpr float MassMuon = MassMuonMinus;
+constexpr float MassPionCharged = MassPiPlus;
+constexpr float MassPionNeutral = MassPi0;
+constexpr float MassKaonCharged = MassKPlus;
+constexpr float MassKaonNeutral = MassK0;
+constexpr float MassLambda = MassLambda0;
+constexpr float MassHyperhydrog4 = MassHyperHydrogen4;
+constexpr float MassHyperhelium4 = MassHyperHelium4;
 
 constexpr float LightSpeedCm2S = 299792458.e2;           // C in cm/s
 constexpr float LightSpeedCm2NS = LightSpeedCm2S * 1e-9; // C in cm/ns
