0e8b66643d9797a938ca9633d71844423195a8bb
Vít Kučera <vit.kucera@cern.ch>
2023-11-20 18:03:21 +0100

    Remove conflicting constants.

--- Common/Constants/include/CommonConstants/PhysicsConstants.h
+++ Common/Constants/include/CommonConstants/PhysicsConstants.h
@@ -22,13 +22,11 @@ namespace o2::constants::physics
 {
 // particles masses
 constexpr float MassPhoton = 0.0;
-constexpr float MassElectron = 0.000511;
 constexpr float MassMuon = 0.105658;
 constexpr float MassPionCharged = 0.139570;
 constexpr float MassPionNeutral = 0.134976;
 constexpr float MassKaonCharged = 0.493677;
 constexpr float MassKaonNeutral = 0.497648;
-constexpr float MassProton = 0.938272;
 constexpr float MassLambda = 1.115683;
 constexpr float MassDeuteron = 1.8756129;
 constexpr float MassTriton = 2.8089211;
@@ -37,8 +35,6 @@ constexpr float MassAlpha = 3.7273794;
 constexpr float MassHyperTriton = 2.992;
 constexpr float MassHyperhydrog4 = 3.931;
 constexpr float MassHyperhelium4 = 3.9218;
-constexpr float MassXiMinus = 1.32171;
-constexpr float MassOmegaMinus = 1.67245;
 
 constexpr float LightSpeedCm2S = 299792458.e2;           // C in cm/s
 constexpr float LightSpeedCm2NS = LightSpeedCm2S * 1e-9; // C in cm/ns
