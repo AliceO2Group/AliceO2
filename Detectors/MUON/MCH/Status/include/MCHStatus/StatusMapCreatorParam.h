b6d3618892d8376c00271b5cb2a27f189eb8b28c
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2023-12-03 18:22:53 +0100

    [MRRTF-220] Use statusmap (badchannels+rejectlist) by default (#12373)

--- Detectors/MUON/MCH/Status/include/MCHStatus/StatusMapCreatorParam.h
+++ Detectors/MUON/MCH/Status/include/MCHStatus/StatusMapCreatorParam.h
@@ -24,8 +24,8 @@ namespace o2::mch
  */
 struct StatusMapCreatorParam : public o2::conf::ConfigurableParamHelper<StatusMapCreatorParam> {
 
-  bool useBadChannels = false; ///< reject bad channels (obtained during pedestal calibration runs)
-  bool useRejectList = false;  ///< use extra (relative to bad channels above) rejection list
+  bool useBadChannels = true; ///< reject bad channels (obtained during pedestal calibration runs)
+  bool useRejectList = true;  ///< use extra (relative to bad channels above) rejection list
 
   bool isActive() const { return useBadChannels || useRejectList; }
 
