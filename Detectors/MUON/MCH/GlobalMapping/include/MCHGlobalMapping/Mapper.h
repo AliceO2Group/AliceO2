47ff7ec8b58449298dabba7d049a2ed65aa08f82
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2023-12-08 11:05:24 +0100

    MCH: add some getDsIndices convenience functions (#12406)

--- Detectors/MUON/MCH/GlobalMapping/include/MCHGlobalMapping/Mapper.h
+++ Detectors/MUON/MCH/GlobalMapping/include/MCHGlobalMapping/Mapper.h
@@ -12,6 +12,9 @@
 #ifndef O2_MCH_GLOBAL_MAPPING_MAPPER_H_
 #define O2_MCH_GLOBAL_MAPPING_MAPPER_H_
 
+#include "MCHConditions/Cathode.h"
+#include "MCHConditions/Chamber.h"
+#include "MCHConditions/Plane.h"
 #include "MCHGlobalMapping/DsIndex.h"
 #include <set>
 #include <string>
@@ -20,6 +23,17 @@ namespace o2::mch::dcs
 {
 /** get the list of dual sampa indices corresponding to a given DCS Alias */
 std::set<int> aliasToDsIndices(std::string_view alias);
+
+/** get indices of all dual sampas of a set of cathodes (={deId,plane}).
+ * returned set might be empty if input Cathodes are not valid ones. */
+std::set<DsIndex> getDsIndices(const std::set<dcs::Cathode>& cathodes);
+
+/** get indices of all dual sampas of given plane of a chamber */
+std::set<DsIndex> getDsIndices(dcs::Chamber ch, dcs::Plane plane);
+
+/** get indices of all dual sampas of a given set of solars */
+std::set<DsIndex> getDsIndices(const std::set<int>& solarIds);
+
 } // namespace o2::mch::dcs
 
 #endif
