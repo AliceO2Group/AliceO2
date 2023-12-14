d51aced2e50e6b91002dae4482fc5efc9580386e
nicolovalle <35177278+nicolovalle@users.noreply.github.com>
2023-12-14 10:10:18 +0100

    ITS and MFT dead damp builder workflow (#12459)
    * ITS and MFT dead damp builder workflow
    
    * removing trailing spaces
    
    * removing unused struct
    
    * fixing format
    
    * optimization
    
    ---------
    
    Co-authored-by: Nicolo Valle <nicolo.valle@cern.ch>

---
+++ Detectors/ITSMFT/common/workflow/include/ITSMFTWorkflow/DeadMapBuilderSpec.h
@@ -0,0 +1,129 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// @file   DeadMapBuilderSpec.h
+
+#ifndef O2_ITSMFT_DEADMAP_BUILDER_
+#define O2_ITSMFT_DEADMAP_BUILDER_
+
+#include <sys/stat.h>
+#include <filesystem>
+#include <string>
+#include <vector>
+#include <array>
+#include <set>
+#include <deque>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+// Boost library for easy access of host name
+#include <boost/asio/ip/host_name.hpp>
+
+#include "Framework/CCDBParamSpec.h"
+#include "Framework/DataProcessorSpec.h"
+#include "Framework/Task.h"
+#include "Framework/ControlService.h"
+#include "Framework/ConfigParamRegistry.h"
+#include "Framework/RawDeviceService.h"
+#include "Framework/WorkflowSpec.h"
+#include "Framework/Task.h"
+#include "Framework/DataTakingContext.h"
+#include "Framework/TimingInfo.h"
+#include <fairmq/Device.h>
+
+#include <ITSMFTReconstruction/RawPixelDecoder.h> //o2::itsmft::RawPixelDecoder
+#include "DetectorsCalibration/Utils.h"
+#include "DetectorsCommonDataFormats/FileMetaData.h"
+#include "DetectorsBase/GRPGeomHelper.h" //nicolo
+#include "CCDB/CcdbApi.h"
+#include "CommonUtils/MemFileHelper.h"
+
+// ROOT includes
+#include "TTree.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TF1.h"
+#include "TFile.h"
+
+using namespace o2::framework;
+using namespace o2::itsmft;
+
+namespace o2
+{
+namespace itsmft
+{
+
+class ITSMFTDeadMapBuilder : public Task
+{
+ public:
+  ITSMFTDeadMapBuilder(std::string datasource, bool doMFT);
+  ~ITSMFTDeadMapBuilder() override;
+
+  void init(InitContext& ic) final;
+  void run(ProcessingContext& pc) final;
+  void endOfStream(EndOfStreamContext& ec) final;
+
+  void stop() final;
+
+  //////////////////////////////////////////////////////////////////
+ private:
+  std::string mSelfName;
+
+  bool mRunMFT = false;
+  bool mDoLocalOutput = false;
+  uint16_t N_CHIPS;
+  uint16_t N_CHIPS_ITSIB = o2::itsmft::ChipMappingITS::getNChips(0);
+  int mTFLength = 32; // TODO find utility for proper value -- o2::base::GRPGeomHelper::getNHBFPerTF() returns 128 see https://github.com/AliceO2Group/AliceO2/blob/051b56f9f136e7977e83f5d26d922db9bd6ecef5/Detectors/Base/src/GRPGeomHelper.cxx#L233 and correct also default option is getSpec
+
+  uint mStepCounter = 0;
+  uint mTFCounter = 0;
+
+  std::string mObjectName;
+  std::string mLocalOutputDir;
+
+  std::string MAP_VERSION = "1"; // to change in case the encoding or the format change
+
+  std::vector<uint16_t>* mDeadMapTF = nullptr;
+
+  Long64_t mFirstOrbitTF = 0x0;
+
+  std::string mDataSource = "chipsstatus";
+
+  int mTFSampling = 1000;
+
+  TTree* mTreeObject = nullptr;
+
+  void finalizeOutput();
+  void PrepareOutputCcdb(DataAllocator& output);
+
+  // Utils
+
+  uint16_t getElementIDFromChip(uint16_t);
+
+  o2::framework::DataTakingContext mDataTakingContext{};
+  o2::framework::TimingInfo mTimingInfo{};
+
+  // Flag to avoid that endOfStream and stop are both done
+  bool isEnded = false;
+
+  // Run stop requested flag for EoS operations
+  bool mRunStopRequested = false;
+};
+
+// Create a processor spec
+o2::framework::DataProcessorSpec getITSMFTDeadMapBuilderSpec(std::string datasource, bool doMFT);
+
+} // namespace itsmft
+} // namespace o2
+
+#endif
---
+++ Detectors/ITSMFT/common/workflow/src/DeadMapBuilderSpec.cxx
@@ -0,0 +1,309 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// @file   DeadMapBuilderSpec.cxx
+
+#include "ITSMFTWorkflow/DeadMapBuilderSpec.h"
+#include "CommonUtils/FileSystemUtils.h"
+#include "CCDB/BasicCCDBManager.h"
+#include "DataFormatsITSMFT/Digit.h"
+#include "DataFormatsITSMFT/CompCluster.h"
+
+namespace o2
+{
+namespace itsmft
+{
+
+//////////////////////////////////////////////////////////////////////////////
+// Default constructor
+ITSMFTDeadMapBuilder::ITSMFTDeadMapBuilder(std::string datasource, bool doMFT)
+  : mDataSource(datasource), mRunMFT(doMFT)
+{
+  std::string detector = doMFT ? "MFT" : "ITS";
+  mSelfName = o2::utils::Str::concat_string("ITSMFTDeadMapBuilder_", detector);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// Default deconstructor
+ITSMFTDeadMapBuilder::~ITSMFTDeadMapBuilder()
+{
+  // Clear dynamic memory
+  delete mDeadMapTF;
+  delete mTreeObject;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+void ITSMFTDeadMapBuilder::init(InitContext& ic)
+{
+
+  LOG(info) << "ITSMFTDeadMapBuilder init... " << mSelfName;
+
+  if (mRunMFT) {
+    N_CHIPS = o2::itsmft::ChipMappingMFT::getNChips();
+  } else {
+    N_CHIPS = o2::itsmft::ChipMappingITS::getNChips();
+  }
+
+  mDeadMapTF = new std::vector<uint16_t>{};
+
+  mTreeObject = new TTree("ccdb_object", "ccdb_object");
+  mTreeObject->Branch("orbit", &mFirstOrbitTF);
+  mTreeObject->Branch("deadmap", &mDeadMapTF);
+
+  mTFSampling = ic.options().get<int>("tf-sampling");
+  mTFLength = ic.options().get<int>("tf-length");
+  mDoLocalOutput = ic.options().get<bool>("local-output");
+  mObjectName = ic.options().get<std::string>("output-filename");
+  mLocalOutputDir = ic.options().get<std::string>("output-dir");
+
+  LOG(info) << "Sampling one TF every " << mTFSampling;
+
+  return;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+uint16_t ITSMFTDeadMapBuilder::getElementIDFromChip(uint16_t chip)
+{
+  // MFT - no grouping
+  if (mRunMFT) {
+    return (uint16_t)chip;
+  }
+  // ITS - group chips into lanes
+  // TODO: does o2::itsmft::ChipMappingITS already contain this?
+  else {
+    if (chip < N_CHIPS_ITSIB) {
+      return (uint16_t)chip;
+    } else {
+      return N_CHIPS_ITSIB + (uint16_t)((chip - N_CHIPS_ITSIB) / 7);
+    }
+  }
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+void ITSMFTDeadMapBuilder::finalizeOutput()
+{
+
+  if (mDoLocalOutput) {
+    std::string localoutfilename = mLocalOutputDir + "/" + mObjectName;
+    TFile outfile(localoutfilename.c_str(), "RECREATE");
+    outfile.cd();
+    mTreeObject->Write();
+    outfile.Close();
+  }
+  return;
+
+} // finalizeOutput
+
+//////////////////////////////////////////////////////////////////////////////
+// Main running function
+void ITSMFTDeadMapBuilder::run(ProcessingContext& pc)
+{
+  if (mRunStopRequested) { // give up when run stop request arrived
+    return;
+  }
+
+  std::chrono::time_point<std::chrono::high_resolution_clock> start;
+  std::chrono::time_point<std::chrono::high_resolution_clock> end;
+
+  start = std::chrono::high_resolution_clock::now();
+
+  mTFCounter++;
+
+  mFirstOrbitTF = pc.services().get<o2::framework::TimingInfo>().firstTForbit;
+
+  if ((Long64_t)(mFirstOrbitTF / mTFLength) % mTFSampling != 0) {
+    return;
+  }
+
+  mStepCounter++;
+  LOG(info) << "Processing step #" << mStepCounter << " out of " << mTFCounter << " TF received. First orbit " << mFirstOrbitTF;
+
+  mDeadMapTF->clear();
+
+  std::vector<bool> ElementsStatus(getElementIDFromChip(N_CHIPS), false);
+
+  if (mDataSource == "digits") {
+    const auto elements = pc.inputs().get<gsl::span<o2::itsmft::Digit>>("elements");
+    const auto ROFs = pc.inputs().get<gsl::span<o2::itsmft::ROFRecord>>("ROFs");
+    for (const auto& rof : ROFs) {
+      auto elementsInTF = rof.getROFData(elements);
+      for (const auto& el : elementsInTF) {
+        uint16_t chipID = (uint16_t)el.getChipIndex();
+        ElementsStatus.at(getElementIDFromChip(chipID)) = true;
+      }
+    }
+  } else if (mDataSource == "clusters") {
+    const auto elements = pc.inputs().get<gsl::span<o2::itsmft::CompClusterExt>>("elements");
+    const auto ROFs = pc.inputs().get<gsl::span<o2::itsmft::ROFRecord>>("ROFs");
+    for (const auto& rof : ROFs) {
+      auto elementsInTF = rof.getROFData(elements);
+      for (const auto& el : elementsInTF) {
+        uint16_t chipID = (uint16_t)el.getSensorID();
+        ElementsStatus.at(getElementIDFromChip(chipID)) = true;
+      }
+    }
+  } else if (mDataSource == "chipsstatus") {
+    const auto elements = pc.inputs().get<gsl::span<char>>("elements");
+    for (uint16_t chipID = 0; chipID < elements.size(); chipID++) {
+      if (elements[chipID]) {
+        ElementsStatus.at(getElementIDFromChip(chipID)) = true;
+      }
+    }
+  }
+
+  int CountDead = 0;
+
+  for (uint16_t el = 0; el < ElementsStatus.size(); el++) {
+    if (ElementsStatus.at(el)) {
+      continue;
+    }
+    CountDead++;
+    bool previous_dead = (el > 0 && !ElementsStatus.at(el - 1));
+    bool next_dead = (el < ElementsStatus.size() - 1 && !ElementsStatus.at(el + 1));
+    if (!previous_dead && next_dead) {
+      mDeadMapTF->push_back(el | (uint16_t)(0x8000));
+    } else if (previous_dead && next_dead) {
+      continue;
+    } else {
+      mDeadMapTF->push_back(el);
+    }
+  }
+
+  LOG(info) << "TF contains " << CountDead << " dead elements, saved into " << mDeadMapTF->size() << " words.";
+
+  // filling the tree
+  mTreeObject->Fill();
+
+  end = std::chrono::high_resolution_clock::now();
+  int difference = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+
+  LOG(info) << "Elapsed time in TF processing: " << difference / 1000. << " ms";
+
+  return;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+void ITSMFTDeadMapBuilder::PrepareOutputCcdb(DataAllocator& output)
+{
+
+  long tstart = o2::ccdb::getCurrentTimestamp();
+  long secinyear = 365L * 24 * 3600;
+  long tend = o2::ccdb::getFutureTimestamp(secinyear);
+
+  std::map<std::string, std::string> md = {
+    {"map_version", MAP_VERSION}};
+
+  std::string path = mRunMFT ? "MFT/Calib" : "ITS/Calib/";
+  std::string name_str = "time_dead_map";
+
+  o2::ccdb::CcdbObjectInfo info((path + name_str), "time_dead_map", mObjectName, md, tstart, tend);
+
+  auto image = o2::ccdb::CcdbApi::createObjectImage(mTreeObject, &info);
+
+  info.setAdjustableEOV();
+
+  LOG(info) << "Sending object " << info.getPath() << "/" << info.getFileName()
+            << " of size " << image->size() << "bytes, valid for "
+            << info.getStartValidityTimestamp() << " : " << info.getEndValidityTimestamp();
+
+  if (mRunMFT) {
+    output.snapshot(Output{o2::calibration::Utils::gDataOriginCDBPayload, "MFT_TimeDeadMap", 0}, *image.get());
+    output.snapshot(Output{o2::calibration::Utils::gDataOriginCDBWrapper, "MFT_TimeDeadMap", 0}, info);
+  } else {
+    output.snapshot(Output{o2::calibration::Utils::gDataOriginCDBPayload, "ITS_TimeDeadMap", 0}, *image.get());
+    output.snapshot(Output{o2::calibration::Utils::gDataOriginCDBWrapper, "ITS_TimeDeadMap", 0}, info);
+  }
+
+  return;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// O2 functionality allowing to do post-processing when the upstream device
+// tells that there will be no more input data
+void ITSMFTDeadMapBuilder::endOfStream(EndOfStreamContext& ec)
+{
+  if (!isEnded && !mRunStopRequested) {
+    LOG(info) << "endOfStream report:" << mSelfName;
+    finalizeOutput();
+    PrepareOutputCcdb(ec.outputs());
+    isEnded = true;
+  }
+  return;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// DDS stop method: simply close the latest tree
+void ITSMFTDeadMapBuilder::stop()
+{
+  if (!isEnded) {
+    LOG(info) << "stop() report:" << mSelfName;
+    finalizeOutput();
+    if (mDoLocalOutput) {
+      LOG(info) << "stop() not sending object as output. ccdb will not be populated.";
+    } else {
+      LOG(error) << "stop() not sending object as output. ccdb will not be populated.";
+    }
+    isEnded = true;
+  }
+  return;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+DataProcessorSpec getITSMFTDeadMapBuilderSpec(std::string datasource, bool doMFT)
+{
+  o2::header::DataOrigin detOrig;
+  if (doMFT) {
+    detOrig = o2::header::gDataOriginMFT;
+  } else {
+    detOrig = o2::header::gDataOriginITS;
+  }
+
+  std::vector<InputSpec> inputs;
+
+  if (datasource == "digits") {
+    inputs.emplace_back("elements", detOrig, "DIGITS", 0, Lifetime::Timeframe);
+    inputs.emplace_back("ROFs", detOrig, "DIGITSROF", 0, Lifetime::Timeframe);
+  } else if (datasource == "clusters") {
+    inputs.emplace_back("elements", detOrig, "COMPCLUSTERS", 0, Lifetime::Timeframe);
+    inputs.emplace_back("ROFs", detOrig, "CLUSTERSROF", 0, Lifetime::Timeframe);
+  } else if (datasource == "chipsstatus") {
+    inputs.emplace_back("elements", detOrig, "CHIPSSTATUS", 0, Lifetime::Timeframe);
+  } else {
+    return DataProcessorSpec{0x0}; // TODO: ADD PROTECTION
+  }
+
+  std::vector<OutputSpec> outputs;
+  if (doMFT) {
+    outputs.emplace_back(ConcreteDataTypeMatcher{o2::calibration::Utils::gDataOriginCDBPayload, "MFT_TimeDeadMap"});
+    outputs.emplace_back(ConcreteDataTypeMatcher{o2::calibration::Utils::gDataOriginCDBWrapper, "MFT_TimeDeadMap"});
+  } else {
+    outputs.emplace_back(ConcreteDataTypeMatcher{o2::calibration::Utils::gDataOriginCDBPayload, "ITS_TimeDeadMap"});
+    outputs.emplace_back(ConcreteDataTypeMatcher{o2::calibration::Utils::gDataOriginCDBWrapper, "ITS_TimeDeadMap"});
+  }
+
+  std::string detector = doMFT ? "mft" : "its";
+  std::string objectname_default = detector + "_time_deadmap.root";
+
+  return DataProcessorSpec{
+    "itsmft-deadmap-builder_" + detector,
+    inputs,
+    outputs,
+    AlgorithmSpec{adaptFromTask<ITSMFTDeadMapBuilder>(datasource, doMFT)},
+    Options{{"tf-sampling", VariantType::Int, 1000, {"Process every Nth TF. Selection according to first TF orbit."}},
+            {"tf-length", VariantType::Int, 32, {"Orbits per TF."}},
+            {"output-filename", VariantType::String, objectname_default, {"ROOT object file name."}},
+            {"local-output", VariantType::Bool, false, {"Save ROOT tree file locally."}},
+            {"output-dir", VariantType::String, "./", {"ROOT tree local output directory."}}}};
+}
+
+} // namespace itsmft
+} // namespace o2
