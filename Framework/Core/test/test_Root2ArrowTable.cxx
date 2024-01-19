cd2008fc3641841895b9bd6a8eb21c095d558cfb
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2024-01-19 08:41:04 +0100

    DPL: drop commented code

--- Framework/Core/test/test_Root2ArrowTable.cxx
+++ Framework/Core/test/test_Root2ArrowTable.cxx
@@ -47,7 +47,6 @@ TEST_CASE("RootTree2Table")
   t1.Branch("ij", ij, "ij[2]/I");
   //fill the tree
   for (Int_t i = 0; i < 1000; i++) {
-    //gRandom->Rannor(xyz[0], xyz[1]);
     xyz[0] = 1;
     xyz[1] = 2;
     xyz[2] = 3;
@@ -92,16 +91,12 @@ TEST_CASE("RootTree2Table")
     REQUIRE(array->length() == 3000);
     const float* c = reinterpret_cast<float const*>(array->values()->data());
 
-    //auto array = std::static_pointer_cast<arrow::FixedSizeBinaryArray>(table->column(0)->chunk(0));
-    //CHECK_EQUAL(array->byte_width(), sizeof(float[3]));
-    //const float* c = reinterpret_cast<float const*>(array->Value(0));
 
     CHECK(c[0] == 1);
     CHECK(c[1] == 2);
     CHECK(c[2] == 1);
   }
   {
-    //auto values = std::static_pointer_cast<arrow::FixedSizeBinaryArray>(table->column(1)->chunk(0));
     auto chunkToUse = table->column(1)->chunk(0);
     chunkToUse = std::dynamic_pointer_cast<arrow::FixedSizeListArray>(chunkToUse)->values();
     auto array = std::static_pointer_cast<arrow::Int32Array>(chunkToUse);
