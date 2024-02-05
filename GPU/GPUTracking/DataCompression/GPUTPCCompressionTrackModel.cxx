10b3d0e3f1ab4d50050b30eb5b9380f957edbf7a
shahoian <ruben.shahoyan@cern.ch>
2024-02-05 03:00:59 +0100

    Adjust TrackParComp ELoss contribution to sigma(q/pt)^2

--- GPU/GPUTracking/DataCompression/GPUTPCCompressionTrackModel.cxx
+++ GPU/GPUTracking/DataCompression/GPUTPCCompressionTrackModel.cxx
@@ -840,7 +840,7 @@ GPUd() void GPUTPCCompressionTrackModel::calculateMaterialCorrection()
 
   // Approximate energy loss fluctuation (M.Ivanov)
 
-  const float knst = 0.07f; // To be tuned.
+  const float knst = 0.0007f; // To be tuned.
   mMaterial.sigmadE2 = knst * mMaterial.EP2 * qpt;
   mMaterial.sigmadE2 = mMaterial.sigmadE2 * mMaterial.sigmadE2;
 
