0fccb3561539740c5a2d40f3ef1cef29e892f9ea
David Rohr <drohr@jwdt.org>
2024-02-21 01:05:02 +0100

    GPU TPC: Fix accessing dead channel map with invalid pad outside of active volume

--- GPU/GPUTracking/SliceTracker/GPUTPCTrackletConstructor.cxx
+++ GPU/GPUTracking/SliceTracker/GPUTPCTrackletConstructor.cxx
@@ -383,8 +383,8 @@ GPUdic(2, 1) void GPUTPCTrackletConstructor::UpdateTracklet(int /*nBlocks*/, int
       } while (false);
 #if !defined(__OPENCL__) || defined(__OPENCLCPP__)
       if (!found && tracker.GetConstantMem()->calibObjects.dEdxCalibContainer) {
-        unsigned int pad = tracker.Param().tpcGeometry.LinearY2Pad(tracker.ISlice(), iRow, yUncorrected) + 0.5f;
-        if (tracker.GetConstantMem()->calibObjects.dEdxCalibContainer->isDead(tracker.ISlice(), iRow, pad)) {
+        int pad = tracker.Param().tpcGeometry.LinearY2Pad(tracker.ISlice(), iRow, yUncorrected) + 0.5f;
+        if ((unsigned int)pad < tracker.Param().tpcGeometry.NPads(iRow) && tracker.GetConstantMem()->calibObjects.dEdxCalibContainer->isDead(tracker.ISlice(), iRow, pad)) {
           r.mNMissed--;
           rowHit = CALINK_DEAD_CHANNEL;
         }
