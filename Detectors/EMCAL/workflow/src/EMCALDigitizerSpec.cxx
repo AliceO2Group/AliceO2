b86a39df911cb7e4f9ec237777ed2132ce640603
siragoni <simone.ragoni@cern.ch>
2024-02-28 18:00:13 +0100

    [EMCAL-539,EMCAL-696,EMCAL-697] Added DigitizerTRU initialization

--- Detectors/EMCAL/workflow/src/EMCALDigitizerSpec.cxx
+++ Detectors/EMCAL/workflow/src/EMCALDigitizerSpec.cxx
@@ -404,6 +404,7 @@ void DigitizerSpec::run(framework::ProcessingContext& ctx)
 void DigitizerSpec::configure()
 {
   mDigitizer.init();
+  mDigitizerTRU.init();
 }
 
 void DigitizerSpec::finaliseCCDB(o2::framework::ConcreteDataMatcher& matcher, void* obj)
