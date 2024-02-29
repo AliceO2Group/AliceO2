0931c95100e545e05b57380ed6aeff4dd9464d36
David Rohr <drohr@jwdt.org>
2024-02-29 13:53:33 +0100

    GPU: Disable thrust for RTC but not for per-kernel compilation

--- GPU/Common/GPUCommonAlgorithm.h
+++ GPU/Common/GPUCommonAlgorithm.h
@@ -221,7 +221,7 @@ typedef GPUCommonAlgorithm CAAlgo;
 } // namespace gpu
 } // namespace GPUCA_NAMESPACE
 
-#if (((defined(__CUDACC__) && !defined(__clang__)) || defined(__HIPCC__))) && !defined(GPUCA_GPUCODE_COMPILEKERNELS) && !defined(GPUCA_GPUCODE_HOSTONLY)
+#if (((defined(__CUDACC__) && !defined(__clang__)) || defined(__HIPCC__))) && !defined(GPUCA_GPUCODE_GENRTC) && !defined(GPUCA_GPUCODE_HOSTONLY)
 
 #include "GPUCommonAlgorithmThrust.h"
 
