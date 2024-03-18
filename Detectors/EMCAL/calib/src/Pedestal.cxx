c98ea7683f58dc91e3e2d784b6d621fbbce802d6
Markus Fasel <markus.fasel@cern.ch>
2024-03-18 18:12:52 +0100

    [EMCAL-1112] CCDB object for EMCAL pedestal calibration
    - separate pedestals for HG and LG
    - pedestal as ADC counts (short)
    - histogram representation both vs. cell ID and
      position in EMCAL for both channel types
    - unit test coverage

---
+++ Detectors/EMCAL/calib/src/Pedestal.cxx
@@ -0,0 +1,106 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#include "EMCALBase/Geometry.h"
+#include "EMCALCalib/CalibContainerErrors.h"
+#include "EMCALCalib/Pedestal.h"
+
+#include <fairlogger/Logger.h>
+
+#include <TH1.h>
+#include <TH2.h>
+
+#include <iostream>
+
+#include <gsl/span>
+
+using namespace o2::emcal;
+
+void Pedestal::addPedestalValue(unsigned short cellID, short pedestal, bool isLowGain)
+{
+  if (cellID >= mPedestalValuesHG.size()) {
+    throw CalibContainerIndexException(cellID);
+  }
+  if (isLowGain) {
+    mPedestalValuesLG[cellID] = pedestal;
+  } else {
+    mPedestalValuesHG[cellID] = pedestal;
+  }
+}
+
+short Pedestal::getPedestalValue(unsigned short cellID, bool isLowGain) const
+{
+  if (cellID >= mPedestalValuesHG.size()) {
+    throw CalibContainerIndexException(cellID);
+  }
+  if (isLowGain) {
+    return mPedestalValuesLG[cellID];
+  } else {
+    return mPedestalValuesHG[cellID];
+  }
+}
+
+TH1* Pedestal::getHistogramRepresentation(bool isLowGain) const
+{
+  gsl::span<const short> data;
+  std::string histname, histtitle;
+  if (isLowGain) {
+    histname = "PedestalLG";
+    histtitle = "Pedestal values Params low Gain";
+    data = gsl::span<const short>(mPedestalValuesLG);
+  } else {
+    histname = "PedestalHG";
+    histtitle = "Pedestal values Params high Gain";
+    data = gsl::span<const short>(mPedestalValuesHG);
+  }
+
+  auto hist = new TH1S(histname.data(), histtitle.data(), data.size(), -0.5, data.size() - 0.5);
+  hist->SetDirectory(nullptr);
+  for (std::size_t icell{0}; icell < data.size(); ++icell) {
+    hist->SetBinContent(icell + 1, data[icell]);
+  }
+  return hist;
+}
+
+TH2* Pedestal::getHistogramRepresentation2D(bool isLowGain) const
+{
+  gsl::span<const short> data;
+  std::string histname, histtitle;
+  if (isLowGain) {
+    histname = "PedestalDistLG";
+    histtitle = "Pedestal values Params low Gain";
+    data = gsl::span<const short>(mPedestalValuesLG);
+  } else {
+    histname = "PedestalDistHG";
+    histtitle = "Pedestal values Params high Gain";
+    data = gsl::span<const short>(mPedestalValuesHG);
+  }
+
+  const int MAXROWS = 208,
+            MAXCOLS = 96;
+  auto hist = new TH2S(histname.data(), histtitle.data(), MAXCOLS, -0.5, double(MAXCOLS) - 0.5, MAXROWS, -0.5, double(MAXROWS) - 0.5);
+  hist->SetDirectory(nullptr);
+  try {
+    auto geo = Geometry::GetInstance();
+    for (size_t cellID = 0; cellID < data.size(); cellID++) {
+      auto position = geo->GlobalRowColFromIndex(cellID);
+      hist->Fill(std::get<1>(position), std::get<0>(position), data[cellID]);
+    }
+  } catch (o2::emcal::GeometryNotInitializedException& e) {
+    LOG(error) << "Geometry needs to be initialized";
+  }
+  return hist;
+}
+
+bool Pedestal::operator==(const Pedestal& other) const
+{
+  return mPedestalValuesHG == other.mPedestalValuesHG && mPedestalValuesLG == other.mPedestalValuesLG;
+}
