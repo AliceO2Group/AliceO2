45a0042027a0d53824ac6807815de0afc9a8806a
David Rohr <drohr@jwdt.org>
2024-03-21 14:34:31 +0100

    GPU Standalone: Add ninja to the alidist dependencies for the standalone build

--- GPU/GPUTracking/Standalone/cmake/prepare.sh
+++ GPU/GPUTracking/Standalone/cmake/prepare.sh
@@ -11,6 +11,6 @@ else
 fi
 eval "`alienv shell-helper`"
 alienv load O2/latest
-for i in Vc boost fmt CMake ms_gsl Clang; do
+for i in Vc boost fmt CMake ms_gsl Clang ninja; do
   source sw/$ALIARCH/$i/latest/etc/profile.d/init.sh
 done
