bf7f204f2f934d30a4a4c6def5a2c8a7c5d8f4ea
lietava <roman.lietava@cern.ch>
2024-03-21 09:52:50 +0100

    clang

--- Detectors/CTP/workflow/src/RawDecoderSpec.cxx
+++ Detectors/CTP/workflow/src/RawDecoderSpec.cxx
@@ -111,8 +111,7 @@ void RawDecoderSpec::run(framework::ProcessingContext& ctx)
   //
   std::vector<LumiInfo> lumiPointsHBF1;
   std::vector<InputSpec> filter{InputSpec{"filter", ConcreteDataTypeMatcher{"CTP", "RAWDATA"}, Lifetime::Timeframe}};
-  if(mMaxInputSize > 0)
-  {
+  if (mMaxInputSize > 0) {
     size_t payloadSize = 0;
     for (const auto& ref : o2::framework::InputRecordWalker(inputs, filter)) {
       const auto dh = o2::framework::DataRefUtils::getHeader<o2::header::DataHeader*>(ref);
