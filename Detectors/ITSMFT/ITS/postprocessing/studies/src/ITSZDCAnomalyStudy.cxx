550ceabef8d95e943071964c081a43d27075e899
Matteo Concas <matteo.concas@cern.ch>
2024-04-09 18:21:29 +0200

    Add loop on ZDC events

--- Detectors/ITSMFT/ITS/postprocessing/studies/src/ITSZDCAnomalyStudy.cxx
+++ Detectors/ITSMFT/ITS/postprocessing/studies/src/ITSZDCAnomalyStudy.cxx
@@ -14,6 +14,9 @@
 #include "DetectorsBase/GRPGeomHelper.h"
 #include "DataFormatsParameters/GRPObject.h"
 
+// ZDC
+#include "DataFormatsZDC/RecEventFlat.h"
+
 #include "Framework/Task.h"
 #include "Framework/Logger.h"
 
@@ -34,6 +37,9 @@ class ITSZDCAnomalyStudy : public Task
   void run(ProcessingContext&) final;
   void endOfStream(EndOfStreamContext&) final;
   void finaliseCCDB(ConcreteDataMatcher&, void*) final;
+
+  // Custom
+  void process(o2::globaltracking::RecoContainer& recoData);
 
  private:
   std::shared_ptr<o2::base::GRPGeomRequest> mGGCCDBRequest;
@@ -61,10 +67,39 @@ void ITSZDCAnomalyStudy::finaliseCCDB(ConcreteDataMatcher& matcher, void* obj)
   if (o2::base::GRPGeomHelper::instance().finaliseCCDB(matcher, obj)) {
     return;
   }
-//   if (matcher == ConcreteDataMatcher("ITS", "CLUSDICT", 0)) {
-//     setClusterDictionary((const o2::itsmft::TopologyDictionary*)obj);
-//     return;
-//   }
+  //   if (matcher == ConcreteDataMatcher("ITS", "CLUSDICT", 0)) {
+  //     setClusterDictionary((const o2::itsmft::TopologyDictionary*)obj);
+  //     return;
+  //   }
+}
+
+// Custom area
+void ITSZDCAnomalyStudy::process(o2::globaltracking::RecoContainer& recoData)
+{
+  o2::zdc::RecEventFlat ev;
+
+  LOGP(info, "Processing RecoContainer");
+  auto RecBC = recoData.getZDCBCRecData();
+  auto Energy = recoData.getZDCEnergy();
+  auto TDCData = recoData.getZDCTDCData();
+  auto Info2 = recoData.getZDCInfo();
+
+  ev.init(RecBC, Energy, TDCData, Info2);
+  while (ev.next()) {
+    // cout<<(int)(ev.getNTDC())<<endl;
+
+    int32_t itdc = 0;
+
+    int nhit = ev.NtdcV(itdc);
+
+    for (int32_t ipos = 0; ipos < nhit; ipos++) {
+      double mytdc = o2::zdc::FTDCVal * ev.TDCVal[itdc][ipos];
+      // ht[ich]->Fill(mytdc);
+      if (itdc == o2::zdc::TDCZNAC && mytdc > 5.7 && mytdc < 8.7) {
+        printf("ZNAC background %f hit on %u.%04u\n", mytdc, ev.ir.orbit, ev.ir.bc);
+      }
+    }
+  }
 }
 
 // getter
