88838a81d230cee1f6ad1a5038e9bca515d13bff
Piotr Konopka <piotr.jan.konopka@cern.ch>
2024-04-22 08:29:20 +0200

    Allow for smooth move to the new naming of QC flags (#13053)

--- DataFormats/QualityControl/test/testTimeRangeFlagCollection.cxx
+++ DataFormats/QualityControl/test/testTimeRangeFlagCollection.cxx
@@ -9,7 +9,7 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-#define BOOST_TEST_MODULE Test quality_control TimeRangeFlagCollection class
+#define BOOST_TEST_MODULE Test quality_control QualityControlFlagCollection class
 #define BOOST_TEST_MAIN
 #define BOOST_TEST_DYN_LINK
 
@@ -18,20 +18,21 @@
 // STL
 #include <sstream>
 // o2 includes
-#include "DataFormatsQualityControl/TimeRangeFlagCollection.h"
-#include "DataFormatsQualityControl/TimeRangeFlag.h"
+#include "DataFormatsQualityControl/QualityControlFlagCollection.h"
+#include "DataFormatsQualityControl/QualityControlFlag.h"
+#include "DataFormatsQualityControl/FlagTypeFactory.h"
 
 using namespace o2::quality_control;
 
-BOOST_AUTO_TEST_CASE(test_TimeRangeFlagCollection_Methods)
+BOOST_AUTO_TEST_CASE(test_QualityControlFlagCollection_Methods)
 {
-  TimeRangeFlag trf1{12, 34, FlagReasonFactory::BadTracking(), "comment", "source"};
-  TimeRangeFlag trf2{10, 34, FlagReasonFactory::BadTracking(), "comment", "source"};
+  QualityControlFlag trf1{12, 34, FlagTypeFactory::BadTracking(), "comment", "source"};
+  QualityControlFlag trf2{10, 34, FlagTypeFactory::BadTracking(), "comment", "source"};
 
-  TimeRangeFlagCollection trfc1{"Raw data checks", "TOF", {10, 20000}, 12345, "LHC22k5", "passMC", "qc_mc"};
+  QualityControlFlagCollection trfc1{"Raw data checks", "TOF", {10, 20000}, 12345, "LHC22k5", "passMC", "qc_mc"};
   trfc1.insert(trf1); // by copy
   trfc1.insert(trf2);
-  trfc1.insert({50, 77, FlagReasonFactory::Invalid()}); // by move
+  trfc1.insert({50, 77, FlagTypeFactory::Invalid()}); // by move
   BOOST_CHECK_EQUAL(trfc1.size(), 3);
   BOOST_CHECK_EQUAL(trfc1.getName(), "Raw data checks");
   BOOST_CHECK_EQUAL(trfc1.getDetector(), "TOF");
@@ -42,16 +43,16 @@ BOOST_AUTO_TEST_CASE(test_TimeRangeFlagCollection_Methods)
   BOOST_CHECK_EQUAL(trfc1.getPassName(), "passMC");
   BOOST_CHECK_EQUAL(trfc1.getProvenance(), "qc_mc");
 
-  TimeRangeFlagCollection trfc2{"Reco checks", "TOF"};
-  trfc2.insert({50, 77, FlagReasonFactory::Invalid()}); // this is a duplicate to an entry in trfc1
-  trfc2.insert({51, 77, FlagReasonFactory::Invalid()});
-  trfc2.insert({1234, 3434, FlagReasonFactory::LimitedAcceptance()});
-  trfc2.insert({50, 77, FlagReasonFactory::LimitedAcceptance()});
+  QualityControlFlagCollection trfc2{"Reco checks", "TOF"};
+  trfc2.insert({50, 77, FlagTypeFactory::Invalid()}); // this is a duplicate to an entry in trfc1
+  trfc2.insert({51, 77, FlagTypeFactory::Invalid()});
+  trfc2.insert({1234, 3434, FlagTypeFactory::LimitedAcceptance()});
+  trfc2.insert({50, 77, FlagTypeFactory::LimitedAcceptance()});
   BOOST_CHECK_EQUAL(trfc2.size(), 4);
 
   // Try merging. Duplicate entries should be left in the 'other' objects.
   // Notice that we merge the two partial TRFCs into the third, which covers all cases
-  TimeRangeFlagCollection trfc3{"ALL", "TOF"};
+  QualityControlFlagCollection trfc3{"ALL", "TOF"};
   trfc3.merge(trfc1);
   trfc3.merge(trfc2);
   BOOST_CHECK_EQUAL(trfc1.size(), 0);
@@ -59,14 +60,14 @@ BOOST_AUTO_TEST_CASE(test_TimeRangeFlagCollection_Methods)
   BOOST_CHECK_EQUAL(trfc3.size(), 6);
 
   // Try const merging. It should copy the elements and keep the 'other' intact.
-  TimeRangeFlagCollection trfc4{"ALL", "TOF"};
+  QualityControlFlagCollection trfc4{"ALL", "TOF"};
   const auto& constTrfc3 = trfc3;
   trfc4.merge(constTrfc3);
   BOOST_CHECK_EQUAL(trfc3.size(), 6);
   BOOST_CHECK_EQUAL(trfc4.size(), 6);
 
   // Try merging different detectors - it should throw.
-  TimeRangeFlagCollection trfc5{"ALL", "TPC"};
+  QualityControlFlagCollection trfc5{"ALL", "TPC"};
   BOOST_CHECK_THROW(trfc5.merge(trfc3), std::runtime_error);
   BOOST_CHECK_THROW(trfc5.merge(constTrfc3), std::runtime_error);
 
@@ -79,31 +80,31 @@ BOOST_AUTO_TEST_CASE(test_TimeRangeFlagCollection_Methods)
   }
 }
 
-BOOST_AUTO_TEST_CASE(test_TimeRangeFlagCollection_IO)
+BOOST_AUTO_TEST_CASE(test_QualityControlFlagCollection_IO)
 {
   {
-    TimeRangeFlagCollection trfc1{"xyz", "TST"};
+    QualityControlFlagCollection trfc1{"xyz", "TST"};
 
     std::stringstream store;
     trfc1.streamTo(store);
 
-    TimeRangeFlagCollection trfc2{"xyz", "TST"};
+    QualityControlFlagCollection trfc2{"xyz", "TST"};
     trfc2.streamFrom(store);
 
     BOOST_CHECK_EQUAL(trfc2.size(), 0);
   }
   {
-    TimeRangeFlagCollection trfc1{"xyz", "TST"};
-    trfc1.insert({50, 77, FlagReasonFactory::Invalid(), "a comment", "a source"});
-    trfc1.insert({51, 77, FlagReasonFactory::Invalid()});
-    trfc1.insert({1234, 3434, FlagReasonFactory::LimitedAcceptance()});
-    trfc1.insert({50, 77, FlagReasonFactory::LimitedAcceptance()});
-    trfc1.insert({43434, 63421, FlagReasonFactory::NotBadFlagExample()});
+    QualityControlFlagCollection trfc1{"xyz", "TST"};
+    trfc1.insert({50, 77, FlagTypeFactory::Invalid(), "a comment", "a source"});
+    trfc1.insert({51, 77, FlagTypeFactory::Invalid()});
+    trfc1.insert({1234, 3434, FlagTypeFactory::LimitedAcceptance()});
+    trfc1.insert({50, 77, FlagTypeFactory::LimitedAcceptance()});
+    trfc1.insert({43434, 63421, FlagTypeFactory::NotBadFlagExample()});
 
     std::stringstream store;
     trfc1.streamTo(store);
 
-    TimeRangeFlagCollection trfc2{"xyz", "TST"};
+    QualityControlFlagCollection trfc2{"xyz", "TST"};
     trfc2.streamFrom(store);
 
     BOOST_REQUIRE_EQUAL(trfc1.size(), trfc2.size());
@@ -115,7 +116,7 @@ BOOST_AUTO_TEST_CASE(test_TimeRangeFlagCollection_IO)
     std::stringstream store;
     store << "start,end,flag_id,invalid,header,format\n";
     store << R"(123,345,11,"fdsa",1,"comment","source")";
-    TimeRangeFlagCollection trfc1{"A", "TST"};
+    QualityControlFlagCollection trfc1{"A", "TST"};
     BOOST_CHECK_THROW(trfc1.streamFrom(store), std::runtime_error);
   }
   {
@@ -128,7 +129,7 @@ BOOST_AUTO_TEST_CASE(test_TimeRangeFlagCollection_IO)
     store << R"(123,345,,"fdsa",1,"comment","source")" << '\n';
     store << R"(123,345,11,"",1,"comment","source")" << '\n';
     store << R"(123,345,11,"fdsa",,"comment","source")" << '\n';
-    TimeRangeFlagCollection trfc1{"A", "TST"};
+    QualityControlFlagCollection trfc1{"A", "TST"};
     BOOST_CHECK_NO_THROW(trfc1.streamFrom(store));
     BOOST_CHECK_EQUAL(trfc1.size(), 0);
   }
