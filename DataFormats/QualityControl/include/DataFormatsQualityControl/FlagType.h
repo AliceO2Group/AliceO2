639b9b2cf8ff172fb7d36145f7734d87624e0399
Piotr Konopka <piotr.jan.konopka@cern.ch>
2024-04-26 14:33:57 +0200

    QC-1160 Move to the new names for QC flags (#13073)
    Needs new QC release.
    The documentation will be updated in a separate PR.

--- DataFormats/QualityControl/include/DataFormatsQualityControl/FlagType.h
+++ DataFormats/QualityControl/include/DataFormatsQualityControl/FlagType.h
@@ -12,11 +12,53 @@
 #ifndef O2_QUALITYCONTROL_FLAGTYPE_H
 #define O2_QUALITYCONTROL_FLAGTYPE_H
 
-#include "DataFormatsQualityControl/FlagReasons.h"
+// STL
+#include <iosfwd>
+#include <cstdint>
 
-namespace o2::quality_control
+// ROOT includes
+#include <Rtypes.h>
+
+namespace o2
 {
-using FlagType = o2::quality_control::FlagReason;
-}
+namespace quality_control
+{
 
+class FlagTypeFactory;
+class QualityControlFlagCollection;
+
+class FlagType
+{
+ private:
+  uint16_t mId;
+  std::string mName;
+  bool mBad; // if true, data should become bad by default
+
+  // By making the constructor private and FlagTypes available only in the FlagTypeFactory
+  // we forbid to declare any flags in the user code. If you need a new FlagType, please add it FlagTypeFactory.
+ private:
+  FlagType(uint16_t id, const char* name, bool bad) : mId(id), mName(name), mBad(bad) {}
+
+ public:
+  FlagType();
+  FlagType& operator=(const FlagType&) = default;
+  FlagType(const FlagType&) = default;
+  bool operator==(const FlagType& rhs) const;
+  bool operator!=(const FlagType& rhs) const;
+  bool operator<(const FlagType& rhs) const;
+  bool operator>(const FlagType& rhs) const;
+
+  uint16_t getID() const { return mId; }
+  const std::string& getName() const { return mName; }
+  bool getBad() const { return mBad; }
+
+  friend std::ostream& operator<<(std::ostream& os, FlagType const& me);
+  friend class FlagTypeFactory;
+  friend class QualityControlFlagCollection;
+
+  ClassDefNV(FlagType, 1);
+};
+
+} // namespace quality_control
+} // namespace o2
 #endif // O2_QUALITYCONTROL_FLAGTYPE_H
