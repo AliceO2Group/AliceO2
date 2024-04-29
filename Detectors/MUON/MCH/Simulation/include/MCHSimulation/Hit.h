66e37734846465be3e0db1fd8337e47e0c267e36
mwinn2 <43405856+mwinn2@users.noreply.github.com>
2024-04-29 13:16:51 +0200

    Inclination angle effects in MCH digitizer (#13038)
    * first prototype for inclination and impact on resolution

--- Detectors/MUON/MCH/Simulation/include/MCHSimulation/Hit.h
+++ Detectors/MUON/MCH/Simulation/include/MCHSimulation/Hit.h
@@ -25,19 +25,25 @@ class Hit : public ::o2::BasicXYZEHit<float>
 
  public:
   Hit(int trackId = 0, short detElemId = 0, math_utils::Point3D<float> entrancePoint = {}, const math_utils::Point3D<float> exitPoint = {},
-      float eloss = 0.0, float length = 0.0, float tof = 0.0)
-    : ::o2::BasicXYZEHit<float>(entrancePoint.x(), entrancePoint.y(), entrancePoint.z(), tof, eloss, trackId, detElemId), mLength{length}, mExitPoint(exitPoint)
+      float eloss = 0.0, float length = 0.0, float entranceTof = 0.0, float eTot = 0.0)
+    : ::o2::BasicXYZEHit<float>(entrancePoint.x(), entrancePoint.y(), entrancePoint.z(), entranceTof, eloss, trackId, detElemId), mLength{length}, mExitPoint(exitPoint), mEtot{eTot}
   {
   }
 
   math_utils::Point3D<float> entrancePoint() const { return GetPos(); }
   math_utils::Point3D<float> exitPoint() const { return mExitPoint; }
 
+  // time in s
+  float entranceTof() const { return GetTime(); }
+  // particle energy in GeV
+  float eTot() const { return mEtot; }
+
   short detElemId() const { return GetDetectorID(); }
 
  private:
   float mLength = {};
   math_utils::Point3D<float> mExitPoint = {};
+  float mEtot = {};
   ClassDefNV(Hit, 1);
 };
 
