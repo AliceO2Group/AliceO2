0c56a09aa772a5352d459e08eb328590497b6663
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2024-05-04 09:33:41 +0200

    DPL: improve messages on caught exceptions
    - Provide a bit of context in the printout
    - Use signposts

--- Framework/Core/test/test_AllCrashTypes.sh
+++ Framework/Core/test/test_AllCrashTypes.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -e
 echo $PATH
 printf "ok\nTesting runtime-init..."
-o2-framework-crashing-workflow --crash-type=runtime-init --completion-policy=quit -b --run | grep -q "Exception caught: This is a std::runtime_error" || { printf "runtime error not found" ; exit 1; }
+o2-framework-crashing-workflow --crash-type=runtime-init --completion-policy=quit -b --run | grep -q "Exception caught while in Init: This is a std::runtime_error. Exiting with 1." || { printf "runtime error not found" ; exit 1; }
 printf "ok\nTesting framework-init..."
-o2-framework-crashing-workflow --crash-type=framework-init --completion-policy=quit -b --run | grep -q "Exception caught: This is a o2::framework::runtime_error" || { printf "framework error not found" ;  exit 1; }
+o2-framework-crashing-workflow --crash-type=framework-init --completion-policy=quit -b --run | grep -q "Exception caught while in Init: This is a o2::framework::runtime_error. Exiting with 1." || { printf "framework error not found" ;  exit 1; }
 printf "ok\nTesting framework-run..."
 o2-framework-crashing-workflow --crash-type=framework-run --completion-policy=quit -b --run | grep -q "Unhandled o2::framework::runtime_error reached the top of main of o2-framework-crashing-workflow, device shutting down. Reason: This is a o2::framework::runtime_error" || { printf "framework error not found" ; exit 1; }
 printf "ok\nTesting runtime-run..."
