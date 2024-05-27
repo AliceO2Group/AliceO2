a1917c36e3e291057fb7a34679aa0a4e6287becb
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2024-05-27 10:26:00 +0200

    Drop support for arrow < 11

--- dependencies/O2Dependencies.cmake
+++ dependencies/O2Dependencies.cmake
@@ -30,11 +30,7 @@ include(FeatureSummary)
 include(FindThreads)
 
 find_package(Arrow CONFIG)
-if(${Arrow_VERSION} VERSION_LESS 11)
-find_package(Gandiva CONFIG PATHS ${Arrow_DIR} QUIET)
-else()
 find_package(Gandiva CONFIG)
-endif()
 set_package_properties(Arrow PROPERTIES TYPE REQUIRED)
 set_package_properties(Gandiva PROPERTIES TYPE REQUIRED)
 
