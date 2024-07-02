2359d74fbf9aa91b4d8f0269d06596fd3d668d1f
shahoian <ruben.shahoyan@cern.ch>
2024-07-02 17:56:21 +0200

    Fix setting dump flag for corrupted raw data
    But disable producing dumps by default on EPNs.
    Could be enabled by defining DPL env.var ALPIDE_ERR_DUMPS=1 or ALPIDE_ERR_DUMPS=2 for
    dumping single HBF of the corrupted link or the whole TF for all links resp.

--- Detectors/ITSMFT/common/reconstruction/src/RUDecodeData.cxx
+++ Detectors/ITSMFT/common/reconstruction/src/RUDecodeData.cxx
@@ -57,7 +57,7 @@ void RUDecodeData::fillChipStatistics(int icab, const ChipPixelData* chipData)
   cableLinkPtr[icab]->chipStat.nHits += chipData->getData().size();
   uint32_t action = 0;
   if (chipData->getErrorFlags()) {
-    cableLinkPtr[icab]->chipStat.addErrors(*chipData, verbosity);
+    action = cableLinkPtr[icab]->chipStat.addErrors(*chipData, verbosity);
     auto compid = ChipError::composeID(cableLinkPtr[icab]->feeID, chipData->getChipID());
     auto& chErr = chipErrorsTF[compid];
     chErr.first++;
