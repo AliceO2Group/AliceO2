8bd95971af1e58690bd1d0fa07de4366aafb55d6
avojarot <maksym.kizitskyi@nure.ua>
2024-07-25 14:39:43 +0200

    final adjustments

--- GPU/Common/test/testSMatrixImp.cu
+++ GPU/Common/test/testSMatrixImp.cu
@@ -10,21 +10,7 @@
 // or submit itself to any jurisdiction.
 
 /// \file testGPUSMatrixImp.cu
-/// \author Matteo Concas
-
-// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
-// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
-// All rights not expressly granted are reserved.
-//
-// This software is distributed under the terms of the GNU General Public
-// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
-//
-// In applying this license CERN does not waive the privileges and immunities
-// granted to it by virtue of its status as an Intergovernmental Organization
-// or submit itself to any jurisdiction.
-
-/// \file testGPUSMatrixImpUnified.cu
-/// \author Matteo Concas
+/// \author Matteo Concas, Maksym KIzitskyi
 
 #define BOOST_TEST_MODULE Test GPUSMatrixImpl
 #ifdef __HIPCC__
