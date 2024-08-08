feb58729b0d5f88217a3c521b783c45fed26449b
swenzel <sandro.wenzel@cern.ch>
2024-08-08 14:39:22 +0200

    Consistency fix in G4 physics model - fix EMCAL sampling factor
    improvement for G4 physics model (options):
    
    - offer all physics lists with and without EMV (fast em physics) mode
    - better consistency between enum name and actual physics string
    - possibility that user gives own string for physics list
    
    EMCAL detector:
    
    - provide a reasonable sampling factor also for Geant4 physics lists
      not mentioned so far
      (fixing a problem when we switch from FTFP_BERT_EMV to FTFP_BERT for instance)

--- Common/SimConfig/include/SimConfig/G4Params.h
+++ Common/SimConfig/include/SimConfig/G4Params.h
@@ -22,17 +22,24 @@ namespace conf
 
 // enumerating the possible G4 physics settings
 enum class EG4Physics {
-  kFTFP_BERT_optical = 0,         /* just ordinary */
-  kFTFP_BERT_optical_biasing = 1, /* with biasing enabled */
-  kFTFP_INCLXX_optical = 2,       /* special INCL++ version */
-  kFTFP_BERT_HP_optical = 3       /* enable low energy neutron transport */
+  kFTFP_BERT_optical = 0,             /* just ordinary */
+  kFTFP_BERT_optical_biasing = 1,     /* with biasing enabled */
+  kFTFP_INCLXX_optical = 2,           /* special INCL++ version */
+  kFTFP_BERT_HP_optical = 3,          /* enable low energy neutron transport */
+  kFTFP_BERT_EMV_optical = 4,         /* just ordinary with faster electromagnetic physics */
+  kFTFP_BERT_EMV_optical_biasing = 5, /* with biasing enabled with faster electromagnetic physics */
+  kFTFP_INCLXX_EMV_optical = 6,       /* special INCL++ version */
+  kFTFP_BERT_EMV_HP_optical = 7,      /* enable low energy neutron transport */
+  kUSER = 8                           /* allows to give own string combination */
 };
 
 // parameters to influence the G4 engine
 struct G4Params : public o2::conf::ConfigurableParamHelper<G4Params> {
-  EG4Physics physicsmode = EG4Physics::kFTFP_BERT_optical; // physics mode with which to configure G4
+  EG4Physics physicsmode = EG4Physics::kFTFP_BERT_EMV_optical; // default physics mode with which to configure G4
 
   std::string configMacroFile = ""; // a user provided g4Config.in file (otherwise standard one fill be taken)
+  std::string userPhysicsList = ""; // possibility to directly give physics list as string
+
   std::string const& getPhysicsConfigString() const;
 
   O2ParamDef(G4Params, "G4");
