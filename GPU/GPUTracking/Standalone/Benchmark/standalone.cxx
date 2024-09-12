105235d42ae1e6b46154dcecb5ea576b8647070c
David Rohr <drohr@jwdt.org>
2024-09-12 14:16:23 +0200

    GPU Standalone: --runGPUforce setting was not respected correctly

--- GPU/GPUTracking/Standalone/Benchmark/standalone.cxx
+++ GPU/GPUTracking/Standalone/Benchmark/standalone.cxx
@@ -266,7 +266,12 @@ int ReadConfiguration(int argc, char** argv)
     } else if (GPUReconstruction::CheckInstanceAvailable(GPUReconstruction::DeviceType::OCL, configStandalone.proc.debugLevel >= 2)) {
       configStandalone.gpuType = "OCL";
     } else {
+      if (configStandalone.runGPUforce) {
+        printf("No GPU backend / device found, running on CPU is disabled due to runGPUforce\n");
+        return 1;
+      }
       configStandalone.runGPU = false;
+      configStandalone.gpuType = "CPU";
     }
   }
 
