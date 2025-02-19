7b7f48570c7d7cd6847620d9858a02054cd5e265
David Rohr <drohr@jwdt.org>
2025-02-20 00:11:02 +0100

    fix Vulkan

--- GPU/GPUTracking/display/backend/GPUDisplayBackendVulkan.h
+++ GPU/GPUTracking/display/backend/GPUDisplayBackendVulkan.h
@@ -22,7 +22,7 @@
 #include <unordered_map>
 #include <utils/vecpod.h>
 
-namespace GPUCA_NAMESPACE::gpu
+namespace o2::gpu
 {
 
 class GPUDisplayBackendVulkan : public GPUDisplayBackend
@@ -133,8 +133,6 @@ class GPUDisplayBackendVulkan : public GPUDisplayBackend
   bool mEnableValidationLayers = false;
 
   vk::Instance mInstance;
-  vk::DynamicLoader mDL;
-  vk::DispatchLoaderDynamic mDLD;
   vk::DebugUtilsMessengerEXT mDebugMessenger;
   vk::PhysicalDevice mPhysicalDevice;
   vk::Device mDevice;
@@ -217,6 +215,6 @@ class GPUDisplayBackendVulkan : public GPUDisplayBackend
 
   vk::Fence mSingleCommitFence;
 };
-} // namespace GPUCA_NAMESPACE::gpu
+} // namespace o2::gpu
 
 #endif
