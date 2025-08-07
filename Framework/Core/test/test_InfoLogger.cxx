a2b3223ceef4af742b03810ee940d5c8769705bb
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2025-08-07 15:41:11 +0200

    DPL: drop more boost_test usage

--- Framework/Core/test/test_InfoLogger.cxx
+++ Framework/Core/test/test_InfoLogger.cxx
@@ -8,16 +8,12 @@
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
-#define BOOST_TEST_MODULE Test Framework InfoLoggerTest
-#define BOOST_TEST_MAIN
-#define BOOST_TEST_DYN_LINK
-
-#include <boost/test/unit_test.hpp>
+#include <catch_amalgamated.hpp>
 
 #include <InfoLogger/InfoLogger.hxx>
 using namespace AliceO2::InfoLogger;
 
-BOOST_AUTO_TEST_CASE(InfoLoggerTest)
+TEST_CASE("InfoLoggerTest")
 {
 
   // define infologger output to stdout, as we don't want to use the default infoLoggerD pipe which might not be running here
@@ -27,5 +23,5 @@ BOOST_AUTO_TEST_CASE(InfoLoggerTest)
   InfoLogger theLog;
 
   // log a test message
-  BOOST_CHECK(theLog.log("This is a log message test to stdout") == 0);
+  CHECK(theLog.log("This is a log message test to stdout") == 0);
 }
