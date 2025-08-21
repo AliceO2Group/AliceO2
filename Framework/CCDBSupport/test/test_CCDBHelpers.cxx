e26ee5258f25b29f3a35c432a196093a57a6ad2e
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2025-08-21 09:14:53 +0200

    WIP DPL Analysis: centralised CCDB support in analysis
    Thanks to the newly added binary view columns we can finally support
    proper CCDB integration in analysis.
    
    In order to do so, the user needs to create a TIMESTAMPED table, i.e. a table
    which is an extension of another one where the timestamps for each rows
    are provided. The extra columns of such timestamped table will be CCDB columns
    where the iterator of each provides access for one specified CCDB object.

--- Framework/CCDBSupport/test/test_CCDBHelpers.cxx
+++ Framework/CCDBSupport/test/test_CCDBHelpers.cxx
@@ -9,43 +9,39 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-#define BOOST_TEST_MODULE Test Framework CCDBHelpers
-#define BOOST_TEST_MAIN
-#define BOOST_TEST_DYN_LINK
-
-#include <boost/test/unit_test.hpp>
-#include "../src/CCDBHelpers.h"
+#include <catch_amalgamated.hpp>
+#include "../src/CCDBFetcherHelper.h"
 
 using namespace o2::framework;
 
-BOOST_AUTO_TEST_CASE(TestSorting)
+TEST_CASE("TestSorting")
 {
-  auto result = CCDBHelpers::parseRemappings("");
-  BOOST_CHECK_EQUAL(result.error, ""); // not an error
+  auto result = CCDBFetcherHelper::parseRemappings("");
+  CHECK(result.error == ""); // not an error
 
-  result = CCDBHelpers::parseRemappings("https");
-  BOOST_CHECK_EQUAL(result.error, "URL should start with either http:// or https:// or file://");
+  result = CCDBFetcherHelper::parseRemappings("https");
+  CHECK(result.error == "URL should start with either http:// or https:// or file://");
 
-  result = CCDBHelpers::parseRemappings("https://alice.cern.ch:8000");
-  BOOST_CHECK_EQUAL(result.error, "Expecting at least one target path, missing `='?");
+  result = CCDBFetcherHelper::parseRemappings("https://alice.cern.ch:8000");
+  CHECK(result.error == "Expecting at least one target path, missing `='?");
 
-  result = CCDBHelpers::parseRemappings("https://alice.cern.ch:8000=");
-  BOOST_CHECK_EQUAL(result.error, "Empty target");
+  result = CCDBFetcherHelper::parseRemappings("https://alice.cern.ch:8000=");
+  CHECK(result.error == "Empty target");
 
-  result = CCDBHelpers::parseRemappings("https://alice.cern.ch:8000=/foo/bar,");
-  BOOST_CHECK_EQUAL(result.error, "Empty target");
+  result = CCDBFetcherHelper::parseRemappings("https://alice.cern.ch:8000=/foo/bar,");
+  CHECK(result.error == "Empty target");
 
-  result = CCDBHelpers::parseRemappings("https://alice.cern.ch:8000=/foo/bar,/foo/bar;");
-  BOOST_CHECK_EQUAL(result.error, "URL should start with either http:// or https:// or file://");
+  result = CCDBFetcherHelper::parseRemappings("https://alice.cern.ch:8000=/foo/bar,/foo/bar;");
+  CHECK(result.error == "URL should start with either http:// or https:// or file://");
 
-  result = CCDBHelpers::parseRemappings("https://alice.cern.ch:8000=/foo/bar,/foo/barbar;file://user/test=/foo/barr");
-  BOOST_CHECK_EQUAL(result.error, "");
-  BOOST_CHECK_EQUAL(result.remappings.size(), 3);
-  BOOST_CHECK_EQUAL(result.remappings["/foo/bar"], "https://alice.cern.ch:8000");
-  BOOST_CHECK_EQUAL(result.remappings["/foo/barbar"], "https://alice.cern.ch:8000");
-  BOOST_CHECK_EQUAL(result.remappings["/foo/barr"], "file://user/test");
+  result = CCDBFetcherHelper::parseRemappings("https://alice.cern.ch:8000=/foo/bar,/foo/barbar;file://user/test=/foo/barr");
+  CHECK(result.error == "");
+  CHECK(result.remappings.size() == 3);
+  CHECK(result.remappings["/foo/bar"] == "https://alice.cern.ch:8000");
+  CHECK(result.remappings["/foo/barbar"] == "https://alice.cern.ch:8000");
+  CHECK(result.remappings["/foo/barr"] == "file://user/test");
 
-  result = CCDBHelpers::parseRemappings("https://alice.cern.ch:8000=/foo/bar;file://user/test=/foo/bar");
-  BOOST_CHECK_EQUAL(result.remappings.size(), 1);
-  BOOST_CHECK_EQUAL(result.error, "Path /foo/bar requested more than once.");
+  result = CCDBFetcherHelper::parseRemappings("https://alice.cern.ch:8000=/foo/bar;file://user/test=/foo/bar");
+  CHECK(result.remappings.size() == 1);
+  CHECK(result.error == "Path /foo/bar requested more than once.");
 }
