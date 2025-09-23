7d6fc54b633536ba913b82c0c2bc76e0b734f093
Nicolas Poffley <nicolas.poffley@gmail.com>
2025-09-23 11:04:28 +0200

    Default param "" for protocol instead of overloading for encode_websocket_handshake_request

--- Framework/Core/src/HTTPParser.cxx
+++ Framework/Core/src/HTTPParser.cxx
@@ -214,27 +214,26 @@ std::string HTTPParserHelpers::calculateAccept(const char* nonce)
   return fmt::format("{}", base);
 }
 
-std::string encode_websocket_handshake_reply(char const* nonce)
+std::string encode_websocket_handshake_reply(char const* nonce, char const* protocol = "")
 {
-  constexpr auto res =
-    "HTTP/1.1 101 Switching Protocols\r\n"
-    "Upgrade: websocket\r\n"
-    "Connection: Upgrade\r\n"
-    "Access-Control-Allow-Origin: \"*\"\r\n"
-    "Sec-WebSocket-Accept: {}\r\n\r\n";
-  return fmt::format(res, HTTPParserHelpers::calculateAccept(nonce));
-}
-
-std::string encode_websocket_handshake_reply(char const* nonce, char const* protocol)
-{
-  constexpr auto res =
-    "HTTP/1.1 101 Switching Protocols\r\n"
-    "Upgrade: websocket\r\n"
-    "Connection: Upgrade\r\n"
-    "Access-Control-Allow-Origin: \"*\"\r\n"
-    "Sec-WebSocket-Protocol: {}\r\n"
-    "Sec-WebSocket-Accept: {}\r\n\r\n";
-  return fmt::format(res, protocol, HTTPParserHelpers::calculateAccept(nonce));
+  if (strlen(protocol) == 0) {
+    constexpr auto res =
+      "HTTP/1.1 101 Switching Protocols\r\n"
+      "Upgrade: websocket\r\n"
+      "Connection: Upgrade\r\n"
+      "Access-Control-Allow-Origin: \"*\"\r\n"
+      "Sec-WebSocket-Accept: {}\r\n\r\n";
+    return fmt::format(res, HTTPParserHelpers::calculateAccept(nonce));
+  } else {
+    constexpr auto res =
+      "HTTP/1.1 101 Switching Protocols\r\n"
+      "Upgrade: websocket\r\n"
+      "Connection: Upgrade\r\n"
+      "Access-Control-Allow-Origin: \"*\"\r\n"
+      "Sec-WebSocket-Protocol: {}\r\n"
+      "Sec-WebSocket-Accept: {}\r\n\r\n";
+    return fmt::format(res, protocol, HTTPParserHelpers::calculateAccept(nonce));
+  }
 }
 
 void parse_http_request(char* start, size_t size, HTTPParser* parser)
