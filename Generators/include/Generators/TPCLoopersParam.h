c156a31d5f393de5d646aa9415fe10fbacbf504e
Marco Giacalone <mgiacalo@gmail.com>
2025-11-20 22:40:33 +0100

    Set automatic interaction rate from collision context

--- Generators/include/Generators/TPCLoopersParam.h
+++ Generators/include/Generators/TPCLoopersParam.h
@@ -37,9 +37,9 @@ struct GenTPCLoopersParam : public o2::conf::ConfigurableParamHelper<GenTPCLoope
   std::string scaler_compton = "${O2_ROOT}/share/Generators/egconfig/ScalerComptonParams.json"; // file with scaler parameters for Compton scattering
   std::string nclxrate = "ccdb://Users/m/mgiacalo/ClustersTrackRatio";                          // file with clusters/rate information per orbit
   std::string colsys = "PbPb";                                                                  // collision system  (PbPb or pp)
-  int intrate = 50000;                                                                          // interaction rate
+  int intrate = -1;                                                                             // Automatic IR from collision context if -1, else user-defined interaction rate in Hz
   bool flat_gas = true; // if true, the gas density is considered flat in the TPC volume
-  int nFlatGasLoopers = 500;  // number of loopers to be generated per event in case of flat gas
+  unsigned int nFlatGasLoopers = 500;  // number of loopers to be generated per event in case of flat gas
   float fraction_pairs = 0.08; // fraction of loopers
   float multiplier[2] = {1., 1.}; // multiplier for pairs and compton loopers for Poissonian and Gaussian sampling
   unsigned int fixedNLoopers[2] = {1, 1}; // fixed number of loopers coming from pairs and compton electrons - valid if flat gas is false and both Poisson and Gaussian params files are empty
