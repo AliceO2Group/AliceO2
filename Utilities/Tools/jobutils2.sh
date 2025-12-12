0749e796eee77cde9650574058536cfc44db1054
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2025-12-12 14:21:21 +0100

    Apply suggestion from @ktf

--- Utilities/Tools/jobutils2.sh
+++ Utilities/Tools/jobutils2.sh
@@ -396,8 +396,8 @@ getNumberOfPhysicalCPUCores() {
   else
     # Do something under GNU/Linux platform
     #
-# Gets the cores per socket by counting unique cores on socket 0.
-# Gets sockets by counting unique socket ids. The grepping is done in any case  to avoid matching comments. 
+    # Gets the cores per socket by counting unique cores on socket 0.
+    # Gets sockets by counting unique socket ids. The grepping is done in any case  to avoid matching comments. 
     CORESPERSOCKET=$(lscpu -p=cpu,socket | grep "^[0-9]\+,0" | sort | uniq | wc -l)
     SOCKETS=$(lscpu -p=socket | grep -e "^[0-9]" | sort | uniq | wc -l)
   fi
