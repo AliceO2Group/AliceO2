f2335b643c956fe12ab37f246a04330f706b4ac5
Sandro Wenzel <sandro.wenzel@cern.ch>
2026-01-05 10:21:41 +0100

    Revert "Add copyright headers"
    This reverts commit 6837bccb95a137dd24d42d44fd4db5579ad3dbf9.

--- Generators/include/TPCLoopers.h
+++ Generators/include/TPCLoopers.h
@@ -1,16 +1,3 @@
-// Copyright 2024-2025 CERN and copyright holders of ALICE O2.
-// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
-// All rights not expressly granted are reserved.
-//
-// This software is distributed under the terms of the GNU General Public
-// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
-//
-// In applying this license CERN does not waive the privileges and immunities
-// granted to it by virtue of its status as an Intergovernmental Organization
-// or submit itself to any jurisdiction.
-
-/// \author M+Giacalone - September 2025
-
 #ifndef ALICEO2_EVENTGEN_TPCLOOPERS_H_
 #define ALICEO2_EVENTGEN_TPCLOOPERS_H_
 
