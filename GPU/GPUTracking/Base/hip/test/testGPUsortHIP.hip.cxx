f508e1c30c9cd4f06ce6c43a175e203f33489632
Vít Kučera <26327373+vkucera@users.noreply.github.com>
2026-02-05 10:25:09 +0100

    Revert "Add missing suffix"
    This reverts commit 81879979abeac8839164d57d33d65a392c4b3aea.

--- GPU/GPUTracking/Base/hip/test/testGPUsortHIP.hip.cxx
+++ GPU/GPUTracking/Base/hip/test/testGPUsortHIP.hip.cxx
@@ -9,7 +9,7 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-/// \file testGPUsortHIP.hip.cxx
+/// \file testGPUsortHIP.hip
 /// \author Michael Lettrich
 
 #define GPUCA_GPUTYPE_VEGA
