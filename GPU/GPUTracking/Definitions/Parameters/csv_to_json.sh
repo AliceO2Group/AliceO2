f8fc9bec998a40177c6effb6b9d0f5ddf200698b
David Rohr <drohr@jwdt.org>
2026-02-05 15:59:08 +0100

    GPU: Add converter scripts for CSV parameter file to JSON and vice versa

---
+++ GPU/GPUTracking/Definitions/Parameters/csv_to_json.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+[[ -z $1 ]] && { echo "Usage: csv_to_json.sh CSV_FILE"; exit 1; }
+
+awk -vFPAT='([^,]*)|(\"([^\"]|\"\")*\")' \
+  'BEGIN {
+     print "{"
+   } {
+     if (count == 0) {
+       for (i = 1; i <= NF; i++) {
+         names[i] = $i
+       }
+     } else if ($1 == "CORE:" || $1 == "LB:" || $1 == "PAR:") {
+       if (paramprinted) print "\n    }"
+       else if (lineprinted) print ""
+       if (catprinted) print "  },"
+       lineprinted = 0
+       paramprinted = 0
+       catprinted = 1
+       gsub(/:$/, "", $1)
+       print "  \""$1"\": {";
+     } else if ($1 != "") {
+       if (lineprinted) print ""
+       if (paramprinted) print "    },"
+       lineprinted = 0
+       paramprinted = 1
+       print "    \""$1"\": {";
+       lineprinted = 0
+       for (i=2; i<=NF; i++) {
+         if ($i != "") {
+           gsub(/^"/, "", $i)
+           gsub(/"$/, "", $i)
+           gsub(/""/, "\"", $i)
+           if (lineprinted) print ","
+           lineprinted = 1
+           printf("      \"%s\": %s", names[i], $i)
+         }
+       }
+     }
+     count++;
+   } END {
+     if (paramprinted) print "\n    }"
+     if (catprinted) print "  }"
+     print "}"
+   }' \
+   $1
