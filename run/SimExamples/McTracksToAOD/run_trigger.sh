d43bb892acd628deac76a2414688fc7bee671973
Marco Giacalone <mgiacalo@gmail.com>
2026-02-11 11:30:23 +0100

    Update examples on AO2D creation from MCTracks

--- run/SimExamples/McTracksToAOD/run_trigger.sh
+++ run/SimExamples/McTracksToAOD/run_trigger.sh
@@ -5,9 +5,9 @@
 
 set -x
 NEVENTS=1000
-
+## Add --aod-writer-keep dangling to o2-sim-mctracks-to-aod to write the AO2D file to disc (as AnalysisResults_trees.root)
 CONFKEY="TriggerExternal.fileName=trigger.macro;TriggerExternal.funcName=trigger()"
-o2-sim-dpl-eventgen -b --nevents ${NEVENTS} --aggregate-timeframe 10 --generator pythia8pp --trigger external \
-                    --vertexMode kDiamondParam --confKeyValues "${CONFKEY}" |\
+o2-sim-dpl-eventgen -b --nEvents ${NEVENTS} --aggregate-timeframe 10 --generator pythia8pp --trigger external \
+                    --vertexMode kDiamondParam --configKeyValues "${CONFKEY}" |\
 o2-sim-mctracks-to-aod -b | o2-analysis-mctracks-to-aod-simple-task -b
 
