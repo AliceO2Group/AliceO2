4499dbfaf6bc76ac3296a668b7f8af0b26ad7d3d
Nicolò Jacazio <njacazio@users.noreply.github.com>
2026-03-11 10:31:14 +0100

    Update inner TOF dimensions in configLayers function

--- Detectors/Upgrades/ALICE3/IOTOF/simulation/src/Detector.cxx
+++ Detectors/Upgrades/ALICE3/IOTOF/simulation/src/Detector.cxx
@@ -61,7 +61,7 @@ void Detector::configLayers(bool itof, bool otof, bool ftof, bool btof, std::str
                             const float x2x0)
 {
 
-  const std::pair<float, float> dInnerTof = {21.f, 124.f}; // Radius and length
+  const std::pair<float, float> dInnerTof = {21.f, 129.f}; // Radius and length
   std::pair<float, float> dOuterTof = {92.f, 680.f};       // Radius and length
   std::pair<float, float> radiusRangeDiskTof = {15.f, 100.f};
   float zForwardTof = 370.f;
