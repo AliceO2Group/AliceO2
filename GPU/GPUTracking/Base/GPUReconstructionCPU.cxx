61a9e88d4a0269c0b1d78dddbdf2e714257bd069
Oliver Rietmann <oliver.rietmann@cern.ch>
2026-03-17 15:20:55 +0100

    Write markdown style to terminal

--- GPU/GPUTracking/Base/GPUReconstructionCPU.cxx
+++ GPU/GPUTracking/Base/GPUReconstructionCPU.cxx
@@ -35,8 +35,10 @@
 
 #include <atomic>
 #include <ctime>
-#include <iomanip>
 #include <iostream>
+#include <format>
+#include <string>
+
 
 #ifndef _WIN32
 #include <unistd.h>
@@ -215,35 +217,62 @@ int32_t GPUReconstructionCPU::ExitDevice()
 }
 
 namespace {
-  void write_header(std::ostream& stream) {
-    stream << "type,count,name,kernel (us),cpu (us),cpu/total,total (us),GB/s,bytes,bytes/call\n";
+void writeHeaderMarkdown(std::ostream& stream) {
+  stream <<  "|   |  count | name                                      |  gpu (us) |  cpu (us) | cpu/tot |  tot (us) |      GB/s |         bytes |    bytes/call |\n";
+  stream <<  "|---|--------|-------------------------------------------|-----------|-----------|---------|-----------|-----------|---------------|---------------|\n";
+}
+
+void writeHeaderCSV(std::ostream& stream) {
+  stream <<  "type,count,name,gpu (us),cpu (us),cpu/total,total (us),GB/s,bytes,bytes/call\n";
+}
+
+struct Row {
+  char type = ' ';
+  std::string name;
+  uint32_t count = 0;
+  double gpu_time = -1.0;
+  double cpu_time = -1.0;
+  double total_time = -1.0;
+  uint32_t memSize = 0;
+  uint32_t statNEvents;
+
+  void writeMarkdown(std::ostream& stream) {
+    double scale = 1000000.0 / statNEvents;
+    stream << "| " << type << " | ";
+    if (count != 0) stream << std::format("{:6} |", count);
+    else stream << "       |";
+    stream << std::format(" {:42}|", name);
+    if (gpu_time != -1.0) stream << std::format("{:10.0f} |", gpu_time * scale);
+    else stream << "           |";
+    if (cpu_time != -1.0) stream << std::format("{:10.0f} |", cpu_time * scale);
+    else stream << "           |";
+    if (cpu_time != -1.0 && total_time != -1.0) stream << std::format("{:8.2f} |", cpu_time / total_time);
+    else stream << "         |";
+    if (total_time != -1.0) stream << std::format("{:10.0f} |", total_time * scale);
+    else stream << "           |";
+    if (memSize != 0 && count != 0) stream << std::format("{:10.3f} |{:14} |{:14} |", memSize / gpu_time * 1e-9, memSize / statNEvents, memSize / statNEvents / count);
+    else stream << "           |               |               |";
+    stream << std::endl;
   }
 
-  struct Row {
-    std::string type = "";
-    std::string name = "";
-    double kernel_time = -1.0;
-    double cpu_time = -1.0;
-    double total_time = -1.0;
-    size_t memSize = 0;
-    uint32_t count = 0;
-
-    void write(std::ostream& stream, uint32_t statNEvents) {
-      double scale = 1000000.0 / statNEvents;
-      stream << type << ",";
-      if (count != 0) stream << count;
-      stream << "," << name << "," << uint32_t(kernel_time * scale) << ",";
-      if (cpu_time != -1.0) stream << uint32_t(cpu_time * scale);
-      stream << ",";
-      if (cpu_time != -1.0 && total_time != -1.0) stream << uint32_t(cpu_time / total_time *100) / 100.0;
-      stream << ",";
-      if (total_time != -1.0) stream << uint32_t(total_time * scale);
-      stream << ",";
-      if (memSize != 0 && count != 0) stream << uint32_t(memSize / kernel_time * 1e-6) * 1e-3 << "," << memSize / statNEvents << "," << memSize / statNEvents / count;
-      else stream << ",,";
-      stream << std::endl;
-    }
-  };
+  void writeCSV(std::ostream& stream) {
+    double scale = 1000000.0 / statNEvents;
+    stream << type << ",";
+    if (count != 0) stream << count;
+    stream << "," << name << ",";
+    if (gpu_time != -1.0) stream << std::format("{:.0f}", gpu_time * scale);
+    stream << ",";
+    if (cpu_time != -1.0) stream << std::format("{:.0f}", cpu_time * scale);
+    stream << ",";
+    if (cpu_time != -1.0 && total_time != -1.0) stream << std::format("{:.2f}", cpu_time / total_time);
+    stream << ",";
+    if (total_time != -1.0) stream << std::format("{:.0f}", total_time * scale);
+    stream << ",";
+    if (memSize != 0 && count != 0) stream << std::format("{:.3f},{},{}", memSize / gpu_time * 1e-9, memSize / statNEvents, memSize / statNEvents / count);
+    else stream << ",,";
+    stream << std::endl;
+  }
+};
 }
 
 int32_t GPUReconstructionCPU::RunChains()
@@ -290,7 +319,7 @@ int32_t GPUReconstructionCPU::RunChains()
     PrintMemoryOverview();
   }
 
-  mStatWallTime = (mTimerTotal.GetElapsedTime() * 1000000. / mStatNEvents);
+  mStatWallTime = mTimerTotal.GetElapsedTime();
   std::string nEventReport;
   if (GetProcessingSettings().debugLevel >= 0 && mStatNEvents > 1) {
     nEventReport += "   (avergage of " + std::to_string(mStatNEvents) + " runs)";
@@ -304,11 +333,12 @@ int32_t GPUReconstructionCPU::RunChains()
     if (!benchmarkCSV.is_open()) {
       GPUError("Could not open timing CSV file '%s' for writing", GetProcessingSettings().timingCSV.c_str());
     } else if (mNEventsProcessed == 1) {
-      write_header(benchmarkCSV);
+      writeHeaderCSV(benchmarkCSV);
     }
   }
 
   if (GetProcessingSettings().debugLevel >= 1) {
+    writeHeaderMarkdown(std::cout);
     for (uint32_t i = 0; i < mTimers.size(); i++) {
       double time = 0;
       if (mTimers[i] == nullptr) {
@@ -328,18 +358,19 @@ int32_t GPUReconstructionCPU::RunChains()
         int32_t stepNum = getRecoStepNum(mTimers[i]->step);
         kernelStepTimes[stepNum] += time;
       }
-      char bandwidth[256] = "";
       Row task_row;
       task_row.type = 'K';
       task_row.name = mTimers[i]->name.c_str();
-      task_row.kernel_time = time;
+      task_row.gpu_time = time;
       task_row.count = mTimers[i]->count;
+      task_row.statNEvents = mStatNEvents;
       if (mTimers[i]->memSize && mStatNEvents && time != 0.) {
         task_row.memSize = mTimers[i]->memSize;
-        snprintf(bandwidth, 256, " (%8.3f GB/s - %'14zu bytes - %'14zu per call)", mTimers[i]->memSize / time * 1e-9, mTimers[i]->memSize / mStatNEvents, mTimers[i]->memSize / mStatNEvents / mTimers[i]->count);
       }
-      if (benchmarkCSV.is_open()) task_row.write(benchmarkCSV, mStatNEvents);
-      printf("Execution Time: Task (%c %8ux): %50s Time: %'10.0f us%s\n", type == 0 ? 'K' : 'C', mTimers[i]->count, mTimers[i]->name.c_str(), time * 1000000 / mStatNEvents, bandwidth);
+      if (benchmarkCSV.is_open()) {
+        task_row.writeCSV(benchmarkCSV);
+      }
+      task_row.writeMarkdown(std::cout);
       if (GetProcessingSettings().resetTimers) {
         mTimers[i]->count = 0;
         mTimers[i]->memSize = 0;
@@ -351,34 +382,40 @@ int32_t GPUReconstructionCPU::RunChains()
       if (kernelStepTimes[i] != 0. || mTimersRecoSteps[i].timerTotal.GetElapsedTime() != 0.) {
         Row reco_step_row;
         reco_step_row.name = std::string(gpudatatypes::RECO_STEP_NAMES[i]) + " (Tasks)";
-        reco_step_row.kernel_time = kernelStepTimes[i];
+        reco_step_row.gpu_time = kernelStepTimes[i];
         reco_step_row.cpu_time = mTimersRecoSteps[i].timerCPU;
         reco_step_row.total_time = mTimersRecoSteps[i].timerTotal.GetElapsedTime();
-        if (benchmarkCSV.is_open()) reco_step_row.write(benchmarkCSV, mStatNEvents);
-        printf("Execution Time: Step              : %11s %38s Time: %'10.0f us %64s ( Total Time : %'14.0f us, CPU Time : %'14.0f us, %'7.2fx )\n", "Tasks",
-               gpudatatypes::RECO_STEP_NAMES[i], kernelStepTimes[i] * 1000000 / mStatNEvents, "", mTimersRecoSteps[i].timerTotal.GetElapsedTime() * 1000000 / mStatNEvents, mTimersRecoSteps[i].timerCPU * 1000000 / mStatNEvents, mTimersRecoSteps[i].timerCPU / mTimersRecoSteps[i].timerTotal.GetElapsedTime());
+        reco_step_row.statNEvents = mStatNEvents;
+        if (benchmarkCSV.is_open()) {
+          reco_step_row.writeCSV(benchmarkCSV);
+        }
+        reco_step_row.writeMarkdown(std::cout);
       }
       if (mTimersRecoSteps[i].bytesToGPU) {
         Row reco_step_row;
         reco_step_row.type = 'D';
         reco_step_row.name = std::string(gpudatatypes::RECO_STEP_NAMES[i]) + " (DMA to GPU)";
-        reco_step_row.kernel_time = mTimersRecoSteps[i].timerToGPU.GetElapsedTime();
+        reco_step_row.gpu_time = mTimersRecoSteps[i].timerToGPU.GetElapsedTime();
         reco_step_row.memSize = mTimersRecoSteps[i].bytesToGPU;
         reco_step_row.count = mTimersRecoSteps[i].countToGPU;
-        if (benchmarkCSV.is_open()) reco_step_row.write(benchmarkCSV, mStatNEvents);
-        printf("Execution Time: Step (D %8ux): %11s %38s Time: %'10.0f us (%8.3f GB/s - %'14zu bytes - %'14zu per call)\n", mTimersRecoSteps[i].countToGPU, "DMA to GPU", gpudatatypes::RECO_STEP_NAMES[i], mTimersRecoSteps[i].timerToGPU.GetElapsedTime() * 1000000 / mStatNEvents,
-               mTimersRecoSteps[i].bytesToGPU / mTimersRecoSteps[i].timerToGPU.GetElapsedTime() * 1e-9, mTimersRecoSteps[i].bytesToGPU / mStatNEvents, mTimersRecoSteps[i].bytesToGPU / mTimersRecoSteps[i].countToGPU);
+        reco_step_row.statNEvents = mStatNEvents;
+        if (benchmarkCSV.is_open()) {
+          reco_step_row.writeCSV(benchmarkCSV);
+        }
+        reco_step_row.writeMarkdown(std::cout);
       }
       if (mTimersRecoSteps[i].bytesToHost) {
         Row reco_step_row;
         reco_step_row.type = 'D';
         reco_step_row.name = std::string(gpudatatypes::RECO_STEP_NAMES[i]) + " (DMA to Host)";
-        reco_step_row.kernel_time = mTimersRecoSteps[i].timerToHost.GetElapsedTime();
+        reco_step_row.gpu_time = mTimersRecoSteps[i].timerToHost.GetElapsedTime();
         reco_step_row.memSize = mTimersRecoSteps[i].bytesToHost;
         reco_step_row.count = mTimersRecoSteps[i].countToHost;
-        if (benchmarkCSV.is_open()) reco_step_row.write(benchmarkCSV, mStatNEvents);
-        printf("Execution Time: Step (D %8ux): %11s %38s Time: %'10.0f us (%8.3f GB/s - %'14zu bytes - %'14zu per call)\n", mTimersRecoSteps[i].countToHost, "DMA to Host", gpudatatypes::RECO_STEP_NAMES[i], mTimersRecoSteps[i].timerToHost.GetElapsedTime() * 1000000 / mStatNEvents,
-               mTimersRecoSteps[i].bytesToHost / mTimersRecoSteps[i].timerToHost.GetElapsedTime() * 1e-9, mTimersRecoSteps[i].bytesToHost / mStatNEvents, mTimersRecoSteps[i].bytesToHost / mTimersRecoSteps[i].countToHost);
+        reco_step_row.statNEvents = mStatNEvents;
+        if (benchmarkCSV.is_open()) {
+          reco_step_row.writeCSV(benchmarkCSV);
+        }
+        reco_step_row.writeMarkdown(std::cout);
       }
       if (GetProcessingSettings().resetTimers) {
         mTimersRecoSteps[i].bytesToGPU = mTimersRecoSteps[i].bytesToHost = 0;
@@ -394,24 +431,28 @@ int32_t GPUReconstructionCPU::RunChains()
       if (mTimersGeneralSteps[i].GetElapsedTime() != 0.) {
         Row general_step_row;
         general_step_row.name = gpudatatypes::GENERAL_STEP_NAMES[i];
-        general_step_row.kernel_time = mTimersGeneralSteps[i].GetElapsedTime();
-        if (benchmarkCSV.is_open()) general_step_row.write(benchmarkCSV, mStatNEvents);
-        printf("Execution Time: General Step      : %50s Time: %'10.0f us\n", gpudatatypes::GENERAL_STEP_NAMES[i], mTimersGeneralSteps[i].GetElapsedTime() * 1000000 / mStatNEvents);
+        general_step_row.gpu_time = mTimersGeneralSteps[i].GetElapsedTime();
+        general_step_row.statNEvents = mStatNEvents;
+        if (benchmarkCSV.is_open()) {
+          general_step_row.writeCSV(benchmarkCSV);
+        }
+        general_step_row.writeMarkdown(std::cout);
       }
     }
     Row wall_row;
     wall_row.name = "Wall";
     if (GetProcessingSettings().debugLevel >= 1) {
-      wall_row.kernel_time = kernelTotal;
-      mStatKernelTime = kernelTotal * 1000000 / mStatNEvents;
-      printf("Execution Time: Total   : %50s Time: %'10.0f us%s\n", "Total Kernel", mStatKernelTime, nEventReport.c_str());
+      wall_row.gpu_time = kernelTotal;
     }
     wall_row.cpu_time = mStatCPUTime;
-    wall_row.total_time = mStatWallTime * mStatNEvents / 1000000;
-    if (benchmarkCSV.is_open()) wall_row.write(benchmarkCSV, mStatNEvents);
-    printf("Execution Time: Total   : %50s Time: %'10.0f us ( CPU Time : %'10.0f us, %7.2fx ) %s\n", "Total Wall", mStatWallTime, mStatCPUTime * 1000000 / mStatNEvents, mStatCPUTime / mTimerTotal.GetElapsedTime(), nEventReport.c_str());
+    wall_row.total_time = mStatWallTime;
+    wall_row.statNEvents = mStatNEvents;
+    if (benchmarkCSV.is_open()) {
+      wall_row.writeCSV(benchmarkCSV);
+    }
+    wall_row.writeMarkdown(std::cout);
   } else if (GetProcessingSettings().debugLevel >= 0) {
-    GPUInfo("Total Wall Time: %10.0f us%s", mStatWallTime, nEventReport.c_str());
+    GPUInfo("Total Wall Time: %10.0f us%s", mStatWallTime * 1000000 / mStatNEvents, nEventReport.c_str());
   }
   if (GetProcessingSettings().resetTimers) {
     mStatNEvents = 0;
