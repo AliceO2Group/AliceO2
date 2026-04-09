88d69a0cdf6897bc5ed5073b5a10b08798bdefde
Justus Rudolph <justus.katte@gmail.com>
2026-04-09 13:59:09 +0200

    merge two if statements with the same if

--- Detectors/Upgrades/ALICE3/FT3/simulation/src/FT3Layer.cxx
+++ Detectors/Upgrades/ALICE3/FT3/simulation/src/FT3Layer.cxx
@@ -400,9 +400,6 @@ void FT3Layer::createLayer(TGeoVolume* motherVolume)
     if (ft3Params.layoutFT3 == kSegmented) {
       // createSeparationLayer_waterCooling(motherVolume, separationLayerName);
       createSeparationLayer(layerVol, separationLayerName);
-    }
-    // create disk faces
-    if (ft3Params.layoutFT3 == kSegmented) {
       module.createModule(0, mLayerNumber, mDirection, mInnerRadius, mOuterRadius, 0., "front", "rectangular", layerVol);
       module.createModule(0, mLayerNumber, mDirection, mInnerRadius, mOuterRadius, 0., "back", "rectangular", layerVol);
     } else if (ft3Params.layoutFT3 == kSegmentedStave) {
