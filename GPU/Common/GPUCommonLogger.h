b3c1e80f60f95bdc950eb061d4c54520b06e7bd4
David Rohr <drohr@jwdt.org>
2026-04-09 11:37:35 +0200

    GPU: Add workaround if fmt not available to GPUCommonLogger

--- GPU/Common/GPUCommonLogger.h
+++ GPU/Common/GPUCommonLogger.h
@@ -16,10 +16,13 @@
 #define GPUCOMMONFAIRLOGGER_H
 
 #include "GPUCommonDef.h"
+#ifndef GPUCA_GPUCODE_DEVICE
+#include <cstdio>
+#endif
 
-#if defined(GPUCA_GPUCODE_DEVICE)
-namespace o2::gpu::detail
+namespace o2::gpu::internal
 {
+#if defined(GPUCA_GPUCODE_DEVICE)
 struct DummyLogger {
   template <typename... Args>
   GPUd() DummyLogger& operator<<(Args... args)
@@ -27,16 +30,25 @@ struct DummyLogger {
     return *this;
   }
 };
-} // namespace o2::gpu::detail
+#else
+template <typename... Args>
+void LOGP_internal(const char* str, Args... args)
+{
+  printf("%s\n", str);
+}
 #endif
+} // namespace o2::gpu::internal
 
-#if defined(__OPENCL__) || (defined(GPUCA_GPUCODE_DEVICE) && !defined(GPUCA_GPU_DEBUG_PRINT))
-#define LOG(...) o2::gpu::detail::DummyLogger()
+#ifdef GPUCA_GPUCODE_DEVICE
+// ---------- begin GPUCA_GPUCODE_DEVICE ----------
+
+#if defined(__OPENCL__) || !defined(GPUCA_GPU_DEBUG_PRINT)
+#define LOG(...) o2::gpu::internal::DummyLogger()
 #define LOGF(...)
 #define LOGP(...)
 
-#elif defined(GPUCA_GPUCODE_DEVICE)
-#define LOG(...) o2::gpu::detail::DummyLogger()
+#else
+#define LOG(...) o2::gpu::internal::DummyLogger()
 // #define LOG(...) static_assert(false, "LOG(...) << ... unsupported in GPU code");
 #define LOGF(type, string, ...)         \
   {                                     \
@@ -44,23 +56,39 @@ struct DummyLogger {
   }
 #define LOGP(...)
 // #define LOGP(...) static_assert(false, "LOGP(...) unsupported in GPU code");
+#endif
 
+// ---------- end GPUCA_GPUCODE_DEVICE ----------
 #elif defined(GPUCA_STANDALONE) || defined(GPUCA_GPUCODE_COMPILEKERNELS) || defined(GPUCA_COMPILEKERNELS)
+// ---------- begin GPUCA_STANDALONE / COMPILEKERNELS ----------
+
 #include <iostream>
 #include <cstdio>
 #define LOG(type) std::cout
-#define LOGF(type, string, ...)         \
-  {                                     \
-    printf(string "\n", ##__VA_ARGS__); \
-  }
-#define LOGP(type, string, ...) \
-  {                             \
-    printf("%s\n", string);     \
-  }
+#define LOGF(type, string, ...) printf(string "\n", ##__VA_ARGS__);
+#if !defined(GPUCA_NO_FMT) && !defined(GPUCA_GPUCODE)
+#define LOGP(type, string, ...) fmt::print(string, ##__VA_ARGS__)
+#else
+#define LOGP(type, string, ...) o2::gpu::internal::LOGP_internal(string, ##__VA_ARGS__)
+#endif
+#if defined(GPUCA_STANDALONE) && !defined(GPUCA_GPUCODE)
+#if !defined(GPUCA_NO_FMT)
+#include <fmt/format.h>
+#else
+namespace fmt
+{
+template <typename... Args>
+static const char* format(Args... args)
+{
+  return "";
+}
+} // namespace fmt
+#endif
+#endif
 
+// ---------- end GPUCA_STANDALONE / COMPILEKERNELS ----------
 #else
 #include <Framework/Logger.h>
-
 #endif
 
 #endif
