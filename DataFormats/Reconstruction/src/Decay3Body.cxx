a5ff5d70f2438e61f9ed0c93c34342321cc2632f
shahoian <ruben.shahoyan@cern.ch>
2026-04-16 17:28:07 +0200

    Tentative improvement of 3-body decay cov. matrix creation

--- DataFormats/Reconstruction/src/Decay3Body.cxx
+++ DataFormats/Reconstruction/src/Decay3Body.cxx
@@ -16,15 +16,14 @@ using namespace o2::dataformats;
 Decay3Body::Decay3Body(const std::array<float, 3>& xyz, const std::array<float, 3>& pxyz, const std::array<float, 6>& covxyz, const Track& tr0, const Track& tr1, const Track& tr2, o2::track::PID pid)
   : mProngs{tr0, tr1, tr2}
 {
-  std::array<float, 21> cov{}, cov1{}, cov2{};
-  tr0.getCovXYZPxPyPzGlo(cov);
+  std::array<float, 21> cov{}, cov0{}, cov1{}, cov2{};
+  tr0.getCovXYZPxPyPzGlo(cov0);
   tr1.getCovXYZPxPyPzGlo(cov1);
   tr2.getCovXYZPxPyPzGlo(cov2);
-  for (int i = 0; i < 21; i++) {
-    cov[i] += cov1[i] + cov2[i];
-  }
+  constexpr int MomInd[6] = {9, 13, 14, 18, 19, 20}; // cov matrix elements for momentum component
   for (int i = 0; i < 6; i++) {
     cov[i] = covxyz[i];
+    cov[MomInd[i]] = cov0[MomInd[i]] + cov1[MomInd[i]] + cov2[MomInd[i]];
   }
   this->set(xyz, pxyz, cov, tr0.getCharge() + tr1.getCharge() + tr2.getCharge(), true, pid);
 }
