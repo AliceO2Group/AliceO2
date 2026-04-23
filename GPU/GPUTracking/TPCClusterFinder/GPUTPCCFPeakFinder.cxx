484d60b5d1a85f5ceaddcfb6cd98aa82d44f86ed
Felix Weiglhofer <felix.weiglhofer@cern.ch>
2026-04-23 16:36:16 +0200

    GPUTPCClusterFinder: Add protection against Out-Of-Bounds read in PeakFinder.

--- GPU/GPUTracking/TPCClusterFinder/GPUTPCCFPeakFinder.cxx
+++ GPU/GPUTracking/TPCClusterFinder/GPUTPCCFPeakFinder.cxx
@@ -108,7 +108,7 @@ GPUd() void GPUTPCCFPeakFinder::findPeaksImpl(int32_t nBlocks, int32_t nThreads,
   CfChargePos pos = positions[CAMath::Min<SizeT>(idx, digitnum - 1)];
   Charge charge = pos.valid() ? chargeMap[pos].unpack() : Charge(0);
 
-  bool hasLostBaseline = padHasLostBaseline[pos.gpad];
+  bool hasLostBaseline = pos.valid() ? padHasLostBaseline[pos.gpad] : true;
   charge = hasLostBaseline ? 0.f : charge;
 
   uint8_t peak = isPeak(smem, charge, pos, SCRATCH_PAD_SEARCH_N, chargeMap, calib, smem.posBcast, smem.buf);
