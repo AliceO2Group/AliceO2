14525c15a17b8a57e5b528b884945cd36e694e0c
fprino <francesco.prino@cern.ch>
2026-06-17 17:55:37 +0200

    add copyright info

--- Detectors/ITSMFT/ITS/macros/test/CheckITSTracksVsROF.C
+++ Detectors/ITSMFT/ITS/macros/test/CheckITSTracksVsROF.C
@@ -1,3 +1,15 @@
+// Copyright 2019-2026 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+
 /// \file CheckITSTracksVsROF.C
 /// \brief Simple macro to check ITS tracks in AO2D files as a function of bunch crossing within a ROF
 
