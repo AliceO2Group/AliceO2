8cb156d7c2442ab4650792be6c5e6e37577fa5e1
maciacco <mario.ciacco@cern.ch>
2026-07-31 15:20:56 +0200

    revert linkdef

--- Detectors/Upgrades/ALICE3/IOTOF/simulation/src/IOTOFSimulationLinkDef.h
+++ Detectors/Upgrades/ALICE3/IOTOF/simulation/src/IOTOFSimulationLinkDef.h
@@ -27,12 +27,12 @@
 #pragma link C++ class o2::iotof::DPLDigitizerParam + ;
 #pragma link C++ class o2::conf::ConfigurableParamHelper < o2::iotof::DPLDigitizerParam> + ;
 
-//#pragma link C++ class o2::iotof::ChipSpecifics + ;
-//#pragma link C++ class o2::iotof::ITOFChipSpecifics + ;
-//#pragma link C++ class o2::iotof::OTOFChipSpecifics + ;
-//#pragma link C++ class o2::iotof::ITOFChipSpecificParam + ;
-//#pragma link C++ class o2::conf::ConfigurableParamPromoter < o2::iotof::ITOFChipSpecificParam, o2::iotof::ITOFChipSpecifics> + ;
-//#pragma link C++ class o2::iotof::OTOFChipSpecificParam + ;
-//#pragma link C++ class o2::conf::ConfigurableParamPromoter < o2::iotof::OTOFChipSpecificParam, o2::iotof::OTOFChipSpecifics> + ;
+#pragma link C++ class o2::iotof::ChipSpecifics + ;
+#pragma link C++ class o2::iotof::ITOFChipSpecifics + ;
+#pragma link C++ class o2::iotof::OTOFChipSpecifics + ;
+#pragma link C++ class o2::iotof::ITOFChipSpecificParam + ;
+#pragma link C++ class o2::conf::ConfigurableParamPromoter < o2::iotof::ITOFChipSpecificParam, o2::iotof::ITOFChipSpecifics> + ;
+#pragma link C++ class o2::iotof::OTOFChipSpecificParam + ;
+#pragma link C++ class o2::conf::ConfigurableParamPromoter < o2::iotof::OTOFChipSpecificParam, o2::iotof::OTOFChipSpecifics> + ;
 
 #endif
