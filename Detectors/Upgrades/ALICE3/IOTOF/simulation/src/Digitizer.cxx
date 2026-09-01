a96d9bf6715a4b4377f2dccf6e4716fe4cb99f14
maciacco <mario.ciacco@cern.ch>
2026-09-01 17:24:01 +0200

    compute bc and tdc time from hit and event time

--- Detectors/Upgrades/ALICE3/IOTOF/simulation/src/Digitizer.cxx
+++ Detectors/Upgrades/ALICE3/IOTOF/simulation/src/Digitizer.cxx
@@ -396,6 +396,8 @@ void Digitizer::fillOutputContainer()
   // mExtraLabelBuffer.pop_front();
 }
 
+// have a addDigit funtion?
+
 void Digitizer::registerDigits(Chip& chip, uint32_t roFrame, double time, int nROF,
                                uint16_t row, uint16_t col, int nElectrons, o2::MCCompLabel& label)
 {
