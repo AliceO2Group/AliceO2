7a2af0b235f5517e72edb9250cbe731417d2102c
Christian Sonnabend <sonnabendch@gmail.com>
2026-09-14 10:14:51 +0200

    Adding copyright header

--- Common/ML/test/onnxruntime-inference/onnxruntime_ep_inference.cxx
+++ Common/ML/test/onnxruntime-inference/onnxruntime_ep_inference.cxx
@@ -1,3 +1,18 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file     onnxruntime_ep_inference.h
+/// \author   Christian Sonnabend <christian.sonnabend@cern.ch>
+/// \brief    A test script for inferencing an ONNX model with a specific execution provider
+
 #include <onnxruntime_cxx_api.h>
 
 #include <algorithm>
