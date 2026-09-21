02d994437a70934069193beac0645603f5a37232
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2026-09-21 11:01:45 +0200

    MathUtils: make SMatrixGPU compile as MSL
    Two things stop the GPU SMatrix port from being usable on Metal, neither of
    them to do with the matrix maths.
    
    MSL rejects variables declared static at function scope, which is how
    MatRepSymGPU::off() caches its offset table. Function-scope constexpr without
    static is accepted, so Metal uses that; every other backend keeps the static.
    
    The streaming operator is constrained with a C++20 requires-clause, already
    hidden from OpenCL because C++ for OpenCL 2021 is C++17. MSL 4.1 reports
    __cplusplus 201703L for the same reason, so it needs the same treatment. Left
    unhidden the declaration does not parse, and the failure cascades through the
    rest of the header.
    
    With these, SMatrixGPU<float, 5, 5, MatRepSymGPU<float, 5>> compiles as MSL and
    SMatrixGPU.h itself reports no diagnostics. Host, CUDA, HIP, OpenCL and cling
    are untouched.

--- Common/MathUtils/include/MathUtils/SMatrixGPU.h
+++ Common/MathUtils/include/MathUtils/SMatrixGPU.h
@@ -340,7 +340,11 @@ class MatRepSymGPU
 
   static GPUdi() int off(int i)
   {
+#ifdef __METAL__ // MSL rejects variables declared static at function scope
+    constexpr auto v = row_offsets_utils::make<D * D>(off1);
+#else
     static constexpr auto v = row_offsets_utils::make<D * D>(off1);
+#endif
     return v[i];
   }
 
@@ -518,7 +522,7 @@ class SMatrixGPU
   R mRep;
 };
 
-#ifndef __OPENCL__ // TODO: current C++ for OpenCL 2021 is at C++17, so no concepts. But we don't need this trick for OpenCL anyway, so we can just hide it.
+#if !defined(__OPENCL__) && !defined(__METAL__) // TODO: current C++ for OpenCL 2021 and MSL 4.1 are both at C++17, so no concepts. But we don't need this trick there anyway, so we can just hide it.
 template <class T, unsigned int D1, unsigned int D2, class R, typename Y, typename X = Y>
   requires(sizeof(typename X::traits_type::pos_type) != 0) // do not provide a template to fair::Logger, etc... (pos_type is a member type of all std::ostream classes)
 GPUd() X& operator<<(Y& y, const SMatrixGPU<T, D1, D2, R>&)
