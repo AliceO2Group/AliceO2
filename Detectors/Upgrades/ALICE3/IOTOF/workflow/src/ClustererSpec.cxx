f3be1eab9b140a2d091e3fceccd8f3a347be0f08
Marcello Di Costanzo <96481191+Marcellocosti@users.noreply.github.com>
2026-09-23 06:01:30 +0200

    [TF3] Implement cluster finder for multiple digits in same chip (#15748)
    * Implement cluster finder with spatial and timing constraints
    
    * Update QA macro
    
    * Before ITS topology implementation
    
    * compute bc and tdc time from hit and event time
    
    * save bc and tdc in digit
    
    * remove unused variable + remove unwanted comments
    
    * Please consider the following formatting changes
    
    * Restore IOTOF macro
    
    * Update cluster means computation
    
    * Please consider the following formatting changes
    
    * Clenup + sigma computation
    
    * Update IOTOF cluster finder
    
    * fix stepping for both passive and active pixel edges + revert change in central framework + add macro test in cmake list
    
    * remove empty lines
    
    * Fix cluster x COG computation + floating-point issue in stepping arithmetics
    
    * Please consider the following formatting changes
    
    * Fix copyright
    
    * Fix formatting
    
    ---------
    
    Co-authored-by: Marcello Di Costanzo <mdicosta@aliceml.cern.ch>
    Co-authored-by: maciacco <mario.ciacco@cern.ch>
    Co-authored-by: ALICE Action Bot <alibuild@cern.ch>

--- Detectors/Upgrades/ALICE3/IOTOF/workflow/src/ClustererSpec.cxx
+++ Detectors/Upgrades/ALICE3/IOTOF/workflow/src/ClustererSpec.cxx
@@ -67,15 +67,17 @@ void ClustererDPL::run(o2::framework::ProcessingContext& pc)
                      mUseMC ? &labels : nullptr,
                      clusterLabels.get());
   LOG(info) << "Clusterization produced " << clusters.size() << " clusters for layer " << iLayer;
+  LOG(info) << "Clusterization produced " << patterns.size() << " patterns for layer " << iLayer;
+  LOG(info) << "Clusterization produced " << clusterROFs.size() << " ROFs for layer " << iLayer;
   const auto subspec = static_cast<o2::framework::DataAllocator::SubSpecificationType>(iLayer);
-  pc.outputs().snapshot(o2::framework::Output{"TF3", "COMPCLUSTERS", subspec}, clusters);
+  pc.outputs().snapshot(o2::framework::Output{"TF3", "CLUSTERS", subspec}, clusters);
   pc.outputs().snapshot(o2::framework::Output{"TF3", "PATTERNS", subspec}, patterns);
   pc.outputs().snapshot(o2::framework::Output{"TF3", "CLUSTERSROF", subspec}, clusterROFs);
   if (mUseMC) {
     pc.outputs().snapshot(o2::framework::Output{"TF3", "CLUSTERSMCTR", subspec}, *clusterLabels);
   }
   totalClusters += clusters.size();
-  LOGP(info, "Pushed {} clusters in {} ROFs for layer {}", clusters.size(), clusterROFs.size(), iLayer);
+  LOGP(info, "Pushed {} clusters, {} patterns, in {} ROFs for layer {}", clusters.size(), patterns.size(), clusterROFs.size(), iLayer);
   LOGP(info, "Pushed {} MC labels for layer {}", mUseMC ? clusterLabels->getNElements() : 0, iLayer);
 }
 
@@ -92,7 +94,7 @@ o2::framework::DataProcessorSpec getClustererSpec(bool useMC)
   }
 
   std::vector<o2::framework::OutputSpec> outputs;
-  outputs.emplace_back("TF3", "COMPCLUSTERS", iLayer, o2::framework::Lifetime::Timeframe);
+  outputs.emplace_back("TF3", "CLUSTERS", iLayer, o2::framework::Lifetime::Timeframe);
   outputs.emplace_back("TF3", "PATTERNS", iLayer, o2::framework::Lifetime::Timeframe);
   outputs.emplace_back("TF3", "CLUSTERSROF", iLayer, o2::framework::Lifetime::Timeframe);
   if (useMC) {
