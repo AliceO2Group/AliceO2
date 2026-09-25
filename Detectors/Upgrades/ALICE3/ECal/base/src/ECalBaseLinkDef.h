341b3156c06b8a47f68c13390e54033cc6d67b66
Sandro Wenzel <sandro.wenzel@cern.ch>
2026-09-25 09:00:29 +0200

    Terminate the LinkDef rules that rootcling rejected
    This fixes the rootcling "missing ; at end of rule" errors in four LinkDef files.
    
    - Three rules in TPCBaseLinkDef.h and the GeometryTGeo rules of ALICE3 ECal, RICH and MID lacked the closing semicolon.
    - rootcling rejected the three ALICE3 rules, so their GeometryTGeo classes had no dictionary.
    - The TPC dictionary is unchanged by the fix.
    
    https://ali-ci.cern.ch/alice-build-logs/AliceO2Group/AliceO2/15843/ee5503a4c432ac109fab4860dde02f104c65e5a6/build_O2_gpu-test-slc10-x86/pretty.html

--- Detectors/Upgrades/ALICE3/ECal/base/src/ECalBaseLinkDef.h
+++ Detectors/Upgrades/ALICE3/ECal/base/src/ECalBaseLinkDef.h
@@ -16,7 +16,7 @@
 #pragma link off all functions;
 
 #pragma link C++ class o2::ecal::Geometry + ;
-#pragma link C++ class o2::ecal::GeometryTGeo +
+#pragma link C++ class o2::ecal::GeometryTGeo + ;
 #pragma link C++ class o2::ecal::ECalBaseParam + ;
 #pragma link C++ class o2::ecal::Hit + ;
 #pragma link C++ class o2::conf::ConfigurableParamHelper < o2::ecal::ECalBaseParam> + ;
